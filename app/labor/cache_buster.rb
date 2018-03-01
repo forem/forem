430e668d7fc7abdecdbe33d351ff84e098ad2463
Ben Halpern <bendhalpern@gmail.com>
2018-03-01 17:26:47 -0500

    Quick fix to internal navigation draft caching issue (#17)
    * Add Algolia to search and add Only My Posts filter
    
    * Actually fix Algolia index possible issue
    
    * Fix search issue
    
    * Make minor adjustments to sponsorship sidebar
    
    * Make submission rules headsup html allowed and remove devise trackable
    
    * Remove devise_trackable from application_controller
    
    * Adjust login CTA for /new
    
    * Adjust string in test to reflect changes
    
    * Quick fix for internal navigatioon draft caching issue

--- app/labor/cache_buster.rb
+++ app/labor/cache_buster.rb
@@ -40,6 +40,7 @@ class CacheBuster
     bust(article.path + "/?i=i")
     bust(article.path + "/comments")
     bust(article.path + "?preview=" + article.password)
+    bust(article.path + "?preview=" + article.password + "?i=i")
     if article.organization.present?
       bust("/#{article.organization.slug}")
     end
