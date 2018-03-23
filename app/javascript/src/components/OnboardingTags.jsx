67fb7ada4c8a3c75482428efbb97a2b6f38ae9c7
Ben Halpern <bendhalpern@gmail.com>
2018-03-22 20:02:07 -0400

    Adjust what shows up in onboarding (#127)

--- app/javascript/src/components/OnboardingTags.jsx
+++ app/javascript/src/components/OnboardingTags.jsx
@@ -19,12 +19,7 @@ class OnboardingTags extends Component {
           <strong class="yellow">What topics are you interested in?</strong> Here are a few tags to follow (There are plenty more you can choose from at any time)
         </p>
         <div className="tags-col-container">
-          <div className="col-1">
-            {tags.slice(0, 6)}
-          </div>
-          <div className="col-2">
-            {tags.slice(6)}
-          </div>
+            {tags}
         </div>
       </div>
     );
