dd207096e209d03ed903bd187231f286ed6cbb31
Ben Halpern <bendhalpern@gmail.com>
2018-03-27 16:18:29 -0400

    Update email logic to not get own articles (#153)
    * Add where published true clause in email logic
    
    * Update email logic to not get own articles
    
    * Customize email digest subject

--- app/mailers/digest_mailer.rb
+++ app/mailers/digest_mailer.rb
@@ -7,8 +7,12 @@ class DigestMailer < ApplicationMailer
             end
     @articles = articles.first(6)
     @digest_email = true
-    mail(from: "yo@dev.to", to: @user.email, subject: "Emai!") do |format|
+    mail(from: "yo@dev.to", to: @user.email, subject: "#{@articles.first.title} #{random_emoji} and #{@articles.size - 1} other top posts from your network.") do |format|
       format.html { render "layouts/mailer" }
     end
+  end
+
+  def random_emoji
+    ["📖","🎉","🙈","🔥","💬","👋","👏","🐶","🦁","🦊","🐙","🦄","❤️"].shuffle.take(3).join
   end
 end
