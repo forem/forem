3f403437c485d14e185fba6904b22ef9321831ba
Ben Halpern <bendhalpern@gmail.com>
2018-03-28 16:19:01 -0400

    Modify dev digest email copy/style (#157)

--- app/mailers/digest_mailer.rb
+++ app/mailers/digest_mailer.rb
@@ -7,12 +7,23 @@ class DigestMailer < ApplicationMailer
             end
     @articles = articles.first(6)
     @digest_email = true
-    mail(from: "yo@dev.to", to: @user.email, subject: "#{@articles.first.title} #{random_emoji} and #{@articles.size - 1} other top posts from your network.") do |format|
+    mail(from: "yo@dev.to", to: @user.email, subject: "#{@articles.first.title} #{random_emoji} and #{@articles.size - 1} #{email_end_phrase}") do |format|
       format.html { render "layouts/mailer" }
     end
   end
 
   def random_emoji
     ["📖","🎉","🙈","🔥","💬","👋","👏","🐶","🦁","🦊","🐙","🦄","❤️"].shuffle.take(3).join
+  end
+
+  def email_end_phrase
+    [
+      "more posts picked just for you",
+      "more trending DEV posts",
+      "other posts you might like",
+      "other posts you should read",
+      "other articles picked for you",
+      "more wonderful posts"
+    ].sample
   end
 end
