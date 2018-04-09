d67c2437bc83723fb9a855092e9417605da62d43
Nick Taylor <nickytonline@users.noreply.github.com>
2018-04-09 17:05:18 -0400

    Dx/add prettier code (#188)
    * Added prettier for front-end code formatting.
    
    * Prettier runs on JSX too now
    
    Modified supported extensions for running prettier.
    
    * Added some documentation in the README about prettier, husky and lint-staged.
    
    * package-lock.json updated.
    
    * Keep lint-staged package-manager agnostic (#196)
    
    * Remove code-breaking comma

--- app/javascript/.eslintrc.js
+++ app/javascript/.eslintrc.js
@@ -1,12 +1,24 @@
 module.exports = {
-  "extends": "airbnb",
-  "settings": {
-    "react": {
-      "pragma": "h"
+  extends: ['airbnb', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 6,
+  },
+  settings: {
+    react: {
+      pragma: 'h',
     },
   },
-  "env": {
-    "jest": true,
-    "browser": true,
+  env: {
+    jest: true,
+    browser: true,
+  },
+  plugins: ['import'],
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/*.test.js', '**/*.test.jsx', '**/*.stories.jsx'],
+      },
+    ],
   },
 };
