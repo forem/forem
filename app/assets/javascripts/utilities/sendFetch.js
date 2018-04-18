18f0003187b7e6a2ae00b9159225df346433fae7
Mac Siri <krairit.siri@gmail.com>
2018-04-17 20:32:45 -0400

    Use JSON instead of FormData for comment-creation (#216)
    * Use JSON instead of FormData for comment-creation
    
    * Fix lint

--- app/assets/javascripts/utilities/sendFetch.js
+++ app/assets/javascripts/utilities/sendFetch.js
@@ -1,64 +1,64 @@
 function sendFetch(switchStatement, body) {
   switch (switchStatement) {
     case 'article-preview':
-      return function (csrfToken) {
+      return function(csrfToken) {
         return window.fetch('/articles/preview', {
           method: 'POST',
           headers: {
             'X-CSRF-Token': csrfToken,
             Accept: 'application/json',
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
           },
           body: body,
-          credentials: 'same-origin'
+          credentials: 'same-origin',
         });
       };
     case 'reaction-creation':
-      return function (csrfToken) {
+      return function(csrfToken) {
         body.append('authenticity_token', csrfToken);
         return window.fetch('/reactions', {
           method: 'POST',
           headers: {
-            'X-CSRF-Token': csrfToken
+            'X-CSRF-Token': csrfToken,
           },
           body: body,
-          credentials: 'same-origin'
+          credentials: 'same-origin',
         });
       };
     case 'image-upload':
-      return function (csrfToken) {
+      return function(csrfToken) {
         body.append('authenticity_token', csrfToken);
         return window.fetch('/image_uploads', {
           method: 'POST',
           headers: {
-            'X-CSRF-Token': csrfToken
+            'X-CSRF-Token': csrfToken,
           },
           body: body,
-          credentials: 'same-origin'
+          credentials: 'same-origin',
         });
       };
     case 'follow-creation':
-      return function (csrfToken) {
+      return function(csrfToken) {
         body.append('authenticity_token', csrfToken);
         return window.fetch('/follows', {
           method: 'POST',
           headers: {
-            'X-CSRF-Token': csrfToken
+            'X-CSRF-Token': csrfToken,
           },
           body: body,
-          credentials: 'same-origin'
+          credentials: 'same-origin',
         });
       };
     case 'comment-creation':
-      return function (csrfToken) {
-        // body is a formdata here
-        return window.fetch(body.action, {
+      return function(csrfToken) {
+        return window.fetch('/comments', {
           method: 'POST',
           headers: {
+            'Content-Type': 'application/json',
             'X-CSRF-Token': csrfToken,
           },
-          body: new FormData(body),
-          credentials: 'same-origin'
+          body: body,
+          credentials: 'same-origin',
         });
       };
     default:
