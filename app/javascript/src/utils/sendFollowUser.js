f75941b8c911bc9f954dcab71ead2cdb81152469
Zhao-Andy <andyzhao.zhao@gmail.com>
2018-04-25 13:54:58 -0400

    Prettier linted stuff

--- app/javascript/src/utils/sendFollowUser.js
+++ app/javascript/src/utils/sendFollowUser.js
@@ -1,25 +1,26 @@
 export default function sendFollowUser(user, successCb, failureCb) {
-    const csrfToken = document.querySelector("meta[name='csrf-token']").content;
+  const csrfToken = document.querySelector("meta[name='csrf-token']").content;
 
-    const formData = new FormData();
-    formData.append('followable_type', 'User');
-    formData.append('followable_id', user.id);
-    formData.append('verb', (user.following ? 'unfollow' : 'follow'));
+  const formData = new FormData();
+  formData.append('followable_type', 'User');
+  formData.append('followable_id', user.id);
+  formData.append('verb', user.following ? 'unfollow' : 'follow');
 
-    fetch('/follows', {
-      method: 'POST',
-      headers: {
-        'X-CSRF-Token': csrfToken,
-      },
-      body: formData,
-      credentials: 'same-origin',
-    }).then((response) => {
-      return response.json();
-    }).then((json) => {
-      console.log('in fetch request: ' + json.outcome)
+  fetch('/follows', {
+    method: 'POST',
+    headers: {
+      'X-CSRF-Token': csrfToken,
+    },
+    body: formData,
+    credentials: 'same-origin',
+  })
+    .then(response => response.json())
+    .then(json => {
+      console.log(`in fetch request: ${  json.outcome}`);
       successCb(json.outcome);
       // json is followed or unfollowed
-    }).catch((error) => {
+    })
+    .catch(error => {
       console.log(error);
     });
 }
