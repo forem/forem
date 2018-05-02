808ea3a466929d249641bd3291ceaedbe4210e33
Ben Halpern <bendhalpern@gmail.com>
2018-05-02 14:14:49 -0400

    Add boost states in for real (#270)
    * Add boost states in for real
    
    * Fix schema issue

--- db/schema.rb
+++ db/schema.rb
@@ -8,9 +8,9 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended that you check this file into your version control system. ❤️
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180502160428) do
+ActiveRecord::Schema.define(version: 20180502174301) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
