2cff66c0f84620f6aeb8a7b408fdfada65602288
Ben Halpern <bendhalpern@gmail.com>
2018-05-21 18:38:23 -0400

    Modify badge page styling and add caching (#336)

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,6 +1,7 @@
 class BadgesController < ApplicationController
-
+  before_action :set_cache_control_headers, only: [:show]
   def show
     @badge = Badge.find_by_slug(params[:slug])
+    set_surrogate_key_header "badges-show-action"
   end
 end
