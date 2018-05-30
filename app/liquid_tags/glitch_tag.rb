3d8f1229ede302a465c735e1baedcef6e60fe41d
Ben Halpern <bendhalpern@gmail.com>
2018-05-30 09:57:14 -0400

    Modify glitch iframe to disallow popups (#358)
    * Take darker of user colors
    
    * Fix Github Readme tag and add Glitch tag
    
    * Modify glitch iframe to disallow popups

--- app/liquid_tags/glitch_tag.rb
+++ app/liquid_tags/glitch_tag.rb
@@ -8,6 +8,7 @@ class GlitchTag < LiquidTagBase
     html = <<-HTML
       <div class="glitch-embed-wrap" style="height: 450px; width: 100%;margin: 1em auto 1.3em">
         <iframe
+          sandbox="allow-same-origin allow-scripts allow-forms"
           src="https://glitch.com/embed/#!/embed/#{@id}?path=index.html"
           alt="#{@id} on glitch"
           style="height: 100%; width: 100%; border: 0;margin:0;padding:0"></iframe>
