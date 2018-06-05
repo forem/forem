97b5ae810c62960e8e132118f52ff85942aad2e6
Ben Halpern <bendhalpern@gmail.com>
2018-06-05 12:54:05 -0400

    Fix badge show title size and badge admin (#391)

--- app/dashboards/badge_dashboard.rb
+++ app/dashboards/badge_dashboard.rb
@@ -24,9 +24,11 @@ class BadgeDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :users,
+    :badge_image,
     :id,
     :title,
+    :slug,
+    :description,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
