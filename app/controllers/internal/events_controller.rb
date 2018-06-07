a889795fb85eba81f2e07b59d4956724eea7f717
Andy Zhao <andyzhao.zhao@gmail.com>
2018-06-07 15:35:09 -0400

    [Done] Fix update and create events (#404)
    * Close out a div properly
    
    * Rename file to .html.erb
    
    * Move new event form back to index page
    
    * Update test to use index view

--- app/controllers/internal/events_controller.rb
+++ app/controllers/internal/events_controller.rb
@@ -2,36 +2,35 @@ module Internal
   class EventsController < ApplicationController
     layout "internal"
     def index
-      @events = Event.order("starts_at ASC")
-    end
-
-    def new
       @event = Event.new(
         location_name: "dev.to/live",
         location_url: "https://dev.to",
-        description_markdown: "*Description* *Pre-requisites:* *Bio*"
+        description_markdown: "*Description* *Pre-requisites:* *Bio*",
       )
+      @events = Event.order("starts_at ASC")
     end
 
     def create
       @event = Event.new(event_params)
-      if @event.save!
+      @events = Event.order("starts_at ASC")
+      if @event.save
+        flash[:success] = "Successfully created event: #{@event.title}"
         redirect_to(action: :index)
       else
         flash[:danger] = @event.errors.full_messages
-        render "new.html.erb"
+        render "index.html.erb"
       end
     end
 
     def update
       @event = Event.find(params[:id])
+      @events = Event.order("starts_at ASC")
       if @event.update(event_params)
         CacheBuster.new.bust "/live_articles"
         flash[:success] = "#{@event.title} was successfully updated"
         redirect_to "/internal/events"
       else
         flash[:danger] = @event.errors.full_messages
-        @events = Event.order("starts_at ASC")
         render "index.html.erb"
       end
     end
