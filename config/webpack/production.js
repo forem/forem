8334c6908f35f3b53817c85fc74b224fe94e9d90
Mac Siri <krairit.siri@gmail.com>
2018-06-11 11:54:38 -0400

    Remove precompilation related codes (#414)

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,3 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+const environment = require('./environment');
 
-const environment = require('./environment')
-
-module.exports = environment.toWebpackConfig()
+module.exports = environment.toWebpackConfig();
--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,5 +1,3 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+const environment = require('./environment');
 
-const environment = require('./environment')
-
-module.exports = environment.toWebpackConfig()
+module.exports = environment.toWebpackConfig();
