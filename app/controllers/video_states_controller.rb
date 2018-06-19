44a96ed93688214f6d9b3b28c9758d4c8394e86d
Mac Siri <krairit.siri@gmail.com>
2018-06-19 16:27:15 -0400

    Clean up spec outputs (#450)
    * Improve BadgeAchievementSpec
    
    * Create specs for PushNotificationSubscription
    
    * Clean-up specs for VideoStatesController
    
    * Refactor spec

--- app/controllers/video_states_controller.rb
+++ app/controllers/video_states_controller.rb
@@ -1,23 +1,21 @@
 class VideoStatesController < ApplicationController
   skip_before_action :verify_authenticity_token
+
   def create
     unless valid_user
-      render json: { message: "invalid_user" }, :status => 422
+      render json: { message: "invalid_user" }, status: 422
       return
     end
     begin
       logger.info "VIDEO STATES: #{params}"
-      request_json = JSON.parse(request.raw_post, {symbolize_names: true})
+      request_json = JSON.parse(request.raw_post, symbolize_names: true)
       logger.info "VIDEO STATES: #{request_json}"
-    rescue
+    rescue StandardError
     end
-    request_json = JSON.parse(request.raw_post, {symbolize_names: true})
-    puts request_json
-    message_json = JSON.parse(request_json[:Message], {symbolize_names: true})
-    puts message_json
-    puts "***"
+    request_json = JSON.parse(request.raw_post, symbolize_names: true)
+    message_json = JSON.parse(request_json[:Message], symbolize_names: true)
     @article = Article.find_by_video_code(message_json[:input][:key])
-    @article.update(video_state: "COMPLETED") #Only is called on completion
+    @article.update(video_state: "COMPLETED") # Only is called on completion
     NotifyMailer.video_upload_complete_email(@article).deliver
     render json: { message: "Video state updated" }
   end
@@ -27,5 +25,4 @@ class VideoStatesController < ApplicationController
     user = nil if !user.has_role?(:super_admin)
     user
   end
-
 end
--- spec/requests/video_states_update_spec.rb
+++ spec/requests/video_states_update_spec.rb
@@ -2,21 +2,21 @@
 require "rails_helper"
 
 RSpec.describe "VideoStatesUpdate", type: :request do
-  before do
-    @user = FactoryBot.create(:user)
-    @user.update(secret:"TEST_SECRET")
-    @user.add_role(:super_admin)
-    @article = FactoryBot.create(:article, video_code: "DUMMY_VID_CODE")
-  end
+  let(:authorized_user) { create(:user, :super_admin, secret: "TEST_SECRET") }
+  let(:regular_user) { create(:user, secret: "TEST_SECRET") }
+  let(:article) { create(:article, video_code: "DUMMY_VID_CODE") }
+
   describe "POST /video_states" do
     it "updates video state" do
-      post "/video_states?key=#{@user.secret}", params: {
-        Message: "{\"input\":{\"key\":\"DUMMY_VID_CODE\"}}"}.to_json
+      input = JSON.unparse(input: { key: article.video_code })
+      post "/video_states?key=#{authorized_user.secret}",
+        params: { Message: input }.to_json
       expect(Article.last.video_state).to eq("COMPLETED")
     end
+
     it "rejects non-authorized users" do
-      @user.remove_role(:super_admin)
-      post "/video_states?key=#{@user.secret}", params: {input: {key: Article.last.video_code}}
+      post "/video_states?key=#{regular_user.secret}",
+        params: { input: { key: article.video_code } }
       expect(response).to have_http_status(422)
     end
   end
