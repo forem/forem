dc56f8be51b0309c919bd3c25051c894ae7ce00a
Ben Halpern <bendhalpern@gmail.com>
2018-06-21 11:06:37 -0400

    Fix canonical url on tag page (#464)
    * Fix canonical url on tag page
    
    * Fix canonical url on tag page

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "The DEV Community <yo@dev.to>"
+  default from: "DEV Community <yo@dev.to>"
   layout "mailer"
   default template_path: ->(mailer) { "mailers/#{mailer.class.name.underscore}" }
 
