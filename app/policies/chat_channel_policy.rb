814490e0699a3c001c922370a5f2a3cc6289153d
Ben Halpern <bendhalpern@gmail.com>
2018-06-21 16:58:21 -0400

    Add chat channel policy and spec (#474)

--- app/policies/chat_channel_policy.rb
+++ app/policies/chat_channel_policy.rb
@@ -1,5 +1,28 @@
 class ChatChannelPolicy < ApplicationPolicy
+
+  def index?
+    user
+  end
+
   def moderate?
     !user_is_banned? && user_is_admin?
+  end
+
+  def show?
+    user_part_of_channel
+  end
+
+  def open?
+    user_part_of_channel
+  end
+
+  private
+
+  def user_part_of_channel_or_open
+    record.present? && (record.channel_type == "open" || record.has_member?(user))
+  end
+
+  def user_part_of_channel
+    record.present? && record.has_member?(user)
   end
 end
---
+++ spec/policies/chat_channel_policy_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe ChatChannelPolicy do
+  subject { described_class.new(user, chat_channel) }
+
+  let(:chat_channel) { create(:chat_channel) }
+
+  context "when user is not signed-in" do
+    let(:user) { nil }
+
+    it { within_block_is_expected.to raise_error(Pundit::NotAuthorizedError) }
+  end
+
+  context "when user is not a part of channel" do
+    let(:user) { build(:user) }
+    it { is_expected.to permit_actions(%i[index]) }
+    it { is_expected.to forbid_actions(%i[show open moderate]) }
+  end
+
+  context "when user is a part of channel" do
+    let(:user) { create(:user) }
+    before { chat_channel.add_users [user] }
+    it { is_expected.to permit_actions(%i[index show open]) }
+    it { is_expected.to forbid_actions(%i[moderate]) }
+  end
+
+  context "when user is an admin but not part of channel" do
+    let(:user) { create(:user) }
+    before { user.add_role(:super_admin) }
+    it { is_expected.to permit_actions(%i[index moderate]) }
+    it { is_expected.to forbid_actions(%i[show open]) }
+  end
+end
