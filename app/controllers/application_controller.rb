aa555c0d244b0acd759d302f2bd363e45ab6aa37
Ben Halpern <bendhalpern@gmail.com>
2018-06-22 13:38:19 -0400

    Modify valid_request_origin? (#483)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -60,8 +60,13 @@ class ApplicationController < ActionController::Base
     # Not sure why, but once we work it out, we can delete this method.
     # We are at least secure for now.
     return if Rails.env.test?
-    logger.info "**REQUEST ORIGIN CHECK** #{request.referer}"
-    request.referer.start_with?(ENV["APP_PROTOCOL"].to_s + ENV["APP_DOMAIN"].to_s)
+    if request.referer.present?
+      request.referer.start_with?(ENV["APP_PROTOCOL"].to_s + ENV["APP_DOMAIN"].to_s)
+    else
+      logger.info "**REQUEST ORIGIN CHECK** #{request.origin}"
+      raise InvalidAuthenticityToken, NULL_ORIGIN_MESSAGE if request.origin == "null"
+      request.origin.nil? || request.origin.gsub("https", "http") == request.base_url.gsub("https", "http")
+    end
   end
 
   def set_no_cache_header
