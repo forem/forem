799aa16378e1222198a6422c675ec6dde855dcee
Ben Halpern <bendhalpern@gmail.com>
2018-06-28 14:28:24 -0400

    Fix /live for workshop (#516)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -49,7 +49,7 @@ class PagesController < ApplicationController
   def live
     @active_channel = ChatChannel.find_by_channel_name("Workshop")
     @chat_channels = [@active_channel].to_json(
-      only: [:channel_name,:channel_type,:last_message_at,:slug, :status])
+      only: [:channel_name,:channel_type,:last_message_at,:slug, :status, :id])
   end
 
   private # helpers
