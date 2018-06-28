4361170c4aabdeaf70d65d6a646c794b2a07a9ce
Ben Halpern <bendhalpern@gmail.com>
2018-06-27 21:25:25 -0400

    Add filters (#510)

--- app/javascript/chat/actions.js
+++ app/javascript/chat/actions.js
@@ -75,11 +75,7 @@ export function conductModeration(
 export function getChannels(query,retrievalID, props, paginationNumber, additionalFilters, successCb, failureCb) {
   const client = algoliasearch(props.algoliaId, props.algoliaKey);
   const index = client.initIndex(props.algoliaIndex);
-<<<<<<< HEAD
   let filters = {...{
-=======
-  index.search(query,{
->>>>>>> b8d2c0033e7046bb5e13d456f89a2a6f328c209a
     hitsPerPage: 30 + paginationNumber,
     page: paginationNumber
   }, ...additionalFilters};
