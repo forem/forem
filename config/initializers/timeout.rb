b45dd391b63fda111ef9babd93ccbf7a1919e924
Andy Zhao <andyzhao.zhao@gmail.com>
2018-07-03 13:09:11 -0400

    Disable rack timeout logs (#530)

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,3 +1,4 @@
 Rack::Timeout.service_timeout = ENV["SERVICE_TIMEOUT"].to_i # seconds
 Rack::Timeout.wait_timeout = 5
 Rack::Timeout.unregister_state_change_observer(:logger) if Rails.env.development?
+Rack::Timeout::Logger.disable
