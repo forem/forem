27d1c14fdb9edfcabff990327f9fb9b5f7bc3369
Nick Taylor <nick@iamdeveloper.com>
2018-07-03 10:11:11 -0400

    Nick/task/dx/vscode chrome debugger (#538)
    * Now you can attach to debug the front-end with VS Code.
    
    * Added Chrome Debugger extension to suggested extensions.

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,6 +2,6 @@ const environment = require('./environment');
 const config = environment.toWebpackConfig();
 
 // For more information, see https://webpack.js.org/configuration/devtool/#devtool
-config.devtool = 'cheap-module-eval-source-map';
+config.devtool = 'eval-source-map';
 
 module.exports = config;
