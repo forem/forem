42e4ae9e83ad42482ff078d6438a4493994bfc9d
Zhao-Andy <andyzhao.zhao@gmail.com>
2018-07-13 10:56:41 -0400

    Fix error handling when submitting blank note

--- app/controllers/internal/feedback_messages_controller.rb
+++ app/controllers/internal/feedback_messages_controller.rb
@@ -15,14 +15,18 @@ class Internal::FeedbackMessagesController < Internal::ApplicationController
     @feedback_message.reviewer_id = feedback_message_params[:reviewer_id]
     note = @feedback_message.find_or_create_note(@feedback_message.feedback_type)
     note.content = feedback_message_params[:note][:content]
-    if @feedback_message.save! && note.save!
+    if @feedback_message.save && note.save
       @feedback_message.touch(:last_reviewed_at)
       flash[:success] = "Report ##{@feedback_message.id} saved. Remember to send emails!"
       redirect_to "/internal/reports?state=#{@feedback_message.feedback_type}"
     else
-      @feedback_messages = FeedbackMessage.where(feedback_type: @feedback_type)
-      flash[:error] = @feedback_message.errors.full_messages
-      render "index.html.erb", state: @feedback_message.feedback_type
+      @feedback_type = @feedback_message.feedback_type
+      @feedback_messages = FeedbackMessage.
+        where(feedback_type: @feedback_type).
+        order("created_at DESC").
+        page(params[:page] || 1).per(25)
+      flash[:danger] = "Something went wrong. Did you leave a blank note?"
+      render "index.html.erb", state: @feedback_type
     end
   end
 
@@ -31,7 +35,11 @@ class Internal::FeedbackMessagesController < Internal::ApplicationController
   def feedback_message_params
     params[:feedback_message].permit(
       :status, :reviewer_id,
+<<<<<<< HEAD
       note: %i[content reason]
+=======
+      note: %i(content reason)
+>>>>>>> Fix error handling when submitting blank note
     )
   end
 end
