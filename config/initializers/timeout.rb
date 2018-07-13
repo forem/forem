6bf23854a1fc367d9e2d112a7a80bd4e7f64cacd
Yechiel Kalmenson <contact@yechiel.me>
2018-07-13 16:44:12 -0400

    Update deprecated methods in rack-timeout gem (#564)
    * update depracated methods in rack-timeout gem
    
    * bump the version of the rack-timeout gem

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,4 +1,5 @@
-Rack::Timeout.service_timeout = ENV["SERVICE_TIMEOUT"].to_i # seconds
-Rack::Timeout.wait_timeout = 5
+Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, wait_timeout: 5, service_timeout: ENV["SERVICE_TIMEOUT"].to_i # seconds
+
 Rack::Timeout.unregister_state_change_observer(:logger) if Rails.env.development?
 Rack::Timeout::Logger.disable
+
