ce965bcedf63f31dd1642879234fc35e893c71d1
Mac Siri <krairit.siri@gmail.com>
2018-07-27 12:04:52 -0400

    Reduce Airbrake's error output (#628)
    * Remove chmod for after_deploy
    
    * Fix lint
    
    * Ignore Pundit::NotAuthorizedError
    
    * Update Travis config
    
    * Turn off Travis' pr notification
    
    * Add CommonsChunkPlugin to Webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -10,5 +10,25 @@
 
 
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+environment.plugins.append(
+  'CommonsChunkVendor',
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    minChunks: (module) => {
+      // this assumes your vendor imports exist in the node_modules directory
+      return module.context && module.context.indexOf('node_modules') !== -1
+    }
+  })
+)
+
+environment.plugins.append(
+  'CommonsChunkManifest',
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'manifest',
+    minChunks: Infinity
+  })
+)
 
 module.exports = environment
