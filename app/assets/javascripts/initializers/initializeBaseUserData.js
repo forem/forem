83d5ea8ed7935c5b4d1d93ba427c16fc8960c7f3
Andy Zhao <andyzhao.zhao@gmail.com>
2018-07-30 10:55:45 -0400

    Filter out user input name (#633)

--- app/assets/javascripts/initializers/initializeBaseUserData.js
+++ app/assets/javascripts/initializers/initializeBaseUserData.js
@@ -34,8 +34,8 @@ function initializeUserSidebar(user) {
 
 function initializeUserProfileContent(user) {
   document.getElementById("sidebar-profile-pic").innerHTML = '<img class="sidebar-profile-pic-img" src="'+user.profile_image_90+'" />'
-  document.getElementById("sidebar-profile-name").innerHTML =  user.name
-  document.getElementById("sidebar-profile-username").innerHTML = '@'+user.username
+  document.getElementById("sidebar-profile-name").innerHTML =  filterXSS(user.name);
+  document.getElementById("sidebar-profile-username").innerHTML = '@'+user.username;
   document.getElementById("sidebar-profile-snapshot-inner").href = "/"+user.username;
 }
 
