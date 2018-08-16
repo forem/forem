f7fda6b1ede019df925ee14de7608b10322061f6
Kohei Sugi <kohei.sg@gmail.com>
2018-08-16 22:35:17 +0900

    Set database cleaner strategy truncation

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -3,10 +3,6 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.before do
-    DatabaseCleaner.strategy = :transaction
-  end
-
   config.before(:each, js: true) do
     DatabaseCleaner.strategy = :truncation
   end
