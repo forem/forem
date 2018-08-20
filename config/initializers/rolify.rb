5b0183a54283fb4b3e140fe26bfc23574cf6c0ef
Monica Powell <mmp2181@columbia.edu>
2018-08-20 10:20:23 -0400

    #260 increase mentee-mentor description length (#405)
    * increase mentee-mentor description length and fix linting errors
    
    * fix char limit issue and refactor tests

--- config/initializers/rolify.rb
+++ config/initializers/rolify.rb
@@ -2,6 +2,6 @@ Rolify.configure do |config|
   # By default ORM adapter is ActiveRecord. uncomment to use mongoid
   # config.use_mongoid
 
-  # Dynamic shortcuts for User class (user.is_admin? like methods). Default is: false
+  # Dynamic shortcuts for User class (user.admin? like methods). Default is: false
   # config.use_dynamic_shortcuts
 end
