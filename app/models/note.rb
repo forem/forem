1f99779545b524379bb5806377f83007f89311eb
Jess Lee <jessleenyc@gmail.com>
2018-08-22 10:38:42 -0400

    remove uniqueness validation

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,4 @@
 class Note < ApplicationRecord
   belongs_to :noteable, polymorphic: true
   validates :reason, :content, presence: true
-  validates :noteable_id, uniqueness:
-    { scope: :reason, message: "limited to one note per noteable per reason" }
 end
