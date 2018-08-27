378f8b3d9d4dcf6287a5894c5e33a41d35e9fad9
Zhao-Andy <andyzhao.zhao@gmail.com>
2018-08-27 12:49:08 -0400

    Update copy of report abuse responses

--- app/helpers/feedback_messages_helper.rb
+++ app/helpers/feedback_messages_helper.rb
@@ -3,7 +3,7 @@ module FeedbackMessagesHelper
     body = <<~HEREDOC
       Hi [*USERNAME*],
 
-      All dev.to members are expected to help foster a welcoming environment for the community and abide by our terms and conditions of use. It's been brought to our attention that you may have violated our code of conduct.   If this behavior continues, we will need to ban your posting privileges on dev.to.
+      All dev.to members are expected to help foster a welcoming environment for the community and abide by our terms and conditions of use. It's been brought to our attention that you may have violated our code of conduct.  If this behavior continues, we will need to ban your posting privileges on dev.to.
 
       If you think there's been a mistake, please reply to this email and we'll sort it out.
 
@@ -20,7 +20,7 @@ module FeedbackMessagesHelper
     body = <<~HEREDOC
       Hi [*USERNAME*],
 
-      We wanted to say thank you for flagging a [comment/post] that was in violation of the dev.to code of conduct and terms of service. Your action has helped us continue our work of fostering an open and welcoming community.
+      We wanted to say thank you for flagging a [*comment/post*] that was in violation of the dev.to code of conduct and terms of service. Your action has helped us continue our work of fostering an open and welcoming community.
 
       We've also removed the offending posts and reached out to the offender(s).
 
@@ -38,7 +38,7 @@ module FeedbackMessagesHelper
     body = <<~HEREDOC
       Hi [*USERNAME*],
 
-      We noticed some comments (made by others) on your [post/comment] that violated the dev.to code of conduct. We want you to know that we have zero tolerance for such behavior, and have removed the offending posts and reached out to the offender(s).
+      We noticed some comments (made by others) on your [*post/comment*] that violated the dev.to code of conduct. We want you to know that we have zero tolerance for such behavior, and have removed the offending posts and reached out to the offender(s).
 
       Thanks for being awesome and please don't hesitate to email us with any questions.  We welcome all feedback and ideas as we continue our work of fostering an open and welcoming community.
 
