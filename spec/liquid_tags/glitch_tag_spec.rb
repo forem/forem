f5ef09f7a7ceb3fd82ffd25843c21c5192731fee
Andy Zhao <andyzhao.zhao@gmail.com>
2018-09-04 16:19:24 -0400

    Fix Liquid Tags (#590)
    * Fix glitch tag
    
    * Add custom solutions for default liquid tags
    
    * Update liquid tags regex
    
    * Add speakerdeck tag and format guide a bit

--- spec/liquid_tags/glitch_tag_spec.rb
+++ spec/liquid_tags/glitch_tag_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe GlitchTag, type: :liquid_template do
   describe "#id" do
     let(:valid_id) { "BXgGcAUjM39" }
+    let(:id_with_quotes) { 'some-id" onload="alert(42)"' }
 
     def generate_tag(id)
       Liquid::Template.register_tag("glitch", GlitchTag)
@@ -11,6 +12,10 @@ RSpec.describe GlitchTag, type: :liquid_template do
 
     it "accepts a valid id" do
       expect { generate_tag(valid_id) }.not_to raise_error
+    end
+
+    it "does not accept double quotes" do
+      expect { generate_tag(id_with_quotes) }.to raise_error(StandardError)
     end
   end
 end
