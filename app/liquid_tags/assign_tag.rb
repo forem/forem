a4c3d97aa9542f1f93e2fefbc9ca6a260c829237
Andy Zhao <andyzhao.zhao@gmail.com>
2018-09-05 17:42:47 -0400

    Fix Liquid Filters and Fix DevComment Tag (#608)
    * Update standard filters
    
    * Update comment tag and rename devcomment tag
    
    * Update tests with new file name
    
    * Refactor liquid tags into null tag
    
    * Fix renaming issue and refactor
    
    * Fix useless assignment via codeclimate
    
    * Add test for null tag
    
    * Update LiquidTags.scss with new file name

--- app/liquid_tags/assign_tag.rb
+++
@@ -1,7 +0,0 @@
-class AssignTag < Liquid::Block
-  def initialize(_tag_name, _markup, _options)
-    raise StandardError.new("Liquid's Assign tag is disabled")
-  end
-end
-
-Liquid::Template.register_tag("assign".freeze, AssignTag)
