7787aecad992c50b0d4b5724c4f406892e458b88
Christine Yen <cyen@christineyen.com>
2018-09-17 20:11:14 -0700

    Correctly alphabetize Gemfile, embed env in dataset names

--- config/initializers/honeycomb.rb
+++ config/initializers/honeycomb.rb
@@ -1,5 +1,5 @@
 HoneycombRails.configure do |conf|
   conf.writekey = ApplicationConfig["HONEYCOMB_API_KEY"]
-  conf.dataset = 'dev.to'
-  conf.db_dataset = 'dev.to-db'
+  conf.dataset = "dev.to-#{Rails.env}"
+  conf.db_dataset = "dev.to-db-#{Rails.env}"
 end
