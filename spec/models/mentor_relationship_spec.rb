de317687dcb7a26dc0581fc3df483a5235208cd3
Mac Siri <krairit.siri@gmail.com>
2018-09-21 15:40:12 -0400

    Update spec

--- spec/models/mentor_relationship_spec.rb
+++ spec/models/mentor_relationship_spec.rb
@@ -3,21 +3,21 @@ require "rails_helper"
 RSpec.describe MentorRelationship, type: :model do
   let(:mentor) { create(:user) }
   let(:mentee) { create(:user) }
-  let(:relationship) { MentorRelationship.create(mentor_id: mentor.id, mentee_id: mentee.id) }
+  let(:relationship) { MentorRelationship.new(mentor: mentor, mentee_id: mentee) }
+
+  describe "validations" do
+    subject { MentorRelationship.new(mentor: mentor, mentee: mentee) }
+
+    it { is_expected.to belong_to(:mentor) }
+    it { is_expected.to belong_to(:mentee) }
+    it { is_expected.to validate_uniqueness_of(:mentor_id).scoped_to(:mentee_id) }
+  end
 
   it "is active" do
     expect(relationship.active).to eq(true)
   end
 
-  it { is_expected.to belong_to(:mentor) }
-  it { is_expected.to belong_to(:mentee) }
-
   it "is not the same user" do
-    expect(MentorRelationship.create(mentor_id: mentor.id, mentee_id: mentor.id)).to be_invalid
-  end
-
-  it "cannot be a duplicate record" do
-    relationship
-    expect(MentorRelationship.create(mentor_id: mentor.id, mentee_id: mentee.id)).to be_invalid
+    expect(MentorRelationship.new(mentor_id: mentor.id, mentee_id: mentor.id)).to be_invalid
   end
 end
