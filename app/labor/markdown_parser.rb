d543b91aaf59457252d83b6ad6a5ffbaee039bee
Gabriel Lebec <glebec@gmail.com>
2018-09-21 15:03:26 -0400

    feat(markdown): add anchors for headers

--- app/labor/markdown_parser.rb
+++ app/labor/markdown_parser.rb
@@ -2,6 +2,12 @@ class MarkdownParser
   include ApplicationHelper
   include CloudinaryHelper
 
+  RENDER_OPTIONS = {
+    hard_wrap: true,
+    filter_html: false,
+    with_toc_data: true
+  }.freeze
+
   def initialize(content)
     @content = content
   end
@@ -12,7 +18,7 @@ class MarkdownParser
 
   def evaluate_markdown
     return if @content.blank?
-    renderer = Redcarpet::Render::HTMLRouge.new(hard_wrap: true, filter_html: false)
+    renderer = Redcarpet::Render::HTMLRouge.new(RENDER_OPTIONS)
     markdown = Redcarpet::Markdown.new(renderer, REDCARPET_CONFIG)
     tag_whitelist = %w(strong em p h1 h2 h3 h4 h5 h6 i u b code pre
                        br ul ol li small sup sub img a span hr blockquote)
@@ -24,7 +30,7 @@ class MarkdownParser
 
   def evaluate_limited_markdown
     return if @content.blank?
-    renderer = Redcarpet::Render::HTMLRouge.new(hard_wrap: true, filter_html: false)
+    renderer = Redcarpet::Render::HTMLRouge.new(RENDER_OPTIONS)
     markdown = Redcarpet::Markdown.new(renderer, REDCARPET_CONFIG)
     tag_whitelist = %w(strong i u b em p br)
     attribute_whitelist = %w(href strong em ref rel src title alt class)
@@ -39,6 +45,7 @@ class MarkdownParser
       hard_wrap: true,
       filter_html: false,
       link_attributes: { rel: "noopener noreferrer", target: "_blank" },
+      with_toc_data: true
     }
     renderer = Redcarpet::Render::HTMLRouge.new(renderer_options)
     markdown = Redcarpet::Markdown.new(renderer, REDCARPET_CONFIG)
@@ -72,7 +79,7 @@ class MarkdownParser
   private
 
   def parse_it
-    renderer = Redcarpet::Render::HTMLRouge.new(hard_wrap: true, filter_html: false)
+    renderer = Redcarpet::Render::HTMLRouge.new(RENDER_OPTIONS)
     markdown = Redcarpet::Markdown.new(renderer, REDCARPET_CONFIG)
     catch_xss_attempts(@content)
     escaped_content = escape_liquid_tags_in_codeblock(@content)
