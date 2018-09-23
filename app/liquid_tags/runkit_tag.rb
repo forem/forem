f5c7911d4ee9235badfaee30cc6a4e0be4a18752
Andy Zhao <andyzhao.zhao@gmail.com>
2018-09-23 16:22:12 -0400

    Add guard clause for loading runkit tag (#714)

--- app/liquid_tags/runkit_tag.rb
+++ app/liquid_tags/runkit_tag.rb
@@ -37,17 +37,19 @@ class RunkitTag < Liquid::Block
       var checkRunkit = setInterval(function() {
         if(typeof(RunKit) !== 'undefined') {
           var targets = document.getElementsByClassName("runkit-element");
-          for (var i = 0; i < targets.length; i++) {
-            var wrapperContent = targets[i].textContent;
-            if(/^(\<iframe src)/.test(wrapperContent) === false) {
-              var preamble = targets[i].children[0].textContent;
-              var content = targets[i].children[1].textContent;
-              targets[i].innerHTML = "";
-              var notebook = RunKit.createNotebook({
-                element: targets[i],
-                source: content,
-                preamble: preamble
-              });
+          if (targets.length > 0) {
+            for (var i = 0; i < targets.length; i++) {
+              var wrapperContent = targets[i].textContent;
+              if(/^(\<iframe src)/.test(wrapperContent) === false) {
+                var preamble = targets[i].children[0].textContent;
+                var content = targets[i].children[1].textContent;
+                targets[i].innerHTML = "";
+                var notebook = RunKit.createNotebook({
+                  element: targets[i],
+                  source: content,
+                  preamble: preamble
+                });
+              }
             }
           }
           clearInterval(checkRunkit);
