e68bb8b8b6e9febad76ff4fe46efef88f4f0ff1d
Ben Halpern <ben@dev.to>
2018-09-27 12:58:31 -0400

    Make home feed more relevant and varied

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -98,7 +98,7 @@ class StoriesController < ApplicationController
   def handle_base_index
     @home_page = true
     @page = (params[:page] || 1).to_i
-    num_articles = user_signed_in? ? 9 : 15
+    num_articles = 15
     @stories = article_finder(num_articles)
 
     if ["week", "month", "year", "infinity"].include?(params[:timeframe])
@@ -114,6 +114,10 @@ class StoriesController < ApplicationController
       @stories = @stories.
         where("reactions_count > ? OR featured = ?", 10, true).
         order("hotness_score DESC")
+      if user_signed_in?
+        offset = [0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7].sample #random offset, weighted more towards zero
+        @stories = @stories.offset(offset)
+      end
       @featured_story = @stories.where.not(main_image: nil).first&.decorate || Article.new
     end
     @stories = @stories.decorate
