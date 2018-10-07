6ed218856f18e97c24b9c3384234faf170816422
Ben Halpern <ben@dev.to>
2018-10-07 12:22:48 -0400

    Update image area and other editor functionality

--- app/javascript/article-form/elements/orgSettings.jsx
+++ app/javascript/article-form/elements/orgSettings.jsx
@@ -6,7 +6,7 @@ const OrgSettings = ({ organization, postUnderOrg, onToggle }) => (
     className='articleform__orgsettings'
     onClick={onToggle}
     >
-    <img src={organization.profile_image_90} /> Post From {organization.name} <button class={postUnderOrg ? 'yes' : 'no'}>{postUnderOrg ? '✅ YES' : '◻️ NO'}</button>
+    <img src={organization.profile_image_90} style={{opacity: postUnderOrg ? '1' : '0.7' }} /> {organization.name} <button class={postUnderOrg ? 'yes' : 'no'}>{postUnderOrg ? '✅ YES' : '◻️ NO'}</button>
   </div>
 );
 
