c0099475d70edecb72adfefc5d560d1e5b118f3f
Andy Zhao <andyzhao.zhao@gmail.com>
2018-10-09 15:09:45 -0400

    Add regex checking for soundcloud links (#874)
    * Add regex checking for soundcloud links
    
    * Remove outdated test

--- spec/liquid_tags/soundcloud_tag_spec.rb
+++ spec/liquid_tags/soundcloud_tag_spec.rb
@@ -4,7 +4,6 @@ require "nokogiri"
 RSpec.describe SoundcloudTag, type: :liquid_template do
   describe "#link" do
     let(:soundcloud_link) { "https://soundcloud.com/user-261265215/dev-to-review-episode-2" }
-    let(:evil_string) { "<SCRIPT SRC=//xss.rocks/.j>soundcloud.com</SCRIPT>" }
     let(:url_segment) { "https://w.soundcloud.com/player/?url" }
 
     def generate_new_liquid(link)
@@ -28,16 +27,6 @@ RSpec.describe SoundcloudTag, type: :liquid_template do
       expect do
         generate_new_liquid("invalid_soundcloud_link")
       end.to raise_error(StandardError)
-    end
-
-    it "strips script input" do
-      allow(ActionController::Base.helpers).to receive(:strip_tags).and_return(evil_string)
-
-      liquid = generate_new_liquid(evil_string)
-      rendered_soundcloud_iframe = liquid.render
-      iframe_src = extract_iframe_src(rendered_soundcloud_iframe)
-
-      expect(iframe_src[url_segment]).not_to include("<SCRIPT SRC=//xss.rocks/.j>")
     end
   end
 end
