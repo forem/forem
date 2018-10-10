162c2daf5e55f57ca33a6eda54ce1df3921c176b
Mac Siri <krairit.siri@gmail.com>
2018-10-10 14:34:29 -0400

    Simplify Docker config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,15 +1,8 @@
 # rubocop:disable Metrics/BlockLength
-#
-def yarn_integrity_enabled?
-  ENV['YARN_INTEGRITY_ENABLED'] == 'true'
-end
 
 Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
-  config.webpacker.check_yarn_integrity = yarn_integrity_enabled?
-
-  # Verifies that versions and hashed value of the package contents in the project's package.json
-  config.webpacker.check_yarn_integrity = yarn_integrity_enabled?
+  config.webpacker.check_yarn_integrity = false
 
   # Settings specified here will take precedence over those in config/application.rb.
 
