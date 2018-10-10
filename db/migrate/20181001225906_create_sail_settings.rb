16cb899f4abe4a03b01321ffd24f6a027ac5ae3b
Vinicius Stock <vinicius.stock@outlook.com>
2018-10-10 18:13:33 -0300

    Address PR comments

--- db/migrate/20181001225906_create_sail_settings.rb
+++ db/migrate/20181001225906_create_sail_settings.rb
@@ -10,13 +10,11 @@ class CreateSailSettings < ActiveRecord::Migration[5.1]
     end
 
     [
-        { name: :display_sponsors, cast_type: :boolean, value: false, description: "Home page sponsor display" },
-        { name: :live_starting_soon, cast_type: :boolean, value: false, description: "/live event is starting soon." },
-        { name: :live_is_live, cast_type: :boolean, value: false, description: "/live page showing live event" },
-        { name: :she_coded, cast_type: :boolean, value: false, description: "Toggle #shecoded sidebar" },
-        { name: :upcoming_events, cast_type: :boolean, value: true, description: "Toggle upcoming events in sidebar" }
-    ].each { |setting_info| Sail::Setting.create(setting_info) }
-
-    drop_table :flipflop_features
+        { name: :display_sponsors, cast_type: :boolean, value: 'false', description: "Home page sponsor display" },
+        { name: :live_starting_soon, cast_type: :boolean, value: 'false', description: "/live event is starting soon." },
+        { name: :live_is_live, cast_type: :boolean, value: 'false', description: "/live page showing live event" },
+        { name: :she_coded, cast_type: :boolean, value: 'false', description: "Toggle #shecoded sidebar" },
+        { name: :upcoming_events, cast_type: :boolean, value: 'true', description: "Toggle upcoming events in sidebar" }
+    ].each { |setting_info| Sail::Setting.create!(setting_info) }
   end
 end
