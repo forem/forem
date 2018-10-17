7bf9a8ce8913b93246f8d59ca8eb4d7d093e622d
Jess Lee <jessleenyc@gmail.com>
2018-10-17 17:03:20 -0400

    Delayed Jobs Investigation  (#929)
    * disallow nil reactables to be saved
    
    * investigation wip
    
    * passes deleted comment info to async bust cache
    
    * fixes failing cache buster spec
    
    * Create user_delete_a_comment_spec.rb
    
    * Fix broken spec
    
    * Binstub RuboCop
    
    * Do not save comments when they are getting destroyed
    
    * break update_reactable into smaller methods

--- spec/labor/cache_buster_spec.rb
+++ spec/labor/cache_buster_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe CacheBuster do
   let(:comment) { create(:comment, user_id: user.id, commentable_id: article.id) }
 
   it "busts comment" do
-    described_class.new.bust_comment(comment)
+    commentable = Article.find(comment.commentable_id)
+    username = User.find(comment.user_id).username
+    described_class.new.bust_comment(commentable, username)
   end
   it "busts article" do
     described_class.new.bust_article(article)
