08ff158403d8a3dfb10c3f78ae4f7e33bad01e9f
ryanwhocodes <22416807+ryanwhocodes@users.noreply.github.com>
2018-10-17 20:19:38 +0100

    Update schema.rb after migration to add medium_url to users

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181008174839) do
+ActiveRecord::Schema.define(version: 20181016181008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
