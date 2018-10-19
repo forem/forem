4a4816778450080388fdd6bf7d3a7889ef24518e
Mac Siri <krairit.siri@gmail.com>
2018-10-19 13:12:07 -0400

    Replace sprocket-es6 with new uglifier feature (#928)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   }
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
