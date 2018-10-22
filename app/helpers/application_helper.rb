325d3ac152f1b119e633f94cb021a00162c904b7
Ben Halpern <ben@dev.to>
2018-10-22 13:28:42 -0400

    Modify follow_button to remove session context

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -154,7 +154,7 @@ module ApplicationHelper
   end
 
   def follow_button(followable, style = "full")
-    tag.button (user_signed_in? ? "" : "+ FOLLOW"),
+    tag :button,
       class: "cta follow-action-button",
       data: {
         info: { id: followable.id, className: followable.class.name, style: style }.to_json,
