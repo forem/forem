e07fd27ef5058c7a93f9328f0984fc53c2dbf406
Mac Siri <krairit.siri@gmail.com>
2018-10-23 12:00:56 -0400

    Revert "Change from #after_save to #after_create"
    This reverts commit 548ee0d2c0d56a1aa67316468f4106ac9f437662.

--- app/observers/article_observer.rb
+++ app/observers/article_observer.rb
@@ -1,5 +1,5 @@
 class ArticleObserver < ActiveRecord::Observer
-  def after_create(article)
+  def after_save(article)
     return if Rails.env.development?
     if article.published && article.published_at > 30.seconds.ago
       SlackBot.delay.ping "New Article Published: #{article.title}\nhttps://dev.to#{article.path}",
