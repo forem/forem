f039f8acb132454030ff23949af20257c3f1bb20
Mac Siri <krairit.siri@gmail.com>
2018-11-13 16:43:44 -0500

    Update Onboarding specs

--- app/javascript/src/__tests__/Onboarding.test.jsx
+++ app/javascript/src/__tests__/Onboarding.test.jsx
@@ -14,7 +14,7 @@ describe('<Onboarding />', () => {
     document.body.setAttribute('data-user', null);
   });
 
-  const fakeResponse = JSON.stringify([
+  const fakeTagResponse = JSON.stringify([
     {
       bg_color_hex: '#000000',
       id: 715,
@@ -36,23 +36,10 @@ describe('<Onboarding />', () => {
   ]);
 
   const dataUser = JSON.stringify({
-    followed_tag_names: [
-      {
-        bg_color_hex: '#000000',
-        id: 715,
-        name: 'discuss',
-        text_color_hex: '#ffffff',
-      },
-      {
-        bg_color_hex: '#f7df1e',
-        id: 6,
-        name: 'javascript',
-        text_color_hex: '#000000',
-      },
-    ],
+    followed_tag_names: ['javascript'],
   });
 
-  const usersState = [
+  const fakeUsersToFollowResponse = JSON.stringify([
     {
       id: 1,
       name: 'Ben Halpern',
@@ -68,11 +55,11 @@ describe('<Onboarding />', () => {
       name: 'dev.to staff',
       profile_image_url: 'dev.jpg',
     },
-  ];
+  ]);
 
   describe('when user is not logged in', () => {
     beforeEach(() => {
-      fetch.mockResponse(fakeResponse);
+      fetch.mockResponseOnce(fakeTagResponse);
       document.body.setAttribute(
         'data-user',
         JSON.stringify({ saw_onboarding: true }),
@@ -95,7 +82,7 @@ describe('<Onboarding />', () => {
   describe('Welcome page', () => {
     let context;
     beforeEach(async () => {
-      fetch.mockResponse(fakeResponse);
+      fetch.mockResponseOnce(fakeTagResponse);
       document.body.setAttribute('data-user', dataUser);
       context = deep(<Onboarding />);
       await flushPromises();
@@ -116,8 +103,12 @@ describe('<Onboarding />', () => {
 
   describe('Follow tag page', () => {
     let context;
+    const meta = document.createElement('meta');
+    meta.setAttribute('name', 'csrf-token');
+    document.body.appendChild(meta);
+
     beforeEach(async () => {
-      fetch.mockResponseOnce(fakeResponse);
+      fetch.mockResponseOnce(fakeTagResponse);
       document.body.setAttribute('data-user', dataUser);
       context = deep(<Onboarding />);
       context.setState({ pageNumber: 2 });
@@ -138,7 +129,7 @@ describe('<Onboarding />', () => {
     });
 
     test('NEXT button works', async () => {
-      fetch.mockResponseOnce(JSON.stringify(usersState));
+      fetch.mockResponseOnce(fakeUsersToFollowResponse);
       context
         .find('.button')
         .at(1)
@@ -147,15 +138,43 @@ describe('<Onboarding />', () => {
       expect(context.state('pageNumber')).toEqual(3);
       expect(context).toMatchSnapshot();
     });
+
+    test('each tag can be clicked', () => {
+      context.find('.onboarding-tag-link').map(tag => {
+        tag.simulate('click');
+      });
+      expect(context.state('allTags').map(tag => tag.following)).toEqual([
+        true,
+        true,
+        true,
+      ]);
+    });
+
+    test('each tag can be clicked (in weird combinations)', () => {
+      context
+        .find('.onboarding-tag-link')
+        .at(1)
+        .simulate('click');
+      expect(context.state('allTags').map(tag => tag.following)).toEqual([
+        false,
+        true,
+        false,
+      ]);
+    });
   });
 
   describe('Follow dev members page', () => {
     let context;
     beforeEach(async () => {
-      fetch.mockResponse(fakeResponse);
+      fetch.mockResponseOnce(fakeTagResponse);
       document.body.setAttribute('data-user', dataUser);
-      context = shallow(<Onboarding />);
-      context.setState({ pageNumber: 3, usersState });
+      context = deep(<Onboarding />);
+      fetch.mockResponseOnce(fakeUsersToFollowResponse);
+      context.setState({ pageNumber: 2 });
+      context
+        .find('.button')
+        .at(1)
+        .simulate('click');
       await flushPromises();
     });
 
@@ -182,104 +201,107 @@ describe('<Onboarding />', () => {
     });
   });
 
-  xit('display if there is current user', async () => {
-    document.body.setAttribute('data-user', dataUser);
-    fetch.mockResponse(fakeResponse);
-    const context = shallow(<Onboarding />);
-    await flushPromises();
-    expect(context).toMatchSnapshot();
-  });
-
-  xit('allows user to interact properly', async () => {
-    document.body.setAttribute('data-user', dataUser);
-    const meta = document.createElement('meta');
-    meta.setAttribute('name', 'csrf-token');
-    document.body.appendChild(meta);
-    fetch.mockResponseOnce(fakeResponse);
-
-    const context = deep(<Onboarding />, { depth: 3 });
-    // context.setState({ users: usersState });
-    // context.rerender();
-    await flushPromises();
-    context.find('.button').simulate('click');
-
-    // going to page two
-    expect(context.state('pageNumber')).toEqual(2);
-    expect(context.find('[onClick]')).toMatchSnapshot();
-
-    // going to page three
-    context
-      .find('.button')
-      .at(1)
-      .simulate('click');
-    console.log(context.state());
-    expect(context.state('pageNumber')).toEqual(3);
-    // going to page four
-    context
-      .find('.button')
-      .at(1)
-      .simulate('click');
-    expect(context.state('pageNumber')).toEqual(4);
-    // going to page five
-    context
-      .find('.button')
-      .at(1)
-      .simulate('click');
-    expect(context.state('pageNumber')).toEqual(5);
-    fetch.mockResponse(JSON.stringify({ outcome: 'onboarding closed' }));
-    // going back to page four
-    context
-      .find('.button')
-      .at(0)
-      .simulate('click');
-    expect(context.state('pageNumber')).toEqual(4);
-    // going back to page five
-    context
-      .find('.button')
-      .at(1)
-      .simulate('click');
-    expect(context.state('pageNumber')).toEqual(5);
-    // evaluting the button text on page five
-    expect(
-      context
-        .find('[onClick]')
-        .at(2)
-        .text(),
-    ).toEqual("LET'S GO");
-    context
-      .find('.button')
-      .at(1)
-      .simulate('click');
-    // clicking the final button
-    await flushPromises();
-    expect(context.state('showOnboarding')).toEqual(false);
-  });
-
-  xit('allow user to follow tags flawlessly', async () => {
-    const dataUser = JSON.stringify({
-      followed_tag_names: [
-        {
-          bg_color_hex: '#000000',
-          id: 715,
-          name: 'discuss',
-          text_color_hex: '#ffffff',
-        },
-        {
-          bg_color_hex: '#f7df1e',
-          id: 6,
-          name: 'javascript',
-          text_color_hex: '#000000',
-        },
-      ],
+  describe('User info page', () => {
+    let context;
+    beforeEach(async () => {
+      fetch.mockResponseOnce(fakeTagResponse);
+      document.body.setAttribute('data-user', dataUser);
+      context = deep(<Onboarding />);
+      context.setState({ pageNumber: 4 });
+      await flushPromises();
     });
-    document.body.setAttribute('data-user', dataUser);
-    const meta = document.createElement('meta');
-    meta.setAttribute('name', 'csrf-token');
-    document.body.appendChild(meta);
-    fetch.mockResponseOnce(fakeResponse);
-    const context = await deep(<Onboarding />, { depth: 10 });
-    context.setState({ pageNumber: 2 });
-    context.rerender();
-    console.log(context.find('.onboarding-tag-link-follow'));
+
+    test('renders properly', () => {
+      expect(context).toMatchSnapshot();
+    });
+
+    test('NEXT button works', () => {
+      context
+        .find('.button')
+        .at(1)
+        .simulate('click');
+      expect(context.state('pageNumber')).toEqual(5);
+    });
+
+    test('BACK button works', () => {
+      context
+        .find('.button')
+        .at(0)
+        .simulate('click');
+      expect(context.state('pageNumber')).toEqual(3);
+    });
+
+    test('forms can be filled and submitted', () => {
+      const targets = [
+        'summary',
+        'location',
+        'employment_title',
+        'employer_name',
+        'mostly_work_with',
+        'currently_learning',
+      ];
+      const findAndUpdate = target => {
+        // TODO: we shouldn't have to add name. This might have to do with preact-render-spy
+        const event = { target: { value: 'TEST', name: target } };
+        context.find(<input name={target} />).simulate('change', event);
+      };
+
+      targets.forEach(findAndUpdate);
+      context
+        .find('.button')
+        .at(1)
+        .simulate('click');
+      expect(context.state('pageNumber')).toEqual(5);
+      const idealResult = targets.reduce((accu, item) => {
+        accu[item] = 'TEST';
+        return accu;
+      }, {});
+      expect(context.state('profileInfo')).toEqual(idealResult);
+    });
+  });
+
+  describe('Final page', () => {
+    let context;
+    beforeEach(async () => {
+      fetch.mockResponseOnce(fakeTagResponse);
+      document.body.setAttribute('data-user', dataUser);
+      const meta = document.createElement('meta');
+      meta.setAttribute('name', 'csrf-token');
+      document.body.appendChild(meta);
+      context = deep(<Onboarding />);
+      context.setState({ pageNumber: 5, profileInfo: { summary: 'hi there' } });
+      await flushPromises();
+    });
+
+    it('renders properly', () => {
+      expect(context).toMatchSnapshot();
+    });
+
+    it('special next button exists and should reroute user', () => {
+      const next = context.find('.button').at(1);
+      expect(next.text()).toEqual("LET'S GO");
+      next.simulate('click');
+      expect(context.state('pageNumber')).toEqual(5);
+      expect(context).toMatchSnapshot();
+    });
+
+    it('sends proper update on close', async () => {
+      fetch.mockResponseOnce(JSON.stringify({ outcome: 'onboarding closed' }));
+      context
+        .find('.button')
+        .at(1)
+        .simulate('click');
+      await flushPromises();
+      expect(context.state('showOnboarding')).toEqual(false);
+    });
+
+    it('BACK button works', () => {
+      context
+        .find('.button')
+        .at(0)
+        .simulate('click');
+      expect(context.state('pageNumber')).toEqual(4);
+      expect(context).toMatchSnapshot();
+    });
   });
 });
