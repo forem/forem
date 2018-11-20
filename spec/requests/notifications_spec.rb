aed549be6bfffb25179d25cd829a7a8daf71eeb0
Mac Siri <krairit.siri@gmail.com>
2018-11-20 11:11:47 -0500

    Remove focus [ci skip]

--- spec/requests/notifications_spec.rb
+++ spec/requests/notifications_spec.rb
@@ -172,13 +172,13 @@ RSpec.describe "NotificationsIndex", type: :request do
         expect(notifications.count).to eq 2
       end
 
-      it "properly renders reactable titles", focus: true do
+      it "properly renders reactable titles" do
         mock_heart_reaction_notifications(1, %w(unicorn like readinglist), special_characters_article)
         get "/notifications"
         expect(response.body).to include special_characters_article.title
       end
 
-      it "properly renders reactable titles for multiple reactions", focus: true do
+      it "properly renders reactable titles for multiple reactions" do
         amount = rand(3..10)
         mock_heart_reaction_notifications(amount, %w(unicorn like readinglist), special_characters_article)
         get "/notifications"
