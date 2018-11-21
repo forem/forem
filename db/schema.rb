915e2d77b6e6d6434912085e921c54e68223a897
rhymes <rhymesete@gmail.com>
2018-11-21 17:13:36 +0100

    Export articles/posts (#576)
    * Add EditorConfig file for all editors
    
    * Add article export service
    
    * Add setting to request an export of all articles
    
    * On the outside they are called posts
    
    * Add articles exported email to mailer and service
    
    * Refactor to one public method and test flags
    
    * Trigger the export if it was requested and send the email
    
    * Recreated migration file with generic export fields
    
    * Rename fields and switch to a whitelist
    
    * Refactor ArticleExportService into a more generic structure
    
    * Rename articles_exported_email to export_email
    
    * Fix notify mailer spec
    
    * Rename Exporter::Exporter to Exporter::Service
    
    * Remove commented out line
    
    * Removed body_html, coordinates and updated_at from export
    
    * Invert DJ config
    
    * Update spec

--- db/schema.rb
+++ db/schema.rb
@@ -711,6 +711,8 @@ ActiveRecord::Schema.define(version: 20181120170350) do
     t.string "employment_title"
     t.string "encrypted_password", default: "", null: false
     t.integer "experience_level"
+    t.boolean "export_requested", default: false
+    t.datetime "exported_at"
     t.string "facebook_url"
     t.boolean "feed_admin_publish_permission", default: true
     t.boolean "feed_mark_canonical", default: false
