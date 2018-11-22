edafbabee45c1ed36898387b436e9bdd88cb4fc3
Ben Halpern <bendhalpern@gmail.com>
2018-11-21 20:03:56 -0500

    Initial basic paginionation-ish for notifications (#1175)

--- app/assets/javascripts/initializers/initNotifications.js
+++ app/assets/javascripts/initializers/initNotifications.js
@@ -3,6 +3,7 @@ function initNotifications() {
   markNotificationsAsRead();
   initReactions();
   listenForNotificationsBellClick();
+  initPagination();
 }
 
 function markNotificationsAsRead() {
@@ -115,4 +116,20 @@ function listenForNotificationsBellClick() {
       document.getElementById('notifications-number').classList.remove('showing');
     };
   }, 180);
+}
+
+function initPagination() {
+  var el = document.getElementById("notifications-pagination")
+  if (el) {
+    window.fetch(el.dataset.paginationPath, {
+      method: 'GET',
+      credentials: 'same-origin'
+    }).then(function (response) {
+      if (response.status === 200) {
+        response.text().then(function(html){
+          el.innerHTML = html
+        });
+      }
+    });
+  }
 }
