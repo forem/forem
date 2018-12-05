67d234f62232cba1d8b5f7d7b96b804467410c52
Vinicius Stock <vinicius.stock@outlook.com>
2018-12-05 20:50:46 -0200

    Move setting creation to config/sail.yml

--- db/migrate/20181001225906_create_sail_settings.rb
+++ db/migrate/20181001225906_create_sail_settings.rb
@@ -8,13 +8,5 @@ class CreateSailSettings < ActiveRecord::Migration[5.1]
       t.timestamps
       t.index ["name"], name: "index_settings_on_name", unique: true
     end
-
-    [
-        { name: :display_sponsors, cast_type: :boolean, value: 'false', description: "Home page sponsor display" },
-        { name: :live_starting_soon, cast_type: :boolean, value: 'false', description: "/live event is starting soon." },
-        { name: :live_is_live, cast_type: :boolean, value: 'false', description: "/live page showing live event" },
-        { name: :she_coded, cast_type: :boolean, value: 'false', description: "Toggle #shecoded sidebar" },
-        { name: :upcoming_events, cast_type: :boolean, value: 'true', description: "Toggle upcoming events in sidebar" }
-    ].each { |setting_info| Sail::Setting.create!(setting_info) }
   end
 end
