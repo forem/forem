aace27220e597262236c55ffb7e334b0a9b93597
Mac Siri <krairit.siri@gmail.com>
2018-12-10 15:50:12 -0500

    Update spec

--- db/schema.rb
+++ db/schema.rb
@@ -747,6 +747,7 @@ ActiveRecord::Schema.define(version: 20181130224531) do
     t.string "location"
     t.boolean "looking_for_work", default: false
     t.boolean "looking_for_work_publicly", default: false
+    t.string "mastodon_url"
     t.string "medium_url"
     t.datetime "membership_started_at"
     t.text "mentee_description"
@@ -813,7 +814,6 @@ ActiveRecord::Schema.define(version: 20181130224531) do
     t.string "username"
     t.string "website_url"
     t.datetime "workshop_expiration"
-    t.string "mastodon_url"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["language_settings"], name: "index_users_on_language_settings", using: :gin
     t.index ["organization_id"], name: "index_users_on_organization_id"
