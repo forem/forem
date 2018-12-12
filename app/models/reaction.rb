e192e201c3a41528133a05d0359534b428bc1c49
Mac Siri <krairit.siri@gmail.com>
2018-12-12 17:08:04 -0500

    Change callback

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -18,7 +18,7 @@ class Reaction < ApplicationRecord
 
   before_save :assign_points
   after_save :update_reactable, :async_bust
-  before_destroy :update_reactable, :clean_up_before_destroy
+  after_destroy :update_reactable, :clean_up_before_destroy
 
   class << self
     def count_for_article(id)
