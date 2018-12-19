8e4f6891e5bd87c7fb4e47477ceab96fcbe4c6c8
Jess Lee <jessleenyc@gmail.com>
2018-12-19 15:44:36 -0500

    Include conditional for adding tag mods to connect channel (#1366)
    * fix slug typo
    
    * fix specs
    
    * use blank? instead of nil?

--- app/labor/assign_tag_moderator.rb
+++ app/labor/assign_tag_moderator.rb
@@ -4,7 +4,9 @@ module AssignTagModerator
       user = User.find(user_id)
       tag = Tag.find_by(name: tag_names[index])
       user.add_role(:tag_moderator, tag)
-      ChatChannel.find_by(slug: "tag-moderators").add_users(user)
+      if user.chat_channels.find_by(slug: "tag-moderators").blank?
+        ChatChannel.find_by(slug: "tag-moderators").add_users(user)
+      end
       NotifyMailer.tag_moderator_confirmation_email(user, tag.name).deliver unless tag.name == "go"
     end
   end
