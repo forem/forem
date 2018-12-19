d8ff236f695392cc4707648df97d931c2eb156a7
Jess Lee <jessleenyc@gmail.com>
2018-12-19 13:39:50 -0500

    Fix slug typo (#1364)

--- app/labor/assign_tag_moderator.rb
+++ app/labor/assign_tag_moderator.rb
@@ -4,7 +4,7 @@ module AssignTagModerator
       user = User.find(user_id)
       tag = Tag.find_by(name: tag_names[index])
       user.add_role(:tag_moderator, tag)
-      ChatChannel.find_by_slug("tag_moderators").add_users(user)
+      ChatChannel.find_by_slug("tag-moderators").add_users(user)
       NotifyMailer.tag_moderator_confirmation_email(user, tag.name).deliver unless tag.name == "go"
     end
   end
