8f163a0f914e7a786fce001356516b056af82697
Timothy Ng <timnjj@gmail.com>
2018-12-22 15:52:30 -0800

    Add presence validation for ApiSecret description

--- spec/models/api_secret_spec.rb
+++ spec/models/api_secret_spec.rb
@@ -4,6 +4,7 @@ require "rails_helper"
 RSpec.describe ApiSecret, type: :model do
   describe "validations" do
     it { is_expected.to belong_to(:user) }
+    it { is_expected.to validate_presence_of(:description) }
   end
 end
 # rubocop:enable RSpec/ExampleLength, RSpec/MultipleExpectations
