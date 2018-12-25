0ee9f7190341246a05e94f6afaaa40f93570f5fe
Timothy Ng <timnjj@gmail.com>
2018-12-25 15:25:40 -0800

    Remove length restriction on ApiSecret.description

--- spec/models/api_secret_spec.rb
+++ spec/models/api_secret_spec.rb
@@ -4,6 +4,5 @@ RSpec.describe ApiSecret, type: :model do
   describe "validations" do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to validate_presence_of(:description) }
-    it { is_expected.to validate_length_of(:description).is_at_least(1).is_at_most(ApiSecret::DESCRIPTION_MAX_LENGTH) }
   end
 end
