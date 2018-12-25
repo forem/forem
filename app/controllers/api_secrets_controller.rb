7b398b847e0523112d49b85e55d3e41f065ff8eb
Timothy Ng <timnjj@gmail.com>
2018-12-25 12:10:36 -0800

    Add error message flash for ApiSecretsController actions

--- app/controllers/api_secrets_controller.rb
+++ app/controllers/api_secrets_controller.rb
@@ -8,16 +8,20 @@ class ApiSecretsController < ApplicationController
     @secret.user_id = current_user.id
     if @secret.save
       flash[:notice] = "Your access token has been generated: #{@secret.secret}. Be sure to copy it to somewhere safe now. You won’t be able to see it again!"
-      redirect_back(fallback_location: root_path)
+    else
+      flash[:error] = "An error occurred. Please try again or send an email to: yo@dev.to"
     end
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     authorize @secret
     if @secret.destroy
       flash[:notice] = "Your access token has been revoked."
-      redirect_back(fallback_location: root_path)
+    else
+      flash[:error] = "An error occurred. Please try again or send an email to: yo@dev.to"
     end
+    redirect_back(fallback_location: root_path)
   end
 
   private
