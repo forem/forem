faffedd36db69caa3811409b4ab6d3d21a5d0c42
Timothy Ng <timnjj@gmail.com>
2018-12-25 13:27:53 -0800

    Add length validation to api secret description

--- app/models/api_secret.rb
+++ app/models/api_secret.rb
@@ -1,5 +1,7 @@
 class ApiSecret < ApplicationRecord
+  DESCRIPTION_MAX_LENGTH = 30
+
   has_secure_token :secret
   belongs_to :user
-  validates :description, presence: true
+  validates :description, presence: true, length: { in: 1..DESCRIPTION_MAX_LENGTH }
 end
--- spec/models/api_secret_spec.rb
+++ spec/models/api_secret_spec.rb
@@ -1,10 +1,9 @@
-# rubocop:disable RSpec/ExampleLength, RSpec/MultipleExpectations
 require "rails_helper"
 
 RSpec.describe ApiSecret, type: :model do
   describe "validations" do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_length_of(:description).is_at_least(1).is_at_most(ApiSecret::DESCRIPTION_MAX_LENGTH) }
   end
 end
-# rubocop:enable RSpec/ExampleLength, RSpec/MultipleExpectations
