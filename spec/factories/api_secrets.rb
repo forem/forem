5f76e73b31cdbdea34cdf8474d6187b52eca50db
Timothy Ng <timnjj@gmail.com>
2018-12-25 13:48:24 -0800

    Truncate ApiSecret factory objects descriptions to prevent validation error

--- spec/factories/api_secrets.rb
+++ spec/factories/api_secrets.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :api_secret do
     user
-    description { Faker::Lorem.sentence }
+    description { Faker::Lorem.sentence.truncate(ApiSecret::DESCRIPTION_MAX_LENGTH) }
     secret      { SecureRandom.base58(24) }
   end
 end
