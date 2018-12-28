064afb83cdafc4bc42e4fe5eb9d8110fab702e70
rhymes <rhymesete@gmail.com>
2018-12-28 20:09:32 +0100

    Fix typos

--- spec/labor/user_similarity_spec.rb
+++ spec/labor/user_similarity_spec.rb
@@ -6,24 +6,24 @@ RSpec.describe UserSimilarity, vcr: {} do
   let(:dissimilar_user) { create(:user, summary: "I like Haskell and functional programming") }
 
   it "returns similar user" do
-    simialar_score = UserSimilarity.new(user, similar_user).score
-    dissimialar_score = UserSimilarity.new(user, dissimilar_user).score
-    expect(simialar_score).to be > dissimialar_score
+    similar_score = UserSimilarity.new(user, similar_user).score
+    dissimilar_score = UserSimilarity.new(user, dissimilar_user).score
+    expect(similar_score).to be > dissimilar_score
   end
 
   it "Is not affected by stop words" do
     user.summary = user.summary + " throughout yourself can indeed otherwise thru yourselves through yours by inc others"
     dissimilar_user.summary = dissimilar_user.summary + " throughout yourself can indeed otherwise thru yourselves through yours by inc others"
-    simialar_score = UserSimilarity.new(user, similar_user).score
-    dissimialar_score = UserSimilarity.new(user, dissimilar_user).score
-    expect(simialar_score).to be > dissimialar_score
+    similar_score = UserSimilarity.new(user, similar_user).score
+    dissimilar_score = UserSimilarity.new(user, dissimilar_user).score
+    expect(similar_score).to be > dissimilar_score
   end
 
   it "Is affected by non-stop words" do
     user.mentee_description = "Hot dogs languages punk rock hello"
     dissimilar_user.mentor_description = "Hot dogs languages punk rock hello "
-    simialar_score = UserSimilarity.new(user, similar_user).score
-    dissimialar_score = UserSimilarity.new(user, dissimilar_user).score
-    expect(simialar_score).to be < dissimialar_score
+    similar_score = UserSimilarity.new(user, similar_user).score
+    dissimilar_score = UserSimilarity.new(user, dissimilar_user).score
+    expect(similar_score).to be < dissimilar_score
   end
 end
