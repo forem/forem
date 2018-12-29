9e5365cb528c603118406490855ba8c5587f51b4
Abraham Williams <4braham@gmail.com>
2018-12-29 12:18:27 -0600

    Remove unnecessary variable assignment (#1414)

--- app/observers/reaction_observer.rb
+++ app/observers/reaction_observer.rb
@@ -1,12 +1,11 @@
 class ReactionObserver < ActiveRecord::Observer
   def after_create(reaction)
     if reaction.category == "vomit"
-      emoji = ":cry:"
       SlackBot.delay.ping(
         "#{reaction.user.name} (https://dev.to#{reaction.user.path}) \nreacted with a #{reaction.category} on\nhttps://dev.to#{reaction.reactable.path}",
         channel: "abuse-reports",
         username: "abuse_bot",
-        icon_emoji: emoji,
+        icon_emoji: ":cry:",
       )
     end
   rescue StandardError
