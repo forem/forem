ec1582c6ad00ba073a764a050f1166a7a6309b1d
rhymes <rhymesete@gmail.com>
2019-01-02 22:35:19 +0100

    Fix Rubocop Security/Open (#1447)
    * Re-run rubocop autogen config
    
    * Fix Layout/* violations
    
    * Fix Security/Open and use HEAD if GET is not needed

--- app/services/user_role_service.rb
+++ app/services/user_role_service.rb
@@ -17,10 +17,11 @@ class UserRoleService
   def update_tag_moderators(user_ids, tag)
     users = user_ids.map do |id|
       User.find(id)
-    rescue ActiveRecord::RecordNotFound
+    rescue ActiveRecord::RecordNotFound # rubocop:disable Layout/RescueEnsureAlignment
       tag.errors[:moderator_ids] << ": user id #{id} was not found"
     end
     return false if tag.errors[:moderator_ids].present?
+
     # Don't have to worry about comparing old and new values.
     tag.tag_moderator_ids.each do |id|
       User.find(id).remove_role(:tag_moderator, tag)
