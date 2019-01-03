2afc1c67a8b207877035c760b827e69a12db6700
Jess Lee <jessleenyc@gmail.com>
2019-01-03 18:26:19 -0500

    wip remove callbacks from banisher

--- app/services/moderator/banisher.rb
+++ app/services/moderator/banisher.rb
@@ -11,7 +11,7 @@ module Moderator
       @admin = admin
     end
 
-    def banish
+    def hello
       return unless user.comments.where("created_at < ?", 150.days.ago).empty?
 
       new_name = "spam_#{rand(10000)}"
@@ -60,6 +60,84 @@ module Moderator
       user.save!
       CacheBuster.new.bust("/#{user.old_username}")
       user.update!(old_username: nil)
+    end
+
+    def reassign_and_bust_username
+      new_name = "spam_#{rand(10000)}"
+      new_username = "spam_#{rand(10000)}"
+      if User.find_by(name: new_name) || User.find_by(username: new_username)
+        new_name = "spam_#{rand(10000)}"
+        new_username = "spam_#{rand(10000)}"
+      end
+      user.update_columns(name: new_name, username: new_username)
+      CacheBuster.new.bust("/#{user.old_username}")
+      user.update_columns(old_username: nil)
+    end
+
+    def remove_profile_info
+      user.update_columns(twitter_username: "", github_username: "", website_url: "", summary: "", location: "", education: "", employer_name: "", employer_url: "", employment_title: "", mostly_work_with: "", currently_learning:"", currently_hacking_on: "", available_for: "")
+
+      user.update_columns(email_public: false, facebook_url: nil, dribbble_url: nil, medium_url: nil, stackoverflow_url: nil, behance_url: nil, linkedin_url: nil, gitlab_url: nil, mastodon_url: nil)
+
+      user.update_columns(remote_profile_image_url: "https://thepracticaldev.s3.amazonaws.com/i/99mvlsfu5tfj9m7ku25d.png") if Rails.env.production?
+    end
+
+    def add_banned_role
+      user.add_role :banned
+      unless user.notes.where(reason: "banned").any?
+        user.notes.
+          create!(reason: "banned", content: "spam account", author: admin)
+      end
+    end
+
+    def delete_reactions
+      user.reactions.each &:delete
+    end
+
+    def clear_cache(path)
+      return unless Rails.env.production?
+
+      HTTParty.post("https://api.fastly.com/purge/https://dev.to#{path}", headers: { "Fastly-Key" => ApplicationConfig["FASTLY_API_KEY"] })
+    end
+
+    def delete_comments
+      user.comments.each do |comment|
+        path = comment.path
+        clear_cache(path)
+        comment.reactions.each &:delete
+        comment.delete
+      end
+    end
+
+    def delete_follows
+      user.follows.each &:delete
+    end
+
+    def delete_articles
+      user.articles.each do |article|
+        article.reactions.each &:delete
+        article.comments.each do |comment|
+          comment.reactions.each &:delete
+          comment.clear_cache(comment.path)
+          comment.delete
+        end
+        clear_cache(article.path)
+        # article.remove_from_index!
+        article.delete
+      end
+    end
+
+    def banish
+      # return unless user.comments.where("created_at < ?", 150.days.ago).empty?
+      reassign_and_bust_username
+      remove_profile_info
+      add_banned_role
+      delete_reactions
+      delete_comments
+      delete_follows
+      delete_articles
+      user.remove_from_index!
+      user.save!
     end
   end
 end
--- spec/controllers/internal_users_controller_spec.rb
+++ spec/controllers/internal_users_controller_spec.rb
@@ -3,78 +3,77 @@ require "rails_helper"
 RSpec.describe "internal/users", type: :request do
   context "when banishing user" do
     let(:user) { create(:user) }
-    let(:article) { create(:article, user: user) }
+    let(:user2) { create(:user) }
+    let(:user3) { create(:user) }
     let(:super_admin) { create(:user, :super_admin) }
-    let(:article2) { create(:article, user: super_admin) }
+    let(:article) { create(:article, user: user) }
+    let(:article2) { create(:article, user: user2) }
 
     before do
       sign_in super_admin
-      Delayed::Worker.delay_jobs = true
-      Delayed::Worker.destroy_failed_jobs = false
+      # Delayed::Worker.delay_jobs = true
+      # Delayed::Worker.destroy_failed_jobs = false
     end
 
     after do
-      Delayed::Worker.delay_jobs = false
+      # Delayed::Worker.delay_jobs = false
     end
 
     def banish_user
       post "/internal/users/#{user.id}/banish"
-      Delayed::Worker.new(quiet: true).work_off
+      # Delayed::Worker.new(quiet: true).work_off
       user.reload
     end
 
-    def create_dependents
-      # Article 1
-      create(:reaction, reactable: article, reactable_type: "Article", user: user)
-      parent_comment = create(:comment, commentable_type: "Article", commentable: article, user: user)
-      create(:comment, commentable_type: "Article", commentable: article, user: user, parent: parent_comment)
-
-      # Article 2 written by super_admin
-      create(:reaction, reactable: article2, reactable_type: "Article", user: user)
-      parent_comment2 = create(:comment, commentable_type: "Article", commentable: article2, user: user)
-      create(:comment, commentable_type: "Article", commentable: article2, user: super_admin, parent: parent_comment2)
-
-      user.follow(super_admin)
-
-      Delayed::Worker.new(quiet: true).work_off
+    def dependents_for_offending_user_article
+      # create user2 comment on offending user article
+      comment = create(:comment, commentable_type: "Article", commentable: article, user: user2)
+      # create user3 reaction to user2 comment
+      create(:reaction, reactable: comment, reactable_type: "Comment", user: user3)
+      # create user3 comment response to user2 comment
+      comment2 = create(:comment, commentable_type: "Article", commentable: article, user: user2, ancestry: comment.id)
+      # create user2 reaction to user3 comment response
+      Reaction.create(reactable: comment2, reactable_type: "Comment", user: user2)
+      # create user3 reaction to offending article
+      create(:reaction, reactable: article, reactable_type: "Article", user: user3, category: "like")
     end
 
-    it "works" do
+    def offender_activity_on_other_content
+      # offender reacts to user2 article
+      create(:reaction, reactable: article2, reactable_type: "Article", user: user)
+      # offender comments on user2 article
+      comment = create(:comment, commentable_type: "Article", commentable: article2, user: user)
+      # user3 reacts to offender comment
+      create(:reaction, reactable: comment, reactable_type: "Comment", user: user3)
+    end
+
+    it "deletes all dependents from offending user article" do
+      dependents_for_offending_user_article
+      offender_activity_on_other_content
+      banish_user
+      expect(user.username).to include("spam_")
+      expect(user.articles.count).to eq(0)
+      expect(Comment.count).to eq(0)
+      expect(Reaction.count).to eq(0)
+      expect(user.currently_learning).to eq("")
+      expect(user.banned).to eq(true)
+      expect(Note.count).to eq(1)
+    end
+
+    xit "works" do
       create(:reaction, reactable: article, reactable_type: "Article", user: user)
       create(:comment, commentable_type: "Article", commentable: article, user: user)
-      Delayed::Worker.new(quiet: true).work_off
+      Delayed::Worker.new(quiet: false).work_off
+      user.currently_learning = "blah blah balh"
       banish_user
       expect(user.username).to include("spam_")
       expect(Article.count).to eq(0)
       expect(Comment.count).to eq(0)
       expect(Reaction.count).to eq(0)
+      expect(user.currently_learning).to eq("")
+      expect(user.banned).to eq(true)
+      expect(Note.count).to eq(1)
     end
 
-    xcontext "when offender is a spam user" do
-      before do
-        create(:reaction, reactable: article, reactable_type: "Article", user: user)
-        create(:comment, commentable_type: "Article", commentable: article, user: user)
-        Delayed::Worker.new(quiet: true).work_off
-      end
-
-      it "works" do
-        banish_user
-        expect(Delayed::Job.where("failed_at IS NOT NULL").count).to eq(0)
-        expect(user.old_username).to eq(nil)
-        expect(user.twitter_username).to eq("")
-      end
-    end
-
-    xcontext "when offender has rich activity " do
-      it "works" do
-        create_dependents
-        banish_user
-        # failed = Delayed::Job.where("failed_at IS NOT NULL").first
-        # failed = YAML.load(failed.handler) if failed
-        expect(Delayed::Job.where("failed_at IS NOT NULL").count).to eq(0)
-        expect(user.old_username).to eq(nil)
-        expect(user.twitter_username).to eq("")
-      end
-    end
   end
 end
