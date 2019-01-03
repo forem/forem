f18f48ea0b538e39dfd60777da6b734375dc1cf1
Mac Siri <krairit.siri@gmail.com>
2019-01-03 17:14:28 -0500

    Add UpdateRecordJob's spec

--- spec/jobs/reaction/update_records_job_spec.rb
+++ spec/jobs/reaction/update_records_job_spec.rb
@@ -1,5 +1,24 @@
 require "rails_helper"
 
 RSpec.describe Reaction::UpdateRecordsJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:reactable) { create(:article) }
+
+  describe "#perform" do
+    it "works" do
+      expect { described_class.new.perform(reactable, user) }.not_to raise_error
+    end
+  end
+
+  describe "#perform_later" do
+    before { described_class.perform_later(reactable, user) }
+
+    it "enqueue jobs properly" do
+      expect(described_class).to have_been_enqueued
+    end
+
+    it "adds job to the :default queue" do
+      expect(enqueued_jobs.last[:queue]).to eq("default")
+    end
+  end
 end
