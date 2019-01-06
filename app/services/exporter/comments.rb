c3070a7c3d26eb745cdf799b8925f81e29a1806e
rhymes <rhymesete@gmail.com>
2019-01-06 20:12:06 +0100

    Export comments

---
+++ app/services/exporter/comments.rb
@@ -0,0 +1,47 @@
+module Exporter
+  class Comments
+    attr_reader :name
+    attr_reader :user
+
+    def initialize(user)
+      @name = :comments
+      @user = user
+    end
+
+    def export(id_code: nil)
+      comments = user.comments
+      comments = comments.where(id_code: id_code) if id_code.present?
+      json_comments = jsonify_comments(comments)
+
+      { "#{name}.json" => json_comments }
+    end
+
+    private
+
+    def allowed_attributes
+      %i[
+        body_markdown
+        created_at
+        deleted
+        edited
+        edited_at
+        id_code
+        markdown_character_count
+        positive_reactions_count
+        processed_html
+        reactions_count
+        receive_notifications
+      ]
+    end
+
+    def jsonify_comments(comments)
+      comments_to_jsonify = []
+      # load comments in batches, we don't want to hog the DB
+      # if a user has lots and lots of comments
+      comments.find_each do |comment|
+        comments_to_jsonify << comment
+      end
+      comments_to_jsonify.to_json(only: allowed_attributes)
+    end
+  end
+end
