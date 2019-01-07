5c1a55246540fd9b5468391fd5e21ef27b942e60
Marcos Bellucci <mbellucci@salesforce.com>
2019-01-07 11:27:58 -0300

    Fix codeclimate issues

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,22 +3,22 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   include Devise::Controllers::Rememberable
 
   def twitter
-    callback_for('twitter')
+    callback_for("twitter")
   end
 
   def github
-    callback_for('github')
+    callback_for("github")
   end
 
   private
 
   def callback_for(provider)
-    cta_variant = request.env["omniauth.params"]['state'].to_s
+    cta_variant = request.env["omniauth.params"]["state"].to_s
     @user = AuthorizationService.new(request.env["omniauth.auth"], current_user, cta_variant).get_user
     if @user.persisted? && @user.valid?
       remember_me(@user)
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+      set_flash_message(:notice, :success, kind: provider.to_s.capitalize) if is_navigational_format?
     elsif @user.persisted? && @user.errors.full_messages.join(", ").include?("username has already been taken")
       redirect_to "/settings?state=previous-registration"
     else
