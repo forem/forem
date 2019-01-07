6a9802eeff07805a534f4063b15b869d7dea8e54
Marcos Bellucci <mbellucci@salesforce.com>
2019-01-07 15:40:34 -0300

    Introduce explanatory methods

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -15,11 +15,11 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def callback_for(provider)
     cta_variant = request.env["omniauth.params"]["state"].to_s
     @user = AuthorizationService.new(request.env["omniauth.auth"], current_user, cta_variant).get_user
-    if @user.persisted? && @user.valid?
+    if persisted_and_valid?
       remember_me(@user)
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: provider.to_s.capitalize) if is_navigational_format?
-    elsif @user.persisted? && @user.errors.full_messages.join(", ").include?("username has already been taken")
+    elsif persisted_but_username_taken?
       redirect_to "/settings?state=previous-registration"
     else
       session["devise.#{provider}_data"] = request.env["omniauth.auth"]
@@ -27,5 +27,13 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:alert] = user_errors
       redirect_to new_user_registration_url
     end
+  end
+
+  def persisted_and_valid?
+    @user.persisted? && @user.valid?
+  end
+
+  def persisted_but_username_taken?
+    @user.persisted? && @user.errors.full_messages.join(", ").include?("username has already been taken")
   end
 end
