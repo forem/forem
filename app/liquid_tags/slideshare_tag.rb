6a656d319c8457dedb38b421842fac3b268f6d8b
Andrew Bone <AndrewB05@gmail.com>
2019-01-08 15:28:58 +0000

    Create slideshare_tag.rb

---
+++ app/liquid_tags/slideshare_tag.rb
@@ -0,0 +1,33 @@
+class SlideshareTag < LiquidTagBase
+  def initialize(tag_name, key, tokens)
+    super
+    @key    = validate key
+    @height = 450
+  end
+
+  def render(_context)
+    finalize_html <<-HTML
+      <iframe 
+        src="//www.slideshare.net/slideshow/embed_code/key/#{@key}" 
+        alt="#{@key} on slideshare.net"
+        width="100%"
+        height="#{@height}"
+        frameborder="0"
+        scrolling="no"
+        allowfullscreen> 
+      </iframe>
+    HTML
+  end
+
+  private
+  
+  def validate(key)
+    if key =~ /[a-zA-Z0-9]{14}/
+      key
+    else
+      raise StandardError, "Invalid Slideshare Key"
+    end
+  end
+end
+
+Liquid::Template.register_tag("slideshare", SlideshareTag)
