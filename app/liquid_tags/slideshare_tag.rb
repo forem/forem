0148d7a3fed1e9d709138b5029d2a4a5985f6ad8
Ben Halpern <bendhalpern@gmail.com>
2019-01-08 17:28:56 +0000

    Strip whitespace
    Co-Authored-By: Link2Twenty <AndrewB05@gmail.com>

--- app/liquid_tags/slideshare_tag.rb
+++ app/liquid_tags/slideshare_tag.rb
@@ -23,7 +23,7 @@ class SlideshareTag < LiquidTagBase
 
   def validate(key)
     if key.match?(/\A[a-zA-Z0-9]{14}\Z/)
-      key
+      key.strip
     else
       raise StandardError, "Invalid Slideshare Key"
     end
