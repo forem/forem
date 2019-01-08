b66dba8e7fc0cb2730bf374843ed77e76c1ce179
Zhao-Andy <andyzhao.zhao@gmail.com>
2019-01-08 17:22:02 -0500

    Use consistent syntax for user creation

--- spec/requests/stories_show_spec.rb
+++ spec/requests/stories_show_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 RSpec.describe "StoriesShow", type: :request do
-  let(:user) { FactoryBot.create(:user) }
-  let(:article) { FactoryBot.create(:article, user_id: user.id) }
+  let(:user)         { create(:user) }
+  let(:article)      { create(:article, user_id: user.id) }
 
   describe "GET /:username/:slug" do
     it "renders to appropriate page" do
