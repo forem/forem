a6f1fc9cceb492f31d46872215f4368fee32c28e
Dave Follett <davefollett@gmail.com>
2019-01-15 00:03:43 -0500

    Added the 'initalpath' and 'module' options to the codesandbox embed and updated the help page.

--- app/liquid_tags/codesandbox_tag.rb
+++ app/liquid_tags/codesandbox_tag.rb
@@ -2,10 +2,11 @@ class CodesandboxTag < LiquidTagBase
   def initialize(tag_name, id, tokens)
     super
     @id = parse_id(id)
+    @query = parse_options(id)
   end
 
   def render(_context)
-    '<iframe src="https://codesandbox.io/embed/' + @id + '"
+    '<iframe src="https://codesandbox.io/embed/' + @id + @query + '"
       style="width:100%; height:calc(300px + 8vw); border:0; border-radius: 4px; overflow:hidden;"
       sandbox="allow-same-origin allow-scripts allow-forms allow-top-navigation-by-user-activation"
     </iframe>'
@@ -14,17 +15,38 @@ class CodesandboxTag < LiquidTagBase
   private
 
   def parse_id(input)
-    input_no_space = input.delete(" ")
-    if valid_id?(input_no_space)
-      input_no_space
-    else
-      raise StandardError, "Invalid codesandbox Id"
+    id = input.split(" ").first
+    raise StandardError, "Invalid codesandbox ID" unless valid_id?(id)
 
-    end
+    id
   end
 
   def valid_id?(id)
     id =~ /\A[a-zA-Z0-9\-]{0,60}\Z/
+  end
+
+  def parse_options(input)
+    _, *options = input.split(" ")
+
+    # Validation
+    validated_options = options.map { |o| valid_option(o) }.reject { |e| e == nil }
+    raise StandardError, "Invalid Options" unless options.empty? || !validated_options.empty?
+
+    query = options.join("&")
+
+    if query.blank?
+      query
+    else
+      "?#{query}"
+    end
+  end
+
+  def valid_option(option)
+    if option.start_with?("initalpath=", "module=")
+      option
+    else
+      ""
+    end
   end
 end
 
