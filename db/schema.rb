5ad66244d50fc4798391c2b74a97e63495e6aa38
Ben Halpern <ben@dev.to>
2019-01-15 12:36:10 -0500

    Fix RSS issues by not refetching users as often

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190109212351) do
+ActiveRecord::Schema.define(version: 20190115155656) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20190109212351) do
     t.string "canonical_url"
     t.integer "collection_id"
     t.integer "collection_position"
+    t.string "comment_template"
     t.integer "comments_count", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "crossposted_at"
@@ -744,6 +745,7 @@ ActiveRecord::Schema.define(version: 20190109212351) do
     t.datetime "exported_at"
     t.string "facebook_url"
     t.boolean "feed_admin_publish_permission", default: true
+    t.datetime "feed_fetched_at", default: "2017-01-01 05:00:00"
     t.boolean "feed_mark_canonical", default: false
     t.string "feed_url"
     t.integer "following_orgs_count", default: 0, null: false
