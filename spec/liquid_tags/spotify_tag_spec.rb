7c7403d446ae480a60f95e77cff179ae154e0331
Kyle Boe <ksboe1@gmail.com>
2019-01-16 13:50:43 -0800

    update specs to reflect url removal - #1546

--- spec/liquid_tags/spotify_tag_spec.rb
+++ spec/liquid_tags/spotify_tag_spec.rb
@@ -2,34 +2,20 @@ require "rails_helper"
 
 RSpec.describe SpotifyTag, type: :liquid_template do
   describe "#link" do
-    let(:valid_uri)     { 'spotify:track:0K1UpnetfCKtcNu37rJmCg' }
-    let(:invalid_uri)   { 'asdfasdf:asdfasdf:asdfasdf'}
-    let(:valid_url)     { 'https://open.spotify.com/track/0K1UpnetfCKtcNu37rJmCg'}
-    let(:invalid_url)   { 'https://google.com' }
+    let(:valid_uri)     { "spotify:track:0K1UpnetfCKtcNu37rJmCg" }
+    let(:invalid_uri)   { "asdfasdf:asdfasdf:asdfasdf" }
 
     def generate_tag(link)
       Liquid::Template.register_tag("spotify", SpotifyTag)
       Liquid::Template.parse("{% spotify #{link} %}")
     end
 
-    context 'when link is a URI' do
-      it "does not raise an error if valid" do
-        expect { generate_tag(valid_uri) }.not_to raise_error
-      end
-
-      it "raises an error if invalid" do
-        expect { generate_tag(invalid_uri) }.to raise_error(StandardError, "Invalid Spotify Link - Be sure You're linking to a specific track / album / artist / playlist")
-      end
+    it "does not raise an error if the uri is valid" do
+      expect { generate_tag(valid_uri) }.not_to raise_error
     end
 
-    context 'when link is a URL' do
-      it "does not raise an error if valid" do
-        expect { generate_tag(valid_url) }.not_to raise_error
-      end
-
-      it "raises an error if invalid" do
-        expect { generate_tag(invalid_url) }.to raise_error(StandardError, "Invalid Spotify Link - Be sure You're linking to a specific track / album / artist / playlist")
-      end
+    it "raises an error if the uri is invalid" do
+      expect { generate_tag(invalid_uri) }.to raise_error(StandardError, "Invalid Spotify Link - Be sure You're linking to a specific track / album / artist / playlist")
     end
   end
 end
