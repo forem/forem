65b123f1e2a027f78212c2aaea880480816186ad
Ben Halpern <ben@dev.to>
2019-01-16 15:24:22 -0500

    Fix test score calc

--- spec/features/homepage/user_visits_homepage_articles_spec.rb
+++ spec/features/homepage/user_visits_homepage_articles_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 describe "User visits a homepage", type: :feature do
-  let!(:article) { create(:article, reactions_count: 12, score: 12, featured: true) }
-  let!(:article2) { create(:article, reactions_count: 20, score: 12) }
+  let!(:article) { create(:article, reactions_count: 12, score: 15, featured: true) }
+  let!(:article2) { create(:article, reactions_count: 20, score: 20) }
   let!(:bad_article) { create(:article, reactions_count: 0) }
 
   context "when no options specified" do
@@ -13,6 +13,8 @@ describe "User visits a homepage", type: :feature do
     end
 
     it "shows correct articles", js: true do
+      article.update_column(:score, 15)
+      article2.update_column(:score, 15)
       expect(page).to have_selector(".single-article", count: 2)
       expect(page).to have_text(article.title)
       expect(page).to have_text(article2.title)
