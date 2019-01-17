8722270d21b640db7528fef25db4809ab4c1b881
Andrew Bone <AndrewB05@gmail.com>
2019-01-17 17:31:49 +0000

    Revert test to Approvals

--- spec/liquid_tags/jsfiddle_tag_spec.rb
+++ spec/liquid_tags/jsfiddle_tag_spec.rb
@@ -17,9 +17,9 @@ RSpec.describe JSFiddleTag, type: :liquid_template do
     end
 
     it "accepts jsfiddle link" do
-      expect do
-        generate_new_liquid(jsfiddle_link)
-      end.not_to raise_error
+      liquid = generate_new_liquid(jsfiddle_link)
+      rendered_jsfiddle_iframe = liquid.render
+      Approvals.verify(rendered_jsfiddle_iframe, name: "jsfiddle_liquid_tag", format: :html)
     end
 
     it "accepts jsfiddle link with a / at the end" do
