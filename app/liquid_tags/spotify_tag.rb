36b42b2ccd83e53f80e3b7fe48f3416891b40b41
Kyle Boe <ksboe1@gmail.com>
2019-01-20 11:01:33 -0800

    Feature/spotify liquid tag (#1573)
    * WIP spotify liquid tags and specs - #1546
    
    * update naming for clarity
    
    * update for rubocop styling
    
    * drop url support - #1546
    
    * bugfix and add correct heights - #1546
    
    * update specs to reflect url removal - #1546
    
    * update standard error message in spec - #1573

---
+++ app/liquid_tags/spotify_tag.rb
@@ -0,0 +1,47 @@
+class SpotifyTag < LiquidTagBase
+  URI_REGEXP = /spotify:(track|user|artist|album|episode).+(?<=:)\w{22}/.freeze
+  TYPE_HEIGHT = {
+    track: 80,
+    user: 330,
+    artist: 240,
+    album: 240,
+    episode: 80
+  }.freeze
+
+  def initialize(tag_name, uri, tokens)
+    super
+    @parsed_uri = parse_uri(uri)
+    @height = TYPE_HEIGHT[@parsed_uri[1].to_sym]
+  end
+
+  def render(_context)
+    html = <<-HTML
+      <iframe
+        width="100%"
+        height="#{@height}px"
+        scrolling="no"
+        frameborder="0"
+        allowtransparency="true"
+        allow="encrypted-media"
+        src="#{generate_embed_link(@parsed_uri)}">
+      </iframe>
+    HTML
+    finalize_html(html)
+  end
+
+  private
+
+  def parse_uri(uri)
+    URI_REGEXP.match(uri) || raise_error
+  end
+
+  def generate_embed_link(parsed_uri)
+    parsed_uri.string.split(":")[1..-1].unshift("https://open.spotify.com/embed").join("/")
+  end
+
+  def raise_error
+    raise StandardError, "Invalid Spotify Link - Be sure you're using the uri of a specific track, album, artist, playlist, or podcast episode."
+  end
+end
+
+Liquid::Template.register_tag("spotify", SpotifyTag)
