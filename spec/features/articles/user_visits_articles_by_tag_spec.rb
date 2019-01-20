be05580bfa6219375250bcbc5508aacf18a3cb64
Anna Buianova <lightallloy@gmail.com>
2019-01-20 19:57:24 +0300

    Refactored stories views (#1584)

--- spec/features/articles/user_visits_articles_by_tag_spec.rb
+++ spec/features/articles/user_visits_articles_by_tag_spec.rb
@@ -11,42 +11,59 @@ describe "User visits articles by tag", type: :feature do
   let!(:article3) { create(:article, tags: "functional, javascript", user: author, published_at: Time.now - 2.weeks) }
 
   context "when user hasn't logged in" do
-    before { visit "/t/javascript" }
+    context "when 2 articles" do
+      before { visit "/t/javascript" }
 
-    it "shows the header", js: true do
-      within("h1") { expect(page).to have_text("javascript") }
-    end
+      it "shows the header", js: true do
+        within("h1") { expect(page).to have_text("javascript") }
+      end
 
-    it "shows the follow button", js: true do
-      within("h1") { expect(page).to have_button("+ FOLLOW") }
-    end
+      it "shows the follow button", js: true do
+        within("h1") { expect(page).to have_button("+ FOLLOW") }
+      end
 
-    it "shows time buttons" do
-      within("#on-page-nav-controls") do
-        expect(page).to have_link("WEEK", href: "/t/javascript/top/week")
-        expect(page).to have_link("INFINITY", href: "/t/javascript/top/infinity")
-        expect(page).to have_link("LATEST", href: "/t/javascript/latest")
+      it "shows time buttons" do
+        within("#on-page-nav-controls") do
+          expect(page).to have_link("WEEK", href: "/t/javascript/top/week")
+          expect(page).to have_link("INFINITY", href: "/t/javascript/top/infinity")
+          expect(page).to have_link("LATEST", href: "/t/javascript/latest")
+        end
+      end
+
+      it "shows correct articles count", js: true do
+        expect(page).to have_selector(".single-article", count: 2)
+      end
+
+      it "shows the correct articles" do
+        within("#articles-list") do
+          expect(page).to have_text(article.title)
+          expect(page).to have_text(article3.title)
+          expect(page).not_to have_text(article2.title)
+        end
+      end
+
+      it "when user clicks 'week'" do
+        click_on "WEEK"
+        within("#articles-list") do
+          expect(page).to have_text(article.title)
+          expect(page).not_to have_text(article3.title)
+          expect(page).not_to have_text(article2.title)
+        end
       end
     end
 
-    it "shows correct articles count", js: true do
-      expect(page).to have_selector(".single-article", count: 2)
-    end
-
-    it "shows the correct articles" do
-      within("#articles-list") do
-        expect(page).to have_text(article.title)
-        expect(page).to have_text(article3.title)
-        expect(page).not_to have_text(article2.title)
+    context "when more articles" do
+      before do
+        create_list(:article, 3, tags: "javascript", user: author, published_at: Time.now)
+        visit "/t/javascript"
       end
-    end
 
-    it "when user clicks 'week'" do
-      click_on "WEEK"
-      within("#articles-list") do
-        expect(page).to have_text(article.title)
-        expect(page).not_to have_text(article3.title)
-        expect(page).not_to have_text(article2.title)
+      it "shows the sign_in invitation", js: true do
+        within("#substories") do
+          expect(page).to have_content("amazing humans who code")
+          expect(page).to have_link("TWITTER")
+          expect(page).to have_link("GITHUB")
+        end
       end
     end
   end
