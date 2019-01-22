338ad0a6b5a9cb19c20b7fd4a4f14a81ede5b6e2
Mac Siri <krairit.siri@gmail.com>
2019-01-22 14:57:17 -0500

    Enable require_dependency for Liquid tags in prod (#1613)

--- config/initializers/liquid.rb
+++ config/initializers/liquid.rb
@@ -1,14 +1,9 @@
 # Our custom Liquid tags are registered to Liquid::Template at the bottom of
 # each files. Each Liquid tags will need to be loaded/required before the main
 # Liquid gem is evoked, hence the need for the fix below.
-#
-# Because files are eagerloaded in production, this fix is only
-# applicable in development (and test, when needed)
 
-if Rails.env.development? || Rails.env.test?
-  Rails.application.config.to_prepare do
-    Dir.glob(Rails.root.join("app/liquid_tags/*.rb")).sort.each do |filename|
-      require_dependency filename
-    end
+Rails.application.config.to_prepare do
+  Dir.glob(Rails.root.join("app/liquid_tags/*.rb")).sort.each do |filename|
+    require_dependency filename
   end
 end
