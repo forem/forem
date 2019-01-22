510a2aac48955e39e24d9e4da20c52b67c5ac6ff
Zhao-Andy <andyzhao.zhao@gmail.com>
2019-01-22 14:02:52 -0500

    Use idempotent update instead of toggle

--- app/controllers/article_mutes_controller.rb
+++ app/controllers/article_mutes_controller.rb
@@ -4,7 +4,7 @@ class ArticleMutesController < ApplicationController
   def update
     @article = Article.find_by(id: params[:id], user_id: current_user.id)
     authorize @article
-    @article.update(receive_notifications: !@article.receive_notifications)
+    @article.update(receive_notifications: permitted_attributes(@article)[:receive_notifications])
     redirect_to "/dashboard"
   end
 end
