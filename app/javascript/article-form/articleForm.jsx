13c75f9444c465035fd0e7daee713528f875e3ac
Ali Spittel <ali07cat07@gmail.com>
2019-01-23 12:44:26 -0500

    implement autosave in sessionStorage

--- app/javascript/article-form/articleForm.jsx
+++ app/javascript/article-form/articleForm.jsx
@@ -1,10 +1,12 @@
 import 'preact/devtools';
 import { h, Component } from 'preact';
 import linkState from 'linkstate';
+import ImageUploadIcon from 'images/image-upload.svg';
+import ThreeDotsIcon from 'images/three-dots.svg';
 import { submitArticle, previewArticle } from './actions';
 import BodyMarkdown from './elements/bodyMarkdown';
 import BodyPreview from './elements/bodyPreview';
-import Description from './elements/description';
+// import Description from './elements/description';
 import PublishToggle from './elements/publishToggle';
 import Notice from './elements/notice';
 import Tags from './elements/tags';
@@ -14,8 +16,6 @@ import ImageManagement from './elements/imageManagement';
 import MoreConfig from './elements/moreConfig';
 import OrgSettings from './elements/orgSettings';
 import Errors from './elements/errors';
-import ImageUploadIcon from 'images/image-upload.svg';
-import ThreeDotsIcon from 'images/three-dots.svg';
 // import CodeMirror from 'codemirror';
 // import 'codemirror/mode/markdown/markdown';
 
@@ -27,6 +27,9 @@ export default class ArticleForm extends Component {
     const organization = this.props.organization
       ? JSON.parse(this.props.organization)
       : null;
+
+    this.url = window.location.href;
+
     this.state = {
       id: article.id || null,
       title: article.title || '',
@@ -47,13 +50,38 @@ export default class ArticleForm extends Component {
       moreConfigShowing: false,
       mainImage: article.main_image || null,
       organization,
-      postUnderOrg: article.organization_id ? true : false,
+      postUnderOrg: !!article.organization_id,
       errors: null,
     };
   }
 
   componentDidMount() {
     initEditorResize();
+
+    window.addEventListener('beforeunload', e => {
+      sessionStorage.setItem(
+        this.url,
+        JSON.stringify({
+          title: this.state.title,
+          tagList: this.state.tagList,
+          mainImage: this.state.mainImage,
+          bodyMarkdown: this.state.bodyMarkdown,
+        }),
+      );
+      e.returnValue = '';
+    });
+
+    const previousContent = JSON.parse(
+      sessionStorage.getItem(window.location.href),
+    );
+    if (previousContent) {
+      this.setState({
+        title: previousContent.title || '',
+        tagList: previousContent.tagList || '',
+        mainImage: previousContent.mainImage || null,
+        bodyMarkdown: previousContent.bodyMarkdown || '',
+      });
+    }
     // const editor = document.getElementById('article_body_markdown');
     // const myCodeMirror = CodeMirror(editor, {
     //   mode: 'markdown',
@@ -121,10 +149,10 @@ export default class ArticleForm extends Component {
 
   handleConfigChange = e => {
     e.preventDefault();
-    let newState = {}
+    const newState = {};
     newState[e.target.name] = e.target.value;
-    this.setState(newState)
-  }
+    this.setState(newState);
+  };
 
   handleMainImageUrlChange = payload => {
     this.setState({
@@ -214,47 +242,64 @@ export default class ArticleForm extends Component {
     const errorsArea = errors ? <Errors errorsList={errors} /> : '';
     let editorView = '';
     if (previewShowing) {
-      editorView = <div>{errorsArea}{orgArea}{imageArea}<BodyPreview previewHTML={previewHTML} articleState={this.state} version="article-preview" /></div>;
+      editorView = (
+        <div>
+          {errorsArea}
+          {orgArea}
+          {imageArea}
+          <BodyPreview
+            previewHTML={previewHTML}
+            articleState={this.state}
+            version="article-preview"
+          />
+        </div>
+      );
     } else if (helpShowing) {
       editorView = <BodyPreview previewHTML={helpHTML} version="help" />;
     } else {
-        editorView = <div>
-                      {errorsArea}
-                      {orgArea}
-                      {imageArea}
-                      <Title defaultValue={title} onChange={linkState(this, 'title')} />
-                      <div className="articleform__detailfields">
-                        <Tags defaultValue={tagList} onInput={linkState(this, 'tagList')} />
-                        <button
-                          className="articleform__detailsButton articleform__detailsButton--image"
-                          onClick={this.toggleImageManagement}
-                        >
-                          <img src={ImageUploadIcon} /> IMAGES
-                        </button>
-                        <button
-                          className="articleform__detailsButton articleform__detailsButton--moreconfig"
-                          onClick={this.toggleMoreConfig}
-                        >
-                          <img src={ThreeDotsIcon} />
-                        </button>
-                      </div>
-                      <BodyMarkdown
-                        defaultValue={bodyMarkdown}
-                        onChange={linkState(this, 'bodyMarkdown')}
-                      />
-                        <button
-                          className="articleform__detailsButton articleform__detailsButton--image articleform__detailsButton--bottom"
-                          onClick={this.toggleImageManagement}
-                        >
-                          <img src={ImageUploadIcon} /> IMAGES
-                        </button>
-                        <button
-                          className="articleform__detailsButton articleform__detailsButton--moreconfig articleform__detailsButton--bottom"
-                          onClick={this.toggleMoreConfig}
-                        >
-                          <img src={ThreeDotsIcon} />
-                        </button>
-                      </div>
+      editorView = (
+        <div>
+          {errorsArea}
+          {orgArea}
+          {imageArea}
+          <Title defaultValue={title} onChange={linkState(this, 'title')} />
+          <div className="articleform__detailfields">
+            <Tags defaultValue={tagList} onInput={linkState(this, 'tagList')} />
+            <button
+              className="articleform__detailsButton articleform__detailsButton--image"
+              onClick={this.toggleImageManagement}
+            >
+              <img src={ImageUploadIcon} />
+              {' '}
+IMAGES
+            </button>
+            <button
+              className="articleform__detailsButton articleform__detailsButton--moreconfig"
+              onClick={this.toggleMoreConfig}
+            >
+              <img src={ThreeDotsIcon} />
+            </button>
+          </div>
+          <BodyMarkdown
+            defaultValue={bodyMarkdown}
+            onChange={linkState(this, 'bodyMarkdown')}
+          />
+          <button
+            className="articleform__detailsButton articleform__detailsButton--image articleform__detailsButton--bottom"
+            onClick={this.toggleImageManagement}
+          >
+            <img src={ImageUploadIcon} />
+            {' '}
+IMAGES
+          </button>
+          <button
+            className="articleform__detailsButton articleform__detailsButton--moreconfig articleform__detailsButton--bottom"
+            onClick={this.toggleMoreConfig}
+          >
+            <img src={ThreeDotsIcon} />
+          </button>
+        </div>
+      );
     }
     return (
       <form className="articleform__form" onSubmit={this.onSubmit}>
