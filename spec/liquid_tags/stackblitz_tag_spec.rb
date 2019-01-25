613d3d49c3e82989a1565c62ca3f37c7b7c932d0
Andrew Bone <AndrewB05@gmail.com>
2019-01-25 17:03:01 +0000

    Coding style consistency (stackblitz liquid tag) (#1649)
    * Style consistancy
    
    Make style more consistent with other liquid tags
    
    * Style consistency
    
    Make style more consistent with other liquid tags
    
    * Style consistency
    
    Make style more consistent with other liquid tags
    
    * rubocop compliance
    
    * rubocop compliance

--- spec/liquid_tags/stackblitz_tag_spec.rb
+++ spec/liquid_tags/stackblitz_tag_spec.rb
@@ -3,6 +3,13 @@ require "rails_helper"
 RSpec.describe StackblitzTag, type: :liquid_template do
   describe "#id" do
     let(:stackblitz_id) { "ball-demo" }
+    let(:stackblitz_id_with_view) { "ball-demo view=preview" }
+    
+    xss_links = %w(
+      //evil.com/?ball-demo
+      https://ball-demo.evil.com
+      ball-demo" onload='alert("xss")'
+    )
 
     def generate_new_liquid(id)
       Liquid::Template.register_tag("stackblitz", StackblitzTag)
@@ -12,6 +19,24 @@ RSpec.describe StackblitzTag, type: :liquid_template do
     it "renders iframe" do
       liquid = generate_new_liquid(stackblitz_id)
       expect(liquid.render).to include("<iframe")
+    end
+
+    it "rejects invalid stackblitz id" do
+      expect do
+        generate_new_liquid("https://google.com")
+      end.to raise_error(StandardError)
+    end
+
+    it "accepts stackblitz id with a view parameter" do
+      expect do
+        generate_new_liquid(stackblitz_id_with_view)
+      end.not_to raise_error
+    end
+
+    it "rejects XSS attempts" do
+      xss_links.each do |link|
+        expect { generate_new_liquid(link) }.to raise_error(StandardError)
+      end
     end
   end
 end
