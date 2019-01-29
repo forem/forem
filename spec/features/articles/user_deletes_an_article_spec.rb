4c2144fe3934da0c03e57ed38a2a7a0a80bbf22d
Mac Siri <krairit.siri@gmail.com>
2019-01-29 15:47:08 -0500

    Remove JS from spec

--- spec/features/articles/user_deletes_an_article_spec.rb
+++ spec/features/articles/user_deletes_an_article_spec.rb
@@ -1,17 +1,13 @@
 require "rails_helper"
 
-RSpec.describe "Deleting Article", js: true, type: :feature do
+RSpec.describe "Deleting Article", type: :feature do
   let(:article) { create(:article) }
 
-  before do
-    # Notification.send_to_followers(article, "Published")
-  end
-
-  it "author of article deletes own article", driver: :chrome do
+  it "author of article deletes own article" do
     sign_in article.user
     visit "/dashboard"
     click_on "DELETE"
-    click_on "DELETE"
+    click_on "DELETE" # This is for confirming deletion
     expect(page).to have_text("Write your first post now")
   end
 end
