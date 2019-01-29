1e212a8eaf43b7f0de696f8049ac04b633f35d1a
Mac Siri <krairit.siri@gmail.com>
2019-01-29 17:57:43 -0500

    Refactor moderation spec

--- spec/requests/moderations_spec.rb
+++ spec/requests/moderations_spec.rb
@@ -1,48 +1,45 @@
 require "rails_helper"
 
-RSpec.describe "Moderations", type: :request do
+RSpec.describe "Moderations", type: :request, proper_status: true do
   let(:user) { create(:user) }
-  let(:article) { create(:article, user_id: user.id) }
+  let(:article) { create(:article, user: user) }
   let(:comment) do
     create(:comment,
            commentable_id: article.id,
            commentable_type: "Article",
-           user_id: user.id)
+           user: user)
   end
 
-  describe "GET /mod on articles" do
-    context "when user is trusted" do
-      it "responds with 200" do
-        user.add_role :trusted
-        sign_in user
-        get article.path + "/mod"
-        expect(response).to have_http_status(200)
-      end
+  context "when user is not trusted" do
+    before do
+      sign_in user
     end
 
-    context "when user is not trusted", proper_status: true do
-      it "responds with 404" do
-        get article.path + "/mod"
-        expect(response).to have_http_status(404)
-      end
+    it "does not grant access article moderation" do
+      get "/username/random-article/mod"
+      expect(response).to have_http_status(404)
+    end
+
+    it "does not grant access to comment moderation" do
+      get "/username/comment/1/mod"
+      expect(response).to have_http_status(404)
     end
   end
 
-  describe "GET moderations comment" do
-    context "when user is trusted" do
-      it "responds with 200" do
-        user.add_role :trusted
-        sign_in user
-        get comment.path + "/mod"
-        expect(response).to have_http_status(200)
-      end
+  context "when user is trusted" do
+    before do
+      user.add_role :trusted
+      sign_in user
     end
 
-    context "when user is not trusted", proper_status: true do
-      it "responds with 404" do
-        get comment.path + "/mod"
-        expect(response).to have_http_status(404)
-      end
+    it "grant acess to article moderation" do
+      get article.path + "/mod"
+      expect(response).to have_http_status(200)
+    end
+
+    it "grant acess to comment moderation" do
+      get comment.path + "/mod"
+      expect(response).to have_http_status(200)
     end
   end
 end
