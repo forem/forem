e9e4e20bd2b94f6dfe0f4bbb5c398fff9bed3ed4
Mac Siri <krairit.siri@gmail.com>
2019-01-30 16:19:36 -0500

    Update moderation spec

--- spec/requests/moderations_spec.rb
+++ spec/requests/moderations_spec.rb
@@ -1,44 +1,49 @@
 require "rails_helper"
 
-RSpec.describe "Moderations", type: :request, proper_status: true do
-  let(:user) { create(:user) }
-  let(:article) { create(:article, user: user) }
-  let(:comment) do
-    create(:comment,
-           commentable_id: article.id,
-           commentable_type: "Article",
-           user: user)
+RSpec.shared_examples "an elevated privilege required request" do |path|
+  context "when not logged-in" do
+    it "does not grant acesss", proper_status: true do
+      get path
+      expect(response).to have_http_status(404)
+    end
+
+    it "raises Pundit::NotAuthorizedError internally" do
+      expect { get path }.to raise_error(Pundit::NotAuthorizedError)
+    end
   end
 
   context "when user is not trusted" do
-    before do
-      sign_in user
-    end
+    before { sign_in create(:user) }
 
-    it "does not grant access article moderation" do
-      get "/username/random-article/mod"
+    it "does not grant acesss", proper_status: true do
+      get path
       expect(response).to have_http_status(404)
     end
 
-    it "does not grant access to comment moderation" do
-      get "/username/comment/1/mod"
-      expect(response).to have_http_status(404)
+    it "internally raise Pundit::NotAuthorized internally" do
+      expect { get path }.to raise_error(Pundit::NotAuthorizedError)
     end
   end
+end
+
+RSpec.describe "Moderations", type: :request do
+  let(:user) { create(:user, :trusted) }
+  let(:article) { create(:article) }
+  let(:comment) { create(:comment, commentable: article) }
+
+  it_behaves_like "an elevated privilege required request", "/username/random-article/mod"
+  it_behaves_like "an elevated privilege required request", "/username/comment/1/mod"
 
   context "when user is trusted" do
-    before do
-      user.add_role :trusted
-      sign_in user
+    before { sign_in user }
+
+    it "grant acess to comment moderation" do
+      get comment.path + "/mod"
+      expect(response).to have_http_status(200)
     end
 
     it "grant acess to article moderation" do
       get article.path + "/mod"
-      expect(response).to have_http_status(200)
-    end
-
-    it "grant acess to comment moderation" do
-      get comment.path + "/mod"
       expect(response).to have_http_status(200)
     end
   end
