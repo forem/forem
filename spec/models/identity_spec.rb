68f54022ab35c98f684ea43525e9d1283df8c819
Mac Siri <krairit.siri@gmail.com>
2019-02-04 12:57:15 -0500

    Create Identity spec

---
+++ spec/models/identity_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe Identity, type: :model do
+  it { is_expected.to belong_to(:user) }
+  it { is_expected.to validate_presence_of(:uid) }
+  it { is_expected.to validate_presence_of(:provider) }
+  it { is_expected.to validate_uniqueness_of(:uid).scoped_to(:provider) }
+  it { is_expected.to validate_uniqueness_of(:provider).scoped_to(:uid) }
+  it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(:provider) }
+  it { is_expected.to validate_inclusion_of(:provider).in_array(%w(github twitter)) }
+  it { is_expected.to serialize(:auth_data_dump) }
+
+  describe ".find_for_oauth" do
+    it "works" do
+      allow(Identity).to receive(:find_or_create_by)
+      auth = { uid: 0, provider: "github" }
+      described_class.find_for_oauth(instance_double("request", auth))
+      expect(Identity).to have_received(:find_or_create_by).with(auth)
+    end
+  end
+end
