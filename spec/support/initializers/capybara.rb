c3769f68cb82257d183ee09d75f7352d6362aada
Mac Siri <krairit.siri@gmail.com>
2019-02-04 17:10:54 -0500

    Revert capybara config

--- spec/support/initializers/capybara.rb
+++ spec/support/initializers/capybara.rb
@@ -4,12 +4,24 @@ require "selenium/webdriver"
 Capybara.server = :puma, { Silent: true }
 Capybara.default_max_wait_time = 5
 
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.register_driver :headless_chrome do |app|
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+      chromeOptions: { args: %w(headless disable-gpu no-sandbox window-size=1400,2000) },
+    )
+end
+
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :rack_test
   end
 
   config.before(:each, type: :system, js: true) do
-    driven_by :selenium_chrome_headless
+    driven_by :headless_chrome
   end
 end
