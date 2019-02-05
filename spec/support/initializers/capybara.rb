1923eb8f3425d8dbfa15e32a23693b4ad99567af
Mac Siri <krairit.siri@gmail.com>
2019-02-05 16:50:09 -0500

    Change capybara driver

--- spec/support/initializers/capybara.rb
+++ spec/support/initializers/capybara.rb
@@ -11,7 +11,6 @@ RSpec.configure do |config|
   end
 
   config.before(:each, type: :system, js: true) do
-    # driven_by :selenium_chrome_headless
-    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+    driven_by :selenium_chrome_headless, screen_size: [1400, 1400]
   end
 end
