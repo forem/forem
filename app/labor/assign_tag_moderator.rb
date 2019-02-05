989d5ae554fb132962fe8253fa4f751ca2aff996
Jess Lee <jessleenyc@gmail.com>
2019-02-05 17:18:38 -0500

    create tag show page and add tag moderation form

--- app/labor/assign_tag_moderator.rb
+++ app/labor/assign_tag_moderator.rb
@@ -1,8 +1,8 @@
 module AssignTagModerator
-  def self.add_tag_moderators(user_ids, tag_names)
+  def self.add_tag_moderators(user_ids, tag_ids)
     user_ids.each_with_index do |user_id, index|
       user = User.find(user_id)
-      tag = Tag.find_by(name: tag_names[index])
+      tag = Tag.find(tag_ids[index])
       user.add_role(:tag_moderator, tag)
       if user.chat_channels.find_by(slug: "tag-moderators").blank?
         ChatChannel.find_by(slug: "tag-moderators").add_users(user)
