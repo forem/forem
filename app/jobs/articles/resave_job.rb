83f1401643803de0f9a30f6b337cc24b4119d933
lightalloy <lightallloy@gmail.com>
2019-02-06 10:53:33 +0300

    Add spec for the articles resave_job

--- app/jobs/articles/resave_job.rb
+++ app/jobs/articles/resave_job.rb
@@ -2,8 +2,7 @@ module Articles
   class ResaveJob < ApplicationJob
     queue_as :articles_resave
 
-    def perform(article_ids)
-      cache_buster = CacheBuster.new
+    def perform(article_ids, cache_buster = CacheBuster.new)
       Article.where(id: article_ids).find_each do |article|
         cache_buster.bust(article.path)
         cache_buster.bust(article.path + "?i=i")
--- spec/jobs/articles/resave_job_spec.rb
+++ spec/jobs/articles/resave_job_spec.rb
@@ -8,5 +8,17 @@ RSpec.describe Articles::ResaveJob, type: :job do
         Articles::ResaveJob.perform_later([1, 2])
       end.to have_enqueued_job.with([1, 2]).on_queue("articles_resave")
     end
+
+    it "busts cache" do
+      article = create(:article)
+      path = article.path
+
+      cache_buster = double
+      allow(cache_buster).to receive(:bust)
+
+      Articles::ResaveJob.perform_now([article.id], cache_buster)
+      expect(cache_buster).to have_received(:bust).with(path).once
+      expect(cache_buster).to have_received(:bust).with(path + "?i=i").once
+    end
   end
 end
