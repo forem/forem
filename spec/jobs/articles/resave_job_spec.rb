39b943de7081fd82b3064d2c663b6f5377f12f7d
lightalloy <lightallloy@gmail.com>
2019-02-06 11:11:03 +0300

    Refactored job specs a bit

--- spec/jobs/articles/resave_job_spec.rb
+++ spec/jobs/articles/resave_job_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Articles::ResaveJob, type: :job do
     it "enqueues the job" do
       ActiveJob::Base.queue_adapter = :test
       expect do
-        Articles::ResaveJob.perform_later([1, 2])
+        described_class.perform_later([1, 2])
       end.to have_enqueued_job.with([1, 2]).on_queue("articles_resave")
     end
 
@@ -16,7 +16,7 @@ RSpec.describe Articles::ResaveJob, type: :job do
       cache_buster = double
       allow(cache_buster).to receive(:bust)
 
-      Articles::ResaveJob.perform_now([article.id], cache_buster)
+      described_class.perform_now([article.id], cache_buster)
       expect(cache_buster).to have_received(:bust).with(path).once
       expect(cache_buster).to have_received(:bust).with(path + "?i=i").once
     end
--- spec/jobs/articles/score_calc_job_spec.rb
+++ spec/jobs/articles/score_calc_job_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Articles::ScoreCalcJob, type: :job do
     it "enqueues the job" do
       ActiveJob::Base.queue_adapter = :test
       expect do
-        Articles::ScoreCalcJob.perform_later(1)
+        described_class.perform_later(1)
       end.to have_enqueued_job.with(1).on_queue("articles_score_calc")
     end
   end
