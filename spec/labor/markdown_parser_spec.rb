58771c9f7209f07e3d4340724991de4af07775cc
Carl Saunders <carl.saunders@deadlybyte.co.uk>
2019-02-07 21:25:45 +0000

    Added <kbd> tag support to markdown editors in article posts and comments.

--- spec/labor/markdown_parser_spec.rb
+++ spec/labor/markdown_parser_spec.rb
@@ -67,6 +67,13 @@ RSpec.describe MarkdownParser do
     end
   end
 
+  context "when provided with kbd tag" do
+    it "leaves the kbd tag in place" do
+      inline_kbd = generate_and_parse_markdown("<kbd>Ctrl</kbd> + <kbd>,</kbd>")
+      expect(inline_code).to eq('<kbd>Ctrl</kbd> + <kbd>,</kbd>')
+    end
+  end
+
   describe "#tags_used" do
     let(:parsed_markdown) { described_class.new("{% youtube oHg5SJYRHA0 %}") }
 
