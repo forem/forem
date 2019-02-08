54e9f08180b4421b94f6c7a0e44eb54640e2d52f
Mac Siri <krairit.siri@gmail.com>
2019-02-08 12:12:03 -0500

    Improve cancellation spec

--- app/controllers/stripe_cancellations_controller.rb
+++ app/controllers/stripe_cancellations_controller.rb
@@ -2,11 +2,11 @@ class StripeCancellationsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :create
 
   def create
-    verify_stripe_payload
-    event
+    verify_stripe_signature
+    unsubscribe_customer
   end
 
-  def verify_stripe_payload
+  def verify_stripe_signature
     payload = request.body.read
     sig_header = request.env["HTTP_STRIPE_SIGNATURE"]
 
@@ -26,13 +26,13 @@ class StripeCancellationsController < ApplicationController
   end
 
   def unsubscribe_customer
-    Stripe::Event.retrieve(params[:id])
+    event = Stripe::Event.retrieve(params[:id])
     stripe_id = event.data.object.customer
     customer = Stripe::Customer.retrieve(stripe_id)
     monthly_dues = event.data.object.items.data[0].plan.amount
     user = User.where(stripe_id_code: stripe_id).first
     MembershipService.new(customer, user, monthly_dues).unsubscribe_customer
-    render body: nil, status: 201
+    render body: nil, status: 200
   rescue Stripe::APIConnectionError, Stripe::StripeError
     render body: nil, status: 400
   end
--- spec/requests/stripe_cancellations_spec.rb
+++ spec/requests/stripe_cancellations_spec.rb
@@ -1,35 +1,32 @@
 require "rails_helper"
 
 RSpec.describe "StripeCancellations", type: :request do
-  let(:user) { create(:user, :super_admin) }
-  let(:mock_instance) { instance_double(MembershipService) }
+  let(:user) { create(:user) }
   let(:stripe_helper) { StripeMock.create_test_helper }
-  let(:stripe_event_client) { instance_double (Stripe::Event) }
-  let(:stubbed_stripe_event)
 
   before do
-    allow_any_instance_of(StripeCancellationsController).to receive(:verify_stripe_payload).and_return(:true)
     StripeMock.start
-    sign_in user
-    user.update(stripe_id_code: "CUS_123")
+    allow_any_instance_of(StripeCancellationsController).to receive(:verify_stripe_signature).and_return(true)
   end
 
   after { StripeMock.stop }
 
   it "mocks a stripe cancellation webhook" do
+    customer = Stripe::Customer.create(
+      email: user.email,
+      source: stripe_helper.generate_card_token,
+    )
+    MembershipService.new(customer, user, 12).subscribe_customer
+    user.reload
+    expect(user.monthly_dues).not_to eq(0)
+
     event = StripeMock.mock_webhook_event(
-      'customer.subscription.deleted',
-      { :customer => user.stripe_id_code, :total_count => 1 })
-
-    monthly_dues = event.data.object.items.data[0].plan.amount
-      binding.pry
-    post "/stripe_subscriptions", params: {
-      amount: monthly_dues,
-      stripe_token: stripe_helper.generate_card_token
-    }
-
+      "customer.subscription.deleted",
+      customer: user.stripe_id_code, total_count: 1,
+    )
     post "/stripe_cancellations", params: event.as_json
+    user.reload
     expect(user.monthly_dues).to eq(0)
+    expect(response).to have_http_status(200)
   end
-
 end
