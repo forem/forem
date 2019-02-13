70d12364bcce1b4da324352a94446c85de1653c0
Jess Lee <jessleenyc@gmail.com>
2019-02-13 15:56:19 -0500

    Add Stripe Cancellation Webhook (#1682)

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,7 @@
 Rails.configuration.stripe = {
   publishable_key: ApplicationConfig["STRIPE_PUBLISHABLE_KEY"],
-  secret_key: ApplicationConfig["STRIPE_SECRET_KEY"]
+  secret_key: ApplicationConfig["STRIPE_SECRET_KEY"],
+  stripe_cancellation_secret: ApplicationConfig["STRIPE_CANCELLATION_SECRET"]
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
