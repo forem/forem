002ae0fad902113bd9d4684d616b9e8e05beb361
Carl Saunders <carl.saunders@deadlybyte.co.uk>
2019-02-17 02:30:35 +0000

    Added <kbd> tag support to markdown editors in article posts and comm… (#1761)
    * Added <kbd> tag support to markdown editors in article posts and comments.
    
    * Fixed typo in test.
    
    * Fixed test - now compares against inner html.
    
    * Removed .showpage-signi-cta as dead code was reinstated accidentally via git merge.
    
    * Use sass placeholder as no need to output the .kbd class in the css.
    
    * Reinstated original css as accidentally changed with merge.

--- app/labor/markdown_parser.rb
+++ app/labor/markdown_parser.rb
@@ -39,7 +39,7 @@ class MarkdownParser
     renderer = Redcarpet::Render::HTMLRouge.new(hard_wrap: true, filter_html: false)
     markdown = Redcarpet::Markdown.new(renderer, REDCARPET_CONFIG)
     allowed_tags = %w(strong abbr aside em p h1 h2 h3 h4 h5 h6 i u b code pre
-                      br ul ol li small sup sub img a span hr blockquote)
+                      br ul ol li small sup sub img a span hr blockquote kbd)
     allowed_attributes = %w(href strong em ref rel src title alt class)
     ActionController::Base.helpers.sanitize markdown.render(@content).html_safe,
     tags: allowed_tags,
