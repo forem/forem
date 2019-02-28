909a47d6ea8513d7ab16a68beaba98fe2a678f6f
Ben Halpern <ben@dev.to>
2019-02-28 13:03:19 -0500

    Slight modification in /mod for exp level stuff

--- app/controllers/moderations_controller.rb
+++ app/controllers/moderations_controller.rb
@@ -7,7 +7,7 @@ class ModerationsController < ApplicationController
       includes(:rating_votes).
       where("rating_votes_count < 3").
       where("score > -5").
-      order("featured_number DESC").limit(50)
+      order("hotness_score DESC").limit(50)
     if params[:tag].present?
       @articles = @articles.
         cached_tagged_with(params[:tag])
