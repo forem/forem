5b856c89c32021d41818133a90a24419cd7db3dd
Miguel Piedrafita <github@miguelpiedrafita.com>
2019-02-28 21:51:20 +0100

    Implement Blogcast liquid tag (#1891)

---
+++ app/liquid_tags/blogcast_tag.rb
@@ -0,0 +1,38 @@
+class BlogcastTag < LiquidTagBase
+  def initialize(tag_name, id, tokens)
+    super
+    @id = parse_id(id)
+  end
+
+  def render(_context)
+    html = <<-HTML
+      <div class="ltag_blogcast">
+        <iframe frameborder="0"
+          scrolling="no"
+          id="blogcast_#{@id}"
+          mozallowfullscreen="true"
+          src="https://blogcast.host/embed/#{@id}"
+          style="width:100%;height:132px;overflow:hidden;"
+          webkitallowfullscreen="true"></iframe>
+      </div>
+    HTML
+    finalize_html(html)
+  end
+
+  private
+
+  def parse_id(input)
+    input_no_space = input.delete(" ")
+    if valid_id?(input_no_space)
+      input_no_space
+    else
+      raise StandardError, "Invalid Blogcast Id"
+    end
+  end
+
+  def valid_id?(id)
+    (id =~ /\A\d{1,9}\Z/i)&.zero?
+  end
+end
+
+Liquid::Template.register_tag("blogcast", BlogcastTag)
