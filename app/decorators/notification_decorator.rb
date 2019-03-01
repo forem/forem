94aab65611cd61521c1647a46cb7bcfb8c040f57
Andy Zhao <andyzhao.zhao@gmail.com>
2019-03-01 15:25:45 -0500

    Add Milestone Notifications (#1920)
    * Test milestone notifications
    
    * Add initial milestone notifications
    
    * Add extra milestone goalposts
    
    * Move send reaction milestone
    
    * Use proper twilio-video package version
    
    * Add random giphys gifs to milestones
    
    * Make images responsive and add addl gifs
    
    * Add guard statement for older articles
    
    * Update app/views/notifications/_milestone.html.erb
    
    Link to Giphy page instead of source page
    
    * Generate gifs via class and factor in aspect ratio
    
    * Add border radius to gifs
    
    Co-Authored-By: Zhao-Andy <andyzhao.zhao@gmail.com>

--- app/decorators/notification_decorator.rb
+++ app/decorators/notification_decorator.rb
@@ -10,4 +10,12 @@ class NotificationDecorator < Draper::Decorator
     end
     struct.new(json_data[type]["class"]["name"], json_data[type]["id"])
   end
+
+  def milestone_type
+    action.split("::")[1]
+  end
+
+  def milestone_count
+    action.split("::")[2]
+  end
 end
