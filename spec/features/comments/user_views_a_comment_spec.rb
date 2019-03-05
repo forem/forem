48b6c8fe520bf7fea9835790e896fd21087f20c3
rhymes <rhymesete@gmail.com>
2019-03-05 13:03:41 +0100

    Use Timecop to avoid flakiness due to time zones in tests

--- spec/features/comments/user_views_a_comment_spec.rb
+++ spec/features/comments/user_views_a_comment_spec.rb
@@ -3,24 +3,27 @@ require "rails_helper"
 RSpec.describe "Viewing a comment", type: :feature, js: true do
   let(:user) { create(:user) }
   let(:article) { create(:article, user_id: user.id, show_comments: true) }
-  let!(:comment) { create(:comment, commentable: article, user: user) }
+  let(:comment) { create(:comment, commentable: article, user: user) }
+  let!(:timestamp) { "2019-03-04T10:00:00Z" }
 
   before do
+    Timecop.freeze(timestamp)
     sign_in user
     visit comment.path
   end
 
+  after do
+    Timecop.return
+  end
+
   context "when showing the date" do
     it "shows the readable publish date" do
-      # %e blank pads days from 1 to 9, but the double space isn't in the HTML
-      comment_date = comment.readable_publish_date.gsub("  ", " ")
-      expect(page).to have_selector(".comment-date", text: comment_date)
+      expect(page).to have_selector(".comment-date", text: "Mar 4")
     end
 
     it "embeds the published timestamp" do
-      ts = comment.decorate.published_timestamp
-      timestamp_selector = ".comment-date[data-published-timestamp='#{ts}']"
-      expect(page).to have_selector(timestamp_selector)
+      selector = ".comment-date[data-published-timestamp='#{timestamp}']"
+      expect(page).to have_selector(selector)
     end
   end
 end
