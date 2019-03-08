45bddaefbb0edeb0fa12d59ca7bf873dda2aafab
haved <krogstie.havard@gmail.com>
2019-03-08 13:09:11 +0100

    Fix codeclimate issues

--- app/assets/javascripts/utilities/timeAgo.js
+++ app/assets/javascripts/utilities/timeAgo.js
@@ -1,13 +1,3 @@
-function timeAgo(oldTimeInSeconds, maxDisplayedAge = 60*60*24-1) {
-  const timeNow = new Date() / 1000;
-  const diff = Math.round(timeNow - oldTimeInSeconds);
-
-  if(diff > maxDisplayedAge)
-    return '';
-
-  return "<span class='time-ago-indicator'>(" + secondsToHumanUnitAgo(diff) + ")</span>";
-}
-
 function secondsToHumanUnitAgo(seconds) {
 	const times = [
 		['second', 1],
@@ -23,14 +13,25 @@ function secondsToHumanUnitAgo(seconds) {
 		return "Just now";
 
 	let scale = 0;
-	//If the amount of seconds is more than a minute, we change the scale to minutes
-	//If the amount of seconds then is more than an hour, we change the scale to hours
-	//This continues until the unit above our current scale is longer than `seconds`, or doesn't exist
+	// If the amount of seconds is more than a minute, we change the scale to minutes
+	// If the amount of seconds then is more than an hour, we change the scale to hours
+	// This continues until the unit above our current scale is longer than `seconds`, or doesn't exist
 	while(scale+1 < times.length && seconds >= times[scale+1][1])
-		scale++;
+		scale+=1;
 
 	const wholeUnits = Math.floor(seconds / times[scale][1]);
 	const unitName = times[scale][0] + (wholeUnits === 1 ? '' : 's');
 
 	return wholeUnits + " " + unitName + " ago";
 }
+
+function timeAgo(oldTimeInSeconds, maxDisplayedAge = 60*60*24-1) {
+  const timeNow = new Date() / 1000;
+  const diff = Math.round(timeNow - oldTimeInSeconds);
+
+  if(diff > maxDisplayedAge)
+    return '';
+
+  return "<span class='time-ago-indicator'>(" + secondsToHumanUnitAgo(diff) + ")</span>";
+}
+
