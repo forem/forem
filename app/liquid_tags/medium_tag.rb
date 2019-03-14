45b4a4b8e6b4608384f21e981b743d837fbb51c7
Edison Yap <edisonywh@gmail.com>
2019-03-15 00:19:36 +0800

    Add ability to embed Medium URL as liquid tag
    - Added new liquid tag - "{% medium https://medium.com}"
    - Added a scraper - `MediumArticleRetrievalService` which crawls the
    given Medium URL for informations needed for the liquid tag

---
+++ app/liquid_tags/medium_tag.rb
@@ -0,0 +1,43 @@
+class MediumTag < LiquidTagBase
+  include ApplicationHelper
+  include ActionView::Helpers::TagHelper
+  attr_reader :response
+
+  def initialize(_tag_name, url, _tokens)
+    @response = parse_url_for_medium_article(url)
+  end
+
+  def render(_context)
+    <<-HTML
+      <div class='ltag__link'>
+        <a href='#{response[:url]}' class='ltag__link__link'>
+          <div class='ltag__link__pic'>
+            <img src='#{response[:author_image]}' alt='#{response[:author]}'/>
+          </div></a>
+          <a href='#{response[:url]}' class='ltag__link__link'>
+            <div class='ltag__link__content'>
+              <h2>#{response[:title]}</h2>
+              <h3>#{response[:author]}</h3>
+              <div class='ltag__link__taglist'>#{response[:reading_time]}</div>
+            </div>
+        </a>
+      </div>
+    HTML
+  end
+
+  private
+
+  def parse_url_for_medium_article(url)
+    sanitized_article_url = ActionController::Base.helpers.strip_tags(url).strip
+
+    MediumArticleRetrievalService.new(sanitized_article_url).call
+  rescue StandardError
+    raise_error
+  end
+
+  def raise_error
+    raise StandardError, "Invalid link URL or link URL does not exist"
+  end
+end
+
+Liquid::Template.register_tag("medium", MediumTag)
