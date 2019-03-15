4ca8338c1c731e2d929be5fdab8c178bbbd92920
Ben Halpern <ben@dev.to>
2019-03-15 18:29:01 -0400

    Add originally_published_at for more accurate crossposted info

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190315151829) do
+ActiveRecord::Schema.define(version: 20190315222044) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 20190315151829) do
     t.string "main_tag_name_for_social"
     t.string "name_within_collection"
     t.integer "organization_id"
+    t.datetime "originally_published_at"
     t.integer "page_views_count", default: 0
     t.boolean "paid", default: false
     t.string "password"
