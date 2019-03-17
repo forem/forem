6fadfad682ceb52441a36abf9100ee6fe01e8858
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2019-03-17 13:15:42 -0700

    Add ability to respond to .png for social previews controller
    When adding .png to the url, these routes will now generate an image and
    redirect to it.
    
    This leaves the existing pages as they were, so we're backwards
    compatible with all the existing links out in the world.

--- spec/requests/social_previews_spec.rb
+++ spec/requests/social_previews_spec.rb
@@ -6,10 +6,31 @@ RSpec.describe "SocialPreviews", type: :request do
   let(:organization) { create(:organization) }
   let(:article) { create(:article, user_id: user.id) }
 
+  before do
+    stub_request(:post, /hcti.io/).
+      to_return(status: 200,
+                body: '{ "url": "https://hcti.io/v1/image/6c52de9d-4d37-4008-80f8-67155589e1a1" }',
+                headers: { "Content-Type" => "application/json" })
+  end
+
   describe "GET /social_previews/article/:id" do
     it "renders proper article title" do
       get "/social_previews/article/#{article.id}"
       expect(response.body).to include CGI.escapeHTML(article.title)
+    end
+
+    it "renders shecoded template when tagged with shecoded" do
+      she_coded_article = create(:article, tags: "shecoded")
+
+      get "/social_previews/article/#{she_coded_article.id}"
+
+      expect(response).to render_template(:shecoded)
+    end
+
+    it "requesting png generates an image and redirects to image url" do
+      get "/social_previews/article/#{article.id}.png"
+
+      expect(response).to redirect_to("https://hcti.io/v1/image/6c52de9d-4d37-4008-80f8-67155589e1a1")
     end
   end
 
@@ -18,19 +39,37 @@ RSpec.describe "SocialPreviews", type: :request do
       get "/social_previews/user/#{user.id}"
       expect(response.body).to include CGI.escapeHTML(user.name)
     end
+
+    it "requesting png generates an image and redirects to image url" do
+      get "/social_previews/user/#{user.id}.png"
+
+      expect(response).to redirect_to("https://hcti.io/v1/image/6c52de9d-4d37-4008-80f8-67155589e1a1")
+    end
   end
 
-  describe "GET /social_previews/user/:id" do
+  describe "GET /social_previews/organization/:id" do
     it "renders proper organization name" do
       get "/social_previews/organization/#{organization.id}"
       expect(response.body).to include CGI.escapeHTML(organization.name)
     end
+
+    it "requesting png generates an image and redirects to image url" do
+      get "/social_previews/organization/#{organization.id}.png"
+
+      expect(response).to redirect_to("https://hcti.io/v1/image/6c52de9d-4d37-4008-80f8-67155589e1a1")
+    end
   end
 
-  describe "GET /social_previews/user/:id" do
+  describe "GET /social_previews/tag/:id" do
     it "renders proper tag name" do
       get "/social_previews/tag/#{tag.id}"
       expect(response.body).to include CGI.escapeHTML(tag.name)
+    end
+
+    it "requesting png generates an image and redirects to image url" do
+      get "/social_previews/tag/#{tag.id}.png"
+
+      expect(response).to redirect_to("https://hcti.io/v1/image/6c52de9d-4d37-4008-80f8-67155589e1a1")
     end
   end
 end
