dab9fd0186d686f24728a44f0ffd8c3a39ac128d
Zhao-Andy <andyzhao.zhao@gmail.com>
2019-03-20 10:52:46 -0400

    Add tech support permission method

--- app/models/user.rb
+++ app/models/user.rb
@@ -310,6 +310,10 @@ class User < ApplicationRecord
     has_role?(:super_admin) || has_role?(:admin)
   end
 
+  def tech_support?
+    has_role?(:tech_support) || has_role(:super_admin?)
+  end
+
   def trusted
     Rails.cache.fetch("user-#{id}/has_trusted_role", expires_in: 200.hours) do
       has_role? :trusted
