7d59e15e6d96d28dc835b1c3777d946180a6f373
Zhao-Andy <andyzhao.zhao@gmail.com>
2019-03-20 10:51:19 -0400

    Add tech support role

--- app/models/role.rb
+++ app/models/role.rb
@@ -13,6 +13,7 @@ class Role < ApplicationRecord
               in: %w[
                 super_admin
                 admin
+                tech_support
                 tag_moderator
                 trusted
                 banned
