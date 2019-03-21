031467f8d8a6d53fe72bbbfa84f1d9e3dd92e75b
Zhao-Andy <andyzhao.zhao@gmail.com>
2019-03-21 13:27:17 -0400

    Refactor pro dashboard and chart decorator

--- app/facades/dashboard/pro.rb
+++ app/facades/dashboard/pro.rb
@@ -1,75 +1,40 @@
 module Dashboard
   class Pro
-    attr_reader :user_or_org
+    attr_reader :user_or_org, :article_data, :article_ids, :reaction_data, :comment_data, :follow_data
 
     def initialize(user_or_org)
       @user_or_org = user_or_org
+      @article_data ||= Article.where("#{user_or_org.class.name.downcase}_id" => user_or_org.id, published: true)
+      @article_ids ||= @article_data.pluck(:id)
+      @reaction_data ||= Reaction.two_months_data(@article_ids)
+      @comment_data ||= Comment.two_months_data(@article_ids)
+      @follow_data ||= Follow.two_months_data(followable_type: user_or_org.class.name, followable_id: user_or_org.id)
     end
 
-    def user_or_org_article_ids
-      @user_or_org_article_ids ||=
-        Article.where("#{user_or_org.class.name.downcase}_id" => user_or_org.id, published: true).pluck(:id)
+    def data_by_time(timeframe, type)
+      ChartDecorator.decorate(
+        send("#{type}_data").select do |model_instance|
+          case timeframe
+          when :this_week
+            model_instance.created_at > 1.week.ago
+          when :last_week
+            model_instance.created_at > 2.weeks.ago && model_instance.created_at < 1.week.ago
+          when :this_month
+            model_instance.created_at > 1.month.ago
+          when :last_month
+            model_instance.created_at > 2.months.ago && model_instance.created_at < 1.month.ago
+          end
+        end.sort_by(&:created_at).reverse, # comma is for last argument because Rubocop
+      )
     end
 
-    def this_week_reactions
-      ChartDecorator.decorate(Reaction.where(reactable_id: user_or_org_article_ids, reactable_type: "Article").where("created_at > ?", 1.week.ago).order("created_at ASC"))
-    end
-
-    def this_week_reactions_count
-      this_week_reactions.size
-    end
-
-    def last_week_reactions_count
-      Reaction.where(reactable_id: user_or_org_article_ids, reactable_type: "Article").where("created_at > ? AND created_at < ?", 2.weeks.ago, 1.week.ago).size
-    end
-
-    def this_month_reactions_count
-      Reaction.where(reactable_id: user_or_org_article_ids, reactable_type: "Article").where("created_at > ?", 1.month.ago).size
-    end
-
-    def last_month_reactions_count
-      Reaction.where(reactable_id: user_or_org_article_ids, reactable_type: "Article").where("created_at > ? AND created_at < ?", 2.months.ago, 1.months.ago).size
-    end
-
-    def this_week_comments
-      ChartDecorator.decorate(Comment.where(commentable_id: user_or_org_article_ids, commentable_type: "Article").where("created_at > ?", 1.week.ago))
-    end
-
-    def this_week_comments_count
-      this_week_comments.size
-    end
-
-    def last_week_comments_count
-      Comment.where(commentable_id: user_or_org_article_ids, commentable_type: "Article").where("created_at > ? AND created_at < ?", 2.weeks.ago, 1.week.ago).size
-    end
-
-    def this_month_comments_count
-      Comment.where(commentable_id: user_or_org_article_ids, commentable_type: "Article").where("created_at > ?", 1.month.ago).size
-    end
-
-    def last_month_comments_count
-      Comment.where(commentable_id: user_or_org_article_ids, commentable_type: "Article").where("created_at > ? AND created_at < ?", 2.months.ago, 1.months.ago).size
-    end
-
-    def this_week_followers_count
-      Follow.where(followable_id: user_or_org.id, followable_type: user_or_org.class.name).where("created_at > ?", 1.week.ago).size
-    end
-
-    def last_week_followers_count
-      Follow.where(followable_id: user_or_org.id, followable_type: user_or_org.class.name).where("created_at > ? AND created_at < ?", 2.weeks.ago, 1.week.ago).size
-    end
-
-    def this_month_followers_count
-      Follow.where(followable_id: user_or_org.id, followable_type: user_or_org.class.name).where("created_at > ?", 1.month.ago).size
-    end
-
-    def last_month_followers_count
-      Follow.where(followable_id: user_or_org.id, followable_type: user_or_org.class.name).where("created_at > ? AND created_at < ?", 2.months.ago, 1.months.ago).size
+    def total_views
+      article_data.pluck(:organic_page_views_count, :page_views_count).flatten.sum
     end
 
     def reactors
-      User.where(id: Reaction.where(reactable_id: user_or_org_article_ids, reactable_type: "Article").
-        order("created_at DESC").limit(100).pluck(:user_id))
+      User.where(id: Reaction.where(reactable_id: article_ids, reactable_type: "Article").
+        order("created_at DESC").limit(100).pluck(:user_id).uniq)
     end
   end
 end
