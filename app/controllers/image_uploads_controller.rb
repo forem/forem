df2621a8c056f512e1718c96ece263a0f05526da
rhymes <rhymesete@gmail.com>
2019-03-21 17:31:51 +0100

    Implement image upload's error handling (#2018)

--- app/controllers/image_uploads_controller.rb
+++ app/controllers/image_uploads_controller.rb
@@ -4,13 +4,28 @@ class ImageUploadsController < ApplicationController
 
   def create
     authorize :image_upload
+
     uploader = ArticleImageUploader.new
-    uploader.store!(params[:image])
+    begin
+      uploader.store!(params[:image])
+    rescue CarrierWave::IntegrityError => e # client error
+      respond_to do |format|
+        format.json { render json: { error: e.message }, status: :unprocessable_entity }
+      end
+      return
+    rescue CarrierWave::ProcessingError # server error
+      respond_to do |format|
+        format.json { render json: { error: "A server error has occurred!" }, status: :server_error }
+      end
+      return
+    end
+
     link = if params[:wrap_cloudinary]
              ApplicationController.helpers.cloud_cover_url(uploader.url)
            else
              uploader.url
            end
+
     respond_to do |format|
       format.json { render json: { link: link }, status: 200 }
     end
