c56113e335480cf0ffd80f24bce61f6b039063e0
Anna Buianova <lightallloy@gmail.com>
2019-03-28 23:10:23 +0300

    Optimize sign in: Identity #2061 (#2231)
    * Add unique index to identity
    
    * Conditional uniqueness validations on identities
    
    * Remove redundant Identity specs

---
+++ db/migrate/20190327090030_add_indexes_to_indentities.rb
@@ -0,0 +1,6 @@
+class AddIndexesToIndentities < ActiveRecord::Migration[5.1]
+  def change
+    add_index :identities, %i[provider uid], unique: true
+    add_index :identities, %i[provider user_id], unique: true
+  end
+end
