d0b06f2f6f738bb3679c003fb6f9f7c5af37f1b5
lightalloy <lightallloy@gmail.com>
2019-03-28 11:37:40 +0300

    Remove redundant Identity specs

--- spec/models/identity_spec.rb
+++ spec/models/identity_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe Identity, type: :model do
   it { is_expected.to validate_presence_of(:uid) }
   it { is_expected.to validate_presence_of(:provider) }
   it { is_expected.to validate_uniqueness_of(:uid).scoped_to(:provider) }
-  # it { is_expected.to validate_uniqueness_of(:provider).scoped_to(:uid) }
   it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(:provider) }
   it { is_expected.to validate_inclusion_of(:provider).in_array(%w[github twitter]) }
   it { is_expected.to serialize(:auth_data_dump) }
@@ -16,23 +15,6 @@ RSpec.describe Identity, type: :model do
       auth = { uid: 0, provider: "github" }
       described_class.find_for_oauth(instance_double("request", auth))
       expect(Identity).to have_received(:find_or_create_by).with(auth)
-    end
-  end
-
-  context "when a new record" do
-    it "validates uniqueness of provider + uid" do
-      create(:identity, provider: "twitter", uid: 100, user: create(:user))
-      identity = build(:identity, provider: "twitter", uid: 100, user: create(:user))
-      expect(identity).not_to be_valid
-      expect(identity.errors[:uid].size).to eq(1)
-    end
-
-    it "validates uniqueness of provider + user_id" do
-      user = create(:user)
-      create(:identity, provider: "twitter", uid: 100, user: user)
-      identity = build(:identity, provider: "twitter", uid: 11, user: user)
-      expect(identity).not_to be_valid
-      expect(identity.errors[:user_id].size).to eq(1)
     end
   end
 end
