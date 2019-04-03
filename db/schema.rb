05098f063c41723250babdf32481823eb399742f
Mac Siri <krairit.siri@gmail.com>
2019-04-03 14:14:40 -0400

    Update Rails version on README [ci skip]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190401213605) do
+ActiveRecord::Schema.define(version: 2019_04_01_213605) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
