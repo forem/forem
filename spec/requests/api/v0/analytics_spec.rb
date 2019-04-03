a8eb0c7b6e7bc3785f8222f2281bf792254616db
Zhao-Andy <andyzhao.zhao@gmail.com>
2019-04-03 15:55:44 -0400

    Update tests to use request headers and add stricter tests

--- spec/requests/api/v0/analytics_spec.rb
+++ spec/requests/api/v0/analytics_spec.rb
@@ -1,14 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "Api::V0::Analytics", type: :request do
-  let(:user)              { create(:user) }
-  let(:api_token)         { create(:api_secret, user: user) }
-  let(:org)               { create(:organization) }
-  let(:pro_user)          { create(:user, :pro) }
-  let(:pro_api_token)     { create(:api_secret, user: pro_user) }
-  let(:pro_org_member)    { create(:user, :pro, :org_member) }
-  let(:org_member_token)  { create(:api_secret, user: pro_org_member) }
-
   describe "GET /api/analytics/totals" do
     include_examples "GET /api/analytics/:endpoint authorization examples", "totals"
   end
@@ -17,8 +9,28 @@ RSpec.describe "Api::V0::Analytics", type: :request do
     include_examples "GET /api/analytics/:endpoint authorization examples", "historical", "&start=2019-03-29"
 
     context "when the start parameter is not included" do
-      it "raises an ArgumentError" do
-        expect { get "/api/analytics/historical" }.to raise_error ArgumentError
+      before { get "/api/analytics/historical" }
+
+      it "renders a status of 422" do
+        expect(response.status).to eq 422
+      end
+
+      it "renders the proper error message in JSON" do
+        error_message = "Required 'start' parameter is missing"
+        expect(JSON.parse(response.body)["error"]).to eq error_message
+      end
+    end
+
+    context "when the start parameter has the incorrect format" do
+      before { get "/api/analytics/historical?start=2019/2/2" }
+
+      it "renders a status of 422" do
+        expect(response.status).to eq 422
+      end
+
+      it "renders the proper error message in JSON" do
+        error_message = "Date parameters 'start' or 'end' must be in the format of 'yyyy-mm-dd'"
+        expect(JSON.parse(response.body)["error"]).to eq error_message
       end
     end
   end
--- spec/support/api_analytics_shared_examples.rb
+++ spec/support/api_analytics_shared_examples.rb
@@ -1,21 +1,38 @@
-RSpec.shared_examples "GET /api/analytics/:endpoint authorization examples" do |endpoint, extra_params|
+RSpec.shared_examples "GET /api/analytics/:endpoint authorization examples" do |endpoint, params|
+  let(:user)              { create(:user) }
+  let(:api_token)         { create(:api_secret, user: user) }
+  let(:org)               { create(:organization) }
+  let(:pro_user)          { create(:user, :pro) }
+  let(:pro_api_token)     { create(:api_secret, user: pro_user) }
+  let(:pro_org_member)    { create(:user, :pro, :org_member) }
+  let(:org_member_token)  { create(:api_secret, user: pro_org_member) }
+
   context "when an invalid token is given" do
-    it "raises the ActiveRecord::RecordNotFound error" do
-      invalid_token = "abadskajdlsak"
-      expect { get "/api/analytics/#{endpoint}?api_token=#{invalid_token}#{extra_params}" }.
-        to raise_error ActiveRecord::RecordNotFound
+    before { get "/api/analytics/#{endpoint}?#{params}", headers: { "HTTP_API_KEY" => "abadskajdlsak" } }
+
+    it "renders an error message: 'Not authorized' in JSON" do
+      expect(JSON.parse(response.body)["error"]).to eq "Not authorized"
+    end
+
+    it "has a status 401" do
+      expect(response.status).to eq 401
     end
   end
 
   context "when a valid token is given but the user is not a pro" do
-    it "raises the ActiveRecord::RecordNotFound error" do
-      expect { get "/api/analytics/#{endpoint}?api_token=#{api_token.secret}#{extra_params}" }.
-        to raise_error ActiveRecord::RecordNotFound
+    before { get "/api/analytics/#{endpoint}?#{params}", headers: { "HTTP_API_KEY" => "abadskajdlsak" } }
+
+    it "renders an error message: 'Not authorized' in JSON" do
+      expect(JSON.parse(response.body)["error"]).to eq "Not authorized"
+    end
+
+    it "has a status 401" do
+      expect(response.status).to eq 401
     end
   end
 
   context "when a valid token is given and the user is a pro" do
-    before { get "/api/analytics/#{endpoint}?api_token=#{pro_api_token.secret}#{extra_params}" }
+    before { get "/api/analytics/#{endpoint}?#{params}", headers: { "HTTP_API_KEY" => pro_api_token.secret } }
 
     it "renders JSON as the content type" do
       expect(response.content_type).to eq "application/json"
@@ -23,15 +40,22 @@ RSpec.shared_examples "GET /api/analytics/:endpoint authorization examples" do |
   end
 
   context "when attempting to view organization analytics without belonging to the organization" do
-    it "raises the ActiveRecord::RecordNotFound error" do
-      expect { get "/api/analytics/#{endpoint}?api_token=#{pro_api_token.secret}&organization_id=#{org.id}#{extra_params}" }.
-        to raise_error ActiveRecord::RecordNotFound
+    before do
+      get "/api/analytics/#{endpoint}?organization_id=#{org.id}#{params}", headers: { "HTTP_API_KEY" => pro_api_token.secret }
+    end
+
+    it "renders an error message: 'Not authorized' in JSON" do
+      expect(JSON.parse(response.body)["error"]).to eq "Not authorized"
+    end
+
+    it "has a status 401" do
+      expect(response.status).to eq 401
     end
   end
 
   context "when attempting to view organization analytics and being a member of the organization" do
     before do
-      get "/api/analytics/#{endpoint}?api_token=#{org_member_token.secret}&organization_id=#{pro_org_member.organization_id}#{extra_params}"
+      get "/api/analytics/#{endpoint}?organization_id=#{pro_org_member.organization_id}#{params}", headers: { "HTTP_API_KEY" => org_member_token.secret }
     end
 
     it "renders JSON as the content type" do
@@ -40,10 +64,10 @@ RSpec.shared_examples "GET /api/analytics/:endpoint authorization examples" do |
   end
 
   context "when attempting to view another organization analytics and not belonging to that organization" do
-    it "raises the ActiveRecord::RecordNotFound error" do
+    it "responds with status 401 unauthorized" do
       org = create(:organization)
-      expect { get "/api/analytics/#{endpoint}?api_token=#{org_member_token.secret}&organization_id=#{org.id}#{extra_params}" }.
-        to raise_error ActiveRecord::RecordNotFound
+      get "/api/analytics/#{endpoint}?organization_id=#{org.id}#{params}", headers: { "HTTP_API_KEY" => org_member_token.secret }
+      expect(response.status).to eq 401
     end
   end
 end
