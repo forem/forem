76b0d22d5dfe610f75ff8b5ecbae2ca61d7e2bbd
Zhao-Andy <andyzhao.zhao@gmail.com>
2019-04-03 12:14:03 -0400

    Enforce date parameters to follow a specific format

--- app/controllers/api/v0/analytics_controller.rb
+++ app/controllers/api/v0/analytics_controller.rb
@@ -20,6 +20,7 @@ module Api
 
       def historical
         raise ArgumentError, "Required 'start' parameter is missing" if params[:start].blank?
+        raise ArgumentError, "Date parameters 'start' or 'end' must be in the format of 'yyyy-mm-dd'" unless valid_date_params?
 
         user = get_authenticated_user!
 
@@ -64,6 +65,15 @@ module Api
 
       def belongs_to_org?(user, org)
         user.organization_id == org.id
+      end
+
+      def valid_date_params?
+        date_regex = /\A\d{4}-\d{1,2}-\d{1,2}\Z/ # for example, 2019-03-22 or 2019-2-1
+        if params[:end]
+          (params[:start] =~ date_regex)&.zero? && (params[:end] =~ date_regex)&.zero?
+        else
+          (params[:start] =~ date_regex)&.zero?
+        end
       end
     end
   end
