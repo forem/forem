47d87dcffa468ef62d2e6762d0db638ed6bdf6ae
Ben Halpern <ben@dev.to>
2019-04-04 18:42:10 -0400

    Finalize suggest-a-tweet functionality

--- app/controllers/buffer_updates_controller.rb
+++ app/controllers/buffer_updates_controller.rb
@@ -14,7 +14,7 @@ class BufferUpdatesController < ApplicationController
     BufferUpdate.create(
       article_id: @article.id,
       composer_user_id: current_user.id,
-      body_text: params[:buffer_update][:body_text],
+      body_text: modified_body_text,
       social_service_name: "twitter",
       buffer_profile_id_code: ApplicationConfig["BUFFER_TWITTER_ID"],
       status: "pending",
@@ -29,10 +29,10 @@ class BufferUpdatesController < ApplicationController
         BufferUpdate.create(
           article_id: @article.id,
           composer_user_id: current_user.id,
-          body_text: params[:buffer_update][:body_text],
+          body_text: modified_body_text,
           social_service_name: "twitter",
           buffer_profile_id_code: tag.buffer_profile_id_code,
-          tag_id: params[:buffer_update][:tag_id],
+          tag_id: tag.id,
           status: "pending",
         )
       end
@@ -42,9 +42,11 @@ class BufferUpdatesController < ApplicationController
   def modified_body_text
     @user = @article.user
     if @user.twitter_username.present?
-      params[:buffer_update][:body_text] + "\n{ author: @#{@user.twitter_username} } #DEVCommunity"
+      params[:buffer_update][:body_text] +
+        "\n\n{ author: @#{@user.twitter_username} } #DEVCommunity\n#{ApplicationConfig['APP_PROTOCOL']}#{ApplicationConfig['APP_DOMAIN']}#{@article.path}"
     else
-      params[:buffer_update][:body_text]
+      params[:buffer_update][:body_text] +
+        " #DEVCommunity\n#{ApplicationConfig['APP_PROTOCOL']}#{ApplicationConfig['APP_DOMAIN']}#{@article.path}"
     end
   end
 end
