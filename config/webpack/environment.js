136fd4ecaa2e97a7c1cb80d939f664a6290a8dbf
Mac Siri <krairit.siri@gmail.com>
2019-04-04 17:37:24 -0400

    Replace CommonsChunkPlugin with SplitChunk

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,34 +1,5 @@
-// const { environment } = require('@rails/webpacker');
-// const customConfig = require('./custom');
-
-// environment.config.set('resolve.extensions', ['.foo', '.bar']);
-// environment.config.set('output.filename', '[name].js');
-// environment.config.merge(customConfig);
-// environment.config.delete('output.chunkFilename');
-
-// module.exports = environment;
-
-
 const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
 
-environment.plugins.append(
-  'CommonsChunkVendor',
-  new webpack.optimize.CommonsChunkPlugin({
-    name: 'vendor',
-    minChunks: (module) => {
-      // this assumes your vendor imports exist in the node_modules directory
-      return module.context && module.context.indexOf('node_modules') !== -1
-    }
-  })
-)
-
-environment.plugins.append(
-  'CommonsChunkManifest',
-  new webpack.optimize.CommonsChunkPlugin({
-    name: 'manifest',
-    minChunks: Infinity
-  })
-)
+environment.splitChunks();
 
 module.exports = environment
