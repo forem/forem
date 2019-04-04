a731cca0be9f3f1926e0765dcb90533bf063e0fc
rhymes <rhymesete@gmail.com>
2019-04-04 23:36:31 +0200

    Enable per_form_csrf_tokens

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -6,8 +6,5 @@
 #
 # Read the Guide for Upgrading Ruby on Rails for more info on each option.
 
-# Enable per-form CSRF tokens. Previous versions had false.
-Rails.application.config.action_controller.per_form_csrf_tokens = false
-
 # Enable origin-checking CSRF mitigation. Previous versions had false.
 Rails.application.config.action_controller.forgery_protection_origin_check = false
