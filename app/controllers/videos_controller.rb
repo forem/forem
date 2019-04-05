175569fb975d4ea2b3dc37348570ae0e1efbd400
Jess Lee <jessleenyc@gmail.com>
2019-04-05 14:02:03 -0400

    "/videos" (#2291)
    * set up video route
    
    * add video index
    
    * update styling
    
    * fix mobile full width styling
    
    * bust cache when new video is created
    
    * add unlimited scrolling

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,13 +1,19 @@
 class VideosController < ApplicationController
-  after_action :verify_authorized
+  after_action :verify_authorized, except: %i[index]
+  before_action :set_cache_control_headers
 
   def new
     authorize :video
   end
 
+  def index
+    @video_articles = Article.where.not(video: nil, video_thumbnail_url: nil).where(published: true).order("published_at DESC").page(params[:page].to_i).per(12)
+  end
+
   def create
     authorize :video
     @article = ArticleWithVideoCreationService.new(article_params, current_user).create!
+    CacheBuster.new.bust "/videos"
 
     render action: "js_response"
   end
