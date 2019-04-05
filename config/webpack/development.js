ba905a9dba7f2770e3f0f2b3fcf62c4f354cacb7
Mac Siri <krairit.siri@gmail.com>
2019-04-05 12:07:09 -0400

    Remove source maps config

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,7 +1,3 @@
 const environment = require('./environment');
-const config = environment.toWebpackConfig();
 
-// For more information, see https://webpack.js.org/configuration/devtool/#devtool
-config.devtool = 'eval-source-map';
-
-module.exports = config;
+module.exports = environment.toWebpackConfig();
