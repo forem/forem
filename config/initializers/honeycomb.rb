1c2b3b6dd680cd0a7e75ede271ccbe01dfb06d60
rhymes <rhymesete@gmail.com>
2019-04-05 20:38:58 +0200

    Fix Style/GlobalVars

--- config/initializers/honeycomb.rb
+++ config/initializers/honeycomb.rb
@@ -1,21 +1,27 @@
 require "libhoney"
 
+module Honeycomb
+  class << self
+    attr_accessor :client
+  end
+end
+
 key = ApplicationConfig["HONEYCOMB_API_KEY"]
 dataset = "dev.to-#{Rails.env}"
 
-$libhoney = if Rails.env.development? || Rails.env.test?
-              Libhoney::NullClient.new
-            else
-              Libhoney::Client.new(
-                writekey: key,
-                dataset: dataset,
-                user_agent_addition: HoneycombRails::USER_AGENT_SUFFIX,
-              )
-            end
+Honeycomb.client = if Rails.env.development? || Rails.env.test?
+                     Libhoney::NullClient.new
+                   else
+                     Libhoney::Client.new(
+                       writekey: key,
+                       dataset: dataset,
+                       user_agent_addition: HoneycombRails::USER_AGENT_SUFFIX,
+                     )
+                   end
 
 HoneycombRails.configure do |conf|
   conf.writekey = key
   conf.dataset = dataset
   conf.db_dataset = "dev.to-db-#{Rails.env}"
-  conf.client = $libhoney
+  conf.client = Honeycomb.client
 end
