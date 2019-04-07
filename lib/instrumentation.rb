069ff333844effa55a7d81ec4f4d9b216bc00680
rhymes <rhymesete@gmail.com>
2019-04-07 15:23:45 +0200

    Use default instrumentation and add user info to event

--- lib/instrumentation.rb
+++ lib/instrumentation.rb
@@ -1,6 +1,10 @@
 require "rack/honeycomb/middleware"
 
 module Instrumentation
+  def self.included(base)
+    base.before_action :add_user_info_to_honeycomb_event
+  end
+
   def add_param_context(*keys)
     keys.each do |key|
       Rack::Honeycomb.add_field(request.env, key, params[key])
@@ -13,10 +17,10 @@ module Instrumentation
     end
   end
 
-  def append_to_honeycomb(request, controller_name)
-    Rack::Honeycomb.add_field(request.env, "trace.trace_id", request.request_id)
-    Rack::Honeycomb.add_field(request.env, "trace.span_id", request.request_id)
-    Rack::Honeycomb.add_field(request.env, :service_name, "rails")
-    Rack::Honeycomb.add_field(request.env, :name, controller_name)
+  def add_user_info_to_honeycomb_event
+    return unless current_user
+
+    Rack::Honeycomb.add_field(request.env, "user.id", current_user.id)
+    Rack::Honeycomb.add_field(request.env, "user.email", current_user.email)
   end
 end
