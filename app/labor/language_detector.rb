bd95c6a8bf234af9ef35d2eff06f43ff39bf4d2a
Ben Halpern <bendhalpern@gmail.com>
2019-04-08 15:47:29 -0400

    Fix human language detection (#2338)
    * Update language detection
    
    * Modify feed to account for languages

--- app/labor/language_detector.rb
+++ app/labor/language_detector.rb
@@ -5,26 +5,17 @@ class LanguageDetector
 
   def detect
     response = get_language
-    response["result"] if response["confidence"] > 0.8
+    response[:code] if response[:reliable]
   rescue StandardError
     nil
   end
 
   def get_language
-    return { "result" => "en", "confidence" => 0.9 } unless Rails.env.production?
-
-    client = Algorithmia.client(ApplicationConfig["ALGORITHMIA_KEY"])
-    algo = client.algo("miguelher/LanguageDetector/0.1.0")
-    algo.pipe(text).result
+    CLD.detect_language(text)
   end
 
   def text
-    @article.title + "\n" +
-      non_default_description.to_s +
+    @article.title + ". " +
       FrontMatterParser::Parser.new(:md).call(@article.body_markdown).content.split("`")[0]
-  end
-
-  def non_default_description
-    @article.description + "\n" unless @article.description.include? "From the DEV community"
   end
 end
