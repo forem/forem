f3a6effedcfc382ce8b03c983905323e62e270fe
Mario See <mariocsee@users.noreply.github.com>
2019-04-11 17:05:36 -0400

    dev ascii art and console log message

---
+++ app/assets/javascripts/hello-dev.js
@@ -0,0 +1,27 @@
+var bigDev =
+  '                                                                            \n \
+                                                                            \n \
+                                                                            \n \
+                                                                            \n \
+                                                                            \n \
+              -oooooooo/-      .+ooooooooo:  +ooo+        oooo/             \n \
+              +MMMMMMMMMMm+   -NMMMMMMMMMMs  +MMMM:      /MMMM/             \n \
+              +MMMNyyydMMMMy  /MMMMyyyyyyy/   mMMMd      mMMMd              \n \
+              +MMMm    :MMMM. /MMMN           /MMMM/    /MMMM:              \n \
+              +MMMm    .MMMM- /MMMN            dMMMm    mMMMh               \n \
+              +MMMm    .MMMM- /MMMMyyyy+       :MMMM/  +MMMM-               \n \
+              +MMMm    .MMMM- /MMMMMMMMy        hMMMm  NMMMy                \n \
+              +MMMm    .MMMM- /MMMMoooo:        -MMMM+oMMMM-                \n \
+              +MMMm    .MMMM- /MMMN              yMMMmNMMMy                 \n \
+              +MMMm    +MMMM. /MMMN              .MMMMMMMM.                 \n \
+              +MMMMdddNMMMMo  /MMMMddddddd+       sMMMMMMs                  \n \
+              +MMMMMMMMMNh:   .mMMMMMMMMMMs        yMMMMs                   \n \
+              .///////:-        -/////////-         .::.                    \n \
+                                                                            \n \
+                                                                            \n \
+                                                                            \n \
+                                                                           \n';
+console.log(
+  bigDev,
+  "Hey there! Interested in the code behind dev.to? Well you're in luck - we're open source! Come say hi, tell us about your problems, or even lend a hand in our repo - https://github.com/thepracticaldev/dev.to/"
+);
