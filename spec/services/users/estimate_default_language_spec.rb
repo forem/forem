4abceb705130d742f704c9f8ebd46778206ed80b
lightalloy <lightallloy@gmail.com>
2019-04-11 13:42:04 +0300

    Move estimating user language logic to a service object

---
+++ spec/services/users/estimate_default_language_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+RSpec.describe Users::EstimateDefaultLanguage, type: :service do
+  xit "estimates default language when the email is nil" do
+    no_email_user = create(:user, email: nil)
+    described_class.call(no_email_user)
+    no_email_user.reload
+    expect(no_email_user.estimated_default_language).to eq(nil)
+  end
+
+  it "estimates default language to be nil" do
+    user = create(:user)
+    described_class.call(user)
+    expect(user.estimated_default_language).to eq(nil)
+  end
+
+  it "estimates default language to be japan with jp email" do
+    user = create(:user, email: "anna@example.jp")
+    described_class.call(user)
+    expect(user.estimated_default_language).to eq("ja")
+  end
+
+  it "estimates default language based on identity data dump" do
+    user = create(:user)
+    create(:identity, provider: :twitter, user: user, auth_data_dump: { "extra" => { "raw_info" => { "lang" => "it" } } })
+    described_class.call(user)
+    user.reload
+    expect(user.estimated_default_language).to eq("it")
+  end
+
+  it "sets preferred_languages_array" do
+    user = create(:user, email: "annaboo@example.jp")
+    described_class.call(user)
+    user.reload
+    expect(user.decorate.preferred_languages_array).to include("ja")
+  end
+end
