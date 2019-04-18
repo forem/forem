a4fdb8b349de9e28add3d7b8f610f76fa75c1896
Ali Spittel <ali07cat07@gmail.com>
2019-04-18 13:45:12 -0400

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -755,7 +755,6 @@ ActiveRecord::Schema.define(version: 2019_04_17_171019) do
   end
 
   create_table "users", id: :serial, force: :cascade do |t|
-    t.boolean "allows_article_edits", default: true
     t.integer "articles_count", default: 0, null: false
     t.string "available_for"
     t.integer "badge_achievements_count", default: 0, null: false
