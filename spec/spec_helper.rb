7ba586d50839b6d3a1071c5a628699a58c7ad238
Ben Halpern <bendhalpern@gmail.com>
2019-04-18 11:56:38 -0400

    Temporarily comment out random (#2486)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -97,7 +97,7 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = :random
+  # config.order = :random
 
   # Seed global randomization in this process using the `--seed` CLI option.
   # Setting this allows you to use `--seed` to deterministically reproduce
