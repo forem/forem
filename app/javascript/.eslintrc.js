2c14c42bb6a780369d325d3979178fc69e737329
Nick Taylor <nick@iamdeveloper.com>
2019-04-19 10:58:21 -0400

    Fix some frontend linting issues (#2495) [ci skip]

--- app/javascript/.eslintrc.js
+++ app/javascript/.eslintrc.js
@@ -22,9 +22,12 @@ module.exports = {
       },
     ],
     'import/prefer-default-export': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   },
   globals: {
     InstantClick: false,
     filterXSS: false,
+    Pusher: false,
+    algoliasearch: false,
   },
 };
