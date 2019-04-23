a4584efc276905562a31de3d14a31dde324bb194
rhymes <rhymesete@gmail.com>
2019-04-23 18:38:11 +0200

    Add mailers specs for tracking (#2523)
    * Add tracking and missing specs for DigestMailer
    
    * Add tracking and missing specs for MembershipMailer
    
    * Add tracking and missing specs for NotifyMailer
    
    * Add tracking and missing specs for ScholarshipMailer

--- spec/mailers/membership_mailer_spec.rb
+++ spec/mailers/membership_mailer_spec.rb
@@ -5,32 +5,73 @@ RSpec.describe MembershipMailer, type: :mailer do
 
   describe "#new_membership_subscription_email" do
     it "renders proper subject" do
-      user = create(:user)
-      new_membership_subscription_email = described_class.new_membership_subscription_email(
-        user, "level_1_member"
-      )
-      expect(new_membership_subscription_email.subject).to include("Thanks for subscribing")
+      email = described_class.new_membership_subscription_email(user, "level_1_member")
+      expect(email.subject).to include("Thanks for subscribing")
     end
 
     it "renders proper receiver" do
-      user = create(:user)
-      new_membership_subscription_email = described_class.new_membership_subscription_email(
-        user, "level_1_member"
-      )
-      expect(new_membership_subscription_email.to).to eq([user.email])
+      email = described_class.new_membership_subscription_email(user, "level_1_member")
+      expect(email.to).to eq([user.email])
+    end
+
+    it "includes the tracking pixel" do
+      email = described_class.new_membership_subscription_email(user, "level_1_member")
+      expect(email.html_part.body).to include("open.gif")
+    end
+
+    it "includes UTM params" do
+      email = described_class.new_membership_subscription_email(user, "level_1_member")
+      expect(email.html_part.body).to include(CGI.escape("utm_medium=email"))
+      expect(email.html_part.body).to include(CGI.escape("utm_source=membership_mailer"))
+      expect(email.html_part.body).to include(CGI.escape("utm_campaign=new_membership_subscription_email"))
+    end
+  end
+
+  describe "#subscription_update_confirm_email" do
+    it "renders proper subject" do
+      email = described_class.subscription_update_confirm_email(user)
+      expect(email.subject).to include("Your subscription has been updated.")
+    end
+
+    it "renders proper receiver" do
+      email = described_class.subscription_update_confirm_email(user)
+      expect(email.to).to eq([user.email])
+    end
+
+    it "includes the tracking pixel" do
+      email = described_class.subscription_update_confirm_email(user)
+      expect(email.html_part.body).to include("open.gif")
+    end
+
+    it "includes UTM params" do
+      email = described_class.subscription_update_confirm_email(user)
+      expect(email.html_part.body).to include(CGI.escape("utm_medium=email"))
+      expect(email.html_part.body).to include(CGI.escape("utm_source=membership_mailer"))
+      expect(email.html_part.body).to include(CGI.escape("utm_campaign=subscription_update_confirm_email"))
     end
   end
 
   describe "#subscription_cancellation_email" do
     it "renders proper subject" do
-      user = create(:user)
-      subscription_cancellation_email = described_class.subscription_cancellation_email(user)
-      expect(subscription_cancellation_email.subject).to include("Sorry to lose you")
+      email = described_class.subscription_cancellation_email(user)
+      expect(email.subject).to include("Sorry to lose you")
     end
+
     it "renders proper receiver" do
-      user = create(:user)
-      subscription_cancellation_email = described_class.subscription_cancellation_email(user)
-      expect(subscription_cancellation_email.to).to eq([user.email])
+      email = described_class.subscription_cancellation_email(user)
+      expect(email.to).to eq([user.email])
+    end
+
+    it "includes the tracking pixel" do
+      email = described_class.subscription_cancellation_email(user)
+      expect(email.html_part.body).to include("open.gif")
+    end
+
+    it "includes UTM params" do
+      email = described_class.subscription_cancellation_email(user)
+      expect(email.html_part.body).to include(CGI.escape("utm_medium=email"))
+      expect(email.html_part.body).to include(CGI.escape("utm_source=membership_mailer"))
+      expect(email.html_part.body).to include(CGI.escape("utm_campaign=subscription_cancellation_email"))
     end
   end
 end
