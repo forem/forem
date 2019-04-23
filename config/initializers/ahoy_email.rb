7fa8f5e1f4bc29e6abf6c289aa6aee2ecf225ab2
rhymes <rhymesete@gmail.com>
2019-04-23 11:27:39 +0200

    Upgrade ahoy_email to 1.0.3

---
+++ config/initializers/ahoy_email.rb
@@ -0,0 +1,5 @@
+# enable tracking for open, click and UTM params
+AhoyEmail.api = true
+AhoyEmail.default_options[:open] = true
+AhoyEmail.default_options[:click] = true
+AhoyEmail.default_options[:utm_params] = true
