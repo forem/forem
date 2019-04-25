63b0d2d72ed719b566f28c98ccc14d5fcb07b97b
rhymes <rhymesete@gmail.com>
2019-04-25 13:52:43 +0200

    Upgrade dry-struct to 0.7.0 (#2530) [ci skip]

--- app/services/notifications/reactions/reaction_data.rb
+++ app/services/notifications/reactions/reaction_data.rb
@@ -1,15 +1,12 @@
-require "dry-types"
-require "dry-struct"
-
 module Notifications
   module Reactions
     module Types
-      include Dry::Types.module
+      include Dry.Types
     end
 
     class ReactionData < Dry::Struct
       attribute :reactable_id, Types::Strict::Integer
-      attribute :reactable_type, Types::Strict::String
+      attribute :reactable_type, Types::Strict::String.enum("Article", "Comment")
       attribute :reactable_user_id, Types::Strict::Integer
     end
   end
