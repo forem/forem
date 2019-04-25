4c7cd25c14dc1103d5ded962eea9add81625fa3d
Mario See <mariocsee@users.noreply.github.com>
2019-04-25 15:05:09 -0400

    codeclimate refactor
    Method definitions must not be nested. Use lambda instead.

--- app/liquid_tags/dev_comment_tag.rb
+++ app/liquid_tags/dev_comment_tag.rb
@@ -3,20 +3,21 @@ class DevCommentTag < LiquidTagBase
 
   def initialize(_tag_name, id_code, _tokens)
     @id_code = id_code.strip
-
-    def render(_context)
-      comment = find_comment
-      ActionController::Base.new.render_to_string(
-        partial: PARTIAL,
-        locals: { comment: comment },
-      )
-    end
-
-    def find_comment
-      Comment.find(@id_code.to_i(26))
-    rescue ActiveRecord::RecordNotFound
-      raise StandardError, "Invalid comment ID or comment does not exist"
+    class << self
+      def render(_context)
+        comment = find_comment
+        ActionController::Base.new.render_to_string(
+          partial: PARTIAL,
+          locals: { comment: comment },
+        )
       end
+
+      def find_comment
+        Comment.find(@id_code.to_i(26))
+      rescue ActiveRecord::RecordNotFound
+        raise StandardError, "Invalid comment ID or comment does not exist"
+      end
+    end
   end
 end
 
