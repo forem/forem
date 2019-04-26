f6d2f8eba6ae759c36551646ea4a74320651b84f
Ben Halpern <ben@dev.to>
2019-04-26 19:01:35 -0400

    Fix some listing/credit bugs (#2584)
    * Fix some listing/credit bugs
    
    * Tack on codepen team link fix
    
    * Fix test
    
    * Actually fix test

--- spec/requests/credits_spec.rb
+++ spec/requests/credits_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe "Credits", type: :request do
   describe "GET /credits" do
     let(:user) { create(:user) }
+    let(:organization) { create(:organization) }
 
     before do
       sign_in user
@@ -11,6 +12,17 @@ RSpec.describe "Credits", type: :request do
     it "shows credits page" do
       get "/credits"
       expect(response.body).to include("You have")
+    end
+    it "shows credits page if user belongs to an org" do
+      user.update_column(:organization_id, organization.id)
+      get "/credits"
+      expect(response.body).to include("You have")
+    end
+    it "shows credits page if user belongs to an org and is org admin" do
+      user.update_column(:organization_id, organization.id)
+      user.update_column(:org_admin, true)
+      get "/credits"
+      expect(response.body).to include(organization.name)
     end
   end
 
