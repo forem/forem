7f797f0bbea2a51b587757b27316ad221cba5b19
Corey Alexander <coreyja@gmail.com>
2019-04-27 20:13:17 -0400

    Add support for expired tokens and add spec

--- app/models/streams/twitch_credentials.rb
+++ app/models/streams/twitch_credentials.rb
@@ -1,51 +1,45 @@
 module Streams
   class TwitchCredentials
-    ACCESS_TOKEN_CACHE_KEY = :twitch_access_token
+    ACCESS_TOKEN_AND_EXPIRATION_CACHE_KEY = :twitch_access_token_with_expiration
 
-    include Singleton
+    class << self
+      def access_token
+        token, exp = Rails.cache.fetch(ACCESS_TOKEN_AND_EXPIRATION_CACHE_KEY)
 
-    def access_token
-      Rails.cache.fetch(ACCESS_TOKEN_CACHE_KEY, expires_in: 7.days) do
-        connection.post(
+        if token.nil? || Time.zone.now >= exp
+          token, exp = get_new_token
+          Rails.cache.write(ACCESS_TOKEN_AND_EXPIRATION_CACHE_KEY, [token, exp])
+        end
+
+        token
+      end
+
+      def generate_client
+        Faraday.new("https://api.twitch.tv/helix", headers: { "Authorization" => "Bearer #{access_token}" }) do |faraday|
+          faraday.request :json
+          faraday.response :json
+          faraday.adapter Faraday.default_adapter
+        end
+      end
+
+      private
+
+      def get_new_token
+        resp = connection.post(
           "https://id.twitch.tv/oauth2/token",
           client_id: ApplicationConfig["TWITCH_CLIENT_ID"],
           client_secret: ApplicationConfig["TWITCH_CLIENT_SECRET"],
           grant_type: :client_credentials,
-        ).body["access_token"]
+        )
+        [resp.body["access_token"], resp.body["expires_in"].seconds.from_now]
       end
-    end
 
-    def self.access_token
-      instance.access_token
-    end
-
-    def revoke_token(token)
-      connection.post(
-        "https://id.twitch.tv/oauth2/revoke",
-        client_id: ApplicationConfig["TWITCH_CLIENT_ID"],
-        token: token,
-      )
-    end
-
-    def generate_client
-      Faraday.new("https://api.twitch.tv/helix", headers: { "Authorization" => "Bearer #{access_token}" }) do |faraday|
-        faraday.request :json
-        faraday.response :json
-        faraday.adapter Faraday.default_adapter
-      end
-    end
-
-    def self.generate_client
-      instance.generate_client
-    end
-
-    private
-
-    def connection
-      Faraday.new do |faraday|
-        faraday.request :json
-        faraday.response :json
-        faraday.adapter Faraday.default_adapter
+      def connection
+        Faraday.new do |faraday|
+          faraday.request :json
+          faraday.response :json
+          faraday.adapter Faraday.default_adapter
+        end
       end
     end
   end
