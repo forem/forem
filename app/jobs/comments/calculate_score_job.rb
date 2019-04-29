6f39fbdc8d3c76148fa881f9b24c6cff111ffc65
Kobe <KDRaypole01@indianatech.net>
2019-04-29 19:13:50 -0400

    Fix CalculateScoreJob

--- app/jobs/comments/calculate_score_job.rb
+++ app/jobs/comments/calculate_score_job.rb
@@ -4,8 +4,8 @@ module Comments
 
     def perform(comment_id)
       comment = Comment.find_by(id: comment_id)
-      comment.update_column(:score, BlackBox.comment_quality_score(self))
-      comment.update_column(:spaminess_rating, BlackBox.calculate_spaminess(self))
+      comment.update_column(:score, BlackBox.comment_quality_score(comment))
+      comment.update_column(:spaminess_rating, BlackBox.calculate_spaminess(comment))
       comment.root.save unless comment.is_root?
     end
   end
