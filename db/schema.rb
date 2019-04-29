9d00aac77ee3cf27d8293e1084455a91216ed3be
Corey Alexander <coreyja@gmail.com>
2019-04-29 18:11:53 -0400

    Embed Twitch Live Streaming  (#2591)
    * Get a job created that can create a webhook subscription for a twitch user login
    
    * Remove ngork url
    
    * Refactor add store the access token in the cache
    
    * Get a controller stood up to recieve the webhooks. Now they just need to be processed
    
    * Get User columns added and got webhook controller bones working
    
    * Update the webhook job to use the User
    
    * Add a way for the User to input their Twitch User Name. Plus a linter fix
    
    * Delay webhook registration when profile is updated
    
    * Don't add _ in username
    
    * Use String columns
    
    * Quick fix and add some more requests specs
    
    * Specs for the webhook job
    
    * Get a show page Twitch Live Streams. Just a straight embed of the Twitch Everything Embed UI. Works surprisingly well responsively, and works on all screen sizes
    
    * Fix Gemfile.lock from merge issues
    
    * Add support for expired tokens and add spec
    
    * Add secrets to webhook registration and clean up spec to remove token logic
    
    * Verify webhook secret and spec it
    
    * Add rake task to enqueue webhook registration for all Users. This can be used from Heroku Scheduler
    
    * Update the lease seconds to be for 5 days
    
    * Actually lets do 7 so we can refresh twice a week and try to make sure that we can always miss one
    
    * Hijack the existing Twitch logo instead of making a duplicate one
    
    * Remove comment and replace with log line
    
    * Remove some white space
    
    * Log to Airbrake when webhook errors occur
    
    * Move to passing in an id instead of User object
    
    * Extract logic from Job to Service object
    
    * Capitilize in the view
    
    * Move out of models and into services
    
    * Remove letover stub
    
    * Remove one usage of Faraday
    
    * Use HTTParty for all the HTTP here

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -809,6 +811,7 @@ ActiveRecord::Schema.define(version: 2019_04_25_210432) do
     t.inet "current_sign_in_ip"
     t.string "currently_hacking_on"
     t.string "currently_learning"
+    t.string "currently_streaming_on"
     t.boolean "display_sponsors", default: true
     t.string "dribbble_url"
     t.string "editor_version", default: "v1"
@@ -920,6 +923,7 @@ ActiveRecord::Schema.define(version: 2019_04_25_210432) do
     t.string "text_only_name"
     t.string "top_languages"
     t.string "twitch_url"
+    t.string "twitch_username"
     t.datetime "twitter_created_at"
     t.integer "twitter_followers_count"
     t.integer "twitter_following_count"
