a6d22ad5f622223f6d6d83d640c714d2b909acc1
Mario See <mariocsee@users.noreply.github.com>
2019-05-01 15:32:13 -0400

    handle finding comment inside initialize

--- app/liquid_tags/dev_comment_tag.rb
+++ app/liquid_tags/dev_comment_tag.rb
@@ -2,19 +2,18 @@ class DevCommentTag < LiquidTagBase
   PARTIAL = "comments/liquid".freeze
 
   def initialize(_tag_name, id_code, _tokens)
-    @id_code = id_code.strip
+    @comment = find_comment(id_code.strip)
   end
-  
+
   def render(_context)
-    comment = find_comment
     ActionController::Base.new.render_to_string(
       partial: PARTIAL,
-      locals: { comment: comment },
+      locals: { comment: @comment },
     )
   end
 
-  def find_comment
-    Comment.find(@id_code.to_i(26))
+  def find_comment(id_code)
+    Comment.find(id_code.to_i(26))
   rescue ActiveRecord::RecordNotFound
     raise StandardError, "Invalid comment ID or comment does not exist"
   end
