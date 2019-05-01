a1d2070e5f52a182ba8ffa90cf38165128dfc85e
Mario See <mariocsee@users.noreply.github.com>
2019-05-01 16:04:44 -0400

    unnest and fix error handling

--- app/liquid_tags/link_tag.rb
+++ app/liquid_tags/link_tag.rb
@@ -3,54 +3,44 @@ class LinkTag < LiquidTagBase
   PARTIAL = "articles/liquid".freeze
 
   def initialize(_tag_name, slug_or_path_or_url, _tokens)
-    @slug_or_path_or_url = ActionController::Base.helpers.strip_tags(slug_or_path_or_url).strip
+    @article = get_article(slug_or_path_or_url)
+    @title = strip_tags @article.title
+  end
 
-    class << self
-      def article_hash
-        path = Addressable::URI.parse(@slug_or_path_or_url).path
-        path.slice!(0) if path.starts_with?("/") # remove leading slash if present
-        path.slice!(-1) if path.ends_with?("/") # remove trailing slash if present
-        Addressable::Template.new("{username}/{slug}").extract(path)&.symbolize_keys
-      end
-    end
+  def render(_context)
+    ActionController::Base.new.render_to_string(
+      partial: PARTIAL,
+      locals: { article: @article, title: @title },
+    )
+  end
 
-    @hash = article_hash
-    raise StandardError, "Invalid link URL or link URL does not exist" if @hash.nil?
+  def get_article(slug)
+    slug = ActionController::Base.helpers.strip_tags(slug).strip
+    article = find_article_by_user(article_hash(slug)) || find_article_by_org(article_hash(slug))
+    raise StandardError, "Invalid link URL or link URL does not exist" unless article
 
-    class << self
-      def render(_context)
-        article = get_article
-        title = strip_tags article.title
-        profile_img = ProfileImage.new(article.user).get(150)
-        ActionController::Base.new.render_to_string(
-          partial: PARTIAL,
-          locals: { article: article, title: title, profile_img: profile_img },
-        )
-      end
+    article
+  end
 
-      def get_article
-        article = find_article_by_user(@hash) || find_article_by_org(@hash)
-        raise StandardError, "Invalid link URL or link URL does not exist" if article.nil?
+  def article_hash(slug)
+    path = Addressable::URI.parse(slug).path
+    path.slice!(0) if path.starts_with?("/") # remove leading slash if present
+    path.slice!(-1) if path.ends_with?("/") # remove trailing slash if present
+    Addressable::Template.new("{username}/{slug}").extract(path)&.symbolize_keys
+  end
 
-        article
-      rescue StandardError
-        raise StandardError, "Invalid link URL or link URL does not exist"
-      end
+  def find_article_by_user(hash)
+    user = User.find_by(username: hash[:username])
+    return unless user
 
-      def find_article_by_user(hash)
-        user = User.find_by(username: hash[:username])
-        return unless user
+    user.articles.where(slug: hash[:slug])&.first
+  end
 
-        user.articles.where(slug: hash[:slug])&.first
-      end
+  def find_article_by_org(hash)
+    org = Organization.find_by(slug: hash[:username])
+    return unless org
 
-      def find_article_by_org(hash)
-        org = Organization.find_by(slug: hash[:username])
-        return unless org
-
-        org.articles.where(slug: hash[:slug])&.first
-      end
-    end
+    org.articles.where(slug: hash[:slug])&.first
   end
 end
 
