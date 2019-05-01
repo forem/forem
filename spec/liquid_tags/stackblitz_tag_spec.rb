e843d9dc967a8b212e23c512ff73d8e18a282e44
Westbrook Johnson <westbrook.johnson@gmail.com>
2019-05-01 16:28:37 -0400

    Add file argument to Stackblitz embed. (#2590)

--- spec/liquid_tags/stackblitz_tag_spec.rb
+++ spec/liquid_tags/stackblitz_tag_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe StackblitzTag, type: :liquid_template do
   describe "#id" do
     let(:stackblitz_id) { "ball-demo" }
     let(:stackblitz_id_with_view) { "ball-demo view=preview" }
+    let(:stackblitz_id_with_file) { "ball-demo file=style.css" }
+    let(:stackblitz_id_with_view_and_file) { "ball-demo view=preview file=style.css" }
 
     xss_links = %w(
       //evil.com/?ball-demo
@@ -31,6 +33,23 @@ RSpec.describe StackblitzTag, type: :liquid_template do
       expect do
         generate_new_liquid(stackblitz_id_with_view)
       end.not_to raise_error
+    end
+
+    it "accepts stackblitz id with a file parameter" do
+      expect do
+        generate_new_liquid(stackblitz_id_with_file)
+      end.not_to raise_error
+    end
+
+    it "accepts stackblitz id with a view and file parameter" do
+      expect do
+        generate_new_liquid(stackblitz_id_with_view_and_file)
+      end.not_to raise_error
+    end
+
+    it "parses stackblitz if with a view and file parameter" do
+      liquid = generate_new_liquid(stackblitz_id_with_view_and_file)
+      expect(liquid.render).to include("https://stackblitz.com/edit/ball-demo?embed=1&view=preview&file=style.css")
     end
 
     it "rejects XSS attempts" do
