b247cdeebb8ce5b947459e199d9067045a832cd6
Mario See <mariocsee@users.noreply.github.com>
2019-05-02 17:53:32 -0400

    split line

--- app/liquid_tags/podcast_tag.rb
+++ app/liquid_tags/podcast_tag.rb
@@ -16,7 +16,10 @@ class PodcastTag < LiquidTagBase
     @episode = fetch_podcast(link)
     @podcast ||= Podcast.new
     @style = render_style
-    @podcast_links = [["iTunes", @podcast.itunes_url, cloudinary(IMAGE_LINK["iTunes".downcase.to_sym], 40, 90, "png")], ["Overcast", @podcast.overcast_url, cloudinary(IMAGE_LINK[name.downcase.to_sym], 40, 90, "png")], ["Android", @podcast.android_url, cloudinary(IMAGE_LINK[name.downcase.to_sym], 40, 90, "png")], ["RSS", @podcast.feed_url, cloudinary(IMAGE_LINK[name.downcase.to_sym], 40, 90, "png")]]
+    @podcast_links = [["iTunes", @podcast.itunes_url, cloudinary(IMAGE_LINK["iTunes".downcase.to_sym], 40, 90, "png")],
+                      ["Overcast", @podcast.overcast_url, cloudinary(IMAGE_LINK[name.downcase.to_sym], 40, 90, "png")],
+                      ["Android", @podcast.android_url, cloudinary(IMAGE_LINK[name.downcase.to_sym], 40, 90, "png")],
+                      ["RSS", @podcast.feed_url, cloudinary(IMAGE_LINK[name.downcase.to_sym], 40, 90, "png")]];
   end
 
   def render(_context)
