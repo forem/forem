5e64ae874e98af74b8fc9b1711fdf9561b667dfe
Ben Halpern <ben@dev.to>
2019-05-02 15:58:58 -0400

    Remove includes user from stories query to reduce num queries and memory (#2662)
    * Add profile_image_90 to serialized data on article
    
    * Remove includes user from stories query

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -79,7 +79,7 @@ class StoriesController < ApplicationController
     @tag = params[:tag].downcase
     @page = (params[:page] || 1).to_i
     @tag_model = Tag.find_by(name: @tag) || not_found
-    @moderators = User.with_role(:tag_moderator, @tag_model)
+    @moderators = User.with_role(:tag_moderator, @tag_model).select(:username, :profile_image, :id)
     if @tag_model.alias_for.present?
       redirect_to "/t/#{@tag_model.alias_for}"
       return
@@ -140,7 +140,6 @@ class StoriesController < ApplicationController
     @user = @organization
     @stories = ArticleDecorator.decorate_collection(@organization.articles.published.
       limited_column_select.
-      includes(:user).
       order("published_at DESC").page(@page).per(8))
     @article_index = true
     @organization_article_index = true
@@ -252,7 +251,7 @@ class StoriesController < ApplicationController
 
   def article_finder(num_articles)
     tag = params[:tag]
-    articles = Article.published.includes(:user).limited_column_select.page(@page).per(num_articles)
+    articles = Article.published.limited_column_select.page(@page).per(num_articles)
     articles = articles.cached_tagged_with(tag) if tag.present? # More efficient than tagged_with
     articles
   end
