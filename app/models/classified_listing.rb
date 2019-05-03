bc7fc8297304edf68112147bb99c9c3f8296364f
Jess Lee <jessleenyc@gmail.com>
2019-05-03 12:36:22 -0400

    update category details

--- app/models/classified_listing.rb
+++ app/models/classified_listing.rb
@@ -61,15 +61,15 @@ class ClassifiedListing < ApplicationRecord
 
   def self.categories_available
     {
-      "cfp" => { cost: 1, name: "Conference CFP", rules: "Currently open for proposals, with link to form" },
-      "contractors" => { cost: 1, name: "Available for Hire", rules: "You are available for hire." },
-      "collabs" => { cost: 1, name: "Contributors/Collaborators Wanted" },
-      "education" => { cost: 1, name: "Education/Courses", rules: "Educational material and/or schools/bootcamps" },
+      "cfp" => { cost: 1, name: "Conference CFP", rules: "Currently open for proposals, with link to form." },
+      "forhire" => { cost: 1, name: "Available for Hire", rules: "You are available for hire." },
+      "collabs" => { cost: 1, name: "Contributors/Collaborators Wanted", rules: "Projects looking for volunteers. Not job listings." },
+      "education" => { cost: 1, name: "Education/Courses", rules: "Educational material and/or schools/bootcamps." },
       "jobs" => { cost: 25, name: "Job Listings", rules: "Companies offering employment right now." },
-      "mentors" => { cost: 1, name: "Offering Mentorship"},
-      "products" => { cost: 10, name: "Products/Tools", rules: "Must be available right now" },
-      "mentees" => { cost: 1, name: "Seeking a Mentor"},
-      "sale" => { cost: 1, name: "Stuff for Sale", rules: "Personally owned physical items for sale" },
+      "mentors" => { cost: 1, name: "Offering Mentorship", rules: "You are available to mentor someone." },
+      "products" => { cost: 10, name: "Products/Tools", rules: "Must be available right now." },
+      "mentees" => { cost: 1, name: "Seeking a Mentor", rules: "You are looking for a mentor." },
+      "forsale" => { cost: 1, name: "Stuff for Sale", rules: "Personally owned physical items for sale" },
       "events" => { cost: 1, name: "Upcoming Events", rules: "In-person or online events with date included" },
       "misc" => { cost: 1, name: "Miscellaneous", rules: "Must not fit in any other category." }
     }
