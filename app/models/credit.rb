ef28c825acf5dc56e39fd9097292ae0ac294e9e3
Jess Lee <jessleenyc@gmail.com>
2019-05-03 13:56:39 -0400

    add method to award and remove credits (#2681)

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -3,4 +3,16 @@ class Credit < ApplicationRecord
 
   belongs_to    :user, optional: true
   belongs_to    :organization, optional: true
+
+  def self.add_to(user, amount)
+    credit_objects = []
+    amount.times do
+      credit_objects << Credit.new(user_id: user.id)
+    end
+    Credit.import credit_objects
+  end
+
+  def self.remove_from(user, amount)
+    user.credits.where(spent: false).limit(amount).delete_all
+  end
 end
