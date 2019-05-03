99b3c01e19a824c8e96a1bca912af7a44be962af
rhymes <rhymesete@gmail.com>
2019-05-03 18:43:23 +0200

    Use en-US for short dates and user locale for long

--- app/assets/javascripts/initializers/initializeCommentDate.js
+++ app/assets/javascripts/initializers/initializeCommentDate.js
@@ -1,5 +1,5 @@
 /* Show comment date/time according to user's locale */
-/* global timestampToLocalTime */
+/* global timestampToLocalDateTime */
 
 function initializeCommentDate() {
   // example: "Wednesday, April 3, 2019, 2:55:14 PM"
@@ -25,13 +25,23 @@ function initializeCommentDate() {
     var ts = commentDates[i].getAttribute('data-published-timestamp');
 
     // add a full datetime to the comment date string, visible on hover
-    var hoverTime = timestampToLocalTime(ts, hoverTimeOptions);
+    // `navigator.language` is used for full date times to allow the hover date
+    // to be localized according to the user's locale
+    var hoverTime = timestampToLocalDateTime(
+      ts,
+      navigator.language,
+      hoverTimeOptions,
+    );
     commentDates[i].setAttribute('title', hoverTime);
 
     // replace the comment short visible date with the equivalent localized one
     var visibleDate = commentDates[i].querySelector('a');
     if (visibleDate) {
-      var localVisibleDate = timestampToLocalTime(ts, visibleDateOptions);
+      var localVisibleDate = timestampToLocalDateTime(
+        ts,
+        'en-US', // en-US because for now we want all users to see `Apr 3`
+        visibleDateOptions,
+      );
       if (localVisibleDate) {
         visibleDate.innerHTML = localVisibleDate;
       }
