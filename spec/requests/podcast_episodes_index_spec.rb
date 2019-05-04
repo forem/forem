ad668263c5c1deff5a906e9dc3dc6a39dfda81f2
Ben Halpern <ben@dev.to>
2019-05-04 15:30:40 -0400

    Allow users to follow podcasts (#2690)
    * Allow users to follow podcasts
    
    * Only display podcasts user follows
    
    * Add main_color_hex to factory
    
    * Fix podcast test and modify styles
    
    * Fix tests
    
    * Remove weird test

--- spec/requests/podcast_episodes_index_spec.rb
+++ spec/requests/podcast_episodes_index_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "PodcastEpisodesSpec", type: :request do
   describe "GET podcast episodes index" do
     it "renders page with proper sidebar" do
       get "/pod"
-      expect(response.body).to include("<h1>Podcasts</h1>")
+      expect(response.body).to include("If you know of a great dev podcast")
     end
   end
 end
