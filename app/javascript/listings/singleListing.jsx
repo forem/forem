e9b4b4b8c80cfc3e1be2104feb78e2da31723e9f
Andy Zhao <andyzhao.zhao@gmail.com>
2019-05-04 19:39:49 -0400

    WIP Add modal for single listings (#2688)
    * Add new line at the bottom of the file
    
    * Fix some linter errors
    
    * Add overlay and links to single listings
    
    * Clean up some code
    
    * Add escape key functionality to quit overlay
    
    * Lint some code
    
    * Add modal functionality
    
    * Update styling for modal
    
    * Fix some linter warnings
    
    * Preserve the correct link all the time

--- app/javascript/listings/singleListing.jsx
+++ app/javascript/listings/singleListing.jsx
@@ -1,26 +1,55 @@
-import { h, Component } from 'preact';
 import PropTypes from 'prop-types';
+import { h } from 'preact';
 
 export const SingleListing = ({listing, onAddTag, currentUserId, onChangeCategory}) => {
   const tagLinks = listing.tag_list.map(tag => (
     <a href={`/listings?t=${tag}`} onClick={e => onAddTag(e, tag)} data-no-instant>{tag}</a>
   ));
+
   const editButton = currentUserId === listing.user_id ? <a href={`/listings/${listing.id}/edit`} className="classified-listing-edit-button">・edit</a> : <a href={`/report-abuse?url=https://dev.to/listings/${listing.category}/${listing.slug}`}>・report abuse</a>;
-  return (
-    <div className="single-classified-listing">
-      <div className="listing-content">
-        <h3>{ listing.title }</h3>
-        <div className="single-classified-listing-body" dangerouslySetInnerHTML={{ __html: listing.processed_html }} />
-        <div className="single-classified-listing-tags">{ tagLinks }</div>
-        <div className="single-classified-listing-author-info">
-          <a href={`/listings/${listing.category}`} onClick={e => onChangeCategory(e, listing.category)}>{listing.category}</a>
-          ・
-          <a href={`/${listing.author.username}`}>{listing.author.name}</a>
-          {editButton}
+
+  const handleOpenModal = (e) => {
+    e.preventDefault()
+
+    const overlay = document.getElementById('listing-overlay')
+    overlay.dataset.originLink = window.location.href
+    window.history.pushState('', '', e.target.href)
+    overlay.style.display = 'block'
+    overlay.focus()
+    overlay.tabIndex = 0
+
+    const { listingId } = e.target.dataset
+    const clonedListing = document.getElementById(`single-classified-listing-${listingId}`).cloneNode(true)
+    clonedListing.id = `single-classified-listing-modal-${listingId}`
+    clonedListing.className += ' singlelisting__modal'
+    overlay.insertAdjacentElement('afterend', clonedListing)
+  }
+
+  const listingCard = () => {
+    return(
+      <div className="single-classified-listing" id={`single-classified-listing-${listing.id}`}>
+        <div className="listing-content">
+          <h3>
+            <a href={`/listings/${listing.category}/${listing.slug}`} data-no-instant onClick={handleOpenModal} data-listing-id={listing.id}>
+              {listing.title}
+            </a>
+          </h3>
+          <div className="single-classified-listing-body" dangerouslySetInnerHTML={{ __html: listing.processed_html }} />
+          <div className="single-classified-listing-tags">{tagLinks}</div>
+          <div className="single-classified-listing-author-info">
+            <a href={`/listings/${listing.category}`} onClick={e => onChangeCategory(e, listing.category)}>{listing.category}</a>
+            ・
+            <a href={`/${listing.author.username}`}>{listing.author.name}</a>
+            {editButton}
+          </div>
         </div>
       </div>
-    </div>
-);
+    )
+  }
+
+  return (
+    listingCard()
+  );
 }
 
 SingleListing.propTypes = {
