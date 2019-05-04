7ca487a576c63a31447478c709d493636e763750
Jess Lee <jessleenyc@gmail.com>
2019-05-04 09:06:42 -0400

    Add report abuse link to listings (#2684)
    * add report abuse link
    
    * add listings category on abuse form
    
    * include listings category

--- app/javascript/listings/singleListing.jsx
+++ app/javascript/listings/singleListing.jsx
@@ -5,7 +5,7 @@ export const SingleListing = ({listing, onAddTag, currentUserId, onChangeCategor
   const tagLinks = listing.tag_list.map(tag => (
     <a href={`/listings?t=${tag}`} onClick={e => onAddTag(e, tag)} data-no-instant>{tag}</a>
   ));
-  const editButton = currentUserId === listing.user_id ? <a href={`/listings/${listing.id}/edit`} className="classified-listing-edit-button">edit</a> : '';
+  const editButton = currentUserId === listing.user_id ? <a href={`/listings/${listing.id}/edit`} className="classified-listing-edit-button">・edit</a> : <a href={`/report-abuse?url=https://dev.to/listings/${listing.category}/${listing.slug}`}>・report abuse</a>;
   return (
     <div className="single-classified-listing">
       <div className="listing-content">
