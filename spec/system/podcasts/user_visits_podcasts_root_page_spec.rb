79218fde602b3a1f25dfa29566219cecad466064
Ben Halpern <ben@dev.to>
2019-05-04 15:03:26 -0400

    Remove weird test

--- spec/system/podcasts/user_visits_podcasts_root_page_spec.rb
+++ spec/system/podcasts/user_visits_podcasts_root_page_spec.rb
@@ -6,12 +6,6 @@ RSpec.describe "User visits /pod page", type: :system do
 
   before { visit "/pod" }
 
-  it "displays the header" do
-    within "h4" do
-      expect(page).to have_text("podcasts")
-    end
-  end
-
   it "displays the podcasts" do
     within "#articles-list" do
       expect(page).to have_link(nil, href: podcast_episode.path)
