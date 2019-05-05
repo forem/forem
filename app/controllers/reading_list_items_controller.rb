7f09989ea4a904248ab564bcf4feb4db40f3adad
Ben Halpern <ben@dev.to>
2019-05-05 19:46:15 -0400

    Fix caching issue (#2700)

--- app/controllers/reading_list_items_controller.rb
+++ app/controllers/reading_list_items_controller.rb
@@ -1,11 +1,9 @@
 class ReadingListItemsController < ApplicationController
-  before_action :set_cache_control_headers, only: [:index]
 
   def index
     @reading_list_items_index = true
     set_view
     generate_algolia_search_key
-    set_surrogate_key_header "reading-list-index"
   end
 
   def update
