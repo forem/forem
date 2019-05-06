4e10677cac8bc2296987bb81989ac18401912e0c
rhymes <rhymesete@gmail.com>
2019-05-06 17:26:18 +0200

    Use time HTML5 element and refactor date in partial

--- app/assets/javascripts/initializers/initializeCommentDate.js
+++ app/assets/javascripts/initializers/initializeCommentDate.js
@@ -13,38 +13,21 @@ function initializeCommentDate() {
     second: 'numeric',
   };
 
-  // example: "Apr 3"
-  var visibleDateOptions = {
-    month: 'short',
-    day: 'numeric',
-  };
-
-  var commentDates = document.getElementsByClassName('comment-date');
+  var commentDates = document.querySelectorAll('.comment-date time');
   for (var i = 0; i < commentDates.length; i += 1) {
     // get UTC timestamp set by the server
-    var ts = commentDates[i].getAttribute('data-published-timestamp');
+    var ts = commentDates[i].getAttribute('datetime');
 
-    // add a full datetime to the comment date string, visible on hover
-    // `navigator.language` is used for full date times to allow the hover date
-    // to be localized according to the user's locale
-    var hoverTime = timestampToLocalDateTime(
-      ts,
-      navigator.language,
-      hoverTimeOptions,
-    );
-    commentDates[i].setAttribute('title', hoverTime);
-
-    // replace the comment short visible date with the equivalent localized one
-    var visibleDate = commentDates[i].querySelector('a');
-    if (visibleDate) {
-      var localVisibleDate = timestampToLocalDateTime(
+    if (ts) {
+      // add a full datetime to the comment date string, visible on hover
+      // `navigator.language` is used for full date times to allow the hover date
+      // to be localized according to the user's locale
+      var hoverTime = timestampToLocalDateTime(
         ts,
-        'en-US', // en-US because for now we want all users to see `Apr 3`
-        visibleDateOptions,
+        navigator.language,
+        hoverTimeOptions,
       );
-      if (localVisibleDate) {
-        visibleDate.innerHTML = localVisibleDate;
-      }
+      commentDates[i].setAttribute('title', hoverTime);
     }
   }
 }
