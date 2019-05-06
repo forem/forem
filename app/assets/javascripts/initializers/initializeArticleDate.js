3b3cff3cd7736ebef9c20b1088a0eaff982c6561
rhymes <rhymesete@gmail.com>
2019-05-06 23:08:35 +0200

    Refactor date initializers

--- app/assets/javascripts/initializers/initializeArticleDate.js
+++ app/assets/javascripts/initializers/initializeArticleDate.js
@@ -1,35 +1,10 @@
 /* Show article date/time according to user's locale */
-/* global timestampToLocalDateTime */
+/* global addLocalizedDateTimeToElementsTitles */
 
 function initializeArticleDate() {
-  // example: "Wednesday, April 3, 2019, 2:55:14 PM"
-  var hoverTimeOptions = {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-  };
+  var articlesDates = document.querySelectorAll(
+    '.single-article time, article time, .single-other-article time',
+  );
 
-  var selectors =
-    '.single-article time, article time, .single-other-article time';
-  var articleDates = document.querySelectorAll(selectors);
-  for (var i = 0; i < articleDates.length; i += 1) {
-    // get UTC timestamp set by the server
-    var ts = articleDates[i].getAttribute('datetime');
-
-    if (ts) {
-      // add a full datetime to the comment date string, visible on hover
-      // `navigator.language` is used for full date times to allow the hover date
-      // to be localized according to the user's locale
-      var hoverTime = timestampToLocalDateTime(
-        ts,
-        navigator.language,
-        hoverTimeOptions,
-      );
-      articleDates[i].setAttribute('title', hoverTime);
-    }
-  }
+  addLocalizedDateTimeToElementsTitles(articlesDates, 'datetime');
 }
--- app/assets/javascripts/initializers/initializeCommentDate.js
+++ app/assets/javascripts/initializers/initializeCommentDate.js
@@ -1,33 +1,8 @@
 /* Show comment date/time according to user's locale */
-/* global timestampToLocalDateTime */
+/* global addLocalizedDateTimeToElementsTitles */
 
 function initializeCommentDate() {
-  // example: "Wednesday, April 3, 2019, 2:55:14 PM"
-  var hoverTimeOptions = {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-  };
+  var commentsDates = document.querySelectorAll('.comment-date time');
 
-  var commentDates = document.querySelectorAll('.comment-date time');
-  for (var i = 0; i < commentDates.length; i += 1) {
-    // get UTC timestamp set by the server
-    var ts = commentDates[i].getAttribute('datetime');
-
-    if (ts) {
-      // add a full datetime to the comment date string, visible on hover
-      // `navigator.language` is used for full date times to allow the hover date
-      // to be localized according to the user's locale
-      var hoverTime = timestampToLocalDateTime(
-        ts,
-        navigator.language,
-        hoverTimeOptions,
-      );
-      commentDates[i].setAttribute('title', hoverTime);
-    }
-  }
+  addLocalizedDateTimeToElementsTitles(commentsDates, 'datetime');
 }
