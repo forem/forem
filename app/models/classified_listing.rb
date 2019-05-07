50b2a0ac7471f8e44bc0b83ceadf4ea8ca94c0af
Jess Lee <jessleenyc@gmail.com>
2019-05-07 07:42:25 -0400

    update product credits

--- app/models/classified_listing.rb
+++ app/models/classified_listing.rb
@@ -67,7 +67,7 @@ class ClassifiedListing < ApplicationRecord
       "education" => { cost: 1, name: "Education/Courses", rules: "Educational material and/or schools/bootcamps." },
       "jobs" => { cost: 25, name: "Job Listings", rules: "Companies offering employment right now." },
       "mentors" => { cost: 1, name: "Offering Mentorship", rules: "You are available to mentor someone." },
-      "products" => { cost: 5, name: "Products/Tools", rules: "Must be available right now." },
+      "products" => { cost: 10, name: "Products/Tools", rules: "Must be available right now." },
       "mentees" => { cost: 1, name: "Seeking a Mentor", rules: "You are looking for a mentor." },
       "forsale" => { cost: 1, name: "Stuff for Sale", rules: "Personally owned physical items for sale." },
       "events" => { cost: 1, name: "Upcoming Events", rules: "In-person or online events with date included." },
