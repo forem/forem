0cf030696920fb44d844f7fd746b25d24078f9a1
Jess Lee <jessleenyc@gmail.com>
2019-05-08 13:43:00 -0400

    delete user messages in delete process

--- app/services/moderator/manage_activity_and_roles.rb
+++ app/services/moderator/manage_activity_and_roles.rb
@@ -44,6 +44,7 @@ module Moderator
       user.reactions.delete_all
       user.follows.delete_all
       Follow.where(followable_id: user.id, followable_type: "User").delete_all
+      user.messages.delete_all
       user.chat_channel_memberships.delete_all
       user.mentions.delete_all
       user.badge_achievements.delete_all
