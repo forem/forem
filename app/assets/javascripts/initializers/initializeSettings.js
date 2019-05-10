d4b882753c63fda9f33d0d6b6e960816c6d0e831
Bolarinwa Balogun <bo.lariin@hotmail.com>
2019-05-10 15:47:32 -0400

    Add Fetch Now to RSS Settings (#2770)
    * Add Fetch Now to RSS Settings
    
    * Resolve issues raised

--- app/assets/javascripts/initializers/initializeSettings.js
+++ app/assets/javascripts/initializers/initializeSettings.js
@@ -1,7 +1,27 @@
-function initializeSettings(){
-  if (document.getElementById("settings-org-secret")){
-    document.getElementById("settings-org-secret").onclick = function(event){
-      event.target.select()
-    }
+/* global timestampToLocalDateTime */
+
+function initializeSettings() {
+  if (document.getElementById('settings-org-secret')) {
+    document.getElementById('settings-org-secret').onclick = function(event) {
+      event.target.select();
+    };
+  }
+
+  if (document.getElementById('rss-fetch-time')) {
+    var timeNode = document.getElementById('rss-fetch-time');
+    var timeStamp = timeNode.getAttribute('datetime');
+    var timeOptions = {
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    };
+
+    timeNode.textContent = timestampToLocalDateTime(
+      timeStamp,
+      navigator.language,
+      timeOptions,
+    );
   }
 }
