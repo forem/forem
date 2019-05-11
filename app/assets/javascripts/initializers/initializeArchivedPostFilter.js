4721e0cfbecdf87827003c322875b28675c1b74e
Dana Scheider <dana.scheider@envato.com>
2019-05-11 21:22:10 +0800

    Enable link to toggle showing/hiding archived articles

---
+++ app/assets/javascripts/initializers/initializeArchivedPostFilter.js
@@ -0,0 +1,37 @@
+function showArchivedPosts() {
+  var archivedPosts = document.getElementsByClassName(
+    'single-article-archived',
+  );
+  console.log('showing');
+
+  for (var i = 0; i < archivedPosts.length; i += 1) {
+    archivedPosts[i].classList.remove('hidden');
+  }
+}
+
+function hideArchivedPosts() {
+  var archivedPosts = document.getElementsByClassName(
+    'single-article-archived',
+  );
+
+  for (var i = 0; i < archivedPosts.length; i += 1) {
+    archivedPosts[i].classList.add('hidden');
+  }
+}
+function toggleArchivedPosts(e) {
+  var link = e.target;
+
+  if (link.innerHTML.match(/Show/)) {
+    link.innerHTML = 'Hide Archived';
+    showArchivedPosts();
+  } else {
+    link.innerHTML = 'Show Archived';
+    hideArchivedPosts();
+  }
+}
+
+function initializeArchivedPostFilter() {
+  var link = document.getElementById('toggleArchivedLink');
+
+  link.addEventListener('click', toggleArchivedPosts);
+}
