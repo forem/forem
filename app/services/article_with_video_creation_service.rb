7b99ce62fcb2a4e7d107889e745cc526fe1ea007
rhymes <rhymesete@gmail.com>
2019-05-14 00:27:23 +0200

    Regenerate rubocop todo and fix trivial violations

--- app/services/article_with_video_creation_service.rb
+++ app/services/article_with_video_creation_service.rb
@@ -11,7 +11,6 @@ class ArticleWithVideoCreationService
 
   def create!
     Article.create! do |article|
-
       article = initial_article_with_params(article)
       article.processed_html = ""
       article.user_id = @current_user.id
