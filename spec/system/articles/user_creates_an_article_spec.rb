d5b727a3973bfebdf6d57ffb7045669047dd1b47
rhymes <rhymesete@gmail.com>
2019-05-15 10:24:51 +0200

    Fix system test to edit posts

--- spec/system/articles/user_creates_an_article_spec.rb
+++ spec/system/articles/user_creates_an_article_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Creating an article with the editor", type: :system do
   let(:user) { create(:user) }
-  let!(:article_body) { file_fixture("article_published.txt").read }
+  let!(:template) { file_fixture("article_published.txt").read }
 
   before do
     sign_in user
@@ -10,7 +10,7 @@ RSpec.describe "Creating an article with the editor", type: :system do
 
   it "creates a new article", js: true, retry: 3 do
     visit new_path
-    fill_in "article_body_markdown", with: article_body
+    fill_in "article_body_markdown", with: template
     click_button "SAVE CHANGES"
     expect(page).to have_selector("header h1", text: "Sample Article")
   end
