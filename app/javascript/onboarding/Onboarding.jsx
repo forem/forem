511c88aa57b32984d1e7d2bddf3407da47da18ad
Ali Spittel <ali07cat07@gmail.com>
2019-05-16 12:29:45 -0400

    add in first slide and change slide functionality

--- app/javascript/onboarding/Onboarding.jsx
+++ app/javascript/onboarding/Onboarding.jsx
@@ -1,12 +1,43 @@
 import 'preact/devtools';
+import WelcomeSlide from './components/WelcomeSlide';
 import { h, Component } from 'preact';
 
 export default class Onboarding extends Component {
   constructor(props) {
     super(props);
+
+    this.nextSlide = this.nextSlide.bind(this);
+    this.slides = [<WelcomeSlide />];
+
+    this.state = {
+      currentSlide: 0,
+    };
+  }
+
+  nextSlide() {
+    let nextSlide = this.state.currentSlide + 1;
+    if (nextSlide < this.slides.length)
+      this.setState({
+        currentSlide: nextSlide,
+      });
+  }
+
+  prevSlide() {
+    let prevSlide = this.state.currentSlide - 1;
+    if (prevSlide > 0) {
+      this.setState({
+        currentSlide: prevSlide,
+      });
+    }
   }
 
   render() {
-    return <div>Hello, World</div>;
+    return (
+      <div>
+        {this.slides[this.state.currentSlide]}
+        <button onClick={this.prevSlide}>BACK</button>
+        <button onClick={this.nextSlide}>NEXT</button>
+      </div>
+    );
   }
 }
---
+++ app/javascript/onboarding/components/WelcomeSlide.jsx
@@ -0,0 +1,14 @@
+import 'preact/devtools';
+import { h, Component } from 'preact';
+
+export default () => (
+  <div>
+    <h1>Welcome to the DEV community!</h1>
+    <p>
+      DEV is where programmers share ideas and help each other grow. It’s a
+      global community for contributing and discovering great ideas, having
+      debates, and making friends.
+    </p>
+    <p>A couple quick questions for you before you get started...</p>
+  </div>
+);
