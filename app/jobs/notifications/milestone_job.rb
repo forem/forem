d9b06f07f9ea9ab048167e2dc9016cbfa63dff26
rhymes <rhymesete@gmail.com>
2019-05-17 00:02:00 +0200

    Unpack milestone hash

--- app/jobs/notifications/milestone_job.rb
+++ app/jobs/notifications/milestone_job.rb
@@ -2,11 +2,11 @@ module Notifications
   class MilestoneJob < ApplicationJob
     queue_as :send_milestone_notification
 
-    def perform(milestone_hash, service = Notifications::Milestone::Send)
-      article = Article.find_by(id: milestone_hash[:article_id])
+    def perform(type, article_id, service = Notifications::Milestone::Send)
+      article = Article.find_by(id: article_id)
       return unless article
 
-      service.call(milestone_hash[:type], article)
+      service.call(type, article)
     end
   end
 end
