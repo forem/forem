37af81837944e1a920aa564b3fe01a394760b75e
Jess Lee <jessleenyc@gmail.com>
2019-05-17 16:08:25 -0400

    code climate refactor

--- app/observers/article_observer.rb
+++ app/observers/article_observer.rb
@@ -2,15 +2,18 @@ class ArticleObserver < ApplicationObserver
   def after_create(article)
     return if Rails.env.development?
 
-    if article.published && article.published_at > 30.seconds.ago
-      SlackBot.delay.ping "New Article Published: #{article.title}\nhttps://dev.to#{article.path}",
-                          channel: "activity",
-                          username: "article_bot",
-                          icon_emoji: ":writing_hand:"
-
-    end
+    ping_new_article
     warned_user_ping(article)
   rescue StandardError => e
     Rails.logger.error(e)
+  end
+
+  def ping_new_article
+    return unless article.published && article.published_at > 30.seconds.ago
+
+    SlackBot.delay.ping "New Article Published: #{article.title}\nhttps://dev.to#{article.path}",
+                        channel: "activity",
+                        username: "article_bot",
+                        icon_emoji: ":writing_hand:"
   end
 end
