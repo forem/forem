b935fdd92065f69821c1919a29913d165e3963a7
Jess Lee <jessleenyc@gmail.com>
2019-05-17 15:59:07 -0400

    Limit number of cover image uploads (#2851)

--- app/labor/rate_limit_checker.rb
+++ app/labor/rate_limit_checker.rb
@@ -4,17 +4,27 @@ class RateLimitChecker
     @user = user
   end
 
+  class UploadRateLimitReached < StandardError; end
+
   def limit_by_situation(situation)
     result = case situation
              when "comment_creation"
                user.comments.where("created_at > ?", 30.seconds.ago).size > 9
              when "published_article_creation"
                user.articles.published.where("created_at > ?", 30.seconds.ago).size > 9
+             when "image_upload"
+               Rails.cache.read("#{user.id}_image_upload").to_i > 9
              else
                false
              end
     ping_admins if result == true
     result
+  end
+
+  def track_image_uploads
+    count = Rails.cache.read("#{@user.id}_image_upload").to_i
+    count += 1
+    Rails.cache.write("#{@user.id}_image_upload", count, expires_in: 30.seconds)
   end
 
   def limit_by_email_recipient_address(address)
