1714c44ba403a3854419dab60523c155ec48d51c
Jess Lee <jessleenyc@gmail.com>
2019-05-17 18:43:15 -0400

    fix specs

--- app/observers/application_observer.rb
+++ app/observers/application_observer.rb
@@ -2,7 +2,7 @@ class ApplicationObserver < ActiveRecord::Observer
   def warned_user_ping(activity)
     return unless activity.user.warned == true
 
-    SlackBot.delay.ping "@#{activity.user.username} just posted.\nManage User- https://dev.to/internal/users/#{activity.user.id}\nPost- https://dev.to#{activity.path}",
+    SlackBot.delay.ping "Activity: https://dev.to/#{activity.path}\nManage @#{activity.user.username}: https://dev.to/internal/users/#{activity.user.id}",
                         channel: "warned-user-activity",
                         username: "sloan_watch_bot",
                         icon_emoji: ":sloan:"
--- app/observers/article_observer.rb
+++ app/observers/article_observer.rb
@@ -2,13 +2,13 @@ class ArticleObserver < ApplicationObserver
   def after_create(article)
     return if Rails.env.development?
 
-    ping_new_article
+    ping_new_article(article)
     warned_user_ping(article)
   rescue StandardError => e
     Rails.logger.error(e)
   end
 
-  def ping_new_article
+  def ping_new_article(article)
     return unless article.published && article.published_at > 30.seconds.ago
 
     SlackBot.delay.ping "New Article Published: #{article.title}\nhttps://dev.to#{article.path}",
