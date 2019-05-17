2c21cddb450d8a658d21e58d030651b4eef2dd48
Corey Alexander <coreyja@gmail.com>
2019-05-16 22:43:34 -0400

    Get the mocking working with a sample repo

--- spec/system/user/user_edits_integrations_spec.rb
+++ spec/system/user/user_edits_integrations_spec.rb
@@ -2,9 +2,20 @@ require "rails_helper"
 
 RSpec.describe "User edits their profile", type: :system, js: true do
   let(:user) { create(:user, saw_onboarding: true) }
+  let(:github_response_body) do
+    [
+      {
+        "id" => 1_296_269,
+        "node_id" => "MDEwOlJlcG9zaXRvcnkxMjk2MjY5",
+        "name" => "Hello-World",
+        "full_name" => "octocat/Hello-World"
+      },
+    ]
+  end
 
   before do
     sign_in user
+    stub_request(:get, "https://api.github.com/user/repos?per_page=100").to_return(status: 200, body: github_response_body.to_json, headers: { "Content-Type" => "application/json" })
   end
 
   describe "via visiting /settings" do
