4a05032cb738d5538ae705e72301c7e2108fce34
Ben Halpern <ben@dev.to>
2019-05-20 10:08:51 -0400

    [WIP] Connect misc improvements/debugs (#2880)
    * WIP Connect misc improvements/debugs
    
    * Modify tests and update snapshots
    
    * Change GitHub sidecar copy

--- app/javascript/packs/Chat.jsx
+++ app/javascript/packs/Chat.jsx
@@ -1,49 +1,20 @@
 import { h, render } from 'preact';
-import { getUserDataAndCsrfToken } from '../chat/util';
 import Chat from '../chat/chat';
 
-function initializeChat(loadChat) {
-  getUserDataAndCsrfToken()
-    .then(loadChat)
-    .catch(error => {
-      // eslint-disable-next-line no-console
-      console.error('Unable to load chat', error);
-    });
-}
-
-function renderChat(root) {
-  render(<Chat {...root.dataset} />, root, root.firstChild);
-}
-
-const loadChat = ({ currentUser, csrfToken }) => {
+function loadElement() {
   const root = document.getElementById('chat');
 
-  if (!root) {
-    return;
-  }
-
-  window.currentUser = currentUser;
-  window.csrfToken = csrfToken;
-
-  const placeholder = document.getElementById('chat_placeholder');
-
-  renderChat(root);
-
-  if (placeholder) {
-    root.removeChild(placeholder);
-  }
-
-  window.InstantClick.on('change', () => {
-    if (window.location.href.indexOf('/connect') === -1) {
-      return;
+  if (root) {
+    render(<Chat {...root.dataset} />, root, root.firstChild);
+    const placeholder = document.getElementById('chat_placeholder');
+    if (placeholder) {
+      root.removeChild(placeholder);
     }
-
-    renderChat(root);
-  });
-};
-
-initializeChat(loadChat);
+  }
+}
 
 window.InstantClick.on('change', () => {
-  initializeChat(loadChat);
+  loadElement();
 });
+
+loadElement();
