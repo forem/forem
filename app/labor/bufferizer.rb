959c1cd87562fbb3181b808e126239357dcfc034
Jess Lee <jessleenyc@gmail.com>
2019-05-23 19:00:34 -0400

    wip

--- app/labor/bufferizer.rb
+++ app/labor/bufferizer.rb
@@ -10,35 +10,40 @@ class Bufferizer
   end
 
   def satellite_tweet!
-    article.tags.find_each do |tag|
-      BufferUpdate.buff!(article.id, twitter_buffer_text, tag.buffer_profile_id_code, "twitter", tag.id) if tag.buffer_profile_id_code.present?
+    @article.tags.find_each do |tag|
+      BufferUpdate.buff!(@article.id, twitter_buffer_text, tag.buffer_profile_id_code, "twitter", tag.id) if tag.buffer_profile_id_code.present?
     end
-    article.update(last_buffered: Time.current)
+    @article.update(last_buffered: Time.current)
   end
 
   def main_teet!
-    BufferUpdate.buff!(article.id, twitter_buffer_text, ApplicationConfig["BUFFER_TWITTER_ID"], "twitter", nil)
-    article.update(last_buffered: Time.current)
+    BufferUpdate.buff!(@article.id, twitter_buffer_text, ApplicationConfig["BUFFER_TWITTER_ID"], "twitter", nil)
+    @article.update(last_buffered: Time.current)
   end
 
   def facebook_post!
-    BufferUpdate.buff!(article.id, fb_buffer_text, ApplicationConfig["BUFFER_FACEBOOK_ID"], "facebook")
-    BufferUpdate.buff!(article.id, fb_buffer_text, ApplicationConfig["BUFFER_LINKEDIN_ID"], "linkedin")
-    article.update(facebook_last_buffered: Time.current)
+    BufferUpdate.buff!(@article.id, fb_buffer_text, ApplicationConfig["BUFFER_FACEBOOK_ID"], "facebook")
+    BufferUpdate.buff!(@article.id, fb_buffer_text, ApplicationConfig["BUFFER_LINKEDIN_ID"], "linkedin")
+    @article.update(facebook_last_buffered: Time.current)
   end
 
   def listings_tweet!
     binding.pry
-    BufferUpdate.buff!(article.id)
+    # buffer_profile_id_code = ApplicationConfig["BUFFER_LISTINGS_PROFILE"]
+    BufferUpdate.send_to_buffer(listings_twitter_text, 123)
   end
 
   private
 
   def twitter_buffer_text
-    "#{text} https://dev.to#{article.path}" if text.size <= 255
+    "#{text} https://dev.to#{@article.path}" if text.size <= 255
   end
 
   def fb_buffer_text
-    "#{text} https://dev.to#{article.path}"
+    "#{text} https://dev.to#{@article.path}"
+  end
+
+  def listings_twitter_text
+    "#{text} https://dev.to#{@listing.path}" if text.size <= 255
   end
 end
