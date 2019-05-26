3841a5b2b039198658efe4d5bf9c26c2c97b4662
Kartikey Tanna <tannakartikey@gmail.com>
2019-05-26 21:15:03 +0530

    Sending email only if record not nil
    As per the review

--- app/jobs/badge_achievements/send_email_notification_job.rb
+++ app/jobs/badge_achievements/send_email_notification_job.rb
@@ -4,7 +4,7 @@ module BadgeAchievements
 
     def perform(badge_achievement_id)
       badge_achievement = BadgeAchievement.find_by(id: badge_achievement_id)
-      NotifyMailer.new_badge_email(badge_achievement).deliver_now
+      NotifyMailer.new_badge_email(badge_achievement).deliver_now if badge_achievement
     end
   end
 end
