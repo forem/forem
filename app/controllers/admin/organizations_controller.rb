72229f39ffa0925e4fb302c4aa437a132104dafa
Zhao-Andy <andyzhao.zhao@gmail.com>
2019-06-04 15:45:02 -0400

    Touch profile_updated_at when updating org

--- app/controllers/admin/organizations_controller.rb
+++ app/controllers/admin/organizations_controller.rb
@@ -8,6 +8,11 @@ module Admin
     #   @resources = Organization.all.paginate(10, params[:page])
     # end
 
+    def update
+      super
+      @requested_resource.touch(:profile_updated_at)
+    end
+
     # Define a custom finder by overriding the `find_resource` method:
     # def find_resource(param)
     #   Organization.find_by!(slug: param)
