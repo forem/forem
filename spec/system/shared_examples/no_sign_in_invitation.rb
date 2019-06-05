f54ac8a0c2c3b98eeb4f45388c89337ac72b0650
Ben Halpern <ben@dev.to>
2019-06-05 13:25:12 -0400

    Fix tests and modify reserved words (tack on)

--- spec/system/shared_examples/no_sign_in_invitation.rb
+++ spec/system/shared_examples/no_sign_in_invitation.rb
@@ -1,7 +1,7 @@
 RSpec.shared_examples "no sign_in invitation" do
   it "no sign_in invitation", js: true do
     within("#substories") do
-      expect(page).not_to have_content("Sign in to customize your feed")
+      expect(page).not_to have_content("amazing humans who code")
       expect(page).not_to have_link("TWITTER")
       expect(page).not_to have_link("GITHUB")
     end
--- spec/system/shared_examples/shows_sign_in_invitation.rb
+++ spec/system/shared_examples/shows_sign_in_invitation.rb
@@ -1,7 +1,7 @@
 RSpec.shared_examples "shows the sign_in invitation" do
   it "shows the sign_in invitation", js: true do
     within("#substories") do
-      expect(page).to have_content("Sign in to customize your feed")
+      expect(page).to have_content("amazing humans who code")
       expect(page).to have_link("TWITTER")
       expect(page).to have_link("GITHUB")
     end
