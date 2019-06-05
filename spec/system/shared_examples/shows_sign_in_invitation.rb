3990d08437ad4c8479840482bbdb462ec91ca864
Ben Halpern <ben@dev.to>
2019-06-05 13:41:36 -0400

    Removed less useful shared examples from rspec

--- spec/system/shared_examples/shows_sign_in_invitation.rb
+++
@@ -1,9 +0,0 @@
-RSpec.shared_examples "shows the sign_in invitation" do
-  it "shows the sign_in invitation", js: true do
-    within("#substories") do
-      expect(page).to have_content("amazing humans who code")
-      expect(page).to have_link("TWITTER")
-      expect(page).to have_link("GITHUB")
-    end
-  end
-end
