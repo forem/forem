ddeec265ae2a6a08a7395839f7bd02933c6ccd14
Mac Siri <krairit.siri@gmail.com>
2019-06-09 15:05:19 -0400

    Simplify clipboardCopy

--- app/javascript/packs/clipboardCopy.js
+++ app/javascript/packs/clipboardCopy.js
@@ -1,10 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const waitingForOnboarding = setInterval(() => {
-    if (window.WebComponents) {
-      window.WebComponents.waitFor(() => {
-        import('@github/clipboard-copy-element');
-      });
-      clearInterval(waitingForOnboarding);
-    }
-  }, 500);
+  window.WebComponents.waitFor(() => {
+    import('@github/clipboard-copy-element');
+  });
 });
