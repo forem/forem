9e385075c1dc42a572c3bbc137ac74b1deb64bbc
Mac Siri <krairit.siri@gmail.com>
2019-06-09 13:09:05 -0400

    Make WebComponent loading async

--- app/javascript/packs/clipboardCopy.js
+++ app/javascript/packs/clipboardCopy.js
@@ -1,1 +1,10 @@
-import '@github/clipboard-copy-element';
+document.addEventListener('DOMContentLoaded', () => {
+  const waitingForOnboarding = setInterval(() => {
+    if (window.WebComponents) {
+      window.WebComponents.waitFor(() => {
+        import('@github/clipboard-copy-element');
+      });
+      clearInterval(waitingForOnboarding);
+    }
+  }, 500);
+});
