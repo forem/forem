da5871587556ff53dde199e2e6a8a52b33b8dad9
lightalloy <lightallloy@gmail.com>
2019-06-12 14:17:27 +0300

    Add ahoy_messages index concurrently

--- db/migrate/20190410124957_add_index_to_ahoy_messages_to.rb
+++ db/migrate/20190410124957_add_index_to_ahoy_messages_to.rb
@@ -1,5 +1,7 @@
 class AddIndexToAhoyMessagesTo < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
   def change
-    add_index :ahoy_messages, :to
+    add_index :ahoy_messages, :to, algorithm: :concurrently
   end
 end
