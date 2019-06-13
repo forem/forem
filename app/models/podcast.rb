bb588d51c3ab9e0ae4dea13522bb4bf0d0154fe0
lightalloy <lightallloy@gmail.com>
2019-06-13 15:39:24 +0300

    Validate podcast slug uniqueness only if it was changed

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -11,7 +11,7 @@ class Podcast < ApplicationRecord
             uniqueness: true,
             format: { with: /\A[a-zA-Z0-9\-_]+\Z/ },
             exclusion: { in: ReservedWords.all, message: "slug is reserved" }
-  validate :unique_slug_including_users_orgs
+  validate :unique_slug_including_users_and_orgs, if: :slug_changed?
 
   after_save :bust_cache
   after_create :pull_all_episodes
@@ -22,7 +22,7 @@ class Podcast < ApplicationRecord
 
   private
 
-  def unique_slug_including_users_orgs
+  def unique_slug_including_users_and_orgs
     errors.add(:slug, "is taken.") if User.find_by(username: slug) || Organization.find_by(slug: slug)
   end
 
