b7431725962b889bf6ed285f170788cff59346cc
Zhao-Andy <andyzhao.zhao@gmail.com>
2019-06-13 09:36:32 -0400

    Deprecated article mutes in favor of notification subscriptions

--- app/controllers/notification_subscriptions_controller.rb
+++ app/controllers/notification_subscriptions_controller.rb
@@ -9,10 +9,12 @@ class NotificationSubscriptionsController < ApplicationController
   def upsert
     raise RateLimitChecker::UploadRateLimitReached if RateLimitChecker.new(current_user).limit_by_situation("notification_subscription")
 
-    @notification_subscription = NotificationSubscription.find_or_initialize_by(user_id: current_user.id, notifiable_id: params[:notifiable_id], notifiable_type: params[:notifiable_type])
+    @notification_subscription = NotificationSubscription.find_or_initialize_by(user_id: current_user.id, notifiable_id: params[:notifiable_id], notifiable_type: params[:notifiable_type].capitalize)
     if params[:subscription_status] == "true"
+      @notification_subscription.notifiable.update(receive_notifications: false) if current_user_is_author_and_notifiable_is_article?
       @notification_subscription.delete
     else
+      @notification_subscription.notifiable.update(receive_notifications: true) if current_user_is_author_and_notifiable_is_article?
       @notification_subscription.save!
     end
 
@@ -23,10 +25,18 @@ class NotificationSubscriptionsController < ApplicationController
     result = @notification_subscription.persisted?
     respond_to do |format|
       format.json { render json: result }
+      format.html { redirect_to request.referer }
     end
   rescue RateLimitChecker::UploadRateLimitReached
     respond_to do |format|
       format.json { render json: "Subscription rate limit reached".to_json }
     end
+  end
+
+  private
+
+  def current_user_is_author_and_notifiable_is_article?
+    # TODO: think of a better solution for handling mute notifications in dashboard / manage
+    current_user.id == @notification_subscription.notifiable.user_id && @notification_subscription.notifiable_type == "Article"
   end
 end
