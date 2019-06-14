7f82fb52193e6a43ad24cd4fd90fce91ff8e0541
Mario See <mariocsee@users.noreply.github.com>
2019-06-14 10:45:01 -0400

    finalize tests

--- app/javascript/listings/listingDashboard.jsx
+++ app/javascript/listings/listingDashboard.jsx
@@ -57,7 +57,7 @@ export class ListingDashboard extends Component {
           <div className="dashboard-listings-header">
             <h3>Credits</h3>
             {creditCount(selectedListings, userCredits, orgs)}
-            <a href="/credits/purchase" data-no-instant>Buy More Credits</a>
+            <a href="/credits/purchase" data-no-instant>Buy Credits</a>
           </div>
         </div>
         <div className="dashboard-listings-view">
