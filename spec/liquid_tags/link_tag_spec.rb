f05cdd3b726bbf480f0a924ff886d3001ffe7830
Tyler VanBlargan <12vanblart@gmail.com>
2019-06-17 12:14:39 -0400

    Add Date to Link Liquid Tag (#3126) [ci ski]

--- spec/liquid_tags/link_tag_spec.rb
+++ spec/liquid_tags/link_tag_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe LinkTag, type: :liquid_template do
         <a href='#{article.path}' class='ltag__link__link'>
           <div class='ltag__link__content'>
             <h2>#{ActionController::Base.helpers.strip_tags(article.title)}</h2>
-            <h3>#{article.user.name} ・ #{article.reading_time} min read</h3>
+            <h3>#{article.user.name} ・ #{article.readable_publish_date} ・ #{article.reading_time} min read</h3>
             <div class='ltag__link__taglist'>
               #{tags}
             </div>
