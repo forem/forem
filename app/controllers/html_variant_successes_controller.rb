e2887c40b04f40e0f46501dca7bd93cba3c1419c
cyrillefr <cyrille.vasseur@gmail.com>
2019-06-19 03:36:33 +0200

    Move HtmlVariantSuccess.create delay calls to ActiveJob (#3173)
    - add AJ job + job spec
      - refactor call

--- app/controllers/html_variant_successes_controller.rb
+++ app/controllers/html_variant_successes_controller.rb
@@ -1,6 +1,6 @@
 class HtmlVariantSuccessesController < ApplicationController
   def create
-    HtmlVariantSuccess.delay.create(html_variant_id: params[:html_variant_id], article_id: params[:article_id])
+    HtmlVariantSuccessCreateJob.perform_later(html_variant_id: params[:html_variant_id], article_id: params[:article_id])
     head :ok
   end
 end
