f3a84af82c43992325af484e970877b18b62c70d
rhymes <rhymesete@gmail.com>
2019-06-24 15:33:41 +0200

    Refactor JS analytics client

--- app/javascript/analytics/client.js
+++ app/javascript/analytics/client.js
@@ -1,13 +1,7 @@
 import handleFetchAPIErrors from '../src/utils/errors';
 
-export function callHistoricalAPI(
-  date,
-  { organizationId, articleId },
-  callback,
-) {
-  let url = `/api/analytics/historical?start=${
-    date.toISOString().split('T')[0]
-  }`;
+function callAnalyticsAPI(path, date, { organizationId, articleId }, callback) {
+  let url = `${path}?start=${date.toISOString().split('T')[0]}`;
 
   if (organizationId) {
     url = `${url}&organization_id=${organizationId}`;
@@ -24,26 +18,28 @@ export function callHistoricalAPI(
     .catch(error => console.error(error)); // we should come up with better error handling
 }
 
+export function callHistoricalAPI(
+  date,
+  { organizationId, articleId },
+  callback,
+) {
+  callAnalyticsAPI(
+    '/api/analytics/historical',
+    date,
+    { organizationId, articleId },
+    callback,
+  );
+}
+
 export function callReferrersAPI(
   date,
   { organizationId, articleId },
   callback,
 ) {
-  let url = `/api/analytics/referrers?start=${
-    date.toISOString().split('T')[0]
-  }`;
-
-  if (organizationId) {
-    url = `${url}&organization_id=${organizationId}`;
-  }
-  if (articleId) {
-    url = `${url}&article_id=${articleId}`;
-  }
-
-  fetch(url)
-    .then(handleFetchAPIErrors)
-    .then(response => response.json())
-    .then(callback)
-    // eslint-disable-next-line no-console
-    .catch(error => console.error(error)); // we should come up with better error handling
+  callAnalyticsAPI(
+    '/api/analytics/referrers',
+    date,
+    { organizationId, articleId },
+    callback,
+  );
 }
