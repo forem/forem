17852e91e1e85ef12cfa76b61225fc759366b985
rhymes <rhymesete@gmail.com>
2019-06-24 15:33:27 +0200

    Add not null and empty default to domain and path

--- db/migrate/20190624093012_add_domain_and_path_to_page_views.rb
+++ db/migrate/20190624093012_add_domain_and_path_to_page_views.rb
@@ -1,7 +1,7 @@
 class AddDomainAndPathToPageViews < ActiveRecord::Migration[5.2]
   def change
-    add_column :page_views, :domain, :string
+    add_column :page_views, :domain, :string, null: false, default: ""
     add_index :page_views, :domain
-    add_column :page_views, :path, :string
+    add_column :page_views, :path, :string, null: false, default: ""
   end
 end
