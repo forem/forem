b2b954ebaaa68d0d76f261246689cd10c8dd8ea0
rhymes <rhymesete@gmail.com>
2019-06-25 16:50:13 +0200

    Add index on domain concurrently

--- db/migrate/20190624093012_add_domain_and_path_to_page_views.rb
+++ db/migrate/20190624093012_add_domain_and_path_to_page_views.rb
@@ -1,7 +1,6 @@
 class AddDomainAndPathToPageViews < ActiveRecord::Migration[5.2]
   def change
     add_column :page_views, :domain, :string
-    add_index :page_views, :domain
     add_column :page_views, :path, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_24_093012) do
+ActiveRecord::Schema.define(version: 2019_06_25_143841) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
