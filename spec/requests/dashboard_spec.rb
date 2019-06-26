3b6a38c526727d2eafbfc9e521d675f05cf8129d
Zhao-Andy <andyzhao.zhao@gmail.com>
2019-06-26 14:06:09 -0400

    Hide delete article button for non-authors

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "Dashboards", type: :request do
   let(:super_admin)   { create(:user, :super_admin) }
   let(:pro_user)      { create(:user, :pro) }
   let(:article)       { create(:article, user: user) }
+  let(:unpublished_article) { create(:article, user: user, published: false) }
 
   describe "GET /dashboard" do
     context "when not logged in" do
@@ -16,18 +17,25 @@ RSpec.describe "Dashboards", type: :request do
     end
 
     context "when logged in" do
-      it "renders user's articles" do
+      before do
         sign_in user
         article
+      end
+
+      it "renders user's articles" do
         get "/dashboard"
         expect(response.body).to include(CGI.escapeHTML(article.title))
       end
 
       it 'does not show "STATS" for articles' do
-        sign_in user
-        article
         get "/dashboard"
         expect(response.body).not_to include("STATS")
+      end
+
+      it "renders the delete button for drafts" do
+        unpublished_article
+        get "/dashboard"
+        expect(response.body).to include "DELETE"
       end
     end
 
@@ -67,8 +75,18 @@ RSpec.describe "Dashboards", type: :request do
         create(:organization_membership, user: user, organization: organization, type_of_user: "admin")
         article.update(organization_id: organization.id)
         sign_in user
-        get "/dashboard/organization"
-        expect(response.body).to include "#{CGI.escapeHTML(organization.name)} ("
+        get "/dashboard/organization/#{organization.id}"
+        expect(response.body).to include "dashboard-collection-org-details"
+      end
+
+      it "does not render the delete button for other org member's drafts" do
+        create(:organization_membership, user: user, organization: organization, type_of_user: "member")
+        create(:organization_membership, user: second_user, organization: organization, type_of_user: "admin")
+        unpublished_article.update(organization_id: organization.id)
+        sign_in second_user
+        get "/dashboard/organization/#{organization.id}"
+        expect(response.body).not_to include "DELETE"
+        expect(response.body).to include unpublished_article.title
       end
     end
   end
