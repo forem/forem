33b38724759e3aabfe605a0a595bef49d864fe76
Anna Buianova <lightallloy@gmail.com>
2019-07-02 16:18:27 +0300

    Podcasts reachable status #2952 (#3341)
    * Podcasts reachable status #2952
    
    * Specs for podcasts statuses

--- spec/system/podcasts/user_visits_podcasts_root_page_spec.rb
+++ spec/system/podcasts/user_visits_podcasts_root_page_spec.rb
@@ -3,6 +3,8 @@ require "rails_helper"
 RSpec.describe "User visits /pod page", type: :system do
   let!(:podcast_episode) { create(:podcast_episode) }
   let!(:podcast_episode2) { create(:podcast_episode) }
+  let(:podcast) { create(:podcast, reachable: false) }
+  let!(:un_podcast_episode) { create(:podcast_episode, podcast: podcast) }
 
   before { visit "/pod" }
 
@@ -10,6 +12,12 @@ RSpec.describe "User visits /pod page", type: :system do
     within "#articles-list" do
       expect(page).to have_link(nil, href: podcast_episode.path)
       expect(page).to have_link(nil, href: podcast_episode2.path)
+    end
+  end
+
+  it "doesn't display an unreachable podcast" do
+    within "#articles-list" do
+      expect(page).not_to have_link(nil, href: un_podcast_episode.path)
     end
   end
 end
