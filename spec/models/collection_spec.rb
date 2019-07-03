9ca6d5f480290ffe1be84f397f82dcc5130df92b
Mac Siri <krairit.siri@gmail.com>
2019-07-03 12:11:04 -0400

    Simplify spec

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -1,24 +1,13 @@
 require "rails_helper"
 
 RSpec.describe Collection, type: :model do
-  include ActiveSupport::Testing::TimeHelpers
-
   let(:user) { create(:user) }
-  let(:collection) { create(:collection, :with_articles, user_id: user.id) }
+  let(:collection) { create(:collection, :with_articles, user: user) }
 
   describe "when a single article in collection is updated" do
     it "touches all articles in the collection" do
-      article = collection.articles.sample
-      duration = 3.days
-
-      old_times = collection.articles.map { |a| a.updated_at.to_i }
-      new_times = old_times.map { |t| (t + duration).to_i }
-
-      travel(duration) do
-        expect do
-          article.touch
-        end.to change { collection.reload.articles.map { |a| a.updated_at.to_i } }.from(old_times).to(new_times)
-      end
+      random_article = collection.articles.sample
+      expect { random_article.touch }.to change { collection.articles.map(&:updated_at) }
     end
   end
 end
