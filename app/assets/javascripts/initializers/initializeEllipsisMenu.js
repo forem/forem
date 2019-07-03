b738b0d530d51e2a7f78d8d532e0646d5b1d7626
Mac Siri <krairit.siri@gmail.com>
2019-07-03 13:40:09 -0400

    Fix unarchive issue

--- app/assets/javascripts/initializers/initializeEllipsisMenu.js
+++ app/assets/javascripts/initializers/initializeEllipsisMenu.js
@@ -58,9 +58,10 @@ function handleFormSubmit(e) {
   var values = getFormValues(form);
   var data = JSON.stringify(values);
 
-  var xhr = new XMLHttpRequest();
   var formData = new FormData(form);
   var method = formData.get('_method') || 'post';
+
+  var xhr = new XMLHttpRequest();
   xhr.open(method.toUpperCase(), form.action);
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.send(data);
