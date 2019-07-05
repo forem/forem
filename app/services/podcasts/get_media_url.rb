79ecd16cf700ffeba5c09f78f89f9433ee73d590
lightalloy <lightallloy@gmail.com>
2019-07-05 14:17:02 +0300

    Podcasts::GetMediaUrl class and specs

---
+++ app/services/podcasts/get_media_url.rb
@@ -0,0 +1,41 @@
+module Podcasts
+  class GetMediaUrl
+    def initialize(enclosure_url)
+      @enclosure_url = enclosure_url
+    end
+
+    def self.call(*args)
+      new(*args).call
+    end
+
+    def call
+      was_http = !enclosure_url.starts_with?("https")
+      https_url = enclosure_url.sub(/http:/, "https:")
+
+      # check https url first
+      if url_reachable?(https_url)
+        reachable = true
+        url = https_url
+      # if https is unreachable, check http url (if it was provided)
+      else
+        reachable = was_http ? url_reachable?(enclosure_url) : false
+        url = enclosure_url
+      end
+      result_sctruct.new(https: url.starts_with?("https"), reachable: reachable, url: url)
+    end
+
+    private
+
+    attr_reader :enclosure_url
+
+    def result_sctruct
+      Struct.new(:https, :reachable, :url, keyword_init: true)
+    end
+
+    def url_reachable?(url)
+      HTTParty.head(url).code == 200
+    rescue Net::OpenTimeout, Errno::ECONNREFUSED
+      false
+    end
+  end
+end
