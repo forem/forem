50192a469918505c30dfaa6a654a1711456f2af3
rhymes <rhymesete@gmail.com>
2019-07-08 10:37:42 +0200

    Localize ledger datetimes

--- app/assets/javascripts/initializers/initializeCreditsPage.js
+++ app/assets/javascripts/initializers/initializeCreditsPage.js
@@ -1,8 +1,13 @@
-/* global addLocalizedDateTimeToElementsTitles */
+/* global localizeTimeElements */
 function initializeCreditsPage() {
-  // const dates = document.querySelectorAll('.ledger time');
-  // const replaceText = (element, localizedDateTime) => {
-  //   element.textContent = localizedDateTime;
-  // }
-  // addLocalizedDateTimeToElementsTitles(dates, 'datetime', { callback: replaceText });
+  const datetimes = document.querySelectorAll('.ledger time');
+
+  localizeTimeElements(datetimes, {
+    year: 'numeric',
+    month: 'short',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
 }
