a3dbd487ce66a72881edb27224f393bdfe59b407
Mario See <mariocsee@users.noreply.github.com>
2019-07-09 15:30:06 -0400

    shorten limit and update location in edit page

--- app/models/classified_listing.rb
+++ app/models/classified_listing.rb
@@ -18,7 +18,7 @@ class ClassifiedListing < ApplicationRecord
                     length: { maximum: 128 }
   validates :body_markdown, presence: true,
                             length: { maximum: 400 }
-  validates :location, length: { maximum: 64 }
+  validates :location, length: { maximum: 32 }
   validate :restrict_markdown_input
   validate :validate_tags
   validate :validate_category
