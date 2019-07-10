df2d83c936d14570d7ce85df9fdee76c92657ba2
rhymes <rhymesete@gmail.com>
2019-07-10 14:27:52 +0200

    Use constants for enums

--- app/models/sponsorship.rb
+++ app/models/sponsorship.rb
@@ -1,10 +1,21 @@
 class Sponsorship < ApplicationRecord
+  LEVELS = %w[gold silver bronze tag media devrel].freeze
+  STATUSES = %w[none pending live].freeze
+  # media has no fixed amount of credits
+  CREDITS = {
+    gold: 6_000,
+    silver: 500,
+    bronze: 100,
+    tag: 300,
+    devrel: 500
+  }.with_indifferent_access.freeze
+
   belongs_to :user
   belongs_to :organization, inverse_of: :sponsorships
   belongs_to :sponsorable, polymorphic: true, optional: true
 
   validates :user, :organization, :featured_number, presence: true
-  validates :level, inclusion: { in: %w[gold silver bronze tag media devrel] }
-  validates :status, inclusion: { in: %w[none pending live] }
+  validates :level, inclusion: { in: LEVELS }
+  validates :status, inclusion: { in: STATUSES }
   validates :url, url: { allow_blank: true, no_local: true, schemes: %w[http https] }
 end
--- spec/models/sponsorship_spec.rb
+++ spec/models/sponsorship_spec.rb
@@ -6,13 +6,22 @@ RSpec.describe Sponsorship, type: :model do
   it { is_expected.to belong_to(:sponsorable).optional }
   it { is_expected.to validate_presence_of(:user) }
   it { is_expected.to validate_presence_of(:organization) }
-  it { is_expected.to validate_inclusion_of(:level).in_array(%w[gold silver bronze tag media devrel]) }
-  it { is_expected.to validate_inclusion_of(:status).in_array(%w[none pending live]) }
+  it { is_expected.to validate_inclusion_of(:level).in_array(Sponsorship::LEVELS) }
+  it { is_expected.to validate_inclusion_of(:status).in_array(Sponsorship::STATUSES) }
   it { is_expected.to allow_values(nil).for(:expires_at) }
   it { is_expected.not_to allow_values(nil).for(:featured_number) }
   it { is_expected.to have_db_index(:level) }
   it { is_expected.to have_db_index(:status) }
   it { is_expected.to have_db_index(%i[sponsorable_id sponsorable_type]) }
+
+  describe "constants" do
+    it "has the correct values for constants" do
+      expect(Sponsorship::LEVELS).to eq(%w[gold silver bronze tag media devrel])
+      expect(Sponsorship::STATUSES).to eq(%w[none pending live])
+      expected_credits = { gold: 6000, silver: 500, bronze: 100, tag: 300, devrel: 500 }.with_indifferent_access
+      expect(Sponsorship::CREDITS).to eq(expected_credits)
+    end
+  end
 
   describe "#url" do
     let(:sponsorship) { build(:sponsorship) }
