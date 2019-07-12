7f4a66bd7147f581bbb1bec6e3a89b62663f1ece
lightalloy <lightallloy@gmail.com>
2019-07-12 15:55:29 +0300

    Define model in migration for safety in the long term

--- db/migrate/20190705114625_add_https_status_to_podcast_episodes.rb
+++ db/migrate/20190705114625_add_https_status_to_podcast_episodes.rb
@@ -1,4 +1,6 @@
 class AddHttpsStatusToPodcastEpisodes < ActiveRecord::Migration[5.2]
+  class PodcastEpisode < ApplicationRecord; end
+
   def change
     add_column :podcast_episodes, :https, :boolean, default: true
 
