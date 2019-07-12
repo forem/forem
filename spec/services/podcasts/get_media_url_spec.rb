a2851202ce048c9eb0d308af915efac8ce59eba4
lightalloy <lightallloy@gmail.com>
2019-07-12 15:39:26 +0300

    Remove redundant comments

--- spec/services/podcasts/get_media_url_spec.rb
+++ spec/services/podcasts/get_media_url_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Podcasts::GetMediaUrl do
   let(:https_url) { "https://hello.example.com" }
   let(:http_url) { "http://hello.example.com" }
 
-  # https, reachable
   it "https, reachable" do
     stub_request(:head, https_url).to_return(status: 200)
     result = described_class.call(https_url)
@@ -13,7 +12,6 @@ RSpec.describe Podcasts::GetMediaUrl do
     expect(result.url).to eq(https_url)
   end
 
-  # https, unreachable
   it "https, unrechable" do
     stub_request(:head, https_url).to_return(status: 404)
     result = described_class.call(https_url)
@@ -22,7 +20,6 @@ RSpec.describe Podcasts::GetMediaUrl do
     expect(result.url).to eq(https_url)
   end
 
-  # http, https reachable
   it "http, https reachable" do
     stub_request(:head, https_url).to_return(status: 200)
     result = described_class.call(http_url)
@@ -31,7 +28,6 @@ RSpec.describe Podcasts::GetMediaUrl do
     expect(result.url).to eq(https_url)
   end
 
-  # http, https unreachable, http reachable
   it "http, https unreachable, http reachable" do
     httparty_result = double
     allow(httparty_result).to receive(:code).and_return(200)
@@ -43,7 +39,6 @@ RSpec.describe Podcasts::GetMediaUrl do
     expect(result.url).to eq(http_url)
   end
 
-  # http, https unreachable, http unreachable
   it "http, https unreachable" do
     allow(HTTParty).to receive(:head).with(https_url).and_raise(Errno::ECONNREFUSED)
     allow(HTTParty).to receive(:head).with(http_url).and_raise(Errno::ECONNREFUSED)
