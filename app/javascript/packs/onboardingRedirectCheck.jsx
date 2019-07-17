fd63508ffcb3dc73c552199a34f257a0d2d473b3
Zhao-Andy <andyzhao.zhao@gmail.com>
2019-07-17 08:46:56 -0400

    Reuse ready prototype logic

--- app/javascript/packs/onboardingRedirectCheck.jsx
+++ app/javascript/packs/onboardingRedirectCheck.jsx
@@ -1,5 +1,13 @@
 import { getUserDataAndCsrfToken } from '../chat/util';
 
+HTMLDocument.prototype.ready = new Promise(resolve => {
+  if (document.readyState !== 'loading') {
+    return resolve();
+  }
+  document.addEventListener('DOMContentLoaded', () => resolve());
+  return null;
+});
+
 function redirectableLocation() {
   return (
     window.location.pathname !== '/onboarding' &&
@@ -7,17 +15,15 @@ function redirectableLocation() {
   );
 }
 
-document.onreadystatechange = () => {
-  if (document.readyState === 'complete') {
-    getUserDataAndCsrfToken()
-      .then(({ currentUser }) => {
-        if (redirectableLocation() && !currentUser.saw_onboarding) {
-          window.location = `${window.location.origin}/onboarding`;
-        }
-      })
-      .catch(error => {
-        // eslint-disable-next-line no-console
-        console.error('Error getting user and CSRF Token', error);
-      });
-  }
-};
+document.ready.then(
+  getUserDataAndCsrfToken()
+    .then(({ currentUser }) => {
+      if (redirectableLocation() && !currentUser.saw_onboarding) {
+        window.location = `${window.location.origin}/onboarding`;
+      }
+    })
+    .catch(error => {
+      // eslint-disable-next-line no-console
+      console.error('Error getting user and CSRF Token', error);
+    }),
+);
