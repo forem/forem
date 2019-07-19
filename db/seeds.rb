ec6d8b347677eb76b068e2d225a7ff40ca10d849
Zhao-Andy <andyzhao.zhao@gmail.com>
2019-07-19 08:24:21 -0400

    Add additional identity data for internal/users/edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,7 +44,12 @@ User.clear_index!
     token: i.to_s,
     secret: i.to_s,
     user: user,
-    auth_data_dump: { "extra" => { "raw_info" => { "lang" => "en" } } },
+    auth_data_dump: {
+      "extra" => {
+        "raw_info" => { "lang" => "en" }
+      },
+      "info" => { "nickname" => user.username }
+    },
   )
 end
 
