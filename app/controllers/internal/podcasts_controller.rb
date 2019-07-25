6a4a76918dc8d5c19752303124fac21a6805df38
lightalloy <lightallloy@gmail.com>
2019-07-25 17:46:50 +0300

    Adding and removing podcast admin roles

--- app/controllers/internal/podcasts_controller.rb
+++ app/controllers/internal/podcasts_controller.rb
@@ -1,17 +1,16 @@
 class Internal::PodcastsController < Internal::ApplicationController
   layout "internal"
 
+  before_action :find_podcast, only: %i[edit update remove_admin add_admin]
+
   def index
     @podcasts = Podcast.order("created_at DESC").page(params[:page]).per(50)
     @podcasts = @podcasts.where("podcasts.title ILIKE :search", search: "%#{params[:search]}%") if params[:search].present?
   end
 
-  def edit
-    @podcast = Podcast.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @podcast = Podcast.find(params[:id])
     if @podcast.update(podcast_params)
       redirect_to internal_podcast_path(@podcast), notice: "Всё супер-ок"
     else
@@ -19,16 +18,38 @@ class Internal::PodcastsController < Internal::ApplicationController
     end
   end
 
-  private
-
-  # TODO: implement
   def remove_admin
-    # user.remove_role(:podcast_admin, podcast)
+    user = User.find_by(id: params[:podcast][:user_id])
+    unless user
+      redirect_to edit_internal_podcast_path(@podcast), notice: "No such user"
+      return
+    end
+    removed_roles = user.remove_role(:podcast_admin, @podcast)
+    if removed_roles.empty?
+      redirect_to internal_podcast_path(@podcast), notice: "Error"
+    else
+      redirect_to internal_podcasts_path, notice: "Removed roles"
+    end
   end
 
-  # TODO: implement
   def add_admin
-    # user.add_role(:podcast_admin, podcast)
+    user = User.find_by(id: params[:user_id])
+    unless user
+      redirect_to edit_internal_podcast_path(@podcast), notice: "No such user"
+      return
+    end
+    role = user.add_role(:podcast_admin, @podcast)
+    if role.persisted?
+      redirect_to internal_podcasts_path, notice: "Added role"
+    else
+      redirect_to internal_podcast_path(@podcast), notice: "Error"
+    end
+  end
+
+  private
+
+  def find_podcast
+    @podcast = Podcast.find(params[:id])
   end
 
   def podcast_params
