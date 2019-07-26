ffe37f22f377c18de76e6ce8927ba8b6aa9751d2
lightalloy <lightallloy@gmail.com>
2019-07-26 14:26:39 +0300

    More internal podcasts specs

--- spec/requests/internal/podcasts_spec.rb
+++ spec/requests/internal/podcasts_spec.rb
@@ -1,15 +1,27 @@
 require "rails_helper"
 
 RSpec.describe "/internal/podcasts", type: :request do
-  describe "PUT /internal/podcasts/:id" do
-    let(:admin) { create(:user, :super_admin) }
-    let(:podcast) { create(:podcast) }
-    let(:user) { create(:user) }
+  let(:admin) { create(:user, :super_admin) }
+  let(:podcast) { create(:podcast) }
+  let(:user) { create(:user) }
 
+  before do
+    sign_in admin
+  end
+
+  describe "GET /internal/podcasts" do
     before do
-      sign_in admin
+      create_list(:podcast, 3)
+      user.add_role(:podcast_admin, Podcast.order("random()").first)
     end
 
+    it "renders success" do
+      get internal_podcasts_path
+      expect(response).to be_success
+    end
+  end
+
+  describe "Adding admin" do
     it "adds an admin" do
       expect do
         post add_admin_internal_podcast_path(podcast.id), params: { podcast: { user_id: user.id } }
@@ -22,7 +34,9 @@ RSpec.describe "/internal/podcasts", type: :request do
       post add_admin_internal_podcast_path(podcast.id), params: { podcast: { user_id: user.id + 1 } }
       expect(response).to redirect_to(edit_internal_podcast_path(podcast))
     end
+  end
 
+  describe "Removing admin" do
     it "removes an admin" do
       user.add_role(:podcast_admin, podcast)
       expect do
@@ -34,6 +48,20 @@ RSpec.describe "/internal/podcasts", type: :request do
     it "does nothing when removing an admin for non-existent user" do
       delete remove_admin_internal_podcast_path(podcast.id), params: { podcast: { user_id: user.id + 1 } }
       expect(response).to redirect_to(edit_internal_podcast_path(podcast))
+    end
+  end
+
+  describe "Updating" do
+    it "updates" do
+      put internal_podcast_path(podcast), params: { podcast: { title: "hello", feed_url: "https://pod.example.com/rss.rss" } }
+      podcast.reload
+      expect(podcast.title).to eq("hello")
+      expect(podcast.feed_url).to eq("https://pod.example.com/rss.rss")
+    end
+
+    it "redirects after update" do
+      put internal_podcast_path(podcast), params: { podcast: { title: "hello", feed_url: "https://pod.example.com/rss.rss" } }
+      expect(response).to redirect_to(internal_podcasts_path)
     end
   end
 end
