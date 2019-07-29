cfe495dc47c687f09fa7364cee32ad335ec643f3
lightalloy <lightallloy@gmail.com>
2019-07-29 17:12:10 +0300

    Remove commented code

--- spec/services/notifications/new_mention/send_spec.rb
+++ spec/services/notifications/new_mention/send_spec.rb
@@ -4,11 +4,6 @@ RSpec.describe Notifications::NewMention::Send, type: :service do
   let(:user) { create(:user) }
   let(:comment) { create(:comment, commentable: create(:article)) }
   let(:mention) { create(:mention, mentionable: comment, user: user) }
-  # let(:user2)           { create(:user) }
-  # let(:user3)           { create(:user) }
-  # let(:organization)    { create(:organization) }
-  # let(:article)         { create(:article, user_id: user.id, page_views_count: 4000, positive_reactions_count: 70) }
-  # let!(:follow)         { user.follow(user2) }
 
   it "creates a mention notification" do
     expect do
