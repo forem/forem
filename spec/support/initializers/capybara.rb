dca53a4f9a9dea0cf7f115b58e13e27d0e634380
Mac Siri <krairit.siri@gmail.com>
2019-07-30 17:27:46 -0400

    Update Capybara options

--- spec/support/initializers/capybara.rb
+++ spec/support/initializers/capybara.rb
@@ -9,16 +9,15 @@ Capybara.default_max_wait_time = 5
 
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w[headless disable-gpu no-sandbox window-size=1400,2000] },
+    chromeOptions: {
+      args: %w[
+        headless disable-gpu no-sandbox
+        --window-size=1980,1080 --enable-features=NetworkService,NetworkServiceInProcess
+      ]
+    },
   )
-  Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
-end
 
-Capybara.register_driver :chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w[no-sandbox window-size=1400,2000] },
-  )
-  Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+  Capybara::Selenium::Driver.new app, browser: :chrome, desired_capabilities: capabilities
 end
 
 RSpec.configure do |config|
