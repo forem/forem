02bd0b7c534ced0bc77a9c62ce1dcd96f2548a82
Mac Siri <krairit.siri@gmail.com>
2019-07-30 16:59:30 -0400

    Update Chromedriver version

--- spec/support/initializers/capybara.rb
+++ spec/support/initializers/capybara.rb
@@ -2,7 +2,7 @@ require "capybara/rails"
 require "capybara/rspec"
 require "webdrivers/chromedriver"
 
-Webdrivers::Chromedriver.required_version = "73.0.3683.68"
+Webdrivers::Chromedriver.required_version = "76.0.3809.68"
 Webdrivers.cache_time = 86_400
 
 Capybara.default_max_wait_time = 5
