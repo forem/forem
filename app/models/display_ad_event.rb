9a2c856dd41f6c2370bec0d12c034cdbf3137b1e
Mac Siri <krairit.siri@gmail.com>
2019-08-01 16:38:27 -0400

    Update Complexity linters (#3507) [ci skip]

--- app/models/display_ad_event.rb
+++ app/models/display_ad_event.rb
@@ -1,8 +1,7 @@
 class DisplayAdEvent < ApplicationRecord
-
   belongs_to :display_ad
   belongs_to :user
 
-  validates :category, inclusion: {in: %w[impression click]}
-  validates :context_type, inclusion: {in: %w[home]}
+  validates :category, inclusion: { in: %w[impression click] }
+  validates :context_type, inclusion: { in: %w[home] }
 end
