1cd34cea8c61070130486f091b08ac250be9bd76
lightalloy <lightallloy@gmail.com>
2019-08-01 12:26:43 +0300

    Update schema.rb with db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_122552) do
+ActiveRecord::Schema.define(version: 2019_08_01_083510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
