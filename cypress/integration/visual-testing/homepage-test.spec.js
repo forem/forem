e69dc11ecd6f4a031515cf60e89e9fce0328f0fb
Megha Sachdev <meghasachdev12@gmail.com>
2019-08-02 12:36:34 -0700

    Adding Test

--- cypress/integration/visual-testing/homepage-test.spec.js
+++ cypress/integration/visual-testing/homepage-test.spec.js
@@ -29,4 +29,17 @@ describe('Visual  Regression Tests', () => {
       cy.eyesCheckWindow('Code of Conduct');
     });
   });
+
+  it('should redirect to about us', () => {
+    cy.get('#loggedoutmenu.menu.logged-out').invoke('show');
+    const elem = cy.get('[data-cy-href=aboutus]');
+    cy.wait(2000)
+    elem.contains('All about dev.to').invoke('show');
+    return elem.invoke('attr', 'href').then(href => {
+      cy.visit(`${baseURL}${href}`);
+      cy.wait(2000);
+      cy.eyesCheckWindow('All about dev.to');
+    });
+  });
+
 });
