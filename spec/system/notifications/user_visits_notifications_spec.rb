d6b8f32b15e4649f8de4440e04091a1fdd3aa2d8
Kasper Meyer <hi@kaspermeyer.com>
2019-08-02 17:35:35 +0200

    Improve readability of notification system specs

--- spec/system/notifications/user_visits_notifications_spec.rb
+++ spec/system/notifications/user_visits_notifications_spec.rb
@@ -1,42 +1,37 @@
 require "rails_helper"
 
-RSpec.describe "Visiting notifications", type: :system do
-  let_it_be(:user) { create(:user) }
-  let_it_be(:other_user) { create(:user) }
+RSpec.describe "User visits notifications", type: :system do
+  let(:reader) { create(:user) }
+  let(:author) { create(:user) }
+  let(:article) { create(:article, :with_notification_subscription, user: author) }
+  let(:comment) { create(:comment, commentable: article, user: reader) }
 
   before do
-    user.follow(other_user)
-    sign_in user
-    Timecop.travel(10.minutes.ago)
+    reader.follow(author)
+    article.update_column(:published_at, 10.minutes.ago)
+    comment.update_column(:created_at, 10.minutes.ago)
   end
 
-  after do
-    Timecop.return
-  end
-
-  context "when visiting post notifications" do
-    let(:article) { create(:article, :with_notification_subscription, user: other_user) }
-
+  context "when viewing post notifications" do
     before do
       Notification.send_to_followers_without_delay(article, "Published")
+      sign_in reader
       visit "/notifications/posts"
     end
 
-    it "shows the timestamp in a human readable format", js: true do
+    it "shows the timestamp in a human-readable format", js: true do
       expect(page).to have_selector(".time-ago-indicator", text: "(10 mins ago)")
     end
   end
 
-  context "when visiting comment notifications" do
-    let(:article) { create(:article, :with_notification_subscription, user: user) }
-    let(:comment) { create(:comment, commentable: article, user: other_user) }
-
+  context "when viewing comment notifications" do
     before do
       Notification.send_new_comment_notifications_without_delay(comment)
+      sign_in author
       visit "/notifications/comments"
     end
 
-    it "shows the timestamp in a human readable format", js: true do
+    it "shows the timestamp in a human-readable format", js: true do
       expect(page).to have_selector(".time-ago-indicator", text: "(10 mins ago)")
     end
   end
