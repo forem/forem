9faa3645db1d79041eeff9b3e55d5c89c79a9447
Mario See <mariocsee@users.noreply.github.com>
2019-08-06 13:35:49 -0400

    displayed_classified_listing should be published

--- app/controllers/classified_listings_controller.rb
+++ app/controllers/classified_listings_controller.rb
@@ -6,14 +6,16 @@ class ClassifiedListingsController < ApplicationController
   before_action :authenticate_user!, only: %i[edit update new dashboard]
 
   def index
-    @displayed_classified_listing = ClassifiedListing.find_by!(slug: params[:slug]) if params[:slug]
+    published_listings = ClassifiedListing.where(published: true)
+    display_listing = published_listings.find_by(slug: params[:slug]) if params[:slug]
+    @displayed_classified_listing = display_listing unless display_listing.nil?
 
     if params[:view] == "moderate"
       return redirect_to "/internal/listings/#{@displayed_classified_listing.id}/edit"
     end
 
     @classified_listings = if params[:category].blank?
-                             ClassifiedListing.where(published: true).
+                             published_listings.
                                order("bumped_at DESC").
                                includes(:user, :organization, :taggings).
                                limit(12)
