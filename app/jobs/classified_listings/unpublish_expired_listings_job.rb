c21b23bc7bbc89f36d968069ac014f79e6ee60f9
Mario See <mariocsee@users.noreply.github.com>
2019-08-20 12:18:20 -0400

    add job to class name

--- app/jobs/classified_listings/unpublish_expired_listings_job.rb
+++ app/jobs/classified_listings/unpublish_expired_listings_job.rb
@@ -1,9 +1,9 @@
 module ClassifiedListings
-  class UnpublishExpiredListings < ApplicationJob
+  class UnpublishExpiredListingsJob < ApplicationJob
     queue_as :classified_listings_unpublish_expired
 
     def perform
-      expired_listings = ClassifiedListing.where(published: true).where(expire_on: Time.zone.today)
+      expired_listings = ClassifiedListing.where(published: true).where(expire_on: Date.today)
       return if expired_listings.empty?
 
       expired_listings.each do |listing|
