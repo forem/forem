0147d8bd26f83442511b3e0d376b291cbd0b6dd5
Nick Taylor <nick@iamdeveloper.com>
2019-08-21 10:44:23 -0400

    Fixed some lint issues (#3782)

--- app/assets/javascripts/utilities/showModal.js
+++ app/assets/javascripts/utilities/showModal.js
@@ -1,13 +1,8 @@
-function showModal(context) {
-  document.getElementById('global-signup-modal').style.display = 'block';
-  document.getElementById('global-signup-modal').classList.add('showing');
-  document.getElementsByTagName('body')[0].classList.add('modal-open');
-  initSignupModal();
-}
+'use strict';
 
 function initSignupModal() {
   if (document.getElementById('global-signup-modal')) {
-    document.getElementById('global-signup-modal-bg').onclick = function() {
+    document.getElementById('global-signup-modal-bg').onclick = () => {
       document.getElementById('global-signup-modal').style.display = 'none';
       document
         .getElementById('global-signup-modal')
@@ -15,4 +10,11 @@ function initSignupModal() {
       document.getElementsByTagName('body')[0].classList.remove('modal-open');
     };
   }
+}
+
+function showModal(context) {
+  document.getElementById('global-signup-modal').style.display = 'block';
+  document.getElementById('global-signup-modal').classList.add('showing');
+  document.getElementsByTagName('body')[0].classList.add('modal-open');
+  initSignupModal();
 }
