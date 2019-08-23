4e89d1f6561c9e02285f47d7b4a7dd573e1674c8
Ben Halpern <bendhalpern@gmail.com>
2019-08-23 12:48:29 -0400

    Add onboarding variants and update variant display ratio (#3812)

--- app/controllers/internal/growth_controller.rb
+++ app/controllers/internal/growth_controller.rb
@@ -2,6 +2,6 @@ class Internal::GrowthController < Internal::ApplicationController
   layout "internal"
 
   def index
-    @variants = %w[0 1 2 3 4 5 6]
+    @variants = %w[0 1 2 3 4 5 6 7 8]
   end
 end
