5754b4a169b712d0fb7509eb9ca12358266c239b
Andy Zhao <andyzhao.zhao@gmail.com>
2019-08-22 22:04:45 -0400

    Add comment markdown to warned_user_ping (#3750)
    * Add comment markdown to warned_user_ping
    
    * Add missing argument

--- app/observers/application_observer.rb
+++ app/observers/application_observer.rb
@@ -2,9 +2,18 @@ class ApplicationObserver < ActiveRecord::Observer
   def warned_user_ping(activity)
     return unless activity.user.warned == true
 
-    SlackBotPingJob.perform_later message: "Activity: https://dev.to#{activity.path}\nManage @#{activity.user.username}: https://dev.to/internal/users/#{activity.user.id}",
+    SlackBotPingJob.perform_later message: message(activity),
                                   channel: "warned-user-comments",
                                   username: "sloan_watch_bot",
                                   icon_emoji: ":sloan:"
+  end
+
+  def message(activity)
+    <<~HEREDOC
+      Activity: https://dev.to#{activity.path}
+      #{'Comment text: ' + activity.body_markdown.truncate(300) if activity.class.name == 'Comment'}
+      ---
+      Manage commenter - @#{activity.user.username}: https://dev.to/internal/users/#{activity.user.id},
+    HEREDOC
   end
 end
