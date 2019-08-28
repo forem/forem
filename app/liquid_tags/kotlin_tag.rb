7e86bb05abe1dddb4e20f560f9a4b9dbd328632d
Zhao-Andy <andyzhao.zhao@gmail.com>
2019-08-28 12:25:09 -0400

    Small adjustments and lint

--- app/liquid_tags/kotlin_tag.rb
+++ app/liquid_tags/kotlin_tag.rb
@@ -23,26 +23,25 @@ class KotlinTag < LiquidTagBase
     end
     hostname_ok = url.hostname == "pl.kotl.in"
     short = url.path.delete("/")
-    raise_error unless hostname_ok && is_valid_param(short)
-    self.parse_params(url, short)
+    raise_error unless hostname_ok && valid_param?(short)
+    parse_params(url, short)
   end
 
   def self.parse_params(url, short)
-    query = url.query.nil? ?  [] : URI.decode_www_form(url.query)
-    result = {:short => short}
-    [:from, :to, :theme, :readOnly].each { |param|
+    query = url.query.nil? ? [] : URI.decode_www_form(url.query)
+    result = { short: short }
+    %i[from to theme readOnly].each do |param|
       value = query.assoc(param.id2name)&.last
-      result[param] = is_valid_param(value) ? value : ""
-    }
+      result[param] = valid_param?(value) ? value : ""
+    end
     result
   end
 
-  def self.is_valid_param(value)
-    return false if value.nil?
-    value.match(/^[a-zA-Z0-9]+$/) != nil
+  def self.valid_param?(value)
+    !value&.match(/^[a-zA-Z0-9]+$/)&.nil?
   end
 
-  def self.raise_error
+  def raise_error
     raise StandardError, "Invalid Kotlin Playground URL"
   end
 end
--- spec/liquid_tags/kotlin_tag_spec.rb
+++ spec/liquid_tags/kotlin_tag_spec.rb
@@ -9,12 +9,12 @@ RSpec.describe KotlinTag, type: :liquid_template do
     end
 
     it "accepts only Kotlin Playground links" do
-      badurl = 'https://example.com'
+      badurl = "https://example.com"
       expect do
         generate_new_liquid(badurl)
       end.to raise_error(StandardError)
 
-      badurl = 'not even an URL'
+      badurl = "not even an URL"
       expect do
         generate_new_liquid(badurl)
       end.to raise_error(StandardError)
@@ -25,10 +25,10 @@ RSpec.describe KotlinTag, type: :liquid_template do
     end
 
     it "parses URL correctly" do
-      check("https://pl.kotl.in/owreUFFUG", {:from =>"", :readOnly =>"", :short =>"owreUFFUG", :theme =>"", :to => ""})
-      check("https://pl.kotl.in/owreUFFUG?theme=dracula&from=3&to=6&readOnly=true",  {:from =>"3", :readOnly =>"true", :short =>"owreUFFUG", :theme =>"dracula", :to =>"6"})
-      check("https://pl.kotl.in/owreUFFUG?theme=dracula&readOnly=true", {:from =>"", :readOnly =>"true", :short =>"owreUFFUG", :theme =>"dracula", :to =>""})
-      check("https://pl.kotl.in/owreUFFUG?from=3&to=6", {:from =>"3", :readOnly =>"", :short =>"owreUFFUG", :theme =>"", :to =>"6"})
+      check("https://pl.kotl.in/owreUFFUG", from: nil, readOnly: nil, short: "owreUFFUG", theme: nil, to: nil)
+      check("https://pl.kotl.in/owreUFFUG?theme=dracula&from=3&to=6&readOnly=true", from: "3", readOnly: "true", short: "owreUFFUG", theme: "dracula", to: "6")
+      check("https://pl.kotl.in/owreUFFUG?theme=dracula&readOnly=true", from: nil, readOnly: "true", short: "owreUFFUG", theme: "dracula", to: nil)
+      check("https://pl.kotl.in/owreUFFUG?from=3&to=6", from: "3", readOnly: nil, short: "owreUFFUG", theme: nil, to: "6")
     end
 
     it "renders correctly a Kotlin Playground link" do
