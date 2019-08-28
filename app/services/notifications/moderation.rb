208bbb37575f4076615a570ebcbf0db39d77e7a6
Ben Halpern <bendhalpern@gmail.com>
2019-08-27 21:54:09 -0400

    Improve mod roundrobin notification and internal tooling (#3855)
    * Improve mod roundrobin notification and internal tooling
    
    * Fix tests

--- app/services/notifications/moderation.rb
+++ app/services/notifications/moderation.rb
@@ -1,10 +1,10 @@
 module Notifications
   module Moderation
-    MODERATORS_AVAILABILITY_DELAY = 28.hours
+    MODERATORS_AVAILABILITY_DELAY = 22.hours
     SUPPORTED = [Comment].freeze
 
     def self.available_moderators
-      User.with_role(:trusted).where("last_moderation_notification < ?", MODERATORS_AVAILABILITY_DELAY.ago)
+      User.with_role(:trusted).where("last_moderation_notification < ?", MODERATORS_AVAILABILITY_DELAY.ago).where(mod_roundrobin_notifications: true)
     end
   end
 end
