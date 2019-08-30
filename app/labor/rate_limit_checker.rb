6c14ee2b5f61c409ce50a5b786f12850b4deeb45
Mac Siri <krairit.siri@gmail.com>
2019-08-30 09:47:18 -0400

    Change situation to action

--- app/labor/rate_limit_checker.rb
+++ app/labor/rate_limit_checker.rb
@@ -1,5 +1,5 @@
 class RateLimitChecker
-  attr_reader :user, :situation
+  attr_reader :user, :action
 
   def initialize(user = nil)
     @user = user
@@ -7,8 +7,8 @@ class RateLimitChecker
 
   class UploadRateLimitReached < StandardError; end
 
-  def limit_by_situation(situation)
-    result = case situation
+  def limit_by_action(action)
+    result = case action
              when "comment_creation"
                user.comments.where("created_at > ?", 30.seconds.ago).size > 9
              when "published_article_creation"
@@ -19,7 +19,7 @@ class RateLimitChecker
                false
              end
     if result
-      @situation = situation
+      @action = action
       ping_admins
     end
     result
@@ -32,16 +32,16 @@ class RateLimitChecker
   end
 
   def limit_by_email_recipient_address(address)
-    # This is related to the recipient, not the "user" initiator, like in situation.
+    # This is related to the recipient, not the "user" initiator, like in action.
     EmailMessage.where(to: address).
       where("sent_at > ?", 2.minutes.ago).size > 5
   end
 
   def ping_admins
-    RateLimitCheckerJob.perform_later(user.id, situation)
+    RateLimitCheckerJob.perform_later(user.id, action)
   end
 
   def ping_admins_without_delay
-    RateLimitCheckerJob.perform_now(user.id, situation)
+    RateLimitCheckerJob.perform_now(user.id, action)
   end
 end
--- spec/labor/rate_limit_checker_spec.rb
+++ spec/labor/rate_limit_checker_spec.rb
@@ -4,28 +4,28 @@ RSpec.describe RateLimitChecker do
   let(:user) { create(:user) }
   let(:article) { create(:article, user_id: user.id) }
 
-  it ".limit_by_situation returns false for invalid case" do
-    expect(described_class.new(user).limit_by_situation("random-nothing")).to eq(false)
+  it ".limit_by_action returns false for invalid case" do
+    expect(described_class.new(user).limit_by_action("random-nothing")).to eq(false)
   end
 
-  it ".limit_by_situation returns true if too many comments at once" do
+  it ".limit_by_action returns true if too many comments at once" do
     create_list(:comment, 10, user_id: user.id, commentable_id: article.id)
-    expect(described_class.new(user).limit_by_situation("comment_creation")).to eq(true)
+    expect(described_class.new(user).limit_by_action("comment_creation")).to eq(true)
   end
 
-  it ".limit_by_situation returns false if allowed comment" do
+  it ".limit_by_action returns false if allowed comment" do
     create_list(:comment, 2, user_id: user.id, commentable_id: article.id)
-    expect(described_class.new(user).limit_by_situation("comment_creation")).to eq(false)
+    expect(described_class.new(user).limit_by_action("comment_creation")).to eq(false)
   end
 
-  it ". limit_by_situation returns true if too many published articles at once" do
+  it ". limit_by_action returns true if too many published articles at once" do
     create_list(:article, 10, user_id: user.id, published: true)
-    expect(described_class.new(user).limit_by_situation("published_article_creation")).to eq(true)
+    expect(described_class.new(user).limit_by_action("published_article_creation")).to eq(true)
   end
 
-  it ".limit_by_situation returns false if published articles comment" do
+  it ".limit_by_action returns false if published articles comment" do
     create_list(:article, 2, user_id: user.id, published: true)
-    expect(described_class.new(user).limit_by_situation("published_article_creation")).to eq(false)
+    expect(described_class.new(user).limit_by_action("published_article_creation")).to eq(false)
   end
 
   it ".limit_by_email_recipient_address returns true if too many published articles at once" do
