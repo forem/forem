b2e05e0ef239e5534b573535accb3b17206da9af
Zhao-Andy <andyzhao.zhao@gmail.com>
2019-08-30 17:01:52 -0400

    WIP test custom deploy script

---
+++ docs/deploy-script.js
@@ -0,0 +1,29 @@
+import { exec } from 'child_process';
+const site = process.env.URL;
+console.log(site)
+const subdomain = site.split('/')[site.split('/').length - 1];
+
+let buildCommand;
+switch (subdomain) {
+  case 'docs.dev.to':
+    buildCommand = 'npm install -g gitdocs@latest && gitdocs build';
+    break;
+  case 'storybook.dev.to':
+    buildCommand = 'npm run build-storybook';
+    break;
+  default:
+    throw `Domain ${domain} is invalid`;
+}
+
+async function execute(command) {
+  return await exec(command, function(error, stdout, stderr) {
+    if (error) {
+      throw error;
+    }
+    console.log(`site: ${site}`);
+    console.log(`domain: ${domain}`);
+    console.log(stdout);
+  });
+}
+
+execute(buildCommand);
