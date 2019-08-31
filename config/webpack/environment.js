1dda67085e7691f9436769d66357e2fc2590b03a
Nick Taylor <nick@iamdeveloper.com>
2019-08-30 23:51:14 -0400

    webpacker/webpack upgrade.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -8,27 +8,7 @@
 
 // module.exports = environment;
 
+const { environment } = require('@rails/webpacker');
+const webpack = require('webpack');
 
-const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
-
-environment.plugins.append(
-  'CommonsChunkVendor',
-  new webpack.optimize.CommonsChunkPlugin({
-    name: 'vendor',
-    minChunks: (module) => {
-      // this assumes your vendor imports exist in the node_modules directory
-      return module.context && module.context.indexOf('node_modules') !== -1
-    }
-  })
-)
-
-environment.plugins.append(
-  'CommonsChunkManifest',
-  new webpack.optimize.CommonsChunkPlugin({
-    name: 'manifest',
-    minChunks: Infinity
-  })
-)
-
-module.exports = environment
+module.exports = environment;
