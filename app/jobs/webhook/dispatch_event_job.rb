79930ace8d45b1db89fcbb6ce437d0b9f388f781
lightalloy <lightallloy@gmail.com>
2019-09-02 15:41:09 +0300

    Use Addressable::URI to parse endpoint url

--- app/jobs/webhook/dispatch_event_job.rb
+++ app/jobs/webhook/dispatch_event_job.rb
@@ -3,7 +3,7 @@ module Webhook
     queue_as :dispatch_webhook_events
 
     def perform(endpoint_url:, payload:, client: HTTParty)
-      uri = URI.parse(endpoint_url)
+      uri = Addressable::URI.parse(endpoint_url)
       client.post(uri, headers: { "Content-Type" => "application/json" }, body: payload)
     end
   end
