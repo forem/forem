c9a5bb8239e3e4125b2be410f10dd73ed68c96cd
lightalloy <lightallloy@gmail.com>
2019-09-03 15:06:55 +0300

    Add fields for ArticleSerializer

--- app/serializers/webhook/article_serializer.rb
+++ app/serializers/webhook/article_serializer.rb
@@ -2,6 +2,10 @@ module Webhook
   class ArticleSerializer
     include FastJsonapi::ObjectSerializer
     set_type :article
-    attributes :title, :body_markdown, :processed_html, :published, :cached_tag_list
+    belongs_to :user
+    attributes :archived, :automatically_renew, :body_html, :body_markdown, :cached_tag_list,
+               :cached_user_name, :canonical_url, :created_at, :description, :edited_at,
+               :language, :main_image, :processed_html, :published, :published_at, :reading_time,
+               :removed_for_abuse, :score, :slug, :title, :updated_at
   end
 end
