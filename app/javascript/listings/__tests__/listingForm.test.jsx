190ab675e0b87084f106cc4b7341c6dcf582f139
Mario See <mariocsee@users.noreply.github.com>
2019-09-05 13:50:21 -0400

    add html doc setup

--- app/javascript/listings/__tests__/listingForm.test.jsx
+++ app/javascript/listings/__tests__/listingForm.test.jsx
@@ -3,6 +3,39 @@ import { deep, shallow } from 'preact-render-spy';
 import { JSDOM } from 'jsdom';
 import ListingForm from '../listingForm';
 
+const doc = new JSDOM('<!doctype html><html><body></body></html>');
+global.document = doc;
+global.document.body = ` 
+<body data-user-status="logged-in" 
+  data-pusher-key="5bcdbcc8dd2763f1d538" 
+  class="default default-article-body pro-status-false" 
+  data-user=${JSON.stringify(
+    {'id':11,
+    'name':'Mario See',
+    'username':'mariocsee',
+    'profile_image_90':'/uploads/user/profile_image/11/08f6aa82-3fc4-44dd-acc4-317ca694f48a.jpeg',
+    'followed_tag_names':[],
+    'followed_tags':'[]',
+    'followed_user_ids':[9,5,8,2,4,6,7,1,3,10],
+    'followed_organization_ids':[],
+    'followed_podcast_ids':[],
+    'reading_list_ids':[],
+    'saw_onboarding':true,
+    'checked_code_of_conduct':true,
+    'checked_terms_and_conditions':true,
+    'number_of_comments':0,
+    'display_sponsors':true,
+    'trusted':false,
+    'experience_level':null,
+    'preferred_languages_array':['en'],
+    'config_body_class':'default default-article-body pro-status-false',
+    'onboarding_variant_version':'6',
+    'pro':false
+  })}
+  data-loaded="true">
+</body>`;
+global.window = doc.defaultView;
+
 const organizations = '{}';
 const listing = '{}';
 const categoriesForSelect = '[]';
