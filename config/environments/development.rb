73d5ab6043d71e87b90db8c13a97f9fa7ff8443b
Zhao-Andy <andyzhao.zhao@gmail.com>
2019-09-10 19:12:27 -0400

    Use Redis as session storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,6 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-  config.redis_host = "localhost"
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
