27b4039bea13d0995eb9ba21cb2d50f2dde8f834
Zhao-Andy <andyzhao.zhao@gmail.com>
2019-09-11 08:28:40 -0400

    Use ApplicationConfig instead

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :redis_store,
-  key: ENV["SESSION_KEY"],
-  servers: ENV["REDIS_URL"],
-  expires_in: ENV["SESSION_EXPIRY_SECONDS"].to_i
+  key: ApplicationConfig["SESSION_KEY"],
+  servers: ApplicationConfig["REDIS_URL"],
+  expires_in: ApplicationConfig["SESSION_EXPIRY_SECONDS"].to_i
