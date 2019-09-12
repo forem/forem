5e181efba69e29458af9bc1792088a679c7c594f
Mac Siri <krairit.siri@gmail.com>
2019-09-12 19:51:56 -0400

    Update html_variant_policy_spec

--- spec/policies/html_variant_policy_spec.rb
+++ spec/policies/html_variant_policy_spec.rb
@@ -1,18 +1,20 @@
 require "rails_helper"
 
-RSpec.describe HtmlVariantPolicy do
+RSpec.describe HtmlVariantPolicy, type: :policy do
   subject { described_class.new(user, html_variant) }
 
   context "when user is not an admin" do
-    let(:user) { build(:user) }
-    let(:html_variant) { build(:html_variant) }
+    let(:user)         { build_stubbed(:user) }
+    let(:html_variant) { build_stubbed(:html_variant) }
 
     it { is_expected.to forbid_actions(%i[index show edit update create]) }
   end
 
   context "when user is an admin" do
-    let(:user) { build(:user, :super_admin) }
-    let(:html_variant) { build(:html_variant) }
+    let(:user)         { build_stubbed(:user) }
+    let(:html_variant) { build_stubbed(:html_variant) }
+
+    before { allow(user).to receive(:has_role?).with(:super_admin).and_return(true) }
 
     it { is_expected.to permit_actions(%i[index show edit update create]) }
   end
