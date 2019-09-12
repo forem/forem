e150440254bb6b944aef8400e51f1ce4744afb2c
Mac Siri <krairit.siri@gmail.com>
2019-09-12 19:15:16 -0400

    Update block_policy_spec

--- spec/policies/block_policy_spec.rb
+++ spec/policies/block_policy_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
-RSpec.describe BlockPolicy do
+RSpec.describe BlockPolicy, type: :policy do
   subject { described_class.new(user, block) }
 
-  let(:block) { build(:block) }
+  let(:block) { build_stubbed(:block) }
 
   let(:valid_attributes) do
     %i[input_html input_css input_javascript featured index_position publish_now]
@@ -16,13 +16,15 @@ RSpec.describe BlockPolicy do
   end
 
   context "when signed in as a regular user" do
-    let(:user) { build(:user) }
+    let(:user) { build_stubbed(:user) }
 
     it { is_expected.to forbid_actions(%i[index show new edit create update destroy]) }
   end
 
   context "when user is signed in as a super admin" do
-    let(:user) { build(:user, :super_admin) }
+    let(:user) { build_stubbed(:user) }
+
+    before { allow(user).to receive(:has_role?).with(:super_admin).and_return(true) }
 
     it { is_expected.to permit_actions(%i[index show new edit create update destroy]) }
     it { is_expected.to permit_mass_assignment_of(valid_attributes) }
