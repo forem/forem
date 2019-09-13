12b370fccf894a5ce3999988b7c053c79078eefc
Mac Siri <krairit.siri@gmail.com>
2019-09-13 10:23:58 -0400

    Update stripe_subscription_policy_spec

--- spec/policies/stripe_subscription_policy_spec.rb
+++ spec/policies/stripe_subscription_policy_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe StripeSubscriptionPolicy do
+RSpec.describe StripeSubscriptionPolicy, type: :policy do
   subject { described_class.new(user, :stripe_subscription) }
 
   context "when user is not signed in" do
@@ -10,12 +10,14 @@ RSpec.describe StripeSubscriptionPolicy do
   end
 
   context "when user is signed in" do
-    let(:user) { build(:user) }
+    let(:user) { build_stubbed(:user) }
 
     it { is_expected.to permit_actions(%i[create update destroy]) }
 
     context "when user is banned" do
-      let(:user) { build(:user, :banned) }
+      let(:user) { build_stubbed(:user) }
+
+      before { allow(user).to receive(:has_role?).with(:banned).and_return(true) }
 
       it { is_expected.to forbid_actions(%i[create update]) }
     end
