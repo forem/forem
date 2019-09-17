2f422e77f170e7c99a632bd303eb0eadc892dad5
lightalloy <lightallloy@gmail.com>
2019-09-17 11:16:03 +0300

    Skip sending events when a draft is destroyed

--- spec/services/articles/destroyer_spec.rb
+++ spec/services/articles/destroyer_spec.rb
@@ -2,6 +2,11 @@ require "rails_helper"
 
 RSpec.describe Articles::Destroyer do
   let(:article) { create(:article) }
+  let(:event_dispatcher) { double }
+
+  before do
+    allow(event_dispatcher).to receive(:call)
+  end
 
   it "destroys an article" do
     described_class.call(article)
@@ -15,9 +20,13 @@ RSpec.describe Articles::Destroyer do
   end
 
   it "calls events dispatcher" do
-    event_dispatcher = double
-    allow(event_dispatcher).to receive(:call)
     described_class.call(article, event_dispatcher)
     expect(event_dispatcher).to have_received(:call).with("article_destroyed", article)
+  end
+
+  it "doesn't call a dispatched when a draft is destroyed" do
+    draft = create(:article, published: false)
+    described_class.call(draft, event_dispatcher)
+    expect(event_dispatcher).not_to have_received(:call)
   end
 end
