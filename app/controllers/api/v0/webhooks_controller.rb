0a16bf934a0304057e46336007664746009f15e0
Anna Buianova <lightallloy@gmail.com>
2019-09-18 23:25:40 +0300

    Added association between webhook_endpoints and oauth apps (#4060)
    * Added association between webhook_endpoints and oauth apps
    
    * Limit webhooks access by oauth_app_id when authorized by doorkeeper
    
    * Use rails route helpers in webhooks api test

--- app/controllers/api/v0/webhooks_controller.rb
+++ app/controllers/api/v0/webhooks_controller.rb
@@ -7,25 +7,35 @@ module Api
       skip_before_action :verify_authenticity_token, only: %w[create destroy]
 
       def index
-        @webhooks = @user.webhook_endpoints.order(:id)
+        @webhooks = webhooks_scope.order(:id)
       end
 
       def create
-        @webhook = @user.webhook_endpoints.create!(webhook_params)
+        @webhook = @user.webhook_endpoints.new(webhook_params)
+        @webhook.oauth_application_id = doorkeeper_token.application_id if doorkeeper_token
+        @webhook.save!
         render "show", status: :created
       end
 
       def show
-        @webhook = @user.webhook_endpoints.find(params[:id])
+        @webhook = webhooks_scope.find(params[:id])
       end
 
       def destroy
-        webhook = @user.webhook_endpoints.find(params[:id])
+        webhook = webhooks_scope.find(params[:id])
         webhook.destroy!
         head :no_content
       end
 
       private
+
+      def webhooks_scope
+        if doorkeeper_token
+          @user.webhook_endpoints.for_app(doorkeeper_token.application_id)
+        else
+          @user.webhook_endpoints
+        end
+      end
 
       def webhook_params
         params.require(:webhook_endpoint).permit(:target_url, :source, events: [])
