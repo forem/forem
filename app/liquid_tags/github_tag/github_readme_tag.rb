f7df039225f082c68c0709e05854f863623b924c
John Curcio <john@curcio.dev>
2019-09-18 21:19:55 -0300

    Improve semantics of invalid option error

--- app/liquid_tags/github_tag/github_readme_tag.rb
+++ app/liquid_tags/github_tag/github_readme_tag.rb
@@ -47,7 +47,7 @@ class GithubTag
       _, *options = opts.split(" ")
 
       validated_options = options.map { |option| valid_option(option) }.reject(&:nil?)
-      raise StandardError, "Invalid Options" unless options.empty? || !validated_options.empty?
+      raise StandardError, "Invalid Options" unless options.empty? || validated_options.any?
 
       options
     end
