c080490eb9e3e042cfeb87dbd02885220f770846
Mac Siri <krairit.siri@gmail.com>
2019-09-19 15:25:36 -0400

    Add back validation and add test

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,6 +4,10 @@ class Role < ApplicationRecord
   belongs_to :resource,
              polymorphic: true, optional: true
 
+  validates :resource_type,
+            inclusion: { in: Rolify.resource_types },
+            allow_nil: true
+
   validates :name,
             inclusion: {
               in: %w[
