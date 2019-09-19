c9d5982cccba9fe08bc9130656cb755f2a5ab7e7
rhymes <rhymesete@gmail.com>
2019-09-19 09:30:12 +0200

    Add tests for ApplicationHelper.beautified_url

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -7,4 +7,22 @@ RSpec.describe ApplicationHelper, type: :helper do
       expect(helper.community_qualified_name).to eq(expected_name)
     end
   end
+
+  describe "#beautified_url" do
+    it "strips the protocol" do
+      expect(helper.beautified_url("https://github.com")).to eq("github.com")
+    end
+
+    it "strips params" do
+      expect(helper.beautified_url("https://github.com?a=3")).to eq("github.com")
+    end
+
+    it "strips the last forward slash" do
+      expect(helper.beautified_url("https://github.com/")).to eq("github.com")
+    end
+
+    it "does not strip the path" do
+      expect(helper.beautified_url("https://github.com/rails")).to eq("github.com/rails")
+    end
+  end
 end
