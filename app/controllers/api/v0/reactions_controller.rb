c5cb61f932ccf7482594016aac9c65f4d6eeae8a
John Curcio <john@curcio.dev>
2019-09-20 11:19:07 -0300

    Move Reaction.create delay calls to ActiveJob (#3136) (#4022)

--- app/controllers/api/v0/reactions_controller.rb
+++ app/controllers/api/v0/reactions_controller.rb
@@ -24,7 +24,7 @@ module Api
         verify_authenticity_token
         reactable_ids = JSON.parse(params[:articles]).map { |article| article["id"] }
         reactable_ids.each do |article_id|
-          Reaction.delay.create(
+          Reactions::CreateJob.perform_later(
             user_id: current_user.id,
             reactable_id: article_id,
             reactable_type: "Article",
