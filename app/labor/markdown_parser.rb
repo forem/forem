3e23b3d568c5d62a258f00cfd1f172895231d5cb
Mario See <mariocsee@gmail.com>
2019-09-25 12:43:29 -0400

    Limit listing's permitted markdown tags (#3882)

--- app/labor/markdown_parser.rb
+++ app/labor/markdown_parser.rb
@@ -79,7 +79,7 @@ class MarkdownParser
 
     renderer = Redcarpet::Render::HTMLRouge.new(hard_wrap: true, filter_html: false)
     markdown = Redcarpet::Markdown.new(renderer, REDCARPET_CONFIG)
-    allowed_tags = %w[strong abbr aside em p h1 h2 h3 h4 h5 h6 i u b code pre
+    allowed_tags = %w[strong abbr aside em p h4 h5 h6 i u b code pre
                       br ul ol li small sup sub a span hr blockquote kbd]
     allowed_attributes = %w[href strong em ref rel src title alt class]
     ActionController::Base.helpers.sanitize markdown.render(@content).html_safe,
