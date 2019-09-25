3b0f6f283403ceb8a3bfc1bfcab606f93da93b55
Andy Zhao <andyzhao.zhao@gmail.com>
2019-09-25 15:31:56 -0400

    Refactor remove_all and remove_each notifications and fix orphaned notifications (#3570)

--- app/services/articles/destroyer.rb
+++ app/services/articles/destroyer.rb
@@ -4,8 +4,8 @@ module Articles
 
     def call(article, event_dispatcher = Webhook::DispatchEvent)
       article.destroy!
-      Notification.remove_all_without_delay(notifiable_id: article.id, notifiable_type: "Article", action: "Published")
-      Notification.remove_all(notifiable_id: article.id, notifiable_type: "Article", action: "Reaction")
+      Notification.remove_all_without_delay(notifiable_ids: article.id, notifiable_type: "Article")
+      Notification.remove_all(notifiable_ids: article.comments.pluck(:id), notifiable_type: "Comment") if article.comments.exists?
       event_dispatcher.call("article_destroyed", article) if article.published?
     end
   end
