b645617a6447d1b7f82eabfa6d48d6bf934bd8fb
Arun Kumar Mohan <arunmohandm@gmail.com>
2019-10-01 18:53:01 -0500

    Delete messages without instantiating ActiveRecord objects

--- app/models/chat_channel.rb
+++ app/models/chat_channel.rb
@@ -44,7 +44,7 @@ class ChatChannel < ApplicationRecord
   end
 
   def clear_channel
-    messages.find_each(&:destroy!)
+    messages.delete_all
     Pusher.trigger(pusher_channels, "channel-cleared", { chat_channel_id: id }.to_json)
     true
   rescue Pusher::Error => e
