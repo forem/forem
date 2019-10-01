fc6f2e0920923f66c51b4e73dcf551e353ece7dd
lightalloy <lightallloy@gmail.com>
2019-10-01 15:39:09 +0300

    Limit access to the API actions when authorized by doorkeeper token

--- spec/requests/api/v0/articles_spec.rb
+++ spec/requests/api/v0/articles_spec.rb
@@ -231,20 +231,20 @@ RSpec.describe "Api::V0::Articles", type: :request do
         post api_articles_path, headers: { "api-key" => api_secret.secret, "content-type" => "application/json" }
         expect(response).to have_http_status(:unauthorized)
       end
+
+      it "fails when oauth's access_token" do
+        access_token = create(:doorkeeper_access_token, resource_owner_id: user.id)
+        headers = { "authorization" => "Bearer #{access_token.token}", "content-type" => "application/json" }
+
+        post api_articles_path, params: { article: { title: Faker::Book.title } }.to_json, headers: headers
+        expect(response).to have_http_status(:unauthorized)
+      end
     end
 
     describe "when authorized" do
       def post_article(**params)
         headers = { "api-key" => api_secret.secret, "content-type" => "application/json" }
         post api_articles_path, params: { article: params }.to_json, headers: headers
-      end
-
-      it "supports oauth's access_token" do
-        access_token = create(:doorkeeper_access_token, resource_owner_id: user.id)
-        headers = { "authorization" => "Bearer #{access_token.token}", "content-type" => "application/json" }
-
-        post api_articles_path, params: { article: { title: Faker::Book.title } }.to_json, headers: headers
-        expect(response).to have_http_status(:created)
       end
 
       it "fails if no params are given" do
@@ -481,6 +481,17 @@ RSpec.describe "Api::V0::Articles", type: :request do
         put path, headers: { "api-key" => api_secret.secret, "content-type" => "application/json" }
         expect(response).to have_http_status(:unauthorized)
       end
+
+      it "fails with oauth's access_token" do
+        access_token = create(:doorkeeper_access_token, resource_owner_id: user.id)
+        headers = { "authorization" => "Bearer #{access_token.token}", "content-type" => "application/json" }
+
+        title = Faker::Book.title
+        body_markdown = "foobar"
+        params = { title: title, body_markdown: body_markdown }
+        put path, params: { article: params }.to_json, headers: headers
+        expect(response).to have_http_status(:unauthorized)
+      end
     end
 
     describe "when authorized" do
@@ -489,19 +500,6 @@ RSpec.describe "Api::V0::Articles", type: :request do
       def put_article(**params)
         headers = { "api-key" => api_secret.secret, "content-type" => "application/json" }
         put path, params: { article: params }.to_json, headers: headers
-      end
-
-      it "supports oauth's access_token" do
-        access_token = create(:doorkeeper_access_token, resource_owner_id: user.id)
-        headers = { "authorization" => "Bearer #{access_token.token}", "content-type" => "application/json" }
-
-        title = Faker::Book.title
-        body_markdown = "foobar"
-        params = { title: title, body_markdown: body_markdown }
-        put path, params: { article: params }.to_json, headers: headers
-        expect(response).to have_http_status(:ok)
-        expect(article.reload.title).to eq(title)
-        expect(article.body_markdown).to eq(body_markdown)
       end
 
       it "returns not found if the article does not belong to the user" do
