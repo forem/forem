b172afaa9e67e7c06bf53279ab381c35b3f1bbcd
Zachary Dixon <rissk13@gmail.com>
2019-10-01 21:38:12 -0500

    Fixed some eslint errors

--- app/assets/javascripts/initializers/initializeArticleReactions.js
+++ app/assets/javascripts/initializers/initializeArticleReactions.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Set reaction count to correct number
 function setReactionCount(reactionName, newCount) {
   var reactionClassList = document.getElementById(
@@ -36,62 +38,32 @@ function hasUserReacted(reactionName) {
 function getNumReactions(reactionName) {
   var num = document.getElementById('reaction-number-' + reactionName)
     .textContent;
-  if (num == '') {
+  if (num === '') {
     return 0;
   }
-  return parseInt(num);
+  return parseInt(num, 10);
 }
 
-function initializeArticleReactions() {
-  setCollectionFunctionality();
-  setTimeout(function() {
-    if (document.getElementById('article-body')) {
-      var articleId = document.getElementById('article-body').dataset.articleId;
-      if (document.getElementById('article-reaction-actions')) {
-        var ajaxReq;
-        var thisButt = this;
-        if (window.XMLHttpRequest) {
-          ajaxReq = new XMLHttpRequest();
-        } else {
-          ajaxReq = new ActiveXObject('Microsoft.XMLHTTP');
-        }
-        ajaxReq.onreadystatechange = function() {
-          if (ajaxReq.readyState == XMLHttpRequest.DONE) {
-            var json = JSON.parse(ajaxReq.response);
-            json.article_reaction_counts.forEach(function(reaction) {
-              setReactionCount(reaction.category, reaction.count);
-            });
-            json.reactions.forEach(function(reaction) {
-              if (
-                document.getElementById('reaction-butt-' + reaction.category)
-              ) {
-                showUserReaction(reaction.category, 'not-user-animated');
-              }
-            });
-          }
-        };
-        ajaxReq.open('GET', '/reactions?article_id=' + articleId, true);
-        ajaxReq.send();
-      }
-    }
-    var reactionButts = document.getElementsByClassName(
-      'article-reaction-butt',
+function setCollectionFunctionality() {
+  if (document.getElementById('collection-link-inbetween')) {
+    var inbetweenLinks = document.getElementsByClassName(
+      'collection-link-inbetween',
     );
-    for (var i = 0; i < reactionButts.length; i++) {
-      reactionButts[i].onclick = function(e) {
-        reactToArticle(articleId, this.dataset.category);
-      };
-    }
-    if (document.getElementById('jump-to-comments')) {
-      document.getElementById('jump-to-comments').onclick = function(e) {
+    var inbetweenLinksLength = inbetweenLinks.length;
+    for (var i = 0; i < inbetweenLinks.length; i += 1) {
+      inbetweenLinks[i].onclick = function onClickInbetweenLinks(e) {
         e.preventDefault();
-        document.getElementById('comments').scrollIntoView({
-          behavior: 'instant',
-          block: 'start',
-        });
+        var els = document.getElementsByClassName('collection-link-hidden');
+        var elsLength = els.length;
+        for (var j = 0; j < elsLength; j += 1) {
+          els[0].classList.remove('collection-link-hidden');
+        }
+        for (var k = 0; k < inbetweenLinksLength; k += 1) {
+          inbetweenLinks[0].className = 'collection-link-hidden';
+        }
       };
     }
-  }, 3);
+  }
 }
 
 function reactToArticle(articleId, reaction) {
@@ -109,14 +81,13 @@ function reactToArticle(articleId, reaction) {
   var userStatus = document
     .getElementsByTagName('body')[0]
     .getAttribute('data-user-status');
-  sendHapticMessage('medium');
-  if (userStatus == 'logged-out') {
+  window.sendHapticMessage('medium');
+  if (userStatus === 'logged-out') {
     showModal('react-to-article');
     return;
-  } else {
-    toggleReaction();
-    document.getElementById('reaction-butt-' + reaction).disabled = true;
   }
+  toggleReaction();
+  document.getElementById('reaction-butt-' + reaction).disabled = true;
 
   function createFormdata() {
     /*
@@ -132,39 +103,74 @@ function reactToArticle(articleId, reaction) {
 
   getCsrfToken()
     .then(sendFetch('reaction-creation', createFormdata()))
-    .then(function(response) {
+    .then(function getCsrfTokenCallback(response) {
       if (response.status === 200) {
         return response.json().then(() => {
           document.getElementById('reaction-butt-' + reaction).disabled = false;
         });
-      } else {
-        toggleReaction();
-        document.getElementById('reaction-butt-' + reaction).disabled = false;
       }
+      toggleReaction();
+      document.getElementById('reaction-butt-' + reaction).disabled = false;
+      return null;
     })
-    .catch(function(error) {
+    .catch(function getCsrfTokenCatch(error) {
       toggleReaction();
       document.getElementById('reaction-butt-' + reaction).disabled = false;
     });
 }
 
-
-function setCollectionFunctionality() {
-  if (document.getElementById('collection-link-inbetween')) {
-    var inbetweenLinks = document.getElementsByClassName('collection-link-inbetween');
-    var inbetweenLinksLength = inbetweenLinks.length
-    for (var i = 0; i < inbetweenLinks.length; i++) {
-      inbetweenLinks[i].onclick = function(e) {
-        e.preventDefault();
-        var els = document.getElementsByClassName('collection-link-hidden');
-        var elsLength = els.length
-        for (var i = 0; i < elsLength; i++) {
-          els[0].classList.remove('collection-link-hidden');
+function initializeArticleReactions() {
+  setCollectionFunctionality();
+  setTimeout(function reactionsTimeout() {
+    if (document.getElementById('article-body')) {
+      var articleId = document.getElementById('article-body').dataset.articleId;
+      if (document.getElementById('article-reaction-actions')) {
+        var ajaxReq;
+        var thisButt = this;
+        if (window.XMLHttpRequest) {
+          ajaxReq = new XMLHttpRequest();
+        } else {
+          ajaxReq = new ActiveXObject('Microsoft.XMLHTTP');
         }
-        for (var i = 0; i < inbetweenLinksLength; i++) {
-          inbetweenLinks[0].className = 'collection-link-hidden'
-        }
+        ajaxReq.onreadystatechange = function updateReactionsOnReady() {
+          if (ajaxReq.readyState === XMLHttpRequest.DONE) {
+            var json = JSON.parse(ajaxReq.response);
+            json.article_reaction_counts.forEach(function initSetReactionCount(
+              reaction,
+            ) {
+              setReactionCount(reaction.category, reaction.count);
+            });
+            json.reactions.forEach(function initShowUserReaction(reaction) {
+              if (
+                document.getElementById('reaction-butt-' + reaction.category)
+              ) {
+                showUserReaction(reaction.category, 'not-user-animated');
+              }
+            });
+          }
+        };
+        ajaxReq.open('GET', '/reactions?article_id=' + articleId, true);
+        ajaxReq.send();
       }
-    }  
-  }
+    }
+    var reactionButts = document.getElementsByClassName(
+      'article-reaction-butt',
+    );
+    for (var i = 0; i < reactionButts.length; i += 1) {
+      reactionButts[i].onclick = function initReactToArticle(e) {
+        reactToArticle(articleId, this.dataset.category);
+      };
+    }
+    if (document.getElementById('jump-to-comments')) {
+      document.getElementById(
+        'jump-to-comments',
+      ).onclick = function jumpToComments(e) {
+        e.preventDefault();
+        document.getElementById('comments').scrollIntoView({
+          behavior: 'instant',
+          block: 'start',
+        });
+      };
+    }
+  }, 3);
 }
