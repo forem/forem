af7c1cbf0cdc8a8b762fcb33e37fe723a437e655
Saurabh Daware <saurabhdaware99@gmail.com>
2019-10-03 00:46:27 +0530

    color preview ui done

---
+++ app/javascript/packs/colorPreview.js
@@ -0,0 +1,5 @@
+// const bgColorField = document.getElementById('bg-color-colorfield');
+// const textColorField = document.getElementById('text-color-colorfield');
+
+// const bgTextField = document.getElementById('bg-color-textfield');
+// const colorTextField = document.getElementById('text-color-textfield');
