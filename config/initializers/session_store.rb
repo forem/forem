65fb282b9a6c405b97e0584e4dbd1bd8af131adf
rhymes <rhymesete@gmail.com>
2019-10-03 11:52:21 +0200

    Make sure the expiration time can't zero

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
+# we want a default in case the expiration is not set or set to 0
+# because 0 is an invalid value
+app_config_expires_in = ApplicationConfig["SESSION_EXPIRY_SECONDS"].to_i
+expires_in = app_config_expires_in.positive? ? app_config_expires_in : 2.weeks.to_i
+
 Rails.application.config.session_store :redis_store,
-  key: ApplicationConfig["SESSION_KEY"],
-  servers: ApplicationConfig["REDIS_URL"],
-  expires_in: ApplicationConfig["SESSION_EXPIRY_SECONDS"].to_i
+                                       key: ApplicationConfig["SESSION_KEY"],
+                                       servers: ApplicationConfig["REDIS_URL"],
+                                       expires_in: expires_in
