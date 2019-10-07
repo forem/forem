9d2f4c5d0e83121a8e7180e970ff712b443073bf
alebruck <alebruck@gmail.com>
2019-10-08 00:37:55 +0200

    Render full url for relative path links

--- app/lib/redcarpet/render/html_rouge.rb
+++ app/lib/redcarpet/render/html_rouge.rb
@@ -14,10 +14,12 @@ module Redcarpet
         @options[:link_attributes]&.each do |attribute, value|
           link_attributes += %( #{attribute}="#{value}")
         end
-        if (/\A(https?:\/\/)/.match? link) || link.nil?
+        if (/https?:\/\/[\S]+/.match? link) || link.nil?
           %(<a href="#{link}"#{link_attributes}>#{content}</a>)
-        else
+        elsif /\.{1}/.match? link
           %(<a href="//#{link}"#{link_attributes}>#{content}</a>)
+        else
+          %(<a href="https://dev.to#{link}"#{link_attributes}>#{content}</a>)
         end
       end
 
