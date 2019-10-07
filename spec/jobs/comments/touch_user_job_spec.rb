a2ec1d01bddb09f15494cd449c96a6eb08dc5f73
Austen Madden <austen@data-axle.com>
2019-10-07 12:25:29 -0400

    Avoid time comparison precision issues

--- spec/jobs/comments/touch_user_job_spec.rb
+++ spec/jobs/comments/touch_user_job_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Comments::TouchUserJob, type: :job do
     let!(:article) { FactoryBot.create(:article) }
     let!(:comment) { FactoryBot.create(:comment, commentable: article) }
     let(:user) { comment.user }
-    let(:touched_at) { 5.minutes.from_now }
+    let(:touched_at) { 5.minutes.from_now.beginning_of_minute }
 
     it "touches user updated_at and last_comment_at columns", :aggregate_failures do
       Timecop.freeze(touched_at) do
