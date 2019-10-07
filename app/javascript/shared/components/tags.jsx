17e9fcabe20d3ee9a88e54170eeb2e911c3a33f4
torianne02 <victoria.fluharty@gmail.com>
2019-10-07 09:19:39 -0700

    adds comments to fixed code for clarification

--- app/javascript/shared/components/tags.jsx
+++ app/javascript/shared/components/tags.jsx
@@ -356,8 +356,10 @@ class Tags extends Component {
             }
           });
         }
+        // updates searchResults array according to what is being typed by user
+        // allows user to choose a tag when they've typed the partial or whole word
         this.setState({
-          searchResults: content.hits
+          searchResults: content.hits,
         });
       });
   }
