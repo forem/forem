269fa2a710eb01b642d9585be528b5098a069a12
Sagar Choudhary <sagar.choudhary96@gmail.com>
2019-10-08 23:08:47 +0530

    change: onchange method to native js eventListener

--- app/assets/javascripts/initializers/initializeDashboardSort.js
+++ app/assets/javascripts/initializers/initializeDashboardSort.js
@@ -2,8 +2,10 @@
 
 function initializeDashboardSort() {
   if (document.getElementById('dashhboard_sort')) {
-    document.getElementById('dashhboard_sort').onchange = () => {
-      window.location = '/dashboard?sort=' + this.value;
-    };
+    document
+      .getElementById('dashhboard_sort')
+      .addEventListener('change', event => {
+        window.location = '/dashboard?sort=' + event.target.value;
+      });
   }
 }
