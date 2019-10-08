975662c9b9d77a2f90516793c6198b7c580db78d
Duc Nguyen <vanducld@gmail.com>
2019-10-09 04:06:46 +0700

    Feat: #3498 Display episode's published_at (#4272)
    * feat: #3498 Display episode's published_at
    
    * feat: #3498 Move methods to decorator
    
    * feat: #3498 Add check if published_at present
    
    * feat: #3498 Add testcase for showing published_at
    
    * Fix build failure #3498
    
    https://travis-ci.com/thepracticaldev/dev.to/builds/130734935
    
    * feat: #3498 Fix testcase failed on Travis 130738295
    
    * feat: #3498 Address comments on PR #4272
    
    * feat: #3498 Delete unused
    
    * feat: #3498 Refactor the use of decorate

--- app/decorators/podcast_episode_decorator.rb
+++ app/decorators/podcast_episode_decorator.rb
@@ -8,4 +8,18 @@ class PodcastEpisodeDecorator < ApplicationDecorator
   def cached_tag_list_array
     (tag_list || "").split(", ")
   end
+
+  def readable_publish_date
+    return unless published_at
+
+    if published_at.year == Time.current.year
+      published_at.strftime("%b %e")
+    else
+      published_at.strftime("%b %e '%y")
+    end
+  end
+
+  def published_timestamp
+    published_at&.utc&.iso8601
+  end
 end
