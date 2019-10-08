daad91474ca2ef6e13e270c7b829a84b3ced752a
Sagar Choudhary <sagar.choudhary96@gmail.com>
2019-10-08 21:16:37 +0530

    Refactor: initializeDashboardSort.js

--- app/assets/javascripts/initializers/initializeDashboardSort.js
+++ app/assets/javascripts/initializers/initializeDashboardSort.js
@@ -1,6 +1,8 @@
+'use strict';
+
 function initializeDashboardSort() {
   if (document.getElementById('dashhboard_sort')) {
-    document.getElementById('dashhboard_sort').onchange = function() {
+    document.getElementById('dashhboard_sort').onchange = () => {
       window.location = '/dashboard?sort=' + this.value;
     };
   }
