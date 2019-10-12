ee6d52d164d6fc24e0e46f74ad755bf7ad276f67
Neto Chaves <netobac1@gmail.com>
2019-10-12 19:32:22 -0300

    refactor initializeTimeFixer.js

--- app/assets/javascripts/initializers/initializeTimeFixer.js
+++ app/assets/javascripts/initializers/initializeTimeFixer.js
@@ -1,3 +1,51 @@
+'use strict';
+
+/* eslint-disable no-param-reassign */
+
+function getFormat(options, formatValue) {
+  return new Intl.DateTimeFormat('en-US', options).format(formatValue);
+}
+
+function convertUtcTime(utcTime) {
+  var time = new Date(utcTime);
+  var options = {
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZoneName: 'short',
+  };
+  return getFormat(options, time);
+}
+
+function convertUtcDate(utcDate) {
+  var date = new Date(utcDate);
+  var options = {
+    month: 'short',
+    day: 'numeric',
+  };
+  return getFormat(options, date);
+}
+
+function convertCalEvent(utc) {
+  var date = new Date(utc);
+  var options = {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+  };
+
+  return getFormat(options, date);
+}
+
+function updateLocal(elements, convertCallback) {
+  var local;
+  for (var i = 0; i < elements.length; i += 1) {
+    local = convertCallback(elements[i].dataset.datetime);
+    elements[i].innerHTML = local;
+  }
+}
+
 function initializeTimeFixer() {
   var utcTime = document.getElementsByClassName('utc-time');
   var utcDate = document.getElementsByClassName('utc-date');
@@ -7,65 +55,7 @@ function initializeTimeFixer() {
     return;
   }
 
-  function convertUtcTime(utcTime) {
-    var time = new Date(utcTime);
-    var options = {
-      hour: 'numeric',
-      minute: 'numeric',
-      timeZoneName: 'short'
-    };
-    time = new Intl.DateTimeFormat('en-US', options).format(time);
-    return time;
-  }
-
-  function updateLocalTime(times) {
-    var localTime;
-    for (var i = 0; i < times.length; i++) {
-      localTime = convertUtcTime(times[i].dataset.datetime);
-      times[i].innerHTML = localTime;
-    }
-  }
-
-  function convertUtcDate(utcDate) {
-    var date = new Date(utcDate);
-    var options = {
-      month: 'short',
-      day: 'numeric',
-    };
-    date = new Intl.DateTimeFormat('en-US', options).format(date);
-    return date;
-  }
-
-  function updateLocalDate(dates) {
-    var localDate;
-    for (var i = 0; i < dates.length; i++) {
-      localDate = convertUtcDate(dates[i].dataset.datetime);
-      dates[i].innerHTML = localDate;
-    }
-  }
-
-  function convertCalEvent(utc) {
-    var date = new Date(utc);
-    var options = {
-      weekday: 'long',
-      month: 'long',
-      day: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric',
-    };
-    date = new Intl.DateTimeFormat('en-US', options).format(date);
-    return date;
-  }
-
-  function updateCalendarTime(utcTimes) {
-    var calTime;
-    for (var i = 0; i < utcTimes.length; i++) {
-      calTime = convertCalEvent(utcTimes[i].innerHTML);
-      utcTimes[i].innerHTML = calTime;
-    }
-  }
-
-  updateLocalTime(utcTime);
-  updateLocalDate(utcDate);
-  updateCalendarTime(utc);
+  updateLocal(utcTime, convertUtcTime);
+  updateLocal(utcDate, convertUtcDate);
+  updateLocal(utc, convertCalEvent);
 }
