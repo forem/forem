0c62554cc5255e3fc09bc2342d899bfed3a5ae31
Jacob Herrington <jacobherringtondeveloper@gmail.com>
2019-10-14 08:35:37 -0500

    Remove unused onboarding files (#4254)
    * Remove commented code
    
    * Format onboarding SCSS
    
    * Remove old onboarding files
    
    * Remove tests for deleted React components
    
    * Remove outdated onboarding stories

--- app/javascript/src/components/__stories__/OnboardingWelcomeThread.stories.jsx
+++
@@ -1,8 +0,0 @@
-import { h } from 'preact';
-import { storiesOf } from '@storybook/react';
-import { globalModalDecorator } from './story-decorators';
-import OnboardingWelcomeThread from '../OnboardingWelcomeThread';
-
-storiesOf('OnboardingWelcomeThread', module)
-  .addDecorator(globalModalDecorator)
-  .add('Default', () => <OnboardingWelcomeThread />);
