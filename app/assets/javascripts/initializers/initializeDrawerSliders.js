0985a67150e3b12053e2120983fdb72216a733cc
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2019-10-14 12:06:51 +0530

    Refactor: initializeDrawerSliders.js

--- app/assets/javascripts/initializers/initializeDrawerSliders.js
+++ app/assets/javascripts/initializers/initializeDrawerSliders.js
@@ -1,57 +1,66 @@
-function initializeDrawerSliders() {
-  if(!initializeSwipeGestures.called) {
-    swipeState = "middle";
-    initializeSwipeGestures();
-  }
-  if (document.getElementById("on-page-nav-controls")){
-    if (document.getElementById("sidebar-bg-left")){
-      document.getElementById("sidebar-bg-left").onclick = function(){
-        swipeState = "middle";
-        slideSidebar("left","outOfView");
-      }
-    }
-    if (document.getElementById("sidebar-bg-right")){
-      document.getElementById("sidebar-bg-right").onclick = function(){
-        swipeState = "middle";
-        slideSidebar("right","outOfView");
-      }
-    }
+'use strict';
 
-    if (document.getElementById("on-page-nav-butt-left")){
-      document.getElementById("on-page-nav-butt-left").onclick = function(){
-        swipeState = "left"
-        slideSidebar("left","intoView");
-      }
-    }
-    if (document.getElementById("on-page-nav-butt-right")){
-      document.getElementById("on-page-nav-butt-right").onclick = function(){
-        swipeState = "right"
-        slideSidebar("right","intoView");
-      }
-    }
-    InstantClick.on('change', function() {
-      document.getElementsByTagName('body')[0].classList.remove('modal-open');
-      slideSidebar("right","outOfView");
-      slideSidebar("left","outOfView");
+function listenForNarrowMenuClick() {
+  var navLinks = document.getElementsByClassName('narrow-nav-menu');
+  var narrowFeedButt = document.getElementById('narrow-feed-butt');
+  var narrowNavMenu = document.getElementById('narrow-nav-menu');
+  for (let i = 0; i < navLinks.length; i += 1) {
+    narrowNavMenu.classList.remove('showing');
+  }
+  if (narrowFeedButt) {
+    narrowFeedButt.addEventListener('click', () => {
+      narrowNavMenu.classList.add('showing');
     });
-    listenForNarrowMenuClick();
+  }
+  for (let i = 0; i < navLinks.length; i += 1) {
+    navLinks[i].addEventListener('click', () => {
+      narrowNavMenu.classList.remove('showing');
+    });
   }
 }
 
-function listenForNarrowMenuClick(event) {
-  var navLinks = document.getElementsByClassName("narrow-nav-menu");
-  var narrowFeedButt = document.getElementById("narrow-feed-butt");
-  for (var i = 0; i < navLinks.length; i++) {
-    document.getElementById("narrow-nav-menu").classList.remove("showing");
+/* global initializeSwipeGestures */
+/* global slideSidebar */
+/* global InstantClick */
+function initializeDrawerSliders() {
+  if (!initializeSwipeGestures.called) {
+    initializeSwipeGestures();
   }
-  if (narrowFeedButt) {
-    narrowFeedButt.onclick = function(){
-      document.getElementById("narrow-nav-menu").classList.add("showing");
+  if (document.getElementById('on-page-nav-controls')) {
+    if (document.getElementById('sidebar-bg-left')) {
+      document
+        .getElementById('sidebar-bg-left')
+        .addEventListener('click', () => {
+          slideSidebar('left', 'outOfView');
+        });
     }
-  }
-  for (var i = 0; i < navLinks.length; i++) {
-    navLinks[i].onclick = function(event){
-      document.getElementById("narrow-nav-menu").classList.remove("showing");
+    if (document.getElementById('sidebar-bg-right')) {
+      document
+        .getElementById('sidebar-bg-right')
+        .addEventListener('click', () => {
+          slideSidebar('right', 'outOfView');
+        });
     }
+
+    if (document.getElementById('on-page-nav-butt-left')) {
+      document
+        .getElementById('on-page-nav-butt-left')
+        .addEventListener('click', () => {
+          slideSidebar('left', 'intoView');
+        });
+    }
+    if (document.getElementById('on-page-nav-butt-right')) {
+      document
+        .getElementById('on-page-nav-butt-right')
+        .addEventListener('click', () => {
+          slideSidebar('right', 'intoView');
+        });
+    }
+    InstantClick.on('change', () => {
+      document.getElementsByTagName('body')[0].classList.remove('modal-open');
+      slideSidebar('right', 'outOfView');
+      slideSidebar('left', 'outOfView');
+    });
+    listenForNarrowMenuClick();
   }
 }
