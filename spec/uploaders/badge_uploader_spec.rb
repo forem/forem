c76350f9a302c05b534923b1dc3d3cc76c2871fc
rhymes <rhymesete@gmail.com>
2019-10-14 16:46:44 +0200

    Add specs for BadgeUploader

--- spec/uploaders/badge_uploader_spec.rb
+++ spec/uploaders/badge_uploader_spec.rb
@@ -1,10 +1,11 @@
 require "rails_helper"
 require "carrierwave/test/matchers"
 
-describe ArticleImageUploader do
+describe BadgeUploader do
   include CarrierWave::Test::Matchers
 
-  let_it_be(:uploader) { described_class.new }
+  let_it_be(:badge) { create(:badge) }
+  let_it_be(:uploader) { described_class.new(badge, :badge_image) }
   let_it_be(:image_jpg) { fixture_file_upload("files/800x600.jpg", "image/jpeg") }
   let_it_be(:image_png) { fixture_file_upload("files/800x600.png", "image/png") }
   let_it_be(:image_webp) { fixture_file_upload("files/800x600.webp", "image/webp") }
@@ -20,23 +21,12 @@ describe ArticleImageUploader do
   end
 
   it "stores files in the correct directory" do
-    expect(uploader.store_dir).to eq("i/")
-  end
-
-  describe "filename" do
-    it "defaults to nil" do
-      expect(uploader.filename).to be_nil
-    end
-
-    it "contains the original file extension when a file is stored" do
-      uploader.store!(image_jpg)
-      expect(uploader.filename).to match(/\.jpg/)
-    end
+    expect(uploader.store_dir).to eq("uploads/badge/badge_image/#{badge.id}")
   end
 
   describe "formats" do
     it "permits a set of extensions" do
-      expect(uploader.extension_whitelist).to eq(%w[jpg jpeg jpe gif png ico bmp dng])
+      expect(uploader.extension_whitelist).to eq(%w[jpg jpeg gif png])
     end
 
     it "permits jpegs" do
