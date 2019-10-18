a550662ef48be462afed7ba5a08ac6dfea558d6f
Neto Chaves <netobac1@gmail.com>
2019-10-18 18:43:08 -0300

    fix eslint errors

--- app/javascript/listings/elements/title.jsx
+++ app/javascript/listings/elements/title.jsx
@@ -3,28 +3,29 @@ import { h } from 'preact';
 
 const domId = 1;
 
-const Title = ( { onChange, defaultValue }) => (
+const Title = ({ onChange, defaultValue }) => (
   <div className="field">
-    <label className="listingform__label" htmlFor={domId}>Title</label>
-    <input
-      type="text"
-      className="listingform__input"
-      id={domId}
-      name="classified_listing[title]"
-      maxLength="128"
-      size="128"
-      placeholder="128 characters max, plain text"
-      autoComplete="off"
-      value={defaultValue}
-      onInput={onChange}
-    />
+    <label className="listingform__label" htmlFor={domId}>
+      Title
+      <input
+        type="text"
+        className="listingform__input"
+        id={domId}
+        name="classified_listing[title]"
+        maxLength="128"
+        size="128"
+        placeholder="128 characters max, plain text"
+        autoComplete="off"
+        value={defaultValue}
+        onInput={onChange}
+      />
+    </label>
   </div>
-)
+);
 
 Title.propTypes = {
   onChange: PropTypes.func.isRequired,
   defaultValue: PropTypes.string.isRequired,
-}
-
+};
 
 export default Title;
