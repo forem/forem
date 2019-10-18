2867f04da0c51c6674fc8aedfc19107b866649ba
Allen Br <53116218+allenbrkn@users.noreply.github.com>
2019-10-18 17:50:04 +0530

    updated via JS to not only show profile-dropdown on current_user

--- app/assets/javascripts/initializers/initializeUserProfilePage.js
+++ app/assets/javascripts/initializers/initializeUserProfilePage.js
@@ -1,11 +1,23 @@
 'use strict';
 
 function initializeUserProfilePage() {
-  if (document.getElementById('user-profile-dropdown')) {
-    document.getElementById('user-profile-dropdown').onclick = () => {
-      document
-        .getElementById('user-profile-dropdownmenu')
-        .classList.toggle('showing');
-    };
+  if(document.querySelector('.profile-dropdown')){
+    var profileDropdownDiv = document.querySelector('.profile-dropdown');
+    var currentUser = userData();
+    var profileUser = JSON.parse(profileDropdownDiv.getAttribute('data-info'));
+
+    if(currentUser.username == profileUser.username){
+      profileDropdownDiv.hidden = true;
+    }
+    else{
+      profileDropdownDiv.hidden = false;
+      if (document.getElementById('user-profile-dropdown')) {
+        document.getElementById('user-profile-dropdown').onclick = () => {
+          document
+            .getElementById('user-profile-dropdownmenu')
+            .classList.toggle('showing');
+        };
+      }
+    }
   }
 }
