0d2506f9ef79a32f7474001337161314d1062134
Tamas Fodor <ftamas.mail@gmail.com>
2019-10-19 23:09:06 +0200

    Fix eslint issues in article-form's jsx files (#4217)
    * fix eslint issues in article-form's jsx files
    
    * Use e.key instead of deprecated e.keyCode
    
    * Put back whitespace but in html format
    
    * use re-usable SetupImageButton comp, use window.confirm, put back console.log temporarily
    
    * import deep
    
    * fix CoverImage className prop issue
    
    * update jest snap files
    
    * remove no-restricted-globals from eslint-disable-next-line

--- app/javascript/article-form/elements/bodyPreview.jsx
+++ app/javascript/article-form/elements/bodyPreview.jsx
@@ -7,6 +7,12 @@ const CoverImage = ({ className, imageSrc, imageAlt }) => (
   </div>
 );
 
+CoverImage.propTypes = {
+  className: PropTypes.string.isRequired,
+  imageSrc: PropTypes.string.isRequired,
+  imageAlt: PropTypes.string.isRequired,
+};
+
 function titleArea(previewResponse, version, articleState) {
   if (version === 'help') {
     // possibly something different here in future.
@@ -25,7 +31,7 @@ function titleArea(previewResponse, version, articleState) {
 
   let coverImage = '';
   if (previewResponse.cover_image && previewResponse.cover_image.length > 0) {
-    coverImage = previewResponse.cover_image
+    coverImage = previewResponse.cover_image;
   } else if (articleState.mainImage) {
     coverImage = articleState.mainImage;
   }
@@ -76,6 +82,7 @@ const BodyPreview = ({ previewResponse, version, articleState }) => (
     {titleArea(previewResponse, version, articleState)}
     <div
       className="body"
+      // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={{ __html: previewResponse.processed_html }}
       style={{ width: '90%' }}
     />
@@ -88,12 +95,6 @@ const previewResponsePropTypes = PropTypes.shape({
   tags: PropTypes.array,
   cover_image: PropTypes.string,
 });
-
-CoverImage.propTypes = {
-  className: PropTypes.string,
-  imageSrc: PropTypes.string.isRequired,
-  imageAlt: PropTypes.string.isRequired
-}
 
 BodyPreview.propTypes = {
   previewResponse: previewResponsePropTypes.isRequired,
