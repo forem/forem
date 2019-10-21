afd181838afad7473b81138ab07c21427fd33726
Megha Sachdev <meghasachdev12@gmail.com>
2019-10-20 18:12:17 -0700

    removing applitools

--- cypress/integration/visual-testing/homepage-test.spec.js
+++ cypress/integration/visual-testing/homepage-test.spec.js
@@ -8,16 +8,9 @@ describe('Visual  Regression Tests', () => {
     cy.eyesClose();
   });
 
-  it('should open home page', () => {
-    cy.eyesCheckWindow('Main Page');
-  });
-
   it('should display sign up options', () => {
     cy.get('#navigation-butt').click();
-    cy.eyesCheckWindow({
-      sizeMode: 'selector', //mode
-      selector: '.menu.logged-out',
-    });
+    cy.get('#navigation-butt').should('be.visible');
   });
 
   it('should redirect to code of conduct', () => {
@@ -26,20 +19,23 @@ describe('Visual  Regression Tests', () => {
     return elem.invoke('attr', 'href').then(href => {
       cy.visit(`${baseURL}${href}`);
       cy.wait(2000);
-      cy.eyesCheckWindow('Code of Conduct');
+      cy.get('h1')
+        .should('be.visible')
+        .contains('Code of Conduct');
     });
   });
 
   it('should redirect to about us', () => {
     cy.get('#loggedoutmenu.menu.logged-out').invoke('show');
     const elem = cy.get('[data-cy-href=aboutus]');
-    cy.wait(2000)
+    cy.wait(2000);
     elem.contains('All about dev.to').invoke('show');
     return elem.invoke('attr', 'href').then(href => {
       cy.visit(`${baseURL}${href}`);
       cy.wait(2000);
-      cy.eyesCheckWindow('All about dev.to');
+      cy.get('h1')
+        .should('be.visible')
+        .contains('More information about dev.to');
     });
   });
-
 });
