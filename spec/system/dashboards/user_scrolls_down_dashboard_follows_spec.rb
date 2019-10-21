c9a1701635938ec516f94c661ea840846189049f
John Curcio <john@curcio.dev>
2019-10-21 12:49:34 -0300

    Use let! to create scrolling test data

--- spec/system/dashboards/user_scrolls_down_dashboard_follows_spec.rb
+++ spec/system/dashboards/user_scrolls_down_dashboard_follows_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe "Infinite scroll on dashboard", type: :system, js: true do
   let(:default_per_page) { 5 }
   let(:total_records) { default_per_page * 2 }
   let(:user) { create(:user) }
-  let(:users) { create_list(:user, total_records) }
-  let(:tags) { create_list(:tag, total_records) }
-  let(:organizations) { create_list(:organization, total_records) }
-  let(:podcasts) { create_list(:podcast, total_records) }
+  let!(:users) { create_list(:user, total_records) }
+  let!(:tags) { create_list(:tag, total_records) }
+  let!(:organizations) { create_list(:organization, total_records) }
+  let!(:podcasts) { create_list(:podcast, total_records) }
 
   before do
     sign_in user
