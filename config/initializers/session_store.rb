6c767e262f001eef8571263cdba6e51477ed13d1
rhymes <rhymesete@gmail.com>
2019-10-23 18:07:38 +0200

    Add signed, secure and httponly to the Redis session cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,10 +2,15 @@
 
 # we want a default in case the expiration is not set or set to 0
 # because 0 is an invalid value
-app_config_expires_in = ApplicationConfig["SESSION_EXPIRY_SECONDS"].to_i
-expires_in = app_config_expires_in.positive? ? app_config_expires_in : 2.weeks.to_i
+app_config_expires_after = ApplicationConfig["SESSION_EXPIRY_SECONDS"].to_i
+expires_after = app_config_expires_after.positive? ? app_config_expires_after : 2.weeks.to_i
 
+# see <https://github.com/redis-store/redis-rails#session-storage> for configuration options,
+# httponly has been added in <https://github.com/redis-store/redis-actionpack/pull/17>
 Rails.application.config.session_store :redis_store,
                                        key: ApplicationConfig["SESSION_KEY"],
                                        servers: ApplicationConfig["REDIS_URL"],
-                                       expires_in: expires_in
+                                       expire_after: expires_after,
+                                       signed: true,
+                                       secure: Rails.env.production?,
+                                       httponly: true
