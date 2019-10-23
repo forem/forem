2848bcd1564480aa49ba6ea19cb009a4dc8db812
Neto Chaves <netobac1@gmail.com>
2019-10-23 13:42:23 -0300

    fix spacing

--- app/javascript/listings/elements/title.jsx
+++ app/javascript/listings/elements/title.jsx
@@ -7,19 +7,19 @@ const Title = ({ onChange, defaultValue }) => (
   <div className="field">
     <label className="listingform__label" htmlFor={domId}>
       Title
-      <input
-        type="text"
-        className="listingform__input"
-        id={domId}
-        name="classified_listing[title]"
-        maxLength="128"
-        size="128"
-        placeholder="128 characters max, plain text"
-        autoComplete="off"
-        value={defaultValue}
-        onInput={onChange}
-      />
     </label>
+    <input
+      type="text"
+      className="listingform__input"
+      id={domId}
+      name="classified_listing[title]"
+      maxLength="128"
+      size="128"
+      placeholder="128 characters max, plain text"
+      autoComplete="off"
+      value={defaultValue}
+      onInput={onChange}
+    />
   </div>
 );
 
