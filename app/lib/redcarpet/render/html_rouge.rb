8ac2ac8bdd584aa6eeffc8271d30e87244463519
Melvin Yeo <melvinyzw@gmail.com>
2019-10-28 17:27:47 +0800

    Change block_code to accept opt

--- app/lib/redcarpet/render/html_rouge.rb
+++ app/lib/redcarpet/render/html_rouge.rb
@@ -6,7 +6,7 @@ module Redcarpet
       include Rouge::Plugins::Redcarpet
 
       # overrided method to add line number support
-      def block_code(code, language, line_numbers = false, start_line = 1)
+      def block_code(code, language, opts = {})
         lexer =
           begin
             # add fix to handle language with upper case
@@ -23,9 +23,12 @@ module Redcarpet
           code.gsub! %r{^    }, "\t"
         end
 
-        formatter = Rouge::Formatters::HTMLLegacy.new(css_class: "highlight #{lexer.tag}",
-                                                      line_numbers: line_numbers,
-                                                      start_line: start_line)
+        # append default css_class to block_code
+        unless opts.key? "css_class"
+          opts["css_class"] = "highlight #{lexer.tag}"
+        end
+
+        formatter = Rouge::Formatters::HTMLLegacy.new(opts)
         formatter.format(lexer.lex(code))
       end
 
