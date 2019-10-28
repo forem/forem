67fe6cc5f88501b087fd16fb976987d29bf88961
mstruve <mollylbs@gmail.com>
2019-10-28 13:09:09 -0500

    check for REDISCLOUD_URL and fallback on REDIS_URL when connecting to redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,6 @@
-require "redis"
+# DEV uses the RedisCloud Heroku Add-On which comes with the predefiend env variable REDISCLOUD_URL
+redis_url = ENV["REDISCLOUD_URL"]
+redis_url ||= ApplicationConfig["REDIS_URL"]
 
-uri = URI.parse(ApplicationConfig["REDISCLOUD_URL"])
-RedisClient = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+uri = URI.parse(redis_url)
+RedisClient = Redis.new(host: uri.host, port: uri.port)
