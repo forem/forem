d4df8672d2a4b7018758de922b56c4e77f6826fe
mstruve <mollylbs@gmail.com>
2019-10-28 14:46:58 -0500

    dont parse Redis url in initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,5 @@
-# DEV uses the RedisCloud Heroku Add-On which comes with the predefiend env variable REDISCLOUD_URL
+# DEV uses the RedisCloud Heroku Add-On which comes with the predefined env variable REDISCLOUD_URL
 redis_url = ENV["REDISCLOUD_URL"]
 redis_url ||= ApplicationConfig["REDIS_URL"]
 
-uri = URI.parse(redis_url)
-RedisClient = Redis.new(host: uri.host, port: uri.port)
+RedisClient = Redis.new(url: redis_url)
