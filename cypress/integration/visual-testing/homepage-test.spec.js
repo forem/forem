1f9f1160ce1879967bf88cd1eaf1aaf1666e718e
Megha Sachdev <meghasachdev12@gmail.com>
2019-10-28 21:30:11 -0700

    Fixing review comments

--- cypress/integration/visual-testing/homepage-test.spec.js
+++ cypress/integration/visual-testing/homepage-test.spec.js
@@ -2,40 +2,37 @@ import { baseURL, eyesOpen } from '../../utils';
 describe('Visual  Regression Tests', () => {
   beforeEach(() => {
     cy.visit(baseURL);
-    eyesOpen('Home Page');
-  });
-  afterEach(() => {
-    cy.eyesClose();
   });
 
   it('should display sign up options', () => {
-    cy.get('#navigation-butt').click();
     cy.get('#navigation-butt').should('be.visible');
   });
 
   it('should redirect to code of conduct', () => {
-    const elem = cy.get('[data-cy-href=codeofconduct]');
-    elem.contains('Code of Conduct');
-    return elem.invoke('attr', 'href').then(href => {
-      cy.visit(`${baseURL}${href}`);
-      cy.wait(2000);
-      cy.get('h1')
-        .should('be.visible')
-        .contains('Code of Conduct');
-    });
+    cy.get('[data-cy=codeofconduct]')
+      .contains('Code of Conduct')
+      .invoke('attr', 'href')
+      .then(href => {
+        cy.visit(`${baseURL}${href}`);
+        cy.get('h1')
+          .should('be.visible')
+          .contains('Code of Conduct');
+      });
   });
 
   it('should redirect to about us', () => {
     cy.get('#loggedoutmenu.menu.logged-out').invoke('show');
-    const elem = cy.get('[data-cy-href=aboutus]');
-    cy.wait(2000);
-    elem.contains('All about dev.to').invoke('show');
-    return elem.invoke('attr', 'href').then(href => {
-      cy.visit(`${baseURL}${href}`);
-      cy.wait(2000);
-      cy.get('h1')
-        .should('be.visible')
-        .contains('More information about dev.to');
-    });
+    cy.get('[data-cy=aboutus]')
+      .contains('All about dev.to')
+      .parent()
+      .invoke('attr', 'href')
+      .then(href => {
+        cy.visit(`${baseURL}${href}`);
+        console.log('hi');
+        console.log(`${href}`);
+        cy.get('h1')
+          .should('be.visible')
+          .contains('More information about dev.to');
+      });
   });
 });
