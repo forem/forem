36d1df7f49fc31de7c4678eb6230ed59a29c0d4e
rhymes <rhymesete@gmail.com>
2019-10-30 15:46:57 +0100

    Routine rubocop lint fixes (#4638) [ci skip]

--- spec/models/user_block_spec.rb
+++ spec/models/user_block_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe UserBlock, type: :model do
     it { is_expected.to validate_inclusion_of(:config).in_array(%w[default]) }
 
     it "prevents the blocker from blocking itself" do
-      user_block = UserBlock.new(blocker_id: 1, blocked_id: 1, config: "default")
+      user_block = described_class.new(blocker_id: 1, blocked_id: 1, config: "default")
       expect(user_block.valid?).to eq false
       expect(user_block.errors.full_messages).to include "Blocker can't be the same as the blocked_id"
     end
