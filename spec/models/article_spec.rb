d2d273904c32a9d7b3e082c486f42e6b0cc093f6
Andy Stabler <andy_60@hotmail.co.uk>
2019-10-30 14:32:41 +0000

    Refactor collection spec (#4597) [ci skip]

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe Article, type: :model do
   it { is_expected.to validate_length_of(:cached_tag_list).is_at_most(126) }
   it { is_expected.to belong_to(:user) }
   it { is_expected.to belong_to(:organization).optional }
-  it { is_expected.to belong_to(:collection).optional }
+  it { is_expected.to belong_to(:collection).optional.touch(true) }
   it { is_expected.to have_many(:comments) }
   it { is_expected.to have_many(:reactions).dependent(:destroy) }
   it { is_expected.to have_many(:notifications).dependent(:delete_all) }
