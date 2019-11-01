9b17d4496a6a450f633079111ffd8cf7cfa3c619
Neto Chaves <netobac1@gmail.com>
2019-11-01 17:53:33 -0300

    refactor colorPreview

--- app/javascript/packs/colorPreview.js
+++ app/javascript/packs/colorPreview.js
@@ -1,52 +1,47 @@
+/* eslint-disable no-param-reassign */
 
-function initPreview() {
-  const colorField = {
-    // input type="color"
-    bgColor: document.getElementById('bg-color-colorfield'),
-    textColor: document.getElementById('text-color-colorfield'),
-  };
-  
-  const textField = {
-    // input type="text"
-    bgColor: document.getElementById('bg-color-textfield'),
-    textColor: document.getElementById('text-color-textfield'),
-  };
-  
-  const preview = document.getElementById('color-select-preview-logo');
-  
-  // Assigns input[type='text'] values to input[type='color']
-  function updateColorFields() {
-    colorField.bgColor.value = textField.bgColor.value;
-    colorField.textColor.value = textField.textColor.value;
-  }
-  
-  // Updating text fields when color fields are changed
-  function updateTextFields() {
-    textField.bgColor.value = colorField.bgColor.value;
-    textField.textColor.value = colorField.textColor.value;
-  }
-  
-  // Updates Preview Colors
-  function updatePreview() {
-    preview.style.backgroundColor = textField.bgColor.value;
-    preview.style.fill = textField.textColor.value;
-  }
-  
-  // Event Watchers
-  // When color fields change -> updateTextField values and refresh preview
-  function watchColorFields() {
-    updateTextFields();
+const preview = document.getElementById('color-select-preview-logo');
+
+const colorField = {
+  // input type="color"
+  bgColor: document.getElementById('bg-color-colorfield'),
+  textColor: document.getElementById('text-color-colorfield'),
+};
+
+const textField = {
+  // input type="text"
+  bgColor: document.getElementById('bg-color-textfield'),
+  textColor: document.getElementById('text-color-textfield'),
+};
+
+// updates field bgColor and textColor to targetField respective fields
+function updateFields(field, targetField) {
+  field.bgColor.value = targetField.bgColor.value;
+  field.textColor.value = targetField.textColor.value;
+}
+
+// Updates Preview Colors
+function updatePreview() {
+  preview.style.backgroundColor = textField.bgColor.value;
+  preview.style.fill = textField.textColor.value;
+}
+
+// Event Watchers
+// When color fields change -> updateTextField values and refresh preview
+function watchColorFields() {
+  updateFields(textField, colorField);
+  updatePreview();
+}
+
+// When text fields change -> updateColorField values and refresh preview
+function watchTextFields(e) {
+  if (e.target.value.match(/#[0-9a-f]{6}/gi)) {
+    updateFields(colorField, textField);
     updatePreview();
   }
-  
-  // When text fields change -> updateColorField values and refresh preview
-  function watchTextFields(e) {
-    if (e.target.value.match(/#[0-9a-f]{6}/gi)) {
-      updateColorFields();
-      updatePreview();
-    }
-  }
-  
+}
+
+function initPreview() {
   if (preview) {
     // Event Listeners
     colorField.bgColor.addEventListener('input', watchColorFields);
@@ -55,7 +50,7 @@ function initPreview() {
     textField.textColor.addEventListener('keyup', watchTextFields);
 
     // on init
-    updateColorFields();
+    updateFields(colorField, textField);
     updatePreview();
     preview.style.display = 'inline-block';
   }
