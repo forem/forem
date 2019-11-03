73a73855b1bf049d33473e3e0018083fa0fd6ec9
ibrahim-13 <ib.arshad777@gmail.com>
2019-11-03 23:55:02 +0600

    Moved following_users_ids to Redis

--- app/models/user.rb
+++ app/models/user.rb
@@ -240,7 +240,7 @@ class User < ApplicationRecord
   end
 
   def cached_following_users_ids
-    Rails.cache.fetch(
+    RedisRailsCache.fetch(
       "user-#{id}-#{updated_at}-#{following_users_count}/following_users_ids",
       expires_in: 120.hours,
     ) do
