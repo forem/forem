94aece3c5ddc9de1cc444521c45589f355dc139c
Francisco Quintero <frajaquico@yahoo.es>
2019-11-06 17:41:05 -0500

    Parse markdown text reddit content for RedditTag

--- app/services/reddit_json_from_url_service.rb
+++ app/services/reddit_json_from_url_service.rb
@@ -1,10 +1,13 @@
 # app/services/reddit_json_from_url_service.rb
 class RedditJsonFromUrlService
+  include ActionView::Helpers::SanitizeHelper
+
   def initialize(url)
     @url = ActionController::Base.helpers.strip_tags(url).strip
   end
 
   def parse
+    # Requests to Reddit require a custom `User-Agent` header to prevent 429 errors
     json = HTTParty.get("#{@url}.json", headers: { "User-Agent" => "ThePracticalDev" })
     # The JSON response is an array with two items.
     # The first one is the post itself, the second one are the comments
@@ -18,8 +21,18 @@ class RedditJsonFromUrlService
       post_hint: data["post_hint"],
       image_url: data["url"],
       thumbnail: data["thumbnail"],
-      selftext: data["selftext"],
+      selftext: parse_markdown_content(data["selftext"]),
       selftext_html: data["selftext_html"]
     }
+  end
+
+  private
+
+  def parse_markdown_content(content)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTMLRouge, REDCARPET_CONFIG)
+    text = markdown.render(content)
+
+    truncated = HTML_Truncator.truncate(text, 60)
+    sanitize(truncated)
   end
 end
