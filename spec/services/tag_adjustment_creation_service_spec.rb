008b9f67cd826531410b3862876ceea6c6fd6f35
spencer page <pagespence@yahoo.com>
2019-11-07 14:57:32 -0800

    added tests to specs

--- spec/services/tag_adjustment_creation_service_spec.rb
+++ spec/services/tag_adjustment_creation_service_spec.rb
@@ -2,13 +2,13 @@ require "rails_helper"
 
 RSpec.describe TagAdjustmentCreationService do
   let(:user) { create(:user) }
-  let(:article) { create(:article) }
+  let(:article) { create(:article, tags: tag.name) }
   let(:tag) { create(:tag) }
 
-  def create_tag_adjustment
+  def create_tag_adjustment(type)
     described_class.new(
       user,
-      adjustment_type: "removal",
+      adjustment_type: type,
       status: "committed",
       tag_name: tag.name,
       article_id: article.id,
@@ -20,19 +20,38 @@ RSpec.describe TagAdjustmentCreationService do
     user.add_role(:tag_moderator, tag)
   end
 
-  it "creates tag adjustment" do
-    tag_adjustment = create_tag_adjustment
+  describe "creates tag adjustment" do
+    it "with adjustment_type removal" do
+      tag_adjustment = create_tag_adjustment("removal")
+      expect(tag_adjustment).to be_valid
+      expect(tag_adjustment.tag_id).to eq(tag.id)
+      expect(tag_adjustment.status).to eq("committed")
+    end
 
-    expect(tag_adjustment).to be_valid
-    expect(tag_adjustment.tag_id).to eq(tag.id)
-    expect(tag_adjustment.status).to eq("committed")
+    it "with adjustment_type addition" do
+      tag_adjustment = create_tag_adjustment("addition")
+
+      expect(tag_adjustment).to be_valid
+      expect(tag_adjustment.tag_id).to eq(tag.id)
+      expect(tag_adjustment.status).to eq("committed")
+    end
   end
 
-  it "creates notification" do
-    perform_enqueued_jobs do
-      tag_adjustment = create_tag_adjustment
-      expect(Notification.last.user_id).to eq(article.user_id)
-      expect(Notification.last.json_data["adjustment_type"]).to eq(tag_adjustment.adjustment_type)
+  describe "creates notification" do
+    it "with adjustment_type removal" do
+      perform_enqueued_jobs do
+        tag_adjustment = create_tag_adjustment("removal")
+        expect(Notification.last.user_id).to eq(article.user_id)
+        expect(Notification.last.json_data["adjustment_type"]).to eq(tag_adjustment.adjustment_type)
+      end
+    end
+
+    it "with adjustment_type addition" do
+      perform_enqueued_jobs do
+        tag_adjustment = create_tag_adjustment("addition")
+        expect(Notification.last.user_id).to eq(article.user_id)
+        expect(Notification.last.json_data["adjustment_type"]).to eq(tag_adjustment.adjustment_type)
+      end
     end
   end
 end
