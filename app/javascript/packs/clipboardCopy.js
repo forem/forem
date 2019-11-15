1493c932f4059f59b94eae8fb386897a80380ad9
Mac Siri <krairit.siri@gmail.com>
2019-11-15 15:34:59 -0500

    Conditionally load clipboard polyfill

--- app/javascript/packs/clipboardCopy.js
+++ app/javascript/packs/clipboardCopy.js
@@ -1,6 +1,18 @@
-import clipboard from 'clipboard-polyfill';
+function loadScript(src, done) {
+  var js = document.createElement('script');
+  js.src = src;
+  js.onload = function() {
+    done();
+  };
+  js.onerror = function() {
+    done(new Error('Failed to load script ' + src));
+  };
+  document.head.appendChild(js);
+}
 
-window.clipboard = clipboard;
+if (!window.clipboard && !window.Clipboard) {
+  loadScript("https://cdnjs.cloudflare.com/ajax/libs/clipboard-polyfill/2.8.6/clipboard-polyfill.js")
+}
 
 window.WebComponents.waitFor(() => {
   import('@github/clipboard-copy-element');
