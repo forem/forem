1b1a564b2b4c0b1aac9ccaae3cb2f7ac750e9c1c
Mac Siri <krairit.siri@gmail.com>
2019-11-18 18:25:35 -0500

    Follow Nick's suggestion

--- app/javascript/packs/clipboardCopy.js
+++ app/javascript/packs/clipboardCopy.js
@@ -1,6 +1,21 @@
-import clipboard from 'clipboard-polyfill';
+HTMLDocument.prototype.ready = new Promise(resolve => {
+  if (document.readyState !== 'loading') {
+    return resolve();
+  }
+  document.addEventListener('DOMContentLoaded', () => resolve());
+  return null;
+});
 
-window.clipboard = clipboard;
+document.ready.then(() => {
+  if (
+    !window.clipboard &&
+    !window.Clipboard &&
+    !navigator.clipboard &&
+    !navigator.Clipboard
+  ) {
+    window.clipboard = import('clipboard-polyfill');
+  }
+});
 
 window.WebComponents.waitFor(() => {
   import('@github/clipboard-copy-element');
