19f8a0d28f660cba8885ca4a936f702222ec2ea7
Mac Siri <krairit.siri@gmail.com>
2019-11-18 18:04:44 -0500

    Revert "Conditionally load clipboard polyfill"
    This reverts commit 1493c932f4059f59b94eae8fb386897a80380ad9.

--- app/javascript/packs/clipboardCopy.js
+++ app/javascript/packs/clipboardCopy.js
@@ -1,18 +1,6 @@
-function loadScript(src, done) {
-  var js = document.createElement('script');
-  js.src = src;
-  js.onload = function() {
-    done();
-  };
-  js.onerror = function() {
-    done(new Error('Failed to load script ' + src));
-  };
-  document.head.appendChild(js);
-}
+import clipboard from 'clipboard-polyfill';
 
-if (!window.clipboard && !window.Clipboard) {
-  loadScript("https://cdnjs.cloudflare.com/ajax/libs/clipboard-polyfill/2.8.6/clipboard-polyfill.js")
-}
+window.clipboard = clipboard;
 
 window.WebComponents.waitFor(() => {
   import('@github/clipboard-copy-element');
