b0eec90b24b6fe969596bacb1e385618c5642c2e
rhymes <rhymesete@gmail.com>
2019-11-20 16:58:11 +0100

    Add additional keys to site configuration (#4861) [deploy]
    * Add rate_limit_follow_count_daily to SiteConfig
    
    * Add favicon_url to SiteConfig
    
    * Add logo_svg to SiteConfig
    
    * Add staff_user_id to SiteConfig
    
    * Add default_site_email to SiteConfig
    
    * Add social_networks_handle to SiteConfig
    
    * Add ga_view_id and ga_fetch_rate to SiteConfig
    
    * Add mailchimp newsletters IDs to SiteConfig
    
    * Add periodic_email_digest_max, periodic_email_digest_min to SiteConfig

--- app/controllers/internal/configs_controller.rb
+++ app/controllers/internal/configs_controller.rb
@@ -1,7 +1,9 @@
 class Internal::ConfigsController < Internal::ApplicationController
   layout "internal"
 
-  def show; end
+  def show
+    @logo_svg = SiteConfig.logo_svg.html_safe # rubocop:disable Rails/OutputSafety
+  end
 
   def create
     config_params.keys.each do |key|
@@ -13,7 +15,15 @@ class Internal::ConfigsController < Internal::ApplicationController
   private
 
   def config_params
-    # put this stuff in the policy or in the model
-    params.require(:site_config).permit(:main_social_image)
+    allowed_params = %i[
+      staff_user_id default_site_email social_networks_handle
+      main_social_image favicon_url logo_svg
+      rate_limit_follow_count_daily
+      ga_view_id ga_fetch_rate
+      mailchimp_newsletter_id mailchimp_sustaining_members_id
+      mailchimp_tag_moderators_id mailchimp_community_moderators_id
+      periodic_email_digest_max periodic_email_digest_min
+    ]
+    params.require(:site_config).permit(allowed_params)
   end
 end
--- spec/requests/internal/configs_spec.rb
+++ spec/requests/internal/configs_spec.rb
@@ -19,11 +19,96 @@ RSpec.describe "/internal/config", type: :request do
       sign_in(admin)
     end
 
-    it "updates main_social_image" do
-      SiteConfig.main_social_image = "https://dummyimage.com/100x100"
-      expected_image_url = "https://dummyimage.com/300x300"
-      post "/internal/config", params: { site_config: { main_social_image: expected_image_url } }
-      expect(SiteConfig.main_social_image).to eq(expected_image_url)
+    describe "staff" do
+      it "updates staff_user_id" do
+        post "/internal/config", params: { site_config: { staff_user_id: 2 } }
+        expect(SiteConfig.staff_user_id).to eq(2)
+      end
+
+      it "updates default_site_email" do
+        expected_email = "foo@bar.com"
+        post "/internal/config", params: { site_config: { default_site_email: expected_email } }
+        expect(SiteConfig.default_site_email).to eq(expected_email)
+      end
+
+      it "updates social_networks_handle" do
+        expected_handle = "tpd"
+        post "/internal/config", params: { site_config: { social_networks_handle: expected_handle } }
+        expect(SiteConfig.social_networks_handle).to eq(expected_handle)
+      end
+    end
+
+    describe "images" do
+      it "updates main_social_image" do
+        expected_image_url = "https://dummyimage.com/300x300"
+        post "/internal/config", params: { site_config: { main_social_image: expected_image_url } }
+        expect(SiteConfig.main_social_image).to eq(expected_image_url)
+      end
+
+      it "updates favicon_url" do
+        expected_image_url = "https://dummyimage.com/300x300"
+        post "/internal/config", params: { site_config: { favicon_url: expected_image_url } }
+        expect(SiteConfig.favicon_url).to eq(expected_image_url)
+      end
+
+      it "updates logo_svg" do
+        expected_image_url = "https://dummyimage.com/300x300"
+        post "/internal/config", params: { site_config: { logo_svg: expected_image_url } }
+        expect(SiteConfig.logo_svg).to eq(expected_image_url)
+      end
+    end
+
+    describe "rate limits" do
+      it "updates rate_limit_follow_count_daily" do
+        post "/internal/config", params: { site_config: { rate_limit_follow_count_daily: 3 } }
+        expect(SiteConfig.rate_limit_follow_count_daily).to eq(3)
+      end
+    end
+
+    describe "Google Analytics Reporting API v4" do
+      it "updates ga_view_id" do
+        post "/internal/config", params: { site_config: { ga_view_id: "abc" } }
+        expect(SiteConfig.ga_view_id).to eq("abc")
+      end
+
+      it "updates ga_fetch_rate" do
+        post "/internal/config", params: { site_config: { ga_fetch_rate: 3 } }
+        expect(SiteConfig.ga_fetch_rate).to eq(3)
+      end
+    end
+
+    describe "Mailchimp lists IDs" do
+      it "updates mailchimp_newsletter_id" do
+        post "/internal/config", params: { site_config: { mailchimp_newsletter_id: "abc" } }
+        expect(SiteConfig.mailchimp_newsletter_id).to eq("abc")
+      end
+
+      it "updates mailchimp_sustaining_members_id" do
+        post "/internal/config", params: { site_config: { mailchimp_sustaining_members_id: "abc" } }
+        expect(SiteConfig.mailchimp_sustaining_members_id).to eq("abc")
+      end
+
+      it "updates mailchimp_tag_moderators_id" do
+        post "/internal/config", params: { site_config: { mailchimp_tag_moderators_id: "abc" } }
+        expect(SiteConfig.mailchimp_tag_moderators_id).to eq("abc")
+      end
+
+      it "updates mailchimp_community_moderators_id" do
+        post "/internal/config", params: { site_config: { mailchimp_community_moderators_id: "abc" } }
+        expect(SiteConfig.mailchimp_community_moderators_id).to eq("abc")
+      end
+    end
+
+    describe "Email digest frequency" do
+      it "updates periodic_email_digest_max" do
+        post "/internal/config", params: { site_config: { periodic_email_digest_max: 1 } }
+        expect(SiteConfig.periodic_email_digest_max).to eq(1)
+      end
+
+      it "updates periodic_email_digest_min" do
+        post "/internal/config", params: { site_config: { periodic_email_digest_min: 3 } }
+        expect(SiteConfig.periodic_email_digest_min).to eq(3)
+      end
     end
   end
 end
