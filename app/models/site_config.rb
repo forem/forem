b360b5cbbfa15742f699c4074677f6a19279c860
rhymes <rhymesete@gmail.com>
2019-11-20 16:58:11 +0100

    Add additional keys to site configuration (#4861) [deploy]
    * Add rate_limit_follow_count_daily to SiteConfig
    
    * Add favicon_url to SiteConfig
    
    * Add logo_svg to SiteConfig
    
    * Add staff_user_id to SiteConfig
    
    * Add default_site_email to SiteConfig
    
    * Add social_networks_handle to SiteConfig
    
    * Add ga_view_id and ga_fetch_rate to SiteConfig
    
    * Add mailchimp newsletters IDs to SiteConfig
    
    * Add periodic_email_digest_max, periodic_email_digest_min to SiteConfig

--- app/models/site_config.rb
+++ app/models/site_config.rb
@@ -8,5 +8,32 @@ class SiteConfig < RailsSettings::Base
   # the cache, or call SiteConfig.clear_cache
   cache_prefix { "v1" }
 
-  field :main_social_image, default: "https://thepracticaldev.s3.amazonaws.com/i/6hqmcjaxbgbon8ydw93z.png"
+  # staff account
+  field :staff_user_id, type: :integer, default: 1 # will replace DEVTO_USER_ID
+  field :default_site_email, type: :string, default: "yo@dev.to"
+  field :social_networks_handle, type: :string, default: "thepracticaldev" # will replace SITE_TWITTER_HANDLE
+
+  # images
+  field :main_social_image, type: :string, default: "https://thepracticaldev.s3.amazonaws.com/i/6hqmcjaxbgbon8ydw93z.png"
+  field :favicon_url, type: :string, default: "favicon.ico"
+  field :logo_svg, type: :string, default: ""
+
+  # rate limits
+  field :rate_limit_follow_count_daily, type: :integer, default: 500
+
+  # Google Analytics Reporting API v4
+  # <https://developers.google.com/analytics/devguides/reporting/core/v4>
+  field :ga_view_id, type: :string, default: ""
+  field :ga_fetch_rate, type: :integer, default: 25
+
+  # Mailchimp lists IDs
+  # <https://mailchimp.com/developer/>
+  field :mailchimp_newsletter_id, type: :string, default: ""
+  field :mailchimp_sustaining_members_id, type: :string, default: ""
+  field :mailchimp_tag_moderators_id, type: :string, default: ""
+  field :mailchimp_community_moderators_id, type: :string, default: ""
+
+  # Email digest frequency
+  field :periodic_email_digest_max, type: :integer, default: 0
+  field :periodic_email_digest_min, type: :integer, default: 2
 end
