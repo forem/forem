9f0b7ccfda02da1018cb8a55ac2d18a69a80e06d
Ben Halpern <ben@dev.to>
2019-11-20 16:46:10 -0500

    Removed missing attribue code

--- app/jobs/comments/touch_user_job.rb
+++ app/jobs/comments/touch_user_job.rb
@@ -6,10 +6,6 @@ module Comments
       comment = Comment.find_by(id: comment_id)
       user = comment&.user
       user&.touch(:updated_at, :last_comment_at)
-      user&.update_columns(
-        trailing_7_day_comments_count: user&.comments&.where("created_at > ?", 7.days.ago)&.size || 0,
-        trailing_28_day_comments_count: user&.comments&.where("created_at > ?", 28.days.ago)&.size || 0,
-      )
     end
   end
 end
