deadf3097058197a28fa379ef45c77f39c09296b
Ben Halpern <ben@dev.to>
2019-11-22 08:43:52 -0500

    Fix some safe navigation logic

--- app/jobs/comments/calculate_score_job.rb
+++ app/jobs/comments/calculate_score_job.rb
@@ -4,9 +4,10 @@ module Comments
 
     def perform(comment_id)
       comment = Comment.find_by(id: comment_id)
+      return unless comment
 
-      comment&.update_columns(score: BlackBox.comment_quality_score(comment), spaminess_rating: BlackBox.calculate_spaminess(comment))
-      comment&.user&.calculate_score
+      comment.update_columns(score: BlackBox.comment_quality_score(comment), spaminess_rating: BlackBox.calculate_spaminess(comment))
+      comment.user.calculate_score
       comment.root.save if comment && !comment.is_root?
     end
   end
