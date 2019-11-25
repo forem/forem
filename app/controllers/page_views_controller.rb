603aed0a216cc0dcc8ba7d5840160f74ebf64f5d
Ben Halpern <ben@dev.to>
2019-11-25 09:22:08 -0500

    Remove extra dependencies for PageViewsController

--- app/controllers/page_views_controller.rb
+++ app/controllers/page_views_controller.rb
@@ -1,5 +1,8 @@
-class PageViewsController < ApplicationController
-  # No policy needed. All views are for all users
+class PageViewsController < ActionController::Metal
+  # ActionController::Metal because we do not need all bells and whistles of ApplicationController
+  include ActionController::StrongParameters
+  include ActionController::Head
+
   def create
     page_view_create_params = if session_current_user_id
                                 page_view_params.merge(user_id: session_current_user_id)
@@ -28,7 +31,7 @@ class PageViewsController < ApplicationController
   private
 
   def update_article_page_views
-    return if Rails.env.production? && rand(8) != 1 # We don't need to update the article page views every time.
+    return if Rails.env.production? && rand(10) != 1 # We don't need to update the article page views every time.
 
     @article = Article.find(page_view_params[:article_id])
     new_page_views_count = @article.page_views.sum(:counts_for_number_of_views)
@@ -42,7 +45,7 @@ class PageViewsController < ApplicationController
   end
 
   def update_organic_page_views
-    return if Rails.env.production? && rand(20) != 1 # We need to do this operation only once in a while.
+    return if Rails.env.production? && rand(50) != 1 # We need to do this operation only once in a while.
 
     page_views_from_google_com = @article.page_views.where(referrer: "https://www.google.com/")
 
@@ -56,5 +59,10 @@ class PageViewsController < ApplicationController
     organic_count_past_month_count = page_views_from_google_com.
       where("created_at > ?", 1.month.ago).sum(:counts_for_number_of_views)
     @article.update_column(:organic_page_views_past_month_count, organic_count_past_month_count) if organic_count_past_month_count > @article.organic_page_views_past_month_count
+  end
+
+  def session_current_user_id
+    # custom defined here, because controller does not inherent from application_controller
+    session["warden.user.user.key"].flatten[0] if session["warden.user.user.key"].present?
   end
 end
