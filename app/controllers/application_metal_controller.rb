e653ac1a544d94e97b1f925c676bb871deb99ae2
Ben Halpern <ben@dev.to>
2019-11-26 13:28:59 -0500

    Add request forgery protection to metal

--- app/controllers/application_metal_controller.rb
+++ app/controllers/application_metal_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationMetalController < ActionController::Metal
   # Any shared behavior across metal-oriented controllers can go here.
+
+  # These are basic things we likely want for any metal controllers
+  include ActionController::Cookies
+  include ActionController::RequestForgeryProtection
+  # ActionController modules which may not be used in each controller can go in
+  # the specific controller.
+
+  protect_from_forgery with: :exception, prepend: true
+
   include SessionCurrentUser
+  include ValidRequest
 end
