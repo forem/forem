f12bbbf0d784b217150d75e191e5e30b52781a9c
lightalloy <lightallloy@gmail.com>
2019-11-27 14:36:27 +0300

    Remove blank like from schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_11_06_102826) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
