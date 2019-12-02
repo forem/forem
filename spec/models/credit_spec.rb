de602bf600ab7b6daa9fe03ad45ef2e64a4cc8e6
rhymes <rhymesete@gmail.com>
2019-12-02 12:04:35 +0100

    Optimize and refactor credit spec

--- spec/models/credit_spec.rb
+++ spec/models/credit_spec.rb
@@ -1,58 +1,63 @@
 require "rails_helper"
 
 RSpec.describe Credit, type: :model do
-  let(:user) { create(:user) }
-  let(:organization) { create(:organization) }
-  let(:random_number) { rand(100) }
+  let_it_be(:user) { create(:user) }
+  let_it_be(:organization) { create(:organization) }
 
   it { is_expected.to belong_to(:user).optional }
   it { is_expected.to belong_to(:organization).optional }
   it { is_expected.to belong_to(:purchase).optional }
 
-  xit "counts credits for user" do
-    # See https://github.com/magnusvk/counter_culture/issues/259
+  context "when caching counters" do
+    let_it_be(:user_credits) { create_list(:credit, 2, user: user) }
+    let_it_be(:org_credits) { create_list(:credit, 1, organization: organization) }
 
-    create_list(:credit, random_number, user: user)
-    described_class.counter_culture_fix_counts
-    expect(user.reload.credits_count).to eq(random_number)
-  end
+    describe "#credits_count" do
+      it "counts credits for user" do
+        # See https://github.com/magnusvk/counter_culture/issues/259
+        described_class.counter_culture_fix_counts
+        expect(user.reload.credits_count).to eq(user.credits.size)
+      end
 
-  it "counts credits for organization" do
-    create_list(:credit, random_number, organization: organization)
-    described_class.counter_culture_fix_counts
-    expect(organization.reload.credits_count).to eq(random_number)
-  end
+      it "counts credits for organization" do
+        described_class.counter_culture_fix_counts
+        expect(organization.reload.credits_count).to eq(organization.credits.size)
+      end
+    end
 
-  it "counts the number of unspent credits for a user" do
-    create_list(:credit, random_number, user: user)
-    expect(user.reload.unspent_credits_count).to eq(random_number)
-  end
+    describe "#unspent_credits_count" do
+      it "counts the number of unspent credits for a user" do
+        expect(user.reload.unspent_credits_count).to eq(user.credits.unspent.size)
+      end
 
-  it "counts the number of spent credits for a user" do
-    create_list(:credit, random_number, user: user, spent: true)
-    expect(user.reload.spent_credits_count).to eq(random_number)
-  end
+      it "counts the number of unspent credits for an organization" do
+        expect(organization.reload.unspent_credits_count).to eq(organization.credits.unspent.size)
+      end
+    end
 
-  it "counts the number of unspent credits for an organization" do
-    create_list(:credit, random_number, organization: organization)
-    expect(organization.reload.unspent_credits_count).to eq(random_number)
-  end
+    describe "#spent_credits_count" do
+      it "counts the number of spent credits for a user" do
+        create_list(:credit, 1, user: user, spent: true)
+        expect(user.reload.spent_credits_count).to eq(user.credits.spent.size)
+      end
 
-  it "counts the number of spent credits for an organization" do
-    create_list(:credit, random_number, organization: organization, spent: true)
-    expect(organization.reload.spent_credits_count).to eq(random_number)
+      it "counts the number of spent credits for an organization" do
+        create_list(:credit, 1, organization: organization, spent: true)
+        expect(organization.reload.spent_credits_count).to eq(organization.credits.spent.size)
+      end
+    end
   end
 
   describe "#purchase" do
-    let(:listing) { create(:classified_listing) }
+    let_it_be(:credit) { build(:credit) }
 
-    it "associates to a purchase" do
-      credit = create(:credit, purchase: listing)
-      expect(credit.purchase).to eq(listing)
+    it "is valid with a purchase" do
+      credit.purchase = build(:classified_listing)
+      expect(credit).to be_valid
     end
 
     it "is valid without a purchase" do
-      credit = create(:credit, purchase: nil)
+      credit.purchase = nil
       expect(credit).to be_valid
     end
   end
