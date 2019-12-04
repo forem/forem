fd818b9442cbe8bf11b56f7b6a1dea43fb31b7f4
mstruve <mollylbs@gmail.com>
2019-12-04 13:42:59 -0600

    fix typo, consolidate to single config file, remove after_deploy.sh script

--- after_deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-REPOSITORY=https://github.com/thepracticaldev/dev.to_private/
-REVISION="$(git rev-parse HEAD)"
-COMMIT_AUTHOR=$(git --no-pager log -1 --pretty=format:'%an')
-PAYLOAD='{ "environment":"production", "repository":"'${REPOSITORY}'", "revision":"'${REVISION}'", "username":"'${COMMIT_AUTHOR}'"}'
--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -1,5 +1,7 @@
 Honeybadger.configure do |config|
   # Can be used to implement more programatic error handling
   # https://docs.honeybadger.io/lib/ruby/getting-started/ignoring-errors.html#ignore-programmatically
-  config.api_key = ENV.fetch("HONEYBADGER_API_KEY", "api_key")
+  config.api_key = ApplicationConfig["HONEYBADGER_API_KEY"]
+  config.exceptions.ignore += [Pundit::NotAuthorizedError, ActiveRecord::RecordNotFound]
+  config.request.filter_keys += %w[authorization]
 end
