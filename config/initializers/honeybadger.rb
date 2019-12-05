db673530ac56e2262e692e32e9e49152f95ffe03
Molly Struve <mollylbs@gmail.com>
2019-12-05 16:38:35 -0600

    Group SIGTERM fetch_all_rss rake task Honeybadgers together (#5021) [deploy]

--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -1,8 +1,15 @@
+# Can be used to implement more programatic error handling
+# https://docs.honeybadger.io/lib/ruby/getting-started/ignoring-errors.html#ignore-programmatically
+
 Honeybadger.configure do |config|
-  # Can be used to implement more programatic error handling
-  # https://docs.honeybadger.io/lib/ruby/getting-started/ignoring-errors.html#ignore-programmatically
   config.api_key = ApplicationConfig["HONEYBADGER_API_KEY"]
   config.revision = ApplicationConfig["HEROKU_SLUG_COMMIT"]
   config.exceptions.ignore += [Pundit::NotAuthorizedError, ActiveRecord::RecordNotFound]
   config.request.filter_keys += %w[authorization]
+
+  config.before_notify do |notice|
+    if notice.error_message&.include?("SIGTERM") && notice.component&.include?("fetch_all_rss")
+      notice.fingerprint = notice.error_message
+    end
+  end
 end
