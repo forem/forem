63286df25c52a59c50464ba69179bf7f4e6ee3ed
Zhao-Andy <andyzhao.zhao@gmail.com>
2019-12-06 14:20:33 -0500

    Add tests for comment hiding visibility

--- spec/requests/shared_examples/comment_hide_or_unhide_request.rb
+++ spec/requests/shared_examples/comment_hide_or_unhide_request.rb
@@ -1,10 +1,10 @@
-RSpec.shared_examples "PATCH /comments/:comment_id/hide or unhide" do |argument_hash|
+RSpec.shared_examples "PATCH /comments/:comment_id/hide or unhide" do |args|
   let(:commentable_author) { create(:user) }
   let(:article) { create(:article, user: commentable_author) }
-  let(:commentable_comment) { create(:comment, commentable: article, user: commentable_author) }
+  let(:parent_comment) { create(:comment, commentable: article, user: commentable_author) }
 
   it "returns 401 if user is not logged in" do
-    patch "/comments/1/#{argument_hash[:path]}", headers: { HTTP_ACCEPT: "application/json" }
+    patch "/comments/1/#{args[:path]}", headers: { HTTP_ACCEPT: "application/json" }
     expect(response).to have_http_status(:unauthorized)
   end
 
@@ -13,7 +13,7 @@ RSpec.shared_examples "PATCH /comments/:comment_id/hide or unhide" do |argument_
 
     it "returns unauthorized" do
       expect do
-        patch "/comments/#{commentable_comment.id}/#{argument_hash[:path]}", headers: { HTTP_ACCEPT: "application/json" }
+        patch "/comments/#{parent_comment.id}/#{args[:path]}", headers: { HTTP_ACCEPT: "application/json" }
       end.to raise_error(Pundit::NotAuthorizedError)
     end
   end
@@ -21,11 +21,11 @@ RSpec.shared_examples "PATCH /comments/:comment_id/hide or unhide" do |argument_
   context "when logged in as the commentable author" do
     before do
       sign_in commentable_author
-      patch "/comments/#{commentable_comment.id}/#{argument_hash[:path]}", headers: { HTTP_ACCEPT: "application/json" }
+      patch "/comments/#{parent_comment.id}/#{args[:path]}", headers: { HTTP_ACCEPT: "application/json" }
     end
 
     it "returns a proper JSON response" do
-      expect(JSON.parse(response.body)).to eq("hidden" => argument_hash[:hidden])
+      expect(JSON.parse(response.body)).to eq("hidden" => args[:hidden])
     end
 
     it "returns 200 on a good request" do
