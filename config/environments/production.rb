6243d497d4d519b728169b12992c26fcc672528a
Molly Struve <mollylbs@gmail.com>
2019-12-09 11:37:53 -0600

    Remove memcache dalli gem and setup code (#5057) [deploy]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,19 +99,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # TODO: Remove after cache store flip
-  if ENV["USE_REDIS"]
-    DEFAULT_EXPIRATION = 24.hours.to_i.freeze
-    config.cache_store = :redis_store, ENV["REDIS_URL"], { expires_in: DEFAULT_EXPIRATION }
-  else
-    config.cache_store = :dalli_store,
-                         (ENV["MEMCACHIER_SERVERS"] || "").split(","),
-                         { username: ENV["MEMCACHIER_USERNAME"],
-                           password: ENV["MEMCACHIER_PASSWORD"],
-                           failover: true,
-                           socket_timeout: 1.5,
-                           socket_failure_delay: 0.2 }
-  end
+  DEFAULT_EXPIRATION = 24.hours.to_i.freeze
+  config.cache_store = :redis_store, ENV["REDIS_URL"], { expires_in: DEFAULT_EXPIRATION }
 
   config.app_domain = ENV["APP_DOMAIN"]
 
