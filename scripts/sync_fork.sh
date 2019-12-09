4d962fdecf4831a380555b2ba05cf630dedd4e2e
Robin Kretzschmar <robin.kretzschmar@yahoo.de>
2019-12-09 14:20:29 +0000

    Add script to sync fork with latest changes from upstream

---
+++ scripts/sync_fork.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# sync this fork with upstream to get the latest changes
+git remote add upstream https://github.com/thepracticaldev/dev.to
+git fetch upstream
+git checkout master
+git merge upstream/master
+
