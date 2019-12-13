9912bbcdb88a29bcf5baedd9b98b2151050e4d39
Molly Struve <mollylbs@gmail.com>
2019-12-13 14:07:33 -0600

    Asynchronously Update Algolia after a PageView Update (#5076) [deploy]

--- app/jobs/search/index_job.rb
+++ app/jobs/search/index_job.rb
@@ -3,7 +3,7 @@ module Search
     queue_as :search_index
 
     def perform(record_type, record_id)
-      raise InvalidRecordType unless %w[Comment Article User].include?(record_type)
+      raise InvalidRecordType unless %w[Comment Article User PageView].include?(record_type)
 
       record = record_type.constantize.find_by(id: record_id)
       return unless record
