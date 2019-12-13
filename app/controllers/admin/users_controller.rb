efb3094991133f4cb0c85e1b7c6fe5c1804338ac
Lucas Hiago <lucashiago63@gmail.com>
2019-12-13 17:10:33 -0300

    Refactor method in users_controller.rb

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,45 @@
 module Admin
   class UsersController < Admin::ApplicationController
+    USER_PARAMS = %i[
+      name
+      email
+      username
+      twitter_username
+      github_username
+      profile_image
+      website_url
+      summary
+      email_newsletter
+      email_comment_notifications
+      email_follower_notifications
+      organization_id
+      org_admin
+      bg_color_hex
+      text_color_hex
+      employer_name
+      employer_url
+      employment_title
+      currently_learning
+      available_for
+      mostly_work_with
+      currently_hacking_on
+      location
+      email_public
+      education
+      feed_url
+      reputation_modifier
+      saw_onboarding
+      scholar_email
+      facebook_url
+      behance_url
+      dribbble_url
+      medium_url
+      gitlab_url
+      linkedin_url
+      twitch_url
+      instagram_url
+    ].freeze
+
     def update
       user = User.find(params[:id])
       if user.errors.messages.blank? && user.update(user_params)
@@ -13,45 +53,7 @@ module Admin
     private
 
     def user_params
-      accessible = %i[
-        name
-        email
-        username
-        twitter_username
-        github_username
-        profile_image
-        website_url
-        summary
-        email_newsletter
-        email_comment_notifications
-        email_follower_notifications
-        organization_id
-        org_admin
-        bg_color_hex
-        text_color_hex
-        employer_name
-        employer_url
-        employment_title
-        currently_learning
-        available_for
-        mostly_work_with
-        currently_hacking_on
-        location
-        email_public
-        education
-        feed_url
-        reputation_modifier
-        saw_onboarding
-        scholar_email
-        facebook_url
-        behance_url
-        dribbble_url
-        medium_url
-        gitlab_url
-        linkedin_url
-        twitch_url
-        instagram_url
-      ]
+      accessible = USER_PARAMS
       accessible << %i[password password_confirmation] if params[:user][:password].present?
       verify_usernames params.require(:user).permit(accessible)
     end
