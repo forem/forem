a5dafd4dcdd255d7e38c4e0063c07c34e34d8fac
Keith Bennett <keithrbennett@gmail.com>
2019-12-17 00:13:15 +0700

    Fix rubocop Rails.root.path complaints (#5114)

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module PracticalDeveloper
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.load_path += Dir[Rails.root.join("my/locales/*.{rb,yml}").to_s]
     # config.i18n.default_locale = :de
 
     config.autoload_paths += Dir["#{config.root}/app/labor/"]
