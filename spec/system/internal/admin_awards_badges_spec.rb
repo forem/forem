40d5be75ef7f5af2657787005dfe0e7821dcdb92
jacobherrington <jacobherringtondeveloper@gmail.com>
2019-12-18 10:02:32 -0600

    Test Badge notifications

--- spec/system/internal/admin_awards_badges_spec.rb
+++ spec/system/internal/admin_awards_badges_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe "Admin awards badges", type: :system do
   let(:admin) { create(:user, :super_admin) }
   let(:user) { create(:user) }
   let(:user2) { create(:user) }
-  let(:badges) { Badge.all.map { |b| CGI.escapeHTML(b.title) } }
+  let(:badges) { Badge.pluck(:title) }
 
-  def submit_form
+  def award_two_badges
     find(:xpath, "//option[contains(text(), \"#{badges.last}\")]").select_option
     fill_in "usernames", with: "#{user.username}, #{user2.username}"
     fill_in "message_markdown", with: "He who controls the spice controls the universe."
@@ -30,13 +30,19 @@ RSpec.describe "Admin awards badges", type: :system do
   end
 
   it "awards badges" do
-    expect { submit_form }.to change { user.badges.count }.by(1).
-      and change { user2.badges.count }.by(1).
-      and change(enqueued_jobs, :size).by_at_least(1)
+    expect { award_two_badges }.to change { user.badges.count }.by(1).
+      and change { user2.badges.count }.by(1)
     expect(page).to have_content("BadgeRewarder task ran!")
 
     visit "/#{user.username}/"
 
     expect(page).to have_link(href: "/badge/#{Badge.last.slug}")
+  end
+
+  it "notifies users of new badges" do
+    expect { award_two_badges }.to enqueue_job(Notifications::NewBadgeAchievementJob).
+      exactly(2).times.
+      and enqueue_job(BadgeAchievements::SendEmailNotificationJob).
+      exactly(2).times
   end
 end
