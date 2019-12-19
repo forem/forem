fe74164506bf1004092db0cac1987607de19c0f2
Lucas Hiago <lucashiago63@gmail.com>
2019-12-18 21:28:20 -0300

    Create methods to return the params

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,45 +1,5 @@
 module Admin
   class UsersController < Admin::ApplicationController
-    USER_PARAMS = %i[
-      name
-      email
-      username
-      twitter_username
-      github_username
-      profile_image
-      website_url
-      summary
-      email_newsletter
-      email_comment_notifications
-      email_follower_notifications
-      organization_id
-      org_admin
-      bg_color_hex
-      text_color_hex
-      employer_name
-      employer_url
-      employment_title
-      currently_learning
-      available_for
-      mostly_work_with
-      currently_hacking_on
-      location
-      email_public
-      education
-      feed_url
-      reputation_modifier
-      saw_onboarding
-      scholar_email
-      facebook_url
-      behance_url
-      dribbble_url
-      medium_url
-      gitlab_url
-      linkedin_url
-      twitch_url
-      instagram_url
-    ].freeze
-
     def update
       user = User.find(params[:id])
       if user.errors.messages.blank? && user.update(user_params)
@@ -53,7 +13,7 @@ module Admin
     private
 
     def user_params
-      accessible = USER_PARAMS
+      accessible = accessible_params
       accessible << %i[password password_confirmation] if params[:user][:password].present?
       verify_usernames params.require(:user).permit(accessible)
     end
@@ -63,6 +23,62 @@ module Admin
       user_params[:twitter_username] = nil if user_params[:twitter_username] == ""
       user_params[:github_username] = nil if user_params[:github_username] == ""
       user_params
+    end
+
+    def accessible_params
+      attributes_params | url_params | other_params
+    end
+
+    def attributes_params
+      %i[
+        name
+        email
+        username
+        twitter_username
+        github_username
+        profile_image
+        employment_title
+        currently_learning
+        available_for
+        mostly_work_with
+        currently_hacking_on
+        location
+        email_public
+        education
+      ]
+    end
+
+    def url_params
+      %i[
+        facebook_url
+        behance_url
+        dribbble_url
+        medium_url
+        gitlab_url
+        linkedin_url
+        twitch_url
+        instagram_url
+        website_url
+        employer_url
+        feed_url
+      ]
+    end
+
+    def other_params
+      %i[
+        email_newsletter
+        email_comment_notifications
+        email_follower_notifications
+        summary
+        organization_id
+        org_admin
+        bg_color_hex
+        text_color_hex
+        employer_name
+        reputation_modifier
+        saw_onboarding
+        scholar_email
+      ]
     end
   end
 end
