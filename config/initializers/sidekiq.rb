11689929eff4f735300aa9033df2e778f86b7496
rhymes <rhymesete@gmail.com>
2019-12-27 19:04:50 +0100

    Disable Sidekiq web sessions to make it work with Redis sessions (#5266) [deploy]

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,8 @@
+require "sidekiq/web"
+# Sidekiq sessions don't play well with Redis based sessions and Devise
+# <https://github.com/mperham/sidekiq/wiki/Monitoring#sessions-being-lost>
+Sidekiq::Web.set :sessions, false
+
 Sidekiq.configure_server do |config|
   sidekiq_url = ApplicationConfig["REDIS_SIDEKIQ_URL"] || ApplicationConfig["REDIS_URL"]
   config.redis = { url: sidekiq_url }
