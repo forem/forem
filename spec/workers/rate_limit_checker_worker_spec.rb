0dfeb1e7d76253358620a201d72fbf92322e6172
mstruve <mollylbs@gmail.com>
2019-12-27 14:27:54 -0600

    Change RateLimitCheckerJob to RateLimitCheckerWorker and move to sidekiq

--- spec/workers/rate_limit_checker_worker_spec.rb
+++ spec/workers/rate_limit_checker_worker_spec.rb
@@ -1,23 +1,16 @@
 require "rails_helper"
 
-RSpec.describe RateLimitCheckerJob, type: :job do
-  include_examples "#enqueues_job", "rate_limit_checker", 2
-
-  describe "#perform_later" do
+RSpec.describe RateLimitCheckerWorker, type: :worker do
+  describe "#perform" do
     let(:user) { create(:user) }
     let(:service) { PingAdmins }
+    let(:worker) { subject }
 
     before { allow(service).to receive(:call) }
 
-    it "enqueues the job" do
-      expect do
-        described_class.perform_later(user.id)
-      end.to have_enqueued_job.with(user.id).on_queue("rate_limit_checker")
-    end
-
     it "calls a service" do
       perform_enqueued_jobs do
-        described_class.perform_now(user.id, "test")
+        worker.perform(user.id, "test")
 
         expect(service).to have_received(:call).with(user, "test").once
       end
@@ -25,7 +18,7 @@ RSpec.describe RateLimitCheckerJob, type: :job do
 
     it "does nothing for non-existent user" do
       perform_enqueued_jobs do
-        described_class.perform_now(nil, "test")
+        worker.perform(nil, "test")
 
         expect(service).not_to have_received(:call)
       end
