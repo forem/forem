297acdfff2f52138df29c41c5d262859146d3d0d
rhymes <rhymesete@gmail.com>
2019-12-30 15:25:53 +0100

    Remove unused double

--- spec/jobs/articles/detect_human_language_job_spec.rb
+++ spec/jobs/articles/detect_human_language_job_spec.rb
@@ -8,10 +8,8 @@ RSpec.describe Articles::DetectHumanLanguageJob, type: :job do
       let_it_be(:article) { create(:article) }
 
       it "updates article language with detected language" do
-        detector = double
-        allow(detector).to receive(:detect).and_return("en")
-
         described_class.perform_now(article.id)
+
         expect(article.language).to eql("en")
       end
     end
