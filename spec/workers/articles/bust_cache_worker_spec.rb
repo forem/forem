1943e1da7965e44a96e9c94cfaa9da41c8986b42
mstruve <mollylbs@gmail.com>
2019-12-30 12:40:04 -0600

    spec improvements

--- spec/workers/articles/bust_cache_worker_spec.rb
+++ spec/workers/articles/bust_cache_worker_spec.rb
@@ -1,43 +1,43 @@
 require "rails_helper"
 
+class NewBuster
+  def self.bust_article(*); end
+end
+
 RSpec.describe Articles::BustCacheWorker, type: :worker do
   describe "#perform" do
-    let(:article) { FactoryBot.create(:article) }
-    let(:cache_buster) { double }
     let(:worker) { subject }
-    let(:buster) { "CacheBuster" }
 
-    before do
-      allow(buster).to receive(:constantize).and_return(cache_buster)
-      allow(cache_buster).to receive(:bust_article)
-    end
+    context "with article" do
+      let(:article) { double }
+      let(:article_id) { 1 }
 
-    context "with cache buster defined" do
-      it "busts cache with defined buster" do
-        new_buster = "NewBuster"
-        allow(new_buster).to receive(:constantize).and_return(cache_buster)
-        allow(cache_buster).to receive(:bust_article)
-        worker.perform(article.id, new_buster)
-        expect(cache_buster).to have_received(:bust_article).with(article)
+      before do
+        allow(Article).to receive(:find_by).with(id: article_id).and_return(article)
       end
-    end
 
-    context "without cache buster defined" do
-      it "busts cache with default" do
+      it "with cache buster defined busts cache with defined buster" do
+        allow(NewBuster).to receive(:bust_article)
+        worker.perform(article_id, "NewBuster")
+        expect(NewBuster).to have_received(:bust_article).with(article)
+      end
+
+      it "without cache buster defined busts cache with default" do
         allow(CacheBuster).to receive(:bust_article)
-        worker.perform(article.id)
+        worker.perform(article_id)
         expect(CacheBuster).to have_received(:bust_article).with(article)
       end
     end
 
     context "without article" do
       it "does not error" do
-        expect { worker.perform(nil, buster) }.not_to raise_error
+        expect { worker.perform(nil, "CacheBuster") }.not_to raise_error
       end
 
       it "does not bust cache" do
+        allow(CacheBuster).to receive(:bust_article)
         worker.perform(nil)
-        expect(cache_buster).not_to have_received(:bust_article)
+        expect(CacheBuster).not_to have_received(:bust_article)
       end
     end
   end
