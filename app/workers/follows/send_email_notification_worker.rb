087e97c4aca9fef6348bbb98f066c801c046c9fe
swiknaba <swiknaba@users.noreply.github.com>
2020-01-01 13:56:43 +0100

    remove trailing ::

--- app/workers/follows/send_email_notification_worker.rb
+++ app/workers/follows/send_email_notification_worker.rb
@@ -4,7 +4,7 @@ module Follows
     sidekiq_options queue: :mailers, retry: 10
 
     def perform(follow_id, mailer = NotifyMailer.name)
-      follow = ::Follow.find_by(id: follow_id, followable_type: "User")
+      follow = Follow.find_by(id: follow_id, followable_type: "User")
       return unless follow&.followable.present? && follow.followable.receives_follower_notifications?
 
       return if EmailMessage.where(user_id: follow.followable_id).
