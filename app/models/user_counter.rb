197fabe35abe6dacec728ceaf9f02afc249a5773
rhymes <rhymesete@gmail.com>
2020-01-06 10:31:21 +0100

    Add validators and specs

--- app/models/user_counter.rb
+++ app/models/user_counter.rb
@@ -2,6 +2,8 @@ class UserCounter < ApplicationRecord
   belongs_to :user
 
   serialize :data, HashSerializer
-
   store_accessor :data, :comments_7_days
+
+  validates :user, presence: true, uniqueness: true
+  validates :comments_7_days, numericality: { only_integer: true }
 end
