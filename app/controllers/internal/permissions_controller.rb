8698fed939e38fee05d12866131b2a0c85c61dca
Jacob Herrington <jacobherringtondeveloper@gmail.com>
2020-01-06 06:58:03 -0600

    Refactor/internal ui update (#5339) [deploy]
    * Move internal styles out of layout
    
    * Upgrade to Bootstrap 4.4 and redo nav
    
    * Redesign /internal/users/index
    
    * Refactor /internal/tags/show
    
    * Refactor /internal/broadcasts/index
    
    * Refactor /internal/broadcasts/index
    
    * Refactor /internal/pages/index
    
    * Refactor /internal/mods/index
    
    * Fix internal nav highlighting
    
    * Refactor internal/organizations/index
    
    * Refactor internal/growth
    
    * Refactor internal/badges
    
    * Refactor /internal/podcasts/
    
    * Refactor internal listings filter
    
    * Refactor /internal/endpoints/
    
    * Refactor /internal/tools/
    
    * Refactor /internal/chat_channels/
    
    * Refactor /internal/config/
    
    * Refactor /internal/feedback_messages
    
    This is the one that worries me the most. I made a strong effort not to
    touch any IDs and the JavaScript appears to be working after manual
    testing.
    
    This view probably needs some systems specs covering it.
    
    * Remove unused CSS
    
    * Refactor feedback_messages/show
    
    * Refactor internal/comments
    
    * Refactor internal/podcasts/edit
    
    * Refactor internal/users/show
    
    * Refactor internal/users/edit
    
    * Refactor internal/organizations/show
    
    * Refactor internal/permissions
    
    * Refactor internal/tags/show
    
    * Refactor internal/pages/form
    
    * Add events to navbar
    
    * Refactor internal/events
    
    * Refactor internal/brodcasts
    
    * Refactor internal/articles
    
    * Refactor internal/listings
    
    * Remove unused CSS
    
    * Load jquery earlier

--- app/controllers/internal/permissions_controller.rb
+++ app/controllers/internal/permissions_controller.rb
@@ -2,6 +2,6 @@ class Internal::PermissionsController < Internal::ApplicationController
   layout "internal"
 
   def index
-    @users = User.with_role(:admin).union(User.with_role(:super_admin)).union(User.with_role(:single_resource_admin, :any))
+    @users = User.with_role(:admin).union(User.with_role(:super_admin)).union(User.with_role(:single_resource_admin, :any)).page(params[:page]).per(50)
   end
 end
