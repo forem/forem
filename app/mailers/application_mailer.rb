e11bb4c05fd33d565d58759073e34dbdfec38cca
mstruve <mollylbs@gmail.com>
2020-01-07 18:58:32 -0500

    Set default email in application mailer using ENV variable

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "DEV Community <#{SiteConfig.default_site_email}>"
+  default from: "DEV Community <#{ApplicationConfig['DEFAULT_SITE_EMAIL']}>"
   layout "mailer"
   default template_path: ->(mailer) { "mailers/#{mailer.class.name.underscore}" }
 
