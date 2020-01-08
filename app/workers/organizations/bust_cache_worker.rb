851ad515ee25dfcbe740f488da695f597d7031ad
Maykon Menezes <maykon.meneses@gmail.com>
2020-01-08 15:46:00 -0300

    refactored org bust cache worker and organization_spec

--- app/workers/organizations/bust_cache_worker.rb
+++ app/workers/organizations/bust_cache_worker.rb
@@ -4,12 +4,12 @@ module Organizations
 
     sidekiq_options queue: :high_priority, retry: 10
 
-    def perform(organization_id, slug, cache_buster = CacheBuster)
+    def perform(organization_id, slug)
       organization = Organization.find_by(id: organization_id)
 
       return unless organization
 
-      cache_buster.bust_organization(organization, slug)
+      CacheBuster.bust_organization(organization, slug)
     end
   end
 end
