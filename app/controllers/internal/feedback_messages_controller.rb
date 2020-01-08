3ef4088807eda4d7a5e491dbd88dc9d7f6cc16df
jacobherrington <jacobherringtondeveloper@gmail.com>
2020-01-07 18:16:49 -0600

    Add filtering to internal/reports/
    This change adds some simple filtering options to the reports view. We
    may need to eventually do some real search functionality for parts of
    internal, but this should serve our immediate needs.
    
    Unrelated note: I think this view would be a prime candidate for
    rebuilding with ajax instead of local forms.

--- app/controllers/internal/feedback_messages_controller.rb
+++ app/controllers/internal/feedback_messages_controller.rb
@@ -4,11 +4,24 @@ class Internal::FeedbackMessagesController < Internal::ApplicationController
   def index
     @feedback_type = params[:state] || "abuse-reports"
     @status = params[:status] || "Open"
+
     @feedback_messages = FeedbackMessage.
       where(feedback_type: @feedback_type, status: @status).
       includes(:reporter, :notes).
       order("feedback_messages.created_at DESC").
       page(params[:page] || 1).per(5)
+
+    if params[:reporter].present?
+      @feedback_messages = @feedback_messages.
+        joins("JOIN users ON users.id = feedback_messages.reporter_id").
+        where("username ILIKE :reporter", reporter: "%#{params[:reporter]}%")
+    end
+
+    if params[:reported_url].present?
+      @feedback_messages = @feedback_messages.
+        where("reported_url ILIKE :reported_url", reported_url: "%#{params[:reported_url]}%")
+    end
+
     @email_messages = EmailMessage.find_for_reports(@feedback_messages)
     @new_articles = Article.published.includes(:user).limit(120).order("created_at DESC").where("score > ? AND score < ?", -10, 8)
     @possible_spam_users = User.where("github_created_at > ? OR twitter_created_at > ? OR length(name) > ?", 50.hours.ago, 50.hours.ago, 30).
