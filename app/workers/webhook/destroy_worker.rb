c5631fa30d7a2d2cfc6f992f11c0004817f2ac1f
Lucas Hiago <lucashiago63@gmail.com>
2020-01-09 17:35:44 -0300

    Change DestroyJob to DestroyWorker and move to sidekiq

--- app/workers/webhook/destroy_worker.rb
+++ app/workers/webhook/destroy_worker.rb
@@ -1,6 +1,8 @@
 module Webhook
-  class DestroyJob < ApplicationJob
-    queue_as :webhook_destroy
+  class DestroyWorker
+    include Sidekiq::Worker
+
+    sidekiq_options queue: :low_priority, retry: 10
 
     def perform(user_id:, application_id:)
       Webhook::Endpoint.where(user_id: user_id, oauth_application_id: application_id).destroy_all
