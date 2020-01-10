f78b89e7636fb3f2265bee5d5a19286ec476e540
Ben Halpern <bendhalpern@gmail.com>
2020-01-10 12:33:37 -0500

    Add suggested tags to SiteConfig (#5426) [deploy]
    * Initial base work
    
    * Add suggested tags config
    
    * Downcase suggested tag input

--- app/controllers/api/v0/tags_controller.rb
+++ app/controllers/api/v0/tags_controller.rb
@@ -4,6 +4,7 @@ module Api
       caches_action :index,
                     cache_path: proc { |c| c.params.permit! },
                     expires_in: 10.minutes
+      before_action :set_cache_control_headers, only: %i[onboarding] # essentially static content
       respond_to :json
 
       def index
@@ -12,7 +13,8 @@ module Api
       end
 
       def onboarding
-        @tags = Tag.where(name: Tag::NAMES)
+        set_surrogate_key_header "siteconfig/onboarding-tags"
+        @tags = Tag.where(name: SiteConfig.suggested_tags)
       end
     end
   end
