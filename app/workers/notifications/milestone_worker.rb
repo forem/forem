607ca30b536b958f84faf723d41f61382b2c57d7
Lucas Hiago <lucashiago63@gmail.com>
2020-01-13 16:55:23 -0300

    Remove service args from perform method in Milestone Worker and update specs

--- app/workers/notifications/milestone_worker.rb
+++ app/workers/notifications/milestone_worker.rb
@@ -4,11 +4,11 @@ module Notifications
 
     sidekiq_options queue: :low_priority, retry: 10
 
-    def perform(type, article_id, service = Notifications::Milestone::Send)
+    def perform(type, article_id)
       article = Article.find_by(id: article_id)
       return unless article
 
-      service.call(type, article)
+      Notifications::Milestone::Send.call(type, article)
     end
   end
 end
