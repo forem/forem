c7d10a7f5e5033a2b5b3f814bab951dd76d03f04
Michael Kohl <citizen428@gmail.com>
2020-01-14 23:53:33 +0700

    Return 429 Too Many Requests instead of raising (#5504)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -47,7 +47,11 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    raise if RateLimitChecker.new(current_user).limit_by_action("comment_creation")
+    if RateLimitChecker.new(current_user).limit_by_action("comment_creation")
+      skip_authorization
+      render json: {}, status: :too_many_requests
+      return
+    end
 
     @comment = Comment.new(permitted_attributes(Comment))
     @comment.user_id = current_user.id
