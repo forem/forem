f500e3591d60e83f5af230089ad22edc364b0b36
mstruve <mollylbs@gmail.com>
2020-01-14 17:58:18 -0500

    Send additional Sidekiq data to Honeycomb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,4 +8,8 @@ Sidekiq.configure_server do |config|
   # On Heroku this configuration is overridden and Sidekiq will point at the redis
   # instance given by the ENV variable REDIS_PROVIDER
   config.redis = { url: sidekiq_url }
+
+  config.server_middleware do |chain|
+    chain.add Sidekiq::HoneycombMiddleware
+  end
 end
