cb1bb4825e0f564473dc47a7be1138bb5dd7c550
mstruve <mollylbs@gmail.com>
2020-01-14 13:57:00 -0500

    Raise an error if the ENV variable DEPLOY_STATUS is set to blocked to prevent any deploys

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-STATEMENT_TIMEOUT=180000 bundle exec rails db:migrate && rails runner "puts 'app load success'"
+STATEMENT_TIMEOUT=180000 bundle exec rails db:migrate && rails runner "puts 'app load success'; raise 'Deploy blocked' if ENV['DEPLOY_STATUS']=='blocked'"
