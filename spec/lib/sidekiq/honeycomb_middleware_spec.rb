dab5fe17eb8a3f4990c558d94bd44eec27c321fa
mstruve <mollylbs@gmail.com>
2020-01-15 13:46:02 -0500

    add additional test for middleware and run Sidekiq inline in a block

--- spec/lib/sidekiq/honeycomb_middleware_spec.rb
+++ spec/lib/sidekiq/honeycomb_middleware_spec.rb
@@ -21,18 +21,39 @@ RSpec.describe Sidekiq::HoneycombMiddleware do
   end
 
   before do
-    Sidekiq::Testing.inline!
-
     Sidekiq::Testing.server_middleware do |chain|
       chain.add described_class
     end
   end
 
   it "sends an event with expected keys" do
-    TestSidekiqWorker.perform_async(*args)
+    Sidekiq::Testing.inline! do
+      TestSidekiqWorker.perform_async("dont fail")
+    end
 
-    collected_data = Honeycomb.client.send("client").events.map(&:data).first
+    collected_data = Honeycomb.client.send("client").events.map(&:data).detect { |h| h["sidekiq.args"] == expected_hash["sidekiq.args"] }
     expect(collected_data).to include(expected_hash)
+  end
+
+  context "without args" do
+    let(:expected_hash) do
+      {
+        "sidekiq.class" => TestSidekiqWorker.to_s,
+        "sidekiq.queue" => "low_priority",
+        "sidekiq.jid" => instance_of(String),
+        "sidekiq.args" => [],
+        "sidekiq.result" => "success"
+      }
+    end
+
+    it "sends an event with expected keys" do
+      Sidekiq::Testing.inline! do
+        TestSidekiqWorker.perform_async
+      end
+
+      collected_data = Honeycomb.client.send("client").events.map(&:data).detect { |h| h["sidekiq.args"] == expected_hash["sidekiq.args"] }
+      expect(collected_data).to include(expected_hash)
+    end
   end
 
   context "with an error" do
@@ -48,9 +69,11 @@ RSpec.describe Sidekiq::HoneycombMiddleware do
     end
 
     it "sends an event with expected keys" do
-      expect { TestSidekiqWorker.perform_async("fail") }.to raise_error(StandardError)
+      Sidekiq::Testing.inline! do
+        expect { TestSidekiqWorker.perform_async("fail") }.to raise_error(StandardError)
+      end
 
-      collected_data = Honeycomb.client.send("client").events.map(&:data).detect { |h| h.key?("sidekiq.error") }
+      collected_data = Honeycomb.client.send("client").events.map(&:data).detect { |h| h["sidekiq.args"] == error_hash["sidekiq.args"] }
       expect(collected_data).to include(error_hash)
     end
   end
