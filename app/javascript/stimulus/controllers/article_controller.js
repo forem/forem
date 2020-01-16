2ce1465e97c60b9060f65a77e8de6da766345177
jacobherrington <jacobherringtondeveloper@gmail.com>
2020-01-16 09:19:20 -0600

    Rename uncommunicative methods

--- app/javascript/stimulus/controllers/article_controller.js
+++ app/javascript/stimulus/controllers/article_controller.js
@@ -1,30 +1,32 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-    static targets = [ "featuredNumber" ]
+  static targets = ['featuredNumber'];
 
-    boost() {
-        const seconds = new Date().getTime() / 1000;
-        this.featuredNumberTarget.value = Math.round(seconds);
-    }
+  increaseFeaturedNumber() {
+    // Increases the article's chances of being seen
+    const seconds = new Date().getTime() / 1000;
+    this.featuredNumberTarget.value = Math.round(seconds);
+  }
 
-    sink() {
-        const seconds = new Date().getTime() / 1080;
-        this.featuredNumberTarget.value = Math.round(seconds);
-    }
+  decreaseFeaturedNumber() {
+    // Decreases the article's chances of being seen
+    const seconds = new Date().getTime() / 1080;
+    this.featuredNumberTarget.value = Math.round(seconds);
+  }
 
-    highlightElement() {
-        this.element.classList.add("highlighted-bg", "highlighted-border");
-        setTimeout(() => {
-            this.element.classList.remove("highlighted-bg");
-        }, 350);
-    }
+  highlightElement() {
+    this.element.classList.add('highlighted-bg', 'highlighted-border');
+    setTimeout(() => {
+      this.element.classList.remove('highlighted-bg');
+    }, 350);
+  }
 
-    get articleId() {
-        return parseInt(this.data.get("id"), 10);
-    }
+  get articleId() {
+    return parseInt(this.data.get('id'), 10);
+  }
 
-    set articleId(value) {
-        this.data.set("id", value);
-    }
+  set articleId(value) {
+    this.data.set('id', value);
+  }
 }
