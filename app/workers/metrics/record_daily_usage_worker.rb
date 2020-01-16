00accbb11c3ba6e7d721c586624e2a55b62815f8
Ben Halpern <ben@dev.to>
2020-01-16 18:33:18 -0500

    Add active users count to datadog

--- app/workers/metrics/record_daily_usage_worker.rb
+++ app/workers/metrics/record_daily_usage_worker.rb
@@ -4,14 +4,22 @@ module Metrics
     sidekiq_options queue: :low_priority, retry: 10
 
     def perform
-      articles_min_15_score_past_24h = Article.published.where("score >= ? AND published_at > ?", 15, 1.day).size
+      articles_min_15_score_past_24h = Article.published.where("score >= ? AND published_at > ?", 15, 1.day.ago).size
       DataDogStatsClient.count("articles.min_15_score_past_24h", articles_min_15_score_past_24h, tags: { resource: "articles" })
 
-      first_articles_past_24h = Article.where(nth_published_by_author: 1).where("published_at > ?", 1.day).size
+      first_articles_past_24h = Article.where(nth_published_by_author: 1).where("published_at > ?", 1.day.ago).size
       DataDogStatsClient.count("articles.first_past_24h", first_articles_past_24h, tags: { resource: "articles" })
 
       new_users_min_1_comment_past_24h = User.where("comments_count >= ? AND created_at > ?", 1, 24.hours.ago).size
       DataDogStatsClient.count("users.new_min_1_comment_past_24h", new_users_min_1_comment_past_24h, tags: { resource: "users" })
+
+      nagative_reactions_past_24h = Reaction.where("points < 0").where("created_at > ?", 1.day.ago).size
+      DataDogStatsClient.count("reactions.negative_past_24h", nagative_reactions_past_24h, tags: { resource: "reactions" })
+
+      reports_past_24_hours = FeedbackMessage.where(category: ["spam", "other", "rude or vulgar", "harassment"]).where("created_at > ?", 1.day.ago).size
+      DataDogStatsClient.count("feedback_messages.reports_past_24_hours", reports_past_24_hours, tags: { resource: "feedback_messages" })
+
+      get_days_active_past_week_counts
     end
 
     private
@@ -23,11 +31,11 @@ module Metrics
       end
       non_new_user_ids = User.where("created_at < ?", 7.days.ago).where(id: ids_by_day.flatten).pluck(:id)
       new_user_ids = User.where("created_at > ?", 7.days.ago).where(id: ids_by_day.flatten).pluck(:id)
-      record_active_days_of_group(non_new_user_ids, "established")
-      record_active_days_of_group(new_user_ids, "new")
+      record_active_days_of_group(ids_by_day, non_new_user_ids, "established")
+      record_active_days_of_group(ids_by_day, new_user_ids, "new")
     end
 
-    def record_active_days_of_group(user_ids, group)
+    def record_active_days_of_group(ids_by_day, user_ids, group)
       user_ids_by_day = ids_by_day.map { |a| a & user_ids }
       distinct_user_values = user_ids_by_day.flatten.group_by(&:itself).transform_values(&:count).values
       distinct_counts = distinct_user_values.group_by(&:itself).transform_values(&:count)
