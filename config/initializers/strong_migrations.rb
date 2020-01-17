9ef1ae512bdefc746df01c1a447dff29afc56e0a
Michael Kohl <citizen428@gmail.com>
2020-01-17 21:45:44 +0700

    Add strong_migrations gem (#5522)
    * Add strong_migrations gem
    
    * Incorporate PR feedback
    
    * Move gem to group :development, :test

---
+++ config/initializers/strong_migrations.rb
@@ -0,0 +1,5 @@
+# https://github.com/ankane/strong_migrations#existing-migrations
+StrongMigrations.start_after = 2019_12_27_114543
+
+# https://github.com/ankane/strong_migrations#target-version
+StrongMigrations.target_postgresql_version = 11
