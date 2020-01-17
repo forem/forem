3f3c93af88b7da6848234e5cd62b9addff328b19
mstruve <mollylbs@gmail.com>
2020-01-16 19:10:50 -0500

    Setup SitemapGenerator and CarrierWave to use new AWS creds when ENV var is set

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,13 +6,23 @@ CarrierWave.configure do |config|
     config.storage = :file
   else
     config.fog_provider = "fog/aws"
-    config.fog_credentials = {
-      provider: "AWS",
-      aws_access_key_id: ApplicationConfig["AWS_ID"],
-      aws_secret_access_key: ApplicationConfig["AWS_SECRET"],
-      region: ApplicationConfig["AWS_DEFAULT_REGION"]
-    }
-    config.fog_directory = ApplicationConfig["AWS_BUCKET_NAME"]
+    if ENV["USE_NEW_AWS"]
+      config.fog_credentials = {
+        provider: "AWS",
+        aws_access_key_id: ApplicationConfig["AWS_UPLOAD_ID"],
+        aws_secret_access_key: ApplicationConfig["AWS_UPLOAD_SECRET"],
+        region: ApplicationConfig["AWS_UPLOAD_REGION"]
+      }
+      config.fog_directory = ApplicationConfig["AWS_UPLOAD_BUCKET_NAME"]
+    else
+      config.fog_credentials = {
+        provider: "AWS",
+        aws_access_key_id: ApplicationConfig["AWS_ID"],
+        aws_secret_access_key: ApplicationConfig["AWS_SECRET"],
+        region: ApplicationConfig["AWS_DEFAULT_REGION"]
+      }
+      config.fog_directory = ApplicationConfig["AWS_BUCKET_NAME"]
+    end
     config.storage = :fog
   end
 end
