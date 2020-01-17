bfe95ed2894d2a81c15c7d3e8c3f5f4dcbfff467
Ben Halpern <bendhalpern@gmail.com>
2020-01-17 08:52:38 -0500

    Send past week aggregate x day active user data to datadog (#5553) [deploy]
    * Initial days active work
    
    * Add active users count to datadog

--- spec/workers/metrics/record_daily_usage_worker_spec.rb
+++ spec/workers/metrics/record_daily_usage_worker_spec.rb
@@ -8,7 +8,9 @@ RSpec.describe Metrics::RecordDailyUsageWorker, type: :worker do
   let_it_be(:first_article) { create(:article, score: 15, nth_published_by_author: 1) }
   let_it_be(:second_article) { create(:article, score: 5, nth_published_by_author: 2) }
   let_it_be(:third_article) { create(:article, score: 38, nth_published_by_author: 3) }
-
+  let_it_be(:user) { create(:user, :trusted) }
+  let_it_be(:reaction) { create(:reaction, category: "vomit", user: user, reactable: first_article) }
+  let_it_be(:feedback_message) { create(:feedback_message, :abuse_report) }
   describe "#perform" do
     before do
       allow(DataDogStatsClient).to receive(:count)
@@ -31,6 +33,18 @@ RSpec.describe Metrics::RecordDailyUsageWorker, type: :worker do
       expect(
         DataDogStatsClient,
       ).to have_received(:count).with("users.new_min_1_comment_past_24h", 2, Hash).at_least(1)
+    end
+
+    it "records negative reactions" do
+      expect(
+        DataDogStatsClient,
+      ).to have_received(:count).with("reactions.negative_past_24h", 1, Hash).at_least(1)
+    end
+
+    it "records report feedback_messages" do
+      expect(
+        DataDogStatsClient,
+      ).to have_received(:count).with("feedback_messages.reports_past_24_hours", 1, Hash).at_least(1)
     end
   end
 end
