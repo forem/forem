86cfe0096d50985ea49e452c204a2fd01a0295ee
mstruve <mollylbs@gmail.com>
2020-01-17 11:37:20 -0500

    Only setup StrongMigrations in dev or test

--- config/initializers/strong_migrations.rb
+++ config/initializers/strong_migrations.rb
@@ -1,5 +1,7 @@
-# https://github.com/ankane/strong_migrations#existing-migrations
-StrongMigrations.start_after = 2019_12_27_114543
+if Rails.env.test? || Rails.env.development?
+  # https://github.com/ankane/strong_migrations#existing-migrations
+  StrongMigrations.start_after = 20_191_227_114_543
 
-# https://github.com/ankane/strong_migrations#target-version
-StrongMigrations.target_postgresql_version = 11
+  # https://github.com/ankane/strong_migrations#target-version
+  StrongMigrations.target_postgresql_version = 11
+end
