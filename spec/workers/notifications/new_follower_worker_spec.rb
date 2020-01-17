5ec752f1ec76a351e3cbdb206e393a1e8d57fdc0
Alex <alexandersmith223@gmail.com>
2020-01-17 10:02:49 -0800

    Add back boolean argument to new spec

--- spec/workers/notifications/new_follower_worker_spec.rb
+++ spec/workers/notifications/new_follower_worker_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Notifications::NewFollowerWorker, type: :worker do
   let(:follow_data) { { followable_type: "User", followable_id: 1, follower_id: 2 } }
   let(:worker) { subject }
 
-  include_examples "#enqueues_on_correct_queue", "medium_priority", [{}, {}]
+  include_examples "#enqueues_on_correct_queue", "medium_priority", [{}, true]
 
   describe "#perform" do
     let(:new_follower_service) { Notifications::NewFollower::Send }
