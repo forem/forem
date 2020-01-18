b344483982cd81390f918474ca6fc70fee28fc55
ashab <asha.ab@gmail.com>
2020-01-18 12:13:52 -0500

    Randomize spec execution
    So it can generate a seed for each test execution which can then
    be used to by `rspec --bisect` to repro order dependent flaky specs.
    
    This is one approach to identify the root cause for https://github.com/thepracticaldev/dev.to/issues/5228
    
    It appears this is failing because `current_user` is `nil` before this spec is
    run.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -125,6 +125,8 @@ RSpec.configure do |config|
 
   config.infer_spec_type_from_file_location!
 
+  config.order = :random
+
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
