0fc7bff84232f3bdbbb4db277de6fff32061d077
Ben Halpern <ben@dev.to>
2020-01-20 15:49:28 -0500

    Add to one more place and remove from ItemListItem

--- app/javascript/src/components/ItemList/ItemListItem.jsx
+++ app/javascript/src/components/ItemList/ItemListItem.jsx
@@ -21,7 +21,7 @@ export const ItemListItem = ({ item, children }) => {
         <div className="item-title">{adaptedItem.title}</div>
 
         <div className="item-details">
-          <a className="item-user" href={`/${adaptedItem.user.username}`} data-hover-details={`users/${adaptedItem.user.id}`}>
+          <a className="item-user" href={`/${adaptedItem.user.username}`}>
             <img src={adaptedItem.user.profile_image_90} alt="Profile Pic" />
             {`${adaptedItem.user.name}・`}
             {adaptedItem.visitedDate
