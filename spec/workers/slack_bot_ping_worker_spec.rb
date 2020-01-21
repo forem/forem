6cb5300f110d9b385bfbf1065b20376e22d42fbe
Alex <alexandersmith223@gmail.com>
2020-01-21 10:24:43 -0800

    Specs for String keys and nil data

--- spec/workers/slack_bot_ping_worker_spec.rb
+++ spec/workers/slack_bot_ping_worker_spec.rb
@@ -21,5 +21,30 @@ RSpec.describe SlackBotPingWorker, type: :worker do
                                                     username: "sloan_watch_bot",
                                                     icon_emoji: ":sloan:")
     end
+
+    it "does nothing if there is missing data" do
+      worker.perform(
+        message: nil,
+        channel: nil,
+        username: nil,
+        icon_emoji: nil,
+      )
+
+      expect(SlackBot).not_to have_received(:ping)
+    end
+
+    it "works with keys as Strings" do
+      worker.perform(
+        "message" => "hello",
+        "channel" => "#help",
+        "username" => "sloan_watch_bot",
+        "icon_emoji" => ":sloan:",
+      )
+
+      expect(SlackBot).to have_received(:ping).with("hello", # message
+                                                    channel: "#help",
+                                                    username: "sloan_watch_bot",
+                                                    icon_emoji: ":sloan:")
+    end
   end
 end
