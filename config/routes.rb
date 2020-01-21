3865dff3a311b5a3aee2b0906e3f81882777f605
mstruve <mollylbs@gmail.com>
2020-01-21 08:37:01 -0500

    set sidekiq session to be the same as Rails to prevent forbidden UI errors

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   require "sidekiq/web"
   authenticated :user, ->(user) { user.tech_admin? } do
     mount DelayedJobWeb, at: "/delayed_job"
+
+    Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_key_base]
     mount Sidekiq::Web => "/sidekiq"
   end
 
