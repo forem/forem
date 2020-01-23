d980a0cb168e25eeebb96ebcb30b0a2e7a99c03f
Lucas Hiago <lucashiago63@gmail.com>
2020-01-23 14:09:15 -0300

    Update bust homepage cache worker and related spec

--- spec/workers/reactions/bust_homepage_cache_worker_spec.rb
+++ spec/workers/reactions/bust_homepage_cache_worker_spec.rb
@@ -4,29 +4,30 @@ RSpec.describe Reactions::BustHomepageCacheWorker, type: :worker do
   describe "#perform" do
     let(:user) { create(:user) }
     let(:article) { create(:article, featured: true) }
-    let(:reaction) { create(:reaction, reactable: article, user: user) }
-    let(:comment) { create(:comment, commentable: article) }
-    let(:comment_reaction) { create(:reaction, reactable: comment, user: user) }
     let(:worker) { subject }
-    let(:buster) { double }
-
-    before do
-      allow(buster).to receive(:bust)
-    end
 
     it "busts the homepage cache when reactable is an Article" do
-      worker.perform(reaction.id, buster)
-      expect(buster).to have_received(:bust).exactly(4)
+      reaction = create(:reaction, reactable: article, user: user)
+      allow(CacheBuster).to receive(:bust)
+
+      worker.perform(reaction.id)
+
+      expect(CacheBuster).to have_received(:bust).exactly(4)
     end
 
     it "doesn't bust the homepage cache when reactable is a Comment" do
-      worker.perform(comment_reaction.id, buster)
-      expect(buster).not_to have_received(:bust)
+      comment = create(:comment, commentable: article)
+      comment_reaction = create(:reaction, reactable: comment, user: user)
+      allow(CacheBuster).to receive(:bust)
+
+      worker.perform(comment_reaction.id)
+
+      expect(CacheBuster).not_to have_received(:bust)
     end
 
     it "doesn't fail if a reaction doesn't exist" do
       expect do
-        worker.perform(Reaction.maximum(:id).to_i + 1, buster)
+        worker.perform(Reaction.maximum(:id).to_i + 1)
       end.not_to raise_error
     end
   end
