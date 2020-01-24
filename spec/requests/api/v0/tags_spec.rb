667257003c83b683c289a5bd2dcfcc412b476c38
rhymes <rhymesete@gmail.com>
2020-01-24 14:23:10 +0100

    Make Travis happy

--- spec/requests/api/v0/tags_spec.rb
+++ spec/requests/api/v0/tags_spec.rb
@@ -2,17 +2,20 @@ require "rails_helper"
 
 RSpec.describe "Api::V0::Tags", type: :request do
   describe "GET /api/tags" do
-    let_it_be(:tag) { create(:tag, taggings_count: 10) }
-
     it "returns tags" do
+      create(:tag, taggings_count: 10)
+
       get api_tags_path
+
       expect(response.parsed_body.size).to eq(1)
     end
 
     it "returns tags with the correct json representation" do
-      get api_tags_path
-      response_tag = response.parsed_body.first
+      tag = create(:tag, taggings_count: 10)
 
+      get api_tags_path
+
+      response_tag = response.parsed_body.first
       expect(response_tag.keys).to match_array(%w[id name bg_color_hex text_color_hex])
       expect(response_tag["id"]).to eq(tag.id)
       expect(response_tag["name"]).to eq(tag.name)
@@ -21,23 +24,28 @@ RSpec.describe "Api::V0::Tags", type: :request do
     end
 
     it "orders tags by taggings_count in a descending order" do
+      tag = create(:tag, taggings_count: 10)
       other_tag = create(:tag, taggings_count: tag.taggings_count + 1)
 
       get api_tags_path
+
       expected_result = [other_tag.id, tag.id]
       expect(response.parsed_body.map { |t| t["id"] }).to eq(expected_result)
     end
 
     it "supports pagination" do
-      create_list(:tag, 2)
+      create_list(:tag, 3)
 
       get api_tags_path, params: { page: 1, per_page: 2 }
       expect(response.parsed_body.length).to eq(2)
+
       get api_tags_path, params: { page: 2, per_page: 2 }
       expect(response.parsed_body.length).to eq(1)
     end
 
     it "sets the correct edge caching surrogate key for all tags" do
+      tag = create(:tag, taggings_count: 10)
+
       get api_tags_path
 
       expected_key = ["tags", tag.record_key].to_set
@@ -46,17 +54,20 @@ RSpec.describe "Api::V0::Tags", type: :request do
   end
 
   describe "GET /api/tags/onboarding" do
-    let_it_be(:tag) { create(:tag, name: SiteConfig.suggested_tags.first) }
-
     it "returns tags" do
+      create(:tag, name: SiteConfig.suggested_tags.first)
+
       get onboarding_api_tags_path
+
       expect(response.parsed_body.size).to eq(1)
     end
 
     it "returns tags with the correct json representation" do
-      get onboarding_api_tags_path
-      response_tag = response.parsed_body.first
+      tag = create(:tag, name: SiteConfig.suggested_tags.first)
 
+      get onboarding_api_tags_path
+
+      response_tag = response.parsed_body.first
       expect(response_tag.keys).to match_array(%w[id name bg_color_hex text_color_hex following])
       expect(response_tag["id"]).to eq(tag.id)
       expect(response_tag["name"]).to eq(tag.name)
@@ -74,6 +85,8 @@ RSpec.describe "Api::V0::Tags", type: :request do
     end
 
     it "sets the correct edge caching surrogate key for all tags" do
+      tag = create(:tag, name: SiteConfig.suggested_tags.first)
+
       get onboarding_api_tags_path
 
       expected_key = ["tags", tag.record_key].to_set
