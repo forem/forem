c33fca99567e652c25287e40e6e6582d9da36f07
Lucas Hiago <lucashiago63@gmail.com>
2020-01-23 22:33:02 -0300

    Update spec

--- spec/workers/notifications/remove_all_by_action_worker_spec.rb
+++ spec/workers/notifications/remove_all_by_action_worker_spec.rb
@@ -2,12 +2,11 @@ require "rails_helper"
 
 RSpec.describe Notifications::RemoveAllByActionWorker, type: :worker do
   describe "#perform" do
-    let(:remove_all_by_action_service) { double }
     let(:article) { create(:article) }
     let(:worker) { subject }
 
     before do
-      allow(remove_all_by_action_service).to receive(:call)
+      allow(Notifications::RemoveAllByAction).to receive(:call)
     end
 
     it "calls the service" do
