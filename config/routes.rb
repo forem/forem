82f2281b1c139d919de6b88065774f5bc742f075
Ben Halpern <bendhalpern@gmail.com>
2020-01-28 16:49:34 -0500

    Create dynamic robots.txt page (#5794) [deploy]
    * Create dynamic robots.txt page
    
    * Add tests and remove cachebust

--- config/routes.rb
+++ config/routes.rb
@@ -270,6 +270,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root
   get "/about" => "pages#about"
+  get "/robots.:format" => "pages#robots"
   get "/api", to: redirect("https://docs.dev.to/api")
   get "/privacy" => "pages#privacy"
   get "/terms" => "pages#terms"
