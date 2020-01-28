73b29c98b8a161be71f1ee3975d554fac0f2e96f
rhymes <rhymesete@gmail.com>
2020-01-28 17:43:04 +0100

    Improve ChatChannelsController

--- app/controllers/api/v0/chat_channels_controller.rb
+++ app/controllers/api/v0/chat_channels_controller.rb
@@ -1,8 +1,13 @@
 module Api
   module V0
     class ChatChannelsController < ApiController
+      SHOW_ATTRIBUTES_FOR_SERIALIZATION = %i[id description channel_name].freeze
+
       def show
-        @chat_channel = ChatChannel.find(params[:id])
+        @chat_channel = ChatChannel.
+          select(SHOW_ATTRIBUTES_FOR_SERIALIZATION).
+          find(params[:id])
+
         error_not_found unless @chat_channel.has_member?(current_user)
       end
     end
