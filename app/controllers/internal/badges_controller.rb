cfe5d15fd0ad7ed6c09baab0d20ac6ac076cea9f
Vaidehi Joshi <vaidehi.sj@gmail.com>
2020-02-03 14:58:55 -0800

    Improve dropdown UX for internal/badges
    Add a "blank" option to the dropdown to list of badges on internal/badges route.
    Also add some checks to handle if an admin accidentally tries to award a "blank" badge.

--- app/controllers/internal/badges_controller.rb
+++ app/controllers/internal/badges_controller.rb
@@ -1,5 +1,6 @@
 class Internal::BadgesController < Internal::ApplicationController
   layout "internal"
+  before_action :ensure_badge, only: :award_badges
 
   def index
     @badges = Badge.all
@@ -7,16 +8,22 @@ class Internal::BadgesController < Internal::ApplicationController
 
   def award_badges
     usernames = permitted_params[:usernames].split(/\s*,\s*/)
-    badge_slug = permitted_params[:badge]
     message = permitted_params[:message_markdown].presence || "Congrats!"
-    BadgeRewarder.award_badges(usernames, badge_slug, message)
+    BadgeRewarder.award_badges(usernames, permitted_params[:badge], message)
     flash[:success] = "BadgeRewarder task ran!"
     redirect_to internal_badges_url
+  rescue ArgumentError => e
+    flash[:danger] = e
+    redirect_to "/internal/badges"
   end
 
   private
 
   def permitted_params
     params.permit(:usernames, :badge, :message_markdown)
+  end
+
+  def ensure_badge
+    raise ArgumentError, "You must choose a badge to award" if permitted_params[:badge].blank?
   end
 end
