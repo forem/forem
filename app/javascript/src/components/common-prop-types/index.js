16eb5191d491a57f3d7b2e055d52efb081b5f52e
Nick Taylor <nick@iamdeveloper.com>
2020-02-03 15:28:21 -0500

    Proptypes cleanup.

--- app/javascript/src/components/common-prop-types/index.js
+++ app/javascript/src/components/common-prop-types/index.js
@@ -1,5 +1,3 @@
-import userPropTypes from './user-prop-types';
-import defaultChildrenPropTypes from './default-children-prop-types';
-
+export * from './user-prop-types';
+export * from './default-children-prop-types';
 export * from './organization-prop-type';
-export { userPropTypes, defaultChildrenPropTypes };
