dcb5b9f804b8e831779c7e61402fafbb6cf27c97
Alex <alexandersmith223@gmail.com>
2020-02-03 14:14:12 -0800

    Update references to HtmlVariantSuccessCreateWorker

--- app/controllers/html_variant_successes_controller.rb
+++ app/controllers/html_variant_successes_controller.rb
@@ -2,7 +2,7 @@ class HtmlVariantSuccessesController < ApplicationMetalController
   include ActionController::Head
 
   def create
-    HtmlVariantSuccessCreateJob.perform_later(html_variant_id: params[:html_variant_id], article_id: params[:article_id])
+    HtmlVariantSuccessCreateWorker.perform_async(params[:html_variant_id], params[:article_id])
     head :ok
   end
 end
