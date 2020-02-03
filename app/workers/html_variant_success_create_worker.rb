fc0546475baaa4341f457184358c8cb8edfb1c35
Alex <alexandersmith223@gmail.com>
2020-02-03 14:12:53 -0800

    Create HtmlVariantSuccessCreateWorker

---
+++ app/workers/html_variant_success_create_worker.rb
@@ -0,0 +1,11 @@
+class HtmlVariantSuccessCreateWorker
+  include Sidekiq::Worker
+
+  sidekiq_options queue: :default, retry: 10
+
+  def perform(html_variant_id, article_id)
+    return unless html_variant_id && article_id
+
+    HtmlVariantSuccess.create(html_variant_id: html_variant_id, article_id: article_id)
+  end
+end
