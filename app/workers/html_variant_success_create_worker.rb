d2bcf6aa128b2fb0ea1ee2d03d78602d086f59bb
Alex <alexandersmith223@gmail.com>
2020-02-03 14:45:35 -0800

    Remove unecessary guard clause

--- app/workers/html_variant_success_create_worker.rb
+++ app/workers/html_variant_success_create_worker.rb
@@ -4,8 +4,6 @@ class HtmlVariantSuccessCreateWorker
   sidekiq_options queue: :default, retry: 10
 
   def perform(html_variant_id, article_id)
-    return unless html_variant_id && article_id
-
     HtmlVariantSuccess.create(html_variant_id: html_variant_id, article_id: article_id)
   end
 end
