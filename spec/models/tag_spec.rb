0c42ea2b432752c319ef952613bb6bb4ae83061f
Alex <alexandersmith223@gmail.com>
2020-02-04 09:24:37 -0800

    Move Tags::BustCacheJob to Sidekiq (#5870) [deploy]
    * Create Tags::BustCacheWorker
    
    * Create Tags::BustCacheWorker spec
    
    * Update references to new worker

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -65,6 +65,8 @@ RSpec.describe Tag, type: :model do
   end
 
   it "triggers cache busting on save" do
-    expect { build(:tag).save }.to have_enqueued_job.on_queue("tags_bust_cache")
+    sidekiq_assert_enqueued_with(job: Tags::BustCacheWorker, args: [tag.name]) do
+      tag.save
+    end
   end
 end
