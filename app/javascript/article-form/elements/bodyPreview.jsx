b09a0c60f38012348f7534584c76e7354e9c050b
Vaidehi Joshi <vaidehi.sj@gmail.com>
2020-02-04 08:32:06 -0800

    Add comment to highlight difference between previewing in v1 and v2 editors (#5894)
    A follow-up to https://github.com/thepracticaldev/dev.to/pull/5873.

--- app/javascript/article-form/elements/bodyPreview.jsx
+++ app/javascript/article-form/elements/bodyPreview.jsx
@@ -29,6 +29,10 @@ function titleArea(previewResponse, version, articleState) {
     });
   }
 
+  // The v2 editor stores its cover image in articleState.mainImage, while the v1 editor
+  // stores it as previewRespose.cover_image. When previewing, we handle both by
+  // defaulting to setting the cover image to the mainImage on the article (v2),
+  //  and only using the cover image from the previewResponse if it exists (v1).
   let coverImage = articleState.mainImage || '';
   if (articleState.previewShowing) {
     // In preview state, use the cover_image from previewResponse.
