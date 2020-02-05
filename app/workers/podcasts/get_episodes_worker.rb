b8a23ef69472cea1a85fbb94b9e808ae97485f70
Alex <alexandersmith223@gmail.com>
2020-02-05 12:19:53 -0800

    Change arguments to Hash

--- app/workers/podcasts/get_episodes_worker.rb
+++ app/workers/podcasts/get_episodes_worker.rb
@@ -4,11 +4,19 @@ module Podcasts
 
     sidekiq_options queue: :high_priority, retry: 10
 
-    def perform(podcast_id:, limit: 1000, force_update: false, feed: Podcasts::Feed)
+    def perform(podcast_data = {})
+      # Sidekiq turns arguments into Strings so the Ruby keyword argument sorcery doesn't work here
+      # prevent any mismatch between String keys and Symbol keys
+      podcast_data.symbolize_keys!
+
+      podcast_id = podcast_data[:podcast_id]
+      limit = podcast_data[:limit] || 1_000
+      force_update = podcast_data[:force_update] || false
+
       podcast = Podcast.find_by(id: podcast_id)
       return unless podcast
 
-      feed.new(podcast).get_episodes(limit: limit, force_update: force_update)
+      Podcasts::Feed.new(podcast).get_episodes(limit: limit, force_update: force_update)
     end
   end
 end
