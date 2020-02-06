411d8d2e0f5711e78a0202a1f88af42181af32cd
Alex <alexandersmith223@gmail.com>
2020-02-06 08:46:37 -0800

    Add comment to explain parameters of #perform

--- app/workers/podcasts/get_episodes_worker.rb
+++ app/workers/podcasts/get_episodes_worker.rb
@@ -4,6 +4,8 @@ module Podcasts
 
     sidekiq_options queue: :high_priority, retry: 10
 
+    # podcast_data should be a Hash with keys :podcast_id, :limit, and :force_update
+    # :limit and :force_update are both optional - there are default values
     def perform(podcast_data = {})
       # Sidekiq turns arguments into Strings so the Ruby keyword argument sorcery doesn't work here
       # prevent any mismatch between String keys and Symbol keys
