2c6c4dd6b1e69b084cdb91f7721aaed9a2ca5df3
spencer page <pagespence@yahoo.com>
2020-02-06 17:26:15 -0800

    fix merge conflict

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -143,8 +143,10 @@ class CommentsController < ApplicationController
       @comment.deleted = true
       @comment.save!
     end
-    location = @comment.commentable ? URI.parse(@comment.commentable.path).path : "/"
-    redirect_to location, notice: "Comment was successfully deleted."
+    redirect = @comment.commentable&.path || user_path(current_user)
+    # NOTE: Brakeman doesn't like redirecting to a path, because of a "possible
+    # unprotected redirect". Using URI.parse().path is the recommended workaround.
+    redirect_to URI.parse(redirect).path, notice: "Comment was successfully deleted."
   end
 
   def delete_confirm
