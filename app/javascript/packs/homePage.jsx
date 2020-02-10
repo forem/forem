86a788fb1efe22822460835343110350134be183
Nick Taylor <nick@iamdeveloper.com>
2020-02-10 12:21:15 -0500

    Fixed logic for loading user data on homepage.

--- app/javascript/packs/homePage.jsx
+++ app/javascript/packs/homePage.jsx
@@ -28,7 +28,7 @@ if (sidebarListingsMinimizeButton) {
  * Renders tags followed in the left side bar of the homepage.
  *
  * @param {HTMLElement} tagsFollowedContainer DOM element to render tags followed.
- * @param {object} user The currently logged on user
+ * @param {object} user The currently logged on user, null if not logged on.
  */
 function renderTagsFollowed(tagsFollowedContainer, user = userData()) {
   if (user === null) {
@@ -54,14 +54,37 @@ function renderTagsFollowed(tagsFollowedContainer, user = userData()) {
   });
 }
 
-renderTagsFollowed(document.getElementById('sidebar-nav-followed-tags'));
+let waitingForDataLoad = setTimeout(function dataLoadedCheck() {
+  const { user = null, userStatus } = document.body.dataset;
+
+  if (userStatus === 'logged-out') {
+    // Nothing to do, the user is not logged on.
+    return;
+  }
+
+  if (userStatus === 'logged-in' && user !== null) {
+    // We have user data, render followed tags.
+    clearTimeout(waitingForDataLoad);
+    renderTagsFollowed(document.getElementById('sidebar-nav-followed-tags'));
+    return;
+  }
+
+  // No user data yet for the logged on user, poll once more.
+  waitingForDataLoad = setTimeout(dataLoadedCheck, 40);
+}, 40);
 
 InstantClick.on('receive', (_url, body, title) => {
+  if (document.body.dataset.userStatus !== 'logged-in') {
+    // Nothing to do, the user is not logged on.
+    return false;
+  }
+
   const tagsFollowedContainer = body.querySelector(
     '#sidebar-nav-followed-tags',
   );
 
   if (!tagsFollowedContainer) {
+    // Not on the homepage, so nothing to do.
     return false;
   }
 
