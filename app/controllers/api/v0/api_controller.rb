cc26c0cf83d058ca545e55d8d5739b00f89b2eb3
rhymes <rhymesete@gmail.com>
2020-02-11 17:06:03 +0100

    Let ApiController inherit from ActionController::Base to have its own lifecycle

--- app/controllers/api/v0/api_controller.rb
+++ app/controllers/api/v0/api_controller.rb
@@ -1,4 +1,9 @@
 class Api::V0::ApiController < ApplicationController
+  protect_from_forgery with: :exception, prepend: true
+
+  include ValidRequest
+  include Pundit
+
   def cors_set_access_control_headers
     headers["Access-Control-Allow-Origin"] = "*"
     headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS"
