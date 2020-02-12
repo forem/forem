e3744fa46c84b3299be4ef4a062c17b51c2a2459
rhymes <rhymesete@gmail.com>
2020-02-12 12:02:49 +0100

    Add specs and fix bugs for NotificationDecorator

---
+++ spec/decorators/notification_decorator_spec.rb
@@ -0,0 +1,46 @@
+require "rails_helper"
+
+RSpec.describe NotificationDecorator, type: :decorator do
+  describe "#mocked_object" do
+    it "returns empty struct if the notification is new" do
+      notification = build(:notification)
+      result = notification.decorate.mocked_object("user")
+
+      expect(result.name).to be_empty
+      expect(result.id).to be_nil
+    end
+
+    it "returns class name and id for the reactable in a struct" do
+      comment = create(:comment, commentable: create(:article, organization: create(:organization)))
+      notification = Notification.send_new_comment_notifications_without_delay(comment)
+
+      result = notification.decorate.mocked_object("user")
+      expect(result.name).to eq("User")
+      expect(result.id).to eq(comment.user.id)
+    end
+  end
+
+  describe "#milestone_type" do
+    it "returns empty string if there is no action" do
+      notification = build(:notification)
+      expect(notification.decorate.milestone_type).to be_empty
+    end
+
+    it "returns the type of the milestone action" do
+      notification = build(:notification, action: "Milestone::Reaction::64")
+      expect(notification.decorate.milestone_type).to eq("Reaction")
+    end
+  end
+
+  describe "#milestone_count" do
+    it "returns empty string if there is no action" do
+      notification = build(:notification)
+      expect(notification.decorate.milestone_count).to be_empty
+    end
+
+    it "returns the count of the milestone action" do
+      notification = build(:notification, action: "Milestone::Reaction::64")
+      expect(notification.decorate.milestone_count).to eq("64")
+    end
+  end
+end
