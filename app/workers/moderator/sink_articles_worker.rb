56281f432c327c463501832934f479609befdb6a
Zhao-Andy <17884966+Zhao-Andy@users.noreply.github.com>
2020-02-12 15:26:12 -0500

    Use medium priority for sink worker

--- app/workers/moderator/sink_articles_worker.rb
+++ app/workers/moderator/sink_articles_worker.rb
@@ -2,7 +2,7 @@ module Moderator
   class SinkArticlesWorker
     include Sidekiq::Worker
 
-    sidekiq_options queue: :high_priority, retry: 10
+    sidekiq_options queue: :medium_priority, retry: 10
 
     def perform(user_id)
       user = User.find_by(id: user_id)
