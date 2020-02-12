95a56a12e167fb4df727b13c20cdd1ccf3c4ec19
rhymes <rhymesete@gmail.com>
2020-02-12 15:31:01 +0100

    Convert PodcastEpisodeDecorator to a Rails decorator

--- app/decorators/podcast_episode_decorator.rb
+++ app/decorators/podcast_episode_decorator.rb
@@ -1,6 +1,4 @@
-class PodcastEpisodeDecorator < ApplicationDecorator
-  delegate_all
-
+class PodcastEpisodeDecorator < BaseDecorator
   def comments_to_show_count
     cached_tag_list_array.include?("discuss") ? 75 : 25
   end
