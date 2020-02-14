69ebe779bb1ce51258481269c984767f3b80ec2c
Michael Kohl <citizen428@gmail.com>
2020-02-15 02:28:27 +0700

    Improve Docker configuration (#6055)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -119,6 +119,19 @@ Rails.application.configure do
       end
     end
   end
+
+  # Docker specific development configuration
+  if File.file?("/.dockerenv")
+    # Using shell tools so we don't need to require Socket and IPAddr
+    host_ip = `/sbin/ip route|awk '/default/ { print $3 }'`.strip
+    logger = Logger.new(STDOUT)
+    logger.info "Whitelisting #{host_ip} for BetterErrors and Web Console"
+
+    if defined?(BetterErrors::Middleware)
+      BetterErrors::Middleware.allow_ip!(host_ip)
+    end
+    config.web_console.whitelisted_ips << host_ip
+  end
 end
 
 Rails.application.routes.default_url_options = { host: Rails.application.config.app_domain }
