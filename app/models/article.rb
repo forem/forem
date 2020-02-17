7e23582c727a9160a03a0750d2e52dd4bf21cf5d
Ridhwana <ridhwana.khan16@gmail.com>
2020-02-17 16:59:07 +0200

    refactor: use the existing function in order to spawn the one where we need approval

--- app/models/article.rb
+++ app/models/article.rb
@@ -80,7 +80,7 @@ class Article < ApplicationRecord
 
   scope :cached_tagged_with, ->(tag) { where("cached_tag_list ~* ?", "^#{tag},| #{tag},|, #{tag}$|^#{tag}$") }
 
-  scope :cached_tagged_by_approval_with, ->(tag) { where("cached_tag_list ~* ? AND approved = true", "^#{tag},| #{tag},|, #{tag}$|^#{tag}$") }
+  scope :cached_tagged_by_approval_with, ->(tag) { cached_tagged_with(tag).where(approved: true) }
 
   scope :active_help, lambda {
                         published.
