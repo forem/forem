d974c5e94758a4720e3ff5d9908bd83b018e1352
Josh Puetz <josh@dev.to>
2020-02-17 15:20:13 -0600

    Whoops!

--- app/controllers/stories/feeds_controller.rb
+++ app/controllers/stories/feeds_controller.rb
@@ -1,18 +1,6 @@
 class Stories::FeedsController < ApplicationController
   respond_to :json
 
-  ARTICLE_ATTRIBUTES = %i[
-    title path id user_id comments_count positive_reactions_count organization_id
-    reading_time video_thumbnail_url video video_duration_in_minutes language
-    experience_level_rating experience_level_rating_distribution cached_user
-    cached_organization main_image
-  ].freeze
-  ARTICLE_METHODS = %i[
-    readable_publish_date cached_tag_list_array flare_tag class_name
-    cloudinary_video_url video_duration_in_minutes published_at_int
-    published_timestamp
-  ].freeze
-
   def show
     @stories = assign_feed_stories
   end
