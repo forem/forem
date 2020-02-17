509d068eaee7d6e477c8f58c484b116c93c8fc70
Julianna Tetreault <juliannatetreault@gmail.com>
2020-02-17 07:41:25 -0700

    Add Welcome Thread Broadcast to generator.rb

--- app/services/broadcasts/welcome_notification/generator.rb
+++ app/services/broadcasts/welcome_notification/generator.rb
@@ -18,11 +18,25 @@ module Broadcasts
 
         # Once it has the appropriate Broadcast to be sent, it should send a notification for it:
         # `Notification.send_welcome_notification(receiver_id, welcome_broadcast.id)`
+        # welcome_broadcast = # Rubocop will NOT leave me alone and keeps throwing errors in this entire method, so this will have to stay like this for now
+        return unless !has_commented_on_welcome_thread? && receiver_id == user.id # I feel like a check for the User's id is necessary here...
+
+        # return unless !has_commented_on_welcome_thread? # I CANNOT believe I forgot to eveluate this statement before... :facepalm:
+
+        Broadcast.find_by(title: "Welcome: welcome_thread") # Find the correct, welcome_thread in this case, Broadcast
+        Notification.send_welcome_notification(receiver_id, welcome_broadcast.id) # send the welcome_notification once the appropriate Broadcast has been found...still working on this
+        # end
+      end
+
+      def has_commented_on_welcome_thread?
+        # byebug # My BFF again
+        # articles.comments.commentable.where("title LIKE 'Welcome Thread - %'") && user.comments_count > 1
+        articles.comments.commentable.where(["title = ?", "Welcome Thread"]) && user.comments_count > 1 # Still trying to figure out what to do here, but due to spec failures, I am having a hard time hitting byebug/pry to inspect these values and their returns
       end
 
       private
 
-      attr_reader :receiver_id
+      attr_reader :receiver_id # Convention is good :)
     end
   end
 end
--- spec/services/broadcasts/welcome_notification/generator_spec.rb
+++ spec/services/broadcasts/welcome_notification/generator_spec.rb
@@ -2,6 +2,14 @@ require "rails_helper"
 
 RSpec.describe Broadcasts::WelcomeNotification::Generator, type: :service do
   describe "::call" do
+    subject(:welcome_notification) { WelcomeNotification.new(user_id(broadcast_id)) }
+
+    let(:user) { create(:user) }
+    let(:broadcast) { create(:broadcast, :welcome_broadcast) } # why does eager loading throw a rubocop error here?
+    let(:article) { create(:article) }
+    let(:not_commented_on) { create(:comment, commentable: article, title: "Welcome Thread") }
+    let(:commented_on) { create(:comment, user: user, commentable: article, title: "Welcome Thread") }
+
     context "when sending a set_up_profile notification" do
       xit "generates the appropriate broadcast to be sent to a user"
       xit "it sends a welcome notification for that broadcast"
@@ -9,11 +17,19 @@ RSpec.describe Broadcasts::WelcomeNotification::Generator, type: :service do
       xit "does not send a notification to a user who has set up their profile"
     end
 
-    context "when sending a welcome_thread notification" do
+    context "when sending a welcome_thread notification", :aggregate_failures do
       xit "generates the appropriate broadcast to be sent to a user"
-      xit "it sends a welcome notification for that broadcast"
-      xit "it does not send duplicate welcome notification for that broadcast"
-      xit "does not send a notification to a user who has commented in a welcome thread"
+      xit "sends a welcome notification for that broadcast" do
+        expect(welcome_notification.call).to be_success
+      end
+
+      xit "does not send duplicate welcome notification for that broadcast" do
+        expect(user).to have_received(:call).with(broadcast).once
+      end
+
+      xit "does not send a notification to a user who has commented in a welcome thread" do
+        # expect(user)
+      end
     end
 
     context "when sending a twitter_connect notification" do
