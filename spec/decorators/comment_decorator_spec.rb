cc53b55a4a20406273ef7a88e339c34e440863a7
rhymes <rhymesete@gmail.com>
2020-02-17 15:32:32 +0100

    [deploy] Switch decorators from Draper to Rails builtin decorations (#6040)
    * Convert SponsorshipDecorator to Rails decorator
    
    * Remove empty ReactionDecorator
    
    * Rename SponsorshipDecorator#level_color_hex to level_background_color
    
    * Convert CommentDecorator to Rails decorator
    
    * Add specs and fix bugs for NotificationDecorator
    
    * Convert NotificationDecorator to Rails decorator
    
    * Add specs and fix bugs for OrganizationDecorator
    
    * Convert OrganizationDecorator to Rails decorator
    
    * Add specs to PodcastEpisodeDecorator and fix bugs
    
    * Convert PodcastEpisodeDecorator to a Rails decorator
    
    * Add missing specs for ArticleDecorator and fix bugs
    
    * Convert ArticleDecorator to Rails decorator
    
    * Add missing specs to UserDecorator and fix issues
    
    * Convert UserDecorator to Rails decorator
    
    * Add .decorate_collection method
    
    * Add decorator for AR relations and remove Draper
    
    * Rename BaseDecorator back to ApplicationDecorator
    
    * Rename .decorate_ back to .decorate
    
    * Restore decorate_collection (WTH)
    
    * Add decorated?
    
    * Fix bugs and failing specs
    
    * Decoration should always be explicit

--- spec/decorators/comment_decorator_spec.rb
+++ spec/decorators/comment_decorator_spec.rb
@@ -3,20 +3,21 @@ require "rails_helper"
 RSpec.describe CommentDecorator, type: :decorator do
   describe "#low_quality" do
     let(:threshold) { CommentDecorator::LOW_QUALITY_THRESHOLD }
+    let(:comment) { build(:comment) }
 
     it "returns true if the comment is low quality" do
-      comment = build_stubbed(:comment, score: threshold - 1).decorate
-      expect(comment.low_quality).to be(true)
+      comment.score = threshold - 1
+      expect(comment.decorate.low_quality).to be(true)
     end
 
     it "returns false if the comment score is on threshold quality" do
-      comment = build_stubbed(:comment, score: threshold).decorate
-      expect(comment.low_quality).to be(false)
+      comment.score = threshold
+      expect(comment.decorate.low_quality).to be(false)
     end
 
     it "returns false if the comment is a good quality" do
-      comment = build_stubbed(:comment, score: threshold + 1).decorate
-      expect(comment.low_quality).to be(false)
+      comment.score = threshold + 1
+      expect(comment.decorate.low_quality).to be(false)
     end
   end
 
