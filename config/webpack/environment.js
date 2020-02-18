a15c1da340fcb6766b3dec937e447737c7f9249a
Nick Taylor <nick@iamdeveloper.com>
2020-02-18 18:25:36 -0500

    Enabled erb support for webpacker.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,29 +6,30 @@
 // environment.config.merge(customConfig);
 // environment.config.delete('output.chunkFilename');
 
-// module.exports = environment;
+// environment.loaders.append('erb', erb)
 
-
-const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
+const { environment } = require('@rails/webpacker');
+const erb = require('./loaders/erb');
+const webpack = require('webpack');
 
 environment.plugins.append(
   'CommonsChunkVendor',
   new webpack.optimize.CommonsChunkPlugin({
     name: 'vendor',
-    minChunks: (module) => {
+    minChunks: module => {
       // this assumes your vendor imports exist in the node_modules directory
-      return module.context && module.context.indexOf('node_modules') !== -1
-    }
-  })
-)
+      return module.context && module.context.indexOf('node_modules') !== -1;
+    },
+  }),
+);
 
 environment.plugins.append(
   'CommonsChunkManifest',
   new webpack.optimize.CommonsChunkPlugin({
     name: 'manifest',
-    minChunks: Infinity
-  })
-)
+    minChunks: Infinity,
+  }),
+);
 
-module.exports = environment
+environment.loaders.append('erb', erb);
+module.exports = environment;
