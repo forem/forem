20ff3f812d58f3907c038ac94288fb2c4337bc46
Alex <alexandersmith223@gmail.com>
2020-02-17 20:02:23 -0800

    Change find_by! to find

--- app/workers/search/classified_listing_es_index_worker.rb
+++ app/workers/search/classified_listing_es_index_worker.rb
@@ -5,7 +5,7 @@ module Search
     sidekiq_options queue: :high_priority
 
     def perform(classified_listing_id)
-      classified_listing = ::ClassifiedListing.find_by!(id: classified_listing_id)
+      classified_listing = ::ClassifiedListing.find(classified_listing_id)
       classified_listing.index_to_elasticsearch_inline
     end
   end
