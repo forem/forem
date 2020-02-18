a7dc893f50c3752da7982d221433b94d98f3dc29
Nick Taylor <nick@iamdeveloper.com>
2020-02-18 09:42:37 -0500

    Updates to Storybook for home page makeover. (#6130)

--- app/javascript/.storybook/config.js
+++ app/javascript/.storybook/config.js
@@ -1,5 +1,6 @@
 import { configure } from '@storybook/react';
 import '../../assets/stylesheets/minimal.scss';
+import './storybook.scss';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../', true, /.stories.jsx$/);
