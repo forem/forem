303865d099e5d34b57f735e700d87dd7ddd55168
jacobherrington <jacobherringtondeveloper@gmail.com>
2020-02-18 15:33:14 -0600

    Add a confirm for user vomits and use JS to display the button

--- app/javascript/profileDropdown/abuseButton.js
+++ app/javascript/profileDropdown/abuseButton.js
@@ -1,42 +1,55 @@
+/* eslint-disable no-alert */
+
 export default function initAbuseButton() {
+  // eslint-disable-next-line no-undef
+  const user = userData();
+
   const abuseButton = document.getElementById(
     'user-profile-dropdownmenu-abuse-button',
   );
-  if (!abuseButton) {
-    // button not always present when this is called
-    return;
-  }
 
   const { profileUserId } = abuseButton.dataset;
 
+  if (user.trusted) {
+    abuseButton.style.display = 'block';
+  }
+
+  function isFlagged() {
+    return abuseButton.textContent === 'Remove Abuse Flag';
+  }
+
   function flagAsAbusive() {
-    fetch(`/reactions`, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'X-CSRF-Token': window.csrfToken,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        reactable_id: profileUserId,
-        category: 'vomit',
-        reactable_type: 'User',
-      }),
-    })
-      .then(response => response.json())
-      .then(response => {
-        if (response.result === 'create') {
-          abuseButton.innerText = 'Remove Abuse Flag';
-        } else if (response.result === 'destroy') {
-          abuseButton.innerText = 'Flag as Abusive';
-        }
+    const confirm = isFlagged()
+      ? true
+      : window.confirm(`Are you sure you want to flag this account?`);
+    if (confirm) {
+      fetch(`/reactions`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'X-CSRF-Token': window.csrfToken,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          reactable_id: profileUserId,
+          category: 'vomit',
+          reactable_type: 'User',
+        }),
       })
-      .catch(e => {
-        // eslint-disable-next-line no-alert
-        window.alert(
-          `Something went wrong: ${e}. -- Please refresh the page to try again.`,
-        );
-      });
+        .then(response => response.json())
+        .then(response => {
+          if (response.result === 'create') {
+            abuseButton.innerText = 'Remove Abuse Flag';
+          } else if (response.result === 'destroy') {
+            abuseButton.innerText = 'Flag as Abusive';
+          }
+        })
+        .catch(e => {
+          window.alert(
+            `Something went wrong: ${e}. -- Please refresh the page to try again.`,
+          );
+        });
+    }
   }
 
   abuseButton.addEventListener('click', flagAsAbusive);
