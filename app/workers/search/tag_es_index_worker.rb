9c387b4f891c5b944837f17a9bbd9cfeb34d004c
Alex <alexandersmith223@gmail.com>
2020-02-19 11:14:42 -0800

    Change find_by to find for better error messaging

--- app/workers/search/tag_es_index_worker.rb
+++ app/workers/search/tag_es_index_worker.rb
@@ -5,7 +5,7 @@ module Search
     sidekiq_options queue: :high_priority
 
     def perform(tag_id)
-      tag = ::Tag.find_by!(id: tag_id)
+      tag = ::Tag.find(tag_id)
       tag.index_to_elasticsearch_inline
     end
   end
