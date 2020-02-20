335eb44c285e9ec7edf820be2cf60eba3cda4cc0
Mac Siri <krairit.siri@gmail.com>
2020-02-20 15:48:20 -0500

    Change logger.info to logger.error

--- app/labor/mailchimp_bot.rb
+++ app/labor/mailchimp_bot.rb
@@ -162,7 +162,7 @@ class MailchimpBot
   end
 
   def report_error(exception)
-    logger.info(exception)
+    logger.error(exception)
     DataDogStatsClient.increment("mailchimp.errors", tags: [action: "failed", user_id: user.id, source: "gibbon-gem"])
   end
 
