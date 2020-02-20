4f1dddaebdefbb47b7f5a85ad49b4b952741e975
rhymes <rhymesete@gmail.com>
2020-02-20 23:50:26 +0100

    Log to Rails and to Datadog

--- app/workers/data_update_worker.rb
+++ app/workers/data_update_worker.rb
@@ -5,6 +5,7 @@ class DataUpdateWorker
   def perform
     DataUpdateScript.scripts_to_run.each do |script|
       script.mark_as_run!
+      log_status(script)
 
       run_script(script)
     end
@@ -15,15 +16,27 @@ class DataUpdateWorker
   def run_script(script)
     require script.file_path
 
-    Rails.logger.info("Data update script: #{script.file_name}: running")
     script.file_class.new.run
 
     script.mark_as_finished!
-    Rails.logger.info("Data update script: #{script.file_name}: finished")
+    log_status(script)
   rescue StandardError => e
     script.mark_as_failed!
-    Rails.logger.error("Data update script: #{script.file_name}: failed")
+    log_status(script)
 
     Honeybadger.notify(e, context: { script_id: script.id })
+  end
+
+  def log_status(script)
+    status = script.status
+    file_name = script.file_name
+
+    logger_destination = status == :failed ? :error : :info
+    Rails.logger.public_send(
+      logger_destination,
+      "time=#{Time.current.rfc3339}, script=#{file_name}, status=#{status}",
+    )
+
+    DataDogStatsClient.increment("data_update_scripts.status", tags: ["status:#{status}", "script_name:#{file_name}"])
   end
 end
