0aed86785bb7ba7c5744aa52ab547e4e549cd33e
rhymes <rhymesete@gmail.com>
2020-02-20 10:35:25 +0100

    Make it more similar to Rails 5.2 default

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: 'eslint:recommended',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {},
+};
