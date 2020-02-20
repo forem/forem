b70a0d8e262597d82ef67c2de496c4ec09a65318
Mac Siri <krairit.siri@gmail.com>
2020-02-20 15:44:58 -0500

    Change Honeybadger back to Logger

--- app/labor/mailchimp_bot.rb
+++ app/labor/mailchimp_bot.rb
@@ -162,8 +162,8 @@ class MailchimpBot
   end
 
   def report_error(exception)
+    logger.info(exception)
     DataDogStatsClient.increment("mailchimp.errors", tags: [action: "failed", user_id: user.id, source: "gibbon-gem"])
-    Honeybadger.notify(exception)
   end
 
   def target_md5_email
