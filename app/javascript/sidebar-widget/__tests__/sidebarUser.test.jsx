d6dd52e953284c45861adfd6254fdb97f78f1b81
ludwiczakpawel <ludwiczakpawel@gmail.com>
2020-02-20 23:12:56 +0100

    Homepage Improvements, round II (#6202) [deploy]
    * remove border from ad and fix cut-off
    
    * improving ellipsis for tags
    
    * improving ellipsis for tags
    
    * fixing underline between each tag on hover
    
    * user avatars alignment in left sidebar
    
    * test commit
    
    * reverting test
    
    * test commit
    
    * reverting test
    
    * index unassigned
    
    * user suggestions in right sidebar on tag page, fixes
    
    * improving widgets a little
    
    * line height for category
    
    * Update tests
    
    Co-authored-by: Ben Halpern <bendhalpern@gmail.com>

--- app/javascript/sidebar-widget/__tests__/sidebarUser.test.jsx
+++ app/javascript/sidebar-widget/__tests__/sidebarUser.test.jsx
@@ -4,19 +4,20 @@ import render from 'preact-render-to-json';
 import SidebarUser from '../sidebarUser';
 
 const user = {
-  id: 1234
-}
+  id: 1234,
+};
 const followUser = jest.fn();
 
-const renderedSideBar = props => shallow(
-  <SidebarUser
-    key={user.id}
-    user={user}
-    followUser={followUser}
-    index={0}
-    {...props}
-  />
-)
+const renderedSideBar = props =>
+  shallow(
+    <SidebarUser
+      key={user.id}
+      user={user}
+      followUser={followUser}
+      index={0}
+      {...props}
+    />,
+  );
 
 describe('<SidebarUser />', () => {
   it('renders properly', () => {
@@ -26,27 +27,24 @@ describe('<SidebarUser />', () => {
         user={user}
         followUser={followUser}
         index={0}
-      />);
+      />,
+    );
     expect(tree).toMatchSnapshot();
   });
 
   it('triggers the onClick', () => {
-    renderedSideBar().find('.widget-list-item__follow-button').simulate('click');
+    renderedSideBar()
+      .find('.widget-list-item__follow-button')
+      .simulate('click');
     expect(followUser).toHaveBeenCalled();
   });
 
   it('shows if the user is followed or not', () => {
-    expect(renderedSideBar({ user: { following: true } }).contains('✓ FOLLOWING')).toBe(true);
-    expect(renderedSideBar({ user: { following: false } }).contains('+ FOLLOW')).toBe(true);
-  });
-
-  it('shows a <br /> if the index equals 2', () => {
-    expect(renderedSideBar({ index: 2 }).find(<br />).length > 0).toBe(true);
-    expect(renderedSideBar({ index: 2 }).find(<hr />).length > 0).toBe(false);
-  });
-
-  it('shows a <hr /> if the index differs from 2', () => {
-    expect(renderedSideBar({ index: 1 }).find(<hr />).length > 0).toBe(true);
-    expect(renderedSideBar({ index: 3 }).find(<br />).length > 0).toBe(false);
+    expect(
+      renderedSideBar({ user: { following: true } }).contains('✓ FOLLOWING'),
+    ).toBe(true);
+    expect(
+      renderedSideBar({ user: { following: false } }).contains('+ FOLLOW'),
+    ).toBe(true);
   });
 });
