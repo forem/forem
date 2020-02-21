48f5a34465279ac709d6f7216d1d378e56c3336c
Michael Kohl <citizen428@gmail.com>
2020-02-21 11:12:17 +0700

    Clean up chat channels when deleting users

--- app/services/users/delete_activity.rb
+++ app/services/users/delete_activity.rb
@@ -8,7 +8,7 @@ module Users
       user.follows.delete_all
       Follow.where(followable_id: user.id, followable_type: "User").delete_all
       user.messages.delete_all
-      user.chat_channel_memberships.delete_all
+      Users::CleanupChatChannels.call(user)
       user.mentions.delete_all
       user.badge_achievements.delete_all
       user.github_repos.delete_all
