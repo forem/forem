37a4c1c5535417df942ca639a1e148e234e3e42f
Alex <alexandersmith223@gmail.com>
2020-02-21 17:35:59 -0800

    Add search methods

--- app/services/search/classified_listing.rb
+++ app/services/search/classified_listing.rb
@@ -5,7 +5,17 @@ module Search
     MAPPINGS = JSON.parse(File.read("config/elasticsearch/mappings/classified_listings.json"), symbolize_names: true).freeze
 
     class << self
+      def search_documents(params:)
+        query_hash = Search::QueryBuilders::ClassifiedListing.new(params).as_hash
+        results = search(body: query_hash)
+        results.dig("hits", "hits").map { |cl_doc| cl_doc.dig("_source") }
+      end
+
       private
+
+      def search(body:)
+        SearchClient.search(index: INDEX_ALIAS, body: body)
+      end
 
       def index_settings
         if Rails.env.production?
