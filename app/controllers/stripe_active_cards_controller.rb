091279f9ccae8a76d6b40b801e87231d07a8af06
Alex <alexandersmith223@gmail.com>
2020-02-22 11:40:52 -0800

    Update Stripe tags in controller for Datadog

--- app/controllers/stripe_active_cards_controller.rb
+++ app/controllers/stripe_active_cards_controller.rb
@@ -11,14 +11,14 @@ class StripeActiveCardsController < ApplicationController
       Rails.logger.info("Stripe Add New Card Success - #{current_user.username}")
       flash[:settings_notice] = "Your billing information has been updated"
     else
-      DatadogStatsClient.increment("stripe.errors", tags: ["event:new_subscription", "user_id:#{current_user.id}"])
+      DatadogStatsClient.increment("stripe.errors", tags: ["action:create", "user_id:#{current_user.id}"])
 
       Rails.logger.error("Stripe Add New Card Failure - #{current_user.username}")
       flash[:error] = "There was a problem updating your billing info."
     end
     redirect_to user_settings_path(:billing)
   rescue Payments::CardError, Payments::InvalidRequestError => e
-    DatadogStatsClient.increment("stripe.errors", tags: ["event:new_subscription", "user_id:#{current_user.id}"])
+    DatadogStatsClient.increment("stripe.errors", tags: ["action:create", "user_id:#{current_user.id}"])
     redirect_to user_settings_path(:billing), flash: { error: e.message }
   end
 
@@ -34,7 +34,7 @@ class StripeActiveCardsController < ApplicationController
       Rails.logger.info("Stripe Card Update Success - #{current_user.username}")
       flash[:settings_notice] = "Your billing information has been updated"
     else
-      DatadogStatsClient.increment("stripe.errors", tags: ["event:update_subscription", "user_id:#{current_user.id}"])
+      DatadogStatsClient.increment("stripe.errors", tags: ["action:update", "user_id:#{current_user.id}"])
 
       Rails.logger.error("Stripe Card Update Failure - #{current_user.username}")
       flash[:error] = "There was a problem updating your billing info."
@@ -42,7 +42,7 @@ class StripeActiveCardsController < ApplicationController
 
     redirect_to user_settings_path(:billing)
   rescue Payments::CardError, Payments::InvalidRequestError => e
-    DatadogStatsClient.increment("stripe.errors", tags: ["event:update_subscription", "user_id:#{current_user.id}"])
+    DatadogStatsClient.increment("stripe.errors", tags: ["action:update", "user_id:#{current_user.id}"])
 
     redirect_to user_settings_path(:billing), flash: { error: e.message }
   end
