f371c1f84b3527b4312c731bc2d7de4d98b1f8e1
Nick Taylor <nick@iamdeveloper.com>
2020-02-24 16:19:38 -0500

    Converted <SaveButton /> to a stateful component.

--- app/javascript/articles/components/SaveButton.jsx
+++ app/javascript/articles/components/SaveButton.jsx
@@ -1,39 +1,62 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import PropTypes from 'prop-types';
 import { articlePropTypes } from '../../src/components/common-prop-types';
 
-export const SaveButton = ({ article, isBookmarked, onClick }) => {
-  if (article.class_name === 'Article') {
-    return (
-      <button
-        type="button"
-        className={`article-engagement-count engage-button bookmark-button ${
-          isBookmarked ? 'selected' : ''
-        }`}
-        data-reactable-id={article.id}
-        onClick={onClick}
-      >
-        <span className="bm-initial">SAVE</span>
-        <span className="bm-success">SAVED</span>
-      </button>
-    );
-  }
-  if (article.class_name === 'User') {
-    return (
-      <button
-        type="button"
-        style={{ width: '122px' }}
-        className="article-engagement-count engage-button follow-action-button"
-        data-info={`{"id":${article.id},"className":"User"}`}
-        data-follow-action-button
-      >
-        &nbsp;
-      </button>
-    );
+export class SaveButton extends Component {
+  componentDidMount() {
+    const { isBookmarked } = this.props;
+    this.setState({ buttonText: isBookmarked ? 'SAVED' : 'SAVE' });
   }
 
-  return null;
-};
+  render() {
+    const { buttonText } = this.state;
+    const { article, isBookmarked, onClick } = this.props;
+    const mouseOut = _e => {
+      this.setState({ buttonText: isBookmarked ? 'SAVED' : 'SAVE' });
+    };
+    const mouseOver = _e => {
+      if (isBookmarked) {
+        this.setState({ buttonText: 'UNSAVE' });
+      }
+    };
+
+    if (article.class_name === 'Article') {
+      return (
+        <button
+          type="button"
+          className={`article-engagement-count engage-button bookmark-button ${
+            isBookmarked ? 'selected' : ''
+          }`}
+          data-reactable-id={article.id}
+          onClick={onClick}
+          onMouseOver={mouseOver}
+          onFocus={mouseOver}
+          onMouseout={mouseOut}
+          onBlur={mouseOut}
+        >
+          <span className={isBookmarked ? 'bm-success' : 'bm-initial'}>
+            {buttonText}
+          </span>
+        </button>
+      );
+    }
+    if (article.class_name === 'User') {
+      return (
+        <button
+          type="button"
+          style={{ width: '122px' }}
+          className="article-engagement-count engage-button follow-action-button"
+          data-info={`{"id":${article.id},"className":"User"}`}
+          data-follow-action-button
+        >
+          &nbsp;
+        </button>
+      );
+    }
+
+    return null;
+  }
+}
 
 SaveButton.propTypes = {
   article: articlePropTypes.isRequired,
