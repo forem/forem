dcc60afc3a7fc8c84cac7b13a53647ce3e792dea
Nick Taylor <nick@iamdeveloper.com>
2020-02-25 13:05:35 -0500

    Fixed when renderFeed is imported for a logged on user.

--- app/javascript/packs/homePage.jsx
+++ app/javascript/packs/homePage.jsx
@@ -65,45 +65,47 @@ function renderTagsFollowed(tagsFollowedContainer, user = userData()) {
   });
 }
 
-import('./homePageFeed').then(({ renderFeed }) => {
-  const feedTimeFrame = frontPageFeedPathNames.get(window.location.pathname);
+const feedTimeFrame = frontPageFeedPathNames.get(window.location.pathname);
 
-  let waitingForDataLoad = setTimeout(function dataLoadedCheck() {
-    const { user = null, userStatus } = document.body.dataset;
+let waitingForDataLoad = setTimeout(function dataLoadedCheck() {
+  const { user = null, userStatus } = document.body.dataset;
 
-    if (userStatus === 'logged-out') {
-      return;
-    }
+  if (userStatus === 'logged-out') {
+    return;
+  }
 
-    if (userStatus === 'logged-in' && user !== null) {
+  if (userStatus === 'logged-in' && user !== null) {
+    clearTimeout(waitingForDataLoad);
+
+    import('./homePageFeed').then(({ renderFeed }) => {
       // We have user data, render followed tags.
-      clearTimeout(waitingForDataLoad);
       renderFeed(feedTimeFrame);
-      renderTagsFollowed(document.getElementById('sidebar-nav-followed-tags'));
-      return;
-    }
 
-    // No user data yet for the logged on user, poll once more.
-    waitingForDataLoad = setTimeout(dataLoadedCheck, 40);
-  }, 40);
+      InstantClick.on('change', () => {
+        const { userStatus: currentUserStatus } = document.body.dataset;
 
-  InstantClick.on('change', () => {
-    const { userStatus } = document.body.dataset;
+        if (currentUserStatus === 'logged-out') {
+          return;
+        }
 
-    if (userStatus === 'logged-out') {
-      return;
-    }
+        const url = new URL(window.location);
+        const changedFeedTimeFrame = frontPageFeedPathNames.get(url.pathname);
 
-    const url = new URL(window.location);
-    const changedFeedTimeFrame = frontPageFeedPathNames.get(url.pathname);
+        if (!frontPageFeedPathNames.has(url.pathname)) {
+          return;
+        }
 
-    if (!frontPageFeedPathNames.has(url.pathname)) {
-      return;
-    }
+        renderFeed(changedFeedTimeFrame);
+      });
+    });
 
-    renderFeed(changedFeedTimeFrame);
-  });
-});
+    renderTagsFollowed(document.getElementById('sidebar-nav-followed-tags'));
+    return;
+  }
+
+  // No user data yet for the logged on user, poll once more.
+  waitingForDataLoad = setTimeout(dataLoadedCheck, 40);
+}, 40);
 
 InstantClick.on('receive', (address, body, title) => {
   if (document.body.dataset.userStatus !== 'logged-in') {
