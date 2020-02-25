607d8d71dec4a342de87e9cce8a4d8cc6a935d13
Molly Struve <mollylbs@gmail.com>
2020-02-25 12:29:19 -0500

    Add index missing check to podcast migration (#6292) [deploy]

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_02_24_153122) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
