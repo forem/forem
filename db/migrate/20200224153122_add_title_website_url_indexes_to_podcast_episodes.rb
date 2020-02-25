cb9bf59b6e003fb6a0a5d93e180e560fc4b2aaa3
Molly Struve <mollylbs@gmail.com>
2020-02-25 12:29:19 -0500

    Add index missing check to podcast migration (#6292) [deploy]

--- db/migrate/20200224153122_add_title_website_url_indexes_to_podcast_episodes.rb
+++ db/migrate/20200224153122_add_title_website_url_indexes_to_podcast_episodes.rb
@@ -1,8 +1,14 @@
 class AddTitleWebsiteUrlIndexesToPodcastEpisodes < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
+  include IndexMigrationHelpers
 
-  def change
-    add_index :podcast_episodes, :title, algorithm: :concurrently
-    add_index :podcast_episodes, :website_url, algorithm: :concurrently
+  def up
+    add_index_if_missing(:podcast_episodes, :title, algorithm: :concurrently)
+    add_index_if_missing(:podcast_episodes, :website_url, algorithm: :concurrently)
+  end
+
+  def down
+    remove_index_if_exists(:podcast_episodes, column: :title, algorithm: :concurrently)
+    remove_index_if_exists(:podcast_episodes, column: :website_url, algorithm: :concurrently)
   end
 end
