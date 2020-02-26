fc9b9c23bc638596acaa78e1d648f954d2fa7e01
Mac Siri <krairit.siri@gmail.com>
2020-02-26 16:25:40 -0500

    Create more spec

--- spec/liquid_tags/katex_tag_spec.rb
+++ spec/liquid_tags/katex_tag_spec.rb
@@ -7,12 +7,16 @@ RSpec.describe KatexTag, type: :liquid_tag do
       Liquid::Template.parse("{% katex %}#{content}{% endkatex %}")
     end
 
-    it "generates proper div with content" do
+    it "generates Katex output" do
       content = "c = \\pm\\sqrt{a^2 + b^2}"
-
       rendered = generate_katex_liquid(content).render
-
       verify(format: :html) { rendered }
+    end
+
+    it "generates Katex errors" do
+      content = "\\c = \\pm\\sqrt{a^2 + b^2}"
+      rendered = generate_katex_liquid(content).render
+      expect(rendered).to include("ParseError: KaTeX parse error: ")
     end
   end
 end
