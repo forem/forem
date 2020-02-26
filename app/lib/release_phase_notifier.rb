ba1e120037fb94d48933e674741b0fb8acae184e
mstruve <mollylbs@gmail.com>
2020-02-26 12:40:20 -0500

    bail if we have no variables, also dont let Slack API errors break the release phase

--- app/lib/release_phase_notifier.rb
+++ app/lib/release_phase_notifier.rb
@@ -1,5 +1,7 @@
 class ReleasePhaseNotifier
   def self.ping_slack
+    return unless ApplicationConfig["SLACK_WEBHOOK_URL"].present? && ApplicationConfig["SLACK_DEPLOY_CHANNEL"].present?
+
     client = Slack::Notifier.new(
       ApplicationConfig["SLACK_WEBHOOK_URL"],
       channel: ApplicationConfig["SLACK_DEPLOY_CHANNEL"],
@@ -7,5 +9,7 @@ class ReleasePhaseNotifier
     )
 
     client.ping("Release Phase Failed: #{ENV['FAILED_COMMAND']}")
+  rescue Slack::Notifier::APIError => e
+    Rails.logger.info("Slack API error occured: #{e.message}")
   end
 end
--- spec/lib/release_phase_notifier_spec.rb
+++ spec/lib/release_phase_notifier_spec.rb
@@ -2,6 +2,11 @@ require "rails_helper"
 
 RSpec.describe ReleasePhaseNotifier, type: :lib do
   describe ".ping_slack" do
+    before do
+      allow(ApplicationConfig).to receive(:[]).with("SLACK_WEBHOOK_URL").and_return("url")
+      allow(ApplicationConfig).to receive(:[]).with("SLACK_DEPLOY_CHANNEL").and_return("channel")
+    end
+
     it "sends a failure message to slack" do
       mock_slack = Slack::Notifier.new("url")
       ENV["FAILED_COMMAND"] = "rake db:migrate"
@@ -11,6 +16,19 @@ RSpec.describe ReleasePhaseNotifier, type: :lib do
 
       described_class.ping_slack
       expect(mock_slack).to have_received(:ping).with(failure_message)
+    end
+
+    it "bails when config variables are missing" do
+      allow(ApplicationConfig).to receive(:[]).with("SLACK_WEBHOOK_URL").and_return("")
+      allow(ApplicationConfig).to receive(:[]).with("SLACK_DEPLOY_CHANNEL").and_return("")
+
+      expect { described_class.ping_slack }.not_to raise_error
+    end
+
+    it "rescues any Slack API Errors" do
+      allow(Slack::Notifier).to receive(:new).and_raise(Slack::Notifier::APIError.new)
+
+      expect { described_class.ping_slack }.not_to raise_error
     end
   end
 end
