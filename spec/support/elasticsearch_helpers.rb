7d2ca0fb1265289b9dffe7d3173ba33993e8b56a
Alex <alexandersmith223@gmail.com>
2020-02-27 14:41:48 -0800

    Remove manual Array conversion for Array.wrap()

--- spec/support/elasticsearch_helpers.rb
+++ spec/support/elasticsearch_helpers.rb
@@ -1,9 +1,7 @@
 # Only included in specs with elasticsearch: true
 module ElasticsearchHelpers
   def index_documents(resources)
-    resources = [resources] unless resources.respond_to?(:each)
-
-    resources.each(&:index_to_elasticsearch_inline)
+    Array.wrap(resources).each(&:index_to_elasticsearch_inline)
     described_class.refresh_index
   end
 end
