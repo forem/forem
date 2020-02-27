6c219fc58115628b08b6221558b27cd9439f3c6f
Vaidehi Joshi <vaidehi.sj@gmail.com>
2020-02-27 13:53:07 -0800

    Remove sent attribute from broadcasts table
    Now that 6318 and 6319 have been merged, we no longer need the `sent` attribute on the Broadcast model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_192145) do
+ActiveRecord::Schema.define(version: 2020_02_27_214321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -227,7 +227,6 @@ ActiveRecord::Schema.define(version: 2020_02_26_192145) do
     t.boolean "active", default: false
     t.text "body_markdown"
     t.text "processed_html"
-    t.boolean "sent", default: false
     t.string "title"
     t.string "type_of"
   end
