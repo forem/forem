fdab47fe517a75da325b7137853fce0a39dc9deb
lightalloy <lightallloy@gmail.com>
2020-02-28 11:28:01 +0300

    Moving sponsorships to /internal

--- app/controllers/internal/sponsorships_controller.rb
+++ app/controllers/internal/sponsorships_controller.rb
@@ -5,11 +5,22 @@ class Internal::SponsorshipsController < Internal::ApplicationController
     @sponsorships = Sponsorship.includes(:organization, :user).order("created_at desc").page(params[:page]).per(50)
   end
 
-  def new
-    @sponsorship = Sponsorship.new
+  def edit
+    @sponsorship = Sponsorship.find(params[:id])
   end
 
-  def create
-    @sponsorship = Sponsorship.new
+  def update
+    @sponsorship = Sponsorship.find(params[:id])
+    if @sponsorship.update(sponsorship_params)
+      redirect_to internal_sponsorships_path
+    else
+      render action: :edit
+    end
+  end
+
+  private
+
+  def sponsorship_params
+    params.require(:sponsorship).permit(%i[status expired_at])
   end
 end
