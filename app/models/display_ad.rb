194f9bd0fbd771a3cea10b5a4fa0f9f9f928a8ae
cyrillefr <cyrille.vasseur@gmail.com>
2020-03-02 18:50:36 +0100

    Excludes some files from linting and deletes redundancies (#6275)

--- app/models/display_ad.rb
+++ app/models/display_ad.rb
@@ -26,11 +26,10 @@ class DisplayAd < ApplicationRecord
     renderer = Redcarpet::Render::HTMLRouge.new(hard_wrap: true, filter_html: false)
     markdown = Redcarpet::Markdown.new(renderer)
     initial_html = markdown.render(body_markdown)
-
     # Temporarily disable the sanitisation in order to launch the SheCoded Campaign.
     # TODO: [@thepracticaldev/cool] find an alternate solution.
 
-    # stripped_html = ActionController::Base.helpers.sanitize initial_html.html_safe,
+    # stripped_html = ActionController::Base.helpers.sanitize initial_html,
     #                                                         tags: %w[a em i b u br img h1 h2 h3 h4 div style],
     #                                                         attributes: %w[href target src height width style]
     stripped_html = initial_html.html_safe
