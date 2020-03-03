1f6dad4ffca335c807d85904ab3543700fa57822
cyrillefr <cyrille.vasseur@gmail.com>
2020-03-03 15:49:52 +0100

    Fixes html_safe issues from comment domain
     - use of html_safe is switched from views to other areas

--- app/liquid_tags/tweet_tag.rb
+++ app/liquid_tags/tweet_tag.rb
@@ -22,26 +22,27 @@ class TweetTag < LiquidTagBase
   end
 
   def self.script
-    'var videoPreviews = document.getElementsByClassName("ltag__twitter-tweet__media__video-wrapper");
-      [].forEach.call(videoPreviews, function(el){
-        el.onclick= function(e){
-          var divHeight = el.offsetHeight;
-          el.style.maxHeight = divHeight + "px";
-          el.getElementsByClassName("ltag__twitter-tweet__media--video-preview")[0].style.display = "none";
-          el.getElementsByClassName("ltag__twitter-tweet__video")[0].style.display = "block";
-          el.getElementsByTagName("video")[0].play();
-        }
-      })
-      var tweets = document.getElementsByClassName("ltag__twitter-tweet__main");
-      [].forEach.call(tweets, function(tweet){
-        tweet.onclick= function(e){
-          if (e.target.nodeName == "A" || e.target.parentElement.nodeName == "A"){
-            return;
+    "".html_safe +
+      'var videoPreviews = document.getElementsByClassName("ltag__twitter-tweet__media__video-wrapper");
+        [].forEach.call(videoPreviews, function(el){
+          el.onclick= function(e){
+            var divHeight = el.offsetHeight;
+            el.style.maxHeight = divHeight + "px";
+            el.getElementsByClassName("ltag__twitter-tweet__media--video-preview")[0].style.display = "none";
+            el.getElementsByClassName("ltag__twitter-tweet__video")[0].style.display = "block";
+            el.getElementsByTagName("video")[0].play();
           }
-          window.open(tweet.dataset.url,"_blank");
-        }
-      });
-      '
+        })
+        var tweets = document.getElementsByClassName("ltag__twitter-tweet__main");
+        [].forEach.call(tweets, function(tweet){
+          tweet.onclick= function(e){
+            if (e.target.nodeName == "A" || e.target.parentElement.nodeName == "A"){
+              return;
+            }
+            window.open(tweet.dataset.url,"_blank");
+          }
+        });
+        '
   end
 
   private
