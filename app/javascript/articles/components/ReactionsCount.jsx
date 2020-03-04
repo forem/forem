758d1d537dbfe8df663bf961299c1d4569f11dc7
Alex <alexandersmith223@gmail.com>
2020-03-04 10:43:57 -0800

    Now reactions count are always shown. (#6456) [deploy]

--- app/javascript/articles/components/ReactionsCount.jsx
+++ app/javascript/articles/components/ReactionsCount.jsx
@@ -5,23 +5,19 @@ import { articlePropTypes } from '../../src/components/common-prop-types';
 export const ReactionsCount = ({ article, icon }) => {
   const totalReactions = article.positive_reactions_count || 0;
 
-  if (totalReactions > 0) {
-    return (
-      <div className="article-engagement-count reactions-count">
-        <a href={article.path}>
-          <img src={icon} alt="heart" loading="lazy" />
-          <span
-            id={`engagement-count-number-${article.id}`}
-            className="engagement-count-number"
-          >
-            {totalReactions}
-          </span>
-        </a>
-      </div>
-    );
-  }
-
-  return null;
+  return (
+    <div className="article-engagement-count reactions-count">
+      <a href={article.path}>
+        <img src={icon} alt="heart" loading="lazy" />
+        <span
+          id={`engagement-count-number-${article.id}`}
+          className="engagement-count-number"
+        >
+          {totalReactions}
+        </span>
+      </a>
+    </div>
+  );
 };
 
 ReactionsCount.propTypes = {
