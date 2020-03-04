33c5d13c05adc61956830a61d869a72989aa8cee
mstruve <mollylbs@gmail.com>
2020-03-04 16:06:09 -0500

    Create hypershield schema to shield sensitive data in production

---
+++ config/initializers/hypershield.rb
@@ -0,0 +1,16 @@
+Hypershield.schemas = {
+  hypershield: {
+    hide: %w[
+      auth_data_dump
+      encrypted
+      message
+      message_html
+      message_markdown
+      password
+      previous_refresh_token
+      refresh_token
+      secret
+      token
+    ]
+  }
+}
