dd1c435eebb8412db1056ed3a538149120e8b21a
Nick Taylor <nick@iamdeveloper.com>
2020-03-04 12:42:53 -0500

    Now reactions count are always shown.

--- app/javascript/articles/__tests__/FeaturedArticle.test.jsx
+++ app/javascript/articles/__tests__/FeaturedArticle.test.jsx
@@ -5,6 +5,10 @@ import '../../../assets/javascripts/lib/xss';
 import '../../../assets/javascripts/utilities/timeAgo';
 import { FeaturedArticle } from '../FeaturedArticle';
 
+const featuredArticleNoReactions = {
+  ...featuredArticle,
+  positive_reactions_count: 0,
+};
 const commonProps = {
   reactionsIcon: assetPath('reactions-stack.png'),
   commentsIcon: assetPath('comments-bubble.png'),
@@ -17,6 +21,18 @@ describe('<FeaturedArticle /> component', () => {
         {...commonProps}
         isBookmarked={false}
         article={featuredArticle}
+        currentTag="javascript"
+      />,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render with the reactions icon when the reactions count is 0', () => {
+    const tree = render(
+      <FeaturedArticle
+        {...commonProps}
+        isBookmarked={false}
+        article={featuredArticleNoReactions}
         currentTag="javascript"
       />,
     );
