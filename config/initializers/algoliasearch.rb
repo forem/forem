3ec46f517f6bcf04e537a092f2947f50a6af79ec
Alex <alexandersmith223@gmail.com>
2020-03-05 09:23:22 -0800

    Remove more Algolia from ClassifiedListing (#6463) [deploy]
    * Remove ClassifiedListing from algolia initializer
    
    * Remove reference to remove_from_index

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -11,7 +11,6 @@ else
     restrictIndices: [
       "searchables_#{Rails.env}",
       "ordered_articles_#{Rails.env}",
-      "ClassifiedListing_#{Rails.env}",
       "ordered_articles_by_published_at_#{Rails.env}",
       "ordered_articles_by_positive_reactions_count_#{Rails.env}",
       "ordered_comments_#{Rails.env}",
