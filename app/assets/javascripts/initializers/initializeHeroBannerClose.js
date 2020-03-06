38c4883794685c0ada41238cbbab6e4b68b2539d
benhalpern <bendhalpern@gmail.com>
2020-03-06 08:28:37 -0500

    Switch from cookies to localstorage for hero banner

--- app/assets/javascripts/initializers/initializeHeroBannerClose.js
+++ app/assets/javascripts/initializers/initializeHeroBannerClose.js
@@ -1,21 +1,12 @@
 'use strict';
 
-function setExpiryForCookie() {
-  // in the future we probably want to set the expiry value dynamically from the campaign config
-  // based on how long the campaign is running for.
-  const date = new Date();
-  const daysUntilExpiry = 5;
-  date.setDate(date.getDate() + daysUntilExpiry);
-  return date.toGMTString();
-}
-
 function initializeHeroBannerClose() {
   let banner = document.getElementById('js-hero-banner');
   let closeIcon = document.getElementById('js-hero-banner__x');
 
   if (banner && closeIcon) {
     closeIcon.addEventListener('click', () => {
-      document.cookie = `heroBanner=false; expires=${setExpiryForCookie()};`;
+      localStorage.setItem('exited_hero', 'shecoded'); // Hardcoded. TODO: generalize.
       banner.style.display = 'none';
     });
   }
