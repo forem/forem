ed55aace9bba0bae7f311954c7cdbc55c7929ef3
Mac Siri <krairit.siri@gmail.com>
2020-03-05 20:53:16 -0500

    Add spec

--- spec/liquid_tags/katex_tag_spec.rb
+++ spec/liquid_tags/katex_tag_spec.rb
@@ -13,6 +13,20 @@ RSpec.describe KatexTag, type: :liquid_tag do
       verify(format: :html) { rendered }
     end
 
+    it "includes the css style tag only once when rendering multiple" do
+      content = <<~CONTENT
+        {% katex %}
+          hello
+        {% endkatex %}
+        {% katex %}
+          Hello
+        {% endkatex %}
+      CONTENT
+      Liquid::Template.register_tag("katex", described_class)
+      render = Liquid::Template.parse(content).render
+      expect(Nokogiri::HTML(render).css("link").count).to eq(1)
+    end
+
     it "generates Katex errors" do
       content = "\\c = \\pm\\sqrt{a^2 + b^2}"
       rendered = generate_katex_liquid(content).render
