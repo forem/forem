a91580c4cbc39f4fb76914009fab46b56223cb90
Ridhwana <Ridhwana.Khan16@gmail.com>
2020-03-06 15:01:35 +0200

    Cookie Expiry for the Campaign Hero Banner and Event Listener update (#6496) [deploy]
    * refactor: add an eventListener for click
    
    * chore: oops
    
    * feat: add expiry for the cookie 5 days from now
    
    * chore: update comment
    
    * chore: add strict back

--- app/assets/javascripts/initializers/initializeHeroBannerClose.js
+++ app/assets/javascripts/initializers/initializeHeroBannerClose.js
@@ -1,13 +1,22 @@
 'use strict';
 
+function setExpiryForCookie() {
+  // in the future we probably want to set the expiry value dynamically from the campaign config
+  // based on how long the campaign is running for.
+  const date = new Date();
+  const daysUntilExpiry = 5;
+  date.setDate(date.getDate() + daysUntilExpiry);
+  return date.toGMTString();
+}
+
 function initializeHeroBannerClose() {
-  let banner = document.getElementById("js-hero-banner");
-  let closeIcon = document.getElementById("js-hero-banner__x");
+  let banner = document.getElementById('js-hero-banner');
+  let closeIcon = document.getElementById('js-hero-banner__x');
 
   if (banner && closeIcon) {
-    closeIcon.onclick = (e) => {
-      document.cookie = "heroBanner=false";
-      banner.style.display = "none";
-    }
+    closeIcon.addEventListener('click', () => {
+      document.cookie = `heroBanner=false; expires=${setExpiryForCookie()};`;
+      banner.style.display = 'none';
+    });
   }
 }
