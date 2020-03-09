a66dc2693abd86d6d82954deb5355c77050e301d
Vaidehi Joshi <vaidehi.sj@gmail.com>
2020-03-09 14:38:39 -0700

    Refactor debounce usage into debounceAction util (#6506)
    * Refactor debounce usage into debounceAction util
    
    * Allow debounceAction's config to be merged, allow for named arguments
    
    Also clean up the documentation as necessary!

--- app/javascript/chat/chat.jsx
+++ app/javascript/chat/chat.jsx
@@ -1,6 +1,5 @@
 import { h, Component } from 'preact';
 import PropTypes from 'prop-types';
-import debounce from 'lodash.debounce';
 import ConfigImage from '../../assets/images/three-dots.svg';
 import {
   conductModeration,
@@ -25,6 +24,7 @@ import Video from './video';
 import View from './view';
 
 import setupPusher from '../src/utils/pusher';
+import debounceAction from '../src/utils/debounceAction';
 
 export default class Chat extends Component {
   static propTypes = {
@@ -39,9 +39,8 @@ export default class Chat extends Component {
     const chatChannels = JSON.parse(props.chatChannels);
     const chatOptions = JSON.parse(props.chatOptions);
 
-    this.debouncedChannelFilter = debounce(
+    this.debouncedChannelFilter = debounceAction(
       this.triggerChannelFilter.bind(this),
-      300,
     );
 
     this.state = {
