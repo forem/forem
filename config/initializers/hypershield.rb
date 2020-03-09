3a7b8781decc0472fda6bd124c57acfbb2c6a527
Molly Struve <mollylbs@gmail.com>
2020-03-09 08:54:31 -0400

    Create Hypershield Schema to Shield Sensitive data in Production (#6461) [deploy]

---
+++ config/initializers/hypershield.rb
@@ -0,0 +1,18 @@
+Hypershield.schemas = {
+  hypershield: {
+    hide: %w[
+      auth_data_dump
+      email
+      encrypted
+      encrypted_password
+      message
+      message_html
+      message_markdown
+      password
+      previous_refresh_token
+      refresh_token
+      secret
+      token
+    ]
+  }
+}
