2b313b97a1406e8bbf82224d75d94f21d3de2f6f
jitendra <jitendrarajpurohit@skynox.tech>
2020-03-10 17:40:29 +0530

    error message formatting updated

--- app/controllers/chat_channels_controller.rb
+++ app/controllers/chat_channels_controller.rb
@@ -30,11 +30,11 @@ class ChatChannelsController < ApplicationController
   end
 
   def update
-    ChatChannelUpdateService.new(@chat_channel, chat_channel_params).update
-    if @chat_channel.valid?
+    if ChatChannelUpdateService.new(@chat_channel, chat_channel_params).update
       flash[:settings_notice] = "Channel Settings Updated."
     else
-      flash[:settings_notice] = "#{@chat_channel.errors.full_messages}"
+      flash[:settings_notice] = @chat_channel.valid? ? @chat_channel.errors.full_messages.to_sentence :
+        "Channel Settings Updation Failed. Try Again Later."
     end
     redirect_to "/chat_channel_memberships/#{@chat_channel.mod_memberships.where(user_id: current_user.id).first.id}/edit"
   end
