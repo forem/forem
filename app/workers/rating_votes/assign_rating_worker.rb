724482b5f8c1d2cae3b90da8e0f4d912ffda27b9
Ben Halpern <bendhalpern@gmail.com>
2020-03-10 10:59:49 -0400

    Create implicit experience level rating vote upon creating of readinglist reaction (#6522) [deploy]
    * Do not render or link to empty rss feeds
    
    * s
    
    * Move calculation to worker
    
    * remove xit test
    
    * Also allow implicit rating votes in comments
    
    * Fix alignment issue
    
    * Remove comment logic

---
+++ app/workers/rating_votes/assign_rating_worker.rb
@@ -0,0 +1,21 @@
+module RatingVotes
+  class AssignRatingWorker
+    include Sidekiq::Worker
+
+    sidekiq_options queue: :low_priority, retry: 10
+
+    def perform(article_id, group = "experience_level")
+      article = Article.find_by(id: article_id)
+      return unless article
+
+      ratings = article.rating_votes.where(group: group).pluck(:rating)
+      average = ratings.sum / ratings.size
+
+      article.update_columns(
+        experience_level_rating: average,
+        experience_level_rating_distribution: ratings.max - ratings.min,
+        last_experience_level_rating_at: Time.current,
+      )
+    end
+  end
+end
