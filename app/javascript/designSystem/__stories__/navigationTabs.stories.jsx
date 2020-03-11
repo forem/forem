f91db3ebc673ec5a9782b8d5ec3748f5342fcdd7
Nick Taylor <nick@iamdeveloper.com>
2020-03-11 14:24:48 -0400

    Split up navigation tab stories.

--- app/javascript/designSystem/__stories__/navigationTabs.stories.jsx
+++ app/javascript/designSystem/__stories__/navigationTabs.stories.jsx
@@ -3,25 +3,23 @@ import { storiesOf } from '@storybook/react';
 
 import './typography.scss';
 
-storiesOf('Base/Components/Navigation', module).add('Navigation Tabs', () => (
-  <div className="container">
-    <div className="body">
+storiesOf('Base/Components/Navigation/Tabs', module)
+  .add('Description', () => (
+    <div className="container">
       <h2>Navigation: Tabs</h2>
       <p>Use tabs as 2nd level navigation or filtering options.</p>
     </div>
-
-    <div>
-      <div className="crayons-tabs">
-        <a href="#" className="crayons-tabs__item crayons-tabs__item--current">
-          Feed
-        </a>
-        <a href="#" className="crayons-tabs__item">
-          Popular
-        </a>
-        <a href="#" className="crayons-tabs__item">
-          Latest
-        </a>
-      </div>
+  ))
+  .add('Default', () => (
+    <div className="crayons-tabs">
+      <a href="/" className="crayons-tabs__item crayons-tabs__item--current">
+        Feed
+      </a>
+      <a href="/" className="crayons-tabs__item">
+        Popular
+      </a>
+      <a href="/" className="crayons-tabs__item">
+        Latest
+      </a>
     </div>
-  </div>
-));
+  ));
