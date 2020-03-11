17c5baebf8ee75a6212769105e7636445db51f13
Nick Taylor <nick@iamdeveloper.com>
2020-03-11 16:49:00 -0400

    Split up Button stories.

--- app/javascript/designSystem/__stories__/buttons.stories.jsx
+++ app/javascript/designSystem/__stories__/buttons.stories.jsx
@@ -3,9 +3,9 @@ import { storiesOf } from '@storybook/react';
 
 import './typography.scss';
 
-storiesOf('Base/Components', module).add('Buttons', () => (
-  <div className="container">
-    <div className="body">
+storiesOf('Base/Components/Buttons', module)
+  .add('Description', () => (
+    <div className="container">
       <h2>Buttons</h2>
       <p>
         Use Danger style only for destructive actions like removing something.
@@ -26,64 +26,63 @@ storiesOf('Base/Components', module).add('Buttons', () => (
         vertically).
       </p>
     </div>
-    <div>
-      <a href="#" className="crayons-btn">
-        Button label
-      </a>
-      <br />
-      <br />
-      <a href="#" className="crayons-btn crayons-btn--full">
-        Full Button label
-      </a>
-      <br />
-      <br />
-      <a href="#" className="crayons-btn crayons-btn--secondary">
-        Secondary Button label
-      </a>
-      <br />
-      <br />
-      <a href="#" className="crayons-btn crayons-btn--outlined">
-        Outlined Button label
-      </a>
-      <br />
-      <br />
-      <a href="#" className="crayons-btn crayons-btn--text">
-        Text Button label
-      </a>
-      <br />
-      <br />
-      <a href="#" className="crayons-btn crayons-btn--danger">
-        Danger Button label
-      </a>
-      <br />
-      <br />
-      <a href="#" className="crayons-btn crayons-btn--icon-left">
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          className="crayons-icon"
-        >
-          <path d="M9.99999 15.172L19.192 5.979L20.607 7.393L9.99999 18L3.63599 11.636L5.04999 10.222L9.99999 15.172Z" />
-        </svg>
-        Button
-      </a>
-      <br />
-      <br />
-      <a
-        href="#"
-        className="crayons-btn crayons-btn--secondary crayons-btn--full crayons-btn--icon-left"
+  ))
+  .add('Default', () => (
+    <a href="/" className="crayons-btn">
+      Button label
+    </a>
+  ))
+  .add('Full', () => (
+    <a href="/" className="crayons-btn crayons-btn--full">
+      Full Button label
+    </a>
+  ))
+  .add('Secondary', () => (
+    <a href="/" className="crayons-btn crayons-btn--secondary">
+      Secondary Button label
+    </a>
+  ))
+  .add('Outlined', () => (
+    <a href="/" className="crayons-btn crayons-btn--outlined">
+      Outlined Button label
+    </a>
+  ))
+  .add('Text', () => (
+    <a href="/" className="crayons-btn crayons-btn--text">
+      Text Button label
+    </a>
+  ))
+  .add('Danger', () => (
+    <a href="/" className="crayons-btn crayons-btn--danger">
+      Danger Button label
+    </a>
+  ))
+  .add('Icon Left', () => (
+    <a href="/" className="crayons-btn crayons-btn--icon-left">
+      <svg
+        width="24"
+        height="24"
+        xmlns="http://www.w3.org/2000/svg"
+        className="crayons-icon"
       >
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          className="crayons-icon"
-        >
-          <path d="M9.99999 15.172L19.192 5.979L20.607 7.393L9.99999 18L3.63599 11.636L5.04999 10.222L9.99999 15.172Z" />
-        </svg>
-        Button
-      </a>
-    </div>
-  </div>
-));
+        <path d="M9.99999 15.172L19.192 5.979L20.607 7.393L9.99999 18L3.63599 11.636L5.04999 10.222L9.99999 15.172Z" />
+      </svg>
+      Button
+    </a>
+  ))
+  .add('Secondary/Full/Icon Left', () => (
+    <a
+      href="/"
+      className="crayons-btn crayons-btn--secondary crayons-btn--full crayons-btn--icon-left"
+    >
+      <svg
+        width="24"
+        height="24"
+        xmlns="http://www.w3.org/2000/svg"
+        className="crayons-icon"
+      >
+        <path d="M9.99999 15.172L19.192 5.979L20.607 7.393L9.99999 18L3.63599 11.636L5.04999 10.222L9.99999 15.172Z" />
+      </svg>
+      Button
+    </a>
+  ));
