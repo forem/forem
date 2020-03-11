0653785296c42fe392dc9265e6ebce2923f7cbac
Nick Taylor <nick@iamdeveloper.com>
2020-03-11 19:48:39 -0400

    Split up Form Component stories.

--- app/javascript/designSystem/__stories__/formElements.stories.jsx
+++ app/javascript/designSystem/__stories__/formElements.stories.jsx
@@ -3,62 +3,72 @@ import { storiesOf } from '@storybook/react';
 
 import './typography.scss';
 
-storiesOf('Base/Components/HTML', module).add('Form Components', () => (
-  <div className="container">
-    <div className="body">
+storiesOf('Base/Components/HTML/Form Components', module).add(
+  'Description',
+  () => (
+    <div className="container">
       <h2>Form elements</h2>
     </div>
-    <div>
-      <input
-        type="text"
-        className="crayons-textfield"
-        placeholder="This is placeholder..."
-      />
-      <br />
-      <br />
-      <input
-        type="text"
-        className="crayons-textfield"
-        placeholder="This is placeholder..."
-        value="Disabled field"
-        disabled
-      />
-    </div>
+  ),
+);
 
-    <div>
-      <textarea
-        className="crayons-textfield"
-        placeholder="This is placeholder..."
-      />
-      <br />
-      <br />
-      <textarea
-        className="crayons-textfield"
-        placeholder="This is placeholder..."
-        disabled
-      >
-        Disabled textarea
-      </textarea>
-    </div>
+storiesOf('Base/Components/HTML/Form Components/Text Field', module)
+  .add('Default', () => (
+    <input
+      type="text"
+      className="crayons-textfield"
+      placeholder="This is placeholder..."
+    />
+  ))
+  .add('Disabled', () => (
+    <input
+      type="text"
+      className="crayons-textfield"
+      placeholder="This is placeholder..."
+      value="Disabled field"
+      disabled
+    />
+  ));
 
-    <div>
-      <input type="checkbox" className="crayons-checkbox" />
-      <input type="checkbox" className="crayons-checkbox" checked />
-      <input type="checkbox" className="crayons-checkbox" disabled />
-      <input type="checkbox" className="crayons-checkbox" checked disabled />
-    </div>
+storiesOf('Base/Components/HTML/Form Components/Multiline Text Field', module)
+  .add('Default', () => (
+    <textarea
+      className="crayons-textfield"
+      placeholder="This is placeholder..."
+    />
+  ))
+  .add('Disabled', () => (
+    <textarea
+      className="crayons-textfield"
+      placeholder="This is placeholder..."
+      disabled
+    >
+      Disabled textarea
+    </textarea>
+  ));
 
-    <div>
-      <input type="radio" name="n1" className="crayons-radio" />
-      <input type="radio" name="n1" className="crayons-radio" checked />
-      <input type="radio" name="n2" className="crayons-radio" disabled />
-      <input
-        type="radio"
-        name="n2"
-        className="crayons-radio"
-        checked
-        disabled
-      />
-    </div>
-  </div>
-));
+storiesOf('Base/Components/HTML/Form Components/Checkbox', module)
+  .add('Default', () => <input type="checkbox" className="crayons-checkbox" />)
+  .add('Checked', () => (
+    <input type="checkbox" className="crayons-checkbox" checked />
+  ))
+  .add('Disabled', () => (
+    <input type="checkbox" className="crayons-checkbox" disabled />
+  ))
+  .add('Checked (Disabled)', () => (
+    <input type="checkbox" className="crayons-checkbox" checked disabled />
+  ));
+
+storiesOf('Base/Components/HTML/Form Components/Radio Button', module)
+  .add('Default', () => (
+    <input type="radio" name="n1" className="crayons-radio" />
+  ))
+  .add('Checked', () => (
+    <input type="radio" name="n1" className="crayons-radio" checked />
+  ))
+  .add('Disabled', () => (
+    <input type="radio" name="n2" className="crayons-radio" disabled />
+  ))
+  .add('Checked (Disabled)', () => (
+    <input type="radio" name="n2" className="crayons-radio" checked disabled />
+  ));
