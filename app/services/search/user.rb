1b030c441924a15551872ad31e94c151a068e211
mstruve <mollylbs@gmail.com>
2020-03-12 10:12:01 -0400

    Implement user search backend

--- app/services/search/user.rb
+++ app/services/search/user.rb
@@ -3,9 +3,48 @@ module Search
     INDEX_NAME = "users_#{Rails.env}".freeze
     INDEX_ALIAS = "users_#{Rails.env}_alias".freeze
     MAPPINGS = JSON.parse(File.read("config/elasticsearch/mappings/users.json"), symbolize_names: true).freeze
+    DEFAULT_PAGE = 0
+    DEFAULT_PER_PAGE = 20
 
     class << self
+      def search_documents(params:)
+        set_query_size(params)
+        query_hash = Search::QueryBuilders::User.new(params).as_hash
+
+        results = search(body: query_hash)
+        hits = results.dig("hits", "hits").map do |user_doc|
+          prepare_doc(user_doc.dig("_source"))
+        end
+        paginate_hits(hits, params)
+      end
+
       private
+
+      def prepare_doc(hit)
+        {
+          user: {
+            username: hit["username"],
+            name: hit["username"],
+            profile_image_90: hit["profile_image_90"]
+          },
+          title: hit["name"],
+          path: hit["path"],
+          id: hit["id"]
+        }
+      end
+
+      def set_query_size(params)
+        params[:page] ||= DEFAULT_PAGE
+        params[:per_page] ||= DEFAULT_PER_PAGE
+
+        # pages start at 0
+        params[:size] = params[:per_page].to_i * (params[:page].to_i + 1)
+      end
+
+      def paginate_hits(hits, params)
+        start = params[:per_page] * params[:page]
+        hits[start, params[:per_page]] || []
+      end
 
       def index_settings
         if Rails.env.production?
