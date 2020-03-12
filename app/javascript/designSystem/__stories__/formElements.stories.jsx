8edfbfcd3aff5f6c1fd9374caf84c85e6be7fa6f
Nick Taylor <nick@iamdeveloper.com>
2020-03-11 21:40:01 -0400

    Split up Form Component stories some more.

--- app/javascript/designSystem/__stories__/formElements.stories.jsx
+++ app/javascript/designSystem/__stories__/formElements.stories.jsx
@@ -2,12 +2,43 @@ import { h } from 'preact';
 import { storiesOf } from '@storybook/react';
 
 import './typography.scss';
+import { defaultChildrenPropTypes } from '../../src/components/common-prop-types';
+
+const Fieldset = ({ children }) => (
+  <fieldset style={{ border: 'none' }}>{children}</fieldset>
+);
+
+Fieldset.propTypes = {
+  children: defaultChildrenPropTypes.isRequired,
+};
 
 storiesOf('Base/Components/HTML/Form Components', module).add(
   'Description',
   () => (
     <div className="container">
       <h2>Form elements</h2>
+      <p>
+        Because of accessibility most (ideally all) fields should have label
+        above.
+      </p>
+      <p>
+        Fields can also have optional description - between Label and Field
+        itself.
+      </p>
+      <p>
+        Fields can also have additional optional description, for example
+        characters count.
+      </p>
+      <h3>Fields with Checkboxes & Radios</h3>
+      <p>
+        Labels for checkboxes and radios should be placed next to the form
+        element.
+      </p>
+      <p>Using additional description is optional.</p>
+      <p>
+        It is possible to group checkboxes or radios into logical sections.
+        Section may require having it’s own label (title).
+      </p>
     </div>
   ),
 );
@@ -28,6 +59,25 @@ storiesOf('Base/Components/HTML/Form Components/Text Field', module)
       value="Disabled field"
       disabled
     />
+  ))
+  .add('With <label /> and Descriptions', () => (
+    <div className="crayons-field">
+      <label htmlFor="t1" className="crayons-field__label">
+        Textfield Label
+        <p className="crayons-field__description">
+          This is some description for a textfield lorem ipsum...
+        </p>
+      </label>
+      <input
+        type="text"
+        id="t1"
+        className="crayons-textfield"
+        placeholder="This is placeholder..."
+      />
+      <p className="crayons-field__description">
+        Another description just in case...
+      </p>
+    </div>
   ));
 
 storiesOf('Base/Components/HTML/Form Components/Multiline Text Field', module)
@@ -57,6 +107,62 @@ storiesOf('Base/Components/HTML/Form Components/Checkbox', module)
   ))
   .add('Checked (Disabled)', () => (
     <input type="checkbox" className="crayons-checkbox" checked disabled />
+  ))
+  .add('Checkbox with <label />', () => (
+    <div className="crayons-field crayons-field--checkbox">
+      <input type="checkbox" id="c2" className="crayons-checkbox" />
+      <label htmlFor="c2" className="crayons-field__label">
+        Raspberry
+      </label>
+    </div>
+  ))
+  .add('Checkbox with <label /> and Description', () => (
+    <div className="crayons-field crayons-field--checkbox">
+      <input type="checkbox" id="c2" className="crayons-checkbox" />
+      <label htmlFor="c2" className="crayons-field__label">
+        Raspberry
+        <p className="crayons-field__description">
+          This is some description for a textfield lorem ipsum...
+        </p>
+      </label>
+    </div>
+  ))
+  .add('Checkbox Group', () => (
+    <Fieldset>
+      <div className="crayons-field crayons-field--checkbox">
+        <input
+          type="checkbox"
+          name="checkboxGroup"
+          id="c1"
+          className="crayons-checkbox"
+        />
+        <label htmlFor="c1" className="crayons-field__label">
+          Raspberry
+        </label>
+      </div>
+      <div className="crayons-field crayons-field--checkbox">
+        <input
+          type="checkbox"
+          name="checkboxGroup"
+          id="c2"
+          className="crayons-checkbox"
+        />
+        <label htmlFor="c2" className="crayons-field__label">
+          Strawberry
+        </label>
+      </div>
+      <div className="crayons-field crayons-field--checkbox">
+        <input
+          type="checkbox"
+          name="checkboxGroup"
+          id="c3"
+          className="crayons-checkbox"
+        />
+        <label htmlFor="c3" className="crayons-field__label">
+          Blueberry
+        </label>
+      </div>
+    </Fieldset>
   ));
 
 storiesOf('Base/Components/HTML/Form Components/Radio Button', module)
@@ -71,4 +177,60 @@ storiesOf('Base/Components/HTML/Form Components/Radio Button', module)
   ))
   .add('Checked (Disabled)', () => (
     <input type="radio" name="n2" className="crayons-radio" checked disabled />
+  ))
+  .add('With <label />', () => (
+    <div className="crayons-field crayons-field--radio">
+      <input type="radio" name="name1" id="r2" className="crayons-radio" />
+      <label htmlFor="r2" className="crayons-field__label">
+        Raspberry
+      </label>
+    </div>
+  ))
+  .add('With <label /> and Description', () => (
+    <div className="crayons-field crayons-field--radio">
+      <input type="radio" name="name1" id="r2" className="crayons-radio" />
+      <label htmlFor="r2" className="crayons-field__label">
+        Raspberry
+        <p className="crayons-field__description">
+          This is some description for a textfield lorem ipsum...
+        </p>
+      </label>
+    </div>
+  ))
+  .add('Radio Button Group', () => (
+    <Fieldset>
+      <div className="crayons-field crayons-field--radio">
+        <input
+          type="radio"
+          name="radioGroup"
+          id="r1"
+          className="crayons-radio"
+        />
+        <label htmlFor="r1" className="crayons-field__label">
+          Raspberry
+        </label>
+      </div>
+      <div className="crayons-field crayons-field--radio">
+        <input
+          type="radio"
+          name="radioGroup"
+          id="r2"
+          className="crayons-radio"
+        />
+        <label htmlFor="r2" className="crayons-field__label">
+          Strawberry
+        </label>
+      </div>
+      <div className="crayons-field crayons-field--radio">
+        <input
+          type="radio"
+          name="radioGroup"
+          id="r3"
+          className="crayons-radio"
+        />
+        <label htmlFor="r3" className="crayons-field__label">
+          Blueberry
+        </label>
+      </div>
+    </Fieldset>
   ));
