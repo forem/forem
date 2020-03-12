ce692c9fbd768b1ee874e35398378d27d50077e9
Liz Fong-Jones <lizf@honeycomb.io>
2020-03-12 12:25:33 -0400

    Make prerun.sh actually set the environment

--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -6,5 +6,5 @@ if [ "$DYNOTYPE" == "scheduler" ] || [ "$DYNOTYPE" == "run" ]; then
   # Disable Automatic Beeline integrations to prevent unnecessary
   # data from being tracked in Honeycomb
   # more info here: https://docs.honeycomb.io/getting-data-in/ruby/beeline/#using-env-variables-to-control-framework-integrations
-  HONEYCOMB_DISABLE_AUTOCONFIGURE="true"
+  export HONEYCOMB_DISABLE_AUTOCONFIGURE="true"
 fi
