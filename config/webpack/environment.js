aeb264444de7c620ffd164cbfafada27e9ff4286
Nick Taylor <nick@iamdeveloper.com>
2020-03-16 15:49:11 -0400

    Added a vendor chunk which gets code split wherever it's needed.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,8 +2,30 @@
 const { environment } = require('@rails/webpacker');
 const erb = require('./loaders/erb');
 
-// Enable the default config
-environment.splitChunks();
+environment.splitChunks(config => {
+  return {
+    ...config,
+    optimization: {
+      ...config.optimization,
+      splitChunks: {
+        ...config.optimization.splitChunks,
+        cacheGroups: {
+          vendor: {
+            test: /node_modules/,
+            chunks: 'initial',
+            name: 'vendor',
+            enforce: true,
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
+        },
+      },
+    },
+  };
+});
 
 // We don't want babel-loader running on the node_modules folder.
 environment.loaders.delete('nodeModules');
