7fdcba5af2001c5e0225521f4c2387e14a68174a
Michael Kohl <citizen428@gmail.com>
2020-03-17 16:42:41 +0700

    Allow KaTeX to render inline (#6670) [deploy]

--- app/liquid_tags/katex_tag.rb
+++ app/liquid_tags/katex_tag.rb
@@ -9,11 +9,12 @@ class KatexTag < Liquid::Block
   def render(context)
     block = Nokogiri::HTML(super).at("body").text
 
-    parsed_content = begin
-                       Katex.render(block, display_mode: true)
-                     rescue ExecJS::ProgramError => e
-                       e.message
-                     end
+    parsed_content =
+      begin
+        Katex.render(block, display_mode: !inline?)
+      rescue ExecJS::ProgramError => e
+        e.message
+      end
 
     should_render_css = !context[KATEX_EXISTED]
 
@@ -23,8 +24,18 @@ class KatexTag < Liquid::Block
 
     ActionController::Base.new.render_to_string(
       partial: PARTIAL,
-      locals: { parsed_content: parsed_content, should_render_css: should_render_css },
+      locals: {
+        parsed_content: parsed_content,
+        should_render_css: should_render_css,
+        inline: inline?
+      },
     )
+  end
+
+  private
+
+  def inline?
+    @inline ||= @markup.split(" ").include?("inline")
   end
 end
 
