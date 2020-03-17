cb4d41025eaf0206328d5e3df28e40c7d2c92749
Nick Taylor <nick@iamdeveloper.com>
2020-03-17 08:00:29 -0400

    Added an explanation about the webpack splitChunks configuration.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,6 +2,14 @@
 const { environment } = require('@rails/webpacker');
 const erb = require('./loaders/erb');
 
+/*
+The customizations below are to create the vendor chunk. The vendor chunk is no longer consumed like it was in webpacker 3.
+There is no longer one vendor bundle. It gets code split based on what webpacker packs need. All the object spreading e.g. `...config.optimization` is to keep
+the existing configuration and only override/add what is necessary.
+
+The cache groups section is the default cache groups in webpack 4. See https://webpack.js.org/plugins/split-chunks-plugin/#optimizationsplitchunks.
+It does not appear to be the default with webpacker 4.
+*/
 environment.splitChunks(config => {
   return {
     ...config,
