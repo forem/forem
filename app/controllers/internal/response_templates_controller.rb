923ce8b9318ca5e3410c867e398e693e395dc297
Zhao-Andy <17884966+Zhao-Andy@users.noreply.github.com>
2020-03-17 08:59:10 -0400

    Add internal response template controller

---
+++ app/controllers/internal/response_templates_controller.rb
@@ -0,0 +1,58 @@
+class Internal::ResponseTemplatesController < Internal::ApplicationController
+  layout "internal"
+
+  def index
+    @response_templates = if params[:filter]
+                            ResponseTemplate.where(type_of: params[:filter])
+                          else
+                            ResponseTemplate.all
+                          end
+    @response_templates = @response_templates.page(params[:page]).per(50)
+  end
+
+  def create
+    @response_template = ResponseTemplate.new(permitted_params)
+    if @response_template.save
+      flash[:success] = "Response Template: \"#{@response_template.title}\" saved successfully."
+      redirect_to("/internal/response_templates/#{@response_template.id}/edit")
+    else
+      flash[:danger] = @response_template.errors.full_messages.to_sentence
+      @response_templates = ResponseTemplate.all.page(params[:page]).per(50)
+      render :index
+    end
+  end
+
+  def edit
+    @response_template = ResponseTemplate.find(params[:id])
+  end
+
+  def update
+    @response_template = ResponseTemplate.find(params[:id])
+
+    if @response_template.update(permitted_attributes(ResponseTemplate))
+      flash[:success] = "The response template \"#{@response_template.title}\" was updated."
+    else
+      flash[:danger] = @response_template.errors.full_messages.to_sentence
+    end
+
+    redirect_back(fallback_location: "/internal/response_templates/#{@response_template.id}")
+  end
+
+  def destroy
+    @response_template = ResponseTemplate.find(params[:id])
+
+    if @response_template.destroy
+      flash[:success] = "The response template \"#{@response_template.title}\" was deleted."
+    else
+      flash[:danger] = @response_template.errors.full_messages.to_sentence # this will probably never fail
+    end
+
+    redirect_to "/internal/response_templates"
+  end
+
+  private
+
+  def permitted_params
+    params.require(:response_template).permit(:body_markdown, :user_id, :content, :title, :type_of, :content_type)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -61,6 +61,7 @@ Rails.application.routes.draw do
       end
     end
     resources :reactions, only: [:update]
+    resources :response_templates, only: %i[index edit create update destroy]
     resources :chat_channels, only: %i[index create update]
     resources :reports, only: %i[index show], controller: "feedback_messages" do
       collection do
