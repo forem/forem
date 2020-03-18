797c184a4e85b8babe9a3af0b77a14eb382694c1
benhalpern <bendhalpern@gmail.com>
2020-03-18 15:16:11 -0400

    Finalize monthly sitemap

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -3,25 +3,20 @@ class SitemapsController < ApplicationController
 
   def show
     date_string = params[:sitemap].gsub("sitemap-", "").gsub(".xml", "")
-    date = Time.zone.parse(date_string).at_beginning_of_month
-    end_date = (date + 1.month).at_beginning_of_month
-    @change_frequency = change_frequency(date)
-    @articles = Article.where("published_at > ? AND published_at < ? AND score > ?", date, end_date, 3).
-      pluck(:path, :last_comment_at)
-    render layout: false
-  end
-
-  private
-
-  def change_frequency(date)
-    if date > 1.day.ago
-      "hourly"
-    elsif date > 1.week.ago
-      "daily"
-    elsif date > 1.month.ago
-      "weekly"
-    else
-      "monthly"
+    not_found unless date_string.match?(/\A[A-Z][a-z][a-z]\-\d{4}\z/)
+    begin
+      date = Time.zone.parse(date_string).at_beginning_of_month
+    rescue ArgumentError
+      not_found
     end
+    end_date = (date + 1.month).at_beginning_of_month
+    @articles = Article.published.where("published_at > ? AND published_at < ? AND score > ?", date, end_date, 3).pluck(:path, :last_comment_at)
+
+    response.headers["Surrogate-Control"] = if date > 1.month.ago
+                                              "max-age=8640, stale-while-revalidate=43200, stale-if-error=86400"
+                                            else
+                                              "max-age=259200, stale-while-revalidate=432000, stale-if-error=86400"
+                                            end
+    render layout: false
   end
 end
---
+++ spec/requests/sitemaps_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+RSpec.describe "Sitemaps", type: :request do
+  describe "GET /sitemap-*" do
+    it "renders xml file" do
+      get "/sitemap-Mar-2011.xml"
+      expect(response.content_type).to eq("application/xml")
+    end
+
+    it "renders not found if incorrect input" do
+      expect { get "/sitemap-March-2011.xml" }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it "renders not found if not valid date" do
+      expect { get "/sitemap-Mak-2011.xml" }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it "renders proper surrogate header for recent sitemap" do
+      get "/sitemap-#{3.days.ago.strftime('%b-%Y')}.xml"
+      expect(response.header["Surrogate-Control"]).to include("8640")
+    end
+
+    it "renders proper surrogate header for older sitemap" do
+      get "/sitemap-#{35.days.ago.strftime('%b-%Y')}.xml"
+      expect(response.header["Surrogate-Control"]).to include("259200")
+    end
+
+    it "sends a surrogate key (for Fastly's user)" do
+      create(:article)
+      create(:article)
+      create(:article)
+      create(:article)
+      Article.limit(3).update_all(published_at: 3.months.ago, score: 10)
+      get "/sitemap-#{3.months.ago.strftime('%b-%Y')}.xml"
+      expect(response.body).to include(Article.first.path)
+      expect(response.body).not_to include(Article.last.path)
+      expect(response.content_type).to eq("application/xml")
+    end
+  end
+end
