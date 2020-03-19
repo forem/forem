3d19a1ba3633b0af0f8cca495482ed8a535a35a6
rhymes <rhymesete@gmail.com>
2020-03-19 16:10:52 +0100

    Use Slack::Messengers::Feedback and refactor test

--- spec/requests/feedback_messages_create_spec.rb
+++ spec/requests/feedback_messages_create_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "feedback_messages", type: :request do
     def verify_captcha_and_slack_ping
       allow_any_instance_of(FeedbackMessagesController).
         to receive(:recaptcha_verified?).and_return(true)
-      allow(SlackBot).to receive(:ping).and_return(true)
+      allow(SlackClient).to receive(:ping).and_return(true)
     end
     # rubocop:enable RSpec/AnyInstance
 
@@ -22,17 +22,23 @@ RSpec.describe "feedback_messages", type: :request do
     context "with valid params" do
       before do
         verify_captcha_and_slack_ping
-        post "/feedback_messages", params: valid_abuse_report_params
       end
 
-      it "creates feedback message with filled form" do
-        expect(FeedbackMessage.last.message).to eq(
+      it "creates a feedback message" do
+        expect do
+          post feedback_messages_path, params: valid_abuse_report_params
+        end.to change(FeedbackMessage, :count).by(1)
+
+        feedback_message = FeedbackMessage.last
+        expect(feedback_message.message).to eq(
           valid_abuse_report_params[:feedback_message][:message],
         )
       end
 
-      it "send a Slack message when completed" do
-        expect(SlackBot).to have_received(:ping)
+      it "queues a slack message to be sent" do
+        sidekiq_assert_enqueued_with(job: SlackBotPingWorker) do
+          post feedback_messages_path, params: valid_abuse_report_params
+        end
       end
     end
 
@@ -41,47 +47,64 @@ RSpec.describe "feedback_messages", type: :request do
         post "/feedback_messages", params: valid_abuse_report_params
         expect(response.body).to include("Make sure the forms are filled")
       end
+
+      it "queues a slack message to be sent" do
+        sidekiq_assert_no_enqueued_jobs(only: SlackBotPingWorker) do
+          post feedback_messages_path, params: valid_abuse_report_params
+        end
+      end
     end
 
-    context "when a logged in user submits a report" do
+    context "when a user submits a report" do
       let(:user) { create(:user) }
 
       before do
         verify_captcha_and_slack_ping
         sign_in(user)
-        post "/feedback_messages", params: valid_abuse_report_params
       end
 
-      it "adds the logged in user as the reporter" do
-        expect(FeedbackMessage.find_by(reporter_id: user.id)).not_to eq(nil)
+      it "creates a feedback message reported by the user" do
+        post feedback_messages_path, params: valid_abuse_report_params
+
+        expect(FeedbackMessage.exists?(reporter_id: user.id)).to be(true)
       end
 
-      it "send a Slack message when completed" do
-        expect(SlackBot).to have_received(:ping)
+      it "queues a slack message to be sent" do
+        sidekiq_assert_enqueued_jobs(1, only: SlackBotPingWorker) do
+          post feedback_messages_path, params: valid_abuse_report_params
+        end
       end
     end
 
-    context "when a signed out users submits a report" do
+    context "when an anonymous user submits a report" do
       before do
         verify_captcha_and_slack_ping
-        post "/feedback_messages", params: valid_abuse_report_params
       end
 
       it "does not add any user as the reporter" do
-        expect(FeedbackMessage.last.reporter_id).to eq(nil)
+        post "/feedback_messages", params: valid_abuse_report_params
+
+        expect(FeedbackMessage.last.reporter).to be(nil)
       end
 
-      it "send a Slack message when completed" do
-        expect(SlackBot).to have_received(:ping)
+      it "queues a slack message to be sent" do
+        sidekiq_assert_enqueued_jobs(1, only: SlackBotPingWorker) do
+          post feedback_messages_path, params: valid_abuse_report_params
+        end
       end
 
-      it "redirects to /feedback_message and continues to the index page" do
-        expect(response).to redirect_to "/feedback_messages"
+      it "redirects to the index page" do
+        post "/feedback_messages", params: valid_abuse_report_params
+
+        expect(response).to redirect_to(feedback_messages_path)
       end
 
       it "redirects and continues to the index page with the correct message" do
+        post "/feedback_messages", params: valid_abuse_report_params
+
         follow_redirect!
-        expect(response.body).to include "Thank you for your report."
+
+        expect(response.body).to include("Thank you for your report.")
       end
     end
   end
