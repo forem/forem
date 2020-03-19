cad93674ff2e3ca5cf25ae2f6f0fb980a3a47c5c
ludwiczakpawel <ludwiczakpawel@gmail.com>
2020-03-19 20:34:09 +0100

    New colors (#6650)
    * add tons of colors
    
    * add tons of colors
    
    * moving cheese around and prepping for new theming system
    
    * documenting
    
    * .
    
    * moving theme code to own files
    
    * whitespaces

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -83,6 +83,12 @@ class PagesController < ApplicationController
     )
   end
 
+  def crayons
+    @page = Page.find_by(slug: "crayons")
+    render :show if @page
+    set_surrogate_key_header "crayons_page"
+  end
+
   private
 
   def latest_published_thread(tag_name)
