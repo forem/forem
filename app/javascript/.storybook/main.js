2260fccdfa5522a5cd5117065dc94c6e35ff4807
Nick Taylor <nick@iamdeveloper.com>
2020-03-20 13:56:33 -0400

    Added mixin/function to handle SASS compilation for Sprockets and Storybook's webpack.

--- app/javascript/.storybook/main.js
+++ app/javascript/.storybook/main.js
@@ -10,7 +10,19 @@ module.exports = {
   webpackFinal: async (config, { configType }) => {
     config.module.rules.push({
       test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          options: {
+            // The injected environment variable is so that SASS mixins/functions can handle
+            // generating correct CSS for Sprockets or webpack when in Storybook.
+            // an example of it's usage can be found in /app/assets/stylesheets/_mixins.scss
+            prependData: '$environment: "storybook";',
+          },
+        },
+      ],
       include: path.resolve(__dirname, '../../'),
     });
 
