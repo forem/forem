390a0fb5454aae2dd7ee6031f3844638d1b071f0
mstruve <mollylbs@gmail.com>
2020-03-23 12:01:42 -0500

    refactor feed content prepare doc methods

--- app/services/search/feed_content.rb
+++ app/services/search/feed_content.rb
@@ -23,28 +23,23 @@ module Search
       def prepare_doc(hit)
         source = hit.dig("_source")
         source["tag_list"] = hit["tags"]&.map { |t| t["name"] } || []
-        source["id"] = hit.dig("_source", "id").split("_").last.to_i
-        source["tag_list"] = hit.dig("_source", "tags")&.map { |t| t["name"] } || []
-        source["flare_tag"] = hit.dig("_source", "flare_tag_hash")
-        source["user_id"] = hit.dig("_source", "user", "id")
+        source["id"] = source["id"].split("_").last.to_i
+        source["tag_list"] = source["tags"]&.map { |t| t["name"] } || []
+        source["flare_tag"] = source["flare_tag_hash"]
+        source["user_id"] = source.dig("user", "id")
         source["highlight"] = hit["highlight"]
-        source["readable_publish_date"] = hit.dig("_source", "readable_publish_date_string")
+        source["readable_publish_date"] = source["readable_publish_date_string"]
 
-        source.merge!(timestamps_hash(hit))
-
-        source
+        source.merge(timestamps_hash(hit))
       end
 
       def timestamps_hash(hit)
-        if hit.dig("_source", "published_at")
-          published_at_timestamp = DateTime.parse(hit.dig("_source", "published_at")).in_time_zone
-          {
-            "published_at_int" => published_at_timestamp.to_i,
-            "published_timestamp" => published_at_timestamp
-          }
-        else
-          { "published_at_int" => nil, "published_timestamp" => nil }
-        end
+        published_at = hit.dig("_source", "published_at")
+        published_at_timestamp = Time.zone.parse(published_at || "")
+        {
+          "published_at_int" => published_at_timestamp.to_i,
+          "published_timestamp" => published_at
+        }
       end
 
       def index_settings
