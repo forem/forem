027ac715037ee6ca1afa73f6bed0e30b7cc2cad3
rhymes <rhymesete@gmail.com>
2020-03-23 16:06:56 +0100

    Fix specs

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Collection, type: :model do
 
   describe "validations" do
     it { is_expected.to belong_to(:user) }
-    it { is_expected.to belong_to(:organization).optional }
     it { is_expected.to have_many(:articles) }
 
     it { is_expected.to validate_presence_of(:user_id) }
