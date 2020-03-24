560d772fef7aba5634994b75724b7165915348a4
Zhao-Andy <17884966+Zhao-Andy@users.noreply.github.com>
2020-03-24 17:35:04 -0400

    Use proper indentation

--- app/controllers/response_templates_controller.rb
+++ app/controllers/response_templates_controller.rb
@@ -4,19 +4,19 @@ class ResponseTemplatesController < ApplicationController
   def index
     not_found unless current_user
     @response_templates = if params[:type_of] && params[:personal_included] != "true"
-                          result = ResponseTemplate.where(user_id: nil, type_of: params[:type_of])
-                          handle_authorization(result)
-                          result
-                        elsif params[:type_of] == "mod_comment" && params[:personal_included] == "true"
-                          result = ResponseTemplate.
-                            where(user_id: nil, type_of: "mod_comment").
-                            union(user_id: current_user.id, type_of: "personal_comment")
-                          handle_authorization(result)
-                          result
-                        else
-                          skip_authorization
-                          ResponseTemplate.where(user_id: current_user.id)
-                        end
+                            result = ResponseTemplate.where(user_id: nil, type_of: params[:type_of])
+                            handle_authorization(result)
+                            result
+                          elsif params[:type_of] == "mod_comment" && params[:personal_included] == "true"
+                            result = ResponseTemplate.
+                              where(user_id: nil, type_of: "mod_comment").
+                              union(user_id: current_user.id, type_of: "personal_comment")
+                            handle_authorization(result)
+                            result
+                          else
+                            skip_authorization
+                            ResponseTemplate.where(user_id: current_user.id)
+                          end
   end
 
   def create
