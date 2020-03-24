c762eab80f1d7871e880675aa330fbae28ff33f3
Andy Zhao <17884966+Zhao-Andy@users.noreply.github.com>
2020-03-24 05:49:37 -0400

    Update call to action box text styling (#6795)
    * Use constant for type_of_user
    
    * Add comment for clarity
    
    * Remove legacy inline styles
    
    * Move comment above the render

--- app/models/organization_membership.rb
+++ app/models/organization_membership.rb
@@ -2,9 +2,11 @@ class OrganizationMembership < ApplicationRecord
   belongs_to :user
   belongs_to :organization
 
+  USER_TYPES = %w[admin member guest].freeze
+
   validates :user_id, :organization_id, :type_of_user, presence: true
   validates :user_id, uniqueness: { scope: :organization_id }
-  validates :type_of_user, inclusion: { in: %w[admin member guest] }
+  validates :type_of_user, inclusion: { in: USER_TYPES }
 
   after_create :update_user_organization_info_updated_at
   after_destroy :update_user_organization_info_updated_at
