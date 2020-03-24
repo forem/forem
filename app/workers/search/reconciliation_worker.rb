ad801f870340320e983e893acd789c47411723de
Alex <alexandersmith223@gmail.com>
2020-03-24 14:42:50 -0700

    Refactor to only log counts

--- app/workers/search/reconciliation_worker.rb
+++ app/workers/search/reconciliation_worker.rb
@@ -1,5 +1,5 @@
-# This worker checks the number of records for each Elasticsearch index and
-# compares it to the number of records in our database.
+# This worker records the number of records in an Elasticsearch index and the
+# database to Datadog for comparison.
 #
 # For indexes that don't match a single model (no model or multiple models) we
 # need to implement a custom db_count method on the Search class to do the
@@ -10,66 +10,30 @@ module Search
 
     sidekiq_options queue: :low_priority, retry: 5
 
-    # search_class - a Search module to check the counts for.
-    #
-    # margin_of_error - (Integer/Float) Defaults to 0. This defines how far off
-    # the counts can be before an error is raised. This can be a number greater
-    # than 1 to denote a count of records. If this value is less than 1, it
-    # will be treated as a percentage (i.e. 0.10 is 10%).
-    #
-    # use_estimate_count - (Boolean) Defaults to false. If true it will
-    # prioritize using ApplicationRecord.estimated_count over Model.count.
-    def perform(search_class, margin_of_error = 0, use_estimated_count = false)
+    def perform(search_class)
       search_class = search_class.constantize
 
-      db_count = db_count(search_class, use_estimated_count)
+      db_count = db_count(search_class)
       index_count = Search::Client.count(index: search_class::INDEX_ALIAS).dig("count")
-      record_difference = (db_count - index_count).abs
-      percentage_difference = (record_difference / db_count.to_f).round(2)
 
-      tags = {
-        search_class: search_class,
-        db_count: db_count,
-        index_count: index_count,
-        record_difference: record_difference,
-        percentage_difference: percentage_difference,
-        margin_of_error: margin_of_error,
-        use_estimated_count: use_estimated_count,
-        action: "record_count"
-      }
-
-      if count_mismatched?(margin_of_error, percentage_difference, record_difference)
-        tags[:record_count] = "mismatch"
-        DatadogStatsClient.increment("elasticsearch", tags: tags)
-
-        # This will force the job to retry
-        raise ReconciliationMismatch, "#{search_class} record count mismatch"
-      else
-        tags[:record_count] = "match"
-        DatadogStatsClient.increment("elasticsearch", tags: tags)
-      end
+      DatadogStatsClient.increment("elasticsearch", tags: {
+                                     search_class: search_class,
+                                     db_count: db_count,
+                                     index_count: index_count,
+                                     action: "search_reconciliation"
+                                   })
     end
-
-    class ReconciliationMismatch < StandardError; end
 
     private
 
-    def db_count(search_class, use_estimated_count)
+    def db_count(search_class)
       model = search_class.to_s.demodulize.safe_constantize
 
-      return model.estimated_count if use_estimated_count && model&.estimated_count
+      return model.estimated_count if model.respond_to?(:estimated_count) && model&.estimated_count
 
-      return model.count if model.respond_to?(:count)
+      return model.count if model&.count
 
       search_class.db_count
-    end
-
-    def count_mismatched?(margin_of_error, percentage_difference, record_difference)
-      if margin_of_error > 0.0 && margin_of_error < 1.0
-        percentage_difference > margin_of_error
-      else
-        record_difference > margin_of_error
-      end
     end
   end
 end
--- spec/workers/search/reconciliation_worker_spec.rb
+++ spec/workers/search/reconciliation_worker_spec.rb
@@ -1,92 +1,26 @@
 require "rails_helper"
 
 RSpec.describe Search::ReconciliationWorker, type: :worker, elasticsearch: true do
-  include_examples "#enqueues_on_correct_queue", "low_priority", [0]
+  include_examples "#enqueues_on_correct_queue", "low_priority", [Search::Tag.to_s]
 
   describe "#perform" do
     let(:worker) { subject }
-    let(:search_tag) { Search::Tag }
 
-    it "Records a matching count" do
-      allow(DatadogStatsClient).to receive(:increment)
+    it "Records counts to Datadog" do
+      search_class = Search::Tag
       create(:tag, :search_indexed)
+      search_class.refresh_index
 
-      search_tag.refresh_index
-      worker.perform(search_tag.to_s)
+      allow(DatadogStatsClient).to receive(:increment)
 
-      tags = { search_class: search_tag, db_count: 1, index_count: 1, record_difference: 0, percentage_difference: 0.00, margin_of_error: 0, action: "record_count", record_count: "match" }
+      worker.perform(search_class.to_s)
+
+      tags = { search_class: search_class, db_count: 1, index_count: 1, action: "search_reconciliation" }
 
       expect(DatadogStatsClient).to have_received(:increment).with(
         "elasticsearch",
         tags: hash_including(tags),
       )
-    end
-
-    it "Uses the margin_of_error argument" do
-      allow(DatadogStatsClient).to receive(:increment)
-      create(:tag, :search_indexed)
-      tag2 = create(:tag, :search_indexed)
-
-      search_tag.refresh_index
-      tag2.delete
-
-      expect { worker.perform(search_tag.to_s, 1.0) }.not_to raise_error
-
-      tags = { search_class: search_tag, db_count: 1, index_count: 2, record_difference: 1, percentage_difference: 1.0, margin_of_error: 1.0, use_estimated_count: false, action: "record_count", record_count: "match" }
-
-      expect(DatadogStatsClient).to have_received(:increment).with(
-        "elasticsearch",
-        tags: hash_including(tags),
-      )
-    end
-
-    it "Uses the use_estimated_count argument" do
-      allow(ClassifiedListing).to receive(:estimated_count)
-
-      worker.perform(Search::ClassifiedListing.to_s, 0, true)
-
-      expect(ClassifiedListing).to have_received(:estimated_count).once
-    end
-
-    it "Records a mismatching count and raises an error" do
-      allow(DatadogStatsClient).to receive(:increment)
-      create(:tag, :search_indexed)
-      tag2 = create(:tag, :search_indexed)
-
-      search_tag.refresh_index
-      tag2.delete
-
-      expect { worker.perform(search_tag.to_s) }.to raise_error(Search::ReconciliationWorker::ReconciliationMismatch)
-
-      tags = { search_class: search_tag, db_count: 1, index_count: 2, record_difference: 1, percentage_difference: 1.0, margin_of_error: 0, use_estimated_count: false, action: "record_count", record_count: "mismatch" }
-
-      expect(DatadogStatsClient).to have_received(:increment).with(
-        "elasticsearch",
-        tags: hash_including(tags),
-      )
-    end
-
-    it "Uses the db_count method on the Search class if no model is found" do
-      # Piggyback off of Tags index for testing
-      stub_const "Search::Base::INDEX_ALIAS", Search::Tag::INDEX_ALIAS
-      create(:tag, :search_indexed)
-      search_tag.refresh_index
-
-      allow(DatadogStatsClient).to receive(:increment)
-      allow(Search::Base).to receive(:db_count).and_return(1)
-
-      tags = { search_class: Search::Base, db_count: 1, index_count: 1, record_difference: 0, percentage_difference: 0.00, margin_of_error: 0, use_estimated_count: false, action: "record_count", record_count: "match" }
-
-      worker.perform(Search::Base.to_s)
-
-      expect(DatadogStatsClient).to have_received(:increment).with(
-        "elasticsearch",
-        tags: hash_including(tags),
-      )
-    end
-
-    it "Raises an error if no model or db_count method is found" do
-      expect { worker.perform(Search::Base.to_s) }.to raise_error(Search::Errors::SubclassResponsibility)
     end
   end
 end
