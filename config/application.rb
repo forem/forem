88b8dc34f3b19eb2caf71df8c32f8d2401fe9639
rhymes <rhymesete@gmail.com>
2020-03-25 11:52:23 +0100

    Silence all Ruby 2.7 deprecation warnings in dev mode

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+# Silence all Ruby 2.7 deprecation warnings in development mode
+$VERBOSE = nil if Rails.env.development?
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
