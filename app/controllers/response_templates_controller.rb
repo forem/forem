5cd90ae0cd29448318a65634449400a09d394b40
Zhao-Andy <17884966+Zhao-Andy@users.noreply.github.com>
2020-03-25 18:07:49 -0400

    Memoize @response_template

--- app/controllers/response_templates_controller.rb
+++ app/controllers/response_templates_controller.rb
@@ -3,53 +3,51 @@ class ResponseTemplatesController < ApplicationController
 
   def create
     authorize ResponseTemplate
-    @response_template = ResponseTemplate.new(permitted_attributes(ResponseTemplate))
-    @response_template.user_id = current_user.id
-    @response_template.content_type = "body_markdown"
-    @response_template.type_of = "personal_comment"
+    response_template.user_id = current_user.id
+    response_template.content_type = "body_markdown"
+    response_template.type_of = "personal_comment"
 
-    if @response_template.save
-      flash[:settings_notice] = "Your response template \"#{@response_template.title}\" was created."
+    if response_template.save
+      flash[:settings_notice] = "Your response template \"#{response_template.title}\" was created."
     else
-      flash[:error] = "Response template error: #{@response_template.errors.full_messages.to_sentence}"
+      flash[:error] = "Response template error: #{response_template.errors.full_messages.to_sentence}"
+      previous_title = permitted_attributes(ResponseTemplate)[:title]
+      previous_content = permitted_attributes(ResponseTemplate)[:content]
     end
 
     redirect_to user_settings_path(tab: "response-templates", id: @response_template.id)
   end
 
   def destroy
-    @response_template = ResponseTemplate.find(params[:id])
-    authorize @response_template
+    authorize response_template
 
-    if @response_template.destroy
-      flash[:settings_notice] = "Your response template \"#{@response_template.title}\" was deleted."
+    if response_template.destroy
+      flash[:settings_notice] = "Your response template \"#{response_template.title}\" was deleted."
     else
-      flash[:error] = @response_template.errors.full_messages.to_sentence # this will probably never fail
+      flash[:error] = response_template.errors.full_messages.to_sentence # this will probably never fail
     end
 
     redirect_to user_settings_path(tab: "response-templates")
   end
 
   def update
-    @response_template = ResponseTemplate.find(params[:id])
-    authorize @response_template
+    authorize response_template
 
-    if @response_template.update(permitted_attributes(ResponseTemplate))
-      flash[:settings_notice] = "Your response template \"#{@response_template.title}\" was updated."
+    if response_template.update(permitted_attributes(ResponseTemplate))
+      flash[:settings_notice] = "Your response template \"#{response_template.title}\" was updated."
+      redirect_to user_settings_path(tab: "response-templates", id: response_template.id)
     else
       flash[:error] = "Response template error: #{@response_template.errors.full_messages.to_sentence}"
     end
-    redirect_to user_settings_path(tab: "response-templates", id: @response_template.id, previous_content: permitted_attributes(ResponseTemplate)[:content])
   end
 
   private
 
-  def handle_authorization(records)
-    case params[:type_of]
-    when "mod_comment"
-      authorize records, :moderator_index?
+  def response_template
+    @response_template ||= if params[:id].present?
+      ResponseTemplate.find(params[:id])
     else
-      authorize records, :admin_index?
+      ResponseTemplate.new(permitted_attributes(ResponseTemplate))
     end
   end
 end
