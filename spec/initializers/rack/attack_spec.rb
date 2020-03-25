d1db58a986067db09a68750c9118a4928b32fcdf
mstruve <mollylbs@gmail.com>
2020-03-25 11:50:54 -0500

    throttle requests made to search endpoints

---
+++ spec/initializers/rack/attack_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+describe Rack::Attack, type: :request do
+  describe "search_throttle" do
+    before do
+      redis_url = "redis://localhost:6379"
+      cache_db = ActiveSupport::Cache::RedisStore.new(redis_url)
+      allow(Rails).to receive(:cache) { cache_db }
+    end
+
+    it "throttles /search endpoints based on IP" do
+      Timecop.freeze do
+        valid_responses = Array.new(5).map do
+          get "/search/users", headers: { "REMOTE_ADDR" => "5.6.7.8" }
+        end
+        throttled_response = get "/search/users", headers: { "REMOTE_ADDR" => "5.6.7.8" }
+        new_ip_response = get "/search/users", headers: { "REMOTE_ADDR" => "1.1.1.1" }
+
+        valid_responses.each { |r| expect(r).not_to eq(429) }
+        expect(throttled_response).to eq(429)
+        expect(new_ip_response).not_to eq(429)
+      end
+    end
+  end
+end
