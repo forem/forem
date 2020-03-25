30300544a24ecc32a07b322b09b9ff3ae2380191
Alex <alexandersmith223@gmail.com>
2020-03-25 13:52:54 -0700

    Log Elasticsearch index counts to Datadog (#6845) [deploy]

--- app/workers/metrics/record_data_counts_worker.rb
+++ app/workers/metrics/record_data_counts_worker.rb
@@ -1,5 +1,5 @@
 module Metrics
-  class RecordDbTableCountsWorker
+  class RecordDataCountsWorker
     include Sidekiq::Worker
     sidekiq_options queue: :low_priority, retry: 10
 
@@ -9,6 +9,11 @@ module Metrics
         estimate = model.estimated_count
         Rails.logger.info("db_table_size", table_info: { table_name: model.table_name, table_size: estimate })
         DatadogStatsClient.gauge("postgres.db_table_size", estimate, tags: { table_name: model.table_name })
+
+        next unless model.const_defined?(:SEARCH_CLASS)
+
+        document_count = model::SEARCH_CLASS.document_count
+        DatadogStatsClient.gauge("elasticsearch.document_count", document_count, tags: { table_name: model.table_name })
       end
     end
   end
