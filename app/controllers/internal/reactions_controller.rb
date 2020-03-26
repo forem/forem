38d56542eba00eed160bf12728cb25ef1572d1c3
Jacob Herrington <jacobherringtondeveloper@gmail.com>
2020-03-26 16:59:24 -0500

    [deploy] Moderator and admin actions log (#6797)
    * Add audit logging for negative reactions
    
    This change saves requests that create negative reactions to the audit
    log. We need to be monitoring admin and moderator actions throughout the
    app to increase visiblity on how our tools are being used and identify
    abuse.
    
    Additionally, this will help east concerns about giving elevated users
    more powerful tools.
    
    * Add a basic UI for moderator audit logs
    
    * Log experience level ratings
    
    * Log tag adjustments
    
    * UI tweaks for moderator actions log
    
    * Add logging to internal reactions controller
    
    * Add logging for admin actions on users and articles
    
    * Add searching to the moderator actions table
    
    * Move audit instrumentation out of concern
    
    We are able to use a PORO here instead of using a Rails concern. Moving
    this also makes syntax to audit something quite a bit more explicit and
    obvious to future developers
    
    * Change moderator logs pagination page length
    
    * Move auditing to after action filters
    
    * Add moderator actions to the internal navbar
    
    * Add request spec for internal moderator actions
    
    * Use request params to populate AuditLog slug

--- app/controllers/internal/reactions_controller.rb
+++ app/controllers/internal/reactions_controller.rb
@@ -1,4 +1,8 @@
 class Internal::ReactionsController < Internal::ApplicationController
+  after_action only: [:update] do
+    Audit::Logger.log(:moderator, current_user, params.dup)
+  end
+
   def update
     @reaction = Reaction.find(params[:id])
     @reaction.update(status: params[:reaction][:status])
