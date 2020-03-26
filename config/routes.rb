1a989bef07834ad46ea789a0775e406cdcbc4f3f
Andy Zhao <17884966+Zhao-Andy@users.noreply.github.com>
2020-03-26 14:48:00 -0400

    [deploy] Add response templates to user settings (#6823)
    * Add response templates controller, routes, and view
    
    * Add response templates settings page
    
    * Remove dead code
    
    * Use proper variable oops
    
    * Use proper indentation
    
    * Use before action for checking current_user
    
    * Remove index action temporarily
    
    * Use url helpers for redirects
    
    * Use form_with over form_tag and form_for
    
    * Persist previous content if edit fails
    
    * Use exists for performance
    
    * Remove index spec and add CRUD specs
    
    * Memoize @response_template
    
    * Use workaround for sad paths b/c render doesn't work
    
    * Refactor and extract permitted_attributes into var
    
    * Use present over exists to preload into memory
    
    * Use last over first
    
    * Lint for codeclimate
    
    * Use two redirects for happy and sad create path
    
    * Remove proper index route oops
    
    * Codeclimate comma sigh

--- config/routes.rb
+++ config/routes.rb
@@ -165,6 +165,7 @@ Rails.application.routes.draw do
   end
   resources :twitch_live_streams, only: :show, param: :username
   resources :reactions, only: %i[index create]
+  resources :response_templates, only: %i[create edit update destroy]
   resources :feedback_messages, only: %i[index create]
   resources :organizations, only: %i[update create]
   resources :followed_articles, only: [:index]
