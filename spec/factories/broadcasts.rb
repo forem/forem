61a4b87e55cf47df52d3ea077c89ca0fead3ef6b
Mac Siri <krairit.siri@gmail.com>
2020-03-27 15:21:40 -0400

    Implement authentication welcome notification (#6846)

--- spec/factories/broadcasts.rb
+++ spec/factories/broadcasts.rb
@@ -1,22 +1,30 @@
 FactoryBot.define do
   factory :broadcast do
-    active { false }
+    active { true }
 
     factory :welcome_broadcast do
-      title { "Welcome Notification: welcome_thread" }
-      type_of { "Welcome" }
+      title          { "Welcome Notification: welcome_thread" }
+      type_of        { "Welcome" }
       processed_html { "Sloan here again! 👋 DEV is a friendly community. Why not introduce yourself by leaving a comment in <a href='/welcome'>the welcome thread</a>!" }
+    end
+
+    factory :twitter_connect_broadcast do
+      title          { "Welcome Notification: twitter_connect" }
+      type_of        { "Welcome" }
+      processed_html { "You're on a roll! 🎉 Let's connect your <a href='/settings'> Twitter account</a> to complete your identity so that we don't think you're a robot. 🤖" }
+    end
+
+    factory :github_connect_broadcast do
+      title          { "Welcome Notification: github_connect" }
+      type_of        { "Welcome" }
+      processed_html { "You're on a roll! 🎉 Let's connect your <a href='/settings'> GitHub account</a> to complete your identity so that we don't think you're a robot. 🤖" }
     end
 
     # TODO: [@thepracticaldev/delightful] Remove onboarding factory once welcome notifications are live.
     factory :onboarding_broadcast do
-      title { "Welcome Notification" }
-      type_of { "Onboarding" }
+      title          { "Welcome Notification" }
+      type_of        { "Onboarding" }
       processed_html { "Welcome! Introduce yourself in our <a href='/welcome'>welcome thread!</a>" }
     end
-  end
-
-  trait :active do
-    active { true }
   end
 end
