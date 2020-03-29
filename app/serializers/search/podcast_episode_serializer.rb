4c8a924b8c1e41927cffd5703048beec2ef607fe
rhymes <rhymesete@gmail.com>
2020-03-29 19:05:52 +0200

    Ignore unused podcast_episodes columns

--- app/serializers/search/podcast_episode_serializer.rb
+++ app/serializers/search/podcast_episode_serializer.rb
@@ -5,7 +5,7 @@ module Search
     attribute :id, &:search_id
 
     attributes :body_text, :class_name, :comments_count,
-               :featured, :featured_number, :hotness_score, :path,
+               :featured, :hotness_score, :path,
                :positive_reactions_count, :published, :published_at, :quote,
                :reactions_count, :search_score, :subtitle, :summary, :title,
                :website_url
