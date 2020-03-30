c6a882d3cea46cdaf1b40b5f0b892a346ce4a229
Zhao-Andy <17884966+Zhao-Andy@users.noreply.github.com>
2020-03-30 11:54:05 -0400

    Add test for edit and delete response templates

---
+++ spec/system/user/user_settings_response_templates_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe "User uses response templates settings", type: :system do
+  let!(:user) { create(:user) }
+  let(:response_template) { create(:response_template, user: user) }
+
+  context "when user is signed in" do
+    before do
+      sign_in user
+      response_template
+    end
+
+    context "when user has a response template already" do
+      it "can go to the edit page of the response template" do
+        visit "/settings/response-templates"
+        click_link "EDIT"
+        expect(page).to have_current_path "/settings/response-templates/#{response_template.id}", ignore_query: true
+      end
+
+      it "can delete a response template properly", js: true do
+        visit "/settings/response-templates"
+        click_button "DELETE"
+        accept_alert
+        expect(ResponseTemplate.count).to eq 0
+      end
+    end
+  end
+end
