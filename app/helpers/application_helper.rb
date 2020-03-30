b653c19d4ff174cecd7579f8319127b89af4d187
Ridhwana <ridhwana.khan16@gmail.com>
2020-03-30 22:03:32 +0200

    chore: update or codeclimate

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -165,7 +165,7 @@ module ApplicationHelper
     start_year = ApplicationConfig["COMMUNITY_COPYRIGHT_START_YEAR"]
     current_year = Time.current.year
     return start_year if current_year == start_year
-    return current_year if start_year.strip.length == 0
+    return current_year if start_year.strip.length.zero?
 
     "#{start_year} - #{current_year}"
   end
