f84a2690d6396e019d4a49baee861b32e5d36488
Vaidehi Joshi <vaidehi.sj@gmail.com>
2020-03-30 18:03:29 -0700

    Add a discuss/ask notification(s) to seeds + factories

--- spec/factories/broadcasts.rb
+++ spec/factories/broadcasts.rb
@@ -32,6 +32,24 @@ FactoryBot.define do
       processed_html { "Hi, it's me again! 👋 Now that you're a part of the DEV community, let's focus on personalizing your content. You can start by <a href='/tags'>following some tags</a> to help customize your feed! 🎉" }
     end
 
+    factory :start_discussion do
+      title          { "Welcome Notification: start_discussion" }
+      type_of        { "Welcome" }
+      processed_html { "Sloan here! 👋 I noticed that you haven't <a href='https://dev.to/t/discuss'>started a discussion</a> yet. Starting a discussion is easy to do; just click on 'Write a Post' in the sidebar of the tag page to get started!" }
+    end
+
+    factory :ask_question do
+      title          { "Welcome Notification: ask_question" }
+      type_of        { "Welcome" }
+      processed_html { "Sloan here! 👋 I noticed that you haven't <a href='https://dev.to/t/explainlikeimfive'>asked a question</a> yet. Asking a question is easy to do; just click on 'Write a Post' in the sidebar of the tag page to get started!" }
+    end
+
+    factory :discuss_and_ask do
+      title          { "Welcome Notification: discuss_and_ask" }
+      type_of        { "Welcome" }
+      processed_html { "Sloan here! 👋 I noticed that you haven't <a href='https://dev.to/t/explainlikeimfive'>asked a question</a> or <a href='https://dev.to/t/discuss'>started a discussion</a> yet. It's easy to do both of these; just click on 'Write a Post' in the sidebar of the tag page to get started!" }
+    end
+
     # TODO: [@thepracticaldev/delightful] Remove onboarding factory once welcome notifications are live.
     factory :onboarding_broadcast do
       title          { "Welcome Notification" }
