9bde694e64c43dea22d69b9c3c409f2fddc2d93f
mstruve <mollylbs@gmail.com>
2020-04-02 14:14:41 -0500

    only search for users that are not banned

--- spec/services/search/query_builders/user_spec.rb
+++ spec/services/search/query_builders/user_spec.rb
@@ -26,6 +26,29 @@ RSpec.describe Search::QueryBuilders::User, type: :service do
       expect(filter.as_hash.dig("query", "bool", "must")).to match_array(exepcted_query)
     end
 
+    it "applies EXCLUDED_TERM_KEYS by default" do
+      filter = described_class.new({})
+      exepcted_filters = [
+        { "terms" => { "roles" => ["banned"] } },
+      ]
+      expect(filter.as_hash.dig("query", "bool", "must_not")).to match_array(exepcted_filters)
+    end
+
+    it "applies EXCLUDED_TERM_KEYS and QUERY_KEYS" do
+      params = { search_fields: "test" }
+      filter = described_class.new(params)
+      exepcted_query = [{
+        "simple_query_string" => {
+          "query" => "test*", "fields" => [:search_fields], "lenient" => true, "analyze_wildcard" => true
+        }
+      }]
+      exepcted_filters = [
+        { "terms" => { "roles" => ["banned"] } },
+      ]
+      expect(filter.as_hash.dig("query", "bool", "must_not")).to match_array(exepcted_filters)
+      expect(filter.as_hash.dig("query", "bool", "must")).to match_array(exepcted_query)
+    end
+
     it "ignores params we don't support" do
       params = { not_supported: "trash", search_fields: "cfp" }
       filter = described_class.new(params)
