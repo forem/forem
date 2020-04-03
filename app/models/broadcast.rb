09defeecfa8458d6c37ce535c8d3831b5ffbade7
Vaidehi Joshi <vaidehi.sj@gmail.com>
2020-04-03 07:59:06 -0700

    Remove onboarding type broadcasts
    Also clean up some pending comments.

--- app/models/broadcast.rb
+++ app/models/broadcast.rb
@@ -4,8 +4,7 @@ class Broadcast < ApplicationRecord
   has_many :notifications, as: :notifiable, inverse_of: :notifiable
 
   validates :title, :type_of, :processed_html, presence: true
-  # TODO: [@thepracticaldev/delightful] Remove Onboarding type once we have launched welcome notifications.
-  validates :type_of, inclusion: { in: %w[Announcement Onboarding Welcome] }
+  validates :type_of, inclusion: { in: %w[Announcement Welcome] }
 
   scope :active, -> { where(active: true) }
 
