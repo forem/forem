35000799bd73de61c8c6fd546a5ca9f4189ebc19
Juliano Lima <julianodgtz@gmail.com>
2020-04-02 23:26:15 -0300

    Remove shouldRender from MessageModal

--- app/javascript/listings/elements/messageModal.jsx
+++ app/javascript/listings/elements/messageModal.jsx
@@ -8,68 +8,63 @@ const MessageModal = ({
   onSubmit,
   onChangeDraftingMessage,
 }) => {
-  const shouldRender = listining && listining.contact_via_connect;
   const isCurrentUserOnListining = listining.user_id !== currentUserId;
 
   return (
-    shouldRender && (
-      <form
-        id="listings-message-form"
-        className="listings-contact-via-connect"
-        onSubmit={onSubmit}
-      >
-        {isCurrentUserOnListining ? (
-          <p>
-            <b>
-              Contact
-              {` ${listining.author.name} `}
-              via DEV Connect
-            </b>
-          </p>
-        ) : (
-          <p>
-            This is your active listing. Any member can contact you via this
-            form.
-          </p>
-        )}
-        <textarea
-          value={message}
-          onChange={onChangeDraftingMessage}
-          id="new-message"
-          rows="4"
-          cols="70"
-          placeholder="Enter your message here..."
-        />
-        <button type="submit" value="Submit" className="submit-button cta">
-          SEND
-        </button>
+    <form
+      id="listings-message-form"
+      className="listings-contact-via-connect"
+      onSubmit={onSubmit}
+    >
+      {isCurrentUserOnListining ? (
         <p>
-          {isCurrentUserOnListining ? (
-            <em>
-              Message must be relevant and on-topic with the listing. All
-              {' '}
-              private interactions 
-              {' '}
-              <b>must</b>
-              {' '}
-              abide by the
-              {' '}
-              <a href="/code-of-conduct">code of conduct</a>
-            </em>
-          ) : (
-            <em>
-              All private interactions 
-              {' '}
-              <b>must</b>
-              {' '}
-              abide by the
-              {' '}
-              <a href="/code-of-conduct">code of conduct</a>
-            </em>
-          )}
+          <b>
+            Contact
+            {` ${listining.author.name} `}
+            via DEV Connect
+          </b>
         </p>
-      </form>
-    )
+      ) : (
+        <p>
+          This is your active listing. Any member can contact you via this form.
+        </p>
+      )}
+      <textarea
+        value={message}
+        onChange={onChangeDraftingMessage}
+        id="new-message"
+        rows="4"
+        cols="70"
+        placeholder="Enter your message here..."
+      />
+      <button type="submit" value="Submit" className="submit-button cta">
+        SEND
+      </button>
+      <p>
+        {isCurrentUserOnListining ? (
+          <em>
+            Message must be relevant and on-topic with the listing. All private
+            interactions 
+            {' '}
+            <b>must</b>
+            {' '}
+            abide by the
+            {' '}
+            <a href="/code-of-conduct">code of conduct</a>
+          </em>
+        ) : (
+          <em>
+            All private interactions 
+            {' '}
+            <b>must</b>
+            {' '}
+            abide by the
+            {' '}
+            <a href="/code-of-conduct">code of conduct</a>
+          </em>
+        )}
+      </p>
+    </form>
   );
 };
 
@@ -81,7 +76,6 @@ MessageModal.propTypes = {
       name: PropTypes.string.isRequired,
     }).isRequired,
     user_id: PropTypes.number.isRequired,
-    contact_via_connect: PropTypes.bool.isRequired,
   }).isRequired,
   onSubmit: PropTypes.func.isRequired,
   onChangeDraftingMessage: PropTypes.func.isRequired,
