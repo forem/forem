bd73a8dc13f3e5589144a2a69c3640e2f0ba46d3
Vaidehi Joshi <vaidehi.sj@gmail.com>
2020-04-03 13:11:49 -0700

    Remove onboarding type broadcasts (#7067) [deploy]
    Also clean up some pending comments.

--- spec/services/notifications/welcome_notification/send_spec.rb
+++ spec/services/notifications/welcome_notification/send_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe Notifications::WelcomeNotification::Send, type: :service do
     end
 
     it "creates a new welcome notification", :aggregate_failures do
-      welcome_broadcast = create(:onboarding_broadcast)
+      welcome_broadcast = create(:set_up_profile_broadcast)
       welcome_notification = described_class.call(create(:user).id, welcome_broadcast)
 
       expect(welcome_notification).to be_kind_of(Notification)
       expect(welcome_notification.notifiable_type).to eq "Broadcast"
-      expect(welcome_notification.action).to eq "Onboarding"
+      expect(welcome_notification.action).to eq "Welcome"
       expect(welcome_notification.json_data["broadcast"]["processed_html"]).to eq welcome_broadcast.processed_html
     end
   end
