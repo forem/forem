f3a5673d13bb3d0a2c604f4587b6016b6ab51504
Juliano Lima <juliano.lima@nubank.com.br>
2020-04-03 12:19:26 -0300

    Remove shouldRender from ModalBackground

--- app/javascript/listings/elements/modalBackground.jsx
+++ app/javascript/listings/elements/modalBackground.jsx
@@ -1,18 +1,16 @@
 import { h } from 'preact';
 import PropTypes from 'prop-types';
 
-const ModalBackground = ({ shouldRender, onClick }) =>
-  shouldRender && (
-    <div
-      className="classified-listings-modal-background"
-      onClick={onClick}
-      role="presentation"
-      id="classified-listings-modal-background"
-    />
-  );
+const ModalBackground = ({ onClick }) => (
+  <div
+    className="classified-listings-modal-background"
+    onClick={onClick}
+    role="presentation"
+    id="classified-listings-modal-background"
+  />
+);
 
 ModalBackground.propTypes = {
-  shouldRender: PropTypes.bool.isRequired,
   onClick: PropTypes.func.isRequired,
 };
 
