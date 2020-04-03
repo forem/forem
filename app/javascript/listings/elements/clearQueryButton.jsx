21136956c7f1a83cf952f37b47e540981bb09c24
Juliano Lima <julianodgtz@gmail.com>
2020-04-02 23:04:32 -0300

    Remove shouldRender prop

--- app/javascript/listings/elements/clearQueryButton.jsx
+++ app/javascript/listings/elements/clearQueryButton.jsx
@@ -1,16 +1,14 @@
 import { h } from 'preact';
 import PropTypes from 'prop-types';
 
-const ClearQueryButton = ({ onClick, shouldRender }) =>
-  shouldRender ? (
-    <button type="button" className="classified-search-clear" onClick={onClick}>
-      ×
-    </button>
-  ) : null;
+const ClearQueryButton = ({ onClick }) => (
+  <button type="button" className="classified-search-clear" onClick={onClick}>
+    ×
+  </button>
+);
 
 ClearQueryButton.propTypes = {
   onClick: PropTypes.func.isRequired,
-  shouldRender: PropTypes.bool.isRequired,
 };
 
 export default ClearQueryButton;
