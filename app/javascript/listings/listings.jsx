03e035ac4d8fa43ea6a4ae2d5036cf9e77e33f85
Juliano Lima <julianodgtz@gmail.com>
2020-04-04 18:44:03 -0300

    Remove resizeMasonryItem from listings

--- app/javascript/listings/listings.jsx
+++ app/javascript/listings/listings.jsx
@@ -8,35 +8,13 @@ import AllListings from './elements/allListings';
 import SelectedTags from './elements/selectedTags';
 import CategoryLinks from './elements/categoryLinks';
 import NextPageButton from './elements/nextPageButton';
+import { resizeMasonryItem } from './utils';
 
 /**
  * How many listings to show per page
  * @constant {number}
  */
 const LISTING_PAGE_SIZE = 75;
-
-function resizeMasonryItem(item) {
-  /* Get the grid object, its row-gap, and the size of its implicit rows */
-  const grid = document.getElementsByClassName('classifieds-columns')[0];
-  const rowGap = parseInt(
-    window.getComputedStyle(grid).getPropertyValue('grid-row-gap'),
-    10,
-  );
-  const rowHeight = parseInt(
-    window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'),
-    10,
-  );
-
-  const rowSpan = Math.ceil(
-    (item.querySelector('.listing-content').getBoundingClientRect().height +
-      rowGap) /
-      (rowHeight + rowGap),
-  );
-
-  /* Set the spanning as calculated above (S) */
-  // eslint-disable-next-line no-param-reassign
-  item.style.gridRowEnd = `span ${rowSpan}`;
-}
 
 function resizeAllMasonryItems() {
   // Get all item class objects in one list
