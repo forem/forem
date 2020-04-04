28b16abf332f65555950764f9a30a8643cd590ce
Juliano Lima <julianodgtz@gmail.com>
2020-04-04 17:22:21 -0300

    Create selectedTags component

---
+++ app/javascript/listings/elements/selectedTags.jsx
@@ -0,0 +1,34 @@
+import { h } from 'preact';
+import PropTypes from 'prop-types';
+
+const SelectedTags = ({ tags, onClick, onKeyPress }) =>
+  tags.map((tag) => (
+    <span className="classified-tag">
+      <a
+        href="/listings?tags="
+        className="tag-name"
+        onClick={onClick}
+        data-no-instant
+      >
+        <span>{tag}</span>
+        <span
+          className="tag-close"
+          onClick={onClick}
+          data-no-instant
+          role="button"
+          onKeyPress={onKeyPress}
+          tabIndex="0"
+        >
+          ×
+        </span>
+      </a>
+    </span>
+  ));
+
+SelectedTags.propTypes = {
+  tags: PropTypes.isRequired,
+  onClick: PropTypes.func.isRequired,
+  onKeyPress: PropTypes.func.isRequired,
+};
+
+export default SelectedTags;
