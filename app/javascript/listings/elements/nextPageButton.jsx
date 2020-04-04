72ae451da2e2e80606be1779e992b0519d6a0e8d
Juliano Lima <julianodgtz@gmail.com>
2020-04-04 18:28:44 -0300

    Create NextPageButon component

---
+++ app/javascript/listings/elements/nextPageButton.jsx
@@ -0,0 +1,16 @@
+import { h } from 'preact';
+import PropTypes from 'prop-types';
+
+const NextPageButton = ({ onClick }) => (
+  <div className="classifieds-load-more-button">
+    <button onClick={onClick} type="button">
+      Load More Listings
+    </button>
+  </div>
+);
+
+NextPageButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default NextPageButton;
