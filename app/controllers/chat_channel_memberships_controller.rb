6b9ae145d0d3425261cacd79e36cd792a93c5520
Parasgr-code <paras.gaur@skynox.tech>
2020-04-06 21:15:16 +0530

    chat action messages made realtion

--- app/controllers/chat_channel_memberships_controller.rb
+++ app/controllers/chat_channel_memberships_controller.rb
@@ -46,7 +46,7 @@ class ChatChannelMembershipsController < ApplicationController
       @chat_channel_membership.destroy
       flash[:settings_notice] = "Invitation removed."
     else
-      Message.create("message_markdown" => "@#{current_user.username} removed @#{@chat_channel_membership.user.username} from #{@chat_channel_membership.channel_name}", "user_id" => current_user.id, "chat_channel_id" => @chat_channel_membership.chat_channel_id, "chat_action" => "removed_from_channel")
+      send_chat_action_message("@#{current_user.username} removed @#{@chat_channel_membership.user.username} from #{@chat_channel_membership.channel_name}", current_user, @chat_channel_membership.chat_channel_id, "removed_from_channel")
       @chat_channel_membership.update(status: "removed_from_channel")
       flash[:settings_notice] = "Removed #{@chat_channel_membership.user.name}"
     end
@@ -70,7 +70,7 @@ class ChatChannelMembershipsController < ApplicationController
     @chat_channel_membership = ChatChannelMembership.find(params[:id])
     authorize @chat_channel_membership
     channel_name = @chat_channel_membership.chat_channel.channel_name
-    Message.create("message_markdown" => "@#{current_user.username} left #{@chat_channel_membership.channel_name}", "user_id" => current_user.id, "chat_channel_id" => @chat_channel_membership.chat_channel_id, "chat_action" => "left_channel")
+    send_chat_action_message("@#{current_user.username} left #{@chat_channel_membership.channel_name}", current_user, @chat_channel_membership.chat_channel_id, "left_channel")
     @chat_channel_membership.update(status: "left_channel")
     @chat_channels_memberships = []
     flash[:settings_notice] = "You have left the channel #{channel_name}. It may take a moment to be removed from your list."
@@ -87,12 +87,58 @@ class ChatChannelMembershipsController < ApplicationController
     if permitted_params[:user_action] == "accept"
       @chat_channel_membership.update(status: "active")
       channel_name = @chat_channel_membership.chat_channel.channel_name
-      Message.create("message_markdown" => "@#{current_user.username} joined #{@chat_channel_membership.channel_name}", "user_id" => current_user.id, "chat_channel_id" => @chat_channel_membership.chat_channel_id, "chat_action" => "joined")
+      send_chat_action_message("@#{current_user.username} joined #{@chat_channel_membership.channel_name}", current_user, @chat_channel_membership.chat_channel_id, "joined")
       flash[:settings_notice] = "Invitation to  #{channel_name} accepted. It may take a moment to show up in your list."
     else
       @chat_channel_membership.update(status: "rejected")
       flash[:settings_notice] = "Invitation rejected."
     end
     redirect_to chat_channel_memberships_path
+  end
+
+  def send_chat_action_message(message, user, channel_id, action)
+    @temp_message_id = (0...20).map { ("a".."z").to_a[rand(8)] }.join
+    @message = Message.create("message_markdown" => message, "user_id" => user.id, "chat_channel_id" => channel_id, "chat_action" => action)
+    pusher_message_created(false)
+  end
+
+  def pusher_message_created(is_single)
+    return unless @message.valid?
+
+    begin
+      message_json = create_pusher_payload(@message, @temp_message_id)
+      if is_single
+        Pusher.trigger("private-message-notifications-#{@message.user_id}", "message-created", message_json)
+      else
+        Pusher.trigger(@message.chat_channel.pusher_channels, "message-created", message_json)
+      end
+    rescue Pusher::Error => e
+      logger.info "PUSHER ERROR: #{e.message}"
+    end
+  end
+
+  def create_pusher_payload(new_message, temp_id)
+    payload = {
+      temp_id: temp_id,
+      id: new_message.id,
+      user_id: new_message.user.id,
+      chat_channel_id: new_message.chat_channel.id,
+      chat_channel_adjusted_slug: new_message.chat_channel.adjusted_slug(current_user, "sender"),
+      channel_type: new_message.chat_channel.channel_type,
+      username: new_message.user.username,
+      profile_image_url: ProfileImage.new(new_message.user).get(width: 90),
+      message: new_message.message_html,
+      markdown: new_message.message_markdown,
+      edited_at: new_message.edited_at,
+      timestamp: Time.current,
+      color: new_message.preferred_user_color,
+      reception_method: "pushed",
+      action: new_message.chat_action
+    }
+
+    if new_message.chat_channel.group?
+      payload[:chat_channel_adjusted_slug] = new_message.chat_channel.adjusted_slug
+    end
+    payload.to_json
   end
 end
