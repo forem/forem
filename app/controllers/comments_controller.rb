1d63a17733e17ba3dd9b3141e242ca0b3c0c90d6
Zhao-Andy <17884966+Zhao-Andy@users.noreply.github.com>
2020-04-06 09:17:51 -0400

    Add audit logging for mod_create and internal actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,9 @@ class CommentsController < ApplicationController
   before_action :set_cache_control_headers, only: [:index]
   before_action :authenticate_user!, only: %i[preview create hide unhide]
   after_action :verify_authorized
+  after_action only: [:moderator_create] do
+    Audit::Logger.log(:moderator, current_user, params.dup)
+  end
 
   # GET /comments
   # GET /comments.json
