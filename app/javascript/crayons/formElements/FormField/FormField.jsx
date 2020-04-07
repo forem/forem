2d08fc089d46f361238076bf4af47852493d68d6
Nick Taylor <nick@iamdeveloper.com>
2020-04-07 09:50:12 -0400

    Now variant for a <FormField /< is optional

--- app/javascript/crayons/formElements/FormField/FormField.jsx
+++ app/javascript/crayons/formElements/FormField/FormField.jsx
@@ -2,15 +2,27 @@ import { h } from 'preact';
 import PropTypes from 'prop-types';
 import { defaultChildrenPropTypes } from '../../../src/components/common-prop-types';
 
+// Only radio and checkboxes require an additional CSS class (variant class). Other form elements do not.
+
 export const FormField = ({ children, variant }) => {
   return (
-    <div className={`crayons-field crayons-field--${variant}`}>{children}</div>
+    <div
+      className={`crayons-field${
+        variant && variant.length > 0 ? ` crayons-field--${variant}` : ''
+      }`}
+    >
+      {children}
+    </div>
   );
 };
 
 FormField.displayName = 'FormField';
 
+FormField.defaultProps = {
+  variant: undefined,
+};
+
 FormField.propTypes = {
   children: defaultChildrenPropTypes.isRequired,
-  variant: PropTypes.oneOf(['radio', 'checkbox']).isRequired,
+  variant: PropTypes.oneOf(['radio', 'checkbox']),
 };
