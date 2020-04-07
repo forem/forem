eeed7f6c542ac17452527d8db36a40391cbd3dc1
Michael Kohl <citizen428@gmail.com>
2020-04-07 22:57:13 +0700

    Introduce URL.sanitized_referer helper (#7122) [deploy]
    * Introduce URL.sanitized_referer helper
    
    * Make service worker path a constant
    
    * Simplify redirect logic
    
    * Actually use helper
    
    * Remove random file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -36,7 +36,7 @@ class PagesController < ApplicationController
   end
 
   def report_abuse
-    referer = URI(request.referer || "").path == "/serviceworker.js" ? nil : request.referer
+    referer = URL.sanitized_referer(request.referer)
     reported_url = params[:reported_url] || params[:url] || referer
     @feedback_message = FeedbackMessage.new(
       reported_url: reported_url&.chomp("?i=i"),
