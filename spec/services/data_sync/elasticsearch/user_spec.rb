a4896e6f207aab6c84dd2f4af43dcf594b2c4147
Molly Struve <mollylbs@gmail.com>
2020-04-07 02:39:23 -0500

    hook up data syncing for articles and switch to using call (#7115)

--- spec/services/data_sync/elasticsearch/user_spec.rb
+++ spec/services/data_sync/elasticsearch/user_spec.rb
@@ -3,13 +3,13 @@ require "rails_helper"
 RSpec.describe DataSync::Elasticsearch::User, type: :service do
   let!(:user) { create(:user) }
 
-  describe "#sync_documents" do
+  describe "#call" do
     it "reindexes RELATED_DOCS when sync is needed " do
       syncer = described_class.new(user, username: %w[name1 name2])
       described_class::RELATED_DOCS.each do |method_name|
         allow(syncer).to receive(method_name).and_call_original
       end
-      syncer.sync_documents
+      syncer.call
       described_class::RELATED_DOCS.each do |method_name|
         expect(syncer).to have_received(method_name)
       end
@@ -18,7 +18,7 @@ RSpec.describe DataSync::Elasticsearch::User, type: :service do
     it "does not reindex when sync is not needed" do
       syncer = described_class.new(user, stackoverflow_url: [nil, "url"])
       described_class::RELATED_DOCS.each { |method_name| allow(syncer).to receive(method_name) }
-      syncer.sync_documents
+      syncer.call
       described_class::RELATED_DOCS.each do |method_name|
         expect(syncer).not_to have_received(method_name)
       end
