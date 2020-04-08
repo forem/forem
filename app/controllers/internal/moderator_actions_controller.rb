4334a544bd43d2cea2ee3cd6e5cd3f473396c555
Zhao-Andy <17884966+Zhao-Andy@users.noreply.github.com>
2020-04-08 16:20:16 -0400

    Override application_controller resource b/c custom name

--- app/controllers/internal/moderator_actions_controller.rb
+++ app/controllers/internal/moderator_actions_controller.rb
@@ -1,6 +1,11 @@
 class Internal::ModeratorActionsController < Internal::ApplicationController
   layout "internal"
 
+  # overrides internal/application_controller.rb#authorization_resource because this has a custom controller name
+  def authorization_resource
+    AuditLog
+  end
+
   def index
     @q = AuditLog.
       includes(:user).
