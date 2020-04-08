8a328a891074f0b5577ee8484e8ca5de9ccbf8ad
Julianna Tetreault <juliannatetreault@gmail.com>
2020-04-08 15:01:15 -0600

    Add additional test coverage to users/show.html.erb   *Add additional tests to user_show_spec   *Ensure that all tests pass   *Small refactor to users/show.html.erb JSON data structure

--- spec/requests/user/user_show_spec.rb
+++ spec/requests/user/user_show_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "UserShow", type: :request do
-  let_it_be(:user) { create(:user) }
+  let_it_be(:user) { create(:user, email_public: true, employment_title: "SEO", employer_name: "DEV", currently_hacking_on: "JSON-LD", education: "DEV University", linkedin_url: "www.linkedin.com") }
 
   describe "GET /:slug (user)" do
     before do
@@ -20,14 +20,32 @@ RSpec.describe "UserShow", type: :request do
       expect(response.body).to include CGI.escapeHTML(user.summary)
     end
 
+    it "renders the proper education for a user" do
+      expect(response.body).to include CGI.escapeHTML(user.education)
+    end
+
+    it "renders the proper currently hacking on info for a user" do
+      expect(response.body).to include CGI.escapeHTML(user.currently_hacking_on)
+    end
+
+    it "renders the proper job title for a user" do
+      expect(response.body).to include CGI.escapeHTML(user.employment_title)
+    end
+
+    it "renders the proper employer name for a user" do
+      expect(response.body).to include CGI.escapeHTML(user.employer_name)
+    end
+
+    it "renders the proper linkedin url for a user" do
+      expect(response.body).to include CGI.escapeHTML(user.linkedin_url)
+    end
+
     it "renders the proper additional username for a user when one is present" do
       user.update(github_username: "username")
       expect(response.body).to include CGI.escapeHTML(user.github_username)
     end
 
-    xit "renders the proper email for a user when one is public and present" do
-      user.update(email_public: true, email: "user@dev.to")
-      #   user.reload
+    it "renders the proper email for a user when one is public and present" do
       expect(response.body).to include CGI.escapeHTML(user.email)
     end
   end
