8133a7b79969828bcb0ee075f937452b5d279438
ludwiczakpawel <ludwiczakpawel@gmail.com>
2020-04-08 11:46:58 +0200

    .

--- app/javascript/articles/components/Author.jsx
+++ app/javascript/articles/components/Author.jsx
@@ -1,12 +1,17 @@
 import { h } from 'preact';
-import { articlePropTypes } from '../../src/components/common-prop-types';
-import { organizationPropType } from '../../src/components/common-prop-types';
+import {
+  articlePropTypes,
+  organizationPropType,
+} from '../../src/components/common-prop-types';
 
 export const Author = ({ article, organization }) => (
   <div className="crayons-story__meta">
     <div className="crayons-story__author-pic">
       {organization && !document.getElementById('organization-article-index') && (
-        <a href={`/${organization.slug}`} className="crayons-logo crayons-logo--l">
+        <a
+          href={`/${organization.slug}`}
+          className="crayons-logo crayons-logo--l"
+        >
           <img
             alt={`${organization.name} logo`}
             src={organization.profile_image_90}
@@ -16,12 +21,13 @@ export const Author = ({ article, organization }) => (
         </a>
       )}
 
-      <a 
-        href={`/${article.user.username}`} 
+      <a
+        href={`/${article.user.username}`}
         className={`crayons-avatar ${
-          organization && !document.getElementById('organization-article-index') ? 'crayons-avatar--s absolute -right-2 -bottom-2 border-solid border-2 border-base-inverted' : 'crayons-avatar--l'
+          organization && !document.getElementById('organization-article-index')
+            ? 'crayons-avatar--s absolute -right-2 -bottom-2 border-solid border-2 border-base-inverted'
+            : 'crayons-avatar--l'
         }`}
-
       >
         <img
           src={article.user.profile_image_90}
@@ -31,22 +37,31 @@ export const Author = ({ article, organization }) => (
         />
       </a>
     </div>
-    
+
     <div>
       <p>
-        <a href={`/${article.user.username}`} className="crayons-story__secondary fw-bold">
+        <a
+          href={`/${article.user.username}`}
+          className="crayons-story__secondary fw-bold"
+        >
           {filterXSS(
             article.class_name === 'User'
               ? article.user.username
               : article.user.name,
           )}
-        </a> 
-        {organization && !document.getElementById('organization-article-index') && (
-          <span>
-            <span className="crayons-story__tertiary fw-normal">for</span> 
-            <a href={`/${organization.slug}`} className="crayons-story__secondary fw-bold">{organization.name}</a>
-          </span>
-        )}
+        </a>
+        {organization &&
+          !document.getElementById('organization-article-index') && (
+            <span>
+              <span className="crayons-story__tertiary fw-normal">for</span>
+              <a
+                href={`/${organization.slug}`}
+                className="crayons-story__secondary fw-bold"
+              >
+                {organization.name}
+              </a>
+            </span>
+          )}
       </p>
       <a href={article.path} className="crayons-story__tertiary">
         Posted 7 hours ago
