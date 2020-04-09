d3c49e84af8567b3aac0d5b5bff75eb0845333b8
ludwiczakpawel <ludwiczakpawel@gmail.com>
2020-04-09 09:30:23 +0200

    adjustments

--- app/javascript/articles/components/ReactionsCount.jsx
+++ app/javascript/articles/components/ReactionsCount.jsx
@@ -6,7 +6,14 @@ export const ReactionsCount = ({ article }) => {
 
   return (
     <a href={article.path} className="crayons-story__details__item">
-	   <svg className="crayons-icon" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M18.884 12.595l.01.011L12 19.5l-6.894-6.894.01-.01A4.875 4.875 0 0112 5.73a4.875 4.875 0 016.884 6.865zM6.431 7.037a3.375 3.375 0 000 4.773L12 17.38l5.569-5.569a3.375 3.375 0 10-4.773-4.773L9.613 10.22l-1.06-1.062 2.371-2.372a3.375 3.375 0 00-4.492.25v.001z"/></svg>
+      <svg
+        className="crayons-icon mr-1"
+        width="24"
+        height="24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M18.884 12.595l.01.011L12 19.5l-6.894-6.894.01-.01A4.875 4.875 0 0112 5.73a4.875 4.875 0 016.884 6.865zM6.431 7.037a3.375 3.375 0 000 4.773L12 17.38l5.569-5.569a3.375 3.375 0 10-4.773-4.773L9.613 10.22l-1.06-1.062 2.371-2.372a3.375 3.375 0 00-4.492.25v.001z" />
+      </svg>
       {totalReactions}
       <span className="hidden s:inline">&nbsp;reactions</span>
     </a>
