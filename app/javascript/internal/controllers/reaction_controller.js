89eeb493aba825ed9869059327145aa6c69c67b0
Arit Amana <msarit@gmail.com>
2020-04-09 16:49:20 -0400

    spinning my wheels

--- app/javascript/internal/controllers/reaction_controller.js
+++ app/javascript/internal/controllers/reaction_controller.js
@@ -1,5 +1,17 @@
 import { Controller } from 'stimulus';
 
-export default class ReactionController extends Controller {
-  static targets = ['status', 'id', 'reactable'];
+export default class VomitReactionController extends Controller {
+  static targets = ['status'];
+
+  // reactableUserCheck(){
+  //   if(this.statusTarget.getAttribute('data-reactable') === 'user'){
+  //
+  //   }
+  // }
+
+  updateReactionStatus() {
+    // console.log(`Reaction Status: ${this.statusTarget.value}`);
+    // console.log(`Reaction ID: ${this.statusTarget.id}`);
+    console.log(`User? ${this.statusTarget.getAttribute('data-id')}`);
+  }
 }
