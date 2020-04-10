08f2d874461969b002083e364c56f4e8e7460358
Arit Amana <msarit@gmail.com>
2020-04-10 11:54:08 -0400

    Hook html file to controllerJS file

--- app/javascript/internal/controllers/reaction_controller.js
+++ app/javascript/internal/controllers/reaction_controller.js
@@ -1,17 +1,67 @@
 import { Controller } from 'stimulus';
 
 export default class VomitReactionController extends Controller {
-  static targets = ['status'];
+  static targets = ['invalid', 'status'];
 
-  // reactableUserCheck(){
-  //   if(this.statusTarget.getAttribute('data-reactable') === 'user'){
-  //
-  //   }
-  // }
+  // eslint-disable-next-line class-methods-use-this
+  updateReaction(id, status) {
+    fetch(`/internal/reactions/${id}`, {
+      method: 'PATCH',
+      headers: {
+        Accept: 'application/json',
+        'X-CSRF-Token': document.querySelector("meta[name='csrf-token']")
+          .content,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id,
+        status,
+      }),
+      credentials: 'same-origin',
+    }).then((response) =>
+      response
+        .json()
+        .then((json) => {
+          if (json.outcome === 'Success') {
+            // removeReactionDiv(id)
+          } else {
+            alert(json.error);
+          }
+        })
+        .catch((error) => {
+          alert(error);
+        }),
+    );
+  }
 
-  updateReactionStatus() {
-    // console.log(`Reaction Status: ${this.statusTarget.value}`);
-    // console.log(`Reaction ID: ${this.statusTarget.id}`);
-    console.log(`User? ${this.statusTarget.getAttribute('data-id')}`);
+  updateReactionInvalid() {
+    this.updateReaction(this.reactionId, this.invalidStatus);
+  }
+
+  reactableUserCheck() {
+    if (this.reactableType === 'user') {
+      // eslint-disable-next-line no-restricted-globals
+      if (confirm('You are confirming a User vomit reaction; are you sure?')) {
+        this.updateReaction(this.reactionId, this.confirmedStatus);
+      }
+    } else {
+      this.updateReaction(this.reactionId, this.confirmedStatus);
+    }
+  }
+
+  get reactionId() {
+    return parseInt(this.data.get('id'), 10);
+  }
+
+  get confirmedStatus() {
+    return this.statusTarget.dataset.status;
+  }
+
+  get reactableType() {
+    return this.statusTarget.dataset.reactable;
+  }
+
+  get invalidStatus() {
+    return this.invalidTarget.dataset.altstatus;
   }
 }
