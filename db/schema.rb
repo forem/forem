46c67e95ee67041096189d3a34292810551e949f
Michael Kohl <citizen428@gmail.com>
2020-04-10 12:32:40 +0700

    Fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -307,6 +307,7 @@ ActiveRecord::Schema.define(version: 2020_04_09_050122) do
     t.string "slug"
     t.string "status", default: "active"
     t.datetime "updated_at", null: false
+    t.index ["slug"], name: "index_chat_channels_on_slug", unique: true
   end
 
   create_table "classified_listing_categories", force: :cascade do |t|
