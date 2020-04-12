a5daa2b5882ca6039a007d0491c40f564be30241
Juliano Lima <juliano.lima@nubank.com.br>
2020-04-12 10:26:27 -0300

    Adds section on selectedTags component

--- app/javascript/listings/elements/selectedTags.jsx
+++ app/javascript/listings/elements/selectedTags.jsx
@@ -1,29 +1,34 @@
 import { h } from 'preact';
 import { selectedTagsPropTypes } from '../../src/components/common-prop-types';
 
-const SelectedTags = ({ tags, onClick, onKeyPress }) =>
-  tags.map((tag) => (
-    <span className="classified-tag" key={tag.id}>
-      <a
-        href="/listings?tags="
-        className="tag-name"
-        onClick={onClick}
-        data-no-instant
-      >
-        <span>{tag}</span>
-        <span
-          className="tag-close"
-          onClick={onClick}
-          data-no-instant
-          role="button"
-          onKeyPress={onKeyPress}
-          tabIndex="0"
-        >
-          ×
+const SelectedTags = ({ tags, onClick, onKeyPress }) => {
+  return (
+    <section>
+      {tags.map((tag) => (
+        <span className="classified-tag" key={tag.id}>
+          <a
+            href="/listings?tags="
+            className="tag-name"
+            onClick={onClick}
+            data-no-instant
+          >
+            <span>{tag}</span>
+            <span
+              className="tag-close"
+              onClick={onClick}
+              data-no-instant
+              role="button"
+              onKeyPress={onKeyPress}
+              tabIndex="0"
+            >
+              ×
+            </span>
+          </a>
         </span>
-      </a>
-    </span>
-  ));
+      ))}
+    </section>
+  );
+};
 
 SelectedTags.propTypes = selectedTagsPropTypes;
 
