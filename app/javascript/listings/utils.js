3bc14cb7d4a4da8d35fc5c69764767dc76494358
Juliano Lima <juliano.lima@nubank.com.br>
2020-04-13 12:42:04 -0300

    Remove console

--- app/javascript/listings/utils.js
+++ app/javascript/listings/utils.js
@@ -54,7 +54,6 @@ export function resizeAllMasonryItems() {
 }
 
 export function getQueryParams() {
-  console.log('document here', document.location.search);
   let queryString = document.location.search;
   queryString = queryString.split('+').join(' ');
 
