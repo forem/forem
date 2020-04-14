b51852e8668bdf331429e216ead73d22600de550
Alex <alexandersmith223@gmail.com>
2020-04-14 10:11:27 -0400

    [deploy] Remove fastly-rails gem (#7257)
    * Remove fastly-rails gem
    
    * Remove FastlyRails.configuration references
    
    * Remove FastlyRails initializer

--- spec/requests/reactions_spec.rb
+++ spec/requests/reactions_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe "Reactions", type: :request do
   let(:article) { create(:article, user: user) }
   let(:comment) { create(:comment, commentable: article) }
 
-  let_it_be(:max_age) { FastlyRails.configuration.max_age }
-  let_it_be(:stale_if_error) { FastlyRails.configuration.stale_if_error }
+  let_it_be(:max_age) { 1.day.to_i }
+  let_it_be(:stale_if_error) { 26_400 }
 
   describe "GET /reactions?article_id=:article_id" do
     before do
