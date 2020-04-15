d2945a797ec4f857f7a8bfb168bf32cb165d764e
ludwiczakpawel <ludwiczakpawel@gmail.com>
2020-04-15 15:46:49 +0200

    counting logic

--- app/javascript/articles/components/CommentsCount.jsx
+++ app/javascript/articles/components/CommentsCount.jsx
@@ -2,22 +2,38 @@ import { h } from 'preact';
 import PropTypes from 'prop-types';
 
 export const CommentsCount = ({ count, articlePath }) => {
+  const commentsSVG = (
+    <svg
+      className="crayons-icon mr-1"
+      width="24"
+      height="24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M10.5 5h3a6 6 0 110 12v2.625c-3.75-1.5-9-3.75-9-8.625a6 6 0 016-6zM12 15.5h1.5a4.501 4.501 0 001.722-8.657A4.5 4.5 0 0013.5 6.5h-3A4.5 4.5 0 006 11c0 2.707 1.846 4.475 6 6.36V15.5z" />
+    </svg>
+  );
   if (count > 0) {
     return (
       <a
         href={`${articlePath}#comments`}
         className="crayons-story__details__item"
       >
-        <svg
-          className="crayons-icon mr-1"
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M10.5 5h3a6 6 0 110 12v2.625c-3.75-1.5-9-3.75-9-8.625a6 6 0 016-6zM12 15.5h1.5a4.501 4.501 0 001.722-8.657A4.5 4.5 0 0013.5 6.5h-3A4.5 4.5 0 006 11c0 2.707 1.846 4.475 6 6.36V15.5z" />
-        </svg>
+        {commentsSVG}
         {count}
-        <span className="hidden s:inline">&nbsp;comments</span>
+        <span className="hidden s:inline">
+          &nbsp;comment
+          {count !== 1 ? 's' : ''}
+        </span>
+      </a>
+    );
+  } if (count === 0) {
+    return (
+      <a
+        href={`${articlePath}#comments`}
+        className="crayons-story__details__item"
+      >
+        {commentsSVG}
+        <span className="hidden s:inline">Add comment</span>
       </a>
     );
   }
