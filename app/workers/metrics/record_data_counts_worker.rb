61f807d1303410d043d23886f69cc9cecd538d22
Molly Struve <mollylbs@gmail.com>
2020-04-15 03:53:21 -0500

    Record DB counts with `count`, Fallback on estimated_count (#7286)
    * call count on model and if that times out fallback on estimated
    
    * improve specs

--- app/workers/metrics/record_data_counts_worker.rb
+++ app/workers/metrics/record_data_counts_worker.rb
@@ -6,9 +6,14 @@ module Metrics
     def perform
       models = [User, Article, Organization, Comment, Podcast, PodcastEpisode, ClassifiedListing, PageView, Notification]
       models.each do |model|
-        estimate = model.estimated_count
-        Rails.logger.info("db_table_size", table_info: { table_name: model.table_name, table_size: estimate })
-        DatadogStatsClient.gauge("postgres.db_table_size", estimate, tags: { table_name: model.table_name })
+        db_count = begin
+                     model.count
+                   rescue ActiveRecord::QueryCanceled
+                     model.estimated_count
+                   end
+
+        Rails.logger.info("db_table_size", table_info: { table_name: model.table_name, table_size: db_count })
+        DatadogStatsClient.gauge("postgres.db_table_size", db_count, tags: { table_name: model.table_name })
 
         next unless model.const_defined?(:SEARCH_CLASS)
 
