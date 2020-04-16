25859c97fa3e4106607ec9cecc7cf06400e5b6b5
Nick Taylor <nick@iamdeveloper.com>
2020-04-16 15:52:56 -0400

    wip

--- app/javascript/Snackbar/Snackbar.jsx
+++ app/javascript/Snackbar/Snackbar.jsx
@@ -1,23 +1,56 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import PropTypes from 'prop-types';
 import { Button, ButtonGroup } from '@crayons';
 import { defaultChildrenPropTypes } from '../src/components/common-prop-types';
 
-export const Snackbar = ({ children, actions = [] }) => (
-  <div className="crayons-snackbar__item flex">
-    <div className="crayons-snackbar__body">{children}</div>
-    <div className="crayons-snackbar__actions">
-      {/* TODO: Figure out what the key should be for mapped buttons */}
-      <ButtonGroup>
-        {actions.map(({ text, handler }) => (
-          <Button variant="secondary" onClick={handler} key={text}>
-            {text}
-          </Button>
-        ))}
-      </ButtonGroup>
-    </div>
-  </div>
-);
+export class Snackbar extends Component {
+  constructor(props) {
+    super(props);
+
+    const { children, lifespan = 5000 } = props;
+
+    // 5 second lifespan by default before the snackbar is removed
+    this.state = {
+      lifespan,
+      contents: children,
+    };
+  }
+
+  componentDidMount() {
+    const { lifespan } = this.state;
+
+    setTimeout(() => {
+      this.setState({ contents: undefined });
+    }, lifespan);
+  }
+
+  render() {
+    const { actions = [] } = this.props;
+    const { contents } = this.state;
+
+    return contents ? (
+      <div className="crayons-snackbar">
+        <div
+          className="crayons-snackbar__item flex"
+          ref={(element) => {
+            this.element = element;
+          }}
+        >
+          <div className="crayons-snackbar__body">{contents}</div>
+          <div className="crayons-snackbar__actions">
+            <ButtonGroup>
+              {actions.map(({ text, handler }) => (
+                <Button variant="secondary" onClick={handler} key={text}>
+                  {text}
+                </Button>
+              ))}
+            </ButtonGroup>
+          </div>
+        </div>
+      </div>
+    ) : null;
+  }
+}
 
 Snackbar.displayName = 'Snackbar';
 
@@ -29,4 +62,5 @@ Snackbar.propTypes = {
       handler: PropTypes.func.isRequired,
     }),
   ).isRequired,
+  lifespan: PropTypes.number.isRequired,
 };
--- app/javascript/Snackbar/__stories__/Snackbar.stories.jsx
+++ app/javascript/Snackbar/__stories__/Snackbar.stories.jsx
@@ -1,15 +1,64 @@
 import { h } from 'preact';
 import { action } from '@storybook/addon-actions';
+import { number } from '@storybook/addon-knobs';
 import { Snackbar } from '../Snackbar';
 
 export default {
   title: 'App Components/Snackbar',
-  decorators: [
-    (story) => <div className="crayons-snackbar absolute">{story()}</div>,
-  ],
 };
 
-export const Default = () => <Snackbar>Hello world!</Snackbar>;
+export const Description = () => (
+  <div className="container">
+    <div className="body">
+      <h2>Snackbars</h2>
+      <p>
+        Snackbars inform users of a process that an app has performed. They
+        appear temporarily, towards the bottom of the screen. They shouldn’t
+        interrupt the user experience, and they don’t require user input to
+        disappear.
+      </p>
+      <p>
+        A Snackbar can contain a single action. Because they disappear
+        automatically, the action shouldn’t be “Dismiss” or “Cancel.”
+      </p>
+      <p>
+        A Snackbar disappears after 5000ms by default. Countdown will be paused
+        when user mouse over the snackbar.
+      </p>
+    </div>
+    <div>
+      <h3>Usage</h3>
+      <p>
+        The Snackbar component has a default lifespan of 5000ms, if `no
+        lifespan` prop is provided.
+      </p>
+      <h4>No Actions</h4>
+      <pre>
+        &lt;Snackbar lifespan=&quot;&apos;4000&quot;&apos;&gt;Hello
+        World!&lt;/Snackbar&gt;
+      </pre>
+      <Snackbar lifespan="4000">Hello World!</Snackbar>
+      <h4>With One or More Actions</h4>
+      <pre>
+        const actions = &#91; &#123; text: &apos;Action 1&apos;, handler: event
+        =&gt; &#123; console.log&#40;&apos;Action 1 clicked&apos;&#41; &#125;
+        &#125;, &#123; text: &apos;Action 2&apos;, handler: event =&gt; &#123;
+        console.log&#40;&apos;Action 2 clicked&apos;&#41; &#125; &#125;&#93;
+        &#13; &#13; &#13; &lt;Snackbar lifespan=&quot;&apos;3000&quot;&apos;
+        actions= &#123;actions&#125; &gt;Hello World!&lt;/Snackbar&gt;
+      </pre>
+      <Snackbar lifespan="4000">Hello World!</Snackbar>
+    </div>
+  </div>
+);
+
+Description.story = {
+  name: 'description',
+};
+
+export const Default = () => (
+  <Snackbar lifespan={number('lifespan', 5000)}>Hello world!</Snackbar>
+);
 
 Default.story = {
   name: 'default',
@@ -22,7 +71,11 @@ export const WithOneAction = () => {
       handler: action('Action 1 fired.'),
     },
   ];
-  return <Snackbar actions={actions}>Hello world!</Snackbar>;
+  return (
+    <Snackbar actions={actions} lifespan={number('lifespan', 5000)}>
+      Hello world!
+    </Snackbar>
+  );
 };
 
 WithOneAction.story = {
@@ -44,7 +97,11 @@ export const WithMultipleActions = () => {
       handler: action('Action 3 fired.'),
     },
   ];
-  return <Snackbar actions={actions}>Hello world!</Snackbar>;
+  return (
+    <Snackbar actions={actions} lifespan={number('lifespan', 5000)}>
+      Hello world!
+    </Snackbar>
+  );
 };
 
 WithMultipleActions.story = {
