710936c804ce250146066bbf000384548a8c03df
Ridhwana <ridhwana.khan16@gmail.com>
2020-04-17 13:31:28 +0200

    chore: more robocop

--- app/mailers/digest_mailer.rb
+++ app/mailers/digest_mailer.rb
@@ -1,5 +1,5 @@
 class DigestMailer < ApplicationMailer
-  default from: -> { "#{ApplicationConfig["COMMUNITY_NAME"]} Digest <#{SiteConfig.default_site_email}>" }
+  default from: -> { "#{ApplicationConfig['COMMUNITY_NAME']} Digest <#{SiteConfig.default_site_email}>" }
 
   def digest_email(user, articles)
     @user = user
