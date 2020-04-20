b7e5aaef18220738f23c1e9a84d2a535f8767f76
rhymes <rhymesete@gmail.com>
2020-04-20 17:55:24 +0200

    [deploy] Fix badges slug migration (#7392)

--- db/migrate/20200420130910_add_unique_index_to_badges_slug.rb
+++ db/migrate/20200420130910_add_unique_index_to_badges_slug.rb
@@ -1,7 +1,15 @@
 class AddUniqueIndexToBadgesSlug < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
 
-  def change
+  def up
+    return if index_exists?(:badges, :slug)
+
     add_index :badges, :slug, unique: true, algorithm: :concurrently
+  end
+
+  def down
+    return unless index_exists?(:badges, :slug)
+
+    remove_index :badges, :slug
   end
 end
