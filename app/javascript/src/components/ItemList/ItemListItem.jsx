e4733e8b0c3a3bab410888de47d4778d88df2356
mstruve <mollylbs@gmail.com>
2020-04-22 09:54:24 -0500

    Execute Reading List searches in Elasticsearch

--- app/javascript/src/components/ItemList/ItemListItem.jsx
+++ app/javascript/src/components/ItemList/ItemListItem.jsx
@@ -4,13 +4,12 @@ import { PropTypes } from 'preact-compat';
 
 export const ItemListItem = ({ item, children }) => {
   const adaptedItem = {
-    path: item.article_path || item.searchable_reactable_path,
-    title: item.article_title || item.searchable_reactable_title,
-    user: item.article_user || item.reactable_user,
-    publishedDate: item.reactable_published_date,
-    visitedDate: item.readable_visited_at,
-    readingTime: item.article_reading_time || item.reading_time,
-    tags: item.article_tags || item.reactable_tags,
+    path: item.reactable.path,
+    title: item.reactable.title,
+    user: item.reactable.user,
+    publishedDate: item.reactable.published_date_string,
+    readingTime: item.reactable.reading_time,
+    tags: item.reactable.tags,
   };
 
   // update readingTime to 1 min if the reading time is less than 1 min
@@ -24,17 +23,15 @@ export const ItemListItem = ({ item, children }) => {
           <a className="item-user" href={`/${adaptedItem.user.username}`}>
             <img src={adaptedItem.user.profile_image_90} alt="Profile Pic" />
             {`${adaptedItem.user.name}・`}
-            {adaptedItem.visitedDate
-              ? `visited on ${adaptedItem.visitedDate}・`
-              : `${adaptedItem.publishedDate}・`}
+            {`${adaptedItem.publishedDate}・`}
             {`${adaptedItem.readingTime} min read・`}
           </a>
 
           {adaptedItem.tags.length > 0 ? (
             <span className="item-tags">
-              {adaptedItem.tags.map(tag => (
-                <a className="item-tag" href={`/t/${tag}`}>
-                  {`#${tag}`}
+              {adaptedItem.tags.map((tag) => (
+                <a className="item-tag" href={`/t/${tag.name}`}>
+                  {`#${tag.name}`}
                 </a>
               ))}
             </span>
@@ -49,30 +46,19 @@ export const ItemListItem = ({ item, children }) => {
   );
 };
 
-const historyItemPropTypes = PropTypes.shape({
-  article_path: PropTypes.string.isRequired,
-  article_title: PropTypes.string.isRequired,
-  article_user: PropTypes.shape({
-    username: PropTypes.string.isRequired,
-    profile_image_90: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-  }),
-  article_reading_time: PropTypes.number.isRequired,
-  readable_visited_at: PropTypes.string.isRequired,
-  article_tags: PropTypes.arrayOf(PropTypes.string).isRequired,
-});
-
 const readingListItemPropTypes = PropTypes.shape({
-  searchable_reactable_path: PropTypes.string.isRequired,
-  searchable_reactable_title: PropTypes.string.isRequired,
-  reactable_user: PropTypes.shape({
-    username: PropTypes.string.isRequired,
-    profile_image_90: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-  }),
-  reading_time: PropTypes.number.isRequired,
-  reactable_published_date: PropTypes.string.isRequired,
-  reactable_tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+  reactable: {
+    path: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    reading_time: PropTypes.number.isRequired,
+    published_date_string: PropTypes.string.isRequired,
+    user: PropTypes.shape({
+      username: PropTypes.string.isRequired,
+      profile_image_90: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    }),
+    tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+  },
 });
 
 ItemListItem.defaultProps = {
@@ -80,7 +66,6 @@ ItemListItem.defaultProps = {
 };
 
 ItemListItem.propTypes = {
-  item: PropTypes.oneOfType([historyItemPropTypes, readingListItemPropTypes])
-    .isRequired,
+  item: PropTypes.oneOfType([readingListItemPropTypes]).isRequired,
   children: PropTypes.element,
 };
--- app/javascript/src/components/ItemList/__tests__/ItemListItem.test.jsx
+++ app/javascript/src/components/ItemList/__tests__/ItemListItem.test.jsx
@@ -3,55 +3,45 @@ import render from 'preact-render-to-json';
 import { shallow } from 'preact-render-spy';
 import { ItemListItem } from '../ItemListItem';
 
-const historyItem = {
-  article_path: '/article',
-  article_title: 'Title',
-  article_user: {
-    username: 'bob',
-    profile_image_90: 'https://dummyimage.com/90x90',
-    name: 'Bob',
-  },
-  article_reading_time: 1,
-  readable_visited_at: 'Jun 29',
-  article_tags: ['discuss'],
-};
-
 const item = {
-  searchable_reactable_path: '/article',
-  searchable_reactable_title: 'Title',
-  reactable_user: {
-    username: 'bob',
-    profile_image_90: 'https://dummyimage.com/90x90',
-    name: 'Bob',
+  reactable: {
+    path: '/article',
+    title: 'Title',
+    published_date_string: 'Jun 29',
+    reading_time: 1,
+    user: {
+      username: 'bob',
+      profile_image_90: 'https://dummyimage.com/90x90',
+      name: 'Bob',
+    },
+    tags: [{ name: 'discuss' }],
   },
-  reading_time: 1,
-  reactable_published_date: 'Jun 29',
-  reactable_tags: ['discuss'],
 };
 
 describe('<ItemListItem />', () => {
-  it('renders properly with a history item', () => {
-    const tree = render(<ItemListItem item={historyItem} />);
-    expect(tree).toMatchSnapshot();
-  });
-
   it('renders properly with a readinglist item', () => {
     const tree = render(<ItemListItem item={item} />);
     expect(tree).toMatchSnapshot();
   });
 
   it('renders with readingtime of 1 min if reading time is less than 1 min.', () => {
-    const wrapper = shallow(<ItemListItem item={{...item, reading_time: 0.5 }} />);
+    const wrapper = shallow(
+      <ItemListItem item={{ ...item, reading_time: 0.5 }} />,
+    );
     expect(wrapper.find('.item-user').text()).toContain('1 min read');
   });
 
   it('renders with readingtime of 1 min if reading time is null.', () => {
-    const wrapper = shallow(<ItemListItem item={{...item, reading_time: null }} />);
+    const wrapper = shallow(
+      <ItemListItem item={{ ...item, reading_time: null }} />,
+    );
     expect(wrapper.find('.item-user').text()).toContain('1 min read');
   });
 
   it('renders without any tags if the tags array is empty.', () => {
-    const wrapper = shallow(<ItemListItem item={{...item, reactable_tags: [] }} />);
+    const wrapper = shallow(
+      <ItemListItem item={{ ...item, reactable_tags: [] }} />,
+    );
     expect(wrapper.find('.item-user').text()).toContain('1 min read');
   });
 });
