Planetchloe/dev.to

Refactor jbuilder for articles

app/helpers/articles_helper.rb
@@ -54,4 +54,8 @@ module ArticlesHelper
     host.gsub!("medium.com", "Medium")
     host.delete_prefix("www.")
   end
+
+  def formated_date(timestamp)
+    timestamp&.utc&.iso8601
+  end
 end

app/views/api/v0/articles/_article.json.jbuilder
@@ -1,20 +1,25 @@
-json.type_of                  "article"
-json.id                       article.id
-json.title                    article.title
-json.description              article.description
-json.cover_image              cloud_cover_url(article.main_image)
-json.readable_publish_date    article.readable_publish_date
-json.social_image             article_social_image_url(article)
-json.slug                     article.slug
-json.path                     article.path
-json.url                      article.url
-json.canonical_url            article.processed_canonical_url
-json.comments_count           article.comments_count
-json.positive_reactions_count article.positive_reactions_count
-json.collection_id            article.collection_id
-json.created_at               article.created_at.utc.iso8601
-json.edited_at                article.edited_at&.utc&.iso8601
-json.crossposted_at           article.crossposted_at&.utc&.iso8601
-json.published_at             article.published_at&.utc&.iso8601
-json.last_comment_at          article.last_comment_at&.utc&.iso8601
-json.published_timestamp      article.published_timestamp
+json.type_of "article"
+
+json.extract!(
+  article,
+  :id,
+  :title,
+  :description,
+  :readable_publish_date,
+  :slug,
+  :path,
+  :url,
+  :comments_count,
+  :positive_reactions_count,
+  :collection_id,
+  :published_timestamp,
+)
+
+json.cover_image     cloud_cover_url(article.main_image)
+json.social_image    article_social_image_url(article)
+json.canonical_url   article.processed_canonical_url
+json.created_at      formated_date(article.created_at)
+json.edited_at       formated_date(article.edited_at)
+json.crossposted_at  formated_date(article.crosspost)
+json.published_at    formated_date(article.published)
+json.last_comment_at formated_date(article.last_comment_at)

forem/forem

app/helpers/articles_helper.rb
@@ -54,4 +54,8 @@ module ArticlesHelper
     host.gsub!("medium.com", "Medium")
     host.delete_prefix("www.")
   end
+
+  def utc_iso_timestamp(timestamp)
+    timestamp&.utc&.iso8601
+  end
 end

app/views/api/v0/articles/_article.json.jbuilder
@@ -1,20 +1,25 @@
-json.type_of                  "article"
-json.id                       article.id
-json.title                    article.title
-json.description              article.description
-json.cover_image              cloud_cover_url(article.main_image)
-json.readable_publish_date    article.readable_publish_date
-json.social_image             article_social_image_url(article)
-json.slug                     article.slug
-json.path                     article.path
-json.url                      article.url
-json.canonical_url            article.processed_canonical_url
-json.comments_count           article.comments_count
-json.positive_reactions_count article.positive_reactions_count
-json.collection_id            article.collection_id
-json.created_at               article.created_at.utc.iso8601
-json.edited_at                article.edited_at&.utc&.iso8601
-json.crossposted_at           article.crossposted_at&.utc&.iso8601
-json.published_at             article.published_at&.utc&.iso8601
-json.last_comment_at          article.last_comment_at&.utc&.iso8601
-json.published_timestamp      article.published_timestamp
+json.type_of "article"
+
+json.extract!(
+  article,
+  :id,
+  :title,
+  :description,
+  :readable_publish_date,
+  :slug,
+  :path,
+  :url,
+  :comments_count,
+  :positive_reactions_count,
+  :collection_id,
+  :published_timestamp,
+)
+
+json.cover_image     cloud_cover_url(article.main_image)
+json.social_image    article_social_image_url(article)
+json.canonical_url   article.processed_canonical_url
+json.created_at      utc_iso_timestamp(article.created_at)
+json.edited_at       utc_iso_timestamp(article.edited_at)
+json.crossposted_at  utc_iso_timestamp(article.crossposted_at)
+json.published_at    utc_iso_timestamp(article.published_at)
+json.last_comment_at utc_iso_timestamp(article.last_comment_at)