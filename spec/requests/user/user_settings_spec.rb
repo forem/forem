b6664456c0e9db5ab03f9968904e64f23a61ecce
Alex <alexandersmith223@gmail.com>
2020-04-23 15:30:59 -0400

    [deploy] Validate filenames server-side (#7405)

--- spec/requests/user/user_settings_spec.rb
+++ spec/requests/user/user_settings_spec.rb
@@ -151,6 +151,15 @@ RSpec.describe "UserSettings", type: :request do
       expect(DatadogStatsClient).to have_received(:increment).with("image_upload_error", tags)
     end
 
+    it "returns error if Profile image file name is too long" do
+      profile_image = fixture_file_upload("files/800x600.png", "image/png")
+      allow(profile_image).to receive(:original_filename).and_return("#{'a_very_long_filename' * 15}.png")
+
+      put "/users/#{user.id}", params: { user: { tab: "profile", profile_image: profile_image } }
+
+      expect(response).to have_http_status(:bad_request)
+    end
+
     context "when requesting an export of the articles" do
       def send_request(flag = true)
         put "/users/#{user.id}", params: {
