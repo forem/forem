aca25294deb273dc899030c8699922bc0af9a470
rhymes <rhymesete@gmail.com>
2020-04-24 20:03:13 +0200

    Refactor sign_in_path and authentication_path

--- app/services/authentication/providers.rb
+++ app/services/authentication/providers.rb
@@ -55,17 +55,15 @@ module Authentication
     end
 
     # Returns the sign in URL for the given provider
-    def self.sign_in_url(provider_name, state: nil)
+    def self.sign_in_path(provider_name, params = {})
       url_helpers = Rails.application.routes.url_helpers
 
       callback_url_helper = "user_#{provider_name}_omniauth_callback_path"
-      params = {
+      mandatory_params = {
         callback_url: URL.url(url_helpers.public_send(callback_url_helper))
       }
 
-      params[:state] = state if state
-
-      url_helpers.public_send("user_#{provider_name}_omniauth_authorize_path", params)
+      authentication_path(provider_name, params.merge(mandatory_params))
     end
   end
 end
