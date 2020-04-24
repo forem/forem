9f0d2c08956630e0e4dbf64840707d0710ec603c
rhymes <rhymesete@gmail.com>
2020-04-24 17:25:36 +0200

    Add Providers(.available|.enabled|.enabled?)

--- app/services/authentication/providers.rb
+++ app/services/authentication/providers.rb
@@ -1,11 +1,48 @@
 module Authentication
   module Providers
-    # TODO: [thepracticaldev/oss] raise exception if provider is available but not enabled for this app
-    # TODO: [thepracticaldev/oss] add available providers, enabled providers
+    # Retrieves a provider that is both available and enabled
     def self.get!(provider_name)
-      "Authentication::Providers::#{provider_name.titleize}".constantize
-    rescue NameError => e
-      raise ::Authentication::Errors::ProviderNotFound, e
+      name = provider_name.to_s.titleize
+
+      "Authentication::Providers::#{name}".safe_constantize.tap do |klass|
+        unless klass
+          raise(
+            ::Authentication::Errors::ProviderNotFound,
+            "Provider #{name} is not available!",
+          )
+        end
+
+        unless enabled?(provider_name)
+          raise(
+            ::Authentication::Errors::ProviderNotEnabled,
+            "Provider #{name} is not enabled!",
+          )
+        end
+      end
+    end
+
+    # Returns available providers
+    def self.available
+      # as providers are lazily loaded, we need to load them all
+      # for .subclasses to work correctly
+      providers_path = Rails.root.join(
+        "app/services/authentication/providers/*.rb",
+      ).cleanpath
+      Dir[providers_path].each { |file| load(file) }
+
+      Authentication::Providers::Provider.subclasses.map do |subclass|
+        subclass.name.demodulize.downcase.to_sym
+      end.sort
+    end
+
+    # Returns enabled providers
+    def self.enabled
+      SiteConfig.authentication_providers.map(&:to_sym).sort
+    end
+
+    # Returns true if a provider is enabled, false otherwise
+    def self.enabled?(provider_name)
+      enabled.include?(provider_name.to_sym)
     end
   end
 end
