43f5d78f20e9d2ee1abb141bb7205de203a69567
Thibault <933kss@gmail.com>
2020-04-27 20:36:49 +0200

    Add nil value check

--- app/workers/organizations/update_organization_articles_paths_worker.rb
+++ app/workers/organizations/update_organization_articles_paths_worker.rb
@@ -5,8 +5,11 @@ module Organizations
     sidekiq_options queue: :high_priority
 
     def perform(organization_id, old_slug, new_slug)
-      articles = Organization.find_by(id: organization_id).articles
-      articles.find_each { |article| article.update(path: article.path.gsub(old_slug, new_slug)) }
+      organization = Organization.find_by(id: organization_id)
+
+      return unless organization
+
+      organization.articles.find_each { |article| article.update(path: article.path.gsub(old_slug, new_slug)) }
     end
   end
 end
