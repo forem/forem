17b2486c513720b30f8528303e3f749f6e083ba8
rhymes <rhymesete@gmail.com>
2020-04-27 18:00:12 +0200

    [deploy] Replace (black|white)list with more inclusive language (#7459)
    * Replace isBlacklisted
    
    * Update fastly.rake task
    
    * More renames
    
    * Rename Faslty config file
    
    * More changes
    
    * Replace isForbiddenFromPreloading with isNotPreloadable
    
    * Skip problematic test
    
    * Re-enable

--- config/initializers/docker.rb
+++ config/initializers/docker.rb
@@ -3,7 +3,7 @@ if Rails.env.development? && File.file?("/.dockerenv")
   # Using shell tools so we don't need to require Socket and IPAddr
   host_ip = `/sbin/ip route|awk '/default/ { print $3 }'`.strip
   logger = Logger.new(STDOUT)
-  logger.info "Whitelisting #{host_ip} for BetterErrors and Web Console"
+  logger.info "Allowing #{host_ip} for BetterErrors and Web Console"
 
   if defined?(BetterErrors::Middleware)
     BetterErrors::Middleware.allow_ip!(host_ip)
