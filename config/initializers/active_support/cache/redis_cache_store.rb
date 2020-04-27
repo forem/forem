30729f4dfa50423f2e3ea746a35a645ccf979908
Tam Chau <chauviettam@gmail.com>
2020-04-27 20:53:16 +0900

    add comment to explain monkey patch for RedisCacheStore#increment

--- config/initializers/active_support/cache/redis_cache_store.rb
+++ config/initializers/active_support/cache/redis_cache_store.rb
@@ -2,6 +2,9 @@ module ActiveSupport
   module Cache
     class RedisCacheStore
       def increment(name, amount = 1, options = nil)
+        # With Rails 5.2.4, RedisCacheStore#increment method does not support option :expires_in.
+        # In order to workaround, we call expire command if the key is incremented at the first time.
+        # This patch should be removed when upgrading to Rails 6.
         instrument :increment, name, amount: amount do
           failsafe :increment do
             options = merged_options(options)
