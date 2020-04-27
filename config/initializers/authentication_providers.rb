e171f046637d79769193c02b2ed5344b68392528
rhymes <rhymesete@gmail.com>
2020-04-27 13:44:06 +0200

    Put require_dependency in the correct place

--- config/initializers/authentication_providers.rb
+++
@@ -1,7 +0,0 @@
-# We preload the authentication module on initialization
-# to make sure providers are correctly preloaded
-# both in development and in production and ready to be used when needed
-# at runtime
-Dir[Rails.root.join("app/services/authentication/**/*.rb")].each do |f|
-  require_dependency(f)
-end
