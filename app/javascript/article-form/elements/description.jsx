24dff649939a49a67d6a0ada9ad460d930ba7111
Juliano Lima <juliano.lima@nubank.com.br>
2020-04-29 08:42:00 -0300

    Remove id from article-form description

--- app/javascript/article-form/elements/description.jsx
+++ app/javascript/article-form/elements/description.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 const Description = ({ onChange, defaultValue }) => (
   <textarea
     className="articleform__description"
-    id="article-form-description"
     type="text"
     placeholder="description"
     name="description"
