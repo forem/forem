106156e9bc90f4be5930d7bea51a3079f022b99a
Sarthak <7lovesharma7@gmail.com>
2020-04-29 17:27:31 +0530

    🛠 Svg code optimized

--- app/javascript/chat/content.jsx
+++ app/javascript/chat/content.jsx
@@ -15,6 +15,18 @@ export default class Content extends Component {
     if (!this.props.resource) {
       return '';
     }
+    const smartSvgIcon = (content, d) => (
+      <svg
+        data-content={content}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+      >
+        <path data-content={content} fill="none" d="M0 0h24v24H0z" />
+        <path data-content={content} d={d} />
+      </svg>
+    );
 
     return (
       <div
@@ -26,19 +38,10 @@ export default class Content extends Component {
           className="activechatchannel__activecontentexitbutton crayons-btn crayons-btn--secondary"
           data-content="exit"
         >
-          <svg
-            data-content="exit"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-          >
-            <path data-content="exit" fill="none" d="M0 0h24v24H0z" />
-            <path
-              data-content="exit"
-              d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z"
-            />
-          </svg>
+          {smartSvgIcon(
+            'exit',
+            'M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z',
+          )}
         </button>
         <button
           className="activechatchannel__activecontentexitbutton activechatchannel__activecontentexitbutton--fullscreen crayons-btn crayons-btn--secondary"
@@ -46,26 +49,15 @@ export default class Content extends Component {
           style={{ left: '39px' }}
         >
           {' '}
-          <svg
-            data-content="fullscreen"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-          >
-            <path data-content="fullscreen" fill="none" d="M0 0h24v24H0z" />
-            {fullscreen ? (
-              <path
-                data-content="fullscreen"
-                d="M18 7h4v2h-6V3h2v4zM8 9H2V7h4V3h2v6zm10 8v4h-2v-6h6v2h-4zM8 15v6H6v-4H2v-2h6z"
-              />
-            ) : (
-              <path
-                data-content="fullscreen"
-                d="M20 3h2v6h-2V5h-4V3h4zM4 3h4v2H4v4H2V3h2zm16 16v-4h2v6h-6v-2h4zM4 19h4v2H2v-6h2v4z"
-              />
-            )}
-          </svg>
+          {fullscreen
+            ? smartSvgIcon(
+                'fullscreen',
+                'M18 7h4v2h-6V3h2v4zM8 9H2V7h4V3h2v6zm10 8v4h-2v-6h6v2h-4zM8 15v6H6v-4H2v-2h6z',
+              )
+            : smartSvgIcon(
+                'fullscreen',
+                'M20 3h2v6h-2V5h-4V3h4zM4 3h4v2H4v4H2V3h2zm16 16v-4h2v6h-6v-2h4zM4 19h4v2H2v-6h2v4z',
+              )}
         </button>
         {display(this.props)}
       </div>
