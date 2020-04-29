b30406a50e491c53c3dce5be03bcdf8112b043df
Nick Taylor <nick@iamdeveloper.com>
2020-04-29 15:25:17 -0400

    Extracted <CogIcon /> to it's own file because other features are going to need it.

--- app/javascript/CommentSubscription/CommentSubscription.jsx
+++ app/javascript/CommentSubscription/CommentSubscription.jsx
@@ -7,6 +7,7 @@ import {
   FormField,
   RadioButton,
 } from '@crayons';
+import { CogIcon } from '../icons';
 
 export const COMMENT_SUBSCRIPTION_TYPE = Object.freeze({
   ALL: 'all_comments',
@@ -38,17 +39,49 @@ export class CommentSubscription extends Component {
 
   componentDidUpdate() {
     const { showOptions } = this.state;
+    const { base: element } = this.dropdownElement;
 
     if (showOptions) {
       window.addEventListener('scroll', this.dropdownPlacementHandler);
       this.dropdownPlacementHandler();
+
+      this.leftOptionsPanel = (event) => {
+        event.stopPropagation();
+        this.mousedOut = true;
+      };
+
+      element.addEventListener('mouseout', this.leftOptionsPanel, true);
+
+      this.enteredOptionsPanel = (_event) => {
+        this.mousedOut = false;
+      };
+
+      element.addEventListener('mouseover', this.enteredOptionsPanel);
+
+      this.closeOnOutsideClick = (_event) => {
+        if (!this.mousedOut) {
+          return;
+        }
+
+        this.setState({ showOptions: false });
+      };
+
+      window.addEventListener('click', this.closeOnOutsideClick);
     } else {
+      element.removeEventListener('mouseout', this.leftOptionsPanel);
+      element.removeEventListener('mouseover', this.enteredOptionsPanel);
       window.removeEventListener('scroll', this.dropdownPlacementHandler);
+      window.removeEventListener('click', this.closeOnOutsideClick);
     }
   }
 
   componentWillUnmount() {
+    const { base: element } = this.dropdownElement;
+
+    element.removeEventListener('mouseout', this.leftOptionsPanel);
+    element.removeEventListener('mouseover', this.enteredOptionsPanel);
     window.removeEventListener('scroll', this.dropdownPlacementHandler);
+    window.removeEventListener('click', this.closeOnOutsideClick);
   }
 
   dropdownPlacementHandler = () => {
@@ -83,20 +116,6 @@ export class CommentSubscription extends Component {
       positionType = 'relative',
     } = this.props;
 
-    const CogIcon = () => (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        role="img"
-        aria-labelledby="ai2ols8ka2ohfp0z568lj68ic2du21s"
-        className="crayons-icon"
-      >
-        <title id="ai2ols8ka2ohfp0z568lj68ic2du21s">Preferences</title>
-        <path d="M12 1l9.5 5.5v11L12 23l-9.5-5.5v-11L12 1zm0 2.311L4.5 7.653v8.694l7.5 4.342 7.5-4.342V7.653L12 3.311zM12 16a4 4 0 110-8 4 4 0 010 8zm0-2a2 2 0 100-4 2 2 0 000 4z" />
-      </svg>
-    );
-
     return (
       <div className={positionType}>
         <ButtonGroup
@@ -127,6 +146,7 @@ export class CommentSubscription extends Component {
               icon={CogIcon}
               contentType="icon"
               onClick={(_event) => {
+                this.mousedOut = false;
                 this.setState({ showOptions: !showOptions });
               }}
             />
