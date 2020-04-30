c222f78760bd85bdf4202dde47a4892238338b3e
Alex <alexandersmith223@gmail.com>
2020-04-30 15:17:07 -0400

    [deploy] Turn off broken article show spec - date time (#7634)
    * Turn off broken spec
    
    * Restore original spec

--- spec/requests/articles/articles_show_spec.rb
+++ spec/requests/articles/articles_show_spec.rb
@@ -23,15 +23,12 @@ RSpec.describe "ArticlesShow", type: :request do
       expect(response.body).to include CGI.escapeHTML(article.readable_publish_date)
     end
 
-    it "renders the proper modified at date" do
+    # This is a flakey spec. Due to how we are localizing date times, sometimes
+    # the date is off. See https://github.com/thepracticaldev/dev.to/issues/7086.
+    xit "renders the proper modified at date" do
       article.update(edited_at: Time.zone.now)
       get article.path
-
-      # This is a flakey spec. Due to how we are localizing date times, sometimes the date is off by
-      # 1 day. See https://github.com/thepracticaldev/dev.to/issues/7086.
-      correct_date = CGI.escapeHTML(article.edited_at.strftime("%b %d, %Y"))
-      buffer_date = CGI.escapeHTML((article.edited_at + 1.day).strftime("%b %d, %Y"))
-      expect(response.body).to match(/#{correct_date}|#{buffer_date}/)
+      expect(response.body).to include GI.escapeHTML(article.edited_at.strftime("%b %d, %Y"))
     end
 
     it "renders the proper author" do
