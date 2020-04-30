90ecb36cff7267fe48057cbe5b91b55f0a4305fd
Molly Struve <mollylbs@gmail.com>
2020-04-30 15:25:05 -0500

    Cut down on description length by removing redundant wording
    Co-authored-by: Vaidehi Joshi <vaidehi.sj@gmail.com>

--- app/labor/rate_limit_checker.rb
+++ app/labor/rate_limit_checker.rb
@@ -10,12 +10,12 @@ class RateLimitChecker
   }.with_indifferent_access.freeze
 
   CONFIGURABLE_RATES = {
-    rate_limit_follow_count_daily: { min: 0, placeholder: 500, description: "Used to limit the amount of users a person can follow daily" },
-    rate_limit_comment_creation: { min: 0, placeholder: 9, description: "Used to limit the amount of comments a user can create within 30 seconds" },
-    rate_limit_published_article_creation: { min: 0, placeholder: 9, description: "Used to limit the amount of articles a user can create within 30 seconds" },
-    rate_limit_image_upload: { min: 0, placeholder: 9, description: "Used to limit the amount of images a user can upload within 30 seconds" },
-    rate_limit_email_recipient: { min: 0, placeholder: 5, description: "Used to limit the amount of emails we send to a user within 2 minutes" },
-    rate_limit_organization_creation: { min: 1, placeholder: 1, description: "Number of organizations a user can create within a 5 minute period" }
+    rate_limit_follow_count_daily: { min: 0, placeholder: 500, description: "The number of users a person can follow daily" },
+    rate_limit_comment_creation: { min: 0, placeholder: 9, description: "The number of comments a user can create within 30 seconds" },
+    rate_limit_published_article_creation: { min: 0, placeholder: 9, description: "The number of articles a user can create within 30 seconds" },
+    rate_limit_image_upload: { min: 0, placeholder: 9, description: "The number of images a user can upload within 30 seconds" },
+    rate_limit_email_recipient: { min: 0, placeholder: 5, description: "The number of emails we send to a user within 2 minutes" },
+    rate_limit_organization_creation: { min: 1, placeholder: 1, description: "The number of organizations a user can create within a 5 minute period" }
   }.freeze
 
   def initialize(user = nil)
