6a6b76f2ff9864fbd0cca3ac85bd67e7f5f8a661
Alex <alexandersmith223@gmail.com>
2020-05-01 01:55:11 -0400

    Refactor update

--- lib/tasks/fastly.rake
+++ lib/tasks/fastly.rake
@@ -14,6 +14,6 @@ namespace :fastly do
       next
     end
 
-    FastlyConfig::Update.run
+    FastlyConfig::Update.call
   end
 end
--- spec/tasks/fastly_spec.rb
+++ spec/tasks/fastly_spec.rb
@@ -11,11 +11,11 @@ RSpec.describe "Fastly tasks", type: :task do
       %w[FASTLY_API_KEY FASTLY_SERVICE_ID].each do |var|
         allow(ApplicationConfig).to receive(:[]).with(var).and_return(nil)
       end
-      allow(FastlyConfig::Update).to receive(:run)
+      allow(FastlyConfig::Update).to receive(:call)
 
       Rake::Task["fastly:update_configs"].invoke
 
-      expect(FastlyConfig::Update).not_to have_received(:run)
+      expect(FastlyConfig::Update).not_to have_received(:call)
     end
   end
 end
