01c4dbb438fa6063f85a0d942899acd324b3d018
Josh Puetz <josh@dev.to>
2020-05-03 11:01:22 -0500

    Dont show 'more comment' button for more than 2 comments

--- app/javascript/articles/components/CommentsList.jsx
+++ app/javascript/articles/components/CommentsList.jsx
@@ -2,31 +2,43 @@ import { h } from 'preact';
 import PropTypes from 'prop-types';
 import { CommentListItem } from './CommentListItem';
 
+const numberOfCommentsToShow = 2;
+
 function linkToCommentsSection(articlePath) {
   const str = `${articlePath}#comments-container`;
   return str;
+}
+
+function moreCommentsButton(comments, articlePath, totalCount) {
+  let button = '';
+  if (totalCount > numberOfCommentsToShow) {
+    button = (
+      <div className="crayons-story__comments__actions">
+        <a
+          href={linkToCommentsSection(articlePath)}
+          className="crayons-btn crayons-btn--secondary fs-s mr-2"
+        >
+          See all 
+          {' '}
+          {totalCount}
+          {' '}
+          comments
+        </a>
+      </div>
+    );
+  }
+  return button;
 }
 
 export const CommentsList = ({ comments, articlePath, totalCount }) => {
   if (comments && comments.length > 0) {
     return (
       <div className="crayons-story__comments">
-        {comments.slice(0, 2).map((comment) => {
+        {comments.slice(0, numberOfCommentsToShow).map((comment) => {
           return <CommentListItem comment={comment} />;
         })}
 
-        <div className="crayons-story__comments__actions">
-          <a
-            href={linkToCommentsSection(articlePath)}
-            className="crayons-btn crayons-btn--secondary fs-s mr-2"
-          >
-            See all 
-            {' '}
-            {totalCount}
-            {' '}
-            comments
-          </a>
-        </div>
+        {moreCommentsButton(comments, articlePath, totalCount)}
       </div>
     );
   }
