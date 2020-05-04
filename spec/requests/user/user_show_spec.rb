6ae13ec96a8c5564a58e88e45ee5f29dd3f9eac0
Julianna Tetreault <juliannatetreault@gmail.com>
2020-05-04 14:41:22 -0600

    Wrap test in rubocop:disable to resolve ExampleLength errors   * Removes let blocks that once held extracted code   * Adds code back to the data structure as inline code   * Resolves even more rubocop errors... hopefully

--- spec/requests/user/user_show_spec.rb
+++ spec/requests/user/user_show_spec.rb
@@ -5,35 +5,6 @@ RSpec.describe "UserShow", type: :request do
   let(:doc) { Nokogiri::HTML(response.body) }
   let(:text) { doc.at('script[type="application/ld+json"]').text }
   let(:response_json) { JSON.parse(text) }
-  let(:main_entity_of_page) { { "@type" => "WebPage", "@id" => URL.user(user) } }
-  let(:same_as) do
-    [
-      "https://twitter.com/#{user.twitter_username}",
-      "https://github.com/#{user.github_username}",
-      user.mastodon_url,
-      user.facebook_url,
-      user.youtube_url,
-      user.linkedin_url,
-      user.behance_url,
-      user.stackoverflow_url,
-      user.dribbble_url,
-      user.medium_url,
-      user.gitlab_url,
-      user.instagram_url,
-      user.twitch_username,
-      user.website_url,
-    ]
-  end
-
-  let(:disambiguating_description) do
-    [
-      user.mostly_work_with,
-      user.currently_hacking_on,
-      user.currently_learning,
-    ]
-  end
-
-  let(:works_for) { [{ "@type" => "Organization", "name" => user.employer_name, "url" => user.employer_url }] }
 
   describe "GET /:slug (user)" do
     before do
@@ -44,23 +15,53 @@ RSpec.describe "UserShow", type: :request do
       expect(response).to have_http_status(:ok)
     end
 
+    # rubocop:disable Rspec/ExampleLength
     it "renders the proper JSON-LD for a user" do
       expect(response_json).to include(
         "@context" => "http://schema.org",
         "@type" => "Person",
-        "mainEntityOfPage" => main_entity_of_page,
+        "mainEntityOfPage" => {
+          "@type" => "WebPage",
+          "@id" => URL.user(user)
+        },
         "url" => URL.user(user),
-        "sameAs" => same_as,
+        "sameAs" => [
+          "https://twitter.com/#{user.twitter_username}",
+          "https://github.com/#{user.github_username}",
+          user.mastodon_url,
+          user.facebook_url,
+          user.youtube_url,
+          user.linkedin_url,
+          user.behance_url,
+          user.stackoverflow_url,
+          user.dribbble_url,
+          user.medium_url,
+          user.gitlab_url,
+          user.instagram_url,
+          user.twitch_username,
+          user.website_url,
+        ],
         "image" => ProfileImage.new(user).get(width: 320),
         "name" => user.name,
         "email" => user.email,
         "jobTitle" => user.employment_title,
         "description" => user.summary,
-        "disambiguatingDescription" => disambiguating_description,
-        "worksFor" => works_for,
+        "disambiguatingDescription" => [
+          user.mostly_work_with,
+          user.currently_hacking_on,
+          user.currently_learning,
+        ],
+        "worksFor" => [
+          {
+            "@type" => "Organization",
+            "name" => user.employer_name,
+            "url" => user.employer_url
+          },
+        ],
         "alumniOf" => user.education,
       )
     end
+    # rubocop:enable Rspec/ExampleLength
   end
 
   context "when user signed in" do
