ddf6a06bc2f2dae6afbfbcd5ff9c11ba250162f4
Nick Taylor <nick@iamdeveloper.com>
2020-05-04 13:53:43 -0400

    Fixed a typo in a a folder name. (#7671)

--- app/javascript/crayons/Dropdown/__stories__/dropdown.description.stories.jsx
+++ app/javascript/crayons/Dropdown/__stories__/dropdown.description.stories.jsx
@@ -1,6 +1,6 @@
 import { h } from 'preact';
 
-import '../../storybook-utiltiies/designSystem.scss';
+import '../../storybook-utilities/designSystem.scss';
 
 export default {
   title: 'Components/Dropdowns',
