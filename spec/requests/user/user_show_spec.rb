cbda45fb1ac5e1c6de9bd04aa40d0279d8ba8043
Julianna Tetreault <juliannatetreault@gmail.com>
2020-05-04 13:54:44 -0600

    Refactor user.update_columns to be a single method

--- spec/requests/user/user_show_spec.rb
+++ spec/requests/user/user_show_spec.rb
@@ -37,12 +37,26 @@ RSpec.describe "UserShow", type: :request do
 
   describe "GET /:slug (user)" do
     before do
-      user.update_columns(employment_title: "SEO", employer_name: "DEV", employer_url: "www.dev.to")
-      user.update_columns(currently_learning: "Preact", mostly_work_with: "Ruby", currently_hacking_on: "JSON-LD")
-      user.update_columns(mastodon_url: "www.example.com", facebook_url: "www.facebook.com/example", linkedin_url: "www.linkedin.com/company/example/")
-      user.update_columns(youtube_url: "www.youtube.com/example", behance_url: "www.behance.com/example", stackoverflow_url: "www.stackoverflow.com/example")
-      user.update_columns(dribbble_url: "www.dribbble.com/example", medium_url: "www.medium.com/example", gitlab_url: "www.gitlab.com/example")
-      user.update_columns(instagram_url: "www.instagram.com/example", twitch_username: "Example007", website_url: "www.example.com/example")
+      user.update_columns(
+        employment_title: "SEO",
+        employer_name: "DEV",
+        employer_url: "www.dev.to",
+        currently_learning: "Preact",
+        mostly_work_with: "Ruby",
+        currently_hacking_on: "JSON-LD",
+        mastodon_url: "www.example.com",
+        facebook_url: "www.facebook.com/example",
+        linkedin_url: "www.linkedin.com/company/example/",
+        youtube_url: "www.youtube.com/example",
+        behance_url: "www.behance.com/example",
+        stackoverflow_url: "www.stackoverflow.com/example",
+        dribbble_url: "www.dribbble.com/example",
+        medium_url: "www.medium.com/example",
+        gitlab_url: "www.gitlab.com/example",
+        instagram_url: "www.instagram.com/example",
+        twitch_username: "Example007",
+        website_url: "www.example.com/example",
+      )
       get user.path
     end
 
