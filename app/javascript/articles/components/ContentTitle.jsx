ebfa66befe7fdcfc21d9502b7e4bb414a31b53a0
ludwiczakpawel <ludwiczakpawel@gmail.com>
2020-05-04 11:37:33 +0200

    flare tag

--- app/javascript/articles/components/ContentTitle.jsx
+++ app/javascript/articles/components/ContentTitle.jsx
@@ -6,7 +6,7 @@ export const ContentTitle = ({ article }) => (
     <a href={article.path} id={`article-link-${article.id}`}>
       {article.flare_tag && (
         <span
-          className="crayons-indicator"
+          className="crayons-story__flare-tag"
           style={{
             background: article.flare_tag.bg_color_hex,
             color: article.flare_tag.text_color_hex,
@@ -16,11 +16,11 @@ export const ContentTitle = ({ article }) => (
         </span>
       )}
       {article.class_name === 'PodcastEpisode' && (
-        <span className="crayons-indicator">podcast</span>
+        <span className="crayons-story__flare-tag">podcast</span>
       )}
       {article.class_name === 'User' && (
         <span
-          className="crayons-indicator"
+          className="crayons-story__flare-tag"
           style={{ background: '#5874d9', color: 'white' }}
         >
           person
