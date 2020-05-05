c10d82cfa1817001bc46d1d45ca18a8c0554d77e
ludwiczakpawel <ludwiczakpawel@gmail.com>
2020-05-05 12:40:22 +0200

    fixing buttons

--- app/assets/javascripts/initializers/initializeArticleDate.js
+++ app/assets/javascripts/initializers/initializeArticleDate.js
@@ -1,11 +1,11 @@
-'use strict';
+
 
 /* Show article date/time according to user's locale */
 /* global addLocalizedDateTimeToElementsTitles */
 
 function initializeArticleDate() {
   var articlesDates = document.querySelectorAll(
-    '.single-article time, article time, .single-other-article time',
+    '.crayons-story time, article time, .single-other-article time',
   );
 
   addLocalizedDateTimeToElementsTitles(articlesDates, 'datetime');
