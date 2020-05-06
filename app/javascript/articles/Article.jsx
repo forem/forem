2c67afc268e7f4ab1670c7073e8d411d87458dac
Nick Taylor <nick@iamdeveloper.com>
2020-05-06 10:48:18 -0400

    Got the semantic HTML right, I think...

--- app/javascript/articles/Article.jsx
+++ app/javascript/articles/Article.jsx
@@ -28,78 +28,80 @@ export const Article = ({
 
   return (
     <article
-      tabIndex="0"
       className={`crayons-story w-100 align-left border-0 ${
         isFeatured && 'crayons-story--featured'
       }`}
       id={isFeatured && 'featured-story-marker'}
       data-featured-article={`articles-${article.id}`}
       data-content-user-id={article.user_id}
-      type="button"
-      onClick={(event) => {
-        const { classList } = event.target;
-
-        // This looks a little messy, but it's the only
-        // way to make the entire card clickable.
-        if (
-          classList.contains('crayons-story') ||
-          classList.contains('crayons-story__top') ||
-          classList.contains('crayons-story__body') ||
-          classList.contains('crayons-story__indention') ||
-          classList.contains('crayons-story__title') ||
-          classList.contains('crayons-story__tags') ||
-          classList.contains('crayons-story__bottom')
-        ) {
-          window.location = article.path;
-        }
-      }}
     >
-      {article.cloudinary_video_url && <Video article={article} />}
+      <div
+        role="presentation"
+        onClick={(event) => {
+          const { classList } = event.target;
 
-      {isFeatured && <ArticleCoverImage article={article} />}
-      <div className="crayons-story__body">
-        <div className="crayons-story__top">
-          <Meta article={article} organization={article.organization} />
-        </div>
+          // This looks a little messy, but it's the only
+          // way to make the entire card clickable.
+          if (
+            classList.contains('crayons-story') ||
+            classList.contains('crayons-story__top') ||
+            classList.contains('crayons-story__body') ||
+            classList.contains('crayons-story__indention') ||
+            classList.contains('crayons-story__title') ||
+            classList.contains('crayons-story__tags') ||
+            classList.contains('crayons-story__bottom')
+          ) {
+            window.location = article.path;
+          }
+        }}
+      >
+        {article.cloudinary_video_url && <Video article={article} />}
 
-        <div className="crayons-story__indention">
-          <ContentTitle article={article} />
-          <TagList tags={article.tag_list} />
+        {isFeatured && <ArticleCoverImage article={article} />}
+        <div className="crayons-story__body">
+          <div className="crayons-story__top">
+            <Meta article={article} organization={article.organization} />
+          </div>
 
-          {article.class_name === 'Article' && (
-            // eslint-disable-next-line no-underscore-dangle
-            <SearchSnippet highlightText={article.highlight} />
-          )}
+          <div className="crayons-story__indention">
+            <ContentTitle article={article} />
+            <TagList tags={article.tag_list} />
 
-          <div className="crayons-story__bottom">
-            {article.class_name !== 'User' && (
-              <div className="crayons-story__details">
-                <ReactionsCount article={article} />
-                <CommentsCount
-                  count={article.comments_count}
-                  articlePath={article.path}
-                />
-              </div>
+            {article.class_name === 'Article' && (
+              // eslint-disable-next-line no-underscore-dangle
+              <SearchSnippet highlightText={article.highlight} />
             )}
 
-            <div className="crayons-story__save">
-              <ReadingTime readingTime={article.reading_time} />
+            <div className="crayons-story__bottom">
+              {article.class_name !== 'User' && (
+                <div className="crayons-story__details">
+                  <ReactionsCount article={article} />
+                  <CommentsCount
+                    count={article.comments_count}
+                    articlePath={article.path}
+                  />
+                </div>
+              )}
 
-              <SaveButton
-                article={article}
-                isBookmarked={isBookmarked}
-                onClick={bookmarkClick}
-              />
+              <div className="crayons-story__save">
+                <ReadingTime readingTime={article.reading_time} />
+
+                <SaveButton
+                  article={article}
+                  isBookmarked={isBookmarked}
+                  onClick={bookmarkClick}
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <CommentsList
-        comments={article.top_comments}
-        articlePath={article.path}
-        totalCount={article.comments_count}
-      />
+        <CommentsList
+          comments={article.top_comments}
+          articlePath={article.path}
+          totalCount={article.comments_count}
+        />
+      </div>
     </article>
   );
 };
