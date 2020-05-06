2e08eaeb4f8f8abae3f7c94cf1074891e53aa363
Julianna Tetreault <juliannatetreault@gmail.com>
2020-05-06 11:35:31 -0600

    Refactor articles_show_spec.rb to better test JSON data   * Adds additional tests around JSON-LD data   * Removes original tests for JSON-LD from articles_show_spec

--- spec/requests/articles/articles_show_spec.rb
+++ spec/requests/articles/articles_show_spec.rb
@@ -2,8 +2,6 @@ require "rails_helper"
 
 RSpec.describe "ArticlesShow", type: :request do
   let_it_be(:user) { create(:user) }
-  let_it_be(:organization) { create(:organization) }
-  let_it_be(:organization_article) { create(:article, organization_id: organization.id) }
   let_it_be(:article, reload: true) { create(:article, user: user, published: true) }
 
   describe "GET /:slug (articles)" do
@@ -15,29 +13,54 @@ RSpec.describe "ArticlesShow", type: :request do
       expect(response).to have_http_status(:ok)
     end
 
-    it "renders the proper title" do
-      expect(response.body).to include CGI.escapeHTML(article.title)
+    # rubocop:disable Rspec/ExampleLength
+    it "renders the proper JSON-LD for an article" do
+      doc = Nokogiri::HTML(response.body)
+      text = doc.at('script[type="application/ld+json"]').text
+      response_json = JSON.parse(text)
+      expect(response_json).to include(
+        "@context" => "http://schema.org",
+        "@type" => "Article",
+        "mainEntityOfPage" => {
+          "@type" => "WebPage",
+          "@id" => URL.article(article)
+        },
+        "url" => URL.article(article),
+        "image" => [
+          ApplicationController.helpers.article_social_image_url(article, width: 1080, height: 1080),
+          ApplicationController.helpers.article_social_image_url(article, width: 1280, height: 720),
+          ApplicationController.helpers.article_social_image_url(article, width: 1600, height: 900),
+        ],
+        "publisher" => {
+          "@context" => "http://schema.org",
+          "@type" => "Organization",
+          "name" => "#{ApplicationConfig['COMMUNITY_NAME']} Community",
+          "logo" => {
+            "@context" => "http://schema.org",
+            "@type" => "ImageObject",
+            "url" => ApplicationController.helpers.cloudinary(SiteConfig.logo_png, 192, "png"),
+            "width" => "192",
+            "height" => "192"
+          }
+        },
+        "headline" => article.title,
+        "author" => {
+          "@context" => "http://schema.org",
+          "@type" => "Person",
+          "url" => URL.user(user),
+          "name" => user.name
+        },
+        "datePublished" => article.published_timestamp,
+        "dateModified" => article.edited_at&.iso8601 || article.published_timestamp,
+      )
     end
-
-    it "renders the proper published at date" do
-      expect(response.body).to include CGI.escapeHTML(article.readable_publish_date)
-    end
-
-    # This is a flakey spec. Due to how we are localizing date times, sometimes
-    # the date is off. See https://github.com/thepracticaldev/dev.to/issues/7086.
-    xit "renders the proper modified at date" do
-      article.update(edited_at: Time.zone.now)
-      get article.path
-      expect(response.body).to include GI.escapeHTML(article.edited_at.strftime("%b %d, %Y"))
-    end
-
-    it "renders the proper author" do
-      expect(response.body).to include CGI.escapeHTML(article.cached_user_username)
-    end
+    # rubocop:enable Rspec/ExampleLength
 
     it "renders the proper organization for an article when one is present" do
-      get organization.path
-      expect(response.body).to include CGI.escapeHTML(organization_article.title)
+      org = create(:organization)
+      org_article = create(:article, organization_id: org.id)
+      get org.path
+      expect(response.body).to include(org_article.title)
     end
   end
 
