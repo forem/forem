0d0302ff90654cab9fa877c494f1961ce0ec5ed2
Molly Struve <mollylbs@gmail.com>
2020-05-06 08:27:17 -0500

    Force Unique Jobs for User Initiated Jobs and Cache Jobs (#7688)

--- app/workers/articles/score_calc_worker.rb
+++ app/workers/articles/score_calc_worker.rb
@@ -2,7 +2,7 @@ module Articles
   class ScoreCalcWorker
     include Sidekiq::Worker
 
-    sidekiq_options queue: :medium_priority
+    sidekiq_options queue: :medium_priority, lock: :until_executing
 
     def perform(article_id)
       article = Article.find_by(id: article_id)
