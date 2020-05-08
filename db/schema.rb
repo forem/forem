aab4231e143c61118dfc2d55fcf14b63c653aef4
Arit Amana <msarit@gmail.com>
2020-05-08 11:52:29 -0400

    Fix phantom schema.rb changes

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_05_04_075409) do
+  
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -329,6 +330,7 @@ ActiveRecord::Schema.define(version: 2020_05_04_075409) do
     t.datetime "bumped_at"
     t.string "cached_tag_list"
     t.string "category"
+    t.bigint "classified_listing_category_id"
     t.boolean "contact_via_connect", default: false
     t.datetime "created_at", null: false
     t.bigint "organization_id"
