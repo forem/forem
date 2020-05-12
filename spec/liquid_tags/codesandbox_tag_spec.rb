20f028172b7d8e1f7174c0fdaf2e36e15fc7a01a
Eryk Napierała <eryk.piast@gmail.com>
2020-05-12 14:55:09 +0200

    Allow to specify view parameter for codesandbox (#7781)

--- spec/liquid_tags/codesandbox_tag_spec.rb
+++ spec/liquid_tags/codesandbox_tag_spec.rb
@@ -5,11 +5,15 @@ RSpec.describe CodesandboxTag, type: :liquid_tag do
     let(:valid_id) { "22qaa1wcxr" }
     let(:valid_id_with_initialpath) { "68jkdlsaie initialpath=/initial/path/file.js" }
     let(:valid_id_with_module) { "28qvv1wvxr module=/path/to/module.html" }
+    let(:valid_id_with_view) { "28qvv1wvxr view=editor" }
     let(:valid_id_with_runonclick) { "28qvv1wvxr runonclick=1" }
     let(:valid_id_with_initialpath_and_module) { "43lkjfdauf initialpath=/initial-path/file.js module=/path/to/module.html" }
+    let(:valid_id_with_initialpath_and_view) { "43lkjfdauf initialpath=/initial-path/file.js view=split" }
     let(:valid_id_with_runonclick_and_module) { "43lkjfdauf runonclick=1 module=/path/to/module.html" }
+    let(:valid_id_with_runonclick_and_view) { "28qvv1wvxr runonclick=1 view=preview" }
     let(:valid_id_with_initialpath_and_runonclick) { "43lkjfdauf initialpath=/initial-path/file.js runonclick=1" }
     let(:valid_id_with_initialpath_and_module_and_runonclick) { "43lkjfdauf initialpath=/initial-path/file.js module=/path/to/module.html runonclick=1" }
+    let(:valid_id_with_initialpath_and_module_and_runonclick_and_view) { "43lkjfdauf initialpath=/initial-path/file.js module=/path/to/module.html runonclick=1 view=split" }
     let(:valid_id_with_special_characters) { "68jkfdsasa initialpath=/.@%_- module=-/%@._" }
 
     let(:bad_ids) do
@@ -23,6 +27,8 @@ RSpec.describe CodesandboxTag, type: :liquid_tag do
         "43lkjfdauf module= initialpath=",
         "89fadksjhe random=/stuff",
         "initialpath=/initial/path/file.js",
+        "view=foobar",
+        "view=",
         "54jfadslkj module=stuff \"onmouseover='alert(\"XSS\")'",
         "42fadfdaf;",
         "%&*($#@$&=",
@@ -49,6 +55,11 @@ RSpec.describe CodesandboxTag, type: :liquid_tag do
       expect(liquid.render).to include("<iframe")
     end
 
+    it "accepts a vaild id with view" do
+      liquid = generate_tag(valid_id_with_view)
+      expect(liquid.render).to include("<iframe")
+    end
+
     it "accepts a vaild id with runonclick" do
       liquid = generate_tag(valid_id_with_runonclick)
       expect(liquid.render).to include("<iframe")
@@ -56,6 +67,11 @@ RSpec.describe CodesandboxTag, type: :liquid_tag do
 
     it "accepts a vaild id with initialpath and module" do
       liquid = generate_tag(valid_id_with_initialpath_and_module)
+      expect(liquid.render).to include("<iframe")
+    end
+
+    it "accepts a vaild id with initialpath and view" do
+      liquid = generate_tag(valid_id_with_initialpath_and_view)
       expect(liquid.render).to include("<iframe")
     end
 
@@ -69,8 +85,18 @@ RSpec.describe CodesandboxTag, type: :liquid_tag do
       expect(liquid.render).to include("<iframe")
     end
 
+    it "accepts a vaild id with runonclick and view" do
+      liquid = generate_tag(valid_id_with_runonclick_and_view)
+      expect(liquid.render).to include("<iframe")
+    end
+
     it "accepts a vaild id with initialpath and module and runonclick" do
       liquid = generate_tag(valid_id_with_initialpath_and_module_and_runonclick)
+      expect(liquid.render).to include("<iframe")
+    end
+
+    it "accepts a vaild id with initialpath and module and runonclick and view" do
+      liquid = generate_tag(valid_id_with_initialpath_and_module_and_runonclick_and_view)
       expect(liquid.render).to include("<iframe")
     end
 
