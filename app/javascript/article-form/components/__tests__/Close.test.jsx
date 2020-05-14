a22b91e3558bf0d8a89eef649a3edea7f37e6b9f
Ridhwana <ridhwana.khan16@gmail.com>
2020-05-14 16:39:16 +0200

    update the Close test

--- app/javascript/article-form/components/__tests__/Close.test.jsx
+++ app/javascript/article-form/components/__tests__/Close.test.jsx
@@ -1,5 +1,6 @@
 import { h } from 'preact';
 import render from 'preact-render-to-json';
+import { shallow } from 'preact-render-spy';
 import { Close } from '../Close';
 
 describe('<Close />', () => {
@@ -7,4 +8,19 @@ describe('<Close />', () => {
     const tree = render(<Close />);
     expect(tree).toMatchSnapshot();
   });
+
+  it('shows the modal', () => {
+    const container = shallow(<Close />);
+    expect(container.find('.crayons-article-form__close').exists()).toEqual(
+      true,
+    );
+  });
+
+  // TODO: how to test a closed modal
+  // it('closes the modal', () => {
+  //   const container = shallow(<Close />);
+  //   const closeBtn = container.find('.crayons-article-form__close a');
+  //   closeBtn.simulate('click');
+  //   expect(container.find('.crayons-article-form__close').exists()).toEqual(false);
+  // });
 });
