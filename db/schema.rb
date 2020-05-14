dce682bc825afa221526d5fc7192c53a4cbe644b
Parasgr-code <paras.gaur@skynox.tech>
2020-05-14 20:03:00 +0530

    changes in joining request responses

--- db/schema.rb
+++ db/schema.rb
@@ -309,6 +309,7 @@ ActiveRecord::Schema.define(version: 2020_05_04_075409) do
     t.string "slug"
     t.string "status", default: "active"
     t.datetime "updated_at", null: false
+    t.index ["slug"], name: "index_chat_channels_on_slug", unique: true
   end
 
   create_table "classified_listing_categories", force: :cascade do |t|
