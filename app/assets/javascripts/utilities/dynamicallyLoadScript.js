67192d5c9cfd2b464a614b408757364dd26b9506
Dmitry Maksyoma <ledestin@gmail.com>
2020-05-15 19:49:21 +1200

    Update app/assets/javascripts/utilities/dynamicallyLoadScript.js
    Co-authored-by: Nick Taylor <nick@iamdeveloper.com>

--- app/assets/javascripts/utilities/dynamicallyLoadScript.js
+++ app/assets/javascripts/utilities/dynamicallyLoadScript.js
@@ -3,7 +3,7 @@
 function dynamicallyLoadScript(url) {
   if (document.querySelector(`script[src='${url}']`)) return;
 
-  var script = document.createElement('script');
+  const script = document.createElement('script');
   script.src = url;
 
   document.head.appendChild(script);
