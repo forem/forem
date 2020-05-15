0ae835c73f5c66179be2c440e03eaf7dd4dd4d48
Ben Halpern <bendhalpern@gmail.com>
2020-05-15 08:02:11 -0400

    Open in new window properly on ctrl+click etc (#7814)
    * Open in new window properly on ctrl+click etc
    
    * Trigger meta click for comment area too

--- app/javascript/articles/components/CommentListItem.jsx
+++ app/javascript/articles/components/CommentListItem.jsx
@@ -13,8 +13,13 @@ export const CommentListItem = ({ comment }) => (
     className="crayons-comment pl-2 cursor-pointer"
     role="presentation"
     onClick={(_event) => {
-      InstantClick.preload(comment.path);
-      InstantClick.display(comment.path);
+      if (_event.which > 1 || _event.metaKey || _event.ctrlKey) { // Indicates should open in _blank
+        window.open(comment.path, '_blank');
+      } else {
+        const fullUrl = window.location.origin + comment.path // InstantClick deals with full urls
+        InstantClick.preload(fullUrl);
+        InstantClick.display(fullUrl);
+      }
     }}
   >
     <div className="crayons-comment__meta">
