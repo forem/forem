06ab8116356fb9d64eb49d923f19b5516ef1fb0e
Arit Amana <msarit@gmail.com>
2020-05-15 10:46:33 -0400

    Update language to reflect new UI

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_05_11_224704) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
