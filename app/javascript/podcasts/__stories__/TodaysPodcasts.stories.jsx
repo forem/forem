ce0e13764ae1586ce45604182f2f90ae3889fcfb
Nick Taylor <nick@iamdeveloper.com>
2020-05-15 13:08:02 -0400

    Fixed article stories in Storybook (#7884)

--- app/javascript/podcasts/__stories__/TodaysPodcasts.stories.jsx
+++ app/javascript/podcasts/__stories__/TodaysPodcasts.stories.jsx
@@ -4,7 +4,6 @@ import '../../../assets/stylesheets/articles.scss';
 import { TodaysPodcasts } from '../TodaysPodcasts';
 import { PodcastEpisode } from '../PodcastEpisode';
 import { podcastArticle } from '../../articles/__tests__/utilities/articleUtilities';
-import { articleDecorator } from '../../articles/__stories__/articleDecorator';
 
 const episodes = [
   podcastArticle,
@@ -16,12 +15,11 @@ const episodes = [
 
 export default {
   title: `App Components/Podcasts/Today's Episodes`,
-  decorators: [articleDecorator],
 };
 
 export const Standard = () => (
   <TodaysPodcasts>
-    {episodes.map(episode => (
+    {episodes.map((episode) => (
       <PodcastEpisode episode={episode} />
     ))}
   </TodaysPodcasts>
