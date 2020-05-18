dbb5c752ab92eb276b9443371226cefd26d8dede
Molly Struve <mollylbs@gmail.com>
2020-05-18 10:36:01 -0500

    [deploy] Sample Noisy Honeycomb Spans (#7891)

---
+++ app/lib/honeycomb/noise_cancelling_sampler.rb
@@ -0,0 +1,27 @@
+module Honeycomb
+  class NoiseCancellingSampler
+    extend Honeycomb::DeterministicSampler
+
+    NOISY_COMMANDS = [
+      "GET rails-settings-cached/v1",
+      "TIME",
+      "BEGIN",
+      "COMMIT",
+    ].freeze
+
+    def self.sample(fields)
+      if NOISY_COMMANDS.include?(fields["redis.command"]) || NOISY_COMMANDS.include?(fields["sql.active_record.sql"])
+        rate = 100
+        [should_sample(rate, fields["trace.trace_id"]), rate]
+      elsif fields["redis.command"]&.start_with?("BRPOP")
+        rate = 1000
+        [should_sample(rate, fields["trace.trace_id"]), rate]
+      elsif fields["redis.command"]&.start_with?("INCRBY") || fields["redis.command"]&.start_with?("TTL")
+        rate = 100
+        [should_sample(rate, fields["trace.trace_id"]), rate]
+      else
+        [true, 1]
+      end
+    end
+  end
+end
