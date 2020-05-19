c84772f4a293f0e86a6d827322707be8d9960925
Josh Puetz <joshpuetz@gmail.com>
2020-05-19 22:07:11 +0000

    Create tables for Flipper gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_085746) do
+ActiveRecord::Schema.define(version: 2020_05_19_220213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -502,6 +502,22 @@ ActiveRecord::Schema.define(version: 2020_05_15_085746) do
     t.string "variant"
     t.index ["experiment", "created_at"], name: "index_field_test_memberships_on_experiment_and_created_at"
     t.index ["participant_type", "participant_id", "experiment"], name: "index_field_test_memberships_on_participant", unique: true
+  end
+
+  create_table "flipper_features", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "key", null: false
+    t.datetime "updated_at", null: false
+    t.index ["key"], name: "index_flipper_features_on_key", unique: true
+  end
+
+  create_table "flipper_gates", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "feature_key", null: false
+    t.string "key", null: false
+    t.datetime "updated_at", null: false
+    t.string "value"
+    t.index ["feature_key", "key", "value"], name: "index_flipper_gates_on_feature_key_and_key_and_value", unique: true
   end
 
   create_table "follows", id: :serial, force: :cascade do |t|
