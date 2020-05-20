3a1536465d1d9036c57ddb3f1c85550aa42ac857
Nick Taylor <nick@iamdeveloper.com>
2020-05-20 11:57:54 -0400

    Some JS cleanup for mobile view select event handler.

--- app/assets/javascripts/initializers/initializeDrawerSliders.js
+++ app/assets/javascripts/initializers/initializeDrawerSliders.js
@@ -1,73 +1,68 @@
-function mobileListenForFilterSelect(event) {
-  const feedFilterSelect = document.getElementById('feed-filter-select');
-  const changeFilter = (event) => {
-    const url = event.target.value;
-    InstantClick.preload(url);
-    InstantClick.display(url);
-  };
-  if (feedFilterSelect) {
-    feedFilterSelect.addEventListener('change', changeFilter);
-  }
-}
-
 function initializeDrawerSliders() {
-  if(!initializeSwipeGestures.called) {
-    swipeState = "middle";
+  if (!initializeSwipeGestures.called) {
+    swipeState = 'middle';
     initializeSwipeGestures();
   }
-  if (document.getElementById("on-page-nav-controls")){
-    if (document.getElementById("sidebar-bg-left")){
-      document.getElementById("sidebar-bg-left").onclick = function(){
-        swipeState = "middle";
-        slideSidebar("left","outOfView");
-      }
+  if (document.getElementById('on-page-nav-controls')) {
+    if (document.getElementById('sidebar-bg-left')) {
+      document.getElementById('sidebar-bg-left').onclick = function () {
+        swipeState = 'middle';
+        slideSidebar('left', 'outOfView');
+      };
     }
-    if (document.getElementById("sidebar-bg-right")){
-      document.getElementById("sidebar-bg-right").onclick = function(){
-        swipeState = "middle";
-        slideSidebar("right","outOfView");
-      }
+    if (document.getElementById('sidebar-bg-right')) {
+      document.getElementById('sidebar-bg-right').onclick = function () {
+        swipeState = 'middle';
+        slideSidebar('right', 'outOfView');
+      };
     }
 
-    if (document.getElementById("on-page-nav-butt-left")){
-      document.getElementById("on-page-nav-butt-left").onclick = function(){
-        swipeState = "left"
-        slideSidebar("left","intoView");
-      }
+    if (document.getElementById('on-page-nav-butt-left')) {
+      document.getElementById('on-page-nav-butt-left').onclick = function () {
+        swipeState = 'left';
+        slideSidebar('left', 'intoView');
+      };
     }
-    if (document.getElementById("on-page-nav-butt-right")){
-      document.getElementById("on-page-nav-butt-right").onclick = function(){
-        swipeState = "right"
-        slideSidebar("right","intoView");
-      }
+    if (document.getElementById('on-page-nav-butt-right')) {
+      document.getElementById('on-page-nav-butt-right').onclick = function () {
+        swipeState = 'right';
+        slideSidebar('right', 'intoView');
+      };
     }
-    InstantClick.on('change', function() {
+    InstantClick.on('change', function () {
       document.getElementsByTagName('body')[0].classList.remove('modal-open');
-      slideSidebar("right","outOfView");
-      slideSidebar("left","outOfView");
+      slideSidebar('right', 'outOfView');
+      slideSidebar('left', 'outOfView');
     });
     listenForNarrowMenuClick();
   }
 
-  if ( document.getElementById("feed-filter-select")) {
-    mobileListenForFilterSelect();
+  const feedFilterSelect = document.getElementById('feed-filter-select');
+
+  if (feedFilterSelect) {
+    feedFilterSelect.addEventListener('change', (event) => {
+      const url = event.target.value;
+
+      InstantClick.preload(url);
+      InstantClick.display(url);
+    });
   }
 }
 
 function listenForNarrowMenuClick(event) {
-  var navLinks = document.getElementsByClassName("narrow-nav-menu");
-  var narrowFeedButt = document.getElementById("narrow-feed-butt");
+  var navLinks = document.getElementsByClassName('narrow-nav-menu');
+  var narrowFeedButt = document.getElementById('narrow-feed-butt');
   for (var i = 0; i < navLinks.length; i++) {
-    document.getElementById("narrow-nav-menu").classList.remove("showing");
+    document.getElementById('narrow-nav-menu').classList.remove('showing');
   }
   if (narrowFeedButt) {
-    narrowFeedButt.onclick = function(){
-      document.getElementById("narrow-nav-menu").classList.add("showing");
-    }
+    narrowFeedButt.onclick = function () {
+      document.getElementById('narrow-nav-menu').classList.add('showing');
+    };
   }
   for (var i = 0; i < navLinks.length; i++) {
-    navLinks[i].onclick = function(event){
-      document.getElementById("narrow-nav-menu").classList.remove("showing");
-    }
+    navLinks[i].onclick = function (event) {
+      document.getElementById('narrow-nav-menu').classList.remove('showing');
+    };
   }
 }
