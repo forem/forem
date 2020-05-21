5886e877b10c05e9356dd2afcd158b6c17532183
Dana Scheider <dana.scheider@gmail.com>
2020-05-22 00:07:51 +1000

    [deploy] Gracefully handle missing linked users from comments (#7941)

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -91,7 +91,7 @@ class UserDecorator < ApplicationDecorator
       },
     ]
     colors |= WHITE_TEXT_COLORS
-    colors[id % 10]
+    colors[(id || rand(100)) % 10]
   end
 
   # returns true if the user has been suspended and has no content
