2ca505ac880b52f4ff8dd07386566f328dcca608
Mac Siri <krairit.siri@gmail.com>
2020-05-21 11:34:21 -0400

    Expand FeedContent specs

---
+++ spec/system/search/display_comments_search_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe "Display articles search spec", type: :system, js: true, elasticsearch: "FeedContent" do
+  before do
+    stub_request(:post, "http://www.google-analytics.com/collect")
+  end
+
+  it "returns correct results for a search" do
+    query = "<marquee='alert(document.cookie)'>XSS"
+    found_comment = create(:comment, body_markdown: query)
+    index_documents_for_search_class([found_comment], Search::FeedContent)
+
+    url_encoded_query = CGI.escape(query)
+    visit "/search?q=#{url_encoded_query}&filters=class_name:Comment"
+
+    verify(format: :html) do
+      # Not shown here: search snippet is properly escaped
+      Nokogiri::HTML(page.body).css(".crayons-story__snippet").first.to_s
+    end
+  end
+end
