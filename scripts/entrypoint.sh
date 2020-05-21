8a96621bbb635cbfb46b612780ce14544c9d4509
Joe Doss <jdoss@dev.to>
2020-05-21 01:07:02 -0500

    This reworks the compose files so each task that is needed to start the app has a correct wait command with dockerize. It also adds in containers for doing yarn and bundle things for development. Since we mount the code directory inside the container we lose our pre-containerized gems and node_modules.
    This means users can run:
    
    Linux
    podman-compose -f container-compose.yml up
    
    Mac
    docker-compose up
    
    and it should correctly build the app stack with containers!

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,6 +6,8 @@ if [ -f tmp/pids/server.pid ]; then
   rm -f tmp/pids/server.pid
 fi
 
+bundle exec rake search:setup
 bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
+bundle exec rake data_updates:run
 
 exec "$@"
