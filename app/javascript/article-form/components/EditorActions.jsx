9d8f85fdc5d0b21f3924f160de4106edeec9af49
Nick Taylor <nick@iamdeveloper.com>
2020-05-22 21:50:46 -0400

    Small refactor.

--- app/javascript/article-form/components/EditorActions.jsx
+++ app/javascript/article-form/components/EditorActions.jsx
@@ -3,6 +3,21 @@ import PropTypes from 'prop-types';
 import { Button } from '@crayons';
 import { Options } from './Options';
 
+const Icon = () => (
+  <svg
+    width="24"
+    className="crayons-icon"
+    height="24"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    role="img"
+    aria-labelledby="75abcb76478519ca4eb9"
+  >
+    <title id="75abcb76478519ca4eb9">Post options</title>
+    <path d="M12 1l9.5 5.5v11L12 23l-9.5-5.5v-11L12 1zm0 2.311L4.5 7.653v8.694l7.5 4.342 7.5-4.342V7.653L12 3.311zM12 16a4 4 0 110-8 4 4 0 010 8zm0-2a2 2 0 100-4 2 2 0 000 4z" />
+  </svg>
+);
+
 export class EditorActions extends Component {
   constructor(props) {
     super(props);
@@ -39,21 +54,6 @@ export class EditorActions extends Component {
     } = this.props;
 
     const { moreConfigShowing } = this.state;
-
-    const Icon = () => (
-      <svg
-        width="24"
-        className="crayons-icon"
-        height="24"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-        role="img"
-        aria-labelledby="75abcb76478519ca4eb9"
-      >
-        <title id="75abcb76478519ca4eb9">Post options</title>
-        <path d="M12 1l9.5 5.5v11L12 23l-9.5-5.5v-11L12 1zm0 2.311L4.5 7.653v8.694l7.5 4.342 7.5-4.342V7.653L12 3.311zM12 16a4 4 0 110-8 4 4 0 010 8zm0-2a2 2 0 100-4 2 2 0 000 4z" />
-      </svg>
-    );
 
     return submitting ? (
       <div className="crayons-article-form__footer">
