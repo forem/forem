0f061e7954bcaaefab147f6e63fee459ddbeeb72
rhymes <rhymesete@gmail.com>
2020-05-25 18:03:18 +0200

    [deploy] Update webpacker configuration (#8004)

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,3 +1,7 @@
+/* global process, module, require */
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 const environment = require('./environment');
 
 const config = environment.toWebpackConfig();
