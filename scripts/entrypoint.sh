c7c0dc84eaef46c992386c0c3c7cb5d06eed863d
Joe Doss <jdoss@dev.to>
2020-05-26 11:29:29 -0500

    Adjust the seed and sidekiq compose entries so they do not use the web entrypoint and set the REDIS_URL and REDIS_SESSIONS_URL env vars for seed.
    Add in some echos to the entrypoint.sh.
    
    Also set docker-compose to use :delegated on mount points to speed things up.
    
    https://docs.docker.com/storage/bind-mounts/#configure-mount-consistency-for-macos

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,8 +6,11 @@ if [ -f tmp/pids/server.pid ]; then
   rm -f tmp/pids/server.pid
 fi
 
+echo "Running rake search:setup..."
 bundle exec rake search:setup
+echo "Running rake db:migrate..."
 bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
+echo "Running rake data_updates:run..."
 bundle exec rake data_updates:run
 
 exec "$@"
