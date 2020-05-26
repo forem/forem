170351a69991e85220beb3db14b9058d263efd1d
Zhao-Andy <17884966+Zhao-Andy@users.noreply.github.com>
2020-05-26 14:39:14 -0400

    Comment out old script

--- lib/data_update_scripts/20200518173504_update_public_reactions_count_from_positive_reactions_count.rb
+++ lib/data_update_scripts/20200518173504_update_public_reactions_count_from_positive_reactions_count.rb
@@ -1,8 +1,8 @@
 module DataUpdateScripts
   class UpdatePublicReactionsCountFromPositiveReactionsCount
     def run
-      ActiveRecord::Base.connection.execute("UPDATE articles SET public_reactions_count = positive_reactions_count, previous_public_reactions_count = previous_positive_reactions_count")
-      ActiveRecord::Base.connection.execute("UPDATE comments SET public_reactions_count = positive_reactions_count")
+      # ActiveRecord::Base.connection.execute("UPDATE articles SET public_reactions_count = positive_reactions_count, previous_public_reactions_count = previous_positive_reactions_count")
+      # ActiveRecord::Base.connection.execute("UPDATE comments SET public_reactions_count = positive_reactions_count")
     end
   end
 end
