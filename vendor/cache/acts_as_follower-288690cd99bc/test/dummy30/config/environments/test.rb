92abfde43bfff55dd4861ed2500d352e14549296
Joe Doss <jdoss@users.noreply.github.com>
2020-05-27 11:29:55 -0500

    Let's cache our gems in vendor/cache! (#8066)
    * Let's cache our gems in vendor/cache!
    
    This PR adds in bundle package caching which will help speed up deployments by
    caching all of the gems that we use in our git repo. This was done with the
    following commands on a fresh repo clone:
    
    bundle install
    bundle package --all
    
    This cached all of our gems in vendor/cache and when you do a bundle install to
    update gems from the Gemfile, it will update our gem cache too.
    
    You can read more about bundle package here:
    
    https://bundler.io/man/bundle-package.1.html
    
    * Add in bundle config set cache_all true to bin/setup so it gets set for everyone.

---
+++ vendor/cache/acts_as_follower-288690cd99bc/test/dummy30/config/environments/test.rb
@@ -0,0 +1,20 @@
+Dummy::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb
+
+  # The test environment is used exclusively to run your application's
+  # test suite.  You never need to work with it otherwise.  Remember that
+  # your test database is "scratch space" for the test suite and is wiped
+  # and recreated between test runs.  Don't rely on the data there!
+  config.cache_classes = true
+
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = true
+
+  # Use SQL instead of Active Record's schema dumper when creating the test database.
+  # This is necessary if your schema can't be completely dumped by the schema dumper,
+  # like if you have constraints or database-specific column types
+  # config.active_record.schema_format = :sql
+
+  # Print deprecation notices to the stderr
+  config.active_support.deprecation = :stderr
+end
