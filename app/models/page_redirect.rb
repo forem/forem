4f1e417c7a05a0053746e2240db9b5ef38b4dd33
Alex <alexandersmith223@gmail.com>
2020-05-27 13:13:07 -0400

    Create PageRedirect model

---
+++ app/models/page_redirect.rb
@@ -0,0 +1,12 @@
+class PageRedirect < ApplicationRecord
+  validates :old_slug, presence: true, uniqueness: true
+  validates :new_slug, presence: true
+
+  before_save :increment_version, if: :will_save_change_to_new_slug?
+
+  private
+
+  def increment_version
+    self.version += 1
+  end
+end
