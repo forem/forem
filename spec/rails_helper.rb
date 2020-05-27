ad5efdab7c0409192cd670c6b22220144453f254
snackattas <zach.attas@gmail.com>
2020-05-27 09:08:12 -0500

    include rales backtrace

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -151,7 +151,7 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   # Filter lines from Rails gems in backtraces.
-  config.filter_rails_from_backtrace!
+  # config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
