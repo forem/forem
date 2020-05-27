5e50ebe71fa8edcc2d031be522f735149088fdd6
Alex <alexandersmith223@gmail.com>
2020-05-27 18:47:03 -0400

    Use ransack for search

--- app/controllers/internal/page_redirects_controller.rb
+++ app/controllers/internal/page_redirects_controller.rb
@@ -2,14 +2,7 @@ class Internal::PageRedirectsController < Internal::ApplicationController
   layout "internal"
 
   def index
-    @page_redirects = PageRedirect
-
-    if params[:search].present?
-      @page_redirects = @page_redirects.where("page_redirects.old_slug ILIKE :search OR
-                                               page_redirects.new_slug ILIKE :search",
-                                              search: "%#{params[:search]}%")
-    end
-
-    @page_redirects = @page_redirects.order(created_at: :desc).page(params[:page] || 1).per(50)
+    @q = PageRedirect.order(created_at: :desc).ransack(params[:q])
+    @page_redirects = @q.result.page(params[:page] || 1).per(25)
   end
 end
