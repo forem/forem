b4d3a951a2e284f3d8fcb65575f237a3dd77b065
Joe Doss <jdoss@dev.to>
2020-05-27 22:58:48 -0500

    Move to the consolidated app_initializer:setup rake task for bootstrapping the app.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,11 +6,7 @@ if [ -f tmp/pids/server.pid ]; then
   rm -f tmp/pids/server.pid
 fi
 
-echo "Running rake search:setup..."
-bundle exec rake search:setup
-echo "Running rake db:migrate..."
-bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
-echo "Running rake data_updates:run..."
-bundle exec rake data_updates:run
+echo "Running rake app_initializer:setup..."
+bundle exec rake app_initializer:setup
 
 exec "$@"
