e419d6d26520863867562b4bb71831af72a8b4ca
Alex <alexandersmith223@gmail.com>
2020-05-27 21:44:10 -0400

    Small fixes and hookup new and create

--- app/controllers/internal/page_redirects_controller.rb
+++ app/controllers/internal/page_redirects_controller.rb
@@ -3,6 +3,22 @@ class Internal::PageRedirectsController < Internal::ApplicationController
 
   before_action :set_page_redirect, only: %i[edit update destroy]
 
+  def new
+    @page_redirect = PageRedirect.new
+  end
+
+  def create
+    @page_redirect = PageRedirect.new(page_redirect_params)
+
+    if @page_redirect.save
+      flash[:success] = "Page Redirect created successfully!"
+      redirect_to internal_page_redirects_path
+    else
+      flash[:danger] = @page_redirect.errors.full_messages.to_sentence
+      render new_internal_page_redirect_path
+    end
+  end
+
   def index
     @q = PageRedirect.order(created_at: :desc).ransack(params[:q])
     @page_redirects = @q.result.page(params[:page] || 1).per(25)
@@ -12,22 +28,22 @@ class Internal::PageRedirectsController < Internal::ApplicationController
 
   def update
     if @page_redirect.update(page_redirect_params.merge({ overridden: true }))
-      flash[:success] = "Page Redirect updated successfully"
+      flash[:success] = "Page Redirect updated successfully!"
+      redirect_to edit_internal_page_redirect_path(@page_redirect)
     else
       flash[:danger] = @page_redirect.errors.full_messages.to_sentence
+      render :edit
     end
-
-    redirect_to edit_internal_page_redirect_path(@page_redirect)
   end
 
   def destroy
     if @page_redirect.destroy
-      flash[:success] = "Page Redirect destroyed successfully"
-      redirect_to internal_page_redirects_path
+      flash[:success] = "Page Redirect destroyed successfully!"
     else
       flash[:danger] = @page_redirect.errors.full_messages.to_sentence
-      redirect_to edit_internal_page_redirect_path(@page_redirect)
     end
+
+    redirect_back(fallback_location: internal_page_redirects_path)
   end
 
   private
