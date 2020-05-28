b881e9a24b66a9ee439adf3effb1a2aca9c685a0
Francisco Quintero <frajaquico@yahoo.es>
2020-05-28 15:42:33 -0500

    Move RedditJsonFromUrlService methods to RedditTag
    Also changed the way the request was being mocked. Previously, it was by
    mocking the service class now I'm mocking HTTParty directly and returning
    the JSON structure Reddit uses.

--- app/liquid_tags/reddit_tag.rb
+++ app/liquid_tags/reddit_tag.rb
@@ -1,9 +1,12 @@
 class RedditTag < LiquidTagBase
+  include ActionView::Helpers::SanitizeHelper
+
   PARTIAL = "liquids/reddit".freeze
+  URL_REGEXP = /\Ahttps\:\/\/(www.)?reddit.com/.freeze
 
   def initialize(_tag_name, url, _tokens)
-    @url = url.strip
-    @reddit_content = RedditJsonFromUrlService.new(@url).parse
+    @url = ActionController::Base.helpers.strip_tags(url).strip
+    @reddit_content = parse_url
   end
 
   def render(_context)
@@ -21,6 +24,49 @@ class RedditTag < LiquidTagBase
         markdown_text: @reddit_content[:selftext]
       },
     )
+  end
+
+  private
+
+  def parse_url
+    validate_url
+
+    # Requests to Reddit require a custom `User-Agent` header to prevent 429 errors
+    json = HTTParty.get("#{@url}.json", headers: { "User-Agent" => "#{ApplicationConfig['COMMUNITY_NAME']} (#{URL.url})" })
+
+    # The JSON response is an array with two items.
+    # The first one is the post itself, the second one are the comments
+    data = json.first["data"]["children"][0]["data"]
+
+    {
+      author: data["author"],
+      title: data["title"],
+      post_url: @url,
+      created_at: Time.zone.at(data["created_utc"]).strftime("%b %e '%y"),
+      post_hint: data["post_hint"],
+      image_url: data["url"],
+      thumbnail: data["thumbnail"],
+      selftext: parse_markdown_content(data["selftext"]),
+      selftext_html: data["selftext_html"]
+    }
+  end
+
+  def parse_markdown_content(content)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTMLRouge, REDCARPET_CONFIG)
+    text = markdown.render(content)
+
+    sanitize(HTML_Truncator.truncate(text, 60))
+  end
+
+  def validate_url
+    return true if valid_url?(@url.delete(" ")) && (@url =~ URL_REGEXP)&.zero?
+
+    raise StandardError, "Invalid Reddit link: #{@url}"
+  end
+
+  def valid_url?(url)
+    url = URI.parse(url)
+    url.is_a?(URI::HTTP)
   end
 end
 
