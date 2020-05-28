d9fc7961c3a04d3ca038070155b84b01c481bb1d
Alex <alexandersmith223@gmail.com>
2020-05-27 20:11:58 -0400

    Add edit view with destroy button

--- app/controllers/internal/page_redirects_controller.rb
+++ app/controllers/internal/page_redirects_controller.rb
@@ -1,8 +1,42 @@
 class Internal::PageRedirectsController < Internal::ApplicationController
   layout "internal"
 
+  before_action :set_page_redirect, only: %i[edit update destroy]
+
   def index
     @q = PageRedirect.order(created_at: :desc).ransack(params[:q])
     @page_redirects = @q.result.page(params[:page] || 1).per(25)
+  end
+
+  def edit; end
+
+  def update
+    if @page_redirect.update(page_redirect_params.merge({ overridden: true }))
+      flash[:success] = "Page Redirect updated successfully"
+    else
+      flash[:danger] = @page_redirect.errors.full_messages
+    end
+
+    redirect_to edit_internal_page_redirect_path(@page_redirect)
+  end
+
+  def destroy
+    if @page_redirect.destroy
+      flash[:success] = "Page Redirect destroyed successfully"
+      redirect_to internal_page_redirects_path
+    else
+      flash[:danger] = @page_redirect.errors.full_messages
+      redirect_to edit_internal_page_redirect_path(@page_redirect)
+    end
+  end
+
+  private
+
+  def set_page_redirect
+    @page_redirect = PageRedirect.find(params[:id])
+  end
+
+  def page_redirect_params
+    params.require(:page_redirect).permit(:old_slug, :new_slug)
   end
 end
