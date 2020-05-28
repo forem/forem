b2744386e6b33b20ff0acc2734c6fdd77fc7466b
snackattas <zach.attas@gmail.com>
2020-05-28 00:20:02 -0500

    tweaks copied files names, changes to --format documentation

--- scripts/copy_specs.sh
+++ scripts/copy_specs.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 specs=(
-  spec/system/internal/admin_bans_or_warns_user_spec
-  spec/system/notifications/notifications_page_spec
-  spec/system/internal/admin_manages_organizations_spec
-  spec/system/articles/user_visits_article_stats_spec
+  spec/system/internal/admin_bans_or_warns_user
+  spec/system/notifications/notifications_page
+  spec/system/internal/admin_manages_organizations
+  spec/system/articles/user_visits_article_stats
 )
 num_copies=50
 
 for spec in ${specs[*]}; do
-  echo $spec
   for i in $(seq 1 $num_copies); do
-    copy="$spec-$i.rb"
-    echo $copy
-    cp "$spec.rb" $copy
+    copy=${spec}_${i}_spec.rb
+    cp ${spec}_spec.rb $copy
   done
 done
