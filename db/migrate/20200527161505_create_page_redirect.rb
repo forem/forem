ebf3fbbd591fbe761e76138af9fc76a27ce147d3
Alex <alexandersmith223@gmail.com>
2020-05-29 11:35:31 -0400

    slug --> path 🙈

--- db/migrate/20200527161505_create_page_redirect.rb
+++ db/migrate/20200527161505_create_page_redirect.rb
@@ -1,16 +1,16 @@
 class CreatePageRedirect < ActiveRecord::Migration[5.2]
   def change
     create_table :page_redirects do |t|
-      t.string :old_slug, null: false
-      t.string :new_slug, null: false
+      t.string :old_path, null: false
+      t.string :new_path, null: false
       t.string :source, null: false, default: "service"
       t.integer :version, null: false, default: 0
       t.timestamps null: false
     end
 
-    add_index :page_redirects, :old_slug, unique: true
-    add_index :page_redirects, :new_slug
-    add_index :page_redirects, %i[old_slug new_slug], unique: true
+    add_index :page_redirects, :old_path, unique: true
+    add_index :page_redirects, :new_path
+    add_index :page_redirects, %i[old_path new_path], unique: true
     add_index :page_redirects, :version
     add_index :page_redirects, :source
   end
--- spec/models/page_redirect_spec.rb
+++ spec/models/page_redirect_spec.rb
@@ -4,25 +4,25 @@ RSpec.describe PageRedirect, type: :model do
   subject { build(:page_redirect) }
 
   describe "validations" do
-    it { is_expected.to validate_uniqueness_of(:old_slug) }
-    it { is_expected.to validate_presence_of(:old_slug) }
-    it { is_expected.to validate_presence_of(:new_slug) }
+    it { is_expected.to validate_uniqueness_of(:old_path) }
+    it { is_expected.to validate_presence_of(:old_path) }
+    it { is_expected.to validate_presence_of(:new_path) }
     it { is_expected.to validate_presence_of(:source) }
     it { is_expected.to validate_inclusion_of(:source).in_array(described_class::SOURCES) }
   end
 
   describe "before_save" do
-    it "increments the version by 1 if the new_slug is updated" do
+    it "increments the version by 1 if the new_path is updated" do
       page_redirect = create(:page_redirect)
       expect(page_redirect.version).to eq 1
-      page_redirect.update(new_slug: "/some-new-slug")
+      page_redirect.update(new_path: "/some-new-path")
       expect(page_redirect.version).to eq 2
     end
 
-    it "does not increment the version if a field other than new_slug is updated" do
+    it "does not increment the version if a field other than new_path is updated" do
       page_redirect = create(:page_redirect)
       expect(page_redirect.version).to eq 1
-      page_redirect.update(old_slug: "/some-old-slug")
+      page_redirect.update(old_path: "/some-old-path")
       expect(page_redirect.version).to eq 1
     end
   end
