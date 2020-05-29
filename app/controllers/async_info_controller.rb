cae0389a091e015a456d39d6d7fdf2a0f84e42a0
mstruve <mollylbs@gmail.com>
2020-05-29 15:15:25 -0500

    Optimization: Remove Comment Count From Aysnc user_data

--- app/controllers/async_info_controller.rb
+++ app/controllers/async_info_controller.rb
@@ -53,7 +53,6 @@ class AsyncInfoController < ApplicationController
         saw_onboarding: @user.saw_onboarding,
         checked_code_of_conduct: @user.checked_code_of_conduct,
         checked_terms_and_conditions: @user.checked_terms_and_conditions,
-        number_of_comments: @user.comments.count,
         display_sponsors: @user.display_sponsors,
         trusted: @user.trusted,
         moderator_for_tags: @user.moderator_for_tags,
