866f0e6c3763732be8733a29fa3994d476935795
benhalpern <bendhalpern@gmail.com>
2020-05-29 14:48:04 -0400

    Update copy on editor help

--- app/javascript/article-form/components/Help.jsx
+++ app/javascript/article-form/components/Help.jsx
@@ -63,7 +63,7 @@ export class Help extends Component {
       <div className="spec__basic-tag-input-help crayons-article-form__help crayons-article-form__help--tags">
         <h4 className="mb-2 fs-l">Tagging Guidelines</h4>
         <ul className="list-disc pl-6 color-base-70">
-          <li>Tags will help the right people find your post.</li>
+          <li>Tags help people find your post.</li>
           <li>
             Think of tags as the topics or categories that best describe your
             post.
@@ -72,7 +72,7 @@ export class Help extends Component {
             Add up to four tags per post. Combine tags to reach the appropriate
             subcommunities.
           </li>
-          <li>Try to use existing tags whenever possible.</li>
+          <li>Use existing tags whenever possible.</li>
           <li>
             Some tags, such as “help” or “healthydebate”, have special posting
             guidelines.
