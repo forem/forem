d540d470bc0d8bc33ebaa644c650fa39b03bbe2a
Molly Struve <mollylbs@gmail.com>
2020-05-31 13:21:32 -0500

    [deploy] Optimization: Remove Unused Fields From Async user_data, optimize a couple other methods (#8173)

--- app/controllers/async_info_controller.rb
+++ app/controllers/async_info_controller.rb
@@ -56,8 +56,6 @@ class AsyncInfoController < ApplicationController
         display_sponsors: @user.display_sponsors,
         trusted: @user.trusted,
         moderator_for_tags: @user.moderator_for_tags,
-        experience_level: @user.experience_level,
-        preferred_languages_array: @user.preferred_languages_array,
         config_body_class: @user.config_body_class,
         pro: @user.pro?,
         created_at: @user.created_at
