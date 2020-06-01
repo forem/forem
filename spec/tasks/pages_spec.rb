bf3286349f7ae6687770aa8355745f2609122266
Fernando Valverde <fdov88@gmail.com>
2020-06-01 11:26:10 -0600

    Replaces fixture with tmp file

--- spec/tasks/pages_spec.rb
+++ spec/tasks/pages_spec.rb
@@ -1,18 +1,18 @@
 require "rails_helper"
 
 RSpec.describe "Pages tasks", type: :task do
-  let!(:fixture_path) { Rails.root.join("spec/fixtures/files/page.html") }
+  let!(:filepath) { Rails.root.join("tmp/page.html") }
   let!(:original_content) { "<h1>Hello Page</h1>" }
   let!(:new_content) { "<h2>Hello updated Page</h2>" }
 
   before do
     Rake::Task.clear
     PracticalDeveloper::Application.load_tasks
-    File.write(fixture_path, original_content)
+    File.write(filepath, original_content)
   end
 
   # Ensure the fixture never changes after each run
-  after { File.write(fixture_path, original_content) }
+  after { File.delete(filepath) if File.file?(filepath) }
 
   describe "#sync" do
     it "updates a Page HTML content when a given file to sync has changed" do
@@ -21,13 +21,13 @@ RSpec.describe "Pages tasks", type: :task do
       threads = []
       threads << Thread.new do
         # Thread that runs the long lived rake task
-        Rake::Task["pages:sync"].invoke(page.slug, fixture_path)
+        Rake::Task["pages:sync"].invoke(page.slug, filepath)
       end
 
       threads << Thread.new do
         # This thread gives the rake task time to boot before updating the file
         sleep 0.5
-        File.write(fixture_path, new_content)
+        File.write(filepath, new_content)
 
         # Wait again so the rake task can do its work before killing it
         sleep 0.5
