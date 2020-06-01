de7befaf9334e2586c0ee965631fe1e5b2c4aac3
Alex <alexandersmith223@gmail.com>
2020-06-01 19:43:09 -0400

    Fix comment

--- app/controllers/internal/application_controller.rb
+++ app/controllers/internal/application_controller.rb
@@ -2,7 +2,7 @@ class Internal::ApplicationController < ApplicationController
   before_action :authorize_admin
   after_action :verify_authorized
 
-  # This is to used in app/views/internal/shared/_navbar.html.erb to build the
+  # This is used in app/views/internal/shared/_navbar.html.erb to build the
   # side navbar in alphabetical order.
   MENU_ITEMS = [
     { name: "articles",           controller: "articles" },
