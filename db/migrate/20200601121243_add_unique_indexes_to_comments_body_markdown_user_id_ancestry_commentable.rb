5dba524003a8ca811258676cb29761d01c28c08d
Molly Struve <mollylbs@gmail.com>
2020-06-02 11:43:50 -0500

    [deploy] Add If Statements to Skip Migrations If Index Exists (#8236)

--- db/migrate/20200601121243_add_unique_indexes_to_comments_body_markdown_user_id_ancestry_commentable.rb
+++ db/migrate/20200601121243_add_unique_indexes_to_comments_body_markdown_user_id_ancestry_commentable.rb
@@ -1,13 +1,33 @@
 class AddUniqueIndexesToCommentsBodyMarkdownUserIdAncestryCommentable < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
 
-  def change
+  def up
+    return if index_exists?(
+      :comments,
+      %i[body_markdown user_id ancestry commentable_id commentable_type],
+      name: :index_comments_on_body_markdown_user_id_ancestry_commentable
+    )
+
     add_index(
       :comments,
       %i[body_markdown user_id ancestry commentable_id commentable_type],
       unique: true,
       algorithm: :concurrently,
       name: :index_comments_on_body_markdown_user_id_ancestry_commentable
+    )
+  end
+
+  def down
+    return unless index_exists?(
+      :comments,
+      %i[body_markdown user_id ancestry commentable_id commentable_type],
+      name: :index_comments_on_body_markdown_user_id_ancestry_commentable
+    )
+
+    remove_index(
+      :comments,
+      name: :index_comments_on_body_markdown_user_id_ancestry_commentable,
+      algorithm: :concurrently
     )
   end
 end
