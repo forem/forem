ae11b8410a7c8389abefe2a999c6823484142fc4
Nick Taylor <nick@iamdeveloper.com>
2020-06-03 22:41:40 -0400

    Migrated tests for <Formfield />

--- app/javascript/crayons/formElements/__tests__/FormField.test.jsx
+++ app/javascript/crayons/formElements/__tests__/FormField.test.jsx
@@ -1,20 +1,43 @@
 import { h } from 'preact';
-import render from 'preact-render-to-json';
+import { render } from '@testing-library/preact';
+import { axe } from 'jest-axe';
 import { FormField, RadioButton } from '@crayons';
 
 describe('<FormField /> component', () => {
-  it('should render', () => {
-    const tree = render(<FormField />);
-    expect(tree).toMatchSnapshot();
+  it('should have no a11y violations when rendered', async () => {
+    const { container } = render(<FormField />);
+
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
   });
 
-  it('should render with contents', () => {
-    const tree = render(
+  it('should have no a11y violations when rendered with content', async () => {
+    const { container } = render(
+      <FormField>
+        {' '}
+        <RadioButton id="some-id" value="some-value" name="some-name" />
+        <label htmlFor="some-id">Some Label</label>
+      </FormField>,
+    );
+
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
+
+  it('should render', () => {
+    const { container } = render(<FormField />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render with content', () => {
+    const { container } = render(
       <FormField>
         <RadioButton id="some-id" value="some-value" name="some-name" />
         <label htmlFor="some-id">Some Label</label>
       </FormField>,
     );
-    expect(tree).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
   });
 });
