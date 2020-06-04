b9fc02730b27dce9d0d2c88758900c181c4776ba
Nick Taylor <nick@iamdeveloper.com>
2020-06-03 22:40:44 -0400

    Migrated tests for <Dropdown />

--- app/javascript/crayons/Dropdown/__tests__/Dropdown.test.jsx
+++ app/javascript/crayons/Dropdown/__tests__/Dropdown.test.jsx
@@ -1,27 +1,42 @@
 import { h } from 'preact';
-import render from 'preact-render-to-json';
+import { render } from '@testing-library/preact';
+import { axe } from 'jest-axe';
 import { Dropdown } from '@crayons';
 
 describe('<Dropdown />', () => {
-  it('renders properly', () => {
-    const tree = render(
+  it('should have no a11y violations when rendered', async () => {
+    const { container } = render(
       <Dropdown>
         Hey, I&apos;m a dropdown content! Lorem ipsum dolor sit amet,
         consectetur adipisicing elit. Sequi ea voluptates quaerat eos
         consequuntur temporibus.
       </Dropdown>,
     );
-    expect(tree).toMatchSnapshot();
+
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
   });
 
-  it('renders properly with additional CSS classes', () => {
-    const tree = render(
+  it('should render', () => {
+    const { container } = render(
+      <Dropdown>
+        Hey, I&apos;m a dropdown content! Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit. Sequi ea voluptates quaerat eos
+        consequuntur temporibus.
+      </Dropdown>,
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render with additional CSS classes', () => {
+    const { container } = render(
       <Dropdown className="some-additional-css-class">
         Hey, I&apos;m a dropdown content! Lorem ipsum dolor sit amet,
         consectetur adipisicing elit. Sequi ea voluptates quaerat eos
         consequuntur temporibus.
       </Dropdown>,
     );
-    expect(tree).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
   });
 });
