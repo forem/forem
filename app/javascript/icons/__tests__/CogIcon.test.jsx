449e54939a9c08d356a94e0cee403baa57bcab60
Nick Taylor <nick@iamdeveloper.com>
2020-06-05 17:00:25 -0400

    Added a111y tests to <CogIcon />

--- app/javascript/icons/__tests__/CogIcon.test.jsx
+++ app/javascript/icons/__tests__/CogIcon.test.jsx
@@ -1,8 +1,15 @@
 import { h } from 'preact';
 import { render } from '@testing-library/preact';
+import { axe } from 'jest-axe';
 import { CogIcon } from '../CogIcon';
 
 describe('<CommentSubscription />', () => {
+  it('should have no a11y violations', async () => {
+    const { container } = render(<CogIcon />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
   it('should render', () => {
     const { container } = render(<CogIcon />);
 
