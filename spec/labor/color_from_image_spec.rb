950d05177f4010658578b4b09f8c8fb7e2401033
Ben Halpern <bendhalpern@gmail.com>
2020-06-05 15:03:26 -0400

    [deploy] Replace defunct Algorithmia with Miro for color detection (#8296)
    * Replace defuncked Algorithmia with Miro for color detection
    
    * Stub robohash

--- spec/labor/color_from_image_spec.rb
+++ spec/labor/color_from_image_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 RSpec.describe ColorFromImage, type: :labor do
   it "returns a color" do
     expected_regexp = /\A#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})\z/
-    expect(described_class.new("https://example.com").main).to match expected_regexp
+    expect(described_class.new(File.expand_path("spec/fixtures/files/image_gps_data.jpg")).main).to match expected_regexp
   end
 end
