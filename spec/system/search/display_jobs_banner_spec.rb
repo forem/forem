ee3792f4470e7e1d7b1e6faad8569ac2f7750846
Molly Struve <mollylbs@gmail.com>
2020-06-08 17:58:00 -0500

    [deploy] Use Javascript to Display Jobs Banner (#8303)
    * Use Javascript to Display Jobs Banner
    
    * one equal is enough
    
    * stub SiteConfig values
    
    * Return banner for all searches containing jobs, ensure banner is always at top, remove old job partial
    
    * check for query before executing function

---
+++ spec/system/search/display_jobs_banner_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe "Display Jobs Banner spec", type: :system, js: true do
+  before do
+    stub_request(:post, "http://www.google-analytics.com/collect")
+    allow(SiteConfig).to receive(:jobs_url).and_return("www.very_cool_jobs_website.com")
+  end
+
+  context "when SiteConfig.display_jobs_banner is false" do
+    it "does not show jobs banner" do
+      allow(SiteConfig).to receive(:display_jobs_banner).and_return(false)
+      visit "/search?q=jobs"
+
+      expect(page).not_to have_content("Interested in joining our team?")
+    end
+  end
+
+  context "when SiteConfig.display_jobs_banner is true" do
+    before { allow(SiteConfig).to receive(:display_jobs_banner).and_return(true) }
+
+    it "displays job banner for job search" do
+      visit "/search?q=jobs"
+
+      expect(page).to have_content("Interested in joining our team?")
+      expect(find_link("open roles")["href"]).to include(SiteConfig.jobs_url)
+    end
+
+    it "does not display jobs banner for other searches" do
+      visit "/search?q=ruby"
+
+      expect(page).not_to have_content("Interested in joining our team?")
+    end
+  end
+end
