4da715d464d887868b31097e19ddc98bd34192ef
Vaidehi Joshi <vaidehi.sj@gmail.com>
2020-06-08 17:23:13 -0700

    Add a "x" close button to announcements, fix styling to accommodate it

--- app/assets/javascripts/initializers/initializeBroadcast.js
+++ app/assets/javascripts/initializers/initializeBroadcast.js
@@ -28,9 +28,24 @@ function initializeBroadcast() {
     return; // Only append HTML once, on first render.
   }
 
-  const bannerDiv = `<div class='broadcast-data ${
-    banner_class || ''
-  }'>${html}</div>`;
+  if (banner_class) {
+    // FIXME: clean this up!
+    const classes = banner_class.split(' ');
+    if (classes[0]) {
+      el.classList.add(classes[0]);
+    }
+    if (classes[1]) {
+      el.classList.add(classes[1]);
+    }
+  }
+
+  const closeButton = `<button class="close-announcement">
+    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M6.99974 5.58623L11.9497 0.63623L13.3637 2.05023L8.41374 7.00023L13.3637 11.9502L11.9497 13.3642L6.99974 8.41423L2.04974 13.3642L0.635742 11.9502L5.58574 7.00023L0.635742 2.05023L2.04974 0.63623L6.99974 5.58623Z" fill="white" />
+    </svg>
+  </button>`;
+
+  const bannerDiv = `<div class='broadcast-data'>${html}</div>${closeButton}`;
   el.insertAdjacentHTML('afterbegin', bannerDiv);
 }
 /* eslint-enable camelcase */
