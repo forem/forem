d6c8609588f22a310b4b2a85ec6156ce3aa2db8b
Arit Amana <32520970+msarit@users.noreply.github.com>
2020-06-11 16:48:35 -0400

    [Mod Center] Implement grid layout; format Date (#8417)
    * Implement grid layout; format published date
    
    * Revert phantom schema.rb changes
    
    * Correct JS error; adjust inbox columns

--- app/javascript/moderationArticles/singleArticle/index.jsx
+++ app/javascript/moderationArticles/singleArticle/index.jsx
@@ -17,6 +17,42 @@ const SingleArticle = ({
     );
   });
 
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  const get12HourTime = (date) => {
+    const minutes = date.getMinutes();
+    let hours = date.getHours();
+    const AmOrPm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12 || 12;
+
+    return `${hours}:${minutes} ${AmOrPm}`;
+  };
+
+  const formatDate = (timestamp) => {
+    const dateToday = new Date();
+    const origDatePublished = new Date(timestamp);
+
+    if (dateToday.toDateString() === origDatePublished.toDateString()) {
+      return get12HourTime(origDatePublished);
+    }
+    return `${
+      months[origDatePublished.getMonth()]
+    } ${origDatePublished.getDate()}`;
+  };
+
   return (
     <div className="moderation-single-article">
       <span className="article-title">
@@ -26,7 +62,7 @@ const SingleArticle = ({
         {tags}
       </span>
       <span className="article-author">{user.name}</span>
-      <span className="article-published-at">{publishedAt}</span>
+      <span className="article-published-at">{formatDate(publishedAt)}</span>
     </div>
   );
 };
