b9a83d3132d89b9098d21a0895a8aba026d83d6a
Nick Taylor <nick@iamdeveloper.com>
2020-06-11 06:04:59 -0400

    Migrating Frontend test to preact-testing-library Part 1 (#8376)
    * Upgraded to Preact 10.4.4.
    
    * Removed old testing tools for frontend.
    
    * Added preact-testing-library to the project.
    
    * Updated frontend docs in regards to testing tools.
    
    * Added optional additional props to button.
    
    * Added optional additional props to dropdown.
    
    * Added @testing-library/jest-dom
    
    * migrated comment subscription tests.
    
    * Migrated tests for <Search />.
    
    * Migrated tests for <SnackbarItem />
    
    * Migrated tests for <Dropdown />
    
    * Migrated tests for <Formfield />
    
    * Migrated tests for <ButtonGroup />
    
    * Migrated tests for <Button />
    
    * Added a111y tests to <ButtonGroup />.
    
    * Added a111y tests to <RadioButton />.
    
    * Migrated tests for <Article />
    
    * Migrated tests for <LoadingArticle />
    
    * Migrated tests for <CommentList />
    
    * Migrated tests for <Alert />
    
    * Migrated tests for <ChannelButton />
    
    * Migrated tests for <Article />
    
    * Migrated tests for <ChannelRequest />
    
    * Migrated tests for <GithubRrepos />
    
    * Migrated tests for <CogIcon />
    
    * Updated snapshot files to use container.innerHTML instead of container.
    
    * Added a111y tests to <CogIcon />
    
    * Migrated tests for chat <Message />
    
    * Migrated tests for <TagsFollowed />
    
    * Migrated tests for <BodyMarkdown />
    
    * Migrated tests for <ClearQueryButton />
    
    * Migrated tests for chat <Content />
    
    * Migrated tests for chat <ContactViaConnect />
    
    * Migrated tests for chat <Compose />
    
    * Migrated tests for <SingleRepo />
    
    * Migrated tests for <Channels />
    
    * Migrated tests for <Chat />
    
    * Migrated tests for <AllListings />
    
    * Fixed a snapshot test for the <RadioButton />.
    
    * Migrated tests for <ListingDashboard />

--- app/javascript/listings/__tests__/AllListings.test.jsx
+++ app/javascript/listings/__tests__/AllListings.test.jsx
@@ -1,5 +1,6 @@
 import { h } from 'preact';
-import render from 'preact-render-to-json';
+import { render } from '@testing-library/preact';
+import { axe } from 'jest-axe';
 import AllListings from '../components/AllListings';
 
 const firstListing = {
@@ -27,12 +28,12 @@ const secondListing = {
   processed_html:
     '\u003cp\u003eEius et ullam. Dolores et qui. Quis \u003cstrong\u003equi\u003c/strong\u003e omnis.\u003c/p\u003e\n',
   slug: 'illo-iure-quos-ereerr-5hk7',
-  title: 'Second tag.',
+  title: 'This is an awesome listing',
   tags: ['functional', 'clojure'],
   user_id: 1,
   author: {
-    name: 'Mrs. Ashahir',
-    username: 'mrschristiansenyoko',
+    name: 'Mr. Rogers',
+    username: 'fred',
     profile_image_90:
       '/uploads/user/profile_image/7/4b1c980a-beb0-4a5f-b3f2-acc91adc503c.png',
   },
@@ -43,14 +44,14 @@ const thirdListing = {
   category: 'misc',
   location: 'West Refugio',
   processed_html:
-    '\u003cp\u003eEius et ullam. Dolores et qui. Quis \u003cstrong\u003equi\u003c/strong\u003e omnis.\u003c/p\u003e\n',
+    '\u003cp\u003eBobby says hello. Eius et ullam. Dolores et qui. Quis \u003cstrong\u003equi\u003c/strong\u003e omnis.\u003c/p\u003e\n',
   slug: 'illo-iure-fss-ssasas-5hk7',
-  title: 'Illo iure quos perspiciatis.',
+  title: 'Illo iure quos perspiciatis',
   tags: ['twitter', 'learning'],
   user_id: 1,
   author: {
     name: 'Mrs. John Mack',
-    username: 'mrschristiansenyoko',
+    username: 'mrsjohnmack',
     profile_image_90:
       '/uploads/user/profile_image/7/4b1c980a-beb0-4a5f-b3f2-acc91adc503c.png',
   },
@@ -76,8 +77,128 @@ const getProps = () => ({
 const renderAllListings = () => render(<AllListings {...getProps()} />);
 
 describe('<AllListings />', () => {
-  it('Should render all listings', () => {
-    const context = renderAllListings();
-    expect(context).toMatchSnapshot();
+  it('should have no a11y violations', async () => {
+    const { container } = renderAllListings();
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
+
+  it('should render the given listings', async () => {
+    const { getByTestId, getByText } = renderAllListings();
+
+    // Ensure each listing is present
+    const titleOptions = {
+      selector: 'h3 > a',
+    };
+
+    // 1st listings
+    getByTestId('single-listing-20');
+
+    // listing title
+    const listing1Title = getByText('Mentor wanted', titleOptions);
+
+    expect(listing1Title.getAttribute('href')).toEqual(
+      '/listings/misc/illo-iure-quos-htyashsayas-5hk7',
+    );
+
+    // listing body
+    getByText(/Eius et ullam. Dolores et qui. Quis/, {
+      selector: '[data-testid="single-listing-20"] *',
+    });
+
+    // listing tags
+    const goTag = getByText('go', {
+      selector: '[data-testid="single-listing-20"] a',
+    });
+
+    expect(goTag.getAttribute('href')).toEqual('/listings?t=go');
+
+    const gitTag = getByText('git', {
+      selector: '[data-testid="single-listing-20"] a',
+    });
+
+    expect(gitTag.getAttribute('href')).toEqual('/listings?t=git');
+
+    // listing author
+    const listing1Author = getByText('Mrs. Yoko Christiansen', {
+      selector: '[data-testid="single-listing-20"] a',
+    });
+
+    expect(listing1Author.getAttribute('href')).toEqual('/mrschristiansenyoko');
+
+    // 2nd listing
+    getByTestId('single-listing-21');
+
+    // listing title
+    const listing2Title = getByText('This is an awesome listing', titleOptions);
+
+    expect(listing2Title.getAttribute('href')).toEqual(
+      '/listings/misc/illo-iure-quos-ereerr-5hk7',
+    );
+
+    // listing body
+    getByText(/Eius et ullam. Dolores et qui. Quis/, {
+      selector: '[data-testid="single-listing-21"] *',
+    });
+
+    // listing tags
+    const functionalTag = getByText('functional', {
+      selector: '[data-testid="single-listing-21"] a',
+    });
+
+    expect(functionalTag.getAttribute('href')).toEqual(
+      '/listings?t=functional',
+    );
+
+    const clojureTag = getByText('clojure', {
+      selector: '[data-testid="single-listing-21"] a',
+    });
+
+    expect(clojureTag.getAttribute('href')).toEqual('/listings?t=clojure');
+
+    // listing author
+    const listing2Author = getByText('Mr. Rogers', {
+      selector: '[data-testid="single-listing-21"] a',
+    });
+
+    expect(listing2Author.getAttribute('href')).toEqual('/fred');
+
+    // 3rd listing
+    getByTestId('single-listing-22');
+
+    // listing title
+    const listing3Title = getByText(
+      'Illo iure quos perspiciatis',
+      titleOptions,
+    );
+
+    expect(listing3Title.getAttribute('href')).toEqual(
+      '/listings/misc/illo-iure-fss-ssasas-5hk7',
+    );
+
+    // listing body
+    getByText(/Bobby says hello. Eius et ullam. Dolores et qui. Quis/, {
+      selector: '[data-testid="single-listing-22"] *',
+    });
+
+    // listing tags
+    const twitterTag = getByText('twitter', {
+      selector: '[data-testid="single-listing-22"] a',
+    });
+
+    expect(twitterTag.getAttribute('href')).toEqual('/listings?t=twitter');
+
+    const learningTag = getByText('learning', {
+      selector: '[data-testid="single-listing-22"] a',
+    });
+
+    expect(learningTag.getAttribute('href')).toEqual('/listings?t=learning');
+
+    const listing3Author = getByText('Mrs. John Mack', {
+      selector: '[data-testid="single-listing-22"] a',
+    });
+
+    expect(listing3Author.getAttribute('href')).toEqual('/mrsjohnmack');
   });
 });
