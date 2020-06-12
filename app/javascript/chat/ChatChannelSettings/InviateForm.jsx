94f1b06f6d3ea72831a2344484c6ead01ba1c943
Nick Taylor <nick@iamdeveloper.com>
2020-06-12 09:09:42 -0400

    Migrated tests for chat <ModSection />

--- app/javascript/chat/ChatChannelSettings/InviateForm.jsx
+++ app/javascript/chat/ChatChannelSettings/InviateForm.jsx
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types';
 const InviteForm = ({
   handleChatChannelInvitations,
   invitationUsernames,
-  handleInvitationUsernames
+  handleInvitationUsernames,
 }) => {
   return (
-    <div className="crayons-card p-4 grid gap-2 mb-4">
+    <div data-testid="invite-form" className="crayons-card p-4 grid gap-2 mb-4">
       <div className="crayons-field">
         <label
           className="crayons-field__label invitation_form_title"
-          htmlFor="chat_channel_membership_Usernames to Invite"
+          htmlFor="chat_channel_membership_invitation_usernames"
         >
           Usernames to invite
         </label>
@@ -35,13 +35,13 @@ const InviteForm = ({
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 InviteForm.propTypes = {
   handleInvitationUsernames: PropTypes.func.isRequired,
   handleChatChannelInvitations: PropTypes.func.isRequired,
-  invitationUsernames: PropTypes.func.isRequired
-}
+  invitationUsernames: PropTypes.func.isRequired,
+};
 
 export default InviteForm;
