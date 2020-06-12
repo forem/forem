d5a6ca2baeb6f0130ae236b136c006869b0a9610
rhymes <rhymesete@gmail.com>
2020-06-12 14:30:52 +0200

    Replace PollSkips find_or_create_by with create_or_find_by

--- app/models/poll_skip.rb
+++ app/models/poll_skip.rb
@@ -1,13 +1,4 @@
 class PollSkip < ApplicationRecord
   belongs_to :poll
   belongs_to :user
-
-  validate :one_vote_per_poll_per_user
-
-  private
-
-  def one_vote_per_poll_per_user
-    already_voted = poll.poll_votes.where(user_id: user_id).any? || poll.poll_skips.where(user_id: user_id).any?
-    errors.add(:base, "cannot vote more than once in one poll") if already_voted
-  end
 end
