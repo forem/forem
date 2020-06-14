f9c4eb0ef9426e9b73070952206c83a9ec299299
Thibault <933kss@gmail.com>
2020-06-15 00:16:45 +0200

    Add send webmention worker

---
+++ app/workers/web_mentions/send_web_mention.rb
@@ -0,0 +1,17 @@
+module WebMentions
+  class SendWebMention
+    include Sidekiq::Worker
+    sidekiq_options queue: :low_priority, retry: 15, lock: :until_executing
+
+    def perform(comment_id)
+      comment = Comment.find_by(id: comment_id)
+      return unless comment
+
+      return unless comment.commentable.support_webmentions
+
+      canonical_url = comment.commentable.canonical_url
+      article_url = ApplicationConfig["APP_DOMAIN"] + id.commentable.path
+      WebMentions::WebMentionHandler.call(canonical_url, article_url)
+    end
+  end
+end
