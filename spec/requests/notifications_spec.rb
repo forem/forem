ee7e90c4e99c3b97e028618ad839f10dd3b93124
benhalpern <bendhalpern@gmail.com>
2020-06-14 14:43:35 -0400

    Fix specs

--- spec/requests/notifications_spec.rb
+++ spec/requests/notifications_spec.rb
@@ -187,12 +187,6 @@ RSpec.describe "NotificationsIndex", type: :request do
         expect(has_both_names(response.body)).to be true
       end
 
-      it "does not include private reactions" do
-        mock_heart_reaction_notifications(50, %w[like unicorn thumbsdown])
-        get "/notifications"
-        expect(response.body).not_to include("Thumbsdown")
-      end
-
       it "renders the proper message for multiple reactions" do
         random_amount = rand(3..10)
         mock_heart_reaction_notifications(random_amount, %w[unicorn like])
@@ -588,6 +582,7 @@ RSpec.describe "NotificationsIndex", type: :request do
     context "when user is trusted" do
       let(:user) { create(:user, :trusted) }
       let(:reaction) { create(:thumbsdown_reaction, user: user) }
+      let(:like_reaction) { create(:reaction, user: user) }
 
       it "allow sees thumbsdown category" do
         sign_in user
@@ -595,6 +590,17 @@ RSpec.describe "NotificationsIndex", type: :request do
         get "/notifications"
         expect(response.body).to include("Notifications")
       end
+
+      it "does not show notification" do
+        other_user = create(:user)
+        sign_in other_user
+        Notification.send_reaction_notification_without_delay(reaction, other_user)
+        Notification.send_reaction_notification_without_delay(like_reaction, other_user)
+        get "/notifications"
+        expect(response.body).to include("Like")
+        expect(response.body).not_to include("Thumbsdown")
+      end
+
     end
 
     context "when a user has a new welcome notification" do
