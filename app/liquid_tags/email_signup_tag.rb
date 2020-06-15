0d651aa6b902bd930a9f9fdd9286ed914246d0b4
Alex <alexandersmith223@gmail.com>
2020-06-15 14:18:06 -0400

    Try removing email_signup_tag to fix Travis

--- app/liquid_tags/email_signup_tag.rb
+++
@@ -1,9 +0,0 @@
-class EmailSignupTag < LiquidTagBase
-  def initialize(_tag_name, cta_text, _tokens)
-    @cta_text = cta_text.strip
-  end
-
-  def render(context); end
-end
-
-Liquid::Template.register_tag("email_signup", EmailSignupTag)
