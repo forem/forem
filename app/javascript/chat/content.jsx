8b9670abd7dbb4115d84188c2423617d91433244
narender2031 <narender2031@gmail.com>
2020-06-16 02:30:41 +0530

    Skynox/fix eslint and redirection bug (#8513)
    * Feature 🚀 : Ability to delete messages in chat channels
    
    - Sending message ID to frontend
    - Deleting Message
    - Use pusher to delete message realtime
    
    * Minor Bug 🐞: Show message action only for current user
    
    - User can delete or edit their own messages
    
    * Test cases added
    
    * Bug 🐞: Update message id for receiver
    
    Message id was not sent to receiver by pusher
    
    * Refactoring🛠: Message controller refactoring
    
    * Test Cases📝 : Specs for Delete message added
    
    * Feature 🚀 : Ability to edit messages
    
    * Test Cases📝 : Specs for Edit message added
    
    * fix Content component esLint issue
    
    * fix @all link
    
    Co-authored-by: Sarthak Sharma <7lovesharma7@gmail.com>

--- app/javascript/chat/content.jsx
+++ app/javascript/chat/content.jsx
@@ -7,87 +7,108 @@ import ChatChannelSettings from './ChatChannelSettings/ChatChannelSettings';
 
 export default class Content extends Component {
   static propTypes = {
-    resource        : PropTypes.object,
-    activeChannelId : PropTypes.number,
-    pusherKey       : PropTypes.string,
-    fullscreen      : PropTypes.bool
+    resource: PropTypes.shape({
+      data: PropTypes.any,
+      type_of: PropTypes.string.isRequired,
+      handleRequestRejection: PropTypes.func,
+      handleRequestApproval: PropTypes.func,
+      handleJoiningRequest: PropTypes.func,
+      activeMembershipId: PropTypes.func,
+    }).isRequired,
+    fullscreen: PropTypes.bool.isRequired,
+    onTriggerContent: PropTypes.func.isRequired,
   };
 
   render() {
-    const { onTriggerContent, fullscreen } = this.props;
-    if (!this.props.resource) {
+    const { onTriggerContent, fullscreen, resource } = this.props;
+    if (!resource) {
       return '';
     }
 
     const smartSvgIcon = (content, d) => (
-      <svg data-content={content} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+      <svg
+        data-content={content}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+      >
         <path data-content={content} fill="none" d="M0 0h24v24H0z" />
         <path data-content={content} d={d} />
       </svg>
     );
 
+    const Display = () => {
+      switch (resource.type_of) {
+        case 'loading-user':
+          return <div className="loading-user" />;
+        case 'article':
+          return <Article resource={resource} />;
+        case 'channel-request':
+          return (
+            <ChannelRequest
+              resource={resource.data}
+              handleJoiningRequest={resource.handleJoiningRequest}
+            />
+          );
+        case 'channel-request-manager':
+          return (
+            <RequestManager
+              resource={resource.data}
+              handleRequestRejection={resource.handleRequestRejection}
+              handleRequestApproval={resource.handleRequestApproval}
+            />
+          );
+        case 'chat-channel-setting':
+          return (
+            <ChatChannelSettings
+              resource={resource.data}
+              activeMembershipId={resource.activeMembershipId}
+            />
+          );
+        default:
+          return null;
+      }
+    };
+
     return (
+      // eslint-disable-next-line jsx-a11y/click-events-have-key-events
       <div
         className="activechatchannel__activecontent activechatchannel__activecontent--sidecar"
         id="chat_activecontent"
         onClick={onTriggerContent}
+        role="button"
+        tabIndex="0"
       >
         <button
           className="activechatchannel__activecontentexitbutton crayons-btn crayons-btn--secondary"
           data-content="exit"
+          type="button"
         >
           {smartSvgIcon(
             'exit',
-            'M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z'
+            'M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z',
           )}
         </button>
         <button
           className="activechatchannel__activecontentexitbutton activechatchannel__activecontentexitbutton--fullscreen crayons-btn crayons-btn--secondary"
           data-content="fullscreen"
           style={{ left: '39px' }}
+          type="button"
         >
-  {' '}
-  {fullscreen ? (
-            smartSvgIcon(
-              'fullscreen',
-              'M18 7h4v2h-6V3h2v4zM8 9H2V7h4V3h2v6zm10 8v4h-2v-6h6v2h-4zM8 15v6H6v-4H2v-2h6z'
-            )
-          ) : (
-            smartSvgIcon(
-              'fullscreen',
-              'M20 3h2v6h-2V5h-4V3h4zM4 3h4v2H4v4H2V3h2zm16 16v-4h2v6h-6v-2h4zM4 19h4v2H2v-6h2v4z'
-            )
-          )}
+          {' '}
+          {fullscreen
+            ? smartSvgIcon(
+                'fullscreen',
+                'M18 7h4v2h-6V3h2v4zM8 9H2V7h4V3h2v6zm10 8v4h-2v-6h6v2h-4zM8 15v6H6v-4H2v-2h6z',
+              )
+            : smartSvgIcon(
+                'fullscreen',
+                'M20 3h2v6h-2V5h-4V3h4zM4 3h4v2H4v4H2V3h2zm16 16v-4h2v6h-6v-2h4zM4 19h4v2H2v-6h2v4z',
+              )}
         </button>
-        {display(this.props)}
+        <Display resource={resource} />
       </div>
     );
-  }
-}
-
-function display(props) {
-  const { resource } = props;
-
-  switch(resource.type_of) {
-    case 'loading-user':
-      return <div className="loading-user" />
-    case 'article':
-      return <Article resource={resource} />;
-    case 'channel-request':
-      return <ChannelRequest resource={resource.data} handleJoiningRequest={resource.handleJoiningRequest} />;
-    case 'channel-request-manager': 
-      return (
-        <RequestManager
-          resource={resource.data}
-          handleRequestRejection={resource.handleRequestRejection}
-          handleRequestApproval={resource.handleRequestApproval}
-        />
-      )
-    case 'chat-channel-setting':
-      return (
-        <ChatChannelSettings resource={resource.data} activeMembershipId={resource.activeMembershipId} />
-      );
-    default:
-      return null;
   }
 }
