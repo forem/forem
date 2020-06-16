f878444bbe7c99bb5bfa9f14ce4d986b1278f1f9
Alex <alexandersmith223@gmail.com>
2020-06-15 21:39:40 -0400

    z

--- spec/requests/email_signups_spec.rb
+++ spec/requests/email_signups_spec.rb
@@ -1,10 +1,10 @@
 require "rails_helper"
 
 RSpec.describe "EmailSignups", type: :request do
-  let(:article_with_email_signup) { create(:article, body_markdown: "---\ntitle: Email Signup#{rand(1000)}\npublished: true\n---\n\n{% email_signup 'CTA text' %}") }
+  let(:article_with_email_signup) { create(:article, body_markdown: "---\ntitle: Email Signup#{rand(1000)}---\n\n{% email_signup 'CTA text' %}") }
 
   describe "POST /email_signups - EmailSignups#create" do
-    it "creates a UserSubscription" do
+    it "FINALLY WORKS, PLZ!" do
       expect(true).to eq true
     end
   end
