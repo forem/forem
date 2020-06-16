4a50ef093ccca017881e23d18ebf18d2ed953dfb
Alex <alexandersmith223@gmail.com>
2020-06-15 22:20:58 -0400

    k

--- spec/requests/email_signups_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-RSpec.describe "EmailSignups", type: :request do
-  describe "will you work...please?" do
-    it "just work...please!!" do
-      expect(true).to eq true
-    end
-  end
-end
