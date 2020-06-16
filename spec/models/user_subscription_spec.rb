af740e2150524e705aed2e6a3ad5f8163e5e30a7
Alex <alexandersmith223@gmail.com>
2020-06-16 18:25:46 -0400

    Specs for subscribed_to_user_subscriptions_count

--- spec/models/user_subscription_spec.rb
+++ spec/models/user_subscription_spec.rb
@@ -11,4 +11,25 @@ RSpec.describe UserSubscription, type: :model do
     it { is_expected.to validate_inclusion_of(:user_subscription_sourceable_type).in_array(%w[Article]) }
     it { is_expected.to validate_uniqueness_of(:subscriber_id).scoped_to(:user_subscription_sourceable_type, :user_subscription_sourceable_id) }
   end
+
+  describe "counter_culture" do
+    let(:user) { create(:user) }
+
+    context "when a UserSubscription is created" do
+      it "increments subscribed_to_user_subscriptions_count on user" do
+        expect do
+          create(:user_subscription, subscriber: user)
+        end.to change { user.reload.subscribed_to_user_subscriptions_count }.by(1)
+      end
+    end
+
+    context "when a reaction is destroyed" do
+      it "decrements subscribed_to_user_subscriptions_count on user" do
+        user_subscription = create(:user_subscription, subscriber: user)
+        expect do
+          user_subscription.destroy
+        end.to change { user.reload.subscribed_to_user_subscriptions_count }.by(-1)
+      end
+    end
+  end
 end
