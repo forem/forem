09901979b4f99dc698af61023fb16ed173e5300f
Alex <alexandersmith223@gmail.com>
2020-06-15 23:06:24 -0400

    z

---
+++ spec/requests/test_travis_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe "TestTravis", type: :request do
+  describe "will you work...please?" do
+    it "just work...please!!" do
+      expect(true).to eq true
+    end
+  end
+end
