d7d540a544a5d8090595a8dd80e60e8310f68eed
Ben Halpern <bendhalpern@gmail.com>
2020-06-17 14:16:09 -0400

    Update spec/services/users/profile_image_generator_spec.rb
    Co-authored-by: Mac Siri <krairit.siri@gmail.com>

--- spec/services/users/profile_image_generator_spec.rb
+++ spec/services/users/profile_image_generator_spec.rb
@@ -1,5 +1,9 @@
 require "rails_helper"
 
+RSpec.configure do |c|
+  include CloudinaryHelper
+end
+
 RSpec.describe Users::ProfileImageGenerator, type: :service do
   it "returns a cloudinary url" do
     expect(described_class.call).to start_with("https://res.cloudinary.com/")
