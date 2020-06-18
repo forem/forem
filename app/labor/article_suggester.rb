8211669512f03255910c7723d22bee76bee96f31
mstruve <mollylbs@gmail.com>
2020-06-18 12:44:37 -0500

    fix Travis and code climate failures

--- app/labor/article_suggester.rb
+++ app/labor/article_suggester.rb
@@ -3,8 +3,8 @@ class ArticleSuggester
     @article = article
   end
 
-  def articles(max: 4, user_signed_in: false)
-    if article.cached_tag_list_array.any?
+  def articles(max: 4)
+    if cached_tag_list_array.any?
       # avoid loading more data if we don't need to
       tagged_suggestions = suggestions_by_tag(max: max)
       return tagged_suggestions if tagged_suggestions.size == max
@@ -15,11 +15,10 @@ class ArticleSuggester
       other_articles = other_suggestions(
         max: num_remaining_needed,
         ids_to_ignore: tagged_suggestions.pluck(:id),
-        user_signed_in: user_signed_in,
       )
       tagged_suggestions.union(other_articles)
     else
-      other_suggestions(max: max, user_signed_in: user_signed_in)
+      other_suggestions(max: max)
     end
   end
 
@@ -27,17 +26,18 @@ class ArticleSuggester
 
   attr_reader :article
 
-  def other_suggestions(max: 4, ids_to_ignore: [], user_signed_in: false)
+  def other_suggestions(max: 4, ids_to_ignore: [])
     ids_to_ignore << article.id
     Article.published.
-      where.not(id: ids_to_ignore, user_id: article.user_id).
+      where.not(id: ids_to_ignore).
+      where.not(user_id: article.user_id).
       order("hotness_score DESC").
       offset(rand(0..offset)).
       first(max)
   end
 
-  def suggestions_by_tag(max: 4, user_signed_in: false)
-    Article.published.tagged_with(article.cached_tag_list_array, any: true).
+  def suggestions_by_tag(max: 4)
+    Article.published.tagged_with(cached_tag_list_array, any: true).
       where.not(user_id: article.user_id).
       where("organic_page_views_past_month_count > 5").
       order("hotness_score DESC").
@@ -47,5 +47,9 @@ class ArticleSuggester
 
   def offset
     Rails.env.production? ? 200 : 0
+  end
+
+  def cached_tag_list_array
+    (article.cached_tag_list || "").split(", ")
   end
 end
