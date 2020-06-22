9fd565d71e6fcc512c05cee616698d71b1b25b12
Vaidehi Joshi <vaidehi.sj@gmail.com>
2020-06-22 15:57:20 -0700

    Only track server-side events when needed (#8839) [deploy]
    Ensures that we don't create new visits for non-logged-in users, bots,
    and users with cookies disabled on every site visit. This will only
    allow server-side visits when there is an event to be tracked.

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -6,3 +6,6 @@ Ahoy.api = true
 
 # set to false to disable geocode tracking
 Ahoy.geocode = false
+
+# only create visits server-side when needed for events and `visitable`
+Ahoy.server_side_visits = :when_needed
