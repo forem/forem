e36200df6f6e9510b736138fb0e9c1d10f11a2f5
Arit Amana <msarit@gmail.com>
2020-06-22 15:32:38 -0400

    Show alert when using ModCenter on mobile

--- app/javascript/packs/modCenter.jsx
+++ app/javascript/packs/modCenter.jsx
@@ -5,8 +5,15 @@ let elementLoaded = false;
 
 function loadElement() {
   const root = document.getElementById('mod-index-list');
+  const isMobileDevice = typeof window.orientation !== 'undefined';
+
   if (root) {
     render(<ModerationArticles />, root);
+  }
+
+  if (isMobileDevice) {
+    // eslint-disable-next-line no-alert
+    alert('The Mod Center is best viewed on desktop');
   }
 }
 
