a05f4cd62e9fd989f1f1c58e3778855da9a588d0
rhymes <rhymesete@gmail.com>
2020-06-23 12:30:21 +0200

    Use Action Mailer parameterized syntax

--- app/services/users/request_destroy.rb
+++ app/services/users/request_destroy.rb
@@ -4,8 +4,10 @@ module Users
 
     def call(user)
       token = SecureRandom.hex(10)
+
       Rails.cache.write("user-destroy-token-#{user.id}", token, expires_in: 12.hours)
-      NotifyMailer.account_deletion_requested_email(user, token).deliver
+
+      NotifyMailer.with(user: user, token: token).account_deletion_requested_email.deliver_now
     end
   end
 end
