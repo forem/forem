d53a891536b1e64896fb991647a9041c6b0e4d42
Zhao-Andy <17884966+Zhao-Andy@users.noreply.github.com>
2020-06-23 12:08:53 -0400

    Use snackbar instead of alert

--- app/javascript/packs/modCenter.jsx
+++ app/javascript/packs/modCenter.jsx
@@ -1,19 +1,27 @@
 import { h, render } from 'preact';
 import { ModerationArticles } from '../modCenter/moderationArticles';
+import { addSnackbarItem, Snackbar } from '../Snackbar';
 
 let elementLoaded = false;
 
 function loadElement() {
   const root = document.getElementById('mod-index-list');
   const isMobileDevice = typeof window.orientation !== 'undefined';
+  const snackZone = document.getElementById('snack-zone');
 
   if (root) {
     render(<ModerationArticles />, root);
   }
 
+  if (snackZone) {
+    render(<Snackbar lifespan="0" />, snackZone);
+  }
+
   if (isMobileDevice) {
-    // eslint-disable-next-line no-alert
-    alert('The Mod Center is best viewed on desktop');
+    addSnackbarItem({
+      message: 'The Mod Center is best viewed on desktop.',
+      addCloseButton: true,
+    });
   }
 }
 
