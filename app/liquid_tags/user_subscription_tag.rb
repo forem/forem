68ed13c9448fb15816a2e53c1f9de1a14c267a8c
Alex <alexandersmith223@gmail.com>
2020-06-23 17:33:17 -0400

    More JS updates

--- app/liquid_tags/user_subscription_tag.rb
+++ app/liquid_tags/user_subscription_tag.rb
@@ -7,11 +7,16 @@ class UserSubscriptionTag < LiquidTagBase
     }
 
     function showSignedIn() {
-      subscriptionSignedOut.style.display = 'none';
+      const subscriptionSignedIn = document.getElementById('subscription-signed-in');
       subscriptionSignedIn.style.display = 'block';
+
+      const subscriptionSignedOut = document.getElementById('subscription-signed-out');
+      subscriptionSignedOut.style.display = 'none';
     }
 
     function addSignInClickEvent() {
+      const signInBtn = document.getElementById('sign-in-btn');
+
       if (signInBtn !== null) {
         signInBtn.addEventListener('click', function(e) {
           if (typeof showModal !== "undefined") {
@@ -21,44 +26,67 @@ class UserSubscriptionTag < LiquidTagBase
       }
     }
 
-    async function fetchBaseData() {
-      let params = new URLSearchParams({
-        author_id: authorId,
-        source_type: "Article",
-        source_id: articleId
-      }).toString();
-
-      let headers = {
-        Accept: 'application/json',
-        'X-CSRF-Token': window.csrfToken,
-        'Content-Type': 'application/json',
-      }
-
-      let response = await fetch(`/user_subscriptions/base_data?${params}`, {
-        method: 'GET',
-        headers: headers,
-        credentials: 'same-origin',
-      });
-
-      let data = await response.json()
-
-      return data;
-    }
-
-    const articleId = document.getElementById('article-body').dataset.articleId;
-    const authorId = document.getElementById('article-show-container').dataset.authorId;
-    const signInBtn = document.getElementById('sign-in-btn');
-    const subscriptionSignedIn = document.getElementById('subscription-signed-in');
-    const subscriptionSignedOut = document.getElementById('subscription-signed-out');
-
-    const userSubscriptionBaseData = fetchBaseData();
-
     // The markup defaults to signed out UX
     if (isUserSignedIn()) {
       showSignedIn();
     } else {
       addSignInClickEvent();
     }
+
+    document.addEventListener("DOMContentLoaded", function() {
+      function fetchBaseData() {
+        const articleId = document.getElementById('article-body').dataset.articleId;
+        const authorId = document.getElementById('article-show-container').dataset.authorId;
+
+        const params = new URLSearchParams({
+          author_id: authorId,
+          source_type: "Article",
+          source_id: articleId
+        }).toString();
+
+        const headers = {
+          Accept: 'application/json',
+          'X-CSRF-Token': window.csrfToken,
+          'Content-Type': 'application/json',
+        }
+
+        return fetch(`/user_subscriptions/base_data?${params}`, {
+          method: 'GET',
+          headers: headers,
+          credentials: 'same-origin',
+        }).then(function(response) {
+          if (response.ok) {
+            return response.json();
+          } else {
+            console.error(`Base data error: ${response.status} - ${response.statusText}`);
+          }
+        });
+      }
+
+      function updateSubcriptionElements() {
+        fetchBaseData().then(function(response) {
+          updateElements('.author-username', response.author.username);
+          updateElements('.subscriber-email', response.subscriber.email);
+          // updateElements('.author-image', response.author.profile_image_90);
+
+          // if (response.subscriber.is_subscribed) {
+            // showSubscribed();
+          // }
+        });
+      }
+
+      function updateElements(identifier, value) {
+        const elements = document.querySelectorAll(identifier);
+
+        elements.forEach(function(element) {
+          element.innerHTML = value;
+        });
+      }
+
+      if (isUserSignedIn()) {
+        updateSubcriptionElements();
+      }
+    });
   JAVASCRIPT
 
   def initialize(_tag_name, cta_text, _tokens)
