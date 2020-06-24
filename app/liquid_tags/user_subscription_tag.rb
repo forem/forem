e864c73343581fc200e04c1bd63c89f7d1792940
Alex <alexandersmith223@gmail.com>
2020-06-23 23:43:57 -0400

    Reorganize

--- app/liquid_tags/user_subscription_tag.rb
+++ app/liquid_tags/user_subscription_tag.rb
@@ -19,122 +19,124 @@ class UserSubscriptionTag < LiquidTagBase
       });
     }
 
+    function showConfirmationModal() {
+      document.getElementById('user-subscription-confirmation-modal').style.display = 'block';
+    }
+
+    function hideConfirmationModal() {
+      document.getElementById('user-subscription-confirmation-modal').style.display = 'none';
+    }
+
+    function submitSubscription() {
+      // TODO hit API to create subscription
+      console.log("SUBSCRIBED");
+    }
+
+    function addClickEventListeners() {
+      document.getElementById('subscribe-btn').addEventListener("click", function() {
+        showConfirmationModal();
+      });
+
+      document.getElementById('cancel-btn').addEventListener("click", function() {
+        hideConfirmationModal();
+      });
+
+      document.getElementById('close-confirmation-modal').addEventListener("click", function() {
+        hideConfirmationModal();
+      });
+
+      document.getElementById('confirmation-btn').addEventListener("click", function() {
+        submitSubscription();
+      });
+    }
+
+    function fetchIsSubscribed() {
+      const articleId = document.getElementById('article-body').dataset.articleId;
+
+      const params = new URLSearchParams({
+        source_type: "Article",
+        source_id: articleId
+      }).toString();
+
+      const headers = {
+        Accept: 'application/json',
+        'X-CSRF-Token': window.csrfToken,
+        'Content-Type': 'application/json',
+      }
+
+      return fetch(`/user_subscriptions/subscribed?${params}`, {
+        method: 'GET',
+        headers: headers,
+        credentials: 'same-origin',
+      }).then(function(response) {
+        if (response.ok) {
+          return response.json();
+        } else {
+          console.error(`Base data error: ${response.status} - ${response.statusText}`);
+        }
+      });
+    }
+
+    function checkIfSubscribed() {
+      fetchIsSubscribed().then(function(response) {
+
+        if (response.is_subscribed) {
+          showSubscribed();
+        } else {
+          updateSubscriberData();
+        }
+      });
+    }
+
+    function showSubscribed() {
+      console.log("ALREADY SUBSCRIBED");
+    }
+
+    function updateSubscriberData() {
+      const subscriber = userData();
+
+      if (subscriber) {
+        updateElementsInnerHTML('.subscriber-email', subscriber.email);
+        updateProfileImages('.subscriber-profile-image', subscriber);
+      }
+    }
+
+    function updateElementsInnerHTML(identifier, value) {
+      const elements = document.querySelectorAll(identifier);
+
+      elements.forEach(function(element) {
+        element.innerHTML = value;
+      });
+    }
+
+    function updateProfileImages(identifier, subscriber) {
+      const profileImages = document.querySelectorAll(`img${identifier}`);
+
+      profileImages.forEach(function(profileImage) {
+        profileImage.src = subscriber.profile_image_90;
+        profileImage.alt = `${subscriber.username} profile image`;
+        profileImage.style.display = 'block';
+      });
+
+      const profileImageWrappers = document.querySelectorAll(`span${identifier}`);
+
+      profileImageWrappers.forEach(function(profileImageWrapper) {
+        profileImageWrapper.style.display = 'inline-block';
+      });
+    }
+
     // The markup defaults to signed out UX
     if (isUserSignedIn()) {
       showSignedIn();
+      addClickEventListeners();
+
+      // We need access to some DOM elements (i.e. csrf token, article id, userData, etc.)
+      document.addEventListener("DOMContentLoaded", function() {
+        checkIfSubscribed();
+      });
     } else {
       addSignInClickEvent();
     }
-
-    // We need access to some DOM elements (i.e. csrf token, article id, etc.)
-    document.addEventListener("DOMContentLoaded", function() {
-      function fetchIsSubscribed() {
-        const articleId = document.getElementById('article-body').dataset.articleId;
-
-        const params = new URLSearchParams({
-          source_type: "Article",
-          source_id: articleId
-        }).toString();
-
-        const headers = {
-          Accept: 'application/json',
-          'X-CSRF-Token': window.csrfToken,
-          'Content-Type': 'application/json',
-        }
-
-        return fetch(`/user_subscriptions/subscribed?${params}`, {
-          method: 'GET',
-          headers: headers,
-          credentials: 'same-origin',
-        }).then(function(response) {
-          if (response.ok) {
-            return response.json();
-          } else {
-            console.error(`Base data error: ${response.status} - ${response.statusText}`);
-          }
-        });
-      }
-
-      function checkIfSubscribed() {
-        fetchIsSubscribed().then(function(response) {
-
-          if (response.is_subscribed) {
-            // showSubscribed();
-            console.log("ALREADY SUBSCRIBED");
-          }
-        });
-      }
-
-      function updateSubscriberData() {
-        const subscriber = userData();
-
-        if (subscriber) {
-          updateElementsInnerHTML('.subscriber-email', subscriber.email);
-          updateProfileImages('.subscriber-profile-image', subscriber);
-        }
-      }
-
-      function updateElementsInnerHTML(identifier, value) {
-        const elements = document.querySelectorAll(identifier);
-
-        elements.forEach(function(element) {
-          element.innerHTML = value;
-        });
-      }
-
-      function updateProfileImages(identifier, subscriber) {
-        const profileImages = document.querySelectorAll(`img${identifier}`);
-
-        profileImages.forEach(function(profileImage) {
-          profileImage.src = subscriber.profile_image_90;
-          profileImage.alt = `${subscriber.username} profile image`;
-          profileImage.style.display = 'block';
-        });
-
-        const profileImageWrappers = document.querySelectorAll(`span${identifier}`);
-
-        profileImageWrappers.forEach(function(profileImageWrapper) {
-          profileImageWrapper.style.display = 'inline-block';
-        });
-      }
-
-      function showConfirmationModal() {
-        document.getElementById('user-subscription-confirmation-modal').style.display = 'block';
-      }
-
-      function hideConfirmationModal() {
-        document.getElementById('user-subscription-confirmation-modal').style.display = 'none';
-      }
-
-      function submitSubscription() {
-        // TODO hit API to create subscription
-        console.log("SUBSCRIBED");
-      }
-
-      function addClickEventListeners() {
-        document.getElementById('subscribe-btn').addEventListener("click", function() {
-          showConfirmationModal();
-        });
-
-        document.getElementById('cancel-btn').addEventListener("click", function() {
-          hideConfirmationModal();
-        });
-
-        document.getElementById('close-confirmation-modal').addEventListener("click", function() {
-          hideConfirmationModal();
-        });
-
-        document.getElementById('confirmation-btn').addEventListener("click", function() {
-          submitSubscription();
-        });
-      }
-
-      if (isUserSignedIn()) {
-        updateSubscriberData();
-        checkIfSubscribed();
-        addClickEventListeners();
-      }
-    });
   JAVASCRIPT
 
   def initialize(_tag_name, cta_text, _tokens)
