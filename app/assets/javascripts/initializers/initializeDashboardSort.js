a5bf308da55c73aff52fb023a998d04be118fb11
ludwiczakpawel <ludwiczakpawel@gmail.com>
2020-06-24 10:48:32 +0200

    .

--- app/assets/javascripts/initializers/initializeDashboardSort.js
+++ app/assets/javascripts/initializers/initializeDashboardSort.js
@@ -1,11 +1,35 @@
+/* global InstantClick */
+
 'use strict';
 
 function initializeDashboardSort() {
-  if (document.getElementById('dashhboard_sort')) {
-    document
-      .getElementById('dashhboard_sort')
-      .addEventListener('change', event => {
-        window.location = '/dashboard?sort=' + event.target.value;
-      });
+  const sortSelector = document.getElementById('dashhboard_sort');
+  if (sortSelector) {
+    sortSelector.addEventListener('change', (event) => {
+      const url = event.target.value;
+
+      InstantClick.preload('/dashboard?sort=' + url);
+      InstantClick.display('/dashboard?sort=' + url);
+    });
+  }
+
+  const sourceSelector = document.getElementById('dashboard_author');
+  if (sourceSelector) {
+    sourceSelector.addEventListener('change', (event) => {
+      const url = event.target.value;
+
+      InstantClick.preload(url);
+      InstantClick.display(url);
+    });
+  }
+
+  const mobileNavDashboard = document.getElementById('mobile_nav_dashboard');
+  if (mobileNavDashboard) {
+    mobileNavDashboard.addEventListener('change', (event) => {
+      const url = event.target.value;
+
+      InstantClick.preload(url);
+      InstantClick.display(url);
+    });
   }
 }
