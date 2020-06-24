e673736b54297f70ac3c16daa66ad9a16fc40022
ludwiczakpawel <ludwiczakpawel@gmail.com>
2020-06-24 15:09:58 +0200

    js refactor

--- app/assets/javascripts/initializers/initializeDashboardSort.js
+++ app/assets/javascripts/initializers/initializeDashboardSort.js
@@ -2,34 +2,24 @@
 
 'use strict';
 
+function selectNavigation(select, urlPrefix) {
+  const trigger = document.getElementById(select);
+
+  if (trigger) {
+    trigger.addEventListener('change', (event) => {
+      let url = event.target.value;
+      if (urlPrefix) {
+        url = urlPrefix + url;
+      }
+
+      InstantClick.preload(url);
+      InstantClick.display(url);
+    });
+  }
+}
+
 function initializeDashboardSort() {
-  const sortSelector = document.getElementById('dashboard_sort');
-  if (sortSelector) {
-    sortSelector.addEventListener('change', (event) => {
-      const url = event.target.value;
-
-      InstantClick.preload('/dashboard?sort=' + url);
-      InstantClick.display('/dashboard?sort=' + url);
-    });
-  }
-
-  const sourceSelector = document.getElementById('dashboard_author');
-  if (sourceSelector) {
-    sourceSelector.addEventListener('change', (event) => {
-      const url = event.target.value;
-
-      InstantClick.preload(url);
-      InstantClick.display(url);
-    });
-  }
-
-  const mobileNavDashboard = document.getElementById('mobile_nav_dashboard');
-  if (mobileNavDashboard) {
-    mobileNavDashboard.addEventListener('change', (event) => {
-      const url = event.target.value;
-
-      InstantClick.preload(url);
-      InstantClick.display(url);
-    });
-  }
+  selectNavigation('dashboard_sort', '/dashboard?sort=');
+  selectNavigation('dashboard_author');
+  selectNavigation('mobile_nav_dashboard');
 }
