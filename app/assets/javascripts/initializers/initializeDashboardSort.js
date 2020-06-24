2dac149102da6b95a5f347a9439dbce73dd594b0
rhymes <rhymesete@gmail.com>
2020-06-24 14:03:27 +0200

    Fix typo in dashboard sort element (#8875)

--- app/assets/javascripts/initializers/initializeDashboardSort.js
+++ app/assets/javascripts/initializers/initializeDashboardSort.js
@@ -1,11 +1,11 @@
 'use strict';
 
 function initializeDashboardSort() {
-  if (document.getElementById('dashhboard_sort')) {
-    document
-      .getElementById('dashhboard_sort')
-      .addEventListener('change', event => {
-        window.location = '/dashboard?sort=' + event.target.value;
-      });
+  const dashboardSorter = document.getElementById('dashboard_sort');
+
+  if (dashboardSorter) {
+    dashboardSorter.addEventListener('change', (event) => {
+      window.location.assign(`/dashboard?sort=${event.target.value}`);
+    });
   }
 }
