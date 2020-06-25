4b814ef73f37c6b10646ba8da0bd51bf025d06dc
rhymes <rhymesete@gmail.com>
2020-06-25 19:14:27 +0200

    Revert "Hide Connect Apple button behind a feature flag"
    This reverts commit 105bde0373389a4eb9b6e948f60734c7e0e99cba.

--- app/helpers/feature_flag.rb
+++ app/helpers/feature_flag.rb
@@ -1,6 +1,6 @@
 module FeatureFlag
   class << self
-    delegate :enable, :enabled?, :disable, :exist?, to: Flipper
+    delegate :enabled?, :exist?, to: Flipper
 
     def accessible?(feature_flag_name, *args)
       feature_flag_name.blank? || !exist?(feature_flag_name) || enabled?(feature_flag_name, *args)
