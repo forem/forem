e21534a60f1a0b79960e9aba45e3b63b9bbafb75
mstruve <mollylbs@gmail.com>
2020-06-29 16:44:40 -0500

    remove https

--- spec/support/initializers/capybara.rb
+++ spec/support/initializers/capybara.rb
@@ -10,10 +10,12 @@ Capybara.default_max_wait_time = 10
 # allow browser websites, so that "webdrivers" can access their binaries
 # see <https://github.com/titusfortner/webdrivers/wiki/Using-with-VCR-or-WebMock>
 allowed_sites = [
-  "https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver",
+  "chromedriver.storage.googleapis.com",
+  "github.com/mozilla/geckodriver/releases",
+  "selenium-release.storage.googleapis.com",
+  "developer.microsoft.com/en-us/microsoft-edge/tools/webdriver",
   "api.knapsackpro.com",
-  "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_83.0.4103",
-] + Webdrivers::Common.subclasses.map(&:base_url)
+]
 
 WebMock.disable_net_connect!(allow_localhost: true, allow: allowed_sites)
 
