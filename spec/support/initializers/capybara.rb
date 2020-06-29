b69dd2a8d3159b1f0a74785ffa0bfe5e8694f880
mstruve <mollylbs@gmail.com>
2020-06-29 15:18:04 -0500

    just base url

--- spec/support/initializers/capybara.rb
+++ spec/support/initializers/capybara.rb
@@ -12,15 +12,7 @@ RSpec.configure do |config|
   end
 
   config.before(:each, type: :system, js: true) do
-    stub_request(:get, "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_83.0.4103").
-      with(
-        headers: {
-          "Accept" => "*/*",
-          "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
-          "Host" => "chromedriver.storage.googleapis.com",
-          "User-Agent" => "Ruby"
-        },
-      ).
+    stub_request(:get, "https://chromedriver.storage.googleapis.com").
       to_return(status: 200, body: "", headers: {})
 
     if ENV["SELENIUM_URL"].present?
