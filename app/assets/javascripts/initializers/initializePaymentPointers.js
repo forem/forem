ed03fd4862f73d6a861f05124bc00e7d897d444a
Ridhwana <ridhwana.khan16@gmail.com>
2020-06-30 21:17:31 +0200

    fix: basePointer will not exist if the site config payment pointer is not present and hence cause other specs to fail :( and a console eror

--- app/assets/javascripts/initializers/initializePaymentPointers.js
+++ app/assets/javascripts/initializers/initializePaymentPointers.js
@@ -2,10 +2,10 @@ function initializePaymentPointers() {
   var userPointer = document.getElementById('author-payment-pointer');
   var basePointer = document.getElementById('base-payment-pointer');
   var meta = document.querySelector("meta[name='monetization']");
-  
+
   if (userPointer) {
     meta.content = userPointer.dataset.paymentPointer;
-  } else {
+  } else if (basePointer) {
     meta.content = basePointer.dataset.paymentPointer;;
   }
 }
