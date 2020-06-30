bcf0b6e9d74450d8131b84309976c25c2ead22c5
Nick Taylor <nick@dev.to>
2020-06-30 15:52:00 -0400

    Added the cover image upload spinner.

--- app/javascript/article-form/components/ArticleCoverImage.jsx
+++ app/javascript/article-form/components/ArticleCoverImage.jsx
@@ -1,29 +1,32 @@
-import { h, Component } from 'preact';
+import { h, Component, Fragment } from 'preact';
 import PropTypes from 'prop-types';
 import { generateMainImage } from '../actions';
 import { validateFileInputs } from '../../packs/validateFileInputs';
 import { Button } from '@crayons';
+import { Spinner } from '@crayons/Spinner/Spinner';
 
 export class ArticleCoverImage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      uploadError: false,
-      uploadErrorMessage: null,
-    };
-  }
+  state = {
+    uploadError: false,
+    uploadErrorMessage: null,
+    uploadingImage: false,
+  };
+
+  onImageUploadSuccess = (...args) => {
+    this.props.onMainImageUrlChange(...args);
+    this.setState({ uploadingImage: false });
+  };
 
   handleMainImageUpload = (e) => {
     e.preventDefault();
 
+    this.setState({ uploadingImage: true });
     this.clearUploadError();
-    const validFileInputs = validateFileInputs();
 
-    if (validFileInputs) {
+    if (validateFileInputs()) {
       const payload = { image: e.target.files, wrap_cloudinary: true };
-      const { onMainImageUrlChange } = this.props;
 
-      generateMainImage(payload, onMainImageUrlChange, this.onUploadError);
+      generateMainImage(payload, this.onImageUploadSuccess, this.onUploadError);
     }
   };
 
@@ -36,6 +39,7 @@ export class ArticleCoverImage extends Component {
 
   onUploadError = (error) => {
     this.setState({
+      uploadingImage: false,
       uploadError: true,
       uploadErrorMessage: error.message,
     });
@@ -51,11 +55,12 @@ export class ArticleCoverImage extends Component {
 
   render() {
     const { mainImage } = this.props;
-    const { uploadError, uploadErrorMessage } = this.state;
+    const { uploadError, uploadErrorMessage, uploadingImage } = this.state;
     const uploadLabel = mainImage ? 'Change' : 'Add a cover image';
+
     return (
       <div className="crayons-article-form__cover" role="presentation">
-        {mainImage && (
+        {!uploadingImage && mainImage && (
           <img
             src={mainImage}
             className="crayons-article-form__cover__image"
@@ -65,24 +70,32 @@ export class ArticleCoverImage extends Component {
           />
         )}
         <div className="flex items-center">
-          <Button variant="outlined" className="mr-2 whitespace-nowrap">
-            <label htmlFor="cover-image-input">{uploadLabel}</label>
-            <input
-              id="cover-image-input"
-              type="file"
-              onChange={this.handleMainImageUpload}
-              accept="image/*"
-              className="w-100 h-100 absolute left-0 right-0 top-0 bottom-0 overflow-hidden opacity-0 cursor-pointer"
-              data-max-file-size-mb="25"
-            />
-          </Button>
-          {mainImage && (
-            <Button
-              variant="ghost-danger"
-              onClick={this.triggerMainImageRemoval}
-            >
-              Remove
-            </Button>
+          {uploadingImage ? (
+            <span class="lh-base pl-1 border-0 py-2 inline-block">
+              <Spinner /> Uploading...
+            </span>
+          ) : (
+            <Fragment>
+              <Button variant="outlined" className="mr-2 whitespace-nowrap">
+                <label htmlFor="cover-image-input">{uploadLabel}</label>
+                <input
+                  id="cover-image-input"
+                  type="file"
+                  onChange={this.handleMainImageUpload}
+                  accept="image/*"
+                  className="w-100 h-100 absolute left-0 right-0 top-0 bottom-0 overflow-hidden opacity-0 cursor-pointer"
+                  data-max-file-size-mb="25"
+                />
+              </Button>
+              {mainImage && (
+                <Button
+                  variant="ghost-danger"
+                  onClick={this.triggerMainImageRemoval}
+                >
+                  Remove
+                </Button>
+              )}
+            </Fragment>
           )}
         </div>
         {uploadError && (
