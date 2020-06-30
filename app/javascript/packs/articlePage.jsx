3f84a78587cd6cc102f4394eec32ef3dfccdb763
benhalpern <bendhalpern@gmail.com>
2020-06-29 22:40:05 -0400

    Initial POC work for expand button

--- app/javascript/packs/articlePage.jsx
+++ app/javascript/packs/articlePage.jsx
@@ -20,6 +20,24 @@ const userDataIntervalID = setInterval(async () => {
     return;
   }
 
+  // Somewhat works. Not elegent or fully functional.
+  const codeBlocks = document.querySelectorAll('div.highlight');
+
+  codeBlocks.forEach(block => {
+    block.insertAdjacentHTML('beforeend', '<button class="crayons-btn crayons-btn--secondary crayons-btn--icon crayons-btn--s pt-0"><svg data-content="fullscreen" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="15" height="15"><path data-content="fullscreen" fill="none" d="M0 0h24v24H0z"></path><path data-content="fullscreen" d="M20 3h2v6h-2V5h-4V3h4zM4 3h4v2H4v4H2V3h2zm16 16v-4h2v6h-6v-2h4zM4 19h4v2H2v-6h2v4z"></path></svg></button>');
+    block.querySelector('button').onclick = (e) => {
+      document.body.classList.toggle('modal-open');
+      if (document.body.classList.contains('modal-open')) {
+        block.classList.add('crayons-fullscreen');
+        block.insertAdjacentHTML('beforeBegin', '<div class="crayons-fullscreenbg"></div>');
+      } else {
+        block.classList.remove('crayons-fullscreen');
+        //crayons-fullscreenbg is just a random placeholder.
+        document.querySelector('.crayons-fullscreenbg').style.display = 'none';
+      }
+    }
+  });
+  
   if (userStatus === 'logged-in' && user !== null) {
     // Load the comment subscription button for logged on users.
     clearInterval(userDataIntervalID);
