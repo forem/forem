a2ce583634b675d4e0cfe001a02505e7ec6743a6
Alex <alexandersmith223@gmail.com>
2020-07-01 15:08:54 -0400

    Cleanup spec

--- spec/models/user_subscription_spec.rb
+++ spec/models/user_subscription_spec.rb
@@ -3,7 +3,6 @@ require "rails_helper"
 RSpec.describe UserSubscription, type: :model do
   subject { build(:user_subscription) }
 
-  # TODO: (Alex Smith) - update roles before release
   let(:subscriber) { create(:user) }
   let(:source) { create(:article, :with_user_subscription_tag_role_user, with_user_subscription_tag: true) }
 
@@ -13,7 +12,7 @@ RSpec.describe UserSubscription, type: :model do
     it { is_expected.to validate_presence_of(:subscriber_id) }
     it { is_expected.to validate_presence_of(:subscriber_email) }
     it { is_expected.to validate_presence_of(:author_id) }
-    it { is_expected.to validate_inclusion_of(:user_subscription_sourceable_type).in_array(["Article"]) }
+    it { is_expected.to validate_inclusion_of(:user_subscription_sourceable_type).in_array(%w[Article]) }
     it { is_expected.to validate_uniqueness_of(:subscriber_id).scoped_to(%i[subscriber_email user_subscription_sourceable_type user_subscription_sourceable_id]) }
 
     it "validates the source is active" do
