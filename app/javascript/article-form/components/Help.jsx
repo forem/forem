afc27c0f796d43d93e254de1466f0da0d1ef1d19
ludwiczakpawel <ludwiczakpawel@gmail.com>
2020-07-07 08:27:07 +0200

    .

--- app/javascript/article-form/components/Help.jsx
+++ app/javascript/article-form/components/Help.jsx
@@ -219,7 +219,7 @@ export class Help extends Component {
                       <span class="fs-xs">```</span>
                     </td>
                     <td>
-                      <div class="highlight p-2">
+                      <div class="highlight p-2 overflow-hidden">
                         <code>code block</code>
                       </div>
                     </td>
