e1313a2bc4de5fc4839302a589f8f734daa6648a
Julianna Tetreault <juliannatetreault@gmail.com>
2020-07-07 11:47:04 -0600

    Adds Announcement and Welcome Notification models   - Adds an Announcement model in prep for polymorphism   - Adds a WelcomeNotification model in prep for polymorphism

---
+++ lib/data_update_scripts/20200708163323_backfill_columns_for_broadcasts.rb
@@ -0,0 +1,9 @@
+module DataUpdateScripts
+  class BackfillColumnsForBroadcasts
+    def run
+      # Place your data update logic here
+      # Make sure your code is idempotent and can be run safely
+      # multiple times at any time
+    end
+  end
+end
