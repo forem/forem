960754931236d3b03ac89b868d6424d6e1d21b3a
Mac Siri <krairit.siri@gmail.com>
2020-07-08 05:55:53 -0400

    Use custom UglifierCompressor to enable source-maps (#9188)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   }
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.assets.js_compressor = :uglify_with_source_maps
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
