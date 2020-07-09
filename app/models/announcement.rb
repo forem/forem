dca98fb9afa89b13f25bf1be526a790d8fb8901c
Julianna Tetreault <juliannatetreault@gmail.com>
2020-07-09 13:54:22 -0600

    Move Announcement-related code from Broadcast model to Announcement model   - Moves validations for banner_style to announcement.rb   - Removes banner_style-related code from broadcast.rb

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,3 +1,24 @@
 class Announcement < ApplicationRecord
+  VALID_BANNER_STYLES = %w[default brand success warning error].freeze
+  resourcify
+
   has_one :broadcast, as: :broadcastable
+
+  validates :banner_style, inclusion: { in: VALID_BANNER_STYLES }, allow_blank: true
+  validate  :single_active_announcement_broadcast
+
+  private
+
+  def single_active_announcement_broadcast
+    # Only add errors if we are trying to modify an announcement
+    # broadcast while another announcement broadcast is already
+    # active to ensure that only one can be active at a time.
+    active_broadcasts = Broadcast.announcement.active
+    first_broadcast = active_broadcasts.order(id: :asc).limit(1)
+    return unless active &&
+      type_of == "Announcement" &&
+      ![nil, id].include?(first_broadcast.pick(:id))
+
+    errors.add(:base, "You can only have one active announcement broadcast")
+  end
 end
--- app/models/broadcast.rb
+++ app/models/broadcast.rb
@@ -1,5 +1,4 @@
 class Broadcast < ApplicationRecord
-  VALID_BANNER_STYLES = %w[default brand success warning error].freeze
   resourcify
 
   belongs_to :broadcastable, polymorphic: true
@@ -7,10 +6,8 @@ class Broadcast < ApplicationRecord
   belongs_to :welcome_notification, class_name: "WelcomeNotification", optional: true
 
   validates :title, uniqueness: { scope: :type_of }, presence: true
-  validates :type_of, :processed_html, presence: true
+  validates :type_of, :processed_html, :broadcastable_type, :broadcastable_id, presence: true
   validates :type_of, inclusion: { in: %w[Announcement Welcome] }
-  validates :banner_style, inclusion: { in: VALID_BANNER_STYLES }, allow_blank: true
-  validate  :single_active_announcement_broadcast
 
   before_save :update_active_status_updated_at, if: :will_save_change_to_active?
 
@@ -23,19 +20,6 @@ class Broadcast < ApplicationRecord
   end
 
   private
-
-  def single_active_announcement_broadcast
-    # Only add errors if we are trying to modify an announcement
-    # broadcast while another announcement broadcast is already
-    # active to ensure that only one can be active at a time.
-    active_broadcasts = Broadcast.announcement.active
-    first_broadcast = active_broadcasts.order(id: :asc).limit(1)
-    return unless active &&
-      type_of == "Announcement" &&
-      ![nil, id].include?(first_broadcast.pick(:id))
-
-    errors.add(:base, "You can only have one active announcement broadcast")
-  end
 
   def update_active_status_updated_at
     self.active_status_updated_at = Time.current
