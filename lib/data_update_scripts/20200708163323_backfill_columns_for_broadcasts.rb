96afaf8d27ef6162d89857f588a3e268effaadac
Julianna Tetreault <juliannatetreault@gmail.com>
2020-07-09 09:16:03 -0600

    Add data_update script to backfill type_of column for Broadcasts

--- lib/data_update_scripts/20200708163323_backfill_columns_for_broadcasts.rb
+++
@@ -1,9 +0,0 @@
-module DataUpdateScripts
-  class BackfillColumnsForBroadcasts
-    def run
-      # Place your data update logic here
-      # Make sure your code is idempotent and can be run safely
-      # multiple times at any time
-    end
-  end
-end
