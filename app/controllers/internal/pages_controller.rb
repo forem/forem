ffc96db209f03c0efc02acacb4c2b42718965ab1
rhymes <rhymesete@gmail.com>
2020-07-09 16:25:44 +0200

    Rubocop: fix additional violations in todo file (#9218)

--- app/controllers/internal/pages_controller.rb
+++ app/controllers/internal/pages_controller.rb
@@ -1,41 +1,43 @@
-class Internal::PagesController < Internal::ApplicationController
-  layout "internal"
+module Internal
+  class PagesController < Internal::ApplicationController
+    layout "internal"
 
-  def index
-    @pages = Page.all
-  end
+    def index
+      @pages = Page.all
+    end
 
-  def new
-    @page = Page.new
-  end
+    def new
+      @page = Page.new
+    end
 
-  def edit
-    @page = Page.find(params[:id])
-  end
+    def edit
+      @page = Page.find(params[:id])
+    end
 
-  def update
-    @page = Page.find(params[:id])
-    @page.update!(page_params)
-    redirect_to "/internal/pages"
-  end
+    def update
+      @page = Page.find(params[:id])
+      @page.update!(page_params)
+      redirect_to "/internal/pages"
+    end
 
-  def create
-    @page = Page.new(page_params)
-    @page.save!
-    redirect_to "/internal/pages"
-  end
+    def create
+      @page = Page.new(page_params)
+      @page.save!
+      redirect_to "/internal/pages"
+    end
 
-  def destroy
-    @page = Page.find(params[:id])
-    @page.destroy
-    redirect_to "/internal/pages"
-  end
+    def destroy
+      @page = Page.find(params[:id])
+      @page.destroy
+      redirect_to "/internal/pages"
+    end
 
-  private
+    private
 
-  def page_params
-    allowed_params = %i[title slug body_markdown body_html body_json description template is_top_level_path
-                        social_image]
-    params.require(:page).permit(allowed_params)
+    def page_params
+      allowed_params = %i[title slug body_markdown body_html body_json description template is_top_level_path
+                          social_image]
+      params.require(:page).permit(allowed_params)
+    end
   end
 end
--- app/controllers/internal/permissions_controller.rb
+++ app/controllers/internal/permissions_controller.rb
@@ -1,11 +1,13 @@
-class Internal::PermissionsController < Internal::ApplicationController
-  layout "internal"
+module Internal
+  class PermissionsController < Internal::ApplicationController
+    layout "internal"
 
-  def index
-    @users = User.with_role(:admin).
-      union(User.with_role(:super_admin)).
-      union(User.with_role(:single_resource_admin, :any)).
-      page(params[:page]).
-      per(50)
+    def index
+      @users = User.with_role(:admin).
+        union(User.with_role(:super_admin)).
+        union(User.with_role(:single_resource_admin, :any)).
+        page(params[:page]).
+        per(50)
+    end
   end
 end
