9fd3009698d9c681ed597d4c1a9757f9872818a7
Julianna Tetreault <juliannatetreault@gmail.com>
2020-07-10 11:47:14 -0600

    Add unique index on broadcastable_type and broadcastable_id on the Broadcasts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_173524) do
+ActiveRecord::Schema.define(version: 2020_07_10_174257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -306,6 +306,7 @@ ActiveRecord::Schema.define(version: 2020_07_07_173524) do
     t.string "title"
     t.string "type_of"
     t.datetime "updated_at"
+    t.index ["broadcastable_type", "broadcastable_id"], name: "index_broadcasts_on_broadcastable_type_and_broadcastable_id", unique: true
     t.index ["title", "type_of"], name: "index_broadcasts_on_title_and_type_of", unique: true
   end
 
