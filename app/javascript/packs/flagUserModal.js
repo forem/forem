7b8b59ededac8537553fd51b2439ca0bef7d86b3
Arit Amana <msarit@gmail.com>
2020-07-10 12:29:38 -0400

    Revert changes

--- app/javascript/packs/flagUserModal.js
+++ app/javascript/packs/flagUserModal.js
@@ -6,55 +6,54 @@ export default function initializeFlagUserModal(
   articleId = null,
 ) {
   const flagUserModalHTML = `
-<div class="crayons-modal crayons-modal--s absolute flag-user-modal">
-  <div class="crayons-modal__box">
-    <header class="crayons-modal__box__header flag-user-modal-header">
-      <h2>Flag User</h2>
-      <button type="button" class="crayons-btn crayons-btn--icon crayons-btn--ghost modal-header-close-icon">
-        <svg width="24" height="24" viewBox="0 0 24 24" class="crayons-icon" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636l4.95 4.95z" />
-        </svg>
-      </button>
-    </header>
-    <div class="crayons-modal__box__body flag-user-modal-body">
-      <span>
-        Thanks for keeping DEV safe. Here is what you can do to flag this user:
-      </span>
-      <div class="crayons-fields">
-        <div class="crayons-field crayons-field--radio">
-          <input
-            type="radio"
-            id="vomit-all"
-            name="flag-user"
-            class="crayons-radio"
-            data-reactable-id="${articleAuthorId}"
-            data-category="vomit"
-            data-reactable-type="User">
-          <label for="vomit-all" class="crayons-field__label">
-            Make all posts by this author less visible
-            <p class="crayons-field__description">
-              This author consistently posts content that violates DEV's code of conduct because it is harassing, offensive or spammy.
-            </p>
-          </label>
+    <div class="crayons-modal crayons-modal--s absolute flag-user-modal">
+      <div class="crayons-modal__box">
+        <header class="crayons-modal__box__header flag-user-modal-header">
+          <h2>Flag User</h2>
+          <button type="button" class="crayons-btn crayons-btn--icon crayons-btn--ghost modal-header-close-icon">
+            <svg width="24" height="24" viewBox="0 0 24 24" class="crayons-icon" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636l4.95 4.95z" />
+            </svg>
+          </button>
+        </header>
+        <div class="crayons-modal__box__body flag-user-modal-body">
+          <span>
+            Thanks for keeping DEV safe. Here is what you can do to flag this user:
+          </span>
+          <div class="crayons-fields">
+            <div class="crayons-field crayons-field--radio">
+              <input
+                type="radio"
+                id="vomit-all"
+                name="flag-user"
+                class="crayons-radio"
+                data-reactable-id="${articleAuthorId}"
+                data-category="vomit"
+                data-reactable-type="User">
+              <label for="vomit-all" class="crayons-field__label">
+                Make all posts by this author less visible
+                <p class="crayons-field__description">
+                  This author consistently posts content that violates DEV's code of conduct because it is harassing, offensive or spammy.
+                </p>
+              </label>
+            </div>
+            <a href="/report-abuse?url=${
+              modCenterArticleUrl
+                ? `${document.location.origin}${modCenterArticleUrl}`
+                : document.location
+            }" class="fs-base abuse-report-link">Report other inappropriate conduct</a>
+          </div>
+          <div class="buttons-container">
+            <a href="#" class="crayons-btn crayons-btn--danger" id="confirm-flag-user-action">Confirm action</a>
+            <a href="#" class="crayons-btn crayons-btn--secondary" id="cancel-flag-user-action">Cancel</a>
+          </div>
         </div>
-        <a href="/report-abuse?url=${
-          modCenterArticleUrl
-            ? `${document.location.origin}${modCenterArticleUrl}`
-            : document.location
-        }" class="fs-base abuse-report-link">Report other inappropriate conduct</a>
       </div>
-      <div class="buttons-container">
-        <a href="#" class="crayons-btn crayons-btn--danger" id="confirm-flag-user-action">Confirm action</a>
-        <a href="#" class="crayons-btn crayons-btn--secondary" id="cancel-flag-user-action">Cancel</a>
-      </div>
+      <div class="crayons-modal__overlay"></div>
     </div>
-  </div>
-  <div class="crayons-modal__overlay"></div>
-</div>
-`;
+  `;
 
   const toggleFlagUserModal = () => {
-
     const modalContainer = document.querySelector('.flag-user-modal-container');
     modalContainer.classList.toggle('hidden');
 
@@ -72,9 +71,9 @@ export default function initializeFlagUserModal(
   const modContainer = articleId
     ? document.getElementById(`mod-iframe-${articleId}`)
     : document.getElementById('mod-container');
-    document.querySelector(
-      '.flag-user-modal-container',
-    ).innerHTML = flagUserModalHTML;
+  document.querySelector(
+    '.flag-user-modal-container',
+  ).innerHTML = flagUserModalHTML;
   if (modContainer) {
     modContainer.addEventListener('load', () => {
       modContainer.contentWindow.document
