fb892eab7d4b45b95786c4ffd2be901e2ac587b1
Julianna Tetreault <juliannatetreault@gmail.com>
2020-07-13 14:56:14 -0600

    Replaces type_of with broadcastable_type in the Broadcast model   - Adds #single_active_announcement_broadcast back to private

--- app/models/broadcast.rb
+++ app/models/broadcast.rb
@@ -5,21 +5,35 @@ class Broadcast < ApplicationRecord
   belongs_to :announcement, class_name: "Announcement", optional: true
   belongs_to :welcome_notification, class_name: "WelcomeNotification", optional: true
 
-  validates :title, uniqueness: { scope: :type_of }, presence: true
-  validates :type_of, :processed_html, :broadcastable_type, :broadcastable_id, presence: true
-  validates :type_of, inclusion: { in: %w[Announcement Welcome] }
+  validates :title, uniqueness: { scope: :broadcastable_type }, presence: true
+  validates :processed_html, :broadcastable_type, :broadcastable_id, presence: true
+  validates :broadcastable_type, inclusion: { in: %w[Announcement Welcome] }
+  validate  :single_active_announcement_broadcast
 
   before_save :update_active_status_updated_at, if: :will_save_change_to_active?
 
   scope :active, -> { where(active: true) }
-  scope :announcement, -> { where(type_of: "Announcement") }
-  scope :welcome, -> { where(type_of: "Welcome") }
+  scope :announcement, -> { where(broadcastable_type: "Announcement") }
+  scope :welcome, -> { where(broadcastable_type: "Welcome") }
 
   def get_inner_body(content)
     Nokogiri::HTML(content).at("body").inner_html
   end
 
   private
+
+  def single_active_announcement_broadcast
+    # Only add errors if we are trying to modify an announcement
+    # broadcast while another announcement broadcast is already
+    # active to ensure that only one can be active at a time.
+    active_broadcasts = Broadcast.announcement.active
+    first_broadcast = active_broadcasts.order(id: :asc).limit(1)
+    return unless active &&
+      broadcastable_type == "Announcement" &&
+      ![nil, id].include?(first_broadcast.pick(:id))
+
+    errors.add(:base, "You can only have one active announcement broadcast")
+  end
 
   def update_active_status_updated_at
     self.active_status_updated_at = Time.current
