d93306d80808f77d573186f03c85d459a19e3f42
Julianna Tetreault <juliannatetreault@gmail.com>
2020-07-13 10:37:22 -0600

    Adjust data_update script name to trigger script rerun

--- lib/data_update_scripts/20200708163323_backfill_broadcastable_for_broadcasts.rb
+++ lib/data_update_scripts/20200708163323_backfill_broadcastable_for_broadcasts.rb
@@ -1,5 +1,5 @@
 module DataUpdateScripts
-  class BackfillBroadcastableTypeForBroadcasts
+  class BackfillBroadcastableForBroadcasts
     def run
       Broadcast.where(broadcastable_type: nil).find_each do |cast|
         cast.update!(broadcastable_type: cast.type_of)
