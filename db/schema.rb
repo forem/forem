eeacc1488d598d25f567cd5b26451b61dfdbc450
Lisa Sy <lisasyis@gmail.com>
2020-07-13 16:20:44 -0700

    Update mod center layout & color fixes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_07_10_174257) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
