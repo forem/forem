b5ad25eec3eb1e3c3bfb228b76c8fa576e3289f4
benhalpern <bendhalpern@gmail.com>
2020-07-14 18:55:26 -0400

    Fix up tests

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -6,7 +6,7 @@ module Api
 
       SHOW_ATTRIBUTES_FOR_SERIALIZATION = %i[
         id username name summary twitter_username github_username website_url
-        location created_at profile_image
+        location created_at profile_image registered
       ].freeze
       private_constant :SHOW_ATTRIBUTES_FOR_SERIALIZATION
 
--- spec/requests/internal/invitations_spec.rb
+++ spec/requests/internal/invitations_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "/internal/invitations", type: :request do
-  let(:user) { create(:user, registered: false) }
+  let(:user) { create(:user) }
   let(:admin) { create(:user, :super_admin) }
 
   before do
@@ -10,6 +10,7 @@ RSpec.describe "/internal/invitations", type: :request do
 
   describe "GETS /internal/invitations" do
     it "renders to appropriate page" do
+      user.update_column(:registered, false)
       get "/internal/invitations"
       expect(response.body).to include(user.username)
     end
@@ -17,15 +18,15 @@ RSpec.describe "/internal/invitations", type: :request do
 
   describe "GETS /internal/invitations/new" do
     it "renders to appropriate page" do
-      get "/internal/invitations"
+      get "/internal/invitations/new"
       expect(response.body).to include("Email:")
     end
   end
 
   describe "POST /internal/invitations" do
     it "creates new invitation" do
-      get "/internal/invitations"
-      expect(response.body).to include("Email:")
+      post "/internal/invitations", params: {email: "hey#{rand(1000)}@email.co", name: "Roger #{1000}"}
+      expect(User.last.registered).to be false
     end
   end
 end
