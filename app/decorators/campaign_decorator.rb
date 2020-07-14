3efbed8a3c8255b7da789b583889bc01b4c63b37
Michael Kohl <citizen428@dev.to>
2020-07-14 09:45:35 +0700

    Introduce domain model for campaigns

---
+++ app/decorators/campaign_decorator.rb
@@ -0,0 +1,13 @@
+class CampaignDecorator < ApplicationDecorator
+  include ActionView::Helpers::AssetTagHelper
+  include ActionView::Helpers::UrlHelper
+
+  def sidebar_image
+    return unless active?
+
+    img = image_tag(object.sidebar_image, class: "block w-100 h-auto radius-default", width: 1000, height: 420)
+    return link_to(img, url) if url
+
+    img
+  end
+end
