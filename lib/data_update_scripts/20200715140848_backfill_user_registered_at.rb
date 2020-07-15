f3f6a7b2359c5f12d08991721dae6bf248753a96
Ben Halpern <bendhalpern@gmail.com>
2020-07-15 10:48:05 -0400

    Update lib/data_update_scripts/20200715140848_backfill_user_registered_at.rb

--- lib/data_update_scripts/20200715140848_backfill_user_registered_at.rb
+++ lib/data_update_scripts/20200715140848_backfill_user_registered_at.rb
@@ -1,7 +1,7 @@
 module DataUpdateScripts
   class BackfillUserRegisteredAt
     def run
-      User.where(registered_at: nil).find_each do |user|
+      User.where(registered_at: nil, registered: true).find_each do |user|
         user.update_column(:registered_at, user.created_at)
       end
     end
