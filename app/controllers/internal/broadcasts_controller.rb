9fb1ea9628f90082e7035f9bc002cf659b626c9d
Julianna Tetreault <juliannatetreault@gmail.com>
2020-07-16 13:16:45 -0600

    Remove all references to cta_text, cta_url, and headline from WelcomeNotifications

--- app/controllers/internal/broadcasts_controller.rb
+++ app/controllers/internal/broadcasts_controller.rb
@@ -1,6 +1,7 @@
 module Internal
   class BroadcastsController < Internal::ApplicationController
     layout "internal"
+    before_action :find_broadcastable, only: %i[create update]
 
     def index
       @broadcasts = if params[:broadcastable_type]
@@ -23,7 +24,30 @@ module Internal
     end
 
     def create
-      @broadcast = Broadcast.new(broadcast_params)
+      # broadcast = Broadcast.new(broadcast_params)
+
+      # broadcastable = case params[:broadcastable_type]
+      #                 when "WelcomeNotification"
+      #                   WelcomeNotification.create
+      #                 when "Announcement"
+      #                   Announcement.create
+      #                 end
+
+      # @broadcast = Broadcast.create(
+      #   title: broadcast.title,
+      #   processed_html: broadcast.processed_html,
+      #   active: broadcast.active,
+      #   banner_style: broadcast.banner_style,
+      #   broadcastable: broadcastable,
+      # )
+      # @broadcast = @broadcastable.broadcasts.new(broadcast_params[:broadcasts])
+      # @broadcast = broadcastable.broadcast.new(broadcast_params)
+      # @broadcast = Broadcast.create(
+      #   broadcast_params,
+      #   broadcastable,
+      # )
+      # @broadcast = broadcast_params[:broadcastable_type].constantize.create
+      @broadcast = @broadcastable.new(broadcast_params)
 
       if @broadcast.save
         flash[:success] = "Broadcast has been created!"
@@ -35,7 +59,19 @@ module Internal
     end
 
     def update
-      @broadcast = Broadcast.find(params[:id])
+      # @broadcast = Broadcast.find(params[:id])
+      # @broadcast = Broadcast.find_by(broadcast_params[:broadcastable_id][:broadcastable_type])
+      # @broadcast = @broadcastable.broadcast
+
+      # broadcastable_type = case params[:broadcastable_type]
+      #                      when "WelcomeNotification"
+      #                        WelcomeNotification.find(params[:id])
+      #                      when "Announcement"
+      #                        Announcement.find(params[:id])
+      #                      end
+
+      # @broadcast = Broadcast.find(params[broadcastable])
+      @broadcast = broadcastable_type.constantize.find_by(id: broadcastable_id)
 
       if @broadcast.update(broadcast_params)
         flash[:success] = "Broadcast has been updated!"
@@ -59,6 +95,33 @@ module Internal
     end
 
     private
+
+    def find_broadcastable
+      broadcastable_type = case params[:broadcastable_type]
+                           when "WelcomeNotification"
+                             # WelcomeNotification.create
+                             broadcast_params[:broadcastable_type].constantize.create
+                           #  byebug
+                           when "Announcement"
+                             # Announcement.create
+                             broadcast_params[:broadcastable_type].constantize.create
+                           end
+
+      # klass = broadcast_params[:broadcastable_type].constantize.create
+      # byebug
+      # @broadcastable = broadcastable_type.find(broadcastable_type.id)
+      # @broadcastable = broadcastable_type.find(broadcast_params[:id])
+      @broadcastable = broadcastable_type.where(id: broadcastable_type.id)
+    end
+
+    # def set_broadcastable
+    #   @broadcastable = case params[:broadcastable_type]
+    #                    when "WelcomeNotification"
+    #                     WelcomeNotification.find(params[:welcome_notification_id])
+    #                    when "Announcement"
+    #                     Announcement.find(params[:announcement_id])
+    #                    end
+    # end
 
     def broadcast_params
       params.permit(:title, :processed_html, :broadcastable_type, :broadcastable_id, :banner_style, :active)
--- spec/models/welcome_notification_spec.rb
+++ spec/models/welcome_notification_spec.rb
@@ -3,7 +3,4 @@ require "rails_helper"
 RSpec.describe WelcomeNotification, type: :model do
   it { is_expected.to have_many(:notifications) }
   it { is_expected.to have_one(:broadcast) }
-  it { is_expected.to validate_presence_of(:cta_text) }
-  it { is_expected.to validate_presence_of(:cta_url) }
-  it { is_expected.to validate_presence_of(:headline) }
 end
