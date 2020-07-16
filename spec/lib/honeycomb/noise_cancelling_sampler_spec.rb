faf724c85d22bfa61393191aa67136a81693ca75
Shelby Spees <shelbyspees@gmail.com>
2020-07-16 11:00:20 -0700

    Don't stub `should_sample` for the default case

--- spec/lib/honeycomb/noise_cancelling_sampler_spec.rb
+++ spec/lib/honeycomb/noise_cancelling_sampler_spec.rb
@@ -3,17 +3,16 @@ require "rails_helper"
 RSpec.describe Honeycomb::NoiseCancellingSampler do
   let(:trace_id) { "TRACE_ID" }
 
-  before { allow(described_class).to receive(:should_sample) }
-
   context "without a noisy command" do
     it "does not sample" do
       result = described_class.sample({ "request.path" => "/me", "trace.trace_id" => trace_id })
-      expect(described_class).not_to have_received(:should_sample)
       expect(result).to eq([true, 1])
     end
   end
 
   context "with a redis command" do
+    before { allow(described_class).to receive(:should_sample) }
+
     it "samples if its a NOISY_COMMANDS" do
       described_class.sample({ "redis.command" => "TIME", "trace.trace_id" => trace_id })
       expect(described_class).to have_received(:should_sample).with(100, trace_id)
@@ -41,6 +40,8 @@ RSpec.describe Honeycomb::NoiseCancellingSampler do
   end
 
   context "with a active_record SQL command" do
+    before { allow(described_class).to receive(:should_sample) }
+
     it "samples if its a NOISY_COMMANDS" do
       described_class.sample({ "sql.active_record.sql" => "COMMIT", "trace.trace_id" => trace_id })
       expect(described_class).to have_received(:should_sample).with(100, trace_id)
