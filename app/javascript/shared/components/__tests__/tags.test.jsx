42b17682889d60262eec87b3444056feacca9a1f
jacobherrington <jacobherringtondeveloper@gmail.com>
2020-07-16 21:59:03 -0500

    Increase JS test coverage for CI

--- app/javascript/shared/components/__tests__/tags.test.jsx
+++ app/javascript/shared/components/__tests__/tags.test.jsx
@@ -1,5 +1,5 @@
 import { h } from 'preact';
-import { render } from '@testing-library/preact';
+import { render, fireEvent } from '@testing-library/preact';
 import { axe } from 'jest-axe';
 import Tags from '../tags';
 
@@ -15,5 +15,28 @@ describe('<Tags />', () => {
     const results = await axe(container);
 
     expect(results).toHaveNoViolations();
+  });
+
+  describe('handleKeyDown', () => {
+    it('does not call preventDefault on used keyCode', () => {
+      const { getByTestId } = render(
+        <Tags defaultValue="defaultValue" listing />,
+      );
+
+      Event.prototype.preventDefault = jest.fn();
+
+      const tests = [
+        { key: 'a', code: '65' },
+        { key: '1', code: '49' },
+        { key: ',', code: '188' },
+        { key: 'Enter', code: '13' },
+      ];
+
+      tests.forEach((eventPayload) => {
+        fireEvent.keyDown(getByTestId('tag-input'), eventPayload);
+      });
+
+      expect(Event.prototype.preventDefault).not.toHaveBeenCalled();
+    });
   });
 });
