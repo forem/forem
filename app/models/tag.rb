ab827c93ae694da2ade20571c4296668b6bb6cc4
Michael Kohl <citizen428@dev.to>
2020-07-20 21:00:51 +0700

    Standardize ActiveRecord order clauses (#9395)
    * Change simple order clauses
    
    * Change nested order clauses

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -53,7 +53,7 @@ class Tag < ActsAsTaggableOn::Tag
   end
 
   def tag_moderator_ids
-    User.with_role(:tag_moderator, self).order("id ASC").pluck(:id)
+    User.with_role(:tag_moderator, self).order(id: :asc).pluck(:id)
   end
 
   def self.bufferized_tags
