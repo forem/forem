b19fcbbc4585f146a4e6966c14919ea1817e3fbe
Sarthak Sharma <7lovesharma7@gmail.com>
2020-07-20 17:38:31 +0530

    [deploy] 🚀 Feature: Chat channel membership manager component (#8945)
    * Feature 🚀 : Ability to delete messages in chat channels
    
    - Sending message ID to frontend
    - Deleting Message
    - Use pusher to delete message realtime
    
    * Minor Bug 🐞: Show message action only for current user
    
    - User can delete or edit their own messages
    
    * Test cases added
    
    * Bug 🐞: Update message id for receiver
    
    Message id was not sent to receiver by pusher
    
    * Refactoring🛠: Message controller refactoring
    
    * Test Cases📝 : Specs for Delete message added
    
    * Feature 🚀 : Ability to edit messages
    
    * Test Cases📝 : Specs for Edit message added
    
    * Merge conflict resolved
    
    * fix video content issue
    
    * add UI for membership management
    
    * add api to update memberahip role
    
    * add methods for manage membership
    
    * Add integration test cases
    
    * add emoji for admin
    
    * Open member profile in sidecar only
    
    * 🐞 Problem with direct channel sidecar
    
    * 🐞 Few other UI enhancements
    
    * fix mod typo
    
    * add limit upto 4 for display active memberships
    
    * fix UI issues
    
    * fix action ui for membership
    
    * fix sidebar redirection issue
    
    * fix svg buttons
    
    * add test cases
    
    * fixed broken spec
    
    * fix PR suggestions
    
    * remove not used code
    
    * fix typo
    
    * fix PR suggestion
    
    * fix typos
    
    * add invitation url expiry
    
    * fix specs
    
    * fix PR suggestions
    
    * removed unused gem
    
    * remove presenter format
    
    * handle invitation link expiry with redis
    
    * fix PR suggestions
    
    * user can view non-discoverable channel invitation link
    
    * fix typos
    
    * remove commented code
    
    * add spacing
    
    * PR suggestions
    
    * remove class from button
    
    * replace componentDidMount with commen function
    
    * remove action button for single membership
    
    * add chat message on update role
    
    * add spece between lines
    
    Co-authored-by: Narender Singh <narender2031@gmail.com>
    Co-authored-by: Fernando Valverde <fdov88@gmail.com>

---
+++ app/javascript/common-prop-types/membership-prop-type.js
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+export const defaulMembershipPropType = PropTypes.shape({
+  name: PropTypes.string.isRequired,
+  membership_id: PropTypes.number.isRequired,
+  user_id: PropTypes.number.isRequired,
+  role: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+});
