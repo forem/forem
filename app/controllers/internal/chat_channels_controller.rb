5b379478611cb57dad72ac285f5b39cb0eb85630
Veggier <47649859+Veggier@users.noreply.github.com>
2020-07-22 09:43:38 -0400

    Ability for admins to remove user from internal chat channels (#9235)
    * change ui
    
    * admins can remove users from internal chat channels
    
    * rename destroy to remove_user
    
    * change syntax
    
    * add a single user
    
    * remove a single user --debug
    
    * remove a single user --debug
    
    * remove a single user
    
    * remove a single user
    
    * remove a single user
    
    * remove a single user
    
    * add test for remove single user from internal chat_channel
    
    * test
    
    * sytax change
    
    * add delete user for channel test
    
    * add more test case for the remove user from chat channel
    
    * change to single test case

--- app/controllers/internal/chat_channels_controller.rb
+++ app/controllers/internal/chat_channels_controller.rb
@@ -23,14 +23,24 @@ module Internal
       redirect_back(fallback_location: "/internal/chat_channels")
     end
 
+    def remove_user
+      @chat_channel = ChatChannel.find(params[:id])
+      @chat_channel.remove_user(user_by_param)
+      redirect_back(fallback_location: "/internal/chat_channels")
+    end
+
     private
 
     def users_by_param
       User.where(username: chat_channel_params[:usernames_string].downcase.delete(" ").split(","))
     end
 
+    def user_by_param
+      User.find_by(username: chat_channel_params[:username_string])
+    end
+
     def chat_channel_params
-      allowed_params = %i[usernames_string channel_name]
+      allowed_params = %i[usernames_string channel_name username_string]
       params.require(:chat_channel).permit(allowed_params)
     end
   end
