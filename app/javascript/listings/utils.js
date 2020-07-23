136fd46a878e5adcb28b9e6af63f928815f1c363
ludwiczakpawel <ludwiczakpawel@gmail.com>
2020-07-23 13:47:14 +0200

    preload adjustments

--- app/javascript/listings/utils.js
+++ app/javascript/listings/utils.js
@@ -8,7 +8,7 @@ export const MATCH_LISTING = [
   'single-listing-container__inner',
   'listing-filters',
   'listings-modal-background',
-  'close-listing-modal'
+  'close-listing-modal',
 ];
 
 export function updateListings(listings) {
@@ -42,10 +42,15 @@ export function getQueryParams() {
 function resizeMasonryItem(item) {
   /* Get the grid object, its row-gap, and the size of its implicit rows */
   const grid = document.getElementsByClassName('listings-columns')[0];
-  const rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'), 10);
-  const rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'), 10);
+  const rowGap = parseInt(
+    window.getComputedStyle(grid).getPropertyValue('grid-row-gap'),
+    10,
+  );
+  const rowHeight = 0;
   const rowSpan = Math.ceil(
-    (item.querySelector('.listing-content').getBoundingClientRect().height + rowGap) / (rowHeight + rowGap),
+    (item.querySelector('.listing-content').getBoundingClientRect().height +
+      rowGap) /
+      (rowHeight + rowGap),
   );
 
   /* Set the spanning as calculated above (S) */
