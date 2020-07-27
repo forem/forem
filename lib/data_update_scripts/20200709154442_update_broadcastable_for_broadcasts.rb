5804f2c6c07eb676ad19915c61e315ccf6088094
Julianna Tetreault <juliannatetreault@gmail.com>
2020-07-27 15:18:03 -0600

    Add error logging to broadcastable data_update script

--- lib/data_update_scripts/20200709154442_update_broadcastable_for_broadcasts.rb
+++ lib/data_update_scripts/20200709154442_update_broadcastable_for_broadcasts.rb
@@ -5,7 +5,11 @@ module DataUpdateScripts
         case cast.broadcastable_type
         when "Welcome"
           broadcastable = WelcomeNotification.create
-          cast.update!(broadcastable: broadcastable)
+          result = cast.update(broadcastable: broadcastable)
+          next if result
+
+          Honeybadger.context(broadcast_id: cast.id, errors: cast.errors_as_sentence)
+          Honeybadger.nofity("Broadcast Update Failed")
         when "Announcement"
           broadcastable = Announcement.create(banner_style: cast.banner_style)
           cast.update!(broadcastable: broadcastable)
