dd6fe4cbef870987826aaadf3150155033e58144
Julianna Tetreault <juliannatetreault@gmail.com>
2020-07-27 10:42:54 -0600

    Fix schema rebase issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_20_213710) do
+ActiveRecord::Schema.define(version: 2020_07_26_215928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
