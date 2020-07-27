f9137b2abd01ef9e9b5d28977e056fd34505d6c1
Sophia <sophiali124@berkeley.edu>
2020-07-27 14:09:28 -0700

    [deploy] Update `<ImageUploader />` class component to use hooks  (#9369)

--- app/javascript/article-form/components/ImageUploader.jsx
+++ app/javascript/article-form/components/ImageUploader.jsx
@@ -1,4 +1,5 @@
-import { h, Component } from 'preact';
+import { h } from 'preact';
+import { useReducer } from 'preact/hooks';
 import { generateMainImage } from '../actions';
 import { validateFileInputs } from '../../packs/validateFileInputs';
 import { ClipboardButton } from './ClipboardButton';
@@ -35,136 +36,169 @@ const ImageIcon = () => (
 
 ImageIcon.displayName = 'ImageIcon';
 
-export class ImageUploader extends Component {
-  state = {
+function imageUploaderReducer(state, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case 'uploading_image':
+      return {
+        ...state,
+        uploadError: false,
+        uploadingErrorMessage: null,
+        uploadingImage: true,
+        insertionImageUrls: [],
+        showImageCopiedMessage: false,
+      };
+
+    case 'upload_error':
+      return {
+        ...state,
+        insertionImageUrls: [],
+        uploadError: true,
+        uploadErrorMessage: payload.errorMessage,
+        uploadingImage: false,
+      };
+
+    case 'show_copied_image_message':
+      return {
+        ...state,
+        showImageCopiedMessage: true,
+      };
+
+    case 'upload_image_success':
+      return {
+        ...state,
+        insertionImageUrls: payload.insertionImageUrls,
+        uploadingImage: false,
+      };
+
+    default:
+      return state;
+  }
+}
+
+export const ImageUploader = () => {
+  const [state, dispatch] = useReducer(imageUploaderReducer, {
     insertionImageUrls: [],
     uploadError: false,
     uploadErrorMessage: null,
     showImageCopiedMessage: false,
     uploadingImage: false,
-  };
+  });
 
-  onUploadError = (error) => {
-    this.setState({
-      insertionImageUrls: [],
-      uploadError: true,
-      uploadErrorMessage: error.message,
-      uploadingImage: false,
+  const {
+    uploadingImage,
+    showImageCopiedMessage,
+    uploadErrorMessage,
+    uploadError,
+    insertionImageUrls,
+  } = state;
+
+  let imageMarkdownInput = null;
+
+  function onUploadError(error) {
+    dispatch({
+      type: 'upload_error',
+      payload: { errorMessage: error.message },
     });
-  };
+  }
 
-  copyText = () => {
-    this.imageMarkdownInput = document.getElementById(
+  function copyText() {
+    imageMarkdownInput = document.getElementById(
       'image-markdown-copy-link-input',
     );
 
     if (isNativeAndroid()) {
-      AndroidBridge.copyToClipboard(this.imageMarkdownInput.value);
-      this.setState({ showImageCopiedMessage: true });
+      AndroidBridge.copyToClipboard(imageMarkdownInput.value);
+      dispatch({
+        type: 'show_copied_image_message',
+      });
     } else if (isClipboardSupported()) {
       navigator.clipboard
-        .writeText(this.imageMarkdownInput.value)
+        .writeText(imageMarkdownInput.value)
         .then(() => {
-          this.setState({ showImageCopiedMessage: true });
+          dispatch({
+            type: 'show_copied_image_message',
+          });
         })
         .catch((_err) => {
-          this.execCopyText();
+          execCopyText();
         });
     } else {
-      this.execCopyText();
+      execCopyText();
     }
-  };
+  }
 
-  handleInsertionImageUpload = (e) => {
+  function handleInsertionImageUpload(e) {
     const { files } = e.target;
 
     if (files.length > 0 && validateFileInputs()) {
       const payload = { image: files };
-
-      this.setState({
-        uploadError: false,
-        uploadErrorMessage: null,
-        uploadingImage: true,
-        insertionImageUrls: [],
-        showImageCopiedMessage: false,
+      dispatch({
+        type: 'uploading_image',
       });
 
-      generateMainImage(
-        payload,
-        this.handleInsertImageUploadSuccess,
-        this.onUploadError,
-      );
+      generateMainImage(payload, handleInsertImageUploadSuccess, onUploadError);
     }
-  };
+  }
 
-  handleInsertImageUploadSuccess = (response) => {
-    this.setState({
-      uploadingImage: false,
-      insertionImageUrls: response.links,
+  function handleInsertImageUploadSuccess(response) {
+    dispatch({
+      type: 'upload_image_success',
+      payload: { insertionImageUrls: response.links },
     });
-  };
+  }
 
-  execCopyText() {
-    this.imageMarkdownInput.setSelectionRange(
-      0,
-      this.imageMarkdownInput.value.length,
-    );
+  function execCopyText() {
+    imageMarkdownInput.setSelectionRange(0, imageMarkdownInput.value.length);
     document.execCommand('copy');
-    this.setState({ showImageCopiedMessage: true });
+    dispatch({
+      type: 'show_copied_image_message',
+    });
   }
 
-  render() {
-    const {
-      insertionImageUrls,
-      uploadError,
-      uploadErrorMessage,
-      showImageCopiedMessage,
-      uploadingImage,
-    } = this.state;
-
-    return (
-      <div className="flex items-center">
-        {uploadingImage ? (
-          <span class="lh-base pl-3 border-0 py-2 inline-block">
-            <Spinner /> Uploading...
-          </span>
-        ) : (
-          <Button
-            className="mr-2 fw-normal"
-            variant="ghost"
-            contentType="icon-left"
-            icon={ImageIcon}
+  return (
+    <div className="flex items-center">
+      {uploadingImage ? (
+        <span class="lh-base pl-3 border-0 py-2 inline-block">
+          <Spinner /> Uploading...
+        </span>
+      ) : (
+        <Button
+          className="mr-2 fw-normal"
+          variant="ghost"
+          contentType="icon-left"
+          icon={ImageIcon}
+          tabIndex="-1"
+        >
+          Upload image
+          <input
+            type="file"
+            id="image-upload-field"
+            onChange={handleInsertionImageUpload}
+            className="w-100 h-100 absolute left-0 right-0 top-0 bottom-0 overflow-hidden opacity-0 cursor-pointer"
+            multiple
+            accept="image/*"
+            data-max-file-size-mb="25"
             tabIndex="-1"
-          >
-            Upload image
-            <input
-              type="file"
-              id="image-upload-field"
-              onChange={this.handleInsertionImageUpload}
-              className="w-100 h-100 absolute left-0 right-0 top-0 bottom-0 overflow-hidden opacity-0 cursor-pointer"
-              multiple
-              accept="image/*"
-              data-max-file-size-mb="25"
-              tabIndex="-1"
-              aria-label="Upload an image"
-            />
-          </Button>
-        )}
-
-        {insertionImageUrls.length > 0 && (
-          <ClipboardButton
-            onCopy={this.copyText}
-            imageUrls={insertionImageUrls}
-            showCopyMessage={showImageCopiedMessage}
+            aria-label="Upload an image"
           />
-        )}
+        </Button>
+      )}
 
-        {uploadError && (
-          <span className="color-accent-danger">{uploadErrorMessage}</span>
-        )}
-      </div>
-    );
-  }
-}
+      {insertionImageUrls.length > 0 && (
+        <ClipboardButton
+          onCopy={copyText}
+          imageUrls={insertionImageUrls}
+          showCopyMessage={showImageCopiedMessage}
+        />
+      )}
+
+      {uploadError && (
+        <span className="color-accent-danger">{uploadErrorMessage}</span>
+      )}
+    </div>
+  );
+};
 
 ImageUploader.displayName = 'ImageUploader';
