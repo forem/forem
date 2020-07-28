ec34e68acf0e431286bea9340c07be6bc1c26e2f
benhalpern <bendhalpern@gmail.com>
2020-07-27 22:29:08 -0400

    Move require to within module

--- lib/carrierwave_monkeypatch.rb
+++ lib/carrierwave_monkeypatch.rb
@@ -1,10 +1,8 @@
-require "fog"
-
 # @forem/systems Force "public_url" even when fog_public is false via this monkeypatch
 # Because we still want the "public" version path in all current scenarios.
 
-
 if Rails.env.production?
+  require "fog"
   module CarrierWave
     module Storage
       class Fog < Abstract
