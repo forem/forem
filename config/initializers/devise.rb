f06b85fd2c6e58cd3a72484dfe4ceb0da6caa182
Michael Kohl <citizen428@dev.to>
2020-07-29 08:54:23 +0700

    Increase limit for login attempts

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -220,7 +220,7 @@ Devise.setup do |config|
 
   # Number of authentication tries before locking an account if lock_strategy
   # is failed attempts.
-  config.maximum_attempts = 3
+  config.maximum_attempts = 5
 
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
   config.unlock_in = 1.hour
