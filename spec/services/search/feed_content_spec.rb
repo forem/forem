6ac637b9ef4c74e09201065abe662e2d1e9e8952
Molly Struve <mollylbs@gmail.com>
2020-07-30 08:24:11 -0500

    [deploy] Boost Scores For Adjacent Words (#9562)
    * Feature:Boost Scores For Adjacent Words
    
    * using equal for spec array to ensure order

--- spec/services/search/feed_content_spec.rb
+++ spec/services/search/feed_content_spec.rb
@@ -88,6 +88,18 @@ RSpec.describe Search::FeedContent, type: :service do
         doc_ids = feed_docs.map { |t| t.dig("id") }
         expect(doc_ids).to include(article1.id, article2.id)
       end
+
+      it "bumps scores for words closer together" do
+        allow(article1).to receive(:body_text).and_return("Ruby I dont know maybe is cool")
+        allow(article2).to receive(:body_text).and_return("Ruby is cool I dont know maybe")
+        index_documents([article1, article2])
+        query_params = { size: 5, search_fields: "ruby is cool" }
+
+        feed_docs = described_class.search_documents(params: query_params)
+        expect(feed_docs.count).to eq(2)
+        doc_ids = feed_docs.map { |t| t.dig("id") }
+        expect(doc_ids).to eq([article2.id, article1.id])
+      end
     end
 
     context "with a filter term" do
