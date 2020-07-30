4b1f6efe6c73949783102ee1b4f810e768f7bffd
Arit Amana <msarit@gmail.com>
2020-07-30 09:35:49 -0400

    Add new route

--- config/routes.rb
+++ config/routes.rb
@@ -186,6 +186,7 @@ Rails.application.routes.draw do
   resources :comments, only: %i[create update destroy] do
     patch "/hide", to: "comments#hide"
     patch "/unhide", to: "comments#unhide"
+    patch "/admin_delete", to: "comments#admin_delete"
     collection do
       post "/moderator_create", to: "comments#moderator_create"
     end
