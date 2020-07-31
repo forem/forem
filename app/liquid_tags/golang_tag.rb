38fc19d2408c73a18ca93effdfbd444a8817c535
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-07-30 22:23:39 -0400

    Add URL parsing

--- app/liquid_tags/golang_tag.rb
+++ app/liquid_tags/golang_tag.rb
@@ -1,10 +1,10 @@
 class GolangTag < LiquidTagBase
   PARTIAL = "liquids/golang".freeze
+  URL_REGEXP = %r{(http|https)://play.golang.com/p/[a-zA-Z0-9\-/]*}.freeze
 
   def initialize(_tag_name, link, _parse_context)
     super
-    stripped_link = ActionController::Base.helpers.strip_tags(link)
-    @embedded_url = stripped_link.split(" ").first
+    @link = parse_link(link)
   end
 
   def render(_context)
@@ -14,6 +14,24 @@ class GolangTag < LiquidTagBase
         url: @embedded_url
       },
     )
+  end
+
+  private
+
+  def parse_link(link)
+    stripped_link = ActionController::Base.helpers.strip_tags(link)
+    the_link = stripped_link.split(" ").first
+    raise_error unless valid_link?(the_link)
+    the_link
+  end
+
+  def valid_link?(link)
+    link_no_space = link.delete(" ")
+    (link_no_space =~ URL_REGEXP)&.zero?
+  end
+
+  def raise_error
+    raise StandardError, "Invalid Golang Playground URL"
   end
 end
 
