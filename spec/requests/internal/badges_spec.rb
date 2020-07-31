b9613f01bbebc0828322fa3688abfd40edb81176
benhalpern <bendhalpern@gmail.com>
2020-07-31 12:04:20 -0400

    Award badges asynchronously in internal

--- spec/requests/internal/badges_spec.rb
+++ spec/requests/internal/badges_spec.rb
@@ -17,32 +17,33 @@ RSpec.describe "/internal/badges", type: :request do
     end
 
     it "awards badges" do
-      expect do
-        post internal_badges_award_badges_path, params: {
-          badge: badge.slug,
-          usernames: "#{user.username}, #{user2.username}",
-          message_markdown: "Hinder me? Thou fool. No living man may hinder me!"
-        }
-      end.to change { user.badges.count }.by(1).and change { user2.badges.count }.by(1)
+      allow(BadgeAchievements::BadgeAwardWorker).to receive(:perform_async)
+      post internal_badges_award_badges_path, params: {
+        badge: badge.slug,
+        usernames: "#{user.username}, #{user2.username}",
+        message_markdown: "Hinder me? Thou fool. No living man may hinder me!"
+      }
+      expect(BadgeAchievements::BadgeAwardWorker).to have_received(:perform_async).with(badge.slug, "#{user.username}, #{user2.username}", "Hinder me? Thou fool. No living man may hinder me!")
+      expect(request.flash[:success]).to include("Badges are being rewarded. The task will finish shortly.")
     end
 
     it "awards badges without a message" do
-      expect do
-        post internal_badges_award_badges_path, params: {
-          badge: badge.slug,
-          usernames: "#{user.username}, #{user2.username}",
-          message_markdown: ""
-        }
-      end.to change { user.badges.count }.by(1).and change { user2.badges.count }.by(1)
+      allow(BadgeAchievements::BadgeAwardWorker).to receive(:perform_async)
+      post internal_badges_award_badges_path, params: {
+        badge: badge.slug,
+        usernames: "#{user.username}, #{user2.username}",
+        message_markdown: ""
+      }
+      expect(BadgeAchievements::BadgeAwardWorker).to have_received(:perform_async).with(badge.slug, "#{user.username}, #{user2.username}", "")
+      expect(request.flash[:success]).to include("Badges are being rewarded. The task will finish shortly.")
     end
 
     it "does not award a badge and raises an error if a badge is not specified" do
-      expect do
-        post internal_badges_award_badges_path, params: {
-          usernames: "#{user.username}, #{user2.username}",
-          message_markdown: ""
-        }
-      end.to change { user.badges.count }.by(0)
+      post internal_badges_award_badges_path, params: {
+        usernames: "#{user.username}, #{user2.username}",
+        message_markdown: ""
+      }
+      expect(BadgeAchievements::BadgeAwardWorker).not_to have_received(:perform_async).with(badge.slug, "#{user.username}, #{user2.username}", "")
     end
   end
 end
