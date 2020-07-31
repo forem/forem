6edb476acdaaf1bb67b8640fc892b93d3ab5d0c2
Michael Kohl <citizen428@dev.to>
2020-07-31 20:59:48 +0700

    [deploy] Remove UserOptionalField (#9579)
    * Remove UserOptionalField
    
    * Remove optional fields from user and users factory
    
    * Remove optional fields from user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,6 @@ end
 RSpec.describe User, type: :model do
   let(:user) { create(:user) }
   let(:other_user) { create(:user) }
-  let(:user_with_user_optional_fields) { create(:user, :with_user_optional_fields) }
   let(:org) { create(:organization) }
 
   before { omniauth_mock_providers_payload }
@@ -132,13 +131,6 @@ RSpec.describe User, type: :model do
           .dependent(:delete_all)
       end
       # rubocop:enable RSpec/NamedSubject
-
-      it "has at most three optional fields" do
-        expect(user_with_user_optional_fields).to have_many(:user_optional_fields).dependent(:destroy)
-        fourth_field = user_with_user_optional_fields.user_optional_fields.create(label: "some field",
-                                                                                  value: "some value")
-        expect(fourth_field).not_to be_valid
-      end
 
       it { is_expected.not_to allow_value("#xyz").for(:bg_color_hex) }
       it { is_expected.not_to allow_value("#xyz").for(:text_color_hex) }
