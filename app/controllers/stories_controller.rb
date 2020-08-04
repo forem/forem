5b979c37bb61b82893ab764016c4ea5c8070b1c6
Josh Puetz <joshpuetz@gmail.com>
2020-08-04 13:16:17 -0500

    Correct some lingering `/admin` paths (#9630) [deploy]

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -245,7 +245,7 @@ class StoriesController < ApplicationController
 
   def redirect_if_view_param
     redirect_to "/internal/users/#{@user.id}" if params[:view] == "moderate"
-    redirect_to "/admin/users/#{@user.id}/edit" if params[:view] == "admin"
+    redirect_to "/resource_admin/users/#{@user.id}/edit" if params[:view] == "admin"
   end
 
   def redirect_if_show_view_param
