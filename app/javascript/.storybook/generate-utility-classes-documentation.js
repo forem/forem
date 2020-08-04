deac99e4752de9f31ce850941bb127d020111f17
Nick Taylor <nick@dev.to>
2020-08-04 00:07:11 -0400

    Generating color documentation.

---
+++ app/javascript/.storybook/generate-utility-classes-documentation.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const fs = require('fs');
+const file = fs.promises;
+const stylesheetsDirectory = path.resolve(
+  __dirname,
+  '../../assets/stylesheets',
+);
+
+const themeFiles = [
+  { theme: 'default', themeFile: 'config/_colors.scss' },
+  { theme: 'hacker', themeFile: 'themes/hacker.scss' },
+  { theme: 'minimal', themeFile: 'themes/minimal.scss' },
+  { theme: 'night', themeFile: 'themes/night.scss' },
+  { theme: 'pink', themeFile: 'themes/pink.scss' },
+];
+
+(async () => {
+  const generatedStoriesFolder = path.join(
+    __dirname,
+    '../../javascript/generated_stories/__stories__',
+  );
+
+  if (!fs.existsSync(generatedStoriesFolder)) {
+    fs.mkdirSync(generatedStoriesFolder, { recursive: true });
+  }
+
+  try {
+    const storybookContent = [];
+    storybookContent.push(`import { h } from 'preact';
+
+  import '../../crayons/storybook-utilities/designSystem.scss';
+
+  export default {
+    title: '2_Base/Color',
+  };`);
+
+    for (const { theme, themeFile } of themeFiles) {
+      const colorFile = path.join(stylesheetsDirectory, themeFile);
+      const colorFileContents = await file.readFile(colorFile);
+
+      storybookContent.push(`
+  export const ${theme}ThemeColors = () => <div class="container">
+    <code><pre>{\`${colorFileContents}\`}</pre></code>
+  </div>
+
+  ${theme}ThemeColors.story = { name: '${theme} theme colors' };
+  `);
+    }
+
+    await file.writeFile(
+      path.join(generatedStoriesFolder, `colors.stories.jsx`),
+      storybookContent.join(''),
+    );
+  } catch (error) {
+    console.error(error);
+  }
+})();
