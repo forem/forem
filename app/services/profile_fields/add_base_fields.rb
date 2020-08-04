b34f10aef5ca350ce112a9bf6dc71141ca434256
Michael Kohl <citizen428@dev.to>
2020-08-04 12:13:03 +0700

    Add email checkbox to base fields

--- app/services/profile_fields/add_base_fields.rb
+++ app/services/profile_fields/add_base_fields.rb
@@ -2,6 +2,7 @@ module ProfileFields
   class AddBaseFields
     include FieldDefinition
 
+    field "Display email on profile", :check_box
     field "Name", :text_field, placeholder: "John Doe"
     field "Website URL", :text_field, placeholder: "https://yoursite.com"
     field "Summary", :text_area, placeholder: "A short bio..."
