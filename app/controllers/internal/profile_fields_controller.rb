5603b5879b671a7c3613b72aaffbdcbdf65def31
Ridhwana <ridhwana.khan16@gmail.com>
2020-08-04 14:28:14 +0200

    feat: add a create and destroy route

--- app/controllers/internal/profile_fields_controller.rb
+++ app/controllers/internal/profile_fields_controller.rb
@@ -12,10 +12,22 @@ module Internal
       redirect_to "/internal/profile_fields"
     end
 
+    def create
+      @profile_field = ProfileField.new(profile_field_params)
+      @profile_field.save!
+      redirect_to "/internal/profile_fields"
+    end
+
+    def destroy
+      @profile_field = ProfileField.find(params[:id])
+      @profile_field.destroy
+      redirect_to "/internal/profile_fields"
+    end
+
     private
 
     def profile_field_params
-      allowed_params = %i[input_type label active placeholder_text]
+      allowed_params = %i[input_type label active placeholder_text explanation]
       params.require(:profile_field).permit(allowed_params)
     end
   end
