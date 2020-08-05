09bbdeffca68e5a4674b20a32795890af49e0203
Michael Kohl <citizen428@dev.to>
2020-08-05 11:03:15 +0700

    Simplify ProfileFields::ImportFromCsv

--- app/services/profile_fields/import_from_csv.rb
+++ app/services/profile_fields/import_from_csv.rb
@@ -3,16 +3,6 @@ module ProfileFields
     HEADERS = %i[label input_type placeholder_text description].freeze
 
     def self.call(file)
-      new(file).import
-    end
-
-    attr_reader :file
-
-    def initialize(file)
-      @file = file
-    end
-
-    def import
       CSV.foreach(file, headers: HEADERS, skip_blanks: true) do |row|
         ProfileField.create(row.to_h)
       end
