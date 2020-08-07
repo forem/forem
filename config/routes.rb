600fd40cca139162bfce487cb16900fe8550b231
Nick Taylor <nick@dev.to>
2020-08-07 10:16:36 -0400

    Updated docs.dev.to to docs.forem.com

--- config/routes.rb
+++ config/routes.rb
@@ -348,7 +348,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root
   get "/robots.:format" => "pages#robots"
-  get "/api", to: redirect("https://docs.dev.to/api")
+  get "/api", to: redirect("https://docs.forem.com/api")
   get "/privacy" => "pages#privacy"
   get "/terms" => "pages#terms"
   get "/contact" => "pages#contact"
