8a90452dc6d99b9a6a8f0151f764764d8199bed2
rhymes <rhymes@hey.com>
2020-08-07 11:56:08 +0200

    Delete with SQL

--- lib/data_update_scripts/20200805102636_remove_orphaned_ahoy_rows.rb
+++ lib/data_update_scripts/20200805102636_remove_orphaned_ahoy_rows.rb
@@ -1,16 +1,32 @@
 module DataUpdateScripts
   class RemoveOrphanedAhoyRows
     def run
-      user_ids = User.ids
+      # Delete all Ahoy::Events belonging to users that don't exist anymore
+      ActiveRecord::Base.connection.execute(
+        <<~SQL,
+          DELETE FROM ahoy_events
+          WHERE user_id IS NOT NULL
+          AND user_id NOT IN (SELECT id FROM users);
+        SQL
+      )
 
-      # Delete all Ahoy::Events belonging to a user that does not exist anymore
-      Ahoy::Event.where.not(user_id: nil).where.not(user_id: user_ids).find_each(&:destroy)
+      # Delete all Ahoy::Messages belonging to users that don't exist anymore
+      ActiveRecord::Base.connection.execute(
+        <<~SQL,
+          DELETE FROM ahoy_messages
+          WHERE user_id IS NOT NULL
+          AND user_id NOT IN (SELECT id FROM users);
+        SQL
+      )
 
-      # Delete all Ahoy::Messages belonging to a user that does not exist anymore
-      Ahoy::Message.where.not(user_id: nil).where.not(user_id: user_ids).find_each(&:destroy)
-
-      # Delete all Ahoy::Visits belonging to a user that does not exist anymore
-      Ahoy::Visit.where.not(user_id: nil).where.not(user_id: user_ids).find_each(&:destroy)
+      # Delete all Ahoy::Visits belonging to users that don't exist anymore
+      ActiveRecord::Base.connection.execute(
+        <<~SQL,
+          DELETE FROM ahoy_visits
+          WHERE user_id IS NOT NULL
+          AND user_id NOT IN (SELECT id FROM users);
+        SQL
+      )
     end
   end
 end
