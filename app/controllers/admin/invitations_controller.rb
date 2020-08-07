1c566e0ec44a5ca15704fd3809795d5f66b02c7f
Josh Puetz <joshpuetz@gmail.com>
2020-08-07 09:36:26 -0500

    [deploy] Move `/internal` to `/admin (#9639)
    * First draft - all the big changes
    
    * Changing some more references to 'internal'
    
    * Relocate internal request tests to admin
    
    * Relocate internal system tests to admin
    
    * Fix trailing space
    
    * Test fix
    
    * Move queries from internal to admin
    
    * Docs updates
    
    * Rename internal stimuls controllers to admin (plus docs)
    
    * Rename admin layout
    
    * Fix routing after rebase
    
    * Fixes for latest added admin interfaces
    
    * Serviceworker ignore paths

---
+++ app/controllers/admin/invitations_controller.rb
@@ -0,0 +1,23 @@
+module Admin
+  class InvitationsController < Admin::ApplicationController
+    layout "admin"
+
+    def index
+      @invitations = User.where(registered: false).page(params[:page]).per(50)
+    end
+
+    def new; end
+
+    def create
+      email = params.dig(:user, :email)
+      name = params.dig(:user, :name)
+      username = "#{name.downcase.tr(' ', '_').gsub(/[^0-9a-z ]/i, '')}_#{rand(1000)}"
+      User.invite!(email: email,
+                   name: name,
+                   username: username,
+                   remote_profile_image_url: Users::ProfileImageGenerator.call,
+                   registered: false)
+      redirect_to admin_invitations_path
+    end
+  end
+end
--- spec/requests/internal/pages_spec.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-require "requests/shared_examples/internal_policy_dependant_request"
-
-RSpec.describe "/internal/pages", type: :request do
-  it_behaves_like "an InternalPolicy dependant request", Page do
-    let(:request) { get "/internal/pages" }
-  end
-end
