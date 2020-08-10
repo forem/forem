cc8bfcb5c07050b4642c749b381923b130693fa8
rhymes <rhymes@hey.com>
2020-08-10 16:57:12 +0200

    Enable Rubocop 0.89 cops and fix Lint/ violations (#9709)
    * Enable new Lint/ cops and run rubocop -a
    
    * Fixing last remaining things with rubocop -a
    
    * Enable and fix Style/ExplicitBlockArgument and Style/GlobalStdStream
    
    * Forgot parenthesis

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -109,7 +109,7 @@ Rails.application.configure do
   config.log_level = :debug
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger           = ActiveSupport::Logger.new($stdout)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
