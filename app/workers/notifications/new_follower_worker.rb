be72dfc7919e0fc33eb5cc722ce4ed1a6d1b5cea
rhymes <rhymes@hey.com>
2020-08-10 11:46:05 +0200

    Keep regular parameters for workers

--- app/workers/notifications/new_follower_worker.rb
+++ app/workers/notifications/new_follower_worker.rb
@@ -4,7 +4,7 @@ module Notifications
 
     sidekiq_options queue: :medium_priority, retry: 10
 
-    def perform(follow_data, is_read: false)
+    def perform(follow_data, is_read = false) # rubocop:disable Style/OptionalBooleanParameter
       Notifications::NewFollower::Send.call(follow_data, is_read: is_read)
     end
   end
