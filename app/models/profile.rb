3c940a27b9fb01b4b2bde693ec2fe4f88aad47ca
Michael Kohl <citizen428@dev.to>
2020-08-10 11:53:42 +0700

    Add specs

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,14 @@
 class Profile < ApplicationRecord
   belongs_to :user
 
-  # Automatically generate typed accessors for all active profile fields
-  ProfileField.active.find_each do |field|
-    store_attribute :data, field.attribute_name, field.type
+  # This method generates typed accessors for all active profile fields
+  def self.define_store_accessors!
+    ProfileField.active.find_each do |field|
+      store_attribute :data, field.attribute_name, field.type
+    end
   end
+
+  define_store_accessors!
+
+  validates :data, presence: true
 end
