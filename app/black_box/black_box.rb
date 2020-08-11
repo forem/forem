d844cf0ac1a2d55a7462a334094eb22285fcf413
benhalpern <bendhalpern@gmail.com>
2020-08-11 08:12:46 -0400

    Create option for calculating score without external black box

--- app/black_box/black_box.rb
+++ app/black_box/black_box.rb
@@ -16,10 +16,7 @@ class BlackBox
         reaction_points = (reaction_points * 0.8).to_i # watercooler posts shouldn't get as much love in feed
       end
 
-      article_hotness = function_caller.call(
-        "blackbox-production-articleHotness",
-        { article: article, user: article.user }.to_json,
-      ).to_i
+      article_hotness = last_mile_hotness_calc(function_caller)
 
       (
         article_hotness + reaction_points + recency_bonus + super_recent_bonus +
@@ -38,6 +35,7 @@ class BlackBox
     def calculate_spaminess(story, function_caller = FunctionCaller)
       # accepts comment or article as story
       return 100 unless story.user
+      return 100 unless ENV["AWS_SDK_KEY"].present? # Skip this if we don't have a private spam score for now
 
       function_caller.call("blackbox-production-spamScore",
                            { story: story, user: story.user }.to_json).to_i
@@ -49,6 +47,22 @@ class BlackBox
       size_bonus = body_markdown.size > 200 ? 2 : 0
       code_bonus = body_markdown.include?("`") ? 1 : 0
       size_bonus + code_bonus
+    end
+
+    def last_mile_hotness_calc(function_caller)
+      if ENV["AWS_SDK_KEY"].present? && ENV["AWS_SDK_KEY"] != "foobarbaz"
+        function_caller.call(
+          "blackbox-production-articleHotness",
+          { article: article, user: article.user }.to_json,
+        ).to_i
+      else
+        # Simple calculation that takes in published at time and scores
+        # Same order of magnitude calculation as existing private function
+        # Gives credit to new articles and articles which score well from users and mods
+        article.published_at.to_i/10000 +
+        (article.score * 5000) +
+        (article.comment_score * 5000)
+      end
     end
   end
 end
