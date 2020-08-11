93e442a3e9264197d930e39dffad1b4ad9c4dd40
Ben Halpern <bendhalpern@gmail.com>
2020-08-11 15:31:57 -0400

    Create option for calculating score without external black box (#9735)
    * Create option for calculating score without external black box
    
    * Create option for calculating score without external black box
    
    * Fix article not present error
    
    * Modify tests
    
    * Fix tests
    
    * Fix stubbing in tests
    
    * Fix function caller contingency

--- app/black_box/black_box.rb
+++ app/black_box/black_box.rb
@@ -16,10 +16,7 @@ class BlackBox
         reaction_points = (reaction_points * 0.8).to_i # watercooler posts shouldn't get as much love in feed
       end
 
-      article_hotness = function_caller.call(
-        "blackbox-production-articleHotness",
-        { article: article, user: article.user }.to_json,
-      ).to_i
+      article_hotness = last_mile_hotness_calc(article, function_caller)
 
       (
         article_hotness + reaction_points + recency_bonus + super_recent_bonus +
@@ -38,6 +35,8 @@ class BlackBox
     def calculate_spaminess(story, function_caller = FunctionCaller)
       # accepts comment or article as story
       return 100 unless story.user
+      return 0 if ENV["AWS_SDK_KEY"].blank? # Skip this if we don't have a private spam score for now
+      return 0 if ENV["AWS_SDK_KEY"] == "foobarbaz" # Also skip if placeholder
 
       function_caller.call("blackbox-production-spamScore",
                            { story: story, user: story.user }.to_json).to_i
@@ -49,6 +48,22 @@ class BlackBox
       size_bonus = body_markdown.size > 200 ? 2 : 0
       code_bonus = body_markdown.include?("`") ? 1 : 0
       size_bonus + code_bonus
+    end
+
+    def last_mile_hotness_calc(article, function_caller)
+      if ENV["AWS_SDK_KEY"].present? && ENV["AWS_SDK_KEY"] != "foobarbaz"
+        function_caller.call(
+          "blackbox-production-articleHotness",
+          { article: article, user: article.user }.to_json,
+        ).to_i
+      else
+        # Simple calculation that takes in published at time and scores
+        # Same order of magnitude calculation as existing private function
+        # Gives credit to new articles and articles which score well from users and mods
+        article.published_at.to_i / 10_000 +
+          (article.score * 5000) +
+          (article.comment_score * 5000)
+      end
     end
   end
 end
