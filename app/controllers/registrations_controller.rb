afa996c263cb475423314e25cf228ce98e75f6db
benhalpern <bendhalpern@gmail.com>
2020-08-12 09:03:53 -0400

    Editor version

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,7 +11,8 @@ class RegistrationsController < Devise::RegistrationsController
 
   def create
     build_resource(sign_up_params)
-
+    resource.saw_onboarding = false
+    resource.editor_version = "v2"
     resource.save
     yield resource if block_given?
     if resource.persisted?
