50ebad84c0ff480f30e49ef7804c2b5600869721
Julianna Tetreault <juliannatetreault@gmail.com>
2020-08-13 09:42:59 -0600

    Adds functions to update valid status in reaction_controller.js

--- app/javascript/admin/controllers/reaction_controller.js
+++ app/javascript/admin/controllers/reaction_controller.js
@@ -2,7 +2,7 @@
 import { Controller } from 'stimulus';
 
 export default class ReactionController extends Controller {
-  static targets = ['invalid', 'confirmed'];
+  static targets = ['invalid', 'confirmed', 'valid'];
 
   // eslint-disable-next-line class-methods-use-this
   /* eslint no-alert: "error" */
@@ -41,6 +41,10 @@ export default class ReactionController extends Controller {
     this.updateReaction(this.reactionId, this.invalidStatus);
   }
 
+  updateReactionValid() {
+    this.updateReaction(this.reactionId, this.validStatus);
+  }
+
   reactableUserCheck() {
     if (this.reactableType === 'user') {
       // eslint-disable-next-line no-restricted-globals
@@ -66,5 +70,9 @@ export default class ReactionController extends Controller {
 
   get invalidStatus() {
     return this.invalidTarget.dataset.altstatus;
+  }
+
+  get validStatus() {
+    return this.validTarget.dataset.altstatus;
   }
 }
