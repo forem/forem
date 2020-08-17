336aee9dc429bddc6a223f8ed0b07b59e98c19ec
mstruve <mollylbs@gmail.com>
2020-08-17 17:28:40 -0500

    Refactor:Move Yearly Badge Awarder to Sidekiq Cron

--- app/workers/badge_achievements/badge_award_worker.rb
+++ app/workers/badge_achievements/badge_award_worker.rb
@@ -5,7 +5,11 @@ module BadgeAchievements
     sidekiq_options queue: :high_priority, retry: 10
 
     def perform(usernames, badge_slug, message)
-      BadgeRewarder.award_badges(usernames, badge_slug, message)
+      if BadgeRewarder.respond_to?(badge_slug)
+        BadgeRewarder.public_send(badge_slug)
+      else
+        BadgeRewarder.award_badges(usernames, badge_slug, message)
+      end
     end
   end
 end
