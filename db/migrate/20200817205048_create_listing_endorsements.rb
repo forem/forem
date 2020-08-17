d3aa55e45d21e66876a2f12b10c1f73e590644cf
Oghenebrume50 <raphlbrume@gmail.com>
2020-08-17 22:58:09 +0100

    create the classified_listing_endorsements table

--- db/migrate/20200817205048_create_listing_endorsements.rb
+++ db/migrate/20200817205048_create_listing_endorsements.rb
@@ -1,7 +1,10 @@
 class CreateListingEndorsements < ActiveRecord::Migration[6.0]
   def change
-    create_table :listing_endorsements do |t|
-
+    create_table :classified_listing_endorsements do |t|
+      t.string :content
+      t.boolean :approved, default: false
+      t.references :classified_listing, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
