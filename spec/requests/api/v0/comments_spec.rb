f60b6a18a3bf14ea08e08d2694a1fc8902ed6958
justin <justinkrasner98@gmail.com>
2020-08-17 11:40:19 -0400

    added test to ensure a date is returned for comment

--- spec/requests/api/v0/comments_spec.rb
+++ spec/requests/api/v0/comments_spec.rb
@@ -97,6 +97,13 @@ RSpec.describe "Api::V0::Comments", type: :request do
       expect(response.headers["surrogate-key"].split.to_set).to eq(expected_key)
     end
 
+    it "returns date created" do
+      get api_comments_path(a_id: article.id)
+      expect(find_root_comment(response)).to include(
+        "created_at" => article.created_at.utc.iso8601
+      )
+    end
+    
     context "when a comment is deleted" do
       before do
         child_comment.update(deleted: true)
