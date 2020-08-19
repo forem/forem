3b6bb1aaad978129ac22a52acfbd19882efa8352
Mac Siri <krairit.siri@gmail.com>
2020-08-19 17:06:39 -0400

    Hmm

--- spec/requests/articles/articles_show_spec.rb
+++ spec/requests/articles/articles_show_spec.rb
@@ -40,7 +40,8 @@ RSpec.describe "ArticlesShow", type: :request do
           "logo" => {
             "@context" => "http://schema.org",
             "@type" => "ImageObject",
-            "url" => ApplicationController.helpers.cloudinary(SiteConfig.logo_png, 192, 80, "png"),
+            "url" => ApplicationController.helpers.optimized_image_url(SiteConfig.logo_png, width: 192,
+                                                                                            fetch_format: "png"),
             "width" => "192",
             "height" => "192"
           }
