a28cbca829adf9758d78d4b3deb4114afcaad197
Mac Siri <krairit.siri@gmail.com>
2020-08-19 13:55:39 -0400

    Use ApplicationController for liquid render (#9867)

--- app/liquid_tags/soundcloud_tag.rb
+++ app/liquid_tags/soundcloud_tag.rb
@@ -7,7 +7,7 @@ class SoundcloudTag < LiquidTagBase
   end
 
   def render(_context)
-    ActionController::Base.new.render_to_string(
+    ApplicationController.render(
       partial: PARTIAL,
       locals: {
         link: @link,
