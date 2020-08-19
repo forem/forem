5eda086e782ef65d5fc14811de57a0efa90909ba
Mac Siri <krairit.siri@gmail.com>
2020-08-19 13:55:39 -0400

    Use ApplicationController for liquid render (#9867)

--- app/liquid_tags/vimeo_tag.rb
+++ app/liquid_tags/vimeo_tag.rb
@@ -9,7 +9,7 @@ class VimeoTag < LiquidTagBase
   end
 
   def render(_context)
-    ActionController::Base.new.render_to_string(
+    ApplicationController.render(
       partial: PARTIAL,
       locals: {
         id: @id,
