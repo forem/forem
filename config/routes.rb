58f504426541fdfcc6bce33b3a1a445efd09a2d8
Julianna Tetreault <juliannatetreault@gmail.com>
2020-08-20 09:02:21 -0600

    Adds badge_achievements to Admin menu items and add comment to routes.rb   -Add badge_achievements to the Admin nav bar   -Add a comment regarding redirects for badge_achievements

--- config/routes.rb
+++ config/routes.rb
@@ -117,6 +117,8 @@ Rails.application.routes.draw do
     resources :webhook_endpoints, only: :index
     resource :config
     resources :badges, only: %i[index edit update new create]
+    # These redirects serve as a safegaurd to prevent 404s for any Admins
+    # who have the old badge_achievement URLs bookmarked.
     get "/badges/badge_achievements", to: redirect("/admin/badge_achievements")
     get "/badges/badge_achievements/award_badges", to: redirect("/admin/badge_achievements/award_badges")
     resources :badge_achievements, only: %i[index destroy]
