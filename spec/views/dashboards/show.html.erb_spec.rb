9e35c6bed6bff7e59eb52882891c2be04738807e
Mac Siri <krairit.siri@gmail.com>
2020-08-20 21:49:50 -0400

    Create view spec

--- spec/views/dashboards/show.html.erb_spec.rb
+++ spec/views/dashboards/show.html.erb_spec.rb
@@ -1,10 +1,26 @@
 require "rails_helper"
 
 RSpec.describe "dashboards/show.html.erb", type: :view do
-  it "works" do
-    stub_template "actions_mobile" => "This content"
-    stub_template "analytics" => "This content"
-    assign(:user, create(:user))
-    render
+  before do
+    stub_template "dashboards/_actions_mobile.html.erb" => "stubbed content"
+    stub_template "dashboards/_analytics.html.erb" => "stubbed content"
+    stub_template "dashboards/_actions.html.erb" => "stubbed content"
+
+    Imgproxy.config.key = "hmm"
+    SiteConfig.mascot_image_url = "https://i.imgur.com/fKYKgo4.png"
+  end
+
+  after do
+    Imgproxy.config.key = nil
+    SiteConfig.mascot_image_url = nil
+  end
+
+  context "when using Imgproxy" do
+    it "renders mascot image properly" do
+      assign(:user, create(:user))
+      assign(:articles, [])
+      render
+      expect(rendered).to match(%r{unsafe/w:300/aHR0cHM6Ly9pLmlt/Z3VyLmNvbS9mS1lL/Z280LnBuZw})
+    end
   end
 end
