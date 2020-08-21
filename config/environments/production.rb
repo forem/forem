0f3ddbfabf8efbcdc0b967dbff407ca85abd2528
benhalpern <bendhalpern@gmail.com>
2020-08-21 13:20:42 -0400

    Remove redirect config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,6 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
-  config.ssl_options = { redirect: false }
 
 
   # Use the lowest log level to ensure availability of diagnostic information
