ad85964d8536477123b8050fdc4d89ca2f2f77e4
rhymes <rhymes@hey.com>
2020-08-23 19:39:19 +0200

    Display all authentication providers in mailer footer

--- app/helpers/authentication_helper.rb
+++ app/helpers/authentication_helper.rb
@@ -15,8 +15,11 @@ module AuthenticationHelper
     end
   end
 
-  def current_user_authentication_enabled_providers
-    providers = Authentication::Providers.enabled & current_user.identities.pluck(:provider).map(&:to_sym)
+  # Returns the list of enabled providers for the given user, defaults to `current_user`
+  def authentication_enabled_providers_for_user(user = nil)
+    user ||= current_user
+
+    providers = Authentication::Providers.enabled & user.identities.pluck(:provider).map(&:to_sym)
     providers.sort.map do |provider_name|
       Authentication::Providers.get!(provider_name)
     end
