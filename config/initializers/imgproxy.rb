b15d3d176617e2bc399e7064f275f43359462875
Mac Siri <krairit.siri@gmail.com>
2020-08-24 16:25:11 -0400

    Use ApplicationConfig instead of ENV

--- config/initializers/imgproxy.rb
+++ config/initializers/imgproxy.rb
@@ -2,15 +2,15 @@ Imgproxy.configure do |config|
   # imgproxy endpoint
   #
   # Full URL to where your imgproxy lives.
-  config.endpoint = ENV["IMGPROXY_ENDPOINT"]
+  config.endpoint = ApplicationConfig["IMGPROXY_ENDPOINT"]
 
   # Next, you have to provide your signature key and salt.
   # If unsure, check out https://github.com/imgproxy/imgproxy/blob/master/docs/configuration.md first.
 
   # Hex-encoded signature key
-  config.hex_key = ENV["IMGPROXY_KEY"]
+  config.hex_key = ApplicationConfig["IMGPROXY_KEY"]
   # Hex-encoded signature salt
-  config.hex_salt = ENV["IMGPROXY_SALT"]
+  config.hex_salt = ApplicationConfig["IMGPROXY_SALT"]
 
   # Base64 encode all URLs
   config.base64_encode_urls = true
