561f19c20ec70d43ab911601121eaa694edc2058
benhalpern <bendhalpern@gmail.com>
2020-08-24 19:41:59 -0400

    Fully set RELEASE_FOOTPRINT with the expectation of setting it before launch

--- config/initializers/initialization_cachebust.rb
+++ config/initializers/initialization_cachebust.rb
@@ -4,9 +4,5 @@ CacheBuster.bust("/shell_bottom")
 CacheBuster.bust("/async_info/shell_version")
 CacheBuster.bust("/onboarding")
 
-# Date/time string ending in minute.
-# Used as part of cache key. Minute precision is fine.
-# This replaces HEROKU_SLUG_COMMIT everywhere where we don't need exact release-based-precision
-# But if anyone has a better way to automate this incrementation in the simplest way that would be consistent
-# across all processes 100% of the time, feel free to replace this value :)
-ENV["RELEASE_FOOTPRINT"] = Time.current.strftime("%y%m%d%H%M")
+# We will set RELEASE_FOOTPRINT in our Forem Cloud environment, or use HEROKU_SLUG_COMMIT if set (e.g. Heroku env)
+ENV["RELEASE_FOOTPRINT"] ||= ENV["HEROKU_SLUG_COMMIT"]
