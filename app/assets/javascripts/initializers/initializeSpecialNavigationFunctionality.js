a47f96ce705ec2b18851d723cb1c9c1bc3540d09
Fernando Valverde <fdov88@gmail.com>
2020-08-24 12:43:06 -0600

    Uses user_signed_in? on top nav bar

--- app/assets/javascripts/initializers/initializeSpecialNavigationFunctionality.js
+++ app/assets/javascripts/initializers/initializeSpecialNavigationFunctionality.js
@@ -1,7 +1,7 @@
 function initializeSpecialNavigationFunctionality() {
   var connectLink = document.getElementById('connect-link');
   var notificationsLink = document.getElementById('notifications-link');
-  var moderationLink = document.getElementById('moderation-link')
+  var moderationLink = document.getElementById('moderation-link');
   if (document.getElementById('notifications-container')) {
     notificationsLink.blur();
     notificationsLink.classList.add('top-bar__link--current');
@@ -21,11 +21,13 @@ function initializeSpecialNavigationFunctionality() {
     moderationLink.classList.remove('top-bar__link--current');
   }
 
-  if (document.body.dataset.userStatus === "logged-in") {
-    document.getElementById('top-bar-logged-in').classList.remove('hidden');
-    document.getElementById('top-bar-logged-out').classList.add('hidden');
+  var loggedInBar = document.getElementById('top-bar-logged-in');
+  var loggedOutBar = document.getElementById('top-bar-logged-out');
+  if (document.body.dataset.userStatus === 'logged-in') {
+    loggedInBar.classList.remove('hidden');
+    loggedOutBar.classList.add('hidden');
   } else {
-    document.getElementById('top-bar-logged-in').classList.add('hidden');
-    document.getElementById('top-bar-logged-out').classList.remove('hidden');
+    loggedInBar.classList.add('hidden');
+    loggedOutBar.classList.remove('hidden');
   }
 }
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   def new
     if user_signed_in?
-      redirect_to root_path
+      redirect_to dashboard_path
     else
       super
     end
