71fc8f03ff0c030c477e7073e4176535f973b807
Joe Doss <jdoss@users.noreply.github.com>
2020-08-25 10:06:53 -0500

    Export RELEASE_FOOTPRINT on container start. (#9987)
    RELEASE_FOOTPRINT will be set to the output of date -u +'%Y-%m-%dT%H:%M:%SZ'
    (2020-08-25T02:02:21Z for example) which will fix https://github.com/forem/forem/pull/9904
    
    This will be reset to a new date and time string every time the container starts.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,6 +6,8 @@ if [ -f tmp/pids/server.pid ]; then
   rm -f tmp/pids/server.pid
 fi
 
+export RELEASE_FOOTPRINT=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
+
 echo "Running rake app_initializer:setup..."
 bundle exec rake app_initializer:setup
 
