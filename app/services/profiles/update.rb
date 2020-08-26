cd6a3423d340089127afbdbe45e5aa27e177494c
Michael Kohl <citizen428@dev.to>
2020-08-26 11:15:54 +0700

    Update service object

--- app/services/profiles/update.rb
+++ app/services/profiles/update.rb
@@ -1,19 +1,36 @@
 module Profiles
-  module Update
+  class Update
     def self.call(profile, updated_attributes = {})
+      new(profile, updated_attributes).call
+    end
+
+    attr_reader
+
+    def initialize(profile, updated_attributes)
+      @profile = profile
+      @updated_attributes = updated_attributes
+      @success = false
+    end
+
+    def call
       # We don't update `data` directly. This uses the defined store_attributes
       # so we can make use of their typecasting.
-      profile.assign_attributes(updated_attributes)
+      @profile.assign_attributes(@updated_attributes)
 
       # Before saving, filter out obsolete profile fields
-      profile.data.slice!(*Profile.attributes)
-      profile.save
+      @profile.data.slice!(*Profile.attributes)
+
+      return unless @profile.save
 
       # Propagate changes back to the `users` table
-      user_attributes = profile.data.transform_keys do |key|
+      user_attributes = @profile.data.transform_keys do |key|
         Profile::MAPPED_ATTRIBUTES.fetch(key, key).to_s
       end
-      profile.user.update(user_attributes)
+      @success = true if @profile.user.update(user_attributes)
+    end
+
+    def success?
+      @success
     end
   end
 end
