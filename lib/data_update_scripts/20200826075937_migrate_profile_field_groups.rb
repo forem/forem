2ab3b16a29fed0d616b40b8ef77638b90beb811b
Michael Kohl <citizen428@dev.to>
2020-08-27 12:15:21 +0700

    Add guard clause to data update script

--- lib/data_update_scripts/20200826075937_migrate_profile_field_groups.rb
+++ lib/data_update_scripts/20200826075937_migrate_profile_field_groups.rb
@@ -1,6 +1,9 @@
 module DataUpdateScripts
   class MigrateProfileFieldGroups
     def run
+      # ensure we can run this after the group column gets removed
+      return unless "group".in?(ProfileField.column_names)
+
       profile_field_groups = Hash.new do |hash, key|
         hash[key] = ProfileFieldGroup.find_or_create_by(name: key)
       end
