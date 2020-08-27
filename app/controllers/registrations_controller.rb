c449cab9a6ff50ecf24e0bdd69c98c728d78bfb8
rhymes <rhymes@hey.com>
2020-08-27 15:38:16 +0200

    Transfer User.registered.estimated_count to the controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,8 @@ class RegistrationsController < Devise::RegistrationsController
   prepend_before_action :require_no_authentication, only: []
 
   def new
+    @registered_users_count = User.registered.estimated_count
+
     if user_signed_in?
       redirect_to dashboard_path
     else
