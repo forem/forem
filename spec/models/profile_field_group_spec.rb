27e720815dfceeb2c75edec9324c202c3b377b18
Michael Kohl <citizen428@dev.to>
2020-08-27 14:44:56 +0700

    Fix specs

--- spec/models/profile_field_group_spec.rb
+++ spec/models/profile_field_group_spec.rb
@@ -1,6 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe ProfileFieldGroup, type: :model do
+  subject { create(:profile_field_group) }
+
   it { is_expected.to have_many(:profile_fields).dependent(:nullify) }
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name) }
