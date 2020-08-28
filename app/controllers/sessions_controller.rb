82e7589b1e427cd5733c14105d72414f6ab3d809
rhymes <rhymes@hey.com>
2020-08-28 15:31:07 +0200

    Delete cookie correctly by specifying the domain

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < Devise::SessionsController
   def destroy
     # Let's say goodbye to all the cookies when someone signs out.
-    cookies.each do |cookie|
-      cookies.delete cookie[0]
-    end
+    domain = Rails.env.production? ? ApplicationConfig["APP_DOMAIN"] : nil
+    cookies.delete(domain: domain)
+
     super
   end
 end
