113e73c2e56c90ffc18d77def3492412024bb24a
rhymes <rhymes@hey.com>
2020-08-28 14:57:19 +0200

    Fix userData as undefined error

--- app/assets/javascripts/utilities/userData.js
+++ app/assets/javascripts/utilities/userData.js
@@ -3,5 +3,11 @@
 function userData() {
   const { user = null } = document.body.dataset;
 
+  // [@forem/oss]: there's an edge case in which `user` has the value of "undefined",
+  // this results in is a JSON syntax error (`user` should be a hash, not a string).
+  if (user === 'undefined') {
+    return {};
+  }
+
   return JSON.parse(user);
 }
