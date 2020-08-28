a04aec0ac76a7fc87d88488ba2bc470cf601c16a
benhalpern <bendhalpern@gmail.com>
2020-08-28 11:44:13 -0400

    Fix indentation issue

--- app/lib/constants/role.rb
+++ app/lib/constants/role.rb
@@ -8,14 +8,14 @@ module Constants
                   "Pro"].freeze
 
     SPECIAL_ROLES = ["Admin",
-                    "Super Admin",
-                    "Resource Admin: Article",
-                    "Resource Admin: Comment",
-                    "Resource Admin: BufferUpdate",
-                    "Resource Admin: ChatChannel",
-                    "Resource Admin: Page",
-                    "Resource Admin: FeedbackMessage",
-                    "Resource Admin: Config",
-                    "Resource Admin: Broadcast"].freeze
+                     "Super Admin",
+                     "Resource Admin: Article",
+                     "Resource Admin: Comment",
+                     "Resource Admin: BufferUpdate",
+                     "Resource Admin: ChatChannel",
+                     "Resource Admin: Page",
+                     "Resource Admin: FeedbackMessage",
+                     "Resource Admin: Config",
+                     "Resource Admin: Broadcast"].freeze
   end
 end
