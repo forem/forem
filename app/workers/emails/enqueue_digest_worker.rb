aed36f4620ba7b945326a318b766b120288a69cb
mstruve <mollylbs@gmail.com>
2020-08-28 13:57:36 -0500

    Bug Fix:Add send_email_digest Rake Task for DEV

--- app/workers/emails/enqueue_digest_worker.rb
+++ app/workers/emails/enqueue_digest_worker.rb
@@ -5,6 +5,11 @@ module Emails
     sidekiq_options queue: :medium_priority, retry: 15
 
     def perform
+      # Temporary
+      # @sre:mstruve This is temporary until we have an efficient way to handle this job
+      # for our large DEV community. Smaller Forems should be able to handle it no problem
+      return if SiteConfig.community_name == "DEV"
+
       EmailDigest.send_periodic_digest_email
     end
   end
