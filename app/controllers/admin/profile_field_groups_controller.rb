dadc18c354d3990e623b6fce7840bd7c1372352d
Ridhwana <ridhwana.khan16@gmail.com>
2020-08-31 16:14:56 +0200

    feat: edit a group

--- app/controllers/admin/profile_field_groups_controller.rb
+++ app/controllers/admin/profile_field_groups_controller.rb
@@ -5,6 +5,16 @@ module Admin
     ].freeze
     layout "admin"
 
+    def update
+      profile_field_group = ProfileFieldGroup.find(params[:id])
+      if profile_field_group.update(profile_field_group_params)
+        flash[:success] = "Profile field #{profile_field_group.name} updated"
+      else
+        flash[:error] = "Error: #{profile_field_group.errors_as_sentence}"
+      end
+      redirect_to admin_profile_fields_path
+    end
+
     def create
       @profile_field_group = ProfileFieldGroup.new(profile_field_group_params)
       if @profile_field_group.save
