41d9d67b7826f91c623c08e588e73f736ce94514
Ben Halpern <bendhalpern@gmail.com>
2020-08-31 10:53:37 -0400

    Purge core edge/fragment caches on config update (#10098)
    * Clear core fragment caches on config update
    
    * Edged caches
    
    * Edged caches

--- app/controllers/admin/configs_controller.rb
+++ app/controllers/admin/configs_controller.rb
@@ -96,8 +96,11 @@ module Admin
     end
 
     def bust_relevant_caches
-      # Needs to change when suggested_tags is edited.
-      CacheBuster.bust("/tags/onboarding")
+      CacheBuster.bust("/tags/onboarding") # Needs to change when suggested_tags is edited.
+      CacheBuster.bust("/shell_top") # Cached at edge, sent to service worker.
+      CacheBuster.bust("/shell_bottom") # Cached at edge, sent to service worker.
+      CacheBuster.bust("/onboarding") # Page is cached at edge.
+      Rails.cache.delete_matched(ApplicationConfig["RELEASE_FOOTPRINT"]) # Delete all caches tied to this key.
     end
 
     def campaign_params
