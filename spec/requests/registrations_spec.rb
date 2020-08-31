5ff2ad743783a364e98979d0c26405c48ea2f892
Lisa Sy <lisa@dev.to>
2020-08-31 09:55:27 -0700

    Update main authentication view to improve visual design (#9856) [deploy]
    * Update mobile version of /new registration view
    
    * Optimize styles for desktop view
    
    * Fix typo
    
    * Fix typo
    
    * Add twemojis in footer area
    
    * Update messaging to fulfill all intents
    
    * cache
    
    * cache
    
    * hr-label fix on mobile
    
    * hr-label fix on mobile
    
    * crayons for checkbox
    
    * crayons for checkbox
    
    * remove old styles
    
    * move signin to appropriate folder
    
    * Remove schema changes
    
    * Fix registraction specs and hide password hint for no SSO enabled
    
    * Disable email hint for password reset page
    
    * Fix specs
    
    * Revert "Disable email hint for password reset page"
    
    This reverts commit b33a6dda4c13534541294281f83f7ad5a4864c0d.
    
    * Transfer User.registered.estimated_count to the controller
    
    * Fix import path
    
    Co-authored-by: ludwiczakpawel <ludwiczakpawel@gmail.com>
    Co-authored-by: rhymes <rhymes@hey.com>

--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -3,23 +3,39 @@ require "rails_helper"
 RSpec.describe "Registrations", type: :request do
   let(:user) { create(:user) }
 
-  describe "GET /enter" do
+  describe "Sign up" do
     context "when not logged in" do
-      it "shows the sign in page (with self-serve auth)" do
-        get "/enter"
-        expect(response.body).to include "Great to have you"
+      it "shows the sign in page with single sign on options" do
+        get sign_up_path
+
+        Authentication::Providers.enabled.each do |provider_name|
+          provider = Authentication::Providers.get!(provider_name)
+
+          expect(response.body).to include("Continue with #{provider.official_name}")
+        end
       end
 
-      it "shows the sign in text" do
-        get "/enter"
-        expect(response.body).to include "If you have a password"
+      it "shows the sign in text for password based authentication" do
+        get sign_up_path
+
+        expect(response.body).to include("Have a password? Continue with your email address")
       end
 
-      it "shows invite-only text if no self-serve" do
-        SiteConfig.authentication_providers = []
-        get "/enter"
-        expect(response.body).to include "If you have a password"
-        expect(response.body).not_to include "Sign in by social auth"
+      it "does not show the password based authentication hint if there are no single sign in options enabled" do
+        allow(Authentication::Providers).to receive(:enabled).and_return([])
+
+        get sign_up_path
+
+        expect(response.body).not_to include("Have a password? Continue with your email address")
+      end
+
+      it "only shows the single sign on options if they are present" do
+        allow(Authentication::Providers).to receive(:enabled).and_return([])
+
+        get sign_up_path
+
+        expect(response.body).to include("Password")
+        expect(response.body).not_to include("Continue with")
       end
     end
 
@@ -27,8 +43,8 @@ RSpec.describe "Registrations", type: :request do
       it "redirects to /dashboard" do
         sign_in user
 
-        get "/enter"
-        expect(response).to redirect_to("/dashboard")
+        get sign_up_path
+        expect(response).to redirect_to(dashboard_path)
       end
     end
   end
