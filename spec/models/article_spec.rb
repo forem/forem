6ddf0e72dcc2cd51e051c4d152d9817634043a72
rhymes <rhymes@hey.com>
2020-08-31 16:26:23 +0200

    Fix merge fail part 2

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -37,8 +37,8 @@ RSpec.describe Article, type: :model do
     it { is_expected.to validate_length_of(:title).is_at_most(128) }
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_presence_of(:user_id) }
-    it { is_expected.to validate_uniqueness_of(:canonical_url).allow_blank }
-    it { is_expected.to validate_uniqueness_of(:feed_source_url).allow_blank }
+    it { is_expected.to validate_uniqueness_of(:canonical_url).allow_nil }
+    it { is_expected.to validate_uniqueness_of(:feed_source_url).allow_nil }
     it { is_expected.to validate_uniqueness_of(:slug).scoped_to(:user_id) }
 
     it { is_expected.not_to allow_value("foo").for(:main_image_background_hex_color) }
