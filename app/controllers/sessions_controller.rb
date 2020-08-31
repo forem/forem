32659f95e5fb95548563739a265a9857dbc86cf0
rhymes <rhymes@hey.com>
2020-08-31 13:23:34 +0200

    Fix clearing of all cookies on logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < Devise::SessionsController
   def destroy
     # Let's say goodbye to all the cookies when someone signs out.
     domain = Rails.env.production? ? ApplicationConfig["APP_DOMAIN"] : nil
-    cookies.delete(domain: domain)
+    cookies.clear(domain: domain)
 
     super
   end
