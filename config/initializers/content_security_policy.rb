fe073f61c995b2a6b77622823778bfa6d7a7c061
benhalpern <bendhalpern@gmail.com>
2020-09-01 19:04:01 -0400

    Change from secure_headers to rails config

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -5,12 +5,13 @@
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
 Rails.application.config.content_security_policy do |policy|
-  #   policy.default_src :self, :https
-  #   policy.font_src    :self, :https, :data
-  #   policy.img_src     :self, :https, :data
-  #   policy.object_src  :none
-  #   policy.script_src  :self, :https
-  #   policy.style_src   :self, :https
+    policy.default_src :self, :https
+    policy.font_src    :self, :https, :data
+    policy.img_src     :self, :https, :data
+    policy.object_src  :none
+    policy.script_src  :self, :https, :unsafe_inline
+    policy.style_src   :self, :https, :unsafe_inline
+    policy.connect_src :self, :https, :unsafe_inline, "*.pusher.com"
 
   #   # Specify URI for violation reports
   #   # policy.report_uri "/csp-violation-report-endpoint"
