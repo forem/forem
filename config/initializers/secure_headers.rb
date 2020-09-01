63616a78a6d17ea82e59e7b2202d96c1a46546b5
benhalpern <bendhalpern@gmail.com>
2020-09-01 15:50:35 -0400

    Add connect-src

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,17 +1,11 @@
 SecureHeaders::Configuration.default do |config|
-  config.cookies = {
-    secure: true, # mark all cookies as "Secure"
-    httponly: true, # mark all cookies as "HttpOnly"
-    samesite: {
-      lax: true # mark all cookies as SameSite=lax
-    }
-  }
   config.csp = {
     default_src: %w(https: 'self'),
     font_src: %w('self' data: https:),
     img_src: %w('self' https: data:),
     object_src: %w('none'),
     script_src: %w(https: 'unsafe-inline'),
+    connect_src: %w(connect-src 'self' 'unsafe-inline' *.pusher.com),
     style_src: %w('self' https: 'unsafe-inline')
   }
 end
