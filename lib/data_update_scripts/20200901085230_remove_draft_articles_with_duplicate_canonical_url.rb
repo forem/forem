c3bba9a58bbbf596a52448fab6da543712de33bf
rhymes <rhymes@hey.com>
2020-09-01 16:37:53 +0200

    Remove draft articles with a duplicate canonical_url

---
+++ lib/data_update_scripts/20200901085230_remove_draft_articles_with_duplicate_canonical_url.rb
@@ -0,0 +1,60 @@
+module DataUpdateScripts
+  class RemoveDraftArticlesWithDuplicateCanonicalUrl
+    def run
+      # Currently there are duplicate draft articles in the DB with the same canonical_url.
+
+      # This statement deletes all draft articles in excess found to be duplicate over canonical_url,
+      # excluding those whose body_markdown is different from the other duplicate occurrences
+      ActiveRecord::Base.connection.execute(
+        <<~SQL,
+          WITH duplicates_draft_articles AS
+              (SELECT id
+               FROM
+                   (SELECT id,
+                           published,
+                           body_markdown,
+                           LAG(body_markdown, 1) OVER(PARTITION BY canonical_url
+                                                      ORDER BY id ASC) AS previous_body_markdown,
+                           ROW_NUMBER() OVER(PARTITION BY canonical_url
+                                             ORDER BY id ASC) AS row_number
+                    FROM articles
+                    WHERE canonical_url IS NOT NULL ) duplicates
+               WHERE duplicates.row_number > 1
+                   AND published = 'f' -- drafts
+                   AND body_markdown = previous_body_markdown -- with the same body
+           )
+          DELETE
+          FROM articles
+          WHERE id IN (SELECT id FROM duplicates_draft_articles) RETURNING id;
+        SQL
+      )
+
+      # Now that all duplicates with the same body are gone, we need to deal with duplicate canonical URLs
+      # with different bodies.
+      # We thus select the oldest for removal preserving the most recent one
+      ActiveRecord::Base.connection.execute(
+        <<~SQL,
+          WITH duplicates_draft_articles AS
+              (SELECT id
+               FROM
+                   (SELECT id,
+                           published,
+                           body_markdown,
+                           LAG(body_markdown, 1) OVER(PARTITION BY canonical_url
+                                                      ORDER BY created_at DESC) AS previous_body_markdown,
+                           ROW_NUMBER() OVER(PARTITION BY canonical_url
+                                             ORDER BY created_at DESC) AS row_number
+                    FROM articles
+                    WHERE canonical_url IS NOT NULL ) duplicates
+               WHERE duplicates.row_number > 1
+                   AND published = 'f' -- drafts
+                   AND body_markdown != previous_body_markdown -- with different bodies
+           )
+          DELETE
+          FROM articles
+          WHERE id IN (SELECT id FROM duplicates_draft_articles) RETURNING id;
+        SQL
+      )
+    end
+  end
+end
