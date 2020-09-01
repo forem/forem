985b88963e236d38c04950d950fd773cf3bae3ac
benhalpern <bendhalpern@gmail.com>
2020-09-01 11:50:42 -0400

    Initial secure header functionality

---
+++ config/initializers/secure_headers.rb
@@ -0,0 +1,10 @@
+SecureHeaders::Configuration.default do |config|
+  config.csp = {
+    default_src: %w(https: 'self'),
+    font_src: %w('self' data: https:),
+    img_src: %w('self' https: data:),
+    object_src: %w('none'),
+    script_src: %w(https:),
+    style_src: %w('self' https: 'unsafe-inline')
+  }
+end
