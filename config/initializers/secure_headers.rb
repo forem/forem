6a6694bc042f3f8f93e92f36227b413c39dd64e5
benhalpern <bendhalpern@gmail.com>
2020-09-01 15:27:35 -0400

    Secure cookies

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,4 +1,11 @@
 SecureHeaders::Configuration.default do |config|
+  config.cookies = {
+    secure: true, # mark all cookies as "Secure"
+    httponly: true, # mark all cookies as "HttpOnly"
+    samesite: {
+      lax: true # mark all cookies as SameSite=lax
+    }
+  }
   config.csp = {
     default_src: %w(https: 'self'),
     font_src: %w('self' data: https:),
