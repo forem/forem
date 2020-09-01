7b5bc89b7c162912e41b3fc94ce8cd61befef079
Ben Halpern <bendhalpern@gmail.com>
2020-09-01 05:16:31 -0400

    Allow admins to set brand color (#10097)
    * Allow admins to set brand color
    
    * Remove extra line
    
    * Fix linting
    
    * Update app/controllers/admin/configs_controller.rb
    
    Co-authored-by: Michael Kohl <citizen428@dev.to>
    
    * Move wcag compare to own class
    
    * Remove unnecessary spacing
    
    Co-authored-by: Michael Kohl <citizen428@dev.to>

--- app/models/site_config.rb
+++ app/models/site_config.rb
@@ -153,6 +153,7 @@ class SiteConfig < RailsSettings::Base
   field :public, type: :boolean, default: 0
   # The default font for all users that have not chosen a custom font yet
   field :default_font, type: :string, default: "sans_serif"
+  field :primary_brand_color_hex, type: :string, default: "#3b49df"
 
   # Broadcast
   field :welcome_notifications_live_at, type: :date
