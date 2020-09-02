2ff9f36a62f9172556da324ac6a00562e512763f
rhymes <rhymes@hey.com>
2020-09-02 20:44:50 +0200

    Session cookie domain hotfix for login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,33 @@
 class SessionsController < Devise::SessionsController
-  def destroy
-    # Let's say goodbye to all the cookies when someone signs out.
-    domain = Rails.env.production? ? ApplicationConfig["APP_DOMAIN"] : nil
-    cookies.clear(domain: domain)
+  def create
+    delete_legacy_cookie
 
     super
+  end
+
+  def destroy
+    # Let's say goodbye to all the cookies when someone signs out.
+    cookies.clear(domain: cookie_domain)
+
+    delete_legacy_cookie
+
+    super
+  end
+
+  private
+
+  def cookie_domain
+    Rails.env.production? ? ApplicationConfig["APP_DOMAIN"] : nil
+  end
+
+  def delete_legacy_cookie
+    domain = cookie_domain
+
+    return unless domain&.starts_with?(".")
+
+    # Deleting the session cookie with the previous app domain, the one without the leading dot.
+    # This should fix the double cookie scenario
+    domain_without_leading_dot = domain.gsub(/\A./, "")
+    cookies.delete(ApplicationConfig["SESSION_KEY"], domain: domain_without_leading_dot)
   end
 end
