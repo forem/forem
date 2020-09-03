ddd48b25a739a9b711248d5a6b5e626402368bcb
Vaidehi Joshi <vaidehi.sj@gmail.com>
2020-09-03 13:17:34 -0700

    [deploy] Move deletion of legacy cookie higher in omniauth callback (#10190)
    * Move deletion of legacy cookie higher in omniauth callback
    
    * Add specific date to hotfix TODO note
    
    * Fix inverted logic that looked for wrong cookie domain

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,29 +6,28 @@ class SessionsController < Devise::SessionsController
   end
 
   def destroy
+    delete_legacy_cookie
+
     # Let's say goodbye to all the cookies when someone signs out.
     cookies.clear(domain: cookie_domain)
-
-    delete_legacy_cookie
 
     super
   end
 
   private
 
-  def cookie_domain
+  def legacy_cookie_domain
     Rails.env.production? ? ApplicationConfig["APP_DOMAIN"] : nil
   end
 
-  # NOTE: this code is a hotfix, and shall be removed soon (around 2 weeks from deployment)
+  def cookie_domain
+    ".#{legacy_cookie_domain}"
+  end
+
+  # TODO: this code is a hotfix, we should remove it after 09/18/2020.
   def delete_legacy_cookie
-    domain = cookie_domain
-
-    return unless domain&.starts_with?(".")
-
-    # Deleting the session cookie with the previous app domain, the one without the leading dot.
-    # This should fix the double cookie scenario
-    domain_without_leading_dot = domain.gsub(/\A./, "")
-    cookies.delete(ApplicationConfig["SESSION_KEY"], domain: domain_without_leading_dot)
+    # Deleting the session cookie with the legacy app domain, which does NOT include a preceding dot.
+    # This should fix the double cookie scenario.
+    cookies.delete(ApplicationConfig["SESSION_KEY"], domain: legacy_cookie_domain)
   end
 end
