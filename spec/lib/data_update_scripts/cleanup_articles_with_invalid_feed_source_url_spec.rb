a3e542b94c8b5a231804df2154eaecf2c10e0e03
rhymes <rhymes@hey.com>
2020-09-04 17:02:47 +0200

    Cleanup specs

--- spec/lib/data_update_scripts/cleanup_articles_with_invalid_feed_source_url_spec.rb
+++ spec/lib/data_update_scripts/cleanup_articles_with_invalid_feed_source_url_spec.rb
@@ -4,8 +4,18 @@ require Rails.root.join(
 )
 
 describe DataUpdateScripts::CleanupArticlesWithInvalidFeedSourceUrl do
+  let(:article) { create(:article) }
+
+  it "sets feed_source_url to canonical_url if the latter is present" do
+    canonical_url = "https://example.com/article"
+    article.update_columns(feed_source_url: "", canonical_url: canonical_url)
+
+    described_class.new.run
+
+    expect(article.reload.feed_source_url).to eq(canonical_url)
+  end
+
   it "sets empty string feed source url to nil" do
-    article = create(:article)
     article.update_columns(feed_source_url: "")
 
     described_class.new.run
@@ -14,7 +24,6 @@ describe DataUpdateScripts::CleanupArticlesWithInvalidFeedSourceUrl do
   end
 
   it "sets totally invalid url to nil" do
-    article = create(:article)
     article.update_columns(feed_source_url: "not a url")
 
     described_class.new.run
@@ -23,7 +32,6 @@ describe DataUpdateScripts::CleanupArticlesWithInvalidFeedSourceUrl do
   end
 
   it "sets an 'almost URL' to a https URL" do
-    article = create(:article)
     article.update_columns(feed_source_url: "dev.to")
 
     described_class.new.run
