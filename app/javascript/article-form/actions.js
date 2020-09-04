f0963c1bfa5d7ed56c0f964a57bde8ab86e6db85
Nick Taylor <nick@dev.to>
2020-09-04 14:12:59 -0400

    Drag and drop an image in the editor (#10145)
    * Refactored drag n drop functionality to a hook and component.
    
    * Add the snackbar to the article editor.
    
    * Now you can drag and drop images in the post body content.
    
    * Cleaning up a few things.
    
    * Now post cover images can be dragged and dropped.
    
    * Removed unused empty function.
    
    * Added @testing-library/preact-hooks so that we can test hooks.
    
    * Some renaming, small refactor.
    
    * Added tests
    
    * Added more tests.
    
    * Added more tests.
    
    * Added message about dropping only one image in post body.
    
    * Added message about dropping only one image for cover image.
    
    * put onDrop into it's own meethod.
    
    * More tests.
    
    * Added some API documentation.
    
    * Updated editor help with drag and drop image help.
    
    * Changed wording.
    
    * Now the alt text for markdown removes the file extension.
    
    * Updated help wording. Thanks @rhymes
    
    * Now the image markdown adds a new line at the end as we currently do not support inline images.

--- app/javascript/article-form/actions.js
+++ app/javascript/article-form/actions.js
@@ -1,3 +1,5 @@
+import { validateFileInputs } from '../packs/validateFileInputs';
+
 export function previewArticle(payload, successCb, failureCb) {
   fetch('/articles/preview', {
     method: 'POST',
@@ -96,4 +98,24 @@ export function generateMainImage(payload, successCb, failureCb) {
       return successCb({ links, image });
     })
     .catch(failureCb);
+}
+
+/**
+ * Processes images for upload.
+ *
+ * @param {FileList} images Images to be uploaded.
+ * @param {Function} handleImageSuccess The handler that runs when the image is uploaded successfully.
+ * @param {Function} handleImageFailure The handler that runs when the image upload fails.
+ */
+export function processImageUpload(
+  images,
+  handleImageSuccess,
+  handleImageFailure,
+) {
+  // Currently only one image is supported for upload.
+  if (images.length > 0 && validateFileInputs()) {
+    const payload = { image: images };
+
+    generateMainImage(payload, handleImageSuccess, handleImageFailure);
+  }
 }
