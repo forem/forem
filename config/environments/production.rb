3d8d1f0e2c12a0dd8abe5f300efb88c1353762c6
benhalpern <bendhalpern@gmail.com>
2020-09-04 13:27:18 -0400

    Remove host from url options and temporarily show errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
