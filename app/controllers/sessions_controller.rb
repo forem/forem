13426e7e6cf4eef29064c1e927cb9479ff040ed8
benhalpern <bendhalpern@gmail.com>
2020-09-05 14:52:09 -0400

    Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,12 @@
 class SessionsController < Devise::SessionsController
   def create
     delete_legacy_cookie
-
-    super
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    remember_me(resource)
+    respond_with resource, location: after_sign_in_path_for(resource)
   end
 
   def destroy
