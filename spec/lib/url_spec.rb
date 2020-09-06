2427f03111f4f707a0ddf2b506114be77c611a7a
benhalpern <bendhalpern@gmail.com>
2020-09-06 12:22:27 -0400

    Update domain test

--- spec/lib/url_spec.rb
+++ spec/lib/url_spec.rb
@@ -3,7 +3,8 @@ require "rails_helper"
 RSpec.describe URL, type: :lib do
   before do
     allow(ApplicationConfig).to receive(:[]).with("APP_PROTOCOL").and_return("https://")
-    allow(ApplicationConfig).to receive(:[]).with("APP_DOMAIN").and_return("dev.to")
+    allow(ApplicationConfig).to receive(:[]).with("APP_DOMAIN").and_return("test.forem.cloud")
+    SiteConfig.app_domain = "dev.to"
   end
 
   describe ".protocol" do
