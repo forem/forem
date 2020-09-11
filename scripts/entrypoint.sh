00b43741cfc93d4b55f39d74ddb26e013bf77034
Joe Doss <jdoss@forem.com>
2020-09-10 22:00:01 -0500

    Adjust the entrypoint script so we have better control over when we run rake assets:precompile and rake app_initializer:setup.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -10,7 +10,22 @@ export RELEASE_FOOTPRINT=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
 export FOREM_BUILD_DATE=$(cat FOREM_BUILD_DATE)
 export FOREM_BUILD_SHA=$(cat FOREM_BUILD_SHA)
 
-echo "Running rake app_initializer:setup..."
-bundle exec rake app_initializer:setup
+case "$@" in
 
-exec "$@"
+  precompile)
+    echo "Running rake assets:precompile..."
+    bundle exec rake assets:precompile
+    ;;
+
+  bootstrap)
+    echo "Running rake app_initializer:setup..."
+    bundle exec rake app_initializer:setup
+    ;;
+
+  *)
+    echo "Running command:"
+    echo "$@"
+    exec "$@"
+    ;;
+
+esac
