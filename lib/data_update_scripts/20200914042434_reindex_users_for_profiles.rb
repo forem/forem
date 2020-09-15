ea5a8a44fd9cdfc78b410e1acd03ab3aba26ae76
Michael Kohl <citizen428@dev.to>
2020-09-15 10:01:29 +0700

    Update data update scripts

--- lib/data_update_scripts/20200914042434_reindex_users_for_profiles.rb
+++ lib/data_update_scripts/20200914042434_reindex_users_for_profiles.rb
@@ -1,7 +1,9 @@
 module DataUpdateScripts
   class ReindexUsersForProfiles
     def run
-      User.find_each(&:index_to_elasticsearch_inline)
+      User.select(:id).in_batches(of: 200) do |batch|
+        Search::BulkIndexWorker.set(queue: :default).perform_async("User", batch.ids)
+      end
     end
   end
 end
