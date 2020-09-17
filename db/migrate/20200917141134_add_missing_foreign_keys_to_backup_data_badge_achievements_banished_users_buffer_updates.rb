8ff6fe5ebb8a0b1fbd1046caffa0c64b804fc252
rhymes <rhymes@hey.com>
2020-09-17 16:20:40 +0200

    Add missing FKs to BackupData BadgeAchievements BanishedUsers BufferUpdates

---
+++ db/migrate/20200917141134_add_missing_foreign_keys_to_backup_data_badge_achievements_banished_users_buffer_updates.rb
@@ -0,0 +1,13 @@
+class AddMissingForeignKeysToBackupDataBadgeAchievementsBanishedUsersBufferUpdates < ActiveRecord::Migration[6.0]
+  def change
+    add_foreign_key :backup_data, :users, column: :instance_user_id, on_delete: :cascade, validate: false
+
+    add_foreign_key :badge_achievements, :users, column: :rewarder_id, on_delete: :nullify, validate: false
+
+    add_foreign_key :banished_users, :users, column: :banished_by_id, on_delete: :nullify, validate: false
+
+    add_foreign_key :buffer_updates, :users, column: :approver_user_id, on_delete: :nullify, validate: false
+    add_foreign_key :buffer_updates, :users, column: :composer_user_id, on_delete: :nullify, validate: false
+    add_foreign_key :buffer_updates, :tags, column: :tag_id, on_delete: :nullify, validate: false
+  end
+end
