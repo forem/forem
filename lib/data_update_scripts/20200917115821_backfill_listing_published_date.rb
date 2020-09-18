86fe8a3f0330fd4958ba25a732dddc806403876f
rafi993 <rafi993@protonmail.com>
2020-09-18 20:51:26 +0530

    Backfilling only published listing

--- lib/data_update_scripts/20200917115821_backfill_listing_published_date.rb
+++ lib/data_update_scripts/20200917115821_backfill_listing_published_date.rb
@@ -3,10 +3,8 @@ module DataUpdateScripts
     def run
       return unless Listing.column_names.include?("originally_published_at")
 
-      Listing.find_each do |listing|
-        if listing.published
-          listing.update!(originally_published_at: listing.created_at)
-        end
+      Listing.where(published: true).find_each do |listing|
+        listing.update!(originally_published_at: listing.created_at)
       end
     end
   end
