5342f9aa2d74a90b8f8114a62437360291b035d2
mstruve <mollylbs@gmail.com>
2020-09-18 09:11:52 -0500

    try reg mode

--- scripts/ci/parallel_specs.sh
+++ scripts/ci/parallel_specs.sh
@@ -6,11 +6,11 @@ echo "******************"
 echo "$PWD"
 echo "******************"
 
-if [ "$KNAPSACK_PRO_TEST_SUITE_TOKEN_RSPEC" = "" ]; then
+# if [ "$KNAPSACK_PRO_TEST_SUITE_TOKEN_RSPEC" = "" ]; then
   KNAPSACK_PRO_ENDPOINT=https://api-disabled-for-fork.knapsackpro.com \
     KNAPSACK_PRO_MAX_REQUEST_RETRIES=0 \
     KNAPSACK_PRO_TEST_SUITE_TOKEN_RSPEC=disabled-for-fork \
     rake knapsack_pro:rspec # use Regular Mode here always
-else
-  rake knapsack_pro:queue:rspec
-fi
+# else
+#   rake knapsack_pro:queue:rspec
+# fi
