6d0c30622652dedced2d98f77c1349cdf808c7bb
mstruve <mollylbs@gmail.com>
2020-09-18 08:53:17 -0500

    remove bundle exec

--- scripts/ci/parallel_specs.sh
+++ scripts/ci/parallel_specs.sh
@@ -10,7 +10,7 @@ if [ "$KNAPSACK_PRO_TEST_SUITE_TOKEN_RSPEC" = "" ]; then
   KNAPSACK_PRO_ENDPOINT=https://api-disabled-for-fork.knapsackpro.com \
     KNAPSACK_PRO_MAX_REQUEST_RETRIES=0 \
     KNAPSACK_PRO_TEST_SUITE_TOKEN_RSPEC=disabled-for-fork \
-    bundle exec rake knapsack_pro:rspec # use Regular Mode here always
+    rake knapsack_pro:rspec # use Regular Mode here always
 else
-  bundle exec rake knapsack_pro:queue:rspec
+  rake knapsack_pro:queue:rspec
 fi
