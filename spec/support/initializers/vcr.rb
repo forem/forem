9fb0b0090585705d18712184718f7c7954d65ea2
mstruve <mollylbs@gmail.com>
2020-09-18 09:30:28 -0500

    add ES and redis to VCR

--- spec/support/initializers/vcr.rb
+++ spec/support/initializers/vcr.rb
@@ -13,7 +13,8 @@ VCR.configure do |config|
     "github.com/mozilla/geckodriver/releases",
     "selenium-release.storage.googleapis.com",
     "developer.microsoft.com/en-us/microsoft-edge/tools/webdriver",
-    "api.knapsackpro.com", "localhost", "127.0.0.1", "0.0.0.0"
+    "api.knapsackpro.com", "localhost", "127.0.0.1", "0.0.0.0",
+    "elasticsearch", "redis"
   )
 
   # Removes all private data (Basic Auth, Set-Cookie headers...)
