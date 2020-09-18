2914b8e5b5ea38648c15a1209a5b109fadedde72
benhalpern <bendhalpern@gmail.com>
2020-09-17 20:14:18 -0400

    Fix spec?

--- spec/liquid_tags/github_tag/github_readme_tag_spec.rb
+++ spec/liquid_tags/github_tag/github_readme_tag_spec.rb
@@ -69,9 +69,6 @@ RSpec.describe GithubTag::GithubReadmeTag, type: :liquid_tag, vcr: true do
     end
 
     it "renders a repository with a missing README" do
-      allow(SiteConfig).to receive(:github_key).and_return("test")
-      allow(SiteConfig).to receive(:github_secret).and_return("test")
-
       allow(Github::OauthClient).to receive(:readme).and_raise(Github::Errors::NotFound)
 
       VCR.use_cassette("github_client_repository") do
