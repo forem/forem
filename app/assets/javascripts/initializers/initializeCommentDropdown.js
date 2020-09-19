forem/forem

commit 1aa8233d9efb36b7fdc1809546c47b31ac863a8f

Adds runtime copy to clipboard (#10323)

app/assets/javascripts/initializers/initializeCommentDropdown.js
@@ -3,12 +3,6 @@
 function initializeCommentDropdown() {
   const announcer = document.getElementById('article-copy-link-announcer');
 
-  function isClipboardSupported() {
-    return (
-      typeof navigator.clipboard !== 'undefined' && navigator.clipboard !== null
-    );
-  }
-
   function removeClass(className) {
     return (element) => element.classList.remove(className);
   }
@@ -34,28 +28,11 @@ function initializeCommentDropdown() {
     }
   }
 
-  function execCopyText() {
-    showAnnouncer();
-    document.execCommand('copy');
-  }
-
   function copyText() {
     const inputValue = document.getElementById('article-copy-link-input').value;
-    if (Runtime.isNativeAndroid('copyToClipboard')) {
-      AndroidBridge.copyToClipboard(inputValue);
+    Runtime.copyToClipboard(inputValue).then(() => {
       showAnnouncer();
-    } else if (isClipboardSupported()) {
-      navigator.clipboard
-        .writeText(inputValue)
-        .then(() => {
-          showAnnouncer();
-        })
-        .catch((err) => {
-          execCopyText();
-        });
-    } else {
-      execCopyText();
-    }
+    });
   }
 
   function shouldCloseDropdown(event) {
@@ -63,8 +40,7 @@ function initializeCommentDropdown() {
       event.target.matches('.dropdown-icon') ||
       event.target.matches('.dropbtn') ||
       event.target.matches('clipboard-copy') ||
-      event.target.matches('clipboard-copy input') ||
-      event.target.matches('clipboard-copy svg') ||
+      document.getElementById('article-copy-icon').contains(event.target) ||
       event.target.parentElement.classList.contains('dropdown-link-row')
     );
   }

app/assets/javascripts/initializers/runtime.js
@@ -51,4 +51,32 @@ class Runtime {
 
     return nativeCheck && namespaceCheck;
   }
+
+  /**
+   * This function copies text to clipboard taking in consideration all
+   * supported platforms.
+   *
+   * @param {string} text to be copied to the clipboard
+   *
+   * @returns {Promise} Resolves when succesful in copying to clipboard
+   */
+  static copyToClipboard(text) {
+    return new Promise((resolve, reject) => {
+      if (Runtime.isNativeAndroid('copyToClipboard')) {
+        AndroidBridge.copyToClipboard(text);
+        resolve();
+      } else if (navigator.clipboard != null) {
+        navigator.clipboard
+          .writeText(text)
+          .then(() => {
+            resolve();
+          })
+          .catch((e) => {
+            reject(e);
+          });
+      } else {
+        reject('Unsupported device unable to copy to clipboard');
+      }
+    });
+  }
 }

app/javascript/article-form/components/ImageUploader.jsx
@@ -7,12 +7,6 @@ import { validateFileInputs } from '../../packs/validateFileInputs';
 import { ClipboardButton } from './ClipboardButton';
 import { Button, Spinner } from '@crayons';
 
-function isClipboardSupported() {
-  return (
-    typeof navigator.clipboard !== 'undefined' && navigator.clipboard !== null
-  );
-}
-
 const ImageIcon = () => (
   <svg
     width="24"
@@ -102,25 +96,11 @@ export const ImageUploader = () => {
       'image-markdown-copy-link-input',
     );
 
-    if (Runtime.isNativeAndroid('copyToClipboard')) {
-      AndroidBridge.copyToClipboard(imageMarkdownInput.value);
+    Runtime.copyToClipboard(imageMarkdownInput.value).then(() => {
       dispatch({
         type: 'show_copied_image_message',
       });
-    } else if (isClipboardSupported()) {
-      navigator.clipboard
-        .writeText(imageMarkdownInput.value)
-        .then(() => {
-          dispatch({
-            type: 'show_copied_image_message',
-          });
-        })
-        .catch((_err) => {
-          execCopyText();
-        });
-    } else {
-      execCopyText();
-    }
+    });
   }
 
   function handleInsertionImageUpload(e) {
@@ -140,14 +120,6 @@ export const ImageUploader = () => {
     dispatch({
       type: 'upload_image_success',
       payload: { insertionImageUrls: response.links },
-    });
-  }
-
-  function execCopyText() {
-    imageMarkdownInput.setSelectionRange(0, imageMarkdownInput.value.length);
-    document.execCommand('copy');
-    dispatch({
-      type: 'show_copied_image_message',
     });
   }
 

app/javascript/chat/ChatChannelSettings/MembershipManager/InvitationLinkManager.jsx
@@ -5,12 +5,6 @@ import PropTypes from 'prop-types';
 
 import { Button } from '@crayons';
 
-function isClipboardSupported() {
-  return (
-    typeof navigator.clipboard !== 'undefined' && navigator.clipboard !== null
-  );
-}
-
 const CopyIcon = () => (
   <svg
     width="24"
@@ -47,31 +41,10 @@ export default class InvitationLinkManager extends Component {
       'chat-channel-unviation-url',
     );
 
-    if (Runtime.isNativeAndroid('copyToClipboard')) {
-      AndroidBridge.copyToClipboard(this.imageMarkdownInput.value);
+    Runtime.copyToClipboard(this.imageMarkdownInput.value).then(() => {
       this.setState({ showImageCopiedMessage: true });
-    } else if (isClipboardSupported()) {
-      navigator.clipboard
-        .writeText(this.imageMarkdownInput.value)
-        .then(() => {
-          this.setState({ showImageCopiedMessage: true });
-        })
-        .catch((_err) => {
-          this.execCopyText();
-        });
-    } else {
-      this.execCopyText();
-    }
+    });
   };
-
-  execCopyText() {
-    this.imageMarkdownInput.setSelectionRange(
-      0,
-      this.imageMarkdownInput.value.length,
-    );
-    document.execCommand('copy');
-    this.setState({ showImageCopiedMessage: true });
-  }
 
   render() {
     const {

commit 0597805012fce8e73c6c4d90ec1372791e582881

Patches bug introduced in #10323
Patch suggested in following comment: https://github.com/forem/forem/pull/8311\#pullrequestreview-492011202

app/assets/javascripts/initializers/initializeCommentDropdown.js
@@ -50,10 +50,13 @@ function initializeCommentDropdown() {
   }
 
   function shouldCloseDropdown(event) {
+    var copyIcon = document.getElementById('article-copy-icon');
+    var isCopyIconChild = copyIcon && copyIcon.contains(event.target);
     return !(
       event.target.matches('.dropdown-icon') ||
       event.target.matches('.dropbtn') ||
       event.target.matches('clipboard-copy') ||
+      isCopyIconChild ||
       event.target.parentElement.classList.contains('dropdown-link-row')
     );
   }