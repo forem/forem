9caf4c9517608f34902a97ab90c4df4413343d2f
ludwiczakpawel <ludwiczakpawel@gmail.com>
2020-09-22 14:25:59 +0200

    .

--- app/assets/javascripts/initializers/initializeSpecialNavigationFunctionality.js
+++ app/assets/javascripts/initializers/initializeSpecialNavigationFunctionality.js
@@ -2,25 +2,33 @@ function initializeSpecialNavigationFunctionality() {
   var connectLink = document.getElementById('connect-link');
   var notificationsLink = document.getElementById('notifications-link');
   var moderationLink = document.getElementById('moderation-link');
+  var searchLink = document.getElementById('search-link');
+  var currentPage = document.getElementById('page-content').dataset.currentPage;
 
   if (connectLink) {
-    if (document.getElementById('notifications-container')) {
+    if (currentPage === 'notifications-index') {
       notificationsLink.blur();
       notificationsLink.classList.add('top-bar__link--current');
     } else {
       notificationsLink.classList.remove('top-bar__link--current');
     }
-    if (document.getElementById('chat')) {
+    if (currentPage === 'chat_channels-index') {
       connectLink.blur();
       connectLink.classList.add('top-bar__link--current');
     } else {
       connectLink.classList.remove('top-bar__link--current');
     }
-    if (document.getElementById('moderation-page')) {
+    if (currentPage === 'moderations-index') {
       moderationLink.blur();
       moderationLink.classList.add('top-bar__link--current');
     } else {
       moderationLink.classList.remove('top-bar__link--current');
+    }
+    if (currentPage === 'stories-search') {
+      searchLink.blur();
+      searchLink.classList.add('top-bar__link--current');
+    } else {
+      searchLink.classList.remove('top-bar__link--current');
     }
   }
 }
