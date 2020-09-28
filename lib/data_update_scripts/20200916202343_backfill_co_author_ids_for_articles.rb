cc30a19b0864dc13404ae03e8d7d3b8240058a59
Julianna Tetreault <juliannatetreault@gmail.com>
2020-09-28 16:55:03 -0600

    Refactor data_update script to use a single SQL statement (thank you, Michael)

--- lib/data_update_scripts/20200916202343_backfill_co_author_ids_for_articles.rb
+++ lib/data_update_scripts/20200916202343_backfill_co_author_ids_for_articles.rb
@@ -2,9 +2,7 @@ module DataUpdateScripts
   class BackfillCoAuthorIdsForArticles
     def run
       articles = Article.where.not(second_user_id: nil).or(Article.where.not(third_user_id: nil))
-      articles.find_each do |article|
-        article.update!(co_author_ids: [article.second_user_id, article.third_user_id])
-      end
+      articles.update_all("co_author_ids = array[second_user_id, third_user_id]")
     end
   end
 end
