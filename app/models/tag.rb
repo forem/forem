f8ed03fd4619edd5943822e5408e65f9dec6aa50
rhymes <rhymes@hey.com>
2020-09-29 18:32:47 +0200

    Fix trailing whitespace

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -15,7 +15,7 @@ class Tag < ActsAsTaggableOn::Tag
   belongs_to :mod_chat_channel, class_name: "ChatChannel", optional: true
 
   has_many :buffer_updates, dependent: :nullify
-  
+
   has_one :sponsorship, as: :sponsorable, inverse_of: :sponsorable, dependent: :destroy
 
   mount_uploader :profile_image, ProfileImageUploader
