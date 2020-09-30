969805a04983043d7a8e6bac8853ce6e76516093
Taniyah-L-Jackson <taniyah-l-jackson-h@gmail.com>
2020-09-29 21:35:37 -0500

    added failsafe function for profile_type and pinnable_type

--- app/models/profile_pin.rb
+++ app/models/profile_pin.rb
@@ -1,11 +1,11 @@
-class ProfilePin < ApplicationRecord
+class GenericPins < ApplicationRecord #migrate later
   belongs_to :pinnable, polymorphic: true
   belongs_to :profile, polymorphic: true
 
   validates :profile_id, presence: true
-  validates :profile_type, inclusion: { in: %w[User] } # Future could be organization, tag, etc.
+  validates :profile_type, inclusion: { in: %w[User Article] } # Future could be organization, tag, etc.
   validates :pinnable_id, presence: true, uniqueness: { scope: %i[profile_id profile_type pinnable_type] }
-  validates :pinnable_type, inclusion: { in: %w[Article] } # Future could be comments, etc.
+  validates :pinnable_type, inclusion: { in: %w[Article Comment] } # Future could be comments, etc.
   validate :only_five_pins_per_profile, on: :create
   validate :pinnable_belongs_to_profile
 
@@ -17,5 +17,10 @@ class ProfilePin < ApplicationRecord
 
   def pinnable_belongs_to_profile
     errors.add(:pinnable_id, "must have proper permissions for pin") if pinnable.user_id != profile_id
+  end
+
+  # If profile_type && pinnable_type = Article
+  def article_to_article
+    errors.add(:parent_type, "cannot pin articles to each other") if pinnable_type == profile_type
   end
 end
