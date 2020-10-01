f9559ed04cd01180814c788b636976eb86cafd7a
Julianna Tetreault <juliannatetreault@gmail.com>
2020-10-01 09:25:37 -0600

    Refactors data_update script to remove nil values from array

--- lib/data_update_scripts/20200916202343_backfill_co_author_ids_for_articles.rb
+++ lib/data_update_scripts/20200916202343_backfill_co_author_ids_for_articles.rb
@@ -2,7 +2,10 @@ module DataUpdateScripts
   class BackfillCoAuthorIdsForArticles
     def run
       articles = Article.where.not(second_user_id: nil).or(Article.where.not(third_user_id: nil))
-      articles.update_all("co_author_ids = array[second_user_id, third_user_id]")
+      articles.find_each do |article|
+        co_author_ids = [article.second_user_id, article.third_user_id].compact
+        article.update_columns(co_author_ids: co_author_ids)
+      end
     end
   end
 end
