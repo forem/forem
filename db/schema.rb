c0afd7d39528122da7c1011563a53933f62a5fb3
Julianna Tetreault <juliannatetreault@gmail.com>
2020-10-02 09:34:44 -0600

    Adds updated schema file to (fingers crossed) resolve Travis failure

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_18_200231) do
+ActiveRecord::Schema.define(version: 2020_10_02_104711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
