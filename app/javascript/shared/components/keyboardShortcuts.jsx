fd5e4d17b72399749fc12d96ad1ac81350d2f131
Andrew Bone <AndrewB05@gmail.com>
2020-10-07 16:11:52 +0100

    Allow single character shortcuts
    update to allow single character shortcuts/hotkeys

--- app/javascript/shared/components/keyboardShortcuts.jsx
+++ app/javascript/shared/components/keyboardShortcuts.jsx
@@ -4,24 +4,37 @@ import { useEffect } from 'preact/hooks';
 
 const KeyboardShortcuts = ({ shortcuts, ...props }) => {
   useEffect(() => {
-    const checkShortcut = (e, keys) => {
-      if (!keys) return;
+    // Return true if element is a form element
+    const isFormField = (element) => {
+      if (!(element instanceof HTMLElement)) {
+        return false;
+      }
+
+      const name = element.nodeName.toLowerCase();
+      const type = (element.getAttribute("type") || "").toLowerCase();
+      return (
+        name === "select" ||
+        name === "textarea" ||
+        (name === "input" && ["submit", "reset", "checkbox", "radio"].indexOf(type) < 0) ||
+        element.isContentEditable
+      );
+    };
+
+    const keyEvent = (e) => {
+      if (e.defaultPrevented) return;
+      // Get special keys
+      const keys = `${e.ctrlKey || e.metaKey ? "ctrl+" : ""}${e.altKey ? "alt+" : ""}${e.shiftKey ? "shift+" : ""}`;
+      // If no special keys are pressed and focus is inside a field return
+      if (e.target instanceof Node && isFormField(e.target) && !keys) return;
       const shortcut = shortcuts[`${keys}${e.code}`];
       if (shortcut) shortcut(e);
     };
 
-    const keyEvent = (e) => {
-      checkShortcut(
-        e,
-        `${(e.ctrlKey || e.metaKey) ? 'ctrl+' : ''}${e.altKey ? 'alt+' : ''}${e.shiftKey ? 'shift+' : ''}`
-      );
-    };
-
-    window.addEventListener('keydown', keyEvent);
+    window.addEventListener("keydown", keyEvent);
 
     return () => {
-      window.removeEventListener('keydown', keyEvent);
-    }
+      window.removeEventListener("keydown", keyEvent);
+    };
   }, [shortcuts]);
 
   return null;
