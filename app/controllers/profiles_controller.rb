f5b3922b96c58d885fb4214fab60796b9205b623
Michael Kohl <citizen428@dev.to>
2020-10-09 10:01:22 +0700

    Don't set experience cookie in ProfilesController

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,6 @@ class ProfilesController < ApplicationController
   def update
     update_result = Profiles::Update.call(current_user, update_params)
     if update_result.success?
-      update_cookie_experience_level
       flash[:settings_notice] = "Your profile has been updated"
     else
       flash[:error] = "Error: #{update_result.error_message}"
@@ -17,11 +16,5 @@ class ProfilesController < ApplicationController
 
   def update_params
     params.permit(profile: Profile.attributes!, user: ALLOWED_USER_PARAMS)
-  end
-
-  def update_cookie_experience_level
-    return if current_user.experience_level.blank?
-
-    cookies.permanent[:user_experience_level] = current_user.experience_level.to_s
   end
 end
