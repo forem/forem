02fbc1090ec0f55e2d36c506d2e8d264254aa5e2
Andrew Bone <AndrewB05@gmail.com>
2020-10-09 09:25:08 +0100

    Move keyboardShortcuts to a hook

--- app/javascript/shared/components/keyboardShortcuts.jsx
+++ app/javascript/shared/components/keyboardShortcuts.jsx
@@ -1,23 +1,29 @@
+import { useEffect } from "preact/hooks";
 import PropTypes from 'prop-types';
 import { h } from 'preact';
-import { useEffect } from 'preact/hooks';
 
 /**
- * Component that can be added to the dom to listen 
+ * hook that can be added to a component to listen
  * for keyboard presses
- * 
+ *
  * @example
  * const shortcuts = {
  *   "ctrl+alt+KeyG": (e) => {
- *     e.preventDefault(); 
+ *     e.preventDefault();
  *     alert("Control Alt G has been pressed")
  *   }
  * }
- * <KeyboardShortcuts shortcuts={shortcuts} />
- * 
+ *
+ * const setEventTarget = useKeyboardShortcuts(shortcuts, someElementOrWindowObject);
+ *
+ * @param {object} shortcuts List of keyboard shortcuts/event
+ * @param {EventTarget} [eventTarget=window] An event target.
+ *
  */
-const KeyboardShortcuts = ({ shortcuts, ...props }) => {
+export function useKeyboardShortcuts(shortcuts, eventTarget = window) {
   useEffect(() => {
+    if (!shortcuts) return;
+
     // Return true if element is a form element
     const isFormField = (element) => {
       if (!(element instanceof HTMLElement)) {
@@ -29,38 +35,64 @@ const KeyboardShortcuts = ({ shortcuts, ...props }) => {
       return (
         name === "select" ||
         name === "textarea" ||
-        (name === "input" && ["submit", "reset", "checkbox", "radio"].indexOf(type) < 0) ||
+        (name === "input" &&
+          ["submit", "reset", "checkbox", "radio"].indexOf(type) < 0) ||
         element.isContentEditable
       );
     };
 
     const keyEvent = (e) => {
       if (e.defaultPrevented) return;
+
       // Get special keys
       const keys = `${e.ctrlKey || e.metaKey ? "ctrl+" : ""}${e.altKey ? "alt+" : ""}${e.shiftKey ? "shift+" : ""}`;
+      
       // If no special keys are pressed and focus is inside a field return
       if (e.target instanceof Node && isFormField(e.target) && !keys) return;
+      
       const shortcut = shortcuts[`${keys}${e.code}`];
       if (shortcut) shortcut(e);
     };
 
-    window.addEventListener("keydown", keyEvent);
+    eventTarget.addEventListener("keydown", keyEvent);
 
     return () => {
-      window.removeEventListener("keydown", keyEvent);
+      eventTarget.removeEventListener("keydown", keyEvent);
     };
-  }, [shortcuts]);
+  }, [shortcuts, eventTarget]);
+}
+
+/**
+ * compoent that can be added to a component to listen
+ * for keyboard presses using the useKeyboardShortcuts hook
+ *
+ * @example
+ * const shortcuts = {
+ *   "ctrl+alt+KeyG": (e) => {
+ *     e.preventDefault();
+ *     alert("Control Alt G has been pressed")
+ *   }
+ * }
+ *
+ * <KeyboardShortcuts shortcuts={shortcuts} />
+ * <KeyboardShortcuts shortcuts={shortcuts} eventTarget={ref.current} />
+ *
+ * @param {object} shortcuts List of keyboard shortcuts/event
+ * @param {EventTarget} [eventTarget=window] An event target.
+ *
+ */
+export function KeyboardShortcuts({ shortcuts, eventTarget }) {
+  useKeyboardShortcuts(shortcuts, eventTarget);
 
   return null;
 }
 
+KeyboardShortcuts.propTypes = {
+  shortcuts: PropTypes.object.isRequired,
+  eventTarget: PropTypes.instanceOf(Element)
+}
+
 KeyboardShortcuts.defaultProps = {
   shortcuts: {},
-};
-
-KeyboardShortcuts.propTypes = {
-  /* shortcuts Object - object containing all shortcut functions */
-  shortcuts: PropTypes.object,
-};
-
-export default KeyboardShortcuts;
+  eventTarget: window
+}
