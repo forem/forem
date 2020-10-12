f4bd1df21ceaa7915ac8e79abe8d4e526b480e4b
Arit Amana <32520970+msarit@users.noreply.github.com>
2020-10-12 12:54:35 -0400

    [Email/Password Authentication] Allow Admins to enable email/password login (#10745)
    * Implement Admins ability to allow email login
    
    * Write specs for admin allow email login
    
    * Fix specs causing builds to fail
    
    * Fix spec causing builds to fail
    
    * Use dummyimage.com for spec images
    
    Co-authored-by: Josh Puetz <joshpuetz@gmail.com>
    
    * Use dummyimage.com
    
    * refactor setting of SiteConfig attributes
    
    * Address PR review comments
    
    * fix indentation
    
    * Use "allow to receive and return" approach for other specs
    
    Co-authored-by: Josh Puetz <joshpuetz@gmail.com>

--- app/controllers/admin/configs_controller.rb
+++ app/controllers/admin/configs_controller.rb
@@ -57,6 +57,7 @@ module Admin
         facebook_secret
         invite_only_mode
         allow_email_password_registration
+        allow_email_password_login
         primary_brand_color_hex
         spam_trigger_terms
         recaptcha_site_key
