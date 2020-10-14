ddaf5f8e57243136ca0da07ff950253037850d33
Rafi <Rafi993@users.noreply.github.com>
2020-10-15 02:35:38 +0530

    Replace globals with node env (#10767)

--- bin/generate-css-utility-classes-docs.js
+++ bin/generate-css-utility-classes-docs.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-/* globals module process require __dirname */
+/* eslint-env node */
+/* eslint-disable no-console */
 
 const fs = require('fs');
 const path = require('path');
