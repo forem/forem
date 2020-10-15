73cfd7219f8f489f89a640ff6397207ff67338a3
Ben Halpern <bendhalpern@gmail.com>
2020-10-15 17:00:49 -0400

    Add all new functionality for trusted (#10816)

--- app/queries/admin/moderators_query.rb
+++ app/queries/admin/moderators_query.rb
@@ -10,8 +10,10 @@ module Admin
 
       relation = if state.to_s == "potential"
                    relation.where(
-                     "id NOT IN (SELECT user_id FROM users_roles WHERE role_id = ?)",
+                     "id NOT IN (SELECT user_id FROM users_roles WHERE role_id = ?)
+                     AND id NOT IN (SELECT user_id FROM users_roles WHERE role_id = ?)",
                      role_id_for(:trusted),
+                     role_id_for(:banned),
                    ).order("users.comments_count" => :desc)
                  else
                    relation.joins(:roles)
