83185466928df5992fd440b9179e033c6d48afbb
rafi993 <rafi993@protonmail.com>
2020-10-15 07:15:04 +0530

    Adding .json to prettier config file name

--- app/javascript/.storybook/main.js
+++ app/javascript/.storybook/main.js
@@ -2,7 +2,7 @@ const path = require('path');
 const marked = require('marked');
 const renderer = new marked.Renderer();
 
-const prettierConfig = require('../../../.prettierrc');
+const prettierConfig = require('../../../.prettierrc.json');
 
 module.exports = {
   stories: ['../**/__stories__/*.stories.jsx'],
