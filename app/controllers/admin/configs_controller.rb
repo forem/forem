831b449c5f2e9de3c2af3004fe7a2f63796caabb
Josh Puetz <josh@dev.to>
2020-10-16 10:08:10 -0500

    Allow all third party auth providers to be deactivated (#10812) [deploy]
    * Tests and refactor
    
    * Adapt to empty params, tests
    
    * Removed duplicated spec
    
    * Constantize
    
    * Extract method
    
    * Typo fix!
    
    * Spec change

--- app/controllers/admin/configs_controller.rb
+++ app/controllers/admin/configs_controller.rb
@@ -1,39 +1,95 @@
 module Admin
   class ConfigsController < Admin::ApplicationController
-    layout "admin"
+    CAMPAIGN_PARAMS =
+      %i[
+        campaign_featured_tags
+        campaign_hero_html_variant_name
+        campaign_sidebar_enabled
+        campaign_sidebar_image
+        campaign_url
+        campaign_articles_require_approval
+      ].freeze
 
-    before_action :extra_authorization_and_confirmation, only: [:create]
-    before_action :validate_inputs, only: [:create]
+    COMMUNITY_PARAMS =
+      %i[
+        community_name
+        community_description
+        community_member_label
+        community_copyright_start_year
+        staff_user_id
+        tagline
+      ].freeze
 
-    def show
-      @confirmation_text = confirmation_text
-    end
+    NEWSLETTER_PARAMS =
+      %i[
+        mailchimp_api_key
+        mailchimp_community_moderators_id
+        mailchimp_newsletter_id
+        mailchimp_sustaining_members_id
+        mailchimp_tag_moderators_id
+      ].freeze
 
-    def create
-      clean_up_params
+    RATE_LIMIT_PARAMS =
+      %i[
+        rate_limit_comment_creation
+        rate_limit_email_recipient
+        rate_limit_follow_count_daily
+        rate_limit_image_upload
+        rate_limit_published_article_creation
+        rate_limit_published_article_antispam_creation
+        rate_limit_organization_creation
+        rate_limit_user_subscription_creation
+        rate_limit_article_update
+        rate_limit_user_update
+        rate_limit_feedback_message_creation
+        rate_limit_listing_creation
+        rate_limit_reaction_creation
+        rate_limit_send_email_confirmation
+      ].freeze
 
-      config_params.each do |key, value|
-        if value.is_a?(Array)
-          SiteConfig.public_send("#{key}=", value.reject(&:blank?)) unless value.empty?
-        elsif value.respond_to?(:to_h)
-          SiteConfig.public_send("#{key}=", value.to_h) unless value.empty?
-        else
-          SiteConfig.public_send("#{key}=", value.strip) unless value.nil?
-        end
-      end
+    MASCOT_PARAMS =
+      %i[
+        mascot_image_description
+        mascot_image_url
+        mascot_footer_image_url
+        mascot_footer_image_width
+        mascot_footer_image_height
+        mascot_user_id
+      ].freeze
 
-      bust_relevant_caches
-      redirect_to admin_config_path, notice: "Site configuration was successfully updated."
-    end
+    IMAGE_PARAMS =
+      %i[
+        favicon_url
+        logo_png
+        logo_svg
+        main_social_image
+        secondary_logo_url
+        left_navbar_svg_icon
+        right_navbar_svg_icon
+      ].freeze
 
-    private
+    ONBOARDING_PARAMS =
+      %i[
+        onboarding_logo_image
+        onboarding_background_image
+        onboarding_taskcard_image
+        suggested_tags
+        suggested_users
+      ].freeze
 
-    def confirmation_text
-      "My username is @#{current_user.username} and this action is 100% safe and appropriate."
-    end
+    JOB_PARAMS =
+      %i[
+        jobs_url
+        display_jobs_banner
+      ].freeze
 
-    def config_params
-      allowed_params = %i[
+    ALLOWED_EMPTY_ENUMERABLES =
+      %i[
+        authentication_providers
+      ].freeze
+
+    ALLOWED_PARAMS =
+      %i[
         ga_tracking_id
         periodic_email_digest_max
         periodic_email_digest_min
@@ -63,28 +119,61 @@ module Admin
         recaptcha_site_key
         recaptcha_secret_key
         video_encoder_key
-      ]
+      ].freeze
 
-      allowed_params = allowed_params |
-        campaign_params |
-        community_params |
-        newsletter_params |
-        rate_limit_params |
-        mascot_params |
-        image_params |
-        onboarding_params |
-        job_params
+    layout "admin"
 
-      has_emails = params[:site_config][:email_addresses].present?
+    before_action :extra_authorization_and_confirmation, only: [:create]
+    before_action :validate_inputs, only: [:create]
+
+    def show
+      @confirmation_text = confirmation_text
+    end
+
+    def create
+      clean_up_params
+
+      config_params.each do |key, value|
+        if value.is_a?(Array)
+          update_siteconfig_with_array(key, value)
+        elsif value.respond_to?(:to_h)
+          SiteConfig.public_send("#{key}=", value.to_h) unless value.empty?
+        else
+          SiteConfig.public_send("#{key}=", value.strip) unless value.nil?
+        end
+      end
+
+      bust_relevant_caches
+      redirect_to admin_config_path, notice: "Site configuration was successfully updated."
+    end
+
+    private
+
+    def confirmation_text
+      "My username is @#{current_user.username} and this action is 100% safe and appropriate."
+    end
+
+    def config_params
+      all_params = ALLOWED_PARAMS |
+        CAMPAIGN_PARAMS |
+        COMMUNITY_PARAMS |
+        NEWSLETTER_PARAMS |
+        RATE_LIMIT_PARAMS |
+        MASCOT_PARAMS |
+        IMAGE_PARAMS |
+        ONBOARDING_PARAMS |
+        JOB_PARAMS
+
+      has_emails = params.dig(:site_config, :email_addresses).present?
       params[:site_config][:email_addresses][:default] = ApplicationConfig["DEFAULT_EMAIL"] if has_emails
-      params.require(:site_config).permit(
-        allowed_params,
+      params&.require(:site_config)&.permit(
+        all_params,
         authentication_providers: [],
         social_media_handles: SiteConfig.social_media_handles.keys,
         email_addresses: SiteConfig.email_addresses.keys,
         meta_keywords: SiteConfig.meta_keywords.keys,
         credit_prices_in_cents: SiteConfig.credit_prices_in_cents.keys,
-      )
+      )&.with_defaults(authentication_providers: [])
     end
 
     def raise_confirmation_mismatch_error
@@ -105,8 +194,10 @@ module Admin
 
     def clean_up_params
       config = params[:site_config]
+      return unless config
+
       %i[sidebar_tags suggested_tags suggested_users].each do |param|
-        config[param] = config[param].downcase.delete(" ") if config[param]
+        config[param] = config[param]&.downcase&.delete(" ") if config[param]
       end
       config[:credit_prices_in_cents]&.transform_values!(&:to_i)
     end
@@ -122,104 +213,19 @@ module Admin
 
     # Validations
     def brand_contrast_too_low
-      hex = params[:site_config][:primary_brand_color_hex]
+      hex = params.dig(:site_config, :primary_brand_color_hex)
       hex.present? && Color::Accessibility.new(hex).low_contrast?
     end
 
     def brand_color_not_hex
-      hex = params[:site_config][:primary_brand_color_hex]
+      hex = params.dig(:site_config, :primary_brand_color_hex)
       hex.present? && !hex.match?(/\A#(\h{6}|\h{3})\z/)
     end
 
-    def campaign_params
-      %i[
-        campaign_featured_tags
-        campaign_hero_html_variant_name
-        campaign_sidebar_enabled
-        campaign_sidebar_image
-        campaign_url
-        campaign_articles_require_approval
-      ]
-    end
+    def update_siteconfig_with_array(key, value)
+      return if value.empty? && ALLOWED_EMPTY_ENUMERABLES.exclude?(key.to_sym)
 
-    def community_params
-      %i[
-        community_name
-        community_description
-        community_member_label
-        community_copyright_start_year
-        staff_user_id
-        tagline
-      ]
-    end
-
-    def newsletter_params
-      %i[
-        mailchimp_api_key
-        mailchimp_community_moderators_id
-        mailchimp_newsletter_id
-        mailchimp_sustaining_members_id
-        mailchimp_tag_moderators_id
-      ]
-    end
-
-    def rate_limit_params
-      %i[
-        rate_limit_comment_creation
-        rate_limit_email_recipient
-        rate_limit_follow_count_daily
-        rate_limit_image_upload
-        rate_limit_published_article_creation
-        rate_limit_published_article_antispam_creation
-        rate_limit_organization_creation
-        rate_limit_user_subscription_creation
-        rate_limit_article_update
-        rate_limit_user_update
-        rate_limit_feedback_message_creation
-        rate_limit_listing_creation
-        rate_limit_reaction_creation
-        rate_limit_send_email_confirmation
-      ]
-    end
-
-    def mascot_params
-      %i[
-        mascot_image_description
-        mascot_image_url
-        mascot_footer_image_url
-        mascot_footer_image_width
-        mascot_footer_image_height
-        mascot_user_id
-      ]
-    end
-
-    def image_params
-      %i[
-        favicon_url
-        logo_png
-        logo_svg
-        main_social_image
-        secondary_logo_url
-        left_navbar_svg_icon
-        right_navbar_svg_icon
-      ]
-    end
-
-    def onboarding_params
-      %i[
-        onboarding_logo_image
-        onboarding_background_image
-        onboarding_taskcard_image
-        suggested_tags
-        suggested_users
-      ]
-    end
-
-    def job_params
-      %i[
-        jobs_url
-        display_jobs_banner
-      ]
+      SiteConfig.public_send("#{key}=", value.reject(&:blank?))
     end
   end
 end
