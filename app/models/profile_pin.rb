d30e23b147b187bb9693709eb6f374383a606946
TJ Jackson <Taniyah.L.Jackson.H@gmail.com>
2020-10-18 21:14:53 -0500

    Update profile_pin.rb

--- app/models/profile_pin.rb
+++ app/models/profile_pin.rb
@@ -1,31 +1,28 @@
 class ProfilePin < ApplicationRecord 
   belongs_to :pinnable, polymorphic: true
   belongs_to :profile, polymorphic: true
+  belongs_to :article
 
   validates :profile_id, presence: true
   validates :profile_type, inclusion: { in: %w[User Article] } # Future could be organization, tag, etc.
   validates :pinnable_id, presence: true, uniqueness: { scope: %i[profile_id profile_type pinnable_type] }
   validates :pinnable_type, inclusion: { in: %w[Article Comment] } # Future could be comments, etc.
   validate :only_five_pins_per_profile, on: :create
+  validate :pin_comment_to_article
   validate :pinnable_belongs_to_profile
   validate :article_to_article
 
   private
 
-  #Pseudo for a list of pinned comments on an article
-  #could possibly be recycled for going through a list of pinned articles
-  index = 0
-  display current profile_pins[index]
+  def get_profile_pins
 
-  def BrowsePinsList
-    increase index by 1
-    scroll to new profile_pins[index] 
+    #get all profile pins where an article has pinned comments
+    article.profile_pins.where("profile_id == ? AND profile_type == 'Article' AND pinnable_type == 'Comment'", params[:article.profile_id]) 
 
-    if profile_pins[index] reaches end of profile_pins length:
-      reset index or return
-    end
-  end    
-  #-----------------------------------------------------------------
+    #sort by decreasing score
+    article.profile_pins.order(score: :desc)k
+
+  end
 
   def only_five_pins_per_profile
     errors.add(:base, "cannot have more than five total pinned posts") if profile.profile_pins.size > 4
@@ -37,6 +34,7 @@ class ProfilePin < ApplicationRecord
 
   # If profile_type && pinnable_type = Article
   def article_to_article
-    errors.add(:parent_type, "cannot pin articles to each other") if pinnable_type == profile_type
+    errors.add(:parent_type, "cannot pin articles to each other") 
+    if pinnable_type == profile_type
   end
 end
