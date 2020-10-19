50b76bdd35294b5657d716619da813263a1e11f2
Julianna Tetreault <juliannatetreault@gmail.com>
2020-10-19 15:55:45 -0600

    Removes user.created_at.nil check from data_update script

--- lib/data_update_scripts/20201019192035_backfill_registered_for_users.rb
+++ lib/data_update_scripts/20201019192035_backfill_registered_for_users.rb
@@ -3,7 +3,7 @@ module DataUpdateScripts
     def run
       users = User.where(registered: false).or(User.where(registered_at: nil))
       users.find_each do |user|
-        next if user.created_at.nil? || user.invitation_accepted_at.nil?
+        next if user.invitation_accepted_at.nil?
 
         user.update_columns(registered: true, registered_at: user.invitation_accepted_at)
       end
