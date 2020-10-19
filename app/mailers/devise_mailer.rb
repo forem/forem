54e55fd75d9aa0a99baf5ccd1f84ee2c27fe3886
benhalpern <bendhalpern@gmail.com>
2020-10-19 14:16:21 -0400

    Change DeviseMailer approach

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,19 +1,8 @@
 class DeviseMailer < Devise::Mailer
-  helper Rails.application.routes.url_helpers
-  helper ApplicationHelper
-  helper AuthenticationHelper
+  before_action :use_site_config_values
 
-  before_action :use_custom_host
-
-  default(
-    from: -> { email_from("Community") },
-  )
-
-  def email_from(topic)
-    "#{SiteConfig.community_name} #{topic} <#{SiteConfig.email_addresses[:default]}>"
-  end
-
-  def use_custom_host
+  def use_site_config_values
+    Devise.mailer_sender = "#{SiteConfig.community_name} Community <#{SiteConfig.email_addresses[:default]}>"
     ActionMailer::Base.default_url_options[:host] = SiteConfig.app_domain
   end
 end
