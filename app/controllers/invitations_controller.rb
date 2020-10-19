86110947c042759fac9a741a59e13e045c37d346
Julianna Tetreault <juliannatetreault@gmail.com>
2020-10-19 12:22:49 -0600

    Adds back accidentally removed code from InvitationsController

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -6,16 +6,17 @@ class InvitationsController < Devise::InvitationsController
   def update
     raw_invitation_token = update_resource_params[:invitation_token]
     self.resource = accept_resource
+    invitation_accepted = resource.errors.empty?
 
     yield resource if block_given?
 
     if invitation_accepted
+      resource.update!(registered_at: Time.current, registered: true)
       if resource.class.allow_insecure_sign_in_after_accept
         flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
         set_flash_message :notice, flash_message if is_flashing_format?
         resource.after_database_authentication
         sign_in(resource_name, resource)
-        resource.update!(registered_at: Time.current, registered: true)
         respond_with resource, location: after_accept_path_for(resource)
       else
         set_flash_message :notice, :updated_not_active if is_flashing_format?
