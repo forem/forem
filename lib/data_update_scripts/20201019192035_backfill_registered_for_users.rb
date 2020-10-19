3a73f4207b43f1011d3079ff7f918d1361768955
Julianna Tetreault <juliannatetreault@gmail.com>
2020-10-19 15:08:05 -0600

    Checks that invitation_accepted_at is nil and modifies return placement

--- lib/data_update_scripts/20201019192035_backfill_registered_for_users.rb
+++ lib/data_update_scripts/20201019192035_backfill_registered_for_users.rb
@@ -1,11 +1,11 @@
 module DataUpdateScripts
   class BackfillRegisteredForUsers
     def run
-      return if User.created_at.nil?
-
       users = User.where(registered: false).or(User.where(registered_at: nil))
       users.find_each do |user|
-        user.update_columns(registered: true, registered_at: user.created_at)
+        next if user.created_at.nil? || user.invitation_accepted_at.nil?
+
+        user.update_columns(registered: true, registered_at: user.invitation_accepted_at)
       end
     end
   end
