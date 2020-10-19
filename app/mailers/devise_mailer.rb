1fc1917146946cb153ac8bb49a06e440f3dd2be0
Ben Halpern <bendhalpern@gmail.com>
2020-10-19 16:55:40 -0400

    [deploy] Fix email sender and url issues (#10883)
    * Fix email sender and url issues
    
    * Add proper tests
    
    * Add tests
    
    * Change DeviseMailer approach
    
    * ZFix devise test

---
+++ app/mailers/devise_mailer.rb
@@ -0,0 +1,8 @@
+class DeviseMailer < Devise::Mailer
+  before_action :use_site_config_values
+
+  def use_site_config_values
+    Devise.mailer_sender = "#{SiteConfig.community_name} Community <#{SiteConfig.email_addresses[:default]}>"
+    ActionMailer::Base.default_url_options[:host] = SiteConfig.app_domain
+  end
+end
