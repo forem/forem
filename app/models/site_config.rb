70bf99ed089bd76d16cf7455fd15010d0681c8d2
Zhao-Andy <17884966+Zhao-Andy@users.noreply.github.com>
2020-10-20 10:45:52 -0400

    Use a configurable hotness score

--- app/models/site_config.rb
+++ app/models/site_config.rb
@@ -54,6 +54,7 @@ class SiteConfig < RailsSettings::Base
   field :experience_low, type: :string, default: "Total Newbies"
   field :experience_high, type: :string, default: "Experienced Users"
   field :tag_feed_minimum_score, type: :integer, default: 1
+  field :minimum_hotness_score, type: :integer, default: 7
 
   # Emails
   field :email_addresses, type: :hash, default: {
