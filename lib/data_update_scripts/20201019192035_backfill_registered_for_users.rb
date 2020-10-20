86db1540bc6abdd5ae8b16d7d177727ca2e1246d
Julianna Tetreault <juliannatetreault@gmail.com>
2020-10-20 08:10:26 -0600

    Removes gaurd clause and fetches users from DB

--- lib/data_update_scripts/20201019192035_backfill_registered_for_users.rb
+++ lib/data_update_scripts/20201019192035_backfill_registered_for_users.rb
@@ -1,10 +1,8 @@
 module DataUpdateScripts
   class BackfillRegisteredForUsers
     def run
-      users = User.where(registered: false).or(User.where(registered_at: nil))
+      users = User.where.not(invitation_accepted_at: nil).where(registered: false).or(User.where(registered_at: nil))
       users.find_each do |user|
-        next if user.invitation_accepted_at.nil?
-
         user.update_columns(registered: true, registered_at: user.invitation_accepted_at)
       end
     end
