86f4c5534f99199283e7db82910d5f434d05adf7
Ridhwana <ridhwana.khan16@gmail.com>
2020-10-20 17:15:54 +0200

    chore: revert changes and make enabled its own method

--- app/policies/video_policy.rb
+++ app/policies/video_policy.rb
@@ -1,15 +1,13 @@
 class VideoPolicy < ApplicationPolicy
+  def new?
+    user.created_at < 2.weeks.ago if user.created_at
+  end
+
   def create?
     user.created_at < 2.weeks.ago if user.created_at
   end
 
   def enabled?
-    new? && SiteConfig.enable_video_upload
-  end
-
-  private
-
-  def new?
-    user.created_at < 2.weeks.ago if user.created_at
+    SiteConfig.enable_video_upload
   end
 end
