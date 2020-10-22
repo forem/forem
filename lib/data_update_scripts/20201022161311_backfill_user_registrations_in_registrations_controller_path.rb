8c98f4e0deab3c973ef01794a6aa2a260ff86a5e
Ben Halpern <bendhalpern@gmail.com>
2020-10-22 19:00:16 -0400

    Update lib/data_update_scripts/20201022161311_backfill_user_registrations_in_registrations_controller_path.rb
    Co-authored-by: Molly Struve <mollylbs@gmail.com>

--- lib/data_update_scripts/20201022161311_backfill_user_registrations_in_registrations_controller_path.rb
+++ lib/data_update_scripts/20201022161311_backfill_user_registrations_in_registrations_controller_path.rb
@@ -3,7 +3,7 @@ module DataUpdateScripts
     def run
       # Users who _were not invited_ should by definition have registered already in order to exist
       User.where(registered_at: nil, invitation_sent_at: nil).find_each do |user|
-        user.update_column(:registered_at, user.created_at, registered: true)
+        user.update_columns(registered_at: user.created_at, registered: true)
       end
     end
   end
