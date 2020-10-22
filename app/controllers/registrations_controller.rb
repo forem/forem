850e9cbc31297d7515c59f200d73ffc38096faa0
Jacob Herrington <jacobherringtondeveloper@gmail.com>
2020-10-22 11:28:40 -0500

    Only show user count for large userbases
    This is a request from one of our early adopter Forems. We should only
    render the user count that appears on most of our sign up CTAs when a
    Forem has a large userbase (initially, I'm setting that to 1000 users).
    
    The change also removes a lot of duplicated markup by using some
    partials to render the copy in the various auth CTAs.
    
    Hopefully, this is written in such a way that changing opening up more
    fine-grained control of these partials is trivial.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,8 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   prepend_before_action :require_no_authentication, only: []
 
   def new
-    @registered_users_count = User.registered.estimated_count
-
     if user_signed_in?
       redirect_to root_path(signin: "true")
     else
