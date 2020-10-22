c454e2343de5b1b3fa07b47d7263ac3eaa4e7494
lightalloy <lightallloy@gmail.com>
2020-10-22 10:29:13 +0300

    Update schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_072557) do
+ActiveRecord::Schema.define(version: 2020_10_19_012200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
