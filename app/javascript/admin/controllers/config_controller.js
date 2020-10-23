b2091723ffe2f3e4ee95d821e502354698dbfb13
Arit Amana <msarit@gmail.com>
2020-10-23 13:46:33 -0400

    Start building generalized Modal blocks

--- app/javascript/admin/controllers/config_controller.js
+++ app/javascript/admin/controllers/config_controller.js
@@ -8,6 +8,45 @@ const emailAuthSettingsSection = document.querySelector(
   '#email-auth-settings-section',
 );
 
+// eslint-disable-next-line no-unused-vars
+const adminConfigModal = (
+  title,
+  body,
+  confirmBtnText,
+  confirmBtnAction,
+  cancelBtnText,
+  cancelBtnAction,
+) => `
+  <div class="crayons-modal crayons-modal--s absolute">
+    <div class="crayons-modal__box">
+      <header class="crayons-modal__box__header">
+        <h2>${title}</h2>
+        <button type="button" class="crayons-btn crayons-btn--icon crayons-btn--ghost">
+          <svg width="24" height="24" viewBox="0 0 24 24" class="crayons-icon" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636l4.95 4.95z" />
+          </svg>
+        </button>
+      </header>
+      <div class="crayons-modal__box__body">
+        ${body}
+        <div>
+          <button
+            class="crayons-btn crayons-btn--danger"
+            data-action="click->config#${confirmBtnAction}">
+            ${confirmBtnText}
+          </button>
+          <button
+            class="crayons-btn crayons-btn--secondary"
+            data-action="click->config#${cancelBtnAction}">
+            ${cancelBtnText}
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="crayons-modal__overlay"></div>
+  </div>
+`;
+
 export default class ConfigController extends Controller {
   static targets = [
     'inviteOnlyMode',
