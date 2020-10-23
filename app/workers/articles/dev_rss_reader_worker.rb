6579a4b5a1b29c4b02aa6250134483606876e659
rhymes <rhymes@hey.com>
2020-10-23 15:09:22 +0200

    Add temporary RSS reader worker to gather monitoring data

---
+++ app/workers/articles/dev_rss_reader_worker.rb
@@ -0,0 +1,17 @@
+# NOTE: [rhymes]
+# This script will soon be removed. We need it to collect monitoring data on
+# Datadog about the production behavior of the `RssReader` class
+module Articles
+  class DevRssReaderWorker
+    include Sidekiq::Worker
+
+    sidekiq_options queue: :medium_priority, retry: 10
+
+    def perform
+      return unless SiteConfig.community_name == "DEV"
+
+      # we force fetch to have realistic data
+      RssReader.get_all_articles(force: true)
+    end
+  end
+end
