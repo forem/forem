e7f30f251fbab4133f60ebc8e2f839c7828c4932
rhymes <rhymes@hey.com>
2020-10-26 16:17:50 +0100

    Fix Devise::Mailer previews

--- spec/mailers/previews/devise.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# :nodoc:
-module Devise
-end
--- spec/mailers/previews/devise/mailer_preview.rb
+++ spec/mailers/previews/devise/mailer_preview.rb
@@ -1,31 +1,24 @@
-# frozen_string_literal: true
-
-# Preview all emails at http://localhost:3000/rails/mailers/devise_mailer
+# Preview all emails at http://localhost:3000/rails/mailers/devise/mailer
 module Devise
-  # :nodoc:
   class MailerPreview < ActionMailer::Preview
     def confirmation_instructions
       Devise::Mailer.confirmation_instructions(User.last, "faketoken")
     end
 
     def email_changed
-      Devise::Mailer.email_changed(User.last, User.last.email)
-    end
-
-    def invitation_instructions
-      Devise::Mailer.email_changed(User.last, "faketoken")
-    end
-
-    def password_changed
       Devise::Mailer.email_changed(User.last)
     end
 
+    def password_change
+      Devise::Mailer.password_change(User.last)
+    end
+
     def reset_password_instructions
-      Devise::Mailer.email_changed(User.last, "faketoken")
+      Devise::Mailer.reset_password_instructions(User.last, "faketoken")
     end
 
     def unlock_instructions
-      Devise::Mailer.reset_password_instructions(User.last, "faketoken")
+      Devise::Mailer.unlock_instructions(User.last, "faketoken")
     end
   end
 end
