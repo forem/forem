bb96c1424a525c2a4cf3826f1e796d82b1d5778c
benhalpern <bendhalpern@gmail.com>
2020-10-27 11:08:09 -0400

    Run GC.compact before fork

--- config/puma.rb
+++ config/puma.rb
@@ -26,6 +26,11 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 #
 workers ENV.fetch("WEB_CONCURRENCY", 2)
 
+# Code that runs before the app begins forking
+before_fork do
+  GC.compact
+end
+
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
