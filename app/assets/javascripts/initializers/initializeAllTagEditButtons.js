b4314cb3bf56cca54fc56897e3356b0bf4d60206
benhalpern <bendhalpern@gmail.com>
2020-10-27 09:44:24 -0400

    Add more check

--- app/assets/javascripts/initializers/initializeAllTagEditButtons.js
+++ app/assets/javascripts/initializers/initializeAllTagEditButtons.js
@@ -4,7 +4,7 @@ function initializeAllTagEditButtons() {
   var tagEditButton = document.getElementById('tag-edit-button');
   var tagAdminButton = document.getElementById('tag-admin-button');
   var user = userData();
-  if (user.admin) {
+  if (user.admin && tagAdminButton) {
     tagAdminButton.style.display = 'inline-block';
     document.getElementById('tag-admin-button').style.display = 'inline-block';
   }
