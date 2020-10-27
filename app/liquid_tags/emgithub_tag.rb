99415c396ab41a8b08d59396075fa6ebcedeb624
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2020-10-27 14:31:59 +0530

    new liquid -> emgithub

---
+++ app/liquid_tags/emgithub_tag.rb
@@ -0,0 +1,56 @@
+require "cgi"
+
+class EmgithubTag < LiquidTagBase
+  PARTIAL = "liquids/emgithub".freeze
+  OPTION_REGEXP = /\A(showBorder|showLineNumbers|showFileMeta)*\z/.freeze
+  LINK_REGEXP = %r{\A(http|https)://github.com/[a-zA-Z0-9]*/[a-zA-Z0-9]*/blob/[a-zA-Z0-9_./#-]*\z}.freeze
+
+  def initialize(_tag_name, link, _parse_context)
+    super
+    @link = parse_link(link)
+    @build_options = parse_options(link)
+  end
+
+  def render(_context)
+    ApplicationController.render(
+      partial: PARTIAL,
+      locals: {
+        link: @link,
+        build_options: @build_options,
+        height: 600
+      },
+    )
+  end
+
+  private
+
+  def valid_option(option)
+    option.match(OPTION_REGEXP)
+  end
+
+  def parse_options(input)
+    stripped_link = ActionController::Base.helpers.strip_tags(input)
+    _, *options = stripped_link.split(" ")
+
+    # Validation
+    validated_options = options.map { |option| valid_option(option) }.reject(&:nil?)
+    raise StandardError, "Invalid Options" unless options.empty? || !validated_options.empty?
+
+    options.length.zero? ? "" : validated_options.join("=on&")
+  end
+
+  def parse_link(link)
+    stripped_link = ActionController::Base.helpers.strip_tags(link)
+    the_link = stripped_link.split(" ").first
+    raise StandardError, "Invalid Github URL" unless valid_link?(the_link)
+
+    CGI.escape(the_link)
+  end
+
+  def valid_link?(link)
+    link_no_space = link.delete(" ")
+    link_no_space.match?(LINK_REGEXP)
+  end
+end
+
+Liquid::Template.register_tag("emgithub", EmgithubTag)
