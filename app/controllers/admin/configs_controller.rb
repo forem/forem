1b4648cd6abb1a704b7399e4a6e79d983cd0451a
Arit Amana <32520970+msarit@users.noreply.github.com>
2020-10-28 13:37:11 -0400

    [Team Email Login] Refactor the Email Authentication flow (#11090)
    * Hooked "Enable" button to hidden checkbox
    
    * Hooked "Close" button to close email settings and show "Enable/Edit" button
    
    * Additional hookups
    
    * Start building generalized Modal blocks
    
    * Everything hooked up except styling and a few Qs
    
    * last of the hookups; ensure logic flow
    
    * clean up
    
    * specs to cover email auth refactor
    
    * Fix bug surfaced by Vaidehi
    
    * Incorporate PR feedback
    
    * prevent email auth disable if invite-only-mode
    
    * adjust emailAuthModal body text
    
    * Sundry improvements
    
    * Last-mile tweaks
    
    * Trying to get 3rd party auth deselect to work
    
    * delete unnecssary function
    
    * remove superfluous comment
    
    * Move inline styling into CSS file
    
    * Incorporate PR feedback
    
    * Incorporate more PR feedback
    
    * Make Confirm btn intent clearer
    
    * Add TODO comment

--- app/controllers/admin/configs_controller.rb
+++ app/controllers/admin/configs_controller.rb
@@ -118,8 +118,7 @@ module Admin
         facebook_key
         facebook_secret
         invite_only_mode
-        allow_email_password_registration
-        allow_email_password_login
+        allow_both_email_signup_and_login
         require_captcha_for_email_password_registration
         primary_brand_color_hex
         spam_trigger_terms
@@ -152,6 +151,8 @@ module Admin
           SiteConfig.public_send("#{key}=", value.strip) unless value.nil?
         end
       end
+
+      toggle_email_password_authentication
 
       redirect_to admin_config_path, notice: "Site configuration was successfully updated."
     end
@@ -209,6 +210,17 @@ module Admin
         config[param] = config[param]&.downcase&.delete(" ") if config[param]
       end
       config[:credit_prices_in_cents]&.transform_values!(&:to_i)
+    end
+
+    def toggle_email_password_authentication
+      if SiteConfig.allow_both_email_signup_and_login
+        SiteConfig.allow_email_password_registration = true
+        SiteConfig.allow_email_password_login = true
+      else
+        SiteConfig.allow_email_password_registration = false
+        SiteConfig.allow_email_password_login = false
+        SiteConfig.invite_only_mode = false
+      end
     end
 
     # Validations
