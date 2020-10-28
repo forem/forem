531f26bad69e6cb03468d1f84d2e4a64c3dea757
Ben Halpern <bendhalpern@gmail.com>
2020-10-28 12:19:21 -0400

    Add admin shortcut to tag page (#11092)
    * Add admin shortcut to tag page
    
    * Fix spec
    
    * Add more check
    
    * Change admin location

--- app/assets/javascripts/initializers/initializeAllTagEditButtons.js
+++ app/assets/javascripts/initializers/initializeAllTagEditButtons.js
@@ -2,11 +2,16 @@
 
 function initializeAllTagEditButtons() {
   var tagEditButton = document.getElementById('tag-edit-button');
+  var tagAdminButton = document.getElementById('tag-admin-button');
   var user = userData();
+  if (user.admin && tagAdminButton) {
+    tagAdminButton.style.display = 'inline-block';
+    document.getElementById('tag-admin-button').style.display = 'inline-block';
+  }
   if (
     user &&
     tagEditButton &&
-    user.moderator_for_tags.indexOf(tagEditButton.dataset.tag) > -1
+    (user.moderator_for_tags.indexOf(tagEditButton.dataset.tag) > -1 || user.admin)
   ) {
     tagEditButton.style.display = 'inline-block';
     document.getElementById('tag-mod-button').style.display = 'inline-block';
