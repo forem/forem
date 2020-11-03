47624ddfcd66d79fb02bfad9344331aa92dd39d2
rhymes <rhymes@hey.com>
2020-11-03 14:02:17 +0100

    Add users param to Feeds::Import (#11234)
    * Add users param to Feeds::Import
    
    * Fix init and specs
    
    * Replace explicit nil check
    
    Co-authored-by: Michael Kohl <me@citizen428.net>
    
    Co-authored-by: Michael Kohl <me@citizen428.net>

--- spec/workers/articles/dev_feeds_import_worker_spec.rb
+++ spec/workers/articles/dev_feeds_import_worker_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Articles::DevFeedsImportWorker, type: :worker do
       expect(Feeds::Import).not_to have_received(:call)
     end
 
-    it "does not call the RssReader if the cache instructs it to cancel" do
+    it "does not call Feeds::Import if the cache instructs it to cancel" do
       allow(SiteConfig).to receive(:community_name).and_return("DEV")
       allow(Rails.cache).to receive(:read).with("cancel_feeds_import").and_return("true")
 
@@ -26,7 +26,7 @@ RSpec.describe Articles::DevFeedsImportWorker, type: :worker do
       expect(Feeds::Import).not_to have_received(:call)
     end
 
-    it "calls the RssReader to get all articles" do
+    it "calls the Feeds::Import to get all articles" do
       allow(SiteConfig).to receive(:community_name).and_return("DEV")
       allow(Rails.cache).to receive(:read).with("cancel_feeds_import").and_return(nil)
       allow(Feeds::Import).to receive(:call)
@@ -34,6 +34,21 @@ RSpec.describe Articles::DevFeedsImportWorker, type: :worker do
       worker.perform
 
       expect(Feeds::Import).to have_received(:call)
+    end
+
+    context "with user ids" do
+      before do
+        allow(SiteConfig).to receive(:community_name).and_return("DEV")
+      end
+
+      it "calls Feeds::Import with the correct users if given user ids" do
+        user = create(:user)
+        allow(Feeds::Import).to receive(:call)
+
+        worker.perform([user.id])
+
+        expect(Feeds::Import).to have_received(:call).with(users: User.where(id: [user.id]))
+      end
     end
   end
 end
