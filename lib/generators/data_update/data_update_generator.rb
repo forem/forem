ed32ddfb6bd468a9f6a16fbd41c708ae72ec99ba
Michael Kohl <citizen428@dev.to>
2020-11-04 09:10:49 +0700

    Use full path instead of filename for require

--- lib/generators/data_update/data_update_generator.rb
+++ lib/generators/data_update/data_update_generator.rb
@@ -3,10 +3,7 @@ class DataUpdateGenerator < Rails::Generators::NamedBase
   class_option :spec, type: :boolean, default: true
 
   def create_data_update_file
-    template(
-      "data_update.rb.tt",
-      File.join("lib", "data_update_scripts", class_path, script_name),
-    )
+    template("data_update.rb.tt", script_path)
 
     return unless options["spec"]
 
@@ -16,7 +13,10 @@ class DataUpdateGenerator < Rails::Generators::NamedBase
     )
   end
 
-  def script_name
-    @script_name ||= "#{Time.current.utc.strftime('%Y%m%d%H%M%S')}_#{file_name}.rb"
+  def script_path
+    @script_path ||= begin
+      script_name = "#{Time.current.utc.strftime('%Y%m%d%H%M%S')}_#{file_name}.rb"
+      File.join("lib", "data_update_scripts", class_path, script_name)
+    end
   end
 end
