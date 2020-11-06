178ae2267e402fd7726fcf8241d78a34dd5baac7
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2020-11-06 12:11:46 +0530

    Fix inline PR comments

--- app/liquid_tags/emgithub_tag.rb
+++ app/liquid_tags/emgithub_tag.rb
@@ -3,12 +3,12 @@ require "cgi"
 class EmgithubTag < LiquidTagBase
   PARTIAL = "liquids/emgithub".freeze
   OPTION_REGEXP = /\A(showBorder|showLineNumbers|showFileMeta)*\z/.freeze
-  LINK_REGEXP = %r{\A(http|https)://github.com/[a-zA-Z0-9]*/[a-zA-Z0-9]*/blob/[a-zA-Z0-9_./#-]*\z}.freeze
+  LINK_REGEXP = %r{\A(http|https)://github.com/\w+/\w+/blob/[a-zA-Z0-9_./#-]*\z}.freeze
 
   def initialize(_tag_name, link, _parse_context)
     super
     @link = parse_link(link)
-    @build_options = parse_options(link)
+    @build_options = parse_options
   end
 
   def render(_context)
@@ -28,21 +28,15 @@ class EmgithubTag < LiquidTagBase
     option.match(OPTION_REGEXP)
   end
 
-  def parse_options(input)
-    stripped_link = ActionController::Base.helpers.strip_tags(input)
-    _, *options = stripped_link.split(" ")
-
-    # Validation
-    validated_options = options.map { |option| valid_option(option) }.reject(&:nil?)
-    raise StandardError, "Invalid Options" unless options.empty? || !validated_options.empty?
-
-    options.length.zero? ? "" : validated_options.join("=on&")
+  def parse_options
+    options = %w[showBorder showLineNumbers showFileMeta]
+    options.join("=on&")
   end
 
   def parse_link(link)
     stripped_link = ActionController::Base.helpers.strip_tags(link)
     the_link = stripped_link.split(" ").first
-    raise StandardError, "Invalid Github URL" unless valid_link?(the_link)
+    raise StandardError, "Invalid GitHub URL" unless valid_link?(the_link)
 
     CGI.escape(the_link)
   end
