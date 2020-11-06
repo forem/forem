2a2afd923863088b538ce2f43d87c46a3f933e29
Michael Kohl <citizen428@dev.to>
2020-11-06 11:50:16 +0700

    Finalize service generator

--- lib/generators/service/service_generator.rb
+++ lib/generators/service/service_generator.rb
@@ -2,18 +2,20 @@ class ServiceGenerator < Rails::Generators::NamedBase
   source_root File.expand_path("templates", __dir__)
 
   argument :arguments, type: :array, default: [], banner: "arguments for .call"
-  class_option :module, type: :string, desc: "Containing module for the service"
-  class_option :specs, type: :boolean, default: true
+  class_option :spec, type: :boolean, default: true
 
   def create_service_file
-    service_dir = Pathname.new("app/services").join(module_name)
-    FileUtils.mkdir_p(service_dir) unless Dir.exist?(service_dir)
+    template(
+      "service.erb",
+      File.join("app/services", class_path, "#{file_name}.rb"),
+    )
 
-    template("service.erb", service_dir.join("#{file_name}.rb"))
-  end
+    return unless options["spec"]
 
-  def module_name
-    @module_name ||= options[:module].to_s.downcase
+    template(
+      "service_spec.erb",
+      File.join("spec/services", class_path, "#{file_name}_spec.rb"),
+    )
   end
 
   def signature
