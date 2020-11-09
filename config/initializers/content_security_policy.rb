05d3fc86fd5afa831be5dc88e0a5eda915241551
rhymes <rhymes@hey.com>
2020-11-09 09:33:21 +0100

    Fix app:update error

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,24 +4,23 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https
+Rails.application.config.content_security_policy do |policy|
+  #   policy.default_src :self, :https
+  #   policy.font_src    :self, :https, :data
+  #   policy.img_src     :self, :https, :data
+  #   policy.object_src  :none
+  #   policy.script_src  :self, :https
+  #   policy.style_src   :self, :https
 
-# If you are using webpack-dev-server then specify webpack-dev-server host
-if Rails.env.development?
-  webpack_host = "#{ApplicationConfig.app_domain_no_port}:3035"
-  policy.connect_src :self, :https, "http://#{webpack_host}", "ws://#{webpack_host}"
+  # If you are using webpack-dev-server then specify webpack-dev-server host
+  if Rails.env.development?
+    webpack_host = "#{ApplicationConfig.app_domain_no_port}:3035"
+    policy.connect_src :self, :https, "http://#{webpack_host}", "ws://#{webpack_host}"
+  end
+
+  #   # Specify URI for violation reports
+  #   # policy.report_uri "/csp-violation-report-endpoint"
 end
-
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
-
-# end
 
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
