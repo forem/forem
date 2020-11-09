f56e1d5590d6aee871dd77d89cd7e1d849dcaa9c
mstruve <mollylbs@gmail.com>
2020-11-09 09:32:07 -0500

    Refactor:Remove more_experience_level_weight_experiment From Feed Tests

--- app/controllers/stories/feeds_controller.rb
+++ app/controllers/stories/feeds_controller.rb
@@ -5,10 +5,7 @@ module Stories
     VARIANTS = {
       "more_tag_weight_more_random_experiment" => :more_tag_weight_more_random_experiment,
       "more_comments_experiment" => :more_comments_experiment,
-      "more_experience_level_weight_experiment" => :more_experience_level_weight_experiment,
       "more_tag_weight_randomized_at_end_experiment" => :more_tag_weight_randomized_at_end_experiment,
-      "more_experience_level_weight_randomized_at_end_experiment" =>
-        :more_experience_level_weight_randomized_at_end_experiment,
       "more_comments_randomized_at_end_experiment" => :more_comments_randomized_at_end_experiment,
       "more_comments_medium_weight_randomized_at_end_experiment" =>
         :more_comments_medium_weight_randomized_at_end_experiment,
