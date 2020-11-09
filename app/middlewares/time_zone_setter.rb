788c0b39ca2742cd61a4103a0b81cf4be6926c0c
rhymes <rhymes@hey.com>
2020-11-09 09:36:17 +0100

    Move middleware in the correct place

---
+++ app/middlewares/time_zone_setter.rb
@@ -0,0 +1,11 @@
+class TimeZoneSetter
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    Time.zone = ActiveSupport::TimeZone.new(ENV["TZ"]) if ENV["TZ"]
+
+    @app.call(env)
+  end
+end
