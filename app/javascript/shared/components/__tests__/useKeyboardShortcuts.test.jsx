08d69bc1a709409a8bd0d1eedcbbcc5ee560d552
Andrew Bone <AndrewB05@gmail.com>
2020-11-10 14:21:39 +0000

    Add chaining to shortcuts (#11113)
    Co-authored-by: Nick Taylor <nick@iamdeveloper.com>

--- app/javascript/shared/components/__tests__/useKeyboardShortcuts.test.jsx
+++ app/javascript/shared/components/__tests__/useKeyboardShortcuts.test.jsx
@@ -1,7 +1,7 @@
 import { h } from 'preact';
 import { renderHook } from '@testing-library/preact-hooks';
 import { fireEvent, render } from '@testing-library/preact';
-import { KeyboardShortcuts, useKeyboardShortcuts } from '../useKeyboardShortcuts.jsx';
+import { KeyboardShortcuts, useKeyboardShortcuts } from '../useKeyboardShortcuts';
 
 describe('Keyboard shortcuts for components', () => {
   describe('useKeyboardShortcuts', () => {
@@ -18,6 +18,40 @@ describe('Keyboard shortcuts for components', () => {
       expect(shortcut.KeyK).toHaveBeenCalledTimes(1);
     });
     
+    it('should fire a function when chained keydown is detected', () => {
+      const shortcut = {
+        "KeyA~KeyB": jest.fn()
+      };
+
+      renderHook(() =>
+        useKeyboardShortcuts(shortcut, document),
+      );
+      fireEvent.keyDown(document, { code: "KeyA" });
+      fireEvent.keyDown(document, { code: "KeyB" });
+
+      expect(shortcut["KeyA~KeyB"]).toHaveBeenCalledTimes(1);
+    });
+    
+    it('should not fire a function when chained keydown is missed, timeout should only be 0ms', async () => {
+      const shortcut = {
+        "KeyA~KeyB": jest.fn()
+      };
+      
+      const timeout = 0;
+
+      renderHook(() =>
+        useKeyboardShortcuts(shortcut, document, { timeout }),
+      );
+      fireEvent.keyDown(document, { code: "KeyA" });
+      
+      await new Promise(resolve => setTimeout(() => {
+        fireEvent.keyDown(document, { code: "KeyB" });
+        resolve();
+      }, 25));
+
+      expect(shortcut["KeyA~KeyB"]).not.toHaveBeenCalled();
+    });
+    
     it('should not add event listener if shortcut object is empty', () => {
       HTMLDocument.prototype.addEventListener = jest.fn();
 
@@ -30,11 +64,13 @@ describe('Keyboard shortcuts for components', () => {
 
     it('should add event listener to window', () => {
       HTMLDocument.prototype.addEventListener = jest.fn();
+      
+      const shortcut = {
+        KeyK: null
+      }
 
       renderHook(() =>
-        useKeyboardShortcuts({
-          KeyK: null
-        }, document),
+        useKeyboardShortcuts(shortcut, document),
       );
 
       expect(HTMLDocument.prototype.addEventListener).toHaveBeenCalledTimes(1);
@@ -58,9 +94,13 @@ describe('Keyboard shortcuts for components', () => {
     it('should remove event listener when the hook is unmounted', () => {
       HTMLDocument.prototype.addEventListener = jest.fn();
       HTMLDocument.prototype.removeEventListener = jest.fn();
+      
+      const shortcut = {
+        KeyK: null
+      }
 
       const { unmount } = renderHook(() =>
-        useKeyboardShortcuts({ KeyK: null }, document),
+        useKeyboardShortcuts(shortcut, document),
       );
 
       unmount();
