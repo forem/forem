94185643cd8ff86bd4daf40e9b6f63763ff8340a
manasa2850 <manasakashyap28@gmail.com>
2020-11-16 01:17:34 +0530

    added validations

--- app/models/podcast_ownership.rb
+++ app/models/podcast_ownership.rb
@@ -1,4 +1,6 @@
 class PodcastOwnership < ApplicationRecord
   belongs_to :owner, class_name: "User"
   belongs_to :podcast
+  validates :podcast_id, uniqueness: { scope: :owner_id }
+  validates :podcast_id, :owner_id, :role, presence: true
 end
