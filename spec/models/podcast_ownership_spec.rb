d5b6bd4e449341163f92b4c952d6550b7cf0241e
manasa2850 <manasakashyap28@gmail.com>
2020-11-16 20:00:51 +0530

    changed spec

--- spec/models/podcast_ownership_spec.rb
+++ spec/models/podcast_ownership_spec.rb
@@ -1,11 +1,11 @@
 require "rails_helper"
 
 RSpec.describe PodcastOwnership, type: :model do
-  let(:podcast_episode) { create(:podcast_episode) }
+  let(:podcast_ownership) { create(:podcast_ownership) }
 
   describe "validations" do
     describe "builtin validations" do
-      subject { podcast_episode }
+      subject { podcast_ownership }
 
       it { is_expected.to belong_to(:podcast) }
       it { is_expected.to belong_to(:user) }
