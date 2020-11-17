8e439e65fae2a0a3c4844a7ce371c32ce7fcb478
Nick Taylor <nick@dev.to>
2020-11-17 17:33:26 -0500

    Added some more comments.

--- app/javascript/utilities/preact/index.js
+++ app/javascript/utilities/preact/index.js
@@ -7,12 +7,15 @@ import { unmountComponentAtNode } from 'preact/compat';
  * Note: This function is calling Preact's render under the hood along with
  * a call to InstantClick's change event that explicitly unmounts the component rendered.
  *
- * @param {ComponentChild} vnode
- * @param {Element | Document | ShadowRoot | DocumentFragment} parent
- * @param {Element | Text} replaceNode
+ * @param {object} vnode The virtual DOM node representing a component.
+ * @param {Element | Document | ShadowRoot | DocumentFragment} parent The DOM element where the component will render.
+ * @param {Element | Text} replaceNode An optional DOM element that must of the given parent.
+ * Instead of inferring where to start rendering, it will update or replace the passed element using Preact's diffing algorithm.
  */
 export function render(vnode, parent, replaceNode) {
   InstantClick.on('change', () => {
+    // We need to explicitly unmount a Preact component since we are using InstantClick. Failing to do so
+    // would leave references to past components.
     parent && unmountComponentAtNode(parent);
   });
 
