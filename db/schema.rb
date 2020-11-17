285439916c5c9aa0249b8ca59741584640dfad0b
manasa2850 <manasakashyap28@gmail.com>
2020-11-17 22:18:16 +0530

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_11_14_151157) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pgcrypto"
