e5958dda9eeeaedb0421f0eae656feef95ee80f1
Ben Halpern <bendhalpern@gmail.com>
2020-11-17 08:43:46 -0500

    Allow Forems to specify which domains are allowed for registration (#11442)
    * Allow authors to restrict which emails can sign up
    
    * Add form and tests
    
    * Check for email presence in allowed email flow
    
    * Fix test domains
    
    * Fix codeclimate issue

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -23,6 +23,7 @@ class RegistrationsController < Devise::RegistrationsController
       resource.registered = true
       resource.registered_at = Time.current
       resource.editor_version = "v2"
+      check_allowed_email(resource) if resource.email.present?
       resource.save if resource.email.present?
       yield resource if block_given?
       if resource.persisted?
@@ -56,5 +57,14 @@ class RegistrationsController < Devise::RegistrationsController
   def recaptcha_verified?
     recaptcha_params = { secret_key: SiteConfig.recaptcha_secret_key }
     params["g-recaptcha-response"] && verify_recaptcha(recaptcha_params)
+  end
+
+  def check_allowed_email(resource)
+    domain = resource.email.split("@").last
+    allow_list = SiteConfig.allowed_registration_email_domains
+    return if allow_list.empty? || allow_list.include?(domain)
+
+    resource.email = nil
+    resource.errors.add(:email, "is not included in allowed domains.")
   end
 end
