57db86efb7d8ac62590136b32c1126ea250a7d99
manasa2850 <manasakashyap28@gmail.com>
2020-11-17 09:52:00 +0530

    modified spec file

--- spec/models/podcast_ownership_spec.rb
+++ spec/models/podcast_ownership_spec.rb
@@ -3,8 +3,10 @@ require "rails_helper"
 RSpec.describe PodcastOwnership, type: :model do
   let(:podcast_ownership) { create(:podcast_ownership) }
 
-  it { is_expected.to belong_to(:podcast) }
-  it { is_expected.to belong_to(:user) }
-  it { is_expected.to validate_presence_of(:podcast_id) }
-  it { is_expected.to validate_presence_of(:user_id) }
+  describe "validations" do
+    it { is_expected.to belong_to(:podcast) }
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to validate_presence_of(:podcast_id) }
+    it { is_expected.to validate_presence_of(:user_id) }
+  end
 end
