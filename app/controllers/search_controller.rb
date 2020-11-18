f70e6cd960cac80ad5439bffea819d161aaa11e4
mstruve <mollylbs@gmail.com>
2020-11-18 17:16:03 -0500

    ensure user is authenticated to use usernames endpoint

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
-  before_action :authenticate_user!, only: %i[tags chat_channels reactions]
+  before_action :authenticate_user!, only: %i[tags chat_channels reactions usernames]
   before_action :format_integer_params
   before_action :sanitize_params, only: %i[listings reactions feed_content]
 
