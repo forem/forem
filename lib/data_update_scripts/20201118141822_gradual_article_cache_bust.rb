2c179994ed0cca2af6f842d4d4200a4318017c91
benhalpern <bendhalpern@gmail.com>
2020-11-18 09:56:44 -0500

    ?i=i variant

--- lib/data_update_scripts/20201118141822_gradual_article_cache_bust.rb
+++ lib/data_update_scripts/20201118141822_gradual_article_cache_bust.rb
@@ -8,13 +8,16 @@ module DataUpdateScripts
       # This uses exponential backoff in order to bust the caches of more recent articles first, but not
       # bust everything in too short a period, so that the edge cache can gradually re-heat without everything
       # going cold at once. Articles with high "hotness score" (e.g. more recent) are the ones most in need of busting.
+
+      # It only busts the "?i=i" variant of the path which is the "internal nav" version, aka the page when swapped
+      # with internal navigation, not the one landed on directly (which wouldn't have cache mismatches)
       Article.published.order("hotness_score DESC").limit(2000).each_with_index do |article, index|
         n = index + 300 # + 300 gives the server time to boot up
-        BustCachePathWorker.set(queue: :high_priority).perform_in(n.seconds, article.path)
+        BustCachePathWorker.set(queue: :high_priority).perform_in(n.seconds, "#{article.path}?i=i")
       end
       Article.published.order("hotness_score DESC").offset(2500).limit(5000).each_with_index do |article, index|
         n = (index * 3) + 450
-        BustCachePathWorker.set(queue: :medium_priority).perform_in(n.seconds, article.path)
+        BustCachePathWorker.set(queue: :medium_priority).perform_in(n.seconds, "#{article.path}?i=i")
       end
     end
   end
