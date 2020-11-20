e180c48177d1064009417908a9215d1ff030ffea
rhymes <rhymes@hey.com>
2020-11-20 13:28:23 +0100

    Add feature flag to RssReaderWorker

--- app/workers/articles/rss_reader_worker.rb
+++ app/workers/articles/rss_reader_worker.rb
@@ -5,13 +5,12 @@ module Articles
     sidekiq_options queue: :medium_priority, retry: 10
 
     def perform
-      # Temporary
-      # @sre:mstruve This is temporary until we have an efficient way to handle this job
-      # for our large DEV community. Smaller Forems should be able to handle it no problem
-      return if SiteConfig.community_name == "DEV"
-
-      # don't force fetch. Fetch "random" subset instead of all of them.
-      RssReader.get_all_articles(force: false)
+      if FeatureFlag.enabled?(:feeds_import)
+        ::Feeds::ImportArticlesWorker.perform_async
+      else
+        # don't force fetch. Fetch "random" subset instead of all of them.
+        ::RssReader.get_all_articles(force: false)
+      end
     end
   end
 end
--- spec/workers/articles/rss_reader_worker_spec.rb
+++ spec/workers/articles/rss_reader_worker_spec.rb
@@ -8,8 +8,28 @@ RSpec.describe Articles::RssReaderWorker, type: :worker do
   describe "#perform" do
     it "updates RssReader articles" do
       allow(RssReader).to receive(:get_all_articles)
+
       worker.perform
+
       expect(RssReader).to have_received(:get_all_articles).with(force: false)
+    end
+
+    it "does not enqueue Feeds::ImportArticlesWorker if the :feeds_import flag is not enabled" do
+      allow(RssReader).to receive(:get_all_articles)
+
+      sidekiq_assert_no_enqueued_jobs do
+        worker.perform
+      end
+
+      expect(RssReader).to have_received(:get_all_articles).with(force: false)
+    end
+
+    it "enqueues Feeds::ImportArticlesWorker if the :feeds_import flag is enabled" do
+      allow(FeatureFlag).to receive(:enabled?).with(:feeds_import).and_return(true)
+
+      sidekiq_assert_enqueued_jobs(1, only: Feeds::ImportArticlesWorker) do
+        worker.perform
+      end
     end
   end
 end
