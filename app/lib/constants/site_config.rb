acb4eb7460dab9a361683f6292e7290156288a94
Mac Siri <krairit.siri@gmail.com>
2020-11-20 16:47:59 -0500

    Set social image to 16:9 ratio

--- app/lib/constants/site_config.rb
+++ app/lib/constants/site_config.rb
@@ -178,7 +178,7 @@ module Constants
         placeholder: SVG_PLACEHOLDER
       },
       main_social_image: {
-        description: "Used as the main image in social networks and OpenGraph",
+        description: "Used as the main image in social networks and OpenGraph. Recommended aspect ratio of 16:9 (600x337px,1200x675px)",
         placeholder: IMAGE_PLACEHOLDER
       },
       mailchimp_api_key: {
