3486a32b9cfa33931e6c070f4cd583441f420fe0
spencer page <pagespence@yahoo.com>
2020-11-19 23:44:14 -0800

    add tests

--- spec/requests/api/v0/organizations_spec.rb
+++ spec/requests/api/v0/organizations_spec.rb
@@ -25,4 +25,41 @@ RSpec.describe "Api::V0::Organizations", type: :request do
       expect(response_organization["joined_at"]).to eq(organization.created_at.utc.iso8601)
     end
   end
+
+  describe "GET /api/organizations/:username/users" do
+    let!(:org_user) { create(:user, :org_member) }
+    let(:organization) { org_user.organizations.first }
+
+    it "returns 404 if the organizations username is not found" do
+      get "/api/organizations/invalid-username/users"
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "supports pagination" do
+      create(:organization_membership, user: create(:user), organization: organization)
+
+      get "/api/organizations/#{organization.username}/users", params: { page: 1, per_page: 1 }
+      expect(response.parsed_body.length).to eq(1)
+
+      get "/api/organizations/#{organization.username}/users", params: { page: 2, per_page: 1 }
+      expect(response.parsed_body.length).to eq(1)
+
+      get "/api/organizations/#{organization.username}/users", params: { page: 3, per_page: 1 }
+      expect(response.parsed_body.length).to eq(0)
+    end
+
+    it "returns the correct json representation of the organizations users", :aggregate_failures do
+      get "/api/organizations/#{organization.username}/users"
+
+      response_org_users = response.parsed_body
+
+      %w[
+        username name twitter_username github_username website_url
+      ].each do |attr|
+        expect(response_org_users.first[attr]).to eq(org_user.public_send(attr))
+      end
+
+      expect(response_org_users.first["profile_image_url"]).to eq(org_user["profile_image_url"])
+    end
+  end
 end
