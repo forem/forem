14e561fd28a221368e93d6a118e26174ff3f95e9
Nick Taylor <nick@dev.to>
2020-11-20 22:21:54 -0500

    Added some more API docs.

--- app/javascript/utilities/__tests__/search.test.js
+++ app/javascript/utilities/__tests__/search.test.js
@@ -5,7 +5,6 @@ import {
   hasInstantClick,
   displaySearchResults,
   fetchSearch,
-  createSearchUrl,
 } from '../search';
 import '../../../assets/javascripts/lib/xss';
 
@@ -257,15 +256,6 @@ describe('Search utilities', () => {
 
       expect(response).toBeInstanceOf(Object);
       expect(response).toMatchObject(expected);
-    });
-  });
-
-  describe('createSearchUrl', () => {
-    it('should return a url string', () => {
-      const dataHash = { name: 'jav', tags: ['one', 'two'] };
-      const responseString = createSearchUrl(dataHash);
-
-      expect(responseString).toEqual('name=jav&tags%5B%5D=one&tags%5B%5D=two');
     });
   });
 });
