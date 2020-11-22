11a450aa7f308ff33a88685b8c24f1d12c57df9a
benhalpern <bendhalpern@gmail.com>
2020-11-22 11:16:34 -0500

    Changed to find_each

--- lib/data_update_scripts/20201120001727_populate_explicit_follow_points.rb
+++ lib/data_update_scripts/20201120001727_populate_explicit_follow_points.rb
@@ -1,7 +1,7 @@
 module DataUpdateScripts
   class PopulateExplicitFollowPoints
     def run
-      Follow.where("points != 1").each do |follow|
+      Follow.where("points != 1").find_each do |follow|
         follow.update_column(:explicit_points, follow.points)
       end
     end
