dc6e0c0d6882e5670c97b4aa7b35632b4affa4a6
manasa2850 <manasakashyap28@gmail.com>
2020-11-23 15:14:32 +0530

    fixed codeclimate

--- app/controllers/podcast_ownerships_controller.rb
+++ app/controllers/podcast_ownerships_controller.rb
@@ -1,40 +1,40 @@
 class PodcastOwnershipsController < ApplicationController
-    def new
-        @podcast_ownership = PodcastOwnership.new
+  def new
+    @podcast_ownership = PodcastOwnership.new
+  end
+
+  def create
+    @podcast_ownership = PodcastOwnership.new(podcast_ownership_params)
+
+    if @podcast_ownership.save
+      flash[:success] = "Owner created successfully"
+      redirect_to pod_path
+    else
+      flash[:error] = "User is already an owner"
+      format.html { render :new }
     end
+  end
 
-    def create
-        @podcast_ownership = PodcastOwnership.new(podcast_ownership_params)
+  def edit
+    @podcast_ownership = PodcastOwnership.find(params[:id])
+  end
 
-        if @podcast_ownership.save
-            flash[:success] = "Owner created successfully"
-            redirect_to pod_path
-        else
-            flash[:error] = "User is already an owner"
-            format.html {render :new}
-        end
+  def update
+    @podcast_ownership = PodcastOwnership.find(params[:id])
+
+    if @podcast_ownership.update(podcast_ownership_params)
+      @podcast_ownership.save
+      flash[:success] = "Updated successfully"
+      redirect_to pod_path
+    else
+      flash[:error] = "Error Updating!"
+      format.html { render :edit }
     end
+  end
 
-    def edit
-        @podcast_ownership = PodcastOwnership.find(params[:id])
-    end
-
-    def update
-        @podcast_ownership = PodcastOwnership.find(params[:id])
-
-        if @podcast_ownership.update_attributes(podcast_ownership_params)
-            @podcast_ownership.save
-            flash[:success] = "Updated successfully"
-            redirect_to pod_path
-        else
-            flash[:error] = "Error Updating!"
-            format.html {render :edit}
-        end
-    end
-
-    def destroy
-        @podcast_ownership = PodcastOwnership.find(params[:id])
-        @podcast_ownership.destroy
-        redirect_to pod_path
-    end
+  def destroy
+    @podcast_ownership = PodcastOwnership.find(params[:id])
+    @podcast_ownership.destroy
+    redirect_to pod_path
+  end
 end
