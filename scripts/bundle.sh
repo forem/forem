0605b56f3f5dd1519d667d1a05ed89a70ebec12a
Joe Doss <jdoss@forem.com>
2020-11-23 09:13:48 -0600

    Change bundle_finished path and update Yarn docker-compose command.

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-if [ -f /opt/apps/forem/bundle_finished ]; then
-  rm -f /opt/apps/forem/bundle_finished
+if [ -f /opt/apps/forem/vendor/bundle/.bundle_finished ]; then
+  rm -f /opt/apps/forem/vendor/bundle/.bundle_finished
 fi
 
 unset BUNDLE_WITHOUT
 bundle config --delete without
 bundle install --local --jobs 20 --retry 5 --path vendor/bundle
-echo $(date --utc +%FT%T%Z) > /opt/apps/forem/bundle_finished
+echo $(date --utc +%FT%T%Z) > /opt/apps/forem/vendor/bundle/.bundle_finished
