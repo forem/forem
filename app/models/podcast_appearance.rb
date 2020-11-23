a1d121cd53276425264ab7bf560858a9442f52ed
NenadPantelic <nenadpantelickg@gmail.com>
2020-11-23 13:10:49 +0100

    Add podcast_appearance role validation forem(#82)

--- app/models/podcast_appearance.rb
+++ app/models/podcast_appearance.rb
@@ -2,6 +2,8 @@ class PodcastAppearance < ApplicationRecord
   belongs_to :user
   belongs_to :podcast_episode
   validates :podcast_episode_id, uniqueness: { scope: :user_id }
+  validates :podcast_episode_id, :user_id, :role, presence: true
+  validates :role, inclusion: { in: %w[host guest], message: "%<value> is not a valid role" }
 
   def podcast_episode_creator_id
     podcast_episode.podcast.creator_id
