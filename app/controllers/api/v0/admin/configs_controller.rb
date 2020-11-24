6caf16600dadc5679558ecdec4dc1e1de54a1d94
benhalpern <bendhalpern@gmail.com>
2020-11-24 14:48:39 -0500

    Add test

--- app/controllers/api/v0/admin/configs_controller.rb
+++ app/controllers/api/v0/admin/configs_controller.rb
@@ -2,21 +2,11 @@ module Api
   module V0
     module Admin
       class ConfigsController < ApiController
-        before_action :authorize_admin
-        after_action :verify_authorized
+        before_action :authenticate!
+        before_action :authorize_super_admin
 
         def show
           @site_configs = SiteConfig.all
-        end
-
-        private
-
-        def authorization_resource
-          self.class.name.demodulize.sub("Controller", "").singularize.constantize
-        end
-
-        def authorize_admin
-          authorize(authorization_resource, :access?, policy_class: InternalPolicy)
         end
       end
     end
--- spec/requests/api/v0/admin/configs_spec.rb
+++ spec/requests/api/v0/admin/configs_spec.rb
@@ -1,18 +1,48 @@
 require "rails_helper"
 
 RSpec.describe "Api::V0::Admin::Configs", type: :request do
-  let(:user) { create(:user, :super_admin) } # not used by every spec but lower times overall
+  let(:api_secret) { create(:api_secret) }
+  let(:user) { api_secret.user }
 
   describe "GET /api/admin/configs/all" do
-    context "super admin signed in" do
+    before do
+      SiteConfig.community_name = "ANYTHING"
+    end
+
+    context "when user is super admin" do
       before do
-        sign_in user
+        user.add_role(:super_admin)
       end
 
-      it "returns json" do
+      it "renders json when passed key" do
+        headers = { "api-key" => api_secret.secret, "content-type" => "application/json" }
+        get "/api/admin/configs/all", headers: headers
+
+        expect(response.parsed_body["community_name"]).to eq SiteConfig.community_name
+      end
+
+      it "renders json when signed in" do
+        sign_in user
         get "/api/admin/configs/all"
 
-        expect(response.parsed_body).to eq "Array"
+        expect(response.parsed_body["community_name"]).to eq SiteConfig.community_name
+      end
+    end
+
+    context "when user is not super admin" do
+      it "renders unauthorized json" do
+        headers = { "api-key" => api_secret.secret, "content-type" => "application/json" }
+        get "/api/admin/configs/all", headers: headers
+
+        expect(response.status).to eq 401
+        expect(response.parsed_body["error"]).to eq "unauthorized"
+      end
+    end
+
+    context "when no user" do
+      it "renders unauthorized json" do
+        get "/api/admin/configs/all"
+        expect(response.status).to eq 401
       end
     end
   end
