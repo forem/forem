5451482ffa5d009d818443260f8e8e75d6b08106
Molly Struve <mollylbs@gmail.com>
2020-11-24 16:36:55 -0500

    Refactor/Optimization:Use more_comments_minimal_weight_randomized_at_end for Signed In Feed (#11604)

--- app/services/articles/feeds/large_forem_experimental.rb
+++ app/services/articles/feeds/large_forem_experimental.rb
@@ -42,6 +42,11 @@ module Articles
           .order(score: :desc).page(@page).per(@number_of_articles)
       end
 
+      def default_home_feed(user_signed_in: false)
+        _featured_story, stories = default_home_feed_and_featured_story(user_signed_in: user_signed_in, ranking: true)
+        stories
+      end
+
       def latest_feed
         published_articles_by_tag.order(published_at: :desc)
           .where("score > ?", MINIMUM_SCORE_LATEST_FEED)
@@ -54,79 +59,15 @@ module Articles
         [featured_story, hot_stories]
       end
 
-      # Test variation: Base
-      def default_home_feed(user_signed_in: false)
-        _featured_story, stories = default_home_feed_and_featured_story(user_signed_in: user_signed_in, ranking: true)
-        stories
-      end
-
-      # Test variation: the more comments a post has, the higher it's rated!
-      def more_comments_experiment
-        @comment_weight = 2
-        _featured_story, stories = default_home_feed_and_featured_story(user_signed_in: true)
-        stories
-      end
-
-      # Test variation: the more comments a post has, the higher it's rated!
-      def more_comments_medium_weight_experiment
-        @comment_weight = 0.5
-        _featured_story, stories = default_home_feed_and_featured_story(user_signed_in: true)
-        stories
-      end
-
-      # Test variation: the more comments a post has, the higher it's rated!
-      def more_comments_minimal_weight_experiment
+      def more_comments_minimal_weight
         @comment_weight = 0.2
         _featured_story, stories = default_home_feed_and_featured_story(user_signed_in: true)
         stories
       end
 
-      def mix_of_everything_experiment
-        case rand(6)
-        when 0
-          default_home_feed(user_signed_in: true)
-        when 1
-          more_comments_experiment
-        when 2
-          more_tag_weight_randomized_at_end_experiment
-        when 3
-          more_comments_randomized_at_end_experiment
-        when 4
-          more_comments_medium_weight_randomized_at_end_experiment
-        when 5
-          more_comments_minimal_weight_randomized_at_end_experiment
-        else
-          default_home_feed(user_signed_in: true)
-        end
-      end
-
-      # Randomized at end group for next three experiments
-      # Rather than randomizing *during* the ranking, rank solely by quality/match
-      # and randomize the top half of the ranked results.
-      # Resulting in more relevance and still more freshness.
-
-      def more_tag_weight_randomized_at_end_experiment
+      def more_comments_minimal_weight_randomized_at_end
         @randomness = 0
-        @tag_weight = 2
-        _featured_story, results = default_home_feed_and_featured_story(user_signed_in: true)
-        first_half(results).shuffle + last_half(results)
-      end
-
-      def more_comments_randomized_at_end_experiment
-        @randomness = 0
-        results = more_comments_experiment
-        first_half(results).shuffle + last_half(results)
-      end
-
-      def more_comments_medium_weight_randomized_at_end_experiment
-        @randomness = 0
-        results = more_comments_medium_weight_experiment
-        first_half(results).shuffle + last_half(results)
-      end
-
-      def more_comments_minimal_weight_randomized_at_end_experiment
-        @randomness = 0
-        results = more_comments_minimal_weight_experiment
+        results = more_comments_minimal_weight
         first_half(results).shuffle + last_half(results)
       end
 
