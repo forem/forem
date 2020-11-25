b55f26b780ec8d803013019f8d201210beab0f7f
benhalpern <bendhalpern@gmail.com>
2020-11-25 09:29:32 -0500

    Change authentication approach

--- app/controllers/api/v0/admin/configs_controller.rb
+++ app/controllers/api/v0/admin/configs_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V0
     module Admin
       class ConfigsController < ApiController
-        before_action :authenticate!
+        before_action :authenticate_with_api_key_or_current_user!
         before_action :authorize_super_admin
 
         def show
