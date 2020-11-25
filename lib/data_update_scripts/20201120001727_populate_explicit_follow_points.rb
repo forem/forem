ae3775c6bf5ff5232a1c116d2fdf3a41173ea0bc
benhalpern <bendhalpern@gmail.com>
2020-11-25 17:08:57 -0500

    Smarter update script

--- lib/data_update_scripts/20201120001727_populate_explicit_follow_points.rb
+++ lib/data_update_scripts/20201120001727_populate_explicit_follow_points.rb
@@ -2,8 +2,7 @@ module DataUpdateScripts
   class PopulateExplicitFollowPoints
     def run
       Follow.connection.execute('UPDATE "follows" SET "implicit_points" = 0')
-      Follow.connection.execute('UPDATE "follows" SET "explicit_points" = 1')
-      Follow.connection.execute('UPDATE "follows" SET "explicit_points" = "points" WHERE points != 1')
+      Follow.connection.execute('UPDATE "follows" SET "explicit_points" = "points"')
     end
   end
 end
