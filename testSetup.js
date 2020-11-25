eb3f47839def4d4f465f5e4c7a46fbe5e3a7eae1
Nick Taylor <nick@dev.to>
2020-11-25 18:44:51 -0500

    Added a comment about why you might get an unhandled rejection exception.

--- testSetup.js
+++ testSetup.js
@@ -3,5 +3,6 @@ import 'jest-axe/extend-expect';
 import './app/assets/javascripts/lib/xss';
 
 process.on('unhandledRejection', (error) => {
+  // Errors thrown here are typically fetch responses that have not been mocked.
   throw error;
 });
