6122d387efb5edcbeff76e6d4f861aee3c05ec36
benhalpern <bendhalpern@gmail.com>
2020-11-25 17:27:11 -0500

    Fix migrations

--- db/migrate/20201125220029_set_default_points_on_follows.rb
+++ db/migrate/20201125220029_set_default_points_on_follows.rb
@@ -1,6 +1,6 @@
 class SetDefaultPointsOnFollows < ActiveRecord::Migration[6.0]
   def change
-    change_column_default :posts, :explicit_points, from: nil, to: 1.0
-    change_column_default :posts, :implicit_points, from: nil, to: 0.0
+    change_column_default :follows, :explicit_points, from: nil, to: 1.0
+    change_column_default :follows, :implicit_points, from: nil, to: 0.0
   end
 end
