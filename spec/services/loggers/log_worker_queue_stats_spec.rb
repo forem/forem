48ce95cba2b4c8c76dd9d1eb997805c0eb8bcc65
Michael Kohl <citizen428@dev.to>
2020-11-27 11:42:21 +0700

    Move LogWorkerQueueStats from labor to services

--- spec/services/loggers/log_worker_queue_stats_spec.rb
+++ spec/services/loggers/log_worker_queue_stats_spec.rb
@@ -1,15 +1,15 @@
 require "rails_helper"
 
-describe Loggers::LogWorkerQueueStats do
+describe Loggers::LogWorkerQueueStats, type: :service do
   it "logs totals" do
     allow(described_class).to receive(:record_totals)
-    described_class.run
+    described_class.call
     expect(described_class).to have_received(:record_totals)
   end
 
   it "logs queue stats" do
     allow(described_class).to receive(:record_queue_stats)
-    described_class.run
+    described_class.call
     expect(described_class).to have_received(:record_queue_stats)
   end
 end
