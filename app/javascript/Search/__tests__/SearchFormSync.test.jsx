f7168f92615f1851dea2ad55414120872633aa9d
Nick Taylor <nick@dev.to>
2020-11-26 22:33:38 -0500

    Almost 100% coverage with two useful tests.

--- app/javascript/Search/__tests__/SearchFormSync.test.jsx
+++ app/javascript/Search/__tests__/SearchFormSync.test.jsx
@@ -6,6 +6,10 @@ import { SearchFormSync } from '../SearchFormSync';
 // There is nothing UI related about this component.
 describe('<SearchFormSync />', () => {
   beforeEach(() => {
+    delete window.location;
+    window.location = new URL(`https://locahost:3000/`);
+    document.body.innerHTML = '';
+
     global.InstantClick = jest.fn(() => ({
       on: jest.fn(),
       off: jest.fn(),
@@ -35,7 +39,9 @@ describe('<SearchFormSync />', () => {
 
     // This part of the DOM would be rendered in the search results from the server side.
     // See search.html.erb.
-    document.body.innerHTML +=
+    // I'm not sure why, but with a portal being created in the code, it seems at least in the test
+    // scenario, the body needs to be reset.
+    document.body.innerHTML =
       '<div id="mobile-search-container"><form></form></div>';
 
     fireEvent(
@@ -48,9 +54,72 @@ describe('<SearchFormSync />', () => {
     // TODO: Near future during work for #10424. I can't figure out why yet, but only in the test scenario does it generate
     // an extra form. It's appears to be remnants of the previous render. Need to investigate.
     // This is why the first item in the array of elements is skipped.
-    const [, desktopSearch, mobileSearch] = await findAllByLabelText('search');
+    const [desktopSearch, mobileSearch] = await findAllByLabelText('search');
 
     expect(desktopSearch.value).toEqual(searchTerm);
     expect(mobileSearch.value).toEqual(searchTerm);
+  });
+
+  it('should synchronize search forms on a subsequent search', async () => {
+    const { findByLabelText, findAllByLabelText } = render(<SearchFormSync />, {
+      container: document.body,
+    });
+
+    // Only one input is rendered at this point because the synchSearchForms custom event is what
+    // tells us that there is a new search form to sync with the existing one.
+    const searchInput = await findByLabelText('search');
+
+    // Because window.location has no search term in it's URL
+    expect(searchInput.value).toEqual('');
+
+    // https://www.theatlantic.com/technology/archive/2012/09/here-it-is-the-best-word-ever/262348/
+    const searchTerm = 'diphthong';
+
+    // simulates a search result returned which contains the server side rendered search form for mobile only.
+    delete window.location;
+    window.location = new URL(`https://locahost:3000/search?q=${searchTerm}`);
+
+    // This part of the DOM would be rendered in the search results from the server side.
+    // See search.html.erb.
+    document.body.innerHTML =
+      '<div id="mobile-search-container"><form></form></div>';
+
+    fireEvent(
+      window,
+      new CustomEvent('syncSearchForms', {
+        detail: { querystring: window.location.search },
+      }),
+    );
+
+    // TODO: Near future during work for #10424. I can't figure out why yet, but only in the test scenario does it generate
+    // an extra form. It's appears to be remnants of the previous render. Need to investigate.
+    // This is why the first item in the array of elements is skipped.
+    let [desktopSearch, mobileSearch] = await findAllByLabelText('search');
+
+    expect(desktopSearch.value).toEqual(searchTerm);
+    expect(mobileSearch.value).toEqual(searchTerm);
+
+    const searchTerm2 = 'diphthong2';
+
+    // simulates a search result returned which contains the server side rendered search form for mobile only.
+    delete window.location;
+    window.location = new URL(`https://locahost:3000/search?q=${searchTerm2}`);
+
+    // This part of the DOM would be rendered in the search results from the server side.
+    // See search.html.erb.
+    document.body.innerHTML =
+      '<div id="mobile-search-container"><form></form></div>';
+
+    fireEvent(
+      window,
+      new CustomEvent('syncSearchForms', {
+        detail: { querystring: window.location.search },
+      }),
+    );
+
+    [desktopSearch, mobileSearch] = await findAllByLabelText('search');
+
+    expect(desktopSearch.value).toEqual(searchTerm2);
+    expect(mobileSearch.value).toEqual(searchTerm2);
   });
 });
