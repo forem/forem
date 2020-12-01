9a4caafb0e76db28f1a04d4fc0301ab749b196d1
Ridhwana <ridhwana.khan16@gmail.com>
2020-12-01 12:54:43 +0200

    refactor: move the color picker to its own initializer file

---
+++ app/assets/javascripts/initializers/initializeColorPicker.js
@@ -0,0 +1,17 @@
+var pickers = Array.from(document.getElementsByClassName('js-color-field'));
+
+function colorValueChange(e) {
+  var field = e.target;
+  var sibling = '';
+  if (field.nextElementSibling) {
+    sibling = field.nextElementSibling;
+  } else {
+    sibling = field.previousElementSibling;
+  }
+
+  sibling.value = field.value;
+}
+
+pickers.forEach(function (picker) {
+  picker.addEventListener('change', colorValueChange);
+});
