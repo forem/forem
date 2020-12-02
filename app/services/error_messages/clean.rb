c5a29826e8a3bf5424ceba57b91525ec2af0a827
Michael Kohl <citizen428@dev.to>
2020-12-02 11:05:36 +0700

    Move app/labor/error_message_cleaner.rb to service (#11679)

---
+++ app/services/error_messages/clean.rb
@@ -0,0 +1,15 @@
+module ErrorMessages
+  class Clean
+    FRONTMATTER_ERROR = /expected key while parsing a block mapping at line/.freeze
+
+    REPLACEMENT_ERROR = "There was a problem parsing the front-matter YAML. " \
+      "Perhaps you need to escape a quote or a colon or something. " \
+      "Email %s if you are having trouble.".freeze
+
+    def self.call(error_message)
+      return error_message unless error_message.match?(FRONTMATTER_ERROR)
+
+      format(REPLACEMENT_ERROR, SiteConfig.email_addresses[:contact])
+    end
+  end
+end
