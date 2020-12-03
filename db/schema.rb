735398759f0705c9fa9306a3729e879416127d29
rhymes <rhymes@hey.com>
2020-12-03 10:46:48 +0100

    Fix schema.db whitespace

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_11_19_153512) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pgcrypto"
