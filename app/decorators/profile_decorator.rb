6615d4444d22b7764b2669c190783f1738a181b3
Jacob Herrington <jacobherringtondeveloper@gmail.com>
2020-12-03 15:44:50 -0600

    Don't render DEV header fields twice

--- app/decorators/profile_decorator.rb
+++ app/decorators/profile_decorator.rb
@@ -1,8 +1,14 @@
 class ProfileDecorator < ApplicationDecorator
+  DEV_HEADER_FIELDS = %w[employment_title employer_name].freeze
+
   def ui_attributes_for(area:)
     # Return a Hash of the profile fields that should be rendered for a given
     # display area, e.g. :left_sidebar
     names = ProfileField.public_send(area).pluck(:attribute_name)
+    # Temporary workaround: DEV specific header fields are hardcoded in the view
+    if SiteConfig.dev_to?
+      names -= DEV_HEADER_FIELDS
+    end
     data.slice(*names).reject { |_, v| v.blank? }
   end
 end
