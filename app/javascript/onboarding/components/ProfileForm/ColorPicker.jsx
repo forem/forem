3758af3161a9b7e844d0a6952478b657d7180a16
Ridhwana <ridhwana.khan16@gmail.com>
2020-12-07 13:03:16 +0200

    refactor: change from component to function

--- app/javascript/onboarding/components/ProfileForm/ColorPicker.jsx
+++ app/javascript/onboarding/components/ProfileForm/ColorPicker.jsx
@@ -1,43 +1,36 @@
-import { h, Component } from 'preact';
+import { h } from 'preact';
 import PropTypes from 'prop-types';
 import { FormField } from '@crayons';
 
-export default class ColorPicker extends Component {
-  render() {
-    const { onColorChange } = this.props;
-    const {
-      attribute_name,
-      placeholder_text,
-      description,
-      label,
-    } = this.props.field;
+function ColorPicker(props) {
+  const { onColorChange } = props;
+  const { attribute_name, placeholder_text, description, label } = props.field;
 
-    return (
-      <FormField>
-        <label class="crayons-field__label" htmlFor={attribute_name}>
-          {label}
-        </label>
-        <div class="flex items-center w-100 m:w-50">
-          <input
-            placeholder={placeholder_text}
-            class="crayons-textfield js-color-field"
-            type="text"
-            name={attribute_name}
-            id={attribute_name}
-            onChange={onColorChange}
-          />
-          <input
-            class="crayons-color-selector js-color-field ml-2"
-            type="color"
-            name={attribute_name}
-            id={attribute_name}
-            onChange={onColorChange}
-          />
-        </div>
-        {description && <p class="crayons-field__description">{description}</p>}
-      </FormField>
-    );
-  }
+  return (
+    <FormField>
+      <label class="crayons-field__label" htmlFor={attribute_name}>
+        {label}
+      </label>
+      <div class="flex items-center w-100 m:w-50">
+        <input
+          placeholder={placeholder_text}
+          class="crayons-textfield js-color-field"
+          type="text"
+          name={attribute_name}
+          id={attribute_name}
+          onChange={onColorChange}
+        />
+        <input
+          class="crayons-color-selector js-color-field ml-2"
+          type="color"
+          name={attribute_name}
+          id={attribute_name}
+          onChange={onColorChange}
+        />
+      </div>
+      {description && <p class="crayons-field__description">{description}</p>}
+    </FormField>
+  );
 }
 
 ColorPicker.propTypes = {
@@ -48,3 +41,5 @@ ColorPicker.propTypes = {
     label: PropTypes.string.isRequired,
   }).isRequired,
 };
+
+export default ColorPicker;
