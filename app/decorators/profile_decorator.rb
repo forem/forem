eb305c05eca7cfa3de01821477479977c278653d
Jacob Herrington <jacobherringtondeveloper@gmail.com>
2020-12-07 09:57:09 -0600

    Move comment for documentation generators

--- app/decorators/profile_decorator.rb
+++ app/decorators/profile_decorator.rb
@@ -1,9 +1,9 @@
 class ProfileDecorator < ApplicationDecorator
   DEV_HEADER_FIELDS = %w[employment_title employer_name].freeze
 
+  # Return a Hash of the profile fields that should be rendered for a given
+  # display area, e.g. :left_sidebar
   def ui_attributes_for(area:)
-    # Return a Hash of the profile fields that should be rendered for a given
-    # display area, e.g. :left_sidebar
     names = ProfileField.public_send(area).pluck(:attribute_name)
     # Temporary workaround: DEV specific header fields are hardcoded in the view
     if SiteConfig.dev_to?
