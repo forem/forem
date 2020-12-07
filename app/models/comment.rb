b578e963f0e3c6b75e56c42f3b97fcec8f88ee9f
rhymes <rhymes@hey.com>
2020-12-07 18:55:41 +0100

    Use custom, dynamic, error pages (#11744)
    * Use ErrorsController to render error pages
    
    * Hide GitHub bug report prompt from anything other than dev.to and forem.dev
    
    * Update service worker to use 404 and 500
    
    * Other fixes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -75,7 +75,7 @@ class Comment < ApplicationRecord
   def path
     "/#{user.username}/comment/#{id_code_generated}"
   rescue StandardError
-    "/404.html"
+    "/404"
   end
 
   def parent_or_root_article
