86eead7a97a9fe266c0e39ffed2cce5937409add
Ridhwana <Ridhwana.Khan16@gmail.com>
2020-12-08 17:50:43 +0200

    Replace the "setup onboarding profile fields" temp rake task with a data update script (#11800)
    * feat: replace the temp rake task with a data update script
    
    * oops we need a user

---
+++ spec/lib/data_update_scripts/set_onboarding_profile_fields_for_existing_forems_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+require Rails.root.join(
+  "lib/data_update_scripts/20201208151516_set_onboarding_profile_fields_for_existing_forems.rb",
+)
+
+describe DataUpdateScripts::SetOnboardingProfileFieldsForExistingForems do
+  before do
+    ProfileField.destroy_all
+  end
+
+  let!(:user) { create(:user) }
+  let!(:profile_field1) { create(:profile_field, label: "summary") }
+  let!(:profile_field2) { create(:profile_field, label: "random") }
+  let(:profile_field3) { create(:profile_field, label: "location") }
+
+  it "toggles show_in_onboarding to true for specific profile fields" do
+    expect do
+      described_class.new.run
+    end.to change { profile_field1.reload.show_in_onboarding }.from(false).to(true)
+    expect(profile_field2.reload.show_in_onboarding).to be false
+  end
+
+  it "updates the labels for specific profile fields" do
+    # note: we update the label manually here because when
+    # we create the profile field the attribute_name is a
+    # underscored version of the label.
+    profile_field3.update(label: "Where are you located?")
+    described_class.new.run
+    expect(profile_field3.reload.label).to eq("Location")
+  end
+end
