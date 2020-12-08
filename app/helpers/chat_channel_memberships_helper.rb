b9822efcff461f0ce6a1f06e9e40465de0100fd4
Sarthak Sharma <7lovesharma7@gmail.com>
2020-12-08 11:40:36 +0530

    🚀 Ability to allow tag moderators to create chat channels (#9741)
    * Feature 🚀 : Ability to delete messages in chat channels
    
    - Sending message ID to frontend
    - Deleting Message
    - Use pusher to delete message realtime
    
    * Minor Bug 🐞: Show message action only for current user
    
    - User can delete or edit their own messages
    
    * Test cases added
    
    * Bug 🐞: Update message id for receiver
    
    Message id was not sent to receiver by pusher
    
    * Refactoring🛠: Message controller refactoring
    
    * Test Cases📝 : Specs for Delete message added
    
    * Feature 🚀 : Ability to edit messages
    
    * Test Cases📝 : Specs for Edit message added
    
    * Merge conflict resolved
    
    * UI completed for Create Channel
    
    * API for create channel & invitations
    
    * 🚀 Combining Backend and Frontend
    
    * Test cases added
    
    * UI completed for Create Channel
    
    * API for create channel & invitations
    
    * 🚀 Combining Backend and Frontend
    
    * Test cases added
    
    * fix suggestions
    
    * add test cases
    
    * replace mod_relations_admin to tag_moderator
    
    * fix test cases
    
    * refactor and fix code climate issues
    
    * Update app/controllers/chat_channel_memberships_controller.rb
    
    Co-authored-by: Michael Kohl <me@citizen428.net>
    
    * fix suggestions
    
    * renaming the methods
    
    * Update app/javascript/chat/chat.jsx
    
    Co-authored-by: Nick Taylor <nick@iamdeveloper.com>
    
    * Update app/javascript/chat/components/createChatModal.jsx
    
    Co-authored-by: Nick Taylor <nick@iamdeveloper.com>
    
    * add jsDoc and add validation
    
    * add PR suggestions
    
    * renmae the file name to  PascalCase
    
    * add styled component
    
    * refactor channel button component
    
    * fix specs
    
    * fix code smell issues
    
    * fix file name issue
    
    * handle error message
    
    * add jsDoc
    
    * Update app/controllers/chat_channels_controller.rb
    
    Co-authored-by: Michael Kohl <me@citizen428.net>
    
    * refactor chat_channel_helpers
    
    * update old hash to new hash
    
    * Update app/javascript/chat/components/ChannelButton.jsx
    
    Co-authored-by: Nick Taylor <nick@iamdeveloper.com>
    
    * Update app/views/chat_channels/index.html.erb
    
    Co-authored-by: Michael Kohl <me@citizen428.net>
    
    * fix syntax error
    
    * Update app/views/chat_channels/index.html.erb
    
    Co-authored-by: Nick Taylor <nick@iamdeveloper.com>
    
    * Policies changed
    
    * The Disable Button issue 🐞
    
    * Fixing Disabled button Issue 🐞
    
    * New group doesn't show up 🐞
    
    Co-authored-by: Narender Singh <narender2031@gmail.com>
    Co-authored-by: Michael Kohl <me@citizen428.net>
    Co-authored-by: Nick Taylor <nick@iamdeveloper.com>

--- app/helpers/chat_channel_memberships_helper.rb
+++ app/helpers/chat_channel_memberships_helper.rb
@@ -15,4 +15,22 @@ module ChatChannelMembershipsHelper
       }
     end
   end
+
+  def membership_users(memberships)
+    memberships.includes(:user).map do |membership|
+      format_membership(membership)
+    end
+  end
+
+  def format_membership(membership)
+    {
+      name: membership.user.name,
+      username: membership.user.username,
+      user_id: membership.user.id,
+      membership_id: membership.id,
+      role: membership.role,
+      status: membership.status,
+      image: Images::Profile.call(membership.user.profile_image_url, length: 90)
+    }
+  end
 end
