8c8a73c387ee061a1b583f81444ee1a77797eab8
Michael Kohl <citizen428@gmail.com>
2020-12-09 14:14:46 +0700

    Update usage

--- app/controllers/twilio_tokens_controller.rb
+++ app/controllers/twilio_tokens_controller.rb
@@ -10,7 +10,7 @@ class TwilioTokensController < ApplicationController
     end
     @chat_channel = ChatChannel.find(video_channel.to_i)
     authorize @chat_channel # show pundit method for chat_channel_policy works here, should always check though
-    @twilio_token = TwilioToken.new(current_user, params[:id]).get
+    @twilio_token = Twilio::GetJwtToken.call(current_user, params[:id])
     render json: { status: "success", token: @twilio_token }, status: :ok
   end
 end
