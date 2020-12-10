dd0b283e1eaedc3e4b44234d8469b0f6a704e40e
rhymes <rhymes@hey.com>
2020-12-10 19:58:25 +0100

    Add some defensive programming

--- app/assets/javascripts/initializers/initializeTouchDevice.js
+++ app/assets/javascripts/initializers/initializeTouchDevice.js
@@ -1,24 +1,34 @@
-var crayonsHeaderMenu = document.getElementById('crayons-header__menu');
-var crayonsHeaderMenuClassList = crayonsHeaderMenu.classList;
-var menuNavButton = document.getElementById('navigation-butt');
-var firstNavLink = document.getElementById('first-nav-link');
-
 function closeHeaderMenu() {
-  menuNavButton.setAttribute('aria-expanded', 'false');
-  setTimeout(() => {
-    crayonsHeaderMenuClassList.remove('showing');
-  }, 5);
+  var menuNavButton = document.getElementById('navigation-butt');
+  if (menuNavButton) {
+    menuNavButton.setAttribute('aria-expanded', 'false');
+  }
+
+  var crayonsHeaderMenu = document.getElementById('crayons-header__menu');
+  if (crayonsHeaderMenu) {
+    setTimeout(() => {
+      crayonsHeaderMenu.classList.remove('showing');
+    }, 5);
+  }
 }
 
 function blurHeaderMenu(event, elementId) {
   setTimeout(() => {
     if (document.activeElement !== document.getElementById(elementId)) {
-      closeHeaderMenu()
+      closeHeaderMenu();
     }
   }, 10);
 }
 
 function toggleHeaderMenu() {
+  var crayonsHeaderMenu = document.getElementById('crayons-header__menu');
+  var menuNavButton = document.getElementById('navigation-butt');
+
+  if (!crayonsHeaderMenu || !menuNavButton) {
+    return;
+  }
+
+  var crayonsHeaderMenuClassList = crayonsHeaderMenu.classList;
   if (crayonsHeaderMenuClassList.contains('showing')) {
     crayonsHeaderMenuClassList.remove('showing');
     menuNavButton.setAttribute('aria-expanded', 'false');
@@ -26,10 +36,13 @@ function toggleHeaderMenu() {
     crayonsHeaderMenuClassList.add('showing');
     menuNavButton.setAttribute('aria-expanded', 'true');
 
-    setTimeout(() => {
-      // focus first item on open
-      firstNavLink.focus();
-    }, 100);
+    var firstNavLink = document.getElementById('first-nav-link');
+    if (firstNavLink) {
+      setTimeout(() => {
+        // focus first item on open
+        firstNavLink.focus();
+      }, 100);
+    }
   }
 }
 
@@ -37,10 +50,16 @@ function initializeTouchDevice() {
   var isTouchDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|DEV-Native-ios/i.test(
     navigator.userAgent,
   );
+
   if (navigator.userAgent === 'DEV-Native-ios') {
     document.body.classList.add('dev-ios-native-body');
   }
+
+  var crayonsHeaderMenu = document.getElementById('crayons-header__menu');
   if (crayonsHeaderMenu) {
+    var menuNavButton = document.getElementById('navigation-butt');
+    var crayonsHeaderMenuClassList = crayonsHeaderMenu.classList;
+
     setTimeout(() => {
       closeHeaderMenu();
       if (isTouchDevice) {
@@ -52,14 +71,19 @@ function initializeTouchDevice() {
           toggleHeaderMenu();
         });
         crayonsHeaderMenu.addEventListener('keyup', (e) => {
-          if (e.key === 'Escape' && crayonsHeaderMenuClassList.contains('showing')) {
+          if (
+            e.key === 'Escape' &&
+            crayonsHeaderMenuClassList.contains('showing')
+          ) {
             crayonsHeaderMenuClassList.remove('showing');
             menuNavButton.focus();
           }
         });
-        document.getElementById('last-nav-link').addEventListener('blur', (e) =>
-          blurHeaderMenu(e, 'second-last-nav-link'),
-        );
+        document
+          .getElementById('last-nav-link')
+          .addEventListener('blur', (e) =>
+            blurHeaderMenu(e, 'second-last-nav-link'),
+          );
         document.addEventListener('click', (e) => {
           // if clicking outside of the menu, close it
           if (!crayonsHeaderMenu.contains(document.activeElement)) {
