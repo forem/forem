e16357489869a0156df9f8ff3af6de5e4352ec15
Ben Halpern <bendhalpern@gmail.com>
2020-12-13 19:44:17 -0500

    Add campaign articles to sidebar controller (#11879)
    * Add campaign articles to sidebar controller
    
    * Fix sidebar additional erb logic

--- app/controllers/sidebars_controller.rb
+++ app/controllers/sidebars_controller.rb
@@ -3,6 +3,21 @@ class SidebarsController < ApplicationController
   before_action :set_cache_control_headers, only: %i[show]
 
   def show
+    get_latest_campaign_articles
     set_surrogate_key_header "home-sidebar"
+  end
+
+  private
+
+  def get_latest_campaign_articles
+    campaign_articles_scope = Article.tagged_with(Campaign.current.featured_tags, any: true)
+      .where("published_at > ? AND score > ?", 4.weeks.ago, 0)
+      .order(hotness_score: :desc)
+
+    requires_approval = Campaign.current.articles_require_approval?
+    campaign_articles_scope = campaign_articles_scope.where(approved: true) if requires_approval
+
+    @campaign_articles_count = campaign_articles_scope.count
+    @latest_campaign_articles = campaign_articles_scope.limit(5).pluck(:path, :title, :comments_count, :created_at)
   end
 end
