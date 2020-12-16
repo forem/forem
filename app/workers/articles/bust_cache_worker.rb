0861f719c105bad4f1184e1565a7be9495434aab
Alex <alexandersmith223@gmail.com>
2020-12-16 13:47:54 -0500

    Update bust_article to BustArticle

--- app/workers/articles/bust_cache_worker.rb
+++ app/workers/articles/bust_cache_worker.rb
@@ -1,9 +1,11 @@
 module Articles
   class BustCacheWorker < BustCacheBaseWorker
-    def perform(article_id, cache_buster = "CacheBuster")
+    def perform(article_id)
       article = Article.find_by(id: article_id)
 
-      cache_buster.constantize.bust_article(article) if article
+      return unless article
+
+      EdgeCache::BustArticle.call(article)
     end
   end
 end
