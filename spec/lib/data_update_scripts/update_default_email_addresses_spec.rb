fd789399e799ae98008377ec36327c00f67d8a81
Ridhwana <ridhwana.khan16@gmail.com>
2020-12-18 10:08:30 +0200

    feat: add a new script that updates the email_addresses hash with the contact key

---
+++ spec/lib/data_update_scripts/update_default_email_addresses_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+require Rails.root.join(
+  "lib/data_update_scripts/20201218080343_update_default_email_addresses.rb",
+)
+
+describe DataUpdateScripts::UpdateDefaultEmailAddresses do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
