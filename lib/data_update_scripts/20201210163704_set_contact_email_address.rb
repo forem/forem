b363f1f13c6ae6cb4587d06d12d0f063673fc5c7
Ridhwana <ridhwana.khan16@gmail.com>
2020-12-18 11:20:24 +0200

    add a comment to old data script

--- lib/data_update_scripts/20201210163704_set_contact_email_address.rb
+++ lib/data_update_scripts/20201210163704_set_contact_email_address.rb
@@ -3,6 +3,8 @@ module DataUpdateScripts
     def run
       return if SiteConfig.email_addresses[:contact].present?
 
+      # This update does not create and update the contact email,
+      # wrote 20201218080343_update_default_email_addresses as a replacement.
       SiteConfig.email_addresses[:contact] = ApplicationConfig["DEFAULT_EMAIL"]
     end
   end
