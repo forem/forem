a7aaef23268ce35fd24047e87d3719af5f254052
Ridhwana <ridhwana.khan16@gmail.com>
2020-12-18 12:38:09 +0200

    fix: test passing

--- spec/lib/data_update_scripts/update_default_email_addresses_spec.rb
+++ spec/lib/data_update_scripts/update_default_email_addresses_spec.rb
@@ -6,21 +6,9 @@ require Rails.root.join(
 describe DataUpdateScripts::UpdateDefaultEmailAddresses do
   let(:contact_email) { "contact@dev.to" }
 
-  before do
-    allow(SiteConfig).to receive(:email_addresses).and_return(
-      {
-        default: "hi@dev.to",
-        business: "business@dev.to",
-        privacy: "privacy@dev.to",
-        members: "members@dev.to"
-      },
-    )
-  end
-
   context "with no contact email set in the email_addresses hash" do
     it "adds the contact email" do
       described_class.new.run
-      puts SiteConfig.email_addresses
       expect(SiteConfig.email_addresses).to include(contact: ApplicationConfig["DEFAULT_EMAIL"])
     end
   end
@@ -29,7 +17,6 @@ describe DataUpdateScripts::UpdateDefaultEmailAddresses do
     it "preserves the current contact email" do
       SiteConfig.email_addresses[:contact] = contact_email
       described_class.new.run
-      SiteConfig.clear_cache
       expect(SiteConfig.email_addresses).to include(contact: contact_email)
     end
   end
