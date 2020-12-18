adffbfc02fa56d5cb458d848c27a977fe68b90a5
Ridhwana <ridhwana.khan16@gmail.com>
2020-12-18 12:00:43 +0200

    feat: add a spec (not passing as yet)

--- spec/lib/data_update_scripts/update_default_email_addresses_spec.rb
+++ spec/lib/data_update_scripts/update_default_email_addresses_spec.rb
@@ -4,5 +4,33 @@ require Rails.root.join(
 )
 
 describe DataUpdateScripts::UpdateDefaultEmailAddresses do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:contact_email) { "contact@dev.to" }
+
+  before do
+    allow(SiteConfig).to receive(:email_addresses).and_return(
+      {
+        default: "hi@dev.to",
+        business: "business@dev.to",
+        privacy: "privacy@dev.to",
+        members: "members@dev.to"
+      },
+    )
+  end
+
+  context "with no contact email set in the email_addresses hash" do
+    it "adds the contact email" do
+      described_class.new.run
+      puts SiteConfig.email_addresses
+      expect(SiteConfig.email_addresses).to include(contact: ApplicationConfig["DEFAULT_EMAIL"])
+    end
+  end
+
+  context "with a contact email set in the email_addresses hash" do
+    it "preserves the current contact email" do
+      SiteConfig.email_addresses[:contact] = contact_email
+      described_class.new.run
+      SiteConfig.clear_cache
+      expect(SiteConfig.email_addresses).to include(contact: contact_email)
+    end
+  end
 end
