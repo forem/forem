c8e56853b7172762205bffc77f1128f2a9eaf297
Ridhwana <Ridhwana.Khan16@gmail.com>
2020-12-18 18:07:48 +0200

    Updates the email_addresses hash with the contact info (#11967)
    * feat: add a new script that updates the email_addresses hash with the contact key
    
    * add a comment to old data script
    
    * feat: add a spec (not passing as yet)
    
    * fix: test passing
    
    * chore: comment script with reason

--- spec/lib/data_update_scripts/update_default_email_addresses_spec.rb
+++ spec/lib/data_update_scripts/update_default_email_addresses_spec.rb
@@ -1,21 +1,10 @@
 require "rails_helper"
 require Rails.root.join(
-  "lib/data_update_scripts/20201210163704_set_contact_email_address.rb",
+  "lib/data_update_scripts/20201218080343_update_default_email_addresses.rb",
 )
 
-describe DataUpdateScripts::SetContactEmailAddress do
+describe DataUpdateScripts::UpdateDefaultEmailAddresses do
   let(:contact_email) { "contact@dev.to" }
-
-  before do
-    allow(SiteConfig).to receive(:email_addresses).and_return(
-      {
-        default: "hi@dev.to",
-        business: "business@dev.to",
-        privacy: "privacy@dev.to",
-        members: "members@dev.to"
-      },
-    )
-  end
 
   context "with no contact email set in the email_addresses hash" do
     it "adds the contact email" do
@@ -28,7 +17,6 @@ describe DataUpdateScripts::SetContactEmailAddress do
     it "preserves the current contact email" do
       SiteConfig.email_addresses[:contact] = contact_email
       described_class.new.run
-      SiteConfig.clear_cache
       expect(SiteConfig.email_addresses).to include(contact: contact_email)
     end
   end
