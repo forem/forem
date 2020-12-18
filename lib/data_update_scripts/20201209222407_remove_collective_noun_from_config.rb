821163be9d459bdfabf572da395a4228781e9c68
Julianna Tetreault <juliannatetreault@gmail.com>
2020-12-18 10:30:06 -0700

    Removes RemoveCollectiveNounFromConfig and updates other script

--- lib/data_update_scripts/20201209222407_remove_collective_noun_from_config.rb
+++
@@ -1,7 +0,0 @@
-module DataUpdateScripts
-  class RemoveCollectiveNounFromConfig
-    def run
-      SiteConfig.where(var: %w[collective_noun collective_noun_disabled]).destroy_all
-    end
-  end
-end
--- lib/data_update_scripts/20201217184442_append_community_to_community_name.rb
+++ lib/data_update_scripts/20201217184442_append_community_to_community_name.rb
@@ -1,8 +1,10 @@
 module DataUpdateScripts
   class AppendCommunityToCommunityName
     def run
+      return unless SiteConfig.collective_noun_disabled.false?
+
       SiteConfig.where(var: "community_name").find_each do |community_name|
-        community_name.update!(value: "#{community_name.value} Community")
+        community_name.update!(value: "#{community_name.value} #{SiteConfig.collective_noun}")
       end
     end
   end
