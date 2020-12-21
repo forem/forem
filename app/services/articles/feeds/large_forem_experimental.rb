4dc70a2c4b74d960f708efb389dd41dd15f76e68
benhalpern <bendhalpern@gmail.com>
2020-12-21 17:47:06 -0500

    Fix tests

--- app/services/articles/feeds/large_forem_experimental.rb
+++ app/services/articles/feeds/large_forem_experimental.rb
@@ -1,6 +1,7 @@
 module Articles
   module Feeds
     class LargeForemExperimental
+      include FieldTest::Helpers
       RANDOM_OFFSET_VALUES = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].freeze
       MINIMUM_SCORE_LATEST_FEED = -20
 
