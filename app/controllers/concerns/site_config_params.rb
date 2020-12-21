421829882873f1083f30ea3dca6599cef8d5183b
benhalpern <bendhalpern@gmail.com>
2020-12-21 12:12:29 -0500

    Fix config params

--- app/controllers/concerns/site_config_params.rb
+++ app/controllers/concerns/site_config_params.rb
@@ -1,11 +1,11 @@
 module SiteConfigParams
-  SPECIAL_PARAMS_TO_ADD = %w[authentication_providers email_addresses meta_keywords credit_prices_in_cents].freeze
-  SPECIAL_PARAMS_TO_REMOVE = %w[auth_providers_to_enable].freeze
+  SPECIAL_PARAMS_TO_ADD = 
+    %w[authentication_providers email_addresses meta_keywords credit_prices_in_cents auth_providers_to_enable].freeze
   def site_config_params
     has_emails = params.dig(:site_config, :email_addresses).present?
     params[:site_config][:email_addresses][:default] = ApplicationConfig["DEFAULT_EMAIL"] if has_emails
     params.require(:site_config)&.permit(
-      (SiteConfig.keys - SPECIAL_PARAMS_TO_REMOVE + SPECIAL_PARAMS_TO_ADD).map(&:to_sym),
+      (SiteConfig.keys + SPECIAL_PARAMS_TO_ADD).map(&:to_sym),
       authentication_providers: [],
       social_media_handles: SiteConfig.social_media_handles.keys,
       email_addresses: SiteConfig.email_addresses.keys,
