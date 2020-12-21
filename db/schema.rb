2e5a96cbe43e1a0d81559faaf2caafe3bbe9dc54
Michael Kohl <citizen428@gmail.com>
2020-12-21 09:43:33 +0700

    Create search documents table

--- db/schema.rb
+++ db/schema.rb
@@ -838,6 +838,15 @@ ActiveRecord::Schema.define(version: 2021_01_21_102114) do
     t.index ["slug"], name: "index_pages_on_slug", unique: true
   end
 
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.bigint "searchable_id"
+    t.string "searchable_type"
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
+  end
+
   create_table "podcast_episode_appearances", force: :cascade do |t|
     t.boolean "approved", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
