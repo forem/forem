b21330167a2b742507da6ab08ad32628507cca70
benhalpern <bendhalpern@gmail.com>
2020-12-22 11:30:31 -0500

    Change spec

--- spec/workers/users/record_field_test_event_worker_spec.rb
+++ spec/workers/users/record_field_test_event_worker_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Users::RecordFieldTestEventWorker, type: :worker do
 
     context "with user who is part of field test" do
       before do
-        field_test(:follow_implicit_points, participant: user)
+        field_test(:feed_top_articles_query, participant: user)
       end
 
       it "records user_creates_reaction field test conversion" do
