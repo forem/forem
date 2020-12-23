74d50544104390360267fd5033669e63834af174
benhalpern <bendhalpern@gmail.com>
2020-12-22 21:10:50 -0500

    Initial work

---
+++ spec/lib/data_update_scripts/set_proper_display_area_for_profile_fields_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+require Rails.root.join(
+  "lib/data_update_scripts/20201223013903_set_proper_display_area_for_profile_fields.rb",
+)
+
+describe DataUpdateScripts::SetProperDisplayAreaForProfileFields do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
