80715e4bfe7b596ce866c5ca4f21c8f888e824a4
Michael Kohl <citizen428@dev.to>
2020-12-25 11:01:19 +0700

    Move constant from initializer to app/lib/constants

---
+++ app/lib/constants/redcarpet.rb
@@ -0,0 +1,15 @@
+module Constants
+  module Redcarpet
+    CONFIG = {
+      autolink: true,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      lax_html_blocks: true,
+      lax_spacing: true,
+      strikethrough: true,
+      superscript: false,
+      tables: true,
+      footnotes: true
+    }.freeze
+  end
+end
