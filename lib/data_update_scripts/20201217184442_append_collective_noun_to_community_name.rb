fa63b1125cacce1504ade9d5daba411505ef114b
Julianna Tetreault <juliannatetreault@gmail.com>
2020-12-28 10:57:31 -0700

    Hotfix: revert change to predicate method to fix script

--- lib/data_update_scripts/20201217184442_append_collective_noun_to_community_name.rb
+++ lib/data_update_scripts/20201217184442_append_collective_noun_to_community_name.rb
@@ -1,7 +1,7 @@
 module DataUpdateScripts
   class AppendCommunityToCommunityName
     def run
-      return unless SiteConfig.collective_noun_disabled?
+      return unless SiteConfig.collective_noun_disabled
 
       SiteConfig.where(var: "community_name").find_each do |community_name|
         community_name.update!(value: "#{community_name.value} #{SiteConfig.collective_noun}")
