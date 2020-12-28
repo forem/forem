320f091a969a755230d74c5d3d45bb4304f88205
Julianna Tetreault <juliannatetreault@gmail.com>
2020-12-28 12:16:03 -0700

    Adds comments to data_update scripts and comments out script logic

--- lib/data_update_scripts/20201218173445_remove_collective_noun_from_config.rb
+++ lib/data_update_scripts/20201218173445_remove_collective_noun_from_config.rb
@@ -2,6 +2,8 @@ module DataUpdateScripts
   class RemoveCollectiveNounFromConfig
     def run
       # SiteConfig.where(var: %w[collective_noun collective_noun_disabled]).destroy_all
+
+      # These columns have been removed via the model, rendering this script useless
     end
   end
 end
