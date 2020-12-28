8baa936deabae412cfb1d48c7f39f387811712a0
Alex <alexandersmith223@gmail.com>
2020-12-28 11:09:10 -0500

    Update bust_podcast_episode to service

--- app/workers/podcast_episodes/bust_cache_worker.rb
+++ app/workers/podcast_episodes/bust_cache_worker.rb
@@ -2,9 +2,10 @@ module PodcastEpisodes
   class BustCacheWorker < BustCacheBaseWorker
     def perform(podcast_episode_id, path, podcast_slug)
       podcast_episode = PodcastEpisode.find_by(id: podcast_episode_id)
-      return if podcast_episode.nil?
 
-      CacheBuster.bust_podcast_episode(podcast_episode, path, podcast_slug)
+      return unless podcast_episode && path && podcast_slug
+
+      EdgeCache::BustPodcastEpisode.call(podcast_episode, path, podcast_slug)
     end
   end
 end
