20de90f62f2a76a7ba0461a8700d0e2d9ca1eab4
Alex <alexandersmith223@gmail.com>
2020-12-28 17:18:39 -0500

    Update argument check

--- app/workers/organizations/bust_cache_worker.rb
+++ app/workers/organizations/bust_cache_worker.rb
@@ -1,9 +1,11 @@
 module Organizations
   class BustCacheWorker < BustCacheBaseWorker
     def perform(organization_id, slug)
+      return unless organization_id && slug
+
       organization = Organization.find_by(id: organization_id)
 
-      return unless organization && slug
+      return unless organization
 
       EdgeCache::BustOrganization.call(organization, slug)
     end
