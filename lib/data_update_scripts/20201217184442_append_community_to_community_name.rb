aeb3c6eefd8286ab14c57fdcd6752141a77db431
Julianna Tetreault <32834804+juliannatetreault@users.noreply.github.com>
2020-12-28 12:29:42 -0700

    Disable Community Name Data Update Scripts (#12054)

--- lib/data_update_scripts/20201217184442_append_community_to_community_name.rb
+++ lib/data_update_scripts/20201217184442_append_community_to_community_name.rb
@@ -1,11 +1,13 @@
 module DataUpdateScripts
   class AppendCommunityToCommunityName
     def run
-      return unless SiteConfig.collective_noun_disabled?
+      # return unless SiteConfig.collective_noun_disabled
 
-      SiteConfig.where(var: "community_name").find_each do |community_name|
-        community_name.update!(value: "#{community_name.value} #{SiteConfig.collective_noun}")
-      end
+      # SiteConfig.where(var: "community_name").find_each do |community_name|
+      #   community_name.update!(value: "#{community_name.value} #{SiteConfig.collective_noun}")
+      # end
+
+      # We cannot access the column directly, so this has been commented out
     end
   end
 end
--- lib/data_update_scripts/20201218173445_remove_collective_noun_from_config.rb
+++ lib/data_update_scripts/20201218173445_remove_collective_noun_from_config.rb
@@ -1,7 +1,9 @@
 module DataUpdateScripts
   class RemoveCollectiveNounFromConfig
     def run
-      SiteConfig.where(var: %w[collective_noun collective_noun_disabled]).destroy_all
+      # SiteConfig.where(var: %w[collective_noun collective_noun_disabled]).destroy_all
+
+      # These columns have been removed via the model, rendering this script useless
     end
   end
 end
