b76163200ba4387164ef2a78922c091ddacbc752
Alex <alexandersmith223@gmail.com>
2020-12-28 13:04:36 -0500

    Fix more specs :)

--- spec/workers/bust_cache_path_worker_spec.rb
+++ spec/workers/bust_cache_path_worker_spec.rb
@@ -6,10 +6,12 @@ RSpec.describe BustCachePathWorker, type: :worker do
   include_examples "#enqueues_on_correct_queue", "high_priority"
 
   describe "#perform" do
+    let(:path) { "/foo" }
+
     it "busts cache for given path" do
-      allow(CacheBuster).to receive(:bust)
-      worker.perform("/foo")
-      expect(CacheBuster).to have_received(:bust).with("/foo")
+      allow(EdgeCache::Bust).to receive(:call).with(path)
+      worker.perform(path)
+      expect(EdgeCache::Bust).to have_received(:call).with(path)
     end
   end
 end
