ce35547150ea039a92a6fb62c4095f1c2b0993d2
Julianna Tetreault <juliannatetreault@gmail.com>
2020-12-29 08:42:00 -0700

    Refactors data_update script logic to be cleaner

--- lib/data_update_scripts/20201228194641_append_collective_noun_to_community_name.rb
+++ lib/data_update_scripts/20201228194641_append_collective_noun_to_community_name.rb
@@ -1,11 +1,9 @@
 module DataUpdateScripts
   class AppendCollectiveNounToCommunityName
     def run
-      if SiteConfig.collective_noun_disabled
-        SiteConfig.community_name
-      else
-        SiteConfig.community_name = "#{SiteConfig.community_name} #{SiteConfig.collective_noun}"
-      end
+      return if SiteConfig.collective_noun_disabled
+
+      SiteConfig.community_name = "#{SiteConfig.community_name} #{SiteConfig.collective_noun}"
     end
   end
 end
