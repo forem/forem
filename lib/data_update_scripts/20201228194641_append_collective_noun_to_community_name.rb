fb3fb80d758142acecb6c077af95e8eb4b949c5b
Julianna Tetreault <juliannatetreault@gmail.com>
2020-12-29 14:40:48 -0700

    Adds an additional check to the scripts return statement

--- lib/data_update_scripts/20201228194641_append_collective_noun_to_community_name.rb
+++ lib/data_update_scripts/20201228194641_append_collective_noun_to_community_name.rb
@@ -1,7 +1,7 @@
 module DataUpdateScripts
   class AppendCollectiveNounToCommunityName
     def run
-      return if SiteConfig.collective_noun_disabled
+      return if SiteConfig.collective_noun_disabled || SiteConfig.collective_noun.blank?
 
       SiteConfig.community_name = "#{SiteConfig.community_name} #{SiteConfig.collective_noun}"
     end
