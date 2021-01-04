2cc5d1023b0e68aa3c8fb2971cc5125035166145
Julianna Tetreault <juliannatetreault@gmail.com>
2021-01-04 16:27:46 -0700

    Adds an unfinished spec around the removal of collective_noun fields

--- lib/data_update_scripts/20201228194641_append_collective_noun_to_community_name.rb
+++ lib/data_update_scripts/20201228194641_append_collective_noun_to_community_name.rb
@@ -2,7 +2,7 @@ module DataUpdateScripts
   class AppendCollectiveNounToCommunityName
     def run
       return unless SiteConfig.respond_to?(:collective_noun_disabled)
-      return unless SiteConfig.collective_noun_disabled? || SiteConfig.collective_noun.blank?
+      return if SiteConfig.collective_noun_disabled || SiteConfig.collective_noun.blank?
 
       SiteConfig.community_name = "#{SiteConfig.community_name} #{SiteConfig.collective_noun}"
     end
--- spec/lib/data_update_scripts/remove_collective_noun_from_site_config_spec.rb
+++ spec/lib/data_update_scripts/remove_collective_noun_from_site_config_spec.rb
@@ -4,9 +4,13 @@ require Rails.root.join(
 )
 
 describe DataUpdateScripts::RemoveCollectiveNounFromSiteConfig do
-  # context "when collective_noun and collective_noun_disabled are removed from the Config" do
-  #   it "detroys the fields" do
-  #     described_class.new.run
-  #   end
-  # end
+  context "when collective_noun and collective_noun_disabled are removed from the Config" do
+    it "detroys the fields" do
+      allow(SiteConfig).to receive(:collective_noun).and_return("Club")
+      expect(SiteConfig.collective_noun).to eq("Club")
+      described_class.new.run
+      expect(SiteConfig).to include(:community_name)
+      expect(SiteConfig).not_to include(var: %w[collective_noun collective_noun_disabled])
+    end
+  end
 end
