8cd15bf305b4a61ccc57f4a3a89b400cde1cde03
Julianna Tetreault <juliannatetreault@gmail.com>
2021-01-04 11:56:38 -0700

    Adds additional guard clauses to collective noun scripts

--- lib/data_update_scripts/20201228194641_append_collective_noun_to_community_name.rb
+++ lib/data_update_scripts/20201228194641_append_collective_noun_to_community_name.rb
@@ -1,7 +1,8 @@
 module DataUpdateScripts
   class AppendCollectiveNounToCommunityName
     def run
-      return if SiteConfig.collective_noun_disabled || SiteConfig.collective_noun.blank?
+      return unless SiteConfig.respond_to?(:collective_noun_disabled)
+      return unless SiteConfig.collective_noun_disabled? || SiteConfig.collective_noun.blank?
 
       SiteConfig.community_name = "#{SiteConfig.community_name} #{SiteConfig.collective_noun}"
     end
