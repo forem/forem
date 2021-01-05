57d02142e7c5cf686dd7a304c0a1768608813dfd
Julianna Tetreault <juliannatetreault@gmail.com>
2021-01-05 14:27:40 -0700

    Reworks remove_collective_noun_from_site_config_spec test and expectations

--- spec/lib/data_update_scripts/remove_collective_noun_from_site_config_spec.rb
+++ spec/lib/data_update_scripts/remove_collective_noun_from_site_config_spec.rb
@@ -5,12 +5,14 @@ require Rails.root.join(
 
 describe DataUpdateScripts::RemoveCollectiveNounFromSiteConfig do
   context "when collective_noun and collective_noun_disabled are removed from the Config" do
-    it "detroys the fields" do
-      allow(SiteConfig).to receive(:collective_noun).and_return("Club")
-      expect(SiteConfig.collective_noun).to eq("Club")
-      described_class.new.run
-      expect(SiteConfig).to include(:community_name)
-      expect(SiteConfig).not_to include(var: %w[collective_noun collective_noun_disabled])
+    it "does not alter the community_name" do
+      allow(SiteConfig).to receive(:community_name).and_return("DEV Club")
+
+      expect do
+        described_class.new.run
+      end.not_to change(SiteConfig, :community_name)
+
+      expect(SiteConfig.community_name).to eq("DEV Club")
     end
   end
 end
