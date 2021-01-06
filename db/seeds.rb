98a39de3856dbc09c8e1f101585f6876589215df
Nick Taylor <nick@dev.to>
2021-01-06 11:28:21 -0500

    Made seeder require relative.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ return if Rails.env.production?
 
 # NOTE: when adding new data, please use the Seeder class to ensure the seed tasks
 # stays idempotent.
-require Rails.root.join("db/seeds/seeder")
+require_relative "seeds/seeder"
 
 # we use this to be able to increase the size of the seeded DB at will
 # eg.: `SEEDS_MULTIPLIER=2 rails db:seed` would double the amount of data
