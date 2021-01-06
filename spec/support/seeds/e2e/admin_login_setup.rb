60db7a3896f824b428d7a2523459e29cf7842e92
Nick Taylor <nick@dev.to>
2021-01-06 06:53:09 -0500

    Fixed seeder require in e2e test seed file.

--- spec/support/seeds/e2e/admin_login_setup.rb
+++ spec/support/seeds/e2e/admin_login_setup.rb
@@ -2,42 +2,9 @@ return if Rails.env.production?
 
 require Rails.root.join("app/models/site_config")
 
-# TODO: Pull this out so that it can be imported here and also imported for db:seed rake task.
-class Seeder
-  def initialize
-    @counter = 0
-  end
-
-  # Used when the block is idempotent by itself and needs no further checks.
-  def create(message)
-    @counter += 1
-    puts "  #{@counter}. #{message}."
-    yield
-  end
-
-  def create_if_none(klass, count = nil)
-    @counter += 1
-    plural = klass.name.pluralize
-
-    if klass.none?
-      message = ["Creating", count, plural].compact.join(" ")
-      puts "  #{@counter}. #{message}."
-      yield
-    else
-      puts "  #{@counter}. #{plural} already exist. Skipping."
-    end
-  end
-
-  def create_if_doesnt_exist(klass, attribute_name, attribute_value)
-    record = klass.find_by("#{attribute_name}": attribute_value)
-    if record.nil?
-      puts "  #{klass} with #{attribute_name} = #{attribute_value} not found, proceeding..."
-      yield
-    else
-      puts "  #{klass} with #{attribute_name} = #{attribute_value} found, skipping."
-    end
-  end
-end
+# NOTE: when adding new data, please use the Seeder class to ensure the seed tasks
+# stays idempotent.
+require Rails.root.join("db/seeds/seeder")
 
 seeder = Seeder.new
 
