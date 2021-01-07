fd038679f8ebe45faaf060d2bec5b2e09c32f1d7
Alex <alexandersmith223@gmail.com>
2021-01-07 13:47:08 -0500

    Update spec wording

--- spec/services/bufferizer/listings_tweet_spec.rb
+++ spec/services/bufferizer/listings_tweet_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe Bufferizer::ListingsTweet, type: :service do
     let(:user) { create(:user) }
     let(:listing) { create(:listing, user_id: user.id) }
 
-    context "when article is nil" do
+    context "when listing is nil" do
       it "doesn't raise an error" do
         tweet = "test tweet"
         expect { described_class.call(nil, tweet) }.not_to raise_error
       end
     end
 
-    context "when post is nil" do
+    context "when tweet is nil" do
       it "doesn't raise an error" do
         expect { described_class.call(listing, nil) }.not_to raise_error
       end
