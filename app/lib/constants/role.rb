ccc5aa6d11657d91f6fb66e6c1a1b047054dadd3
Molly Struve <mollylbs@gmail.com>
2021-01-08 06:51:14 -0600

    Feature:Add ability to add a Tag Resource Admin through Admin (#12172)

--- app/lib/constants/role.rb
+++ app/lib/constants/role.rb
@@ -19,6 +19,7 @@ module Constants
                      "Resource Admin: Broadcast",
                      "Resource Admin: HtmlVariant",
                      "Resource Admin: DisplayAd",
-                     "Resource Admin: ListingCategory"].freeze
+                     "Resource Admin: ListingCategory",
+                     "Resource Admin: Tag"].freeze
   end
 end
