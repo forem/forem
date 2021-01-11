ddce20a22581cec4872ebf5d877cab9994f20b8d
Julianna Tetreault <juliannatetreault@gmail.com>
2021-01-11 14:30:57 -0700

    Adds a custom error for invalid tokens in UsersController#confirm_destroy

---
+++ app/errors/user_destroy_token.rb
@@ -0,0 +1,17 @@
+module UserDestroyToken
+  module Errors
+    class Error < StandardError
+    end
+
+    # user-destroy-tokens are sent to a user's email upon account deletion request.
+    # An error is raised if the token is invalid, which happens if the token has expired.
+    class InvalidToken < Error
+      # rubocop:disable Layout/LineLength
+      def initialize
+        msg = "Your token has expired, please request a new one. Tokens only last for 12 hours after account deletion is initiated."
+        super(msg)
+      end
+      # rubocop:enable Layout/LineLength
+    end
+  end
+end
