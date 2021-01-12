7ee071e261ad14c9e00036aa5ef2efb5584ab20d
Ridhwana <ridhwana.khan16@gmail.com>
2021-01-12 09:54:58 +0200

    test: Forem Instance model to return the correct values when present

---
+++ spec/models/forem_instance_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe ForemInstance, type: :model do
+  describe "deployed_at" do
+    it "sets the RELEASE_FOOTPRINT if present" do
+      allow(ApplicationConfig).to receive(:[]).with("RELEASE_FOOTPRINT").and_return("A deploy date")
+      expect(described_class.deployed_at).to be(ApplicationConfig["RELEASE_FOOTPRINT"])
+    end
+
+    it "sets the HEROKU_RELEASE_CREATED_AT if the RELEASE_FOOTPRINT is not present" do
+      allow(ApplicationConfig).to receive(:[]).with("RELEASE_FOOTPRINT").and_return("")
+      stub_const("ENV", ENV.to_h.merge("HEROKU_RELEASE_CREATED_AT" => "A deploy date set on Heroku"))
+      expect(described_class.deployed_at).to be(ENV["HEROKU_RELEASE_CREATED_AT"])
+    end
+  end
+
+  describe "latest_commit_id" do
+    it "sets the FOREM_BUILD_SHA if present" do
+      allow(ApplicationConfig).to receive(:[]).with("FOREM_BUILD_SHA").and_return("A commit id")
+      expect(described_class.latest_commit_id).to be(ApplicationConfig["FOREM_BUILD_SHA"])
+    end
+
+    it "sets the HEROKU_RELEASE_CREATED_AT if the RELEASE_FOOTPRINT is not present" do
+      allow(ApplicationConfig).to receive(:[]).with("FOREM_BUILD_SHA").and_return("")
+      stub_const("ENV", ENV.to_h.merge("HEROKU_SLUG_COMMIT" => "A Commit ID set from Heroku"))
+      expect(described_class.latest_commit_id).to be(ENV["HEROKU_SLUG_COMMIT"])
+    end
+  end
+end
