61c565149d495fcfbbfe127e64291a5170d0dc54
Ridhwana <ridhwana.khan16@gmail.com>
2021-01-12 11:56:49 +0200

    refactor: static values that can never change during the lifetime of the app should be constants

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -174,7 +174,7 @@ module ApplicationHelper
   end
 
   def release_adjusted_cache_key(path)
-    release_footprint = ForemInstance.deployed_at
+    release_footprint = ForemInstance::DEPLOYED_AT
     return path if release_footprint.blank?
 
     "#{path}-#{params[:locale]}-#{release_footprint}-#{SiteConfig.admin_action_taken_at.rfc3339}"
--- app/models/forem_instance.rb
+++ app/models/forem_instance.rb
@@ -1,4 +1,12 @@
 class ForemInstance
+  DEPLOYED_AT = (
+    ApplicationConfig["RELEASE_FOOTPRINT"].presence || ENV["HEROKU_RELEASE_CREATED_AT"].presence
+  ).freeze
+
+  LATEST_COMMIT_ID = (
+    ApplicationConfig["FOREM_BUILD_SHA"].presence || ENV["HEROKU_SLUG_COMMIT"].presence
+  ).freeze
+
   def self.deployed_at
     ApplicationConfig["RELEASE_FOOTPRINT"].presence || ENV["HEROKU_RELEASE_CREATED_AT"].presence
   end
