62a191ae20d4ccfe5370cdd0a2070bb479122f9e
Alex <alexandersmith223@gmail.com>
2021-01-12 10:44:15 -0500

    Create USER_COLORS constant and constant specs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,10 @@
 module ApplicationHelper
   # rubocop:disable Performance/OpenStruct
+  USER_COLORS = ["#19063A", "#dce9f3"].freeze
+
   DELETED_USER = OpenStruct.new(
     id: nil,
-    darker_color: Hex::Comparer.new(["#19063A", "#dce9f3"]).brightness,
+    darker_color: Hex::Comparer.new(USER_COLORS).brightness,
     username: "[deleted user]",
     name: "[Deleted User]",
     summary: nil,
