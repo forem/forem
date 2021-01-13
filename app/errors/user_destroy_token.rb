84bc555c0445709a9d166eb6411e3d24163c03f2
Julianna Tetreault <juliannatetreault@gmail.com>
2021-01-13 12:27:17 -0700

    Refactor user_destroy_token.rb initialize method to be more succinct

--- app/errors/user_destroy_token.rb
+++ app/errors/user_destroy_token.rb
@@ -7,8 +7,7 @@ module UserDestroyToken
     # An error is raised if the token is invalid, which happens if the token has expired.
     class InvalidToken < Error
       # rubocop:disable Layout/LineLength
-      def initialize
-        msg = "Your token has expired, please request a new one. Tokens only last for 12 hours after account deletion is initiated."
+      def initialize(msg = "Your token has expired, please request a new one. Tokens only last for 12 hours after account deletion is initiated.")
         super(msg)
       end
       # rubocop:enable Layout/LineLength
