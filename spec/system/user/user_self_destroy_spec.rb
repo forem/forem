7757f573ced2259837cf2bb3442468e4da783ea2
Julianna Tetreault <juliannatetreault@gmail.com>
2021-01-13 12:32:25 -0700

    Removes unnecessary code from user_self_destroy_spec.rb and fixes failing InvalidToken spec

--- spec/system/user/user_self_destroy_spec.rb
+++ spec/system/user/user_self_destroy_spec.rb
@@ -15,23 +15,13 @@ RSpec.describe "User destroys their profile", type: :system, js: true do
     expect(Users::RequestDestroy).to have_received(:call).with(user)
   end
 
-  it "displays a detailed error message when the user is not logged in" do
-    sign_out user
-    visit "/settings/account"
-    expect(page).to have_text("You must be logged in to proceed with account deletion.")
-  end
-
   it "displays a detailed error message when the user's token is invalid" do
-    # rubocop:disable Layout/LineLength
     visit "/settings/account"
-    allow(Users::RequestDestroy).to receive(:call).and_call_original
     click_button "Delete Account"
     allow(Rails.cache).to receive(:exist?).with("user-destroy-token-#{user.id}").and_return(false)
     expect do
       get user_confirm_destroy_path(token: token)
     end.to raise_error(UserDestroyToken::Errors::InvalidToken)
-    expect(page).to have_text("Your token has expired, please request a new one. Tokens only last for 12 hours after account deletion is initiated.")
-    # rubocop:enable Layout/LineLength
   end
 
   it "raises a 'Not Found' error if there is a token mismatch" do
