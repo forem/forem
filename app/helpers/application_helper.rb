8cd20f57d4d0cbd471c18e2ed2f72aaf1bae2bfa
Ridhwana <ridhwana.khan16@gmail.com>
2021-01-13 12:42:49 +0200

    revert to method + cache

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -174,7 +174,7 @@ module ApplicationHelper
   end
 
   def release_adjusted_cache_key(path)
-    release_footprint = ForemInstance::DEPLOYED_AT
+    release_footprint = ForemInstance.deployed_at
     return path if release_footprint.blank?
 
     "#{path}-#{params[:locale]}-#{release_footprint}-#{SiteConfig.admin_action_taken_at.rfc3339}"
--- app/models/forem_instance.rb
+++ app/models/forem_instance.rb
@@ -1,9 +1,9 @@
 class ForemInstance
-  DEPLOYED_AT = (
-    ApplicationConfig["RELEASE_FOOTPRINT"].presence || ENV["HEROKU_RELEASE_CREATED_AT"].presence
-  ).freeze
+  def self.deployed_at
+    @deployed_at ||= ApplicationConfig["RELEASE_FOOTPRINT"].presence || ENV["HEROKU_RELEASE_CREATED_AT"].presence
+  end
 
-  LATEST_COMMIT_ID = (
-    ApplicationConfig["FOREM_BUILD_SHA"].presence || ENV["HEROKU_SLUG_COMMIT"].presence
-  ).freeze
+  def self.latest_commit_id
+    @latest_commit_id ||= ApplicationConfig["FOREM_BUILD_SHA"].presence || ENV["HEROKU_SLUG_COMMIT"].presence
+  end
 end
