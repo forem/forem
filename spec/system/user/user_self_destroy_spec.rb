c310bc4aac83533972f8266e009d607750d10809
Julianna Tetreault <juliannatetreault@gmail.com>
2021-01-14 11:47:40 -0700

    Cleans up spec and adds an additional test around logged out user edge case

--- spec/system/user/user_self_destroy_spec.rb
+++ spec/system/user/user_self_destroy_spec.rb
@@ -3,7 +3,6 @@ require "rails_helper"
 RSpec.describe "User destroys their profile", type: :system, js: true do
   let(:user) { create(:user) }
   let(:token) { SecureRandom.hex(10) }
-  let(:mismatched_token) { SecureRandom.hex(10) }
 
   before do
     sign_in user
@@ -16,13 +15,20 @@ RSpec.describe "User destroys their profile", type: :system, js: true do
     expect(Users::RequestDestroy).to have_received(:call).with(user)
   end
 
+  it "displays a detailed error message when the user is not logged in" do
+    sign_out user
+    visit "/users/confirm_destroy/#{token}"
+    expect(page).to have_text("You must be logged in to proceed with account deletion.")
+  end
+
   it "displays a detailed error message when the user's token is invalid" do
     visit "/settings/account"
     click_button "Delete Account"
-    allow(Rails.cache).to receive(:exist?).with("user-destroy-token-#{user.id}").and_return(false)
-    expect do
-      get user_confirm_destroy_path(token: token)
-    end.to raise_error(UserDestroyToken::Errors::InvalidToken)
+    allow(Rails.cache).to receive(:read).and_return(token)
+    get user_confirm_destroy_path(token: nil)
+    # rubocop:disable Layout/LineLength
+    expect(page).to have_text("Your token has expired, please request a new one. Tokens only last for 12 hours after account deletion is initiated.")
+    # rubocop:enable Layout/LineLength
   end
 
   it "raises a 'Not Found' error if there is a token mismatch" do
@@ -30,7 +36,7 @@ RSpec.describe "User destroys their profile", type: :system, js: true do
     click_button "Delete Account"
     allow(Rails.cache).to receive(:read).and_return(token)
     expect do
-      get user_confirm_destroy_path(token: mismatched_token)
+      get user_confirm_destroy_path(token: "mismatched token")
     end.to raise_error(ActionController::RoutingError)
   end
 
