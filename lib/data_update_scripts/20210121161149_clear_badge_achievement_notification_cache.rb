ebf22dc8289334eab5c1a8ca3012cf77db144c6f
Zhao-Andy <17884966+Zhao-Andy@users.noreply.github.com>
2021-01-21 13:05:13 -0500

    Remove unnecessary cache for badge achievement notifications

--- lib/data_update_scripts/20210121161149_clear_badge_achievement_notification_cache.rb
+++
@@ -1,9 +0,0 @@
-module DataUpdateScripts
-  class ClearBadgeAchievementNotificationCache
-    def run
-      Badge.ids.each do |badge_id|
-        Rails.cache.delete("activity-badge-reward-#{badge_id}")
-      end
-    end
-  end
-end
