0a39db7db4ab52cd631cf195252e93f98642ed2f
Michael Kohl <citizen428@dev.to>
2021-01-21 13:28:16 +0700

    Fix spec

--- spec/system/articles/user_fixes_a_draft_spec.rb
+++ spec/system/articles/user_fixes_a_draft_spec.rb
@@ -11,14 +11,14 @@ RSpec.describe "", type: :system do
 
   it "let's the user fix a broken draft without publishing", js: true, aggregate_failures: true do
     # Create a new blog post and add content that validates correctly
-    sign_in create(:user)
+    sign_in create(:user, editor_version: "v2")
     visit new_path
     fill_in "article-form-title", with: "Regression spec"
     fill_in "article_body_markdown", with: correct_liquid_tag
 
     # Save the draft
     click_button "Save draft"
-    expect(page).to have_content("Unpublished post")
+    expect(page).to have_content("Unpublished Post")
 
     # Edit the blog post with content that doesn't pass validations
     click_link("Click to edit")
@@ -33,6 +33,6 @@ RSpec.describe "", type: :system do
     # Fix the error
     fill_in "article_body_markdown", with: correct_liquid_tag
     click_button "Save draft"
-    expect(page).to have_content("Unpublished post")
+    expect(page).to have_content("Unpublished Post")
   end
 end
