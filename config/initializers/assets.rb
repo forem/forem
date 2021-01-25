e170f8cdc961ee5f9f3ecb0613ae247a305b6a80
Michael Kohl <citizen428@gmail.com>
2021-01-25 09:34:59 +0700

    Attempt to fix sassc segfault

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
+# See: https://github.com/sass/sassc-ruby/issues/197
+Rails.application.config.assets.configure do |env|
+  env.export_concurrent = false
+end
+
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.1"
 
