59de5cae0888dd5d87a3d5d77c52f9024ebb46e7
Ridhwana <ridhwana.khan16@gmail.com>
2021-01-25 17:53:56 +0200

    chore: oops remove the functions form the worker to the model so it can be re-used

--- app/workers/data_update_worker.rb
+++ app/workers/data_update_worker.rb
@@ -5,38 +5,8 @@ class DataUpdateWorker
   def perform
     DataUpdateScript.scripts_to_run.each do |script|
       script.mark_as_run!
-      log_status(script)
-
-      run_script(script)
+      script.log_status
+      script.run_script
     end
-  end
-
-  private
-
-  def run_script(script)
-    require script.file_path
-
-    script.file_class.new.run
-
-    script.mark_as_finished!
-    log_status(script)
-  rescue StandardError => e
-    script.mark_as_failed!(e)
-    log_status(script)
-
-    Honeybadger.notify(e, context: { script_id: script.id })
-  end
-
-  def log_status(script)
-    status = script.status
-    file_name = script.file_name
-
-    logger_destination = status.to_sym == :failed ? :error : :info
-    Rails.logger.public_send(
-      logger_destination,
-      "time=#{Time.current.rfc3339}, script=#{file_name}, status=#{status}",
-    )
-
-    DatadogStatsClient.increment("data_update_scripts.status", tags: ["status:#{status}", "script_name:#{file_name}"])
   end
 end
