8380ce5015165eee53ed6b41052c0199a690219f
Ridhwana <ridhwana.khan16@gmail.com>
2021-01-25 17:52:22 +0200

    feat: order the data update scripts by the latest script that ran

--- app/controllers/admin/data_update_scripts_controller.rb
+++ app/controllers/admin/data_update_scripts_controller.rb
@@ -3,7 +3,7 @@ module Admin
     layout "admin"
 
     def index
-      @data_update_scripts = DataUpdateScript.all
+      @data_update_scripts = DataUpdateScript.order(run_at: :desc)
     end
   end
 end
