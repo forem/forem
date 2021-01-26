5bc76bf50491baa1ecb0fba9e896993a0b6f374c
Ridhwana <ridhwana.khan16@gmail.com>
2021-01-26 14:41:36 +0200

    chore: remove newline

--- app/controllers/admin/data_update_scripts_controller.rb
+++ app/controllers/admin/data_update_scripts_controller.rb
@@ -7,12 +7,10 @@ module Admin
     end
 
     def run_data_update_scripts
-
       data_update_script_id = params[:id]
       response = DataUpdateScript.force_run(data_update_script_id)
 
       render json: { response: response }
     end
-
   end
 end
