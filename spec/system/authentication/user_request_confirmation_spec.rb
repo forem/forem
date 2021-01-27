e9a7c031bc8d58bb25576f89085d4ff92be40902
Michael Kohl <citizen428@dev.to>
2021-01-27 18:17:02 +0700

    Fix spec

--- spec/system/authentication/user_request_confirmation_spec.rb
+++ spec/system/authentication/user_request_confirmation_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe "/confirm-email", type: :system do
   it "stays on the same page and displays a flash message", :aggregate_failures do
     visit confirm_email_path
     fill_in "user_email", with: "test@example.com"
-    click_button "Save draft"
+    click_button "Resend confirmation instructions"
 
-    expect(page).to have_current_path("/users/confirmation")
+    expect(page).to have_current_path(user_confirmation_path)
     expected_message = format(ConfirmationsController::FLASH_MESSAGE,
                               email: SiteConfig.email_addresses[:members])
     expect(page).to have_content(expected_message)
