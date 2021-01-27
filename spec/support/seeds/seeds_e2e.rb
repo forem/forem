2a136afed81811f0288da1b01c012e2b2681d80c
Nick Taylor <nick@dev.to>
2021-01-27 10:04:51 -0500

    Added a check to only run for E2E tests.

--- spec/support/seeds/seeds_e2e.rb
+++ spec/support/seeds/seeds_e2e.rb
@@ -1,14 +1,11 @@
 return if Rails.env.production?
+return unless Rails.env.test? && ENV["E2E"].present?
 
 # NOTE: when adding new data, please use the Seeder class to ensure the seed tasks
 # stays idempotent.
 require Rails.root.join("app/lib/seeder")
 
-# we use this to be able to increase the size of the seeded DB at will
-# eg.: `SEEDS_MULTIPLIER=2 rails db:seed` would double the amount of data
 seeder = Seeder.new
-SEEDS_MULTIPLIER = [1, ENV["SEEDS_MULTIPLIER"].to_i].max
-puts "Seeding with multiplication factor: #{SEEDS_MULTIPLIER}\n\n"
 
 ##############################################################################
 # Default development site config if different from production scenario
