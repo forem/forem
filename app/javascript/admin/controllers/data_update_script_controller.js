ff91a30871d570a78b9b1e9b63c5c670cb29ff31
Ridhwana <ridhwana.khan16@gmail.com>
2021-01-28 14:18:03 +0200

    feat: very first draft of using a polling mechanism on the show method after we kickoff the sidekiq job (still need to error handle)

--- app/javascript/admin/controllers/data_update_script_controller.js
+++ app/javascript/admin/controllers/data_update_script_controller.js
@@ -2,19 +2,28 @@ import { Controller } from 'stimulus';
 
 export default class DataUpdateScriptController extends Controller {
   forceRun() {
+
     event.preventDefault()
-    const id = event.target.dataset.value;
 
-    let formData = new FormData();
-    formData.append('id', id);
-
+    const id         = event.target.dataset.value;
     let statusColumn = document.getElementById(`data_update_script_${id}_status`);
     let runAtColumn  = document.getElementById(`data_update_script_${id}_run_at`);
     let button       = document.getElementById(`data_update_script_${id}_button`);
 
+    this.showLoadingIndicators(statusColumn, runAtColumn, button);
+    this.kickoffSidekiqWorker(id, statusColumn, runAtColumn, button);
+    this.pollForResponse(id, statusColumn, runAtColumn, button);
+  }
+
+  showLoadingIndicators(statusColumn, runAtColumn, button) {
     statusColumn.innerHTML = "loading..";
     runAtColumn.innerHTML  = "loading..";
     button.innerHTML       = "loading..";
+  }
+
+  kickoffSidekiqWorker(id, statusColumn, runAtColumn, button) {
+    const formData = new FormData();
+    formData.append('id', id);
 
     fetch(`/admin/data_update_scripts/${id}/force_run`, {
       method: 'POST',
@@ -23,16 +32,55 @@ export default class DataUpdateScriptController extends Controller {
       },
       body: formData,
       credentials: 'same-origin'
-    }).then(response => response.json()
-      .then(json => {
-        let updatedScript      = json.response;
+    })
+    // .then(response => response.json()
+    //   .then(json => {
+    //     // handle the sidekiq worker post response
+    //   })
+    // )
 
-        statusColumn.innerHTML = `${updatedScript.status}`;
-        if(updatedScript.error) {
-          statusColumn.innerHTML += `<div class='fs-xs'> ${updatedScript.error}</div>`
+  }
+
+  pollForResponse(id, statusColumn, runAtColumn, button) {
+    let counter = 0;
+    let pollForStatus = setInterval(() => {
+      counter++;
+      this.checkForUpdatedDataScript(id).then((updatedDataScript) => {
+        if (updatedDataScript) {
+          statusColumn.innerHTML = `${updatedDataScript.status}`;
+          if(updatedDataScript.error) {
+            statusColumn.innerHTML += `<div class='fs-xs'> ${updatedDataScript.error}</div>`
+          }
+          runAtColumn.innerHTML = updatedDataScript.run_at;
+
+          if(updatedDataScript.status !== "succeeded") {
+            button.innerHTML      = `<button onclick=forceRun(${updatedDataScript.id}); return false; type='button' classname='crayons-btn crayons-btn--secondary'>Re run</button>`;
+          }
+
+          clearInterval(pollForStatus);
         }
-        runAtColumn.innerHTML = updatedScript.run_at;
-        button.innerHTML      = `<button onclick=forceRun(${updatedScript.id}); return false; type='button' classname='crayons-btn crayons-btn--secondary'>Re run</button>`
+      });
+      if ( counter > 15 ) {
+        clearInterval(pollForStatus);
+      }
+    }, 1000)
+  }
+
+  checkForUpdatedDataScript(id) {
+    return fetch(`/admin/data_update_scripts/${id}`, {
+      method: 'GET',
+      headers: {
+       'X-CSRF-Token': document.querySelector("meta[name='csrf-token']").content,
+      },
+      credentials: 'same-origin'
+    }).then(response => response.json() //do some error handling
+      .then(json => {
+        let script = json.response;
+        if(script.status === "enqueued" || script.status === "working") {
+          return false;
+        } else {
+          return script;
+        }
       })
     )
   }
