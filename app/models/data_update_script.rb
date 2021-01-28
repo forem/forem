95e558778a04a908d65bd9a1a8e1f73f933f6b7b
Ridhwana <ridhwana.khan16@gmail.com>
2021-01-28 14:15:34 +0200

    fix: move the code from the model back into the worker so that we can reuse it in the controller

--- app/models/data_update_script.rb
+++ app/models/data_update_script.rb
@@ -25,40 +25,6 @@ class DataUpdateScript < ApplicationRecord
       false
     end
 
-    def force_run(id)
-      script = DataUpdateScript.find(id)
-      log_status(script)
-      run_script(script)
-
-      script
-    end
-
-    def run_script(script)
-      require script.file_path
-      script.file_class.new.run
-
-      script.mark_as_finished!
-      log_status(script)
-    rescue StandardError => e
-      script.mark_as_failed!(e)
-      log_status(script)
-
-      Honeybadger.notify(e, context: { script_id: script.id })
-    end
-
-    def log_status(script)
-      status = script.status
-      file_name = script.file_name
-
-      logger_destination = status.to_sym == :failed ? :error : :info
-      Rails.logger.public_send(
-        logger_destination,
-        "time=#{Time.current.rfc3339}, script=#{file_name}, status=#{status}",
-      )
-
-      DatadogStatsClient.increment("data_update_scripts.status", tags: ["status:#{status}", "script_name:#{file_name}"])
-    end
-
     private
 
     def filenames
--- app/workers/data_update_worker.rb
+++ app/workers/data_update_worker.rb
@@ -2,11 +2,47 @@ class DataUpdateWorker
   include Sidekiq::Worker
   sidekiq_options queue: :high_priority, retry: 5
 
-  def perform
-    DataUpdateScript.scripts_to_run.each do |script|
+  def perform(script=nil)
+    if script
+      script = DataUpdateScript.find(id)
       script.mark_as_run!
       script.log_status
       script.run_script
+    else
+      DataUpdateScript.scripts_to_run.each do |script|
+        script.mark_as_run!
+        script.log_status
+        script.run_script
+      end
     end
+  end
+
+  private
+
+  def run_script(script)
+    require script.file_path
+
+    script.file_class.new.run
+
+    script.mark_as_finished!
+    log_status(script)
+  rescue StandardError => e
+    script.mark_as_failed!(e)
+    log_status(script)
+
+    Honeybadger.notify(e, context: { script_id: script.id })
+  end
+
+  def log_status(script)
+    status = script.status
+    file_name = script.file_name
+
+    logger_destination = status.to_sym == :failed ? :error : :info
+    Rails.logger.public_send(
+      logger_destination,
+      "time=#{Time.current.rfc3339}, script=#{file_name}, status=#{status}",
+    )
+
+    DatadogStatsClient.increment("data_update_scripts.status", tags: ["status:#{status}", "script_name:#{file_name}"])
   end
 end
