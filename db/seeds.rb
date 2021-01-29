c2db36e9c77a50511c1c3f54cbecf8014f5d1d6e
Nick Taylor <nick@forem.com>
2021-01-29 07:34:01 -0500

    Cleaning Test Runs for End to End (E2E) Tests (#12143)
    * Added custom seed task
    
    * Added a test for the initial login for the intial admin of a forem instance.
    
    * Renamed admin login seed data file.
    
    * Pulled functions out of main Cypress plugin file and import them now.
    
    * Added some comments.
    
    * Added a comment about Cypress tasks requiring a return value to be considered successful.
    
    * Moved admin_login_setup seed file to spec folder.
    
    * Made change requests in regards to rake task.
    
    * Fixed reference to SiteConfig
    
    * Created the seeder class to be shared for all seed files.
    
    * Added a missing param to function comment.
    
    * Fixed seeder require in e2e test seed file.
    
    * Fixed seeder require in seeds.rb. Why is Rails.root required?
    
    * Added an environment guard for not being production in the e2e seed task.
    
    * Made seeder require relative.
    
    * Trying something for the Elastic Search issue in CI.
    
    * Revert "Trying something for the Elastic Search issue in CI."
    
    This reverts commit 7cb2a963c8ac1f9242c612a1b9fe8ff814605df6.
    
    * Search indices are now removed via bundle exec rake search:destroy
    
    * Now rake search:destroy is used in the Cypress task for resetting data as well.
    
    * Moved Seeder class to app/lib
    
    * Added cypress-rails
    
    * Got two flows working with db rollbacks.
    
    * Trimmed down the e2e dataset for now.
    
    * Added a custom Cypress command to encompass test setup.
    
    * removed unused e2e rake task
    
    * added some user login/password e2e tests.
    
    * Added a note about Cypress not cleaning cookies, so we do for the moment.
    
    * Removed code no longer required.
    
    * Removed comment in test that is no longer pertinent to the test suite.
    
    * Removed data cleaning plugins as cypress rails gem handles it now.
    
    * Added @citizen428's recommendation got raising an error if trying to seed production.
    
    * Temporarily have paralleziation of e2e tests disabled to figure out cypres-rails/knapsack pro integration.
    
    * Doh! Forgot a semi-colon in the Travis config.
    
    * Added a comment about not integrating cypress-rails/knapsack pro right now.
    
    * Removed knapsack pro npm package for now as we aren't using it.
    
    * Reworded comment about cypress-rails/kanpsackpro.
    
    * Removed seeding for tags to follow for onboarding in e2e seed data.
    
    * Made pree2e script e2e:setup so it only runs explicitly when needed.
    
    * Renamed bin/e2e to bin/e2e-ci.
    
    * Created a new script for local e2e testing.
    
    * Removed RAILS_ENV as it's in the e2e-ci script already.
    
    * Fixed an auto corrected command.
    
    * Fixed wording in e2e script prompt.
    
    * Removed bundle exec rake data_updates:run that I had added. Doesn't appear to be necessary.
    
    * Added missing new lines.
    
    * Renamed e2e seed file and only use that one now. THere are no others.
    
    * Some script cleanup.
    
    * Added a check for the E2E environment so as to not pollute system tests.
    
    * Now e2e test server cleanup occurs when the server shuts down.
    
    * Put puts as this is really just for testing.
    
    * Put environment variable in the proper place for runnning bundle exec.
    
    * Added a check to only run for E2E tests.
    
    * Some bash script formatting.
    
    * Removed copy paste irrelevant comments.
    
    * Updated e2e server shutdown message.
    
    * Now a data-testid attribute is used for finding the user account errors panel.
    
    * Made test selectors ignore casing of text.
    
    * Removed prod paranoia check in e2e seed file.
    
    * Refactored spec to use new cy.loginUser(user) command.
    
    * Update app/lib/seeder.rb
    
    Co-authored-by: Michael Kohl <citizen428@dev.to>
    
    * Fixed flakiness that @aitchiss was experiencing.
    
    * Rubocop fix.
    
    * Due to a Cypress issue, fixing cookie clearing with another tweak.
    
    * Bumped the wait time to 500ms if cookies don't clear.
    
    * Added a 'bundle check' to the bin/e2e script as suggested by @katiedavis.
    
    * Removed some white space
    
    Co-authored-by: Michael Kohl <citizen428@dev.to>

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,43 +2,9 @@
 
 return if Rails.env.production?
 
-# NOTE: when adding new data, please use this class to ensure the seed tasks
+# NOTE: when adding new data, please use the Seeder class to ensure the seed tasks
 # stays idempotent.
-class Seeder
-  def initialize
-    @counter = 0
-  end
-
-  # Used when the block is idempotent by itself and needs no further checks.
-  def create(message)
-    @counter += 1
-    puts "  #{@counter}. #{message}."
-    yield
-  end
-
-  def create_if_none(klass, count = nil)
-    @counter += 1
-    plural = klass.name.pluralize
-
-    if klass.none?
-      message = ["Creating", count, plural].compact.join(" ")
-      puts "  #{@counter}. #{message}."
-      yield
-    else
-      puts "  #{@counter}. #{plural} already exist. Skipping."
-    end
-  end
-
-  def create_if_doesnt_exist(klass, attribute_name, attribute_value)
-    record = klass.find_by("#{attribute_name}": attribute_value)
-    if record.nil?
-      puts "  #{klass} with #{attribute_name} = #{attribute_value} not found, proceeding..."
-      yield
-    else
-      puts "  #{klass} with #{attribute_name} = #{attribute_value} found, skipping."
-    end
-  end
-end
+require Rails.root.join("app/lib/seeder")
 
 # we use this to be able to increase the size of the seeded DB at will
 # eg.: `SEEDS_MULTIPLIER=2 rails db:seed` would double the amount of data
