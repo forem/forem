0d13f5f9e72551a1ec5890338facf8dc34c27ad5
Suzanne Aitchison <suzanne@forem.com>
2021-02-03 09:21:40 +0000

    prevent close button click triggering a modal toggle twice

--- app/javascript/shared/components/focusTrap.jsx
+++ app/javascript/shared/components/focusTrap.jsx
@@ -1,27 +1,43 @@
 import PropTypes from 'prop-types';
+import { h, Fragment } from 'preact';
 import { useLayoutEffect, useRef } from 'preact/hooks';
 import { createFocusTrap } from 'focus-trap';
 import { defaultChildrenPropTypes } from '../../common-prop-types';
+import { KeyboardShortcuts } from './useKeyboardShortcuts';
 
 export const FocusTrap = ({ selector, children, onDeactivate }) => {
   const focusTrap = useRef(null);
 
   useLayoutEffect(() => {
-    focusTrap.current = createFocusTrap(selector, { onDeactivate });
+    focusTrap.current = createFocusTrap(selector, {
+      escapeDeactivates: false,
+    });
+
     focusTrap.current.activate();
     return () => {
       focusTrap.current.deactivate();
     };
   });
 
-  return children;
+  const shortcuts = {
+    escape: onDeactivate,
+  };
+
+  return (
+    <Fragment>
+      {children}
+      <KeyboardShortcuts shortcuts={shortcuts} />
+    </Fragment>
+  );
 };
 
 FocusTrap.defaultProps = {
   selector: '.crayons-modal',
+  onDeactivate: () => {},
 };
 
 FocusTrap.propTypes = {
   selector: PropTypes.string,
   children: defaultChildrenPropTypes.isRequired,
+  onDeactivate: PropTypes.func,
 };
