26b03e1738afb7c42d3e6c5e8a1995f3b236a338
Michael Kohl <citizen428@gmail.com>
2021-02-03 12:39:14 +0700

    Remove extra newline

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_01_31_000458) do
 
   # These are extensions that must be enabled in order to support this database
