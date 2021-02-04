f40f11d5311b86b623e3c705b2c3d7827f024f74
Andy Zhao <17884966+Zhao-Andy@users.noreply.github.com>
2021-02-04 14:41:17 -0500

    Delete all notifications instead of destroying (#12577)
    * Delete all notifications instead of destroying
    
    * Update spec to match new association

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -27,7 +27,7 @@ class Organization < ApplicationRecord
   has_many :credits, dependent: :restrict_with_error
   has_many :display_ads, dependent: :destroy
   has_many :listings, dependent: :destroy
-  has_many :notifications, dependent: :destroy
+  has_many :notifications, dependent: :delete_all
   has_many :organization_memberships, dependent: :delete_all
   has_many :profile_pins, as: :profile, inverse_of: :profile, dependent: :destroy
   has_many :sponsorships, dependent: :destroy
