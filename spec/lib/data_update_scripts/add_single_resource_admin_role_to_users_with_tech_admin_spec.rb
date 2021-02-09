be485b906bb50bb1bb56251bbf2a2a41adb7fbf8
Vaidehi Joshi <vaidehi.sj@gmail.com>
2021-02-09 10:04:32 -0800

    Add spec to ensure that script doesn't impact any other user if tech_admin users don't exist

--- spec/lib/data_update_scripts/add_single_resource_admin_role_to_users_with_tech_admin_spec.rb
+++ spec/lib/data_update_scripts/add_single_resource_admin_role_to_users_with_tech_admin_spec.rb
@@ -4,25 +4,35 @@ require Rails.root.join(
 )
 
 describe DataUpdateScripts::AddSingleResourceAdminRoleToUsersWithTechAdmin do
-  let!(:tech_admin1) { create(:user, :tech_admin) }
-  let!(:tech_admin2) { create(:user, :tech_admin) }
   let!(:admin) { create(:user, :admin) }
 
-  it "adds single_resource_admin roles to users with tech_admin roles" do
-    described_class.new.run
-
-    expect(tech_admin1.reload.roles.pluck(:name)).to include("single_resource_admin")
-    expect(tech_admin2.reload.roles.pluck(:name)).to include("single_resource_admin")
+  context "without any tech_admin users" do
+    it "does not add any roles to any other users with roles" do
+      expect do
+        described_class.new.run
+      end.not_to change(admin.reload.roles, :count)
+    end
   end
 
-  it "sets the correct resource type for the single_resource_admin role" do
-    described_class.new.run
-    expect(tech_admin2.reload.roles.pluck(:resource_type)).to include("DataUpdateScript")
-  end
+  context "with tech_admin users" do
+    let!(:tech_admin1) { create(:user, :tech_admin) }
+    let!(:tech_admin2) { create(:user, :tech_admin) }
 
-  it "does not add single_resource_admin roles alongside other roles" do
-    described_class.new.run
-    expect(admin.reload.roles.pluck(:name)).not_to include("single_resource_admin")
-  end
+    it "adds single_resource_admin roles to users with tech_admin roles" do
+      described_class.new.run
 
+      expect(tech_admin1.reload.roles.pluck(:name)).to include("single_resource_admin")
+      expect(tech_admin2.reload.roles.pluck(:name)).to include("single_resource_admin")
+    end
+
+    it "sets the correct resource type for the single_resource_admin role" do
+      described_class.new.run
+      expect(tech_admin2.reload.roles.pluck(:resource_type)).to include("DataUpdateScript")
+    end
+
+    it "does not add single_resource_admin roles alongside other roles" do
+      described_class.new.run
+      expect(admin.reload.roles.pluck(:name)).not_to include("single_resource_admin")
+    end
+  end
 end
