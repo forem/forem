90ea02b22571f0a3a58a38dfef5513597ab8af61
rhymes <rhymes@hey.com>
2021-02-09 16:33:46 +0100

    Add :fastly_http_purge feature flag

---
+++ spec/lib/data_update_scripts/add_fastly_http_purge_feature_flag_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+require Rails.root.join(
+  "lib/data_update_scripts/20210209152726_add_fastly_http_purge_feature_flag.rb",
+)
+
+describe DataUpdateScripts::AddFastlyHttpPurgeFeatureFlag do
+  after do
+    FeatureFlag.remove(:fastly_http_purge)
+  end
+
+  it "adds the :fastly_http_purge flag", :aggregate_failures do
+    expect(FeatureFlag.exist?(:fastly_http_purge)).to be(false)
+
+    described_class.new.run
+
+    expect(FeatureFlag.exist?(:fastly_http_purge)).to be(true)
+  end
+
+  it "works if the flag is already available" do
+    FeatureFlag.add(:fastly_http_purge)
+
+    described_class.new.run
+
+    expect(FeatureFlag.exist?(:fastly_http_purge)).to be(true)
+  end
+end
