e1cc8fc98cfdace60a1305c51a3a7013bb6ecead
benhalpern <bendhalpern@gmail.com>
2021-02-11 14:56:36 -0500

    Fix list of attributes

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -31,9 +31,7 @@ class Profile < ApplicationRecord
     twitch_url
     mastodon_url
     website_url
-    git_lab_url
-    linked_in_url
-    stack_overflow_url
+    dribbble_url
   ].freeze
 
   # NOTE: @citizen428 This is a temporary mapping so we don't break DEV during
