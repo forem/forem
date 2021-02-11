0af8fc1866e781a60ad5372da8885db539388933
Michael Kohl <citizen428@dev.to>
2021-02-11 09:28:59 +0700

    Prevent suspended users from self-deleting and returning (#12503)
    * Add new table and model
    
    * Store banned user username hash on delete
    
    * Prevent previously banned user from signing up again
    
    * Update method name
    
    * Refactor code and add more specs
    
    * Test improvements
    
    * Don't override existing global flash
    
    * Fix typo
    
    * Update spec description
    
    * Update schema.rb
    
    * More schema.rb fixes
    
    * Simplify spec
    
    * Update migration
    
    * Clean up migration
    
    * Rename method
    
    * Add DataDog counter
    
    * Revisit error handling
    
    * Remove spurious empty line
    
    * Make model name more explicit

---
+++ spec/system/authentication/user_with_suspended_username_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe "User with suspended username tried to sign up via OAuth" do
+  before do
+    omniauth_mock_twitter_payload
+
+    allow(SiteConfig)
+      .to receive(:authentication_providers)
+      .and_return(Authentication::Providers.available)
+
+    allow(ForemStatsClient).to receive(:increment)
+  end
+
+  context "when a user has been previously banned", :aggregate_failures do
+    it "displays an error message" do
+      username = OmniAuth.config.mock_auth[:twitter].extra.raw_info.username
+      create(:suspended_username, username: username)
+
+      visit sign_up_path
+      click_on("Continue with Twitter", match: :first)
+      expect(page).to have_current_path(root_path)
+
+      expected_message = ::Authentication::Errors::PreviouslyBanned.new.message
+      expect(page).to have_content(expected_message)
+      expect(ForemStatsClient)
+        .to have_received(:increment)
+        .with("identity.errors",
+              tags: [
+                "error:Authentication::Errors::PreviouslyBanned",
+                "message:#{expected_message}",
+              ])
+    end
+  end
+end
