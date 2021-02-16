2e3f26b473316203f601c72bf46c94cf3f070bc5
Nick Taylor <nick@dev.to>
2021-02-16 16:48:56 -0500

    Small refactor to inline mapping of available tags.

--- app/javascript/readingList/components/ItemListTags.jsx
+++ app/javascript/readingList/components/ItemListTags.jsx
@@ -3,21 +3,20 @@ import { h } from 'preact';
 import PropTypes from 'prop-types';
 
 export const ItemListTags = ({ availableTags, selectedTags, onClick }) => {
-  const tagsHTML = availableTags.map((tag) => (
-    <a
-      className={`crayons-link crayons-link--block ${
-        selectedTags.indexOf(tag) > -1 ? 'crayons-link--current' : ''
-      }`}
-      href={`/t/${tag}`}
-      data-no-instant
-      onClick={(e) => onClick(e, tag)}
-    >
-      {`#${tag}`}
-    </a>
-  ));
   return (
     <nav className="crayons-layout__sidebar-left" data-testid="tags">
-      {tagsHTML}
+      {availableTags.map((tag) => (
+        <a
+          className={`crayons-link crayons-link--block ${
+            selectedTags.indexOf(tag) > -1 ? 'crayons-link--current' : ''
+          }`}
+          href={`/t/${tag}`}
+          data-no-instant
+          onClick={(e) => onClick(e, tag)}
+        >
+          {`#${tag}`}
+        </a>
+      ))}
     </nav>
   );
 };
