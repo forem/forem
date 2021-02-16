d3bf06a4ac282a17f70b02d49728b739bcbf9388
Paweł Ludwiczak <ludwiczakpawel@gmail.com>
2021-02-16 15:58:52 +0100

    ipad sidebar fix

--- app/javascript/packs/homePage.jsx
+++ app/javascript/packs/homePage.jsx
@@ -48,8 +48,12 @@ function renderTagsFollowed(user = userData()) {
 function renderSidebar() {
   const sidebarContainer = document.getElementById('sidebar-wrapper-right');
 
-  // If the screen's width is less than 1024px we don't need this extra data.
-  if (sidebarContainer && window.location.pathname === '/') {
+  // If the screen's width is less than 640 we don't need this extra data.
+  if (
+    sidebarContainer &&
+    screen.width >= 640 &&
+    window.location.pathname === '/'
+  ) {
     window
       .fetch('/sidebars/home')
       .then((res) => res.text())
