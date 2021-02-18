8506945513ca306f1f88a9d4a3562cf30a4849e8
Zhao-Andy <17884966+Zhao-Andy@users.noreply.github.com>
2021-02-18 10:52:40 -0500

    Raise error only if user to delete has 2+ identities

--- app/services/moderator/merge_user.rb
+++ app/services/moderator/merge_user.rb
@@ -31,10 +31,10 @@ module Moderator
     private
 
     def handle_identities
-      error_message = "The user being deleted already has two identities. " \
+      error_message = "The user being deleted already has two or more identities. " \
         "Are you sure this is the right user to be deleted? " \
         "If so, a super admin will need to do this from the console to be safe."
-      raise error_message if @delete_user.identities.count.positive?
+      raise error_message if @delete_user.identities.count >= 2
 
       return true if
         @keep_user.identities.count.positive? ||
