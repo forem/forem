50fa30ed86fe7712b491d2628cf3bc4b9ab1a04f
Michael Kohl <citizen428@gmail.com>
2021-02-19 09:32:37 +0700

    Fix CSRF cookie workaround
    As suggested by @fdoxyz

--- config/initializers/persistent_csrf_token_cookie.rb
+++ config/initializers/persistent_csrf_token_cookie.rb
@@ -14,15 +14,22 @@ module ActionController
     COOKIE_NAME = :_csrf_token
 
     def real_csrf_token(session)
-      csrf_token = cookies.encrypted[COOKIE_NAME] || session[:_csrf_token]
-      csrf_token ||= SecureRandom.base64(AUTHENTICITY_TOKEN_LENGTH)
-      cookies.encrypted[COOKIE_NAME] ||= {
-        value: csrf_token,
-        expires: 1.year.from_now,
-        httponly: true
-      }
-      session[:_csrf_token] = csrf_token
-      Base64.strict_decode64(csrf_token)
+      if respond_to?(:cookies)
+        csrf_token = cookies.encrypted[COOKIE_NAME] || session[:_csrf_token]
+        csrf_token ||= SecureRandom.base64(AUTHENTICITY_TOKEN_LENGTH)
+        cookies.encrypted[COOKIE_NAME] ||= {
+          value: csrf_token,
+          expires: 1.year.from_now,
+          httponly: true
+        }
+        session[:_csrf_token] = csrf_token
+        Base64.strict_decode64(csrf_token)
+      else
+        # Based on:
+        # https://github.com/rails/rails/blob/v6.0.2/actionpack/lib/action_controller/metal/request_forgery_protection.rb#L396
+        session[:_csrf_token] ||= SecureRandom.base64(AUTHENTICITY_TOKEN_LENGTH)
+        Base64.strict_decode64(session[:_csrf_token])
+      end
     end
   end
 end
