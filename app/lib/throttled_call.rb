3f17682d9099376a3ba9253331a0226b7b267359
Michael Kohl <citizen428@gmail.com>
2021-02-24 11:22:13 +0700

    Rename class level interface method

--- app/lib/throttled_call.rb
+++ app/lib/throttled_call.rb
@@ -6,7 +6,7 @@ class ThrottledCall
   #
   # @param key [String, Symbol] used for generating the Redis cache key
   # @param throttle_for [ActiveSupport::Duration] blocking time interval
-  def self.call(key, throttle_for:)
+  def self.perform(key, throttle_for:)
     namespaced_key = format(KEY_TEMPLATE, key: key)
     Rails.cache.fetch(namespaced_key, expires_in: throttle_for) do
       yield if block_given?
--- spec/lib/throttled_call_spec.rb
+++ spec/lib/throttled_call_spec.rb
@@ -16,14 +16,14 @@ RSpec.describe ThrottledCall do
   after { Rails.cache.clear }
 
   it "calls the block if the call is not currently throttled" do
-    described_class.call(:test, throttle_for: 1.minute) { service.call }
+    described_class.perform(:test, throttle_for: 1.minute) { service.call }
 
     expect(service).to have_received(:call)
   end
 
   it "does not call the block if the call is currently throttled" do
-    described_class.call(:test, throttle_for: 1.minute) { service.call }
-    described_class.call(:test, throttle_for: 1.minute) { service.call }
+    described_class.perform(:test, throttle_for: 1.minute) { service.call }
+    described_class.perform(:test, throttle_for: 1.minute) { service.call }
 
     expect(service).to have_received(:call).once
   end
