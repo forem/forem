ae43844563939704f80f03d4a0142462b9b0b668
Ridhwana <ridhwana.khan16@gmail.com>
2021-02-26 16:27:00 +0200

    chore:  remove validation in favor of the foreign keys

--- app/models/users/notification_setting.rb
+++ app/models/users/notification_setting.rb
@@ -1,8 +1,6 @@
 module Users
   class NotificationSetting < ApplicationRecord
     self.table_name_prefix = "users_"
-
-    validates :user_id, presence: true
     validates :email_digest_periodic, inclusion: { in: [true, false] }
 
     alias_attribute :subscribed_to_welcome_notifications?, :welcome_notifications
