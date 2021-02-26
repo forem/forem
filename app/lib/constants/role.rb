216728a71a59f316ade46a0718a5c7495b95762f
Zhao-Andy <17884966+Zhao-Andy@users.noreply.github.com>
2021-02-26 11:17:58 -0500

    Use quotes instead of %w and not outsmart myself :(

--- app/lib/constants/role.rb
+++ app/lib/constants/role.rb
@@ -1,33 +1,33 @@
 module Constants
   module Role
-    BASE_ROLES = %w[
-      Warn
-      Comment Suspend
-      Suspend
-      Regular Member
-      Trusted
-      Pro
+    BASE_ROLES = [
+      "Warn",
+      "Comment Suspend",
+      "Suspend",
+      "Regular Member",
+      "Trusted",
+      "Pro",
     ].freeze
 
-    SPECIAL_ROLES = %w[
-      Admin
-      Tech Admin
-      Super Admin
-      Resource Admin: Article
-      Resource Admin: BadgeAchievement
-      Resource Admin: Badge
-      Resource Admin: Broadcast
-      Resource Admin: BufferUpdate
-      Resource Admin: ChatChannel
-      Resource Admin: Comment
-      Resource Admin: Config
-      Resource Admin: DisplayAd
-      Resource Admin: DataUpdateScript
-      Resource Admin: FeedbackMessage
-      Resource Admin: HtmlVariant
-      Resource Admin: ListingCategory
-      Resource Admin: Page
-      Resource Admin: Tag
+    SPECIAL_ROLES = [
+      "Admin",
+      "Tech Admin",
+      "Super Admin",
+      "Resource Admin: Article",
+      "Resource Admin: BadgeAchievement",
+      "Resource Admin: Badge",
+      "Resource Admin: Broadcast",
+      "Resource Admin: BufferUpdate",
+      "Resource Admin: ChatChannel",
+      "Resource Admin: Comment",
+      "Resource Admin: Config",
+      "Resource Admin: DisplayAd",
+      "Resource Admin: DataUpdateScript",
+      "Resource Admin: FeedbackMessage",
+      "Resource Admin: HtmlVariant",
+      "Resource Admin: ListingCategory",
+      "Resource Admin: Page",
+      "Resource Admin: Tag",
     ].freeze
   end
 end
