e915f46bf4663714c48a877e0b396d491bcc5944
Andy Zhao <17884966+Zhao-Andy@users.noreply.github.com>
2021-02-27 09:53:46 -0500

    Resourcify Badge Achievements (#12832)
    * Resourcify badge achievements
    
    * Add test for single resource admin badge achievements
    
    * Add badge and badge achievement roles
    
    * Alphabetize (mostly) and cleanup syntax for easier writing
    
    * Use quotes instead of %w and not outsmart myself :(
    
    * alpha oops

--- app/lib/constants/role.rb
+++ app/lib/constants/role.rb
@@ -1,27 +1,33 @@
 module Constants
   module Role
-    BASE_ROLES = ["Warn",
-                  "Comment Suspend",
-                  "Suspend",
-                  "Regular Member",
-                  "Trusted",
-                  "Pro"].freeze
+    BASE_ROLES = [
+      "Warn",
+      "Comment Suspend",
+      "Suspend",
+      "Regular Member",
+      "Trusted",
+      "Pro",
+    ].freeze
 
-    SPECIAL_ROLES = ["Admin",
-                     "Super Admin",
-                     "Tech Admin",
-                     "Resource Admin: Article",
-                     "Resource Admin: Comment",
-                     "Resource Admin: BufferUpdate",
-                     "Resource Admin: ChatChannel",
-                     "Resource Admin: Page",
-                     "Resource Admin: FeedbackMessage",
-                     "Resource Admin: Config",
-                     "Resource Admin: Broadcast",
-                     "Resource Admin: HtmlVariant",
-                     "Resource Admin: DisplayAd",
-                     "Resource Admin: ListingCategory",
-                     "Resource Admin: Tag",
-                     "Resource Admin: DataUpdateScript"].freeze
+    SPECIAL_ROLES = [
+      "Admin",
+      "Tech Admin",
+      "Super Admin",
+      "Resource Admin: Article",
+      "Resource Admin: Badge",
+      "Resource Admin: BadgeAchievement",
+      "Resource Admin: Broadcast",
+      "Resource Admin: BufferUpdate",
+      "Resource Admin: ChatChannel",
+      "Resource Admin: Comment",
+      "Resource Admin: Config",
+      "Resource Admin: DisplayAd",
+      "Resource Admin: DataUpdateScript",
+      "Resource Admin: FeedbackMessage",
+      "Resource Admin: HtmlVariant",
+      "Resource Admin: ListingCategory",
+      "Resource Admin: Page",
+      "Resource Admin: Tag",
+    ].freeze
   end
 end
