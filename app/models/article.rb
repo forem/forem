0ff8284a68ba3d306d72ef7ac5808bba0d90b384
Alex Timofeev <atimofeev@reactant.ru>
2021-03-05 18:06:50 +0700

    Exclude code highlight panel markup from RSS description  (#11268)
    * Introduce `plain_html` method with minified article body
    
    * Add corresponding test case

--- app/models/article.rb
+++ app/models/article.rb
@@ -386,6 +386,11 @@ class Article < ApplicationRecord
     self.co_author_ids = list_of_co_author_ids.split(",").map(&:strip)
   end
 
+  def plain_html
+    doc = Nokogiri::HTML.fragment(processed_html)
+    doc.search("highlight__panel").each(&:remove).to_html
+  end
+
   private
 
   def search_score
