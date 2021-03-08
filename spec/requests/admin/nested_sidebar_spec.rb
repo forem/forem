4b902b01c039550b86d79298415808193e7f7ffc
Ridhwana <Ridhwana.Khan16@gmail.com>
2021-03-08 13:14:30 +0200

    RFC#50-P2 Restructuring the sidebar to add nested nav items + tabbed nav items  (#12886)
    * feat: add the nested sidebar with some elements
    
    * feat: create a tabbed nav item menu
    
    * feat: add the tabbed nav_item partial to the views that need tabbed nav items
    
    * fix: change variable back
    
    * feat: style the sidebar a bit more
    
    * chore: add some more styles
    
    * feat: add a spec for the nested navigational items
    
    * refactor: a more dynamic tabbed admin helper
    
    * feat: add some more nav items
    
    * fix: controller for reports
    
    * refactor: shorthand if statement
    
    * chore: add the whitespace back
    
    * refactor: rubocop fixes
    
    * chore: use any
    
    * chore: remove whitespace
    
    * refactor: rename the variable
    
    * refactor: use a DSL style admin helper
    
    * chore: variable renaming and routes
    
    * rubocop: fixes
    
    * refactor: move files to more apt places
    
    * chore: keep overview as it was previously
    
    * Update app/views/admin/secrets/index.html.erb
    
    Co-authored-by: Michael Kohl <me@citizen428.net>
    
    * Update app/views/admin/shared/_tabbed_navbar.erb
    
    Co-authored-by: Michael Kohl <me@citizen428.net>
    
    * Update app/views/admin/badges/index.html.erb
    
    Co-authored-by: Michael Kohl <me@citizen428.net>
    
    * chore: disable blocklength
    
    * refactor: move the logic to the model instead of in the view
    
    * chore: remove get_ prefix
    
    * chore: move the request mangling to a helper that finds the controller and scope
    
    * Update app/helpers/admin_helper.rb
    
    Co-authored-by: rhymes <rhymes@hey.com>
    
    * Update app/helpers/admin_helper.rb
    
    Co-authored-by: rhymes <rhymes@hey.com>
    
    * refactor: Address feedback
    
    * oops
    
    * oops use tr
    
    * feat: update specs
    
    * fix: badge achievements
    
    Co-authored-by: Michael Kohl <me@citizen428.net>
    Co-authored-by: rhymes <rhymes@hey.com>

---
+++ spec/requests/admin/nested_sidebar_spec.rb
@@ -0,0 +1,51 @@
+require "rails_helper"
+
+# @ridhwana: rename to sidebar_spec once we can delete the old spec
+RSpec.describe "admin sidebar", type: :request do
+  let(:super_admin) { create(:user, :super_admin) }
+
+  before do
+    sign_in super_admin
+    allow(FeatureFlag).to receive(:enabled?).and_call_original
+  end
+
+  describe "admin_restructure feature flag" do
+    it "shows the correct options in the sidebar when the feature flag is enabled" do
+      allow(FeatureFlag).to receive(:enabled?).with(:admin_restructure).and_return(true)
+
+      get admin_content_manager_articles_url
+
+      expect(response.body).to include("Content Manager")
+    end
+
+    it "does not show the option in the sidebar when the feature flag is disabled" do
+      allow(FeatureFlag).to receive(:enabled?).with(:admin_restructure).and_return(false)
+
+      get admin_articles_path
+
+      expect(response.body).not_to include("Content Manager")
+    end
+  end
+
+  describe "sidebar menu options" do
+    it "shows parent level and nested child items" do
+      allow(FeatureFlag).to receive(:enabled?).with(:admin_restructure).and_return(true)
+
+      get admin_content_manager_articles_url
+
+      expect(response.body).to include("Advanced")
+      expect(response.body).to include("Developer Tools")
+    end
+  end
+
+  describe "tabbed menu options" do
+    it "shows nested grandchildren items where applicable" do
+      allow(FeatureFlag).to receive(:enabled?).with(:admin_restructure).and_return(true)
+
+      get admin_content_manager_badges_url
+
+      expect(response.body).to include("Library")
+      expect(response.body).to include("Achievements")
+    end
+  end
+end
