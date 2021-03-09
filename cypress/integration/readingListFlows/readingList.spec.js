0a5c1dc7d1faab250198b0c71e254cdf22d84807
Nick Taylor <nick@dev.to>
2021-03-09 13:55:46 -0500

    First pass for E2E tests for the reading list.

--- cypress/integration/readingListFlows/readingList.spec.js
+++ cypress/integration/readingListFlows/readingList.spec.js
@@ -1,18 +1,47 @@
-// import { BREAKPOINTS } from '../../../app/javascript/shared/components/useMediaQuery';
+import { BREAKPOINTS } from '../../../app/javascript/shared/components/useMediaQuery';
 
 describe('Reading List', () => {
+  const mediaMatch = {
+    addListener: () => {},
+    removeListener: () => {},
+  };
+  const pageVisitOptions = {
+    onBeforeLoad(window) {
+      cy.stub(window, 'matchMedia', (query) => {
+        // We need to know the implementation details here, but it's
+        // required since Cypress can't evaluate the match.
+        switch (query) {
+          case `(width <= ${BREAKPOINTS.Medium - 1}px)`:
+            return {
+              ...mediaMatch,
+              matches: window.innerWidth <= BREAKPOINTS.Medium - 1,
+            };
+          case `(width >= ${BREAKPOINTS.Medium}px)`:
+            return {
+              ...mediaMatch,
+              matches: window.innerWidth >= BREAKPOINTS.Medium,
+            };
+
+          default:
+            return {
+              ...mediaMatch,
+              matches: false,
+            };
+        }
+      });
+    },
+  };
+
   beforeEach(() => {
     cy.testSetup();
     cy.fixture('users/articleEditorV1User.json').as('user');
-
     cy.get('@user').then((user) => {
-      cy.loginUser(user).then((_response) => {
-        cy.visit('/readinglist');
-      });
+      cy.loginUser(user);
     });
   });
 
   it('should load an empty reading list', () => {
+    cy.visit('/readinglist');
     cy.intercept(
       Cypress.config().baseUrl +
         'search/reactions?page=0&per_page=80&status%5B%5D=valid&status%5B%5D=confirmed',
@@ -24,10 +53,15 @@ describe('Reading List', () => {
       .findByText(/^Your reading list is empty$/i);
     cy.get('@main').findByText(/^View Archive$/i);
     cy.get('@main').findByLabelText(/^Search...$/i);
-    cy.get('@main').findByText(/^Reading list \(0\)$/);
+    cy.get('@main').findByText(/^Reading list \(0\)$/i);
+    cy.get('@main')
+      .findByLabelText(/^Filter by tag$/i)
+      .should('not.exist');
   });
 
-  it('should load an a reading list with items', () => {
+  it('should load the reading list with items on screens smaller than the medium breakpoint', () => {
+    cy.viewport(BREAKPOINTS.Medium - 1, BREAKPOINTS.Medium);
+    cy.visit('/readinglist', pageVisitOptions);
     cy.intercept(
       Cypress.config().baseUrl +
         'search/reactions?page=0&per_page=80&status%5B%5D=valid&status%5B%5D=confirmed',
@@ -41,5 +75,31 @@ describe('Reading List', () => {
     cy.get('@main').findByText(/^View Archive$/i);
     cy.get('@main').findByLabelText(/Search...$/i);
     cy.get('@main').findByText(/^Reading list \(3\)$/);
+    cy.get('@main').findByLabelText(/^Filter by tag$/i, { selector: 'select' });
+    cy.get('@main')
+      .findByText(/^Filter by tag$/i, { selector: 'legend' })
+      .should('not.exist');
+  });
+
+  it('should load the reading list with items on screens larger than the medium breakpoint', () => {
+    cy.viewport(BREAKPOINTS.Large, 600);
+    cy.visit('/readinglist', pageVisitOptions);
+    cy.intercept(
+      Cypress.config().baseUrl +
+        'search/reactions?page=0&per_page=80&status%5B%5D=valid&status%5B%5D=confirmed',
+      { fixture: 'search/readingList.json' },
+    ).as('readingList');
+    cy.wait('@readingList');
+    cy.findByRole('main')
+      .as('main')
+      .findByText(/^Your reading list is empty$/i)
+      .should('not.exist');
+    cy.get('@main').findByText(/^View Archive$/i);
+    cy.get('@main').findByLabelText(/Search...$/i);
+    cy.get('@main').findByText(/^Reading list \(3\)$/);
+    cy.get('@main').findByText(/^Filter by tag$/i, { selector: 'legend' });
+    cy.get('@main')
+      .findByLabelText(/^Filter by tag$/i, { selector: 'select' })
+      .should('not.exist');
   });
 });
