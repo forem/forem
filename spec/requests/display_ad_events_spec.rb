667437ebf2553c8339f953644263d7e7fd5dd589
Michael Kohl <citizen428@dev.to>
2021-03-09 09:06:11 +0700

    [15-minute fix] Add ThrottledCall to DisplayAdEventsController (#12924)
    * Add ThrottledCall to DisplayAdEventsController
    
    * Update old spec

--- spec/requests/display_ad_events_spec.rb
+++ spec/requests/display_ad_events_spec.rb
@@ -5,13 +5,10 @@ RSpec.describe "DisplayAdEvents", type: :request do
   let(:organization) { create(:organization) }
   let(:display_ad) { create(:display_ad, organization_id: organization.id) }
 
-  describe "POST /display_ad_events" do
+  describe "POST /display_ad_events", throttled_call: true do
     context "when user signed in" do
       before do
         sign_in user
-        # rubocop:disable RSpec/AnyInstance
-        allow_any_instance_of(DisplayAdEventsController).to receive(:skip_ad_update?).and_return(false)
-        # rubocop:enable RSpec/AnyInstance
       end
 
       it "creates a display ad click event" do
@@ -38,10 +35,8 @@ RSpec.describe "DisplayAdEvents", type: :request do
 
       it "creates a display ad success rate" do
         ad_event_params = { display_ad_id: display_ad.id, context_type: "home" }
-
-        4.times do
-          post "/display_ad_events", params: { display_ad_event: ad_event_params.merge(category: "impression") }
-        end
+        impression_params = ad_event_params.merge(category: "impression", user: user)
+        create_list(:display_ad_event, 4, impression_params)
 
         post "/display_ad_events", params: { display_ad_event: ad_event_params.merge(category: "click") }
 
@@ -57,6 +52,19 @@ RSpec.describe "DisplayAdEvents", type: :request do
           }
         }
         expect(DisplayAdEvent.last.user_id).to eq(user.id)
+      end
+
+      it "uses a ThrottledCall for data updates" do
+        post "/display_ad_events", params: {
+          display_ad_event: {
+            display_ad_id: display_ad.id,
+            context_type: "home",
+            category: "impression"
+          }
+        }
+
+        expect(ThrottledCall).to have_received(:perform)
+          .with(:display_ads_data_update, throttle_for: instance_of(ActiveSupport::Duration))
       end
     end
   end
