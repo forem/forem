02beb4c99a442fe9e2643567807126c6bdd86033
Anna Buianova <lightallloy@gmail.com>
2021-03-10 13:08:36 +0300

    Articles update refactoring (#12913)
    * Start refactoring articles update
    
    * Articles update refactoring
    
    * Added a couple of tests for Articles::Updater
    
    * Reorganized code in Articles::Updater and ::Attributes a bit
    
    * A couple of more tests for Articles::Attributes
    
    * Reverted changed in the articles_controller
    
    * Edited_at in Articles::Attributes
    
    * Use Articles::Updater in ArticlesController
    
    * Tests for notifications when published/unpublished an article
    
    * Removed old code
    
    * Return result object from Articles::Updater
    
    * Don't reset articles collection when no series was passed
    
    * Fixed sending notifications when published an article the first time
    
    * Fix articles collection when updating
    
    * Fix setting attributes for articles update
    
    * Spec for articles update via api failure
    
    * More tests for Articles::Updater
    
    * More specs for articles updating
    
    * Pass article instead of article_id to articles updater to avoid loading an article twice
    
    * Remove the redundant check in the articles_controller
    
    * Articles::Attributes refactoring

--- spec/services/articles/updater_spec.rb
+++ spec/services/articles/updater_spec.rb
@@ -7,9 +7,68 @@ RSpec.describe Articles::Updater, type: :service do
   let(:draft) { create(:article, user: user, published: false) }
 
   it "updates an article" do
-    described_class.call(user, article.id, attributes)
+    described_class.call(user, article, attributes)
     article.reload
     expect(article.body_markdown).to eq("sample")
+  end
+
+  it "sets a collection" do
+    attributes[:series] = "collection-slug"
+    described_class.call(user, article, attributes)
+    article.reload
+    expect(article.collection).to be_a(Collection)
+  end
+
+  it "creates a collection for the user, not admin when updated by admin" do
+    admin = create(:user, :super_admin)
+    attributes[:series] = "new-slug"
+    described_class.call(admin, article, attributes)
+    expect(article.reload.collection.user).to eq(article.user)
+  end
+
+  it "sets tags" do
+    attributes[:tags] = %w[ruby productivity]
+    described_class.call(user, article, attributes)
+    article.reload
+    expect(article.tags.pluck(:name).sort).to eq(%w[productivity ruby])
+  end
+
+  describe "result" do
+    it "returns success when saved" do
+      result = described_class.call(user, article, attributes)
+      expect(result.success).to be true
+      expect(result.article).to be_a(ArticleDecorator)
+    end
+
+    it "returns not success when not saved" do
+      invalid_attributes = { body_markdown: nil }
+      result = described_class.call(user, article, invalid_attributes)
+      expect(result.success).to be false
+      expect(result.article.errors.any?).to be true
+    end
+  end
+
+  describe "notifications" do
+    it "sends notifications when an article was published" do
+      attributes[:published] = true
+      sidekiq_assert_enqueued_with(job: Notifications::NotifiableActionWorker) do
+        described_class.call(user, draft, attributes)
+      end
+    end
+
+    it "doesn't send when an article was unpublished" do
+      attributes[:published] = false
+      sidekiq_assert_not_enqueued_with(job: Notifications::NotifiableActionWorker) do
+        described_class.call(user, article, attributes)
+      end
+    end
+
+    it "doesn't send when an article went from published to published" do
+      attributes[:published] = true
+      sidekiq_assert_not_enqueued_with(job: Notifications::NotifiableActionWorker) do
+        described_class.call(user, article, attributes)
+      end
+    end
   end
 
   describe "events dispatcher" do
@@ -20,24 +79,24 @@ RSpec.describe Articles::Updater, type: :service do
     end
 
     it "calls the dispatcher" do
-      described_class.call(user, article.id, attributes, event_dispatcher)
+      described_class.call(user, article, attributes, event_dispatcher)
       expect(event_dispatcher).to have_received(:call).with("article_updated", article)
     end
 
     it "doesn't call the dispatcher when unpublished => unpublished" do
-      described_class.call(user, draft.id, attributes, event_dispatcher)
+      described_class.call(user, draft, attributes, event_dispatcher)
       expect(event_dispatcher).not_to have_received(:call)
     end
 
     it "calls the dispatcher when unpublished => published" do
       attributes[:published] = true
-      described_class.call(user, draft.id, attributes, event_dispatcher)
+      described_class.call(user, draft, attributes, event_dispatcher)
       expect(event_dispatcher).to have_received(:call).with("article_updated", draft)
     end
 
     it "calls the dispatcher when published => unpublished" do
       attributes[:published] = false
-      described_class.call(user, article.id, attributes, event_dispatcher)
+      described_class.call(user, article, attributes, event_dispatcher)
       expect(event_dispatcher).to have_received(:call).with("article_updated", article)
     end
   end
