8935c55e8cf7c661578d5c453f2bddc64ba1ff4a
Ridhwana <ridhwana.khan16@gmail.com>
2021-03-10 09:55:28 +0200

    chore: update comment

--- app/models/admin_menu.rb
+++ app/models/admin_menu.rb
@@ -35,7 +35,7 @@ class AdminMenu
       item(name: "mods"),
       item(name: "moderator actions ads", controller: "moderator_actions"),
       item(name: "privileged reactions"),
-      # item(name: "interaction limits", controller: "" )
+      # item(name: "interaction limits") 
     ]
 
     scope :advanced, "flashlight-line", [
