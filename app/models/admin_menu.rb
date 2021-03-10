d509a945b459ba2d6c1ac8f5485a565b4b47bce7
Ridhwana <ridhwana.khan16@gmail.com>
2021-03-10 11:56:05 +0200

    chore: comment

--- app/models/admin_menu.rb
+++ app/models/admin_menu.rb
@@ -1,3 +1,5 @@
+# This "model" is not backed by the database. Its main purpose is to
+# setup and provide methods of to interact with the admin sidebar and tabbed menu
 class AdminMenu
   # On second level navigation with more children, we reference the default tabs controller. i.e look at developer_tools
   # rubocop:disable Metrics/BlockLength
@@ -35,7 +37,7 @@ class AdminMenu
       item(name: "mods"),
       item(name: "moderator actions ads", controller: "moderator_actions"),
       item(name: "privileged reactions"),
-      # item(name: "interaction limits") 
+      # item(name: "interaction limits")
     ]
 
     scope :advanced, "flashlight-line", [
