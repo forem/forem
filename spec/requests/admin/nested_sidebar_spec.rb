05d424d627c8e5d0bf250f0967a6e6a2b8f4d997
Ridhwana <ridhwana.khan16@gmail.com>
2021-03-10 11:56:28 +0200

    test: add more tests

--- spec/requests/admin/nested_sidebar_spec.rb
+++ spec/requests/admin/nested_sidebar_spec.rb
@@ -48,4 +48,49 @@ RSpec.describe "admin sidebar", type: :request do
       expect(response.body).to include("Achievements")
     end
   end
+
+  describe "profile admin feature flag" do
+    before do
+      allow(FeatureFlag).to receive(:enabled?).with(:admin_restructure).and_return(true)
+    end
+
+    it "does not show the option in the sidebar when the feature flag is disabled" do
+      allow(FeatureFlag).to receive(:enabled?).with(:profile_admin).and_return(false)
+
+      get admin_content_manager_articles_path
+
+      expect(response.body).not_to include("Profile Fields")
+    end
+
+    it "shows the option in the sidebar when the feature flag is enabled" do
+      allow(FeatureFlag).to receive(:enabled?).with(:profile_admin).and_return(true)
+      # allow(AdminMenu).to receive(:new).and_return(AdminMenu.new)
+
+      get admin_content_manager_articles_path
+
+      expect(response.body).to include("Profile Fields")
+    end
+  end
+
+  describe "data update script admin feature flag" do
+    before do
+      allow(FeatureFlag).to receive(:enabled?).with(:admin_restructure).and_return(true)
+    end
+
+    it "does not show the option in the sidebar when the feature flag is disabled" do
+      allow(FeatureFlag).to receive(:enabled?).with(:data_update_scripts).and_return(false)
+
+      get admin_advanced_tools_path
+
+      expect(response.body).not_to include("Data Update Scripts")
+    end
+
+    it "shows the option in the sidebar when the feature flag is enabled" do
+      allow(FeatureFlag).to receive(:enabled?).with(:data_update_scripts).and_return(true)
+
+      get admin_advanced_tools_path
+
+      expect(response.body).to include("Data Update Scripts")
+    end
+  end
 end
