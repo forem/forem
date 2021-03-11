2aa37f92be7391c9e9c560115afe6288ef308dac
Fernando Valverde <fdov88@gmail.com>
2021-03-11 15:50:01 -0600

    Cleanup work for SW

--- app/assets/javascripts/serviceworker-companion.js
+++ app/assets/javascripts/serviceworker-companion.js
@@ -1,30 +1,15 @@
 'use strict';
 
 if ('serviceWorker' in navigator) {
-  // Safari has issues with the service worker, so we'll just skip it on those browsers, and unregister it if it's loaded
-  if (
-    /Safari/i.test(navigator.userAgent) &&
-    /Apple Computer/.test(navigator.vendor)
-  ) {
-    //unregister serviceworker
-    navigator.serviceWorker
-      .getRegistration('/serviceworker.js')
-      .then(function (registration) {
-        if (registration) {
-          registration.unregister();
-        }
-      });
-  } else {
-    navigator.serviceWorker
-      .register('/serviceworker.js', { scope: '/' })
-      .then(function swStart(registration) {
-        // registered!
-      })
-      .catch((error) => {
-        // eslint-disable-next-line no-console
-        console.log('ServiceWorker registration failed: ', error);
-      });
-  }
+  navigator.serviceWorker
+    .register('/serviceworker.js', { scope: '/' })
+    .then(function swStart(registration) {
+      // registered!
+    })
+    .catch((error) => {
+      // eslint-disable-next-line no-console
+      console.log('ServiceWorker registration failed: ', error);
+    });
 }
 
 window.addEventListener('beforeinstallprompt', (e) => {
