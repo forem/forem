9f5203393cb682e55053700bd5c90114768e102b
Daniel Uber <djuber@gmail.com>
2021-03-12 12:50:38 -0600

    Move logic to Article#update_score
    Having realized "sum of reaction scores" was an Article concern, I
    found that we have a worker to score articles that does _exactly_ what
    we're doing here.
    
    Let's use that and allow the vomit reaction presence or absence on a
    user to automatically affect the article calculations.
    
    This is at least n + 1 and maybe worse.

--- app/workers/moderator/sink_articles_worker.rb
+++ app/workers/moderator/sink_articles_worker.rb
@@ -8,21 +8,10 @@ module Moderator
       user = User.find_by(id: user_id)
       return unless user
 
-      articles = Article.where(user: user)
-      user_reactions_total =
-        reaction_score(Reaction.where(reactable: user))
-      articles.each do |article|
-        article.update(score: reaction_score(article.reactions) + user_reactions_total)
-      end
+      Article.where(user: user).each(&:update_score)
     rescue StandardError => e
       ForemStatsClient.count("moderators.sink", 1, tags: ["action:failed", "user_id:#{user.id}"])
       Honeybadger.notify(e)
-    end
-
-    private
-
-    def reaction_score(reactions)
-      reactions.sum(:points)
     end
   end
 end
