f0dc9d3ca9816753c3470bc6cbbd4a652df0d9ca
Alex <alexandersmith223@gmail.com>
2021-03-12 11:24:25 -0500

    Change profile_image_90 logic

--- app/services/search/postgres/username.rb
+++ app/services/search/postgres/username.rb
@@ -11,16 +11,10 @@ module Search
       def self.search_documents(term)
         users = search_users(term)
 
-        # We need to re-map profile_image as the profile_image_90 method on the
-        # User model
         users.map do |user|
-          {
-            id: user.id,
-            name: user.name,
-            profile_image_90: user.profile_image_90,
-            username: user.username
-          }
-        end.as_json
+          user.as_json(only: %i[id name username])
+            .merge("profile_image_90" => user.profile_image_90)
+        end
       end
 
       def self.search_users(term)
