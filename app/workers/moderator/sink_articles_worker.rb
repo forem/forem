c92542c4cfd71ac454ff718a0514e146811a59a8
Daniel Uber <djuber@gmail.com>
2021-03-12 12:43:35 -0600

    Correctly handle raising the score when vomit is cleared
    The initial implementation loaded the articles reactions and the
    user's reactions in order to rescore. This restores that activity.
    
    It occurs to me that article.reactions.sum(:points) +
    user.reactions.sum(:points) feels like something articles should know
    how to do, in which case we can move this into article (#rescore!)?
    and call that instead of knowing how to handle moderation specially here.

--- app/workers/moderator/sink_articles_worker.rb
+++ app/workers/moderator/sink_articles_worker.rb
@@ -9,13 +9,20 @@ module Moderator
       return unless user
 
       articles = Article.where(user: user)
-      reactions = Reaction.where(reactable: user)
+      user_reactions_total =
+        reaction_score(Reaction.where(reactable: user))
       articles.each do |article|
-        article.update(score: article.score + reactions.sum(&:points))
+        article.update(score: reaction_score(article.reactions) + user_reactions_total)
       end
     rescue StandardError => e
       ForemStatsClient.count("moderators.sink", 1, tags: ["action:failed", "user_id:#{user.id}"])
       Honeybadger.notify(e)
+    end
+
+    private
+
+    def reaction_score(reactions)
+      reactions.sum(:points)
     end
   end
 end
--- spec/services/moderator/sink_articles_spec.rb
+++ spec/services/moderator/sink_articles_spec.rb
@@ -28,5 +28,20 @@ RSpec.describe Moderator::SinkArticles, type: :service do
         end
       end.to change(scores, :call).from([0, 0, 0]).to([-50, -50, -50])
     end
+
+    context "when removing a user vomit reaction" do
+      before do
+        # pretend we had a confirmed vomit on the user
+        spam_user.articles.update(score: -50)
+      end
+
+      it "raises all of the user's articles but the moderation score" do
+        expect do
+          sidekiq_perform_enqueued_jobs do
+            described_class.call(spam_user.id)
+          end
+        end.to change(scores, :call).from([-50, -50, -50]).to([0, 0, 0])
+      end
+    end
   end
 end
