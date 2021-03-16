e05f4634aa61bf695c7a15a5cf1d4539377efb87
Josh Puetz <josh@dev.to>
2021-03-16 08:33:25 -0500

    Add test for DevicesController (#12985)
    * Add test for DevicesController
    
    * Remove defensive calls

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -18,14 +18,14 @@ class DevicesController < ApplicationController
   end
 
   def destroy
-    device = Device.find_by(device_params)
+    device = Device.find_by(id: params[:id])
     unless device
       render json: { error: "Not Found", status: 404 }, status: :not_found
       return
     end
 
-    device&.destroy
-    if device&.destroyed?
+    device.destroy
+    if device.destroyed?
       head :no_content
     else
       render json: { error: device.errors_as_sentence }, status: :bad_request
