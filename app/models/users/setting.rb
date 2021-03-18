30bbc6f2c1aaece4be26747dbe8915fe889814aa
Arit Amana <msarit@gmail.com>
2021-03-18 11:42:13 -0400

    Address PR review suggestions

--- app/models/users/setting.rb
+++ app/models/users/setting.rb
@@ -2,11 +2,11 @@ module Users
   class Setting < ApplicationRecord
     self.table_name_prefix = "users_"
 
-    EDITORS = %w[v1 v2].freeze
-    FONTS = %w[serif sans_serif monospace comic_sans open_dyslexic].freeze
-    INBOXES = %w[open private].freeze
-    NAVBARS = %w[default static].freeze
-    THEMES = %w[default night_theme pink_theme minimal_light_theme ten_x_hacker_theme].freeze
+    enum editor_versions: { v2: 0, v1: 1 }
+    enum config_fonts: { comic_sans: 1, monospace: 2, open_dyslexic: 3, sans_serif: 4, serif: 5 }
+    enum inbox_types: { private: 0, open: 1 }
+    enum config_navbars: { default: 0, static: 1 }
+    enum config_themes: { default: 0, minimal_light_theme: 1, night_theme: 2, pink_theme: 3, ten_x_hacker_theme: 4 }
 
     MESSAGES = {
       invalid_config_font: "%<value>s is not a valid font selection",
@@ -18,20 +18,21 @@ module Users
     before_validation :set_config_input
 
     validates :user_id, presence: true
-    validates :config_font, inclusion: { in: FONTS + ["default".freeze], message: MESSAGES[:invalid_config_font] }
+    validates :config_font,
+              inclusion: { in: config_fonts.merge({ default: 0 }).keys, message: MESSAGES[:invalid_config_font] }
     validates :config_font, presence: true
-    validates :config_navbar, inclusion: { in: NAVBARS, message: MESSAGES[:invalid_config_navbar] }
+    validates :config_navbar, inclusion: { in: config_navbars.keys, message: MESSAGES[:invalid_config_navbar] }
     validates :config_navbar, presence: true
-    validates :config_theme, inclusion: { in: THEMES, message: MESSAGES[:invalid_config_theme] }
+    validates :config_theme, inclusion: { in: config_themes.keys, message: MESSAGES[:invalid_config_theme] }
     validates :config_theme, presence: true
-    validates :editor_version, inclusion: { in: EDITORS, message: MESSAGES[:invalid_editor_version] }
+    validates :editor_version, inclusion: { in: editor_versions.keys, message: MESSAGES[:invalid_editor_version] }
 
     validates :experience_level, numericality: { less_than_or_equal_to: 10 }, allow_blank: true
     validates :feed_referential_link, inclusion: { in: [true, false] }
     validates :feed_url, length: { maximum: 500 }, allow_nil: true
 
     validates :inbox_guidelines, length: { maximum: 250 }, allow_nil: true
-    validates :inbox_type, inclusion: { in: INBOXES }
+    validates :inbox_type, inclusion: { in: inbox_types.keys }
 
     validate :validate_feed_url, if: :feed_url_changed?
 
@@ -47,7 +48,7 @@ module Users
       errors.add(:feed_url, e.message)
     end
 
-    def set_config_input
+    def normalize_config_values
       self.config_theme = config_theme&.tr(" ", "_")
       self.config_font = config_font&.tr(" ", "_")
       self.config_navbar = config_navbar&.tr(" ", "_")
