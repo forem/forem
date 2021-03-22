f9923740eac0f675291f1119a0c5c6df15f789d9
lightalloy <lightallloy@gmail.com>
2021-03-22 13:32:49 +0300

    Data update script for updating credits_awarded

--- db/schema.rb
+++ db/schema.rb
@@ -197,6 +197,7 @@ ActiveRecord::Schema.define(version: 2021_03_12_191925) do
   create_table "badges", force: :cascade do |t|
     t.string "badge_image"
     t.datetime "created_at", null: false
+    t.integer "credits_awarded", default: 0
     t.string "description", null: false
     t.string "slug", null: false
     t.string "title", null: false
