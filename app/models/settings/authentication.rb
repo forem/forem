94ba2245055b79da9993a6502afb7f8d87687b4f
Michael Kohl <citizen428@gmail.com>
2021-03-23 15:19:44 +0700

    Sort fields

--- app/models/settings/authentication.rb
+++ app/models/settings/authentication.rb
@@ -6,25 +6,24 @@ module Settings
     # the cache, or call Settings::Authentication.clear_cache
     cache_prefix { "v1" }
 
-    # Authentication
-    field :allow_email_password_registration, type: :boolean, default: false
     field :allow_email_password_login, type: :boolean, default: true
+    field :allow_email_password_registration, type: :boolean, default: false
     field :allowed_registration_email_domains, type: :array, default: %w[], validates: {
       valid_domain_csv: true
     }
-    field :display_email_domain_allow_list_publicly, type: :boolean, default: false
-    field :require_captcha_for_email_password_registration, type: :boolean, default: false
-    field :authentication_providers, type: :array, default: %w[]
-    field :invite_only_mode, type: :boolean, default: false
-    field :twitter_key, type: :string, default: ApplicationConfig["TWITTER_KEY"]
-    field :twitter_secret, type: :string, default: ApplicationConfig["TWITTER_SECRET"]
-    field :github_key, type: :string, default: ApplicationConfig["GITHUB_KEY"]
-    field :github_secret, type: :string, default: ApplicationConfig["GITHUB_SECRET"]
-    field :facebook_key, type: :string
-    field :facebook_secret, type: :string
     field :apple_client_id, type: :string
     field :apple_key_id, type: :string
     field :apple_pem, type: :string
     field :apple_team_id, type: :string
+    field :authentication_providers, type: :array, default: %w[]
+    field :display_email_domain_allow_list_publicly, type: :boolean, default: false
+    field :facebook_key, type: :string
+    field :facebook_secret, type: :string
+    field :github_key, type: :string, default: ApplicationConfig["GITHUB_KEY"]
+    field :github_secret, type: :string, default: ApplicationConfig["GITHUB_SECRET"]
+    field :invite_only_mode, type: :boolean, default: false
+    field :require_captcha_for_email_password_registration, type: :boolean, default: false
+    field :twitter_key, type: :string, default: ApplicationConfig["TWITTER_KEY"]
+    field :twitter_secret, type: :string, default: ApplicationConfig["TWITTER_SECRET"]
   end
 end
