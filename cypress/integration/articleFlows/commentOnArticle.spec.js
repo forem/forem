ff5a2d376f68f341de4bd907f3741ed3ea5a112b
Mac Siri <krairit.siri@gmail.com>
2021-04-01 16:44:27 -0400

    Update cypress/integration/articleFlows/commentOnArticle.spec.js

--- cypress/integration/articleFlows/commentOnArticle.spec.js
+++ cypress/integration/articleFlows/commentOnArticle.spec.js
@@ -202,6 +202,7 @@ describe('Comment on articles', () => {
       cy.findByText('@search_user_1').should('not.be.visible');
     });
 
+    // TODO: Flaky spec
     xit('should reply to a comment with user mention autocomplete', () => {
       cy.intercept(
         { method: 'GET', url: '/search/usernames' },
