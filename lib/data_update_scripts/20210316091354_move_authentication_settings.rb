61328d8a57c9c019eecc6314a020a417e027fc33
Michael Kohl <citizen428@gmail.com>
2021-04-01 08:20:21 +0700

    Update DUS

--- lib/data_update_scripts/20210316091354_move_authentication_settings.rb
+++ lib/data_update_scripts/20210316091354_move_authentication_settings.rb
@@ -3,7 +3,6 @@ module DataUpdateScripts
     AUTHENTICATION_SETTINGS = %w[
       allow_email_password_login
       allow_email_password_registration
-      allowed_registration_email_domains
       apple_client_id
       apple_key_id
       apple_pem
@@ -14,22 +13,27 @@ module DataUpdateScripts
       github_key
       github_secret
       invite_only_mode
-      providers
       require_captcha_for_email_password_registration
       twitter_key
       twitter_secret
     ].freeze
 
+    ATTRIBUTES = %i[var value created_at updated_at].freeze
+
     def run
       SiteConfig.transaction do
         config_relation = SiteConfig.where(var: AUTHENTICATION_SETTINGS)
-        config_values = config_relation.pluck(:var, :value).map do |var, value|
-          timestamp = Time.current
-          { var: var, value: value, created_at: timestamp, updated_at: timestamp }
+        config_values = config_relation.pluck(*ATTRRIBUTES).map do |values|
+          ATTRIBUTES.zip(values).to_h
         end
         Settings::Authentication.insert_all(config_values)
 
-        SiteConfig.where(var: AUTHENTICATION_SETTINGS).destroy_all
+        # This field has a validation we don't want to skip
+        Settings::Authentication.allowed_registration_email_domains =
+          SiteConfig.allowed_registration_email_domains
+
+        # This field got renamed so we migrate it explicitly
+        Settings::Authentication.providers = SiteConfig.authentication_providers
       end
     end
   end
