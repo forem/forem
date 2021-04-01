9113593fdd31615f0b52eed36b59717bd70f8849
Jacob Herrington <jacobherringtondeveloper@gmail.com>
2021-04-01 11:05:08 -0500

    Apply suggestions from code review
    Co-authored-by: Michael Kohl <me@citizen428.net>

--- lib/data_update_scripts/20210119131811_replace_instances_of_banned_and_comment_banned.rb
+++ lib/data_update_scripts/20210119131811_replace_instances_of_banned_and_comment_banned.rb
@@ -3,8 +3,8 @@ module DataUpdateScripts
     def run
       # Update names for the banned and comment_banned roles
       # Details: https://github.com/forem/forem/pull/11581
-      Role.find_by(name: "banned").update(name: "suspended")
-      Role.find_by(name: "comment_banned").update(name: "comment_suspended")
+      Role.find_by(name: "banned")&.update(name: "suspended")
+      Role.find_by(name: "comment_banned")&.update(name: "comment_suspended")
     end
   end
 end
