40b7a2b8d919877781be87b033957465b84de335
Mac Siri <krairit.siri@gmail.com>
2021-04-05 17:10:46 -0400

    Declare DD_VERSION in prerun.sh 2nd try (#13258)

--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -8,9 +8,9 @@ if [ "$DYNOTYPE" == "run" ]; then
   # more info here: https://docs.honeycomb.io/getting-data-in/ruby/beeline/#using-env-variables-to-control-framework-integrations
   export HONEYCOMB_DISABLE_AUTOCONFIGURE="true"
   export HONEYCOMB_INTEGRATIONS=rails,rake,active_support
+fi
 
-  # Set app version based on HEROKU_SLUG_COMMIT
-  if [ -n "$HEROKU_SLUG_COMMIT" ]; then
-    DD_VERSION=$HEROKU_SLUG_COMMIT
-  fi
+# Set app version based on HEROKU_SLUG_COMMIT
+if [ -n "$HEROKU_SLUG_COMMIT" ]; then
+  DD_VERSION=$HEROKU_SLUG_COMMIT
 fi
