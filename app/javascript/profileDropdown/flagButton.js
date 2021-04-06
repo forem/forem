b8030094d21823426f7518cab2a7046c7e8ce25f
Dan Uber <dan@forem.com>
2021-04-06 12:50:42 -0500

    Wait for current user promise before processing current user

--- app/javascript/profileDropdown/flagButton.js
+++ app/javascript/profileDropdown/flagButton.js
@@ -1,5 +1,7 @@
 /* global userData */
 /* eslint-disable no-alert */
+import { getUserDataAndCsrfToken } from '../chat/util';
+
 /**
  * Adds a flag button visible only to trusted users on profile pages.
  * @function initFlag
@@ -15,53 +17,56 @@ export function initFlag() {
     return;
   }
 
-  const user = userData();
-  if (!user) {
-    return;
-  }
-
-  const { profileUserId, profileUserName } = flagButton.dataset;
-  let isUserFlagged = flagButton.dataset.isUserFlagged === 'true';
-  const trustedOrAdmin = user.trusted || user.admin;
-
-  if (!trustedOrAdmin || user.id === parseInt(profileUserId, 10)) {
-    flagButton.remove();
-  }
-
-  function flag() {
-    const confirmFlag = window.confirm(
-      isUserFlagged
-        ? 'Are you sure you want to unflag this person? This will make all of their posts visible again.'
-        : 'Are you sure you want to flag this person? This will make all of their posts less visible.',
-    );
-
-    if (confirmFlag) {
-      fetch('/reactions', {
-        method: 'POST',
-        headers: {
-          'X-CSRF-Token': window.csrfToken,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          reactable_type: 'User',
-          category: 'vomit',
-          reactable_id: profileUserId,
-        }),
-      })
-        .then((response) => response.json())
-        .then((response) => {
-          if (response.result === 'create') {
-            isUserFlagged = true;
-            flagButton.innerHTML = `Unflag ${profileUserName}`;
-          } else {
-            isUserFlagged = false;
-            flagButton.innerHTML = `Flag ${profileUserName}`;
-          }
-        })
-        .catch((e) => window.alert(`Something went wrong: ${e}`));
+  getUserDataAndCsrfToken().then(({ _current_user, _csrfToken }) => {
+    const user = userData();
+    if (!user) {
+      flagButton.remove;
+      return;
     }
-  }
 
-  flagButton.addEventListener('click', flag);
+    const { profileUserId, profileUserName } = flagButton.dataset;
+    let isUserFlagged = flagButton.dataset.isUserFlagged === 'true';
+    const trustedOrAdmin = user.trusted || user.admin;
+
+    if (!trustedOrAdmin || user.id === parseInt(profileUserId, 10)) {
+      flagButton.remove();
+    }
+
+    function flag() {
+      const confirmFlag = window.confirm(
+        isUserFlagged
+          ? 'Are you sure you want to unflag this person? This will make all of their posts visible again.'
+          : 'Are you sure you want to flag this person? This will make all of their posts less visible.',
+      );
+
+      if (confirmFlag) {
+        fetch('/reactions', {
+          method: 'POST',
+          headers: {
+            'X-CSRF-Token': window.csrfToken,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            reactable_type: 'User',
+            category: 'vomit',
+            reactable_id: profileUserId,
+          }),
+        })
+          .then((response) => response.json())
+          .then((response) => {
+            if (response.result === 'create') {
+              isUserFlagged = true;
+              flagButton.innerHTML = `Unflag ${profileUserName}`;
+            } else {
+              isUserFlagged = false;
+              flagButton.innerHTML = `Flag ${profileUserName}`;
+            }
+          })
+          .catch((e) => window.alert(`Something went wrong: ${e}`));
+      }
+    }
+
+    flagButton.addEventListener('click', flag);
+  });
 }
 /* eslint-enable no-alert */
