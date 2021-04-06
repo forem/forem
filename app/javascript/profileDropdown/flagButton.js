3784be15b7d62e8fe7fb7c2fa1f68025321f1268
Dan Uber <dan@forem.com>
2021-04-06 14:13:55 -0500

    Extract button callback registration to function
    This addresses a code climate concern (function exceeded 50 lines) by
    extracting the button behavior to a function of (button, id, name),
    and calls that within the exported initFlag function.

--- app/javascript/profileDropdown/flagButton.js
+++ app/javascript/profileDropdown/flagButton.js
@@ -7,6 +7,47 @@ import { getUserDataAndCsrfToken } from '../chat/util';
  * @function initFlag
  * @returns {(void|undefined)} This function has no useable return value.
  */
+
+function addFlagUserBehavior(flagButton, userId, userName) {
+  let isUserFlagged = flagButton.dataset.isUserFlagged === 'true';
+
+  function flag() {
+    const confirmFlag = window.confirm(
+      isUserFlagged
+        ? 'Are you sure you want to unflag this person? This will make all of their posts visible again.'
+        : 'Are you sure you want to flag this person? This will make all of their posts less visible.',
+    );
+
+    if (confirmFlag) {
+      fetch('/reactions', {
+        method: 'POST',
+        headers: {
+          'X-CSRF-Token': window.csrfToken,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          reactable_type: 'User',
+          category: 'vomit',
+          reactable_id: userId,
+        }),
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          if (response.result === 'create') {
+            isUserFlagged = true;
+            flagButton.innerHTML = `Unflag ${userName}`;
+          } else {
+            isUserFlagged = false;
+            flagButton.innerHTML = `Flag ${userName}`;
+          }
+        })
+        .catch((e) => window.alert(`Something went wrong: ${e}`));
+    }
+  }
+
+  flagButton.addEventListener('click', flag);
+}
+
 export function initFlag() {
   const flagButton = document.getElementById(
     'user-profile-dropdownmenu-flag-button',
@@ -17,56 +58,20 @@ export function initFlag() {
     return;
   }
 
-  getUserDataAndCsrfToken().then(({ _current_user, _csrfToken }) => {
+  getUserDataAndCsrfToken().then(() => {
     const user = userData();
     if (!user) {
       flagButton.remove;
       return;
     }
 
-    const { profileUserId, profileUserName } = flagButton.dataset;
-    let isUserFlagged = flagButton.dataset.isUserFlagged === 'true';
     const trustedOrAdmin = user.trusted || user.admin;
+    const { profileUserId, profileUserName } = flagButton.dataset;
 
     if (!trustedOrAdmin || user.id === parseInt(profileUserId, 10)) {
       flagButton.remove();
     }
-
-    function flag() {
-      const confirmFlag = window.confirm(
-        isUserFlagged
-          ? 'Are you sure you want to unflag this person? This will make all of their posts visible again.'
-          : 'Are you sure you want to flag this person? This will make all of their posts less visible.',
-      );
-
-      if (confirmFlag) {
-        fetch('/reactions', {
-          method: 'POST',
-          headers: {
-            'X-CSRF-Token': window.csrfToken,
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            reactable_type: 'User',
-            category: 'vomit',
-            reactable_id: profileUserId,
-          }),
-        })
-          .then((response) => response.json())
-          .then((response) => {
-            if (response.result === 'create') {
-              isUserFlagged = true;
-              flagButton.innerHTML = `Unflag ${profileUserName}`;
-            } else {
-              isUserFlagged = false;
-              flagButton.innerHTML = `Flag ${profileUserName}`;
-            }
-          })
-          .catch((e) => window.alert(`Something went wrong: ${e}`));
-      }
-    }
-
-    flagButton.addEventListener('click', flag);
+    addFlagUserBehavior(flagButton, profileUserId, profileUserName);
   });
 }
 /* eslint-enable no-alert */
