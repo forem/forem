6da7a5ee890028de546bebb887e43ec1ab2f15a1
Ridhwana <ridhwana.khan16@gmail.com>
2021-04-08 12:32:58 +0200

    oops: fix super

--- app/helpers/feature_flag_url_helper.rb
+++ app/helpers/feature_flag_url_helper.rb
@@ -1,26 +1,27 @@
 module FeatureFlagUrlHelper
-  # this will need to be done for every admin helpe, but it will be localized
+  # this will need to be done for every admin helper, but it will be localized to
   # this file and can be removed once we've tested the new route structure
 
   def admin_tags_path(**kwargs)
-    determine_path("content_manager", "tags", kwargs)
+    return determine_path("content_manager", "tags", kwargs) if FeatureFlag.enabled?(:admin_restructure)
+
+    super
   end
 
   def admin_articles_path(**kwargs)
-    determine_path("content_manager", "articles", kwargs)
+    return determine_path("content_manager", "articles", kwargs) if FeatureFlag.enabled?(:admin_restructure)
+
+    super
   end
 end
 
 private
 
 def determine_path(scope, resource, kwargs)
-  if FeatureFlag.enabled?(:admin_restructure)
-    str = "/admin/#{scope}/#{resource}"
-    if kwargs
-      str += "?#{kwargs.map { |k, v| "#{k}=#{v}" }.join('&')}"
-    end
-    str
-  else
-    super
+  str = "/admin/#{scope}/#{resource}"
+  if kwargs
+    str += "?#{kwargs.map { |k, v| "#{k}=#{v}" }.join('&')}"
   end
+
+  str
 end
