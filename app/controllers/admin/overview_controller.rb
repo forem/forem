931a0e47b66a82db8a70ffb02caade40e970cc89
Jacob Herrington <jacobherringtondeveloper@gmail.com>
2021-04-10 06:01:10 -0500

    Add open reports count

--- app/controllers/admin/overview_controller.rb
+++ app/controllers/admin/overview_controller.rb
@@ -1,6 +1,11 @@
 module Admin
   class OverviewController < Admin::ApplicationController
     layout "admin"
-    def index; end
+    def index
+      @open_abuse_reports_count =
+        FeedbackMessage
+          .where(status: "Open", feedback_type: "abuse-reports")
+          .count
+    end
   end
 end
