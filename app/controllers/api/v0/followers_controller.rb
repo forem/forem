d58b64c750549578a3b0a481f1bd7f6ea5605c39
Michael Kohl <citizen428@dev.to>
2021-04-13 10:12:42 +0700

    Update `GET /followers` API endpoint (#13287)
    * Add created_at to API followers controller
    
    * Add sorting
    
    * Update API docs
    
    * Update app/controllers/api/v0/followers_controller.rb
    
    Co-authored-by: Jamie Gaskins <jgaskins@gmail.com>
    
    * Update app/controllers/api/v0/followers_controller.rb
    
    Co-authored-by: Jamie Gaskins <jgaskins@gmail.com>
    
    * Use correct date format
    
    * Update API docs
    
    * Add JsonApiSortParams concern
    
    * Update controller to use concern
    
    * Rename and add docs
    
    * Update API docs
    
    * Remove leftover file
    
    * Undo gem changes
    
    * Consistently use field vs param
    
    * Reset vendor/cache for git dependencies
    
    Co-authored-by: Jamie Gaskins <jgaskins@gmail.com>

--- app/controllers/api/v0/followers_controller.rb
+++ app/controllers/api/v0/followers_controller.rb
@@ -1,11 +1,13 @@
 module Api
   module V0
     class FollowersController < ApiController
+      include JsonApiSortParam
+
       before_action :authenticate_with_api_key_or_current_user!
       before_action -> { limit_per_page(default: 80, max: 1000) }
 
       USERS_ATTRIBUTES_FOR_SERIALIZATION = %i[
-        id follower_id follower_type
+        id follower_id follower_type created_at
       ].freeze
       private_constant :USERS_ATTRIBUTES_FOR_SERIALIZATION
 
@@ -13,7 +15,7 @@ module Api
         @follows = Follow.followable_user(@user.id)
           .includes(:follower)
           .select(USERS_ATTRIBUTES_FOR_SERIALIZATION)
-          .order(created_at: :desc)
+          .order(order_criteria)
           .page(params[:page])
           .per(@follows_limit)
       end
@@ -23,6 +25,14 @@ module Api
       def limit_per_page(default:, max:)
         per_page = (params[:per_page] || default).to_i
         @follows_limit = [per_page, max].min
+      end
+
+      def order_criteria
+        parse_sort_param(
+          params[:sort],
+          allowed_fields: [:created_at],
+          default_sort: { created_at: :desc },
+        )
       end
     end
   end
