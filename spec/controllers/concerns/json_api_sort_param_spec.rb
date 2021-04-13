d1c3380161fa564097b94e2d3c49880b2e7dca54
Michael Kohl <citizen428@dev.to>
2021-04-13 10:12:42 +0700

    Update `GET /followers` API endpoint (#13287)
    * Add created_at to API followers controller
    
    * Add sorting
    
    * Update API docs
    
    * Update app/controllers/api/v0/followers_controller.rb
    
    Co-authored-by: Jamie Gaskins <jgaskins@gmail.com>
    
    * Update app/controllers/api/v0/followers_controller.rb
    
    Co-authored-by: Jamie Gaskins <jgaskins@gmail.com>
    
    * Use correct date format
    
    * Update API docs
    
    * Add JsonApiSortParams concern
    
    * Update controller to use concern
    
    * Rename and add docs
    
    * Update API docs
    
    * Remove leftover file
    
    * Undo gem changes
    
    * Consistently use field vs param
    
    * Reset vendor/cache for git dependencies
    
    Co-authored-by: Jamie Gaskins <jgaskins@gmail.com>

---
+++ spec/controllers/concerns/json_api_sort_param_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+
+RSpec.describe JsonApiSortParam do
+  let!(:controller) { Class.new { include JsonApiSortParam }.new }
+
+  it "returns a default sort order if there are no sorting params" do
+    params = controller.parse_sort_param(
+      "",
+      allowed_fields: [:created_at],
+      default_sort: { created_at: :desc },
+    )
+    expect(params).to eq({ created_at: :desc })
+  end
+
+  it "filters out non-allowed params", :aggregate_failures do
+    params = controller.parse_sort_param(
+      "-created_at,updated_at",
+      allowed_fields: [:updated_at],
+      default_sort: { created_at: :desc },
+    )
+    expect(params).not_to have_key(:created_at)
+    expect(params).to have_key(:updated_at)
+  end
+
+  it "generates a sort params hash" do
+    params = controller.parse_sort_param(
+      "created_at,-updated_at",
+      allowed_fields: %i[created_at updated_at],
+      default_sort: { created_at: :desc },
+    )
+    expect(params).to eq({ created_at: :asc, updated_at: :desc })
+  end
+
+  it "sorts the params in the correct order" do
+    params = controller.parse_sort_param(
+      "-updated_at,created_at",
+      allowed_fields: %i[created_at updated_at],
+      default_sort: { created_at: :desc },
+    )
+    expect(params.to_a).to eq([%i[created_at asc], %i[updated_at desc]])
+  end
+end
