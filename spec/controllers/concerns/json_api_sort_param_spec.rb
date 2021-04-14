d19ce9ed0d2188015044916acfd3ce253e49866c
Michael Kohl <citizen428@dev.to>
2021-04-14 10:32:07 +0700

    15 minute fix: Add default argument to JsonApiSortParam (#13369)
    * Add default argument to JsonApiSortParam
    
    * Update usage

--- spec/controllers/concerns/json_api_sort_param_spec.rb
+++ spec/controllers/concerns/json_api_sort_param_spec.rb
@@ -1,7 +1,20 @@
 require "rails_helper"
 
 RSpec.describe JsonApiSortParam do
-  let!(:controller) { Class.new { include JsonApiSortParam }.new }
+  let!(:controller) do
+    Class.new(Api::V0::ApiController) { include JsonApiSortParam }.new
+  end
+
+  it "uses the `sort` param of the controller by default" do
+    params = ActionController::Parameters.new(sort: "created_at,-updated_at")
+    allow(controller).to receive(:params).and_return(params)
+
+    params = controller.parse_sort_param(
+      allowed_fields: %i[created_at updated_at],
+      default_sort: { created_at: :desc },
+    )
+    expect(params).to eq({ created_at: :asc, updated_at: :desc })
+  end
 
   it "returns a default sort order if there are no sorting params" do
     params = controller.parse_sort_param(
