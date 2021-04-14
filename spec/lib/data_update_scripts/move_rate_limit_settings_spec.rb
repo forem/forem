ce4944a985976e553f9563e344f32a83a3eb1fb0
Michael Kohl <me@citizen428.net>
2021-04-14 13:18:50 +0700

    Add DUS

---
+++ spec/lib/data_update_scripts/move_rate_limit_settings_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+require Rails.root.join(
+  "lib/data_update_scripts/20210414060839_move_rate_limit_settings.rb",
+)
+
+describe DataUpdateScripts::MoveRateLimitSettings do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
