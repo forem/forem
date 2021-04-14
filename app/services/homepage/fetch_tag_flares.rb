ef66fd35f59f00314d914f9c265d57ef1261e7cd
rhymes <rhymes@hey.com>
2021-04-14 10:32:32 +0200

    Addressed feedback

--- app/services/homepage/fetch_tag_flares.rb
+++ app/services/homepage/fetch_tag_flares.rb
@@ -3,26 +3,24 @@ module Homepage
     ATTRIBUTES = %i[name bg_color_hex text_color_hex].freeze
 
     def self.call(articles)
-      articles_tag_flares = articles_tag_flares(articles)
+      flares = articles_tag_flares(articles)
 
-      tags = Tag.where(name: articles_tag_flares.keys).select(*ATTRIBUTES).index_by(&:name)
+      tags = Tag.where(name: flares.keys).select(*ATTRIBUTES).index_by(&:name)
 
-      tag_flares = {}
-      articles_tag_flares.each do |tag_name, article_ids|
+      flares.each_with_object({}) do |(tag_name, article_ids), tag_flares|
         article_ids.each do |article_id|
           tag_flares[article_id] = tags[tag_name].as_json(only: ATTRIBUTES)
         end
       end
-
-      tag_flares
     end
 
     def self.articles_tag_flares(articles)
       flare_tags_for_articles = Hash.new { |hash, key| hash[key] = [] }
 
+      flare_tag_names = ::Constants::Tags::FLARE_TAG_NAMES.to_set
       articles.pluck(:id, :cached_tag_list).map do |article_id, cached_tag_list|
         tags = cached_tag_list.split(", ")
-        flare_tag = ::Constants::Tags::FLARE_TAG_NAMES.to_set.intersection(tags).first
+        flare_tag = flare_tag_names.intersection(tags).first
         next if flare_tag.blank?
 
         flare_tags_for_articles[flare_tag] << article_id
