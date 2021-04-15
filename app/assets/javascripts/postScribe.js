c7dab5e2e2670add7b828f6019c9a3d7ca580bff
Julien Maury <6878027+jmau111@users.noreply.github.com>
2021-04-15 10:50:35 +0200

    disable no-undef and bail if not defined

--- app/assets/javascripts/postScribe.js
+++ app/assets/javascripts/postScribe.js
@@ -1,19 +1,23 @@
 var waitingOnPostscribe = setInterval(function () {
-  if (typeof (postscribe) === "function") {
-    clearInterval(waitingOnPostscribe);
-    var els = document.getElementsByClassName("ltag_gist-liquid-tag");
-    for (var i = 0; i < els.length; i++) {
-      let current = els[i];
-      postscribe(current, current.firstElementChild.outerHTML, {
-        beforeWrite: function (context) {
-          return function (text) {
-            if (context.childElementCount > 3) {
-              return "";
-            }
-            return text;
+
+  if (typeof postscribe === "undefined") {
+    return false;
+  }
+
+  clearInterval(waitingOnPostscribe);
+  var els = document.getElementsByClassName("ltag_gist-liquid-tag");
+  for (var i = 0; i < els.length; i++) {
+    let current = els[i];
+    // eslint-disable-next-line no-undef
+    postscribe(current, current.firstElementChild.outerHTML, {
+      beforeWrite: function (context) {
+        return function (text) {
+          if (context.childElementCount > 3) {
+            return "";
           }
-        }(current)
-      });
-    }
+          return text;
+        }
+      }(current)
+    });
   }
 }, 500);
