06be39278b345c400c32cee9b39bc5bccdbd547f
Julien Maury <6878027+jmau111@users.noreply.github.com>
2021-04-15 15:19:07 +0200

    put js on subfolder utilities

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,7 @@
 // https://github.com/rails/sprockets/blob/master/UPGRADING.md#manifestjs
 //= link_tree ../images
 //= link_directory ../javascripts .js
+//= link_directory ../javascripts/utilities .js
 //= link_directory ../stylesheets .css
 //= link s3_direct_upload.js
 //= link katex.css
