972f9eb867960df57c81842d10a04c24b08ecfdd
Julien Maury <6878027+jmau111@users.noreply.github.com>
2021-04-17 20:00:17 +0100

    use InstantClick

--- app/javascript/packs/articlePage.jsx
+++ app/javascript/packs/articlePage.jsx
@@ -1,70 +1,72 @@
-import { h, render } from 'preact';
-import { Snackbar, addSnackbarItem } from '../Snackbar';
+import { h, render } from 'preact'; 
+import { Snackbar, addSnackbarItem } from '../Snackbar'; 
 import { addFullScreenModeControl } from '../utilities/codeFullscreenModeSwitcher';
 import { embedGists } from '../shared/components/gist';
-
-const fullscreenActionElements = document.getElementsByClassName(
-  'js-fullscreen-code-action',
-);
-
-if (fullscreenActionElements) {
-  addFullScreenModeControl(fullscreenActionElements);
-}
-
-// The Snackbar for the article page
-const snackZone = document.getElementById('snack-zone');
-
-if (snackZone) {
-  render(<Snackbar lifespan="3" />, snackZone);
-}
-
-// eslint-disable-next-line no-restricted-globals
-top.addSnackbarItem = addSnackbarItem;
-
-const userDataIntervalID = setInterval(async () => {
-  const { user = null, userStatus } = document.body.dataset;
-
-    clearInterval(userDataIntervalID);
-    const root = document.getElementById('comment-subscription');
-    const isLoggedIn = (userStatus === "logged-in");
-
-    try {
-      const {
-        getCommentSubscriptionStatus,
-        setCommentSubscriptionStatus,
-        CommentSubscription,
-      } = await import('../CommentSubscription');
-
-      const { articleId } = document.getElementById('article-body').dataset;
-
-      let subscriptionType = 'not_subscribed';
-
-      if (isLoggedIn && user !== null) {
-        ({ config: subscriptionType } = await getCommentSubscriptionStatus(
-          articleId,
-        ));
-      }
-
-      const subscriptionRequestHandler = async (type) => {
-        const message = await setCommentSubscriptionStatus(articleId, type);
-
-        addSnackbarItem({ message, addCloseButton: true });
-      };
-
-      render(
-        <CommentSubscription
-          subscriptionType={subscriptionType}
-          positionType="static"
-          onSubscribe={subscriptionRequestHandler}
-          onUnsubscribe={subscriptionRequestHandler}
-          isLoggedIn={isLoggedIn}
-        />,
-        root,
-      );
-    } catch (e) {
-      document.getElementById('comment-subscription').innerHTML =
-        '<p className="color-accent-danger">Unable to load Comment Subscription component.<br />Try refreshing the page.</p>';
-    }
+ 
+const fullscreenActionElements = document.getElementsByClassName( 
+  'js-fullscreen-code-action', 
+); 
+ 
+if (fullscreenActionElements) { 
+  addFullScreenModeControl(fullscreenActionElements); 
+} 
+ 
+// The Snackbar for the article page 
+const snackZone = document.getElementById('snack-zone'); 
+ 
+if (snackZone) { 
+  render(<Snackbar lifespan="3" />, snackZone); 
+} 
+ 
+// eslint-disable-next-line no-restricted-globals 
+top.addSnackbarItem = addSnackbarItem; 
+ 
+const userDataIntervalID = setInterval(async () => { 
+  const { user = null, userStatus } = document.body.dataset; 
+ 
+    clearInterval(userDataIntervalID); 
+    const root = document.getElementById('comment-subscription'); 
+    const isLoggedIn = (userStatus === "logged-in"); 
+ 
+    try { 
+      const { 
+        getCommentSubscriptionStatus, 
+        setCommentSubscriptionStatus, 
+        CommentSubscription, 
+      } = await import('../CommentSubscription'); 
+ 
+      const { articleId } = document.getElementById('article-body').dataset; 
+ 
+      let subscriptionType = 'not_subscribed'; 
+ 
+      if (isLoggedIn && user !== null) { 
+        ({ config: subscriptionType } = await getCommentSubscriptionStatus( 
+          articleId, 
+        )); 
+      } 
+ 
+      const subscriptionRequestHandler = async (type) => { 
+        const message = await setCommentSubscriptionStatus(articleId, type); 
+ 
+        addSnackbarItem({ message, addCloseButton: true }); 
+      }; 
+ 
+      render( 
+        <CommentSubscription 
+          subscriptionType={subscriptionType} 
+          positionType="static" 
+          onSubscribe={subscriptionRequestHandler} 
+          onUnsubscribe={subscriptionRequestHandler} 
+          isLoggedIn={isLoggedIn} 
+        />, 
+        root, 
+      ); 
+    } catch (e) { 
+      document.getElementById('comment-subscription').innerHTML = 
+        '<p className="color-accent-danger">Unable to load Comment Subscription component.<br />Try refreshing the page.</p>'; 
+    } 
 });
 
-embedGists();
+window.InstantClick.on('change', () => {
+  embedGists();// todo: still doublon when clicking from a list
+});
