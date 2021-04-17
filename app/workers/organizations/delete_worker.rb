b57f2e65a5a58b8344c179f901508c1935e3d96d
lightalloy <lightallloy@gmail.com>
2021-04-17 11:26:45 +0300

    Renamed user_id in the org delete worker

--- app/workers/organizations/delete_worker.rb
+++ app/workers/organizations/delete_worker.rb
@@ -4,12 +4,12 @@ module Organizations
 
     sidekiq_options queue: :high_priority, retry: 10
 
-    # user_id - the user who deletes the organization
-    def perform(organization_id, user_id)
+    # operator_id - the user who deletes the organization
+    def perform(organization_id, operator_id)
       org = Organization.find_by(id: organization_id)
       return unless org
 
-      user = User.find_by(id: user_id)
+      user = User.find_by(id: operator_id)
       return unless user
 
       Organizations::Delete.call(org)
