954845cd3b2c550687c1dc7a07bd142c60011491
Julien Maury <6878027+jmau111@users.noreply.github.com>
2021-04-18 11:34:02 +0200

    embed only if gists

--- app/javascript/shared/components/gist.js
+++ app/javascript/shared/components/gist.js
@@ -4,18 +4,20 @@ export function embedGists() {
   const waitingOnPostscribe = setInterval(() => {
     clearInterval(waitingOnPostscribe);
     const els = document.getElementsByClassName("ltag_gist-liquid-tag");
-    for (let i = 0; i < els.length; i += 1 ) {
-      const current = els[i];
-      postscribe(current, current.firstElementChild.outerHTML, {
-        beforeWrite: function (context) {
-          return function (text) {
-            if (context.childElementCount > 3) {
-              return "";
+    if (els.length > 0) {
+      for (let i = 0; i < els.length; i += 1 ) {
+        const current = els[i];
+        postscribe(current, current.firstElementChild.outerHTML, {
+          beforeWrite: function (context) {
+            return function (text) {
+              if (context.childElementCount > 3) {
+                return "";
+              }
+              return text;
             }
-            return text;
-          }
-        }(current)
-      });
+          }(current)
+        });
+      }
     }
   }, 500);
 }
