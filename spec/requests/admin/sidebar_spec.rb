e29d62f090caafb8ad574fcb080a89a94925a1a3
Ridhwana <ridhwana.khan16@gmail.com>
2021-04-19 12:51:52 +0200

    feat: rename sidebar spec

--- spec/requests/admin/sidebar_spec.rb
+++ spec/requests/admin/sidebar_spec.rb
@@ -1,5 +1,6 @@
 require "rails_helper"
 
+# @ridhwana: rename to sidebar_spec once we can delete the old spec
 RSpec.describe "admin sidebar", type: :request do
   let(:super_admin) { create(:user, :super_admin) }
 
@@ -8,13 +9,49 @@ RSpec.describe "admin sidebar", type: :request do
     allow(FeatureFlag).to receive(:enabled?).and_call_original
   end
 
-  describe "profile admin feature flag" do
-    it "shows the option in the sidebar when the feature flag is enabled" do
-      allow(FeatureFlag).to receive(:enabled?).with(:profile_admin).and_return(true)
+  describe "admin_restructure feature flag" do
+    it "shows the correct options in the sidebar when the feature flag is enabled" do
+      allow(FeatureFlag).to receive(:enabled?).with(:admin_restructure).and_return(true)
 
       get admin_articles_path
 
-      expect(response.body).to include("Config: Profile Setup")
+      expect(response.body).to include("Content Manager")
+    end
+
+    it "does not show the option in the sidebar when the feature flag is disabled" do
+      allow(FeatureFlag).to receive(:enabled?).with(:admin_restructure).and_return(false)
+
+      get admin_articles_path
+
+      expect(response.body).not_to include("Content Manager")
+    end
+  end
+
+  describe "sidebar menu options" do
+    it "shows parent level and nested child items" do
+      allow(FeatureFlag).to receive(:enabled?).with(:admin_restructure).and_return(true)
+
+      get admin_articles_path
+
+      expect(response.body).to include("Advanced")
+      expect(response.body).to include("Developer Tools")
+    end
+  end
+
+  describe "tabbed menu options" do
+    it "shows nested grandchildren items where applicable" do
+      allow(FeatureFlag).to receive(:enabled?).with(:admin_restructure).and_return(true)
+
+      get admin_badges_path
+
+      expect(response.body).to include("Library")
+      expect(response.body).to include("Achievements")
+    end
+  end
+
+  describe "profile admin feature flag" do
+    before do
+      allow(FeatureFlag).to receive(:enabled?).with(:admin_restructure).and_return(true)
     end
 
     it "does not show the option in the sidebar when the feature flag is disabled" do
@@ -22,26 +59,37 @@ RSpec.describe "admin sidebar", type: :request do
 
       get admin_articles_path
 
-      expect(response.body).not_to include("Config: Profile Setup")
+      expect(response.body).not_to include("Profile Fields")
+    end
+
+    it "shows the option in the sidebar when the feature flag is enabled" do
+      allow(FeatureFlag).to receive(:enabled?).with(:profile_admin).and_return(true)
+
+      get admin_articles_path
+
+      expect(response.body).to include("Profile Fields")
     end
   end
 
   describe "data update script admin feature flag" do
-    it "shows the option in the sidebar when the feature flag is enabled" do
-      allow(FeatureFlag).to receive(:enabled?).with(:data_update_scripts).and_return(true)
-
-      get admin_articles_path
-
-      expect(response.body).to include("Tech Resources")
-      expect(response.body).to include("Data Update Scripts")
+    before do
+      allow(FeatureFlag).to receive(:enabled?).with(:admin_restructure).and_return(true)
     end
 
-    it "does not show the option in the sidebar when the feature flag is disabled" do
+    it "does not show the option in the tabbed header when the feature flag is disabled" do
       allow(FeatureFlag).to receive(:enabled?).with(:data_update_scripts).and_return(false)
 
-      get admin_articles_path
+      get admin_tools_path
 
       expect(response.body).not_to include("Data Update Scripts")
+    end
+
+    it "shows the option in the tabbed header when the feature flag is enabled" do
+      allow(FeatureFlag).to receive(:enabled?).with(:data_update_scripts).and_return(true)
+
+      get admin_tools_path
+
+      expect(response.body).to include("Data Update Scripts")
     end
   end
 end
