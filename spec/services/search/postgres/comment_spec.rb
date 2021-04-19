6d09d54825c4f9f2fbbd18d6b8bdfe1143ba43c6
Alex <alexandersmith223@gmail.com>
2021-04-19 13:43:23 -0400

    Remove PodcastEpisode skipped spec

--- spec/services/search/postgres/comment_spec.rb
+++ spec/services/search/postgres/comment_spec.rb
@@ -21,29 +21,6 @@ RSpec.describe Search::Postgres::Comment, type: :service do
         expect(ids).not_to include(comment_on_unpublished_article.search_id)
         expect(ids).to include(comment_on_published_article.search_id)
       end
-
-      # We don't currently search against comments on podcast episodes
-      xit "does not include comments from PodcastEpisodes that are unpublished", :aggregate_failures do
-        comment_text = "Ruby on Rails rocks!"
-        published_podcast = create(:podcast, published: true)
-        unpublished_podcast = create(:podcast, published: true)
-        published_podcast_episode = create(:podcast_episode, podcast_id: published_podcast.id)
-        unpublished_podcast_episode = create(:podcast_episode, podcast_id: unpublished_podcast.id)
-        comment_on_published_article = create(:comment,
-                                              body_markdown: comment_text,
-                                              commentable: published_podcast_episode)
-        comment_on_unpublished_article = create(:comment,
-                                                body_markdown: comment_text,
-                                                commentable: unpublished_podcast_episode)
-        unpublished_podcast.update_columns(published: false)
-
-        # rubocop:disable Rails/PluckId
-        ids = described_class.search_documents(term: "rails").pluck(:id)
-        # rubocop:enable Rails/PluckId
-
-        expect(ids).not_to include(comment_on_unpublished_article.search_id)
-        expect(ids).to include(comment_on_published_article.search_id)
-      end
     end
 
     context "when describing the result format" do
