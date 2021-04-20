518068b2ac8d8f5d0e8b31179add247459e7242a
Fernando Valverde <fernando@visualcosita.com>
2021-04-20 12:18:00 -0600

    More naming refactor changes

--- app/policies/consumer_app_policy.rb
+++ app/policies/consumer_app_policy.rb
@@ -1,4 +1,4 @@
-# AppIntegration::FOREM_APP_PLATFORMS are all the platforms supported by every
+# ConsumerApp::FOREM_APP_PLATFORMS are all the platforms supported by every
 # Forem instance by default. These are App Integrations that populate their
 # credentials from ENV variables and are not meant to be modified by creators.
 # Creator apps are those dynamically managed by creators in the Admin dashboard.
