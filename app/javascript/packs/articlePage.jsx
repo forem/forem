5a75b29dafc640091a8c40f775dc4b85d7a934f9
Julien Maury <6878027+jmau111@users.noreply.github.com>
2021-04-20 10:40:50 +0200

    delete useless file utility

--- app/javascript/packs/articlePage.jsx
+++ app/javascript/packs/articlePage.jsx
@@ -1,7 +1,7 @@
 import { h, render } from 'preact';
 import { Snackbar, addSnackbarItem } from '../Snackbar';
 import { addFullScreenModeControl } from '../utilities/codeFullscreenModeSwitcher';
-import { embedGists, embedGistsInComments } from '../utilities/gist';
+import { embedGists, embedGistsInComments } from './gist';
 
 const fullscreenActionElements = document.getElementsByClassName(
   'js-fullscreen-code-action',
