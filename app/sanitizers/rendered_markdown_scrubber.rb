6de7f3ca90c75827cd3ed28189621613b9051aff
Andy Z <17884966+Zhao-Andy@users.noreply.github.com>
2021-04-23 12:08:17 -0400

    Remove <i> tag; <em> is used by parser

--- app/sanitizers/rendered_markdown_scrubber.rb
+++ app/sanitizers/rendered_markdown_scrubber.rb
@@ -4,8 +4,8 @@ class RenderedMarkdownScrubber < Rails::Html::PermitScrubber
 
     self.tags = %w[
       a abbr add b blockquote br button center code col colgroup dd del dl dt em figcaption
-      h1 h2 h3 h4 h5 h6 hr i img kbd li mark ol p pre q rp rt ruby small source span strong sub sup table
-      tbody td tfoot th thead time tr u ul video
+      h1 h2 h3 h4 h5 h6 hr img kbd li mark ol p pre q rp rt ruby small source span strong sub sup table
+      tbody td tfoot th thead time tr ul video
     ]
 
     self.attributes = %w[
