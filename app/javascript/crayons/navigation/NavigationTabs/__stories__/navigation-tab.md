26d46f5cadc8da5262594f766bdde9b2cbf9d23c
Suzanne Aitchison <suzanne@forem.com>
2021-04-28 10:53:43 +0100

    update storybook example and docs

--- app/javascript/crayons/navigation/NavigationTabs/__stories__/navigation-tab.md
+++ app/javascript/crayons/navigation/NavigationTabs/__stories__/navigation-tab.md
@@ -1,3 +1,17 @@
-    ## Navigation: Tabs
+## Navigation: Tabs
 
-    Use tabs as 2nd level navigation or filtering options.
+Use tabs as 2nd level navigation or filtering options. These tabs are generally
+used in Forem as collections of **links**. As such, the `crayons-tabs` class
+should be used on a `nav` element and should wrap a list of links.
+
+### Accessibility
+
+When using Crayons Tabs for lists of links, ensure that:
+
+- The `crayons-tabs` class is used on a `nav` element
+- The `nav` element has an `aria-label` (e.g.
+  `aria-label="View posts by period"`)
+- All links inside the `nav` element are contained in list items (`<li>`) in an
+  unordered list (`<ul>`)
+- The currently active link has the attribute `aria-current="page"`, which tells
+  screen reader users which link is currently active
--- app/javascript/crayons/navigation/NavigationTabs/__stories__/navigationTab.html.stories.jsx
+++ app/javascript/crayons/navigation/NavigationTabs/__stories__/navigationTab.html.stories.jsx
@@ -8,17 +8,29 @@ export default {
 };
 
 export const Default = () => (
-  <div className="crayons-tabs">
-    <a href="/" className="crayons-tabs__item crayons-tabs__item--current">
-      Feed
-    </a>
-    <a href="/" className="crayons-tabs__item">
-      Popular
-    </a>
-    <a href="/" className="crayons-tabs__item">
-      Latest
-    </a>
-  </div>
+  <nav className="crayons-tabs">
+    <ul className="crayons-tabs__list">
+      <li>
+        <a
+          href="/"
+          className="crayons-tabs__item crayons-tabs__item--current"
+          aria-current="page"
+        >
+          Feed
+        </a>
+      </li>
+      <li>
+        <a href="/" className="crayons-tabs__item">
+          Popular
+        </a>
+      </li>
+      <li>
+        <a href="/" className="crayons-tabs__item">
+          Latest
+        </a>
+      </li>
+    </ul>
+  </nav>
 );
 
 Default.story = {
