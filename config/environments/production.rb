35c69e1dd920abb1e29c5a29a4de4065a8b86f79
Mac Siri <krairit.siri@gmail.com>
2021-04-28 15:28:38 -0400

    Update config/environments/production.rb
    Co-authored-by: rhymes <rhymes@hey.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -131,7 +131,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: protocol + ENV["APP_DOMAIN"].to_s }
   ActionMailer::Base.smtp_settings = {
     address: ENV["SMTP_ADDRESS"],
-    port: ENV["SMTP_ADDRESS"],
+    port: ENV["SMTP_PORT"],
     authentication: ENV["SMTP_AUTHENTICATION"],
     user_name: ENV["SMTP_USER_NAME"],
     password: ENV["SMTP_PASSWORD"],
