2e5d0b7b10ca38b60b0e75133464558e5298a69f
Ridhwana <ridhwana.khan16@gmail.com>
2021-04-29 14:41:00 +0200

    chore: amend the tests now that the old routes and the admin_restructure flag are removed

--- spec/requests/admin/nested_sidebar_spec.rb
+++ spec/requests/admin/nested_sidebar_spec.rb
@@ -1,30 +1,11 @@
 require "rails_helper"
 
-# @ridhwana: rename to sidebar_spec once we can delete the old spec
 RSpec.describe "admin sidebar", type: :request do
   let(:super_admin) { create(:user, :super_admin) }
 
   before do
     sign_in super_admin
     allow(FeatureFlag).to receive(:enabled?).and_call_original
-  end
-
-  describe "admin_restructure feature flag" do
-    it "shows the correct options in the sidebar when the feature flag is enabled" do
-      allow(FeatureFlag).to receive(:enabled?).with(:admin_restructure).and_return(true)
-
-      get admin_articles_path
-
-      expect(response.body).to include("Content Manager")
-    end
-
-    it "does not show the option in the sidebar when the feature flag is disabled" do
-      allow(FeatureFlag).to receive(:enabled?).with(:admin_restructure).and_return(false)
-
-      get admin_articles_path
-
-      expect(response.body).not_to include("Content Manager")
-    end
   end
 
   describe "sidebar menu options" do
@@ -39,9 +20,7 @@ RSpec.describe "admin sidebar", type: :request do
   end
 
   describe "tabbed menu options" do
-    # NOTE: [@ridhwana] to enable this test again once the
-    # the feature flag is removed
-    xit "shows nested grandchildren items where applicable" do
+    it "shows nested grandchildren items where applicable" do
       allow(FeatureFlag).to receive(:enabled?).with(:admin_restructure).and_return(true)
 
       get admin_badges_path
@@ -86,9 +65,7 @@ RSpec.describe "admin sidebar", type: :request do
       expect(response.body).not_to include("Data Update Scripts")
     end
 
-    # NOTE: [@ridhwana] to enable this test again once the
-    # the feature flag is removed
-    xit "shows the option in the tabbed header when the feature flag is enabled" do
+    it "shows the option in the tabbed header when the feature flag is enabled" do
       allow(FeatureFlag).to receive(:enabled?).with(:data_update_scripts).and_return(true)
 
       get admin_tools_path
