c88fc5d23309d54bb9370e30c61fe3f4be328296
Alex <alexandersmith223@gmail.com>
2021-04-30 09:02:35 -0400

    [Search 2.0] Remove remaining search_2 feature flags (#13591)
    * Remove remaining search_2 feature flags
    
    * Fix specs
    
    * Fix more specs
    
    * Remove leftovers
    
    Co-authored-by: rhymes <rhymes@hey.com>

--- spec/requests/search_spec.rb
+++ spec/requests/search_spec.rb
@@ -99,140 +99,62 @@ RSpec.describe "Search", type: :request, proper_status: true do
   end
 
   describe "GET /search/feed_content" do
-    context "when using Elasticsearch" do
-      let(:mock_documents) { [{ "title" => "article1" }] }
+    let(:homepage_params) { { class_name: "Article", sort_by: "published_at", sort_direction: "desc" } }
 
-      it "returns json" do
-        allow(Search::FeedContent).to receive(:search_documents).and_return(
-          mock_documents,
-        )
+    it "does not call Homepage::FetchArticles when class_name is Article with a search term", :aggregate_failures do
+      allow(Homepage::FetchArticles).to receive(:call)
 
-        get search_feed_content_path
-        expect(response.parsed_body["result"]).to eq(mock_documents)
-      end
+      get search_feed_content_path
+      expect(Homepage::FetchArticles).not_to have_received(:call)
 
-      it "queries only the user index if class_name=User" do
-        allow(Search::FeedContent).to receive(:search_documents)
-        allow(Search::User).to receive(:search_documents).and_return(
-          mock_documents,
-        )
-
-        get search_feed_content_path(class_name: "User")
-        expect(Search::User).to have_received(:search_documents)
-        expect(Search::FeedContent).not_to have_received(:search_documents)
-      end
-
-      it "queries for Articles, Podcast Episodes and Users if no class_name filter is present" do
-        allow(Search::FeedContent).to receive(:search_documents).and_return(
-          mock_documents,
-        )
-        allow(Search::User).to receive(:search_documents).and_return(
-          mock_documents,
-        )
-
-        get search_feed_content_path
-        expect(Search::User).to have_received(:search_documents)
-        expect(Search::FeedContent).to have_received(:search_documents)
-      end
-
-      it "queries for only Articles and Podcast Episodes if class_name!=User" do
-        allow(Search::FeedContent).to receive(:search_documents).and_return(
-          mock_documents,
-        )
-        allow(Search::User).to receive(:search_documents)
-
-        get search_feed_content_path(class_name: "Article")
-        expect(Search::User).not_to have_received(:search_documents)
-        expect(Search::FeedContent).to have_received(:search_documents)
-      end
-
-      it "queries for approved" do
-        allow(Search::FeedContent).to receive(:search_documents).and_return(
-          mock_documents,
-        )
-
-        get search_feed_content_path(class_name: "Article", approved: "true")
-        expect(Search::FeedContent).to have_received(:search_documents).with(
-          params: { "approved" => "true", "class_name" => "Article" },
-        )
-      end
+      get search_feed_content_path(class_name: "Article", search_fields: "keyword")
+      expect(Homepage::FetchArticles).not_to have_received(:call)
     end
 
-    context "when using PostgreSQL for the homepage" do
-      let(:homepage_params) { { class_name: "Article", sort_by: "published_at", sort_direction: "desc" } }
+    it "returns the correct keys", :aggregate_failures do
+      create(:article)
 
-      before do
-        allow(FeatureFlag).to receive(:enabled?).with(:search_2_articles, anything).and_return(true)
-        allow(FeatureFlag).to receive(:enabled?).with(:search_2_homepage, anything).and_return(true)
-      end
+      get search_feed_content_path(homepage_params)
 
-      it "does not call Homepage::FetchArticles when class_name is Article with a search term", :aggregate_failures do
-        allow(Homepage::FetchArticles).to receive(:call)
-
-        get search_feed_content_path
-        expect(Homepage::FetchArticles).not_to have_received(:call)
-
-        get search_feed_content_path(class_name: "Article", search_fields: "keyword")
-        expect(Homepage::FetchArticles).not_to have_received(:call)
-      end
-
-      it "calls Homepage::FetchArticles when class_name is Article" do
-        allow(Homepage::FetchArticles).to receive(:call)
-
-        get search_feed_content_path(homepage_params)
-
-        expect(Homepage::FetchArticles).to have_received(:call)
-      end
-
-      it "returns the correct keys", :aggregate_failures do
-        create(:article)
-
-        get search_feed_content_path(homepage_params)
-
-        expect(response.parsed_body["result"]).to be_present
-      end
-
-      it "parses published_at correctly", :aggregate_failures do
-        article = create(:article)
-
-        get search_feed_content_path(homepage_params.merge(published_at: { gte: article.published_at.iso8601 }))
-        expect(response.parsed_body["result"].first["id"]).to eq(article.id)
-
-        datetime = article.published_at + 1.minute
-        get search_feed_content_path(homepage_params.merge(published_at: { gte: datetime.iso8601 }))
-        expect(response.parsed_body["result"]).to be_empty
-      end
-
-      it "supports the user_id parameter" do
-        allow(Homepage::FetchArticles).to receive(:call)
-
-        get search_feed_content_path(homepage_params.merge(user_id: 1))
-
-        expect(Homepage::FetchArticles).to have_received(:call).with(hash_including(user_id: "1"))
-      end
-
-      it "supports the organization_id parameter" do
-        allow(Homepage::FetchArticles).to receive(:call)
-
-        get search_feed_content_path(homepage_params.merge(organization_id: 1))
-
-        expect(Homepage::FetchArticles).to have_received(:call).with(hash_including(organization_id: "1"))
-      end
-
-      it "supports the tag_names parameter" do
-        allow(Homepage::FetchArticles).to receive(:call)
-
-        get search_feed_content_path(homepage_params.merge(tag_names: %i[ruby]))
-
-        expect(Homepage::FetchArticles).to have_received(:call).with(hash_including(tags: %w[ruby]))
-      end
+      expect(response.parsed_body["result"]).to be_present
     end
 
-    context "when using PostgreSQL for articles" do
-      before do
-        allow(FeatureFlag).to receive(:enabled?).with(:search_2_articles, anything).and_return(true)
-      end
+    it "parses published_at correctly", :aggregate_failures do
+      article = create(:article)
 
+      get search_feed_content_path(homepage_params.merge(published_at: { gte: article.published_at.iso8601 }))
+      expect(response.parsed_body["result"].first["id"]).to eq(article.id)
+
+      datetime = article.published_at + 1.minute
+      get search_feed_content_path(homepage_params.merge(published_at: { gte: datetime.iso8601 }))
+      expect(response.parsed_body["result"]).to be_empty
+    end
+
+    it "supports the user_id parameter" do
+      allow(Homepage::FetchArticles).to receive(:call)
+
+      get search_feed_content_path(homepage_params.merge(user_id: 1))
+
+      expect(Homepage::FetchArticles).to have_received(:call).with(hash_including(user_id: "1"))
+    end
+
+    it "supports the organization_id parameter" do
+      allow(Homepage::FetchArticles).to receive(:call)
+
+      get search_feed_content_path(homepage_params.merge(organization_id: 1))
+
+      expect(Homepage::FetchArticles).to have_received(:call).with(hash_including(organization_id: "1"))
+    end
+
+    it "supports the tag_names parameter" do
+      allow(Homepage::FetchArticles).to receive(:call)
+
+      get search_feed_content_path(homepage_params.merge(tag_names: %i[ruby]))
+
+      expect(Homepage::FetchArticles).to have_received(:call).with(hash_including(tags: %w[ruby]))
+    end
+
+    context "when searching for articles" do
       it "calls Search::Postgres::Article without a class_name" do
         allow(Search::Postgres::Article).to receive(:search_documents)
 
@@ -241,17 +163,7 @@ RSpec.describe "Search", type: :request, proper_status: true do
         expect(Search::Postgres::Article).to have_received(:search_documents)
       end
 
-      it "calls Search::Postgres::Article without a class_name with :search_2_homepage active" do
-        allow(FeatureFlag).to receive(:enabled?).with(:search_2_homepage, anything).and_return(true)
-        allow(Search::Postgres::Article).to receive(:search_documents)
-
-        get search_feed_content_path
-
-        expect(Search::Postgres::Article).to have_received(:search_documents)
-      end
-
-      it "calls Search::Postgres::Article with class_name=Article with :search_2_homepage active" do
-        allow(FeatureFlag).to receive(:enabled?).with(:search_2_homepage, anything).and_return(true)
+      it "calls Search::Postgres::Article with class_name=Article" do
         allow(Search::Postgres::Article).to receive(:search_documents)
 
         get search_feed_content_path(class_name: "Article")
@@ -275,11 +187,7 @@ RSpec.describe "Search", type: :request, proper_status: true do
       end
     end
 
-    context "when using PostgreSQL for comments" do
-      before do
-        allow(FeatureFlag).to receive(:enabled?).with(:search_2_comments).and_return(true)
-      end
-
+    context "when searching for comments" do
       it "returns the correct keys for comments" do
         create(:comment, body_markdown: "Ruby on Rails rocks!")
         get search_feed_content_path(search_fields: "rails", class_name: "Comment")
@@ -299,11 +207,7 @@ RSpec.describe "Search", type: :request, proper_status: true do
       end
     end
 
-    context "when using PostgreSQL for users" do
-      before do
-        allow(FeatureFlag).to receive(:enabled?).with(:search_2_users).and_return(true)
-      end
-
+    context "when using searching for users" do
       it "returns the correct keys", :aggregate_failures do
         create(:user)
 
@@ -324,11 +228,7 @@ RSpec.describe "Search", type: :request, proper_status: true do
       end
     end
 
-    context "when using PostgreSQL for podcasts" do
-      before do
-        allow(FeatureFlag).to receive(:enabled?).with(:search_2_podcast_episodes).and_return(true)
-      end
-
+    context "when using searching for podcasts" do
       it "returns the correct keys for podcasts" do
         create(:podcast_episode, body: "DHH talks about how Ruby on Rails rocks!")
         get search_feed_content_path(search_fields: "rails", class_name: "PodcastEpisode")
@@ -350,57 +250,26 @@ RSpec.describe "Search", type: :request, proper_status: true do
   end
 
   describe "GET /search/reactions" do
+    let(:article) { create(:article) }
+
     before do
       sign_in authorized_user
+      create(:reaction, category: :readinglist, reactable: article, user: authorized_user)
     end
 
-    context "when using Elasticsearch" do
-      let(:mock_response) do
-        { "reactions" => [{ id: 123 }], "total" => 100 }
-      end
+    it "returns the correct keys", :aggregate_failures do
+      get search_reactions_path
 
-      before do
-        allow(Search::ReadingList).to receive(:search_documents).and_return(mock_response)
-      end
-
-      it "returns json with reactions and total" do
-        get search_reactions_path
-
-        expect(response.parsed_body).to eq("result" => [{ "id" => 123 }], "total" => 100)
-      end
-
-      it "accepts array of tag names" do
-        get search_reactions_path(tag_names: [1, 2])
-
-        expect(Search::ReadingList).to(
-          have_received(:search_documents)
-            .with(params: { "tag_names" => %w[1 2] }, user: authorized_user),
-        )
-      end
+      expect(response.parsed_body["result"]).to be_present
+      expect(response.parsed_body["total"]).to eq(1)
     end
 
-    context "when using PostgreSQL" do
-      let(:article) { create(:article) }
+    it "supports the search params" do
+      article.update_columns(title: "Title", cached_tag_list: "ruby, python")
 
-      before do
-        allow(FeatureFlag).to receive(:enabled?).with(:search_2_reading_list).and_return(true)
-        create(:reaction, category: :readinglist, reactable: article, user: authorized_user)
-      end
+      get search_reactions_path(page: 0, per_page: 1, status: %w[valid], tags: %w[ruby], term: "title")
 
-      it "returns the correct keys", :aggregate_failures do
-        get search_reactions_path
-
-        expect(response.parsed_body["result"]).to be_present
-        expect(response.parsed_body["total"]).to eq(1)
-      end
-
-      it "supports the search params" do
-        article.update_columns(title: "Title", cached_tag_list: "ruby, python")
-
-        get search_reactions_path(page: 0, per_page: 1, status: %w[valid], tags: %w[ruby], term: "title")
-
-        expect(response.parsed_body["result"].first["reactable"]).to include("title" => "Title")
-      end
+      expect(response.parsed_body["result"].first["reactable"]).to include("title" => "Title")
     end
   end
 end
