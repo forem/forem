b1b3755ef5971a08cf3deeb2d821ae3b16495d06
Alex <alexandersmith223@gmail.com>
2021-05-03 16:42:32 -0400

    Cleanup unused DataUpdateScripts

--- lib/data_update_scripts/20210118194138_resync_unpublished_articles_comments_elasticsearch_document.rb
+++
@@ -1,9 +0,0 @@
-module DataUpdateScripts
-  class ResyncUnpublishedArticlesCommentsElasticsearchDocument
-    def run
-      # Article.unpublished.where.not(comments_count: 0).each do |article|
-      #   article.comments.each(&:index_to_elasticsearch)
-      # end
-    end
-  end
-end
