25e9da40db8fcd21c3130eaeb06f4bf4324ad745
Ridhwana <Ridhwana.Khan16@gmail.com>
2021-05-03 16:03:38 +0200

    RFC 50 - Remove old admin routes and related code (#13579)
    * chore: remove current_admin routes file
    
    * feat: move all the admin routes into the admin file
    
    * chore: remove conditionals for the sidebar and tabbed navbar
    
    * remove some more notes where we use the admin_restructure feature flag
    
    * chore: amend the tests now that the old routes and the admin_restructure flag are removed
    
    * chore: remove more references to the  admin_restructure flag
    
    * chore: fix the tests
    
    * chore: remove sidebar_spec
    
    * chore: add slash back
    
    * chore: comment it out for now whilst I try and figure out why its not passing

--- cypress/integration/adminFlows/config/emailDigestFrequencySection.js
+++ cypress/integration/adminFlows/config/emailDigestFrequencySection.js
@@ -11,7 +11,7 @@ describe('Email digest frequency Section', () => {
   describe('email digest frequency settings', () => {
     it('can change the frequency', () => {
       cy.get('@user').then(({ username }) => {
-        cy.visit('/admin/config');
+        cy.visit('/admin/customization/config');
         cy.findByTestId('emailDigestSectionForm').as('emailDigestSectionForm');
 
         cy.get('@emailDigestSectionForm')
@@ -33,7 +33,7 @@ describe('Email digest frequency Section', () => {
           .findByText('Update Site Configuration')
           .click();
 
-        cy.url().should('contains', '/admin/config');
+        cy.url().should('contains', '/admin/customization/config');
 
         cy.findByText('Site configuration was successfully updated.').should(
           'be.visible',
