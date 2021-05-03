0bbd81e393549705a63c0c5a10f21d1fbe329bbb
Rafi <Rafi993@users.noreply.github.com>
2021-05-03 19:22:29 +0530

    Update app/javascript/onboarding/components/FollowTags.jsx
    Co-authored-by: Suzanne Aitchison <suzanne@forem.com>

--- app/javascript/onboarding/components/FollowTags.jsx
+++ app/javascript/onboarding/components/FollowTags.jsx
@@ -163,14 +163,14 @@ export class FollowTags extends Component {
                             : tag.text_color_hex,
                         }}
                       >
-                        <svg
+                        { selected && (<svg
                           width="24"
                           height="24"
                           xmlns="http://www.w3.org/2000/svg"
                           className="crayons-icon"
                         >
                           <path d="M9.99999 15.172L19.192 5.979L20.607 7.393L9.99999 18L3.63599 11.636L5.04999 10.222L9.99999 15.172Z" />
-                        </svg>
+                        </svg>)}
                         {selected ? 'Following' : 'Follow'}
                       </button>
                     </div>
