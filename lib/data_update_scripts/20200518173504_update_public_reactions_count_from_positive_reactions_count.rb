519101c5251ae02839f91d6a74de8a8ef029352e
Alex <alexandersmith223@gmail.com>
2021-05-04 09:19:24 -0400

    Cleanup unused DataUpdateScripts (#13640)

--- lib/data_update_scripts/20200518173504_update_public_reactions_count_from_positive_reactions_count.rb
+++
@@ -1,9 +0,0 @@
-module DataUpdateScripts
-  class UpdatePublicReactionsCountFromPositiveReactionsCount
-    def run
-      # ActiveRecord::Base.connection.execute("UPDATE articles SET public_reactions_count = positive_reactions_count,
-      # previous_public_reactions_count = previous_positive_reactions_count")
-      # ActiveRecord::Base.connection.execute("UPDATE comments SET public_reactions_count = positive_reactions_count")
-    end
-  end
-end
