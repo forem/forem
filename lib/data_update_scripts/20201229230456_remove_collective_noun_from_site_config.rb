ecf373502e9dbae06ca1d50586a9ff57c67324fe
Michael Kohl <citizen428@forem.com>
2021-05-06 12:48:20 +0700

    Fix DUS

--- lib/data_update_scripts/20201229230456_remove_collective_noun_from_site_config.rb
+++ lib/data_update_scripts/20201229230456_remove_collective_noun_from_site_config.rb
@@ -1,5 +1,5 @@
 module DataUpdateScripts
-  module RemoveCollectiveNounFromSiteConfig
+  class RemoveCollectiveNounFromSiteConfig
     def run
       SiteConfig.where(var: %w[collective_noun collective_noun_disabled]).destroy_all
     end
