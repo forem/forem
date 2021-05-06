d22e315136336b17e331b01245b3e30575cbfc4e
Michael Kohl <citizen428@forem.com>
2021-05-06 12:09:40 +0700

    undo rename of DUS

--- lib/data_update_scripts/20201229230456_remove_collective_noun_from_site_config.rb
+++ lib/data_update_scripts/20201229230456_remove_collective_noun_from_site_config.rb
@@ -1,9 +1,7 @@
 module DataUpdateScripts
-  module RemoveCollectiveNounFromSettings
-    class General
-      def run
-        Settings::General.where(var: %w[collective_noun collective_noun_disabled]).destroy_all
-      end
+  module RemoveCollectiveNounFromSiteConfig
+    def run
+      SiteConfig.where(var: %w[collective_noun collective_noun_disabled]).destroy_all
     end
   end
 end
