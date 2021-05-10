daa03d557ad8253e41303d29d5192e434ef7e106
Michael Kohl <citizen428@forem.com>
2021-05-10 08:35:32 +0700

    Fix DUS

--- lib/data_update_scripts/20210504060704_move_mascot_settings_backto_site_config.rb
+++ lib/data_update_scripts/20210504060704_move_mascot_settings_backto_site_config.rb
@@ -7,10 +7,10 @@ module DataUpdateScripts
       DataUpdateScript.delete_by(file_name: "20210420050256_move_mascot_settings")
 
       # Migrate back explicitly set values
-      mascot_image_url = SiteConfig.mascot_image_url
+      mascot_image_url = Settings::Mascot.image_url
       SiteConfig.mascot_image_url = mascot_image_url if mascot_image_url
 
-      mascot_user_id = SiteConfig.mascot_user_id
+      mascot_user_id = Settings::Mascot.mascot_user_id
       SiteConfig.mascot_user_id = mascot_user_id if mascot_user_id
     end
   end
