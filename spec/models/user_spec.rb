c5fcb699094fbe4176b552f301504a5feb539574
Michael Kohl <citizen428@dev.to>
2021-05-11 18:55:18 +0700

    Remove mascot footer image  (#13642)
    * Remove footer mascot image
    
    * Move remaining mascot settings back to SiteConfig
    
    * Add data update script
    
    * Update usages
    
    * Fix path
    
    * Remove dead route
    
    * Update tests
    
    * Fix view and spec
    
    * Fix e2e test
    
    * Fix DUS

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -815,7 +815,7 @@ RSpec.describe User, type: :model do
     end
 
     it "returns the user if the account exists" do
-      allow(Settings::Mascot).to receive(:mascot_user_id).and_return(user.id)
+      allow(SiteConfig).to receive(:mascot_user_id).and_return(user.id)
 
       expect(described_class.mascot_account).to eq(user)
     end
