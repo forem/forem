fb7af8e68126349fa8586cc87806cae029bdd0cb
Arit Amana <msarit@gmail.com>
2021-05-11 12:10:58 -0400

    add missing attribute "email_membership_newsletter"

--- lib/data_update_scripts/20210503174302_migrate_relevant_fields_from_users_to_users_notification_settings.rb
+++ lib/data_update_scripts/20210503174302_migrate_relevant_fields_from_users_to_users_notification_settings.rb
@@ -14,6 +14,7 @@ module DataUpdateScripts
                 COALESCE(email_connect_messages, true),
                 COALESCE(email_digest_periodic, false),
                 COALESCE(email_follower_notifications, true),
+                COALESCE(email_membership_newsletter, false),
                 COALESCE(email_mention_notifications, true),
                 COALESCE(email_newsletter, false),
                 COALESCE(email_tag_mod_newsletter, false),
@@ -26,7 +27,7 @@ module DataUpdateScripts
                 NOW()
               FROM users
             )
-            INSERT INTO users_notification_settings (user_id, email_badge_notifications, email_comment_notifications, email_community_mod_newsletter, email_connect_messages, email_digest_periodic, email_follower_notifications, email_mention_notifications, email_newsletter, email_tag_mod_newsletter, email_unread_notifications, mobile_comment_notifications, mod_roundrobin_notifications, reaction_notifications, welcome_notifications, created_at, updated_at)
+            INSERT INTO users_notification_settings (user_id, email_badge_notifications, email_comment_notifications, email_community_mod_newsletter, email_connect_messages, email_digest_periodic, email_follower_notifications, email_membership_newsletter, email_mention_notifications, email_newsletter, email_tag_mod_newsletter, email_unread_notifications, mobile_comment_notifications, mod_roundrobin_notifications, reaction_notifications, welcome_notifications, created_at, updated_at)
               SELECT * FROM notification_settings_data
               ON CONFLICT (user_id) DO UPDATE
                 SET email_badge_notifications = EXCLUDED.email_badge_notifications,
@@ -35,6 +36,7 @@ module DataUpdateScripts
                     email_connect_messages = EXCLUDED.email_connect_messages,
                     email_digest_periodic = EXCLUDED.email_digest_periodic,
                     email_follower_notifications = EXCLUDED.email_follower_notifications,
+                    email_membership_newsletter = EXCLUDED.email_membership_newsletter,
                     email_mention_notifications = EXCLUDED.email_mention_notifications,
                     email_newsletter = EXCLUDED.email_newsletter,
                     email_tag_mod_newsletter = EXCLUDED.email_tag_mod_newsletter,
