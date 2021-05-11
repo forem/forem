5e7444c302bcf07b5669dbaa833722ef80f27d2c
Fernando Valverde <fernando@visualcosita.com>
2021-05-11 08:55:39 -0600

    Expose aasa non-private forems + small tweak to aasa

--- app/controllers/deep_links_controller.rb
+++ app/controllers/deep_links_controller.rb
@@ -9,7 +9,7 @@ class DeepLinksController < ApplicationController
     supported_apps << "R9SWHSQNV8.to.dev.ios" if SiteConfig.dev_to?
     render json: {
       applinks: {
-        apps: supported_apps,
+        apps: [],
         details: supported_apps.map { |app_id| { appID: app_id, paths: ["/*"] } }
       },
       activitycontinuation: {
