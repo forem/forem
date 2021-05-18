126a42b8d1d33a805d25af413699d9494bb60fd3
rhymes <github@rhymes.dev>
2021-05-18 16:42:09 +0200

    Fix autoloading for middlewares

---
+++ config/initializers/set_cookie_domain.rb
@@ -0,0 +1,17 @@
+if Rails.env.production?
+  # Since we must explicitly set the cookie domain in session_store before SiteConfig is available,
+  # this ensures we properly set the cookie to SiteConfig.app_domain at runtime.
+  class SetCookieDomain
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      env["rack.session.options"][:domain] = ".#{SiteConfig.app_domain}"
+
+      @app.call(env)
+    end
+  end
+
+  Rails.configuration.middleware.use SetCookieDomain
+end
