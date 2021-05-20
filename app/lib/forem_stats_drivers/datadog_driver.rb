81c732a3e899c6f05ee901fe7084b8e4cd280f32
rhymes <github@rhymes.dev>
2021-05-20 18:10:25 +0200

    [15 min fix] Fix some autoloading deprecation warnings (#13793)
    * Fix autoloading for RateLimitChecker
    
    * Fix autoloading for ForemStatsClient
    
    * Fix autoloading for middlewares
    
    * Move things to their proper place
    
    * Move middleware usage to the proper place
    
    * Re-add rest-client in datadog driver correctly
    
    * Already required in config/initializers/middlewares.rb
    
    * Bring that back
    
    * Fix Honeybadger spec

--- app/lib/forem_stats_drivers/datadog_driver.rb
+++ app/lib/forem_stats_drivers/datadog_driver.rb
@@ -1,28 +1,32 @@
 require "httpclient"
+require "rest-client"
 
 module ForemStatsDrivers
   class DatadogDriver
     include ActsAsForemStatsDriver
+
     setup_driver do
       Datadog.configure do |c|
         c.tracer env: Rails.env
         c.tracer enabled: ENV["DD_API_KEY"].present?
         c.tracer partial_flush: true
         c.tracer priority_sampling: true
-        c.use :sidekiq
-        c.use :redis, service_name: "redis", describes: { url: ENV["REDIS_URL"] }
-        c.use :redis, service_name: "redis-sessions", describes: { url: ENV["REDIS_SESSIONS_URL"] }
-        c.use :redis, service_name: "redis-sidekiq", describes: { url: ENV["REDIS_SIDEKIQ_URL"] }
-        c.use :redis, service_name: "redis-rpush", describes: { url: ENV["REDIS_RPUSH_URL"] }
-        c.use :rails
-        c.use :http, split_by_domain: false
-        c.use :faraday, split_by_domain: true
+
+        c.use :concurrent_ruby
         c.use :excon, split_by_domain: true
+        c.use :faraday, split_by_domain: true
+        c.use :http, split_by_domain: false
         c.use :httpclient, split_by_domain: false
         c.use :httprb, split_by_domain: true
+        c.use :rails
+        c.use :redis, service_name: "redis", describes: { url: ENV["REDIS_URL"] }
+        c.use :redis, service_name: "redis-rpush", describes: { url: ENV["REDIS_RPUSH_URL"] }
+        c.use :redis, service_name: "redis-sessions", describes: { url: ENV["REDIS_SESSIONS_URL"] }
+        c.use :redis, service_name: "redis-sidekiq", describes: { url: ENV["REDIS_SIDEKIQ_URL"] }
         c.use :rest_client
-        c.use :concurrent_ruby
+        c.use :sidekiq
       end
+
       Datadog::Statsd.new
     end
   end
