60049cbd1384947d6d72ee82ef594f7b7f1ecb91
rhymes <github@rhymes.dev>
2021-05-28 17:14:19 +0200

    Fix parsing of relative paths and add tests

--- app/services/articles/detect_animated_images.rb
+++ app/services/articles/detect_animated_images.rb
@@ -3,6 +3,7 @@ module Articles
     IMAGES_IN_LIQUID_TAGS_SELECTORS = [
       ".liquid-comment img", # CommentTag
       ".ltag-github-readme-tag img", # GithubReadmeTag
+      ".ltag__link__pic img", # LinkTag and MediumTag profile pic
       ".ltag__link__servicename img", # MediumTag
       ".ltag__link__taglist img", # LinkTag
       ".ltag__reddit--container img", # RedditTag
@@ -24,7 +25,7 @@ module Articles
       images.each do |img|
         src = img.attr("src")
         next unless src
-        next unless FastImage.animated?(src)
+        next unless FastImage.animated?(parsed_src(src))
 
         img["data-animated"] = true
         found = true
@@ -32,5 +33,13 @@ module Articles
 
       article.update_columns(processed_html: parsed_html.to_html) if found
     end
+
+    def self.parsed_src(src)
+      uri = URI.parse(src)
+      return src unless uri.relative?
+
+      "#{URL.url}#{src}"
+    end
+    private_class_method :parsed_src
   end
 end
