13c879015fcb441f62d05b6f4f472417301012dd
rhymes <github@rhymes.dev>
2021-05-29 11:00:42 +0200

    Change Articles::DetectAnimatedImagesWorker priority to medium

--- app/workers/articles/detect_animated_images_worker.rb
+++ app/workers/articles/detect_animated_images_worker.rb
@@ -2,7 +2,7 @@ module Articles
   class DetectAnimatedImagesWorker
     include Sidekiq::Worker
 
-    sidekiq_options queue: :high_priority, retry: 5, lock: :until_executing
+    sidekiq_options queue: :medium_priority, retry: 5, lock: :until_executing
 
     def perform(article_id)
       article = Article.find_by(id: article_id)
