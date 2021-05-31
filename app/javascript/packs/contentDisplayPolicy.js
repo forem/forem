38951109f0475587888eab4bf596e78d9f7c617c
Julien Maury <6878027+jmau111@users.noreply.github.com>
2021-05-31 17:45:25 +0200

    use custom pack notification page

--- app/javascript/packs/contentDisplayPolicy.js
+++ app/javascript/packs/contentDisplayPolicy.js
@@ -1,18 +1,10 @@
 import { hideBlockedContent } from '../contentDisplayPolicy/hideBlockedContent';
 import { initHiddenComments } from '../contentDisplayPolicy/initHiddenComments';
-import { embedGists } from '../utilities/gist';
-
-function handleEmbedGists() {
-  const targetNode = document.querySelector('#articles-list');
-  targetNode && embedGists(targetNode);
-}
 
 window.InstantClick.on('change', () => {
   hideBlockedContent();
   initHiddenComments();
-  handleEmbedGists();
 });
 
 hideBlockedContent();
 initHiddenComments();
-handleEmbedGists();
