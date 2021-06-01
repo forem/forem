3aa1abe3f6c4793e596997687d225079cc1b7e5b
Suzanne Aitchison <suzanne@forem.com>
2021-06-01 10:50:19 +0100

    add new window link icon

--- app/javascript/article-form/components/AccessibilitySuggestions.jsx
+++ app/javascript/article-form/components/AccessibilitySuggestions.jsx
@@ -4,6 +4,19 @@ import PropTypes from 'prop-types';
 // Limit the number of suggestions shown so that the UI isn't overwhelmed
 const MAX_SUGGESTIONS = 3;
 
+const ExternalUrlSVG = () => (
+  <svg
+    aria-hidden="true"
+    width="15"
+    height="15"
+    fill="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M10 6v2H5v11h11v-5h2v6a1 1 0 01-1 1H4a1 1 0 01-1-1V7a1 1 0 011-1h6zm11-3v8h-2V6.413l-7.793 7.794-1.414-1.414L17.585 5H13V3h8z" />
+  </svg>
+);
+
 const extractRelevantErrors = (lintErrors) => {
   const imageErrors = [];
   const otherErrors = [];
@@ -74,7 +87,7 @@ export const AccessibilitySuggestions = ({ markdownLintErrors }) => {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Learn more
+                  Learn more <ExternalUrlSVG />
                 </a>
               </span>
             </li>
