06159bd90f5008415204371535b263a089bd6c7f
rhymes <github@rhymes.dev>
2021-06-02 14:40:55 +0200

    Remove duplicate validator

--- app/validators/existing_article_id_validator.rb
+++
@@ -1,9 +0,0 @@
-class ExistingArticleIdValidator < ActiveModel::EachValidator
-  DEFAULT_MESSAGE = "must be a valid Article identifier".freeze
-
-  def validate_each(record, attribute, value)
-    return if Article.exists?(id: value)
-
-    record.errors.add(attribute, (options[:message] || DEFAULT_MESSAGE))
-  end
-end
