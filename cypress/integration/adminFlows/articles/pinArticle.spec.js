30213d3ea1f1e88d29f4baa299d28e4a1a99198d
rhymes <github@rhymes.dev>
2021-06-02 13:25:08 +0200

    Strengthen pinArticle Cypress tests

--- cypress/integration/adminFlows/articles/pinArticle.spec.js
+++ cypress/integration/adminFlows/articles/pinArticle.spec.js
@@ -41,35 +41,57 @@ describe('Pin an article from the admin area', () => {
       .should('be.checked');
   });
 
-  it('should change the pinned article', () => {
+  it('should change the pinned article when choosing to pin a new article', () => {
     cy.findAllByRole('checkbox', { name: 'Pinned' }).first().check();
     cy.findAllByRole('button', { name: 'Submit' }).first().click();
 
-    cy.visit('/admin/content_manager/articles');
+    cy.createArticle({
+      title: 'A new article',
+      tags: ['beginner', 'ruby', 'go'],
+      content: `This is another test article's contents.`,
+      published: true,
+    }).then((response) => {
+      cy.visit(`/admin/content_manager/articles/${response.body.id}`);
+    });
 
-    cy.findAllByRole('checkbox', { name: 'Pinned' }).last().check();
-    cy.findAllByRole('button', { name: 'Pin new article' }).last().click();
-    cy.findAllByRole('button', { name: 'Submit' }).last().click();
-
-    cy.findAllByRole('checkbox', { name: 'Pinned' })
+    cy.findByRole('main')
       .first()
-      .as('pinnedCheckbox');
-    cy.get('@pinnedCheckbox').should('be.checked');
+      .within(() => {
+        cy.findAllByRole('checkbox', { name: 'Pinned' }).last().check();
+        cy.findAllByRole('button', { name: 'Pin new article' }).last().click();
+        cy.findAllByRole('button', { name: 'Submit' }).last().click();
+      });
+
+    cy.findByRole('main')
+      .findAllByRole('checkbox', { name: 'Pinned' })
+      .first()
+      .should('be.checked');
   });
 
-  it('should not change the pinned article', () => {
+  it('should not change the pinned article when choosing to dismiss', () => {
     cy.findAllByRole('checkbox', { name: 'Pinned' }).first().check();
     cy.findAllByRole('button', { name: 'Submit' }).first().click();
 
-    cy.visit('/admin/content_manager/articles');
+    cy.createArticle({
+      title: 'A new article',
+      tags: ['beginner', 'ruby', 'go'],
+      content: `This is another test article's contents.`,
+      published: true,
+    }).then((response) => {
+      cy.visit(`/admin/content_manager/articles/${response.body.id}`);
+    });
 
-    cy.findAllByRole('checkbox', { name: 'Pinned' }).last().check();
-    cy.findAllByRole('button', { name: 'Dismiss' }).last().click();
-    cy.findAllByRole('button', { name: 'Submit' }).last().click();
-
-    cy.findAllByRole('checkbox', { name: 'Pinned' })
+    cy.findByRole('main')
       .first()
-      .as('pinnedCheckbox');
-    cy.get('@pinnedCheckbox').should('not.be.checked');
+      .within(() => {
+        cy.findAllByRole('checkbox', { name: 'Pinned' }).first().check();
+        cy.findAllByRole('button', { name: 'Dismiss' }).first().click();
+        cy.findAllByRole('button', { name: 'Submit' }).first().click();
+      });
+
+    cy.findByRole('main')
+      .findAllByRole('checkbox', { name: 'Pinned' })
+      .first()
+      .should('not.be.checked');
   });
 });
