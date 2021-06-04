e4a29f4634ac01bf3da240eb9f412dd18dc1547e
Alex <alexandersmith223@gmail.com>
2021-06-03 21:28:57 -0400

    Update before_validation call

--- app/models/discussion_lock.rb
+++ app/models/discussion_lock.rb
@@ -2,7 +2,7 @@ class DiscussionLock < ApplicationRecord
   belongs_to :article
   belongs_to :locking_user, class_name: "User"
 
-  before_validation :nullify_notes_and_reason
+  before_validation :nullify_blank_notes_and_reason
 
   validates :article_id, presence: true, uniqueness: true
   validates :locking_user_id, presence: true
