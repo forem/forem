ab5f5dbd50aa6f058e5d484a2e40df84a3cd4666
lightalloy <lightallloy@gmail.com>
2021-06-08 11:45:54 +0300

    Deleted unused data update scripts

--- lib/data_update_scripts/20210322092753_fill_badges_credits_awarded.rb
+++
@@ -1,9 +0,0 @@
-module DataUpdateScripts
-  class FillBadgesCreditsAwarded
-    def run
-      return unless ForemInstance.dev_to?
-
-      Badge.update_all("credits_awarded = 5")
-    end
-  end
-end
