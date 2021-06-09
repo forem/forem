2761a1ff4452ac5380a1cabefa0da539963c7bb4
Mac Siri <krairit.siri@gmail.com>
2021-06-09 13:36:44 -0400

    Move constants

--- app/lib/constants/settings/general.rb
+++ app/lib/constants/settings/general.rb
@@ -101,33 +101,6 @@ module Constants
           description: "Determines which tags are shown on the homepage righthand sidebar",
           placeholder: "List of valid, comma-separated tags e.g. help,discuss,explainlikeimfive,meta"
         },
-        smtp_settings: {
-          address: {
-            description: "Address of the remote mail server",
-            placeholder: "ie. smtp.gmail.com"
-          },
-          port: {
-            description: "The port that your mail server runs on",
-            placeholder: "25"
-          },
-          authentication: {
-            description: " If your mail server requires authentication, " \
-            "you need to specify the authentication type here",
-            placeholder: "ie. plain, login, or cram_md5"
-          },
-          user_name: {
-            description: "If your mail server requires authentication, copy the username from your server",
-            placeholder: ""
-          },
-          password: {
-            description: "If your mail server requires authentication, copy the password from your server",
-            placeholder: ""
-          },
-          domain: {
-            description: "If you need to specify a HELO domain, you can do it here",
-            placeholder: ""
-          }
-        },
         sponsor_headline: {
           description: "Determines the heading text of the main sponsors sidebar above the list of sponsors.",
           placeholder: "Community Sponsors"
