bcb979faba58a47070f7d1db429ee68c5337b4f0
lightalloy <lightallloy@gmail.com>
2021-06-09 16:02:00 +0300

    Removed Settings::General.email_addresses remainders

---
+++ lib/data_update_scripts/20210604104735_remove_email_addresses_scripts.rb
@@ -0,0 +1,7 @@
+module DataUpdateScripts
+  class RemoveEmailAddressesScripts
+    def run
+      DataUpdateScript.delete_by(file_name: "20210509105151_remove_unused_site_config_emails")
+    end
+  end
+end
