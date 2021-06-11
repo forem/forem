36be17ecdb0885a7491ffbf246a1936ed35f718e
Suzanne Aitchison <suzanne@forem.com>
2021-06-11 15:25:28 +0100

    utilise newer dropdown helpers

--- app/javascript/chat/message.jsx
+++ app/javascript/chat/message.jsx
@@ -1,11 +1,12 @@
 import { h } from 'preact';
-import { useState, useRef, useEffect, useCallback } from 'preact/hooks';
+import { useState, useRef, useLayoutEffect } from 'preact/hooks';
 import PropTypes from 'prop-types';
 // eslint-disable-next-line import/no-unresolved
 import ThreeDotsIcon from 'images/overflow-horizontal.svg';
 import { adjustTimestamp } from './util';
 import { ErrorMessage } from './messages/errorMessage';
 import { Button } from '@crayons';
+import { initializeDropdown } from '@utilities/dropdownUtils';
 
 export const Message = ({
   currentUserId,
@@ -23,48 +24,21 @@ export const Message = ({
   onReportMessageTrigger,
   onEditMessageTrigger,
 }) => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const messageWrapperRef = useRef(null);
   const spanStyle = { color };
 
-  const isCurrentUserMessage = userID === currentUserId;
-  const dropdownTriggerId = `dropdown-trigger=${id}`;
+  const triggerElementId = `message-dropdown-trigger-${id}`;
+  const dropdownContentId = `message-dropdown-${id}`;
 
-  const closeDropdownAndFocusTrigger = useCallback(() => {
-    setDropdownOpen(false);
-    document.getElementById(dropdownTriggerId)?.focus();
-  }, [dropdownTriggerId]);
-
-  useEffect(() => {
-    const handleKeyUp = ({ key }) => {
-      if (key === 'Escape') {
-        // Close the menu and return focus to the button which opened it
-        closeDropdownAndFocusTrigger();
-      } else if (key === 'Tab') {
-        if (!messageWrapperRef.current.contains(document.activeElement)) {
-          // Close the menu without stealing focus as the user has tabbed away from the menu options
-          setDropdownOpen(false);
-        }
-      }
-    };
-
-    if (dropdownOpen) {
-      const firstInteractiveElementId = isCurrentUserMessage
-        ? `edit-button-${id}`
-        : `report-button-${id}`;
-      setDropdownOpen(true);
-      document.getElementById(firstInteractiveElementId)?.focus();
-      document.addEventListener('keyup', handleKeyUp);
-    } else {
-      document.removeEventListener('keyup', handleKeyUp);
-    }
-  }, [
-    dropdownOpen,
-    id,
-    isCurrentUserMessage,
-    dropdownTriggerId,
-    closeDropdownAndFocusTrigger,
-  ]);
+  useLayoutEffect(() => {
+    initializeDropdown({
+      triggerElementId,
+      dropdownContentId,
+      onOpen: () => setIsDropdownOpen(true),
+      onClose: () => setIsDropdownOpen(false),
+    });
+  }, [triggerElementId, dropdownContentId]);
 
   if (type === 'error') {
     return <ErrorMessage message={message} />;
@@ -87,26 +61,17 @@ export const Message = ({
   const dropdown = (
     <div className="message__actions">
       <Button
-        id={dropdownTriggerId}
+        id={triggerElementId}
         className={`ellipsis__menubutton ${
-          dropdownOpen ? 'opacity-1' : 'opacity-0'
+          isDropdownOpen ? 'opacity-1' : 'opacity-0'
         }`}
         size="s"
         variant="ghost"
-        onClick={() => {
-          dropdownOpen ? closeDropdownAndFocusTrigger() : setDropdownOpen(true);
-        }}
-        aria-controls={`message-options-dropdown-${id}`}
-        aria-haspopup="true"
-        aria-expanded={dropdownOpen}
       >
         <img src={ThreeDotsIcon} alt="Message options menu" />
       </Button>
 
-      <div
-        id={`message-options-dropdown-${id}`}
-        className={`messagebody__dropdownmenu ${dropdownOpen ? '' : 'hidden'}`}
-      >
+      <div id={dropdownContentId} className="messagebody__dropdownmenu hidden">
         <ul class="list-none">
           <li>
             <Button
@@ -132,30 +97,21 @@ export const Message = ({
   const dropdownReport = (
     <div className="message__actions">
       <Button
-        id={dropdownTriggerId}
+        id={triggerElementId}
         className={`ellipsis__menubutton ${
-          dropdownOpen ? 'opacity-1' : 'opacity-0'
+          isDropdownOpen ? 'opacity-1' : 'opacity-0'
         }`}
         size="s"
         variant="ghost"
-        onClick={() => {
-          dropdownOpen ? closeDropdownAndFocusTrigger() : setDropdownOpen(true);
-        }}
-        aria-controls={`report-options-dropdown-${id}`}
-        aria-haspopup="true"
-        aria-expanded={dropdownOpen}
       >
         <img src={ThreeDotsIcon} alt="Report message options" />
       </Button>
 
       <div
-        id={`report-options-dropdown-${id}`}
-        className={`messagebody__dropdownmenu report__abuse__button ${
-          dropdownOpen ? '' : 'hidden'
-        }`}
+        id={dropdownContentId}
+        className={`messagebody__dropdownmenu report__abuse__button hidden`}
       >
         <Button
-          id={`report-button-${id}`}
           variant="ghost-danger"
           onClick={(_) => onReportMessageTrigger(id)}
         >
