8e2130043b8ea492accc79cf674c71c05ce06940
Julien Maury <6878027+jmau111@users.noreply.github.com>
2021-06-12 13:17:19 +0200

    missing occurrences should have attr aria current

--- cypress/integration/tagsFlows/tagIndexNavigation.spec.js
+++ cypress/integration/tagsFlows/tagIndexNavigation.spec.js
@@ -30,7 +30,6 @@ describe('Tag index page navigation', () => {
   it('should navigate to Week view', () => {
     cy.findByRole('navigation', { name: 'View tagged posts by' }).within(() => {
       cy.findByRole('link', { name: 'Week' }).as('week');
-      cy.get('@week').should('have.attr', 'aria-current', '');
       cy.get('@week').click();
     });
 
@@ -48,7 +47,6 @@ describe('Tag index page navigation', () => {
   it('should navigate to Month view', () => {
     cy.findByRole('navigation', { name: 'View tagged posts by' }).within(() => {
       cy.findByRole('link', { name: 'Month' }).as('month');
-      cy.get('@month').should('have.attr', 'aria-current', '');
       cy.get('@month').click();
     });
 
@@ -66,7 +64,6 @@ describe('Tag index page navigation', () => {
   it('should navigate to Year view', () => {
     cy.findByRole('navigation', { name: 'View tagged posts by' }).within(() => {
       cy.findByRole('link', { name: 'Year' }).as('year');
-      cy.get('@year').should('have.attr', 'aria-current', '');
       cy.get('@year').click();
     });
 
@@ -84,7 +81,6 @@ describe('Tag index page navigation', () => {
   it('should navigate to Infinity view', () => {
     cy.findByRole('navigation', { name: 'View tagged posts by' }).within(() => {
       cy.findByRole('link', { name: 'Infinity' }).as('infinity');
-      cy.get('@infinity').should('have.attr', 'aria-current', '');
       cy.get('@infinity').click();
     });
 
@@ -102,7 +98,6 @@ describe('Tag index page navigation', () => {
   it('should navigate to Latest view', () => {
     cy.findByRole('navigation', { name: 'View tagged posts by' }).within(() => {
       cy.findByRole('link', { name: 'Latest' }).as('latest');
-      cy.get('@latest').should('have.attr', 'aria-current', '');
       cy.get('@latest').click();
     });
 
