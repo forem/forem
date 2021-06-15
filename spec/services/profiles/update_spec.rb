c033ff4198c8d85bca9688290fb5afa4f84bc3e8
Jacob Herrington <jacobherringtondeveloper@gmail.com>
2021-06-15 11:45:18 -0500

    WIP fix issues revealed by services specs

--- spec/services/profiles/update_spec.rb
+++ spec/services/profiles/update_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe Profiles::Update, type: :service do
 
   it "correctly typecasts new attributes", :aggregate_failures do
     described_class.call(user, profile: { location: 123, education: "false" })
-    expect(user.location).to eq "123"
+    expect(user.profile.location).to eq "123"
     expect(profile.education).to eq "false"
   end
 
@@ -87,7 +87,7 @@ RSpec.describe Profiles::Update, type: :service do
 
     it "enqueues resave articles job when changing summary" do
       sidekiq_assert_resave_article_worker(user) do
-        described_class.call(user, profile: { summary: "#{user.summary} changed" })
+        described_class.call(user, profile: { summary: "#{user.profile.summary} changed" })
       end
     end
 
