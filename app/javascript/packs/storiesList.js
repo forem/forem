0d1b8842be0a9a50dd6a1b6556dcf0aecfd1b874
Julien Maury <6878027+jmau111@users.noreply.github.com>
2021-06-15 19:42:25 +0200

    remove legacy function hasClass()

--- app/javascript/packs/storiesList.js
+++ app/javascript/packs/storiesList.js
@@ -1,8 +1,3 @@
-function hasClass(el, cls) {
-  return (
-    el.className && new RegExp(`(\\s|^)${  cls  }(\\s|$)`).test(el.className)
-  );
-}
 const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
 if (isTouchDevice) {
   const activeLinks = document.getElementsByClassName('active');
@@ -10,25 +5,27 @@ if (isTouchDevice) {
     el.classList.remove('active');
   });
 
-  document
-    .getElementById('main-content')
-    .addEventListener('click', (event) => {
-      const clickedEl = event.target;
-      if (
-        hasClass(clickedEl, 'bm-initial') ||
-        hasClass(clickedEl, 'bm-success')
-      ) {
-        //do nothing
-      } else if (hasClass(clickedEl.parentNode, 'crayons-story')) {
-        clickedEl.parentNode.classList.add('active');
-      } else if (hasClass(clickedEl.parentNode.parentNode, 'crayons-story')) {
-        clickedEl.parentNode.parentNode.classList.add('active');
-      } else if (
-        hasClass(clickedEl.parentNode.parentNode.parentNode, 'crayons-story')
-      ) {
-        clickedEl.parentNode.parentNode.parentNode.classList.add('active');
-      }
-    });
+  document.getElementById('main-content').addEventListener('click', (event) => {
+    const clickedEl = event.target;
+    if (
+      clickedEl.classList.contains('bm-initial') ||
+      clickedEl.classList.contains('bm-success')
+    ) {
+      //do nothing
+    } else if (clickedEl.parentNode.classList.contains('crayons-story')) {
+      clickedEl.parentNode.classList.add('active');
+    } else if (
+      clickedEl.parentNode.parentNode.classList.contains('crayons-story')
+    ) {
+      clickedEl.parentNode.parentNode.classList.add('active');
+    } else if (
+      clickedEl.parentNode.parentNode.parentNode.classList.contains(
+        'crayons-story',
+      )
+    ) {
+      clickedEl.parentNode.parentNode.parentNode.classList.add('active');
+    }
+  });
 }
 
 // A custom event that gets dispatched to notify search forms to synchronize their state.
