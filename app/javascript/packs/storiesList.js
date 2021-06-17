ea6a29fdc776b4f089babeaa912bfdd9bb64fe26
Julien Maury <6878027+jmau111@users.noreply.github.com>
2021-06-17 12:46:22 +0200

    remove unused code

--- app/javascript/packs/storiesList.js
+++ app/javascript/packs/storiesList.js
@@ -1,36 +1,6 @@
 /**
- * @file Manages logic to add active class on items when it's a touch device
+ * @file A custom event that gets dispatched to notify search forms to synchronize their state.
  */
-const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
-if (isTouchDevice) {
-  Array.from(document.getElementsByClassName('active')).forEach((el) =>
-    el.classList.remove('active'),
-  );
-
-  document.getElementById('main-content').addEventListener('click', (event) => {
-    const clickedEl = event.target;
-    if (
-      clickedEl.classList.contains('bm-initial') ||
-      clickedEl.classList.contains('bm-success')
-    ) {
-      //do nothing
-    } else if (clickedEl.parentNode.classList.contains('crayons-story')) {
-      clickedEl.parentNode.classList.add('active');
-    } else if (
-      clickedEl.parentNode.parentNode.classList.contains('crayons-story')
-    ) {
-      clickedEl.parentNode.parentNode.classList.add('active');
-    } else if (
-      clickedEl.parentNode.parentNode.parentNode.classList.contains(
-        'crayons-story',
-      )
-    ) {
-      clickedEl.parentNode.parentNode.parentNode.classList.add('active');
-    }
-  });
-}
-
-// A custom event that gets dispatched to notify search forms to synchronize their state.
 window.dispatchEvent(
   new CustomEvent('syncSearchForms', {
     detail: { querystring: location.search },
