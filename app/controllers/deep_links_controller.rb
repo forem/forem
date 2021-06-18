741ba72c830fefbb693dbbb080a1760f29489efb
Fernando Valverde <fernando@visualcosita.com>
2021-06-18 09:05:48 -0600

    Makes Consumer Apps dictate aasa results

--- app/controllers/deep_links_controller.rb
+++ app/controllers/deep_links_controller.rb
@@ -4,10 +4,8 @@ class DeepLinksController < ApplicationController
   # Apple Application Site Association - based on Apple docs guidelines
   # https://developer.apple.com/library/archive/documentation/General/Conceptual/AppSearch/UniversalLinks.html
   def aasa
-    # TODO: [@fdoxyz] Replace these hardcoded identifiers with configurations
-    # creators can use to customize their Forems - `/admin/consumer_apps`
-    supported_apps = ["R9SWHSQNV8.com.forem.app"]
-    supported_apps << "R9SWHSQNV8.to.dev.ios" if ForemInstance.dev_to?
+    @apps = ConsumerApps::FindOrCreateAllQuery.call.where(platform: Device::IOS)
+    supported_apps = @apps.map(&:app_bundle)
     render json: {
       applinks: {
         apps: [],
