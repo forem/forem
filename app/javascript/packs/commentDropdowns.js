d08f93e07bf5e040de312cacd3a2764c2b88c3df
Suzanne Aitchison <suzanne@forem.com>
2021-06-23 12:12:38 +0100

    tidy up error, add some comments

--- app/javascript/packs/commentDropdowns.js
+++ app/javascript/packs/commentDropdowns.js
@@ -27,68 +27,77 @@ const initializeArticlePageDropdowns = () => {
     }
 
     const dropdownContentId = dropdownTrigger.getAttribute('aria-controls');
-    // TODO: there's a brief moment where the trigger exists but the dropdown content doesn't, and we get an error here
-    const { closeDropdown } = initializeDropdown({
-      triggerElementId: dropdownTrigger.id,
-      dropdownContentId,
-    });
-
-    // Add actual link location (SEO doesn't like these "useless" links, so adding in here instead of in HTML)
     const dropdownElement = document.getElementById(dropdownContentId);
-    const reportAbuseWrapper = dropdownElement.querySelector(
-      '.report-abuse-link-wrapper',
-    );
-    if (reportAbuseWrapper) {
-      reportAbuseWrapper.innerHTML = `<a href="${reportAbuseWrapper.dataset.path}" class="crayons-link crayons-link--block">Report abuse</a>`;
+
+    if (dropdownElement) {
+      const { closeDropdown } = initializeDropdown({
+        triggerElementId: dropdownTrigger.id,
+        dropdownContentId,
+      });
+
+      // Add actual link location (SEO doesn't like these "useless" links, so adding in here instead of in HTML)
+      const reportAbuseWrapper = dropdownElement.querySelector(
+        '.report-abuse-link-wrapper',
+      );
+      if (reportAbuseWrapper) {
+        reportAbuseWrapper.innerHTML = `<a href="${reportAbuseWrapper.dataset.path}" class="crayons-link crayons-link--block">Report abuse</a>`;
+      }
+
+      // Initialize the "Copy link" functionality
+      dropdownElement
+        .querySelector('.permalink-copybtn')
+        ?.addEventListener('click', handleCopyPermalink(closeDropdown));
+
+      dropdownTrigger.dataset.initialized = 'true';
     }
-
-    // Initialize the "Copy link" functionality
-    dropdownElement
-      .querySelector('.permalink-copybtn')
-      ?.addEventListener('click', handleCopyPermalink(closeDropdown));
-
-    dropdownTrigger.dataset.initialized = 'true';
   }
 };
 
-const fetchMissingProfilePreviewCard = async (element) => {
-  window
+/**
+ * Function to retrieve the Profile Preview Card for newly added comments, and asynchronously replace the placeholder
+ *
+ * @param {HTMLElement} placeholderElement The <span> placeholder element to be replaced by the preview dropdown
+ */
+const fetchMissingProfilePreviewCard = async (placeholderElement) => {
+  const htmlContent = await window
     .fetch(
-      `/profile_preview_card/show?userid=${element.dataset.jsCommentUserId}&preview_card_id=${element.dataset.jsDropdownContentId}`,
+      `/profile_preview_card/show?userid=${placeholderElement.dataset.jsCommentUserId}&preview_card_id=${placeholderElement.dataset.jsDropdownContentId}`,
     )
-    .then((res) => res.text())
-    .then((response) => {
-      const generatedElement = document.createElement('div');
-      generatedElement.innerHTML = response;
-      element.parentNode.replaceChild(
-        generatedElement.firstElementChild,
-        element,
-      );
-      // Make sure the button inside the dropdown is initialized
-      initializeAllFollowButts();
-    });
+    .then((res) => res.text());
+
+  const generatedElement = document.createElement('div');
+  generatedElement.innerHTML = htmlContent;
+
+  placeholderElement.parentNode.replaceChild(
+    generatedElement.firstElementChild,
+    placeholderElement,
+  );
+
+  // Make sure the button inside the dropdown is initialized
+  initializeAllFollowButts();
+};
+
+/**
+ * When a new comment is added to a dicussion, the preview card dropdown must be dynamically fetched.
+ * This function detects if a preview card placeholder has been added in a given mutation and initiates the profile card fetch.
+ **/
+const checkMutationForProfilePreviewCardPlaceholder = (mutation) => {
+  mutation.addedNodes.forEach((node) => {
+    if (node.nodeType === Node.ELEMENT_NODE) {
+      const placeholder = node.getElementsByClassName(
+        'preview-card-placeholder',
+      )[0];
+      if (placeholder) {
+        fetchMissingProfilePreviewCard(placeholder);
+      }
+    }
+  });
 };
 
 const observer = new MutationObserver((mutationsList) => {
   mutationsList.forEach((mutation) => {
     if (mutation.type === 'childList') {
-      let profilePreviewMutation;
-
-      mutation.addedNodes.forEach((node) => {
-        if (
-          node.nodeType === Node.ELEMENT_NODE &&
-          node.getElementsByClassName('preview-card-placeholder')[0]
-        ) {
-          profilePreviewMutation = node.getElementsByClassName(
-            'preview-card-placeholder',
-          )[0];
-        }
-      });
-
-      if (profilePreviewMutation) {
-        fetchMissingProfilePreviewCard(profilePreviewMutation);
-      }
-
+      checkMutationForProfilePreviewCardPlaceholder(mutation);
       initializeArticlePageDropdowns();
     }
   });
