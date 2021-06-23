08b94984991f785c52233c7d8562d4cc20540730
Dan Uber <dan@forem.com>
2021-06-23 08:53:57 -0500

    Use nokogiri to parse html instead of a regex to match it

--- spec/services/markdown_processor/parser_spec.rb
+++ spec/services/markdown_processor/parser_spec.rb
@@ -43,8 +43,8 @@ RSpec.describe MarkdownProcessor::Parser, type: :service do
   it "does not insert extra newlines in codeblocks when using tildes" do
     code_block = "~~~\nhello\n   there\n~~~"
     parsed = generate_and_parse_markdown(code_block)
-    rendered_code_block = parsed.scan(%r{<code>.*?</code>}m).first
-    expect(rendered_code_block).to eq("<code>hello\n   there\n</code>")
+    rendered_code_block = Nokogiri::HTML(parsed).xpath("//code")
+    expect(rendered_code_block.to_s).to eq("<code>hello\n   there\n</code>")
   end
 
   it "does not insert extra newlines in codeblocks followed by headers" do
@@ -59,8 +59,8 @@ RSpec.describe MarkdownProcessor::Parser, type: :service do
       # header
     RAW
     parsed = generate_and_parse_markdown(code_block)
-    rendered_code_block = parsed.scan(%r{<code>.*?</code>}m).first
-    expect(rendered_code_block).to eq("<code>line 1\n    line 2\n    line 3\n</code>")
+    rendered_code_block = Nokogiri::HTML(parsed).xpath("//code")
+    expect(rendered_code_block.to_s).to eq("<code>line 1\n    line 2\n    line 3\n</code>")
   end
 
   it "does not remove the non-'raw tag related' four dashes" do
