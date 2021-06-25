845a5fade539142763ae31527a450edfdc985ad4
Suzanne Aitchison <suzanne@forem.com>
2021-06-25 10:05:37 +0100

    Profile Preview Cards (RFC#222) - author name in comments (#14056)
    * show dropdowns on pre-existing comments
    
    * show preview on comments, v rough version of showing preview on recently added comments
    
    * tidy up error, add some comments
    
    * show the ama icon when appropriate
    
    * add to comment on article cypress spec
    
    * add test for preview card on article comment
    
    * tidy up
    
    * small refactors
    
    * wait for button init in cypress test
    
    * add test for article author byline on organization post
    
    * Trigger CI builds
    
    * Trigger CI builds

--- spec/support/seeds/seeds_e2e.rb
+++ spec/support/seeds/seeds_e2e.rb
@@ -241,11 +241,42 @@ seeder.create_if_doesnt_exist(Article, "title", "Test article") do
     #{Faker::Markdown.random}
     #{Faker::Hipster.paragraph(sentence_count: 2)}
   MARKDOWN
+  article = Article.create(
+    body_markdown: markdown,
+    featured: true,
+    show_comments: true,
+    user_id: admin_user.id,
+  )
+
+  comment_attributes = {
+    body_markdown: Faker::Hipster.paragraph(sentence_count: 1),
+    user_id: admin_user.id,
+    commentable_id: article.id,
+    commentable_type: "Article"
+  }
+
+  Comment.create!(comment_attributes)
+end
+
+##############################################################################
+
+seeder.create_if_doesnt_exist(Article, "title", "Organization test article") do
+  markdown = <<~MARKDOWN
+    ---
+    title:  Organization test article
+    published: true
+    cover_image: #{Faker::Company.logo}
+    ---
+    #{Faker::Hipster.paragraph(sentence_count: 2)}
+    #{Faker::Markdown.random}
+    #{Faker::Hipster.paragraph(sentence_count: 2)}
+  MARKDOWN
   Article.create(
     body_markdown: markdown,
     featured: true,
     show_comments: true,
     user_id: admin_user.id,
+    organization_id: Organization.first.id,
   )
 end
 
