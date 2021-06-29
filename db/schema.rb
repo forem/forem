161ebfdcbc5e700b86259c21a17feb6f446df612
squarebat <ksidhpuri@gmail.com>
2021-06-29 18:26:45 +0530

    fix: render strikethrough in comment links

--- db/schema.rb
+++ db/schema.rb
@@ -1358,7 +1358,7 @@ ActiveRecord::Schema.define(version: 2021_06_24_153854) do
     t.datetime "last_article_at", default: "2017-01-01 05:00:00"
     t.datetime "last_comment_at", default: "2017-01-01 05:00:00"
     t.datetime "last_followed_at"
-    t.datetime "last_moderation_notification", default: "2017-01-01 05:00:00"
+    t.datetime "last_moderation_notification", default: "2017-01-01 00:00:00"
     t.datetime "last_notification_activity"
     t.string "last_onboarding_page"
     t.datetime "last_reacted_at"
@@ -1439,9 +1439,9 @@ ActiveRecord::Schema.define(version: 2021_06_24_153854) do
 
   create_table "users_gdpr_delete_requests", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
-    t.string "email"
+    t.string "email", null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.string "username"
   end
 
