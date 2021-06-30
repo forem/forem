3e58f24a1309abfc6b626f617ac88498a828737f
Suzanne Aitchison <suzanne@forem.com>
2021-06-30 16:43:02 +0100

    stub unneeded network requests

--- cypress/integration/articleFlows/pinArticle.spec.js
+++ cypress/integration/articleFlows/pinArticle.spec.js
@@ -18,6 +18,15 @@ describe('Pin an article - Non admin user', () => {
     cy.testSetup();
     cy.fixture('users/articleEditorV1User.json').as('user');
 
+    // Responses from these requests are not required for this test, and are stubbed to prevent responses interfering with subsequent tests
+    cy.intercept('/reactions?article**', {
+      body: { reactions: [], article_reaction_counts: [] },
+    });
+    cy.intercept('/reactions?commentable**', {
+      body: { reactions: [], public_reaction_counts: [] },
+    });
+    cy.intercept('/follows**', {});
+
     cy.get('@user').then((user) => {
       cy.loginUser(user).then(() => {
         cy.createArticle({
