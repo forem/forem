8ff398785887ab119074551f8bdcbe22d5df12a8
Suzanne Aitchison <suzanne@forem.com>
2021-06-30 12:13:11 +0100

    break chat message tests into more manageable chunks

--- cypress/integration/chatFlows/messageOptions.spec.js
+++ cypress/integration/chatFlows/messageOptions.spec.js
@@ -3,20 +3,37 @@ describe('Chat message options', () => {
     cy.testSetup();
 
     cy.intercept(
-      { method: 'POST', url: '/chat_channels/1/open' },
+      { method: 'POST', url: '/chat_channels/**/open' },
       { body: {} },
     );
 
-    cy.fixture('users/chatUser1.json').as('user');
-    cy.fixture('users/chatUser2.json').as('user2');
-
+    cy.fixture('users/chatUser2.json').as('user');
     cy.get('@user').then((user) => {
-      cy.loginAndVisit(user, '/connect');
+      cy.loginAndVisit(user, '/connect/test-chat-channel');
     });
   });
 
-  it('should show message option menus', () => {
-    //   Enter the test chat
+  it("should show report options for another user's messages", () => {
+    // Enter the test chat
+    cy.findByRole('button', { name: 'Toggle request manager' }).click();
+    cy.findByRole('button', { name: 'Accept' }).click();
+    cy.findByRole('button', { name: 'Accept' }).should('not.exist');
+
+    // Check the report menu is present and opens
+    cy.findByRole('button', { name: 'Report message options' }).as(
+      'reportMenuButton',
+    );
+    cy.get('@reportMenuButton').click();
+    cy.findByRole('button', { name: 'Report Abuse' }).should('have.focus');
+
+    // Check Escape closes the menu
+    cy.get('body').type('{esc}');
+    cy.get('@reportMenuButton').should('have.focus');
+    cy.findByRole('button', { name: 'Report Abuse' }).should('not.exist');
+  });
+
+  it("should show options for a user's own messages", () => {
+    // Enter the test chat
     cy.findByRole('button', { name: 'Toggle request manager' }).click();
     cy.findByRole('button', { name: 'Accept' }).click();
     // Wait for acceptance to happen
@@ -50,32 +67,5 @@ describe('Chat message options', () => {
     cy.get('@optionsMenuButton').should('have.focus');
     cy.findByRole('button', { name: 'Edit' }).should('not.exist');
     cy.findByRole('button', { name: 'Delete' }).should('not.exist');
-
-    // Log out the current user
-    cy.signOutUser().then(() => {
-      // Log in as someone else to verify the report abuse options
-      cy.get('@user2').then((user) => {
-        cy.loginAndVisit(user, '/connect').then(() => {
-          //   Enter the test chat
-          cy.findByRole('button', { name: 'Toggle request manager' }).click();
-          cy.findByRole('button', { name: 'Accept' }).click();
-          cy.findByRole('button', { name: 'Accept' }).should('not.exist');
-
-          // Check the report menu is present and opens
-          cy.findByRole('button', { name: 'Report message options' }).as(
-            'reportMenuButton',
-          );
-          cy.get('@reportMenuButton').click();
-          cy.findByRole('button', { name: 'Report Abuse' }).should(
-            'have.focus',
-          );
-
-          // Check Escape closes the menu
-          cy.get('body').type('{esc}');
-          cy.get('@reportMenuButton').should('have.focus');
-          cy.findByRole('button', { name: 'Report Abuse' }).should('not.exist');
-        });
-      });
-    });
   });
 });
