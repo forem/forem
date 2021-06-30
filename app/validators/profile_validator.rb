f11f678b2778ab7e384c32fb27330d7e7a2522a6
Michael Kohl <citizen428@forem.com>
2021-06-30 13:41:43 +0700

    Drop profile fields for static attributes

--- app/validators/profile_validator.rb
+++ app/validators/profile_validator.rb
@@ -32,19 +32,10 @@ class ProfileValidator < ActiveModel::Validator
   private
 
   def summary_too_long?(record)
-    # Calling the summary attribute method on record during onboarding
-    # throws a NoMethodError
-    return unless record.respond_to?(SUMMARY_ATTRIBUTE)
-
-    # TODO: [@jacobherrington] This will need to be addressed when the summary
-    # ProfileField is dropped from production.
-    return unless ProfileField.exists?(attribute_name: SUMMARY_ATTRIBUTE)
     return if record.summary.blank?
 
     # Grandfather in people who had a too long summary before
-    # TODO: [@jacobherrington] `record.data_was` can be removed when we delete
-    # the old data and drop the fields from `Profile.static_fields`.
-    previous_summary = record.summary_was || record.data_was[SUMMARY_ATTRIBUTE]
+    previous_summary = record.summary_was
     return if previous_summary && previous_summary.size > MAX_SUMMARY_LENGTH
 
     record.summary.size > MAX_SUMMARY_LENGTH
