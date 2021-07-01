039b973c0ad936044f17b9399276f3fd5172d486
Michael Kohl <citizen428@forem.com>
2021-07-01 11:24:45 +0700

    Update worker

--- lib/data_update_scripts/20210630041322_migrate_data_to_work_field.rb
+++ lib/data_update_scripts/20210630041322_migrate_data_to_work_field.rb
@@ -1,10 +1,10 @@
 module DataUpdateScripts
   class MigrateDataToWorkField
-    QUERY = "data->>'employment_title' <> '' AND data->'work' IS NULL".freeze
+    QUERY = "data->>'employment_title' <> '' AND data->>'work' IS NULL".freeze
 
     def run
-      Profile.where(QUERY).ids.each do |profile_id|
-        MigrateDataToWorkFieldWorker.perform_async(profile_id)
+      Profile.where(QUERY).select(:id).find_each do |profile|
+        MigrateDataToWorkFieldWorker.perform_async(profile.id)
       end
     end
   end
