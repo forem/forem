40dd935c5f6c782171acca9a78d95b7e10e4d501
Fernando Valverde <fernando@visualcosita.com>
2021-07-01 15:22:32 -0600

    Defer initialization until page is ready

--- app/javascript/packs/runtimeBanner.jsx
+++ app/javascript/packs/runtimeBanner.jsx
@@ -8,8 +8,25 @@ function loadElement() {
   }
 }
 
-window.InstantClick.on('change', () => {
-  loadElement();
-});
+function initializeBannerWhenPageIsReady() {
+  setTimeout(() => {
+    if (document.body.getAttribute('data-loaded') === 'true') {
+      // Page hasn't initialized yet. We need to wait until the page is ready
+      initializeBannerWhenPageIsReady();
+    } else {
+      // We're ready to initialize
+      window.InstantClick.on('change', () => {
+        loadElement();
+      });
 
-loadElement();
+      loadElement();
+    }
+  }, 100);
+}
+
+// This pack relies on the same logic as `packs/listings` & `packs/Chat`. The
+// banner lives in every page (including the main feed) and a race condition
+// occurs when the page initializes for the first time or when signing out
+// (no cache). Because of this we defer the initialization until the page is
+// actually ready.
+initializeBannerWhenPageIsReady();
