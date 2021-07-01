60d94cabeaf0e7d4d97ad1fdf6fa937b91609fd5
Fernando Valverde <fernando@visualcosita.com>
2021-07-01 10:36:15 -0600

    Banner v2 - first working version

---
+++ app/javascript/packs/runtimeBanner.jsx
@@ -0,0 +1,15 @@
+import { h, render } from 'preact';
+import { RuntimeBanner } from '../runtimeBanner';
+
+function loadElement() {
+  const container = document.getElementById('runtime-banner-container');
+  if (container) {
+    render(<RuntimeBanner />, container);
+  }
+}
+
+window.InstantClick.on('change', () => {
+  loadElement();
+});
+
+loadElement();
