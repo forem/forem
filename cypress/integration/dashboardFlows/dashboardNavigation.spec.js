16cec7d93d04048947f64c14e40c46ce9cd337d1
Suzanne Aitchison <suzanne@forem.com>
2021-07-07 09:41:35 +0100

    [15 min fix] Add aria-current attribute to current dashboard link (#14128)
    * add aria-current attribute to current dashboard link
    
    * Update app/views/dashboards/_actions.html.erb
    
    Co-authored-by: rhymes <github@rhymes.dev>
    
    * Update app/views/dashboards/_actions.html.erb
    
    Co-authored-by: rhymes <github@rhymes.dev>
    
    * Update app/views/dashboards/_actions.html.erb
    
    Co-authored-by: rhymes <github@rhymes.dev>
    
    * Update app/views/dashboards/_actions.html.erb
    
    Co-authored-by: rhymes <github@rhymes.dev>
    
    * Update app/views/dashboards/_actions.html.erb
    
    Co-authored-by: rhymes <github@rhymes.dev>
    
    * Update app/views/dashboards/_actions.html.erb
    
    Co-authored-by: rhymes <github@rhymes.dev>
    
    * stub network request
    
    * fix typo in stub
    
    * always guard against lack of entries
    
    Co-authored-by: rhymes <github@rhymes.dev>

---
+++ cypress/integration/dashboardFlows/dashboardNavigation.spec.js
@@ -0,0 +1,134 @@
+describe('Dashboard navigation', () => {
+  beforeEach(() => {
+    cy.testSetup();
+    cy.fixture('users/articleEditorV1User.json').as('user');
+
+    cy.intercept('/api/followers/**', []);
+    cy.intercept('/followings/**', []);
+
+    cy.get('@user').then((user) => {
+      cy.loginAndVisit(user, '/dashboard');
+    });
+  });
+
+  it('Shows the Posts dashboard', () => {
+    cy.findByRole('heading', { name: 'Posts' });
+    cy.findByRole('navigation').within(() => {
+      cy.findByRole('link', { name: /Posts/ }).should(
+        'have.attr',
+        'aria-current',
+        'page',
+      );
+
+      const otherLinks = [
+        /Series/,
+        /Followers/,
+        /Following tags/,
+        /Following users/,
+        /Following organizations/,
+        /Following podcasts/,
+      ];
+      otherLinks.forEach((name) => {
+        cy.findByRole('link', { name }).should('not.have.attr', 'aria-current');
+      });
+    });
+  });
+
+  it('Shows the Series dashboard', () => {
+    cy.findByRole('link', { name: /Series/ }).click();
+
+    // The Series dashboard does not show the sidebar like the others
+    cy.findByRole('heading', { name: "Article Editor v1 User's Series" });
+  });
+
+  it('Shows the Followers dashboard', () => {
+    cy.findByRole('link', { name: /Followers/ }).click();
+    cy.findByRole('heading', { name: 'Dashboard » Followers' });
+
+    cy.findByRole('navigation', { name: 'Dashboards' }).within(() => {
+      cy.findByRole('link', { name: /Followers/ }).should(
+        'have.attr',
+        'aria-current',
+        'page',
+      );
+      const otherLinks = [
+        /Posts/,
+        /Series/,
+        /Following tags/,
+        /Following users/,
+        /Following organizations/,
+        /Following podcasts/,
+      ];
+      otherLinks.forEach((name) => {
+        cy.findByRole('link', { name }).should('not.have.attr', 'aria-current');
+      });
+    });
+  });
+
+  it('Shows the Following tags dashboard', () => {
+    cy.findByRole('link', { name: /Following tags/ }).click();
+    cy.findByRole('heading', { name: 'Dashboard » Following tags' });
+    const otherLinks = [
+      /Posts/,
+      /Series/,
+      /Followers/,
+      /Following users/,
+      /Following organizations/,
+      /Following podcasts/,
+    ];
+    otherLinks.forEach((name) => {
+      cy.findByRole('link', { name }).should('not.have.attr', 'aria-current');
+    });
+  });
+
+  it('Shows the Following users dashboard', () => {
+    cy.findByRole('link', { name: /Following users/ }).click();
+    cy.findByRole('heading', { name: 'Dashboard » Following users' });
+
+    const otherLinks = [
+      /Posts/,
+      /Series/,
+      /Followers/,
+      /Following tags/,
+      /Following organizations/,
+      /Following podcasts/,
+    ];
+    otherLinks.forEach((name) => {
+      cy.findByRole('link', { name }).should('not.have.attr', 'aria-current');
+    });
+  });
+
+  it('Shows the Following organizations dashboard', () => {
+    cy.findByRole('link', { name: /Following organizations/ }).click();
+    cy.findByRole('heading', { name: 'Dashboard » Following organizations' });
+
+    const otherLinks = [
+      /Posts/,
+      /Series/,
+      /Followers/,
+      /Following tags/,
+      /Following users/,
+      /Following podcasts/,
+    ];
+    otherLinks.forEach((name) => {
+      cy.findByRole('link', { name }).should('not.have.attr', 'aria-current');
+    });
+  });
+
+  it('Shows the Following podcasts dashboard', () => {
+    cy.findByRole('link', { name: /Following podcasts/ }).click();
+    cy.findByRole('heading', { name: 'Dashboard » Following podcasts' });
+
+    const otherLinks = [
+      /Posts/,
+      /Series/,
+      /Followers/,
+      /Following tags/,
+      /Following users/,
+      /Following organizations/,
+    ];
+    otherLinks.forEach((name) => {
+      cy.findByRole('link', { name }).should('not.have.attr', 'aria-current');
+    });
+  });
+});
