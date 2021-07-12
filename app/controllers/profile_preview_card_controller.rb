904d9846a59a4b44cb6920c03dd397beb8e3a053
Suzanne Aitchison <suzanne@forem.com>
2021-07-12 13:09:12 +0100

    use userId in path, remove need to pass preview card id (#14171)

--- app/controllers/profile_preview_card_controller.rb
+++ app/controllers/profile_preview_card_controller.rb
@@ -2,7 +2,6 @@ class ProfilePreviewCardController < ApplicationController
   layout false
 
   def show
-    @actor = User.find_by(id: params[:user_id])
-    @preview_id = params[:preview_card_id]
+    @actor = User.find_by(id: params[:id])
   end
 end
