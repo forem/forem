23230a12baf0a8a3b6c8cee0b53fba5592d95993
mstruve <mollylbs@gmail.com>
2021-07-15 18:33:27 -0400

    remove ignored columns from user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_12_155135) do
+ActiveRecord::Schema.define(version: 2021_07_15_211923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
