01fa356244e8771b176c4ded41023bf52a35a96d
Keshav Biswa <keshavbiswa21@gmail.com>
2021-07-16 08:29:18 +0530

    removed bust_content_change_cache from the action

--- app/controllers/admin/settings/general_settings_controller.rb
+++ app/controllers/admin/settings/general_settings_controller.rb
@@ -12,7 +12,6 @@ module Admin
         result = ::Settings::General::Upsert.call(settings_params)
         if result.success?
           Audit::Logger.log(:internal, current_user, params.dup)
-          bust_content_change_caches
           redirect_to admin_config_path, notice: "Successfully updated settings."
         else
           redirect_to admin_config_path, alert: "😭 #{result.errors.to_sentence}"
