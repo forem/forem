56bf4d46da3841dc1aa154184d50c62082f24f31
Suzanne Aitchison <suzanne@forem.com>
2021-07-19 07:37:47 +0100

    fix flake in save click test

--- app/assets/javascripts/initializers/initializeReadingListIcons.js
+++ app/assets/javascripts/initializers/initializeReadingListIcons.js
@@ -29,6 +29,7 @@ function highlightButton(button) {
     button.classList.remove('selected');
   }
   button.addEventListener('click', reactToReadingListButtonClick);
+  button.dataset.clickInitialized = true;
 }
 
 function addReadingListCountToHomePage() {
