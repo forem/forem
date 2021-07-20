b46e50c0ebe65170c8053af17bc53fe8db859d78
Suzanne Aitchison <suzanne@forem.com>
2021-07-20 14:20:07 +0100

    add test for the tag index page

--- cypress/integration/tagsFlows/followTag.spec.js
+++ cypress/integration/tagsFlows/followTag.spec.js
@@ -1,28 +1,59 @@
 describe('Follow tag', () => {
-  beforeEach(() => {
-    cy.testSetup();
-    cy.fixture('users/articleEditorV1User.json').as('user');
+  describe('Tag index page', () => {
+    beforeEach(() => {
+      cy.testSetup();
+      cy.fixture('users/articleEditorV1User.json').as('user');
 
-    cy.get('@user').then((user) => {
-      cy.loginAndVisit(user, '/t/tag1').then(() => {
-        cy.findByRole('heading', { name: '# tag1' });
-        cy.get('[data-follow-clicks-initialized]');
+      cy.get('@user').then((user) => {
+        cy.loginAndVisit(user, '/tags').then(() => {
+          cy.findByRole('heading', { name: 'Top tags' });
+          cy.get('[data-follow-clicks-initialized]');
+        });
       });
+    });
+
+    it('Follows and unfollows a tag from the tag index page', () => {
+      cy.intercept('/follows').as('followsRequest');
+      cy.findByRole('button', { name: 'Follow' }).as('followButton');
+
+      cy.get('@followButton').click();
+      cy.wait('@followsRequest');
+
+      cy.get('@followButton').should('have.text', 'Following');
+
+      cy.get('@followButton').click();
+      cy.wait('@followsRequest');
+
+      cy.get('@followButton').should('have.text', 'Follow');
     });
   });
 
-  it('Follows and unfollows a tag', () => {
-    cy.intercept('/follows').as('followsRequest');
-    cy.findByRole('button', { name: 'Follow' }).as('followButton');
+  describe('Tag feed page', () => {
+    beforeEach(() => {
+      cy.testSetup();
+      cy.fixture('users/articleEditorV1User.json').as('user');
 
-    cy.get('@followButton').click();
-    cy.wait('@followsRequest');
+      cy.get('@user').then((user) => {
+        cy.loginAndVisit(user, '/t/tag1').then(() => {
+          cy.findByRole('heading', { name: '# tag1' });
+          cy.get('[data-follow-clicks-initialized]');
+        });
+      });
+    });
 
-    cy.get('@followButton').should('have.text', 'Following');
+    it('Follows and unfollows a tag from the tag feed page', () => {
+      cy.intercept('/follows').as('followsRequest');
+      cy.findByRole('button', { name: 'Follow' }).as('followButton');
 
-    cy.get('@followButton').click();
-    cy.wait('@followsRequest');
+      cy.get('@followButton').click();
+      cy.wait('@followsRequest');
 
-    cy.get('@followButton').should('have.text', 'Follow');
+      cy.get('@followButton').should('have.text', 'Following');
+
+      cy.get('@followButton').click();
+      cy.wait('@followsRequest');
+
+      cy.get('@followButton').should('have.text', 'Follow');
+    });
   });
 });
