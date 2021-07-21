12fced23dabe3841ff40d24a7a3fcbd877c0e5ff
Michael Kohl <citizen428@forem.com>
2021-07-21 11:38:55 +0700

    Update migration

--- db/migrate/20210720042422_drop_custom_profile_fields.rb
+++ db/migrate/20210720042422_drop_custom_profile_fields.rb
@@ -1,12 +1,7 @@
 class DropCustomProfileFields < ActiveRecord::Migration[6.1]
   disable_ddl_transaction!
 
-  def change
+  def up
     drop_table :custom_profile_fields, if_exists: true
-
-    remove_index :custom_profile_fields,
-                 column: [:label, :profile_id],
-                 algorithm: :concurrently,
-                 if_exists: true
   end
 end
