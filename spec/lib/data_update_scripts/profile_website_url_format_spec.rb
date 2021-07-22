c7b6f42477dc63c63f1d3b217183c6b5371ab101
Dan Uber <dan@forem.com>
2021-07-22 13:07:35 -0500

    Add additional test cases to the DUS
    And then handle them.

--- spec/lib/data_update_scripts/profile_website_url_format_spec.rb
+++ spec/lib/data_update_scripts/profile_website_url_format_spec.rb
@@ -1,24 +1,24 @@
 require "rails_helper"
 require Rails.root.join(
-  "lib/data_update_scripts/20210722135549_profile_website_url_format.rb",
+  "lib/data_update_scripts/20210722160452_profile_website_url_format.rb",
 )
 
-describe DataUpdateScripts::ProfileWebsiteUrlFormat do
+describe DataUpdateScripts::ProfileWebsiteUrlFormatFixup do
   let(:nil_profile) { create(:profile, website_url: nil) }
   let(:empty_profile) { create(:profile, website_url: "") }
   let(:valid_profile) { create(:profile, website_url: "https://www.example.com") }
 
-  let(:invalid_profile) do
-    build(:profile, website_url: "www.example.com").tap do |profile|
+  def make_profile_for(website_url)
+    build(:profile, website_url: website_url).tap do |profile|
       profile.save(validate: false)
     end
   end
 
-  let(:unfixable_profile) do
-    build(:profile, website_url: "/local.html").tap do |profile|
-      profile.save(validate: false)
-    end
-  end
+  let(:invalid_profile) { make_profile_for("www.example.com") }
+  let(:unfixable_profile) { make_profile_for("/local.html") }
+  let(:email_profile) { make_profile_for("dan@forem.com") }
+  let(:spacey_profile) { make_profile_for("  example.com  ") }
+  let(:unparseable_profile) { make_profile_for("this will not parse") }
 
   it "does not modify profiles where website url is null" do
     expect { described_class.new.run }.not_to change(nil_profile, :website_url)
@@ -43,6 +43,29 @@ describe DataUpdateScripts::ProfileWebsiteUrlFormat do
     expect { described_class.new.run }
       .to change { unfixable_profile.reload.website_url }
       .from("/local.html")
+      .to("")
+  end
+
+  it "rejects users in the link" do
+    # what was meant to be an email address is actually a valid user@host url
+    # we just don't want to do this
+    expect { described_class.new.run }
+      .to change { email_profile.reload.website_url }
+      .from("dan@forem.com")
+      .to("")
+  end
+
+  it "trims the input to help parsing" do
+    expect { described_class.new.run }
+      .to change { spacey_profile.reload.website_url }
+      .from("  example.com  ")
+      .to("https://example.com")
+  end
+
+  it "handles parse errors by clearing the website url" do
+    expect { described_class.new.run }
+      .to change { unparseable_profile.reload.website_url }
+      .from("this will not parse")
       .to("")
   end
 end
