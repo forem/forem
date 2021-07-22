e422ac0fe319236bc62a71e039285d96cf8f76f2
Alex <alexandersmith223@gmail.com>
2021-07-22 11:19:16 -0400

    PR feedback

--- app/controllers/api/v0/instances_controller.rb
+++ app/controllers/api/v0/instances_controller.rb
@@ -1,23 +1,23 @@
 module Api
   module V0
-    class ForemDirectoriesController < ApiController
-      def index
+    class InstancesController < ApiController
+      before_action :set_no_cache_header
+
+      def show
         render json: {
           cover_image_url: Settings::General.main_social_image,
           description: Settings::Community.community_description,
           logo_image_url: Settings::General.logo_png,
           name: Settings::Community.community_name,
           tagline: Settings::Community.tagline,
-          version: "edge.#{Time.current.strftime('%Y%m%d')}.0",
+          version: "edge.#{Time.now.utc.strftime('%Y%m%d')}.0",
           visibility: visibility
-        }.to_json,
-               status: :ok
+        }, status: :ok
       end
 
       private
 
       def visibility
-        # TODO(ecosystem team) - add logic to determine if a Forem instance is suspended
         return "pending" if Settings::General.waiting_on_first_user
 
         Settings::UserExperience.public ? "public" : "private"
--- config/routes.rb
+++ config/routes.rb
@@ -102,7 +102,7 @@ Rails.application.routes.draw do
           resources :listings, only: [:index], to: "organizations#listings"
           resources :articles, only: [:index], to: "organizations#articles"
         end
-        resources :forem_directories, only: %i[index]
+        resource :instance, only: %i[show]
       end
     end
 
