e02c049187476eac8285d811173d29e01fd8b1c3
zeotuan <zeotuan@gmail.com>
2021-07-27 01:38:13 +1000

    remove old test workaround and outdated comment

--- app/javascript/chat/content.jsx
+++ app/javascript/chat/content.jsx
@@ -45,9 +45,6 @@ export class Content extends Component {
       return '';
     }
     return (
-      // TODO: A button (role="button") cannot contain other interactive elements, i.e. buttons.
-      // TODO: These should have key click events as well.
-      // eslint-disable-next-line jsx-a11y/click-events-have-key-events
       <div
         className="activechatchannel__activecontent activechatchannel__activecontent--sidecar"
         id="chat_activecontent"
