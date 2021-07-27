b75f746ad665837dcc41ecef3bd64d734449e4a2
rhymes <github@rhymes.dev>
2021-07-27 17:52:10 +0200

    Add OrganizationsComponent specs

--- spec/components/admin/users/tools/organizations_component_spec.rb
+++ spec/components/admin/users/tools/organizations_component_spec.rb
@@ -1,13 +1,31 @@
 require "rails_helper"
 
 RSpec.describe Admin::Users::Tools::OrganizationsComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  describe "Add new org membership" do
+    it "renders the section", :aggregate_failures do
+      render_inline(described_class.new(user: user))
+
+      expect(rendered_component).to have_text("Add #{user.name} to a new organization")
+      expect(rendered_component).to have_css("form[action='#{admin_organization_memberships_path}']")
+    end
+  end
+
+  describe "Manage memberships" do
+    it "does not render the section by default" do
+      render_inline(described_class.new(user: user))
+
+      expect(rendered_component).not_to have_text("Manage memberships")
+    end
+
+    it "renders the section if the user belongs to an organization", :aggregate_failures do
+      membership = create(:organization_membership, user: user)
+
+      render_inline(described_class.new(user: user))
+
+      expect(rendered_component).to have_text(membership.organization.name)
+      expect(rendered_component).to have_css("input[value='delete']", visible: false)
+    end
+  end
 end
