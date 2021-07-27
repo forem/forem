91393817685e31ff612ed3b171003a8d1e85143b
rhymes <github@rhymes.dev>
2021-07-27 17:16:43 +0200

    Add CreditsComponent tests

--- spec/components/admin/users/tools/credits_component_spec.rb
+++ spec/components/admin/users/tools/credits_component_spec.rb
@@ -1,13 +1,48 @@
 require "rails_helper"
 
 RSpec.describe Admin::Users::Tools::CreditsComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  describe "Add user credits" do
+    it "renders the section", :aggregate_failures do
+      render_inline(described_class.new(user: user))
+
+      expect(rendered_component).to have_text("available user credit".pluralize(user.unspent_credits_count))
+      expect(rendered_component).to have_css("form[action=\"#{admin_user_tools_credits_path(user)}\"]")
+    end
+  end
+
+  describe "Remove credits" do
+    it "does not render the section by default" do
+      render_inline(described_class.new(user: user))
+
+      expect(rendered_component).not_to have_text("Remove credits")
+    end
+
+    it "renders the section if the user has unspent credits" do
+      Credit.add_to(user, 1)
+
+      render_inline(described_class.new(user: user))
+
+      expect(rendered_component).to have_css("form[action=\"#{admin_user_tools_credits_path(user)}\"]")
+    end
+  end
+
+  describe "Organization credits" do
+    it "does not render the section by default", :aggregate_failures do
+      render_inline(described_class.new(user: user))
+
+      expect(rendered_component).not_to have_text("Add credits to organizations")
+      expect(rendered_component).not_to have_text("Remove credits from organizations")
+    end
+
+    it "renders the section if the user belongs to an organization", :aggregate_failures do
+      create(:organization_membership, user: user)
+
+      render_inline(described_class.new(user: user))
+
+      expect(rendered_component).to have_css('form[id="org_credits_add"]')
+      expect(rendered_component).to have_css('form[id="org_credits_remove"]')
+    end
+  end
 end
