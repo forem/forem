757b27e724ad9c3d45c0168e941a00c07456cdec
Suzanne Aitchison <suzanne@forem.com>
2021-08-05 15:07:47 +0100

    add search test and tag index logged in test

---
+++ cypress/integration/seededFlows/searchFlows/previewProfileFromPostSearchResults.spec.js
@@ -0,0 +1,38 @@
+describe('Preview profile from post search results', () => {
+  beforeEach(() => {
+    cy.testSetup();
+    cy.fixture('users/articleEditorV1User.json').as('user');
+
+    cy.get('@user').then((user) => {
+      cy.loginAndVisit(user, '/search?q=test');
+    });
+  });
+
+  it('shows profile preview cards on posts in search results', () => {
+    cy.findAllByRole('button', { name: 'Admin McAdmin profile details' })
+      .first()
+      .as('previewButton');
+    cy.get('@previewButton').should('have.attr', 'data-initialized');
+    cy.get('@previewButton').click();
+
+    cy.findAllByTestId('profile-preview-card')
+      .first()
+      .within(() => {
+        cy.findByRole('link', {
+          name: 'Admin McAdmin',
+        }).should('have.focus');
+
+        // Check all the expected user data sections are present
+        cy.findByText('Admin user summary');
+        cy.findByText('Software developer at Company');
+        cy.findByText('Edinburgh');
+        cy.findByText('University of Life');
+
+        cy.findByRole('button', { name: 'Follow' }).click();
+
+        // Check that following status has been updated
+        cy.findByRole('button', { name: 'Follow' }).should('not.exist');
+        cy.findByRole('button', { name: 'Following' });
+      });
+  });
+});
