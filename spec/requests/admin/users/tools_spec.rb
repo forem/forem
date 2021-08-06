d01ec2d9c21d01508dcf33c35a53fa6a87f7c9b2
rhymes <github@rhymes.dev>
2021-08-06 13:01:56 +0200

    Fix spec

--- spec/requests/admin/users/tools_spec.rb
+++ spec/requests/admin/users/tools_spec.rb
@@ -2,5 +2,5 @@ require "rails_helper"
 require "view_component/test_helpers"
 
 RSpec.describe "/admin/users/:user_id/tools", type: :request do
-  include_examples "Admin::Users::Tools", :admin_user_tools_path, Admin::Users::ToolsComponent
+  include_examples "Admin::Users::Tools::ShowAction", :admin_user_tools_path, Admin::Users::ToolsComponent
 end
