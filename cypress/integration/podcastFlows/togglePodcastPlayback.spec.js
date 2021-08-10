8e4410ea435158a470798cd2b545e37cfb0f5c9b
aayush-05 <aayushgupta92780@gmail.com>
2021-08-10 21:23:24 +0530

    Add cypress test

--- cypress/integration/podcastFlows/togglePodcastPlayback.spec.js
+++ cypress/integration/podcastFlows/togglePodcastPlayback.spec.js
@@ -5,5 +5,14 @@ describe('Toggle Podcast playback', () => {
 
   it('should toggle podcast playback', () => {
     cy.visit('/pod');
+    cy.contains('div', 'Example media | crow call').click();
+
+    cy.get('#record-crow-call').as('toggleButton');
+
+    cy.get('@toggleButton')
+      .invoke('attr', 'aria-pressed')
+      .should('eq', 'false');
+    cy.get('@toggleButton').click();
+    cy.get('@toggleButton').invoke('attr', 'aria-pressed').should('eq', 'true');
   });
 });
