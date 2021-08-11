a884c568288f9929d35b2f3faa652fc96523b259
Suzanne Aitchison <suzanne@forem.com>
2021-08-11 11:50:49 +0100

    Reinstate skip link on search page (#14463)
    * reinstate skip link on search page
    
    * actually commit the tests this time
    
    * use optional chaining
    
    * Revert "use optional chaining"
    
    This reverts commit 921677f425c2e90f272a6e21dc8a8d37f916e0da.

--- cypress/integration/seededFlows/searchFlows/skipToContent.spec.js
+++ cypress/integration/seededFlows/searchFlows/skipToContent.spec.js
@@ -1,14 +1,33 @@
-// Regression test for https://github.com/forem/forem/issues/13876
 describe('Search on homepage', () => {
-  it('Does not show the "Skip to content" link', () => {
+  beforeEach(() => {
+    cy.testSetup();
     cy.visit('/');
+  });
 
-    cy.get('#header-search')
-      .get('form')
-      .findByPlaceholderText('Search...')
+  it('Does not show the "Skip to content" link if button is pressed', () => {
+    cy.findByRole('search')
+      .findByLabelText('Search term')
+      .type('admin mcadmin');
+
+    cy.findByRole('button', { name: 'Search' }).click();
+
+    cy.url().should('include', '/search');
+
+    // Make sure the page has loaded before assertion
+    cy.findByRole('heading', { name: 'Test article' });
+    cy.get('.skip-content-link').should('not.be.visible');
+  });
+
+  it('Shows "Skip to content" if search is submitted by Enter press', () => {
+    cy.findByRole('search')
+      .findByLabelText('Search term')
       .type('admin mcadmin{enter}');
 
     cy.url().should('include', '/search');
-    cy.get('.skip-content-link').should('not.be.visible');
+    // Make sure the page has loaded before assertion
+    cy.findByRole('heading', { name: 'Test article' });
+
+    cy.get('.skip-content-link').should('be.visible');
+    cy.get('.skip-content-link').should('have.focus');
   });
 });
