9b25d8e9f5fb8492745ba3b66ca60ab6d0566d91
Suzanne Aitchison <suzanne@forem.com>
2021-08-12 14:35:20 +0100

    rough working version complete

--- app/javascript/crayons/navigation/MobileDrawerNavigation/MobileDrawerNavigation.jsx
+++ app/javascript/crayons/navigation/MobileDrawerNavigation/MobileDrawerNavigation.jsx
@@ -3,27 +3,52 @@ import { useState } from 'preact/hooks';
 import { MobileDrawer } from '@crayons/MobileDrawer';
 import { Button } from '@crayons/Button';
 
-// TODO:
-//  displays a heading, and a ... button
-// displays the drawer when open
+const OverflowIcon = () => (
+  <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M7 12a2 2 0 11-4 0 2 2 0 014 0zm7 0a2 2 0 11-4 0 2 2 0 014 0zm5 2a2 2 0 100-4 2 2 0 000 4z"
+    />
+  </svg>
+);
+
+const CheckIcon = () => (
+  <svg
+    aria-hidden="true"
+    className="check-icon"
+    fill="currentColor"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414 4.95 4.95z" />
+  </svg>
+);
+
 export const MobileDrawerNavigation = ({
   headingComponent,
   navigationTitle,
   navigationLinks,
 }) => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const currentPageIndex = navigationLinks.findIndex(
-    (item) => item.isCurrentPage,
-  );
+  const currentPage = navigationLinks.find((item) => item.isCurrentPage);
 
   const Heading = headingComponent;
 
-  //   TODO: add aria-current to the current page
   return (
     <Fragment>
       <div className="flex justify-between">
-        <Heading>Current url name</Heading>
-        <Button onClick={() => setIsDrawerOpen(true)}>Open me</Button>
+        <Heading>{currentPage.displayName}</Heading>
+        <Button
+          aria-label={navigationTitle}
+          icon={OverflowIcon}
+          size="s"
+          contentType="icon"
+          variant="ghost"
+          onClick={() => setIsDrawerOpen(true)}
+        />
       </div>
 
       {isDrawerOpen && (
@@ -31,12 +56,20 @@ export const MobileDrawerNavigation = ({
           title={navigationTitle}
           onClose={() => setIsDrawerOpen(false)}
         >
-          <nav aria-label={navigationTitle}>
+          <nav aria-label={navigationTitle} className="drawer-navigation">
             <ul className="list-none">
-              {navigationLinks.map((linkDetails, index) => (
-                <li key={`link-${linkDetails.url}`} className="py-2">
-                  <a href={linkDetails.url}>{linkDetails.displayName}</a>
-                  {currentPageIndex === index && '!!'}
+              {navigationLinks.map((linkDetails) => (
+                <li
+                  key={`link-${linkDetails.url}`}
+                  className="drawer-navigation__item py-2"
+                >
+                  <a
+                    href={linkDetails.url}
+                    aria-current={linkDetails.isCurrentPage ? 'page' : null}
+                  >
+                    {linkDetails.displayName}
+                  </a>
+                  <CheckIcon />
                 </li>
               ))}
             </ul>
--- app/javascript/crayons/navigation/MobileDrawerNavigation/__stories__/MobileDrawerNavigation.stories.jsx
+++ app/javascript/crayons/navigation/MobileDrawerNavigation/__stories__/MobileDrawerNavigation.stories.jsx
@@ -1,4 +1,4 @@
-import { h } from 'preact';
+import { h, Fragment } from 'preact';
 
 import { MobileDrawerNavigation } from '@crayons';
 
@@ -6,18 +6,31 @@ export default {
   title: 'App Components/MobileDrawerNavigation',
 };
 
-export const Default = () => (
-  <MobileDrawerNavigation
-    headingComponent="h2"
-    navigationTitle="Example MobileDrawerNavigation"
-    navigationLinks={[
-      { url: '/#', displayName: 'Example link 1', isCurrentPage: true },
-      { url: '/#', displayName: 'Example link 2' },
-      { url: '/#', displayName: 'Example link 3' },
-      { url: '/#', displayName: 'Example link 4' },
-    ]}
-  />
-);
+export const Default = () => {
+  return (
+    <Fragment>
+      <MobileDrawerNavigation
+        headingComponent="h2"
+        navigationTitle="Example MobileDrawerNavigation"
+        navigationLinks={[
+          {
+            url: window.location.href,
+            displayName: 'Drawer Navigation',
+            isCurrentPage: true,
+          },
+          { url: '/#', displayName: 'Example link 2' },
+          { url: '/#', displayName: 'Example link 3' },
+          { url: '/#', displayName: 'Example link 4' },
+        ]}
+      />
+      <p className="my-4">Click on the button to view navigation links.</p>
+      <p>
+        NB: As the component is only rendered on this page, "Drawer Navigation"
+        will always be the current page.
+      </p>
+    </Fragment>
+  );
+};
 
 Default.story = {
   name: 'MobileDrawerNavigation',
