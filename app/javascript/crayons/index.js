78a932315feda69238b8eed64f1efaff3097be31
Suzanne Aitchison <suzanne@forem.com>
2021-08-16 08:41:26 +0100

    Create crayons mobile drawer components (#14495)
    * add MobileDrawer component
    
    * WIP begin navigation component
    
    * rough working version complete
    
    * update story to allow for actual navigation
    
    * add component tests
    
    * add docs
    
    * add HTML variants
    
    * add a max width to the drawer

--- app/javascript/crayons/index.js
+++ app/javascript/crayons/index.js
@@ -4,3 +4,5 @@ export * from '@crayons/Dropdown';
 export * from '@crayons/formElements';
 export * from '@crayons/Modal';
 export * from '@crayons/Spinner';
+export * from '@crayons/MobileDrawer';
+export * from '@crayons/navigation';
