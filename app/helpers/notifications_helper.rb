b4100bef707582c9fa4e342ecf3832006be20126
ludwiczakpawel <ludwiczakpawel@gmail.com>
2021-08-17 21:29:21 +0200

    Replace EmojiOne with Twemoji (#14517)

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,11 +2,11 @@ module NotificationsHelper
   REACTION_IMAGES = {
     "like" => "heart-filled.svg",
     "unicorn" => "unicorn-filled.svg",
-    "hands" => "emoji/emoji-one-hands.png",
-    "thinking" => "emoji/emoji-one-thinking.png",
+    "hands" => "twemoji/hands.svg",
+    "thinking" => "twemoji/thinking.svg",
     "readinglist" => "save-filled.svg",
-    "thumbsdown" => "emoji/emoji-one-thumbs-down.png",
-    "vomit" => "emoji/emoji-one-nausea-face.png"
+    "thumbsdown" => "twemoji/thumb-down.svg",
+    "vomit" => "twemoji/suspicious.svg"
   }.freeze
 
   def reaction_image(category)
