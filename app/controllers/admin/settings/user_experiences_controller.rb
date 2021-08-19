dfc1934533c89093e1e83b2e8f1b73090eefcfa1
Arit Amana <msarit@gmail.com>
2021-08-19 18:53:31 -0400

    streamline controller edits

--- app/controllers/admin/settings/user_experiences_controller.rb
+++ app/controllers/admin/settings/user_experiences_controller.rb
@@ -1,6 +1,8 @@
 module Admin
   module Settings
     class UserExperiencesController < Admin::Settings::BaseController
+      private
+
       def authorization_resource
         ::Settings::UserExperience
       end
