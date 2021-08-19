02834ee9fb12be0083db7d07e5a519c346735f2e
Arit Amana <msarit@gmail.com>
2021-08-19 18:42:49 -0400

    complete rate-limit and social partials

--- app/controllers/admin/settings/rate_limits_controller.rb
+++ app/controllers/admin/settings/rate_limits_controller.rb
@@ -1,6 +1,11 @@
 module Admin
   module Settings
     class RateLimitsController < Admin::Settings::BaseController
+      private
+
+      def authorization_resource
+        ::Settings::RateLimit
+      end
     end
   end
 end
