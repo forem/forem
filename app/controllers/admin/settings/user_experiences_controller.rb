61f39c91daf5bb273331ba8893e1204cecd471b1
Arit Amana <msarit@gmail.com>
2021-08-19 18:49:52 -0400

    complete user-ex partial

--- app/controllers/admin/settings/user_experiences_controller.rb
+++ app/controllers/admin/settings/user_experiences_controller.rb
@@ -1,6 +1,9 @@
 module Admin
   module Settings
     class UserExperiencesController < Admin::Settings::BaseController
+      def authorization_resource
+        ::Settings::UserExperience
+      end
     end
   end
 end
