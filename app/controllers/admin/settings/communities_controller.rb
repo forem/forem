38f52be92819281c0b10168db3d3354ffc9b1b9c
Arit Amana <msarit@gmail.com>
2021-08-19 16:42:12 -0400

    complete communities partial async call 🎉

--- app/controllers/admin/settings/communities_controller.rb
+++ app/controllers/admin/settings/communities_controller.rb
@@ -1,6 +1,11 @@
 module Admin
   module Settings
     class CommunitiesController < Admin::Settings::BaseController
+      private
+
+      def authorization_resource
+        ::Settings::Community
+      end
     end
   end
 end
