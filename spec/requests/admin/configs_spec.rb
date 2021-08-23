be7f4ba0172a51d3ae71f06f9310c6cd6f713ea9
Arit Amana <msarit@gmail.com>
2021-08-23 09:29:18 -0400

    Fix failing request spec

--- spec/requests/admin/configs_spec.rb
+++ spec/requests/admin/configs_spec.rb
@@ -807,8 +807,8 @@ RSpec.describe "/admin/customization/config", type: :request do
         end
 
         it "unsets appropriate SMTP config, and apply default value if applicable" do
-          default_address = ApplicationConfig["SMTP_ADDRESS"].presence || nil
-          default_port = ApplicationConfig["SMTP_PORT"].presence.to_i || 25
+          default_address = ApplicationConfig["SMTP_ADDRESS"].presence
+          default_port = (ApplicationConfig["SMTP_PORT"].presence || 25).to_i
           expected_handle = { "address" => "", "port" => "" }
 
           post admin_settings_smtp_settings_path, params: {
