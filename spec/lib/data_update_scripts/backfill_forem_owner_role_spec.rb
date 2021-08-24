0cdd311bf967ab87100ea6da059ea50fc264e5ec
Nick Taylor <nick@dev.to>
2021-08-24 17:16:09 -0400

    Added data update script for backfilling forem owner role.

---
+++ spec/lib/data_update_scripts/backfill_forem_owner_role_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+require Rails.root.join(
+  "lib/data_update_scripts/20210824202334_backfill_forem_owner_role.rb",
+)
+
+describe DataUpdateScripts::BackfillForemOwnerRole do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
