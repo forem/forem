d3d77db0ed875baa9bbb9affb663c203aa5d7d00
Nick Taylor <nick@dev.to>
2021-08-25 10:07:54 -0400

    Added a guard for new Forem instance owners, i.e. empty DB.

--- lib/data_update_scripts/20210824202334_backfill_forem_owner_role.rb
+++ lib/data_update_scripts/20210824202334_backfill_forem_owner_role.rb
@@ -1,7 +1,7 @@
 module DataUpdateScripts
   class BackfillForemOwnerRole
     def run
-      User.with_role(:super_admin).first.add_role(:forem_owner)
+      User.with_role(:super_admin).first&.add_role(:forem_owner)
     end
   end
 end
