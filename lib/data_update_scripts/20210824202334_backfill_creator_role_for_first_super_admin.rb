fbfd0aab4b20f87809c7df225aef9ff24e83384f
Nick Taylor <nick@dev.to>
2021-08-25 16:36:14 -0400

    Some renaming cleanup

--- lib/data_update_scripts/20210824202334_backfill_creator_role_for_first_super_admin.rb
+++ lib/data_update_scripts/20210824202334_backfill_creator_role_for_first_super_admin.rb
@@ -1,5 +1,5 @@
 module DataUpdateScripts
-  class BackfillForemOwnerRole
+  class BackfillCreatorRoleForFirstSuperAdmin
     def run
       User.with_role(:super_admin).first&.add_role(:creator)
     end
--- spec/lib/data_update_scripts/backfill_creator_role_for_first_super_admin_spec.rb
+++ spec/lib/data_update_scripts/backfill_creator_role_for_first_super_admin_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 require Rails.root.join(
-  "lib/data_update_scripts/20210824202334_backfill_forem_owner_role.rb",
+  "lib/data_update_scripts/20210824202334_backfill_creator_role_for_first_super_admin",
 )
 
-describe DataUpdateScripts::BackfillForemOwnerRole do
+describe DataUpdateScripts::BackfillCreatorRoleForFirstSuperAdmin do
   let!(:owner) { create(:user, :super_admin) }
   let!(:admin) { create(:user, :super_admin) }
 
