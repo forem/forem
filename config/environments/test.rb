a84ea0084cee68b82ad95b8650d12d481df03fdb
Michael Kohl <citizen428@forem.com>
2021-08-26 11:27:20 +0700

    Add another entry to Bullet.safe_list

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -82,6 +82,7 @@ Rails.application.configure do
     Bullet.add_safelist(type: :unused_eager_loading, class_name: "Comment", association: :user)
     # @citizen428: We have not yet resolved all user - profile preloads related to profilge generalization
     Bullet.add_safelist(type: :n_plus_one_query, class_name: "User", association: :profile)
+    Bullet.add_safelist(type: :n_plus_one_query, class_name: "Profile", association: :user)
     Bullet.add_safelist(type: :n_plus_one_query, class_name: "User", association: :setting)
     Bullet.add_safelist(type: :n_plus_one_query, class_name: "User", association: :notification_setting)
     # @mstruve: These occur during setting updates, not sure how since we are only dealing with single setting records
