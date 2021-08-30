a8854830595f00f64278a802aa6952fe20239d4b
Michael Kohl <citizen428@dev.to>
2021-08-30 10:27:08 +0700

    Introduce `Settings::Base` (#14138)
    * Introduce Settings::Base
    
    * Temporarily re-add gem
    
    * Update Settings::Base to use RequestStore
    
    * Add after_commit hook
    
    * Restructure code
    
    * Finish specs, rename field -> setting
    
    * Update comment
    
    * Undo unrelated change
    
    * Remove redundant get_default
    
    * Fix Doorkeeper issue
    
    * Update docs/backend/configuration.md
    
    Co-authored-by: Josh Puetz <josh@dev.to>
    
    * Clarify comment and documentation
    
    * Update vendored gems
    
    * Fix nil-handling bug in Settings::Base
    
    * Remove accidentaly changes from Gemfile.lock
    
    * Update vendored gems
    
    Co-authored-by: Josh Puetz <josh@dev.to>

--- app/models/settings/user_experience.rb
+++ app/models/settings/user_experience.rb
@@ -1,21 +1,17 @@
 module Settings
   # Basic UX settings that can be overriden by individual user preferences.
-  class UserExperience < RailsSettings::Base
+  class UserExperience < Base
     self.table_name = :settings_user_experiences
 
     HEX_COLOR_REGEX = /\A#(\h{6}|\h{3})\z/
 
-    # The configuration is cached, change this if you want to force update
-    # the cache, or call Settings::UserExperience.clear_cache
-    cache_prefix { "v1" }
-
     # The default font for all users that have not chosen a custom font yet
-    field :default_font, type: :string, default: "sans_serif"
-    field :feed_strategy, type: :string, default: "basic"
+    setting :default_font, type: :string, default: "sans_serif"
+    setting :feed_strategy, type: :string, default: "basic"
     # basic (current default), rich (cover image on all posts), compact (more minimal)
-    field :feed_style, type: :string, default: "basic"
-    field :home_feed_minimum_score, type: :integer, default: 0
-    field :primary_brand_color_hex, type: :string, default: "#3b49df", validates: {
+    setting :feed_style, type: :string, default: "basic"
+    setting :home_feed_minimum_score, type: :integer, default: 0
+    setting :primary_brand_color_hex, type: :string, default: "#3b49df", validates: {
       format: {
         with: HEX_COLOR_REGEX,
         message: "must be be a 3 or 6 character hex (starting with #)"
@@ -24,7 +20,7 @@ module Settings
     }
     # a non-public forem will redirect all unauthenticated pages to the registration page.
     # a public forem could have more fine-grained authentication (listings ar private etc.) in future
-    field :public, type: :boolean, default: 0
-    field :tag_feed_minimum_score, type: :integer, default: 0
+    setting :public, type: :boolean, default: 0
+    setting :tag_feed_minimum_score, type: :integer, default: 0
   end
 end
