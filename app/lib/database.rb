8d0b1294549beee343e0f66312336a7c2d79c5d5
Dan Uber <dan@forem.com>
2021-08-31 11:18:46 -0500

    Cache tables which are known to exist
    Only check again for negative results.

--- app/lib/database.rb
+++ app/lib/database.rb
@@ -1,13 +1,19 @@
-# Database related utility functions
-module Database
+# Database related utility functionsc
+class Database
   # Checks if the database is ready and the specified table exists. This can be
   # useful during bin/setup and similar tasks. It also works if the connection
   # hasn't been established yet.
   #
   # @param table [String] the name of the table to check for
   def self.table_available?(table)
-    ActiveRecord::Base.connection.table_exists?(table)
+    available_tables[table] ||=
+      ActiveRecord::Base.connection.table_exists?(table)
   rescue ActiveRecord::NoDatabaseError, ActiveRecord::ConnectionNotEstablished
     false
   end
+
+  def self.available_tables
+    @available_tables ||= {}
+  end
+  private_class_method :available_tables
 end
