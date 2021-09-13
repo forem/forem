e4b79d9b536e931b7c22502dd107949cff16e72a
Andy Z <17884966+Zhao-Andy@users.noreply.github.com>
2021-09-13 13:23:35 -0400

    Use regex to replace with https

--- app/services/users/safe_remote_profile_image_url.rb
+++ app/services/users/safe_remote_profile_image_url.rb
@@ -2,9 +2,11 @@ module Users
   module SafeRemoteProfileImageUrl
     # Basic check for nil and blank URLs, alongside likely incomplete URLs, such as just "image.jpg".
     def self.call(url)
-      return url if url.to_s.start_with?("https")
-
-      Users::ProfileImageGenerator.call
+      if url.match?(%r{https?://})
+        url[0..6].gsub("http://", "https://") + url[7..]
+      else
+        Users::ProfileImageGenerator.call
+      end
     end
   end
 end
