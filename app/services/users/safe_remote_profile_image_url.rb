2adae6a4a81815d839b967f91278e31260d7b8d1
Andy Z <17884966+Zhao-Andy@users.noreply.github.com>
2021-09-13 13:55:15 -0400

    Refactor a bit

--- app/services/users/safe_remote_profile_image_url.rb
+++ app/services/users/safe_remote_profile_image_url.rb
@@ -2,11 +2,9 @@ module Users
   module SafeRemoteProfileImageUrl
     # Basic check for nil and blank URLs, alongside likely incomplete URLs, such as just "image.jpg".
     def self.call(url)
-      if url.match?(%r{https?://})
-        url[0..6].gsub("http://", "https://") + url[7..]
-      else
-        Users::ProfileImageGenerator.call
-      end
+      url.gsub!("http://", "https://") if url.match?(%r{https?://})
+
+      url.presence || Users::ProfileImageGenerator.call
     end
   end
 end
