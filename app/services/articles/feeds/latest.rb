ba679de2e53c69f518eade005c00d993703cc42d
Michael Kohl <citizen428@forem.com>
2021-09-14 13:32:05 +0700

    Move constant to correct location

--- app/services/articles/feeds/latest.rb
+++ app/services/articles/feeds/latest.rb
@@ -1,8 +1,8 @@
 module Articles
   module Feeds
-    MINIMUM_SCORE = -20
-
     module Latest
+      MINIMUM_SCORE = -20
+
       def self.call(tag: nil, number_of_articles: 50, page: 1)
         Articles::Feeds::Tag.call(tag)
           .order(published_at: :desc)
