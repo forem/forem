85b2ec0529cdb1b27c0e328eda91ae5a7d377863
Suzanne Aitchison <suzanne@forem.com>
2021-09-14 14:53:38 +0100

    set jsdom env, fix setImmediate issue

--- testSetup.js
+++ testSetup.js
@@ -2,6 +2,8 @@
 import 'jest-axe/extend-expect';
 import './app/assets/javascripts/lib/xss';
 
+global.setImmediate = global.setTimeout;
+
 process.on('unhandledRejection', (error) => {
   // Errors thrown here are typically fetch responses that have not been mocked.
   throw error;
