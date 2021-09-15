a19edf858a155bd36e4395395da66cf2f26a0f9f
Alex <alexandersmith223@gmail.com>
2021-09-15 13:34:31 -0400

    Add display_in_directory setting (#14733)
    * Add display_in_directory setting
    
    * Remove setting from constants
    
    * Update spec to use change

--- app/controllers/api/v0/instances_controller.rb
+++ app/controllers/api/v0/instances_controller.rb
@@ -8,6 +8,7 @@ module Api
           context: ApplicationConfig["FOREM_CONTEXT"],
           cover_image_url: Settings::General.main_social_image,
           description: Settings::Community.community_description,
+          display_in_directory: Settings::UserExperience.display_in_directory,
           logo_image_url: Settings::General.logo_png,
           name: Settings::Community.community_name,
           registered_users_count: User.registered.estimated_count,
