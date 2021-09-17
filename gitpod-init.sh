b83706ab0b0803d5b1f972fe6b1cf453c7effe00
Nick Taylor <nick@forem.com>
2021-09-17 15:18:21 -0400

    Added GitHub CLI to Gitpod. (#14763)

--- gitpod-init.sh
+++ gitpod-init.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Installing the GitHub CLI"
+brew install gh
+
 cp .env_sample .env
 echo "APP_DOMAIN=\"$(gp url 3000 | cut -f 3 -d /)\"" >> .env
 echo 'APP_PROTOCOL="https://"' >> .env
