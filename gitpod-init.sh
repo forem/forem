2f70d562f5ab30355730927751d52ec8f2abe615
Nick Taylor <nick@dev.to>
2021-09-18 18:45:10 -0400

    Pretty sure GEM_HOME will persist now.

--- gitpod-init.sh
+++ gitpod-init.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+gp env GEM_HOME=/workspace/.rvm
+eval $(gp env -e)
+
 cp .env_sample .env
 echo "APP_DOMAIN=\"$(gp url 3000 | cut -f 3 -d /)\"" >> .env
 echo 'APP_PROTOCOL="https://"' >> .env
