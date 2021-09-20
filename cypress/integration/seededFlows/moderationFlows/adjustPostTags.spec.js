469707ccf76e2009097640b993469da1b6743564
Suzanne Aitchison <suzanne@forem.com>
2021-09-20 13:49:56 +0100

    fix error adding removing tags from article mod tools

--- cypress/integration/seededFlows/moderationFlows/adjustPostTags.spec.js
+++ cypress/integration/seededFlows/moderationFlows/adjustPostTags.spec.js
@@ -94,4 +94,54 @@ describe('Adjust post tags', () => {
       });
     });
   });
+
+  describe('from article page', () => {
+    beforeEach(() => {
+      cy.testSetup();
+      cy.fixture('users/adminUser.json').as('user');
+
+      cy.get('@user').then((user) => {
+        cy.loginAndVisit(user, '/admin_mcadmin/tag-test-article');
+      });
+    });
+
+    it('should add a tag to a post', () => {
+      cy.findByRole('heading', { name: 'Tag test article' });
+      cy.findByRole('main').as('main');
+
+      cy.findByRole('link', { name: '#tag2' }).should('not.exist');
+
+      cy.findByRole('button', { name: 'Moderation' }).click();
+      cy.getIframeBody('#mod-container').within(() => {
+        cy.findByRole('button', { name: 'Open adjust tags section' }).click();
+        cy.findByPlaceholderText('Add a tag').type('tag2');
+        cy.findByPlaceholderText('Reason for tag adjustment').type('testing');
+        cy.findByRole('button', { name: 'Submit' }).click();
+      });
+
+      cy.get('@main').within(() => {
+        cy.findByRole('link', { name: '#tag2' });
+      });
+    });
+
+    it('should remove a tag from a post', () => {
+      cy.findByRole('heading', { name: 'Tag test article' });
+      cy.findByRole('main').as('main');
+
+      cy.findByRole('link', { name: '#tag1' }).should('exist');
+      cy.findByRole('button', { name: 'Moderation' }).click();
+
+      cy.getIframeBody('#mod-container').within(() => {
+        cy.findByRole('button', { name: 'Open adjust tags section' }).click();
+        cy.findByPlaceholderText('Reason for tag adjustment').type('testing');
+        cy.findByRole('button', { name: '#tag1 Remove tag' }).click();
+
+        cy.findByRole('button', { name: 'Submit' }).click();
+      });
+
+      cy.get('@main').within(() => {
+        cy.findByRole('link', { name: '#tag1' }).should('not.exist');
+      });
+    });
+  });
 });
