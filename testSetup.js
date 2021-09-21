b7d63a6e8d85d16ae863fff0d801b65ca1d9c7a3
Suzanne Aitchison <suzanne@forem.com>
2021-09-21 10:52:54 +0100

    workaround getComputedStyle issues in all jest tests (#14737)

--- testSetup.js
+++ testSetup.js
@@ -4,6 +4,10 @@ import './app/assets/javascripts/lib/xss';
 
 global.setImmediate = global.setTimeout;
 
+// TODO: Remove this once https://github.com/nickcolley/jest-axe/issues/147 is fixed.
+const { getComputedStyle } = window;
+window.getComputedStyle = (elt) => getComputedStyle(elt);
+
 process.on('unhandledRejection', (error) => {
   // Errors thrown here are typically fetch responses that have not been mocked.
   throw error;
