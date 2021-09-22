4b216931e8ebe5fb5c18a5f3c2ed4f255db68780
Michael Kohl <citizen428@forem.com>
2021-09-22 15:01:52 +0700

    Remove unrelated change, figure out why > 1 later

--- app/services/broadcasts/welcome_notification/generator.rb
+++ app/services/broadcasts/welcome_notification/generator.rb
@@ -99,7 +99,7 @@ module Broadcasts
       end
 
       def user_following_tags?
-        user.cached_followed_tag_names.any?
+        user.cached_followed_tag_names > 1
       end
 
       def welcome_broadcast
