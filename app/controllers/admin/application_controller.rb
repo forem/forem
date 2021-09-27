7ff027d8825c87e77bda2702b9fcf253657cacae
Michael Kohl <citizen428@forem.com>
2021-09-27 14:18:53 +0700

    Remove admin help icon

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,23 +1,7 @@
 module Admin
   class ApplicationController < ApplicationController
     before_action :authorize_admin
-    before_action :assign_help_url
     after_action :verify_authorized
-
-    HELP_URLS = {
-      badges: "https://admin.forem.com/docs/admin/badges",
-      badge_achievements: "https://admin.forem.com/docs/admin/badges",
-      configs: "https://admin.forem.com/docs/admin/config/README",
-      navigation_links: "https://admin.forem.com/docs/admin/navigation-links",
-      pages: "https://admin.forem.com/docs/admin/pages",
-      podcasts: "https://admin.forem.com/docs/admin/podcasts",
-      reports: "https://admin.forem.com/docs/admin/reports",
-      users: "https://admin.forem.com/docs/admin/users/README",
-      html_variants: "https://admin.forem.com/docs/admin/html-variants",
-      display_ads: "https://admin.forem.com/docs/admin/display-ads",
-      chat_channels: "https://admin.forem.com/docs/admin/chat-channels",
-      tags: "https://admin.forem.com/docs/admin/tags"
-    }.freeze
 
     protected
 
@@ -27,10 +11,6 @@ module Admin
 
     def authorize_admin
       authorize(authorization_resource, :access?, policy_class: InternalPolicy)
-    end
-
-    def assign_help_url
-      @help_url = HELP_URLS[controller_name.to_sym]
     end
   end
 end
