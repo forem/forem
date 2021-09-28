80c8e49a859b9d4112f2686716da4694873d80e5
praveen raghav ns <22149498+praveen-raghav-ns@users.noreply.github.com>
2021-09-28 20:00:56 +0530

    Use constants for controller allowed params (#14815)
    * Constants for controller allowed params
    
    * Apply suggestions from code review
    
    Co-authored-by: Michael Kohl <me@citizen428.net>
    
    Co-authored-by: Michael Kohl <me@citizen428.net>

--- app/controllers/users/notification_settings_controller.rb
+++ app/controllers/users/notification_settings_controller.rb
@@ -19,6 +19,7 @@ module Users
                         mod_roundrobin_notifications
                         reaction_notifications
                         welcome_notifications].freeze
+    ONBOARDING_ALLOWED_PARAMS = %i[email_newsletter email_digest_periodic].freeze
 
     def update
       authorize current_user, policy_class: UserPolicy
@@ -37,8 +38,7 @@ module Users
       authorize User
 
       if params[:notifications]
-        permitted_params = %i[email_newsletter email_digest_periodic]
-        current_user.notification_setting.assign_attributes(params[:notifications].permit(permitted_params))
+        current_user.notification_setting.assign_attributes(params[:notifications].permit(ONBOARDING_ALLOWED_PARAMS))
       end
 
       current_user.saw_onboarding = true
