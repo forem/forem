bd7a7bca5caf3ba28b6993a5f94d4d82c99aa319
Ben Halpern <bendhalpern@gmail.com>
2021-09-28 08:36:54 -0400

    Update app/helpers/display_ad_helper.rb
    Co-authored-by: Michael Kohl <citizen428@forem.com>

--- app/helpers/display_ad_helper.rb
+++ app/helpers/display_ad_helper.rb
@@ -1,6 +1,5 @@
 module DisplayAdHelper
   def display_ads_placement_area_options_array
-    DisplayAd::ALLOWED_PLACEMENT_AREAS.map
-      .with_index { |area, index| [DisplayAd::ALLOWED_PLACEMENT_AREAS_HUMAN_READABLE[index], area] }
+    DisplayAd::ALLOWED_PLACEMENT_AREAS_HUMAN_READABLE.zip(DisplayAd::ALLOWED_PLACEMENT_AREAS)      
   end
 end
