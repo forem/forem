a60fba4c2c1bffefc41ecae8aaff8a7d36b2ac05
Julianna Tetreault <juliannatetreault@gmail.com>
2021-09-29 12:24:53 -0600

    Adds styling to new.html.erb and slightly adjusts CreatorSettings#create

--- app/controllers/admin/creator_settings_controller.rb
+++ app/controllers/admin/creator_settings_controller.rb
@@ -7,16 +7,16 @@ module Admin
     def new; end
 
     def create
-      current_user.update!(saw_onboarding: true)
       ::Settings::Community.community_name = settings_params[:community_name]
       ::Settings::General.logo_svg = settings_params[:logo_svg]
       ::Settings::UserExperience.primary_brand_color_hex = settings_params[:primary_brand_color_hex]
       ::Settings::Authentication.invite_only_mode = settings_params[:invite_only]
       ::Settings::UserExperience.public = settings_params[:public]
+      current_user.update!(saw_onboarding: true)
       redirect_to root_path
     rescue StandardError => e
       flash[:error] = e.message
-      redirect_to new_admin_creator_setting_path
+      render new_admin_creator_setting_path
     end
 
     private
