098fd3815a57e50e3b7ab5b57acc2ec3a36e1066
Ridhwana <ridhwana.khan16@gmail.com>
2021-09-30 15:48:40 +0200

    override the method

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -2,8 +2,21 @@ class ConfirmationsController < Devise::ConfirmationsController
   FLASH_MESSAGE = "Email sent! Please contact support at %<email>s if you are "\
                   "having trouble receiving your confirmation instructions.".freeze
 
+  # GET /resource/confirmation?confirmation_token=abcdef
   def show
-    super { sign_in(resource) if resource.errors.empty? && resource.creator? }
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+      if resource.creator?
+        sign_in(resource)
+      else
+        respond_with_navigational(resource) { redirect_to after_confirmation_path_for(resource_name, resource) }
+      end
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity) { render :new }
+    end
   end
 
   def create
