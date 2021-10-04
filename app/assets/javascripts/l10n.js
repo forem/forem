f91f522101d44e36649364f1212222c6ca371110
Yhëhtozr <conlang2012@outlook.com>
2021-10-05 07:43:27 +0900

    [CP] asset js i18n fix uninitialized keys

--- app/assets/javascripts/l10n.js
+++ app/assets/javascripts/l10n.js
@@ -2,18 +2,21 @@
 
 const locale = document.documentElement.lang;
 
-(async () => {
-  let dictionary = await (
-    await fetch(`/javascripts/i18n/${locale}.json`)
-  ).json();
+// we need a sync XHR to ensure the locale file loaded
+let dicRequest = new XMLHttpRequest();
+dicRequest.open('GET', `/javascripts/i18n/${locale}.json`, false);
+dicRequest.send();
 
-  i18next.init({
-    lng: locale,
-    resources: {
-      [locale]: {
-        translation: dictionary,
-      },
+let dictionary = JSON.parse(
+  dicRequest.status === 200 ? dicRequest.responseText : '{}',
+);
+
+i18next.init({
+  lng: locale,
+  resources: {
+    [locale]: {
+      translation: dictionary,
     },
-    interpolation: { prefix: '%{', suffix: '}' },
-  });
-})();
+  },
+  interpolation: { prefix: '%{', suffix: '}' },
+});
