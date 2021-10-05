5cc09767c5df8489b36d33e4bdc9330e253c885c
Ben Halpern <bendhalpern@gmail.com>
2021-10-05 10:11:41 -0400

    Add conditional autovomits to user name and improve user score recalc (#14858)
    * Add conditional autovomits to user name and improve user score recalc
    
    * Remove puts
    
    * Add missing ||
    
    * Fix tests
    
    * Update app/views/users/show.html.erb
    
    Co-authored-by: Michael Kohl <citizen428@forem.com>
    
    * Update app/models/user.rb
    
    Co-authored-by: Michael Kohl <citizen428@forem.com>
    
    * Add in-line score calc principles
    
    Co-authored-by: Michael Kohl <citizen428@forem.com>

--- app/workers/reactions/update_relevant_scores_worker.rb
+++ app/workers/reactions/update_relevant_scores_worker.rb
@@ -6,14 +6,19 @@ module Reactions
 
     def perform(reaction_id)
       reaction = Reaction.find_by(id: reaction_id)
-      return unless reaction&.reactable
+      reactable = reaction&.reactable
+      return unless reactable
 
-      reaction.reactable.touch_by_reaction if reaction.reactable.respond_to?(:touch_by_reaction)
+
+      reactable.touch_by_reaction if reactable.respond_to?(:touch_by_reaction)
       if reaction.reactable.respond_to?(:sync_reactions_count)
         ThrottledCall.perform(:sync_reactions_count, throttle_for: 15.minutes) do
-          reaction.reactable.sync_reactions_count
+          reactable.sync_reactions_count
         end
       end
+
+      reactable.calculate_score if reaction.reactable_type == "User"
+
       return unless reaction.reactable_type == "Article" && Reaction::PUBLIC_CATEGORIES.include?(reaction.category)
 
       Follows::UpdatePointsWorker.perform_async(reaction.reactable_id, reaction.user_id)
