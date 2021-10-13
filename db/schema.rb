53b3ae225097fdf94e76cac4b5fd4551c70550f0
Jamie Gaskins <jgaskins@hey.com>
2021-10-13 13:05:12 -0400

    Add PGHero for more insights into the DB
    Andrew Kane's Ruby gems will continue being added until morale improves!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_08_170433) do
+ActiveRecord::Schema.define(version: 2021_10_13_153230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -856,6 +856,17 @@ ActiveRecord::Schema.define(version: 2021_10_08_170433) do
     t.string "title"
     t.datetime "updated_at", null: false
     t.index ["slug"], name: "index_pages_on_slug", unique: true
+  end
+
+  create_table "pghero_query_stats", force: :cascade do |t|
+    t.bigint "calls"
+    t.datetime "captured_at"
+    t.text "database"
+    t.text "query"
+    t.bigint "query_hash"
+    t.float "total_time"
+    t.text "user"
+    t.index ["database", "captured_at"], name: "index_pghero_query_stats_on_database_and_captured_at"
   end
 
   create_table "podcast_episode_appearances", force: :cascade do |t|
