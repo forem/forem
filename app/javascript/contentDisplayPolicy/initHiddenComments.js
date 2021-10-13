1d579738ed8600b5ea8406f0fa27a775d1bf42e0
lightalloy <lightallloy@gmail.com>
2021-10-13 15:09:01 +0300

    Removed unused code

--- app/javascript/contentDisplayPolicy/initHiddenComments.js
+++ app/javascript/contentDisplayPolicy/initHiddenComments.js
@@ -1,31 +1,5 @@
 /* eslint-disable no-alert */
 export function initHiddenComments() {
-  function hide(commentId) {
-    const confirmMsg = `
-Are you sure you want to hide this comment? It will become hidden in your post, but will still be visible via the comment's permalink.
-
-All child comments in this thread will also be hidden.
-
-For further actions, you may consider blocking this person and/or reporting abuse.
-    `;
-    const confirmHide = window.confirm(confirmMsg);
-    if (confirmHide) {
-      fetch(`/comments/${commentId}/hide`, {
-        method: 'PATCH',
-        headers: {
-          'X-CSRF-Token': window.csrfToken,
-        },
-      })
-        .then((response) => response.json())
-        .then((response) => {
-          if (response.hidden === 'true') {
-            /* eslint-disable-next-line no-restricted-globals */
-            location.reload();
-          }
-        });
-    }
-  }
-
   function unhide(commentId) {
     fetch(`/comments/${commentId}/unhide`, {
       method: 'PATCH',
@@ -43,10 +17,8 @@ For further actions, you may consider blocking this person and/or reporting abus
   }
 
   function showHideCommentsModal(commentId) {
-    const form = document.getElementById("hide-comments-modal__form");
+    const form = document.getElementById('hide-comments-modal__form');
     form.action = `/comments/${commentId}/hide`;
-    // const comment_id_input = form.getElementsByClassName("hide_comment_id")[0];
-    // comment_id_input.value = commentId;
     window.Forem.showModal({
       title: 'Confirm hiding the comment',
       contentSelector: '#hide-comments-modal',
@@ -62,7 +34,6 @@ For further actions, you may consider blocking this person and/or reporting abus
       link.addEventListener('click', (e) => {
         e.preventDefault();
         showHideCommentsModal(commentId);
-        // hide(commentId);
       });
     } else if (hideType === 'unhide') {
       link.addEventListener('click', () => {
