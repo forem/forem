60357f070c1ab47e1f08fe480fa66f068835f0a5
Julianna Tetreault <juliannatetreault@gmail.com>
2021-10-14 16:19:47 -0600

    Consolidate expectations with creatorSettings E2E test

--- cypress/integration/creatorOnboardingFlows/creatorSettings.spec.js
+++ cypress/integration/creatorOnboardingFlows/creatorSettings.spec.js
@@ -10,62 +10,54 @@ describe('Creator Setup Page', () => {
     cy.visit(`${baseUrl}admin/creator_settings/new?referrer=${baseUrl}`);
   });
 
-  it('should display a welcome message', () => {
+  it('should submit the creator settings', () => {
+    // should display a welcome message
     cy.findByText("Lovely! Let's set up your Forem.").should('be.visible');
     cy.findByText('No stress, you can always change it later.').should(
       'be.visible',
     );
-  });
 
-  it('should contain a community name and update the field properly', () => {
+    // should contain a community name and update the field properly
     cy.findByRole('textbox', { name: /community name/i })
       .as('communityName')
       .invoke('attr', 'placeholder')
       .should('eq', 'Climbing Life');
     cy.get('@communityName').type('Climbing Life');
-  });
 
-  it('should contain a logo upload field and upload a logo upon click', () => {
+    // should contain a logo upload field and upload a logo upon click
     cy.findByText(/^Logo/).should('be.visible');
     cy.findByRole('button', { name: /logo/i }).attachFile(
       '/images/admin-image.png',
     );
-  });
 
-  it('should contain a brand color selector field', () => {
+    // should contain a brand color selector field
     cy.findByText(/^Brand color/).should('be.visible');
-  });
 
-  // TODO: Circle back around to testing the selection of a brand color from the color picker,
-  // as this input isn't very testable at the moment. See https://github.com/cypress-io/cypress/issues/7812.
-
-  it.skip('should allow a brand color to be selected upon click', () => {
-    cy.findByRole('color', { name: 'primary_brand_color_hex' }).click();
-  });
-
-  it("should contain a 'Who can join this community?' radio selector field and allow selection upon click", () => {
+    // should contain a 'Who can join this community?' radio selector field and allow selection upon click
     cy.findByText(/^Who can join this community/).should('be.visible');
     cy.findAllByRole('radio').first().check('0');
     cy.findAllByRole('radio').should('be.checked');
-  });
 
-  it("should contain a 'Who can view content in this community?' radio selector field and allow selection upon click", () => {
+    // should contain a 'Who can view content in this community?' radio selector field and allow selection upon click
     cy.findByText(/^Who can view content in this community/).should(
       'be.visible',
     );
     cy.findAllByRole('radio').check('0');
     cy.findAllByRole('radio').should('be.checked');
-  });
 
-  it("should redirect the creator to the home page when the form is completely filled out and 'Finish' is clicked", () => {
+    // should should contain a logo upload field and upload a logo upon click
     cy.findByRole('textbox', { name: /community name/i }).as('communityName');
     cy.get('@communityName').type('Climbing Life');
     cy.findByRole('button', { name: /logo/i }).attachFile(
       '/images/admin-image.png',
     );
+
+    // TODO: Circle back around to testing the selection of a brand color from the color picker,
+    // as this input isn't very testable at the moment. See https://github.com/cypress-io/cypress/issues/7812.
     cy.findByLabelText('Brand color').invoke('attr', 'value', '#ff0000');
     cy.findAllByRole('radio').first().check('0');
     cy.findAllByRole('radio').check('0');
+    // should redirect the creator to the home page when the form is completely filled out and 'Finish' is clicked
     cy.findByRole('button', { name: 'Finish' }).click();
     cy.url().should('equal', '/');
   });
