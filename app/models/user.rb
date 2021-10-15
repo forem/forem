3616c03206365589e6ea7e65c952275cdd59d878
Julianna Tetreault <juliannatetreault@gmail.com>
2021-10-15 11:04:03 -0600

    Removes duplicate #creator? method from User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -371,10 +371,6 @@ class User < ApplicationRecord
     has_role?(:tech_admin) || has_role?(:super_admin)
   end
 
-  def creator?
-    has_role?(:creator)
-  end
-
   def vomitted_on?
     Reaction.exists?(reactable_id: id, reactable_type: "User", category: "vomit", status: "confirmed")
   end
