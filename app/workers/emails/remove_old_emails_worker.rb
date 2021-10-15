820dd9b5a6e76c74eba7b63f56cb96545a4f6909
Jamie Gaskins <jamie@forem.com>
2021-10-15 11:53:55 -0400

    Allow `Emails::RemoveOldEmailsWorker` to run longer (#15085)
    * Allow Emails::RemoveOldEmailsWorker to run longer
    
    We set the SQL statement timeout based on the STATEMENT_TIMEOUT
    environment variable (defaulting to 2.5 seconds in production), but not
    all SQL queries are created equal. Since some may take longer out of
    necessity, this PR introduces a `Model.with_statement_timeout` method
    that allows you to change the SQL statement timeout only for a given
    block.
    
    * EmailMessage doesn't inherit ApplicationRecord
    
    It inherits from `Ahoy::Message`, which inherits directly from
    `ActiveRecord::Base`.
    
    [3] pry(main)> EmailMessage.ancestors
    => [EmailMessage(id: integer, clicked_at: datetime, content: text, feedback_message_id: integer, mailer: string, sent_at: datetime, subject: text, to: text, token: string, user_id: integer, user_type: string, utm_campaign: string, utm_content: string, utm_medium: string, utm_source: string, utm_term: string),
     EmailMessage::GeneratedAssociationMethods,
     EmailMessage::GeneratedAttributeMethods,
     Ahoy::Message(id: integer, clicked_at: datetime, content: text, feedback_message_id: integer, mailer: string, sent_at: datetime, subject: text, to: text, token: string, user_id: integer, user_type: string, utm_campaign: string, utm_content: string, utm_medium: string, utm_source: string, utm_term: string),
     Kaminari::ConfigurationMethods,
     Kaminari::ActiveRecordModelExtension,
     Ahoy::Message::GeneratedAssociationMethods,
     Ahoy::Message::GeneratedAttributeMethods,
     Bullet::SaveWithBulletSupport,
     ActiveRecord::Base,
     ...
    
    * Use milliseconds to match STATEMENT_TIMEOUT units

--- app/workers/emails/remove_old_emails_worker.rb
+++ app/workers/emails/remove_old_emails_worker.rb
@@ -4,8 +4,12 @@ module Emails
 
     sidekiq_options queue: :low_priority, retry: 10
 
+    STATEMENT_TIMEOUT = ENV.fetch("STATEMENT_TIMEOUT_REMOVE_OLD_EMAILS", 10_000).to_i.seconds / 1_000.to_f
+
     def perform
-      EmailMessage.fast_destroy_old_retained_email_deliveries
+      ApplicationRecord.with_statement_timeout STATEMENT_TIMEOUT do
+        EmailMessage.fast_destroy_old_retained_email_deliveries
+      end
     end
   end
 end
