1ab2ddad0af767f8a7e882bd8321338116acf009
Jamie Gaskins <jgaskins@hey.com>
2021-10-15 10:00:10 -0400

    Allow Emails::RemoveOldEmailsWorker to run longer
    We set the SQL statement timeout based on the STATEMENT_TIMEOUT
    environment variable (defaulting to 2.5 seconds in production), but not
    all SQL queries are created equal. Since some may take longer out of
    necessity, this PR introduces a `Model.with_statement_timeout` method
    that allows you to change the SQL statement timeout only for a given
    block.

--- app/workers/emails/remove_old_emails_worker.rb
+++ app/workers/emails/remove_old_emails_worker.rb
@@ -4,8 +4,12 @@ module Emails
 
     sidekiq_options queue: :low_priority, retry: 10
 
+    STATEMENT_TIMEOUT = ENV.fetch("STATEMENT_TIMEOUT_REMOVE_OLD_EMAILS", 10).to_i.seconds
+
     def perform
-      EmailMessage.fast_destroy_old_retained_email_deliveries
+      EmailMessage.with_statement_timeout STATEMENT_TIMEOUT do
+        EmailMessage.fast_destroy_old_retained_email_deliveries
+      end
     end
   end
 end
