4a1ce2f05351e919753ceeb9c0d6bf98dbcdd120
Jamie Gaskins <jgaskins@hey.com>
2021-10-15 14:11:36 -0400

    Remove txn from `with_statement_timeout` block
    Turns out this caused a *different* problem. It would work for the first
    delete batch in `BulkSqlDelete`, but subsequent `DELETE` queries would
    then get their `statement_timeout`s reset to the global setting. The
    issue was that `BulkSqlDelete` uses methods that don't respect nested
    transactions, and we were already resetting the value of the setting to
    its previous value anyway, so there was really no reason to run it
    inside a transaction.
    
    The only failure mode I know of here is that the `ensure` block breaks
    because `connection` is severed. In that case, the value is reset anyway
    when the connection pool replaces the connection so the failure does not
    propagate.

--- app/workers/emails/remove_old_emails_worker.rb
+++ app/workers/emails/remove_old_emails_worker.rb
@@ -4,12 +4,8 @@ module Emails
 
     sidekiq_options queue: :low_priority, retry: 10
 
-    STATEMENT_TIMEOUT = ENV.fetch("STATEMENT_TIMEOUT_REMOVE_OLD_EMAILS", 10_000).to_i.seconds / 1_000.to_f
-
     def perform
-      ApplicationRecord.with_statement_timeout STATEMENT_TIMEOUT do
-        EmailMessage.fast_destroy_old_retained_email_deliveries
-      end
+      EmailMessage.fast_destroy_old_retained_email_deliveries
     end
   end
 end
