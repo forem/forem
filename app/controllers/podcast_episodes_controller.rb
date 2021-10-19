2ad9fee49493cd10333cb6d1d04ae5840afb463c
Paweł Ludwiczak <ludwiczakpawel@gmail.com>
2021-10-19 21:21:19 +0200

    browse is now a bit nicer

--- app/controllers/podcast_episodes_controller.rb
+++ app/controllers/podcast_episodes_controller.rb
@@ -9,7 +9,7 @@ class PodcastEpisodesController < ApplicationController
     @more_podcasts = Podcast.available.order(title: :asc)
     @podcast_episodes = PodcastEpisodeDecorator.decorate_collection(PodcastEpisode
       .available
-      .includes(:podcast).order(published_at: :desc).limit(5))
+      .includes(:podcast).order(published_at: :desc).limit(6))
 
     if params[:q].blank?
       surrogate_keys = ["podcast_episodes_all"] + @podcast_episodes.map(&:record_key)
