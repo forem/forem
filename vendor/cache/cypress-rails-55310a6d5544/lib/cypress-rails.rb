63b69009313b881b718850a259af62f8d0cfb0a2
Mac Siri <krairit.siri@gmail.com>
2021-10-21 17:32:46 -0400

    Upgrade custom cypress-rails

--- vendor/cache/cypress-rails-55310a6d5544/lib/cypress-rails.rb
+++ vendor/cache/cypress-rails-55310a6d5544/lib/cypress-rails.rb
@@ -6,6 +6,7 @@ end
 require "cypress-rails/init"
 require "cypress-rails/open"
 require "cypress-rails/run"
+require "cypress-rails/run_knapsack"
 require "cypress-rails/resets_state"
 require "cypress-rails/initializer_hooks"
 require "cypress-rails/railtie" if defined?(Rails)
