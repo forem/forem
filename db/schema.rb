48e840f2127f1fdf98af9a8aca4b4e3a952bd75b
Ce7in <cetin.muhammed@gmail.com>
2021-10-24 05:11:30 +0300

    The origin field has been added to the user schema.

--- db/schema.rb
+++ db/schema.rb
@@ -1359,7 +1359,7 @@ ActiveRecord::Schema.define(version: 2021_10_19_151431) do
     t.index ["invited_by_type", "invited_by_id"], name: "index_users_on_invited_by_type_and_invited_by_id"
     t.index ["old_old_username"], name: "index_users_on_old_old_username"
     t.index ["old_username"], name: "index_users_on_old_username"
-    t.index ["origin"], name: "index_users_on_origin"
+    t.index ["origin"], name: "index_users_on_origi"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["twitter_username"], name: "index_users_on_twitter_username", unique: true
     t.index ["user_id"], name: "index_users_on_user_id", unique: true
