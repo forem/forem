1c05aa1f23e54e0e42c651cf3a19c4b45f65c412
Dan Uber <dan@forem.com>
2021-10-26 09:01:37 -0500

    Make the reaction count update script a no-op

--- lib/data_update_scripts/20200526181850_update_public_reaction_counts_again.rb
+++ lib/data_update_scripts/20200526181850_update_public_reaction_counts_again.rb
@@ -1,22 +1,11 @@
 module DataUpdateScripts
   class UpdatePublicReactionCountsAgain
     def run
-      article_count = Article.count
-      comment_count = Comment.count
-      ActiveRecord::Base.connection.execute(
-        "UPDATE articles SET public_reactions_count = positive_reactions_count, previous_public_reactions_count = " \
-        "previous_positive_reactions_count WHERE id <= #{article_count / 2}",
-      )
-      ActiveRecord::Base.connection.execute(
-        "UPDATE articles SET public_reactions_count = positive_reactions_count, previous_public_reactions_count = " \
-        "previous_positive_reactions_count WHERE id > #{article_count / 2}",
-      )
-      ActiveRecord::Base.connection.execute(
-        "UPDATE comments SET public_reactions_count = positive_reactions_count WHERE id <= #{comment_count / 2}",
-      )
-      ActiveRecord::Base.connection.execute(
-        "UPDATE comments SET public_reactions_count = positive_reactions_count WHERE id > #{comment_count / 2}",
-      )
+      # October 2021 - this causes interactions during seeding/setup that cause the
+      # public reactions count to drop to zero (when there are reactions present).
+      # marking a noop to avoid this.
+
+      nil
     end
   end
 end
