c4007ce7dfbf831243912ba59fb41fcc8224c593
Paweł Ludwiczak <ludwiczakpawel@gmail.com>
2021-10-26 09:11:21 +0200

    .

--- app/assets/javascripts/initializers/initScrolling.js
+++ app/assets/javascripts/initializers/initScrolling.js
@@ -200,6 +200,7 @@ function insertVideos(videoArticles) {
     document.documentElement.scrollHeight - document.body.scrollTop;
   var newNode = document.createElement('div');
   newNode.innerHTML = newVideosHTML;
+  newNode.className += 'video-collection';
   var singleArticles = document.querySelectorAll(
     '.single-article, .crayons-story',
   );
