59ad0a354506a78b7c05befc0c8e009868ff05fe
Julianna Tetreault <32834804+juliannatetreault@users.noreply.github.com>
2021-10-26 10:00:46 -0600

    Adds a comment to clarify the purpose of CreatorSetting

--- app/models/creator_setting.rb
+++ app/models/creator_setting.rb
@@ -1,3 +1,6 @@
 # rubocop:disable Lint/EmptyClass
+# Heads up: This model is necessary for the
+# Creator Settings work and will break things if removed!
+# This should probably be refactored in the future.
 class CreatorSetting; end
 # rubocop:enable Lint/EmptyClass
