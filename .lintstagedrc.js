dfc990991561545e0b2332a760ee5f63699077e9
Mac Siri <krairit.siri@gmail.com>
2021-10-27 12:28:59 -0400

    Disable i18n-tasks and sort

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,29 +1,29 @@
 // .lintstagedrc.js
 module.exports = {
+  '*.json': ['prettier --write'],
+  '*.md': ['prettier --write --prose-wrap always'],
+  '*.rake': [
+    'bundle exec rubocop --require rubocop-rspec --auto-correct --enable-pending-cops',
+  ],
+  '*.scss': ['prettier --write'],
+  '*.svg': ['svgo --pretty'],
+  '*.{js,jsx}': ['prettier --write', 'eslint --fix', 'jest --findRelatedTests'],
+  './Gemfile': [
+    'bundle exec rubocop --require rubocop-rspec --auto-correct --enable-pending-cops',
+  ],
+  'app/**/*.html.erb': ['bundle exec erblint --autocorrect'],
   'app/assets/config/manifest.js': [
     'prettier --write',
     'eslint --fix -c app/assets/javascripts/.eslintrc.js',
   ],
-  '*.{js,jsx}': ['prettier --write', 'eslint --fix', 'jest --findRelatedTests'],
-  '{app,spec,config,lib}/**/*.rb': [
-    'bundle exec rubocop --require rubocop-rspec --auto-correct --enable-pending-cops',
-  ],
-  'scripts/{release,stage_release}': [
-    'bundle exec rubocop --require rubocop-rspec --auto-correct --enable-pending-cops',
-  ],
   'app/views/**/*.jbuilder': [
     'bundle exec rubocop --require rubocop-rspec --auto-correct --enable-pending-cops',
   ],
-  './Gemfile': [
+  // 'config/locales/*': () => 'bundle exec i18n-tasks normalize',
+  'scripts/{release,stage_release}': [
     'bundle exec rubocop --require rubocop-rspec --auto-correct --enable-pending-cops',
   ],
-  '*.rake': [
+  '{app,spec,config,lib}/**/*.rb': [
     'bundle exec rubocop --require rubocop-rspec --auto-correct --enable-pending-cops',
   ],
-  'config/locales/*': () => 'bundle exec i18n-tasks normalize',
-  'app/**/*.html.erb': ['bundle exec erblint --autocorrect'],
-  '*.json': ['prettier --write'],
-  '*.scss': ['prettier --write'],
-  '*.md': ['prettier --write --prose-wrap always'],
-  '*.svg': ['svgo --pretty'],
 };
