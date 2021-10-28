38722e4d4b00e9b1e77ecc5d4bc9df732a112631
Arit Amana <msarit@gmail.com>
2021-10-28 11:32:37 -0400

    Update Cypress specs

--- cypress/integration/seededFlows/adminFlows/articles/pinArticle.spec.js
+++ cypress/integration/seededFlows/adminFlows/articles/pinArticle.spec.js
@@ -30,18 +30,18 @@ describe('Pin an article from the admin area', () => {
       statusCode: 404,
     });
 
-    cy.findAllByRole('link', { name: 'Pin Post' }).first().click();
+    cy.findAllByRole('button', { name: 'Pin Post' }).first().click();
 
     // Verify that the form has submitted and the page has changed to the post page
     cy.url().should('contain', '/content_manager/articles/');
 
-    cy.findByRole('link', { name: 'Pin Post' }).should('not.exist');
+    cy.findByRole('button', { name: 'Pin Post' }).should('not.exist');
     cy.findByRole('link', { name: 'Unpin Post' }).should('exist');
     cy.findByText(/Pinned post/i).should('exist');
   });
 
-  it('should change the pinned article when choosing to pin a new article', () => {
-    cy.findAllByRole('link', { name: 'Pin Post' }).first().click();
+  it('should display a warning modal when pinning an article, and one is already pinned', () => {
+    cy.findAllByRole('button', { name: 'Pin Post' }).first().click();
 
     cy.createArticle({
       title: 'A new article',
@@ -55,17 +55,47 @@ describe('Pin an article from the admin area', () => {
     cy.findByRole('main')
       .first()
       .within(() => {
-        cy.findAllByRole('link', { name: 'Pin Post' }).last().click();
+        cy.findAllByRole('button', { name: 'Pin Post' }).last().click();
       });
 
+    cy.findByRole('dialog').within(() => {
+      cy.findByRole('heading', {
+        name: "There's another article pinned...",
+        level: 2,
+      });
+    });
+  });
+
+  it('should change the pinned article when choosing to pin a new article', () => {
+    cy.findAllByRole('button', { name: 'Pin Post' }).first().click();
+
+    cy.createArticle({
+      title: 'A new article',
+      tags: ['beginner', 'ruby', 'go'],
+      content: `This is another test article's contents.`,
+      published: true,
+    }).then((response) => {
+      cy.visit(`/admin/content_manager/articles/${response.body.id}`);
+    });
+
     cy.findByRole('main')
-      .findAllByText(/Pinned post/i)
       .first()
-      .should('exist');
+      .within(() => {
+        cy.findAllByRole('button', { name: 'Pin Post' }).last().click();
+      });
+
+    cy.findByRole('dialog').within(() => {
+      cy.findByRole('button', { name: 'Pin new article' }).click();
+    });
+
+    cy.findByRole('main').within(() => {
+      cy.findByText(/A new article/i).should('exist');
+      cy.findByText(/Pinned post/i).should('exist');
+    });
   });
 
   it('should show the pinned post to a logged out user', () => {
-    cy.findAllByRole('link', { name: 'Pin Post' }).first().click();
+    cy.findAllByRole('button', { name: 'Pin Post' }).first().click();
 
     cy.signOutUser();
 
