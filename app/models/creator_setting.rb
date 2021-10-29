8b9dd69046cee4b3d3d40c28283e2876fc58ba2d
Julianna Tetreault <32834804+juliannatetreault@users.noreply.github.com>
2021-10-29 15:55:46 -0600

    Reverts change to CreatorSetting and adds self.abstract = true to it

--- app/models/creator_setting.rb
+++ app/models/creator_setting.rb
@@ -1,6 +1,9 @@
-# rubocop:disable Lint/EmptyClass
-# Heads up: This model is necessary for the
-# Creator Settings work and will break things if removed!
-# This should probably be refactored in the future.
-class CreatorSetting; end
-# rubocop:enable Lint/EmptyClass
+class CreatorSetting < ApplicationRecord
+  # This class exists to take advantage of Rolify for limiting authorization
+  # on internal reports.
+  # NOTE: It is not backed by a database table and should not be expected to
+  # function like a traditional Rails model
+  self.abstract_class = true
+
+  resourcify
+end
