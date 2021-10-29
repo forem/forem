0de2ceb6fda72a19abe10b907bbb96840669a35f
Dan Uber <dan@forem.com>
2021-10-29 10:07:33 -0500

    Add a spec
    Tested that this fails in main and passes on the branch

--- spec/views/articles_spec.rb
+++ spec/views/articles_spec.rb
@@ -64,4 +64,13 @@ RSpec.describe "articles/show", type: :view do
     expect(rendered).to have_text("example.com")
     expect(rendered).to have_text("Updated on")
   end
+
+  it "shows the original publication time for crossposts" do
+    allow(article1).to receive(:canonical_url).and_return("https://example.com/lamas")
+    allow(article1).to receive(:crossposted_at).and_return(Time.current)
+    allow(article1).to receive(:originally_published_at).and_return(Time.zone.at(0))
+    render
+    expect(rendered).to have_text("Originally published at")
+    expect(rendered).not_to have_text("</time>")
+  end
 end
