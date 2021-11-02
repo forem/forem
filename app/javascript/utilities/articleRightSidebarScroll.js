dfc145304f433a86d2a34e9a7cb323c0ef16be58
lyqht <estee.tsw@gmail.com>
2021-11-02 22:58:07 +0800

    add resize observer to article sidebar

--- app/javascript/utilities/articleRightSidebarScroll.js
+++ app/javascript/utilities/articleRightSidebarScroll.js
@@ -25,8 +25,24 @@ export function makeRightSidebarStickyBottom() {
     });
   };
 
-  const observer = new IntersectionObserver(observerCallback, {
+  const resizeObserverCallback = (entries) => {
+    const entry = entries[0];
+    const { blockSize: sidebarHeight } = entry.borderBoxSize[0];
+
+    const sidebarExceedsViewportHeight = sidebarHeight > window.innerHeight;
+
+    if (sidebarExceedsViewportHeight) {
+      intersectionObserver.observe(target);
+    } else {
+      intersectionObserver.unobserve(target);
+    }
+  };
+
+  const intersectionObserver = new IntersectionObserver(observerCallback, {
     threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
   });
-  observer.observe(target);
+
+  const resizeObserver = new ResizeObserver(resizeObserverCallback);
+
+  resizeObserver.observe(target);
 }
