e070adda4af16afcbbe8c56b1d56e64d2addcfbf
Arit Amana <msarit@gmail.com>
2021-11-03 13:13:14 -0400

    complete implementation for profile fields

--- app/javascript/packs/userProfileSettings.js
+++ app/javascript/packs/userProfileSettings.js
@@ -1,26 +1,27 @@
-const user_profile_field_ids = [
-  'user[name]',
-  'user[email]',
-  'user[username]',
-  'profile[website_url]',
-  'profile[location]',
-  'profile[summary]',
-];
+const userFieldIds = ['user[name]', 'user[email]', 'user[username]'];
+const profileFieldIds = Array.from(
+  document.querySelectorAll('[id^="profile["]'),
+).map((node) => node.id);
+const allFieldIds = [...userFieldIds, ...profileFieldIds];
 
 export function fieldCharacterLimits() {
   window.addEventListener('load', () => {
-    user_profile_field_ids.forEach((field_id) => {
+    allFieldIds.forEach((field_id) => {
       const field = document.getElementById(field_id);
       const fieldValueLength = field.value.length;
       const fieldCharacterSpan = document.getElementById(
         field.dataset.characterSpanId,
       );
       fieldCharacterSpan.innerHTML = fieldValueLength;
-
-      document.getElementById(field_id).addEventListener('keyup', (event) => {
-        fieldCharacterSpan.innerHTML = event.target.value.length;
-      });
     });
+
+    document
+      .getElementById('user-profile-form')
+      .addEventListener('keyup', (event) => {
+        document.getElementById(
+          event.target.dataset.characterSpanId,
+        ).innerHTML = event.target.value.length;
+      });
   });
 }
 
