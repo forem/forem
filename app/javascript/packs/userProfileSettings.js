035b87c391018b727d0b4fbebc950b8b87bdd277
Arit Amana <msarit@gmail.com>
2021-11-03 15:20:54 -0400

    Add guard againt irrelevant profile fields

--- app/javascript/packs/userProfileSettings.js
+++ app/javascript/packs/userProfileSettings.js
@@ -18,6 +18,10 @@ export function fieldCharacterLimits() {
     document
       .getElementById('user-profile-form')
       .addEventListener('keyup', (event) => {
+        if (!event.target.dataset.characterSpanId) {
+          return;
+        }
+
         document.getElementById(
           event.target.dataset.characterSpanId,
         ).innerHTML = event.target.value.length;
