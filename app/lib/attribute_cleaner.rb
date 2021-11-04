559cd02f507cdffc71f65fee9124b44a882c37c3
Michael Kohl <citizen428@forem.com>
2021-11-04 12:43:58 +0700

    Add attribute cleaner

---
+++ app/lib/attribute_cleaner.rb
@@ -0,0 +1,29 @@
+# This module provides a method for generating a configurable before_validation
+# hook for cleaning up string attributes: all listed attributes will be
+# replaced with `nil` if they are blank.
+module AttributeCleaner
+  # This method specifies the list of attributes that should be cleaned before
+  # validation happens.
+  #
+  # @param attributes [Array<Symbol|String>]
+  #
+  # @return [void]
+  #
+  # @example Defining a list of attributes to be cleaned
+  #   include AttributeCleaner.for(:attribute1, :attribute2)
+  def self.for(*attributes)
+    Module.new do
+      def self.included(klass)
+        klass.before_validation(:nullify_blank_attributes)
+      end
+
+      define_method(:nullify_blank_attributes) do
+        attributes.each do |attr|
+          next if public_send(attr).present?
+
+          public_send("#{attr}=", nil) if public_send(attr)
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/attribute_cleaner_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+RSpec.describe AttributeCleaner, type: :lib do
+  with_model :TestClass do
+    table do |t|
+      t.string :string_attribute
+    end
+
+    model do
+      # rubocop:disable RSpec/DescribedClass
+      include AttributeCleaner.for(:string_attribute)
+      # rubocop:enable RSpec/DescribedClass
+    end
+  end
+
+  it "adds a before_validation callback to the including model", :aggregate_failures do
+    before_validation_cbs = TestClass._validation_callbacks.filter_map do |cb|
+      cb.filter if cb.kind == :before
+    end
+
+    expect(before_validation_cbs).to eq([:nullify_blank_attributes])
+    expect(TestClass.new).to respond_to(:nullify_blank_attributes)
+  end
+
+  it "replaces empty strings with nil" do
+    test_instance = TestClass.new(string_attribute: "")
+
+    expect { test_instance.validate }
+      .to change(test_instance, :string_attribute).from("").to(nil)
+  end
+
+  it "replaces blank strings with nil" do
+    test_instance = TestClass.new(string_attribute: " ")
+
+    expect { test_instance.validate }
+      .to change(test_instance, :string_attribute).from(" ").to(nil)
+  end
+
+  it "leaves non-blank attributes unchanged" do
+    test_instance = TestClass.new(string_attribute: "Test")
+
+    expect { test_instance.validate }.not_to change(test_instance, :string_attribute)
+  end
+end
