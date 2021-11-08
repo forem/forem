7f5b65221a081f58268214061a7248920cb4b449
Arit Amana <msarit@gmail.com>
2021-11-08 17:09:19 -0500

    remove "if loaded" check

--- app/javascript/packs/userProfileSettings.js
+++ app/javascript/packs/userProfileSettings.js
@@ -4,25 +4,23 @@ const profileFieldIds = Array.from(
 ).map((node) => node.id);
 
 export function fieldCharacterLimits() {
-  if (document.readyState === 'complete') {
-    profileFieldIds.forEach((field_id) => {
-      const field = document.getElementById(field_id);
-      const fieldValueLength = field.value.length;
-      const fieldCharacterSpan = document.getElementById(
-        field.dataset.characterSpanId,
-      );
-      fieldCharacterSpan.innerHTML = fieldValueLength;
-    });
+  profileFieldIds.forEach((field_id) => {
+    const field = document.getElementById(field_id);
+    const fieldValueLength = field.value.length;
+    const fieldCharacterSpan = document.getElementById(
+      field.dataset.characterSpanId,
+    );
+    fieldCharacterSpan.innerHTML = fieldValueLength;
+  });
 
-    userSettingForm.addEventListener('keyup', (event) => {
-      if (!event.target.dataset.characterSpanId) {
-        return;
-      }
+  userSettingForm.addEventListener('keyup', (event) => {
+    if (!event.target.dataset.characterSpanId) {
+      return;
+    }
 
-      document.getElementById(event.target.dataset.characterSpanId).innerHTML =
-        event.target.value.length;
-    });
-  }
+    document.getElementById(event.target.dataset.characterSpanId).innerHTML =
+      event.target.value.length;
+  });
 }
 
 fieldCharacterLimits();
