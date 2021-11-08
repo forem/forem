cb74a9cff767d8b5a87cbad6e1d665643e5db3c1
Fernando Valverde <fernando@visualcosita.com>
2021-11-08 11:01:44 -0600

    More specs

--- cypress/integration/seededFlows/loggedOutFlows/namespacedForemMobile.spec.js
+++ cypress/integration/seededFlows/loggedOutFlows/namespacedForemMobile.spec.js
@@ -9,18 +9,51 @@ describe('Namespaced ForemMobile functions', () => {
       Object.defineProperty(win.navigator, 'platform', { value: 'iPhone' });
     },
   };
-  beforeEach(() => {
-    cy.testSetup();
-  });
 
-  it('should load namespaced functions within ForemWebView context', () => {
-    cy.visit('/', runtimeStub);
-    cy.window().then((win) => {
-      assert.isNumber(win.ForemMobile.retryDelayMs);
-      assert.isFunction(win.ForemMobile.getUserData);
-      assert.isFunction(win.ForemMobile.getInstanceMetadata);
-      assert.isFunction(win.ForemMobile.registerDeviceToken);
-      assert.isFunction(win.ForemMobile.unregisterDeviceToken);
+  beforeEach(() => cy.testSetup());
+
+  describe('within ForemWebView context', () => {
+    beforeEach(() => cy.visit('/', runtimeStub));
+
+    it('should load the ForemMobile namespaced functions', () => {
+      cy.window().then((win) => {
+        assert.isNumber(win.ForemMobile.retryDelayMs);
+        assert.isFunction(win.ForemMobile.getUserData);
+        assert.isFunction(win.ForemMobile.getInstanceMetadata);
+        assert.isFunction(win.ForemMobile.registerDeviceToken);
+        assert.isFunction(win.ForemMobile.unregisterDeviceToken);
+      });
+    });
+
+    it('should return the instance metadata JSON when requested', () => {
+      cy.window().then((win) => {
+        var res = JSON.parse(win.ForemMobile.getInstanceMetadata());
+        assert.isObject(res);
+        assert.equal(res.domain, 'localhost:3000');
+        assert.isString(res.logo);
+        assert.isString(res.name);
+      });
+    });
+
+    it('should return empty user data when logged out', () => {
+      cy.window().then((win) => {
+        assert.isUndefined(win.ForemMobile.getUserData());
+      });
+    });
+
+    it('should return user data JSON when logged in', () => {
+      // Attempt to login user and fetch its data
+      cy.fixture('users/adminUser.json').as('user');
+      cy.get('@user').then((user) => {
+        cy.loginUser(user).then(() => {
+          cy.visit('/settings', runtimeStub)
+            .then(() => cy.window())
+            .then((win) => {
+              var res = JSON.parse(win.ForemMobile.getUserData());
+              assert.isObject(res);
+            });
+        });
+      });
     });
   });
 
