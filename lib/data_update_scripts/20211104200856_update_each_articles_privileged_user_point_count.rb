71dc5767fef0587a1a897988f7d41d6ae12a622b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-11-08 11:28:52 -0500

    Fixing method name to `exists?`
    Prior to this commit, I was using `exist?` which doesn't work for
    ActiveRecord::Relation objects.

--- lib/data_update_scripts/20211104200856_update_each_articles_privileged_user_point_count.rb
+++ lib/data_update_scripts/20211104200856_update_each_articles_privileged_user_point_count.rb
@@ -6,7 +6,7 @@ module DataUpdateScripts
         articles.each do |article|
           # Given that we have a default of 0, don't update if we
           # don't have any reactions.
-          next unless article.reactions.privileged_category.exist?
+          next unless article.reactions.privileged_category.exists?
 
           article.update_column(:privileged_users_reaction_points_sum,
                                 article.reactions.privileged_category.sum(:points))
