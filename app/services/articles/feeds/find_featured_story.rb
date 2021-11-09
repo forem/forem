57db7b8b487ff749dabd11f6aca6fad33487d210
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-11-09 14:25:23 -0500

    Parameterizing featured story requiring main image
    This begins to ease the resolution of #15292.
    
    If we merge #15240, I could see setting an `Article` constant for this
    value and allowing the administrator to choose the particular behavior.
    
    Once we have insight from the product team, we can move forward with a
    more comprehensive solution.

--- app/services/articles/feeds/find_featured_story.rb
+++ app/services/articles/feeds/find_featured_story.rb
@@ -1,9 +1,18 @@
 module Articles
   module Feeds
     module FindFeaturedStory
-      def self.call(stories)
+      # @param stories [ActiveRecord::Relation, #detect] pick a
+      #        featured story from this enumerable
+      # @param must_have_main_image [Boolean] if true, the featured
+      #        story must have a main image
+      # @return [Article]
+      #
+      # @note the must_have_main_image parameter name matches PR #15240
+      def self.call(stories, must_have_main_image: true)
         featured_story =
-          if stories.is_a?(ActiveRecord::Relation)
+          if !must_have_main_image
+            stories.first
+          elsif stories.is_a?(ActiveRecord::Relation)
             stories.where.not(main_image: nil).first
           else
             stories.detect { |story| story.main_image.present? }
