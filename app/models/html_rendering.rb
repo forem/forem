ff4946fa3957b48868c6c9b9d8a414efc3b8e387
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-11-09 19:50:55 -0500

    Adding comparative documentation for HTML tags

--- app/models/html_rendering.rb
+++ app/models/html_rendering.rb
@@ -13,21 +13,27 @@ module HtmlRendering
   # A container module for the allowed tags in various rendering
   # contexts.
   module AllowedTags
-    FEED = %w[a b blockquote br center cite code col colgroup dd del div dl dt em em h1 h2
+    FEED = %w[a b blockquote br center cite code col colgroup dd del div dl dt em h1 h2
               h3 h4 h5 h6 i iframe img li ol p pre q small span strong sup table tbody td
               tfoot th thead time tr u ul].freeze
 
+    # In FEED but not PODCAST_SHOW: [iframe]
+    # In PODCAST_SHOW but not FEED: []
     PODCAST_SHOW = %w[
       a b blockquote br center cite code col colgroup dd del div dl dt em
       h1 h2 h3 h4 h5 h6 i img li ol p pre q small span strong sup table
       tbody td tfoot th thead time tr u ul
     ].freeze
 
+    # In FEED but not DISPLAY_AD: [div i iframe]
+    # In DISPLAY_AD but not FEED: [abbr add figcaption hr kbd mark rp rt ruby source sub video]
     DISPLAY_AD = %w[a abbr add b blockquote br center cite code col colgroup dd del dl dt
                     em figcaption h1 h2 h3 h4 h5 h6 hr img kbd li mark ol p pre q rp rt
                     ruby small source span strong sub sup table tbody td tfoot th thead
                     time tr u ul video].freeze
 
+    # In FEED but not RENDERED_MARKDOWN_SCRUBBER: [div i iframe]
+    # In RENDERED_MARKDOWN_SCRUBBER but not FEED: [abbr add figcaption hr kbd mark rp rt ruby source sub video]
     RENDERED_MARKDOWN_SCRUBBER = %w[a abbr add b blockquote br center cite code col
                                     colgroup dd del dl dt em figcaption h1 h2 h3 h4 h5
                                     h6 hr img kbd li mark ol p pre q rp rt ruby small
