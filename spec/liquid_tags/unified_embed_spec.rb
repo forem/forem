84abd0d93aa521b67ed6dc34744b563508a52b97
Arit Amana <msarit@gmail.com>
2021-11-11 11:44:04 -0500

    add specs for asciinema and codepen liqtags

--- spec/liquid_tags/unified_embed_spec.rb
+++ spec/liquid_tags/unified_embed_spec.rb
@@ -8,5 +8,15 @@ RSpec.describe UnifiedEmbed do
       expect(described_class.find_liquid_tag_for(link: "https://gist.github.com/jeremyf/662585f5c4d22184a6ae133a71bf891a"))
         .to eq(GistTag)
     end
+
+    it "returns AsciinemaTag for a gist url" do
+      expect(described_class.find_liquid_tag_for(link: "https://asciinema.org/a/330532"))
+        .to eq(AsciinemaTag)
+    end
+
+    it "returns CodepenTag for a gist url" do
+      expect(described_class.find_liquid_tag_for(link: "https://codepen.io/elisavetTriant/pen/KKvRRyE"))
+        .to eq(CodepenTag)
+    end
   end
 end
