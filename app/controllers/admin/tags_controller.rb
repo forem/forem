845bc01c69d75a4417931750cb7cad6adc7750f4
Daniel Uber <djuber@gmail.com>
2021-11-15 14:58:24 -0600

    Remove name from allowed tag params in admin (#15382)
    Don't allow passing `tag[name]=` in post params to admin tag update.
    
    There's no field for this in the view, it's not expected that someone
    change the name (it serves as a natural key) - it's preferrable to
    alias an old name to a new tag if a renaming is desired.

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -6,7 +6,6 @@ module Admin
       id supported rules_markdown short_summary pretty_name bg_color_hex
       text_color_hex user_id alias_for badge_id requires_approval
       social_preview_template wiki_body_markdown submission_template
-      name
     ].freeze
 
     before_action :set_default_options, only: %i[index]
