3e41971354b7c862b80205de6ae43281ef2f0edf
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-11-17 11:54:26 -0500

    Adding comments to clarify behavior of spam handling

--- app/services/spam/article_handler.rb
+++ app/services/spam/article_handler.rb
@@ -6,6 +6,11 @@ module Spam
     #
     # @param article [Article] the article to check for spamminess
     # @param attributes [Array<Symbol>] test these attributes of the article.
+    #
+    # @note We are not blocking the spammer from submitting the
+    # article with spam content, but we are instead performing some
+    # administrative tasks to indicate that we're taking notice and
+    # even escalating.
     def self.handle!(article:, attributes: %i[title body_markdown])
       return :not_spam if attributes.none? do |attr|
                             Settings::RateLimit.trigger_spam_for?(text: article.public_send(attr))
@@ -20,6 +25,10 @@ module Spam
 
       return unless Reaction.user_has_been_given_too_many_spammy_reactions?(user: article.user)
 
+      # TODO: What should we do with their active session?  I think we
+      # might want to explore later raising an exception that the
+      # controller rescues and logs the user out.  Because, as written
+      # they might still be logged in but have limited abilities.
       article.user.add_role(:suspended)
 
       Note.create(
