887ec749f676928e66d00981f50e5786631f3396
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-11-19 09:11:25 -0500

    Adding validator domain validator spec

--- app/validators/valid_domain_csv_validator.rb
+++ app/validators/valid_domain_csv_validator.rb
@@ -1,3 +1,7 @@
+# @note While the validator implies a CSV, the implementation is that
+#       we have an array.  Upstream implementors likely accept a CSV
+#       and coerce it into an array.  See Authentication::Base for an
+#       example of coercing the CSV into an array.
 class ValidDomainCsvValidator < ActiveModel::EachValidator
   DEFAULT_MESSAGE = "must be a comma-separated list of valid domains".freeze
   VALID_DOMAIN = /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$/
