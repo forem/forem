aceae00de4af24e15e6d35f058b60884405da816
Nick Taylor <nick@dev.to>
2021-11-22 15:30:46 -0500

    Fixed mobile bridging E2E tests.

--- cypress/integration/seededFlows/loggedOutFlows/namespacedForemMobile.spec.js
+++ cypress/integration/seededFlows/loggedOutFlows/namespacedForemMobile.spec.js
@@ -1,4 +1,8 @@
 describe('Namespaced ForemMobile functions', () => {
+  function waitForMobileToBootstrap() {
+    cy.get('body').should('have.attr', 'data-runtime', 'ForemWebView-iOS');
+  }
+
   // Make the runtime act as ForemWebView context
   const runtimeStub = {
     onBeforeLoad: (win) => {
@@ -10,86 +14,96 @@ describe('Namespaced ForemMobile functions', () => {
     },
   };
 
-  beforeEach(() => cy.testSetup());
-
   describe('within ForemWebView context', () => {
-    beforeEach(() => cy.visit('/', runtimeStub));
-
-    it('should load the ForemMobile namespaced functions', () => {
-      cy.window().then((win) => {
-        assert.isNumber(win.ForemMobile.retryDelayMs);
-        assert.isFunction(win.ForemMobile.getUserData);
-        assert.isFunction(win.ForemMobile.getInstanceMetadata);
-        assert.isFunction(win.ForemMobile.registerDeviceToken);
-        assert.isFunction(win.ForemMobile.unregisterDeviceToken);
-        assert.isFunction(win.ForemMobile.injectJSMessage);
-        assert.isFunction(win.ForemMobile.injectNativeMessage);
-        assert.isFunction(win.ForemMobile.userSessionBroadcast);
-      });
-    });
-
-    it('should return the instance metadata JSON when requested', () => {
-      cy.window().then((win) => {
-        var res = JSON.parse(win.ForemMobile.getInstanceMetadata());
-        assert.isObject(res);
-        assert.equal(res.domain, 'localhost:3000');
-        assert.isString(res.logo);
-        assert.isString(res.name);
-      });
-    });
-
-    it('should return empty user data when logged out', () => {
-      cy.window().then((win) => {
-        assert.isUndefined(win.ForemMobile.getUserData());
-      });
-    });
-
-    it('should return user data JSON when logged in', () => {
-      // Attempt to login user and fetch its data
-
-      // Manual attempt (?)
-      cy.contains('.crayons-btn', 'Log in').click();
-      cy.get('#user_email').type('admin@forem.local');
-      cy.get('#user_password').type('password');
-      cy.get('input[type="submit"]').click();
-      cy.window().then((win) => {
-        var res = JSON.parse(win.ForemMobile.getUserData());
-        assert.isObject(res);
-      });
-
-      // Previous attempt with cy.loginUser():
-      //
-      // cy.fixture('users/adminUser.json').as('user');
-      // cy.get('@user').then((user) => {
-      //   return cy.loginUser(user);
-      // }).then(() => {
-      //   return cy.visit('/settings', runtimeStub);
-      // }).then(() => cy.window()).then((win) => {
-      //   var res = JSON.parse(win.ForemMobile.getUserData());
-      //   assert.isObject(res);
-      // });
-    });
-
-    it('should inject messages using CustomEvent', () => {
-      cy.document()
-        .then((doc) => {
-          doc.addEventListener('ForemMobile', cy.stub().as('bridgeEvent'));
-        })
-        .then(() => cy.window())
-        .then((win) => {
-          win.ForemMobile.injectJSMessage({ action: 'test' });
+    describe('when logged in', () => {
+      beforeEach(() => {
+        cy.testSetup();
+        cy.fixture('users/adminUser.json').as('user');
+        cy.get('@user').then((user) => {
+          cy.loginUser(user).then(() => {
+            cy.visit('/', runtimeStub);
+            waitForMobileToBootstrap();
+          });
         });
+      });
 
-      // on load the app should have sent an event
-      cy.get('@bridgeEvent')
-        .should('have.been.calledOnce')
-        .its('firstCall.args.0.detail')
-        .should('deep.equal', { action: 'test' });
+      it('should load the ForemMobile namespaced functions', () => {
+        cy.window().then((win) => {
+          assert.isNumber(win.ForemMobile.retryDelayMs);
+          assert.isFunction(win.ForemMobile.getUserData);
+          assert.isFunction(win.ForemMobile.getInstanceMetadata);
+          assert.isFunction(win.ForemMobile.registerDeviceToken);
+          assert.isFunction(win.ForemMobile.unregisterDeviceToken);
+          assert.isFunction(win.ForemMobile.injectJSMessage);
+          assert.isFunction(win.ForemMobile.injectNativeMessage);
+          assert.isFunction(win.ForemMobile.userSessionBroadcast);
+        });
+      });
+
+      it('should return the instance metadata JSON when requested', () => {
+        cy.window().then((win) => {
+          var res = JSON.parse(win.ForemMobile.getInstanceMetadata());
+          assert.isObject(res);
+          assert.equal(res.domain, 'localhost:3000');
+          assert.isString(res.logo);
+          assert.isString(res.name);
+        });
+      });
+
+      it('should return user data when logged in', () => {
+        cy.window().then((win) => {
+          cy.fixture('users/adminUser.json').as('user');
+          cy.get('@user').then((expected) => {
+            const actual = JSON.parse(win.ForemMobile.getUserData());
+
+            expect(actual.username).to.equal(expected.username);
+          });
+        });
+      });
+
+      it('should inject messages using CustomEvent', () => {
+        cy.document()
+          .then((doc) => {
+            doc.addEventListener('ForemMobile', cy.stub().as('bridgeEvent'));
+          })
+          .then(() => cy.window())
+          .then((win) => {
+            win.ForemMobile.injectJSMessage({ action: 'test' });
+          });
+
+        // on load the app should have sent an event
+        cy.get('@bridgeEvent')
+          .should('have.been.calledOnce')
+          .its('firstCall.args.0.detail')
+          .should('deep.equal', { action: 'test' });
+      });
+    });
+
+    describe('when logged out', () => {
+      it('should return empty user data when logged out', () => {
+        cy.testSetup();
+        cy.visit('/', runtimeStub);
+        waitForMobileToBootstrap();
+
+        cy.window().then((win) => {
+          assert.isUndefined(win.ForemMobile.getUserData());
+        });
+      });
     });
   });
 
-  it('should not load namespaced functions on other contexts', () => {
-    cy.visit('/');
-    cy.window().then((win) => assert.isUndefined(win.ForemMobile));
+  describe('Non-mobile', () => {
+    it('should not load namespaced functions on other contexts', () => {
+      cy.testSetup();
+      cy.visit('/');
+
+      cy.get('body')
+        .invoke('attr', 'data-runtime')
+        .should('contain', 'Browser-');
+
+      cy.window().then((win) => {
+        assert.isUndefined(win.ForemMobile);
+      });
+    });
   });
 });
