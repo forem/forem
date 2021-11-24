fc3b72f02f4d45c274f172896d12cf847fa7282b
Nick Taylor <nick@dev.to>
2021-11-24 16:00:25 -0500

    wip - Got logo upload working

--- app/controllers/admin/creator_settings_controller.rb
+++ app/controllers/admin/creator_settings_controller.rb
@@ -1,15 +1,17 @@
 module Admin
   class CreatorSettingsController < Admin::ApplicationController
     ALLOWED_PARAMS = %i[community_name logo_svg primary_brand_color_hex invite_only_mode public checked_code_of_conduct
-                        checked_terms_and_conditions].freeze
+                        checked_terms_and_conditions authenticity_token utf8 logo commit locale].freeze
 
     def new; end
 
     def create
       extra_authorization
       ActiveRecord::Base.transaction do
+        raise CarrierWave::IntegrityError if settings_params[:logo].blank?
+
+        ::Settings::General.logo_svg = upload_logo(settings_params[:logo]).url
         ::Settings::Community.community_name = settings_params[:community_name]
-        ::Settings::General.logo_svg = settings_params[:logo_svg]
         ::Settings::UserExperience.primary_brand_color_hex = settings_params[:primary_brand_color_hex]
         ::Settings::Authentication.invite_only_mode = settings_params[:invite_only]
         ::Settings::UserExperience.public = settings_params[:public]
@@ -20,6 +22,8 @@ module Admin
         checked_terms_and_conditions: settings_params[:checked_terms_and_conditions],
       )
       redirect_to root_path
+    # Should I be rescuing CarrierWave::IntegrityError and CarrierWave::ProcessingError
+    # or just leave it as rescue StandardError?
     rescue StandardError => e
       flash.now[:error] = e.message
       render new_admin_creator_setting_path
@@ -33,6 +37,12 @@ module Admin
 
     def settings_params
       params.permit(ALLOWED_PARAMS)
+    end
+
+    def upload_logo(image)
+      ArticleImageUploader.new.tap do |uploader|
+        uploader.store!(image)
+      end
     end
   end
 end
