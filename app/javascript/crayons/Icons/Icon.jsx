5d175ce1fa81b4d1b36ec9b5014bc5ea23772787
ludwiczakpawel <ludwiczakpawel@gmail.com>
2021-11-24 14:27:28 +0100

    Apply suggestions from code review
    Co-authored-by: Suzanne Aitchison <suzanne@forem.com>

--- app/javascript/crayons/Icons/Icon.jsx
+++ app/javascript/crayons/Icons/Icon.jsx
@@ -2,8 +2,7 @@ import { h } from 'preact';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 
-export const Icon = (props) => {
-  const { src: InternalIcon, native, className, ...otherProps } = props;
+export const Icon = ({ src: InternalIcon, native, className, ...otherProps }) => {
 
   return (
     <InternalIcon
@@ -21,5 +20,5 @@ Icon.displayName = 'Icon';
 Icon.propTypes = {
   native: PropTypes.bool,
   className: PropTypes.string,
-  src: PropTypes.ReactNode,
+  src: PropTypes.ReactNode.isRequired,
 };
