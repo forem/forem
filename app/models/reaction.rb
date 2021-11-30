forem/forem

commit 29f6853ee9b3aabd7edfa8a5a7a3f7247a9f5a28

Refactoring Spam Handler (#15412)
* Refactoring Spam Handler

There's considerable repeated logic between checking spam for an article
and spam for a comment and user.

This attempts to send things through channels that are similar and close
in organization.

* Fixing broken spec

* Fixing spec around recent user

* Update app/models/reaction.rb

Co-authored-by: Michael Kohl <citizen428@forem.com>

* Update app/models/reaction.rb

Co-authored-by: Michael Kohl <citizen428@forem.com>

* Consolidating new user query logic

Prior to this commit there were two separate queries around new user
logic.  With this commit, we're changing the logic to repurpose a site
wide setting.

* Generalizing a previously specific message

* Fixing method name

As part of a recommended refactor, I extracted a method, then renamed
it.  I failed to account for that renaming.

This commit fixes that.

Co-authored-by: Michael Kohl <citizen428@forem.com>

app/models/reaction.rb
@@ -81,8 +81,16 @@ class Reaction < ApplicationRecord
     #
     # @return [TrueClass] yup, they're spamming the system.
     # @return [FalseClass] they're not (yet) spamming the system
-    def user_has_been_given_too_many_spammy_reactions?(user:)
-      article_vomits.where(reactable_type: "Article", reactable_id: user.articles.pluck(:id)).size > 2
+    def user_has_been_given_too_many_spammy_article_reactions?(user:, threshold: 2)
+      article_vomits.where(reactable_type: "Article", reactable_id: user.articles.ids).size > threshold
+    end
+
+    # @param user [User] the user who might be spamming the system
+    #
+    # @return [TrueClass] yup, they're spamming the system.
+    # @return [FalseClass] they're not (yet) spamming the system
+    def user_has_been_given_too_many_spammy_comment_reactions?(user:, threshold: 2)
+      article_vomits.where(reactable_type: "Comment", reactable_id: user.comments.ids).size > threshold
     end
   end
 

app/decorators/user_decorator.rb
@@ -102,10 +102,7 @@ class UserDecorator < ApplicationDecorator
   end
 
   def considered_new?
-    min_days = Settings::RateLimit.user_considered_new_days
-    return false unless min_days.positive?
-
-    created_at.after?(min_days.days.ago)
+    Settings::RateLimit.user_considered_new?(user: self)
   end
 
   # Returns the user's public email if it is set and the display_email_on_profile

app/models/article.rb
@@ -812,7 +812,7 @@ class Article < ApplicationRecord
   end
 
   def create_conditional_autovomits
-    Spam::ArticleHandler.handle!(article: self)
+    Spam::Handler.handle_article!(article: self)
   end
 
   def async_bust

app/models/comment.rb
@@ -282,34 +282,13 @@ class Comment < ApplicationRecord
   end
 
   def synchronous_spam_score_check
-    return unless
-      Settings::RateLimit.spam_trigger_terms.any? { |term| Regexp.new(term.downcase).match?(title.downcase) }
+    return unless Settings::RateLimit.trigger_spam_for?(text: [title, body_markdown].join("\n"))
 
     self.score = -1 # ensure notification is not sent if possibly spammy
   end
 
   def create_conditional_autovomits
-    return unless
-      Settings::RateLimit.spam_trigger_terms.any? { |term| Regexp.new(term.downcase).match?(title.downcase) } &&
-        user.registered_at > 5.days.ago
-
-    Reaction.create(
-      user_id: Settings::General.mascot_user_id,
-      reactable_id: id,
-      reactable_type: "Comment",
-      category: "vomit",
-    )
-
-    return unless Reaction.comment_vomits.where(reactable_id: user.comments.pluck(:id)).size > 2
-
-    user.add_role(:suspended)
-    Note.create(
-      author_id: Settings::General.mascot_user_id,
-      noteable_id: user_id,
-      noteable_type: "User",
-      reason: "automatic_suspend",
-      content: "User suspended for too many spammy articles, triggered by autovomit.",
-    )
+    Spam::Handler.handle_comment!(comment: self)
   end
 
   def should_send_email_notification?

app/models/settings/rate_limit.rb
@@ -22,6 +22,20 @@ module Settings
     setting :user_subscription_creation, type: :integer, default: 3
     setting :user_update, type: :integer, default: 15
 
+    # A helper function to determine if we should consider the user a "new" user.
+    #
+    # @note A "new" user is more likely to start spamming than an "old" user.
+    #
+    # @param user [User, UserDecorator]
+    #
+    # @return [Boolean]
+    def self.user_considered_new?(user:)
+      return true unless user
+      return false unless user_considered_new_days.positive?
+
+      user.created_at.after?(user_considered_new_days.days.ago)
+    end
+
     # A helper function to determine if text is spammy.
     #
     # @param text [String] text to check for "spamminess"

app/models/user.rb
@@ -609,16 +609,7 @@ class User < ApplicationRecord
   end
 
   def create_conditional_autovomits
-    return unless Settings::RateLimit.spam_trigger_terms.any? do |term|
-      name.match?(/#{term}/i)
-    end
-
-    Reaction.create!(
-      user_id: Settings::General.mascot_user_id,
-      reactable_id: id,
-      reactable_type: "User",
-      category: "vomit",
-    )
+    Spam::Handler.handle_user!(user: self)
   end
 
   # TODO: @citizen428 I don't want to completely remove this method yet, as we

app/services/spam/handler.rb
@@ -1,0 +1,89 @@
+module Spam
+  # This module is responsible for handling spam in our various user input sources.
+  #
+  # @note We may not immediately block spam but instead slowly escalate our response.
+  module Handler
+    # Test the article for spamminess.  If it's not spammy, don't do anything.
+    #
+    # If it is spammy, escalate the situation!
+    #
+    # @param article [Article] the article to check for spamminess
+    # @param attributes [Array<Symbol>] test these attributes of the article.
+    def self.handle_article!(article:, attributes: %i[title body_markdown])
+      text = attributes.map { |attr| article.public_send(attr) }.join("\n")
+      return :not_spam unless Settings::RateLimit.trigger_spam_for?(text: text)
+
+      issue_spam_reaction_for!(reactable: article)
+
+      return unless Reaction.user_has_been_given_too_many_spammy_article_reactions?(user: article.user)
+
+      suspend!(user: article.user)
+    end
+
+    # Test the comment for spamminess.  If it's not spammy, don't do anything.
+    #
+    # If it is spammy, escalate the situation!
+    #
+    # @param comment [Comment] the comment to check for spamminess
+    def self.handle_comment!(comment:)
+      # TODO: Is this correct logic?  I was trying to reason through
+      # the original logic and I think there's something off on that
+      # original logic.
+      #
+      # I believe the intention of the past logic was that we want to
+      # treat recently registered users with a bit of suspicion.
+      return :not_spam unless Settings::RateLimit.user_considered_new?(user: comment&.user)
+      return :not_spam unless Settings::RateLimit.trigger_spam_for?(text: comment.body_markdown)
+
+      issue_spam_reaction_for!(reactable: comment)
+
+      return unless Reaction.user_has_been_given_too_many_spammy_comment_reactions?(user: comment.user)
+
+      suspend!(user: comment.user)
+    end
+
+    # Test the user for spamminess.  If it's not spammy, don't do anything.
+    #
+    # If it is spammy, escalate the situation!
+    #
+    # @param user [User] the user to check for spamminess
+    def self.handle_user!(user:)
+      return :not_spam unless Settings::RateLimit.trigger_spam_for?(text: user.name)
+
+      issue_spam_reaction_for!(reactable: user)
+    end
+
+    # Suspend the given user because of too many spammy actions.
+    #
+    # @param user [User]
+    #
+    # @todo What should we do with their active session?  I think we
+    #       might want to explore later raising an exception that the
+    #       controller rescues and logs the user out.  Because, as
+    #       written they might still be logged in but have limited
+    #       abilities.
+    def self.suspend!(user:)
+      user.add_role(:suspended)
+
+      Note.create(
+        author_id: Settings::General.mascot_user_id,
+        noteable: user,
+        reason: "automatic_suspend",
+        content: "User suspended for too many spammy interactions, triggered by autovomit.",
+      )
+    end
+    private_class_method :suspend!
+
+    # Have the mascot of this Forem react negatively to this reactable.
+    #
+    # @param reactable [ActiveRecord::Base]
+    def self.issue_spam_reaction_for!(reactable:)
+      Reaction.create(
+        user_id: Settings::General.mascot_user_id,
+        reactable: reactable,
+        category: "vomit",
+      )
+    end
+    private_class_method :issue_spam_reaction_for!
+  end
+end

spec/decorators/user_decorator_spec.rb
@@ -179,18 +179,12 @@ RSpec.describe UserDecorator, type: :decorator do
   end
 
   describe "#considered_new?" do
-    before do
-      allow(Settings::RateLimit).to receive(:user_considered_new_days).and_return(3)
-    end
+    let(:decorated_user) { user.decorate }
 
-    it "returns true for new users" do
-      user.created_at = 1.day.ago
-      expect(user.decorate.considered_new?).to be(true)
-    end
-
-    it "returns false for new users" do
-      user.created_at = 1.year.ago
-      expect(user.decorate.considered_new?).to be(false)
+    it "delegates to Settings::RateLimit.considered_new?" do
+      allow(Settings::RateLimit).to receive(:user_considered_new?).with(user: decorated_user).and_return(true)
+      expect(decorated_user.considered_new?).to be(true)
+      expect(Settings::RateLimit).to have_received(:user_considered_new?).with(user: decorated_user)
     end
   end
 end

commit 19dd1abbc2164de46caa63d7dbc057578c38b02b

Fixing broken logic
In #15412 I introduced faulty logic, and didn't reuse existing scopes.

For comparison, the original code for comment threshold checks was here:
https://github.com/forem/forem/blob/5d5dbaca95d1fa944a687e07709c530ed7cfbd62/app/models/comment.rb#L303

```ruby
Reaction.comment_vomits.where(reactable_id: user.comments.pluck(:id)).size > 2
```

This change restores that and tightens up the scope usage.

app/models/reaction.rb
@@ -82,7 +82,7 @@ class Reaction < ApplicationRecord
     # @return [TrueClass] yup, they're spamming the system.
     # @return [FalseClass] they're not (yet) spamming the system
     def user_has_been_given_too_many_spammy_article_reactions?(user:, threshold: 2)
-      article_vomits.where(reactable_type: "Article", reactable_id: user.articles.ids).size > threshold
+      article_vomits.where(reactable_id: user.articles.ids).size > threshold
     end
 
     # @param user [User] the user who might be spamming the system
@@ -90,7 +90,7 @@ class Reaction < ApplicationRecord
     # @return [TrueClass] yup, they're spamming the system.
     # @return [FalseClass] they're not (yet) spamming the system
     def user_has_been_given_too_many_spammy_comment_reactions?(user:, threshold: 2)
-      article_vomits.where(reactable_type: "Comment", reactable_id: user.comments.ids).size > threshold
+      comment_vomits.where(reactable_id: user.comments.ids).size > threshold
     end
   end
 