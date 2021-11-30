7d9b6058fd66066ab646476304c144b7080efb02
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-11-30 11:44:50 -0500

    Renaming and documenting variables/constants

--- app/services/articles/feeds.rb
+++ app/services/articles/feeds.rb
@@ -2,8 +2,16 @@ module Articles
   module Feeds
     # The default number of days old that an article can be for us
     # to consider it in the relevance feed.
-    DEFAULT_PUBLISHED_SINCE = 7
+    #
+    # @note I believe that it is likely we would extract this constant
+    #       into an adminsitrative setting.  Hence, I want to keep it
+    #       a scalar.
+    DEFAULT_DAYS_SINCE_PUBLISHED = 7
 
+    # @note I believe that it is likely we would extract this constant
+    #       into an administrative setting.  Hence, I want to keep it
+    #       a scalar to ease the implementation details of the admin
+    #       setting.
     NUMBER_OF_HOURS_TO_OFFSET_USERS_LATEST_ARTICLE_VIEWS = 18
 
     # @api private
@@ -19,21 +27,21 @@ module Articles
     #       relevant.
     #
     # @param user [User]
-    # @param published_since [Integer] if someone
+    # @param days_since_published [Integer] if someone
     #        hasn't viewed any articles, give them things from the
     #        database seeds.
     #
     # @return [ActiveSupport::TimeWithZone]
     #
-    # @note the published_since is something carried
+    # @note the days_since_published is something carried
     #       over from the LargeForemExperimental and may not be
     #       relevant given that we have the :daily_factor_decay.
     #       However, this further limitation based on a user's
     #       second most recent page view helps further winnow down
     #       the result set.
-    def self.oldest_published_at_to_consider_for(user:, published_since: DEFAULT_PUBLISHED_SINCE)
+    def self.oldest_published_at_to_consider_for(user:, days_since_published: DEFAULT_DAYS_SINCE_PUBLISHED)
       time_of_second_latest_page_view = user&.page_views&.second_to_last&.created_at
-      return published_since.days.ago unless time_of_second_latest_page_view
+      return days_since_published.days.ago unless time_of_second_latest_page_view
 
       time_of_second_latest_page_view - NUMBER_OF_HOURS_TO_OFFSET_USERS_LATEST_ARTICLE_VIEWS.hours
     end
