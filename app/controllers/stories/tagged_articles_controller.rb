66c9b92059108daf7626293d4bf34a4a6324a214
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-11-30 15:26:34 -0500

    Refactoring to favor parameters over ivars
    Yes, ivars are useful to allow for implicit state.  However, by favoring
    parameters its easier to notice temporal dependencies in method calls.
    
    This helps ensure that we're calling methods in the right order.

--- app/controllers/stories/tagged_articles_controller.rb
+++ app/controllers/stories/tagged_articles_controller.rb
@@ -18,8 +18,8 @@ module Stories
       @article_index = true
       @moderators = User.with_role(:tag_moderator, @tag).select(:username, :profile_image, :id)
 
-      set_number_of_articles
-      set_stories
+      set_number_of_articles(tag: @tag)
+      set_stories(number_of_articles: number_of_articles, tag: @tag, page: @page)
       not_found_if_not_established(page: @page, tag: @tag, stories: @stories)
 
       set_surrogate_key_header "articles-#{@tag}"
@@ -30,45 +30,45 @@ module Stories
 
     private
 
-    def set_number_of_articles
-      @num_published_articles = if @tag.requires_approval?
-                                  @tag.articles.published.where(approved: true).count
+    def set_number_of_articles(tag:)
+      @num_published_articles = if tag.requires_approval?
+                                  tag.articles.published.where(approved: true).count
                                 elsif Settings::UserExperience.feed_strategy == "basic"
-                                  tagged_count
+                                  tagged_count(tag: tag)
                                 else
                                   Rails.cache.fetch("article-cached-tagged-count-#{@tag.name}", expires_in: 2.hours) do
-                                    tagged_count
+                                    tagged_count(tag: tag)
                                   end
                                 end
 
       @number_of_articles = user_signed_in? ? 5 : SIGNED_OUT_RECORD_COUNT
     end
 
-    def set_stories
-      @stories = Articles::Feeds::Tag.call(@tag, number_of_articles: @number_of_articles, page: @page)
+    def set_stories(number_of_articles:, page:, tag:)
+      stories = Articles::Feeds::Tag.call(tag, number_of_articles: number_of_articles, page: page)
 
-      @stories = @stories.where(approved: true) if @tag.requires_approval?
+      stories = stories.where(approved: true) if tag.requires_approval?
 
-      @stories = stories_by_timeframe
-      @stories = @stories.decorate
+      stories = stories_by_timeframe(stories: stories)
+      @stories = stories.decorate
     end
 
-    def tagged_count
-      @tag.articles.published.where("score >= ?", Settings::UserExperience.tag_feed_minimum_score).count
+    def tagged_count(tag:)
+      tag.articles.published.where("score >= ?", Settings::UserExperience.tag_feed_minimum_score).count
     end
 
-    def not_found_if_not_established
-      not_found if @stories.none? && !@tag.supported?
+    def not_found_if_not_established(stories:, tag:)
+      not_found if stories.none? && !tag.supported?
     end
 
-    def stories_by_timeframe
+    def stories_by_timeframe(stories:)
       if %w[week month year infinity].include?(params[:timeframe])
-        @stories.where("published_at > ?", Timeframe.datetime(params[:timeframe]))
+        stories.where("published_at > ?", Timeframe.datetime(params[:timeframe]))
           .order(public_reactions_count: :desc)
       elsif params[:timeframe] == "latest"
-        @stories.where("score > ?", -20).order(published_at: :desc)
+        stories.where("score > ?", -20).order(published_at: :desc)
       else
-        @stories.order(hotness_score: :desc).where("score >= ?", Settings::UserExperience.home_feed_minimum_score)
+        stories.order(hotness_score: :desc).where("score >= ?", Settings::UserExperience.home_feed_minimum_score)
       end
     end
   end
