4465e8a1342eed94b6542875a5ae8fedf5cfc3e3
Paweł Ludwiczak <ludwiczakpawel@gmail.com>
2021-11-30 21:02:46 +0100

    buttonnew as button

--- app/javascript/crayons/Tabs/Tab.jsx
+++ app/javascript/crayons/Tabs/Tab.jsx
@@ -1,15 +1,9 @@
 import { h } from 'preact';
 import classNames from 'classnames/bind';
-import { ButtonNew, Link } from '@crayons';
+import { ButtonNew as Button, Link } from '@crayons';
 
 export const Tab = (props) => {
-  const {
-    className,
-    current,
-    elements,
-    fitted,
-    ...otherProps
-  } = props;
+  const { className, current, elements, fitted, ...otherProps } = props;
 
   const classes = classNames('c-tab', {
     'c-tab--fitted': fitted,
@@ -17,13 +11,15 @@ export const Tab = (props) => {
   });
 
   const sharedProps = {
-    className: classes
-  }
-  const buttonCurrentProps = current ? {'aria-pressed': true} : {'aria-pressed': false};
-  const linkCurrentProps = current && {'aria-current': 'page'};
+    className: classes,
+  };
+  const buttonCurrentProps = current
+    ? { 'aria-pressed': true }
+    : { 'aria-pressed': false };
+  const linkCurrentProps = current && { 'aria-current': 'page' };
 
   return elements === 'buttons' ? (
-    <ButtonNew {...sharedProps} {...buttonCurrentProps} {...otherProps} />
+    <Button {...sharedProps} {...buttonCurrentProps} {...otherProps} />
   ) : (
     <Link block {...sharedProps} {...linkCurrentProps} {...otherProps} />
   );
