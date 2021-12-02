917b6f0ddff7e0d7c2427299fcdf905670a82ba3
Paweł Ludwiczak <ludwiczakpawel@gmail.com>
2021-12-02 21:56:13 +0100

    spec

--- cypress/integration/seededFlows/topNavigationFlows/MemberDropDownButton.spec.js
+++ cypress/integration/seededFlows/topNavigationFlows/MemberDropDownButton.spec.js
@@ -11,8 +11,8 @@ describe('Member Menu Dropdown', () => {
     });
   });
 
-  it('hovering on User Avatar should show User Dropdown menu', () => {
-    cy.findByRole('button', { name: 'Navigation menu' }).trigger('mouseover');
+  it('clicking on User Avatar should show User Dropdown menu', () => {
+    cy.findByRole('button', { name: 'Navigation menu' }).click();
     cy.get('#crayons-header__menu__dropdown__list').within(() => {
       cy.findByRole('link', { name: 'Dashboard' });
       cy.findByRole('link', { name: 'Create Post' });
@@ -23,7 +23,7 @@ describe('Member Menu Dropdown', () => {
   });
 
   it('pressing escape should close the User Dropdown menu', () => {
-    cy.findByRole('button', { name: 'Navigation menu' }).trigger('mouseover');
+    cy.findByRole('button', { name: 'Navigation menu' }).click();
     cy.findByRole('link', { name: 'Dashboard' })
       .as('dashboard')
       .should('be.visible');
@@ -32,40 +32,8 @@ describe('Member Menu Dropdown', () => {
   });
 
   it('close menu on clicking', () => {
-    cy.findByRole('button', { name: 'Navigation menu' }).trigger('mouseover');
+    cy.findByRole('button', { name: 'Navigation menu' }).click();
     cy.findByRole('link', { name: 'Dashboard' }).as('dashboard').click();
     cy.get('@dashboard').should('not.be.visible');
-  });
-
-  it('if User profile is already hovered, clicking on it again should not close it', () => {
-    cy.findByRole('button', { name: 'Navigation menu' }).as(
-      'memberDropDownButton',
-    );
-    cy.get('@memberDropDownButton').trigger('mouseover');
-    cy.findByRole('link', { name: 'Dashboard' }).as('dashboard');
-    cy.get('@memberDropDownButton').click();
-    cy.get('@dashboard').should('be.visible');
-  });
-
-  it('hovering out should close menu', () => {
-    cy.findByRole('button', { name: 'Navigation menu' }).as(
-      'memberDropDownButton',
-    );
-    cy.get('@memberDropDownButton').trigger('mouseover');
-    cy.findByRole('link', { name: 'Dashboard' }).as('dashboard');
-    cy.get('@memberDropDownButton').trigger('mouseout');
-    cy.get('@dashboard').should('not.be.visible');
-  });
-
-  it('Dropdown menu opened by click will not close on hovering out', () => {
-    cy.findByRole('button', { name: 'Navigation menu' }).as(
-      'memberDropDownButton',
-    );
-    cy.get('@memberDropDownButton').trigger('mouseover').click();
-    cy.findByRole('link', { name: 'Dashboard' })
-      .as('dashboard')
-      .should('be.visible');
-    cy.get('@memberDropDownButton').trigger('mouseout');
-    cy.get('@dashboard').should('be.visible');
   });
 });
