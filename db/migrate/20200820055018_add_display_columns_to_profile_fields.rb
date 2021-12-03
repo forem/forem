8d1158c85a3e78728761ea3079761e3778600c53
Josh Soref <jsoref@users.noreply.github.com>
2021-12-03 01:04:04 -0500

    spelling: columns
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- db/migrate/20200820055018_add_display_columns_to_profile_fields.rb
+++ db/migrate/20200820055018_add_display_columns_to_profile_fields.rb
@@ -1,4 +1,4 @@
-class AddDisplayColumsToProfileFields < ActiveRecord::Migration[6.0]
+class AddDisplayColumnsToProfileFields < ActiveRecord::Migration[6.0]
   def change
     add_column :profile_fields, :display_area, :integer, null: false, default: true
     add_column :profile_fields, :show_in_onboarding, :boolean, null: false, default: false
