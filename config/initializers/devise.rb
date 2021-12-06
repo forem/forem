44f035d147276315d991a3b29dcdcf0bdb2ba0f1
Josh Soref <jsoref@users.noreply.github.com>
2021-12-05 23:56:15 -0500

    Spelling fixes to live code

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ TWITTER_OMNIAUTH_SETUP = lambda do |env|
   env["omniauth.strategy"].options[:consumer_secret] = Settings::Authentication.twitter_secret
 end
 
-GITHUB_OMNIUATH_SETUP = lambda do |env|
+GITHUB_OMNIAUTH_SETUP = lambda do |env|
   env["omniauth.strategy"].options[:client_id] = Settings::Authentication.github_key
   env["omniauth.strategy"].options[:client_secret] = Settings::Authentication.github_secret
   env["omniauth.strategy"].options[:scope] = "user:email"
@@ -320,7 +320,7 @@ Devise.setup do |config|
 
   # Fun fact, unless Twitter is last, it doesn't work for some reason.
   config.omniauth :facebook, setup: FACEBOOK_OMNIAUTH_SETUP
-  config.omniauth :github, setup: GITHUB_OMNIUATH_SETUP
+  config.omniauth :github, setup: GITHUB_OMNIAUTH_SETUP
   config.omniauth :apple, setup: APPLE_OMNIAUTH_SETUP
   config.omniauth :forem, setup: FOREM_OMNIAUTH_SETUP, strategy_class: OmniAuth::Strategies::Forem
   config.omniauth :twitter, setup: TWITTER_OMNIAUTH_SETUP
