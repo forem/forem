4cd15cafafa035c873ad6f181936d9884511d50c
Ridhwana <ridhwana.khan16@gmail.com>
2021-12-07 13:08:04 +0200

    refactor: use self

--- app/forms/creator_settings_form.rb
+++ app/forms/creator_settings_form.rb
@@ -21,26 +21,26 @@ class CreatorSettingsForm
   def initialize(attributes = {})
     super
 
-    @checked_code_of_conduct = checked_code_of_conduct
-    @checked_terms_and_conditions = checked_terms_and_conditions
-    @community_name = community_name
-    @invite_only_mode = invite_only_mode
-    @logo = logo
-    @primary_brand_color_hex = primary_brand_color_hex
-    @public = public
+    self.checked_code_of_conduct = checked_code_of_conduct
+    self.checked_terms_and_conditions = checked_terms_and_conditions
+    self.community_name = community_name
+    self.invite_only_mode = invite_only_mode
+    self.logo = logo
+    self.primary_brand_color_hex = primary_brand_color_hex
+    self.public = public
   end
 
   def save
     return false unless valid?
 
-    ::Settings::Community.community_name = @community_name
-    ::Settings::UserExperience.primary_brand_color_hex = @primary_brand_color_hex
-    ::Settings::Authentication.invite_only_mode = @invite_only
-    ::Settings::UserExperience.public = @public
+    ::Settings::Community.community_name = community_name
+    ::Settings::UserExperience.primary_brand_color_hex = primary_brand_color_hex
+    ::Settings::Authentication.invite_only_mode = invite_only
+    ::Settings::UserExperience.public = public
 
-    return unless @logo
+    return unless logo
 
-    logo_uploader = upload_logo(@logo)
+    logo_uploader = upload_logo(logo)
     ::Settings::General.original_logo = logo_uploader.url
     ::Settings::General.resized_logo = logo_uploader.resized_logo.url
   end
