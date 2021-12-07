c4238a37e2f1ae87f1eb6621229c7500324dc877
Suzanne Aitchison <suzanne@forem.com>
2021-12-07 12:32:54 +0000

    remove duplicated tests now in Cypress, enhance Cypress nav menu tests

--- app/javascript/topNavigation/__tests__/utilities.test.js
+++ app/javascript/topNavigation/__tests__/utilities.test.js
@@ -1,16 +1,10 @@
 // TODO: Once this is merged, PR up the removal of this and in other tests and move it to testSetup.js
 import '@testing-library/jest-dom';
-import {
-  getByTestId,
-  getByLabelText,
-  fireEvent,
-  waitFor,
-} from '@testing-library/dom';
+import { getByLabelText } from '@testing-library/dom';
 import {
   getInstantClick,
   initializeMobileMenu,
   setCurrentPageIconLink,
-  initializeMemberMenu,
 } from '../utilities';
 
 // TODO: ★★★ These tests should be promoted to E2E tests once we have that in place. ★★★
@@ -88,109 +82,6 @@ describe('top navigation utilitities', () => {
       setCurrentPageIconLink('page-1', pageEntries);
 
       expect(notificationsLink).toHaveAttribute('aria-current', 'page');
-    });
-  });
-
-  describe('initializeMemberMenu', () => {
-    const initialMenuHTML = `
-      <div class="crayons-header__menu mx-1" id="crayons-header__menu" data-testid="menu-dropdown">
-        <button type="button" class="c-btn c-btn--icon-alone radius-full p-1" id="member-menu-button" aria-expanded="false" aria-label="Navigation menu">
-          <span class="crayons-avatar crayons-avatar--l"><img class="crayons-avatar__image" alt="" id="nav-profile-image" src=""></span>
-        </button>
-        <div class="crayons-dropdown left-2 right-2 s:left-auto crayons-header__menu__dropdown inline-block m:-mr-2 top-100">
-          <ul class="p-0" id="crayons-header__menu__dropdown__list">
-            <li id="user-profile-link-placeholder" class="border-0 border-b-1 border-solid border-base-20 p-1 mb-1">
-              <a id="first-nav-link" class="c-link c-link--block" href="#">
-              </a>
-            </li>
-            <li class="px-1 py-1">
-              <a href="/signout_confirm" class="c-link c-link--block" id="last-nav-link">Sign Out</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    `;
-
-    it('toggles dropdown menu expanded state', async () => {
-      document.body.innerHTML = initialMenuHTML;
-
-      const menuNavButton = getByLabelText(document.body, 'Navigation menu');
-      const memberMenu = getByTestId(document.body, 'menu-dropdown');
-
-      initializeMemberMenu(memberMenu, menuNavButton);
-
-      await waitFor(() =>
-        expect(menuNavButton.getAttribute('aria-expanded')).toEqual('false'),
-      );
-      expect(menuNavButton.getAttribute('aria-expanded')).toEqual('false');
-      expect(memberMenu).not.toHaveClass('showing');
-
-      fireEvent.click(menuNavButton);
-
-      expect(menuNavButton.getAttribute('aria-expanded')).toEqual('true');
-      expect(memberMenu).toHaveClass('showing');
-
-      fireEvent.click(menuNavButton);
-
-      expect(menuNavButton.getAttribute('aria-expanded')).toEqual('false');
-      expect(memberMenu).not.toHaveClass('showing');
-    });
-
-    it('closes menu on Escape key', async () => {
-      document.body.innerHTML = initialMenuHTML;
-
-      const menuNavButton = getByLabelText(document.body, 'Navigation menu');
-      const memberMenu = getByTestId(document.body, 'menu-dropdown');
-
-      initializeMemberMenu(memberMenu, menuNavButton);
-
-      await waitFor(() =>
-        expect(menuNavButton.getAttribute('aria-expanded')).toEqual('false'),
-      );
-
-      fireEvent.click(menuNavButton);
-
-      expect(menuNavButton.getAttribute('aria-expanded')).toEqual('true');
-      expect(memberMenu).toHaveClass('showing');
-
-      fireEvent.keyUp(memberMenu, { key: 'Escape' });
-
-      await waitFor(() =>
-        expect(menuNavButton.getAttribute('aria-expanded')).toEqual('false'),
-      );
-
-      expect(memberMenu).not.toHaveClass('showing');
-    });
-
-    it('closes the menu on click outside', async () => {
-      document.body.innerHTML = `
-        <div data-testid="container">
-          ${initialMenuHTML}
-        </div>
-      `;
-
-      const menuNavButton = getByLabelText(document.body, 'Navigation menu');
-      const memberMenu = getByTestId(document.body, 'menu-dropdown');
-
-      initializeMemberMenu(memberMenu, menuNavButton);
-
-      await waitFor(() =>
-        expect(menuNavButton.getAttribute('aria-expanded')).toEqual('false'),
-      );
-
-      fireEvent.click(menuNavButton);
-
-      expect(menuNavButton.getAttribute('aria-expanded')).toEqual('true');
-      expect(memberMenu).toHaveClass('showing');
-
-      // Click on a link in the menu to close load a page and close the menu.
-      memberMenu.querySelector('a').click();
-
-      await waitFor(() =>
-        expect(menuNavButton.getAttribute('aria-expanded')).toEqual('false'),
-      );
-
-      expect(memberMenu).not.toHaveClass('showing');
     });
   });
 });
--- cypress/integration/seededFlows/topNavigationFlows/MemberDropDownButton.spec.js
+++ cypress/integration/seededFlows/topNavigationFlows/MemberDropDownButton.spec.js
@@ -11,27 +11,67 @@ describe('Member Menu Dropdown', () => {
     });
   });
 
-  it('clicking on User Avatar should show User Dropdown menu', () => {
-    cy.findByRole('button', { name: 'Navigation menu' }).click();
+  it('clicking on User Avatar should open and close User Dropdown menu', () => {
+    cy.findByRole('button', { name: 'Navigation menu' }).as('menuButton');
+    cy.get('@menuButton')
+      .should('have.attr', 'aria-expanded', 'false')
+      .click()
+      .should('have.attr', 'aria-expanded', 'true');
+
     cy.get('#crayons-header__menu__dropdown__list').within(() => {
+      cy.findByRole('link', {
+        name: 'Article Editor v1 User @article_editor_v1_user',
+      }).should('have.focus');
+
       cy.findByRole('link', { name: 'Dashboard' });
       cy.findByRole('link', { name: 'Create Post' });
       cy.findByRole('link', { name: 'Reading list' });
       cy.findByRole('link', { name: 'Settings' });
       cy.findByRole('link', { name: 'Sign Out' });
     });
+
+    cy.get('@menuButton')
+      .click()
+      .should('have.attr', 'aria-expanded', 'false')
+      .should('have.focus');
   });
 
   it('pressing escape should close the User Dropdown menu', () => {
-    cy.findByRole('button', { name: 'Navigation menu' }).click();
+    cy.findByRole('button', { name: 'Navigation menu' }).as('menuButton');
+    cy.get('@menuButton')
+      .should('have.attr', 'aria-expanded', 'false')
+      .click()
+      .should('have.attr', 'aria-expanded', 'true');
+
     cy.findByRole('link', { name: 'Dashboard' })
       .as('dashboard')
       .should('be.visible');
+
     cy.get('body').type('{esc}');
     cy.get('@dashboard').should('not.be.visible');
+    cy.get('@menuButton')
+      .should('have.attr', 'aria-expanded', 'false')
+      .should('have.focus');
   });
 
-  it('close menu on clicking', () => {
+  it('closes menu on click outside', () => {
+    cy.findByRole('button', { name: 'Navigation menu' }).as('menuButton');
+    cy.get('@menuButton')
+      .should('have.attr', 'aria-expanded', 'false')
+      .click()
+      .should('have.attr', 'aria-expanded', 'true');
+
+    cy.findByRole('link', { name: 'Dashboard' })
+      .as('dashboard')
+      .should('be.visible');
+
+    cy.get('body').click('topLeft');
+
+    cy.get('@dashboard').should('not.be.visible');
+    cy.get('@menuButton').should('have.attr', 'aria-expanded', 'false');
+  });
+
+  it('closes menu on selecting a link from the menu', () => {
     cy.findByRole('button', { name: 'Navigation menu' }).click();
     cy.findByRole('link', { name: 'Dashboard' }).as('dashboard').click();
     cy.get('@dashboard').should('not.be.visible');
