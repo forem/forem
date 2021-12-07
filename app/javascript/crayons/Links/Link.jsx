db75deaa871ced6ecb6351f611b830776b8e326a
Paweł Ludwiczak <ludwiczakpawel@gmail.com>
2021-12-07 12:02:36 +0100

    proptype

--- app/javascript/crayons/Links/Link.jsx
+++ app/javascript/crayons/Links/Link.jsx
@@ -49,5 +49,5 @@ Link.propTypes = {
   href: PropTypes.string.isRequired,
   className: PropTypes.string,
   children: defaultChildrenPropTypes,
-  icon: PropTypes.ReactNode,
+  icon: PropTypes.elementType,
 };
