786d8414c2525afb97145960f15c7d9da408d76c
Dan Uber <dan@forem.com>
2021-12-09 12:14:15 -0600

    Don't raise NoMethodError when validating emoji settings
    Cargo culted the unique cross model slug validator setup (which is why
    value is called name here in the validatable).
    
    Don't raise an error when validating a nil emoji, and assert nil and
    empty string are permitted values.
    
    This expects no non-emoji characters (so an empty string would be
    permitted), and nil should be permitted.
    
    I believe this might have been introduced by the string settings
    cleaner (exchanging "" for nil in form inputs).

--- app/validators/emoji_only_validator.rb
+++ app/validators/emoji_only_validator.rb
@@ -2,7 +2,7 @@ class EmojiOnlyValidator < ActiveModel::EachValidator
   DEFAULT_MESSAGE = "contains non-emoji characters or invalid emoji".freeze
 
   def validate_each(record, attribute, value)
-    return if value.gsub(EmojiRegex::RGIEmoji, "").blank?
+    return if !value || value.gsub(EmojiRegex::RGIEmoji, "").blank?
 
     record.errors.add(attribute, options[:message] || DEFAULT_MESSAGE)
   end
