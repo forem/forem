2148784b3f2704166396a3b7d9277f0ce07426e5
Josh Soref <2119212+jsoref@users.noreply.github.com>
2021-12-13 08:35:24 -0500

    Spelling db migrate (#15703)
    * spelling: active
    
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>
    
    * spelling: advertisements
    
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>
    
    * spelling: columns
    
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>
    
    * spelling: identities
    
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>
    
    * spelling: index
    
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>
    
    * spelling: secret
    
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>
    
    * spelling: username
    
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>
    
    Co-authored-by: Josh Soref <jsoref@users.noreply.github.com>

--- db/migrate/20190401100844_add_unique_index_to_users_twitter_username.rb
+++ db/migrate/20190401100844_add_unique_index_to_users_twitter_username.rb
@@ -1,4 +1,4 @@
-class AddUniqueIndexToUsersTwitterUsename < ActiveRecord::Migration[5.1]
+class AddUniqueIndexToUsersTwitterUsername < ActiveRecord::Migration[5.1]
   def change
     add_index :users, :twitter_username, unique: true
   end
