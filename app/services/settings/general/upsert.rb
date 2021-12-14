3b0a6796fe87b96b186c40a5cf0c433673b46be1
Nick Taylor <nick@dev.to>
2021-12-14 11:47:19 -0500

    Refactored logo upload into Upsert.

--- app/services/settings/general/upsert.rb
+++ app/services/settings/general/upsert.rb
@@ -5,7 +5,12 @@ module Settings
       TAG_PARAMS = %w[sidebar_tags suggested_tags].freeze
 
       def self.call(settings)
-        cleaned_params = clean_params(settings)
+        if settings[:logo].present?
+          logo_uploader = upload_logo(settings[:logo])
+          logo_settings = { original_logo: logo_uploader.url, resized_logo: logo_uploader.resized_logo.url }
+        end
+
+        cleaned_params = clean_params((logo_settings.nil? ? settings : settings.merge(logo_settings)).except(:logo))
         result = ::Settings::Upsert.call(cleaned_params, ::Settings::General)
         return result unless result.success?
 
@@ -28,6 +33,12 @@ module Settings
 
         tags = Settings::General.suggested_tags + Settings::General.sidebar_tags
         Tag.find_or_create_all_with_like_by_name(tags)
+      end
+
+      def self.upload_logo(image)
+        LogoUploader.new.tap do |uploader|
+          uploader.store!(image)
+        end
       end
     end
   end
