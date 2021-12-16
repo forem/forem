97d2b4191f37d21a74209046948633ab54e4b284
Mac Siri <krairit.siri@gmail.com>
2021-12-16 11:04:46 -0500

    Update logic

--- lib/cypress-rails/finds_bin.rb
+++ lib/cypress-rails/finds_bin.rb
@@ -1,7 +1,7 @@
 module CypressRails
   class FindsBin
     def call(_dir = Dir.pwd)
-      if ENV["KNAPSACK_PRO_CI_NODE_TOTAL"].present?
+      if ENV["KNAPSACK_PRO_CI_NODE_TOTAL"].present? && ENV["KNAPSACK_PRO_CI_NODE_INDEX"].present?
         "node_modules/.bin/knapsack-pro-cypress"
       else
         "node_modules/.bin/cypress"
