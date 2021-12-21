a3d0236bab1c1ad9bcb285ad7261e860c2d88ad7
Suzanne Aitchison <suzanne@forem.com>
2021-12-21 10:08:50 +0000

    only unpack badge image if badge exists

--- app/serializers/search/tag_serializer.rb
+++ app/serializers/search/tag_serializer.rb
@@ -2,7 +2,9 @@ module Search
   class TagSerializer < ApplicationSerializer
     attributes :id, :name, :hotness_score, :supported, :short_summary, :rules_html, :bg_color_hex
     attribute :badge do |tag|
-      { badge_image: tag.badge.badge_image }
+      if tag.badge
+        { badge_image: tag.badge.badge_image }
+      end
     end
   end
 end
