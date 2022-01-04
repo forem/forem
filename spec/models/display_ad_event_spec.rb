81f782e9bb718592dc9573f8cf2a22ca3c282b66
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2022-01-04 09:11:28 -0500

    Extracting constants and tidying tests
    Prior to this commit, we had a couple of references to magic strings
    regarding DisplayAdEvent objects.  This commit seeks to consolidate that
    behavior.
    
    In a handful of cases we had javascript variables, but as these JS files
    are `erb` templates, we can inject the constant as well.  This reduces
    repetition of knowledge.

--- spec/models/display_ad_event_spec.rb
+++ spec/models/display_ad_event_spec.rb
@@ -1,24 +1,7 @@
 require "rails_helper"
 
 RSpec.describe DisplayAdEvent, type: :model do
-  let(:user) { build(:user) }
-  let(:organization) { build(:organization) }
-  let(:display_ad) { build(:display_ad, organization: organization) }
+  it { is_expected.to validate_inclusion_of(:category).in_array(described_class::VALID_CATEGORIES) }
 
-  describe "#category" do
-    it "is valid with a click category" do
-      event = build(:display_ad_event, category: "click", user: user, display_ad: display_ad)
-      expect(event).to be_valid
-    end
-
-    it "is valid with an impression category" do
-      event = build(:display_ad_event, category: "impression", user: user, display_ad: display_ad)
-      expect(event).to be_valid
-    end
-
-    it "is not valid with an unknown category" do
-      event = build(:display_ad_event, category: "wazoo", user: user, display_ad: display_ad)
-      expect(event).not_to be_valid
-    end
-  end
+  it { is_expected.to validate_inclusion_of(:context_type).in_array(described_class::VALID_CONTEXT_TYPES) }
 end
