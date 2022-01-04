afd5a9f8d04676439f2f10a060a19694b7c4558a
Michael Kohl <me@citizen428.net>
2022-01-04 12:07:27 +0700

    Update stats initializer autoloading

--- config/initializers/stats.rb
+++ config/initializers/stats.rb
@@ -1,1 +1,4 @@
-ForemStatsClient = ForemStatsDriver.new
+# See: https://guides.rubyonrails.org/v7.0/autoloading_and_reloading_constants.html#use-case-1-during-boot-load-reloadable-code
+Rails.application.config.to_prepare do
+  ForemStatsClient = ForemStatsDriver.new # rubocop:disable Lint/ConstantDefinitionInBlock
+end
