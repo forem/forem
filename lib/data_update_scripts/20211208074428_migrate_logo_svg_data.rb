7d98daa41df3daceebcf220867e30c6f83031835
Ridhwana <ridhwana.khan16@gmail.com>
2022-01-05 13:26:41 +0200

    feat: use the good practices for tempfile, update error handling and update the tests

--- lib/data_update_scripts/20211208074428_migrate_logo_svg_data.rb
+++ lib/data_update_scripts/20211208074428_migrate_logo_svg_data.rb
@@ -4,22 +4,27 @@ module DataUpdateScripts
       return if ::Settings::General.try(:logo_svg).blank? ||
         (::Settings::General.try(:original_logo).present? && ::Settings::General.try(:resized_logo).present?)
 
-      temp_svg = Tempfile.new(["logo", ".svg"])
-      temp_svg.write(::Settings::General.logo_svg)
-      temp_svg.close
+      Tempfile.create(["logo", ".svg"]) do |file|
+        file.write(::Settings::General.logo_svg)
 
-      logo_svg_uploader = LogoSvgUploader.new.tap do |uploader|
-        uploader.store!(temp_svg)
+        logo_svg_uploader = LogoSvgUploader.new.tap do |uploader|
+          uploader.store!(file)
+        end
+
+        ::Settings::General.original_logo = logo_svg_uploader.url
+        ::Settings::General.resized_logo = logo_svg_uploader.resized_logo.url
       end
-
-      ::Settings::General.original_logo = logo_svg_uploader.url
-      ::Settings::General.resized_logo = logo_svg_uploader.resized_logo.url
     rescue StandardError => e
       # If we can't convert the logo, then alert ourselves so that we can track the Forems
-      # whose logos could not be converted
-      Rails.logger.error("Could not convert logo_svg for #{Settings::Community.community_name} Forem to a PNG")
-      Honeybadger.notify(e)
-      ForemStatsClient.increment("logo_svg.conversion")
+      # whose logos could not be converted.
+
+      Rails.logger.error("Could not convert logo_svg for #{Settings::Community.community_name} Forem to a PNG.")
+
+      Honeybadger.notify(e, context: {
+                           community_name: Settings::Community.community_name,
+                           app_domain: Settings::General.app_domain,
+                           tags: "failed_svg_conversion"
+                         })
     end
   end
 end
