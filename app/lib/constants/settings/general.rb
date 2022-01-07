42afc81def7112c2dedcbda5f5b2c3ea42b9b123
Ridhwana <ridhwana.khan16@gmail.com>
2022-01-07 12:50:40 +0200

    feat: update the images section

--- app/lib/constants/settings/general.rb
+++ app/lib/constants/settings/general.rb
@@ -36,11 +36,13 @@ module Constants
           placeholder: "a secure token"
         },
         logo_png: {
-          description: "Used as a fallback to the SVG. Recommended minimum of 512x512px",
+          description: "Used as a secondary logo in places like the sign in modals, onboarding, apple touch icons." \
+                       "Recommended minimum of 512x512px",
           placeholder: IMAGE_PLACEHOLDER
         },
         logo_svg: {
-          description: "Used as the SVG logo of the community",
+          description: "Currently being used on the upper left hand corner of your Forem," \
+                       "however it will be deprecated soon in favour of Logo above.",
           placeholder: SVG_PLACEHOLDER
         },
         main_social_image: {
