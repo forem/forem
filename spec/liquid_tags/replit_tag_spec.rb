22a4befa5578b309b888a8a0ff0d179527d75502
Arit Amana <msarit@gmail.com>
2022-01-07 10:06:26 -0500

    add error spec

--- spec/liquid_tags/replit_tag_spec.rb
+++ spec/liquid_tags/replit_tag_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe ReplitTag, type: :liquid_tag do
   describe "#id" do
     let(:replit_id) { "@WigWog/PositiveFineOpensource" }
+    let(:invalid_replit_id) { "@Wig-Wog/PositiveFineOpensource" }
 
     def generate_new_liquid(id)
       Liquid::Template.register_tag("replit", ReplitTag)
@@ -18,6 +19,10 @@ RSpec.describe ReplitTag, type: :liquid_tag do
       liquid = generate_new_liquid(replit_id)
       expect(liquid.render).to include("<iframe")
       expect(liquid.render).to include("https://repl.it/#{replit_id}?lite=true")
+    end
+
+    it "raises an error for invalid replit id" do
+      expect { generate_new_liquid(invalid_replit_id).render }.to raise_error("Invalid Replit URL or @user/slug")
     end
   end
 end
