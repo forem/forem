b7c6309a0a5f22db84deb36b56b96db88837f758
Dan Uber <dan@forem.com>
2022-01-11 13:20:40 -0600

    Generator output
    Add your logic here!

---
+++ spec/lib/data_update_scripts/insert_apple_connect_broadcast_message_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+require Rails.root.join(
+  "lib/data_update_scripts/20220111192031_insert_apple_connect_broadcast_message.rb",
+)
+
+describe DataUpdateScripts::InsertAppleConnectBroadcastMessage do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
