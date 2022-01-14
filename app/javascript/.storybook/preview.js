7c07913aa6091df836a4f5429e06b8d601a37463
ludwiczakpawel <ludwiczakpawel@gmail.com>
2022-01-14 11:06:42 +0100

    Storybook improvements (#15860)
    * storybook backgrounds
    
    * variables for background
    
    * converting colors scss to css
    
    * prepare for theming
    
    * themes
    
    * Update app/javascript/.storybook/preview.js
    
    Co-authored-by: Suzanne Aitchison <suzanne@forem.com>
    
    * restart
    
    * restart
    
    Co-authored-by: Suzanne Aitchison <suzanne@forem.com>

--- app/javascript/.storybook/preview.js
+++ app/javascript/.storybook/preview.js
@@ -2,75 +2,21 @@ import { h } from 'preact';
 import { addDecorator, addParameters } from '@storybook/preact';
 import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
 import { jsxDecorator } from 'storybook-addon-jsx';
+import cssVariablesTheme from '@etchteam/storybook-addon-css-variables-theme';
 import 'focus-visible';
 
 import '../../assets/stylesheets/minimal.scss';
 import '../../assets/stylesheets/views.scss';
 import '../../assets/stylesheets/crayons.scss';
+import LightTheme from '!!style-loader?injectType=lazyStyleTag!css-loader!../../assets/stylesheets/config/_colors.css';
+import DarkTheme from '!!style-loader?injectType=lazyStyleTag!css-loader!../../assets/stylesheets/themes/dark.css';
 import '../../assets/javascripts/lib/xss';
 import '../../assets/javascripts/utilities/timeAgo';
 import './storybook.scss';
 
-function addStylesheet(theme = '') {
-  if (theme === '') {
-    return; // default theme
-  }
-
-  const head = document.head;
-  const link = document.createElement('link');
-
-  link.type = 'text/css';
-  link.rel = 'stylesheet';
-  link.href = `themes/${theme}.css`;
-  link.id = 'dev-theme';
-
-  head.appendChild(link);
-}
-
-function themeSwitcher(event) {
-  const themeNode = document.getElementById('dev-theme');
-  const theme = event.target.value;
-
-  if (themeNode) {
-    themeNode.parentElement.removeChild(themeNode);
-  }
-
-  localStorage.setItem('storybook-crayons-theme', theme);
-
-  addStylesheet(theme);
-}
-
-const THEMES = Object.freeze(['light', 'dark']);
-
-const themeSwitcherDecorator = (storyFn) => {
-  const lastThemeUsed = localStorage.getItem('storybook-crayons-theme') || '';
-
-  addStylesheet(lastThemeUsed);
-
-  return (
-    <div>
-      <label style={{ position: 'absolute', top: 0, left: 0, margin: '1rem' }}>
-        theme{' '}
-        <select onChange={themeSwitcher}>
-          {THEMES.map((theme) => (
-            <option
-              selected={lastThemeUsed === theme}
-              value={theme}
-              key={theme}
-            >
-              {theme}
-            </option>
-          ))}
-        </select>
-      </label>
-      <div id="story-content">{storyFn()}</div>
-    </div>
-  );
-};
-
 addDecorator(jsxDecorator);
-addDecorator(themeSwitcherDecorator);
 addDecorator((Story) => <Story />);
+addDecorator(cssVariablesTheme);
 
 addParameters({
   options: {
@@ -86,27 +32,8 @@ addParameters({
   },
 });
 
-export const Foo = () => <Component />;
-Foo.parameters = {
-  previewTabs: { 'storybook/docs/panel': { index: -1 } },
-};
-
 export const parameters = {
   controls: { expanded: true },
-  options: {
-    storySort: {
-      order: [
-        'Introduction',
-        'Component Library',
-        'Utility-First CSS',
-        'Writing CSS',
-        'Fundamentals',
-        'Components',
-        'App Components',
-        'Utility-First Classes',
-      ],
-    },
-  },
   jsx: {
     filterProps: (val) => val !== undefined,
     functionValue: (fn) => {
@@ -114,7 +41,26 @@ export const parameters = {
       return fn;
     },
   },
-  html: {
-    root: '#story-content',
+  cssVariables: {
+    files: {
+      LightTheme,
+      DarkTheme,
+    },
+  },
+  backgrounds: {
+    default: 'Card',
+    grid: {
+      disable: true,
+    },
+    values: [
+      {
+        name: 'Card',
+        value: 'var(--card-bg)',
+      },
+      {
+        name: 'Body',
+        value: 'var(--body-bg)',
+      },
+    ],
   },
 };
