1330c6c78e8803b7a477a26965866724025bd767
Suzanne Aitchison <suzanne@forem.com>
2022-01-14 11:57:02 +0000

    update cypress tests

--- app/services/search/tag.rb
+++ app/services/search/tag.rb
@@ -3,7 +3,11 @@ module Search
     ATTRIBUTES = %i[id name hotness_score rules_html supported short_summary bg_color_hex badge_id].freeze
 
     def self.search_documents(term)
-      results = ::Tag.search_by_name(term).supported.reorder(hotness_score: :desc).select(*ATTRIBUTES)
+      results = ::Tag.search_by_name(term)
+        .supported.reorder(hotness_score: :desc)
+        .select(*ATTRIBUTES)
+        .includes([:badge])
+
       serialize(results)
     end
 
