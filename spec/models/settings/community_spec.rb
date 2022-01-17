f6006f50fb0c3473fb0216e31a8099e279602e91
Julianna Tetreault <32834804+juliannatetreault@users.noreply.github.com>
2022-01-17 15:10:48 -0700

    Removes community_emoji-related specs from codebase

--- spec/models/settings/community_spec.rb
+++ spec/models/settings/community_spec.rb
@@ -16,18 +16,4 @@ RSpec.describe Settings::Community do
       end.not_to raise_error
     end
   end
-
-  describe "validating emojis strings" do
-    it "allows emoji-only strings" do
-      expect do
-        described_class.community_emoji = "💯"
-      end.not_to raise_error
-    end
-
-    it "rejects non emoji-only strings" do
-      expect do
-        described_class.community_emoji = "abc"
-      end.to raise_error(/contains non-emoji characters or invalid emoji/)
-    end
-  end
 end
