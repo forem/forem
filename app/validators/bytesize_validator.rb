be0e8e04ad10bec958d7ec04d6054fef17f1fb49
Yhëhtozr <conlang2012@outlook.com>
2022-01-19 00:43:50 +0900

    app/validators i18n

--- app/validators/bytesize_validator.rb
+++ app/validators/bytesize_validator.rb
@@ -7,14 +7,16 @@ class BytesizeValidator < ActiveModel::EachValidator
   CHECKS = { maximum: :<= }.freeze
   RESERVED_OPTIONS = %i[maximum too_long].freeze
 
-  ERROR_MISSING_OPTIONS_MESSAGE = "Specify the :maximum option.".freeze
-  ERROR_NON_NEGATIVE_MESSAGE = ":maximum must be a non-negative Integer".freeze
+  # seemingly unused value?
+  # ERROR_MISSING_OPTIONS_MESSAGE = I18n.t("validators.bytesize_validator.specify_the_maximum_option").freeze
 
   def check_validity!
     raise ArgumentError, ERROR_MESSAGE unless options.key?(:maximum)
 
     maximum = options[:maximum]
-    raise ArgumentError, ERROR_NON_NEGATIVE_MESSAGE unless maximum.is_a?(Integer) && maximum >= 0
+    return if maximum.is_a?(Integer) && maximum >= 0
+
+    raise ArgumentError, I18n.t("validators.bytesize_validator.maximum_must_be_a_non_neg")
   end
 
   def validate_each(record, attribute, value)
