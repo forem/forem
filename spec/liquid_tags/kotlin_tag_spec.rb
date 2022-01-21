ac4d987903983b32256a9ccb9be14ca8fc8ba523
Arit Amana <msarit@gmail.com>
2022-01-21 14:29:26 -0500

    correct spec mistake

--- spec/liquid_tags/kotlin_tag_spec.rb
+++ spec/liquid_tags/kotlin_tag_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe KotlinTag, type: :liquid_tag do
       it "generates correct liquid from url with one or more params" do
         liquid1 = generate_new_liquid(valid_url_one_param)
         expect(liquid1.render).to include("<iframe")
-        expect(liquid1.render).to include("src=\"https://pl.kotl.in/owreUFFUG?theme=darcula&amp;from=3&amp;to=6&amp;readOnly=true\"")
+        expect(liquid1.render).to include("src=\"https://pl.kotl.in/owreUFFUG?theme=darcula\"")
 
         liquid2 = generate_new_liquid(valid_url_multiple_params)
         expect(liquid2.render).to include("<iframe")
