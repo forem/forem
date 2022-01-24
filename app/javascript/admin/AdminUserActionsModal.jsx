e8c5e523cec8f2483bf566b0d12a68c8eeb1fb6b
Paweł Ludwiczak <ludwiczakpawel@gmail.com>
2022-01-24 22:28:52 +0100

    pass size

--- app/javascript/admin/AdminUserActionsModal.jsx
+++ app/javascript/admin/AdminUserActionsModal.jsx
@@ -2,12 +2,18 @@ import { h } from 'preact';
 import { useState } from 'preact/hooks';
 import { Modal } from '@crayons/Modal/Modal';
 
-export const AdminUserActionsModal = ({ title, children, open = true }) => {
+export const AdminUserActionsModal = ({
+  title,
+  size,
+  children,
+  open = true,
+}) => {
   const [isOpen, setIsOpen] = useState(open);
 
   return isOpen ? (
     <Modal
       title={title}
+      size={size}
       onClose={() => {
         setIsOpen(false);
       }}
