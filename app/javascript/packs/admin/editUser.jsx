908c48c0df512922ab666205375567652b0e72dc
Nick Taylor <nick@iamdeveloper.com>
2022-01-25 11:14:08 -0500

    Improved modal trigger implementation.

--- app/javascript/packs/admin/editUser.jsx
+++ app/javascript/packs/admin/editUser.jsx
@@ -1,10 +1,11 @@
-import { AdminUserActionsModal } from '@admin/AdminUserActionsModal';
 import { initializeDropdown } from '@utilities/dropdownUtils';
 
 let preact;
 let Modal;
 
-const modalTriggers = document.querySelectorAll('[data-modal-trigger]');
+// Append an empty div to the end of the document so that is does not affect the layout.
+const modalContainer = document.createElement('div');
+document.body.appendChild(modalContainer);
 
 initializeDropdown({
   triggerElementId: 'options-dropdown-trigger',
@@ -12,14 +13,17 @@ initializeDropdown({
 });
 
 const openModal = async (event) => {
-  // Append an empty div to the end of the document so that is does not affect the layout.
-  const modalContainer = document.createElement('div');
-  document.body.appendChild(modalContainer);
+  event.preventDefault();
 
-  const { target: trigger } = event;
+  const { dataset = {} } = event.target;
+
+  if (!Object.prototype.hasOwnProperty.call(dataset, 'modalTrigger')) {
+    // We're not trying to trigger a modal.
+    return;
+  }
 
   // Only load Preact if we haven't already.
-  if (!preact || !AdminUserActionsModal) {
+  if (!preact) {
     [preact, { AdminUserActionsModal: Modal }] = await Promise.all([
       import('preact'),
       import('@admin'),
@@ -28,14 +32,21 @@ const openModal = async (event) => {
 
   const { h, render } = preact;
 
+  if (modalContainer) {
+    // We've loaded a modal at least once in the modal container, so unmount it before rendering a new one.
+    // This allows us to only ever append one div to the body.
+    render(null, modalContainer);
+  }
+
+  const { modalTitle, modalSize, modalContentSelector } = dataset;
+
   // TODO: Where are we pulling the modal body from?
   render(
-    <Modal title={trigger.dataset.modalTitle} size={trigger.dataset.modalSize}>
+    <Modal title={modalTitle} size={modalSize}>
       <div
         // eslint-disable-next-line react/no-danger
         dangerouslySetInnerHTML={{
-          __html: document.querySelector(trigger.dataset.modalContentSelector)
-            .innerHTML,
+          __html: document.querySelector(modalContentSelector).innerHTML,
         }}
       />
     </Modal>,
@@ -43,6 +54,4 @@ const openModal = async (event) => {
   );
 };
 
-modalTriggers.forEach((trigger) =>
-  trigger.addEventListener('click', openModal),
-);
+document.body.addEventListener('click', openModal);
