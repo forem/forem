4920114400eb09c1b9b0712e17b46df483e35ddc
Suzanne Aitchison <suzanne@forem.com>
2022-01-31 09:37:13 +0000

    update data update script

--- spec/lib/data_update_scripts/create_home_navigation_link_spec.rb
+++ spec/lib/data_update_scripts/create_home_navigation_link_spec.rb
@@ -11,9 +11,21 @@ describe DataUpdateScripts::CreateHomeNavigationLink do
   end
 
   it "skips home navigation link creation if already exists" do
-    create(:navigation_link, url: "/")
+    create(:navigation_link, url: "/", name: "Home")
     expect do
       described_class.new.run
     end.not_to change(NavigationLink, :count)
+  end
+
+  it "updates the position of other default navigation links" do
+    link = create(:navigation_link, url: "/example", name: "Example", section: :default, position: 0)
+    described_class.new.run
+    expect(link.reload.position).to eq(2)
+  end
+
+  it "doesn't update the position of other navigation links" do
+    link = create(:navigation_link, url: "/example", name: "Example", section: :other, position: 0)
+    described_class.new.run
+    expect(link.reload.position).to eq(0)
   end
 end
