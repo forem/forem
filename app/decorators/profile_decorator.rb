1b18bbbc29464ef55b2193dbb81042cfcae0f854
Dan Uber <dan@forem.com>
2022-02-02 09:51:57 -0600

    Use the admin supplied label in the sidebar
    The profile decorator is used in the Users#show page to populate the
    sidebar fields. Don't use the attribute name (which we mangled during
    creation, and now generate randomly) as the label, use the label.

--- app/decorators/profile_decorator.rb
+++ app/decorators/profile_decorator.rb
@@ -3,6 +3,10 @@ class ProfileDecorator < ApplicationDecorator
   # display area, e.g. :left_sidebar
   def ui_attributes_for(area:)
     names = ProfileField.public_send(area).pluck(:attribute_name)
-    data.slice(*names).select { |_, v| v.present? }
+    data.slice(*names).select { |_, v| v.present? }.transform_keys { |k| label_for_attribute(k) }
+  end
+
+  def label_for_attribute(attr)
+    ProfileField.find_by(attribute_name: attr).label
   end
 end
