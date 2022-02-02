a8296f94c62a8377bca4d38cd743c3b7286dedaa
Dan Uber <dan@forem.com>
2022-02-02 11:03:02 -0600

    Fix profile preview card request spec
    Remove the assumption that profile responds to a method name based on
    the label for Work and Education fields.

--- spec/requests/profile_preview_cards_spec.rb
+++ spec/requests/profile_preview_cards_spec.rb
@@ -66,10 +66,14 @@ RSpec.describe "ProfilePreviewCards", type: :request do
         get profile_preview_card_path(user), as: :json
 
         preview_card = response.parsed_body
+        labels_to_attrs = %w[Work Education].index_with do |label|
+          ProfileField.find_by(label: label)&.attribute_name
+        end
+
         expect(preview_card["summary"]).to eq(profile.summary)
-        expect(preview_card["work"]).to eq(profile.work)
+        expect(preview_card["work"]).to eq(profile.public_send(labels_to_attrs["Work"]))
         expect(preview_card["location"]).to eq(profile.location)
-        expect(preview_card["education"]).to eq(profile.education)
+        expect(preview_card["education"]).to eq(profile.public_send(labels_to_attrs["Education"]))
         expect(preview_card["created_at"]).to eq(user.created_at.utc.iso8601)
       end
 
@@ -118,10 +122,14 @@ RSpec.describe "ProfilePreviewCards", type: :request do
         get profile_preview_card_path(user), as: :json
 
         preview_card = response.parsed_body
+        labels_to_attrs = %w[Work Education].index_with do |label|
+          ProfileField.find_by(label: label)&.attribute_name
+        end
+
         expect(preview_card["summary"]).to eq(profile.summary)
-        expect(preview_card["work"]).to eq(profile.work)
+        expect(preview_card["work"]).to eq(profile.public_send(labels_to_attrs["Work"]))
         expect(preview_card["location"]).to eq(profile.location)
-        expect(preview_card["education"]).to eq(profile.education)
+        expect(preview_card["education"]).to eq(profile.public_send(labels_to_attrs["Education"]))
         expect(preview_card["created_at"]).to eq(user.created_at.utc.iso8601)
       end
 
