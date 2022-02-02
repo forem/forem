1b98b03bdec8e6435c08f87beaed95e5760c3eb1
Suzanne Aitchison <suzanne@forem.com>
2022-02-02 13:45:55 +0000

    Allow users to pause gifs in a post (#16314)
    * working POC
    
    * refactors and styling
    
    * add some comments
    
    * css tweaks to keep sizes in step
    
    * add cypress test
    
    * comment added to css

--- app/javascript/packs/articlePage.jsx
+++ app/javascript/packs/articlePage.jsx
@@ -6,6 +6,15 @@ import { embedGists } from '../utilities/gist';
 
 /* global Runtime */
 
+const animatedImages = document.querySelectorAll('[data-animated="true"]');
+if (animatedImages.length > 0) {
+  import('@utilities/animatedImageUtils').then(
+    ({ initializePausableAnimatedImages }) => {
+      initializePausableAnimatedImages(animatedImages);
+    },
+  );
+}
+
 const fullscreenActionElements = document.getElementsByClassName(
   'js-fullscreen-code-action',
 );
