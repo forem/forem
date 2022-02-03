1d79faf5080712942ffaa3bc4cce8e5f39cf1857
Ridhwana <ridhwana.khan16@gmail.com>
2022-02-03 16:30:25 +0200

    test: cypress workflow to update smtp settings

---
+++ cypress/integration/seededFlows/adminFlows/config/emailServerSettingsSection.spec.js
@@ -0,0 +1,57 @@
+describe('Email Server Settings Section', () => {
+  beforeEach(() => {
+    cy.testSetup();
+    cy.fixture('users/adminUser.json').as('user');
+
+    cy.get('@user').then((user) => {
+      cy.loginUser(user);
+    });
+  });
+
+  describe('email server settings', () => {
+    it('updates the smtp fields', () => {
+      cy.get('@user').then(() => {
+        cy.visit('/admin/customization/config');
+        cy.findByTestId('emailServerSettings').as('emailServerSettings');
+
+        cy.get('@emailServerSettings')
+          .findByText('Email Server Settings (SMTP)')
+          .click();
+
+        cy.get('@emailServerSettings')
+          .get('#settings_smtp_user_name')
+          .clear()
+          .type('jane_doe');
+
+        cy.get('@emailServerSettings')
+          .get('#settings_smtp_password')
+          .clear()
+          .type('abc123456');
+
+        cy.get('@emailServerSettings')
+          .get('#settings_smtp_address')
+          .clear()
+          .type('smtp.gmail.com');
+
+        cy.get('@emailServerSettings')
+          .get('#settings_smtp_authentication')
+          .clear()
+          .type('plain');
+
+        cy.get('@emailServerSettings').findByText('Update Settings').click();
+
+        cy.url().should('contains', '/admin/customization/config');
+
+        cy.findByText('Successfully updated settings.').should('be.visible');
+
+        cy.get('#settings_smtp_user_name').should('have.value', 'jane_doe');
+
+        cy.get('#settings_smtp_password').should('have.value', 'abc123456');
+
+        cy.get('#settings_smtp_address').should('have.value', 'smtp.gmail.com');
+
+        cy.get('#settings_smtp_authentication').should('have.value', 'plain');
+      });
+    });
+  });
+});
