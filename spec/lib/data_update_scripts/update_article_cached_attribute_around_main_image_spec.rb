42bbb3f97a85fc1c5e5273100d53c5d8173f216c
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2022-02-03 12:07:59 -0500

    Bypassing validiation for data script
    Prior to this commit, we attempted to run a script against all articles
    using validation.  In the case of DEV, we have lots of articles that
    would no longer validate.  This change now updates the column value
    without running validation.
    
    Relates to #16075

--- spec/lib/data_update_scripts/update_article_cached_attribute_around_main_image_spec.rb
+++ spec/lib/data_update_scripts/update_article_cached_attribute_around_main_image_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 require Rails.root.join(
-  "lib/data_update_scripts/20220120052641_backfill_column_main_image_from_frontmatter.rb",
+  "lib/data_update_scripts/20220203165540_update_article_cached_attribute_around_main_image.rb",
 )
 
-describe DataUpdateScripts::BackfillColumnMainImageFromFrontmatter do
+RSpec.describe DataUpdateScripts::UpdateArticleCachedAttributeAroundMainImage do
   let(:article_without_image) { create(:article, with_main_image: false) }
   let(:article_main_image) { create(:article, with_main_image: true) }
   let(:article_markdown_image) do
@@ -14,11 +14,13 @@ describe DataUpdateScripts::BackfillColumnMainImageFromFrontmatter do
   before do
     article_without_image
     article_main_image
-    article_markdown_image
+    # Need to do the following for the script to even run.
+    article_markdown_image.update_column(:main_image_from_frontmatter, false)
   end
 
   it "set main_image_from_frontmatter to true only for articles with cover_image in body_markdown" do
     described_class.new.run
+
     expect(article_without_image.reload.main_image_from_frontmatter).to be false
     expect(article_main_image.reload.main_image_from_frontmatter).to be false
     expect(article_markdown_image.reload.main_image_from_frontmatter).to be true
