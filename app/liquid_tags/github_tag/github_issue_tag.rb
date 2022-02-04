309d9a8f9ae5880321c89e41a669d0bdca4aa639
Arit Amana <msarit@gmail.com>
2022-02-04 13:52:40 -0500

    working both ways for issues PRs except disc and PRreview

--- app/liquid_tags/github_tag/github_issue_tag.rb
+++ app/liquid_tags/github_tag/github_issue_tag.rb
@@ -5,15 +5,13 @@
 #   https://api.github.com/repos/facebook/react/pulls/14105
 # getting a comment belonging to an issue
 #   https://api.github.com/repos/facebook/react/issues/comments/287635042
-class GithubTag < LiquidTagBase
-  class GithubIssueTag < GithubTag
+class GithubTag
+  class GithubIssueTag
     PARTIAL = "liquids/github_issue".freeze
     API_BASE_ENDPOINT = "https://api.github.com/repos/".freeze
     ISSUE_REGEXP = %r{https://github.com/[\w\-]{1,39}/[\w\-]{1,39}/(issues|pull)/\d+((#issuecomment-|#discussion_|#pullrequestreview-)\w+)?}
 
     def initialize(link)
-      super
-
       @orig_link = link
       @link = parse_link(link)
       @content = GithubIssue.find_or_fetch(@link)
@@ -46,18 +44,10 @@ class GithubTag < LiquidTagBase
     attr_reader :content_json
 
     def parse_link(link)
-      match = pattern_match_for(link, [ISSUE_REGEXP])
+      match = link.match(ISSUE_REGEXP)
       raise StandardError, I18n.t("liquid_tags.github_tag.github_issue_tag.invalid_github_issue_pull") unless match
 
       generate_api_link(link)
-
-      # link = ActionController::Base.helpers.strip_tags(link)
-      # link_no_space = link.delete(" ")
-      # if valid_link?(link_no_space)
-      #   generate_api_link(link_no_space)
-      # else
-      #   raise_error
-      # end
     end
 
     def generate_api_link(input)
@@ -85,16 +75,6 @@ class GithubTag < LiquidTagBase
 
       Addressable::URI.parse(API_BASE_ENDPOINT).join(path).to_s
     end
-
-    # def valid_link?(link)
-    #   link_without_domain = link.gsub(%r{.*github\.com/}, "").split("/")
-    #   validations = [
-    #     %r{.*github\.com/}.match?(link),
-    #     link_without_domain.length == 4,
-    #     link_without_domain[3].to_i.positive?,
-    #   ]
-    #   validations.all? || raise_error
-    # end
 
     def raise_error
       raise StandardError, I18n.t("liquid_tags.github_tag.github_issue_tag.invalid_github_issue_pull")
