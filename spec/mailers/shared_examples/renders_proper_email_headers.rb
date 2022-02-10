0c93b1269a72dcb234a4e43fbd4b2c357d45be60
Ridhwana <ridhwana.khan16@gmail.com>
2022-02-10 16:32:14 +0200

    fix tests

--- spec/mailers/shared_examples/renders_proper_email_headers.rb
+++ spec/mailers/shared_examples/renders_proper_email_headers.rb
@@ -9,3 +9,24 @@ RSpec.shared_examples "#renders_proper_email_headers" do
     expect(email["reply_to"].value).to eq(ForemInstance.email)
   end
 end
+# require "rails_helper"
+#
+# RSpec.shared_examples "#renders_proper_email_headers" do
+#   let(:from_email_address) { "noreply@dev.to" }
+#   let(:reply_to_email_address) { "yo@dev.to" }
+#
+#   before do
+#     allow(Settings::SMTP).to receive(:from_email_address).and_return(from_email_address)
+#     allow(Settings::SMTP).to receive(:reply_to_email_address).and_return(reply_to_email_address)
+#   end
+#
+#   it "renders proper sender" do
+#     expect(email.from).to eq([from_email_address])
+#     expected_from = "#{Settings::Community.community_name} <#{from_email_address}>"
+#     expect(email["from"].value).to eq(expected_from)
+#   end
+#
+#   it "renders proper reply_to" do
+#     expect(email["reply_to"].value).to eq(reply_to_email_address)
+#   end
+# end
--- spec/mailers/verification_mailer_spec.rb
+++ spec/mailers/verification_mailer_spec.rb
@@ -2,15 +2,20 @@ require "rails_helper"
 
 RSpec.describe VerificationMailer, type: :mailer do
   let(:user) { create(:user) }
+  let(:from_email_address) { "noreply@forem.com" }
 
   describe "#account_ownership_verification_email" do
+    before do
+      allow(Settings::SMTP).to receive(:from_email_address).and_return(from_email_address)
+    end
+
     it "works correctly" do
       email = described_class.with(user_id: user.id).account_ownership_verification_email
 
       expect(email.subject).not_to be_nil
       expect(email.to).to eq([user.email])
-      expect(email.from).to eq([ForemInstance.from_email_address])
-      from = "#{Settings::Community.community_name} Email Verification <#{ForemInstance.from_email_address}>"
+      expect(email.from).to eq([from_email_address])
+      from = "#{Settings::Community.community_name} Email Verification <#{from_email_address}>"
       expect(email["from"].value).to eq(from)
     end
   end
