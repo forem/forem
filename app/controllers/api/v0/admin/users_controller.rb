549e5bc973fd8527981d75ae00aa1385d27bd8b7
Michael Kohl <me@citizen428.net>
2022-02-10 13:40:11 +0700

    Add Api::V0::Admin::UsersController#create

---
+++ app/controllers/api/v0/admin/users_controller.rb
@@ -0,0 +1,23 @@
+module Api
+  module V0
+    module Admin
+      class UsersController < ApiController
+        before_action :authenticate_with_api_key_or_current_user!
+        before_action :authorize_super_admin
+        skip_before_action :verify_authenticity_token, only: %i[create]
+
+        def create
+          User.invite!(user_params)
+
+          head :ok
+        end
+
+        private
+
+        def user_params
+          { email: params.require(:email), name: params[:name] }.compact_blank
+        end
+      end
+    end
+  end
+end
