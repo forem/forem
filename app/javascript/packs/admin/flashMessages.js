32e4ab6d4ab0df527d8779a980e538d2f7ab3216
Suzanne Aitchison <suzanne@forem.com>
2022-02-15 15:18:04 +0000

    Update app/javascript/packs/admin/flashMessages.js
    Co-authored-by: Nick Taylor <nick@iamdeveloper.com>

--- app/javascript/packs/admin/flashMessages.js
+++ app/javascript/packs/admin/flashMessages.js
@@ -3,6 +3,4 @@ const [ firstFlashDismissBtn ] =
 
 // This allows screen reader users to become aware of the message (as well as bringing focus to the top of the main content).
 // (we don't use aria-live or role="alert" on the message text as these are not reliably announced for content that exists from page load)
-if (firstFlashDismissBtn) {
-  firstFlashDismissBtn.focus();
-}
+firstFlashDismissBtn?.focus();
