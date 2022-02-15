438023a536fc16b7061a7dca8920bca838d3f7b4
Paweł Ludwiczak <ludwiczakpawel@gmail.com>
2022-02-15 21:53:25 +0100

    nasty things

--- app/controllers/admin/overview_controller.rb
+++ app/controllers/admin/overview_controller.rb
@@ -6,19 +6,25 @@ module Admin
         FeedbackMessage.open_abuse_reports.size
 
       @length = params[:period] || 7
+      @length = @length.to_i
       @period = @length.days.ago..Time.current
       @previous_period = (@length * 2).days.ago..@length.days.ago
-      @labels = (0..6).map { |n| n.days.ago.strftime("%b %d") }
+      @labels = (0..@length - 1).map { |n| n.days.ago.strftime("%b %d") }.reverse
+
+      @posts_values = (0..@length - 1).map { |n| Article.where(published_at: n.days.ago.all_day).size }.reverse
+      @comments_values = (0..@length - 1).map { |n| Comment.where(created_at: n.days.ago.all_day).size }.reverse
+      @reactions_values = (0..@length - 1).map { |n| Reaction.where(created_at: n.days.ago.all_day).size }.reverse
+      @new_members_values = (0..@length - 1).map { |n| User.where(registered_at: n.days.ago.all_day).size }.reverse
 
       @analytics = [
         ["Posts", Article.where(published_at: @period).size, Article.where(published_at: @previous_period).size,
-         [0, 1, 2, 3, 4, 5, 6]],
+         @posts_values],
         ["Comments", Comment.where(created_at: @period).size, Comment.where(created_at: @previous_period).size,
-         [0, 1, 2, 3, 4, 5, 6]],
+         @comments_values],
         ["Reactions", Reaction.where(created_at: @period).size, Reaction.where(created_at: @previous_period).size,
-         [0, 1, 2, 3, 4, 5, 6]],
+         @reactions_values],
         ["New members", User.where(registered_at: @period).size, User.where(registered_at: @previous_period).size,
-         [0, 1, 2, 3, 4, 5, 6]],
+         @new_members_values],
       ]
     end
   end
