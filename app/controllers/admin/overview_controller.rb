24ee3b7688e8aa19b41a9d72030ac8b9ca37bcb0
Paweł Ludwiczak <ludwiczakpawel@gmail.com>
2022-02-15 16:37:29 +0100

    pawel playground

--- app/controllers/admin/overview_controller.rb
+++ app/controllers/admin/overview_controller.rb
@@ -4,6 +4,22 @@ module Admin
     def index
       @open_abuse_reports_count =
         FeedbackMessage.open_abuse_reports.size
+
+      @length = params[:period] || 7
+      @period = @length.days.ago..Time.current
+      @previous_period = (@length * 2).days.ago..@length.days.ago
+      @labels = (0..6).map { |n| n.days.ago.strftime("%b %d") }
+
+      @analytics = [
+        ["Posts", Article.where(published_at: @period).size, Article.where(published_at: @previous_period).size,
+         [0, 1, 2, 3, 4, 5, 6]],
+        ["Comments", Comment.where(created_at: @period).size, Comment.where(created_at: @previous_period).size,
+         [0, 1, 2, 3, 4, 5, 6]],
+        ["Reactions", Reaction.where(created_at: @period).size, Reaction.where(created_at: @previous_period).size,
+         [0, 1, 2, 3, 4, 5, 6]],
+        ["New members", User.where(registered_at: @period).size, User.where(registered_at: @previous_period).size,
+         [0, 1, 2, 3, 4, 5, 6]],
+      ]
     end
   end
 end
