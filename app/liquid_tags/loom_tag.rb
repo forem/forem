b8fa33211aa8966c18332e65dee51f314db1c5ae
Arit Amana <msarit@gmail.com>
2022-02-17 14:55:49 -0500

    complete implementation and specs

--- app/liquid_tags/loom_tag.rb
+++ app/liquid_tags/loom_tag.rb
@@ -1,27 +1,29 @@
 class LoomTag < LiquidTagBase
   PARTIAL = "liquids/loom".freeze
-  REGISTRY_REGEXP = %r{https://(www\.)?loom\.com/(share|embed)/\w+}
+  REGISTRY_REGEXP = %r{https://(?:www\.)?loom\.com/(?:share|embed)/(?<video_id>[a-zA-Z0-9]+)(?:\?[\w=-]+)?$}
 
   def initialize(_tag_name, input, _parse_context)
     super
 
-    stripped_input = CGI.unescape_html(strip_tags(input))
-    @url           = check_input(stripped_input)
+    stripped_input  = strip_tags(input)
+    unescaped_input = CGI.unescape_html(stripped_input)
+    @id             = extract_video_id(unescaped_input)
   end
 
   def render(_context)
     ApplicationController.render(
       partial: PARTIAL,
       locals: {
-        url: @url
+        id: @id
       },
     )
   end
 
-  def check_input(input)
-    raise StandardError, I18n.t("liquid_tags.loom_tag.invalid_loom_url") unless input.match?(REGISTRY_REGEXP)
+  def extract_video_id(input)
+    match = pattern_match_for(input, [REGISTRY_REGEXP])
+    raise StandardError, I18n.t("liquid_tag.loom_tag.invalid_loom_url") unless match
 
-    input
+    match[:video_id]
   end
 end
 
--- spec/liquid_tags/loom_tag_spec.rb
+++ spec/liquid_tags/loom_tag_spec.rb
@@ -6,9 +6,13 @@ RSpec.describe LoomTag do
   let(:article) { create(:article) }
   let(:user) { create(:user) }
   let(:parse_context) { { source: article, user: user } }
-  let(:valid_loom_share_url) { "https://loom.com/share/12fb674d39dd4fe281becee7cdbc3cd1" }
-  let(:valid_loom_embed_url) { "https://loom.com/embed/12fb674d39dd4fe281becee7cdbc3cd1" }
-  let(:valid_www_loom_url) { "https://www.loom.com/share/12fb674d39dd4fe281becee7cdbc3cd1" }
+  let(:loom_share_url) { "https://loom.com/share/12fb674d39dd4fe281becee7cdbc3cd1" }
+  let(:loom_embed_url) { "https://loom.com/embed/12fb674d39dd4fe281becee7cdbc3cd1" }
+  let(:www_loom_url) { "https://www.loom.com/share/12fb674d39dd4fe281becee7cdbc3cd1" }
+  let(:loom_url_with_query) do
+    "https://loom.com/share/12fb674d39dd4fe281becee7cdbc3cd1?sharedAppSource=personal_library"
+  end
+  let(:expected_link) { "https://loom.com/embed/12fb674d39dd4fe281becee7cdbc3cd1" }
 
   let(:invalid_loom_urls) do
     [
@@ -17,13 +21,50 @@ RSpec.describe LoomTag do
     ]
   end
 
-  describe "Loom tag" do
+  let(:registry_regexp) do
+    %r{https://(?:www\.)?loom\.com/(?:share|embed)/(?<video_id>[a-zA-Z0-9]+)(?:\?[\w=-]+)?$}
+  end
+
+  def generate_embed(url)
+    UnifiedEmbed.register(described_class, regexp: registry_regexp)
+    Liquid::Template.parse("{% embed #{url} %}")
+  end
+
+  describe "rendering" do
     it "returns StandardError for invalid Loom URL", :aggregate_failures do
       invalid_loom_urls.each do |invalid_url|
         expect do
-          described_class.__send__(:new, "embed", invalid_url, parse_context)
-        end.to raise_error(StandardError, "Invalid Loom URL.")
+          generate_embed(invalid_url)
+        end.to raise_error(StandardError, "Embed URL not valid")
       end
+    end
+
+    it "returns Loom embed for valid Loom share URL" do
+      embed = generate_embed(loom_share_url).render
+
+      expect(embed).to include("<iframe")
+      expect(embed).to include("src=\"#{expected_link}\"")
+    end
+
+    it "returns Loom embed for valid Loom embed URL" do
+      embed = generate_embed(loom_embed_url).render
+
+      expect(embed).to include("<iframe")
+      expect(embed).to include("src=\"#{expected_link}\"")
+    end
+
+    it "returns Loom embed for valid Loom www URL" do
+      embed = generate_embed(www_loom_url).render
+
+      expect(embed).to include("<iframe")
+      expect(embed).to include("src=\"#{expected_link}\"")
+    end
+
+    it "returns Loom embed for valid Loom URL with query paramaters" do
+      embed = generate_embed(loom_url_with_query).render
+
+      expect(embed).to include("<iframe")
+      expect(embed).to include("src=\"#{expected_link}\"")
     end
   end
 end
