9c92ce03dc3526235e0754acb90c9e26ffc7c83d
Arit Amana <32520970+msarit@users.noreply.github.com>
2022-02-22 09:05:55 -0500

    Implement Loom Embed (not merging now) (#16633)
    * started implementation and specs
    
    * complete implementation and specs
    
    * refactor
    
    * update doc
    
    * fix failing specs
    
    * typos will NOT be the death of me

---
+++ app/liquid_tags/loom_tag.rb
@@ -0,0 +1,30 @@
+class LoomTag < LiquidTagBase
+  PARTIAL = "liquids/loom".freeze
+  REGISTRY_REGEXP = %r{https://(?:www\.)?loom\.com/(?:share|embed)/(?<video_id>[a-zA-Z0-9]+)(?:\?[\w=-]+)?$}
+
+  def initialize(_tag_name, input, _parse_context)
+    super
+
+    stripped_input  = strip_tags(input)
+    unescaped_input = CGI.unescape_html(stripped_input)
+    @id             = extract_video_id(unescaped_input)
+  end
+
+  def render(_context)
+    ApplicationController.render(
+      partial: PARTIAL,
+      locals: {
+        id: @id
+      },
+    )
+  end
+
+  def extract_video_id(input)
+    match = pattern_match_for(input, [REGISTRY_REGEXP])
+    raise StandardError, I18n.t("liquid_tags.loom_tag.invalid_loom_url") unless match
+
+    match[:video_id]
+  end
+end
+
+UnifiedEmbed.register(LoomTag, regexp: LoomTag::REGISTRY_REGEXP)
