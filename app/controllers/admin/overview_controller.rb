4062170a9ff5e70ad29423a6fb7bd4756da87118
Paweł Ludwiczak <ludwiczakpawel@gmail.com>
2022-02-23 09:43:42 +0100

    .

--- app/controllers/admin/overview_controller.rb
+++ app/controllers/admin/overview_controller.rb
@@ -2,7 +2,7 @@ module Admin
   class OverviewController < Admin::ApplicationController
     layout "admin"
     def index
-      @length = params[:period] || 7
+      @length = (params[:period] || 7).to_i
       @labels = (0..@length - 1).map { |n| n.days.ago.strftime("%b %d") }.reverse
       @analytics = Admin::ChartsData.new(@length).call
       @data_counts = Admin::DataCounts.call
