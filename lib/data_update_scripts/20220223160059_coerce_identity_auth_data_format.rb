cce9c81371fb307e6e205536352d7647d2498c2d
Daniel Uber <dan@forem.com>
2022-02-23 12:05:45 -0600

    Update lib/data_update_scripts/20220223160059_coerce_identity_auth_data_format.rb
    include suggested hardenings
    
    Co-authored-by: Jamie Gaskins <jgaskins@hey.com>

--- lib/data_update_scripts/20220223160059_coerce_identity_auth_data_format.rb
+++ lib/data_update_scripts/20220223160059_coerce_identity_auth_data_format.rb
@@ -1,9 +1,11 @@
 module DataUpdateScripts
   class CoerceIdentityAuthDataFormat
     def run
-      Identity.where("auth_data_dump ~ '---\n'").find_each do |identity|
-        auth_hash = OmniAuth::AuthHash.new(**identity.auth_data_dump)
-        identity.update(auth_data_dump: auth_hash)
+      Identity.with_statement_timeout(5.minutes) do
+        Identity.where("auth_data_dump ~ '^---\n'").find_each do |identity|
+          auth_hash = OmniAuth::AuthHash.new(**identity.auth_data_dump)
+          identity.update_columns(auth_data_dump: auth_hash)
+        end
       end
     end
   end
