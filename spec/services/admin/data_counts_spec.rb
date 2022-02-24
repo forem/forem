5c6117fa72312650ed22f2dd992edce1f26f407f
benhalpern <bendhalpern@gmail.com>
2022-02-24 18:43:39 -0500

    Clean up tests

--- spec/services/admin/data_counts_spec.rb
+++ spec/services/admin/data_counts_spec.rb
@@ -2,16 +2,10 @@ require "rails_helper"
 
 RSpec.describe Admin::DataCounts, type: :service do
   it "returns proper data type" do
-    expect(described_class.call).to be_an_instance_of(Hash)
+    expect(described_class.call).to be_an_instance_of(OpenStruct)
   end
 
-  it "has proper keys" do
-    keys = %i[open_abuse_reports_count possible_spam_users_count
-              flags_count flags_posts_count flags_comments_count flags_users_count]
-    expect(described_class.call.keys).to eq(keys)
-  end
-
-  it "returns integers" do
-    expect(described_class.call.values).to eq([0, 0, 0, 0, 0, 0])
+  it "returns an integer" do
+    expect(described_class.call.open_abuse_reports_count).to eq(0)
   end
 end
