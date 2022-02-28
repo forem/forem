fa05d3c27a4f83148593115a1a7a8bbfc61ee0e0
Suzanne Aitchison <suzanne@forem.com>
2022-02-28 15:08:03 +0000

    rework

--- app/assets/javascripts/initializers/initializeBodyData.js
+++ app/assets/javascripts/initializers/initializeBodyData.js
@@ -9,47 +9,46 @@ function removeExistingCSRF() {
   }
 }
 
-async function fetchBaseData() {
-  const response = await fetch('/async_info/base_data');
-
-  const jsonResponse = await response.json();
-  const { token, param, broadcast, user, creator } = jsonResponse;
-
-  if (token) {
-    removeExistingCSRF();
-  }
-
-  const newCsrfParamMeta = document.createElement('meta');
-  newCsrfParamMeta.name = 'csrf-param';
-  newCsrfParamMeta.content = param;
-  document.head.appendChild(newCsrfParamMeta);
-
-  const newCsrfTokenMeta = document.createElement('meta');
-  newCsrfTokenMeta.name = 'csrf-token';
-  newCsrfTokenMeta.content = token;
-  document.head.appendChild(newCsrfTokenMeta);
-  document.body.dataset.loaded = 'true';
-
-  if (broadcast) {
-    document.body.dataset.broadcast = broadcast;
-  }
-
-  if (checkUserLoggedIn()) {
-    document.body.dataset.user = user;
-    document.body.dataset.creator = creator;
-    browserStoreCache('set', user);
-
-    setTimeout(() => {
-      if (typeof ga === 'function') {
-        ga('set', 'userId', JSON.parse(user).id);
+function fetchBaseData() {
+  fetch('/async_info/base_data')
+    .then((response) => response.json())
+    .then(({ token, param, broadcast, user, creator }) => {
+      if (token) {
+        removeExistingCSRF();
       }
-    }, 400);
-  } else {
-    // Ensure user data is not exposed if no one is logged in
-    delete document.body.dataset.user;
-    delete document.body.dataset.creator;
-    browserStoreCache('remove');
-  }
+
+      const newCsrfParamMeta = document.createElement('meta');
+      newCsrfParamMeta.name = 'csrf-param';
+      newCsrfParamMeta.content = param;
+      document.head.appendChild(newCsrfParamMeta);
+
+      const newCsrfTokenMeta = document.createElement('meta');
+      newCsrfTokenMeta.name = 'csrf-token';
+      newCsrfTokenMeta.content = token;
+      document.head.appendChild(newCsrfTokenMeta);
+      document.body.dataset.loaded = 'true';
+
+      if (broadcast) {
+        document.body.dataset.broadcast = broadcast;
+      }
+
+      if (checkUserLoggedIn()) {
+        document.body.dataset.user = user;
+        document.body.dataset.creator = creator;
+        browserStoreCache('set', user);
+
+        setTimeout(() => {
+          if (typeof ga === 'function') {
+            ga('set', 'userId', JSON.parse(user).id);
+          }
+        }, 400);
+      } else {
+        // Ensure user data is not exposed if no one is logged in
+        delete document.body.dataset.user;
+        delete document.body.dataset.creator;
+        browserStoreCache('remove');
+      }
+    });
 }
 
 function initializeBodyData() {
