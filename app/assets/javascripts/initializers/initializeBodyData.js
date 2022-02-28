fdd3bfa8e2addd0151a013e948c91e85040cbace
Suzanne Aitchison <suzanne@forem.com>
2022-02-28 14:20:37 +0000

    refactor out xmlhttprequest

--- app/assets/javascripts/initializers/initializeBodyData.js
+++ app/assets/javascripts/initializers/initializeBodyData.js
@@ -9,54 +9,47 @@ function removeExistingCSRF() {
   }
 }
 
-/* TODO: prefer fetch() to XMLHttpRequest */
-function fetchBaseData() {
-  var xmlhttp;
-  xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = () => {
-    if (xmlhttp.readyState === XMLHttpRequest.DONE) {
-      // Assigning CSRF
-      var json = JSON.parse(xmlhttp.responseText);
-      if (json.token) {
-        removeExistingCSRF();
+async function fetchBaseData() {
+  const response = await fetch('/async_info/base_data');
+
+  const jsonResponse = await response.json();
+  const { token, param, broadcast, user, creator } = jsonResponse;
+
+  if (token) {
+    removeExistingCSRF();
+  }
+
+  const newCsrfParamMeta = document.createElement('meta');
+  newCsrfParamMeta.name = 'csrf-param';
+  newCsrfParamMeta.content = param;
+  document.head.appendChild(newCsrfParamMeta);
+
+  const newCsrfTokenMeta = document.createElement('meta');
+  newCsrfTokenMeta.name = 'csrf-token';
+  newCsrfTokenMeta.content = token;
+  document.head.appendChild(newCsrfTokenMeta);
+  document.body.dataset.loaded = 'true';
+
+  if (broadcast) {
+    document.body.dataset.broadcast = broadcast;
+  }
+
+  if (checkUserLoggedIn()) {
+    document.body.dataset.user = user;
+    document.body.dataset.creator = creator;
+    browserStoreCache('set', user);
+
+    setTimeout(() => {
+      if (typeof ga === 'function') {
+        ga('set', 'userId', JSON.parse(user).id);
       }
-      var newCsrfParamMeta = document.createElement('meta');
-      newCsrfParamMeta.name = 'csrf-param';
-      newCsrfParamMeta.content = json.param;
-      document.head.appendChild(newCsrfParamMeta);
-      var newCsrfTokenMeta = document.createElement('meta');
-      newCsrfTokenMeta.name = 'csrf-token';
-      newCsrfTokenMeta.content = json.token;
-      document.head.appendChild(newCsrfTokenMeta);
-      document.body.dataset.loaded = 'true';
-
-      // Assigning Broadcast
-      if (json.broadcast) {
-        document.body.dataset.broadcast = json.broadcast;
-      }
-
-      // Assigning User
-      if (checkUserLoggedIn()) {
-        document.body.dataset.user = json.user;
-        document.body.dataset.creator = json.creator;
-        browserStoreCache('set', json.user);
-
-        setTimeout(() => {
-          if (typeof ga === 'function') {
-            ga('set', 'userId', JSON.parse(json.user).id);
-          }
-        }, 400);
-      } else {
-        // Ensure user data is not exposed if no one is logged in
-        delete document.body.dataset.user;
-        delete document.body.dataset.creator;
-        browserStoreCache('remove');
-      }
-    }
-  };
-
-  xmlhttp.open('GET', '/async_info/base_data', true);
-  xmlhttp.send();
+    }, 400);
+  } else {
+    // Ensure user data is not exposed if no one is logged in
+    delete document.body.dataset.user;
+    delete document.body.dataset.creator;
+    browserStoreCache('remove');
+  }
 }
 
 function initializeBodyData() {
