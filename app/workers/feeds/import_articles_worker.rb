72466687acc1fa2453a629129c9bc63ba4d85b16
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2022-03-02 15:12:03 -0500

    Favoring User scope as parameter for feed fetching
    This change makes it easier to resolve forem/forem#16487.
    
    What do I mean by that?  To address forem/forem#16487, I need to only
    select user's who are authorized to create articles.  In
    forem/forem#16732 I added a method that will allow chaining of a User
    scope.  So with this current commit and forem/forem#16732, I'm
    triangulating on an approach that will make that change easier.
    
    I did not want to conflate those two, as mixing this PR's change and
    what is necessary for the closing PR would create a more complicated
    review.  Not unduly complicated, but one that will require more tests
    and a change in logic.  And for someone reviewing the diff, those
    concerns could easily be lost.
    
    Yes, I have changed the method signature, but that method signature is
    limited to one location:
    
    ```shell
    ❯ rg "Feeds::Import.call"
    
    app/workers/feeds/import_articles_worker.rb
    21:      ::Feeds::Import.call(users_scope: users_scope, earlier_than: earlier_than)
    ```
    
    So I look to the method signature a bit as an internal API, hence the change.
    
    Related to forem/forem#16487

--- app/workers/feeds/import_articles_worker.rb
+++ app/workers/feeds/import_articles_worker.rb
@@ -8,17 +8,17 @@ module Feeds
     # by using YAML to define jobs arguments does not support datetimes evaluated
     # at runtime
     def perform(user_ids = [], earlier_than = nil)
+      users_scope = User
       if user_ids.present?
-        users = User.where(id: user_ids)
+        users_scope = users_scope.where(id: user_ids)
         # we assume that forcing a single import should not take into account
         # the last time a feed was fetched at
         earlier_than = nil
       else
-        users = nil
         earlier_than ||= 4.hours.ago
       end
 
-      ::Feeds::Import.call(users: users, earlier_than: earlier_than)
+      ::Feeds::Import.call(users_scope: users_scope, earlier_than: earlier_than)
     end
   end
 end
