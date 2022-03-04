1867274ee6c38b000560cd5ec5670a92d58f1928
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2022-03-04 11:00:45 -0500

    Adding ArticlePolicy#moderate?
    This commit does three things:
    
    1) Adds and tests the ArticlePolicy#moderate? feature
    2) Refactors the spec helpers to be a bit more flexible
    3) Adds a few more edge case tests around ArticlePolicy
    
    My apologies for conflating these changes, as it makes this commit
    more challenging to review.
    
    The goal is to adequate demonstrate the logic around users who can or
    cannot moderate.  At present, the rules for who can moderate is here:
    
    https://github.com/forem/forem/blob/841491c6ee7f9a46d8033b4b55052316db251863/app/javascript/packs/articleModerationTools.js#L17-L27
    
    ```js
    if (user?.trusted) {
       if (user?.id !== articleAuthorId && !isModerationPage()) {
         initializeActionsPanel(user, path);
         initializeFlagUserModal(articleAuthorId);
         // "/mod" page
       } else if (isModerationPage()) {
         initializeActionsPanel(user, path);
         initializeFlagUserModal(articleAuthorId);
       }
    }
    ```
    
    - Related to forem/forem#16783
    - Closes forem/forem#16784

--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -112,6 +112,24 @@ class ArticlePolicy < ApplicationPolicy
     user_author? || user_super_admin? || user_org_admin? || user_any_admin?
   end
 
+  # @see https://github.com/forem/forem/blob/841491c6ee7f9a46d8033b4b55052316db251863/app/javascript/packs/articleModerationTools.js#L17-L27
+  #      for details regarding original authorization logic for this method.
+  def moderate?
+    # Technically, we could check the limit_post_creation_to_admins? first, but [@jeremyf]'s
+    # operating on a "trying to maintain consistency" approach.
+    require_user_in_good_standing!
+
+    return false if self.class.limit_post_creation_to_admins?
+
+    # Don't let a user moderate their own article.  See for prior UI logic reinforcing this
+    # https://github.com/forem/forem/blob/841491c6ee7f9a46d8033b4b55052316db251863/app/javascript/packs/articleModerationTools.js#L17-L27
+    return false if user_author?
+
+    # Beware a trusted user does not guarantee that they are an admin.  And more specifically, being
+    # an admin does not guarantee being trusted.
+    user.trusted?
+  end
+
   alias admin_featured_toggle? admin_unpublish?
 
   alias new? create?
