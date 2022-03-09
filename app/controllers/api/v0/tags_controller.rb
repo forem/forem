ceca4e8b0d501de6e3808049cdeb659dbfb16583
Suzanne Aitchison <suzanne@forem.com>
2022-03-09 16:10:59 +0000

    undo changes to public api tags

--- app/controllers/api/v0/tags_controller.rb
+++ app/controllers/api/v0/tags_controller.rb
@@ -11,13 +11,9 @@ module Api
         per_page = (params[:per_page] || 10).to_i
         num = [per_page, 1000].min
 
-        if params[:tag_ids].present?
-          @tags = @tags.where(id: params[:tag_ids])
-        elsif params[:tag_names].present?
-          @tags = @tags.where(name: params[:tag_names])
-        end
-
-        @tags = @tags.order(taggings_count: :desc).page(page).per(num)
+        @tags = Tag.select(ATTRIBUTES_FOR_SERIALIZATION)
+          .order(taggings_count: :desc)
+          .page(page).per(num)
 
         set_surrogate_key_header Tag.table_key, @tags.map(&:record_key)
       end
