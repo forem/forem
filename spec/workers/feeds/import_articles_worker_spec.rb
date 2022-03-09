2c28ff11342aa9b1ae6067ed84098f914e321bb5
Dan Uber <dan@forem.com>
2022-03-08 19:24:17 -0600

    Update spec
    Use update_columns to bypass setting validation (checking feed is
    valid/reachable)
    
    Remove unneeded timecop block when passing a set time
    
    Remove sidekiq: fake and allow Import.call to be received

--- spec/workers/feeds/import_articles_worker_spec.rb
+++ spec/workers/feeds/import_articles_worker_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe Feeds::ImportArticlesWorker, type: :worker, sidekiq: :inline do
   include_examples "#enqueues_on_correct_queue", "medium_priority"
 
   describe "#perform" do
-    it "processes jobs for users with feeds", vcr: "feeds_import" do
+    it "processes jobs for users with feeds" do
       allow(Feeds::Import).to receive(:call)
       alice = create(:user)
       bob = create(:user)
-      bob.setting.update(feed_url: feed_url)
+      bob.setting.update_columns(feed_url: feed_url)
 
       # bob has a feed, and alice doesn't, so we only enqueued for bob
 
@@ -27,18 +27,18 @@ RSpec.describe Feeds::ImportArticlesWorker, type: :worker, sidekiq: :inline do
       end
     end
 
-    it "enqueues job for user with the given time", vcr: "feeds_import", sidekiq: :fake do
+    it "enqueues job for user with the given time" do
+      allow(Feeds::Import).to receive(:call)
       user = create(:user)
-      user.setting.update(feed_url: feed_url)
+      user.setting.update_columns(feed_url: feed_url)
 
-      Timecop.freeze(Time.current) do
-        earlier_than = 1.minute.ago
+      earlier_than = 1.minute.ago
+      worker.perform([], earlier_than)
 
-        sidekiq_assert_enqueued_with(job: Feeds::ImportArticlesWorker::ForUser,
-                                     args: [user.id, earlier_than.iso8601]) do
-          worker.perform([], earlier_than)
-        end
-      end
+      expect(Feeds::Import).to have_received(:call).with(
+        users_scope: User.where(id: user.id),
+        earlier_than: earlier_than.iso8601,
+      )
     end
 
     it "calls Feeds::Import with the users from the given user ids and no time" do
