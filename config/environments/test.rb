da3b34869a9754d6e3cfb3ec593432dd8630bc6f
Mac Siri <krairit.siri@gmail.com>
2022-03-11 12:22:08 -0500

    Temp spec changes

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # https://guides.rubyonrails.org/configuring.html#config-cache-classes
-  config.cache_classes = true
+  config.cache_classes = false
 
   # See https://github.com/rails/rails/issues/40613#issuecomment-727283155
   config.action_view.cache_template_loading = true
