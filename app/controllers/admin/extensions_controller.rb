abf40a585d47e613c58ec90107f5ed9c3a4f9ccd
Ridhwana <ridhwana.khan16@gmail.com>
2022-03-17 14:40:31 +0200

    fix: rubucop

--- app/controllers/admin/extensions_controller.rb
+++ app/controllers/admin/extensions_controller.rb
@@ -7,7 +7,8 @@ module Admin
         name: "Listings",
         description: "Once turned on, Listings can be accessed via /listings and members " \
                      "will be able to add listings. " \
-                     "<a href='https://admin.forem.com/docs/advanced-customization/listings'>Learn more about Listings</a>.",
+                     "<a href='https://admin.forem.com/docs/advanced-customization/listings'>" \
+                     "Learn more about Listings</a>.",
         feature_flag_name: :listing_feature,
       ),
     ].freeze
