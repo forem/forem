2c1caab04cb3f9da8705b4adc7edbdcf047886c2
Arit Amana <msarit@gmail.com>
2022-03-18 13:53:53 -0400

    complete fix and update specs

--- spec/support/embeds_helpers.rb
+++ spec/support/embeds_helpers.rb
@@ -1,10 +1,10 @@
 module EmbedsHelpers
-  def stub_request_head(url, status_code = 200)
-    stub_request(:head, url)
+  def stub_get_request(url, status_code = 200)
+    stub_request(:get, url)
       .with(
         headers: {
           Accept: "*/*",
-          "User-Agent": "Ruby"
+          "User-Agent": "#{Settings::Community.community_name} (#{URL.url})"
         },
       ).to_return(status: status_code, body: "", headers: {})
   end
