3ec8075420eb477f197803475ac463ecdfd4ebb5
Julianna Tetreault <32834804+juliannatetreault@users.noreply.github.com>
2022-03-21 13:37:37 -0600

    Adds a DUS spec to test backfilling community names

--- spec/lib/data_update_scripts/backfill_community_emoji_spec.rb
+++ spec/lib/data_update_scripts/backfill_community_emoji_spec.rb
@@ -4,5 +4,18 @@ require Rails.root.join(
 )
 
 describe DataUpdateScripts::BackfillCommunityEmoji do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "does not update the community_name if the community does not have a community_emoji" do
+    Settings::Community.community_name = "Emoji-less Community"
+    expect do
+      described_class.new.run
+    end.not_to change { Settings::Community.community_name }.from("Emoji-less Community")
+  end
+
+  it "updates the community_name if the community has a community_emoji" do
+    Settings::Community.community_name = "Emoji Community"
+    Settings::Community.community_emoji = "🥐"
+
+    expect { described_class.new.run }
+      .to change { Settings::Community.community_name }.from("Emoji Community").to("Emoji Community 🥐")
+  end
 end
