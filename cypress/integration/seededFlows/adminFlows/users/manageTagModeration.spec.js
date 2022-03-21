0dc7cfec220bafc70f87ac881dbd26d380dacd80
Nick Taylor <nick@iamdeveloper.com>
2022-03-21 18:12:12 -0400

    Added more E2E tests

--- cypress/integration/seededFlows/adminFlows/users/manageTagModeration.spec.js
+++ cypress/integration/seededFlows/adminFlows/users/manageTagModeration.spec.js
@@ -9,8 +9,8 @@ describe('Manage Tags Moderated', () => {
     });
   });
 
-  describe('When a tag to moderate is added to the list of moderated tags for a user', () => {
-    it('should persist the added tags', () => {
+  describe('When moderating tags for a user', () => {
+    it('should add tags', () => {
       cy.visit('/admin/users/2');
       cy.findByRole('form', { name: 'Tag moderation' }).within(() => {
         cy.findByRole('textbox', { name: 'Assign tags' })
@@ -19,13 +19,28 @@ describe('Manage Tags Moderated', () => {
           .blur();
 
         cy.findByRole('button', { name: 'Edit tag1' });
-        cy.findByRole('button', { name: 'Edit tag1' });
+        cy.findByRole('button', { name: 'Edit tag2' });
         cy.findByRole('button', { name: 'Submit' }).click();
       });
 
       verifyAndDismissUserUpdatedMessage(
         'Tags moderated by the user have been updated',
       );
+    });
+
+    it('should remove tags', () => {
+      cy.visit('/admin/users/1');
+      cy.findByRole('form', { name: 'Tag moderation' }).within(() => {
+        cy.findByRole('button', { name: 'Remove tag1' }).click();
+        cy.findByRole('button', { name: 'Submit' }).click();
+      });
+
+      verifyAndDismissUserUpdatedMessage(
+        'Tags moderated by the user have been updated',
+      );
+
+      cy.findByRole('button', { name: 'Edit tag1' }).should('not.exist');
+      cy.findByRole('button', { name: 'Edit tag2' }).should('exist');
     });
   });
 });
