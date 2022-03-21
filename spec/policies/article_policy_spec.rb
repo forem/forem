88f4daf952c6ffb612136b4f211b3cf80cdb01fc
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2022-03-21 09:56:12 -0400

    Penciling in a Default Spaces section (#16897)
    * Penciling in a Default Spaces section
    
    This delivers two primary things:
    
    1.  Extracting shared policy examples
    2.  Providing a functioning skeleton for toggling on the
        "limit_post_creation_to_admins" feature.
    
    Important is that once merged, it would be possible for this code to
    "leak" out.  But how that leaks out is also how you can test that the
    feature works.
    
    First, to orient, there is a constraint for a new route.  That
    constraint checks if the feature flag exists (e.g. has been explicitly
    enabled or explicitly disabled).  In other words, at this point, you
    can't accidentally enable the feature via the UI.  But once you have
    enabled the feature, you can then toggle the feature on and off.
    
    So, to test this in the browser:
    
    1. Pull down this branch
    2. Run `rails runner "FeatureFlag.disable(:limit_post_creation_to_admins)"`
    3. Startup your the web server.
    4. Login as an administrator
    5. Goto /admin/content_manager/spaces
       a. Bask in the glory of a plain HTML form
    6. With another browser, login as another non-admin user
    7. Go to /settings/extensions, you should see a section Publishing from RSS.
    8. Now with the admin's session, update the form to turn on
      "limit_post_creation_to_admins"
    9. Back to the non-admin browser, refresh /settings/extensions, you
       should no longer see the section Publishing from RSS.
    
    Note: I have not included an admin menu item as that is related to and
    dependent on some refactors I'm working on (see forem/forem#16888 and
    forem/forem#16847).  So a bit of "security through obsurity"
    
    Note: In the future, once we resolve forem/forem#16490, we'll start
    toggling the "Create a Post" button.
    
    Note: I am not including Cypress tests nor request tests for this
    feature because the implementation details related to the testing via
    that approach are a little too volitale.
    
    Related to forem/forem#16842
    
    * Updating copy based on forem/forem#16893
    
    * styles
    
    * styles
    
    * dark styles
    
    Co-authored-by: Paweł Ludwiczak <ludwiczakpawel@gmail.com>

--- spec/policies/article_policy_spec.rb
+++ spec/policies/article_policy_spec.rb
@@ -1,65 +1,6 @@
 require "rails_helper"
 
-# These are methods that I envision extracting.  However, they are relevant only for these tests.
-# And to extract would require more explict interface definition.
-RSpec.shared_examples "it requires an authenticated user" do
-  let(:user) { nil }
-
-  it "otherwise raises ApplicationPolicy::UserRequiredError" do
-    expect { subject }.to raise_error(ApplicationPolicy::UserRequiredError)
-  end
-end
-
-RSpec.shared_examples "it requires a user in good standing" do
-  let(:user) { create(:user, :suspended) }
-
-  it "otherwise raises ApplicationPolicy::UserSuspendedError" do
-    expect { subject }.to raise_error(ApplicationPolicy::UserSuspendedError)
-  end
-end
-
-RSpec.shared_examples "permitted roles" do |**kwargs|
-  to = kwargs.delete(:to)
-  label = kwargs.except(:to).map { |key, value| "#{key} is #{value}" }.join(" AND ")
-  label = "when #{label} " if label.present?
-
-  Array(to).each do |role|
-    context "#{label}#{role.inspect} authorization" do
-      before { kwargs.each { |k, v| allow(described_class).to receive(k).and_return(v) } }
-
-      if role == :suspended_author
-        let(:author) { suspended_user }
-        let(:user) { author }
-      else
-        let(:user) { public_send(role) }
-      end
-
-      it { is_expected.to be_truthy }
-    end
-  end
-end
-
-RSpec.shared_examples "disallowed roles" do |**kwargs|
-  to = kwargs.delete(:to)
-  label = kwargs.map { |key, value| "#{key} is #{value}" }.join(" AND ")
-  label = "when #{label} " if label.present?
-
-  Array(to).each do |role|
-    context "#{label}#{role.inspect} authorization" do
-      before { kwargs.each { |k, v| allow(described_class).to receive(k).and_return(v) } }
-
-      if role == :suspended_author
-        let(:author) { suspended_user }
-        let(:user) { author }
-      else
-        let(:user) { public_send(role) }
-      end
-
-      it { is_expected.to be_falsey }
-    end
-  end
-end
-
+# See ./spec/policies/shared_examples/authorization_shared_examples.rb for the various shared examples.
 RSpec.describe ArticlePolicy do
   subject(:method_call) { policy.public_send(policy_method) }
 
