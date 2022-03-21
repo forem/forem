5af2e18de5a0fb6ca32a312460daa1b2f9b832ee
Mac Siri <krairit.siri@gmail.com>
2022-03-21 14:05:22 -0400

    Revert "Simplify specs"
    This reverts commit 230514ccea68445b057991d3d536fbb2a590a3b1.

--- spec/services/badges/award_contributor_from_github_spec.rb
+++ spec/services/badges/award_contributor_from_github_spec.rb
@@ -9,45 +9,76 @@ RSpec.describe Badges::AwardContributorFromGithub, type: :service, vcr: true do
     stub_const("#{described_class}::REPOSITORIES", ["forem/DEV-Android"])
   end
 
-  around do |example|
-    VCR.use_cassette("github_client_commits_contributor_badge") { example.run }
-  end
-
-  it "awards contributor badge once" do
+  it "awards contributor badge" do
     user = create(:user, :with_identity, identities: ["github"], uid: "389169")
 
     Timecop.freeze("2021-08-16T13:49:20Z") do
       expect do
-        described_class.call
-        described_class.call
+        VCR.use_cassette("github_client_commits_contributor_badge") do
+          described_class.call
+        end
       end.to change(user.badge_achievements, :count).by(1)
     end
   end
 
-  it "awards 4x contributor badge" do
+  it "awards contributor badge once" do
+    user = create(:user, :with_identity, identities: ["github"], uid: "389169")
+    Timecop.freeze("2021-08-16T13:49:20Z") do
+      expect do
+        VCR.use_cassette("github_client_commits_contributor_badge_twice") do
+          described_class.call
+          described_class.call
+        end
+      end.to change(user.badge_achievements, :count).by(1)
+    end
+  end
+
+  it "awards bronze contributor badge" do
     badge = create(:badge, title: "4x Commit Club")
     user = create(:user, :with_identity, identities: ["github"], uid: "459464")
-
     Timecop.freeze("2021-08-16T13:49:20Z") do
-      expect { described_class.call }.to change(user.badge_achievements.where(badge: badge), :count).by(1)
+      VCR.use_cassette("github_client_commits_contributor_badge") do
+        expect do
+          described_class.call
+        end.to change(user.badge_achievements.where(badge: badge), :count).by(1)
+      end
     end
   end
 
-  it "awards 8x contributor badge" do
+  it "awards silver contributor badge" do
     badge = create(:badge, title: "8x Commit Club")
     user = create(:user, :with_identity, identities: ["github"], uid: "6045239")
-
     Timecop.freeze("2021-08-16T13:49:20Z") do
-      expect { described_class.call }.to change(user.badge_achievements.where(badge: badge), :count).by(1)
+      VCR.use_cassette("github_client_commits_contributor_badge") do
+        expect do
+          described_class.call
+        end.to change(user.badge_achievements.where(badge: badge), :count).by(1)
+      end
     end
   end
 
-  it "awards 16x contributor badge" do
+  it "awards gold contributor badge" do
     badge = create(:badge, title: "16x-commit-club")
     user = create(:user, :with_identity, identities: ["github"], uid: "15793250")
 
     Timecop.freeze("2021-08-16T13:49:20Z") do
-      expect { described_class.call }.to change(user.badge_achievements.where(badge: badge), :count).by(1)
+      VCR.use_cassette("github_client_commits_contributor_badge") do
+        expect do
+          described_class.call
+        end.to change(user.badge_achievements.where(badge: badge), :count).by(1)
+      end
+    end
+  end
+
+  it "awards single commit contributors" do
+    stub_const("#{described_class}::REPOSITORIES", ["forem/forem"])
+    user = create(:user, :with_identity, identities: ["github"], uid: "49699333")
+    Timecop.freeze("2021-08-16T13:49:20Z") do
+      VCR.use_cassette("awards_single_commit_contributors") do
+        expect do
+          described_class.call
+        end.to change(user.badge_achievements, :count).by(1)
+      end
     end
   end
 end
