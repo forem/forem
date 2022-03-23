2b17db7ada3e15f71a27b2e33f950e2bda1297a8
Paweł Ludwiczak <ludwiczakpawel@gmail.com>
2022-03-23 14:45:54 +0100

    jeremy fix

--- app/services/admin/data_counts.rb
+++ app/services/admin/data_counts.rb
@@ -1,5 +1,16 @@
 module Admin
   class DataCounts
+    Response = Struct.new(
+      :open_abuse_reports_count,
+      :possible_spam_users_count,
+      :flags_count,
+      :flags_posts_count,
+      :flags_comments_count,
+      :flags_users_count,
+      keyword_init: true,
+    )
+
+    # @return [Admin::DataCounts::Response]
     def self.call
       # If this class is to be re-used in more situations, we could pass in params
       # to dictate which counts are needed, to reduce unneeded queries.
@@ -17,15 +28,13 @@ module Admin
       flags = Reaction
         .includes(:user, :reactable)
         .privileged_category
-      OpenStruct.new(
-        {
-          open_abuse_reports_count: open_abuse_reports_count,
-          possible_spam_users_count: possible_spam_users_count,
-          flags_count: flags.size,
-          flags_posts_count: flags.where(reactable_type: "Article").size,
-          flags_comments_count: flags.where(reactable_type: "Article").size,
-          flags_users_count: flags.where(reactable_type: "User").size
-        },
+      Response.new(
+        open_abuse_reports_count: open_abuse_reports_count,
+        possible_spam_users_count: possible_spam_users_count,
+        flags_count: flags.size,
+        flags_posts_count: flags.where(reactable_type: "Article").size,
+        flags_comments_count: flags.where(reactable_type: "Article").size,
+        flags_users_count: flags.where(reactable_type: "User").size,
       )
     end
   end
--- spec/services/admin/data_counts_spec.rb
+++ spec/services/admin/data_counts_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe Admin::DataCounts, type: :service do
   it "returns proper data type" do
-    expect(described_class.call).to be_an_instance_of(OpenStruct)
+    expect(described_class.call).to be_an_instance_of(described_class::Response)
   end
 
   it "returns an integer" do
