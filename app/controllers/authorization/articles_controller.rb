ce838672069fd703122156a0d82e5e6ad5398e0a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2022-03-28 17:24:51 -0400

    WIP

--- app/controllers/authorization/articles_controller.rb
+++ app/controllers/authorization/articles_controller.rb
@@ -4,11 +4,11 @@ module Authorization
     layout false
 
     before_action :authenticate_user!
-    after_action :verify_authorized
 
     def create_post_button
-      authorize Article, :create?
-      unless FeatureFlag.enabled?(:limit_post_creation_to_admins)
+      if policy(Article).create?
+        render text: "allowed", status: :ok
+      else
         render file: "public/404.html", status: :not_found
       end
     end
--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -110,15 +110,6 @@ RSpec.describe "Dashboards", type: :request do
 
         expect(response.body).not_to include("turbo-frame")
       end
-
-      it "renders turbo frame when enabled" do
-        FeatureFlag.enable("limit_post_creation_to_admins")
-
-        sign_in user
-        get dashboard_path
-
-        expect(response.body).to include("turbo-frame")
-      end
     end
 
     context "when logged but has no articles nor can create them" do
