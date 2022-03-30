daf267ba695fdcebe22b9ec851dafc272dbe4e9a
Dan Uber <dan@forem.com>
2022-03-30 11:05:25 -0500

    PR feedback: Avoid n+1 query for labels
    the original implementation of "label_for_attribute" had an n+1 query
    looping over each matched key.
    
    Follow suggested improvement and pull labels and attributes at once
    from the db and modify the returned hash.

--- app/decorators/profile_decorator.rb
+++ app/decorators/profile_decorator.rb
@@ -2,13 +2,7 @@ class ProfileDecorator < ApplicationDecorator
   # Return a Hash of the profile fields that should be rendered for a given
   # display area, e.g. :left_sidebar
   def ui_attributes_for(area:)
-    names = ProfileField.public_send(area).pluck(:attribute_name)
-    data.slice(*names)
-      .transform_keys { |k| label_for_attribute(k) }
-      .select { |k, v| k.present? && v.present? }
-  end
-
-  def label_for_attribute(attr)
-    ProfileField.find_by(attribute_name: attr)&.label
+    fields = ProfileField.public_send(area).pluck(:label, :attribute_name).to_h
+    fields.transform_values { |attribute_name| data[attribute_name] }.compact_blank
   end
 end
