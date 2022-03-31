3999a98e672c25c223c7e18657ef4c0b36e11aa3
Monica Mateiu <monica.mateiu@gmail.com>
2022-03-31 09:22:37 +0100

    Display analytics up to yesterday instead of today
    Stop displaying admin analytics for current day, as the data is incomplete. Instead, show analytics for any given period up to yesterday

--- spec/services/admin/charts_data_spec.rb
+++ spec/services/admin/charts_data_spec.rb
@@ -9,13 +9,8 @@ RSpec.describe Admin::ChartsData, type: :service do
     expect(described_class.new.call.map(&:first)).to eq(["Posts", "Comments", "Reactions", "New members"])
   end
 
-  it "returns proper current period number" do
-    create_list(:article, 3, published_at: 4.days.ago)
-    expect(described_class.new.call.first.second).to eq(3)
-  end
-
   it "returns proper previous period number" do
-    create_list(:article, 2, published_at: 9.days.ago)
+    create_list(:article, 2, published_at: 8.days.ago)
     expect(described_class.new.call.first.third).to eq(2)
   end
 
@@ -23,4 +18,27 @@ RSpec.describe Admin::ChartsData, type: :service do
     expect(described_class.new(20).call.first.fourth.size).to eq(20)
   end
 
+  describe "current period" do
+    it "returns proper number of items" do
+      create(:article, published_at: Time.zone.today)
+      create_list(:article, 3, published_at: 4.days.ago)
+      create_list(:article, 2, published_at: 7.days.ago)
+      create(:article, published_at: 8.days.ago)
+
+      expect(described_class.new.call.first.second).to eq(5)
+    end
+
+    it "ignores today" do
+      create(:article, published_at: Time.zone.today)
+
+      expect(described_class.new.call.first.second).to eq(0)
+    end
+
+    it "goes back seven days by default" do
+      create(:article, published_at: 7.days.ago)
+      create(:article, published_at: 8.days.ago)
+
+      expect(described_class.new.call.first.second).to eq(1)
+    end
+  end
 end
