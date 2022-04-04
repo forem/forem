99401449d4de13f82ab66ff9a4697c78ccbbd5d9
Monica Mateiu <monica.mateiu@gmail.com>
2022-04-04 20:17:32 +0100

    Display analytics up to yesterday instead of today (#17064)
    Stop displaying admin analytics for current day, as the data is incomplete. Instead, show analytics for any given period up to yesterday

--- app/controllers/admin/overview_controller.rb
+++ app/controllers/admin/overview_controller.rb
@@ -3,7 +3,7 @@ module Admin
     layout "admin"
     def index
       @length = (params[:period] || 7).to_i
-      @labels = (0..@length - 1).map { |n| n.days.ago.strftime("%b %d") }.reverse
+      @labels = (1..@length).map { |n| n.days.ago.strftime("%b %d") }.reverse
       @analytics = Admin::ChartsData.new(@length).call
       @data_counts = Admin::DataCounts.call
     end
--- spec/requests/admin/overview_spec.rb
+++ spec/requests/admin/overview_spec.rb
@@ -25,4 +25,82 @@ RSpec.describe "/admin", type: :request do
       expect(response.body).to include(ENV["HEROKU_RELEASE_CREATED_AT"])
     end
   end
+
+  describe "analytics" do
+    subject(:body) { response.body }
+
+    before do
+      Timecop.freeze("2019-04-30T12:00:00Z")
+      get admin_path
+    end
+
+    after { Timecop.return }
+
+    it { is_expected.to include("Analytics and trends") }
+    it { is_expected.to include("Yesterday") }
+    it { is_expected.to include("Apr 23") }
+
+    it "displays correct number of posts from past week" do
+      create(:article, published_at: Time.zone.today)
+      create(:article, published_at: 1.day.ago)
+      create(:article, published_at: 7.days.ago)
+      create(:article, published_at: 8.days.ago)
+      get admin_path
+
+      expect(body).to include "2</span> Posts"
+    end
+
+    it "displays correct number of comments from past week" do
+      create(:comment, created_at: Time.zone.today)
+      create(:comment, created_at: 1.day.ago)
+      create(:comment, created_at: 8.days.ago)
+      get admin_path
+
+      expect(body).to include "1</span> Comments"
+    end
+
+    it "displays correct number of reactions from past week" do
+      create(:reaction, created_at: Time.zone.today)
+      create(:reaction, created_at: 3.days.ago)
+      create(:reaction, created_at: 2.weeks.ago)
+      get admin_path
+
+      expect(body).to include "1</span> Reaction"
+    end
+
+    it "displays correct number of new members from past week" do
+      create(:user, registered_at: Time.zone.today)
+      create(:user, registered_at: 2.days.ago)
+      create(:user, registered_at: 10.days.ago)
+      get admin_path
+
+      expect(body).to include "1</span> New members"
+    end
+
+    it "does not display data from previous weeks", :aggregate_failures do
+      create(:article, published_at: 8.days.ago)
+      create(:comment, created_at: 2.weeks.ago)
+      create(:reaction, created_at: 1.month.ago)
+      create(:user, registered_at: 10.days.ago)
+      get admin_path
+
+      expect(body).to include "0</span> Posts"
+      expect(body).to include "0</span> Comments"
+      expect(body).to include "0</span> Reactions"
+      expect(body).to include "0</span> New members"
+    end
+
+    it "does not display data from today", :aggregate_failures do
+      create(:article, published_at: Time.zone.today)
+      create(:comment, created_at: Time.zone.today)
+      create(:reaction, created_at: Time.zone.today)
+      create(:user, registered_at: Time.zone.today)
+      get admin_path
+
+      expect(body).to include "0</span> Posts"
+      expect(body).to include "0</span> Comments"
+      expect(body).to include "0</span> Reactions"
+      expect(body).to include "0</span> New members"
+    end
+  end
 end
