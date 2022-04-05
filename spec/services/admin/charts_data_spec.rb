85749d83d925b2a48ca44b8f727a3395013b7cda
Daniel Uber <dan@forem.com>
2022-04-05 08:51:36 -0500

    Fix "current period" admin charts data examples (#17118)
    * Disable current period specs
    
    These are failing about half of the time (when the test runs east of
    UTC, like Perth or Astana, the counts are incorrect, when the test
    runs west of UTC like America/Chicago or Alaska, it passes).
    
    * Cheat by setting clock to noon UTC
    
    This aligns article published dates in ruby with those calculated by
    postgresql. Copy the decision made in
    https://github.com/forem/forem/pull/3518 for the analytics service.
    
    * Remove testing comments
    
    I had added titles to some of the articles I wanted excluded from the
    result to confirm they were being included.

--- spec/services/admin/charts_data_spec.rb
+++ spec/services/admin/charts_data_spec.rb
@@ -19,6 +19,16 @@ RSpec.describe Admin::ChartsData, type: :service do
   end
 
   describe "current period" do
+    # for the reasons described in the analytics service spec,
+    # we set the time in these tests to midday UTC to avoid the
+    # PostgreSQL DATE() function shifting articles in or out of the
+    # last week when considering N.days.ago locally
+    around do |example|
+      Timecop.freeze("2022-04-05T12:00:00Z")
+      example.run
+      Timecop.return
+    end
+
     it "returns proper number of items" do
       create(:article, published_at: Time.zone.today)
       create_list(:article, 3, published_at: 4.days.ago)
