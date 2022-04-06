459010dd6e5047d0d643f55be16f6ce54afae66f
Julianna Tetreault <32834804+juliannatetreault@users.noreply.github.com>
2022-04-06 08:21:18 -0600

    Adds a spec for #cascading_high_level_roles

--- spec/helpers/admin/users_helper_spec.rb
+++ spec/helpers/admin/users_helper_spec.rb
@@ -23,4 +23,30 @@ describe Admin::UsersHelper do
       expect(formatted_date).to eq date.to_s
     end
   end
+
+  describe "#cascading_high_level_roles" do
+    it "renders the proper role for a Super Admin" do
+      super_admin = create(:user, :super_admin)
+      role = helper.cascading_high_level_roles(super_admin)
+      expect(role).to eq "Super Admin"
+    end
+
+    it "renders the proper role for an Admin" do
+      admin = create(:user, :admin)
+      role = helper.cascading_high_level_roles(admin)
+      expect(role).to eq "Admin"
+    end
+
+    it "renders the proper role for a Resource Admin" do
+      resource_admin = create(:user, :single_resource_admin)
+      role = helper.cascading_high_level_roles(resource_admin)
+      expect(role).to eq "Resource Admin"
+    end
+
+    it "renders the proper role for a user that isn't a Super Admin, Admin, or Resource Admin" do
+      user = create(:user)
+      role = helper.cascading_high_level_roles(user)
+      expect(role).to eq ""
+    end
+  end
 end
