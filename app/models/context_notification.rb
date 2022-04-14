814ca26ef68e021ea0f7c34efb42a844f2a14e16
lightalloy <lightallloy@gmail.com>
2022-04-14 11:56:31 +0300

    Documentation for context notifications

--- app/models/context_notification.rb
+++ app/models/context_notification.rb
@@ -1,3 +1,8 @@
+# This model was created to track notifications for which events have been sent already.
+# E.g. when a notification about a published article is sent (a Notification record is created),
+# we create a ContextNotification record where context_id is the article id,
+# context_type is "Article", and action is "Published".
+# Currently, context notifications are created only for notifications about published articles.
 class ContextNotification < ApplicationRecord
   belongs_to :context, polymorphic: true
 
