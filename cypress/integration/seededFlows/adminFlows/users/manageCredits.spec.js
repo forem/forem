9584f57449c6b9ff67b8fcbf035d3f215ab94d9f
Ridhwana <ridhwana.khan16@gmail.com>
2022-04-22 12:08:18 +0200

    feat: attempt to fix Cypress test

--- cypress/integration/seededFlows/adminFlows/users/manageCredits.spec.js
+++ cypress/integration/seededFlows/adminFlows/users/manageCredits.spec.js
@@ -15,7 +15,7 @@ describe('Manage User Credits', () => {
       cy.fixture('users/adminUser.json').as('user');
       cy.get('@user').then((user) => {
         cy.loginAndVisit(user, '/admin/users');
-        cy.findByRole('link', { name: 'credits_user' }).click();
+        cy.findAllByText('Credits User').first().click();
       });
     });
 
