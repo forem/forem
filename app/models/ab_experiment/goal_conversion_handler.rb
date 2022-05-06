b7bf1743c0a388127be5371c3cf8ed5810a72fbc
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2022-05-06 09:16:28 -0400

    Adding a field test option for post publishing goals
    We have page view and comment view "goals" but have not looked at
    publication "goals".  We're using these "goals" to help us refine the
    feed towards sustainable engagement and community health.
    
    The conjecture on publication goals is that the content on the feed
    itself can nudge folks in their decision to publish posts.
    
    Closes forem/forem#17585

--- app/models/ab_experiment/goal_conversion_handler.rb
+++ app/models/ab_experiment/goal_conversion_handler.rb
@@ -7,6 +7,9 @@ class AbExperiment
   class GoalConversionHandler
     include FieldTest::Helpers
 
+    # The constant says "publishes post"; we're not actually concerned with the state change (from
+    # unpublished to published) but instead counting the number of published articles.
+    USER_PUBLISHES_POST_GOAL = "user_publishes_post".freeze
     USER_CREATES_PAGEVIEW_GOAL = "user_creates_pageview".freeze
     USER_CREATES_COMMENT_GOAL = "user_creates_comment".freeze
 
@@ -40,36 +43,58 @@ class AbExperiment
         # We have special conditional goals for some where we look for past events for cummulative wins
         # Otherwise we convert the goal as given.
       when USER_CREATES_PAGEVIEW_GOAL
-        pageview_goal(experiment,
-                      [7.days.ago, experiment_start_date].max,
-                      "DATE(created_at)",
-                      4,
-                      "user_views_pages_on_at_least_four_different_days_within_a_week")
-        pageview_goal(experiment,
-                      [24.hours.ago, experiment_start_date].max,
-                      "DATE_PART('hour', created_at)",
-                      4,
-                      "user_views_pages_on_at_least_four_different_hours_within_a_day")
-        pageview_goal(experiment,
-                      [14.days.ago, experiment_start_date].max,
-                      "DATE(created_at)",
-                      9,
-                      "user_views_pages_on_at_least_nine_different_days_within_two_weeks")
-        pageview_goal(experiment,
-                      [5.days.ago, experiment_start_date].max,
-                      "DATE_PART('hour', created_at)",
-                      12,
-                      "user_views_pages_on_at_least_twelve_different_hours_within_five_days")
+        convert_pageview_goal(experiment: experiment, experiment_start_date: experiment_start_date)
       when USER_CREATES_COMMENT_GOAL # comments goal. Only page views and comments are currently active.
-        field_test_converted(experiment, participant: user, goal: goal) # base single comment goal.
-        comment_goal(experiment,
-                     [7.days.ago, experiment_start_date].max,
-                     "DATE(created_at)",
-                     4,
-                     "user_creates_comment_on_at_least_four_different_days_within_a_week")
+        convert_comment_goal(experiment: experiment, experiment_start_date: experiment_start_date)
+      when USER_PUBLISHES_POST_GOAL
+        convert_post_goal(experiment: experiment, experiment_start_date: experiment_start_date)
       else
         field_test_converted(experiment, participant: user, goal: goal) # base single comment goal.
       end
+    end
+
+    def convert_pageview_goal(experiment:, experiment_start_date:)
+      pageview_goal(experiment,
+                    [7.days.ago, experiment_start_date].max,
+                    "DATE(created_at)",
+                    4,
+                    "user_views_pages_on_at_least_four_different_days_within_a_week")
+      pageview_goal(experiment,
+                    [24.hours.ago, experiment_start_date].max,
+                    "DATE_PART('hour', created_at)",
+                    4,
+                    "user_views_pages_on_at_least_four_different_hours_within_a_day")
+      pageview_goal(experiment,
+                    [14.days.ago, experiment_start_date].max,
+                    "DATE(created_at)",
+                    9,
+                    "user_views_pages_on_at_least_nine_different_days_within_two_weeks")
+      pageview_goal(experiment,
+                    [5.days.ago, experiment_start_date].max,
+                    "DATE_PART('hour', created_at)",
+                    12,
+                    "user_views_pages_on_at_least_twelve_different_hours_within_five_days")
+    end
+
+    def convert_comment_goal(experiment:, experiment_start_date:)
+      field_test_converted(experiment, participant: user, goal: goal) # base single comment goal.
+      comment_goal(experiment,
+                   [7.days.ago, experiment_start_date].max,
+                   "DATE(created_at)",
+                   4,
+                   "user_creates_comment_on_at_least_four_different_days_within_a_week")
+    end
+
+    def convert_post_goal(experiment:, experiment_start_date:)
+      field_test_converted(experiment, participant: user, goal: goal) # base is we created a post
+      post_goal(experiment,
+                [7.days.ago, experiment_start_date].max,
+                2,
+                "user_publishes_post_at_least_two_times_within_week")
+      post_goal(experiment,
+                [14.days.ago, experiment_start_date].max,
+                2,
+                "user_publishes_post_at_least_two_times_within_two_weeks")
     end
 
     def pageview_goal(experiment, time_start, group_value, min_count, goal)
@@ -77,6 +102,12 @@ class AbExperiment
         .group(group_value).count.values
       page_view_counts.delete(0)
       return unless page_view_counts.size >= min_count
+
+      field_test_converted(experiment, participant: user, goal: goal)
+    end
+
+    def post_goal(experiment, time_start, min_count, goal)
+      return unless user.articles.published.where("published_at > ?", time_start).count >= min_count
 
       field_test_converted(experiment, participant: user, goal: goal)
     end
