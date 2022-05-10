3b3847cb120aa67ca5d66fb43244835cec5da9bb
Julianna Tetreault <32834804+juliannatetreault@users.noreply.github.com>
2022-05-10 14:38:50 -0600

    Revert "Bump jest from 27.5.1 to 28.1.0 (#17612)"
    This reverts commit c2af21e730e4e8a20807b589250f20c878cd706e.

--- customJsDomEnvironment.js
+++ customJsDomEnvironment.js
@@ -1,26 +1,14 @@
 /* eslint-env node */
-const Environment = require('jest-environment-jsdom').default;
+// See https://github.com/jsdom/jsdom/issues/2524#issuecomment-736672511
+const Environment = require('jest-environment-jsdom');
 
 module.exports = class CustomTestEnvironment extends Environment {
-  constructor(config, context) {
-    super(config, context);
-  }
-
   async setup() {
     await super.setup();
-    // See https://github.com/jsdom/jsdom/issues/2524#issuecomment-736672511
     if (typeof this.global.TextEncoder === 'undefined') {
       const { TextEncoder, TextDecoder } = require('util');
       this.global.TextEncoder = TextEncoder;
       this.global.TextDecoder = TextDecoder;
     }
-  }
-
-  async teardown() {
-    await super.teardown();
-  }
-
-  getVmContext() {
-    return super.getVmContext();
   }
 };
