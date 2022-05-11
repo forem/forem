fd609ca37243fa8a243191790ce5c7e8f7cb98ce
Mac Siri <krairit.siri@gmail.com>
2022-05-11 19:07:18 -0400

    Revert "Bump ddtrace from 0.54.2 to 1.0.0 (#17560)"
    This reverts commit eeed96cf9cbd8bba18b6d4a6834a464521005ad2.

--- app/services/instrumentation.rb
+++ app/services/instrumentation.rb
@@ -2,6 +2,6 @@ module Instrumentation
   def instrument(operation, tags: [], &block)
     # TODO: (@jgaskins): Extract the knowledge of which tracing library
     # we're using, like we did with ForemStatsDriver
-    Datadog::Tracing.trace operation, tags: tags, &block
+    Datadog.tracer.trace operation, tags: tags, &block
   end
 end
