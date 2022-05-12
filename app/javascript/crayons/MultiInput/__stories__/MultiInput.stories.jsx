324735b7d90615979ab45cdc7a5316ff9fae8a88
Ridhwana <ridhwana.khan16@gmail.com>
2022-05-12 14:43:50 +0200

    feat: create a crayons component called MultiInput that appears under App Components/MultiInput

---
+++ app/javascript/crayons/MultiInput/__stories__/MultiInput.stories.jsx
@@ -0,0 +1,17 @@
+import { h } from 'preact';
+
+export default {
+  title: 'App Components/MultiInput',
+  argTypes: {},
+  parameters: {
+    docs: {},
+  },
+};
+
+export const Default = () => {
+  return <div>Multi Input</div>;
+};
+
+Default.story = {
+  name: 'default',
+};
