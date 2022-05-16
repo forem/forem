6f2b6d566cbbf303ffac9f1420299ffa2d40eb15
Fernando Valverde <fernando@visualcosita.com>
2022-05-16 10:38:35 -0600

    Long line reformat

--- spec/queries/comments/community_wellness_query_spec.rb
+++ spec/queries/comments/community_wellness_query_spec.rb
@@ -49,8 +49,8 @@ RSpec.describe Comments::CommunityWellnessQuery, type: :query do
       weeks_ago_array = result[index6]["serialized_weeks_ago"].split(",")
       comment_counts_array = result[index6]["serialized_comment_counts"].split(",")
 
-      oldest_comment_date_post_rollout = user6.comments.where("created_at > ?",
-                                                              "2022-05-01").order(created_at: :asc).first.created_at
+      post_rollout_comments = user6.comments.where("created_at > ?", "2022-05-01").order(created_at: :asc)
+      oldest_comment_date_post_rollout = post_rollout_comments.first.created_at
 
       # If `1.34` weeks have passed we should get no more than 2 weeks in the
       # result arrays because of the `limit_release_date_sql_query`. That's what
