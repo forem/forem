b69baa015b1d4135b36d8bde0ce02dc730b8e870
Suzanne Aitchison <suzanne@forem.com>
2022-05-23 17:51:27 +0100

    Revert "is this a preact issue?"
    This reverts commit 5f3f720f8e739cc3dcca7fd54f8d97e08d8bb88c.

--- preact.config.js
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line no-restricted-syntax
-export default function (config, env, helpers) {
-  const css = helpers.getLoadersByName(config, 'css-loader')[0];
-  css.loader.options.modules = false;
-}
