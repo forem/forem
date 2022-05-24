17152cfcad53cd5c38922e8077b372dddfb1b95e
Julianna Tetreault <32834804+juliannatetreault@users.noreply.github.com>
2022-05-24 10:12:50 -0600

    Adjusts GDPR Delete Requests Query search default

--- app/queries/admin/gdpr_delete_requests_query.rb
+++ app/queries/admin/gdpr_delete_requests_query.rb
@@ -3,7 +3,7 @@ module Admin
     QUERY_CLAUSE = "#{GDPRDeleteRequest.table_name}.email ILIKE :search OR " \
                    "#{GDPRDeleteRequest.table_name}.username ILIKE :search".freeze
 
-    def self.call(relation: ::GDPRDeleteRequest.all, search: nil)
+    def self.call(relation: ::GDPRDeleteRequest.all, search: {})
       relation = search_relation(relation, search) if search.presence
 
       relation.order(created_at: :desc)
