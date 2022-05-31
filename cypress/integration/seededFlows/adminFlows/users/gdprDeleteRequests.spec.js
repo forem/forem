b155749db06468c4455b6cbf4d9a627236214dc7
Julianna Tetreault <32834804+juliannatetreault@users.noreply.github.com>
2022-05-31 16:22:25 -0600

    Searchable GDPR Delete Requests Table (#17633)
    * Adds search functionality to GDPR delete requests table
    
    * Adds e2e tests for searching GDPR delete requests
    
    * WIP: Address PR review feedback and add a RSpec test
    
    * Updates spec name to include _query
    
    * Updates Adnub::GDPRDeleteRequests#index
    
    * Adjusts expectations within gdpr_delete_requests_query_spec.rb
    
    * Adjusts GDPR Delete Requests Query search default
    
    * Updates gdpr_delete_requests_query_spec.rb

--- cypress/integration/seededFlows/adminFlows/users/gdprDeleteRequests.spec.js
+++ cypress/integration/seededFlows/adminFlows/users/gdprDeleteRequests.spec.js
@@ -62,4 +62,35 @@ describe('GDPR Delete Requests', () => {
     }).should('not.exist');
     cy.get('@confirmButton').should('exist').should('have.focus');
   });
+
+  it('Searches for a user', () => {
+    // The user should be visible on the page
+    cy.findByRole('table')
+      .findByText('gdpr-delete-user@forem.local')
+      .should('exist');
+
+    // Search for a term that should match the entry
+    searchForMember('delete');
+
+    cy.url().should('contain', 'search=delete');
+    cy.findByRole('table')
+      .findByText('gdpr-delete-user@forem.local')
+      .should('exist');
+
+    // Search for a term that shouldn't match the entry
+    searchForMember('something');
+    cy.url().should('contain', 'search=something');
+    cy.findByRole('table')
+      .findByText('gdpr-delete-user@forem.local')
+      .should('not.exist');
+  });
+
+  const searchForMember = (searchTerm) => {
+    cy.findByRole('textbox', {
+      name: 'Search members by email or username',
+    })
+      .clear()
+      .type(searchTerm);
+    cy.findByRole('button', { name: 'Search' }).click();
+  };
 });
