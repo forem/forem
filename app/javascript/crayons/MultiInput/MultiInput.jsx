170fe16aaf4396e08d6602aae97be8f1500f3cdc
Ridhwana <ridhwana.khan16@gmail.com>
2022-06-02 15:28:47 +0200

    feat: update the classnames and remove unneeded html

--- app/javascript/crayons/MultiInput/MultiInput.jsx
+++ app/javascript/crayons/MultiInput/MultiInput.jsx
@@ -15,41 +15,25 @@ export const MultiInput = ({}) => {
         <label id="multi-select-label" className="">
           Example multi select autocomplete
         </label>
-        <span id="input-description" class="screen-reader-only">
-          Maximum 4 selections
-        </span>
 
-        <div class="screen-reader-only">
-          <p>Selected items:</p>
-          <ul
-            className="screen-reader-only list-none"
-            aria-live="assertive"
-            aria-atomic="false"
-            aria-relevant="additions removals"
-          >
-            <li>two</li>
-            <li>three</li>
-          </ul>
-        </div>
-
-        <div class="c-autocomplete--multi relative">
-          <div class="c-autocomplete--multi__wrapper-border crayons-textfield flex items-center cursor-text">
-            <ul id="combo-selected" class="list-none flex flex-wrap w-100">
+        <div class="c-input--multi relative">
+          <div class="c-input--multi__wrapper-border crayons-textfield flex items-center cursor-text">
+            <ul class="list-none flex flex-wrap w-100">
               <li
-                class="c-autocomplete--multi__selection-list-item w-max"
+                class="c-input--multi__selection-list-item w-max"
                 style="order: 1;"
               >
                 <div role="group" aria-label="two" class="flex mr-1 mb-1 w-max">
                   <button
                     type="button"
-                    class="c-btn c-autocomplete--multi__selected p-1 cursor-text"
+                    class="c-btn c-input--multi__selected p-1 cursor-text"
                     aria-label="Edit two"
                   >
                     two
                   </button>
                   <button
                     type="button"
-                    class="c-btn c-autocomplete--multi__selected p-1"
+                    class="c-btn c-input--multi__selected p-1"
                     aria-label="Remove two"
                   >
                     <svg
@@ -67,7 +51,7 @@ export const MultiInput = ({}) => {
               <li class="self-center" style="order: 3;">
                 <input
                   autocomplete="off"
-                  class="c-autocomplete--multi__input"
+                  class="c-input--multi__input"
                   aria-autocomplete="list"
                   aria-labelledby="multi-select-label selected-items-list"
                   aria-describedby="input-description"
