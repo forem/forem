8fb98b9f3002216505ce16b87c992fea8f7e57e2
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2022-06-02 20:41:31 -0400

    Exposing past feed experiments in /admin/abtests (#17818)
    Prior to this commit, as a matter of undocumented practice, we would
    overwrite past experiments with upcoming experiments.
    
    With this commit, we're leveraging a feature of the field_test gem:
    declaring a winner.  Instead of overwriting an experiment with the next
    experiment, we're going to declare the expiring experiment's winner then
    prepend to the ./config/field_test.yml the new experiment.
    
    The [field_test/app/views/field_test/experiments/index.html.erb][1] then
    uses logic to first render "active" experiments and "completed"
    experiments.  A "completed" experiment is one in which we've declared a
    winner.
    
    Closes forem/forem#17816
    
    [1]:https://github.com/ankane/field_test/blob/master/app/views/field_test/experiments/index.html.erb

--- spec/models/ab_experiment_spec.rb
+++ spec/models/ab_experiment_spec.rb
@@ -4,6 +4,27 @@ RSpec.describe AbExperiment do
   let(:controller) { ApplicationController.new }
   let(:user) { double }
 
+  describe "CURRENT_FEED_STRATEGY_EXPERIMENT" do
+    subject(:variant) { described_class::CURRENT_FEED_STRATEGY_EXPERIMENT }
+
+    it { is_expected.to be_a String }
+
+    it "is a key in the FieldTest.config['experiments']" do
+      expect(FieldTest.config["experiments"].keys).to include(variant)
+    end
+  end
+
+  describe "validate field test experiment configuration" do
+    it "only allow at most one active feed_strategy (e.g. an experiment without a winner)" do
+      field_tests = Psych.load(Rails.root.join("config/field_test.yml").read)
+      active_field_tests = field_tests.fetch("experiments", {})
+        .select { |key, values| key.start_with?("feed_strategy") && !values["winner"] }
+
+      # We are only going to allow
+      expect(active_field_tests.size).to be <= 1
+    end
+  end
+
   describe ".register_conversions_for" do
     it "forwards delegates to Converter.call" do
       allow(described_class::GoalConversionHandler).to receive(:call)
