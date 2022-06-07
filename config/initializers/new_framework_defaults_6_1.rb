29dcb172b5303b4e2107d30e52452b4c3ade5faf
Mac Siri <krairit.siri@gmail.com>
2022-06-07 15:07:10 -0400

    Enable urlsafe_csrf_tokens

--- config/initializers/new_framework_defaults_6_1.rb
+++ config/initializers/new_framework_defaults_6_1.rb
@@ -26,7 +26,7 @@ Rails.application.config.active_job.skip_after_callbacks_if_terminated = true
 #
 # This change is not backwards compatible with earlier Rails versions.
 # It's best enabled when your entire app is migrated and stable on 6.1.
-# Rails.application.config.action_controller.urlsafe_csrf_tokens = true
+Rails.application.config.action_controller.urlsafe_csrf_tokens = true
 
 # Specify whether `ActiveSupport::TimeZone.utc_to_local` returns a time with an
 # UTC offset or a UTC time.
