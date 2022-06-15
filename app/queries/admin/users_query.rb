b4f17ed7dc83864b7de63639db61292a91fd9116
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2022-06-15 08:50:03 -0400

    Allowing UserQuery to filter on multiple roles (#17919)
    I had looked at using `User.with_any_roles` but that creates one User
    query per role passed.  Which is not ideal, given that we want to have
    an ActiveRecord::Relation object on which we'd paginate.
    
    This adjustment does it's best to mimic Forem's role structure and
    adhear to how Rolify builds the underlying query.
    
    An astute reader will notice that in these specs we use `role: "admin"`
    and `roles: ["Admin"]`; the primary reason relates the user interface
    being worked on in forem/forem#17884.  It is my understanding that the
    `role` approach and `roles` approach will be separated by a feature
    flag (we're rolling out the `roles` approach).
    
    Related to
    - forem/forem#17491
    - forem/forem#17884

--- app/queries/admin/users_query.rb
+++ app/queries/admin/users_query.rb
@@ -4,17 +4,62 @@ module Admin
                    "users.email ILIKE :search OR " \
                    "users.username ILIKE :search".freeze
 
-    def self.call(relation: User.registered, options: {})
-      role, search = options.values_at(:role, :search)
+    # @api public
+    # @param relation [ActiveRecord::Relation<User>]
+    # @param role [String, nil]
+    # @param search [String, nil]
+    # @param roles [Array<String>, nil]
+    def self.call(relation: User.registered, role: nil, search: nil, roles: [])
+      # We are at an interstitial moment where we are exposing both the role and roles param.  We
+      # need to favor one or the other.
+      if role.presence
+        relation = relation.with_role(role, :any)
+      elsif roles.presence
+        relation = filter_roles(relation: relation, roles: roles)
+      end
 
-      relation = relation.with_role(role, :any) if role.presence
       relation = search_relation(relation, search) if search.presence
-
       relation.order(created_at: :desc)
     end
 
     def self.search_relation(relation, search)
       relation.where(QUERY_CLAUSE, search: "%#{search.strip}%")
     end
+
+    # Apply the "where" scope to the given relation for the given roles.
+    #
+    # @param relation [ActiveRecord::Relation<User>]
+    # @param roles [Array<String>]
+    #
+    # @note Why not use `relation.with_roles`; As implemented in Rolify's `.with_any_role` performs
+    #       one User query per role passed.  Given that we intend to use pagination, the one User
+    #       query per role is inadequate.
+    #
+    # @see https://github.com/RolifyCommunity/rolify/blob/0c883f4173f409766338b9c6dfc64b0fc8ec8a52/lib/rolify/finders.rb#L26-L32
+    def self.filter_roles(relation:, roles:, role_map: Constants::Role::SPECIAL_ROLES_LABELS_TO_WHERE_CLAUSE)
+      conditions = []
+      values = []
+
+      # Assemble the conditions and positional parameter values
+      roles.each do |role|
+        where_clause = role_map.fetch(role)
+        resource_type = where_clause.fetch(:resource_type, nil)
+        condition = "(#{Role.table_name}.name = ? AND #{Role.table_name}.resource_id IS NULL"
+        values << where_clause.fetch(:name)
+
+        # We need to use `IS NULL` instead of `= NULL` as those are different meanings.
+        if resource_type.nil?
+          condition += " AND #{Role.table_name}.resource_type IS NULL"
+        else
+          condition += " AND #{Role.table_name}.resource_type = ?"
+          values << resource_type
+        end
+        condition += ")"
+        conditions << condition
+      end
+
+      relation.joins(:roles).where(%((#{conditions.join(') OR (')})), *values)
+    end
+    private_class_method :filter_roles
   end
 end
