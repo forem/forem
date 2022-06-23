06b0fc4dcf27ff92b1a1d1d48ad48ac8097cad66
Julianna Tetreault <32834804+juliannatetreault@users.noreply.github.com>
2022-06-23 10:46:53 -0600

    WIP: Adds the banish user modal to member index

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -131,9 +131,15 @@ module Admin
     end
 
     def banish
+      set_banishable_user
       Moderator::BanishUserWorker.perform_async(current_user.id, params[:id].to_i)
       flash[:success] = I18n.t("admin.users_controller.banished")
-      redirect_to admin_user_path(params[:id])
+
+      if request.referer&.include?(admin_user_path(params[:id]))
+        redirect_to admin_user_path(params[:id])
+      else
+        redirect_to admin_users_path
+      end
     end
 
     def full_delete
