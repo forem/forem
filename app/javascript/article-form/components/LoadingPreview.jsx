55aa94119086b3adda15fe24c6aa3d6ec51953e9
Zerg <rajattalesra4914@gmail.com>
2022-06-23 12:04:54 +0530

    Removed unused code

--- app/javascript/article-form/components/LoadingPreview.jsx
+++ app/javascript/article-form/components/LoadingPreview.jsx
@@ -13,17 +13,15 @@ export const LoadingPreview = ({ version }) => {
   return (
     <div data-testid="loading-preview" title="Loading preview...">
       {cover}
-      <div className="crayons-story__body mt-6">
-        <div className="crayons-story__indention w-100">
-          <div className="crayons-scaffold-loading w-50 h-0 py-4 mb-2" />
-          <div className="crayons-story__meta w-100 mb-5">
-            <div className="crayons-scaffold-loading w-10 h-0 py-3 mr-2" />
-            <div className="crayons-scaffold-loading w-15 h-0 py-3" />
-          </div>
-          <div className="crayons-scaffold-loading w-80 h-0 py-3 mb-2" />
-          <div className="crayons-scaffold-loading w-60 h-0 py-3 mb-2" />
-          <div className="crayons-scaffold-loading w-70 h-0 py-3 mb-2" />
+      <div className="crayons-story__indention w-100 mt-6 ">
+        <div className="crayons-scaffold-loading w-50 h-0 py-4 mb-2" />
+        <div className="crayons-story__meta w-100 mb-5">
+          <div className="crayons-scaffold-loading w-10 h-0 py-3 mr-2" />
+          <div className="crayons-scaffold-loading w-15 h-0 py-3" />
         </div>
+        <div className="crayons-scaffold-loading w-80 h-0 py-3 mb-2" />
+        <div className="crayons-scaffold-loading w-60 h-0 py-3 mb-2" />
+        <div className="crayons-scaffold-loading w-70 h-0 py-3 mb-2" />
       </div>
     </div>
   );
