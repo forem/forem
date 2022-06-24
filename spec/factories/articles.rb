0a1b596dbd43ea42bc07cad1229454b64e8feaac
lightalloy <lightallloy@gmail.com>
2022-06-24 14:40:16 +0300

    Added a note and todo to articles factory past trait

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -72,6 +72,9 @@ FactoryBot.define do
     after(:create) { |article| create(:discussion_lock, locking_user_id: article.user_id, article_id: article.id) }
   end
 
+  # NOTE: [@lightalloy] This trait is used to create articles published in the past (with past published_at)
+  # we can't do it directly because of the validation Article#has_correct_published_at?
+  # TODO: [@lightalloy] Remove the trait and its usage when has_correct_published_at? will be removed
   trait :past do
     after(:create) do |article, evaluator|
       article.update_column(:published_at, evaluator.past_published_at)
