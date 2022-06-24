9d4e83a8adafe2e9c5a3a021109160bbe52a0dab
Josh Puetz <hi@joshpuetz.com>
2022-06-24 10:26:52 -0500

    Article test and examples, params

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -1,8 +1,9 @@
 require "rails_helper"
+require "swagger_helper"
 
 RSpec.describe "Api::V1::Articles", type: :request do
   let(:organization) { create(:organization) } # not used by every spec but lower times overall
-  let(:tag) { create(:tag, name: "discuss") }
+  let(:tag) { create(:tag, :with_colors, name: "discuss") }
   let(:article) { create(:article, featured: true, tags: "discuss") }
   let(:new_article) { create(:article) }
   let(:api_secret) { create(:api_secret) }
@@ -13,8 +14,96 @@ RSpec.describe "Api::V1::Articles", type: :request do
     allow(FeatureFlag).to receive(:enabled?).with(:api_v1).and_return(true)
   end
 
-  describe "GET /api/articles" do
-    before { article }
+  describe "GET /articles" do
+    before do
+      article.update_columns(organization_id: organization.id)
+    end
+
+    # rubocop:disable RSpec/VariableName
+    path "/api/articles" do
+      get "Published articles" do
+        tags "articles"
+        description "This endpoint allows the client to retrieve a list of articles.
+
+\"Articles\" are all the posts that users create on DEV that typically
+show up in the feed. They can be a blog post, a discussion question,
+a help thread etc. but is referred to as article within the code.
+
+By default it will return featured, published articles ordered
+by descending popularity.
+
+It supports pagination, each page will contain `30` articles by default."
+        operationId "getArticles"
+        produces "application/json"
+        parameter "$ref": "#/components/parameters/pageParam"
+        parameter "$ref": "#/components/parameters/perPageParam30to1000"
+        parameter name: :tag, in: :query, required: false,
+                  description: "Using this parameter will retrieve articles that contain the requested tag. Articles
+will be ordered by descending popularity.This parameter can be used in conjuction with `top`.",
+                  schema: { type: :string },
+                  example: "discuss"
+        parameter name: :tags, in: :query, required: false,
+                  description: "Using this parameter will retrieve articles with any of the comma-separated tags.
+Articles will be ordered by descending popularity.",
+                  schema: { type: :string },
+                  example: "javascript, css"
+        parameter name: :tags_exclude, in: :query, required: false,
+                  description: "Using this parameter will retrieve articles that do _not_ contain _any_
+of comma-separated tags. Articles will be ordered by descending popularity.",
+                  schema: { type: :string },
+                  example: "node, java"
+        parameter name: :username, in: :query, required: false,
+                  description: "Using this parameter will retrieve articles belonging
+            to a User or Organization ordered by descending publication date.
+            If `state=all` the number of items returned will be `1000` instead of the default `30`.
+            This parameter can be used in conjuction with `state`.",
+                  schema: { type: :string },
+                  example: "ben"
+        parameter name: :state, in: :query, required: false,
+                  description: "Using this parameter will allow the client to check which articles are fresh or rising.
+            If `state=fresh` the server will return fresh articles.
+            If `state=rising` the server will return rising articles.
+            This param can be used in conjuction with `username`, only if set to `all`.",
+                  schema: {
+                    type: :string,
+                    enum: %i[fresh rising all]
+                  },
+                  example: "fresh"
+        parameter name: :top, in: :query, required: false,
+                  description: "Using this parameter will allow the client to return the most popular articles
+in the last `N` days.
+`top` indicates the number of days since publication of the articles returned.
+This param can be used in conjuction with `tag`.",
+                  schema: {
+                    type: :integer,
+                    format: :int32,
+                    minimum: 1
+                  },
+                  example: 2
+        parameter name: :collection_id, in: :query, required: false,
+                  description: "Adding this will allow the client to return the list of articles
+belonging to the requested collection, ordered by ascending publication date.",
+                  schema: {
+                    type: :integer,
+                    format: :int32
+                  },
+                  example: 99
+
+        response "200", "A List of Articles" do
+          schema  type: :array,
+                  items: { "$ref": "#/components/schemas/ArticleIndex" }
+          add_examples
+
+          run_test!
+        end
+
+        response "401", "unauthorized" do
+          let(:Accept) { "application/vnd.forem.api-v1+json" }
+          run_test!
+        end
+      end
+    end
+    # rubocop:enable RSpec/VariableName
 
     it "returns 401 when unauthenticated" do
       get api_articles_path, headers: { "Accept" => "application/vnd.forem.api-v1+json" }
@@ -112,7 +201,6 @@ RSpec.describe "Api::V1::Articles", type: :request do
       end
 
       it "returns org's articles if org's slug is given" do
-        create(:article, user: article.user, organization: organization)
         get api_articles_path(username: organization.slug), headers: v1_headers
         expect(response.parsed_body.size).to eq(1)
       end
--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -14,22 +14,189 @@ RSpec.configure do |config|
   # the root example_group in your specs, e.g. describe "...", swagger_doc: "v2/swagger.json"
   config.swagger_docs = {
     "v1/swagger.yaml" => {
-      openapi: "3.0.1",
+      openapi: "3.0.3",
       info: {
-        title: "API V1",
-        version: "v1"
+        title: "Forem API V1",
+        version: "v1",
+        description: "Access Forem articles, users and other resources via API.
+        For a real-world example of Forem in action, check out [DEV](https://www.dev.to).
+        All endpoints require an 'api-key' header and a accept header.
+
+        Dates and date times, unless otherwise specified, must be in
+        the [RFC 3339](https://tools.ietf.org/html/rfc3339) format."
       },
       paths: {},
       servers: [
         {
-          url: "https://{defaultHost}",
-          variables: {
-            defaultHost: {
-              default: "www.example.com"
+          url: "https://dev.to/api",
+          description: "Production server"
+        },
+      ],
+      components: {
+        parameters: {
+          pageParam: {
+            in: :query,
+            name: :page,
+            required: false,
+            description: "Pagination page",
+            schema: {
+              type: :integer,
+              format: :int32,
+              minimum: 1,
+              default: 1
             }
+          },
+          perPageParam10to1000: {
+            in: :query,
+            name: :per_page,
+            required: false,
+            description: "Page size (the number of items to return per page)",
+            schema: {
+              type: :integer,
+              format: :int32,
+              minimum: 1,
+              maximum: 1000,
+              default: 10
+            }
+          },
+          perPageParam24to1000: {
+            in: :query,
+            name: :per_page,
+            required: false,
+            description: "Page size (the number of items to return per page)",
+            schema: {
+              type: :integer,
+              format: :int32,
+              minimum: 1,
+              maximum: 1000,
+              default: 24
+            }
+          },
+          perPageParam30to1000: {
+            in: :query,
+            name: :per_page,
+            required: false,
+            description: "Page size (the number of items to return per page)",
+            schema: {
+              type: :integer,
+              format: :int32,
+              minimum: 1,
+              maximum: 1000,
+              default: 30
+            }
+          },
+          perPageParam30to100: {
+            in: :query,
+            name: :per_page,
+            required: false,
+            description: "Page size (the number of items to return per page)",
+            schema: {
+              type: :integer,
+              format: :int32,
+              minimum: 1,
+              maximum: 100,
+              default: 30
+            }
+          },
+          perPageParam80to1000: {
+            in: :query,
+            name: :per_page,
+            required: false,
+            description: "Page size (the number of items to return per page)",
+            schema: {
+              type: :integer,
+              format: :int32,
+              minimum: 1,
+              maximum: 1000,
+              default: 80
+            }
+          },
+          listingCategoryParam: {
+            name: :category,
+            in: :query,
+            description: "Using this parameter will return listings belonging to the
+              requested category.",
+            schema: {
+              type: :string
+            },
+            example: "cfp"
           }
         },
-      ]
+        schemas: {
+          ArticleFlareTag: {
+            description: "Flare tag of the article",
+            type: "object",
+            properties: {
+              name: { type: :string },
+              bg_color_hex: { description: "Background color (hexadecimal)", type: :string },
+              text_color_hex: { description: "Text color (hexadecimal)", type: :string }
+            }
+          },
+          ArticleIndex: {
+            description: "Resprentation of an article or post returned in a list",
+            type: :object,
+            properties: {
+              type_of: { type: :string },
+              id: { type: :integer, format: :int32 },
+              title: { type: :string },
+              description: { type: :string },
+              cover_image: { type: :string, format: :url, nullable: true },
+              readable_publish_date: { type: :string },
+              social_image: { type: :string, format: :url },
+              tag_list: { type: :array, items: {
+                type: :string
+              } },
+              tags: { type: :string },
+              slug: { type: :string },
+              path: { type: :string, format: "path" },
+              url: { type: :string, format: :url },
+              canonical_url: { type: :string, format: :url },
+              positive_reactions_count: { type: :integer, format: :int32 },
+              public_reactions_count: { type: :integer, format: :int32 },
+              created_at: { type: :string, format: "date-time" },
+              edited_at: { type: :string, format: "date-time", nullable: true },
+              crossposted_at: { type: :string, format: "date-time", nullable: true },
+              published_at: { type: :string, format: "date-time" },
+              last_comment_at: { type: :string, format: "date-time" },
+              published_timestamp: { description: "Crossposting or published date time", type: :string,
+                                     format: "date-time" },
+              reading_time_minutes: { description: "Reading time, in minutes", type: :integer, format: :int32 },
+              user: { "$ref": "#/components/schemas/SharedUser" },
+              flare_tag: { "$ref": "#/components/schemas/ArticleFlareTag" },
+              organization: { "$ref": "#/components/schemas/SharedOrganization" }
+            },
+            required: %w[type_of id title description cover_image readable_publish_date
+                         social_image tag_list tags slug path url canonical_url comments_count
+                         positive_reactions_count public_reactions_count created_at edited_at
+                         crossposted_at published_at last_comment_at published_timestamp user
+                         reading_time_minutes]
+          },
+          SharedUser: {
+            description: "The resource creator",
+            type: "object",
+            properties: {
+              name: { type: :string },
+              username: { type: :string },
+              twitter_username: { type: :string, nullable: true },
+              github_username: { type: :string, nullable: true },
+              website_url: { type: :string, format: :url, nullable: true },
+              profile_image: { description: "Profile image (640x640)", type: :string },
+              profile_image_90: { description: "Profile image (90x90)", type: :string }
+            }
+          },
+          SharedOrganization: {
+            description: "The organization the resource belongs to",
+            type: "object",
+            properties: {
+              name: { type: :string },
+              username: { type: :string },
+              slug: { type: :string },
+              profile_image: { description: "Profile image (640x640)", type: :string, format: :url },
+              profile_image_90: { description: "Profile image (90x90)", type: :string, format: :url }
+            }
+          }
+        }
+      }
     }
   }
 
@@ -38,4 +205,25 @@ RSpec.configure do |config|
   # the key, this may want to be changed to avoid putting yaml in json files.
   # Defaults to json. Accepts ":json" and ":yaml".
   config.swagger_format = :yaml
+end
+
+# Convenience method for creating an example section for a response section
+module Rswag
+  module Specs
+    module ExampleGroupHelpers
+      def add_examples
+        after do |example|
+          # No metadata to generate for empty responses like 201 and 204.
+          next unless respond_to?(:response) && response&.body.present?
+
+          # Generate the examples for the API docs.
+          example.metadata[:response][:content] = {
+            "application/json" => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+      end
+    end
+  end
 end
