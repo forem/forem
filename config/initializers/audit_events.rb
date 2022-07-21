51d0f072e230af4f95855fc64b5400f59632efe7
Fernando Valverde <fernando@fdo.cr>
2022-07-21 08:03:51 -0600

    Use single line in notification subscribe initializer

--- config/initializers/audit_events.rb
+++ config/initializers/audit_events.rb
@@ -7,6 +7,5 @@
 # Audit::Subscribe.listen :internal, :quest_user
 
 Rails.application.reloader.to_prepare do
-  Audit::Subscribe.listen(:moderator, :internal) unless Rails.env.test?
-  Audit::Subscribe.listen(:admin_api, :internal) unless Rails.env.test?
+  Audit::Subscribe.listen(:moderator, :internal, :admin_api) unless Rails.env.test?
 end
