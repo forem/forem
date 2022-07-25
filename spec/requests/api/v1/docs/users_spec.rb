11001b0bcd549d35b5ce1a054bc150b9d06159b9
Josh Puetz <hi@joshpuetz.com>
2022-07-25 15:49:41 -0500

    Document /api/users/:id/suspend

--- spec/requests/api/v1/docs/users_spec.rb
+++ spec/requests/api/v1/docs/users_spec.rb
@@ -50,7 +50,7 @@ request is completed on the server."
                   },
                   example: 1
 
-        response "204", "Article successfully unpublished" do
+        response "204", "User's articles and comments successfully unpublished" do
           let(:"api-key") { api_secret.secret }
           let(:id) { banned_user.id }
           add_examples
@@ -68,7 +68,7 @@ request is completed on the server."
           run_test!
         end
 
-        response "204", "Unknown User ID (still accepted for async processing" do
+        response "204", "Unknown User ID (still accepted for async processing)" do
           let(:"api-key") { api_secret.secret }
           let(:id) { 10_000 }
           add_examples
@@ -79,66 +79,61 @@ request is completed on the server."
     end
   end
 
-  #   describe "PUT /users/:id/suspend" do
-  #     before do
-  #       user.add_role(:admin)
-  #     end
+  describe "PUT /users/:id/suspend" do
+    before do
+      user.add_role(:admin)
+    end
 
-  #     path "/api/users/{id}/suspend" do
-  #       put "Suspend a User" do
-  #         tags "articles", "comments", "users"
-  #         description "This endpoint allows the client to unpublish all of the articles and
-  # comments created by a user.
+    path "/api/users/{id}/suspend" do
+      put "Suspend a User" do
+        tags "users"
+        description "This endpoint allows the client to a user.
 
-  # The user associated with the API key must have either the 'admin' or 'moderator' role.
+The user associated with the API key must have either the 'admin' or 'moderator' role.
 
-  # This specified users's article and comments will be unpublished and will no longer be
-  # visible to the public. They will remain in the database and will set back to draft status
-  # on the specified user's  dashboard. Any notifications associated with the specified user's
-  # articles and comments will be deleted.
+This specified users will be assigned the 'suspended' role. Suspending a user will stop the
+user from posting new posts and comments. It doesn't delete any of the user's content, just
+prevents them from creating new content while suspended. Users are not notified of their suspension
+in the UI, so if you want them to know about this, you must notify them."
+        operationId "suspendUser"
+        produces "application/json"
+        parameter name: :id, in: :path, required: true,
+                  description: "The ID of the user to suspend.",
+                  schema: {
+                    type: :integer,
+                    format: :int32,
+                    minimum: 1
+                  },
+                  example: 1
 
-  # Note this endpoint unpublishes articles and comments asychronously: it will return a 204 NO CONTENT
-  # status code immediately, but the articles and comments will not be unpublished until the
-  # request is completed on the server."
-  #         operationId "unpublishUser"
-  #         produces "application/json"
-  #         parameter name: :id, in: :path, required: true,
-  #                   description: "The ID of the user to unpublish.",
-  #                   schema: {
-  #                     type: :integer,
-  #                     format: :int32,
-  #                     minimum: 1
-  #                   },
-  #                   example: 1
+        response "204", "User successfully unpublished" do
+          let(:"api-key") { api_secret.secret }
+          let(:id) { banned_user.id }
+          add_examples
 
-  #         response "204", "Article successfully unpublished" do
-  #           let(:"api-key") { api_secret.secret }
-  #           let(:id) { banned_user.id }
-  #           add_examples
+          run_test!
+        end
 
-  #           run_test!
-  #         end
+        response "401", "Unauthorized" do
+          let(:regular_user) { create(:user) }
+          let(:low_security_api_secret) { create(:api_secret, user: regular_user) }
+          let(:"api-key") { low_security_api_secret.secret }
+          let(:id) { banned_user.id }
+          add_examples
 
-  #         response "401", "Unauthorized" do
-  #           let(:regular_user) { create(:user) }
-  #           let(:low_security_api_secret) { create(:api_secret, user: regular_user) }
-  #           let(:"api-key") { low_security_api_secret.secret }
-  #           let(:id) { banned_user.id }
-  #           add_examples
+          run_test!
+        end
 
-  #           run_test!
-  #         end
+        response "404", "Unknown User ID" do
+          let(:"api-key") { api_secret.secret }
+          let(:id) { 10_000 }
+          add_examples
 
-  #         response "204", "Unknown User ID (still accepted for async processing" do
-  #           let(:"api-key") { api_secret.secret }
-  #           let(:id) { 10_000 }
-  #           add_examples
-
-  #           run_test!
-  #         end
-  #       end
-  #     end
-  #   end
+          run_test!
+        end
+      end
+    end
+  end
 end
 # rubocop:enable RSpec/VariableName
 # rubocop:enable RSpec/EmptyExampleGroup
