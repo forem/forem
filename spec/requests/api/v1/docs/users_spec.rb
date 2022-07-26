ca02a1b06f1744d0ef13ee6499cfb90482eb03a3
Josh Puetz <hi@joshpuetz.com>
2022-07-26 09:47:46 -0500

    Update spec/requests/api/v1/docs/users_spec.rb
    Co-authored-by: Suzanne Aitchison <suzanne@forem.com>

--- spec/requests/api/v1/docs/users_spec.rb
+++ spec/requests/api/v1/docs/users_spec.rb
@@ -91,7 +91,7 @@ request is completed on the server."
 
 The user associated with the API key must have either the 'admin' or 'moderator' role.
 
-This specified users will be assigned the 'suspended' role. Suspending a user will stop the
+This specified user will be assigned the 'suspended' role. Suspending a user will stop the
 user from posting new posts and comments. It doesn't delete any of the user's content, just
 prevents them from creating new content while suspended. Users are not notified of their suspension
 in the UI, so if you want them to know about this, you must notify them."
