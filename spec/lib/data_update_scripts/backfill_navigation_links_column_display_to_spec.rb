6afc8dce75814983922f84afa99450b26738e9dd
Suzanne Aitchison <suzanne@forem.com>
2022-08-04 15:59:47 +0100

    test for the backfill DUS

--- spec/lib/data_update_scripts/backfill_navigation_links_column_display_to_spec.rb
+++ spec/lib/data_update_scripts/backfill_navigation_links_column_display_to_spec.rb
@@ -4,5 +4,21 @@ require Rails.root.join(
 )
 
 describe DataUpdateScripts::BackfillNavigationLinksColumnDisplayTo do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "backfills relevant navigation links with 'logged_in' value for display_to" do
+    logged_in_navigation_link = create(:navigation_link, url: "/privacy", display_only_when_signed_in: true,
+                                                         display_to: "all")
+
+    expect do
+      described_class.new.run
+    end.to change { logged_in_navigation_link.reload.display_to }.from("all").to("logged_in")
+  end
+
+  it "leaves visible-to-all navigation links unchanged" do
+    visible_to_all_navigation_link = create(:navigation_link, url: "/readinglist", display_only_when_signed_in: false,
+                                                              display_to: "all")
+
+    expect do
+      described_class.new.run
+    end.not_to change { visible_to_all_navigation_link.reload.display_to }
+  end
 end
