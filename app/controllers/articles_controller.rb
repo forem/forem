fca555cede3756e5d998171cd42f93f336c1eeea
lightalloy <lightallloy@gmail.com>
2022-08-11 21:22:24 +0300

    Fixed specs for editor v1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -328,7 +328,7 @@ class ArticlesController < ApplicationController
       time_zone = Time.find_zone(time_zone_str)
       time_zone ||= Time.find_zone("UTC")
       params["article"]["published_at"] = time_zone.parse("#{date} #{time}")
-    else
+    elsif params["article"]["version"] != "v1"
       params["article"]["published_at"] = nil
     end
 
