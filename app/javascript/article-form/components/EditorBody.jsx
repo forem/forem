0806ced8e3e4099c67180c0016afa3993d3af7b3
Zerg <rajattalesra4914@gmail.com>
2022-08-23 22:35:33 +0530

    Image upload failure added to EditorBody

--- app/javascript/article-form/components/EditorBody.jsx
+++ app/javascript/article-form/components/EditorBody.jsx
@@ -14,28 +14,86 @@ import { useDragAndDrop } from '@utilities/dragAndDrop';
 import { fetchSearch } from '@utilities/search';
 import { AutocompleteTriggerTextArea } from '@crayons/AutocompleteTriggerTextArea';
 
-function handleImageSuccess(textAreaRef) {
+// Placeholder text displayed while an image is uploading
+const UPLOADING_IMAGE_PLACEHOLDER = '![Uploading image](...)';
+
+const handleImageUploading = (textAreaRef) => {
+  return function () {
+    // Function is within the component to be able to access
+    // textarea ref.
+
+    // Update this case:
+    // If a string is selected, then the markdownImageLink should be added at the end of that selection
+    // and selection should be removed.
+    const editableBodyElement = textAreaRef.current;
+
+    const { selectionStart, selectionEnd, value } = editableBodyElement;
+    const before = value.substring(0, selectionStart);
+    const after = value.substring(selectionEnd, value.length);
+    const newSelectionStart = `${before}\n${UPLOADING_IMAGE_PLACEHOLDER}`
+      .length;
+
+    editableBodyElement.value = `${before}\n${UPLOADING_IMAGE_PLACEHOLDER}\n${after}`;
+    editableBodyElement.selectionStart = newSelectionStart;
+    editableBodyElement.selectionEnd = newSelectionStart;
+  };
+};
+
+const handleImageSuccess = (textAreaRef) => {
   return function (response) {
     // Function is within the component to be able to access
     // textarea ref.
     const editableBodyElement = textAreaRef.current;
     const { links } = response;
 
-    const markdownImageLink = `![Image description](${links[0]})\n`;
+    const markdownImageLink = `![Image description](${links[0]})`;
     const { selectionStart, selectionEnd, value } = editableBodyElement;
-    const before = value.substring(0, selectionStart);
-    const after = value.substring(selectionEnd, value.length);
+    if (value.includes(UPLOADING_IMAGE_PLACEHOLDER)) {
+      const newSelectedStart =
+        value.indexOf(UPLOADING_IMAGE_PLACEHOLDER, 0) +
+        markdownImageLink.length;
 
-    editableBodyElement.value = `${before + markdownImageLink} ${after}`;
-    editableBodyElement.selectionStart =
-      selectionStart + markdownImageLink.length;
-    editableBodyElement.selectionEnd = editableBodyElement.selectionStart;
+      editableBodyElement.value = value.replace(
+        UPLOADING_IMAGE_PLACEHOLDER,
+        markdownImageLink,
+      );
+      editableBodyElement.selectionStart = newSelectedStart;
+      editableBodyElement.selectionEnd = newSelectedStart;
+    } else {
+      const before = value.substring(0, selectionStart);
+      const after = value.substring(selectionEnd, value.length);
 
-    // Dispatching a new event so that linkstate, https://github.com/developit/linkstate,
-    // the function used to create the onChange prop gets called correctly.
-    editableBodyElement.dispatchEvent(new Event('input'));
+      editableBodyElement.value = `${before}\n${markdownImageLink}\n${after}`;
+      editableBodyElement.selectionStart =
+        selectionStart + markdownImageLink.length;
+      editableBodyElement.selectionEnd = editableBodyElement.selectionStart;
+    }
   };
-}
+};
+
+const handleImageUploadFailure = (textAreaRef) => {
+  return function (message) {
+    // Function is within the component to be able to access
+    // textarea ref.
+    handleImageFailure(message);
+    const editableBodyElement = textAreaRef.current;
+
+    const { value } = editableBodyElement;
+    if (value.includes(`\n${UPLOADING_IMAGE_PLACEHOLDER}\n`)) {
+      const newSelectionStart = value.indexOf(
+        `\n${UPLOADING_IMAGE_PLACEHOLDER}\n`,
+        0,
+      );
+
+      editableBodyElement.value = value.replace(
+        `\n${UPLOADING_IMAGE_PLACEHOLDER}\n`,
+        '',
+      );
+      editableBodyElement.selectionStart = newSelectionStart;
+      editableBodyElement.selectionEnd = newSelectionStart;
+    }
+  };
+};
 
 export const EditorBody = ({
   onChange,
@@ -47,8 +105,9 @@ export const EditorBody = ({
 
   const { setElement } = useDragAndDrop({
     onDrop: handleImageDrop(
+      handleImageUploading(textAreaRef),
       handleImageSuccess(textAreaRef),
-      handleImageFailure,
+      handleImageUploadFailure(textAreaRef),
     ),
     onDragOver,
     onDragExit,
@@ -56,8 +115,9 @@ export const EditorBody = ({
 
   const setPasteElement = usePasteImage({
     onPaste: handleImagePasted(
+      handleImageUploading(textAreaRef),
       handleImageSuccess(textAreaRef),
-      handleImageFailure,
+      handleImageUploadFailure(textAreaRef),
     ),
   });
 
