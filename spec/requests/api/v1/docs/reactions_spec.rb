276d7990e6973848e2d6093b13a1946631e4c41e
Joshua Wehner <joshua@forem.com>
2022-08-25 14:05:58 +0200

    Add tests for create API endpoint

--- spec/requests/api/v1/docs/reactions_spec.rb
+++ spec/requests/api/v1/docs/reactions_spec.rb
@@ -20,13 +20,61 @@ RSpec.describe "api/v1/reactions", type: :request do
   end
 
   path "/api/reactions/toggle" do
-    describe "post to create reaction" do
-      post("create reaction") do
+    describe "post to toggle reaction" do
+      post("toggle reaction") do
         tags "reactions"
         description(<<-DESCRIBE.strip)
         This endpoint allows the client to toggle the user's reaction to a specified reactable (eg, Article, Comment, or User). For examples:
         * "Like"ing an Article will create a new "like" Reaction from the user for that Articles
         * "Like"ing that Article a second time will remove the "like" from the user
+        DESCRIBE
+
+        produces "application/json"
+        parameter name: :category, in: :query, required: true, schema: {
+          type: :string,
+          enum: Reaction::PUBLIC_CATEGORIES
+        }
+        parameter name: :reactable_id, in: :query, required: true, schema: {
+          type: :integer,
+          format: :int32
+        }
+        parameter name: :reactable_type, in: :query, required: true, schema: {
+          type: :string,
+          enum: Reaction::REACTABLE_TYPES
+        }
+
+        let(:reactable_id) { reactable.id }
+        let(:reactable_type) { "Article" }
+
+        before do
+          result.action = "create"
+        end
+
+        response(200, "successful") do
+          let(:"api-key") { api_secret.secret }
+          add_examples
+
+          run_test!
+        end
+
+        response "401", "unauthorized" do
+          let(:"api-key") { "invalid" }
+          add_examples
+
+          run_test!
+        end
+      end
+    end
+  end
+
+  path "/api/reactions" do
+    describe "post to create reaction" do
+      post("create reaction") do
+        tags "reactions"
+        description(<<-DESCRIBE.strip)
+        This endpoint allows the client to create a reaction to a specified reactable (eg, Article, Comment, or User). For examples:
+        * "Like"ing an Article will create a new "like" Reaction from the user for that Articles
+        * "Like"ing that Article a second time will return the previous "like"
         DESCRIBE
 
         produces "application/json"
