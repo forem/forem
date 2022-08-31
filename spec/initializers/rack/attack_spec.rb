b0e5be6b3d96771e6073795c77f846558472fb78
Mac Siri <krairit.siri@gmail.com>
2022-08-31 17:42:09 -0400

    Revert "Move rack_attack.rb (#18402)"
    This reverts commit 560bff94035b19d7312493892bb02156b3fa8b7b.

--- spec/initializers/rack/attack_spec.rb
+++ spec/initializers/rack/attack_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe Rack, ".attack", type: :request, throttle: true do
+describe Rack::Attack, type: :request, throttle: true do
   before do
     cache_db = ActiveSupport::Cache.lookup_store(:redis_cache_store)
     allow(Rails).to receive(:cache) { cache_db }
