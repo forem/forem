ca2006debd48f33916facfeba3ea2872aeb60bee
Mac Siri <krairit.siri@gmail.com>
2022-08-31 17:42:20 -0400

    Revert "Use remote_ip in Rack::Attack (#18397)"
    This reverts commit 9c0fc6b918bf13220e8e8f58451b30405657c15c.

--- lib/rack/attack/request.rb
+++
@@ -1,9 +0,0 @@
-module Rack
-  class Attack
-    class Request < ::Rack::Request
-      def remote_ip
-        @remote_ip ||= ActionDispatch::Request.new(env).remote_ip
-      end
-    end
-  end
-end
