b5956bc3754027aac87520d6cd7acbc6228f9139
James Wu <ichangjwu@outlook.com>
2022-09-02 02:03:45 +0900

    Add more system specs for viewing a comment

--- spec/system/comments/user_views_a_comment_spec.rb
+++ spec/system/comments/user_views_a_comment_spec.rb
@@ -36,4 +36,34 @@ RSpec.describe "Viewing a comment", type: :system, js: true do
       expect(page).to have_selector(".comment-date time", text: date)
     end
   end
+
+  context "when a year has passed" do
+    before do
+      comment
+      Timecop.freeze(1.year.from_now)
+    end
+
+    it "shows the published date in the correct format" do
+      sign_in user
+      visit comment.path
+      date = comment.created_at.getlocal.strftime("%b %-d '%y")
+
+      expect(page).to have_selector(".comment-date time", text: date)
+    end
+  end
+
+  context "when the comment is edited and a year has passed" do
+    before do
+      comment.update(body_markdown: "This message is edited.", edited_at: 1.day.from_now)
+      Timecop.freeze(1.year.from_now)
+    end
+
+    it "shows the edited date in the correct format" do
+      sign_in user
+      visit comment.path
+      date = comment.edited_at.getlocal.strftime("%b %-d")
+
+      expect(page).to have_content("Edited on #{date}")
+    end
+  end
 end
