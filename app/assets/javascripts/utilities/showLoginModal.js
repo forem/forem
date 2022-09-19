5ac4d4bea24cc273ff76c6c4f22ebc461c064c0d
Ridhwana <ridhwana.khan16@gmail.com>
2022-09-19 12:09:08 +0200

    feat: referrer page

--- app/assets/javascripts/utilities/showLoginModal.js
+++ app/assets/javascripts/utilities/showLoginModal.js
@@ -5,16 +5,18 @@ function showLoginModal(trackingData) {
     contentSelector: '#global-signup-modal',
     overlay: true,
     onOpen: () => {
+      const referrer = location.href;
       document
         .querySelector('#window-modal .js-global-signup-modal__create-account')
-        .addEventListener('click', () => ahoyTracking(trackingData));
+        .addEventListener('click', () => ahoyTracking(trackingData, referrer));
     },
   });
 }
 
-function ahoyTracking(trackingData) {
+function ahoyTracking(trackingData, referrer) {
   ahoy.track('Clicked on Create Account', {
     page: location.href,
+    referrer_page: referrer,
     source: 'modal',
     secondary_source: trackingData.secondary_source,
     trigger: trackingData.trigger,
