5a9f1548dcfd2a3eebc4364f8cbad65835c7bf90
Joshua Wehner <joshua@forem.com>
2022-09-29 15:23:54 +0200

    More testing for rollup

--- spec/services/display_ad_event_rollup_spec.rb
+++ spec/services/display_ad_event_rollup_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe DisplayAdEventRollup, type: :service do
   let(:ad1) { create :display_ad }
   let(:ad2) { create :display_ad }
   let(:user1) { create :user }
+  let(:user2) { create :user }
 
   def override_timestamps
     DisplayAdEvent.record_timestamps = false
@@ -15,25 +16,96 @@ RSpec.describe DisplayAdEventRollup, type: :service do
     (Date.current - num.days).all_day
   end
 
-  it "compacts display_ad_events" do
-    override_timestamps do
-      create(:display_ad_event, created_at: Date.current - 2, display_ad: ad1, user_id: nil, updated_at: Date.current)
-      create(:display_ad_event, created_at: Date.current - 2, display_ad: ad1, user_id: nil, updated_at: Date.current)
-      create(:display_ad_event, created_at: Date.current - 2, display_ad: ad1, user_id: nil, updated_at: Date.current)
+  context "when compacting many rows" do
+    before do
+      override_timestamps do
+        create(:display_ad_event, created_at: Date.current - 2, display_ad: ad1, user_id: nil, updated_at: Date.current)
+        create(:display_ad_event, created_at: Date.current - 2, display_ad: ad1, user_id: nil, updated_at: Date.current)
+        create(:display_ad_event, created_at: Date.current - 2, display_ad: ad1, user_id: nil, updated_at: Date.current)
 
-      create(:display_ad_event, created_at: Date.current - 2, display_ad: ad1, user_id: user1.id,
-                                updated_at: Date.current)
-      create(:display_ad_event, created_at: Date.current - 2, display_ad: ad2, user_id: nil, updated_at: Date.current)
+        create(:display_ad_event, created_at: Date.current - 2, display_ad: ad1, user_id: user1.id,
+                                  updated_at: Date.current)
+        create(:display_ad_event, created_at: Date.current - 2, display_ad: ad2, user_id: nil, updated_at: Date.current)
+      end
     end
 
-    expect(DisplayAdEvent.where(created_at: days_ago_as_range(2)).count).to eq(5)
+    it "compacts one day's display_ad_events" do
+      expect(DisplayAdEvent.where(created_at: days_ago_as_range(2)).count).to eq(5)
 
-    described_class.rollup(Date.current - 2)
+      described_class.rollup(Date.current - 2)
 
-    results = DisplayAdEvent.where(created_at: days_ago_as_range(2))
-    expect(results.count).to eq(3)
-    expect(results.pluck(:display_ad_id)).to contain_exactly(ad1.id, ad1.id, ad2.id)
-    expect(results.pluck(:user_id)).to contain_exactly(nil, nil, user1.id)
-    expect(DisplayAdEvent.where("counts_for > 1").count).to eq(1)
+      expectations = [
+        [ad1.id, nil, 3],
+        [ad1.id, user1.id, 1],
+        [ad2.id, nil, 1],
+      ]
+      results_mapped = DisplayAdEvent.where(created_at: days_ago_as_range(2)).map do |event|
+        [event.display_ad_id, event.user_id, event.counts_for]
+      end
+      expect(results_mapped).to contain_exactly(*expectations)
+    end
+  end
+
+  # separate category
+  it "groups by category" do
+    create(:display_ad_event, category: "impression", display_ad: ad1, user_id: nil)
+    create(:display_ad_event, category: "impression", display_ad: ad1, user_id: nil)
+    create(:display_ad_event, category: "impression", display_ad: ad1, user_id: nil)
+    create(:display_ad_event, category: "click", display_ad: ad1, user_id: nil)
+    create(:display_ad_event, category: "click", display_ad: ad1, user_id: nil)
+
+    described_class.rollup(Date.current)
+    results = DisplayAdEvent.where(created_at: Date.current.all_day)
+    by_category = results.index_by { |r| r["category"] }
+    expect(by_category["impression"]["counts_for"]).to eq(3)
+    expect(by_category["click"]["counts_for"]).to eq(2)
+  end
+
+  # separate display_ad_id
+  it "groups by display_ad_id" do
+    create(:display_ad_event, display_ad: ad1, user_id: nil)
+    create(:display_ad_event, display_ad: ad1, user_id: nil)
+    create(:display_ad_event, display_ad: ad1, user_id: nil)
+    create(:display_ad_event, display_ad: ad2, user_id: nil)
+    create(:display_ad_event, display_ad: ad2, user_id: nil)
+
+    described_class.rollup(Date.current)
+    results = DisplayAdEvent.where(created_at: Date.current.all_day)
+    by_ad = results.index_by { |r| r["display_ad_id"] }
+    expect(by_ad[ad1.id]["counts_for"]).to eq(3)
+    expect(by_ad[ad2.id]["counts_for"]).to eq(2)
+  end
+
+  # separate user_id / null
+  it "groups by user_id (including null / logged-out user)" do
+    create(:display_ad_event, display_ad: ad1, user: user1)
+    create(:display_ad_event, display_ad: ad1, user: user2)
+    create(:display_ad_event, display_ad: ad1, user: user2)
+    create(:display_ad_event, display_ad: ad1, user: nil)
+    create(:display_ad_event, display_ad: ad1, user: nil)
+    create(:display_ad_event, display_ad: ad1, user: nil)
+    create(:display_ad_event, display_ad: ad1, user: nil)
+    create(:display_ad_event, display_ad: ad1, user: nil)
+
+    described_class.rollup(Date.current)
+    results = DisplayAdEvent.where(created_at: Date.current.all_day)
+    by_user = results.index_by { |r| r["user_id"] }
+    expect(by_user[user1.id]["counts_for"]).to eq(1)
+    expect(by_user[user2.id]["counts_for"]).to eq(2)
+    expect(by_user[nil]["counts_for"]).to eq(5)
+  end
+
+  # sums counts_for > 1
+  it "counts previously crunched" do
+    create(:display_ad_event, display_ad: ad1, counts_for: 10, user_id: nil)
+    create(:display_ad_event, display_ad: ad1, counts_for: 15, user_id: nil)
+    create(:display_ad_event, display_ad: ad1, user_id: nil)
+    create(:display_ad_event, display_ad: ad1, user_id: nil)
+    create(:display_ad_event, display_ad: ad1, user_id: nil)
+
+    described_class.rollup(Date.current)
+    results = DisplayAdEvent.where(created_at: Date.current.all_day)
+    expect(results.count).to eq(1)
+    expect(results.first.counts_for).to eq(28)
   end
 end
