7f6b56536f2781e36165e81e3a63e9869109a262
Fernando Valverde <fernando@fdo.cr>
2022-09-29 14:31:31 -0600

    Extract signed in/out logic into helper

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -64,6 +64,16 @@ module CommentsHelper
     end
   end
 
+  def contextual_comment_url(comment, article: nil)
+    # Liquid tag parsing doesn't have Devise/Warden (request middleware) so we
+    # can't use `user_signed_in?`, this is why guard against it.
+    return URL.comment(comment) if request.env["warden"].present? && !user_signed_in?
+
+    # If `user_signed_in?` is true we can provide the article's path to anchor
+    # the comment URL instead of using the permalink.
+    URL.comment(comment, article_path: article&.path)
+  end
+
   private
 
   def nested_comments(tree:, commentable:, is_view_root: false)
