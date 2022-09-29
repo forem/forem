3b575acb56c8c193d031cc693f92db74a4fb1afa
Fernando Valverde <fernando@fdo.cr>
2022-09-29 15:07:17 -0600

    Remove featured_number from articles table (#18514)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_19_222729) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_28_162321) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_stat_statements"
@@ -104,7 +104,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_19_222729) do
     t.float "experience_level_rating", default: 5.0
     t.float "experience_level_rating_distribution", default: 5.0
     t.boolean "featured", default: false
-    t.integer "featured_number"
     t.string "feed_source_url"
     t.integer "hotness_score", default: 0
     t.datetime "last_comment_at", precision: nil, default: "2017-01-01 05:00:00"
@@ -158,7 +157,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_19_222729) do
     t.index ["collection_id"], name: "index_articles_on_collection_id"
     t.index ["comment_score"], name: "index_articles_on_comment_score"
     t.index ["comments_count"], name: "index_articles_on_comments_count"
-    t.index ["featured_number"], name: "index_articles_on_featured_number"
     t.index ["feed_source_url"], name: "index_articles_on_feed_source_url", unique: true, where: "(published IS TRUE)"
     t.index ["feed_source_url"], name: "index_articles_on_feed_source_url_unscoped"
     t.index ["hotness_score", "comments_count"], name: "index_articles_on_hotness_score_and_comments_count"
