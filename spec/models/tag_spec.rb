363adf9bdb738631d68079cd23097be56f30d1dd
Ridhwana <ridhwana.khan16@gmail.com>
2022-10-03 12:23:14 +0200

    feat: remove tag sponsorship validation

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -14,8 +14,6 @@ RSpec.describe Tag, type: :model do
       subject { tag }
 
       it { is_expected.to belong_to(:badge).optional }
-      it { is_expected.to have_one(:sponsorship).inverse_of(:sponsorable).dependent(:destroy) }
-
       it { is_expected.to validate_length_of(:name).is_at_most(30) }
       it { is_expected.to validate_presence_of(:category) }
 
