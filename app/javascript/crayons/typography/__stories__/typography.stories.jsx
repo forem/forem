294da19c327fcf27d1e575bd9512b94f4338aa36
Zerg <rajattalesra4914@gmail.com>
2022-10-10 17:31:42 +0530

    Updated story book annotations

--- app/javascript/crayons/typography/__stories__/typography.stories.jsx
+++ app/javascript/crayons/typography/__stories__/typography.stories.jsx
@@ -79,7 +79,7 @@ export const DefaultLineHeight = () => (
   </div>
 );
 
-DefaultLineHeight.story = { name: 'default line height' };
+DefaultLineHeight.storyName = 'default line height';
 
 export const TightLineHeight = () => (
   <div className="sample-texts">
@@ -98,6 +98,4 @@ export const TightLineHeight = () => (
   </div>
 );
 
-TightLineHeight.story = {
-  name: 'tight line height',
-};
+TightLineHeight.storyName = 'tight line height';
