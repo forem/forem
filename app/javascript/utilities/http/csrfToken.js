0336300a7d4df9251fae44849391c0e4cdccf8df
Ridhwana <ridhwana.khan16@gmail.com>
2022-10-11 15:03:05 +0200

    refactor: create a getCSRFToken function in the packs files so that it can be used in the admin

---
+++ app/javascript/utilities/http/csrfToken.js
@@ -0,0 +1,31 @@
+const MAX_RETRIES = 30;
+const RETRY_INTERVAL = 250;
+
+export function getCSRFToken() {
+  const promise = new Promise((resolve, reject) => {
+    // eslint-disable-next-line consistent-return
+    const waitingOnCSRF = setInterval(() => {
+      const metaTag = document.querySelector("meta[name='csrf-token']");
+      let i = 0;
+      i += 1;
+
+      if (metaTag) {
+        clearInterval(waitingOnCSRF);
+        const authToken = metaTag.getAttribute('content');
+        return resolve(authToken);
+      }
+
+      if (i === MAX_RETRIES) {
+        clearInterval(waitingOnCSRF);
+        Honeybadger.notify(
+          `Could not locate CSRF metatag ${JSON.stringify(
+            localStorage.current_user,
+          )}`,
+        );
+        return reject(new Error('Could not locate CSRF meta tag on the page.'));
+      }
+    }, RETRY_INTERVAL);
+  });
+
+  return promise;
+}
