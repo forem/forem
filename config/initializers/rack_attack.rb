beeb70655d9c7a667df1677f2219c32e9c3baf59
Fernando Valverde <fernando@fdo.cr>
2022-10-20 06:39:07 -0600

    remove rack attack throttle for admin users

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -2,6 +2,30 @@ Rack::Attack.throttled_response_retry_after_header = true
 
 module Rack
   class Attack
+    ADMIN_API_CACHE_KEY = "rack_attack_admin_api_keys".freeze
+    ADMIN_ROLES = %w[admin super_admin tech_admin].freeze
+
+    # Method that checks API Key from request and returns true if it belongs
+    # to an admin, false otherwise. It caches the result to avoid making a
+    # DB request on each API call
+    def self.admin_api_key?(request)
+      api_key = request.env["HTTP_API_KEY"]
+      return false if api_key.nil?
+
+      admin_keys = Rails.cache.fetch(ADMIN_API_CACHE_KEY, expires_in: 12.hours) do
+        ApiSecret.joins(user: :roles)
+          .where(roles: { name: ADMIN_ROLES })
+          .group("api_secrets.id")
+          .pluck(:secret)
+      end
+
+      admin_keys.include?(api_key)
+    end
+
+    def self.write_request?(request)
+      request.put? || request.post? || request.delete?
+    end
+
     class Request < ::Rack::Request
       def track_and_return_ip
         if ApplicationConfig["FASTLY_API_KEY"].present?
@@ -20,13 +44,15 @@ module Rack
     end
 
     throttle("api_throttle", limit: 3, period: 1) do |request|
-      if request.path.starts_with?("/api/") && request.get?
+      api_endpoint = request.path.starts_with?("/api/")
+      if api_endpoint && request.get? && !admin_api_key?(request)
         request.track_and_return_ip
       end
     end
 
     throttle("api_write_throttle", limit: 1, period: 1) do |request|
-      if request.path.starts_with?("/api/") && (request.put? || request.post? || request.delete?)
+      api_endpoint = request.path.starts_with?("/api/")
+      if api_endpoint && write_request?(request) && !admin_api_key?(request)
         Honeycomb.add_field("user_api_key", request.env["HTTP_API_KEY"])
         ip_address = request.track_and_return_ip
         if request.env["HTTP_API_KEY"].present?
