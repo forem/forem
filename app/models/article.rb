f7a93aabbb1c0af7d6ff4d1a8984b818ba300b80
Anna Buianova <lightallloy@gmail.com>
2022-10-21 14:57:20 +0300

    Remove html variants tracking (#18594)
    * Removed code related to tracking html_variants
    
    * Remove code related to HtmlVariant#find_for_test
    
    * Remove spec for HtmlVariant#success_rate

--- app/models/article.rb
+++ app/models/article.rb
@@ -104,8 +104,6 @@ class Article < ApplicationRecord
   has_many :context_notifications, as: :context, inverse_of: :context, dependent: :delete_all
   has_many :context_notifications_published, -> { where(context_notifications: { action: "Published" }) },
            as: :context, inverse_of: :context, class_name: "ContextNotification"
-  has_many :html_variant_successes, dependent: :nullify
-  has_many :html_variant_trials, dependent: :nullify
   has_many :notification_subscriptions, as: :notifiable, inverse_of: :notifiable, dependent: :delete_all
   has_many :notifications, as: :notifiable, inverse_of: :notifiable, dependent: :delete_all
   has_many :page_views, dependent: :delete_all
