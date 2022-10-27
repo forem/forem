61f2f70b6e503f3cae2a5e09b40c6da004010a1a
Zerg <rajattalesra4914@gmail.com>
2022-10-27 14:02:17 +0530

    Test fixes

--- spec/system/user/view_user_index_spec.rb
+++ spec/system/user/view_user_index_spec.rb
@@ -5,15 +5,13 @@ RSpec.describe "User index", type: :system do
   let!(:article) { create(:article, user: user) }
   let!(:other_article) { create(:article) }
   let!(:comment) { create(:comment, user: user, commentable: other_article) }
+  let!(:comment2) { create(:comment, user: user, commentable: other_article) }
   let(:organization) { create(:organization) }
 
   context "when user is unauthorized" do
     before do
-      Timecop.freeze
       visit "/#{user.username}"
     end
-
-    after { Timecop.return }
 
     context "when 1 article" do
       it "shows_header", :aggregate_failures, js: true do
@@ -50,26 +48,54 @@ RSpec.describe "User index", type: :system do
 
   context "when user has an organization membership" do
     before do
-      Timecop.freeze
       user.organization_memberships.create(organization: organization, type_of_user: "member")
       visit "/#{user.username}"
     end
-
-    after { Timecop.return }
 
     it "shows organizations", js: true do
       expect(page).to have_css(".spec-org-titles", text: "Organizations")
     end
   end
 
-  context "when visiting own profile" do
+  context "when user is logged in" do
     before do
-      Timecop.freeze
       sign_in user
       visit "/#{user.username}"
     end
 
-    after { Timecop.return }
+    context "when user visits a profile" do
+      it "shows_comments", :aggregate_failures, js: true do
+        within("#substories div.profile-comment-card") do
+          expect(page).to have_content("Recent comments")
+          expect(page).to have_link(nil, href: comment.path)
+          expect(page).to have_link(nil, href: comment2.path)
+        end
+
+        within("#substories") do
+          expect(page).to have_selector(".profile-comment-card", count: 1)
+        end
+
+        within("#substories .profile-comment-card .profile-comment-row:first-of-type") do
+          comment_date = comment.readable_publish_date.gsub("  ", " ")
+          expect(page).to have_selector(".comment-date", text: comment_date)
+        end
+      end
+
+      it "shows_comment_timestamp", :aggregate_failures, js: true do
+        within("#substories .profile-comment-card .profile-comment-row:first-of-type") do
+          iso8601_date_time = /^((\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})Z)$/
+          timestamp = page.find(".comment-date time")[:datetime]
+          expect(timestamp).to match(iso8601_date_time)
+        end
+      end
+    end
+  end
+
+  context "when visiting own profile" do
+    before do
+      sign_in user
+      visit "/#{user.username}"
+    end
 
     context "when user is logged in" do
       it "shows_header", :aggregate_failures, js: true do
@@ -90,6 +116,14 @@ RSpec.describe "User index", type: :system do
         within("#substories div.profile-comment-card") do
           expect(page).to have_content("Recent comments")
           expect(page).to have_link(nil, href: comment.path)
+        end
+      end
+
+      it "shows_comment_timestamp", :aggregate_failures, js: true do
+        within("#substories .profile-comment-card .profile-comment-row:first-of-type") do
+          iso8601_date_time = /^((\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})Z)$/
+          timestamp = page.find(".comment-date time")[:datetime]
+          expect(timestamp).to match(iso8601_date_time)
         end
       end
 
