6b0b55f1f2fa48f1d3851363f2ef58a8a7cf6513
Kudakwashe Siziva <kudakwashe@ombulabs.com>
2022-10-27 12:31:31 +0200

    Return empty string if content is nil

--- app/models/email_message.rb
+++ app/models/email_message.rb
@@ -2,6 +2,8 @@ class EmailMessage < Ahoy::Message
   belongs_to :feedback_message, optional: true
 
   def html_content
+    return "" if content.nil?
+
     html_index = content.index("<html")
     return content if html_index.nil?
 
