9c90b82670fdb27e6e4220a57218b617d6963384
Ridhwana <ridhwana.khan16@gmail.com>
2022-11-01 11:34:25 +0200

    feat: account for showing display ads with the targeted tags that match the article

--- app/models/display_ad.rb
+++ app/models/display_ad.rb
@@ -32,8 +32,15 @@ class DisplayAd < ApplicationRecord
                              search: "%#{term}%"
                      }
 
-  def self.for_display(area, user_signed_in)
+  def self.for_display(area, user_signed_in, article_tags = [])
     relation = approved_and_published.where(placement_area: area).order(success_rate: :desc)
+
+    if article_tags.any?
+      display_ads_with_no_tags = relation.where(cached_tag_list: "")
+      display_ads_with_targeted_article_tags = relation.cached_tagged_with_any(article_tags)
+
+      relation = display_ads_with_no_tags.or(display_ads_with_targeted_article_tags)
+    end
 
     relation = if user_signed_in
                  relation.where(display_to: %w[all logged_in])
