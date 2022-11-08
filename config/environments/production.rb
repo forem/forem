8502ae9cc6d3a328d295bc0258d4a8f6e723c2d1
RJ Robinson <rrobinson@codeclimate.com>
2022-11-08 15:23:36 -0500

    updated database.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
   config.assets.digest = true
