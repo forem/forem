99f04b91196d60694f2c205aa7b1189d2684113b
Fernando Valverde <fernando@fdo.cr>
2022-12-02 08:28:16 -0600

    GET /articles/me/all (#18797)

--- spec/requests/api/v1/docs/users_spec.rb
+++ spec/requests/api/v1/docs/users_spec.rb
@@ -4,7 +4,7 @@ require "swagger_helper"
 # rubocop:disable RSpec/EmptyExampleGroup
 # rubocop:disable RSpec/VariableName
 
-RSpec.describe "Api::V1::Docs::Users", type: :request do
+RSpec.describe "Api::V1::Docs::Users" do
   let(:Accept) { "application/vnd.forem.api-v1+json" }
   let(:api_secret) { create(:api_secret) }
   let(:user) { api_secret.user }
@@ -28,7 +28,7 @@ or by the user's username.
 For complete documentumenation, see the v0 API docs: https://developers.forem.com/api/v0#tag/users/operation/getUser"
         operationId "getUser"
         produces "application/json"
-        parameter name: :id, in: :path, required: true
+        parameter name: :id, in: :path, required: true, type: :string
 
         response(200, "successful") do
           let(:"api-key") { api_secret.secret }
