a06ab31416912050cefa9b622e3020f78ce37ebd
Joshua Wehner <joshua@forem.com>
2022-12-05 11:12:24 +0100

    DisplayAd API: Write endpoints (#18736)
    * Try a create endpoint
    
    * Try an update endpoint for display_ads
    
    * Try a delete endpoint for display_ads (unpublish)
    
    * Unpublish
    
    * Permit tags as well
    
    * Update docs for DisplayAd tags via API/swagger

--- spec/requests/api/v1/docs/display_ads_spec.rb
+++ spec/requests/api/v1/docs/display_ads_spec.rb
@@ -37,6 +37,69 @@ RSpec.describe "api/v1/display_ads" do
         end
       end
     end
+
+    describe "create a new display ad" do
+      post("display ads") do
+        tags "display ads"
+        description(<<-DESCRIBE.strip)
+        This endpoint allows the client to create a new display ad.
+        DESCRIBE
+
+        produces "application/json"
+        consumes "application/json"
+        parameter name: :display_ad, in: :body, schema: {
+          type: :object,
+          properties: {
+            name: { type: :string, description: "For internal use, helps distinguish ads from one another" },
+            body_markdown: { type: :string, description: "The text (in markdown) of the ad (required)" },
+            approved: { type: :boolean, description: "Ad must be both published and approved to be in rotation" },
+            published: { type: :boolean, description: "Ad must be both published and approved to be in rotation" },
+            organization_id: { type: :int32, description: "Identifies the organization to which the ad belongs" },
+            display_to: { type: :string, enum: DisplayAd.display_tos.keys, default: "all",
+                          description: "Potentially limits visitors to whom the ad is visible" },
+            placement_area: { type: :string, enum: DisplayAd::ALLOWED_PLACEMENT_AREAS,
+                              description: "Identifies which area of site layout the ad can appear in" },
+            tags: { type: :string, description: "Tags on which this ad can be displayed (blank is all/any tags)" }
+          },
+          required: %w[name body_markdown placement_area]
+        }
+
+        let(:display_ad) do
+          {
+            body_markdown: "# Hi, this is ad\nYep, it's an ad",
+            name: "Example Ad",
+            display_to: "all",
+            approved: true,
+            published: true,
+            placement_area: placement_area
+          }
+        end
+
+        let(:placement_area) { "post_comments" }
+
+        response(200, "successful") do
+          let(:"api-key") { api_secret.secret }
+          add_examples
+
+          run_test!
+        end
+
+        response "401", "unauthorized" do
+          let(:"api-key") { "invalid" }
+          add_examples
+
+          run_test!
+        end
+
+        response "422", "unprocessable" do
+          let(:"api-key") { api_secret.secret }
+          let(:placement_area) { "moon" }
+          add_examples
+
+          run_test!
+        end
+      end
+    end
   end
 
   path "/api/display_ads/{id}" do
@@ -78,6 +141,120 @@ RSpec.describe "api/v1/display_ads" do
         response "404", "Unknown DisplayAd ID" do
           let(:"api-key") { api_secret.secret }
           let(:id) { 10_000 }
+          add_examples
+
+          run_test!
+        end
+      end
+    end
+
+    describe "PUT update an ad" do
+      put("display ads") do
+        tags "display ads"
+        description(<<-DESCRIBE.strip)
+        This endpoint allows the client to update the attributes of a single display ad, via its id.
+        DESCRIBE
+
+        produces "application/json"
+        consumes "application/json"
+
+        parameter name: :id,
+                  in: :path,
+                  required: true,
+                  description: "The ID of the user to unpublish.",
+                  schema: {
+                    type: :integer,
+                    format: :int32,
+                    minimum: 1
+                  },
+                  example: 123
+
+        parameter name: :display_ad, in: :body, schema: {
+          type: :object,
+          properties: {
+            name: { type: :string, description: "For internal use, helps distinguish ads from one another" },
+            body_markdown: { type: :string, description: "The text (in markdown) of the ad (required)" },
+            approved: { type: :boolean, description: "Ad must be both published and approved to be in rotation" },
+            published: { type: :boolean, description: "Ad must be both published and approved to be in rotation" },
+            organization_id: { type: :int32, description: "Identifies the organization to which the ad belongs" },
+            display_to: { type: :string, enum: DisplayAd.display_tos.keys, default: "all",
+                          description: "Potentially limits visitors to whom the ad is visible" },
+            placement_area: { type: :string, enum: DisplayAd::ALLOWED_PLACEMENT_AREAS,
+                              description: "Identifies which area of site layout the ad can appear in" },
+            tags: { type: :string, description: "Tags on which this ad can be displayed (blank is all/any tags)" }
+          },
+          required: %w[name body_markdown placement_area]
+        }
+
+        let(:placement_area) { "post_comments" }
+
+        response(200, "successful") do
+          let(:"api-key") { api_secret.secret }
+          let(:id) { display_ad.id }
+          add_examples
+
+          run_test!
+        end
+
+        response(404, "not found") do
+          let(:"api-key") { api_secret.secret }
+          let(:id) { 10_000 }
+          add_examples
+
+          run_test!
+        end
+
+        response "401", "unauthorized" do
+          let(:"api-key") { "invalid" }
+          let(:id) { display_ad.id }
+          add_examples
+
+          run_test!
+        end
+      end
+    end
+  end
+
+  path "/api/display_ads/{id}/unpublish" do
+    describe "PUT to unpublish an ad" do
+      put("unpublish") do
+        tags "display ads"
+        description(<<-DESCRIBE.strip)
+        This endpoint allows the client to remove a display ad from rotation by un-publishing it.
+        DESCRIBE
+
+        produces "application/json"
+
+        parameter name: :id,
+                  in: :path,
+                  required: true,
+                  description: "The ID of the user to unpublish.",
+                  schema: {
+                    type: :integer,
+                    format: :int32,
+                    minimum: 1
+                  },
+                  example: 123
+
+        response(204, "no content") do
+          let(:"api-key") { api_secret.secret }
+          let(:id) { display_ad.id }
+          add_examples
+
+          run_test!
+        end
+
+        response(404, "not found") do
+          let(:"api-key") { api_secret.secret }
+          let(:id) { 10_000 }
+          add_examples
+
+          run_test!
+        end
+
+        response "401", "unauthorized" do
+          let(:"api-key") { "invalid" }
+          let(:id) { display_ad.id }
           add_examples
 
           run_test!
