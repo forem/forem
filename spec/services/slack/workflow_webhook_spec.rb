24cb23f236c52cca7dc1601b224aada3f6c70faa
Mac Siri <krairit.siri@gmail.com>
2022-12-06 12:34:42 -0500

    Update spec

--- spec/services/slack/workflow_webhook_spec.rb
+++ spec/services/slack/workflow_webhook_spec.rb
@@ -1,45 +1,23 @@
 require "rails_helper"
 
-RSpec.describe Slack::Messengers::ArticleFetchedFeed, type: :service do
-  let!(:article) do
-    build(:article).tap do |article|
-      article.title = "Awesome article"
-      article.published_from_feed = true
-    end
+RSpec.describe Slack::WorkflowWebhook, type: :service do
+  it "does nothing when missing SLACK_WORKFLOW_WEBHOOK_URL" do
+    allow(ENV).to receive(:[]).with("SLACK_WORKFLOW_WEBHOOK_URL").and_return(nil)
+    allow(HTTParty).to receive(:post).and_call_original
+
+    described_class.call("test")
+    expect(HTTParty).not_to have_received(:post)
   end
 
-  let(:default_params) { { article: article } }
+  it "send a post request to the webhook url" do
+    allow(ENV).to receive(:[]).with("SLACK_WORKFLOW_WEBHOOK_URL").and_return("https://example.com")
+    allow(HTTParty).to receive(:post).and_return(true)
 
-  it "does not message slack for a new article not coming from the feed" do
-    sidekiq_assert_no_enqueued_jobs(only: Slack::Messengers::Worker) do
-      article.published_from_feed = false
-
-      described_class.call(article: article)
-    end
-  end
-
-  it "contains the correct info", :aggregate_failures do
-    sidekiq_assert_enqueued_jobs(1, only: Slack::Messengers::Worker) do
-      described_class.call(**default_params)
-    end
-
-    job = sidekiq_enqueued_jobs(worker: Slack::Messengers::Worker).last
-    message = job["args"].first["message"]
-
-    expect(message).to include(article.title)
-    expect(message).to include(URL.article(article))
-  end
-
-  it "messages the proper channel with the proper username and emoji", :aggregate_failures do
-    sidekiq_assert_enqueued_jobs(1, only: Slack::Messengers::Worker) do
-      described_class.call(**default_params)
-    end
-
-    job = sidekiq_enqueued_jobs(worker: Slack::Messengers::Worker).last
-    job_args = job["args"].first
-
-    expect(job_args["channel"]).to eq("activity")
-    expect(job_args["username"]).to eq("article_bot")
-    expect(job_args["icon_emoji"]).to eq(":robot_face:")
+    described_class.call("test")
+    expect(HTTParty).to have_received(:post).with(
+      "https://example.com",
+      body: { message: "test" }.to_json,
+      headers: { "Content-Type" => "application/json" },
+    )
   end
 end
