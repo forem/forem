b5fbab0e31691eab802c0c7b6a2a0bbb23075274
Mac Siri <mac@forem.com>
2022-12-09 10:46:20 -0500

    Update ArticleFetchedFeed to use slack workflow (#18801)

--- app/services/feeds/import.rb
+++ app/services/feeds/import.rb
@@ -48,8 +48,6 @@ module Feeds
 
         total_articles_count += articles.length
 
-        articles.each { |article| Slack::Messengers::ArticleFetchedFeed.call(article: article) }
-
         # we use `feed_fetched_at` to mark the last time a particular user's feed has been fetched, parsed and imported
         batch_of_users.update_all(feed_fetched_at: Time.current)
       end
@@ -169,6 +167,7 @@ module Feeds
         next
       end
 
+      Slack::WorkflowWebhookWorker.perform_async("Imported #{articles.length} articles for #{user.username}")
       articles
     end
 
