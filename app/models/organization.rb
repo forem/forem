c9726caf3910d71fe13d89c8f05a1dd52cb8ede3
Fernando Valverde <fernando@fdo.cr>
2023-01-13 08:45:18 -0600

    Split to individual articles on workergits

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -138,7 +138,8 @@ class Organization < ApplicationRecord
   def conditionally_update_articles
     return unless Article::ATTRIBUTES_CACHED_FOR_RELATED_ENTITY.detect { |attr| saved_change_to_attribute?(attr) }
 
-    Organizations::SaveAllArticlesWorker.perform_async(id)
+    article_ids = articles.ids.map { |id| [id] }
+    Organizations::SaveArticleWorker.perform_bulk(article_ids)
   end
 
   def bust_cache
--- spec/workers/organizations/save_article_worker_spec.rb
+++ spec/workers/organizations/save_article_worker_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe Organizations::SaveAllArticlesWorker do
+RSpec.describe Organizations::SaveArticleWorker do
   describe "perform" do
     let(:worker) { subject }
     let(:organization) { create(:organization) }
