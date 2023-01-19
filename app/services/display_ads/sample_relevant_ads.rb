a7120db197058892b46bab50713772e2f01856f5
Ridhwana <ridhwana.khan16@gmail.com>
2023-01-19 10:39:54 +0200

    refactor: pass the relevant paramenters to the newly created service, move the code over to the service, pass through display ads, use the display ads parameter for queries, use instance vars

--- app/services/display_ads/sample_relevant_ads.rb
+++ app/services/display_ads/sample_relevant_ads.rb
@@ -1,9 +1,43 @@
 module DisplayAds
   class SampleRelevantAds
-    def self.call
-      new.call
+    def self.call(display_ads, area, user_signed_in, article_tags)
+      new(display_ads, area, user_signed_in, article_tags).call
     end
 
-    def call; end
+    def initialize(display_ads, area, user_signed_in, article_tags)
+      @display_ads = display_ads
+      @area = area
+      @user_signed_in = user_signed_in
+      @article_tags = article_tags
+    end
+
+    def call
+      relation = @display_ads.approved_and_published.where(placement_area: @area).order(success_rate: :desc)
+
+      if @article_tags.any?
+        display_ads_with_no_tags = relation.where(cached_tag_list: "")
+        display_ads_with_targeted_article_tags = relation.cached_tagged_with_any(@article_tags)
+
+        relation = display_ads_with_no_tags.or(display_ads_with_targeted_article_tags)
+      end
+
+      if @article_tags.blank?
+        relation = relation.where(cached_tag_list: "")
+      end
+
+      relation = if @user_signed_in
+                   relation.where(display_to: %w[all logged_in])
+                 else
+                   relation.where(display_to: %w[all logged_out])
+                 end
+
+      relation.order(success_rate: :desc)
+
+      if rand(8) == 1
+        relation.sample
+      else
+        relation.limit(rand(1..15)).sample
+      end
+    end
   end
 end
