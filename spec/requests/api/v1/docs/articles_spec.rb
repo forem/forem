6555100ded9a8275a8fcff2b4183553f3d9f93ab
Fernando Valverde <fernando@fdo.cr>
2023-01-19 07:37:53 -0600

    Add missing specs

--- spec/requests/api/v1/docs/articles_spec.rb
+++ spec/requests/api/v1/docs/articles_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe "Api::V1::Docs::Articles" do
   let(:user) { api_secret.user }
   let(:user_article) { create(:article, featured: true, tags: "discuss", published: true, user_id: user.id) }
   let(:Accept) { "application/vnd.forem.api-v1+json" }
+  # let!(:sample_articles) { create_list(:article, 3) }
 
   before { stub_const("FlareTag::FLARE_TAG_IDS_HASH", { "discuss" => tag.id }) }
 
@@ -23,6 +24,70 @@ RSpec.describe "Api::V1::Docs::Articles" do
     end
 
     path "/api/articles" do
+      post "Publish article" do
+        tags "articles"
+        description "This endpoint allows the client to create a new article.
+
+\"Articles\" are all the posts that users create on DEV that typically show up in the feed. They can be a blog post, a discussion question, a help thread etc. but is referred to as article within the code."
+        operationId "createArticle"
+        produces "application/json"
+        consumes "application/json"
+        parameter name: :article, in: :body, schema: {
+          type: :object,
+          properties: {
+            title: { type: :string },
+            body_markdown: { type: :string },
+            published: { type: :boolean },
+            series: { type: :string },
+            main_image: { type: :string },
+            canonical_url: { type: :string },
+            description: { type: :string },
+            tags: { type: :string },
+            organization_id: { type: :integer }
+          }
+        }
+
+        response "201", "An Article" do
+          let(:"api-key") { api_secret.secret }
+          let(:article) do
+            {
+              article: {
+                title: "New article",
+                body_markdown: "**New** body for the article",
+                published: true,
+                series: "custom series",
+                main_image: "https://res.cloudinary.com/practicaldev/image/fetch/s--Jbk_rL1D--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://thepracticaldev.s3.amazonaws.com/i/5wfo25724gzgk5e5j50g.jpg",
+                canonical_url: "https://dev.to/fdocr/headless-chrome-dual-mode-tests-for-ruby-on-rails-4p6g",
+                description: "New post example",
+                tags: "ruby selenium capybara rspec",
+                organization_id: organization.id
+              }
+            }
+          end
+          add_examples
+
+          run_test!
+        end
+
+        response "401", "Unauthorized" do
+          let(:"api-key") { nil }
+          let(:id) { published_article.id }
+          let(:article) { { article: {} } }
+          add_examples
+
+          run_test!
+        end
+
+        response "422", "Unprocessable Entity" do
+          let(:"api-key") { api_secret.secret }
+          let(:id) { user_article.id }
+          let(:article) { { article: {} } }
+          add_examples
+
+          run_test!
+        end
+      end
+
       get "Published articles" do
         security []
         tags "articles"
@@ -94,6 +159,33 @@ belonging to the requested collection, ordered by ascending publication date.",
 
         response "200", "A List of Articles" do
           let(:"api-key") { nil }
+          schema  type: :array,
+                  items: { "$ref": "#/components/schemas/ArticleIndex" }
+          add_examples
+
+          run_test!
+        end
+      end
+    end
+  end
+
+  describe "/api/articles/latest" do
+    before { create_list(:article, 3) }
+
+    path "/api/articles/latest" do
+      get "Published articles sorted by published date" do
+        security []
+        tags "articles"
+        description "This endpoint allows the client to retrieve a list of articles. ordered by descending publish date.
+
+It supports pagination, each page will contain 30 articles by default."
+        operationId "getLatestArticles"
+        produces "application/json"
+
+        parameter "$ref": "#/components/parameters/pageParam"
+        parameter "$ref": "#/components/parameters/perPageParam30to1000"
+
+        response "200", "A List of Articles" do
           schema  type: :array,
                   items: { "$ref": "#/components/schemas/ArticleIndex" }
           add_examples
