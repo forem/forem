5be99a04daf794e893c4f5ad26cbab589934cd85
Kevin Gilpin <kgilpin@gmail.com>
2023-01-24 11:48:10 -0500

    test: Enable rspec --only-failures option

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -266,4 +266,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.example_status_persistence_file_path = "tmp/rspec_example_status.txt"
 end
