20f4d21a956f9e6b0d86ecbd008c47a3b711507f
Mac Siri <krairit.siri@gmail.com>
2023-01-31 15:24:20 -0500

    Update Users::ProfileImageGenerator

--- spec/services/users/profile_image_generator_spec.rb
+++ spec/services/users/profile_image_generator_spec.rb
@@ -2,6 +2,6 @@ require "rails_helper"
 
 RSpec.describe Users::ProfileImageGenerator, type: :service do
   it "returns an emoji url" do
-    expect(described_class.call).to start_with("https://emojipedia-us.s3")
+    expect(described_class.call).to include("asset")
   end
 end
