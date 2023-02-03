8777303327a7f3c57ab3bbacfbe30b84c3b47d62
Fernando Valverde <fernando@fdo.cr>
2023-02-03 05:27:38 -0600

    Revert Regexp interpolation

--- app/javascript/utilities/videoPlayback.js
+++ app/javascript/utilities/videoPlayback.js
@@ -28,7 +28,7 @@ export function initializeVideoPlayback() {
   function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, '\\$&');
-    const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`);
+    const regex = new RegExp(`[?&]${  name  }(=([^&#]*)|&|#|$)`);
     const results = regex.exec(url);
     if (!results) return null;
     if (!results[2]) return '';
