078c285663f4cdb0425324e302971e7baa47c172
rt4914 <rajattalesra4914@gmail.com>
2023-02-07 18:40:08 +0530

    Multiple Reactions UI fixes

--- app/assets/javascripts/initializers/initializeArticleReactions.js
+++ app/assets/javascripts/initializers/initializeArticleReactions.js
@@ -46,28 +46,6 @@ function showUserReaction(reactionName, animatedClass) {
   if (reactionDrawerButton && reactionName !== 'readinglist') {
     reactionDrawerButton.classList.add('user-activated', 'user-animated');
   }
-
-  if (animatedClass == 'user-animated') {
-    const activeIcon = reactionButton.querySelector(
-      '.crayons-reaction__icon--active svg',
-    );
-
-    if (activeIcon) {
-      const activeDrawerIcon = reactionDrawerButton.querySelector(
-        '.crayons-reaction__icon--active img',
-      );
-
-      reactionDrawerButton.originalIcon = activeDrawerIcon.outerHTML;
-      activeDrawerIcon.outerHTML = activeIcon.outerHTML;
-
-      setTimeout(function () {
-        document
-          .getElementById('reaction-drawer-trigger')
-          .querySelector('.crayons-reaction__icon--active svg').outerHTML =
-          reactionDrawerButton.originalIcon;
-      }, 1500);
-    }
-  }
 }
 
 function hideUserReaction(reactionName) {
