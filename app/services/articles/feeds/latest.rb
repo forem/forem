823e879d93c549b3df7692658c38c775651d7484
Joshua Wehner <joshua@forem.com>
2023-02-13 13:39:57 +0100

    Restore load-bearing constant

--- app/services/articles/feeds/latest.rb
+++ app/services/articles/feeds/latest.rb
@@ -1,6 +1,9 @@
 module Articles
   module Feeds
     module Latest
+      # NOTE: This constant is no longer used in this context, but may be used elsewhere
+      MINIMUM_SCORE = -20
+
       def self.call(tag: nil, number_of_articles: Article::DEFAULT_FEED_PAGINATION_WINDOW_SIZE, page: 1)
         Articles::Feeds::Tag.call(tag)
           .order(published_at: :desc)
