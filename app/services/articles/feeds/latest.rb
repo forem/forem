957656d48509803ed09bc03a9c612dcdb6cdd789
Joshua Wehner <joshua@forem.com>
2023-02-13 17:18:44 +0100

    Signed-in users get poor quality content

--- app/services/articles/feeds/latest.rb
+++ app/services/articles/feeds/latest.rb
@@ -1,13 +1,15 @@
 module Articles
   module Feeds
     module Latest
-      # NOTE: This constant is no longer used in this context, but may be used elsewhere
       MINIMUM_SCORE = -20
 
-      def self.call(tag: nil, number_of_articles: Article::DEFAULT_FEED_PAGINATION_WINDOW_SIZE, page: 1)
+      def self.call(tag: nil, number_of_articles: nil, page: 1, minimum_score: nil)
+        number_of_articles ||= Article::DEFAULT_FEED_PAGINATION_WINDOW_SIZE
+        minimum_score ||= MINIMUM_SCORE
+
         Articles::Feeds::Tag.call(tag)
           .order(published_at: :desc)
-          .with_at_least_home_feed_minimum_score
+          .where("score > ?", minimum_score)
           .page(page)
           .per(number_of_articles)
       end
