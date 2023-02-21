8437e6e51ec56b52d9b0aa9d845e482b013cc50e
James Wu <ichangjwu@outlook.com>
2023-02-21 13:53:11 +0900

    Add method to reduce color brightness

--- app/services/color/accessibility.rb
+++ app/services/color/accessibility.rb
@@ -1,3 +1,5 @@
+require "color"
+
 module Color
   class Accessibility
     def initialize(hex)
@@ -6,6 +8,16 @@ module Color
 
     def low_contrast?(compared_color = "ffffff", min_contrast = 4.5)
       WCAGColorContrast.ratio(@hex, compared_color.delete("#")) < min_contrast
+    end
+
+    def reduce_brightness(compared_color = "ffffff", min_contrast = 4.5)
+      n = 0
+      hex_color = @hex
+      while WCAGColorContrast.ratio(hex_color, compared_color) < min_contrast
+        n -= 1
+        hex_color = Color::RGB.from_html(hex_color).adjust_brightness(n).hex
+      end
+      hex_color
     end
   end
 end
--- spec/services/color/accessibility_spec.rb
+++ spec/services/color/accessibility_spec.rb
@@ -24,4 +24,23 @@ RSpec.describe Color::Accessibility, type: :service do
   it "determines sufficient contrast with compared color and rate" do
     expect(described_class.new("#225CC9").low_contrast?("#ffffff", 1.0)).to be false
   end
+
+  describe "#reduce_brightness" do
+    let(:color) { described_class.new("#fcf5f5") }
+
+    it "returns a hex color than meets the minimum contrast requirment" do
+      modified_color = color.reduce_brightness("ffffff", 4.5)
+      ratio = WCAGColorContrast.ratio(modified_color, "ffffff")
+      expect(ratio).to be > 4.5
+    end
+
+    context "when the original color meets the minimum contrast requirement" do
+      let(:dark_color) { described_class.new("#041d4a") }
+
+      it "does not modify the original hex color" do
+        color = dark_color.reduce_brightness("ffffff", 4.5)
+        expect(color).to eq "041d4a"
+      end
+    end
+  end
 end
