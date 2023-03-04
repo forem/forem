16c22cea0d5712e9850bb9cc5ce93c1b0dff39e4
TERNOX <43003712+TERNOX@users.noreply.github.com>
2023-03-04 18:23:12 +0200

    Update date_helper_spec.rb

--- spec/helpers/date_helper_spec.rb
+++ spec/helpers/date_helper_spec.rb
@@ -5,14 +5,14 @@ describe DateHelper do
     it "renders date" do
       time = Time.now.utc
       date = time.strftime("%b %-e, %Y")
-      tag = helper.local_date(time)
+      tag = (time)
       expect(tag).to eq "<time datetime=\"#{time.iso8601}\" class=\"date\">#{date}</time>"
     end
 
     it "renders date without year" do
       time = Time.now.utc
       date = time.strftime("%b %-e")
-      tag = helper.local_date(time, show_year: false)
+      tag = (time, show_year: false)
       expect(tag).to eq "<time datetime=\"#{time.iso8601}\" class=\"date-no-year\">#{date}</time>"
     end
   end
