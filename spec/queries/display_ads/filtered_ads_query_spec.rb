16c509135c751c3058e3ea25ea35dea9a42b5769
Lawrence <lawrence@forem.com>
2023-03-06 16:15:14 -0600

    Display Rules for In house and Community Billboards (#19189)
    * init
    
    * Update app/queries/display_ads/filtered_ads_query.rb
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update app/queries/display_ads/filtered_ads_query.rb
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * add display ad query and update caller
    
    * set as keyword args and add org id to callers
    
    * Update app/queries/display_ads/filtered_ads_query.rb
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update app/queries/display_ads/filtered_ads_query.rb
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update app/queries/display_ads/filtered_ads_query.rb
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update app/views/articles/_sticky_nav.html.erb
    
    Co-authored-by: Joshua Wehner <joshua@forem.com>
    
    * Update app/views/articles/show.html.erb
    
    Co-authored-by: Joshua Wehner <joshua@forem.com>
    
    * Update app/queries/display_ads/filtered_ads_query.rb
    
    Spacing
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update app/queries/display_ads/filtered_ads_query.rb
    
    More spacing
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update app/queries/display_ads/filtered_ads_query.rb
    
    Co-authored-by: Joshua Wehner <joshua@forem.com>
    
    * Update app/queries/display_ads/filtered_ads_query.rb
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * fix some broken specs
    
    * add updated spec for billboard rules
    
    * missing comma
    
    * Update spec/queries/display_ads/filtered_ads_query_spec.rb
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * add placement in spec
    
    * fix external spec
    
    * cleanup
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Joshua Wehner <joshua@forem.com>

--- spec/queries/display_ads/filtered_ads_query_spec.rb
+++ spec/queries/display_ads/filtered_ads_query_spec.rb
@@ -9,19 +9,19 @@ RSpec.describe DisplayAds::FilteredAdsQuery, type: :query do
     it "does not return unpublished ads" do
       display_ad.update!(published: false, approved: true)
       expect(described_class.call(display_ads: DisplayAd.all, area: display_ad.placement_area,
-                                  user_signed_in: false)).to be_nil
+                                  organization_id: nil, user_signed_in: false)).to be_nil
     end
 
     it "does not return unapproved ads" do
       display_ad.update!(published: true, approved: false)
       expect(described_class.call(display_ads: DisplayAd.all, area: display_ad.placement_area,
-                                  user_signed_in: false)).to be_nil
+                                  organization_id: nil, user_signed_in: false)).to be_nil
     end
 
     it "returns published and approved ads" do
       display_ad.update!(published: true, approved: true)
       expect(described_class.call(display_ads: DisplayAd.all, area: display_ad.placement_area,
-                                  user_signed_in: false)).to eq(display_ad)
+                                  organization_id: nil, user_signed_in: false)).to eq(display_ad)
     end
   end
 
@@ -41,7 +41,7 @@ RSpec.describe DisplayAds::FilteredAdsQuery, type: :query do
 
       article_tags = %w[linux productivity]
       expect(described_class.call(display_ads: DisplayAd.all, area: "post_comments", user_signed_in: false,
-                                  article_tags: article_tags)).to eq(display_ad)
+                                  organization_id: nil, article_tags: article_tags)).to eq(display_ad)
     end
 
     it "will show display ads that have no tags set" do
@@ -59,7 +59,7 @@ RSpec.describe DisplayAds::FilteredAdsQuery, type: :query do
 
       article_tags = %w[productivity java]
       expect(described_class.call(display_ads: DisplayAd.all, area: "post_comments", user_signed_in: false,
-                                  article_tags: article_tags)).to eq(display_ad)
+                                organization_id: display_ad.organization.id, article_tags: article_tags)).to eq(display_ad)
     end
 
     it "will show no display ads if the available display ads have no tags set or do not contain matching tags" do
@@ -70,7 +70,7 @@ RSpec.describe DisplayAds::FilteredAdsQuery, type: :query do
                           cached_tag_list: "productivity")
       article_tags = %w[javascript]
       expect(described_class.call(display_ads: DisplayAd.all, area: "post_comments", user_signed_in: false,
-                                  article_tags: article_tags)).to be_nil
+                                  organization_id: nil, article_tags: article_tags)).to be_nil
     end
 
     it "will show display ads with no tags set if there are no article tags" do
@@ -87,7 +87,7 @@ RSpec.describe DisplayAds::FilteredAdsQuery, type: :query do
                                                     cached_tag_list: "")
 
       expect(described_class.call(display_ads: DisplayAd.all, area: "post_comments",
-                                  user_signed_in: false)).to eq(display_ad_without_tags)
+                                organization_id: nil, user_signed_in: false)).to eq(display_ad_without_tags)
     end
   end
 
@@ -101,12 +101,12 @@ RSpec.describe DisplayAds::FilteredAdsQuery, type: :query do
 
     it "shows ads that have a display_to of 'logged_in' if a user is signed in" do
       expect(described_class.call(display_ads: DisplayAd.all, area: display_ad2.placement_area,
-                                  user_signed_in: true)).to eq(display_ad2)
+                                organization_id: display_ad2.organization.id, user_signed_in: true)).to eq(display_ad2)
     end
 
     it "shows ads that have a display_to of 'logged_out' if a user is signed in" do
       expect(described_class.call(display_ads: DisplayAd.all, area: display_ad3.placement_area,
-                                  user_signed_in: false)).to eq(display_ad3)
+                                organization_id: display_ad3.organization.id, user_signed_in: false)).to eq(display_ad3)
     end
   end
 
@@ -117,12 +117,38 @@ RSpec.describe DisplayAds::FilteredAdsQuery, type: :query do
 
     it "shows ads that have a display_to of 'all' if a user is signed in" do
       expect(described_class.call(display_ads: DisplayAd.all, area: display_ad.placement_area,
-                                  user_signed_in: true)).to eq(display_ad)
+                                organization_id: display_ad.organization.id, user_signed_in: true)).to eq(display_ad)
     end
 
     it "shows ads that have a display_to of 'all' if a user is not signed in" do
       expect(described_class.call(display_ads: DisplayAd.all, area: display_ad.placement_area,
-                                  user_signed_in: false)).to eq(display_ad)
+                                organization_id: display_ad.organization.id, user_signed_in: false)).to eq(display_ad)
+    end
+  end
+
+  context "when organization is set on ad" do
+    it "shows ads that have that are of type community and associated with the organization" do
+      display_ad = create(:display_ad, organization_id: organization.id,
+                                       published: true,
+                                       approved: true,
+                                       placement_area: "post_comments",
+                                       type_of: "community",
+                                       display_to: "all")
+
+      expect(described_class.call(display_ads: DisplayAd.all, area: display_ad.placement_area,
+                                organization_id: display_ad.organization.id, user_signed_in: false)).to eq(display_ad)
+    end
+
+    it "shows ads that have that are of type in house" do
+      display_ad = create(:display_ad, organization_id: organization.id,
+                                       published: true,
+                                       approved: true,
+                                       placement_area: "post_comments",
+                                       type_of: "in_house",
+                                       display_to: "all")
+
+      expect(described_class.call(display_ads: DisplayAd.all, area: display_ad.placement_area,
+                                  organization_id: display_ad.organization.id, user_signed_in: false)).to eq(display_ad)
     end
   end
 end
