bd6e1cc5d2413091b1ac8ca9612402566551002b
Joshua Wehner <joshua@forem.com>
2023-03-20 14:33:57 +0100

    Ooops, missed saving this

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
   USER_COMMENTS_PARTIAL = "users/comments_section".freeze
   COMMENTS_LOCKED_PARTIAL = "users/comments_locked_cta".freeze
 
-  def user_comments_section(_comments)
+  def user_comments_section
     if user_signed_in?
       USER_COMMENTS_PARTIAL
     else
