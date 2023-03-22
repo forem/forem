640590e35ff59d61ee32ad88b06837dd7ad2e088
Joshua Wehner <joshua@forem.com>
2023-03-22 15:20:00 +0100

    (Really) Enable multiple reactions on #index (#19202)

--- app/assets/javascripts/utilities/buildArticleHTML.js
+++ app/assets/javascripts/utilities/buildArticleHTML.js
@@ -110,16 +110,35 @@ function buildArticleHTML(article, currentUserId = null) {
     var reactionsCount = article.public_reactions_count;
     var reactionsDisplay = '';
     var reactionsText = reactionsCount === 1 ? 'reaction' : 'reactions';
+    var reactionIcons = document.getElementById('reaction-category-resources');
 
     if (article.class_name !== 'User' && reactionsCount > 0) {
-      reactionsDisplay =
-        '<a href="' +
-        article.path +
-        '"' +
-        commentsAriaLabelText +
-        'class="crayons-btn crayons-btn--s crayons-btn--ghost crayons-btn--icon-left"><svg class="crayons-icon" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M18.884 12.595l.01.011L12 19.5l-6.894-6.894.01-.01A4.875 4.875 0 0112 5.73a4.875 4.875 0 016.884 6.865zM6.431 7.037a3.375 3.375 0 000 4.773L12 17.38l5.569-5.569a3.375 3.375 0 10-4.773-4.773L9.613 10.22l-1.06-1.062 2.371-2.372a3.375 3.375 0 00-4.492.25v.001z"/></svg>' +
-        reactionsCount +
-        `<span class="hidden s:inline">&nbsp;${reactionsText}</span></a>`;
+      var icons = [];
+      for (var category of article.public_reaction_categories) {
+        var icon = reactionIcons.querySelector(
+          `img[data-slug=${category.slug}]`,
+        ).outerHTML;
+        icons = icons.concat(
+          `<span class="crayons_icon_container">${icon}</span>`,
+        );
+      }
+      icons.reverse();
+
+      reactionsDisplay = `<a href="${
+        article.path
+      }" class="crayons-btn crayons-btn--s crayons-btn--ghost crayons-btn--icon-left" data-reaction-count data-reactable-id="${
+        article.id
+      }">
+                          <div class="multiple_reactions_aggregate">
+                            <span class="multiple_reactions_icons_container" dir="rtl">
+                                ${icons.join()}
+                            </span>
+                            <span class="aggregate_reactions_counter">
+                              <span class="hidden s:inline">${reactionsCount}&nbsp;${reactionsText}</span>
+                            </span>
+                        </span>
+                          </div>
+                        </a>`;
     }
 
     var picUrl;
