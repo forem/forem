c02cc289067366b071c16122a0fb9a74adff4008
Joshua Wehner <joshua@forem.com>
2023-03-24 17:07:28 +0100

    Use the cross-model-slug existence check

--- app/validators/cross_model_slug_validator.rb
+++ app/validators/cross_model_slug_validator.rb
@@ -3,61 +3,37 @@
 class CrossModelSlugValidator < ActiveModel::EachValidator
   FORMAT_REGEX = /\A[0-9a-z\-_]{2,}\z/
 
-  class_attribute :model_and_attribute_name_for_uniqueness_test
-
-  # Why a class attribute?  Allow for other implementations to extend
-  # this behavior.
-  self.model_and_attribute_name_for_uniqueness_test = {
-    Organization => :slug,
-    Page => :slug,
-    Podcast => :slug,
-    User => :username
-  }
-
   def validate_each(record, attribute, value)
-    format(record, attribute, value)
-    reserved(record, attribute, value)
-    uniqueness(record, attribute, value)
+    return if value.blank?
+
+    correct_format?(record, attribute, value)
+    not_on_reserved_list?(record, attribute, value)
+    unique_across_models?(record, attribute, value)
   end
 
   private
 
-  def reserved(record, attribute, value)
+  def not_on_reserved_list?(record, attribute, value)
     return unless ReservedWords.all.include?(value)
 
     record.errors.add(attribute, I18n.t("validators.cross_model_slug_validator.is_reserved"))
   end
 
-  def format(record, attribute, value)
+  def correct_format?(record, attribute, value)
     return if value.match?(FORMAT_REGEX)
 
     record.errors.add(attribute, I18n.t("validators.cross_model_slug_validator.is_invalid"))
   end
 
-  def uniqueness(record, attribute, value)
-    return unless already_exists?(value: value, record: record, attribute: attribute)
+  def unique_across_models?(record, attribute, value)
+    # attribute_changed? is likely redundant, but is much cheaper than the cross-model exists check
+    return unless record.public_send("#{attribute}_changed?")
+    return unless already_exists?(value)
 
     record.errors.add(attribute, I18n.t("validators.cross_model_slug_validator.is_taken"))
   end
 
-  ##
-  # Answers the question if it's okay for the record to use the given value.
-  #
-  # @param value [String] the value we're to check in the various classes
-  # @param record [ActiveRecord::Base] the record that we're attempting to validate
-  #
-  # @return [TrueClass] if the value already exists across the various classes.
-  # @return [FalseClass] if the given value is not already used.
-  #
-  # @see CLASS_AND_ATTRIBUTE_NAME_FOR_UNIQUENESS_TEST
-  def already_exists?(record:, attribute:, value:)
-    return false unless value
-    return true if value.include?("sitemap-")
-
-    model_and_attribute_name_for_uniqueness_test.detect do |model, attr|
-      next unless record.public_send("#{attribute}_changed?")
-
-      model.exists?(attr => value.downcase)
-    end
+  def already_exists?(value)
+    CrossModelSlug.exists?(value.downcase)
   end
 end
