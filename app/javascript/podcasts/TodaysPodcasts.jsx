114ad62c566bfe186eb8b06859911b8ce1f5a919
TERNOX <43003712+TERNOX@users.noreply.github.com>
2023-03-26 00:14:58 +0200

    Update TodaysPodcasts.jsx

--- app/javascript/podcasts/TodaysPodcasts.jsx
+++ app/javascript/podcasts/TodaysPodcasts.jsx
@@ -5,7 +5,7 @@ export const TodaysPodcasts = ({ children }) => (
   <div className="crayons-story">
     <div className="crayons-story__body">
       <h3 className="crayons-story__headline">
-        <a href="/pod">Today&apos;s Podcasts</a>
+        <a href="/pod">Свіжі випуски подкастів</a>
       </h3>
       {children}
     </div>
