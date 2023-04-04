f8f5d52b6e40633410e9629cac987d29e95d0280
Lawrence S <lawrence@forem.com>
2023-04-04 09:00:27 -0500

    Add getModal test

--- app/javascript/utilities/__tests__/showUserAlertModal.test.js
+++ app/javascript/utilities/__tests__/showUserAlertModal.test.js
@@ -1,8 +1,6 @@
 import { 
-  showModalAfterError, 
   getModal, 
-  getModalHtml, 
-  buildRateLimitText 
+  getModalHtml,
 } from "@utilities/showUserAlertModal";
 
 
@@ -10,5 +8,11 @@ describe('ShowUserAlert Utility', () => {
   it('should return modal html', () => {
     const modalHtml = getModalHtml("Sample text", "Sample Confirm Text");
     expect(modalHtml).toContain('Sample text');
+  });
+
+  it('should return the specified modal html', () => {
+    const modal = getModal("Sample text", "Sample Confirm Text");
+    const textDiv = modal.querySelector(".color-base-70");
+    expect(textDiv).toContain('Sample text');
   });
 });
