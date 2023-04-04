95c29c28628f5eca4949ef45e5321bfe4d2e5f75
Lawrence S <lboogie@Lawrences-Air.attlocal.net>
2023-04-03 21:49:29 -0500

    add logged in test

---
+++ app/javascript/utilities/__tests__/checkUserLoggedIn.test.js
@@ -0,0 +1,15 @@
+import { checkUserLoggedIn } from "@utilities/checkUserLoggedIn";
+
+describe('CheckUserLoggedIn Utility', () => {
+  it('should return false if no body', () => {
+    document.body = null
+    const userLoggedIn = checkUserLoggedIn()
+    expect(userLoggedIn).toEqual(false);
+  });
+
+  it('should return true if user has the logged in attribute', () => {
+    document.body.setAttribute('data-user-status', 'logged-in');
+    const userLoggedIn = checkUserLoggedIn()
+    expect(userLoggedIn).toEqual(true);
+  });
+});
