940e8e00fb79df98be94cc4722ef1da4d42efb48
Lawrence S <lawrence@forem.com>
2023-04-06 15:43:45 -0500

    fix test

--- app/javascript/utilities/__tests__/checkUserLoggedIn.test.js
+++ app/javascript/utilities/__tests__/checkUserLoggedIn.test.js
@@ -1,16 +1,14 @@
-import { checkUserLoggedIn } from "@utilities/checkUserLoggedIn";
+import { checkUserLoggedIn } from '@utilities/checkUserLoggedIn';
 
 describe('CheckUserLoggedIn Utility', () => {
   it('should return false if no body', () => {
-    document.body = undefined;
-    checkUserLoggedIn()
-    const userLoggedIn = false //checkUserLoggedIn()
+    const userLoggedIn = checkUserLoggedIn();
     expect(userLoggedIn).toEqual(false);
   });
 
-  // it('should return true if user has the logged in attribute', () => {
-  //   document.body.setAttribute('data-user-status', 'logged-in');
-  //   const userLoggedIn = checkUserLoggedIn()
-  //   expect(userLoggedIn).toEqual(true);
-  // });
+  it('should return true if user has the logged in attribute', () => {
+    document.body.setAttribute('data-user-status', 'logged-in');
+    const userLoggedIn = checkUserLoggedIn();
+    expect(userLoggedIn).toEqual(true);
+  });
 });
--- app/javascript/utilities/__tests__/showUserAlertModal.test.js
+++ app/javascript/utilities/__tests__/showUserAlertModal.test.js
@@ -1,18 +1,8 @@
-import { 
-  getModalHtml,
-} from "@utilities/showUserAlertModal";
-
+import { getModalHtml } from '@utilities/showUserAlertModal';
 
 describe('ShowUserAlert Utility', () => {
   it('should return modal html', () => {
-    const modalHtml = 'Sample text' //getModalHtml("Sample text", "Sample Confirm Text");
-    getModalHtml("Sample text", "Sample Confirm Text");
+    const modalHtml = getModalHtml('Sample text', 'Sample Confirm Text');
     expect(modalHtml).toContain('Sample text');
   });
-
-  // it('should return the specified modal html', () => {
-  //   const modal = getModal("Sample text", "Sample Confirm Text");
-  //   const textDiv = modal.querySelector(".color-base-70");
-  //   expect(textDiv).toContain('Sample text');
-  // });
 });
