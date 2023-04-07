156386f017e56b9b15f2258c70be0d6ad1fb0604
TERNOX <43003712+TERNOX@users.noreply.github.com>
2023-04-07 05:51:39 +0300

    xcxc

--- app/javascript/article-form/components/Help/EditorFormattingHelp.jsx
+++ app/javascript/article-form/components/Help/EditorFormattingHelp.jsx
@@ -6,6 +6,12 @@ export const EditorFormattingHelp = ({ openModal }) => (
     data-testid="format-help"
     className="crayons-article-form__help crayons-article-form__help--body"
   >
+      <h4 className="mb-2 fs-l">Як краще писати?</h4>
+    <ul className="list-disc pl-6 color-base-70">
+      <li>
+        У нас є <a href="https://kutok.io/editorial/pro_shcho_i_yak_my_pyshemo-3hdk" target="_blank">допис</a>, який докладно розповідає про що краще не писати, а також як саме це робити правильно. Будь ласка, ознайомтеся з ним.
+	  </li>
+	  </ul>
     <h4 className="mb-2 fs-l">Основи редактора</h4>
     <ul className="list-disc pl-6 color-base-70">
       <li>
@@ -13,6 +19,7 @@ export const EditorFormattingHelp = ({ openModal }) => (
         <a href="#markdown" onClick={() => openModal('markdownShowing')}>
           Розмітку
         </a>{' '}
+        щоб красиво оформлювати дописи.
         <details className="fs-s my-1">
           <summary class="cursor-pointer">Найчастіші способи використання</summary>
           <table className="crayons-card crayons-card--secondary crayons-table crayons-table--compact w-100 mt-2 mb-4 lh-tight">
@@ -115,17 +122,10 @@ export const EditorFormattingHelp = ({ openModal }) => (
         .
       </li>
       <li>
-        Окрім зображень для вмісту допису, ви також можете перетягнути зображення обкладинки.<hr>
-		<a href="https://kutok.io/site/gaid_z_vykorystannya_markdown-381i" target="_blank">Повний ґайд розмітки</a>
+        Окрім зображень для вмісту допису, ви також можете перетягнути зображення обкладинки.
       </li>
+	  <li><a href="https://kutok.io/site/gaid_z_vykorystannya_markdown-381i" target="_blank">Повний ґайд розмітки</a></li>
     </ul>
-	
-      <h4 className="mb-2 fs-l">Як краще писати?</h4>
-    <ul className="list-disc pl-6 color-base-70">
-      <li>
-        У нас є <a href="https://kutok.io/editorial/pro_shcho_i_yak_my_pyshemo-3hdk" target="_blank">допис</a>, який докладно розповідає про що краще не писати. Будь ласка, ознайомтеся з ним.
-	  </li>
-	  </ul>
   </div>
 );
 
