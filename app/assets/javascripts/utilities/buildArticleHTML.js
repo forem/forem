57262e2af27d8f5ab43fcf426d7bbb9389d6f18a
TERNOX <43003712+TERNOX@users.noreply.github.com>
2023-04-10 00:32:33 +0300

    сс

--- app/assets/javascripts/utilities/buildArticleHTML.js
+++ app/assets/javascripts/utilities/buildArticleHTML.js
@@ -99,10 +99,10 @@ function buildArticleHTML(article, currentUserId = null) {
         'class="crayons-btn crayons-btn--s crayons-btn--ghost crayons-btn--icon-left "><svg class="crayons-icon" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M10.5 5h3a6 6 0 110 12v2.625c-3.75-1.5-9-3.75-9-8.625a6 6 0 016-6zM12 15.5h1.5a4.501 4.501 0 001.722-8.657A4.5 4.5 0 0013.5 6.5h-3A4.5 4.5 0 006 11c0 2.707 1.846 4.475 6 6.36V15.5z"/></svg>';
       if (commentsCount > 0) {
         commentsDisplay +=
-          '<span class="hidden s:inline">Коментарів:&nbsp;</span></a>'+commentsCount;
+          '<span class="hidden s:inline">Коментарів:&nbsp;'+commentsCount+'</span></a>';
       } else {
         commentsDisplay +=
-          '<span class="hidden s:inline">Add&nbsp;Comment</span></a>';
+          '<span class="hidden s:inline">Додати:&nbsp;коментар</span></a>';
       }
     }
 
@@ -331,7 +331,7 @@ function buildArticleHTML(article, currentUserId = null) {
     }
 	
 	
-	var imageCOVER = '';
+	var imageCOVER = 'Ого це шо';
     if (article.main_image) {
       imageCOVER =
 	    '<div className="crayons-article__cover crayons-article__cover__image__feed"><a href="'+
@@ -342,9 +342,7 @@ function buildArticleHTML(article, currentUserId = null) {
 		article.main_image+
 		'" width="650" alt="'+
 		article.title+
-		'" style= backgroundColor: `$'+
-		article.main_image_background_hex_color+
-		'`,}}/></a></div>';
+		'" /></a></div>';
     }
 
     var navigationLink = `
@@ -383,8 +381,6 @@ function buildArticleHTML(article, currentUserId = null) {
                 <div class="crayons-story__details">
                   ${reactionsDisplay} ${commentsDisplay}
                 </div>\
-                <div class="crayons-story__save">\
-                  ${readingTimeHTML}\
                   ${saveButton}
                 </div>\
               </div>\
