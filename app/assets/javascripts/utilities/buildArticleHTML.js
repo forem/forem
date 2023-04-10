9f7ab5f7da2deb841c36c95227ca4b7f3dfa2b63
TERNOX <43003712+TERNOX@users.noreply.github.com>
2023-04-10 05:08:34 +0300

    vv

--- app/assets/javascripts/utilities/buildArticleHTML.js
+++ app/assets/javascripts/utilities/buildArticleHTML.js
@@ -332,14 +332,14 @@ function buildArticleHTML(article, currentUserId = null) {
 	
 	
 	var imageCOVER = '';
-    if (article.main_image) {
+    if (cloud_cover_url(article.main_image)) {
       imageCOVER = 
 	    'a<div className="crayons-article__cover crayons-article__cover__image__feed"><a href="'+
 		article.path+
 		'"className="crayons-article__cover__image__feed crayons-story__cover__image" title="'+
 		article.title+
 		'"><img className="crayons-article__cover__image__feed" src={'+
-		article.main_image+
+		cloud_cover_url(article.main_image)+
 		'" width="650" alt="'+
 		article.title+
 		'" /></a></div>';
