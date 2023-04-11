835338c7573d44739ed9322ff1daea0917eaa1d9
Lawrence S <lawrence@forem.com>
2023-04-11 14:31:14 -0500

    cypress init

--- app/javascript/utilities/__tests__/sendHapticMessage.test.js
+++ app/javascript/utilities/__tests__/sendHapticMessage.test.js
@@ -1,15 +1,6 @@
 import { sendHapticMessage } from '../sendHapticMessage';
 
 describe('SendHapticMessage Utility', () => {
-  // beforeEach(() => {
-  //   const mockPostMessage = jest.fn();
-  //   global.window.webkit = { messageHandlers: {
-  //     haptic: {
-  //       postMessage: mockPostMessage
-  //     }
-  //   }};
-  // });
-
   it('should call postMessage', async () => {
     const mockPostMessage = jest.fn();
     global.window.webkit = {
---
+++ cypress/e2e/seededFlows/notificationsFlows/notificationInitialization.spec.js
@@ -0,0 +1,18 @@
+describe('Notification initialization', () => {
+  describe('Notification initialization', () => {
+    beforeEach(() => {
+      cy.testSetup();
+      cy.fixture('users/notificationsUser.json').as('user');
+
+      cy.get('@user').then((user) => {
+        cy.loginAndVisit(user, '/notifications');
+      });
+    });
+
+    it('Shows the notification count', () => {
+      cy.findByRole('heading', { name: 'Notifications' }).as('notification');
+
+      cy.get('@notification').find('span').should('have.text', '15');
+    });
+  });
+});
