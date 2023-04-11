309855a90ccf92b2ce11dd1ff3ed4d75b014bf8f
Lawrence S <lawrence@forem.com>
2023-04-11 13:19:57 -0500

    fix haptic tests

--- app/javascript/utilities/__tests__/sendHapticMessage.test.js
+++ app/javascript/utilities/__tests__/sendHapticMessage.test.js
@@ -1,19 +1,33 @@
-import { sendHapticMessage } from "../sendHapticMessage";
+import { sendHapticMessage } from '../sendHapticMessage';
 
 describe('SendHapticMessage Utility', () => {
-  beforeEach(() => {
-    const mockPostMessage = jest.fn();
-    global.window.webkit = { messageHandlers: {
-      haptic: {
-        postMessage: mockPostMessage
-      }
-    }};
-  });
+  // beforeEach(() => {
+  //   const mockPostMessage = jest.fn();
+  //   global.window.webkit = { messageHandlers: {
+  //     haptic: {
+  //       postMessage: mockPostMessage
+  //     }
+  //   }};
+  // });
 
   it('should call postMessage', async () => {
-    await sendHapticMessage("sample message");
+    const mockPostMessage = jest.fn();
+    global.window.webkit = {
+      messageHandlers: {
+        haptic: {
+          postMessage: mockPostMessage,
+        },
+      },
+    };
+    await sendHapticMessage('sample message');
 
     expect(mockPostMessage).toHaveBeenCalled();
-  })
+  });
 
-})
+  it('should log to console otherwise', async () => {
+    global.window = {};
+    global.console.log = jest.fn();
+    await sendHapticMessage('sample message');
+    expect(global.console.log).not.toHaveBeenCalled();
+  });
+});
