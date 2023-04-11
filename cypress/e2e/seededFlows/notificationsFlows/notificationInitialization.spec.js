23e69f2cb0cc6afa0d505345fd2b3828194f4962
Lawrence S <lawrence@forem.com>
2023-04-11 14:50:55 -0500

    update tests

--- cypress/e2e/seededFlows/notificationsFlows/notificationInitialization.spec.js
+++ cypress/e2e/seededFlows/notificationsFlows/notificationInitialization.spec.js
@@ -1,5 +1,22 @@
 describe('Notification initialization', () => {
-  describe('Notification initialization', () => {
+  describe('Home page notifications', () => {
+    beforeEach(() => {
+      cy.testSetup();
+      cy.fixture('users/notificationsUser.json').as('user');
+
+      cy.get('@user').then((user) => {
+        cy.loginUser(user);
+      });
+    });
+
+    it('Shows the notification count', () => {
+      cy.findByRole('heading', { name: 'Notifications' }).as('notification');
+
+      cy.get('@notification').find('span').should('have.text', '');
+    });
+  });
+
+  describe('Notifications page', () => {
     beforeEach(() => {
       cy.testSetup();
       cy.fixture('users/notificationsUser.json').as('user');
