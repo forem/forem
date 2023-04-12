fc28dfaae5085dec1e802653c258e5b7a34587ca
Rajat Talesra <rajat@forem.com>
2023-04-12 18:47:29 +0530

    Support for new ad placement locations on home page feed (#19299)
    * Basic implementation of all 3 ad placements
    
    * UI fixes
    
    * Dropdown bug fix
    
    * Optimised code
    
    * Updated tests
    
    * Updated tes
    
    * EOF added
    
    * Improved a11y
    
    * Nit fix

--- spec/views/stories/sign_in_invitation.html.erb_spec.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-
-RSpec.describe "stories/_sign_in_invitation" do
-  it "has the community member label" do
-    render
-    expect(rendered).to have_text(Settings::Community.member_label.pluralize)
-  end
-end
