314fd1f234c82b508ef28f45232d73c082598e06
Joshua Wehner <joshua@forem.com>
2023-04-20 10:43:06 +0200

    Try to catch unhandled exception

--- app/javascript/packs/hideBookmarkButtons.js
+++ app/javascript/packs/hideBookmarkButtons.js
@@ -1,12 +1,19 @@
 import { getUserDataAndCsrfToken } from '@utilities/getUserDataAndCsrfToken';
 
-getUserDataAndCsrfToken().then(({ currentUser }) => {
-  const currentUserId = currentUser && currentUser.id;
+getUserDataAndCsrfToken().then(
+  ({ currentUser }) => {
+    const currentUserId = currentUser && currentUser.id;
 
-  document.querySelectorAll('.bookmark-button').forEach((button) => {
-    const { articleAuthorId } = button.dataset;
-    if (currentUserId && articleAuthorId == currentUserId) {
-      button.classList.add('hidden');
+    document.querySelectorAll('.bookmark-button').forEach((button) => {
+      const { articleAuthorId } = button.dataset;
+      if (currentUserId && articleAuthorId == currentUserId) {
+        button.classList.add('hidden');
+      }
+    });
+  },
+  (error) => {
+    if (!/find user data/.test(error)) {
+      Honeybadger.notify(error);
     }
-  });
-});
+  },
+);
