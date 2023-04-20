874d727788607734b010e75b7e0e40263c65eec2
Joshua Wehner <joshua@forem.com>
2023-04-20 16:34:00 +0200

    Singular id in RefreshWorker

--- app/workers/audience_segment_refresh_worker.rb
+++ app/workers/audience_segment_refresh_worker.rb
@@ -3,8 +3,7 @@ class AudienceSegmentRefreshWorker
 
   sidekiq_options queue: :low_priority
 
-  def perform(*ids)
-    ids = ids.flatten
-    AudienceSegment.where(id: ids).find_each(&:refresh!)
+  def perform(id)
+    AudienceSegment.find(id).refresh!
   end
 end
--- spec/workers/audience_segment_refresh_worker_spec.rb
+++ spec/workers/audience_segment_refresh_worker_spec.rb
@@ -6,21 +6,14 @@ RSpec.describe AudienceSegmentRefreshWorker, type: :worker do
   let(:segment_finder) { class_double AudienceSegment }
 
   before do
-    allow(segment_finder).to receive(:find_each).and_yield(fake_segment)
-    allow(AudienceSegment).to receive(:where).and_return(segment_finder)
+    allow(AudienceSegment).to receive(:find).and_return(fake_segment)
   end
 
   include_examples "#enqueues_on_correct_queue", "low_priority"
 
   it "refreshes a segment by id" do
-    worker.perform([123])
-    expect(AudienceSegment).to have_received(:where).with(id: [123])
-    expect(fake_segment).to have_received(:refresh!)
-  end
-
-  it "refreshes segments via ids" do
-    worker.perform(123, 345)
-    expect(AudienceSegment).to have_received(:where).with(id: [123, 345])
+    worker.perform(123)
+    expect(AudienceSegment).to have_received(:find).with(123)
     expect(fake_segment).to have_received(:refresh!)
   end
 end
