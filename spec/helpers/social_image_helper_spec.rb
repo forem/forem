403f4d1dc60b31213680e999a4a4bf8192a72636
TERNOX <ternox1@gmail.com>
2023-04-24 04:56:22 +0300

    FFD

--- spec/helpers/social_image_helper_spec.rb
+++ spec/helpers/social_image_helper_spec.rb
@@ -18,7 +18,7 @@ describe SocialImageHelper do
       url = helper.article_social_image_url(article)
 
       expect(url).to match(/#{article.main_image}/)
-      expect(url).to include("c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/")
+      expect(url).to include("c_limit,f_auto,fl_progressive,h_500,q_auto,w_1000/")
     end
 
     it "returns older url2png image if already generated" do
@@ -39,7 +39,7 @@ describe SocialImageHelper do
     it "returns correct manipulation of cloudinary links", cloudinary: true do
       article.update_column(
         :main_image,
-        "https://res.cloudinary.com/practicaldev/image/fetch/s--A-gun7rr--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://res.cloudinary.com/practicaldev/image/fetch/s--hcD8ZkbP--/c_imagga_scale%2Cf_auto%2Cfl_progressive%2Ch_420%2Cq_auto%2Cw_1000/https://dev-to-uploads.s3.amazonaws.com/i/th93d625o27nuz63oeen.png", # rubocop:disable Layout/LineLength
+        "https://res.cloudinary.com/practicaldev/image/fetch/s--A-gun7rr--/c_limit,f_auto,fl_progressive,h_420,q_auto,w_1000/https://res.cloudinary.com/practicaldev/image/fetch/s--hcD8ZkbP--/c_imagga_scale%2Cf_auto%2Cfl_progressive%2Ch_420%2Cq_auto%2Cw_1000/https://dev-to-uploads.s3.amazonaws.com/i/th93d625o27nuz63oeen.png", # rubocop:disable Layout/LineLength
       )
       url = helper.article_social_image_url(article.decorate, width: 1600, height: 900)
 
