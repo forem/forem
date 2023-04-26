55afd6aa92eb55675d70741018341617743ad7d3
Ridhwana <ridhwana.khan16@gmail.com>
2023-04-26 22:01:15 +0200

    spec: article

--- spec/queries/articles/api_search_query_spec.rb
+++ spec/queries/articles/api_search_query_spec.rb
@@ -5,15 +5,15 @@ RSpec.describe Articles::ApiSearchQuery, type: :query do
     create(:article, published: false)
     create(:article, title: "Top ten Interview tips")
     create(:article, title: "Top ten Ruby tips")
-    create(:article, title: "Frontend Frameworks", published_at: 2.days.from_now)
+    create(:article, title: "Frontend Frameworks")
     create(:article)
   end
 
   context "when there is no query parameter" do
     it "shows all published and approved articles" do
       articles = described_class.call({})
-      # The two not included has publiched set at false and in the future.
-      expect(articles.count).to eq(3)
+      # The one not included has publiched set to false.
+      expect(articles.count).to eq(4)
     end
   end
 
@@ -26,6 +26,10 @@ RSpec.describe Articles::ApiSearchQuery, type: :query do
 
   context "when there is a top parameter" do
     it "shows the most popular articles in the last n days" do
+      article = Article.find_by(title: "Frontend Frameworks")
+      article.update_column(:published_at, 30.days.ago)
+
+      # The two not included is the one that has publiched set to false.
       articles = described_class.call({ top: 10 })
       expect(articles.count).to eq(3)
     end
