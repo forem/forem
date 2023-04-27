0126b504e93222fcd7995b285ea06271f1172217
Dhanujha <rampradha2016@gmail.com>
2023-04-27 09:59:02 +0530

    Changes the version date of db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_07_215100) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_30_145039) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_stat_statements"
