8aac1e6c67db76801316ae614c411f6af412182f
Ridhwana <ridhwana.khan16@gmail.com>
2023-05-08 12:32:58 +0200

    feat: add style and width

--- app/liquid_tags/cta_tag.rb
+++ app/liquid_tags/cta_tag.rb
@@ -2,23 +2,46 @@ class CtaTag < Liquid::Block
   include ActionView::Helpers::SanitizeHelper
 
   PARTIAL = "liquids/cta".freeze
+  STYLE_OPTIONS = %w[branded].freeze
+  WIDTH_OPTIONS = %w[inline block].freeze
 
-  def initialize(_tag_name, link, _parse_context)
+  def initialize(_tag_name, options, _parse_context)
     super
-    @link = strip_tags(link.strip)
+
+    options = strip_tags(options.strip).split
+
+    @link = link(options)
+    @style = style(options)
+    @width = width(options)
   end
 
   def render(_context)
     description = Nokogiri::HTML.parse(super).at("body").text.strip
-
     ApplicationController.render(
       partial: PARTIAL,
       locals: {
         link: @link,
         description: description,
-        style: "branded" # in the future we can use this property to expose different cta styles
+        style: @style,
+        width: @width
       },
     )
+  end
+
+  private
+
+  def link(options)
+    options[0]
+  end
+
+  def style(options)
+    styles = options & STYLE_OPTIONS
+    styles.length.positive? ? styles.first : STYLE_OPTIONS.first
+  end
+
+  def width(options)
+    widths = options & WIDTH_OPTIONS
+    widths.length.positive? ? widths.first : WIDTH_OPTIONS.first
   end
 end
 
--- spec/liquid_tags/cta_tag_spec.rb
+++ spec/liquid_tags/cta_tag_spec.rb
@@ -3,20 +3,68 @@ require "rails_helper"
 RSpec.describe CtaTag, type: :liquid_tag do
   describe "#render" do
     let(:link) { "https://dev.to/" }
+    let(:only_link_option) { link }
+
+    let(:style) { "branded" }
+    let(:link_style_options) { "#{link} #{style}" }
+
+    let(:width) { "block" }
+    let(:link_style_width_options) { "#{link} #{style} #{width}" }
+
+    let(:link_width_options) { "#{link} #{width}" }
+
     let(:description) { "DEV Community" }
 
-    def generate_details_liquid(link, description)
+    # test case where no link provided
+    # do we want to account for alignment
+
+    def generate_details_liquid(options, description)
       Liquid::Template.register_tag("cta", described_class)
-      Liquid::Template.parse("{% cta #{link} %} #{description} {% endcta %}")
+      Liquid::Template.parse("{% cta #{options} %} #{description} {% endcta %}")
     end
 
-    it "generates proper details div with link" do
-      rendered = generate_details_liquid(link, description).render
-
-      expect(rendered).to include("href=\"#{link}")
-      expect(rendered).to include("class=\"crayons-btn ml-auto")
+    it "contains the correct static attribuutes" do
+      rendered = generate_details_liquid(link_style_options, description).render
+      expect(rendered).to include("class=\"ltag_cta")
       expect(rendered).to include("role=\"button")
+    end
+
+    it "generates the correct href attribute" do
+      rendered = generate_details_liquid(link_style_options, description).render
+      expect(rendered).to include("href=\"#{link}")
+    end
+
+    it "contains the correct description" do
+      rendered = generate_details_liquid(link_style_options, description).render
       expect(rendered).to include(description)
+    end
+
+    context "when given a style attribute" do
+      it "contains the style attribute provided" do
+        rendered = generate_details_liquid(link_style_options, description).render
+        expect(rendered).to include("class=\"ltag_cta ltag_cta--branded")
+      end
+    end
+
+    context "when not given a style attribute" do
+      it "sets the default style attribute when one is not provided" do
+        rendered = generate_details_liquid(only_link_option, description).render
+        expect(rendered).to include("class=\"ltag_cta ltag_cta--branded")
+      end
+    end
+
+    context "when given a width attribute" do
+      it "contains the width attribute provided" do
+        rendered = generate_details_liquid(link_style_width_options, description).render
+        expect(rendered).to include("block")
+      end
+    end
+
+    context "when not given a width attribute" do
+      it "sets the default width attribute when one is not provided" do
+        rendered = generate_details_liquid(only_link_option, description).render
+        expect(rendered).to include("inline")
+      end
     end
   end
 end
