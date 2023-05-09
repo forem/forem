2fb6c167805665889706bf507ee751272a506911
Lawrence S <lawrence@forem.com>
2023-05-09 10:40:40 -0500

    Remove url since nav_image is string

--- app/assets/javascripts/utilities/buildArticleHTML.js
+++ app/assets/javascripts/utilities/buildArticleHTML.js
@@ -51,11 +51,11 @@ function buildArticleHTML(article, currentUserId = null) {
   if (article && article.class_name === 'Organization') {
     return `<article class="crayons-story">
         <div class="crayons-story__body flex items-start gap-2">
-          <a href="${article.slug}" class="crayons-podcast-episode__cover">
-            <img src="${article.nav_image.url}" alt="${
+            <a href="${article.slug}" class="crayons-podcast-episode__cover">
+              <img src="${article.nav_image}" alt="${
       article.name
     }" loading="lazy" />
-          </a>
+            </a>
           <div>
             <h3 class="crayons-subtitle-2 lh-tight py-1">
               <a href="${article.slug}" class="c-link">
