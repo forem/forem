1b887085d4d639b8583647b569ea197be5797c16
Lawrence S <lawrence@forem.com>
2023-05-09 11:21:57 -0500

    add image url to serializer

--- app/serializers/search/organization_serializer.rb
+++ app/serializers/search/organization_serializer.rb
@@ -1,6 +1,6 @@
 module Search
   class OrganizationSerializer < ApplicationSerializer
     attribute :class_name, -> { "Organization" }
-    attributes :id, :name, :summary, :profile_image, :twitter_username, :nav_image, :slug
+    attributes :id, :name, :summary, :profile_image, :twitter_username, :nav_image, :slug, :profile_image_url
   end
 end
