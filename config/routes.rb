50ae5f6c1e5423d6adf67a0a6af4671288da3c8d
PJ <pj@forem.com>
2023-05-15 09:41:20 +0100

    getting and listing segments

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ Rails.application.routes.draw do
           put "unpublish", on: :member
         end
 
-        resources :segments, controller: "audience_segments", only: %i[create destroy] do
+        resources :segments, controller: "audience_segments", only: %i[index show create destroy] do
           put "add_users", on: :member
           put "remove_users", on: :member
         end
