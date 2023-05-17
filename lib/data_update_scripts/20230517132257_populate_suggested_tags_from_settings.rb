dd8e05ed5467f6b000d4a4ae2d0710917c6417da
Joshua Wehner <joshua@forem.com>
2023-05-17 15:44:47 +0200

    Migrate tags to add suggested boolean column

---
+++ lib/data_update_scripts/20230517132257_populate_suggested_tags_from_settings.rb
@@ -0,0 +1,17 @@
+module DataUpdateScripts
+  class PopulateSuggestedTagsFromSettings
+    cattr_accessor :suggested_tags
+
+    def run(suggested_tags: settings_suggested_tags)
+      return unless suggested_tags.any?
+
+      Tag.where(name: suggested_tags).update_all suggested: true
+    end
+
+    private
+
+    def settings_suggested_tags
+      suggested_tags || Settings::General.suggested_tags
+    end
+  end
+end
