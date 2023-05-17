a6543d4bd16b5711ec6c8975bf1c380cf68eb331
Joshua Wehner <joshua@forem.com>
2023-05-17 16:25:01 +0200

    Better sort order

--- app/queries/tags/suggested_for_onboarding.rb
+++ app/queries/tags/suggested_for_onboarding.rb
@@ -13,7 +13,7 @@ module Tags
 
       Tag
         .where(suggested_for_onboarding_or_supported)
-        .order("taggings_count DESC")
+        .order("suggested DESC, supported DESC, taggings_count DESC, name ASC")
         .limit(MAX)
     end
 
