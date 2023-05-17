b83264e721cad15dd0c12cb6391190d55c2d1ea0
benhalpern <bendhalpern@gmail.com>
2023-05-17 17:22:44 -0400

    Remove double title from mod center item

--- app/javascript/modCenter/moderationArticles.jsx
+++ app/javascript/modCenter/moderationArticles.jsx
@@ -28,11 +28,6 @@ export class ModerationArticles extends Component {
 
     if (selectedDetailsPanel.getAttribute('open') !== null) {
       selectedArticle.innerHTML = `
-      <div class="article-referrer-heading">
-        <a class="article-title-link fw-bold" href=${path}>
-          ${title}
-        </a>
-      </div>
       <div class="iframes-container">
         <iframe class="article-iframe" src="${path}"></iframe>
         <iframe data-testid="mod-iframe-${id}" id="mod-iframe-${id}" class="actions-panel-iframe" id="mod-iframe-${id}" src="${path}/actions_panel/?is_mod_center=true"></iframe>
