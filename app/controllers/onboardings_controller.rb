594d91f678b035b8cd63c2e1ab52905aebb44b5a
Joshua Wehner <joshua@forem.com>
2023-05-18 17:03:00 +0200

    Quick refactor onboardings controller(s), pt5

--- app/controllers/onboardings_controller.rb
+++ app/controllers/onboardings_controller.rb
@@ -1,29 +1,16 @@
 class OnboardingsController < ApplicationController
   before_action :authenticate_user!
+  before_action :check_suspended, only: %i[notifications]
   before_action :set_cache_control_headers, only: %i[show tags]
   before_action :set_no_cache_header, only: %i[update]
   after_action :verify_authorized, only: %i[update checkbox]
 
   ALLOWED_USER_PARAMS = %i[last_onboarding_page username].freeze
   ALLOWED_CHECKBOX_PARAMS = %i[checked_code_of_conduct checked_terms_and_conditions].freeze
+  ALLOWED_NOTIFICATION_PARAMS = %i[email_newsletter email_digest_periodic].freeze
 
   def show
     set_surrogate_key_header "onboarding-slideshow"
-  end
-
-  def checkbox
-    if params[:user]
-      current_user.assign_attributes(params[:user].permit(ALLOWED_CHECKBOX_PARAMS))
-    end
-
-    current_user.saw_onboarding = true
-    authorize User, :onboarding_checkbox_update?
-
-    if current_user.save
-      render json: {}, status: :ok
-    else
-      render json: { errors: errors }, status: :unprocessable_entity
-    end
   end
 
   def tags
@@ -56,6 +43,35 @@ class OnboardingsController < ApplicationController
     end
   end
 
+  def checkbox
+    authorize User, :onboarding_checkbox_update?
+
+    if params[:user]
+      current_user.assign_attributes(params[:user].permit(ALLOWED_CHECKBOX_PARAMS))
+    end
+
+    current_user.saw_onboarding = true
+
+    if current_user.save
+      render json: {}, status: :ok
+    else
+      render json: { errors: errors }, status: :unprocessable_entity
+    end
+  end
+
+  def notifications
+    authorize User, :onboarding_notifications_checkbox_update?
+
+    if params[:notifications]
+      current_user.notification_setting.assign_attributes(params[:notifications].permit(ALLOWED_NOTIFICATION_PARAMS))
+    end
+
+    current_user.saw_onboarding = true
+
+    success = current_user.notification_setting.save
+    notifications_updated_response(success, current_user.notification_setting.errors_as_sentence)
+  end
+
   private
 
   def unset_username?
@@ -68,5 +84,13 @@ class OnboardingsController < ApplicationController
 
   def profile_params
     params[:profile]&.permit(Profile.static_fields + Profile.attributes)
+  end
+
+  def notifications_updated_response(success, errors)
+    status = success ? 200 : 422
+
+    respond_to do |format|
+      format.json { render json: { errors: errors }, status: status }
+    end
   end
 end
