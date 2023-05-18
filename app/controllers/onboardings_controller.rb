3c99ba2341c3b1748ab31859bce0d668e49af513
Joshua Wehner <joshua@forem.com>
2023-05-18 10:49:23 +0200

    Quick refactor onboardings controller(s), pt3

--- app/controllers/onboardings_controller.rb
+++ app/controllers/onboardings_controller.rb
@@ -2,13 +2,28 @@ class OnboardingsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_cache_control_headers, only: %i[show tags]
   before_action :set_no_cache_header, only: %i[follow_users]
-  after_action :verify_authorized, only: [:follow_users]
+  after_action :verify_authorized, only: %i[follow_users checkbox]
 
   ALLOWED_USER_PARAMS = %i[last_onboarding_page username].freeze
   ALLOWED_CHECKBOX_PARAMS = %i[checked_code_of_conduct checked_terms_and_conditions].freeze
 
   def show
     set_surrogate_key_header "onboarding-slideshow"
+  end
+
+  def checkbox
+    if params[:user]
+      current_user.assign_attributes(params[:user].permit(ALLOWED_CHECKBOX_PARAMS))
+    end
+
+    current_user.saw_onboarding = true
+    authorize User, :onboarding_checkbox_update?
+
+    if current_user.save
+      render json: {}, status: :ok
+    else
+      render json: { errors: errors }, status: :unprocessable_entity
+    end
   end
 
   def tags
