1798a1f8456aa69dd931e0fabeaaca91d6bbadeb
Duke Greene <dukegreene@gmail.com>
2023-05-23 10:51:46 -0400

    notify mod on new user articles (#19478)
    * update moderation notification view and message for articles
    
    * update models to allow first published articles to notify mod after create
    
    * update mod notifications worker and spec to support articles
    
    * update mod notification send service and spec to support articles
    
    * update helper and request spec to support article mod notifications
    
    * update spec language to match updated notification message
    
    * Update spec/workers/notifications/create_round_robin_moderation_notifications_worker_spec.rb
    
    Co-authored-by: Ridhwana <Ridhwana.Khan16@gmail.com>
    
    * Update spec/workers/notifications/create_round_robin_moderation_notifications_worker_spec.rb
    
    Co-authored-by: Ridhwana <Ridhwana.Khan16@gmail.com>
    
    * Update spec/workers/notifications/create_round_robin_moderation_notifications_worker_spec.rb
    
    Co-authored-by: Ridhwana <Ridhwana.Khan16@gmail.com>
    
    * Update spec/workers/notifications/create_round_robin_moderation_notifications_worker_spec.rb
    
    Co-authored-by: Ridhwana <Ridhwana.Khan16@gmail.com>
    
    * fix comment and refactor order of send_moderation_notification steps
    
    * add fr.yml article mod notification config
    
    * fix spacing around equal sign per rubocop
    
    * fix spacing around equal sign per rubocop
    
    * Update spec/workers/notifications/create_round_robin_moderation_notifications_worker_spec.rb
    
    Co-authored-by: Ridhwana <Ridhwana.Khan16@gmail.com>
    
    ---------
    
    Co-authored-by: Ridhwana <Ridhwana.Khan16@gmail.com>

--- app/workers/notifications/create_round_robin_moderation_notifications_worker.rb
+++ app/workers/notifications/create_round_robin_moderation_notifications_worker.rb
@@ -6,17 +6,21 @@ module Notifications
 
     MODERATOR_SAMPLE_SIZE = 4
 
-    def perform(notifiable_id)
+    def perform(notifiable_id, notifiable_type)
       random_moderators = Users::SelectModeratorsQuery.call.order(Arel.sql("RANDOM()"))
         .first(MODERATOR_SAMPLE_SIZE)
       return unless random_moderators.any?
 
-      # notifiable is currently only comment
-      notifiable = Comment.find_by(id: notifiable_id)
-      return unless notifiable
+      if notifiable_type == "Comment"
+        notifiable = Comment.find_by(id: notifiable_id)
+        # return if it's a comment whose commentable has been deleted
+        return unless notifiable.commentable
 
-      # return if it's a comment whose commentable has been deleted
-      return unless notifiable.commentable
+      elsif notifiable_type == "Article"
+        notifiable = Article.find_by(id: notifiable_id)
+      end
+
+      return unless notifiable
 
       random_moderators.each do |mod|
         next if mod == notifiable.user
