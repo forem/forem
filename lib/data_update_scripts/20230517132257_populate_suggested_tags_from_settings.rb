8296b68a5363d9fb705f9ef03fcb1c4483c7cc24
Joshua Wehner <joshua@forem.com>
2023-05-23 16:46:24 +0200

    Add a little comment

--- lib/data_update_scripts/20230517132257_populate_suggested_tags_from_settings.rb
+++ lib/data_update_scripts/20230517132257_populate_suggested_tags_from_settings.rb
@@ -2,6 +2,9 @@ module DataUpdateScripts
   class PopulateSuggestedTagsFromSettings
     cattr_accessor :suggested_tags
 
+    # We've added a new column on Tag (suggested) that needs to be populated
+    # from a list that has previously been stored in Settings::General.suggested_tags
+    # later this can go away
     def run(suggested_tags: settings_suggested_tags)
       return unless suggested_tags.any?
 
