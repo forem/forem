77d093c61ed9d8e3f22d277b6176946270343343
PJ <pj@forem.com>
2023-05-24 19:36:11 +0100

    specs that actually work (this time)

--- cypress/e2e/seededFlows/moderationFlows/flagging.spec.js
+++ cypress/e2e/seededFlows/moderationFlows/flagging.spec.js
@@ -1,4 +1,11 @@
 describe('Flagging to admins', () => {
+  const navigateToCommentFlagPage = () => {
+    return cy.get('#comments').within(() => {
+      cy.findByRole('button', { name: 'Toggle dropdown menu' }).click();
+      cy.findByRole('link', { name: 'Moderate' }).click();
+    });
+  };
+
   const findButton = (labelText, { as, highlighted }) => {
     const assertion = highlighted ? 'have.class' : 'not.have.class';
     cy.get('.reaction-button')
@@ -7,72 +14,59 @@ describe('Flagging to admins', () => {
       .should(assertion, 'reacted');
   };
 
-  const flagContentSpec = () => {
-    cy.get('@contentUser').then(({ username }) => {
-      cy.intercept('POST', '/reactions').as('flagRequest');
-
-      findButton('Flag to Admins', { as: 'contentFlag', highlighted: false });
-      findButton(`Flag ${username}`, { as: 'userFlag', highlighted: false });
-
-      cy.get('@contentFlag').click();
-      cy.wait('@flagRequest');
-      cy.reload();
-
-      findButton('Flag to Admins', { as: 'contentFlag', highlighted: true });
-      findButton(`Flag ${username}`, { as: 'userFlag', highlighted: false });
-
-      cy.get('@contentFlag').click();
-      cy.wait('@flagRequest');
-      cy.reload();
-
-      findButton('Flag to Admins', { as: 'contentFlag', highlighted: false });
-      findButton(`Flag ${username}`, { as: 'userFlag', highlighted: false });
-    });
+  const clickWithPageReload = (buttonAlias) => {
+    cy.get(buttonAlias).click();
+    cy.wait('@flagRequest');
+    cy.reload();
   };
 
   beforeEach(() => {
     cy.testSetup();
+
     cy.fixture('users/adminUser.json').as('admin');
-    cy.fixture('users/seriesUser.json').as('contentUser');
+    cy.fixture('users/questionableUser.json').as('contentUser');
+    cy.intercept('POST', '/reactions').as('flagRequest');
+
+    cy.get('@admin').then((admin) => {
+      cy.loginAndVisit(admin, '/series_user/series-test-article-slug');
+    });
   });
 
-  context('when targeting an article', () => {
-    beforeEach(() => {
-      cy.get('@admin').then((admin) => {
-        cy.loginAndVisit(admin, '/series_user/series-test-article-slug/mod');
+  it('flags and unflags only the comment when clicked', () => {
+    navigateToCommentFlagPage().then(() => {
+      cy.get('@contentUser').then(({ username }) => {
+        findButton('Flag to Admins', { as: 'contentFlag', highlighted: false });
+        findButton(`Flag ${username}`, { as: 'userFlag', highlighted: false });
+
+        clickWithPageReload('@contentFlag');
+
+        findButton('Flag to Admins', { as: 'contentFlag', highlighted: true });
+        findButton(`Flag ${username}`, { as: 'userFlag', highlighted: false });
+
+        clickWithPageReload('@contentFlag');
+
+        findButton('Flag to Admins', { as: 'contentFlag', highlighted: false });
+        findButton(`Flag ${username}`, { as: 'userFlag', highlighted: false });
       });
     });
-
-    it('flags and unflags only the article when clicked', flagContentSpec);
   });
 
-  context('when targeting a comment', () => {
-    beforeEach(() => {
-      cy.get('@contentUser')
-        .then((user) => cy.loginUser(user))
-        .then(() =>
-          cy.createArticle({
-            title: 'Test Article',
-            content: 'Test article contents',
-            published: true,
-          }),
-        )
-        .then((response) =>
-          cy.createComment({
-            content: 'This is a test comment.',
-            commentableId: response.body.id,
-            commentableType: 'Article',
-          }),
-        )
-        .then((response) => {
-          cy.signOutUser().then(() => {
-            cy.get('@admin').then((admin) => {
-              cy.loginAndVisit(admin, `${response.body.url}/mod`);
-            });
-          });
-        });
-    });
+  it('flags and unflags only the user when clicked', () => {
+    navigateToCommentFlagPage().then(() => {
+      cy.get('@contentUser').then(({ username }) => {
+        findButton('Flag to Admins', { as: 'contentFlag', highlighted: false });
+        findButton(`Flag ${username}`, { as: 'userFlag', highlighted: false });
 
-    it('flags and unflags only the comment when clicked', flagContentSpec);
+        clickWithPageReload('@userFlag');
+
+        findButton('Flag to Admins', { as: 'contentFlag', highlighted: false });
+        findButton(`Flag ${username}`, { as: 'userFlag', highlighted: true });
+
+        clickWithPageReload('@userFlag');
+
+        findButton('Flag to Admins', { as: 'contentFlag', highlighted: false });
+        findButton(`Flag ${username}`, { as: 'userFlag', highlighted: false });
+      });
+    });
   });
 });
