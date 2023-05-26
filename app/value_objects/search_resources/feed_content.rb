03bb59915dea35d7ffd600260fd961ef0ae7c6d7
Cícero Medeiros <cicero.medeiros@codeminer42.com>
2023-05-26 12:37:11 -0300

    Add feed_content value_object and implementation for search_controller Co-authored-by: Carlos Daniel Pohlod <carlospohlod@gmail.com>

---
+++ app/value_objects/search_resources/feed_content.rb
@@ -0,0 +1,31 @@
+module SearchResources
+  class FeedContent
+    def initialize(feed_params:)
+      @feed_params = feed_params
+    end
+
+    def class_name
+      feed_params[:class_name].to_s.inquiry
+    end
+
+    def article_search?
+      article? && blank_search_fields? && sorted?
+    end
+
+    private
+
+    attr_reader :feed_params
+
+    def article?
+      class_name.Article?
+    end
+
+    def sorted?
+      feed_params[:sort_by].present?
+    end
+
+    def blank_search_fields?
+      feed_params[:search_fields].blank?
+    end
+  end
+end
