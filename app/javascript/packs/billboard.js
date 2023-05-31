3887a91b98d6e3ab52cff65678429f6dbb0b2b0b
Mac Siri <krairit.siri@gmail.com>
2023-05-31 14:51:57 -0400

    Add Error handling to billboards.js

--- app/javascript/packs/billboard.js
+++ app/javascript/packs/billboard.js
@@ -14,20 +14,25 @@ async function generateDisplayAd(element) {
   const { asyncUrl } = element.dataset;
 
   if (asyncUrl) {
-    const response = await window.fetch(`${asyncUrl}`);
-    const htmlContent = await response.text();
+    try {
+      const response = await window.fetch(`${asyncUrl}`);
+      const htmlContent = await response.text();
 
-    const generatedElement = document.createElement('div');
-    generatedElement.innerHTML = htmlContent;
+      const generatedElement = document.createElement('div');
+      generatedElement.innerHTML = htmlContent;
 
-    element.innerHTML = '';
-    element.appendChild(generatedElement);
-    setupDisplayAdDropdown();
-    // This is called here because the ad is loaded asynchronously.
-    // The original code is still in the asset pipeline, so is not importable.
-    // This could be refactored to be importable as we continue that migration.
-    // eslint-disable-next-line no-undef
-    observeDisplayAds();
+      element.innerHTML = '';
+      element.appendChild(generatedElement);
+      setupDisplayAdDropdown();
+      // This is called here because the ad is loaded asynchronously.
+      // The original code is still in the asset pipeline, so is not importable.
+      // This could be refactored to be importable as we continue that migration.
+      // eslint-disable-next-line no-undef
+    } catch (error) {
+      if (!/NetworkError/i.test(error.message)) {
+        Honeybadger.notify(error);
+      }
+    }
   }
 }
 
