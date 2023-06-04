977d820c5c0ce6140df0e49f71c05c1d8bb70702
TERNOX <ternox1@gmail.com>
2023-06-04 05:28:05 +0300

    dd

--- spec/models/reaction_category_spec.rb
+++ spec/models/reaction_category_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe ReactionCategory do
 
   it "lists all category slugs" do
     expect(described_class.all_slugs).to contain_exactly(*%i[exploding_head fire hands like
-                                                             cry_face readinglist thinking
+                                                             raised_hands readinglist thinking
                                                              thumbsdown thumbsup unicorn vomit])
   end
 
