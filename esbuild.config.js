c2570d67a8fcb6f9b7f1b0fbf6a806c13b87c820
dukegreene <duke@forem.com>
2023-06-06 14:20:58 -0400

    update config to avoid watch or context and build more simply

--- esbuild.config.js
+++ esbuild.config.js
@@ -4,23 +4,17 @@
 const glob = require('glob');
 const esbuild = require('esbuild');
 
-async () => {
-  const context = await esbuild.context({
-    entryPoints: glob.sync([
-      'app/javascript/packs/**/*.js',
-      'app/assets/javascripts/**/*.js',
-    ]),
+esbuild
+  .build({
+    entryPoints: glob.sync('app/javascript/packs/initializers/*.js'),
     bundle: true,
     sourcemap: true,
-    assetNames: '[name]-[hash].digested',
-    chunkNames: '[name]-[hash].digested',
-    logLevel: 'info',
-    splitting: true,
+    // assetNames: '[name]-[hash].digested',
+    // chunkNames: '[name]-[hash].digested',
+    // logLevel: 'info',
+    // splitting: true,
     outdir: 'app/assets/builds',
     loader: { '.erb': 'file', '.toml': 'file' },
-    publicPath: 'assets',
-  });
-
-  await context.watch();
-  await context.dispose();
-};
+    // publicPath: 'assets',
+  })
+  .catch(() => process.exit(1));
