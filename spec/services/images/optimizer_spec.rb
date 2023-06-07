b00c8541c05dc90df863d81c3ad6e7fc77865c98
Ben Halpern <bendhalpern@gmail.com>
2023-06-07 09:18:16 -0400

    Update spec/services/images/optimizer_spec.rb
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- spec/services/images/optimizer_spec.rb
+++ spec/services/images/optimizer_spec.rb
@@ -94,7 +94,7 @@ RSpec.describe Images::Optimizer, type: :service do
 
     it "pulls suffix if nested cloudflare url is provided" do
       cloudflare_url = described_class.cloudflare("https://#{ApplicationConfig["CLOUDFLARE_IMAGES_DOMAIN"]}/cdn-cgi/image/width=821,height=900,fit=cover,gravity=auto,format=auto/#{image_url}", width: 821, height: 420)
-      expect(cloudflare_url).to eq("https://#{ApplicationConfig["CLOUDFLARE_IMAGES_DOMAIN"]}/cdn-cgi/image/width=821,height=420,fit=cover,gravity=auto,format=auto/#{image_url}")
+      expect(cloudflare_url).to eq("https://#{ApplicationConfig['CLOUDFLARE_IMAGES_DOMAIN']}/cdn-cgi/image/width=821,height=420,fit=cover,gravity=auto,format=auto/#{image_url}")
     end
   end
 
