488702c81209af1f9bd3cbe7c1c088702be2a11c
dukegreene <duke@forem.com>
2023-06-07 12:15:35 -0400

    add loaders for svg and js, handle crayons externally

--- esbuild.config.js
+++ esbuild.config.js
@@ -6,15 +6,15 @@ const esbuild = require('esbuild');
 
 esbuild
   .build({
-    entryPoints: glob.sync('app/javascript/packs/initializers/*.js'),
+    entryPoints: glob.sync('app/javascript/packs/**/*.*'),
     bundle: true,
     sourcemap: true,
     // assetNames: '[name]-[hash].digested',
     // chunkNames: '[name]-[hash].digested',
-    // logLevel: 'info',
+    logLevel: 'info',
     // splitting: true,
     outdir: 'app/assets/builds',
-    loader: { '.erb': 'file', '.toml': 'file' },
-    // publicPath: 'assets',
+    loader: { '.js': 'jsx', '.erb': 'file', '.toml': 'file', '.svg': 'file' },
+    external: ['@crayons'],
   })
   .catch(() => process.exit(1));
