078f8142d604ca31cb79ba2513fe02ceb8c28d88
Joshua Wehner <joshua@forem.com>
2023-06-20 11:48:44 +0200

    See if this makes codecov happier?

--- app/javascript/onboarding/components/__tests__/FollowUsers.test.jsx
+++ app/javascript/onboarding/components/__tests__/FollowUsers.test.jsx
@@ -95,6 +95,34 @@ describe('FollowUsers', () => {
     expect(queryByText(/Continue/i)).toExist();
   });
 
+  it('should properly pluralize with small follower count', async () => {
+    fetch.mockResponseOnce(
+      JSON.stringify(JSON.parse(fakeUsersResponse).slice(-1)),
+    );
+
+    const { queryByText, findByText, findAllByLabelText, queryAllByLabelText } =
+      renderFollowUsers();
+
+    const selectedUsers = await findAllByLabelText('Following');
+    expect(selectedUsers).toHaveLength(1);
+    expect(queryByText(/Continue/i)).toExist();
+
+    // deselect all then test following count
+    const deselectAllSelector = await findByText(/Deselect all/i);
+
+    fireEvent.click(deselectAllSelector);
+
+    expect(queryAllByLabelText('Follow')).toHaveLength(1);
+    expect(queryByText('Following')).not.toExist();
+    expect(queryByText(/You're not following anyone/i)).toExist();
+
+    // select all then test following count
+    const followAllSelector = await findByText(/Select 1/i);
+
+    fireEvent.click(followAllSelector);
+    expect(queryByText(/You're following 1 person \(everyone\)/i)).toExist();
+  });
+
   it('should update the navigation button text and follow status when you follow/unfollow users', async () => {
     fetch.mockResponse(fakeUsersResponse);
 
