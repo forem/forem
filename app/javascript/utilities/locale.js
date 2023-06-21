832746ffbbb278614a5325b1fdd9b5cd667937bb
Joshua Wehner <joshua@forem.com>
2023-06-21 17:50:16 +0200

    instant locale load is the 'problem' here

--- app/javascript/utilities/locale.js
+++ app/javascript/utilities/locale.js
@@ -2,16 +2,30 @@ import { I18n } from 'i18n-js';
 
 const i18n = new I18n();
 
-const translationsDiv = document.getElementById('i18n-translations');
-if (translationsDiv) {
-  const translations = JSON.parse(translationsDiv.dataset.translations);
-  i18n.store(translations);
+i18n.translationsLoaded = false;
+
+i18n.ensureTranslationsAvailable = function() {
+  if (this.translationsLoaded) {
+    return;
+  }
+
+  const translationsDiv = document.getElementById('i18n-translations');
+
+  if (translationsDiv) {
+    const translations = JSON.parse(translationsDiv.dataset.translations);
+    i18n.store(translations);
+  }
+
+  i18n.defaultLocale = 'en';
+  const { locale: userLocale } = document.body.dataset;
+  if (userLocale) {
+    i18n.locale = userLocale;
+  }
+
+  i18n.translationsLoaded = true;
 }
-i18n.defaultLocale = 'en';
-const { locale: userLocale } = document.body.dataset;
-if (userLocale) {
-  i18n.locale = userLocale;
-}
+
 export function locale(term, params = {}) {
+  i18n.ensureTranslationsAvailable();
   return i18n.t(term, params);
 }
