ba2473b71e22069acae11ee372234159a7d27576
Ben Halpern <bendhalpern@gmail.com>
2023-06-23 09:01:28 -0400

    Add de-duping query

--- db/migrate/20230622151208_remove_existing_segmented_users.rb
+++ db/migrate/20230622151208_remove_existing_segmented_users.rb
@@ -1,6 +1,15 @@
 class RemoveExistingSegmentedUsers < ActiveRecord::Migration[7.0]
   def up
-    SegmentedUser.delete_all
+    sql = <<-SQL
+      DELETE FROM segmented_users deduped_seg_users
+      USING segmented_users current_seg_users
+      WHERE
+        deduped_seg_users.id < current_seg_users.id AND
+        deduped_seg_users.user_id = current_seg_users.user_id AND
+        deduped_seg_users.audience_segment_id = current_seg_users.audience_segment_id;
+    SQL
+
+    ActiveRecord::Base.connection.execute(sql)
   end
 
   def down
