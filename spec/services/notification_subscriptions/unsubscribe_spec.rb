d5b96fe0ad77dddb72606314bb9b9b2c31f74167
Joshua Wehner <joshua@forem.com>
2023-06-29 16:19:05 +0200

    Refactor Unsubscribe service specs

--- spec/services/notification_subscriptions/unsubscribe_spec.rb
+++ spec/services/notification_subscriptions/unsubscribe_spec.rb
@@ -6,52 +6,39 @@ RSpec.describe NotificationSubscriptions::Unsubscribe, type: :service do
   let(:article) { create(:article, user: current_user) }
   let!(:subscription) { create(:notification_subscription, user: current_user, notifiable: article) }
 
-  describe "#call" do
-    context "when a valid subscription ID is provided" do
-      let(:params) { { subscription_id: subscription.id } }
+  context "when a valid subscription ID is provided" do
+    it "destroys the notification subscription" do
+      result = nil # needs to exist before the block or Ruby won't set it
 
-      it "destroys the notification subscription" do
-        expect do
-          described_class.call(current_user, params)
-        end.to change(NotificationSubscription, :count).by(-1)
+      expect do
+        result = described_class.call(current_user, subscription.id)
+      end.to change(NotificationSubscription, :count).by(-1)
 
-        expect(NotificationSubscription.find_by(id: subscription.id)).to be_nil
-      end
+      expect(NotificationSubscription.find_by(id: subscription.id)).to be_nil
 
-      it "returns the destroyed status" do
-        result = described_class.call(current_user, params)
-        expect(result).to eq({ destroyed: true })
-      end
+      expect(result).to eq({ destroyed: true })
     end
+  end
 
-    context "when an invalid subscription ID is provided" do
-      let(:params) { { subscription_id: 9999 } }
+  context "when an invalid subscription ID is provided" do
+    it "does not destroy any notification subscriptions" do
+      result = nil # needs to exist before the block or Ruby won't set it
+      expect do
+        result = described_class.call(current_user, 9999)
+      end.not_to change(NotificationSubscription, :count)
 
-      it "does not destroy any notification subscriptions" do
-        expect do
-          described_class.call(current_user, params)
-        end.not_to change(NotificationSubscription, :count)
-      end
-
-      it "returns an error message" do
-        result = described_class.call(current_user, params)
-        expect(result).to eq({ errors: "Notification subscription not found" })
-      end
+      expect(result).to eq({ errors: "Notification subscription not found" })
     end
+  end
 
-    context "when no subscription ID is provided" do
-      let(:params) { {} }
+  context "when no subscription ID is provided" do
+    it "does not destroy any notification subscriptions" do
+      result = nil # needs to exist before the block or Ruby won't set it
+      expect do
+        result = described_class.call(current_user, nil)
+      end.not_to change(NotificationSubscription, :count)
 
-      it "does not destroy any notification subscriptions" do
-        expect do
-          described_class.call(current_user, params)
-        end.not_to change(NotificationSubscription, :count)
-      end
-
-      it "returns an error message" do
-        result = described_class.call(current_user, params)
-        expect(result).to eq({ errors: "Subscription ID is missing" })
-      end
+      expect(result).to eq({ errors: "Subscription ID is missing" })
     end
   end
 end
