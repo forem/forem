676645807ac60ec18eca6fbd2accee04fd72942b
rt4914 <rajattalesra4914@gmail.com>
2023-06-30 10:56:14 +0530

    Added reaction stats / summary

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -19,16 +19,38 @@ module Admin
                       .order(created_at: :desc)
                       .page(params[:page] || 1).per(50)
                   end
+      @countable_vomits = {}
+      @comments.each do |comment|
+        @countable_vomits[comment.id] = calculate_flags_for_single_comment(comment)
+      end
     end
 
     def show
       @comment = Comment.includes(:user, :commentable).find(params[:id])
+      @countable_vomits = {}
+      @countable_vomits[@comment.id] = calculate_flags_for_single_comment(@comment)
     end
 
     private
 
     def authorize_admin
       authorize Comment, :access?, policy_class: InternalPolicy
+    end
+
+    def calculate_flags_for_single_comment(comment)
+      privileged_comment_reactions = comment.reactions.privileged_category.select do |reaction|
+        reaction.reactable_type == "Comment"
+      end
+      vomit_comment_reactions = privileged_comment_reactions.select { |reaction| reaction.category == "vomit" }
+      vomit_count = 0
+
+      if vomit_comment_reactions.present?
+        vomit_comment_reactions.each do |reaction|
+          vomit_count += 1 if reaction.status != "invalid"
+        end
+      end
+
+      vomit_count
     end
 
     def skip_bullet
