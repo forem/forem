f02b26006f8074b78da6100aca70ead35aff588f
Joshua Wehner <joshua@forem.com>
2023-06-30 14:20:55 +0200

    Jest tests are just tests

--- app/javascript/packs/initializers/__tests__/subscribeButton.test.js
+++ app/javascript/packs/initializers/__tests__/subscribeButton.test.js
@@ -20,10 +20,12 @@ describe('subscribeButton', () => {
     const spanElement = document.createElement('span');
     spanElement.textContent = 'Subscribe to comments';
     button.appendChild(spanElement);
-    button.setAttribute(
-      'data-info',
-      '{"id":164,"user_id":11,"notifiable_id":32,"notifiable_type":"Article","config":"all_comments","created_at":"2023-06-09T05:07:47.272Z","updated_at":"2023-06-09T05:07:47.272Z"}',
-    );
+
+    // Baseline scenario starts Subscribed to all comments on an article
+    button.setAttribute('data-subscription_id','1');
+    button.setAttribute('data-subscribed_to','article');
+    button.setAttribute('data-subscription_mode','all_comments');
+    button.setAttribute('data-article_id','123');
 
     // Store the original fetch function
     originalFetch = global.fetch;
@@ -41,27 +43,38 @@ describe('subscribeButton', () => {
     global.fetch = originalFetch;
   });
 
-  // it('should initialize subscribe buttons', () => {
-  //   expect(button.getAttribute('aria-label')).toBe('Subscribed to comments');
-  //   expect(button.querySelector('span').innerText).toBe(
-  //     'Subscribed to comments',
-  //   );
-  // });
-
-  it('should set label, mobileLabel, and pressed for config "all_comments"', () => {
-    button.setAttribute('data-ancestry', null);
-    updateSubscribeButtonText(button, 'all_comments');
-
+  it('should initialize subscribe buttons', () => {
     expect(button.getAttribute('aria-label')).toBe('Subscribed to comments');
     expect(button.querySelector('span').innerText).toBe(
       'Subscribed to comments',
     );
-    expect(button.getAttribute('aria-pressed')).toBe('true');
   });
 
-  it('should set label, mobileLabel, and pressed for config "top_level_comments"', () => {
-    button.setAttribute('data-ancestry', null);
-    updateSubscribeButtonText(button, 'top_level_comments');
+  it('should update to unsubscribed setting label, mobileLabel, and *not* pressed', () => {
+    updateSubscribeButtonText(button, "unsubscribe");
+
+    expect(button.getAttribute('aria-label')).toBe('Subscribe to comments');
+    expect(button.querySelector('span').innerText).toBe(
+      'Subscribe to comments',
+    );
+    expect(button.getAttribute('aria-pressed')).toBe('false');
+  });
+
+  it('should update without override with blank subscription_id setting label, mobileLabel, and *not* pressed', () => {
+    button.setAttribute('data-subscription_id','');
+
+    updateSubscribeButtonText(button);
+
+    expect(button.getAttribute('aria-label')).toBe('Subscribe to comments');
+    expect(button.querySelector('span').innerText).toBe(
+      'Subscribe to comments',
+    );
+    expect(button.getAttribute('aria-pressed')).toBe('false');
+  });
+
+  it('"top_level_comments" scenario', () => {
+    button.setAttribute('data-subscription_config', 'top_level_comments');
+    updateSubscribeButtonText(button);
 
     expect(button.getAttribute('aria-label')).toBe(
       'Subscribed to top-level comments',
@@ -72,8 +85,9 @@ describe('subscribeButton', () => {
     expect(button.getAttribute('aria-pressed')).toBe('true');
   });
 
-  it('should set label, mobileLabel, and pressed for config "only_author_comments"', () => {
-    updateSubscribeButtonText(button, 'only_author_comments');
+  it('"only_author_comments" scenario', () => {
+    button.setAttribute('data-subscription_config', 'only_author_comments');
+    updateSubscribeButtonText(button);
 
     expect(button.getAttribute('aria-label')).toBe(
       'Subscribed to author comments',
