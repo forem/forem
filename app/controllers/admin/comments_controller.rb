c58b8807c76b119cdc3c920ec70de279e0beab9d
rt4914 <rajattalesra4914@gmail.com>
2023-07-01 13:15:10 +0530

    Applied changes as per PJ's review

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -7,15 +7,13 @@ module Admin
     def index
       @comments = if params[:state]&.start_with?("toplast-")
                     Comment
-                      .includes(:user)
-                      .includes(:commentable)
+                      .includes(:user, :commentable, :reactions)
                       .order(public_reactions_count: :desc)
                       .where("created_at > ?", params[:state].split("-").last.to_i.days.ago)
                       .page(params[:page] || 1).per(50)
                   else
                     Comment
-                      .includes(:user)
-                      .includes(:commentable)
+                      .includes(:user, :commentable, :reactions)
                       .order(created_at: :desc)
                       .page(params[:page] || 1).per(50)
                   end
@@ -26,7 +24,7 @@ module Admin
     end
 
     def show
-      @comment = Comment.includes(:user, :commentable).find(params[:id])
+      @comment = Comment.includes(:user, :commentable, :reactions).find(params[:id])
       @countable_vomits = {}
       @countable_vomits[@comment.id] = calculate_flags_for_single_comment(@comment)
     end
@@ -38,19 +36,9 @@ module Admin
     end
 
     def calculate_flags_for_single_comment(comment)
-      privileged_comment_reactions = comment.reactions.privileged_category.select do |reaction|
-        reaction.reactable_type == "Comment"
+      comment.reactions.privileged_category.count do |reaction|
+        reaction.category == "vomit" && reaction.status != "invalid"
       end
-      vomit_comment_reactions = privileged_comment_reactions.select { |reaction| reaction.category == "vomit" }
-      vomit_count = 0
-
-      if vomit_comment_reactions.present?
-        vomit_comment_reactions.each do |reaction|
-          vomit_count += 1 if reaction.status != "invalid"
-        end
-      end
-
-      vomit_count
     end
 
     def skip_bullet
