494d5bc4840904f130514af19ac2afee990e7ca3
Joshua Wehner <joshua@forem.com>
2023-07-06 11:07:47 +0200

    Try feature flag specific to cypress env

--- cypress/e2e/seededFlows/notificationsFlows/subscribeToCommentsFromNotifications.spec.js
+++ cypress/e2e/seededFlows/notificationsFlows/subscribeToCommentsFromNotifications.spec.js
@@ -9,33 +9,31 @@ describe('Subscribe to Comments from notifications', () => {
   });
 
   it('Subscribes and unsubscribes to comments from notification', () => {
-    // FeatureFlag makes this tricky to test with Cypress
-    // TODO: Uncomment this as part of cleanup for FeatureFlag :subscribe_to_comments
-    // cy.findByRole('heading', { name: 'Notifications' });
-    //
-    // cy.findByRole('button', { name: 'Subscribe to thread' }).as(
-    //   'subscribeButton',
-    // );
-    // cy.get('@subscribeButton').should('have.attr', 'aria-pressed', 'false');
-    // cy.get('@subscribeButton').should('have.attr', 'data-subscription_id', '');
-    // cy.get('@subscribeButton').click();
-    //
-    // cy.findByRole('button', { name: 'Subscribed to thread' }).as(
-    //   'subscribedButton',
-    // );
-    // cy.get('@subscribedButton').contains('Subscribed to thread');
-    // cy.get('@subscribedButton').should('have.attr', 'aria-pressed', 'true');
-    // cy.get('@subscribedButton').should(
-    //   'have.attr',
-    //   'aria-label',
-    //   'Subscribed to thread',
-    // );
-    //
-    // cy.get('@subscribedButton').click();
-    //
-    // cy.findByRole('button', { name: 'Subscribed to thread' }).as(
-    //   'subscribedButton',
-    // );
-    // cy.get('@subscribeButton').should('have.attr', 'data-subscription_id', '');
+    cy.findByRole('heading', { name: 'Notifications' });
+
+    cy.findByRole('button', { name: 'Subscribe to comments' }).as(
+      'subscribeButton',
+    );
+    cy.get('@subscribeButton').should('have.attr', 'aria-pressed', 'false');
+    cy.get('@subscribeButton').should('have.attr', 'data-subscription_id', '');
+    cy.get('@subscribeButton').click();
+
+    cy.findByRole('button', { name: 'Subscribed to comments' }).as(
+      'subscribedButton',
+    );
+    cy.get('@subscribedButton').contains('Subscribed to comments');
+    cy.get('@subscribedButton').should('have.attr', 'aria-pressed', 'true');
+    cy.get('@subscribedButton').should(
+      'have.attr',
+      'aria-label',
+      'Subscribed to comments',
+    );
+
+    cy.get('@subscribedButton').click();
+
+    cy.findByRole('button', { name: 'Subscribed to comments' }).as(
+      'subscribedButton',
+    );
+    cy.get('@subscribeButton').should('have.attr', 'data-subscription_id', '');
   });
 });
