8ebf1a0d7dcad16f71abbc91b54db27ad21e59cf
dukegreene <duke@forem.com>
2023-07-06 16:57:48 -0400

    move actions out of concerns/include pattern and into v1 inline

--- app/controllers/api/v1/admin/organizations_controller.rb
+++ app/controllers/api/v1/admin/organizations_controller.rb
@@ -2,10 +2,33 @@ module Api
   module V1
     module Admin
       class OrganizationsController < ApiController
-        include Api::Admin::OrganizationsController
-
         before_action :authenticate!
         before_action :authorize_super_admin
+
+        def create
+          Organization.create!(organization_params)
+
+          head :ok
+        end
+
+        def update
+          organization = Organization.find(params[:id])
+          organization.update!(organization_params)
+
+          head :ok
+        end
+
+        def destroy
+          Organization.find(params[:id])&.destroy
+
+          head :ok
+        end
+
+        private
+
+        def organization_params
+          params.require(:organization).permit %i[name profile_image slug summary tag_line url]
+        end
       end
     end
   end
--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -3,7 +3,25 @@ module Api
     class OrganizationsController < ApiController
       include Api::OrganizationsController
 
+      ATTRIBUTES_FOR_SERIALIZATION = %i[
+        id name profile_image slug summary tag_line url
+      ].freeze
+      private_constant ATTRIBUTES_FOR_SERIALIZATION
+
       before_action :find_organization, only: %i[users listings articles]
+
+      def index
+        per_page = (params[:per_page] || 30).to_i
+        num = [per_page, per_page_max].min
+        page = params[:page] || 1
+
+        @organization = Organization.select(ATTRIBUTES_FOR_SERIALIZATION).page(page).per(num)
+      end
+
+      def show
+        @organization = Organization.select(SHOW_ATTRIBUTES_FOR_SERIALIZATION)
+          .find_by!(username: params[:username])
+      end
     end
   end
 end
