d3d8362c2f23c267ef27166c8349726b348f3cec
Joshua Wehner <joshua@forem.com>
2023-07-11 14:35:11 +0200

    Delete cached notification views for article and comment

--- app/services/notifications/bust_caches.rb
+++ app/services/notifications/bust_caches.rb
@@ -19,11 +19,32 @@ module Notifications
     end
 
     def call
-      raise "not implemented"
+      Rails.cache.delete_matched(activity_published_article_key)
+      Rails.cache.delete_matched(comment_box_key)
     end
 
     def notifiable
       @notifiable ||= FINDERS.fetch(notifiable_type).find(notifiable_id)
+    end
+
+    def last_user_reaction
+      @last_user_reaction ||= @user.reactions.last&.id
+    end
+
+    def activity_published_article_key
+      return unless effected_article_id
+
+      "*activity-published-article-reactions-#{last_user_reaction}-*-#{effected_article_id}"
+    end
+
+    def comment_box_key
+      "*comment-box-#{last_user_reaction}-*"
+    end
+
+    def effected_article_id
+      return notifiable.id if notifiable.is_a?(Article)
+
+      notifiable.commentable_id if notifiable.commentable_type == "Article"
     end
   end
 end
