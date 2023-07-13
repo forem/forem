4c7b63f9abb45ed7ccbd8942e906f7df94e3ba52
Ben Halpern <bendhalpern@gmail.com>
2023-07-13 13:25:29 -0400

    Update spec/models/article_spec.rb
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1439,7 +1439,8 @@ RSpec.describe Article do
       end
 
       it "doesn't update the language if the probability is too low" do
-        allow_any_instance_of(CLD3::NNetLanguageIdentifier).to receive(:find_language).and_return(double("LanguageOutcome", probability: 0.4, reliable?: true, language: "en"))
+        allow_any_instance_of(CLD3::NNetLanguageIdentifier).to receive(:find_language).and_return(double(
+"LanguageOutcome", probability: 0.4, reliable?: true, language: "en"))
         article.update(title: "This post is so english you'd think it was eating fish and chips")
         expect(article.language).to eq("en")
       end
