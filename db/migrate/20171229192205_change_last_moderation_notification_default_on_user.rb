a27917b1c2c054d8f17816179a76a4fd019a02ff
Helio Cola <helio@tcard.io>
2023-07-17 05:28:44 -0400

    ruby: Upgrade to v3.1.4 (pt. 2, app image)
    Co-authored-with: Josh Klar <josh@klar.sh>

--- db/migrate/20171229192205_change_last_moderation_notification_default_on_user.rb
+++ db/migrate/20171229192205_change_last_moderation_notification_default_on_user.rb
@@ -1,9 +1,9 @@
 class ChangeLastModerationNotificationDefaultOnUser < ActiveRecord::Migration[5.1]
   def up
-    change_column_default :users, :last_moderation_notification, Time.new("2017-01-01")
+    change_column_default :users, :last_moderation_notification, Time.new(2017,1,1)
   end
 
   def down
-    change_column_default :users, :last_moderation_notification, Time.new("2017-01-01")
+    change_column_default :users, :last_moderation_notification, Time.new(2017,1,1)
   end
 end
