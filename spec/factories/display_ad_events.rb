4b9bc595ae9ff12a7716252953836c7f6ab52c9b
lightalloy <lightallloy@gmail.com>
2023-07-20 14:29:49 +0300

    Fixed display_ad_event factory

--- spec/factories/display_ad_events.rb
+++ spec/factories/display_ad_events.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :display_ad_event do
     category { DisplayAdEvent::CATEGORY_IMPRESSION }
     context_type { "home" }
-    billboard
+    association :billboard, factory: :display_ad
   end
 end
