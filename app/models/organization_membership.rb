30cd9a634e2a05a6e8888c6ff9076305d23b1532
rt4914 <rajattalesra4914@gmail.com>
2023-07-28 18:39:38 +0530

    Added worker

--- app/models/organization_membership.rb
+++ app/models/organization_membership.rb
@@ -14,6 +14,8 @@ class OrganizationMembership < ApplicationRecord
   after_create  :update_user_organization_info_updated_at
   after_destroy :update_user_organization_info_updated_at
 
+  after_commit :bust_cache
+
   scope :admin, -> { where(type_of_user: "admin") }
   scope :member, -> { where(type_of_user: %w[admin member]) }
 
@@ -22,6 +24,11 @@ class OrganizationMembership < ApplicationRecord
   #       :organization_memberships dependent: :delete_all`
   def update_user_organization_info_updated_at
     user.touch(:organization_info_updated_at)
-    organization.touch
+  end
+
+  private
+
+  def bust_cache
+    OrganizationMemberships::BustCacheWorker.perform_async(organization.id, organization.slug)
   end
 end
---
+++ app/workers/organization_memberships/bust_cache_worker.rb
@@ -0,0 +1,13 @@
+module OrganizationMemberships
+  class BustCacheWorker < BustCacheBaseWorker
+    def perform(organization_id, slug)
+      return unless organization_id && slug
+
+      organization = Organization.find_by(id: organization_id)
+
+      return unless organization
+
+      EdgeCache::BustOrganization.call(organization, slug)
+    end
+  end
+end
