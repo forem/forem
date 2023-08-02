b8ce5d06305431099da9b05e85adc702fda75425
Ben Halpern <bendhalpern@gmail.com>
2023-08-02 10:15:33 -0400

    Update app/services/images/generate_social_image_magickally.rb
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/services/images/generate_social_image_magickally.rb
+++ app/services/images/generate_social_image_magickally.rb
@@ -1,6 +1,6 @@
 module Images
   MEDIUM_FONT_PATH = Rails.root.join("app/assets/fonts/Roboto-Medium.ttf", "assets", "fonts", "Roboto-Medium.ttf").freeze
-  BOLD_FONT_PATH = Rails.root.join("app", "assets", "fonts", "Roboto-Bold.ttf").freeze
+  BOLD_FONT_PATH = Rails.root.join("app/assets/fonts/Roboto-Bold.ttf", "assets", "fonts", "Roboto-Bold.ttf").freeze
   TEMPLATE_PATH = Rails.root.join("app", "assets", "images", "social_template.png").freeze
   ROUNDED_MASK_PATH = Rails.root.join("app", "assets", "images", "rounded_mask.png").freeze
 
