d999e7f2f6781960651e586e9b92e01b0e9e02e0
Ridhwana <ridhwana.khan16@gmail.com>
2023-08-03 14:02:52 +0200

    feat: update points to explicit_points in the followings_controller

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -15,8 +15,11 @@ class FollowingsController < ApplicationController
   def tags
     relation = current_user.follows_by_type("ActsAsTaggableOn::Tag")
       .select(TAGS_ATTRIBUTES_FOR_SERIALIZATION)
-    relation =
-      params[:controller_action] == "hidden_tags" ? relation.where("points < 0") : relation.where("points > 0")
+    relation = if params[:controller_action] == "hidden_tags"
+                 relation.where("explicit_points < 0")
+               else
+                 relation.where("explicit_points > 0")
+               end
     relation.order(points: :desc)
     @followed_tags = load_follows_and_paginate(relation)
   end
