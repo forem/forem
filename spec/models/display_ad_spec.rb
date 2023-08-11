9c9663a5b5b60c2e2216152942ceba549c13a910
Anna Buianova <lightallloy@gmail.com>
2023-08-11 22:00:07 +0300

    Renamed display_ads to billboards: variables and descriptions in tests (#19920)
    * Renamed display ad variables and descriptions in tests
    
    * Renamed api docs display_ads => billboards
    
    * Updated billboard cypress seeds
    
    * Fixed api billboards specs
    
    * Update db/schema.rb
    
    Co-authored-by: Duke Greene <dukegreene@gmail.com>
    
    ---------
    
    Co-authored-by: Ben Halpern <bendhalpern@gmail.com>
    Co-authored-by: Duke Greene <dukegreene@gmail.com>

--- spec/models/display_ad_spec.rb
+++ spec/models/display_ad_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe DisplayAd do
   let(:organization) { build(:organization) }
-  let(:display_ad) { build(:display_ad, organization: nil) }
+  let(:billboard) { build(:billboard, organization: nil) }
   let(:audience_segment) { create(:audience_segment) }
 
   before { allow(FeatureFlag).to receive(:enabled?).with(:consistent_rendering, any_args).and_return(true) }
@@ -11,7 +11,7 @@ RSpec.describe DisplayAd do
 
   describe "validations" do
     describe "builtin validations" do
-      subject { display_ad }
+      subject { billboard }
 
       it { is_expected.to belong_to(:organization).optional }
       it { is_expected.to have_many(:billboard_events).dependent(:destroy) }
@@ -22,88 +22,88 @@ RSpec.describe DisplayAd do
     end
 
     it "allows sidebar_right" do
-      display_ad.placement_area = "sidebar_right"
-      expect(display_ad).to be_valid
+      billboard.placement_area = "sidebar_right"
+      expect(billboard).to be_valid
     end
 
     it "allows sidebar_left" do
-      display_ad.placement_area = "sidebar_left"
-      expect(display_ad).to be_valid
+      billboard.placement_area = "sidebar_left"
+      expect(billboard).to be_valid
     end
 
     it "allows home_hero with in_house" do
-      display_ad.placement_area = "home_hero"
-      display_ad.type_of = "in_house"
-      expect(display_ad).to be_valid
+      billboard.placement_area = "home_hero"
+      billboard.type_of = "in_house"
+      expect(billboard).to be_valid
     end
 
     it "does not allow home_hero with community" do
-      display_ad.placement_area = "home_hero"
-      display_ad.type_of = "community"
-      expect(display_ad).not_to be_valid
-      expect(display_ad.errors[:type_of])
+      billboard.placement_area = "home_hero"
+      billboard.type_of = "community"
+      expect(billboard).not_to be_valid
+      expect(billboard.errors[:type_of])
         .to include("must be in_house if billboard is a Home Hero")
     end
 
     it "disallows unacceptable placement_area" do
-      display_ad.placement_area = "tsdsdsdds"
-      expect(display_ad).not_to be_valid
+      billboard.placement_area = "tsdsdsdds"
+      expect(billboard).not_to be_valid
     end
 
     it "returns human readable name" do
-      display_ad.placement_area = "sidebar_left_2"
-      expect(display_ad.human_readable_placement_area).to eq("Sidebar Left (Second Position)")
+      billboard.placement_area = "sidebar_left_2"
+      expect(billboard.human_readable_placement_area).to eq("Sidebar Left (Second Position)")
     end
 
     it "allows creator_id to be set" do
-      display_ad.creator = build(:user)
-      expect(display_ad).to be_valid
+      billboard.creator = build(:user)
+      expect(billboard).to be_valid
     end
 
     it "does not require creator to be valid" do
-      display_ad.creator = nil
-      expect(display_ad).to be_valid
+      billboard.creator = nil
+      expect(billboard).to be_valid
     end
 
     it "requires organization_id for community-type ads" do
-      expect(display_ad).to be_valid
+      expect(billboard).to be_valid
 
-      display_ad.type_of = "community"
-      expect(display_ad).not_to be_valid
-      expect(display_ad.errors[:organization]).not_to be_blank
+      billboard.type_of = "community"
+      expect(billboard).not_to be_valid
+      expect(billboard.errors[:organization]).not_to be_blank
 
-      display_ad.organization = organization
-      expect(display_ad).to be_valid
-      expect(display_ad.errors[:organization]).to be_blank
+      billboard.organization = organization
+      expect(billboard).to be_valid
+      expect(billboard.errors[:organization]).to be_blank
     end
 
     it "allows clear audience segment" do
-      display_ad.audience_segment_id = audience_segment.id
-      expect(display_ad).to be_valid
+      billboard.audience_segment_id = audience_segment.id
+      expect(billboard).to be_valid
 
-      display_ad.audience_segment_id = audience_segment.id
-      display_ad.audience_segment_type = audience_segment.type_of
-      expect(display_ad).to be_valid
+      billboard.audience_segment_id = audience_segment.id
+      billboard.audience_segment_type = audience_segment.type_of
+      expect(billboard).to be_valid
     end
 
     it "disallows invalid audience segment" do
-      display_ad.audience_segment_id = audience_segment.id
-      display_ad.audience_segment_type = "something_invalid_here"
+      billboard.audience_segment_id = audience_segment.id
+      billboard.audience_segment_type = "something_invalid_here"
       expect(audience_segment.type_of).not_to eq("something_invalid_here")
-      expect(display_ad).not_to be_valid
+      expect(billboard).not_to be_valid
     end
 
     it "disallows valid but ambiguous audience segment & id mismatch" do
-      display_ad.audience_segment_id = audience_segment.id
-      display_ad.audience_segment_type = "posted"
+      billboard.audience_segment_id = audience_segment.id
+      billboard.audience_segment_type = "posted"
       expect(audience_segment.type_of).not_to eq("posted")
-      expect(display_ad).not_to be_valid
+      expect(billboard).not_to be_valid
     end
 
     it "disallows valid but imprecise manual audience segment type" do
-      display_ad.audience_segment = nil
-      display_ad.audience_segment_type = "manual"
-      expect(display_ad).not_to be_valid
+      billboard.audience_segment = nil
+      billboard.audience_segment_type = "manual"
+      expect(billboard).not_to be_valid
     end
   end
 
@@ -112,35 +112,35 @@ RSpec.describe DisplayAd do
       user = create(:user)
       url = "#{URL.url}/#{user.username}"
       allow(UnifiedEmbed::Tag).to receive(:validate_link).with(any_args).and_return(url)
-      username_ad = create(:display_ad, body_markdown: "Hello! {% embed #{url}} %}")
+      username_ad = create(:billboard, body_markdown: "Hello! {% embed #{url}} %}")
       expect(username_ad.processed_html).to include("/#{user.username}")
       expect(username_ad.processed_html).to include("ltag__user__link")
     end
   end
 
   context "when callbacks are triggered before save" do
-    before { display_ad.save! }
+    before { billboard.save! }
 
     it "generates #processed_html from #body_markdown" do
-      expect(display_ad.processed_html).to start_with("<p>Hello <em>hey</em> Hey hey")
+      expect(billboard.processed_html).to start_with("<p>Hello <em>hey</em> Hey hey")
     end
 
     it "does not render <div>" do
       div_html = "<div>Good morning, how are you?</div>"
       p_html = "<p>Good morning, how are you?</p>"
-      display_ad.update(body_markdown: div_html)
-      display_ad.reload
-      expect(display_ad.processed_html).to eq(p_html)
+      billboard.update(body_markdown: div_html)
+      billboard.reload
+      expect(billboard.processed_html).to eq(p_html)
     end
 
     it "does not render disallowed tags" do
-      display_ad.update(body_markdown: "<style>body { color: black}</style> Hey hey")
-      expect(display_ad.processed_html).to eq("<p>body { color: black} Hey hey</p>")
+      billboard.update(body_markdown: "<style>body { color: black}</style> Hey hey")
+      expect(billboard.processed_html).to eq("<p>body { color: black} Hey hey</p>")
     end
 
     it "does not render disallowed attributes" do
-      display_ad.update(body_markdown: "<p style='margin-top:100px'>Hello <em>hey</em> Hey hey</p>")
-      expect(display_ad.processed_html).to start_with("<p>Hello <em>hey</em> Hey hey</p>")
+      billboard.update(body_markdown: "<p style='margin-top:100px'>Hello <em>hey</em> Hey hey</p>")
+      expect(billboard.processed_html).to start_with("<p>Hello <em>hey</em> Hey hey</p>")
     end
   end
 
@@ -154,9 +154,9 @@ RSpec.describe DisplayAd do
       allow(FastImage).to receive(:size)
       allow(Images::Optimizer).to receive(:call).and_return(image_url)
       image_md = "![Image description](#{image_url})<p style='margin-top:100px'>Hello <em>hey</em> Hey hey</p>"
-      create(:display_ad, body_markdown: image_md, placement_area: "post_comments")
+      create(:billboard, body_markdown: image_md, placement_area: "post_comments")
       expect(FastImage).to have_received(:size).with(image_url, { timeout: 10 })
-      # width is display_ad.prefix_width
+      # width is billboard.prefix_width
       expect(Images::Optimizer).to have_received(:call).with(image_url, width: DisplayAd::POST_WIDTH)
       # Images::Optimizer.call(source, width: width)
     end
@@ -166,7 +166,7 @@ RSpec.describe DisplayAd do
       allow(FastImage).to receive(:size)
       allow(Images::Optimizer).to receive(:call).and_return(image_url)
       image_md = "![Image description](#{image_url})<p style='margin-top:100px'>Hello <em>hey</em> Hey hey</p>"
-      create(:display_ad, body_markdown: image_md, placement_area: "post_sidebar")
+      create(:billboard, body_markdown: image_md, placement_area: "post_sidebar")
       expect(Images::Optimizer).to have_received(:call).with(image_url, width: DisplayAd::SIDEBAR_WIDTH)
     end
 
@@ -175,47 +175,49 @@ RSpec.describe DisplayAd do
       allow(FastImage).to receive(:size)
       allow(Images::Optimizer).to receive(:call).and_return(image_url)
       image_md = "![Image description](#{image_url})<p style='margin-top:100px'>Hello <em>hey</em> Hey hey</p>"
-      create(:display_ad, body_markdown: image_md, placement_area: "feed_second")
+      create(:billboard, body_markdown: image_md, placement_area: "feed_second")
       expect(Images::Optimizer).to have_received(:call).with(image_url, width: DisplayAd::POST_WIDTH)
     end
 
     it "keeps the same processed_html if markdown was not changed" do
-      display_ad = create(:display_ad)
-      html = display_ad.processed_html
-      display_ad.update(name: "Sample billboard")
-      display_ad.reload
-      expect(display_ad.processed_html).to eq(html)
+      billboard = create(:billboard)
+      html = billboard.processed_html
+      billboard.update(name: "Sample billboard")
+      billboard.reload
+      expect(billboard.processed_html).to eq(html)
     end
   end
 
   describe "after_save callbacks" do
-    let!(:display_ad) { create(:display_ad, name: nil) }
+    let!(:billboard) { create(:billboard, name: nil) }
 
     it "generates a name when one does not exist" do
-      display_ad_with_name = create(:display_ad, name: "Test")
+      billboard_with_name = create(:billboard, name: "Test")
 
-      expect(display_ad.name).to eq("Billboard #{display_ad.id}")
-      expect(display_ad_with_name.name).to eq("Test")
+      expect(billboard.name).to eq("Billboard #{billboard.id}")
+      expect(billboard_with_name.name).to eq("Test")
     end
   end
 
   describe ".search_ads" do
-    let!(:ad) { create(:display_ad, name: "This is an Ad", body_markdown: "Ad Body", placement_area: "post_comments") }
+    let!(:billboard) do
+      create(:billboard, name: "This is a billboard", body_markdown: "Billboard Body", placement_area: "post_comments")
+    end
 
     it "finds via name" do
-      expect(described_class.search_ads("this").ids).to contain_exactly(ad.id)
+      expect(described_class.search_ads("this").ids).to contain_exactly(billboard.id)
     end
 
     it "finds via body" do
-      expect(described_class.search_ads("body").ids).to contain_exactly(ad.id)
+      expect(described_class.search_ads("body").ids).to contain_exactly(billboard.id)
     end
 
     it "finds via placement_area" do
-      expect(described_class.search_ads("comment").ids).to contain_exactly(ad.id)
+      expect(described_class.search_ads("comment").ids).to contain_exactly(billboard.id)
     end
 
     it "finds just one result via multiple criteria" do
-      expect(described_class.search_ads("ad").ids).to contain_exactly(ad.id)
+      expect(described_class.search_ads("billboard").ids).to contain_exactly(billboard.id)
     end
 
     it "returns empty when no match" do
@@ -226,27 +228,27 @@ RSpec.describe DisplayAd do
   describe ".validate_tag" do
     it "rejects more than 10 tags" do
       eleven_tags = "one, two, three, four, five, six, seven, eight, nine, ten, eleven"
-      expect(build(:display_ad,
-                   name: "This is an Ad",
-                   body_markdown: "Ad Body",
+      expect(build(:billboard,
+                   name: "This is a billboard",
+                   body_markdown: "Billboard Body",
                    placement_area: "post_comments",
                    tag_list: eleven_tags).valid?).to be(false)
     end
 
     it "rejects tags with length > 30" do
       tags = "'testing tag length with more than 30 chars', tag"
-      expect(build(:display_ad,
-                   name: "This is an Ad",
-                   body_markdown: "Ad Body",
+      expect(build(:billboard,
+                   name: "This is a billboard",
+                   body_markdown: "Billboard Body",
                    placement_area: "post_comments",
                    tag_list: tags).valid?).to be(false)
     end
 
     it "rejects tag with non-alphanumerics" do
       expect do
-        build(:display_ad,
-              name: "This is an Ad",
-              body_markdown: "Ad Body",
+        build(:billboard,
+              name: "This is a billboard",
+              body_markdown: "Billboard Body",
               placement_area: "post_comments",
               tag_list: "c++").validate!
       end.to raise_error(ActiveRecord::RecordInvalid)
@@ -254,21 +256,20 @@ RSpec.describe DisplayAd do
 
     it "always downcase tags" do
       tags = "UPPERCASE, CAPITALIZE"
-      display_ad = create(:display_ad,
-                          name: "This is an Ad",
-                          body_markdown: "Ad Body",
-                          placement_area: "post_comments",
-                          tag_list: tags)
-      expect(display_ad.tag_list).to eq(tags.downcase.split(", "))
+      billboard = create(:billboard, name: "This is a billboard",
+                                     body_markdown: "Billboard Body",
+                                     placement_area: "post_comments",
+                                     tag_list: tags)
+      expect(billboard.tag_list).to eq(tags.downcase.split(", "))
     end
   end
 
   describe ".validate_geolocations" do
     subject(:billboard) do
       build(
-        :display_ad,
-        name: "This is an Ad",
-        body_markdown: "Ad Body",
+        :billboard,
+        name: "This is a billboard",
+        body_markdown: "Billboard Body",
         placement_area: "post_comments",
         target_geolocations: geo_input,
       )
@@ -357,41 +358,41 @@ RSpec.describe DisplayAd do
 
   describe "#exclude_articles_ids" do
     it "processes array of integer ids as expected" do
-      display_ad.exclude_article_ids = ["11"]
-      expect(display_ad.exclude_article_ids).to contain_exactly(11)
+      billboard.exclude_article_ids = ["11"]
+      expect(billboard.exclude_article_ids).to contain_exactly(11)
 
-      display_ad.exclude_article_ids = %w[11 12 13 14]
-      expect(display_ad.exclude_article_ids).to contain_exactly(11, 12, 13, 14)
+      billboard.exclude_article_ids = %w[11 12 13 14]
+      expect(billboard.exclude_article_ids).to contain_exactly(11, 12, 13, 14)
 
-      display_ad.exclude_article_ids = "11,12,13,14"
-      expect(display_ad.exclude_article_ids).to contain_exactly(11, 12, 13, 14)
+      billboard.exclude_article_ids = "11,12,13,14"
+      expect(billboard.exclude_article_ids).to contain_exactly(11, 12, 13, 14)
 
-      display_ad.exclude_article_ids = ""
-      expect(display_ad.exclude_article_ids).to eq([])
+      billboard.exclude_article_ids = ""
+      expect(billboard.exclude_article_ids).to eq([])
 
-      display_ad.exclude_article_ids = []
-      expect(display_ad.exclude_article_ids).to eq([])
+      billboard.exclude_article_ids = []
+      expect(billboard.exclude_article_ids).to eq([])
 
-      display_ad.exclude_article_ids = ["", "", ""]
-      expect(display_ad.exclude_article_ids).to eq([])
+      billboard.exclude_article_ids = ["", "", ""]
+      expect(billboard.exclude_article_ids).to eq([])
 
-      display_ad.exclude_article_ids = [nil]
-      expect(display_ad.exclude_article_ids).to eq([])
+      billboard.exclude_article_ids = [nil]
+      expect(billboard.exclude_article_ids).to eq([])
 
-      display_ad.exclude_article_ids = nil
-      expect(display_ad.exclude_article_ids).to eq([])
+      billboard.exclude_article_ids = nil
+      expect(billboard.exclude_article_ids).to eq([])
     end
 
     it "round-trips to the database as expected" do
-      display_ad.exclude_article_ids = [11]
-      display_ad.save!
-      expect(display_ad.exclude_article_ids).to contain_exactly(11)
+      billboard.exclude_article_ids = [11]
+      billboard.save!
+      expect(billboard.exclude_article_ids).to contain_exactly(11)
 
-      display_ad.update(exclude_article_ids: "11,12,13,14")
-      expect(display_ad.exclude_article_ids).to contain_exactly(11, 12, 13, 14)
+      billboard.update(exclude_article_ids: "11,12,13,14")
+      expect(billboard.exclude_article_ids).to contain_exactly(11, 12, 13, 14)
 
-      display_ad.update(exclude_article_ids: nil)
-      expect(display_ad.exclude_article_ids).to eq([])
+      billboard.update(exclude_article_ids: nil)
+      expect(billboard.exclude_article_ids).to eq([])
     end
   end
 
@@ -405,10 +406,10 @@ RSpec.describe DisplayAd do
     before { allow(AudienceSegmentRefreshWorker).to receive(:perform_async) }
 
     it "refreshes audience segment as an asynchronous callback" do
-      display_ad.save!
+      billboard.save!
       expect(AudienceSegmentRefreshWorker).not_to have_received(:perform_async)
 
-      display_ad.update audience_segment: audience_segment
+      billboard.update audience_segment: audience_segment
       expect(AudienceSegmentRefreshWorker).to have_received(:perform_async)
         .with(audience_segment.id)
     end
@@ -420,17 +421,17 @@ RSpec.describe DisplayAd do
     before { allow(AudienceSegmentRefreshWorker).to receive(:perform_async) }
 
     it "does not need to refresh audience segment" do
-      display_ad.update audience_segment: audience_segment
+      billboard.update audience_segment: audience_segment
       expect(AudienceSegmentRefreshWorker).not_to have_received(:perform_async)
     end
   end
 
   describe "seldom_seen scope" do
     let(:low_impression_count) { DisplayAd::LOW_IMPRESSION_COUNT }
-    let!(:low_impression_ad) { create(:display_ad, impressions_count: low_impression_count - 1) }
-    let!(:high_impression_ad) { create(:display_ad, impressions_count: low_impression_count + 1) }
+    let!(:low_impression_ad) { create(:billboard, impressions_count: low_impression_count - 1) }
+    let!(:high_impression_ad) { create(:billboard, impressions_count: low_impression_count + 1) }
 
-    before { create(:display_ad, priority: true, impressions_count: low_impression_count + 1) }
+    before { create(:billboard, priority: true, impressions_count: low_impression_count + 1) }
 
     it "includes ads with impressions count less than LOW_IMPRESSION_COUNT" do
       expect(described_class.seldom_seen("sidebar_left")).to include(low_impression_ad)
--- spec/requests/billboard_events_spec.rb
+++ spec/requests/billboard_events_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "BillboardEvents" do
   let(:user) { create(:user, :trusted) }
   let(:organization) { create(:organization) }
-  let(:display_ad) { create(:display_ad, organization_id: organization.id) }
+  let(:billboard) { create(:billboard, organization_id: organization.id) }
 
   describe "POST /billboard_events", throttled_call: true do
     context "when user signed in" do
@@ -14,38 +14,38 @@ RSpec.describe "BillboardEvents" do
       it "creates a display ad click event" do
         post "/billboard_events", params: {
           billboard_event: {
-            billboard_id: display_ad.id,
+            billboard_id: billboard.id,
             context_type: BillboardEvent::CONTEXT_TYPE_HOME,
             category: BillboardEvent::CATEGORY_CLICK
           }
         }
-        expect(display_ad.reload.clicks_count).to eq(1)
+        expect(billboard.reload.clicks_count).to eq(1)
       end
 
       it "creates a display ad click event with old params" do
         post "/billboard_events", params: {
           display_ad_event: {
-            display_ad_id: display_ad.id,
+            display_ad_id: billboard.id,
             context_type: BillboardEvent::CONTEXT_TYPE_HOME,
             category: BillboardEvent::CATEGORY_CLICK
           }
         }
-        expect(display_ad.reload.clicks_count).to eq(1)
+        expect(billboard.reload.clicks_count).to eq(1)
       end
 
       it "creates a display ad impression event" do
         post "/billboard_events", params: {
           billboard_event: {
-            billboard_id: display_ad.id,
+            billboard_id: billboard.id,
             context_type: BillboardEvent::CONTEXT_TYPE_HOME,
             category: BillboardEvent::CATEGORY_IMPRESSION
           }
         }
-        expect(display_ad.reload.impressions_count).to eq(1)
+        expect(billboard.reload.impressions_count).to eq(1)
       end
 
       it "creates a display ad success rate" do
-        ad_event_params = { billboard_id: display_ad.id, context_type: BillboardEvent::CONTEXT_TYPE_HOME }
+        ad_event_params = { billboard_id: billboard.id, context_type: BillboardEvent::CONTEXT_TYPE_HOME }
         impression_params = ad_event_params.merge(category: BillboardEvent::CATEGORY_IMPRESSION, user: user)
         create_list(:billboard_event, 4, impression_params)
 
@@ -54,13 +54,13 @@ RSpec.describe "BillboardEvents" do
           params: { billboard_event: ad_event_params.merge(category: BillboardEvent::CATEGORY_CLICK) },
         )
 
-        expect(display_ad.reload.success_rate).to eq(0.25)
+        expect(billboard.reload.success_rate).to eq(0.25)
       end
 
       it "assigns event to current user" do
         post "/billboard_events", params: {
           billboard_event: {
-            billboard_id: display_ad.id,
+            billboard_id: billboard.id,
             context_type: BillboardEvent::CONTEXT_TYPE_HOME,
             category: BillboardEvent::CATEGORY_IMPRESSION
           }
@@ -71,14 +71,14 @@ RSpec.describe "BillboardEvents" do
       it "uses a ThrottledCall for data updates" do
         post "/billboard_events", params: {
           billboard_event: {
-            billboard_id: display_ad.id,
+            billboard_id: billboard.id,
             context_type: BillboardEvent::CONTEXT_TYPE_HOME,
             category: BillboardEvent::CATEGORY_IMPRESSION
           }
         }
 
         expect(ThrottledCall).to have_received(:perform)
-          .with("billboards_data_update-#{display_ad.id}", throttle_for: instance_of(ActiveSupport::Duration))
+          .with("billboards_data_update-#{billboard.id}", throttle_for: instance_of(ActiveSupport::Duration))
       end
     end
   end
