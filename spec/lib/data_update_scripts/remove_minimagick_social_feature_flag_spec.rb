24ceeb78f28bf7f6173f02b931eaf886748c0cff
Ben Halpern <bendhalpern@gmail.com>
2023-08-17 12:28:04 -0400

    Remove unnecessary file

--- spec/lib/data_update_scripts/remove_minimagick_social_feature_flag_spec.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-require Rails.root.join(
-  "lib/data_update_scripts/20230817161811_remove_minimagick_social_feature_flag.rb",
-)
-
-describe DataUpdateScripts::RemoveMinimagickSocialFeatureFlag do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
