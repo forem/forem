b8987528b49f8cfb45d4f7f13ccaddb8e0425499
Mac Siri <krairit.siri@gmail.com>
2023-09-06 21:20:28 -0400

    Change onCreateCommand ordering

--- .devcontainer/devcontainer-init.sh
+++ .devcontainer/devcontainer-init.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if [ -n "$CODESPACE" ]; then
+if [ -n "$CODESPACE_NAME" ]; then
     # CODESPACE is set, perform your file changes here
     echo "CODESPACE environment variable is set. Performing file changes..."
 
     cp .env_sample .env
-    echo "APP_DOMAIN=\"$CODESPACE-3000.app.github.dev\"" >> .env
+    echo "APP_DOMAIN=\"$CODESPACE_NAME-3000.app.github.dev\"" >> .env
     echo 'APP_PROTOCOL="https://"' >> .env
     echo "File changes completed."
 else
