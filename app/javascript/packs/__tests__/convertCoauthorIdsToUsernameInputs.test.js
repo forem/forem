7211517092763827ad3b57d95454970e87129d7c
Joshua Wehner <joshua.wehner@gmail.com>
2023-09-07 10:48:00 +0200

    Coninuing to try to bump coverage

--- app/javascript/packs/__tests__/convertCoauthorIdsToUsernameInputs.test.js
+++ app/javascript/packs/__tests__/convertCoauthorIdsToUsernameInputs.test.js
@@ -20,68 +20,143 @@ describe('convertCoauthorIdsToUsernameInputs', () => {
 
     fetch.resetMocks();
     fetch.mockResponse(fakeFetchResponseJSON());
+  });
 
-    window.document.body.innerHTML = `
-    <form method="post">
-      <div class="crayons-field mb-4">
-        <label for="user_id">Author</label>
-        <select class="crayons-select" name="article[user_id]" id="article_user_id">
-          <option selected="selected" value="1">
-            Alice
-          </option>
-          <option value="2">
-            Bob
-          </option>
-          <option value="3">
-            Charlie
-          </option>
-        </select>
-      </div>
-
-      <div class="crayons-field mb-4">
-        <label for="co_author_ids">Co-authors</label>
-        <div class="crayons-field">
-          <input id="article_ID_co_author_ids_list"
-                 class="article_org_co_author_ids_list"
-                 name="article[co_author_ids_list]"
-                 data-fetch-users="/org7053/members.json"
-                 type="text"
-                 value="3" >
+  describe('when there is no pre-existing id value', () => {
+    beforeEach(() => {
+      window.document.body.innerHTML = `
+      <form method="post">
+        <div class="crayons-field mb-4">
+          <label for="user_id">Author</label>
+          <select class="crayons-select" name="article[user_id]" id="article_user_id">
+            <option selected="selected" value="1">
+              Alice
+            </option>
+            <option value="2">
+              Bob
+            </option>
+            <option value="3">
+              Charlie
+            </option>
+          </select>
         </div>
-      </div>
 
-      <button type="submit" class="crayons-btn">Save</button>
-    </form>
-    `;
+        <div class="crayons-field mb-4">
+          <label for="co_author_ids">Co-authors</label>
+          <div class="crayons-field">
+            <input id="article_ID_co_author_ids_list"
+                   class="article_org_co_author_ids_list"
+                   name="article[co_author_ids_list]"
+                   data-fetch-users="/org7053/members.json"
+                   type="text"
+                   value="" >
+          </div>
+        </div>
+
+        <button type="submit" class="crayons-btn">Save</button>
+      </form>
+      `;
+    });
+
+    it('calls fetch, with exception for author ID', async () => {
+      await convertCoauthorIdsToUsernameInputs();
+      expect(fetch).toHaveBeenCalledWith('/org7053/members.json');
+    });
+
+    it('makes the co-author field hidden', async () => {
+      await convertCoauthorIdsToUsernameInputs();
+      const co_author_field = document.getElementById(
+        'article_ID_co_author_ids_list',
+      );
+      expect(co_author_field.type).toBe('hidden');
+    });
+
+    it('renders matching snapshot', async () => {
+      await convertCoauthorIdsToUsernameInputs();
+      expect(document.forms[0].innerHTML).toMatchSnapshot();
+    });
+
+    it('works as expected', async () => {
+      await convertCoauthorIdsToUsernameInputs();
+      const input = document.querySelector(
+        "input[placeholder='Add up to 4...']",
+      );
+      input.focus();
+      await userEvent.type(input, 'Bob,');
+
+      const hiddenField = document.querySelector(
+        "input[name='article[co_author_ids_list]']",
+      );
+      expect(hiddenField.value).toBe('2');
+    });
   });
 
-  it('calls fetch, with exception for author ID', async () => {
-    await convertCoauthorIdsToUsernameInputs();
-    expect(fetch).toHaveBeenCalledWith('/org7053/members.json');
-  });
+  describe('when there *is* a pre-existing id value', () => {
+    beforeEach(() => {
+      window.document.body.innerHTML = `
+      <form method="post">
+        <div class="crayons-field mb-4">
+          <label for="user_id">Author</label>
+          <select class="crayons-select" name="article[user_id]" id="article_user_id">
+            <option selected="selected" value="1">
+              Alice
+            </option>
+            <option value="2">
+              Bob
+            </option>
+            <option value="3">
+              Charlie
+            </option>
+          </select>
+        </div>
 
-  it('makes the co-author field hidden', async () => {
-    await convertCoauthorIdsToUsernameInputs();
-    const co_author_field = document.getElementById(
-      'article_ID_co_author_ids_list',
-    );
-    expect(co_author_field.type).toBe('hidden');
-  });
+        <div class="crayons-field mb-4">
+          <label for="co_author_ids">Co-authors</label>
+          <div class="crayons-field">
+            <input id="article_ID_co_author_ids_list"
+                   class="article_org_co_author_ids_list"
+                   name="article[co_author_ids_list]"
+                   data-fetch-users="/org7053/members.json"
+                   type="text"
+                   value="3" >
+          </div>
+        </div>
 
-  it('render matching snapshot', async () => {
-    await convertCoauthorIdsToUsernameInputs();
-    expect(document.forms[0].innerHTML).toMatchSnapshot();
-  });
+        <button type="submit" class="crayons-btn">Save</button>
+      </form>
+      `;
+    });
 
-  it('works as expected', async () => {
-    await convertCoauthorIdsToUsernameInputs();
-    const input = document.querySelector("input[placeholder='Add another...']");
-    input.focus();
-    await userEvent.type(input, 'Bob,');
+    it('renders matching snapshot', async () => {
+      await convertCoauthorIdsToUsernameInputs();
+      expect(document.forms[0].innerHTML).toMatchSnapshot();
+    });
 
-    const hiddenField = document.querySelector(
-      "input[name='article[co_author_ids_list]']",
-    );
-    expect(hiddenField.value).toBe('3, 2');
+    it('works as expected', async () => {
+      await convertCoauthorIdsToUsernameInputs();
+      const input = document.querySelector(
+        "input[placeholder='Add another...']",
+      );
+      input.focus();
+      await userEvent.type(input, 'Bob,');
+
+      const hiddenField = document.querySelector(
+        "input[name='article[co_author_ids_list]']",
+      );
+      expect(hiddenField.value).toBe('3, 2');
+    });
+
+    it('can remove previously selected', async () => {
+      await convertCoauthorIdsToUsernameInputs();
+      const deselect = document.querySelector(
+        '.c-autocomplete--multi__selected',
+      );
+      await userEvent.click(deselect);
+
+      const hiddenField = document.querySelector(
+        "input[name='article[co_author_ids_list]']",
+      );
+      expect(hiddenField.value).toBe('');
+    });
   });
 });
