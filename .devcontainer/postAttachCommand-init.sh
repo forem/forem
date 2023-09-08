15a0abd225508c0b30c235c304dd15627e7f8dae
Mac Siri <mac@forem.com>
2023-09-08 13:10:21 +0000

    Add mandatory git pull script

---
+++ .devcontainer/postAttachCommand-init.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# For some reason, Codespace prebuild also caches the codebase
+# at the time of prebuild. This means that if we don't run prebuild
+# on every single commit, the loaded codespace will not be on latest of
+# the chosen branch. This mitigates that.
+#
+# See https://github.com/orgs/community/discussions/58172
+if [ -n "$CODESPACE_NAME" ]; then
+    git pull
+    dip provision
+fi
