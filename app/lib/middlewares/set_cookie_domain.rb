28ea5e6e126eb1fd6de13b5bf02a6a2154f9d859
lightalloy <lightallloy@gmail.com>
2023-09-08 14:25:28 +0300

    Replace SiteConfig with Settings::General in the comments

--- app/lib/middlewares/set_cookie_domain.rb
+++ app/lib/middlewares/set_cookie_domain.rb
@@ -1,6 +1,6 @@
 module Middlewares
-  # Since we must explicitly set the cookie domain in session_store before SiteConfig is available,
-  # this ensures we properly set the cookie to SiteConfig.app_domain at runtime.
+  # Since we must explicitly set the cookie domain in session_store before Settings::General is available,
+  # this ensures we properly set the cookie to Settings::General.app_domain at runtime.
   class SetCookieDomain
     def initialize(app)
       @app = app
