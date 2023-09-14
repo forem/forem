612c4028871e54d86abc89a1160ce2c01921212f
Ben Halpern <bendhalpern@gmail.com>
2023-09-14 16:02:34 -0400

    Fix onboarding with implicit terms acceptance (#20117)

--- app/controllers/profile_field_groups_controller.rb
+++ app/controllers/profile_field_groups_controller.rb
@@ -2,6 +2,13 @@ class ProfileFieldGroupsController < ApplicationController
   def index
     relation = ProfileFieldGroup.includes(:profile_fields)
     @profile_field_groups = onboarding? ? relation.onboarding : relation.all
+    if onboarding?
+      current_user.update(
+        saw_onboarding: true,
+        checked_code_of_conduct: true,
+        checked_terms_and_conditions: true,
+      )
+    end
   end
 
   private
