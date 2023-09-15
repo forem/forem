aa201060e8c57c3756fe7e805d5bc157479bd5ba
Ridhwana <Ridhwana.Khan16@gmail.com>
2023-09-15 15:06:58 +0200

    Override Tag Moderation Actions (#20088)
    * feat: remove the validation for the article_id and tag_name to be unique on a tag_adjustment
    
    * chore: remove unique index
    
    * feat: update the validate_tag method to align with the removal of unique indexes on the table
    
    * feat: do not allow removal and addition of tags that were added/removed by admins
    
    * spec: add tests for the tag_adjustment_spec
    
    * fix: update the validation of tags
    
    * spec: article validation
    
    * spec: test the action_panel helper
    
    * chore: amend language
    
    * add styles
    
    * Empty commit

--- spec/models/tag_adjustment_spec.rb
+++ spec/models/tag_adjustment_spec.rb
@@ -3,13 +3,16 @@ require "rails_helper"
 RSpec.describe TagAdjustment do
   before do
     mod_user.add_role(:tag_moderator, tag)
-    admin_user.add_role(:admin)
+    other_mod.add_role(:tag_moderator, tag)
   end
 
   let(:article) { create(:article, tags: nil) }
   let(:tag) { create(:tag) }
-  let(:admin_user) { create(:user) }
+  let(:admin_user) { create(:user, :admin) }
+  let(:other_admin) { create(:user, :admin) }
+  let(:super_mod) { create(:user, :super_moderator) }
   let(:mod_user) { create(:user) }
+  let(:other_mod) { create(:user) }
   let(:regular_user) { create(:user) }
 
   it { is_expected.to validate_presence_of(:tag_name) }
@@ -23,20 +26,37 @@ RSpec.describe TagAdjustment do
       expect(tag_adjustment).to be_valid
     end
 
-    it "does not allow tag mods to create for other tags" do
-      another_tag = create(:tag)
-      tag_adjustment = build(:tag_adjustment, user_id: mod_user.id, article_id: article.id, tag_id: another_tag.id)
-      expect(tag_adjustment).to be_invalid
+    it "allows tag mods to create a tag adjustment for a tag that has been adjusted by another tag mod" do
+      create(:tag_adjustment, user_id: other_mod.id, article_id: article.id, tag_id: tag.id)
+
+      tag_adjustment = build(:tag_adjustment, user_id: mod_user.id, article_id: article.id, tag_id: tag.id)
+      expect(tag_adjustment).to be_valid
     end
 
-    it "allows admins to create for any tags" do
+    it "does not allow tag mods to create a tag adjustment for other tags" do
+      another_tag = create(:tag)
+      tag_adjustment = build(:tag_adjustment, user_id: mod_user.id, article_id: article.id, tag_id: another_tag.id)
+      expect(tag_adjustment).not_to be_valid
+    end
+
+    it "allows admins to create a tag adjustment for any tags" do
       tag_adjustment = build(:tag_adjustment, user_id: admin_user.id, article_id: article.id, tag_id: tag.id)
       expect(tag_adjustment).to be_valid
     end
 
-    it "does not allow normal users to create for any tags" do
+    it "allows admins and super moderators to create a tag adjustment for a tag that was adjusted by another admin" do
+      create(:tag_adjustment, user_id: admin_user.id, article_id: article.id, tag_id: tag.id)
+
+      tag_adjustment = build(:tag_adjustment, user_id: other_admin.id, article_id: article.id, tag_id: tag.id)
+      expect(tag_adjustment).to be_valid
+
+      tag_adjustment = build(:tag_adjustment, user_id: super_mod.id, article_id: article.id, tag_id: tag.id)
+      expect(tag_adjustment).to be_valid
+    end
+
+    it "does not allow normal users to create a tag adjustment for any tags" do
       tag_adjustment = build(:tag_adjustment, user_id: regular_user.id, article_id: article.id, tag_id: tag.id)
-      expect(tag_adjustment).to be_invalid
+      expect(tag_adjustment).not_to be_valid
     end
   end
 
@@ -56,7 +76,7 @@ RSpec.describe TagAdjustment do
     it "disallows improper adjustment_types" do
       tag_adjustment = build(:tag_adjustment, user_id: mod_user.id, article_id: article.id, tag_id: tag.id,
                                               adjustment_type: "slushie")
-      expect(tag_adjustment).to be_invalid
+      expect(tag_adjustment).not_to be_valid
     end
 
     it "allows proper status" do
@@ -68,7 +88,7 @@ RSpec.describe TagAdjustment do
     it "disallows improper status" do
       tag_adjustment = build(:tag_adjustment, user_id: mod_user.id, article_id: article.id, tag_id: tag.id,
                                               status: "slushiemonkey")
-      expect(tag_adjustment).to be_invalid
+      expect(tag_adjustment).not_to be_valid
     end
   end
 
@@ -77,14 +97,14 @@ RSpec.describe TagAdjustment do
       article_tags_maxed = create(:article, tags: "ruby, javascript, html, css")
       tag_adjustment = build(:tag_adjustment, user_id: admin_user.id, article_id: article_tags_maxed.id,
                                               tag_id: tag.id, tag_name: tag.name)
-      expect(tag_adjustment).to be_invalid
+      expect(tag_adjustment).not_to be_valid
     end
 
     it "does not create if removed tag not on tag_list" do
       article = create(:article, tags: tag.name)
       tag_adjustment = build(:tag_adjustment, user_id: admin_user.id, article_id: article.id,
                                               adjustment_type: "removal")
-      expect(tag_adjustment).to be_invalid
+      expect(tag_adjustment).not_to be_valid
     end
 
     it "ignores case when checking tag_list" do
