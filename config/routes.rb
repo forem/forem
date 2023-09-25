d370245a8d3ccf680a47857b325335a3bcf2231e
Joshua Wehner <joshua@forem.com>
2023-09-25 17:21:06 +0200

    Remove old route

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,6 @@ Rails.application.routes.draw do
     namespace :api, defaults: { format: "json" } do
       scope module: :v1, constraints: ApiConstraints.new(version: 1, default: false) do
         # V1 only endpoints
-        # put "/users/:id/suspend", to: "users#suspend", as: :user_suspend
         put "/articles/:id/unpublish", to: "articles#unpublish", as: :article_unpublish
         put "/users/:id/unpublish", to: "users#unpublish", as: :user_unpublish
 
