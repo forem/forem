6f71a9751343cad8ef8d95f307cdf25bd4556034
dukegreene <duke@forem.com>
2023-09-25 10:42:09 -0400

    finish refactoring fixed tests for tag adjustment history into their own file

--- cypress/e2e/seededFlows/moderationFlows/viewTagAdjustments.spec.js
+++ cypress/e2e/seededFlows/moderationFlows/viewTagAdjustments.spec.js
@@ -44,4 +44,96 @@ describe('View tag adjustments', () => {
       });
     });
   });
+
+  describe('from /mod/tagname page', () => {
+    beforeEach(() => {
+      cy.testSetup();
+      cy.fixture('users/adminUser.json').as('user');
+
+      cy.get('@user').then((user) => {
+        cy.loginAndVisit(user, '/mod/tag1');
+      });
+    });
+
+    // Helper function for pipe command
+    const click = ($el) => $el.click();
+
+    it('should show previous tag adjustments', () => {
+      cy.findByRole('heading', { name: 'Tag test article' }).click();
+
+      cy.getIframeBody('.actions-panel-iframe').within(() => {
+        cy.findByRole('button', {
+          name: 'Open adjust tags section',
+        })
+          .pipe(click)
+          .should('have.attr', 'aria-expanded', 'true');
+
+        cy.get('#tag-history-heading').scrollIntoView();
+        cy.get('#tag-adjustment-history')
+          .find('.tag-adjustment')
+          .should(($div) => {
+            expect($div[0].innerText).to.contain(
+              '# tag1 added by Admin McAdmin\nadding test tag 1',
+            );
+            expect($div[1].innerText).to.contain(
+              '# tag2 added by Admin McAdmin\nadding test tag 2',
+            );
+          });
+
+        cy.get('#expand-tag-history').pipe(click);
+      });
+
+      cy.get('.crayons-modal__box').should(($modalDiv) => {
+        expect($modalDiv[0].innerText).to.contain(
+          '# tag0 added by Admin McAdmin\nadding test tag 0',
+        );
+      });
+    });
+  });
+
+  describe('from article page', () => {
+    beforeEach(() => {
+      cy.testSetup();
+      cy.fixture('users/adminUser.json').as('user');
+
+      cy.get('@user').then((user) => {
+        cy.loginAndVisit(user, '/admin_mcadmin/tag-test-article');
+      });
+    });
+
+    // Helper function for pipe command
+    const click = ($el) => $el.click();
+
+    it('should show previous tag adjustments', () => {
+      cy.findByRole('heading', { name: 'Tag test article' }).click();
+
+      cy.getIframeBody('#mod-container').within(() => {
+        cy.findByRole('button', {
+          name: 'Open adjust tags section',
+        })
+          .pipe(click)
+          .should('have.attr', 'aria-expanded', 'true');
+
+        cy.get('#tag-history-heading').scrollIntoView();
+        cy.get('#tag-adjustment-history')
+          .find('.tag-adjustment')
+          .should(($div) => {
+            expect($div[0].innerText).to.contain(
+              '# tag1 added by Admin McAdmin\nadding test tag 1',
+            );
+            expect($div[1].innerText).to.contain(
+              '# tag2 added by Admin McAdmin\nadding test tag 2',
+            );
+          });
+
+        cy.get('#expand-tag-history').pipe(click);
+      });
+
+      cy.get('.crayons-modal__box').should(($modalDiv) => {
+        expect($modalDiv[0].innerText).to.contain(
+          '# tag0 added by Admin McAdmin\nadding test tag 0',
+        );
+      });
+    });
+  });
 });
