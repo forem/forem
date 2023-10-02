044fe882301bb0f6fea3503116263f4abfe3fdee
Ben Halpern <bendhalpern@gmail.com>
2023-10-02 17:23:37 +0700

    Follow on adjustments to flag counts (#20207)

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -40,9 +40,11 @@ class Reaction < ApplicationRecord
   scope :distinct_categories, -> { select("distinct(reactions.category) as category, reactable_id, reactable_type") }
   scope :live_reactable, lambda {
     joins("LEFT JOIN articles ON reactions.reactable_id = articles.id AND reactions.reactable_type = 'Article'")
+      .joins("LEFT JOIN users ON reactions.reactable_id = users.id AND reactions.reactable_type = 'User'")
       .where("
           CASE
             WHEN reactions.reactable_type = 'Article' THEN articles.published = TRUE
+            WHEN reactions.reactable_type = 'User' THEN users.username NOT LIKE 'spam_%'
             ELSE TRUE
           END
         ")
@@ -204,7 +206,7 @@ class Reaction < ApplicationRecord
   end
 
   def negative_reaction_from_untrusted_user?
-    return if user&.any_admin? || user&.id == Settings::General.mascot_user_id
+    return false if user&.any_admin? || user&.id == Settings::General.mascot_user_id
 
     negative? && !user.trusted?
   end
