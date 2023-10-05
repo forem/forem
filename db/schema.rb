a9e4fc91d9d64ea3e34032caab31f8b6f4171501
Mac Siri <krairit.siri@gmail.com>
2023-10-05 13:47:02 -0400

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_02_150239) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_12_220412) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "ltree"
