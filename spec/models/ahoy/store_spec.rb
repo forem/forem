2873dd7b654875269bdd21c8c8fd14b15da7bf9d
Ben Halpern <bendhalpern@gmail.com>
2023-10-06 11:49:18 +0700

    Work out tests

--- spec/models/ahoy/store_spec.rb
+++ spec/models/ahoy/store_spec.rb
@@ -1,27 +1,21 @@
 require "rails_helper"
 
 RSpec.describe Ahoy::Store do
-  let(:data) { { user_id: 1, started_at: Time.current } }
   let(:user) { create(:user) }
-  let(:mock_request) { instance_double("Request") }
-  let(:store_double) { instance_double(described_class, track_visit: nil, request: mock_request) }
+  let(:data) { { user_id: user.id, started_at: Time.current } }
+  let(:mock_request) { instance_double("Request", headers: { "X-Client-Geo" => "mock_geo", "HTTP_ACCEPT_LANGUAGE" => "mock_language" }, user_agent: "mock_user_agent") }
+
+  # Create a real instance of Ahoy::Store to test behavior
+  let(:store) { described_class.new(request: mock_request) }
 
   before do
-    mock_request.receive_messages(
-      headers: {
-        "X-Client-Geo" => "mock_geo",
-        "HTTP_ACCEPT_LANGUAGE" => "mock_language"
-      },
-    )
     allow(mock_request).to receive(:user_agent).and_return("mock_user_agent")
   end
 
   describe "#track_visit" do
     context "when context is not found" do
       it "creates a new UserVisitContext and increments visit count" do
-        expect do
-          store_double.track_visit(data)
-        end.to change(UserVisitContext, :count).by(1)
+        expect { store.track_visit(data) }.to change(UserVisitContext, :count).by(1)
 
         context = UserVisitContext.last
         expect(context.visit_count).to eq(1)
@@ -40,15 +34,12 @@ RSpec.describe Ahoy::Store do
       end
 
       it "increments the visit count" do
-        expect do
-          store_double.track_visit(data)
-        end.not_to change(UserVisitContext, :count)
-
+        expect { store.track_visit(data) }.not_to change(UserVisitContext, :count)
         expect(existing_context.reload.visit_count).to eq(6)
       end
 
       it "updates the last_visit_at timestamp" do
-        store_double.track_visit(data)
+        store.track_visit(data)
         expect(existing_context.reload.last_visit_at).to be_within(1.second).of(data[:started_at])
       end
     end
@@ -57,11 +48,9 @@ RSpec.describe Ahoy::Store do
       context = create(:user_visit_context, user: user)
       allow(UserVisitContext).to receive(:find_or_initialize_by).and_return(context)
 
-      expect_any_instance_of(Ahoy::DatabaseStore).to receive(:track_visit) do |_, arg|
-        expect(arg[:user_visit_context_id]).to eq(context.id)
-      end
+      expect_any_instance_of(Ahoy::DatabaseStore).to receive(:track_visit).with(hash_including(user_visit_context_id: context.id))
 
-      store_double.track_visit(data)
+      store.track_visit(data)
     end
   end
 end
