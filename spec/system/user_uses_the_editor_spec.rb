20faec4899b2f7735dc3af97da76e14a41996748
Mac Siri <mac@forem.com>
2023-10-16 15:12:53 -0400

    Support chromedriver in container (#20224)

--- spec/system/user_uses_the_editor_spec.rb
+++ spec/system/user_uses_the_editor_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe "Using the editor" do
     within("#article-form") { fill_in "article_body_markdown", with: content }
   end
 
-  describe "Viewing the editor", js: true do
+  describe "Viewing the editor", :js do
     it "renders the logo or Community name as expected" do
       visit "/new"
       expect(page).to have_css(".site-logo")
@@ -32,18 +32,12 @@ RSpec.describe "Using the editor" do
     end
   end
 
-  describe "Previewing an article", js: true do
-    before do
+  describe "Previewing an article", :js do
+    it "fills out form with rich content and click preview", :cloudinary do
       fill_markdown_with(read_from_file(raw_text))
       page.execute_script("window.scrollTo(0, -100000)")
-      find("button", text: /\APreview\z/).click
-    end
+      click_button "Preview"
 
-    after do
-      page.evaluate_script("window.onbeforeunload = function(){}")
-    end
-
-    it "fills out form with rich content and click preview", cloudinary: true do
       article_body = find("div.crayons-article__body")["innerHTML"]
       article_body.gsub!(%r{"https://res\.cloudinary\.com/.{1,}"}, "cloudinary_link")
 
@@ -54,15 +48,17 @@ RSpec.describe "Using the editor" do
         .and include('<blockquote>')
         .and include('Format: <a href=cloudinary_link></a>')
       # rubocop:enable Style/StringLiterals
+
+      page.evaluate_script("window.onbeforeunload = function(){}")
     end
   end
 
-  describe "Submitting an article with v1 editor", js: true do
+  describe "Submitting an article with v1 editor", :js do
     before { user.setting.update!(editor_version: "v1") }
 
-    it "fill out form and submit", cloudinary: true do
+    it "fill out form and submit", :cloudinary do
       fill_markdown_with(read_from_file(raw_text))
-      find("button", text: /\ASave changes\z/).click
+      click_button "Save changes"
       article_body = find(:xpath, "//div[@id='article-body']")["innerHTML"]
       article_body.gsub!(%r{"https://res\.cloudinary\.com/.{1,}"}, "cloudinary_link")
 
@@ -77,16 +73,16 @@ RSpec.describe "Using the editor" do
 
     it "user write and publish an article" do
       fill_markdown_with(template.gsub("false", "true"))
-      find("button", text: /\ASave changes\z/).click
-      ["Sample Article", template[-200..], "test"].each do |text|
-        expect(page).to have_text(text)
-      end
+      click_button "Save changes"
+      expect(page).to have_text("Sample Article")
+      expect(page).to have_text("Suspendisse ac lobortis velit")
+      expect(page).to have_text("test")
     end
 
-    context "without a title", js: true do
+    context "without a title", :js do
       before do
         fill_markdown_with(template.gsub("Sample Article", ""))
-        find("button", text: /\ASave changes\z/).click
+        click_button "Save changes"
       end
 
       it "shows a message that the title cannot be blank" do
@@ -95,7 +91,7 @@ RSpec.describe "Using the editor" do
     end
   end
 
-  describe "using v2 editor", js: true do
+  describe "using v2 editor", :js do
     it "fill out form with rich content and click publish" do
       visit "/new"
       within "form#article-form" do
