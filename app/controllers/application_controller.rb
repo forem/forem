7c74ca663f82490fc5a50a77acbc5d5421ccfae0
Ben Halpern <bendhalpern@gmail.com>
2023-11-08 17:21:53 +0000

    Add locale update script and PT as new locale option

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -254,7 +254,7 @@ class ApplicationController < ActionController::Base
   end
 
   def determine_locale
-    I18n.locale = if %w[en fr].include?(params[:locale])
+    I18n.locale = if %w[en fr pt].include?(params[:locale])
                     params[:locale]
                   else
                     Settings::UserExperience.default_locale
