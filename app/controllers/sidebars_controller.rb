5e61218d84110b57620ccd12ddafb242cb66cc2b
Ben Halpern <bendhalpern@gmail.com>
2023-11-20 18:39:29 +0000

    Add active discussions to sidebar

--- app/controllers/sidebars_controller.rb
+++ app/controllers/sidebars_controller.rb
@@ -3,6 +3,20 @@ class SidebarsController < ApplicationController
 
   def show
     get_latest_campaign_articles
+    return unless user_signed_in?
+
+    tag_names = current_user.cached_followed_tag_names
+    @active_discussions = Article.published
+      .where("published_at > ?", 1.week.ago)
+      .where("comments_count > ?", 0)
+      .with_at_least_home_feed_minimum_score
+      .cached_tagged_with_any(tag_names)
+      .or(Article.featured.published.where("published_at > ?", 1.week.ago)
+        .with_at_least_home_feed_minimum_score)
+      .order("last_comment_at DESC")
+      .limit(5)
+      .pluck(:path, :title, :comments_count, :created_at)
+    @featured_tags = Tag.where(name: Settings::General.sidebar_tags)
   end
 
   private
