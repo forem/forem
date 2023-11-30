0e9c8a367a90eacd3b8bfead2ae668b3cb5d16c0
Jerinkc <jerinkc22@gmail.com>
2023-11-30 15:27:50 +0530

    Minor change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_21_140731) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_21_154642) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "ltree"
