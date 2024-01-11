40d413d4c2e94ae28a8fabcf1979970a8d1183a9
Philip How <philip.j.how@gmail.com>
2024-01-11 17:39:27 +0000

    add css as pages option

---
+++ spec/lib/data_update_scripts/add_css_to_pages_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+require Rails.root.join(
+  "lib/data_update_scripts/20240111143736_add_css_to_pages.rb",
+)
+
+describe DataUpdateScripts::AddCssToPages do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
