7cde81588e010bb056db342cbc9fb314a5c79080
lightalloy <lightallloy@gmail.com>
2024-01-15 16:26:44 +0300

    Don't show low scoring articles in timeframe feeds

--- app/services/articles/feeds/timeframe.rb
+++ app/services/articles/feeds/timeframe.rb
@@ -1,12 +1,14 @@
 module Articles
   module Feeds
     module Timeframe
-      def self.call(timeframe, tag: nil, number_of_articles: Article::DEFAULT_FEED_PAGINATION_WINDOW_SIZE, page: 1)
+      def self.call(timeframe, tag: nil, minimum_score: -20,
+                    number_of_articles: Article::DEFAULT_FEED_PAGINATION_WINDOW_SIZE, page: 1)
         articles = ::Articles::Feeds::Tag.call(tag)
 
         articles
           .where("published_at > ?", ::Timeframe.datetime(timeframe))
           .includes(:distinct_reaction_categories)
+          .where("score > ?", minimum_score)
           .order(score: :desc)
           .page(page)
           .per(number_of_articles)
