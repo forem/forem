bfa3cb817c9682f1c04270dd1a22b54e4709de34
Mac Siri <krairit.siri@gmail.com>
2024-01-30 10:07:04 -0500

    Fix broken spec

--- cypress/e2e/seededFlows/articleFlows/followOrganization.spec.js
+++ cypress/e2e/seededFlows/articleFlows/followOrganization.spec.js
@@ -5,23 +5,20 @@ describe('Follow an organization from article sidebar', () => {
     cy.fixture('users/articleEditorV1User.json').as('user');
 
     cy.get('@user').then((user) => {
-      cy.loginAndVisit(
-        user,
-        '/admin_mcadmin/test-organization-article-slug',
-      ).then(() => {
-        cy.get('[data-follow-clicks-initialized]');
-        cy.findByRole('heading', { name: 'Organization test article' });
-      });
+      cy.loginAndVisit(user, '/admin_mcadmin/test-organization-article-slug');
     });
+    cy.get('[data-follow-clicks-initialized]');
+    cy.findByRole('heading', { name: 'Organization test article' });
   });
 
-  it('Follows an organization from the sidebar', () => {
+  it('Follows and unfollow an organization from the sidebar', () => {
     cy.intercept('/follows').as('followRequest');
 
     cy.contains('Follow').as('followButton');
 
     // Follow
     cy.get('@followButton').click();
+    cy.wait('@followRequest');
     cy.get('@followButton').should('have.text', 'Following');
     cy.get('@followButton').should('have.attr', 'aria-pressed', 'true');
 
@@ -35,18 +32,17 @@ describe('Follow an organization from article sidebar', () => {
     cy.findByRole('main')
       .findByRole('link', { name: 'Bachmanity' })
       .should('exist');
-  });
-
-  it('Unfollows an organization from the sidebar', () => {
-    cy.intercept('/follows').as('followRequest');
-
-    cy.contains('Follow').as('followButton');
-
-    // Follow
-    cy.get('@followButton').click();
 
     // Unfollow
+    cy.visitAndWaitForUserSideEffects(
+      '/admin_mcadmin/test-organization-article-slug',
+    );
+    // cy.intercept('/follows').as('followRequest');
+    cy.contains('Following').as('followButton');
     cy.get('@followButton').click();
+    cy.wait('@followRequest');
+    cy.contains('Following').should('not.exist');
+    cy.contains('Follow').as('followButton');
     cy.get('@followButton').should('have.text', 'Follow');
     cy.get('@followButton').should('have.attr', 'aria-pressed', 'false');
 
