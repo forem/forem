f10765010177800a5c443b027838af904ecd2620
Mac Siri <krairit.siri@gmail.com>
2024-02-02 10:12:20 -0500

    Move husky

---
+++ .husky/install.mjs
@@ -0,0 +1,7 @@
+// Skip Husky install in production and CI
+if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
+  process.exit(0)
+}
+const husky = (await import('husky')).default
+// Install Husky
+husky.install()
