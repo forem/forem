f9fa74feb0a314e8e052ac31413a7465a445e53c
lightalloy <lightallloy@gmail.com>
2024-02-15 17:42:04 +0300

    Add specs for caching article comments, use cache_key_with_version

--- app/workers/comments/calculate_score_worker.rb
+++ app/workers/comments/calculate_score_worker.rb
@@ -11,7 +11,14 @@ module Comments
       score = BlackBox.comment_quality_score(comment)
       score -= 500 if comment.user&.spam?
       comment.update_columns(score: score, updated_at: Time.current)
-      comment.root.save! if !comment.is_root? && comment.root_exists?
+
+      # comment.commentable.touch(:last_comment_at) if comment.commentable.respond_to?(:last_comment_at)
+      # comment.user.touch(:last_comment_at) if comment.user
+      # EdgeCache::Bust.call(comment.commentable.path.to_s) if comment.commentable
+
+      # Comments::BustCacheWorker.new.perform(comment.id)
+
+      # comment.root.save! if !comment.is_root? && comment.root_exists?
     end
   end
 end
