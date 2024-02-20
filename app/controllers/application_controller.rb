b35a8824e4a90e8529186a0197a4c28272947935
Ben Halpern <bendhalpern@gmail.com>
2024-02-20 23:29:49 +0000

    Move field test to the right place

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -221,14 +221,7 @@ class ApplicationController < ActionController::Base
   helper_method :internal_navigation?
 
   def feed_style_preference
-    default = Settings::UserExperience.feed_style
-    return default unless user_signed_in?
-
-    after_date = current_user && current_user.registered_at >= Time.zone.parse("2024-02-20")
-    flag_on = feature_flag_enabled?(:feed_style_test_running)
-    return default unless after_date && flag_on
-
-    field_test(:feed_style_20240220, participant: current_user)
+    Settings::UserExperience.feed_style
   end
   helper_method :feed_style_preference
 
