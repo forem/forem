f88784cc35aea0885ccf5c73bf026cd13e93abbc
Ben Halpern <bendhalpern@gmail.com>
2024-02-21 08:27:26 -0800

    Add configuration for admins to specify compliant email strategies (#20663)
    * Add configuration for admins to specify compliant email strategies
    
    * Add keys for test

--- app/assets/javascripts/initializers/initializeBodyData.js
+++ app/assets/javascripts/initializers/initializeBodyData.js
@@ -12,46 +12,60 @@ function removeExistingCSRF() {
 function fetchBaseData() {
   fetch('/async_info/base_data')
     .then((response) => response.json())
-    .then(({ token, param, broadcast, user, creator }) => {
-      if (token) {
-        removeExistingCSRF();
-      }
+    .then(
+      ({
+        token,
+        param,
+        broadcast,
+        user,
+        creator,
+        client_geolocation,
+        default_email_optin_allowed,
+      }) => {
+        if (token) {
+          removeExistingCSRF();
+        }
 
-      const newCsrfParamMeta = document.createElement('meta');
-      newCsrfParamMeta.name = 'csrf-param';
-      newCsrfParamMeta.content = param;
-      document.head.appendChild(newCsrfParamMeta);
+        const newCsrfParamMeta = document.createElement('meta');
+        newCsrfParamMeta.name = 'csrf-param';
+        newCsrfParamMeta.content = param;
+        document.head.appendChild(newCsrfParamMeta);
 
-      const newCsrfTokenMeta = document.createElement('meta');
-      newCsrfTokenMeta.name = 'csrf-token';
-      newCsrfTokenMeta.content = token;
-      document.head.appendChild(newCsrfTokenMeta);
-      document.body.dataset.loaded = 'true';
+        const newCsrfTokenMeta = document.createElement('meta');
+        newCsrfTokenMeta.name = 'csrf-token';
+        newCsrfTokenMeta.content = token;
+        document.head.appendChild(newCsrfTokenMeta);
+        document.body.dataset.loaded = 'true';
 
-      if (broadcast) {
-        document.body.dataset.broadcast = broadcast;
-      }
+        if (broadcast) {
+          document.body.dataset.broadcast = broadcast;
+        }
 
-      if (checkUserLoggedIn()) {
-        document.body.dataset.user = user;
-        document.body.dataset.creator = creator;
-        browserStoreCache('set', user);
+        if (checkUserLoggedIn()) {
+          document.body.dataset.user = user;
+          document.body.dataset.creator = creator;
+          document.body.dataset.clientGeolocation =
+            JSON.stringify(client_geolocation);
+          document.body.dataset.default_email_optin_allowed =
+            default_email_optin_allowed;
+          browserStoreCache('set', user);
 
-        setTimeout(() => {
-          if (typeof ga === 'function') {
-            ga('set', 'userId', JSON.parse(user).id);
-          }
-          if (typeof gtag === 'function') {
-            gtag('set', 'user_Id', JSON.parse(user).id);
-          }
-        }, 400);
-      } else {
-        // Ensure user data is not exposed if no one is logged in
-        delete document.body.dataset.user;
-        delete document.body.dataset.creator;
-        browserStoreCache('remove');
-      }
-    });
+          setTimeout(() => {
+            if (typeof ga === 'function') {
+              ga('set', 'userId', JSON.parse(user).id);
+            }
+            if (typeof gtag === 'function') {
+              gtag('set', 'user_Id', JSON.parse(user).id);
+            }
+          }, 400);
+        } else {
+          // Ensure user data is not exposed if no one is logged in
+          delete document.body.dataset.user;
+          delete document.body.dataset.creator;
+          browserStoreCache('remove');
+        }
+      },
+    );
 }
 
 function initializeBodyData() {
