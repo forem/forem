78f9bec3e7e0227acecee5ec94cc7cc4728fd63e
Ben Halpern <bendhalpern@gmail.com>
2024-02-28 11:02:58 -0500

    Allow admin to declare whether to include the badge description in message (#20684)
    * Allow admin to declare whether to include the badge description in badge achievement message
    
    * Update spec/services/badges/award_spec.rb
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Add proper acceptance to tests
    
    * Tweak tests
    
    * Switch to kwargs
    
    * Clean up test acceptance
    
    * Change interface
    
    * Update spec/requests/admin/badge_achievements_spec.rb
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update app/services/badges/award.rb
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Adjust interface
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/controllers/admin/badge_achievements_controller.rb
+++ app/controllers/admin/badge_achievements_controller.rb
@@ -32,9 +32,14 @@ module Admin
       end
 
       usernames = permitted_params[:usernames].downcase.split(/\s*,\s*/)
+      include_default_description = permitted_params[:include_default_description] == "1"
       message = permitted_params[:message_markdown].presence ||
         I18n.t("admin.badge_achievements_controller.congrats", community: ::Settings::Community.community_name)
-      BadgeAchievements::BadgeAwardWorker.perform_async(usernames, permitted_params[:badge], message)
+      BadgeAchievements::BadgeAwardWorker
+        .perform_async(usernames,
+                       permitted_params[:badge],
+                       message,
+                       include_default_description)
 
       flash[:success] = I18n.t("admin.badge_achievements_controller.rewarded")
       redirect_to admin_badge_achievements_path
@@ -46,7 +51,7 @@ module Admin
     private
 
     def permitted_params
-      params.permit(:usernames, :badge, :message_markdown)
+      params.permit(:usernames, :badge, :message_markdown, :include_default_description)
     end
   end
 end
