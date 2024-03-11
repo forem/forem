46e59bde98e174b23ecde0cd945eacd21bece44a
Mac Siri <krairit.siri@gmail.com>
2024-03-11 15:34:24 -0400

    Disable by default

--- config/initializers/ahoy_email.rb
+++ config/initializers/ahoy_email.rb
@@ -1,5 +1,5 @@
 # enable tracking for open, click and UTM params
 AhoyEmail.api = false
-AhoyEmail.default_options[:click] = true
+AhoyEmail.default_options[:click] = false
 AhoyEmail.default_options[:utm_params] = false
 AhoyEmail.default_options[:message] = true
