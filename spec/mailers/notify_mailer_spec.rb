81e1e74f58c89da12874f00b3afd8bc1ec53ef49
Ben Halpern <bendhalpern@gmail.com>
2024-03-11 15:33:37 +0000

    Update tests

--- spec/mailers/notify_mailer_spec.rb
+++ spec/mailers/notify_mailer_spec.rb
@@ -143,21 +143,21 @@ RSpec.describe NotifyMailer do
 
       it "includes the rewarding_context_message in the email" do
         expect(email.html_part.body).to include("Hello <a")
-        expect(email.html_part.body).to include(URL.url("/hey"))
+        expect(email.html_part.body).to include("%2Fhey")
       end
 
       it "does not include the nil rewarding_context_message in the email" do
         allow(badge_achievement).to receive(:rewarding_context_message).and_return(nil)
 
         expect(email.html_part.body).not_to include("Hello <a")
-        expect(email.html_part.body).not_to include(URL.url("/hey"))
+        expect(email.html_part.body).not_to include("%2Fhey")
       end
 
       it "does not include the empty rewarding_context_message in the email" do
         allow(badge_achievement).to receive(:rewarding_context_message).and_return("")
 
         expect(email.html_part.body).not_to include("Hello <a")
-        expect(email.html_part.body).not_to include(URL.url("/hey"))
+        expect(email.html_part.body).not_to include("%2Fhey")
       end
     end
 
@@ -180,21 +180,21 @@ RSpec.describe NotifyMailer do
 
       it "includes the rewarding_context_message in the email" do
         expect(email.text_part.body).to include("Hello Yoho")
-        expect(email.text_part.body).not_to include(URL.url("/hey"))
+        expect(email.text_part.body).not_to include("%2Fhey")
       end
 
       it "does not include the nil rewarding_context_message in the email" do
         allow(badge_achievement).to receive(:rewarding_context_message).and_return(nil)
 
         expect(email.text_part.body).not_to include("Hello Yoho")
-        expect(email.text_part.body).not_to include(URL.url("/hey"))
+        expect(email.text_part.body).not_to include("%2Fhey")
       end
 
       it "does not include the empty rewarding_context_message in the email" do
         allow(badge_achievement).to receive(:rewarding_context_message).and_return("")
 
         expect(email.text_part.body).not_to include("Hello Yoho")
-        expect(email.text_part.body).not_to include(URL.url("/hey"))
+        expect(email.text_part.body).not_to include("%2Fhey")
       end
     end
   end
