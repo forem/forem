84df40f6d4fe58f5f8676bebdead82e92cdf1206
lightalloy <lightallloy@gmail.com>
2024-03-19 16:49:01 +0300

    Fixed spec

--- spec/requests/comments_with_cache_spec.rb
+++ spec/requests/comments_with_cache_spec.rb
@@ -5,24 +5,23 @@
 require "rails_helper"
 
 RSpec.describe "ArticleCommentsWithCache" do
-  let(:user) { create(:user, :admin) }
-  let(:article) { create(:article, user: user, published: true) }
+  let(:admin) { create(:user, :admin) }
+  let(:article) { create(:article, user: admin, published: true) }
   let(:user2) { create(:user) }
 
-  let(:parent) { create(:comment, commentable: article, user: user, body_markdown: "parent-comment") }
+  let(:parent) { create(:comment, commentable: article, user: admin, body_markdown: "parent-comment") }
 
   let(:cache_store) { ActiveSupport::Cache.lookup_store(:redis_cache_store) }
   let(:cache) { Rails.cache }
 
   before do
-    sign_in user
     allow(Rails).to receive(:cache).and_return(cache_store)
   end
 
   def assign_spam_role
     sidekiq_perform_enqueued_jobs do
       Moderator::ManageActivityAndRoles.handle_user_roles(
-        admin: user,
+        admin: admin,
         user: user2,
         user_params: {
           note_for_current_role: "note",
@@ -34,6 +33,7 @@ RSpec.describe "ArticleCommentsWithCache" do
 
   describe "GET /:slug (articles)" do
     it "busts comments cache" do
+      sign_in admin
       create(:comment, commentable: article, user: user2, body_markdown: "potential-spam-comment")
       get article.path
       expect(response.body).to include("potential-spam-comment")
@@ -43,9 +43,11 @@ RSpec.describe "ArticleCommentsWithCache" do
     end
 
     it "busts cache when spam comment is a child and a parent" do
+      sign_in admin
+
       comment = create(:comment, commentable: article, user: user2, parent: parent,
                                  body_markdown: "potential-spam-comment")
-      create(:comment, commentable: article, user: user, body_markdown: "child-comment", parent: comment)
+      create(:comment, commentable: article, user: admin, body_markdown: "child-comment", parent: comment)
 
       get article.path
       expect(response.body).to include("potential-spam-comment")
@@ -61,9 +63,11 @@ RSpec.describe "ArticleCommentsWithCache" do
 
   describe "GET /:username/comment/:id_code (root comment path)" do
     it "busts cache for root comment" do
+      sign_in user2
+
       comment = create(:comment, commentable: article, user: user2, parent: parent,
                                  body_markdown: "potential-spam-comment")
-      create(:comment, commentable: article, user: user, body_markdown: "child-comment", parent: comment)
+      create(:comment, commentable: article, user: admin, body_markdown: "child-comment", parent: comment)
 
       get parent.path
       expect(response.body).to include("potential-spam-comment")
