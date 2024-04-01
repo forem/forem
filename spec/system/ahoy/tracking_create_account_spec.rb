52f5dfa4ec9b6a12e1df76c5f93ff2c4cef8764b
Mac Siri <krairit.siri@gmail.com>
2024-04-01 17:18:22 -0400

    Fix ahoy's tracking spec

--- spec/system/ahoy/tracking_create_account_spec.rb
+++ spec/system/ahoy/tracking_create_account_spec.rb
@@ -2,14 +2,8 @@ require "rails_helper"
 
 RSpec.describe "Tracking 'Clicked on Create Account'", :js do
   context "when on the homepage" do
-    let(:user) { create(:user) }
-    # rubocop:disable RSpec/LetSetup
-    # this is to ensure that we see "ca_feed_home_page" which appears
-    # in a card after the fifth article
-    let!(:articles) { create_list(:article, 6, user: user) }
-    # rubocop:enable RSpec/LetSetup
-
     before do
+      create_list(:article, 6)
       visit root_path
     end
 
@@ -21,12 +15,14 @@ RSpec.describe "Tracking 'Clicked on Create Account'", :js do
     it "has the create account tracking element in the hamburger", :aggregate_failures do
       Capybara.current_session.driver.resize(425, 694)
       first(".js-hamburger-trigger").click
+      # expect /message route to receive a request
       expect(page).to have_css('a[data-tracking-id="ca_hamburger_home_page"]')
     end
 
     it "tracks a click with the correct source", :aggregate_failures do
-      expect(Ahoy::Event.count).to eq(0)
-      find('[data-tracking-id="ca_top_nav"]').click
+      expect do
+        find('[data-tracking-id="ca_top_nav"]').click
+      end.to change(Ahoy::Event, :count).by(1)
 
       expect(Ahoy::Event.last.name).to eq("Clicked on Create Account")
       expect(Ahoy::Event.last.properties).to include("source", "page", "version", "source" => "top_navbar")
