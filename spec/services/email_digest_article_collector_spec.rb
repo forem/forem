080f9965e2614b6ff952c0188b78d56550ce049a
Ben Halpern <bendhalpern@gmail.com>
2024-04-08 13:27:35 +0000

    Fiddle with tests

--- spec/services/email_digest_article_collector_spec.rb
+++ spec/services/email_digest_article_collector_spec.rb
@@ -43,30 +43,6 @@ RSpec.describe EmailDigestArticleCollector, type: :service do
       end
     end
 
-    context  "when it's been less than the set number of digest email days, but the author has a recent click" do
-      before do
-        author = create(:user)
-        user.follow(author)
-        user.update(following_users_count: 1)
-        create_list(:article, 3, user_id: author.id, public_reactions_count: 20, score: 20)
-        Ahoy::Message.create(mailer: "DigestMailer#digest_email",
-                             user_id: user.id, sent_at: (2.2).days.ago, clicked_at: 1.day.ago)
-      end
-
-      it "returns articles when user shouldn't receive any" do
-        Settings::General.periodic_email_digest = 3
-        articles = described_class.new(user).articles_to_send
-        expect(articles).not_to be_empty
-      end
-
-      it "returns nothing if, in fact, empty if the Ahoy Message is not clicked" do
-        Ahoy::Message.last.update_column(:clicked_at, nil)
-        Settings::General.periodic_email_digest = 3
-        articles = described_class.new(user).articles_to_send
-        expect(articles).to be_empty
-      end
-    end
-
     context "when it's been more than the set number of digest email days" do
       before do
         Ahoy::Message.create(mailer: "DigestMailer#digest_email", user_id: user.id,
@@ -99,6 +75,75 @@ RSpec.describe EmailDigestArticleCollector, type: :service do
 
         digest2 = described_class.new(user).articles_to_send
         expect(digest2.size).to eq 0
+      end
+    end
+  end
+
+  describe "#should_receive_email?" do
+    let(:periodic_email_digest_days) { 3 }
+
+    before do
+      Settings::General.periodic_email_digest = periodic_email_digest_days
+      create(:ahoy_message, user_id: user.id, sent_at: sent_at, clicked_at: clicked_at)
+    end
+  
+    context "when the user clicked the last email within the lookback period" do
+      let(:sent_at) { 2.days.ago }
+      let(:clicked_at) { 1.day.ago }
+  
+      it "returns true" do
+        collector = EmailDigestArticleCollector.new(user)
+        expect(collector.should_receive_email?).to be true
+      end
+    end
+  
+    context "when the user has not received any emails" do
+      let(:sent_at) { nil }
+      let(:clicked_at) { nil }
+  
+      it "returns true" do
+        collector = EmailDigestArticleCollector.new(user)
+        expect(collector.should_receive_email?).to be true
+      end
+    end
+  
+    context "when the last email was received outside the periodic email digest days" do
+      let(:sent_at) { 4.days.ago }
+      let(:clicked_at) { nil }
+  
+      it "returns true" do
+        collector = EmailDigestArticleCollector.new(user)
+        expect(collector.should_receive_email?).to be true
+      end
+    end
+  
+    context "when the last email was received within the periodic email digest days without click" do
+      let(:sent_at) { 2.days.ago }
+      let(:clicked_at) { nil }
+  
+      it "returns false" do
+        collector = EmailDigestArticleCollector.new(user)
+        expect(collector.should_receive_email?).to be false
+      end
+    end
+  
+    context "when the last email was received just before the periodic email digest days limit" do
+      let(:sent_at) { 3.days.ago }
+      let(:clicked_at) { nil }
+  
+      it "returns true because it exactly matches the edge of the period" do
+        collector = EmailDigestArticleCollector.new(user)
+        expect(collector.should_receive_email?).to be true
+      end
+    end
+  
+    context "when the last email was clicked but outside the lookback period" do
+      let(:sent_at) { 31.days.ago }
+      let(:clicked_at) { 30.days.ago }
+  
+      it "returns true because the click is too old to affect eligibility" do
+        collector = EmailDigestArticleCollector.new(user)
+        expect(collector.should_receive_email?).to be true
       end
     end
   end
