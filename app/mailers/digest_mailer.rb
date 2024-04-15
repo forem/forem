4cca7cb06bddeb72ee6696990b0c3733cfa1d350
Ben Halpern <bendhalpern@gmail.com>
2024-04-15 15:56:41 +0000

    Add subheader a/b test to digest

--- app/mailers/digest_mailer.rb
+++ app/mailers/digest_mailer.rb
@@ -6,6 +6,7 @@ class DigestMailer < ApplicationMailer
     @articles = params[:articles]
     @unsubscribe = generate_unsubscribe_token(@user.id, :email_digest_periodic)
 
+    @subheader_variant = field_test(:digest_subheader_03_18, participant: @user)
     subject = generate_title
 
     # set sendgrid category in the header using smtp api
