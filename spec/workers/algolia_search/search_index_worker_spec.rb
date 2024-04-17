ec4d625c206ebd8e2a12f20b376295a69791bf2a
Mac Siri <krairit.siri@gmail.com>
2024-04-17 13:15:49 -0400

    Refactor

--- spec/workers/algolia_search/search_index_worker_spec.rb
+++ spec/workers/algolia_search/search_index_worker_spec.rb
@@ -1,57 +1,17 @@
 require "rails_helper"
-
-# from https://github.com/algolia/algoliasearch-client-ruby/blob/master/test/algolia/integration/mocks/mock_requester.rb
-class MockRequester
-  attr_accessor :requests
-
-  def initialize
-    @connection = nil
-    @requests   = []
-  end
-
-  def send_request(host, method, path, body, headers, timeout, connect_timeout)
-    request = {
-      host: host,
-      method: method,
-      path: path,
-      body: body,
-      headers: headers,
-      timeout: timeout,
-      connect_timeout: connect_timeout
-    }
-
-    @requests.push(request)
-
-    Algolia::Http::Response.new(
-      status: 200,
-      body: '{"hits": [], "status": "published"}',
-      headers: {},
-    )
-  end
-
-  def get_connection(host)
-    @connection = host
-  end
-
-  def build_url(host)
-    host.protocol + host.url
-  end
-end
 # rubocop:disable RSpec/AnyInstance
 RSpec.describe AlgoliaSearch::SearchIndexWorker, type: :worker do
   let(:user) { create(:user) }
 
   before do
-    mock_requester = MockRequester.new
+    mock_requester = AlgoliaMockRequester.new
     algolia_config = Algolia::Search::Config.new(AlgoliaSearch.configuration)
     mock_client = Algolia::Search::Client.new(algolia_config, http_requester: mock_requester)
-
     AlgoliaSearch.instance_variable_set(:@client, mock_client)
   end
 
   it "remove the record from Algolia if record is deleted" do
     expect_any_instance_of(Algolia::Search::Index).to receive(:delete_object).with(user.id)
-
     described_class.new.perform(user.class.name, user.id, true)
   end
 
