c13bc1f67e6ad5fa9573d4a8f0fef86adfc0c8ea
Mac Siri <krairit.siri@gmail.com>
2024-04-24 08:35:56 -0400

    Add AlgoliaSearch::SearchableTag

---
+++ app/models/concerns/algolia_searchable/searchable_tag.rb
@@ -0,0 +1,22 @@
+module AlgoliaSearchable
+  module SearchableTag
+    extend ActiveSupport::Concern
+
+    included do
+      include AlgoliaSearch
+
+      algoliasearch per_environment: true, enqueue: :trigger_sidekiq_worker do
+        attribute :name, :pretty_name, :short_summary, :hotness_score
+
+        # TODO: verify if this ranking is preferred
+        customRanking ["desc(hotness_score)"]
+      end
+    end
+
+    class_methods do
+      def trigger_sidekiq_worker(record, delete)
+        AlgoliaSearch::SearchIndexWorker.perform_async(record.class.name, record.id, delete)
+      end
+    end
+  end
+end
