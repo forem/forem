0ae6985d50ae0e44afb57826f83e4d74cf6601f2
Mac Siri <krairit.siri@gmail.com>
2024-04-24 09:56:48 -0400

    Add AlgoliaSearchable to Comment

---
+++ app/models/concerns/algolia_searchable/searchable_comment.rb
@@ -0,0 +1,47 @@
+module AlgoliaSearchable
+  module SearchableComment
+    extend ActiveSupport::Concern
+
+    included do
+      include AlgoliaSearch
+
+      algoliasearch(**DEFAULT_ALGOLIA_SETTINGS, if: :good_enough) do
+        # TODO: make sure all are valid attributes
+        attribute :commentable_id, :commentable_type, :path, :readable_publish_date, :parent_id
+        attribute :body_html do
+          truncate_body_html
+        end
+
+        attribute :user do
+          { name: user.name, username: user.username, profile_image: user.profile_image_90 }
+        end
+
+        add_replica "Comment_ordered", inherit: true, per_environment: true do
+          customRanking ["desc(created_at_i)"]
+        end
+      end
+    end
+
+    class_methods do
+      def trigger_sidekiq_worker(record, delete)
+        # TODO: umm make sure this get called in the appropriate place
+        AlgoliaSearch::SearchIndexWorker.perform_async(record.class.name, record.id, delete)
+      end
+    end
+
+    def truncate_body_html
+      # TODO: probably not needed
+      # HTML_Truncator.truncate(
+      #   processed_html,
+      #   500,
+      #   ellipsis: '<a class="comment-read-more" href="' + path + '">... Read Entire Comment</a>',
+      # )
+    end
+
+    def good_enough
+      # TODO: actually idk how _changed would work here.
+      !hidden_by_commentable_user
+    end
+    alias good_enough_changed? good_enough
+  end
+end
