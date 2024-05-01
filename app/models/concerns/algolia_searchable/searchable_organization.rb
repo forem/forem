fc32ea1484d029d03b60aa5423096a217f32fb41
Ben Halpern <bendhalpern@gmail.com>
2024-05-01 14:19:16 +0000

    Add proper frontend for Algolia search

--- app/models/concerns/algolia_searchable/searchable_organization.rb
+++ app/models/concerns/algolia_searchable/searchable_organization.rb
@@ -6,8 +6,12 @@ module AlgoliaSearchable
       algoliasearch(**DEFAULT_ALGOLIA_SETTINGS) do
         attribute :name, :tag_line, :summary, :slug
         attribute :profile_image do
-          profile_image_90
+          { url: profile_image_90 }
         end
+
+        add_attribute(:timestamp) { created_at.to_i }
+        add_replica("Organization_timestamp_desc", per_environment: true) { customRanking ["desc(timestamp)"] }
+        add_replica("Organization_timestamp_asc", per_environment: true) { customRanking ["asc(timestamp)"] }
       end
     end
 
