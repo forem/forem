86ec9a952a30c22cac2962cb55b756aee66553ed
Ben Halpern <bendhalpern@gmail.com>
2024-06-05 08:40:01 -0400

    Update app/workers/emails/send_user_digest_worker.rb
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/workers/emails/send_user_digest_worker.rb
+++ app/workers/emails/send_user_digest_worker.rb
@@ -24,7 +24,7 @@ module Emails
         DigestMailer.with(user: user, articles: articles.to_a, billboards: [first_billboard, second_billboard])
           .digest_email.deliver_now
 
-        event_params = { user_id: user.id, context_type: "email", category: "impression"}
+        event_params = { user_id: user.id, context_type: "email", category: "impression" }
         BillboardEvent.create(event_params.merge(billboard_id: first_billboard.id)) if first_billboard.present?
         BillboardEvent.create(event_params.merge(billboard_id: second_billboard.id)) if second_billboard.present?
       rescue StandardError => e
