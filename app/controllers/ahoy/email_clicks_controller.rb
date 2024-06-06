23b7e18fefa77002a02e70c0f94f5b378d11ae5b
Ben Halpern <bendhalpern@gmail.com>
2024-06-06 09:50:32 -0400

    Update billboard data when click occurs for digest (#21125)

--- app/controllers/ahoy/email_clicks_controller.rb
+++ app/controllers/ahoy/email_clicks_controller.rb
@@ -38,8 +38,23 @@ module Ahoy
                             category: "click",
                             user_id: current_user&.id,
                             context_type: "email")
+      update_billboard_counts
     rescue StandardError => e
       Rails.logger.error "Error processing billboard click: #{e.message}"
+    end
+
+    def update_billboard_counts
+      billboard = Billboard.find_by(id: ahoy_params[:bb].to_i)
+      return unless billboard
+
+      num_impressions = billboard.billboard_events.impressions.sum(:counts_for)
+      num_clicks = billboard.billboard_events.clicks.sum(:counts_for)
+      rate = num_impressions.positive? ? (num_clicks.to_f / num_impressions) : 0
+      billboard.update_columns(
+        success_rate: rate,
+        clicks_count: num_clicks,
+        impressions_count: num_impressions,
+      )
     end
   end
 end
