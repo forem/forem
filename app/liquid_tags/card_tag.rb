54c974277e94443564d24fe0a94d6a1b0abb35cd
Ben Halpern <bendhalpern@gmail.com>
2024-06-12 18:28:14 +0000

    Add card embed

---
+++ app/liquid_tags/card_tag.rb
@@ -0,0 +1,20 @@
+class CardTag < Liquid::Block
+  include ActionView::Helpers::SanitizeHelper
+
+  PARTIAL = "liquids/card".freeze
+
+  def initialize(_tag_name, options, _parse_context)
+    super
+  end
+
+  def render(_context)
+    ApplicationController.render(
+      partial: PARTIAL,
+      locals: {
+        content: super
+      },
+    )
+  end
+end
+
+Liquid::Template.register_tag("card", CardTag)
