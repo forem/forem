b94ae048df32113a0ff4fa3a8f3f8a812fbd2251
Ben Halpern <bendhalpern@gmail.com>
2024-06-18 13:04:16 -0400

    Article collector fallback (#21148)
    * Article collector fallback
    
    * Fix @user issue
    
    * Anti-tags
    
    * Anti-tags

--- app/services/email_digest_article_collector.rb
+++ app/services/email_digest_article_collector.rb
@@ -59,6 +59,20 @@ class EmailDigestArticleCollector
                      .merge(Article.featured.or(Article.cached_tagged_with_any(tags)))
                  end
 
+      # Fallback if there are not enough articles
+      if articles.length < 3
+        articles = Article.select(:title, :description, :path, :cached_user, :cached_tag_list)
+          .published
+          .where("published_at > ?", cutoff_date)
+          .where(email_digest_eligible: true)
+          .not_authored_by(@user.id)
+          .order(order)
+          .limit(RESULTS_COUNT)
+        if @user.cached_antifollowed_tag_names.any?
+          articles = articles.not_cached_tagged_with_any(@user.cached_antifollowed_tag_names)
+        end
+      end
+
       # Pop second article to front if the first article is the same as the last email
       if articles.any? && last_email_includes_title_in_subject?(articles.first.title)
         articles = articles[1..] + [articles.first]
