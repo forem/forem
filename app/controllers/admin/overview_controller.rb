8678c07e154b47b6bfe8031fde643dd7535d425a
Peter Frank <peter.kim.frank@gmail.com>
2024-06-24 13:53:42 +0000

    Add 90-day option to /admin analytics dropdown

--- app/controllers/admin/overview_controller.rb
+++ app/controllers/admin/overview_controller.rb
@@ -3,7 +3,8 @@ module Admin
     layout "admin"
     def index
       @length = (params[:period] || 7).to_i
-      @labels = (1..@length).map { |n| n.days.ago.strftime("%b %d") }.reverse
+      @length = [7, 14, 30, 90].include?(@length) ? @length : 7
+      @labels = (@length.downto(1)).map { |n| n.days.ago.strftime("%b %d") }.reverse
       @analytics = Admin::ChartsData.new(@length).call
       @data_counts = Admin::DataCounts.call
     end
