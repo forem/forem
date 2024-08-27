31ce29157ac5aa06f87047de069f23cfb3919951
Ben Halpern <bendhalpern@gmail.com>
2024-08-27 17:10:30 +0000

    Add new Stripe code option

--- app/controllers/stripe_subscriptions_controller.rb
+++ app/controllers/stripe_subscriptions_controller.rb
@@ -3,16 +3,25 @@ class StripeSubscriptionsController < ApplicationController
 
   def new
     Stripe.api_key = Settings::General.stripe_api_key
+
+    item_code = if ENV.fetch("STRIPE_TAG_MODERATOR_ITEM_CODE", nil).present? && current_user&.tag_moderator?
+                  ENV.fetch("STRIPE_TAG_MODERATOR_ITEM_CODE", nil)
+                elsif params[:item].present? && params[:item] != ENV.fetch("STRIPE_TAG_MODERATOR_ITEM_CODE", nil)
+                  params[:item]
+                else
+                  ENV.fetch("STRIPE_BASE_ITEM_CODE", nil)
+                end
+
     session = Stripe::Checkout::Session.create(
       line_items: [
         {
-          price: params[:item] || ENV.fetch("STRIPE_BASE_ITEM_CODE", nil),
+          price: item_code,
           quantity: 1
         },
       ],
       mode: params[:mode] || "subscription",
       success_url: URL.url(ENV["SUBSCRIPTION_SUCCESS_URL"] || "/settings/billing"),
-      cancel_url: URL.url("/settings/billing"),
+      cancel_url: URL.url(ENV["SUBSCRIPTION_CANCEL_URL"] || "/settings/billing"),
       customer_email: current_user.email,
       metadata: {
         user_id: current_user.id
