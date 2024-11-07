6eb5ebd2ca75dc2507e6dcf60b4d9f35f6587011
Ben Halpern <bendhalpern@gmail.com>
2024-11-07 18:00:54 +0000

    Adjust for dynamic domains if multiple exist

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -8,13 +8,31 @@ expires_after = app_config_expires_after.positive? ? app_config_expires_after : 
 # See https://github.com/redis-store/redis-rails#session-storage for configuration options
 servers = ApplicationConfig["REDIS_SESSIONS_URL"] || ApplicationConfig["REDIS_URL"]
 
-# Domain property should only be set in production
+# Main domain
 domain = Rails.env.production? ? ApplicationConfig["APP_DOMAIN"] : nil
+
+# Domain property should only be set in production
+domain_lambda = lambda do |request|
+  if Rails.env.production?
+    # List of your secondary domains
+    secondary_domains = ApplicationConfig["SECONDARY_APP_DOMAINS"].to_s.split(",").map(&:strip)
+    if secondary_domains.include?(request.host)
+      # For secondary domains, set domain to nil (cookie for current domain)
+      nil
+    else
+      # For main domain, set to ApplicationConfig["APP_DOMAIN"]
+      ApplicationConfig["APP_DOMAIN"]
+    end
+  else
+    # In non-production environments, don't set the domain
+    nil
+  end
+end
 
 # Main session store
 Rails.application.config.session_store :redis_store,
                                        key: ApplicationConfig["SESSION_KEY"],
-                                       domain: domain,
+                                       domain: domain_lambda,
                                        servers: servers,
                                        expire_after: expires_after,
                                        secure: ApplicationConfig["FORCE_SSL_IN_RAILS"] == "true",
