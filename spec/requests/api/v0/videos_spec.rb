22d76d447dc3cf0c62d5ca8cbc715118c5fa9176
Ben Halpern <bendhalpern@gmail.com>
2024-11-12 17:29:01 +0000

    Fix some tests

--- spec/requests/api/v0/videos_spec.rb
+++ spec/requests/api/v0/videos_spec.rb
@@ -73,10 +73,12 @@ RSpec.describe "Api::V0::Videos" do
     end
 
     it "supports pagination" do
-      create_list(
-        :article, 3,
-        user: user, video: "https://example.com", video_thumbnail_url: "https://example.com", title: "video-#{rand(10_000)}"
-      )
+      3.times do
+        create(
+          :article, 3,
+          user: user, video: "https://example.com", video_thumbnail_url: "https://example.com", title: "video-#{rand(10_000)}"
+        )
+      end
 
       get api_videos_path, params: { page: 1, per_page: 2 }
       expect(response.parsed_body.length).to eq(2)
@@ -90,10 +92,12 @@ RSpec.describe "Api::V0::Videos" do
       allow(ApplicationConfig).to receive(:[]).with("APP_PROTOCOL").and_return("http://")
       allow(ApplicationConfig).to receive(:[]).with("API_PER_PAGE_MAX").and_return(2)
 
-      create_list(
-        :article, 3,
-        user: user, video: "https://example.com", video_thumbnail_url: "https://example.com", title: "video-#{rand(10_000)}"
-      )
+      3.times do
+        create(
+          :article, 3,
+          user: user, video: "https://example.com", video_thumbnail_url: "https://example.com", title: "video-#{rand(10_000)}"
+        )
+      end
 
       get api_tags_path, params: { per_page: 10 }
       expect(response.parsed_body.count).to eq(2)
