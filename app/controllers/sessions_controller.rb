662f26d567b192ac877288a91378cd9dc5b533a7
Ben Halpern <bendhalpern@gmail.com>
2025-01-06 22:00:24 +0000

    Fix sign in info

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < Devise::SessionsController
       # Fetch all session IDs for the current user
       user_session_ids = Redis.current.smembers("user:#{current_user.id}:sessions")
 
+      current_user.update_columns(current_sign_in_at: nil, current_sign_in_ip: nil)
       # Delete each session from Redis using the configured session key prefix
       session_key_prefix = ApplicationConfig["SESSION_KEY"]
       user_session_ids.each do |session_id|
