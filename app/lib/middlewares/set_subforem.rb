8fd6dfbc70563b9474a6e2014e093e16e3e618d5
Ben Halpern <bendhalpern@gmail.com>
2025-01-22 20:36:44 +0000

    Set subforem via middleware

--- app/lib/middlewares/set_subforem.rb
+++ app/lib/middlewares/set_subforem.rb
@@ -9,9 +9,9 @@ class SetSubforem
 
       # If you have a dev param or something similar,
       # you'd read request.params[:passed_domain] as well
-      domain = request.params['passed_domain'].presence || request.host
+      domain = request.params["passed_domain"].presence || request.host
 
-      RequestStore.store[:default_subforem_id]     = Subforem.cached_default_id
+      RequestStore.store[:default_subforem_id]    = Subforem.cached_default_id
       RequestStore.store[:subforem_id]            = Subforem.cached_id_by_domain(domain)
       RequestStore.store[:root_subforem_id]       = Subforem.cached_root_id
       RequestStore.store[:root_subforem_domain]   = Subforem.cached_root_domain
