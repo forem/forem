940158d75b9457aaa0816236280102a089f76515
Ben Halpern <bendhalpern@gmail.com>
2025-01-27 21:06:15 +0000

    Adjust quickie share form

--- spec/models/subforem_spec.rb
+++ spec/models/subforem_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Subforem, type: :model do
     expect(Rails.cache).to receive(:delete).with("settings/general-#{subforem.id}")
     expect(Rails.cache).to receive(:delete).with('cached_domains')
     expect(Rails.cache).to receive(:delete).with('subforem_id_to_domain_hash')
+    expect(Rails.cache).to receive(:delete).with('subforem_postable_array')
     expect(Rails.cache).to receive(:delete).with('subforem_root_id')
     expect(Rails.cache).to receive(:delete).with('subforem_default_domain')
     expect(Rails.cache).to receive(:delete).with('subforem_root_domain')
