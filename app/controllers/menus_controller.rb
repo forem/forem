295a007fed524e85da197f7ad828e924a75b707e
Ben Halpern <bendhalpern@gmail.com>
2025-02-07 18:40:36 +0000

    Menu tweaks

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,4 +1,12 @@
 class MenusController < ApplicationController
   before_action :authenticate_user!
-  def show;  end
+  def show
+  
+    @navigation_links = Rails.cache.fetch("navigation_links-#{user_signed_in?}-#{RequestStore.store[:subforem_id]}", expires_in: 15.minutes) do
+      {
+        default_nav_ids: NavigationLink.from_subforem.default_section.ordered.ids,
+        other_nav_ids: NavigationLink.from_subforem.other_section.ordered.ids,
+      }
+    end
+  end
 end
