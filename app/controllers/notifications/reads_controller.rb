b63760d49a2b997708bb614b3ff14ff4e506fc75
Ben Halpern <bendhalpern@gmail.com>
2025-02-10 13:39:11 -0500

    Adjust token-based auth for create action

--- app/controllers/notifications/reads_controller.rb
+++ app/controllers/notifications/reads_controller.rb
@@ -1,6 +1,7 @@
 module Notifications
   class ReadsController < ApplicationController
-    before_action :current_user_by_token, only: [:show]
+    before_action :current_user_by_token, only: [:create]
+    skip_before_action :verify_authenticity_token, if: :token_authenticated?
 
     def create
       render plain: "" && return unless current_user
