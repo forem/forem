7b630868dee75686808526f553b19aef7164e304
Ben Halpern <bendhalpern@gmail.com>
2025-03-21 15:03:56 +0000

    Google auth adjust

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -73,7 +73,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
         Honeybadger.notify("Auth payload", context: { auth_payload: auth_payload })
       end
 
-      if auth_payload["provider"].include?("google") && %w[navbar_basic profile].exlude?(cta_variant)
+      if auth_payload["provider"].include?("google") && %w[navbar_basic profile].exclude?(cta_variant)
         # Generate the token the app will use.
         # (Replace the following with your actual token generation logic.)
         token = generate_auth_token(@user)
