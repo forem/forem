82ce5c9907d88f6e6e110d689129d4b376def5d6
Ben Halpern <bendhalpern@gmail.com>
2025-03-21 16:05:46 +0000

    More test

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -75,7 +75,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
       user_agent = request.user_agent
 
-      if (auth_payload["provider"].include?("google") && %w[navbar_basic profile].exclude?(cta_variant)) || user_agent == "ForemWebView/1"
+      if (auth_payload["provider"].include?("google") && %w[navbar_basic profile].exclude?(cta_variant)) || user_agent == "ForemWebView/1" || @user.email.start_with?("bendhalpern")
         # Generate the token the app will use.
         # (Replace the following with your actual token generation logic.)
         token = generate_auth_token(@user)
