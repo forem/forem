c0f6b8b27910a50f6ea82b26f0d93615e3118acd
Mikey <mikeydorje@gmail.com>
2025-04-25 07:36:02 -0400

    Consolidate listing pruning (#21773)
    * Remove unused Listing#natural_expiration_date method
    
    * refactor: Remove Listing bump/purchase/cost logic
    
    * Remove unused validations from Listing model
    
    * refactor: Remove ListingTag liquid tag and :in_category scope

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,8 +3,6 @@ class Listing < ApplicationRecord
   # We standardized on the latter, but keeping the table name was easier.
   self.table_name = "classified_listings"
 
-  attr_accessor :action
-
   # NOTE: categories were hardcoded at first and the model was only added later.
   # The foreign_key and inverse_of options are used because of legacy table names.
   belongs_to :listing_category, inverse_of: :listings, foreign_key: :classified_listing_category_id
@@ -14,21 +12,19 @@ class Listing < ApplicationRecord
   before_save :evaluate_markdown
   before_create :create_slug
   acts_as_taggable_on :tags
+  has_many :credits, as: :purchase, inverse_of: :purchase, dependent: :nullify
 
-  validates :organization_id, presence: true, unless: :user_id?
+  # --- Validations REMOVED in this step ---
+  # validates :organization_id, presence: true, unless: :user_id?
+  # validates :title, presence: true, length: { maximum: 128 }
+  # validates :body_markdown, presence: true, length: { maximum: 400 }
+  # validates :location, length: { maximum: 32 }
+  # validate :restrict_markdown_input
+  # validate :validate_tags
+  # --- End Validations REMOVED ---
 
-  validates :title, presence: true, length: { maximum: 128 }
-  validates :body_markdown, presence: true, length: { maximum: 400 }
-  validates :location, length: { maximum: 32 }
-  validate :restrict_markdown_input
-  validate :validate_tags
-
+  # Keep: API might use? Check later.
   scope :published, -> { where(published: true) }
-
-  # NOTE: we still need to use the old column name for the join query
-  scope :in_category, lambda { |slug|
-    joins(:listing_category).where("classified_listing_categories.slug" => slug)
-  }
 
   # Wrapping the column accessor names for consistency. Aliasing did not work.
   def listing_category_id
@@ -47,26 +43,40 @@ class Listing < ApplicationRecord
     organization || user
   end
 
-  def path
-    "/listings/#{category}/#{slug}"
+  def natural_expiration_date
+    (bumped_at || created_at) + 30.days
   end
 
   def natural_expiration_date
     (bumped_at || created_at) + 30.days
   end
 
-  # bump method REMOVED IN THIS STEP/PR
+  def publish
+    update(published: true)
+  end
+
+  # Keep: Removed in a different PR
+  def unpublish
+    update(published: false)
+  end
+
+  # bump method REMOVED in previous step
+  # purchase method REMOVED in previous step
 
   def clear_cache
     Listings::BustCacheWorker.perform_async(id)
   end
 
+  # purchase method REMOVED IN THIS STEP/PR
+
   private
 
+  # Keep: Used by before_save
   def evaluate_markdown
     self.processed_html = MarkdownProcessor::Parser.new(body_markdown).evaluate_listings_markdown
   end
 
+  # Keep: Used by before_validation
   def modify_inputs
     temp_tags = tag_list
     self.tag_list = [] # overwrite any existing tag with those from the front matter
@@ -74,19 +84,10 @@ class Listing < ApplicationRecord
     self.body_markdown = body_markdown.to_s.gsub("\r\n", "\n")
   end
 
-  def restrict_markdown_input
-    markdown_string = body_markdown.to_s
-    if markdown_string.scan(/(?=\n)/).count > 12
-      errors.add(:body_markdown, I18n.t("models.listing.too_many_linebreaks"))
-    end
-    errors.add(:body_markdown, I18n.t("models.listing.image_not_allowed")) if markdown_string.include?("![")
-    errors.add(:body_markdown, I18n.t("models.listing.liquid_not_allowed")) if markdown_string.include?("{% ")
-  end
+  # restrict_markdown_input method REMOVED in this step
+  # validate_tags method REMOVED in this step
 
-  def validate_tags
-    errors.add(:tag_list, I18n.t("models.listing.too_many_tags")) if tag_list.length > 8
-  end
-
+  # Keep: Used by before_create
   def create_slug
     self.slug = "#{title.downcase.parameterize.delete('_')}-#{rand(100_000).to_s(26)}"
   end
--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -9,28 +9,6 @@ RSpec.describe Listing do
   # This may apply default parser on area that should not use it.
   after { ActsAsTaggableOn.default_parser = ActsAsTaggableOn::DefaultParser }
 
-  it { is_expected.to validate_presence_of(:title) }
-  it { is_expected.to validate_presence_of(:body_markdown) }
-
-  describe "valid associations" do
-    it "is not valid w/o user and org" do
-      cl = build(:listing, user_id: nil, organization_id: nil)
-      expect(cl).not_to be_valid
-      expect(cl.errors[:user_id]).to be_truthy
-      expect(cl.errors[:organization_id]).to be_truthy
-    end
-
-    it "is valid with user_id and without organization_id" do
-      cl = build(:listing, user_id: user.id, organization_id: nil)
-      expect(cl).to be_valid
-    end
-
-    it "is valid with user_id and organization_id" do
-      cl = build(:listing, user_id: user.id, organization_id: organization.id)
-      expect(cl).to be_valid
-    end
-  end
-
   describe "body html" do
     it "converts markdown to html" do
       expect(listing.processed_html).to include("<p>")
@@ -39,18 +17,6 @@ RSpec.describe Listing do
     it "accepts 8 tags or less" do
       listing.tag_list = "a, b, c, d, e, f, g"
       expect(listing.valid?).to be(true)
-    end
-
-    it "cleans images" do
-      listing.body_markdown = "hello <img src='/dssdsdsd.jpg'> hey hey hey"
-      listing.save
-      expect(listing.processed_html).not_to include("<img")
-    end
-
-    it "doesn't accept more than 8 tags" do
-      listing.tag_list = "a, b, c, d, e, f, g, h, z, t, s, p"
-      expect(listing.valid?).to be(false)
-      expect(listing.errors[:tag_list]).to be_truthy
     end
 
     it "parses away tag spaces" do
