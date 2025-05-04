3d9d3959caed6e1158f2d4903c348bb482c5e888
Mikey Dorje <mikeydorje@gmail.com>
2025-05-04 12:55:08 -0400

    Remove Listing/ListingCategory models and all related code

--- app/controllers/concerns/api/listings_controller.rb
+++ app/controllers/concerns/api/listings_controller.rb
@@ -1,22 +1,15 @@
 module Api
   module ListingsController
     extend ActiveSupport::Concern
-
     include Pundit::Authorization
 
-    ATTRIBUTES_FOR_SERIALIZATION = %i[
-      id user_id organization_id title slug body_markdown cached_tag_list
-      classified_listing_category_id processed_html published created_at
-    ].freeze
-    private_constant :ATTRIBUTES_FOR_SERIALIZATION
 
     def index
       render json: []
     end
 
     def show
-      @listing = Listing.select(ATTRIBUTES_FOR_SERIALIZATION).find(params[:id])
-      set_surrogate_key_header @listing.record_key if @listing
+      render json: {}
     end
 
     def create
@@ -32,9 +25,7 @@ module Api
     end
 
     private
-
     attr_accessor :user
-
     alias current_user user
   end
 end
