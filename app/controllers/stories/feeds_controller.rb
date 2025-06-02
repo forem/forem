7fd486243a396b536c43647f89e20cabc40e6202
Ben Halpern <bendhalpern@gmail.com>
2025-06-02 03:18:59 +0000

    Add logical timeout to relevant following feed

--- app/controllers/stories/feeds_controller.rb
+++ app/controllers/stories/feeds_controller.rb
@@ -134,24 +134,28 @@ module Stories
     end
 
     def relevant_following_feed
-      Article.where(
-          "user_id IN (
-            SELECT followable_id FROM follows
-            WHERE followable_type = 'User'
-              AND follower_type = 'User'
-              AND follower_id = :user_id
-          ) OR organization_id IN (
-            SELECT followable_id FROM follows
-            WHERE followable_type = 'Organization'
-              AND follower_type = 'User'
-              AND follower_id = :user_id
-          )",
-          user_id: current_user.id
-        ).published.from_subforem
-        .where("score > -10")
-        .order("hotness_score DESC")
-        .page(@page)
-        .per(25)
+      Timeout.timeout(2.9) do
+        Article.where(
+            "user_id IN (
+              SELECT followable_id FROM follows
+              WHERE followable_type = 'User'
+                AND follower_type = 'User'
+                AND follower_id = :user_id
+            ) OR organization_id IN (
+              SELECT followable_id FROM follows
+              WHERE followable_type = 'Organization'
+                AND follower_type = 'User'
+                AND follower_id = :user_id
+            )",
+            user_id: current_user.id
+          ).published.from_subforem
+          .where("score > -10")
+          .order("hotness_score DESC")
+          .page(@page)
+          .per(25)
+      end
+    rescue Timeout::Error
+      Article.none
     end
   end
 end
