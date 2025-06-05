8ab2dc5f77e28280f538378a2ed2544942eb9190
Ben Halpern <bendhalpern@gmail.com>
2025-06-05 23:01:03 +0000

    Fix after update

--- app/controllers/stories/feeds_controller.rb
+++ app/controllers/stories/feeds_controller.rb
@@ -116,7 +116,11 @@ module Stories
       activity = current_user.user_activity
       user_ids = activity&.alltime_users || current_user.cached_following_users_ids
       organization_ids = activity&.alltime_organizations || current_user.cached_following_organizations_ids
-      Article.published.from_subforem.where(user_id: user_ids).or(Article.where(organization_id: organization_ids))
+      left_scope  = Article.published.from_subforem.where(user_id: user_ids)
+      right_scope = Article.published.from_subforem.where(organization_id: organization_ids)      
+
+      @articles = left_scope
+        .or(right_scope)
         .where("score > -10")
         .order("published_at DESC")
         .page(@page)
@@ -127,10 +131,15 @@ module Stories
       activity = current_user.user_activity
       user_ids = activity&.alltime_users || current_user.cached_following_users_ids
       organization_ids = activity&.alltime_organizations || current_user.cached_following_organizations_ids
-      Article.published.from_subforem.where(user_id: user_ids).or(Article.where(organization_id: organization_ids))
+      left_scope  = Article.published.from_subforem.where(user_id: user_ids)
+      right_scope = Article.published.from_subforem.where(organization_id: organization_ids)
+      
+
+      @articles = left_scope
+        .or(right_scope)
         .where("score > -10")
         .order("hotness_score DESC")
-        .page(1)
+        .page(@page)
         .per(25)
     end
   end
