b44a7f245a56aeb722dd4f104c34e2eec7b44e23
Ben Halpern <bendhalpern@gmail.com>
2025-06-25 09:23:12 -0400

    Add composite canonical url index (#22067)
    * Add composite canonical url index
    
    * Add composite canonical url index

---
+++ db/migrate/20250625130739_add_composite_index_to_articles.rb
@@ -0,0 +1,9 @@
+class AddCompositeIndexToArticles < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+  def change
+    add_index :articles,
+              [:published, :canonical_url],
+              name: 'index_articles_on_published_and_canonical_url',
+              algorithm: :concurrently
+  end
+end
