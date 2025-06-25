21e613cdb8e4bc2b7e9dc58ddaf059deeab193c1
Ben Halpern <bendhalpern@gmail.com>
2025-06-25 09:41:49 -0400

    Add safety assured to composite index (#22068)

--- db/migrate/20250625130739_add_composite_index_to_articles.rb
+++ db/migrate/20250625130739_add_composite_index_to_articles.rb
@@ -1,9 +1,11 @@
 class AddCompositeIndexToArticles < ActiveRecord::Migration[7.0]
   disable_ddl_transaction!
   def change
-    add_index :articles,
-              [:published, :canonical_url],
-              name: 'index_articles_on_published_and_canonical_url',
-              algorithm: :concurrently
+    safety_assured do
+      add_index :articles,
+                [:published, :canonical_url],
+                name: 'index_articles_on_published_and_canonical_url',
+                algorithm: :concurrently
+    end
   end
 end
