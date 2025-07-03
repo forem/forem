401a33a59d628763daaaa952199a3a9f39215320
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2025-07-03 16:35:38 +0200

    add logger

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,9 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+
+# NOTE: Ruby 3.3.0 compatibility fix
+# In Ruby 3.3.0, the Logger class is not automatically loaded from the standard library.
+# This causes issues with ActiveSupport's logger_thread_safe_level.rb which tries to
+# use Logger::Severity.constants without requiring Logger first.
+require "logger"
