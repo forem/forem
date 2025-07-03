c43d98e3b62bcfa4a8c1432d6286c2627ea59be6
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2025-07-03 15:58:28 +0200

    small change

--- app/models/navigation_link.rb
+++ app/models/navigation_link.rb
@@ -9,6 +9,7 @@ class NavigationLink < ApplicationRecord
 
   validates :name, :url, :icon, presence: true
   validates :url, url: { schemes: %w[https http] }, uniqueness: { scope: :name }
+
   validates :icon, format: SVG_REGEXP
   validates :display_only_when_signed_in, inclusion: { in: [true, false] }
 
