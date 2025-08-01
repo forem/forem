e0893ecc336fad4df87a6d1e48051efe68b3854f
Ben Halpern <bendhalpern@gmail.com>
2025-08-01 18:26:40 +0000

    Tweak sidebar style slightly

--- app/javascript/packs/mainSidebar.js
+++ app/javascript/packs/mainSidebar.js
@@ -24,7 +24,7 @@ fetch(`/stories/feed/?page=1&type_of=discover&passed_domain=${rootDomain}`, {
     feedContainer.innerHTML = data.map((article) => `
       <a href="${article.url}" class="crayons-link crayons-link--contentful crayons-link--contentfulsidebar ${article.id === articleId ? 'active' : ''}" data-article-id="${article.id}">
       ${article.main_image ? `<img src="${article.main_image}" loading="lazy" alt="Cover Image" class="crayons-side-nav__item-cover" width="1000" height="${article.main_image_height}" style="aspect-ratio: 1000 / ${article.main_image_height}">` : ''}
-      <div class="flex items-center">
+      <div class="flex ${article.title.length < 40 ? 'items-center' : ''}">
         <img src="${article.subforem_logo}" alt="Logo" class="crayons-side-nav__item-icon">
         <span class="crayons-side-nav__item-text">${article.title}</span>
       </div>
