802c0809c1cd8b66d53eb83a79a795b5268708e0
Ben Halpern <bendhalpern@gmail.com>
2025-08-06 14:17:24 +0000

    Remove out-of-date tests

--- spec/models/poll_skip_spec.rb
+++ spec/models/poll_skip_spec.rb
@@ -5,16 +5,6 @@ RSpec.describe PollSkip do
   let(:user) { create(:user) }
   let(:poll) { create(:poll, article: article) }
 
-  context "when user has already voted (or skipped) on the poll" do
-    it "is invalid" do
-      vote = build(:poll_skip, user: user, poll: poll)
-      allow(vote.poll).to receive(:vote_previously_recorded_for?).with(user_id: user.id).and_return(true)
-
-      expect(vote).not_to be_valid
-      expect(vote.errors[:base]).to include("cannot vote more than once in one poll")
-    end
-  end
-
   context "when user has not voted nor skipped the poll" do
     it "is valid" do
       vote = build(:poll_skip, user: user, poll: poll)
--- spec/models/poll_vote_spec.rb
+++ spec/models/poll_vote_spec.rb
@@ -9,16 +9,6 @@ RSpec.describe PollVote do
     expect(described_class.new.valid?).to be(false)
   end
 
-  context "when user has already voted (or skipped) on the poll" do
-    it "is invalid" do
-      vote = build(:poll_vote, poll_option: poll.poll_options.last, user: user, poll: poll)
-      allow(vote.poll).to receive(:vote_previously_recorded_for?).with(user_id: user.id).and_return(true)
-
-      expect(vote).not_to be_valid
-      expect(vote.errors[:base]).to include("cannot vote more than once in one poll")
-    end
-  end
-
   context "when user has not voted nor skipped the poll" do
     it "is valid" do
       vote = build(:poll_vote, poll_option: poll.poll_options.last, user: user, poll: poll)
