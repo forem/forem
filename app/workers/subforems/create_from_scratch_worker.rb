5f11318c06e574820f07c26f439c046df496ddb6
Ben Halpern <bendhalpern@gmail.com>
2025-08-19 19:45:54 +0000

    Add page and nav link functionality

--- app/workers/subforems/create_from_scratch_worker.rb
+++ app/workers/subforems/create_from_scratch_worker.rb
@@ -7,6 +7,9 @@ module Subforems
     def perform(subforem_id, brain_dump, name, logo_url, bg_image_url = nil)
       subforem = Subforem.find(subforem_id)
 
+      # Set admin_action_taken_at at start
+      Settings::General.admin_action_taken_at = Time.current
+
       # Set the community name
       Settings::Community.set_community_name(name, subforem_id: subforem.id)
 
@@ -16,6 +19,10 @@ module Subforems
       # Generate AI content
       Ai::CommunityCopy.new(subforem.id, brain_dump).write!
       Ai::ForemTags.new(subforem.id, brain_dump).upsert!
+      Ai::AboutPageGenerator.new(subforem.id, brain_dump, name).generate!
+
+      # Set admin_action_taken_at at finish
+      Settings::General.admin_action_taken_at = Time.current
 
       Rails.logger.info("Successfully created subforem #{subforem.domain} with AI services")
     rescue StandardError => e
