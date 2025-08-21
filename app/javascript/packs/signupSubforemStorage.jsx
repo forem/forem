87d1a26a1bf898e10566c86786acc18522c3d551
Ben Halpern <bendhalpern@gmail.com>
2025-08-21 15:12:19 +0000

    Revert "Add subforems to flow if signing up via root (#22222)"
    This reverts commit 5df86d79c56f04dc3c03082295816b38bab89b97.

--- app/javascript/packs/signupSubforemStorage.jsx
+++
@@ -1,9 +0,0 @@
-// Store subforem ID in localStorage when user visits signup page
-document.addEventListener('DOMContentLoaded', () => {
-  const urlParams = new URLSearchParams(window.location.search);
-  const signupSubforem = urlParams.get('signup_subforem');
-  
-  if (signupSubforem) {
-    localStorage.setItem('signup_subforem_id', signupSubforem);
-  }
-});
