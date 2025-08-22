76ffc8b3b0a63416a1c0a6b8f711254be34275db
Ben Halpern <bendhalpern@gmail.com>
2025-08-22 13:43:27 -0400

    Optimize /mod (#22234)
    * Optimize /mod
    
    * Clean up tests

---
+++ db/migrate/20250821230000_add_moderation_indexes_to_articles.rb
@@ -0,0 +1,32 @@
+class AddModerationIndexesToArticles < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+
+  def change
+    safety_assured do
+      # Composite index for the main moderation query pattern
+      # This covers: published, score range, published_at ordering
+      add_index :articles,
+                [:published, :score, :published_at],
+                name: 'index_articles_on_published_score_published_at_for_moderation',
+                algorithm: :concurrently
+
+      # Index for nth_published_by_author filtering
+      add_index :articles,
+                [:published, :nth_published_by_author],
+                name: 'index_articles_on_published_nth_published_by_author',
+                algorithm: :concurrently
+
+      # Composite index for subforem + published + score queries
+      add_index :articles,
+                [:subforem_id, :published, :score, :published_at],
+                name: 'index_articles_on_subforem_published_score_published_at',
+                algorithm: :concurrently
+
+      # Index for reactions queries used in moderation
+      add_index :reactions,
+                [:reactable_id, :reactable_type, :user_id],
+                name: 'index_reactions_on_reactable_and_user_for_moderation',
+                algorithm: :concurrently
+    end
+  end
+end
