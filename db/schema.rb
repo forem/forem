1df51cc0d140c16d4c616708d5fbd79eec4c969e
Ben Halpern <bendhalpern@gmail.com>
2025-08-26 13:56:40 -0400

    Fix schema ordering (#22249)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_08_21_230004) do
+ActiveRecord::Schema[7.0].define(version: 2025_08_21_230020) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "ltree"
