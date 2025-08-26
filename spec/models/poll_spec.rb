bbff3e160cd1b139b96f02e7c750711eccafe94b
Ben Halpern <bendhalpern@gmail.com>
2025-08-26 10:17:49 -0400

    Modify surveys to specify position and add supplementary text (#22245)

--- spec/models/poll_spec.rb
+++ spec/models/poll_spec.rb
@@ -43,6 +43,32 @@ RSpec.describe Poll, type: :model do
       expect(poll.poll_options.count).to eq(3)
       expect(poll.poll_options.pluck(:markdown)).to eq(poll_options_input_array)
     end
+
+    it "creates poll options with supplementary text after create" do
+      poll_options_input_array = ["Option 1", "Option 2", "Option 3"]
+      poll_options_supplementary_text_array = ["Desc 1", "Desc 2", "Desc 3"]
+      poll = build(:poll, 
+        poll_options_input_array: poll_options_input_array,
+        poll_options_supplementary_text_array: poll_options_supplementary_text_array
+      )
+      poll.save!
+      expect(poll.poll_options.count).to eq(3)
+      expect(poll.poll_options.pluck(:markdown)).to eq(poll_options_input_array)
+      expect(poll.poll_options.pluck(:supplementary_text)).to eq(poll_options_supplementary_text_array)
+    end
+
+    it "sets default position for polls in surveys" do
+      survey = create(:survey)
+      poll1 = create(:poll, survey: survey, position: nil)
+      poll2 = create(:poll, survey: survey, position: nil)
+      
+      # Reload to get the actual positions from the database
+      poll1.reload
+      poll2.reload
+      
+      expect(poll1.position).to eq(0)
+      expect(poll2.position).to eq(1)
+    end
   end
 
   describe "#vote_previously_recorded_for?" do
@@ -84,6 +110,8 @@ RSpec.describe Poll, type: :model do
       create(:poll_vote, poll: poll, poll_option: poll_option1)
       create(:poll_vote, poll: poll, poll_option: poll_option1)
       create(:poll_vote, poll: poll, poll_option: poll_option2)
+      # Update the poll's vote count to match the created votes
+      poll.update_column(:poll_votes_count, 3)
     end
 
     it "returns correct voting data" do
@@ -151,6 +179,35 @@ RSpec.describe Poll, type: :model do
         scale_poll = create(:poll, :scale)
         expect(scale_poll.scale_range).to eq([1, 2, 3, 4, 5])
       end
+    end
+  end
+
+  describe "#move_to_position" do
+    let(:survey) { create(:survey) }
+    let!(:poll1) { create(:poll, survey: survey, position: 0) }
+    let!(:poll2) { create(:poll, survey: survey, position: 1) }
+    let!(:poll3) { create(:poll, survey: survey, position: 2) }
+
+    it "moves poll to new position and bumps others" do
+      poll1.move_to_position(2)
+      
+      expect(poll1.reload.position).to eq(2)
+      expect(poll2.reload.position).to eq(0)
+      expect(poll3.reload.position).to eq(1)
+    end
+
+    it "does nothing when moving to same position" do
+      original_positions = [poll1.position, poll2.position, poll3.position]
+      poll1.move_to_position(0)
+      
+      expect(poll1.reload.position).to eq(0)
+      expect(poll2.reload.position).to eq(1)
+      expect(poll3.reload.position).to eq(2)
+    end
+
+    it "returns early when poll has no survey" do
+      poll_without_survey = create(:poll, survey: nil)
+      expect { poll_without_survey.move_to_position(1) }.not_to change { poll_without_survey.reload.position }
     end
   end
 end
