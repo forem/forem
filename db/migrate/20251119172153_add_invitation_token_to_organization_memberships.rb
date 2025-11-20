2f934e304009cc3de3cbc275e946801add75f987
Ben Halpern <bendhalpern@gmail.com>
2025-11-20 12:32:18 -0500

    Fix migration timeout issue (#22617)

--- db/migrate/20251119172153_add_invitation_token_to_organization_memberships.rb
+++ db/migrate/20251119172153_add_invitation_token_to_organization_memberships.rb
@@ -1,7 +1,11 @@
 class AddInvitationTokenToOrganizationMemberships < ActiveRecord::Migration[7.0]
   disable_ddl_transaction!
+
   def change
-    add_column :organization_memberships, :invitation_token, :string
-    add_index :organization_memberships, :invitation_token, unique: true, algorithm: :concurrently
+    add_column :organization_memberships, :invitation_token, :string, if_not_exists: true
+
+    disable_statement_timeout do
+      add_index :organization_memberships, :invitation_token, unique: true, algorithm: :concurrently, if_not_exists: true
+    end
   end
 end
