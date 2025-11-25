1ee0c32897c3b3b7757ba50f39006e7c04dcf6f1
Ben Halpern <bendhalpern@gmail.com>
2025-11-25 13:09:42 -0500

    Adjust migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_11_25_160000) do
+ActiveRecord::Schema[7.0].define(version: 2025_11_25_160001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "ltree"
