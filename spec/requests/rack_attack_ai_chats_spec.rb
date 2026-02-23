8a9569b3f36835363bf68dbdf026d093de7a6737
Ben Halpern <bendhalpern@gmail.com>
2026-02-23 14:19:48 -0500

    Editor help chat

---
+++ spec/requests/rack_attack_ai_chats_spec.rb
@@ -0,0 +1,52 @@
+require "rails_helper"
+
+RSpec.describe "Rack::Attack AI Chats Throttling", type: :request do
+  include ActiveSupport::Testing::TimeHelpers
+
+  before do
+    # Enable Rack::Attack for this spec
+    Rack::Attack.enabled = true
+    Rack::Attack.reset!
+    Rails.cache.clear
+  end
+
+  after do
+    Rack::Attack.enabled = false
+  end
+
+  describe "ai_chats_throttle" do
+    let(:user) { create(:user) }
+    let(:headers) { { "REMOTE_ADDR" => "1.2.3.4" } }
+
+    before do
+      sign_in user
+      stub_const("AI_AVAILABLE", true)
+    end
+
+    it "throttles POST requests to /ai_chats to 5 per minute per IP" do
+      # Make 5 successful requests
+      5.times do
+        post "/ai_chats", params: { message: "hello", chat_context: "editor" }, headers: headers
+        expect(response).not_to have_http_status(:too_many_requests)
+      end
+
+      # The 6th request should be throttled
+      post "/ai_chats", params: { message: "hello", chat_context: "editor" }, headers: headers
+      expect(response).to have_http_status(:too_many_requests)
+      expect(response.body).to include("Retry later")
+
+      # Advance time to allow requests again
+      travel 1.minute + 1.second do
+        post "/ai_chats", params: { message: "hello", chat_context: "editor" }, headers: headers
+        expect(response).not_to have_http_status(:too_many_requests)
+      end
+    end
+
+    it "does not throttle GET requests to /ai_chats" do
+      10.times do
+        get "/ai_chats", headers: headers
+        expect(response).not_to have_http_status(:too_many_requests)
+      end
+    end
+  end
+end
