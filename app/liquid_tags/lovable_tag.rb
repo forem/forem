cc83d1b33c872383b4352064f99b8fbd3d88c018
Jonathan Gottfried <jonmarkgo@gmail.com>
2026-02-24 11:35:49 -0500

    feat: add Lovable app embed liquid tag

---
+++ app/liquid_tags/lovable_tag.rb
@@ -0,0 +1,30 @@
+class LovableTag < LiquidTagBase
+  PARTIAL = "liquids/lovable".freeze
+  REGISTRY_REGEXP = %r{\Ahttps://[\w-]+\.lovable\.app(?:/[\w.-]*)*/?\z}
+  VALID_URL_REGEXP = %r{\Ahttps://([\w-]+\.lovable\.app(?:/[\w.-]*)*)/?\z}
+
+  def initialize(_tag_name, input, _parse_context)
+    super
+    @url = parse_input(strip_tags(input))
+  end
+
+  def render(_context)
+    ApplicationController.render(
+      partial: PARTIAL,
+      locals: { url: @url },
+    )
+  end
+
+  private
+
+  def parse_input(input)
+    stripped = input.strip
+    match = stripped.match(VALID_URL_REGEXP)
+    raise StandardError, I18n.t("liquid_tags.lovable_tag.invalid_url", default: "Invalid Lovable URL") unless match
+
+    "https://#{match[1].chomp('/')}"
+  end
+end
+
+Liquid::Template.register_tag("lovable", LovableTag)
+UnifiedEmbed.register(LovableTag, regexp: LovableTag::REGISTRY_REGEXP)
