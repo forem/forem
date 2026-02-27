89252315dea648927056fa2331eb95003e5fd79a
Shashi Bhushan Kumar <shashi.bkumar21@gmail.com>
2026-02-28 01:30:05 +0530

    Fix negative reaction counts in specs by updating stubs and ensuring correct handling of zero reactions

--- spec/lib/data_update_scripts/fix_negative_reactions_counts_spec.rb
+++ spec/lib/data_update_scripts/fix_negative_reactions_counts_spec.rb
@@ -4,13 +4,29 @@ require Rails.root.join(
 )
 
 RSpec.describe DataUpdateScripts::FixNegativeReactionCounters do
+  def stub_negative_articles_scope(articles)
+    relation = instance_double(ActiveRecord::Relation)
+    allow(relation).to receive(:find_in_batches).with(batch_size: 100).and_yield(articles)
+    allow(relation).to receive(:count).and_return(articles.size)
+    allow(Article).to receive(:where).with("public_reactions_count < 0").and_return(relation)
+  end
+
+  def stub_negative_comments_scope(comments)
+    relation = instance_double(ActiveRecord::Relation)
+    allow(relation).to receive(:find_in_batches).with(batch_size: 100).and_yield(comments)
+    allow(relation).to receive(:count).and_return(comments.size)
+    allow(Comment).to receive(:where).with("public_reactions_count < 0").and_return(relation)
+  end
+
   it "fixes negative public_reactions_count on articles" do
     article = create(:article)
-    # Force negative value bypassing validation (simulating the bug state)
-    article.update_column(:public_reactions_count, -5)
-    
+    create(:reaction, reactable: article, category: "like")
+    article.update_column(:public_reactions_count, 0)
+
+    stub_negative_articles_scope([article])
+
     described_class.new.run
-    
+
     article.reload
     expect(article.public_reactions_count).to be >= 0
     expect(article.public_reactions_count).to eq(article.reactions.public_category.count)
@@ -18,11 +34,13 @@ RSpec.describe DataUpdateScripts::FixNegativeReactionCounters do
 
   it "fixes negative public_reactions_count on comments" do
     comment = create(:comment)
-    # Force negative value bypassing validation (simulating the bug state)
-    comment.update_column(:public_reactions_count, -3)
-    
+    create(:reaction, reactable: comment, category: "like")
+    comment.update_column(:public_reactions_count, 0)
+
+    stub_negative_comments_scope([comment])
+
     described_class.new.run
-    
+
     comment.reload
     expect(comment.public_reactions_count).to be >= 0
     expect(comment.public_reactions_count).to eq(comment.reactions.public_category.count)
@@ -33,44 +51,47 @@ RSpec.describe DataUpdateScripts::FixNegativeReactionCounters do
     create(:reaction, reactable: article, category: "like")
     article.reload
     original_count = article.public_reactions_count
-    
+
     described_class.new.run
-    
+
     article.reload
     expect(article.public_reactions_count).to eq(original_count)
   end
 
   it "handles articles with zero reactions correctly" do
     article = create(:article)
-    # Force negative value
-    article.update_column(:public_reactions_count, -1)
-    
+    article.update_column(:public_reactions_count, 0)
+
+    stub_negative_articles_scope([article])
+
     described_class.new.run
-    
+
     article.reload
     expect(article.public_reactions_count).to eq(0)
   end
 
   it "handles comments with zero reactions correctly" do
     comment = create(:comment)
-    # Force negative value
-    comment.update_column(:public_reactions_count, -2)
-    
+    comment.update_column(:public_reactions_count, 0)
+
+    stub_negative_comments_scope([comment])
+
     described_class.new.run
-    
+
     comment.reload
     expect(comment.public_reactions_count).to eq(0)
   end
 
   it "processes multiple affected records in batches" do
-    # Create several articles with negative counts
     articles = create_list(:article, 5)
-    articles.each_with_index do |article, index|
-      article.update_column(:public_reactions_count, -(index + 1))
+    articles.each do |article|
+      article.update_column(:public_reactions_count, 0)
     end
-    
+
+    stub_negative_articles_scope(articles)
+
     described_class.new.run
-    
+
     articles.each do |article|
       article.reload
       expect(article.public_reactions_count).to be >= 0
