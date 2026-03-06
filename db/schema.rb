a34c3676324073d03441052afd4df852aec29fc4
Shashi Bhushan Kumar <shashi.bkumar21@gmail.com>
2026-03-07 03:33:29 +0530

    Fix schema.rb formatting and version definition

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_03_06_200919)
+ActiveRecord::Schema[7.0].define(version: 2026_03_06_200919) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "ltree"
