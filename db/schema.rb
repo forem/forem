8f60445e2b50c0c579d4abff5048b7632436109f
Shashi Bhushan Kumar <shashi.bkumar21@gmail.com>
2026-03-07 03:02:31 +0530

    Update schema version to 2026_03_07_140000

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_03_05_140854) do
+ActiveRecord::Schema.define(version: 2026_03_07_140000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "ltree"
