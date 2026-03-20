e03c1c76ad9a6c006c3702d466bacabc30486035
Ben Halpern <ben@dev.to>
2026-03-20 11:50:00 -0400

    Modify concurrent index file to help ensure deployment

--- db/migrate/20260311000002_add_org_page_features_indexes.rb
+++ db/migrate/20260311000002_add_org_page_features_indexes.rb
@@ -1,7 +1,17 @@
 class AddOrgPageFeaturesIndexes < ActiveRecord::Migration[7.0]
   disable_ddl_transaction!
 
-  def change
+  def up
+    if index_exists?(:pages, :organization_id)
+      remove_index :pages, :organization_id, algorithm: :concurrently
+    end
+
     add_index :pages, :organization_id, algorithm: :concurrently
+  end
+
+  def down
+    if index_exists?(:pages, :organization_id)
+      remove_index :pages, :organization_id, algorithm: :concurrently
+    end
   end
 end
