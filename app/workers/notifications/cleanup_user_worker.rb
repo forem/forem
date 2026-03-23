e81d732c645e370fbffbfc82df7711eefc8b4abe
Ben Halpern <ben@dev.to>
2026-03-23 09:22:11 -0400

    Add sidekiq throttling

--- app/workers/notifications/cleanup_user_worker.rb
+++ app/workers/notifications/cleanup_user_worker.rb
@@ -3,6 +3,7 @@ module Notifications
     include Sidekiq::Job
 
     sidekiq_options queue: :low_priority, retry: 5
+    sidekiq_throttle(concurrency: { limit: 5 })
 
     def perform(user_id)
       Notification.fast_cleanup_older_than_150_for(user_id)
