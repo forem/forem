c8e9e328af9c24c7e9713467ec3cd576654ba4bb
Ben Halpern <ben@dev.to>
2026-03-24 12:53:15 -0400

    Ensure connection clear on Rails timeout

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -5,3 +5,12 @@ end
 
 Rack::Timeout.unregister_state_change_observer(:logger) if Rails.env.development?
 Rack::Timeout::Logger.disable
+
+if defined?(Rack::Timeout) && defined?(ActiveRecord::Base)
+  Rack::Timeout.register_state_change_observer(:clear_db_connections_on_timeout) do |env|
+    if env[Rack::Timeout::ENV_INFO_KEY].state == :timed_out
+      ActiveRecord::Base.connection_pool.active_connection?&.disconnect!
+      ActiveRecord::Base.clear_active_connections!
+    end
+  end
+end
