30bab6665d8e001be826fc11b92041d6c488e8ed
Ben Halpern <bendhalpern@gmail.com>
2026-06-11 09:47:47 -0400

    Enable remaining Rails 7.2 framework defaults for upgrade prep

--- config/initializers/new_framework_defaults_7_2.rb
+++ config/initializers/new_framework_defaults_7_2.rb
@@ -11,13 +11,13 @@
 
 # Controls whether Active Job's `#perform_later` and similar methods automatically defer
 # the job queuing to after the current Active Record transaction is committed.
-# Rails.application.config.active_job.enqueue_after_transaction_commit = :default
+Rails.application.config.active_job.enqueue_after_transaction_commit = :default
 
 # Adds image/webp to the list of content types Active Storage considers as an image.
 # Guarded with respond_to? since Active Storage is not loaded in Forem.
-# if Rails.application.config.respond_to?(:active_storage)
-#   Rails.application.config.active_storage.web_image_content_types = %w[image/png image/jpeg image/gif image/webp]
-# end
+if Rails.application.config.respond_to?(:active_storage)
+  Rails.application.config.active_storage.web_image_content_types = %w[image/png image/jpeg image/gif image/webp]
+end
 
 # Enable validation of migration timestamps. When set, an ActiveRecord::InvalidMigrationTimestampError
 # will be raised if the timestamp prefix for a migration is more than a day ahead of the timestamp
@@ -30,6 +30,6 @@ Rails.application.config.active_record.postgresql_adapter_decode_dates = true
 # Enables YJIT as of Ruby 3.3, to bring sizeable performance improvements. If you are
 # deploying to a memory constrained environment you may want to set this to `false`.
 # Guarded because config.yjit is only introduced in Rails 7.2.
-# if Rails.application.config.respond_to?(:yjit)
-#   Rails.application.config.yjit = true
-# end
+if Rails.application.config.respond_to?(:yjit)
+  Rails.application.config.yjit = true
+end
