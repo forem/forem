8deaa3c75d13e5e8ef4b81702e8ef74a955960cc
Francis Tran <xst-tran6832@stthomas.edu>
2026-06-15 12:56:08 -0500

    Update Test

--- spec/models/comment_counter_spec.rb
+++ spec/models/comment_counter_spec.rb
@@ -5,18 +5,32 @@ RSpec.describe Comment, type: :model do
   let(:article) { create(:article, user: user) }
   let(:comment) { create(:comment, user: user, commentable: article) }
 
-  describe "counter cache update on deletion" do
-    it "decrements the commentable comments_count when a childless comment is destroyed" do
+  describe "counter cache behavior" do
+    it "decrements the commentable comments_count when a comment is destroyed" do
       expect {
         comment.destroy
       }.to change { article.reload.comments_count }.by(-1)
     end
 
-    it "does NOT decrement the commentable comments_count when a comment with children is marked as deleted" do
-      create(:comment, user: user, commentable: article, parent: comment)
-      
+    it "decrements when a comment is soft-deleted" do
       expect {
-        comment.destroy # This should trigger the 'else' block in CommentsController#destroy
+        comment.update!(deleted: true)
+      }.to change { article.reload.comments_count }.by(-1)
+    end
+
+    it "increments back when a comment is restored" do
+      comment.update!(deleted: true)
+
+      expect {
+        comment.update!(deleted: false)
+      }.to change { article.reload.comments_count }.by(1)
+    end
+
+    it "ignores already deleted comments when destroyed" do
+      comment.update!(deleted: true)
+
+      expect {
+        comment.destroy
       }.not_to change { article.reload.comments_count }
     end
   end
