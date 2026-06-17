0e6169ce4f594dc87937c0c79b5f8a6e59b854f5
Ben Halpern <bendhalpern@gmail.com>
2026-06-17 16:43:31 -0400

    Make AddIndexesToEventSignups migration idempotent
    This adds guards using index_exists? in both up and down directions to prevent PG::DuplicateTable errors during deployment if some indexes were already created.

--- db/migrate/20260611190731_add_indexes_to_event_signups.rb
+++ db/migrate/20260611190731_add_indexes_to_event_signups.rb
@@ -1,11 +1,19 @@
 class AddIndexesToEventSignups < ActiveRecord::Migration[7.1]
   disable_ddl_transaction!
 
-  def change
-    add_index :event_signups, :user_id, algorithm: :concurrently
-    add_index :event_signups, :event_id, algorithm: :concurrently
-    add_index :event_signups, [:user_id, :event_id], unique: true, algorithm: :concurrently
-    add_index :event_signups, :notified_1_day_before, algorithm: :concurrently
-    add_index :event_signups, :notified_1_hour_before, algorithm: :concurrently
+  def up
+    add_index :event_signups, :user_id, algorithm: :concurrently unless index_exists?(:event_signups, :user_id)
+    add_index :event_signups, :event_id, algorithm: :concurrently unless index_exists?(:event_signups, :event_id)
+    add_index :event_signups, [:user_id, :event_id], unique: true, algorithm: :concurrently unless index_exists?(:event_signups, [:user_id, :event_id])
+    add_index :event_signups, :notified_1_day_before, algorithm: :concurrently unless index_exists?(:event_signups, :notified_1_day_before)
+    add_index :event_signups, :notified_1_hour_before, algorithm: :concurrently unless index_exists?(:event_signups, :notified_1_hour_before)
+  end
+
+  def down
+    remove_index :event_signups, :user_id, algorithm: :concurrently if index_exists?(:event_signups, :user_id)
+    remove_index :event_signups, :event_id, algorithm: :concurrently if index_exists?(:event_signups, :event_id)
+    remove_index :event_signups, [:user_id, :event_id], algorithm: :concurrently if index_exists?(:event_signups, [:user_id, :event_id])
+    remove_index :event_signups, :notified_1_day_before, algorithm: :concurrently if index_exists?(:event_signups, :notified_1_day_before)
+    remove_index :event_signups, :notified_1_hour_before, algorithm: :concurrently if index_exists?(:event_signups, :notified_1_hour_before)
   end
 end
