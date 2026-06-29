9af506fe83ebe05a33d5b23451270a18966585c3
Ben Halpern <bendhalpern@gmail.com>
2026-06-29 15:36:22 -0700

    Optimize leaderboard performance with concurrent index and caching

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -4,14 +4,18 @@ class LeaderboardsController < ApplicationController
   def index
     set_cache_control_headers(60)
     exclude_ids = [Settings::General.mascot_user_id, Settings::Community.staff_user_id].compact
-    query = User.registered
-                .where("score >= 0")
-                .where("badge_achievements_count > 0")
+    cache_key = ["leaderboard/users-v2", exclude_ids.sort].join("/")
+    @users = Rails.cache.fetch(cache_key, expires_in: 10.minutes) do
+      query = User.registered
+                  .where("score >= 0")
+                  .where("badge_achievements_count > 0")
 
-    query = query.where.not(id: exclude_ids) if exclude_ids.any?
+      query = query.where.not(id: exclude_ids) if exclude_ids.any?
 
-    @users = query.order(badge_achievements_count: :desc)
-                  .limit(100)
-                  .includes(badge_achievements: :badge)
+      query.order(badge_achievements_count: :desc)
+            .limit(100)
+            .includes(badge_achievements: :badge)
+            .to_a
+    end
   end
 end
