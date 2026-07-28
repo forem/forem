312d4e5becfb603057eae2ce3f869807aed678d2
Peter Frank <peter.kim.frank@gmail.com>
2026-07-28 14:35:05 -0400

    Report successful RSpec retries

--- spec/support/initializers/ci_csv_formatter.rb
+++ spec/support/initializers/ci_csv_formatter.rb
@@ -3,11 +3,10 @@ return unless ENV["CI"]
 require "csv"
 require "rspec/retry"
 require "singleton"
+require_relative "../rspec_retry_csv_report"
 
 class CSVFormatter
   RSpec::Core::Formatters.register self, :example_passed, :example_failed, :example_pending, :close
-  HEADERS = ["Description", "File", "Status", "Start Date", "Start Time", "Run Time", "Exception",
-             "Backtrace", "Retry #", "Suite Status", "Suite Run Time", "Travis URL", "Travis Branch"].freeze
 
   def initialize(_output)
     @rows = []
@@ -63,13 +62,16 @@ class CSVFormatter
 
     suite_runtime = (Time.zone.now - @suite_start_time).round(3)
 
-    with_headers = { write_headers: true, headers: HEADERS }
+    retry_rows = RSpecRetryFormatterHelper.instance.rows
+    with_headers = { write_headers: true, headers: RSpecRetryCsvReport::HEADERS }
     CSV.open(csv_filename, "w", **with_headers) do |csv|
-      (@rows + RSpecRetryFormatterHelper.instance.rows).each do |row|
+      (@rows + retry_rows).each do |row|
         row += [@suite_status, suite_runtime, ENV.fetch("TRAVIS_BUILD_WEB_URL", nil), ENV.fetch("TRAVIS_BRANCH", nil)]
         csv << row
       end
     end
+
+    RSpecRetryCsvReport.write(rows: retry_rows, suite_status: @suite_status, suite_runtime: suite_runtime)
   end
 end
 
