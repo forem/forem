af3e66f01aef19b0e0ea949a32120c638e829f8f
Ben Halpern <bendhalpern@gmail.com>
2026-08-12 10:33:59 -0400

    Fix release failure during concurrent index creation on articles and comments

--- db/migrate/20260728100100_add_index_to_articles_favorited_by_user_id.rb
+++ db/migrate/20260728100100_add_index_to_articles_favorited_by_user_id.rb
@@ -2,14 +2,25 @@ class AddIndexToArticlesFavoritedByUserId < ActiveRecord::Migration[8.0]
   disable_ddl_transaction!
 
   def up
-    return if index_exists?(:articles, :favorited_by_user_id)
+    safety_assured do
+      db_user = connection.query_value("SELECT current_user")
+      begin
+        execute "ALTER ROLE \"#{db_user}\" SET statement_timeout = 0;"
+        execute "SET statement_timeout = 0;"
 
-    add_index :articles, :favorited_by_user_id, algorithm: :concurrently
+        remove_index :articles, name: "index_articles_on_favorited_by_user_id", if_exists: true,
+                                algorithm: :concurrently
+
+        add_index :articles, :favorited_by_user_id, algorithm: :concurrently
+      ensure
+        execute "ALTER ROLE \"#{db_user}\" RESET statement_timeout;"
+      end
+    end
   end
 
   def down
-    return unless index_exists?(:articles, :favorited_by_user_id)
-
-    remove_index :articles, :favorited_by_user_id, algorithm: :concurrently
+    safety_assured do
+      remove_index :articles, name: "index_articles_on_favorited_by_user_id", if_exists: true, algorithm: :concurrently
+    end
   end
 end
