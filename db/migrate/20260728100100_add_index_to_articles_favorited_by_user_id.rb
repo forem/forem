3fc949719d86f9c6c6c3e549246cfcaad0b7903a
Ben Halpern <bendhalpern@gmail.com>
2026-08-12 11:22:48 -0400

    Switch favorited_by_user_id index migrations to standard DDL index creation

--- db/migrate/20260728100100_add_index_to_articles_favorited_by_user_id.rb
+++ db/migrate/20260728100100_add_index_to_articles_favorited_by_user_id.rb
@@ -1,26 +1,14 @@
 class AddIndexToArticlesFavoritedByUserId < ActiveRecord::Migration[8.0]
-  disable_ddl_transaction!
-
   def up
     safety_assured do
-      db_user = connection.query_value("SELECT current_user")
-      begin
-        execute "ALTER ROLE \"#{db_user}\" SET statement_timeout = 0;"
-        execute "SET statement_timeout = 0;"
-
-        remove_index :articles, name: "index_articles_on_favorited_by_user_id", if_exists: true,
-                                algorithm: :concurrently
-
-        add_index :articles, :favorited_by_user_id, algorithm: :concurrently
-      ensure
-        execute "ALTER ROLE \"#{db_user}\" RESET statement_timeout;"
-      end
+      remove_index :articles, name: "index_articles_on_favorited_by_user_id", if_exists: true
+      add_index :articles, :favorited_by_user_id, if_not_exists: true
     end
   end
 
   def down
     safety_assured do
-      remove_index :articles, name: "index_articles_on_favorited_by_user_id", if_exists: true, algorithm: :concurrently
+      remove_index :articles, name: "index_articles_on_favorited_by_user_id", if_exists: true
     end
   end
 end
