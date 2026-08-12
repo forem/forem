980415abcdd51ed136a63481187ac431bb415165
Ben Halpern <bendhalpern@gmail.com>
2026-08-12 12:30:47 -0400

    Use concurrent index creation with SET statement_timeout = 0 (#23715)

--- db/migrate/20260728100400_add_index_to_comments_favorited_by_user_id.rb
+++ db/migrate/20260728100400_add_index_to_comments_favorited_by_user_id.rb
@@ -1,14 +1,18 @@
 class AddIndexToCommentsFavoritedByUserId < ActiveRecord::Migration[8.0]
+  disable_ddl_transaction!
+
   def up
     safety_assured do
-      remove_index :comments, name: "index_comments_on_favorited_by_user_id", if_exists: true
-      add_index :comments, :favorited_by_user_id, if_not_exists: true
+      execute "SET statement_timeout = 0;"
+      remove_index :comments, name: "index_comments_on_favorited_by_user_id", if_exists: true, algorithm: :concurrently
+      add_index :comments, :favorited_by_user_id, algorithm: :concurrently
     end
   end
 
   def down
     safety_assured do
-      remove_index :comments, name: "index_comments_on_favorited_by_user_id", if_exists: true
+      execute "SET statement_timeout = 0;"
+      remove_index :comments, name: "index_comments_on_favorited_by_user_id", if_exists: true, algorithm: :concurrently
     end
   end
 end
