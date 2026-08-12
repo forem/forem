4381fcfb895a2e7a98147f048fb4bdec15c2d252
Ben Halpern <bendhalpern@gmail.com>
2026-08-12 12:29:58 -0400

    Use concurrent index creation with SET statement_timeout = 0

--- db/migrate/20260728100100_add_index_to_articles_favorited_by_user_id.rb
+++ db/migrate/20260728100100_add_index_to_articles_favorited_by_user_id.rb
@@ -1,14 +1,18 @@
 class AddIndexToArticlesFavoritedByUserId < ActiveRecord::Migration[8.0]
+  disable_ddl_transaction!
+
   def up
     safety_assured do
-      remove_index :articles, name: "index_articles_on_favorited_by_user_id", if_exists: true
-      add_index :articles, :favorited_by_user_id, if_not_exists: true
+      execute "SET statement_timeout = 0;"
+      remove_index :articles, name: "index_articles_on_favorited_by_user_id", if_exists: true, algorithm: :concurrently
+      add_index :articles, :favorited_by_user_id, algorithm: :concurrently
     end
   end
 
   def down
     safety_assured do
-      remove_index :articles, name: "index_articles_on_favorited_by_user_id", if_exists: true
+      execute "SET statement_timeout = 0;"
+      remove_index :articles, name: "index_articles_on_favorited_by_user_id", if_exists: true, algorithm: :concurrently
     end
   end
 end
