3f129d321e1435120aee58b3e553c3917188d24c
Ben Halpern <bendhalpern@gmail.com>
2026-08-17 17:05:51 -0400

    Execute concurrent index creation directly with Migration 7.0 and zero timeout

--- db/migrate/20260814130100_add_index_to_articles_and_comments_ai_disclosure_level.rb
+++ db/migrate/20260814130100_add_index_to_articles_and_comments_ai_disclosure_level.rb
@@ -1,27 +1,32 @@
-class AddIndexToArticlesAndCommentsAiDisclosureLevel < ActiveRecord::Migration[8.0]
+class AddIndexToArticlesAndCommentsAiDisclosureLevel < ActiveRecord::Migration[7.0]
   disable_ddl_transaction!
 
   def up
-    # Drop leftover invalid indexes if a previous attempt was interrupted
-    if connection.select_value("SELECT 1 FROM pg_index i JOIN pg_class c ON c.oid = i.indexrelid WHERE c.relname = 'index_articles_on_ai_disclosure_level' AND NOT i.indisvalid")
-      connection.execute "SET statement_timeout = 0;"
-      connection.execute "DROP INDEX CONCURRENTLY IF EXISTS index_articles_on_ai_disclosure_level;"
+    safety_assured do
+      # Drop leftover invalid indexes if a previous attempt was interrupted
+      if select_value("SELECT 1 FROM pg_index i JOIN pg_class c ON c.oid = i.indexrelid WHERE c.relname = 'index_articles_on_ai_disclosure_level' AND NOT i.indisvalid")
+        execute "SET statement_timeout = 0;"
+        execute "DROP INDEX CONCURRENTLY IF EXISTS index_articles_on_ai_disclosure_level;"
+      end
+
+      if select_value("SELECT 1 FROM pg_index i JOIN pg_class c ON c.oid = i.indexrelid WHERE c.relname = 'index_comments_on_ai_disclosure_level' AND NOT i.indisvalid")
+        execute "SET statement_timeout = 0;"
+        execute "DROP INDEX CONCURRENTLY IF EXISTS index_comments_on_ai_disclosure_level;"
+      end
+
+      execute "SET statement_timeout = 0;"
+      execute "CREATE INDEX CONCURRENTLY IF NOT EXISTS index_articles_on_ai_disclosure_level ON articles (ai_disclosure_level);"
+
+      execute "SET statement_timeout = 0;"
+      execute "CREATE INDEX CONCURRENTLY IF NOT EXISTS index_comments_on_ai_disclosure_level ON comments (ai_disclosure_level);"
     end
-
-    if connection.select_value("SELECT 1 FROM pg_index i JOIN pg_class c ON c.oid = i.indexrelid WHERE c.relname = 'index_comments_on_ai_disclosure_level' AND NOT i.indisvalid")
-      connection.execute "SET statement_timeout = 0;"
-      connection.execute "DROP INDEX CONCURRENTLY IF EXISTS index_comments_on_ai_disclosure_level;"
-    end
-
-    connection.execute "SET statement_timeout = 0;"
-    add_index :articles, :ai_disclosure_level, algorithm: :concurrently unless index_exists?(:articles, :ai_disclosure_level)
-
-    connection.execute "SET statement_timeout = 0;"
-    add_index :comments, :ai_disclosure_level, algorithm: :concurrently unless index_exists?(:comments, :ai_disclosure_level)
   end
 
   def down
-    remove_index :articles, :ai_disclosure_level, algorithm: :concurrently if index_exists?(:articles, :ai_disclosure_level)
-    remove_index :comments, :ai_disclosure_level, algorithm: :concurrently if index_exists?(:comments, :ai_disclosure_level)
+    safety_assured do
+      execute "SET statement_timeout = 0;"
+      execute "DROP INDEX CONCURRENTLY IF EXISTS index_articles_on_ai_disclosure_level;"
+      execute "DROP INDEX CONCURRENTLY IF EXISTS index_comments_on_ai_disclosure_level;"
+    end
   end
 end
