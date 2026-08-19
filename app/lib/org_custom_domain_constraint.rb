a5ed1f00bf8f8ead61b8fcbab2e44c3640f79b73
Ben Halpern <bendhalpern@gmail.com>
2026-08-19 14:01:51 -0400

    Refactor OrgCustomDomainConstraint first segment matching and deduplicate reserved slug regex

--- app/lib/org_custom_domain_constraint.rb
+++ app/lib/org_custom_domain_constraint.rb
@@ -1,29 +1,31 @@
 class OrgCustomDomainConstraint
-  PLATFORM_PATH_PREFIXES = %w[
-    /async_info
-    /reactions
-    /badge_achievements
-    /billboard_events
-    /billboards
-    /bb
-    /display_ads
-    /poll_votes
-    /reading_list_items
-    /followed_articles
-    /feedback_messages
-    /feed_events
-    /auth_pass
-    /api
-    /ahoy
-    /assets
-    /packs
-    /rails
-  ].freeze
+  PLATFORM_FIRST_SEGMENTS = %w[
+    ahoy
+    api
+    assets
+    async_info
+    auth_pass
+    badge_achievements
+    bb
+    billboard_events
+    billboards
+    display_ads
+    feedback_messages
+    feed_events
+    followed_articles
+    packs
+    poll_votes
+    rails
+    reactions
+    reading_list_items
+  ].to_set.freeze
+
+  RESERVED_SLUG_REGEXP = %r{(?!(?:api|assets|packs|rails|r|ahoy|enter|users|p|robots|sitemap-.+|async_info|reactions|billboards|bb|display_ads|auth_pass|search|poll_votes|badge_achievements|billboard_events|reading_list_items|followed_articles|feedback_messages|feed_events)\z)[^/.]+}
 
   def self.custom_domain_org(request)
-    path = request.path.to_s
+    first_segment = request.path.to_s.split("/")[1]
 
-    if PLATFORM_PATH_PREFIXES.any? { |prefix| path.start_with?(prefix) } && request.params[:i] != "i"
+    if PLATFORM_FIRST_SEGMENTS.include?(first_segment) && request.params[:i] != "i"
       return nil
     end
 
