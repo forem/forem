4fa9455fc64467af273909db4c2e7460e0118ecc
Ben Halpern <bendhalpern@gmail.com>
2026-08-19 13:46:36 -0400

    Fix OrgCustomDomainConstraint to allow fetch/preload on article paths

--- app/lib/org_custom_domain_constraint.rb
+++ app/lib/org_custom_domain_constraint.rb
@@ -1,21 +1,29 @@
 class OrgCustomDomainConstraint
+  PLATFORM_PATH_PREFIXES = %w[
+    /async_info
+    /reactions
+    /badge_achievements
+    /billboard_events
+    /billboards
+    /bb
+    /display_ads
+    /poll_votes
+    /reading_list_items
+    /followed_articles
+    /feedback_messages
+    /feed_events
+    /auth_pass
+    /api
+    /ahoy
+    /assets
+    /packs
+    /rails
+  ].freeze
+
   def self.custom_domain_org(request)
-    is_ajax = request.respond_to?(:xhr?) && request.xhr?
-    is_json = request.path.to_s.end_with?(".json") || (request.respond_to?(:accept) && request.accept.to_s.include?("application/json"))
+    path = request.path.to_s
 
-    fetch_mode, fetch_dest =
-      if request.respond_to?(:get_header)
-        [request.get_header("HTTP_SEC_FETCH_MODE"), request.get_header("HTTP_SEC_FETCH_DEST")]
-      elsif request.respond_to?(:headers)
-        [request.headers["Sec-Fetch-Mode"], request.headers["Sec-Fetch-Dest"]]
-      else
-        [nil, nil]
-      end
-
-    is_fetch = fetch_mode == "cors" || fetch_dest == "empty"
-    is_async_path = request.path.to_s.start_with?("/async_info", "/reactions")
-
-    if (is_ajax || is_json || is_fetch || is_async_path) && request.params[:i] != "i"
+    if PLATFORM_PATH_PREFIXES.any? { |prefix| path.start_with?(prefix) } && request.params[:i] != "i"
       return nil
     end
 
