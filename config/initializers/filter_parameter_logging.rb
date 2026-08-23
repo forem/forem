feb6a3c543a6dcdbc535d165abaa1c2ec18f5656
jcsawyer123 <joshua@jcsawyer.me>
2026-08-23 23:43:18 +0100

    feat: account-switch interstitial and scrubbed callback ingress

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -2,5 +2,5 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += %i[
-  password passw secret token _key crypt salt certificate otp ssn
+  password passw secret token _key crypt salt certificate otp ssn continuation
 ]
